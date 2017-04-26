b0884b039fe7ed87a5270156ce9bafa613b57ffa
salimov.alyar <alyar666@users.noreply.github.com>
2017-04-26 20:56:40 +0300

    renaming 'dx-treelist-editor-cell' to 'dx-treelist-select-all'

--- js/ui/tree_list/ui.tree_list.selection.js
+++ js/ui/tree_list/ui.tree_list.selection.js
@@ -6,7 +6,7 @@ var $ = require("../../core/renderer"),
     selectionModule = require("../grid_core/ui.grid_core.selection"),
     extend = require("../../core/utils/extend").extend;
 
-var TREELIST_EDITOR_CELL_CLASS = "dx-treelist-editor-cell";
+var TREELIST_SELECT_ALL_CLASS = "dx-treelist-select-all";
 
 var originalRowClick = selectionModule.extenders.views.rowsView._rowClick;
 
@@ -60,7 +60,7 @@ treeListCore.registerModule("selection", extend(true, {}, selectionModule, {
                 },
 
                 renderSelectAll: function($cell, options) {
-                    $cell.addClass(TREELIST_EDITOR_CELL_CLASS);
+                    $cell.addClass(TREELIST_SELECT_ALL_CLASS);
 
                     var $checkbox = this._renderSelectAllCheckBox($cell);
                     this._attachSelectAllCheckBoxClickEvent($checkbox);
