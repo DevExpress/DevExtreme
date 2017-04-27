c90bc6b74d634da657382e5f3edf7191bc3e0ca7
vostrikova.elena <elena.vostrikova@devexpress.com>
2017-04-27 16:19:11 +0300

    Fix after review

--- testing/tests/DevExpress.ui.widgets/treeViewParts/focusing.js
+++ testing/tests/DevExpress.ui.widgets/treeViewParts/focusing.js
@@ -5,6 +5,9 @@
 var $ = require("jquery"),
     devices = require("core/devices");
 
+var NODE_CLASS = "dx-treeview-node",
+    TOGGLE_ITEM_VISIBILITY_CLASS = "dx-treeview-toggle-item-visibility";
+
 QUnit.module("Focusing");
 
 QUnit.testInActiveWindow("item should have focus-state class after click on it", function(assert) {
@@ -76,60 +79,46 @@ QUnit.test("focus on the item should move scroll position to this item (T226868)
 
 });
 
-QUnit.test("Checkbox should be clicked when treeview in not in focus", function(assert) {
+QUnit.test("PointerDown event at checkbox should not be ignored", function(assert) {
     var data = [
-        { id: 1, parentId: 0, text: "Cats" },
-        { id: 2, parentId: 0, text: "Dogs" },
-        { id: 3, parentId: 0, text: "Tigers" },
-        { id: 4, parentId: 0, text: "Birds" },
-        { id: 5, parentId: 0, text: "Hippos" },
-        { id: 6, parentId: 0, text: "Elefants" },
-        { id: 7, parentId: 0, text: "Cows" }
+        { id: 1, parentId: 0, text: "Cats" }
     ];
 
     var $treeView = initTree({
         dataSource: data,
         dataStructure: "plain",
-        showCheckBoxesMode: "normal",
-        height: 100
+        showCheckBoxesMode: "normal"
     });
 
     var treeView = $treeView.dxTreeView("instance");
     var pointerDownStub = sinon.stub(treeView, "_itemPointerDownHandler");
 
-    var $node = $treeView.find("." + internals.NODE_CLASS).eq(5),
+    var $node = $treeView.find("." + NODE_CLASS).eq(0),
         $checkBox = $node.find(".dx-checkbox");
 
     $checkBox.trigger("dxpointerdown");
 
-    assert.equal(pointerDownStub.callCount, 1, "checkBox should be part of node");
+    assert.equal(pointerDownStub.callCount, 1, "itemPointerDownHandler was called");
 });
 
-QUnit.test("Expansion should work when treeview in not in focus", function(assert) {
+QUnit.test("PointerDown event at expansion arrow should not be ignored", function(assert) {
     var data = [
         { id: 1, parentId: 0, text: "Cats" },
-        { id: 2, parentId: 0, text: "Dogs" },
-        { id: 3, parentId: 0, text: "Tigers" },
-        { id: 4, parentId: 0, text: "Birds" },
-        { id: 5, parentId: 0, text: "Hippos" },
-        { id: 6, parentId: 0, text: "Elefants" },
-        { id: 7, parentId: 0, text: "Cows" },
-        { id: 8, parentId: 6, text: "Mini-elefants" }
+        { id: 2, parentId: 1, text: "Maine Coon" }
     ];
 
     var $treeView = initTree({
         dataSource: data,
-        dataStructure: "plain",
-        height: 100
+        dataStructure: "plain"
     });
 
     var treeView = $treeView.dxTreeView("instance");
     var pointerDownStub = sinon.stub(treeView, "_itemPointerDownHandler");
 
-    var $node = $treeView.find("." + internals.NODE_CLASS).eq(5),
-        $arrow = $node.find("." + internals.TOGGLE_ITEM_VISIBILITY_CLASS);
+    var $node = $treeView.find("." + NODE_CLASS).eq(0),
+        $arrow = $node.find("." + TOGGLE_ITEM_VISIBILITY_CLASS);
 
     $arrow.trigger("dxpointerdown");
 
-    assert.equal(pointerDownStub.callCount, 1, "arrow should be part of node");
+    assert.equal(pointerDownStub.callCount, 1, "itemPointerDownHandler was called");
 });
