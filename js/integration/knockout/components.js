1ed09c7c0d59a266642c5d70a021bf2cd4dbaba3
Andrey Ovchinnikov <ovchinnikov@users.noreply.github.com>
2017-05-02 15:09:44 +0300

    Fixes memory leak after destroying dxToolbar widget

--- js/integration/knockout/components.js
+++ js/integration/knockout/components.js
@@ -1,12 +1,10 @@
 "use strict";
 
 var $ = require("jquery"),
-    errors = require("../../core/errors"),
     Action = require("../../core/action"),
     compileGetter = require("../../core/utils/data").compileGetter,
     ko = require("knockout"),
     iconUtils = require("../../core/utils/icon"),
-    inflector = require("../../core/utils/inflector"),
     clickEvent = require("../../events/click");
 
 // TODO: dxAction as dxComponent?
@@ -57,31 +55,6 @@ ko.bindingHandlers.dxControlsDescendantBindings = {
         };
     }
 };
-
-ko.bindingHandlers.dxPolymorphWidget = {
-    init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
-        var widgetName = ko.utils.unwrapObservable(valueAccessor()).name;
-        if(!widgetName) {
-            return;
-        }
-
-        ko.virtualElements.emptyNode(element);
-
-        if(widgetName === "button" || widgetName === "tabs" || widgetName === "dropDownMenu") {
-            var deprecatedName = widgetName;
-            widgetName = inflector.camelize("dx-" + widgetName);
-            errors.log("W0001", "dxToolbar - 'widget' item field", deprecatedName, "16.1", "Use: '" + widgetName + "' instead");
-        }
-
-        var markup = $("<div data-bind=\"" + widgetName + ": options\">").get(0);
-        ko.virtualElements.prepend(element, markup);
-        var innerBindingContext = bindingContext.extend(valueAccessor);
-        ko.applyBindingsToDescendants(innerBindingContext, element);
-
-        return { controlsDescendantBindings: true };
-    }
-};
-ko.virtualElements.allowedBindings.dxPolymorphWidget = true;
 
 ko.bindingHandlers.dxIcon = {
     init: function(element, valueAccessor) {
