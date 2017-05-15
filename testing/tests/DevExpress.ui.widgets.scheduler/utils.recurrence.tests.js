ec27eb7fde2e7c480d590388163416dcc3bd94ac
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2017-05-15 10:36:39 +0300

    Fix T506859: dxScheduler renders recurrent events incorrectly when a number of occurrences is selected (#111)

--- testing/tests/DevExpress.ui.widgets.scheduler/utils.recurrence.tests.js
+++ testing/tests/DevExpress.ui.widgets.scheduler/utils.recurrence.tests.js
@@ -76,6 +76,30 @@ QUnit.test('getDatesByRecurrence should handle strings with COUNT', function(ass
     assert.deepEqual(dates, [new Date(2015, 0, 1), new Date(2015, 0, 4)], 'date are right');
 });
 
+QUnit.test('getDatesByRecurrence should handle strings with COUNT without range', function(assert) {
+    var dates = recurrenceUtils.getDatesByRecurrence({ rule: 'FREQ=WEEKLY;BYDAY=MO,WE,FR;COUNT=4', start: new Date(2015, 0, 1), min: new Date(2015, 0, 1), max: new Date(2015, 0, 21) });
+
+    assert.deepEqual(dates, [new Date(2015, 0, 2), new Date(2015, 0, 5), new Date(2015, 0, 7), new Date(2015, 0, 9)], 'date are right');
+});
+
+QUnit.test('getDatesByRecurrence should handle strings with COUNT & BYDAY', function(assert) {
+    var dates = recurrenceUtils.getDatesByRecurrence({ rule: 'FREQ=WEEKLY;BYDAY=MO,WE,FR;COUNT=10', start: new Date(2015, 0, 1), min: new Date(2015, 0, 1), max: new Date(2015, 0, 20) });
+
+    assert.deepEqual(dates, [new Date(2015, 0, 2), new Date(2015, 0, 5), new Date(2015, 0, 7), new Date(2015, 0, 9), new Date(2015, 0, 12), new Date(2015, 0, 14), new Date(2015, 0, 16), new Date(2015, 0, 19)], 'date are right');
+});
+
+QUnit.test('getDatesByRecurrence should handle strings with COUNT & BYMONTH', function(assert) {
+    var dates = recurrenceUtils.getDatesByRecurrence({ rule: 'FREQ=MONTHLY;BYMONTHDAY=2;COUNT=10', start: new Date(2017, 0, 1), min: new Date(2017, 0, 1), max: new Date(2017, 11, 20) });
+
+    assert.deepEqual(dates, [new Date(2017, 0, 2), new Date(2017, 1, 2), new Date(2017, 2, 2), new Date(2017, 3, 2), new Date(2017, 4, 2), new Date(2017, 5, 2), new Date(2017, 6, 2), new Date(2017, 7, 2), new Date(2017, 8, 2), new Date(2017, 9, 2)], 'date are right');
+});
+
+QUnit.test('getDatesByRecurrence should handle strings with BYYEARDAY, COUNT & YEARLY', function(assert) {
+    var dates = recurrenceUtils.getDatesByRecurrence({ rule: 'FREQ=YEARLY;BYYEARDAY=200,201;COUNT=10', start: new Date(2017, 0, 1), min: new Date(2017, 6, 1), max: new Date(2018, 7, 1) });
+
+    assert.deepEqual(dates, [new Date(2017, 6, 19), new Date(2017, 6, 20), new Date(2018, 6, 19), new Date(2018, 6, 20)], 'date are right');
+});
+
 QUnit.test('getDatesByRecurrence should not have any string if count is out', function(assert) {
     var dates = recurrenceUtils.getDatesByRecurrence({ rule: 'FREQ=DAILY;INTERVAL=2;COUNT=2', start: new Date(2015, 0, 1), min: new Date(2015, 1, 1), max: new Date(2015, 1, 21) });
 
