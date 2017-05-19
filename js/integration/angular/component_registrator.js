a9586f27368a659c557a19bb2e3eaafd9a35cab4
zhernovkova.anna <anna.zhernovkova@devexpress.com>
2017-05-19 16:16:56 +0300

    Fix T514528: Changing a grid option makes the Angular JS binding refresh many times

--- js/integration/angular/component_registrator.js
+++ js/integration/angular/component_registrator.js
@@ -349,8 +349,8 @@ var ComponentBuilder = Class.inherit({
         }
 
         result._optionChangedCallbacks = this._optionChangedCallbacks;
-        result._disposingCallbacks = this._componentDisposing;
-        result.onActionCreated = function(component, action, config) {
+        result._disposingCallbacks = this._componentDisposing;        
+        result.beforeActionExecute = function(component, action, config) {
             if(config && $.inArray(config.category, SKIP_APPLY_ACTION_CATEGORIES) > -1) {
                 return action;
             }
@@ -374,7 +374,7 @@ var ComponentBuilder = Class.inherit({
             return {
                 templatesRenderAsynchronously: component.option("templatesRenderAsynchronously"),
                 modelByElement: component.option("modelByElement"),
-                onActionCreated: component.option("onActionCreated"),
+                beforeActionExecute: component.option("beforeActionExecute"),
                 nestedComponentOptions: component.option("nestedComponentOptions")
             };
         };
@@ -401,6 +401,8 @@ var ComponentBuilder = Class.inherit({
                     immediateValue = fn();
                     callback(immediateValue);
                 }
+
+                safeApply(function() {}, scope);
 
                 return disposeWatcher;
             },
