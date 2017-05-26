b7613e45c6ca6bb6a1534537aa7c001df88e202b
Aleksey Martynov <AlekseyMartynov@users.noreply.github.com>
2017-05-26 19:43:13 +0300

    language

--- testing/tests/DevExpress.ui.widgets.editors/textEditorParts/caretWorkaround.js
+++ testing/tests/DevExpress.ui.widgets.editors/textEditorParts/caretWorkaround.js
@@ -4,8 +4,8 @@ var browser = require("core/utils/browser"),
     caret = require("ui/text_box/utils.caret");
 
 module.exports = function($input) {
-    // NOTE: We desided to return to this problem when the Edge will update to the 16 version
-    // Edge 15 sets caret to the end of the mask editor when iframe is used and focus was triggered by focus() method
+    // NOTE: We decided to return to this problem when Edge 16 is released
+    // Edge 15 sets the caret to the end of the mask editor when iframe is used and focus is triggered by the focus() method
     var isEdge15 = browser.msie && browser.version >= 15 && browser.version < 16;
 
     // Chrome devtools device emulation
