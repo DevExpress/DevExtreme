5bebed223b70f5672c025bd4e790388225794fa1
Aleksey Martynov <AlekseyMartynov@users.noreply.github.com>
2017-05-31 20:41:48 +0300

    Apply caret workaround for Chrome-emulated WinPhone

--- testing/tests/DevExpress.ui.widgets.editors/textEditorParts/caretWorkaround.js
+++ testing/tests/DevExpress.ui.widgets.editors/textEditorParts/caretWorkaround.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var browser = require("core/utils/browser"),
+    devices = require("core/devices"),
     caret = require("ui/text_box/utils.caret");
 
 module.exports = function($input) {
@@ -8,11 +9,11 @@ module.exports = function($input) {
     // Edge 15 sets the caret to the end of the mask editor when iframe is used and focus is triggered by the focus() method
     var isEdge15 = browser.msie && browser.version >= 15 && browser.version < 16;
 
-    // Chrome devtools device emulation
-    // Same problem for iOS user-agents
-    var isEmulatedIOS = "chrome" in window && browser.safari;
+    var isDesktopChrome = ("chrome" in window) && ("app" in window.chrome) && ("isInstalled" in window.chrome.app),
+        isChromeEmulatedIOS = isDesktopChrome && browser.safari,
+        isChromeEmulatedWinPhone = isDesktopChrome && browser.msie && devices.real().deviceType === "phone";
 
-    if(isEdge15 || isEmulatedIOS) {
+    if(isEdge15 || isChromeEmulatedIOS || isChromeEmulatedWinPhone) {
         $input.focus();
         caret($input, 0);
     }
