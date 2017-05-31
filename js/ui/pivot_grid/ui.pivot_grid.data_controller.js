d0d925e0ea651f1da338dc2166e3b15dadef716a
volnyagin <vconst@users.noreply.github.com>
2017-05-31 13:00:47 +0300

    Fix T518378: Column Area fields are hidden after enabling the virtual scrolling feature in certain situations

--- js/ui/pivot_grid/ui.pivot_grid.data_controller.js
+++ js/ui/pivot_grid/ui.pivot_grid.data_controller.js
@@ -632,7 +632,7 @@ exports.DataController = Class.inherit((function() {
                     if(this.pageIndex() < this.pageCount() - 1) {
                         return this.pageSize();
                     } else {
-                        this.totalItemsCount() % this.pageSize();
+                        return this.totalItemsCount() % this.pageSize();
                     }
                 },
                 items: function() {
@@ -806,8 +806,10 @@ exports.DataController = Class.inherit((function() {
                 columnsScrollController.viewportSize(contentParams.viewportWidth / columnsScrollController.viewportItemSize());
                 columnsScrollController.setContentSize(contentParams.contentWidth);
 
-                columnsScrollController.loadIfNeed();
-                rowsScrollController.loadIfNeed();
+                commonUtils.deferUpdate(function() {
+                    columnsScrollController.loadIfNeed();
+                    rowsScrollController.loadIfNeed();
+                });
 
                 newLeftPosition = columnsScrollController.getViewportPosition() * columnViewportItemSize / oldColumnViewportItemSize;
                 newTopPosition = rowsScrollController.getViewportPosition() * rowViewportItemSize / oldRowViewportItemSize;
