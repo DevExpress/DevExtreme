4057b1c7b706eccbdf7a4796b979ab5aefe1f69e
Ksenia Mordalyuk <KsyushaM@users.noreply.github.com>
2017-06-05 18:31:06 +0300

    Fix T521654: dxList loses selection after reordering items if the DataSource.key option is specified

--- js/ui/collection/ui.collection_widget.edit.js
+++ js/ui/collection/ui.collection_widget.edit.js
@@ -853,7 +853,7 @@ var CollectionWidget = BaseCollectionWidget.inherit({
 
             strategy.moveItemAtIndexToIndex(movingIndex, destinationIndex);
 
-            that.option("selectedItems", that._getItemsByKeys(that._selection.getSelectedItemKeys()));
+            that.option("selectedItems", that._getItemsByKeys(that._selection.getSelectedItemKeys(), that._selection.getSelectedItems()));
 
             if(changingOption === "items") {
                 that._simulateOptionChange(changingOption);
