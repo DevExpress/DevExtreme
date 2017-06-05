3852d739c491f6182fa69ae41fb5c85d8d6adfb7
levkovskiy.dmitry <dmitry.levkovskiy@devexpress.com>
2017-06-05 15:06:13 +0300

    fix after review

--- js/ui/grid_core/ui.grid_core.virtual_scrolling.js
+++ js/ui/grid_core/ui.grid_core.virtual_scrolling.js
@@ -378,9 +378,10 @@ var VirtualScrollingRowsViewExtender = (function() {
                 };
 
             for(i = 0; i < columns.length; i++) {
-                var cssClass = columns[i].cssClass;
+                var classes = that._getCellClasses(columns[i]),
+                    classString = classes.length ? " class='" + classes.join(" ") + "'" : "";
 
-                freeSpaceCellsHtml += columns[i].command === "expand" ? "<td class='" + that.addWidgetPrefix(GROUP_SPACE_CLASS) + " " + cssClass + "'/>" : "<td class='" + cssClass + "'/>";
+                freeSpaceCellsHtml += "<td" + classString + "/>";
             }
 
             while(height > PIXELS_LIMIT) {
@@ -391,6 +392,17 @@ var VirtualScrollingRowsViewExtender = (function() {
 
             container.addClass(that.addWidgetPrefix(TABLE_CLASS));
             container.html(html);
+        },
+
+        _getCellClasses: function(column) {
+            var classes = [],
+                cssClass = column.cssClass,
+                isExpandColumn = column.command === "expand";
+
+            cssClass && classes.push(cssClass);
+            isExpandColumn && classes.push(this.addWidgetPrefix(GROUP_SPACE_CLASS));
+
+            return classes;
         },
 
         _findBottomLoadPanel: function() {
