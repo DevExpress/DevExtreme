5f22d58c9a452564fc41f5516a2e25b5d09769bc
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2017-06-07 11:55:30 +0300

    Fix T522700: dxTagBox - The 'Uncaught TypeError: Cannot read property 'length' of undefined' error appears when the search mode option is set to 'startswith'

--- js/ui/select_box.js
+++ js/ui/select_box.js
@@ -754,7 +754,7 @@ var SelectBox = DropDownList.inherit({
             return;
         }
 
-        var item = this._list && this._list.option("items")[0];
+        var item = this._list && this._getPlainItems(this._list.option("items"))[0];
 
         if(!item) {
             return;
