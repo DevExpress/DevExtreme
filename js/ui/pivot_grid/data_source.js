95546c14dad84ee96d8a279bd825255852831c9a
Elena <elena.vostrikova@devexpress.com>
2017-06-26 18:43:16 +0300

    dxPivotGrid - sorting refactoring (#519)

--- js/ui/pivot_grid/data_source.js
+++ js/ui/pivot_grid/data_source.js
@@ -504,19 +504,26 @@ module.exports = Class.inherit((function() {
         }
     }
 
+    function getMemberForSortBy(sortBy, getAscOrder) {
+        var member = "text";
+        if(sortBy === "none") {
+            member = "index";
+        } else if(getAscOrder || sortBy !== "displayText") {
+            member = "value";
+        }
+        return member;
+    }
+
     function getSortingMethod(field, dataSource, loadOptions, dimensionName, getAscOrder) {
         var sortOrder = getAscOrder ? "asc" : field.sortOrder,
-            sortBy = field.sortBy === "none" ? "none" : getAscOrder ? "value" : field.sortBy === "displayText" ? "text" : "value",
+            sortBy = getMemberForSortBy(field.sortBy, getAscOrder),
             defaultCompare = field.sortingMethod ? function(a, b) {
                 return field.sortingMethod(a, b);
             } : getCompareFunction(function(item) { return item[sortBy]; }),
             summaryValueSelector = !getAscOrder && getFieldSummaryValueSelector(field, dataSource, loadOptions, dimensionName),
             summaryCompare = summaryValueSelector && getCompareFunction(summaryValueSelector),
             sortingMethod = function(a, b) {
-                var result = summaryCompare && summaryCompare(a, b) || 0;
-                if(result === 0) {
-                    result = sortBy === "none" ? a.index - b.index : defaultCompare(a, b);
-                }
+                var result = summaryCompare && summaryCompare(a, b) || defaultCompare(a, b);
                 return sortOrder === "desc" ? -result : result;
             };
 
