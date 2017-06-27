e7944a408ae2c90a40e74c7e5c4f345eac726595
salimov.alyar <alyar666@users.noreply.github.com>
2017-06-27 16:50:47 +0300

    Fix T526383: The error message is overlapped by the column header if the grid has only one row

--- testing/tests/DevExpress.ui.widgets.dataGrid/editing.tests.js
+++ testing/tests/DevExpress.ui.widgets.dataGrid/editing.tests.js
@@ -6788,6 +6788,7 @@ QUnit.test("Show tooltip on focus with set validate in column and edit mode batc
 QUnit.test("Show tooltip on focus for last row with set validate in column and edit mode batch", function(assert) {
     //arrange
     var that = this,
+        $overlayContent,
         rowsView = that.rowsView,
         testElement = $('#container'),
         cells,
@@ -6847,10 +6848,14 @@ QUnit.test("Show tooltip on focus for last row with set validate in column and e
     cells = rowsView.element().find(".dx-data-row").last().find("td");
 
     //assert
+    $overlayContent = cells.eq(1).find(".dx-overlay-content");
     assert.equal(getInputElements(testElement).length, 1, "has input");
-    assert.equal(cells.eq(1).find(".dx-overlay").length, 1, "has tooltip");
+    assert.equal($overlayContent.length, 1, "has tooltip");
     assert.ok(rowsView.element().find(".dx-freespace-row").is(":visible"), "visible freespace row");
     assert.ok(rowsView.element().find(".dx-freespace-row").height() > 0, "freespace row has height ");
+
+    //T526383
+    assert.ok($overlayContent.offset().top >= (cells.eq(1).offset().top + cells.eq(1).height()), "tooltip is under the cell");
 });
 
 //T200857
