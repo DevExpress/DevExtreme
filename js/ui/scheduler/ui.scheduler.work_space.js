3e33f897c40d181bac73d3405eae2364d7b9590a
Ksenia Mordalyuk <KsyushaM@users.noreply.github.com>
2017-06-27 10:36:42 +0300

    Fix T528622 & T528497: change workSpace recalculation after rendering cell templates (#520)
    * Fix T528622 & T528497: change workSpace recalculation after rendering cell templates
    
    * Refactoring
    
    * Test refactoring
    
    * Change headerPanelOffsetRecalculate call

--- js/ui/scheduler/ui.scheduler.work_space.js
+++ js/ui/scheduler/ui.scheduler.work_space.js
@@ -379,6 +379,7 @@ var SchedulerWorkSpace = Widget.inherit({
                 this._changeAllDayVisibility();
                 this.notifyObserver("allDayPanelToggled");
                 this._attachTablesEvents();
+                this.headerPanelOffsetRecalculate();
                 break;
             case "onCellClick":
                 this._createCellClickAction();
@@ -823,11 +824,12 @@ var SchedulerWorkSpace = Widget.inherit({
         }
 
         var headerPanelHeight = this.getHeaderPanelHeight(),
-            headerHeight = this.invoke("getHeaderHeight");
+            headerHeight = this.invoke("getHeaderHeight"),
+            allDayPanelHeight = this.supportAllDayRow() && this.option("showAllDayPanel") ? this.getAllDayHeight() : 0;
 
         headerPanelHeight && this._dateTableScrollable.element().css({
-            "padding-bottom": headerPanelHeight + "px",
-            "margin-bottom": -1 * (parseInt(headerPanelHeight, 10)) + "px"
+            "padding-bottom": allDayPanelHeight + headerPanelHeight + "px",
+            "margin-bottom": -1 * ((parseInt(headerPanelHeight, 10)) + allDayPanelHeight) + "px"
         });
 
         this._$allDayTitle && this._$allDayTitle.css("top", headerHeight + headerPanelHeight + "px");
