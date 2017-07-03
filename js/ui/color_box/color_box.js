9c7f2a9915279d3ef2e31e2dfb13fa5fee96ddbe
Eugeniy Maslyaev <mrjohn0011@users.noreply.github.com>
2017-07-03 10:32:57 +0300

    T531379: dxColorBox does not allow to select a new value via the color picker if applyValueMode is "useButtons"

--- js/ui/color_box/color_box.js
+++ js/ui/color_box/color_box.js
@@ -133,7 +133,7 @@ var ColorBox = DropDownEditor.inherit({
             onApplyButtonClick: null,
             onCancelButtonClick: null,
 
-            buttonsLocation: "bottom after"
+            buttonsLocation: "bottom after",
 
             /**
             * @name dxColorBoxOptions_value
@@ -169,6 +169,7 @@ var ColorBox = DropDownEditor.inherit({
             * @hidden
             * @extend_doc
             */
+            valueChangeEvent: "change"
 
             /**
             * @name dxColorBoxOptions_spellcheck
