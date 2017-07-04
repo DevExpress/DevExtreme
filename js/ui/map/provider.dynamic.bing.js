0d7fd90e3aff0b49d23b3e75fcffb6286d54e694
levkovskiy.dmitry <dmitry.levkovskiy@devexpress.com>
2017-07-04 13:42:08 +0300

    fixes dxMap location in a case when "center" option is "null" and "provider" is "bing" (T532199)

--- js/ui/map/provider.dynamic.bing.js
+++ js/ui/map/provider.dynamic.bing.js
@@ -4,7 +4,8 @@ var $ = require("jquery"),
     Promise = require("../../core/polyfills/promise"),
     DynamicProvider = require("./provider.dynamic"),
     Color = require("../../color"),
-    browser = require("../../core/utils/browser");
+    browser = require("../../core/utils/browser"),
+    isDefined = require("../../core/utils/type").isDefined;
 
 /* global Microsoft */
 var BING_MAP_READY = "_bingScriptReady",
@@ -61,22 +62,27 @@ var BingProvider = DynamicProvider.inherit({
     _geocodedLocations: {},
     _geocodeLocationImpl: function(location) {
         return new Promise(function(resolve) {
-            var searchManager = new Microsoft.Maps.Search.SearchManager(this._map);
-            var searchRequest = {
-                where: location,
-                count: 1,
-                callback: function(searchResponse) {
-                    var result = searchResponse.results[0];
-                    if(result) {
-                        var boundsBox = searchResponse.results[0].location;
+            if(isDefined(location)) {
+                var searchManager = new Microsoft.Maps.Search.SearchManager(this._map);
+                var searchRequest = {
+                    where: location,
+                    count: 1,
+                    callback: function(searchResponse) {
+                        var result = searchResponse.results[0];
+                        if(result) {
+                            var boundsBox = searchResponse.results[0].location;
 
-                        resolve(new Microsoft.Maps.Location(boundsBox.latitude, boundsBox.longitude));
-                    } else {
-                        resolve(new Microsoft.Maps.Location(0, 0));
+                            resolve(new Microsoft.Maps.Location(boundsBox.latitude, boundsBox.longitude));
+                        } else {
+                            resolve(new Microsoft.Maps.Location(0, 0));
+                        }
                     }
-                }
-            };
-            searchManager.geocode(searchRequest);
+                };
+
+                searchManager.geocode(searchRequest);
+            } else {
+                resolve(new Microsoft.Maps.Location(0, 0));
+            }
         }.bind(this));
     },
 
