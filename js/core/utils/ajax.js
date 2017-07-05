d312d265b9489678945bb5f5fe2b18fd8c115b25
Eugen Zha <nightskylark@users.noreply.github.com>
2017-07-05 18:30:09 +0300

    Reanimate tests for jquery v1.x (#631)

--- js/core/utils/ajax.js
+++ js/core/utils/ajax.js
@@ -1,58 +1,62 @@
 "use strict";
 
-var $ = require("../renderer"),
-    jQuery = require("jquery");
+var $ = require("../renderer");
+var jQuery = require("jquery");
 
 var isStatusSuccess = function(status) {
     return 200 <= status && status < 300;
 };
 
 var sendRequest = function(options) {
-    options.xhrFields = options.xhrFields || {};
-    options.xhrFields.responseType = options.responseType;
+    if(!options.responseType && !options.upload) {
+        return jQuery.ajax(options);
+    }
+
+    var xhr = new XMLHttpRequest();
+    var d = $.Deferred();
+    var result = d.promise();
+
+    xhr.open(options.method || "GET", options.url, true);
+
+    xhr["onreadystatechange"] = function(e) {
+        if(xhr.readyState === 4) {
+            if(isStatusSuccess(xhr.status)) {
+                d.resolve(xhr.response);
+            } else {
+                d.reject(xhr);
+            }
+        }
+    };
 
     if(options.upload) {
-        var xhr = new XMLHttpRequest();
-        var d = $.Deferred();
-        var result = d.promise();
-
-        xhr.open(options.method, options.url, true);
         xhr.upload["onprogress"] = options.upload["onprogress"];
         xhr.upload["onloadstart"] = options.upload["onloadstart"];
         xhr.upload["onabort"] = options.upload["onabort"];
-
-        xhr["onreadystatechange"] = function(e) {
-            if(xhr.readyState === 4) {
-                if(isStatusSuccess(xhr.status)) {
-                    d.resolve();
-                } else {
-                    d.reject(xhr);
-                }
-            }
-        };
-
-        if(options.headers) {
-            for(var name in options.headers) {
-                if(options.headers.hasOwnProperty(name)) {
-                    xhr.setRequestHeader(name, options.headers[name]);
-                }
-            }
-        }
-
-        if(options.beforeSend) {
-            options.beforeSend(xhr);
-        }
-
-        xhr.send(options.data);
-
-        result.abort = function() {
-            xhr.abort();
-        };
-
-        return result;
     }
 
-    return jQuery.ajax(options);
+    if(options.responseType === "arraybuffer") {
+        xhr.responseType = options.responseType;
+    }
+
+    if(options.headers) {
+        for(var name in options.headers) {
+            if(options.headers.hasOwnProperty(name)) {
+                xhr.setRequestHeader(name, options.headers[name]);
+            }
+        }
+    }
+
+    if(options.beforeSend) {
+        options.beforeSend(xhr);
+    }
+
+    xhr.send(options.data);
+
+    result.abort = function() {
+        xhr.abort();
+    };
+
+    return result;
 };
 
 exports.sendRequest = sendRequest;
