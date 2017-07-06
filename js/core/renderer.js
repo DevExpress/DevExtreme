5b460ccc55ad17b408a818cc07b5406682f74254
volkov.vladislav <vladislav.volkov@devexpress.com>
2017-07-06 17:13:45 +0300

    Revert "Implement html method"

--- js/core/renderer.js
+++ js/core/renderer.js
@@ -10,7 +10,7 @@ var methods = [
     "width", "height", "outerWidth", "innerWidth", "outerHeight", "innerHeight", "offset", "offsetParent", "position", "scrollLeft", "scrollTop",
     "data", "removeData",
     "on", "off", "one", "trigger", "triggerHandler", "focusin", "focusout", "click",
-    "css", "text",
+    "html", "css", "text",
     "wrapInner", "wrap",
     "each", "val", "index",
     "hide", "show", "toggle", "slideUp", "slideDown", "slideToggle", "focus", "blur", "submit"];
@@ -297,23 +297,23 @@ if(!useJQueryRenderer) {
         return renderer(result);
     };
 
-    initRender.prototype.html = function(value) {
-        if(arguments.length === 0) {
-            return this[0].innerHTML;
-        } else if(arguments[0] === undefined) {
-            return this;
-        }
+    // initRender.prototype.html = function(value) {
+    //     if(arguments.length === 0) {
+    //         return this[0].innerHTML;
+    //     } else if(arguments[0] === undefined) {
+    //         return this;
+    //     }
 
-        this.empty();
+    //     this.empty();
 
-        if(typeof value === "string" || typeof value === "number") {
-            this[0].innerHTML = value;
-        } else {
-            this.append(value);
-        }
+    //     if(typeof value === "string" || typeof value === "number") {
+    //         this[0].innerHTML = value;
+    //     } else {
+    //         this.append(value);
+    //     }
 
-        return this;
-    };
+    //     return this;
+    // };
 
     initRender.prototype.contents = function() {
         return renderer(this[0] ? this[0].childNodes : []);
