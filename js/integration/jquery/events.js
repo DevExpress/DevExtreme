b7ecc35da0a1940c1c1da7023fccb29e60b4ef49
eugen.zha <nightskylark@users.noreply.github.com>
2017-07-07 16:28:02 +0300

    Add eventsEngine module

---
+++ js/integration/jquery/events.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var $ = require("jquery");
+
+module.exports = {
+    on: function(element) {
+        $(element).on.apply($(element), Array.prototype.slice.call(arguments, 1));
+    },
+    off: function(element) {
+        $(element).off.apply($(element), Array.prototype.slice.call(arguments, 1));
+    },
+    trigger: function(element) {
+        $(element).trigger.apply($(element), Array.prototype.slice.call(arguments, 1));
+    }
+};
