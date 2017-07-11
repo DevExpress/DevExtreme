5b53fc432de7afae19e26667916ec9acdc323fe3
Ksenia Mordalyuk <KsyushaM@users.noreply.github.com>
2017-07-11 16:43:46 +0300

    Revert "Fix T533348:  An overnight appointment rendering in the timeline view in dxScheduler"
    This reverts commit a349108a40c0d33d9e8e43faa5f989bd8a1173fe.

--- js/ui/scheduler/ui.scheduler.subscribes.js
+++ js/ui/scheduler/ui.scheduler.subscribes.js
@@ -666,14 +666,7 @@ var subscribes = {
                 tailDuration;
 
             if(isDifferentDate) {
-                var startDayTime = this.option("startDayHour") * toMs("hour"),
-                    appointmentPartDuration = endDate - dateUtils.trimTime(endDate);
-
                 tailDuration = appointmentDuration - (floorQuantityOfDays ? floorQuantityOfDays * dayDuration : dayDuration - visibleDayDuration);
-
-                if(appointmentPartDuration < startDayTime) {
-                    tailDuration += startDayTime - appointmentPartDuration;
-                }
             } else {
                 tailDuration = appointmentDuration % dayDuration;
             }
