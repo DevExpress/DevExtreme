d23eef2a28d37bd6f9c79ef4a6b81a3984eaf64f
salimov.alyar <alyar666@users.noreply.github.com>
2017-07-12 16:54:05 +0300

    eliminated the faults noted in the remarks

--- testing/tests/DevExpress.ui.widgets.dataGrid/editing.tests.js
+++ testing/tests/DevExpress.ui.widgets.dataGrid/editing.tests.js
@@ -10493,10 +10493,13 @@ QUnit.testInActiveWindow("Form should repaint after change data of the column wi
         $inputElement,
         callSetCellValue;
 
-    that.columns[0].setCellValue = function(rowData, value) {
-        callSetCellValue = true;
-        rowData.lastName = "Test2";
-        this.defaultSetCellValue(rowData, value);
+    that.columns[1] = {
+        dataField: "age",
+        setCellValue: function(rowData, value) {
+            callSetCellValue = true;
+            rowData.lastName = "Test2";
+            this.defaultSetCellValue(rowData, value);
+        }
     };
     that.setupModules(that);
     that.renderRowsView();
@@ -10510,13 +10513,14 @@ QUnit.testInActiveWindow("Form should repaint after change data of the column wi
     assert.ok($popupContent.find(".dx-texteditor").first().hasClass("dx-state-focused"), "first cell is focused");
 
     //act
-    $inputElement = $popupContent.find("input").first();
-    $inputElement.val("Test1");
+    $inputElement = $popupContent.find("input[type!='hidden']").eq(1);
+    $inputElement.focus();
+    $inputElement.val(666);
     $inputElement.trigger("change");
     that.clock.tick(500);
 
     //assert
     assert.ok(callSetCellValue, "setCellValue is called");
     assert.strictEqual($popupContent.find("input[type!='hidden']").eq(2).val(), "Test2", "value of the third cell");
-    assert.ok($popupContent.find(".dx-texteditor").first().hasClass("dx-state-focused"), "first cell is focused");
+    assert.ok($popupContent.find(".dx-texteditor").eq(1).hasClass("dx-state-focused"), "second cell is focused");
 });
