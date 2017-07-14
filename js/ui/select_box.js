2827189183bbc000dfc4217e22831b66d8ff18ea
San4es <San4es@users.noreply.github.com>
2017-07-14 13:04:33 +0300

    Fix test & impl for T517486

--- js/ui/select_box.js
+++ js/ui/select_box.js
@@ -383,7 +383,9 @@ var SelectBox = DropDownList.inherit({
     },
 
     _renderInputValue: function() {
-        return this.callBase().always(this._renderInputValueAsync.bind(this));
+        return this.callBase().always(function() {
+            this._renderInputValueAsync();
+        }.bind(this));
     },
 
     _renderInputValueAsync: function() {
@@ -778,6 +780,11 @@ var SelectBox = DropDownList.inherit({
             endPosition = inputElement.value.length;
         inputElement.selectionStart = endPosition;
         inputElement.selectionEnd = endPosition;
+    },
+
+    _dispose: function() {
+        this._renderInputValueAsync = commonUtils.noop;
+        this.callBase();
     },
 
     _optionChanged: function(args) {
