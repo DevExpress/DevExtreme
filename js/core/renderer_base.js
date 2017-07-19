26e06388814baed410bfdec7838af894da093dd9
dxvladislavvolkov <dxvladislavvolkov@users.noreply.github.com>
2017-07-19 11:07:45 +0300

    Implement param method (#742)

--- js/core/renderer_base.js
+++ js/core/renderer_base.js
@@ -716,7 +716,6 @@ renderer.tmpl = function() {
 renderer.templates = function() {
     return $.templates.apply(this, arguments);
 };
-renderer.param = $.param;
 renderer._data = $._data;
 renderer.data = $.data;
 renderer.removeData = $.removeData;
