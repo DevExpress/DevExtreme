91b63f0920dfaf8694470a6c30308d1262751443
volkov.vladislav <vladislav.volkov@devexpress.com>
2017-07-19 12:47:01 +0300

    refactoring

--- js/core/utils/iterator.js
+++ js/core/utils/iterator.js
@@ -25,21 +25,21 @@ var map = function(elems, callback, arg) {
 };
 
 var each = function(values, callback) {
-    if(!!values) {
-        if("length" in values) {
-            for(var i = 0; i < values.length; i++) {
-                if(callback.call(values[i], i, values[i]) === false) {
-                    break;
-                }
-     	    }
-        } else {
-            for(var key in values) {
-                if(callback.call(values[key], key, values[key]) === false) {
-                    break;
-                }
-     	    }
- 	    }
-    }
+    if(!values) return;
+
+    if("length" in values) {
+        for(var i = 0; i < values.length; i++) {
+            if(callback.call(values[i], i, values[i]) === false) {
+                break;
+            }
+     	}
+    } else {
+        for(var key in values) {
+            if(callback.call(values[key], key, values[key]) === false) {
+                break;
+            }
+     	}
+ 	}
 
     return values;
 };
