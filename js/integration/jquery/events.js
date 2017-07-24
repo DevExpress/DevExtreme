eeb79327988fbfeb6f4143ba5f00c10814c779a6
eugen.zha <nightskylark@users.noreply.github.com>
2017-07-24 11:46:31 +0300

    Fix `return` outside of function

--- js/integration/jquery/events.js
+++ js/integration/jquery/events.js
@@ -2,24 +2,21 @@
 
 var $ = require("jquery");
 var eventsEngine = require("../../events/core/events_engine");
-
 var useJQueryRenderer = window.useJQueryRenderer !== false;
 
-if(!useJQueryRenderer) {
-    return;
+if(useJQueryRenderer) {
+    eventsEngine.set({
+        on: function(element) {
+            $(element).on.apply($(element), Array.prototype.slice.call(arguments, 1));
+        },
+        one: function(element) {
+            $(element).one.apply($(element), Array.prototype.slice.call(arguments, 1));
+        },
+        off: function(element) {
+            $(element).off.apply($(element), Array.prototype.slice.call(arguments, 1));
+        },
+        trigger: function(element) {
+            $(element).trigger.apply($(element), Array.prototype.slice.call(arguments, 1));
+        }
+    });
 }
-
-eventsEngine.set({
-    on: function(element) {
-        $(element).on.apply($(element), Array.prototype.slice.call(arguments, 1));
-    },
-    one: function(element) {
-        $(element).one.apply($(element), Array.prototype.slice.call(arguments, 1));
-    },
-    off: function(element) {
-        $(element).off.apply($(element), Array.prototype.slice.call(arguments, 1));
-    },
-    trigger: function(element) {
-        $(element).trigger.apply($(element), Array.prototype.slice.call(arguments, 1));
-    }
-});
