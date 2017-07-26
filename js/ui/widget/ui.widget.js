839406490ac7b8635ae59d0bf4f3229dec7201f7
vostrikova.elena <elena.vostrikova@devexpress.com>
2017-07-26 13:33:21 +0300

    Fix animation in android theme in dxList selectAll item

--- js/ui/widget/ui.widget.js
+++ js/ui/widget/ui.widget.js
@@ -724,7 +724,7 @@ var Widget = DOMComponent.inherit({
 
     _attachFeedbackEvents: function() {
         var that = this,
-            feedbackSelector = that._activeStateUnit,
+            feedbackSelector = that._getFeedbackSelector(),
             activeEventName = eventUtils.addNamespace(feedbackEvents.active, UI_FEEDBACK),
             inactiveEventName = eventUtils.addNamespace(feedbackEvents.inactive, UI_FEEDBACK),
             feedbackAction,
@@ -761,6 +761,10 @@ var Widget = DOMComponent.inherit({
                     });
                 });
         }
+    },
+
+    _getFeedbackSelector: function() {
+        return this._activeStateUnit;
     },
 
     _toggleActiveState: function($element, value) {
