d0c62eff44d01ca68f4f666648f06e68eeb55340
salimov.alyar <alyar666@users.noreply.github.com>
2017-07-27 17:59:48 +0300

    dxTreeList - implementation of the loadChildren method

--- js/ui/tree_list/ui.tree_list.core.js
+++ js/ui/tree_list/ui.tree_list.core.js
@@ -4,6 +4,14 @@ var extend = require("../../core/utils/extend").extend,
     modules = require("../grid_core/ui.grid_core.modules");
 
 extend(exports, modules, {
-    modules: []
+    modules: [],
+
+    foreachNodes: function(nodes, callBack) {
+        for(var i = 0; i < nodes.length; i++) {
+            if(callBack(nodes[i]) !== false && nodes[i].hasChildren && nodes[i].children.length) {
+                this.foreachNodes(nodes[i].children, callBack);
+            }
+        }
+    }
 });
 
