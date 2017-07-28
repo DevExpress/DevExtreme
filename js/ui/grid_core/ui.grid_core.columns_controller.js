8723bc7c481f5215fd4a2648eb497827c73b7187
volnyagin <vconst@users.noreply.github.com>
2017-07-28 18:28:26 +0300

    Fix T538579: dxDataGrid displays duplicate rows when virtual scrolling is used

--- js/ui/grid_core/ui.grid_core.columns_controller.js
+++ js/ui/grid_core/ui.grid_core.columns_controller.js
@@ -1372,6 +1372,7 @@ module.exports = {
                     } else if(isDataSourceLoaded && !that.isAllDataTypesDefined(true) && that.updateColumnDataTypes(dataSource)) {
                         updateColumnChanges(that, "columns");
                         fireColumnsChanged(that);
+                        return $.Deferred().reject().promise();
                     }
                 },
                 reset: function() {
