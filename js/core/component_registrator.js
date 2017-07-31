30d8ac76fa77b0bd4d008dcb72ce371d83205df8
eugen.zha <nightskylark@users.noreply.github.com>
2017-07-31 12:54:04 +0300

    Return renderer componentRegistrator for a while

--- js/core/component_registrator.js
+++ js/core/component_registrator.js
@@ -1,7 +1,9 @@
 "use strict";
 
-var callbacks = require("./component_registrator_callbacks"),
-    publicComponentUtils = require("./utils/public_component");
+var $ = require("./renderer");
+var callbacks = require("./component_registrator_callbacks");
+var errors = require("./errors");
+var publicComponentUtils = require("./utils/public_component");
 
 /**
  * @name registerComponent
@@ -30,5 +32,47 @@ var registerComponent = function(name, namespace, componentClass) {
     publicComponentUtils.name(componentClass, name);
     callbacks.fire(name, componentClass);
 };
+
+var registerRendererComponent = function(name, componentClass) {
+    $.fn[name] = function(options) {
+        var isMemberInvoke = typeof options === "string",
+            result;
+
+        if(isMemberInvoke) {
+            var memberName = options,
+                memberArgs = [].slice.call(arguments).slice(1);
+
+            this.each(function() {
+                var instance = componentClass.getInstance(this);
+
+                if(!instance) {
+                    throw errors.Error("E0009", name);
+                }
+
+                var member = instance[memberName],
+                    memberValue = member.apply(instance, memberArgs);
+
+                if(result === undefined) {
+                    result = memberValue;
+                }
+            });
+        } else {
+            this.each(function() {
+                var instance = componentClass.getInstance(this);
+                if(instance) {
+                    instance.option(options);
+                } else {
+                    new componentClass(this, options);
+                }
+            });
+
+            result = this;
+        }
+
+        return result;
+    };
+};
+
+callbacks.add(registerRendererComponent);
 
 module.exports = registerComponent;
