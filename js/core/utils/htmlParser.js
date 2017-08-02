d9df5bb819032a2b869b7f5b947349c6297760df
Stanislav <112aden358@gmail.com>
2017-08-02 14:45:50 +0300

    Add HTML parser utility

---
+++ js/core/utils/htmlParser.js
@@ -0,0 +1,85 @@
+"use strict";
+
+var isTagName = (/<([a-z][^\/\0>\x20\t\r\n\f]+)/i);
+
+var $ = require("../renderer");
+
+//var merge = require("./array").merge;
+var merge = function(array1, array2) {
+    for(var i = 0; i < array2.length; i++) {
+        array1[array1.length] = array2[i];
+    }
+
+    return array1;
+};
+
+var tagWrappers = {
+    default: {
+        tagsCount: 0,
+        startTags: "",
+        endTags: ""
+    },
+    thead: {
+        tagsCount: 1,
+        startTags: "<table>",
+        endTags: "</table>"
+    },
+    td: {
+        tagsCount: 3,
+        startTags: "<table><tbody><tr>",
+        endTags: "</tr></tbody></table>"
+    },
+    col: {
+        tagsCount: 2,
+        startTags: "<table><colgroup>",
+        endTags: "</colgroup></table>"
+    },
+    tr: {
+        tagsCount: 2,
+        startTags: "<table><tbody>",
+        endTags: "</tbody></table>"
+    },
+};
+
+tagWrappers.tbody = tagWrappers.colgroup = tagWrappers.caption = tagWrappers.tfoot = tagWrappers.thead;
+tagWrappers.th = tagWrappers.td;
+
+var removeScriptTags = function(fragment) {
+    var searchResult = fragment.querySelectorAll("script");
+
+    for(var i = 0; i < searchResult.length; i++) {
+        $(searchResult[i]).remove();
+    }
+};
+
+function buildFragment(html) {
+    var fragment = document.createDocumentFragment();
+    var container = fragment.appendChild(document.createElement("div"));
+
+    var firstRootTag = (isTagName.exec(html) || [ "", "" ])[1].toLowerCase();
+    var tagWrapper = tagWrappers[firstRootTag] || tagWrappers.default;
+
+    container.innerHTML = tagWrapper.startTags + html + tagWrapper.endTags;
+
+    removeScriptTags(fragment);
+
+    for(var i = 0; i < tagWrapper.tagsCount; i++) {
+        container = container.lastChild;
+    }
+
+    var nodes = merge([], container.childNodes);
+
+    return nodes;
+}
+
+var parseHTML = function(html, context) {
+    if(typeof html !== "string") {
+        return null;
+    }
+
+    var parsed = buildFragment(html);
+
+    return parsed;
+};
+
+module.exports = parseHTML;
