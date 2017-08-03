ab60311f01fc7639fe62891e55697a6b488f43c9
Andrey Ovchinnikov <ovchinnikov@users.noreply.github.com>
2017-08-03 12:53:54 +0300

    Move jQuery version check into integration module

--- js/integration/jquery.js
+++ js/integration/jquery.js
@@ -1,9 +1,15 @@
 "use strict";
 
 var jQuery = require("jquery");
+var compareVersions = require("../core/utils/version").compare;
+var errors = require("../core/utils/error");
 
 // Check availability in global environment
 if(jQuery) {
+    if(compareVersions(jQuery.fn.jquery, [1, 10]) < 0) {
+        throw errors.Error("E0012");
+    }
+
     require("./jquery/renderer");
     require("./jquery/themes");
     require("./jquery/events");
