dc390d3d28f47dfae8866d37c713abcc530bede2
anna-zhernovkova <anna-zhernovkova@users.noreply.github.com>
2017-08-04 09:05:03 +0300

    Fix T539633: dxDataGrid - Search Panel with Row Template displays part of the angular expression (#995)

--- js/ui/grid_core/ui.grid_core.search.js
+++ js/ui/grid_core/ui.grid_core.search.js
@@ -348,9 +348,17 @@ module.exports = {
                 _renderCore: function() {
                     this.callBase.apply(this, arguments);
 
+                    //T103538
                     if(this.option("rowTemplate")) {
-                        //T103538
-                        this._highlightSearchText(this._getTableElement());
+                        if(this.option("templatesRenderAsynchronously")) {
+                            clearTimeout(this._highlightTimer);
+
+                            this._highlightTimer = setTimeout(function() {
+                                this._highlightSearchText(this._getTableElement());
+                            }.bind(this));
+                        } else {
+                            this._highlightSearchText(this._getTableElement());
+                        }
                     }
                 },
 
@@ -365,6 +373,11 @@ module.exports = {
                     }
 
                     that.callBase($cell, parameters);
+                },
+
+                dispose: function() {
+                    clearTimeout(this._highlightTimer);
+                    this.callBase();
                 }
             }
         }
