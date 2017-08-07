db0cfb375ff510d63eb33031a4c346b74351ccf1
Ksenia Mordalyuk <KsyushaM@users.noreply.github.com>
2017-08-07 16:04:35 +0300

    Fix T542220: dxScheduler - The allowUpdating option loses its effect after opening the Appointment Editing Form (#1012)

--- js/ui/scheduler/ui.scheduler.js
+++ js/ui/scheduler/ui.scheduler.js
@@ -1871,6 +1871,7 @@ var Scheduler = Widget.inherit({
                 endDateExpr = this.option("endDateExpr");
 
             this._appointmentForm.option("formData", formData);
+            this._appointmentForm.option("readOnly", this._editAppointmentData ? !this._editing.allowUpdating : false);
 
             var startDateFormItem = this._appointmentForm.itemOption(startDateExpr),
                 endDateFormItem = this._appointmentForm.itemOption(endDateExpr);
