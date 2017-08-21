6af4d9a4e71d42bfe8d5e8d9b67c62e3519d4dfd
vostrikova.elena <elena.vostrikova@devexpress.com>
2017-08-21 21:40:44 +0300

    Fix test

--- testing/tests/DevExpress.ui/hierarchicalCollectionWidgetParts/hierarchicalDataAdapter.js
+++ testing/tests/DevExpress.ui/hierarchicalCollectionWidgetParts/hierarchicalDataAdapter.js
@@ -634,23 +634,6 @@ QUnit.test("public node should depend on original node", function(assert) {
 QUnit.module("selection", moduleConfig);
 
 QUnit.test("set recursive selection", function(assert) {
-    this.randomData[1].selected = true;
-    this.randomData[2].selected = true;
-    this.randomData[3].selected = true;
-
-    var dataAdapter = initDataAdapter({
-            items: this.randomData,
-            recursiveSelection: true,
-            dataType: "plain"
-        }),
-        nodes = dataAdapter.getTreeNodes();
-
-    assert.strictEqual(nodes[1].selected, undefined, "item 1 is in undetermined state");
-    assert.ok(nodes[1].items[1].selected, "item 11 was selected");
-    assert.ok(nodes[0].items[0].selected, "item 21 was selected");
-});
-
-QUnit.test("set recursive selection with random data", function(assert) {
     this.plainData[3].selected = true;
     this.plainData[4].selected = true;
     this.plainData[5].selected = true;
@@ -665,6 +648,23 @@ QUnit.test("set recursive selection with random data", function(assert) {
     assert.strictEqual(nodes[0].selected, undefined, "item 1 is in undetermined state");
     assert.ok(nodes[0].items[0].selected, "item 11 was selected");
     assert.ok(nodes[1].items[0].selected, "item 21 was selected");
+});
+
+QUnit.test("set recursive selection with random data", function(assert) {
+    this.randomData[1].selected = true;
+    this.randomData[2].selected = true;
+    this.randomData[3].selected = true;
+
+    var dataAdapter = initDataAdapter({
+            items: this.randomData,
+            recursiveSelection: true,
+            dataType: "plain"
+        }),
+        nodes = dataAdapter.getTreeNodes();
+
+    assert.strictEqual(nodes[1].selected, undefined, "item 1 is in undetermined state");
+    assert.ok(nodes[1].items[1].selected, "item 11 was selected");
+    assert.ok(nodes[0].items[0].selected, "item 21 was selected");
 });
 
 QUnit.test("set recursiveSelection false", function(assert) {
