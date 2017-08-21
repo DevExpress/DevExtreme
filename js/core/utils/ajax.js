3e9a9fe10798ad403f6f2b73c4c0da344bd23b41
volkov.vladislav <vladislav.volkov@devexpress.com>
2017-08-21 17:17:21 +0300

    add some settings for a request

--- js/core/utils/ajax.js
+++ js/core/utils/ajax.js
@@ -2,6 +2,7 @@
 
 var jQuery = require("jquery");
 var Deferred = require("./deferred").Deferred;
+var extendFromObject = require("./extend").extendFromObject;
 
 var isStatusSuccess = function(status) {
     return 200 <= status && status < 300;
@@ -11,12 +12,35 @@ var sendRequest = function(options) {
     if(!options.responseType && !options.upload) {
         return jQuery.ajax(options);
     }
-
     var xhr = new XMLHttpRequest();
     var d = new Deferred();
     var result = d.promise();
+    var contentType = options.contentType || "application/x-www-form-urlencoded; charset=UTF-8";
 
-    xhr.open(options.method || "GET", options.url, true);
+    var dataType = options.dataType || "*";
+    var accepts = {
+        "*": "*/*",
+        text: "text/plain",
+        html: "text/html",
+        xml: "application/xml, text/xml",
+        json: "application/json, text/javascript"
+    };
+    extendFromObject(accepts, options.accepts);
+
+    var acceptHeader = accepts[dataType] ?
+        accepts[dataType] + (dataType !== "*" ? ", */*; q=0.01" : "") :
+        accepts["*"];
+
+    xhr.open(
+        options.method || "GET",
+        options.url,
+        options.async,
+        options.username,
+        options.password);
+
+    xhr.setRequestHeader("Content-Type", contentType);
+    xhr.setRequestHeader("Accept", acceptHeader);
+    xhr.timeout = options.timeout || 0;
 
     xhr["onreadystatechange"] = function(e) {
         if(xhr.readyState === 4) {
@@ -32,6 +56,12 @@ var sendRequest = function(options) {
         xhr.upload["onprogress"] = options.upload["onprogress"];
         xhr.upload["onloadstart"] = options.upload["onloadstart"];
         xhr.upload["onabort"] = options.upload["onabort"];
+    }
+
+    if(options.xhrFields) {
+        for(var field in options.xhrFields) {
+            xhr[field] = options.xhrFields[field];
+        }
     }
 
     if(options.responseType === "arraybuffer") {
