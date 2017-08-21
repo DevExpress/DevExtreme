23add5ef5c9034957a53a0c05273a6aa5c3de648
Alyar <Alyar666@users.noreply.github.com>
2017-08-21 13:17:20 +0300

    dxDataGrid - fix doc comment for the 'sortByGroupSummaryInfo.summaryItem' option (#1102)

--- ts/widgets-web.d.ts
+++ ts/widgets-web.d.ts
@@ -1640,7 +1640,7 @@ declare module DevExpress.ui {
         /** @docid dxDataGridOptions_sortByGroupSummaryInfo */
         sortByGroupSummaryInfo?: Array<{
             /** @docid dxDataGridOptions_sortByGroupSummaryInfo_summaryItem */
-            summaryItem?: string;
+            summaryItem?: any;
 
             /** @docid dxDataGridOptions_sortByGroupSummaryInfo_groupColumn */
             groupColumn?: string;
