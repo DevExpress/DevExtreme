51b25688847ba13de2495ce63bde06f62d870598
anna-zhernovkova <anna-zhernovkova@users.noreply.github.com>
2017-08-28 09:12:08 +0300

    Add item index to arguments of custom template render method (#1162)

--- js/ui/widget/jquery.template.js
+++ js/ui/widget/jquery.template.js
@@ -125,7 +125,7 @@ var Template = TemplateBase.inherit({
     },
 
     _renderCore: function(options) {
-        return $("<div>").append(currentTemplateEngine.render(this._compiledTemplate, options.model)).contents();
+        return $("<div>").append(currentTemplateEngine.render(this._compiledTemplate, options.model, options.index)).contents();
     },
 
     source: function() {
