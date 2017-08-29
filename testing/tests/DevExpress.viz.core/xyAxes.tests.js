DevExpress/DevExtreme

commit 743187d6769f50edf58f7f3e9130fb80bd7e6641

dxChart: Fix axis position after hiding a scrollbar (T548860) (#1171)

js/viz/axes/xy_axes.js
@@ -904,11 +904,9 @@ module.exports = {
             function shiftGroup(side, group) {
                 var attr = {},
                     shift = margins[side] + axesSpacing;
-                attr[isHorizontal ? "translateY" : "translateX"] = (side === "left" || side === "top" ? -1 : 1) * shift;
-                if(margins[side]) {
-                    (group[side] || group).attr(attr);
-                    return shift;
-                }
+                attr[isHorizontal ? "translateY" : "translateX"] = margins[side] ? (side === "left" || side === "top" ? -1 : 1) * shift : 0;
+                (group[side] || group).attr(attr);
+                return shift;
             }
 
             that._axisShift = shiftGroup(options.position, that._axisGroup);

testing/tests/DevExpress.viz.core/xyAxes.tests.js
@@ -2825,7 +2825,10 @@ QUnit.test("All margins are zero", function(assert) {
     this.renderer.g.getCall(5).returnValue.attr.reset();
 
     axis.shift({ top: 0, bottom: 0, left: 0, right: 0 });
-    assert.equal(this.renderer.g.getCall(5).returnValue.attr.callCount, 0);
+    //T548860
+    assert.deepEqual(this.renderer.g.getCall(5).returnValue.attr.lastCall.args[0], {
+        translateX: 0
+    });
 });
 
 QUnit.test("Vertical axis position is left", function(assert) {

commit 06b94d93128525bed09cc808d2c26ebc7a55fe01

dxChart: Fix crosshair label position (regression after #1171)

js/viz/axes/xy_axes.js
@@ -903,8 +903,10 @@ module.exports = {
 
             function shiftGroup(side, group) {
                 var attr = {},
-                    shift = margins[side] + axesSpacing;
-                attr[isHorizontal ? "translateY" : "translateX"] = margins[side] ? (side === "left" || side === "top" ? -1 : 1) * shift : 0;
+                    shift = margins[side] ? margins[side] + axesSpacing : 0;
+
+                attr[isHorizontal ? "translateY" : "translateX"] = (side === "left" || side === "top" ? -1 : 1) * shift;
+
                 (group[side] || group).attr(attr);
                 return shift;
             }

testing/tests/DevExpress.viz.core/xyAxes.tests.js
@@ -2831,6 +2831,15 @@ QUnit.test("All margins are zero", function(assert) {
     });
 });
 
+QUnit.test("getLabelsPosition returns correct value after empty shift", function(assert) {
+    this.options.multipleAxesSpacing = 5;
+    var axis = this.createDrawnAxis();
+
+    axis.shift({ top: 0, bottom: 0, left: 0, right: 0 });
+
+    assert.equal(axis.getLabelsPosition(), 10);
+});
+
 QUnit.test("Vertical axis position is left", function(assert) {
     this.options.isHorizontal = false;
     this.options.position = "left";