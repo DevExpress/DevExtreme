59fc1bdc58b4df5212622465cd74c565ad2ee980
eugen.zha <nightskylark@users.noreply.github.com>
2017-08-30 18:31:21 +0300

    Fix bundle tests

--- testing/tests/Bundles/bundlesParts/events.tests.js
+++ testing/tests/Bundles/bundlesParts/events.tests.js
@@ -1,6 +1,13 @@
 "use strict";
 
 var $ = require("jquery");
+var registerEventCallbacks = require("events/core/event_registrator_callbacks");
+
+var special = {};
+registerEventCallbacks.add(function(name, eventObject) {
+    special[name] = eventObject;
+});
+
 
 QUnit.test("events", function(assert) {
 
@@ -31,7 +38,7 @@ QUnit.test("events", function(assert) {
 
         "dxtransform"
     ], function(_, namespace) {
-        assert.ok($.event.special[namespace], namespace + " event present");
+        assert.ok(special[namespace], namespace + " event present");
     });
 
 });
