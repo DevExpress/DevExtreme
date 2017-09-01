eb69a47dbd79d18aa1e4674cba8a6eed1865473d
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2017-09-01 18:21:59 +0300

    fix types in dxTreeView methods 17_2

--- js/ui/tree_view.js
+++ js/ui/tree_view.js
@@ -1700,7 +1700,17 @@ var TreeView = HierarchicalCollectionWidget.inherit({
     /**
     * @name dxTreeViewMethods_selectItem
     * @publicName selectItem(itemElement)
-    * @param1 itemElement:Node|itemData|key
+    * @param1 itemElement:Node
+    */
+    /**
+    * @name dxTreeViewMethods_selectItem
+    * @publicName selectItem(itemData)
+    * @param1 itemData:Object
+    */
+    /**
+    * @name dxTreeViewMethods_selectItem
+    * @publicName selectItem(key)
+    * @param1 key:any
     */
     selectItem: function(itemElement) {
         this._updateItemSelection(true, itemElement);
@@ -1709,7 +1719,17 @@ var TreeView = HierarchicalCollectionWidget.inherit({
     /**
     * @name dxTreeViewMethods_unselectItem
     * @publicName unselectItem(itemElement)
-    * @param1 itemElement:Node|itemData|key
+    * @param1 itemElement:Node
+    */
+    /**
+    * @name dxTreeViewMethods_unselectItem
+    * @publicName unselectItem(itemData)
+    * @param1 itemData:Object
+    */
+    /**
+    * @name dxTreeViewMethods_unselectItem
+    * @publicName unselectItem(key)
+    * @param1 key:any
     */
     unselectItem: function(itemElement) {
         this._updateItemSelection(false, itemElement);
@@ -1718,7 +1738,17 @@ var TreeView = HierarchicalCollectionWidget.inherit({
     /**
     * @name dxTreeViewMethods_expandItem
     * @publicName expandItem(itemElement)
-    * @param1 itemElement:Node|itemData|key
+    * @param1 itemElement:Node
+    */
+    /**
+    * @name dxTreeViewMethods_expandItem
+    * @publicName expandItem(itemData)
+    * @param1 itemData:Object
+    */
+    /**
+    * @name dxTreeViewMethods_expandItem
+    * @publicName expandItem(key)
+    * @param1 key:any
     */
     expandItem: function(itemElement) {
         this._toggleExpandedState(itemElement, true);
@@ -1727,7 +1757,17 @@ var TreeView = HierarchicalCollectionWidget.inherit({
     /**
     * @name dxTreeViewMethods_collapseItem
     * @publicName collapseItem(itemElement)
-    * @param1 itemElement:Node|itemData|key
+    * @param1 itemElement:Node
+    */
+    /**
+    * @name dxTreeViewMethods_collapseItem
+    * @publicName collapseItem(itemData)
+    * @param1 itemData:Object
+    */
+    /**
+    * @name dxTreeViewMethods_collapseItem
+    * @publicName collapseItem(key)
+    * @param1 key:any
     */
     collapseItem: function(itemElement) {
         this._toggleExpandedState(itemElement, false);
