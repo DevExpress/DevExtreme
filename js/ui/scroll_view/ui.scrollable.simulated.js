8cf7276a92f85f406afedab27a4854a5f89f0d60
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2017-09-04 12:20:57 +0300

    Fix T544872: dxTileView/dxScrollView - The swipe direction is incorrect if rtlEnabled is true (#1239)

--- js/ui/scroll_view/ui.scrollable.simulated.js
+++ js/ui/scroll_view/ui.scrollable.simulated.js
@@ -904,6 +904,10 @@ var SimulatedStrategy = Class.inherit({
         };
     },
 
+    updateBounds: function() {
+        this._scrollers[HORIZONTAL] && this._scrollers[HORIZONTAL]._updateBounds();
+    },
+
     scrollBy: function(distance) {
         var verticalScroller = this._scrollers[VERTICAL],
             horizontalScroller = this._scrollers[HORIZONTAL];
