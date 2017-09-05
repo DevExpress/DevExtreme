23605cd73b6f302928498dc209d16eebb8637368
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2017-09-05 13:15:26 +0300

    fix PivotGridSummaryCell ref

--- js/ui/pivot_grid/ui.pivot_grid.summary_display_modes.js
+++ js/ui/pivot_grid/ui.pivot_grid.summary_display_modes.js
@@ -186,7 +186,7 @@ SummaryCell.prototype = extend(SummaryCell.prototype, {
     * @name dxPivotGridSummaryCell_parent
     * @publicName parent(direction)
     * @param1 direction:string
-    * @return SummaryCell
+    * @return dxPivotGridSummaryCell
     */
     parent: function(direction) {
         var path = this._getPath(direction).slice(),
@@ -225,13 +225,13 @@ SummaryCell.prototype = extend(SummaryCell.prototype, {
     * @name dxPivotGridSummaryCell_grandTotal
     * @publicName grandTotal(direction)
     * @param1 direction:string
-    * @return SummaryCell
+    * @return dxPivotGridSummaryCell
     */
 
     /**
    * @name dxPivotGridSummaryCell_grandTotal
    * @publicName grandTotal()
-   * @return SummaryCell
+   * @return dxPivotGridSummaryCell
    */
     grandTotal: function(direction) {
         var config = {},
@@ -253,7 +253,7 @@ SummaryCell.prototype = extend(SummaryCell.prototype, {
     * @name dxPivotGridSummaryCell_next
     * @publicName next(direction)
     * @param1 direction:string
-    * @return SummaryCell
+    * @return dxPivotGridSummaryCell
     */
 
     /**
@@ -261,7 +261,7 @@ SummaryCell.prototype = extend(SummaryCell.prototype, {
     * @publicName next(direction, allowCrossGroup)
     * @param1 direction:string
     * @param2 allowCrossGroup:bool
-    * @return SummaryCell
+    * @return dxPivotGridSummaryCell
     */
 
     next: function(direction, allowCrossGroup) {
@@ -297,7 +297,7 @@ SummaryCell.prototype = extend(SummaryCell.prototype, {
     * @name dxPivotGridSummaryCell_prev
     * @publicName prev(direction)
     * @param1 direction:string
-    * @return SummaryCell
+    * @return dxPivotGridSummaryCell
     */
 
     /**
@@ -305,7 +305,7 @@ SummaryCell.prototype = extend(SummaryCell.prototype, {
     * @publicName prev(direction, allowCrossGroup)
     * @param1 direction:string
     * @param2 allowCrossGroup:bool
-    * @return SummaryCell
+    * @return dxPivotGridSummaryCell
     */
 
     prev: function(direction, allowCrossGroup) {
@@ -361,7 +361,7 @@ SummaryCell.prototype = extend(SummaryCell.prototype, {
     * @publicName child(direction, fieldValue)
     * @param1 direction:string
     * @param2 fieldValue:number|string
-    * @return SummaryCell
+    * @return dxPivotGridSummaryCell
     */
     child: function(direction, fieldValue) {
         var children = this.children(direction),
@@ -381,7 +381,7 @@ SummaryCell.prototype = extend(SummaryCell.prototype, {
    * @publicName slice(field, value)
    * @param1 field:PivotGridField
    * @param2 value:number|string
-   * @return SummaryCell
+   * @return dxPivotGridSummaryCell
    */
     slice: function(field, value) {
         var that = this,
