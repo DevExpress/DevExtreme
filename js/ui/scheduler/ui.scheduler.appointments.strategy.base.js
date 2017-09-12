8443457c6f3ec1f41a23fe7056e14d35e1f9ee44
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2017-09-12 12:48:03 +0300

    fix tests

--- js/ui/scheduler/ui.scheduler.appointments.strategy.base.js
+++ js/ui/scheduler/ui.scheduler.appointments.strategy.base.js
@@ -452,8 +452,9 @@ var BaseRenderingStrategy = Class.inherit({
 
     _checkWrongEndDate: function(appointment, startDate, endDate) {
         if(!endDate || startDate.getTime() >= endDate.getTime()) {
-            endDate = new Date(startDate.getTime() + this.instance.getAppointmentDurationInMinutes() * 60000);
-            this.instance.fire("setField", "endDate", appointment, endDate);
+            endDate = new Date(startDate.getTime() + this.instance.option("appointmentDurationInMinutes") * 60000);
+            this.instance.invoke("setField", "endDate", appointment, endDate);
+
         }
 
         return endDate;
