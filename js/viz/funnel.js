f75b5eb7d32d435938864087ff587870a08cd782
BingoRUS <bingofirst@gmail.com>
2017-09-12 23:13:19 +0300

    dxFunnel module fixes

--- js/viz/funnel.js
+++ js/viz/funnel.js
@@ -1,13 +1,14 @@
 "use strict";
 
-var Funnel = require("./funnel/funnel");
+var dxFunnel = require("./funnel/funnel");
 
-Funnel.addPlugin(require("./funnel/label").plugin);
-Funnel.addPlugin(require("./core/export").plugin);
-Funnel.addPlugin(require("./core/title").plugin);
-Funnel.addPlugin(require("./components/legend").plugin);
-Funnel.addPlugin(require("./funnel/tracker").plugin);
-Funnel.addPlugin(require("./funnel/tooltip").plugin);
-Funnel.addPlugin(require("./core/loading_indicator").plugin);
+dxFunnel.addPlugin(require("./funnel/label").plugin);
+dxFunnel.addPlugin(require("./core/export").plugin);
+dxFunnel.addPlugin(require("./core/title").plugin);
+dxFunnel.addPlugin(require("./components/legend").plugin);
+dxFunnel.addPlugin(require("./funnel/tracker").plugin);
+dxFunnel.addPlugin(require("./funnel/tooltip").plugin);
+dxFunnel.addPlugin(require("./core/loading_indicator").plugin);
 
-module.exports = Funnel;
+registerComponent("dxFunnel", dxFunnel);
+module.exports = dxFunnel;
