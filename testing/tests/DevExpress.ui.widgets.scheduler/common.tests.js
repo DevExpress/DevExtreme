4c4177c6f457e660997bebf1c03d0902e8345a0b
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2017-09-27 10:02:13 +0300

    fix tests after merge

--- testing/tests/DevExpress.ui.widgets.scheduler/common.tests.js
+++ testing/tests/DevExpress.ui.widgets.scheduler/common.tests.js
@@ -1068,7 +1068,7 @@ QUnit.testStart(function() {
             indicatorTime: new Date(2017, 8, 19)
         });
 
-        var workSpaceWeek = this.instance.element().find(".dx-scheduler-work-space").dxSchedulerWorkSpaceWeek("instance");
+        var workSpaceWeek = this.instance.$element().find(".dx-scheduler-work-space").dxSchedulerWorkSpaceWeek("instance");
 
         assert.deepEqual(workSpaceWeek.option("indicatorTime"), new Date(2017, 8, 19), "workspace has correct indicatorTime");
 
@@ -1083,7 +1083,7 @@ QUnit.testStart(function() {
             indicatorUpdateInterval: 2000
         });
 
-        var workSpaceWeek = this.instance.element().find(".dx-scheduler-work-space").dxSchedulerWorkSpaceWeek("instance");
+        var workSpaceWeek = this.instance.$element().find(".dx-scheduler-work-space").dxSchedulerWorkSpaceWeek("instance");
 
         assert.equal(workSpaceWeek.option("indicatorUpdateInterval"), 2000, "workspace has correct indicatorUpdateInterval");
 
