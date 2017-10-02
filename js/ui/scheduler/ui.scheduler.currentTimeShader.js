9af6b3ec768f51f2c244895118ec7042371e4b8d
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2017-10-02 11:41:47 +0300

    deletion

--- js/ui/scheduler/ui.scheduler.currentTimeShader.js
+++ js/ui/scheduler/ui.scheduler.currentTimeShader.js
@@ -31,11 +31,9 @@ var currentTimeShader = Class.inherit({
     _renderVerticalShader: function() {
         var shaderHeight = this._workspace._getShaderHeight(),
             maxHeight = this._$container.outerHeight();
-            //renderIndicatorLine = true;
 
         if(shaderHeight > maxHeight) {
             shaderHeight = maxHeight;
-            //renderIndicatorLine = false;
         }
 
         if(shaderHeight > 0) {
@@ -45,9 +43,6 @@ var currentTimeShader = Class.inherit({
             var shaderWidth = this._workspace._getShaderWidth();
             var groupCount = this._workspace._getGroupCount() || 1;
             for(var i = 0; i < groupCount; i++) {
-                // if(renderIndicatorLine) {
-                //     this._renderIndicatorLine(this._$indicator, indicatorHeight, indicatorWidth, i, true);
-                // }
                 this._renderTopShader(this._$shader, shaderHeight, shaderWidth, i);
 
                 this._renderBottomShader(this._$shader, maxHeight - shaderHeight, shaderWidth, i);
@@ -62,7 +57,7 @@ var currentTimeShader = Class.inherit({
         width && this._$topShader.width(width) && this._$topShader.height(height);
 
         this._$topShader.css("marginTop", -this._$container.outerHeight() * i);
-        this._$topShader.css("left", this._workspace._getCellCount() * this._workspace.getCellWidth() * i);
+        this._$topShader.css("left", this._workspace._getCellCount() * this._workspace._getRoundedCellWidth() * i);
 
         $shader.append(this._$topShader);
     },
@@ -71,7 +66,7 @@ var currentTimeShader = Class.inherit({
         this._$bottomShader = $("<div>").addClass(DATE_TIME_SHADER_BOTTOM_CLASS);
         this._$bottomShader.width(width - this._workspace.getCellWidth()) && this._$bottomShader.height(height);
 
-        this._$bottomShader.css("left", this._workspace._getCellCount() * this._workspace.getCellWidth() * i);
+        this._$bottomShader.css("left", this._workspace._getCellCount() * this._workspace._getRoundedCellWidth() * i);
 
         $shader.append(this._$bottomShader);
     },
@@ -81,7 +76,7 @@ var currentTimeShader = Class.inherit({
             this._$allDayIndicator = $("<div>").addClass(DATE_TIME_SHADER_ALL_DAY_CLASS);
             this._$allDayIndicator.height(this._workspace.getAllDayHeight());
             this._$allDayIndicator.width(shaderWidth);
-            this._$allDayIndicator.css("left", this._workspace._getCellCount() * this._workspace.getCellWidth() * i);
+            this._$allDayIndicator.css("left", this._workspace._getCellCount() * this._workspace._getRoundedCellWidth() * i);
 
             this._workspace._$allDayPanel.prepend(this._$allDayIndicator);
         }
@@ -90,20 +85,14 @@ var currentTimeShader = Class.inherit({
     _renderHorizontalShader: function() {
         var shaderWidth = this._workspace._getShaderWidth(),
             maxWidth = this._$container.outerWidth();
-            //renderIndicatorLine = true;
 
         if(shaderWidth > maxWidth) {
             shaderWidth = maxWidth;
-            //renderIndicatorLine = false;
         }
 
         if(shaderWidth > 0) {
             this._$shader = $("<div>").addClass(DATE_TIME_SHADER_CLASS);
             this._$shader.width(shaderWidth);
-
-            // if(renderIndicatorLine) {
-            //     this._renderIndicatorLine(this._$indicator, this._$container.outerHeight(), indicatorWidth, 0);
-            // }
         }
     },
 
