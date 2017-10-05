0e60f220a957a17d4b5f12237cc9bfb44c4dc6f9
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2017-10-05 12:04:42 +0300

    fix timelineWeek

--- js/ui/scheduler/ui.scheduler.timeline_week.js
+++ js/ui/scheduler/ui.scheduler.timeline_week.js
@@ -6,8 +6,7 @@ var $ = require("../../core/renderer"),
 
 var TIMELINE_CLASS = "dx-scheduler-timeline-week",
     HEADER_PANEL_CELL_CLASS = "dx-scheduler-header-panel-cell",
-    HEADER_ROW_CLASS = "dx-scheduler-header-row",
-    CELL_WIDTH = 200;
+    HEADER_ROW_CLASS = "dx-scheduler-header-row";
 
 var SchedulerTimelineWeek = SchedulerTimeline.inherit({
     _getElementClass: function() {
@@ -25,7 +24,7 @@ var SchedulerTimelineWeek = SchedulerTimeline.inherit({
             $cells = [],
             colspan = this._getCellCountInDay(),
             cellTemplate = this.option("dateCellTemplate"),
-            headerCellWidth = colspan * CELL_WIDTH;
+            headerCellWidth = colspan * this._getHeaderPanelCellWidth($headerRow);
 
         for(var i = 0; i < this._getWeekDuration() * this.option("intervalCount"); i++) {
             var $th = $("<th>"),
@@ -54,6 +53,10 @@ var SchedulerTimelineWeek = SchedulerTimeline.inherit({
 
         var $row = $("<tr>").addClass(HEADER_ROW_CLASS).append($cells);
         $headerRow.before($row);
+    },
+
+    _getHeaderPanelCellWidth: function($headerRow) {
+        return $headerRow.children().first().outerWidth();
     },
 
     _getWeekDuration: function() {
