b75bbd0f922df49e174b3cb73fc33c465e6e5a4e
Dmitry Semenov <jsdmitry@users.noreply.github.com>
2017-10-09 17:18:39 +0300

    Fix setRowsOpacity for fixed columns

--- js/ui/grid_core/ui.grid_core.column_fixing.js
+++ js/ui/grid_core/ui.grid_core.column_fixing.js
@@ -625,6 +625,13 @@ var RowsViewFixedColumnsExtender = extend({}, baseFixedColumns, {
         }
     },
 
+    setRowsOpacity: function(columnIndex, value) {
+        this.callBase(columnIndex, value);
+
+        var $rows = this._getRowElements(this._fixedTableElement);
+        this._setRowOpacityCore($rows, this.getFixedColumns(), columnIndex, value);
+    },
+
     optionChanged: function(args) {
         var that = this;
 
--- js/ui/grid_core/ui.grid_core.rows.js
+++ js/ui/grid_core/ui.grid_core.rows.js
@@ -917,6 +917,26 @@ module.exports = {
                     return parameters;
                 },
 
+                _setRowOpacityCore: function($rows, visibleColumns, columnIndex, value) {
+                    var columnsController = this._columnsController,
+                        columns = columnsController.getColumns(),
+                        column = columns && columns[columnIndex],
+                        columnID = column && column.isBand && column.index;
+
+                    $.each($rows, function(rowIndex, row) {
+                        if(!$(row).hasClass(GROUP_ROW_CLASS)) {
+                            for(var i = 0; i < visibleColumns.length; i++) {
+                                if(commonUtils.isNumeric(columnID) && columnsController.isParentBandColumn(visibleColumns[i].index, columnID) || visibleColumns[i].index === columnIndex) {
+                                    $rows.eq(rowIndex).children().eq(i).css({ opacity: value });
+                                    if(!commonUtils.isNumeric(columnID)) {
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    });
+                },
+
                 renderNoDataText: gridCoreUtils.renderNoDataText,
 
                 getCellOptions: function(rowIndex, columnIdentifier) {
@@ -1197,27 +1217,8 @@ module.exports = {
                 },
 
                 setRowsOpacity: function(columnIndex, value) {
-                    var that = this,
-                        i,
-                        columnsController = that._columnsController,
-                        visibleColumns = that.getColumns(),
-                        columns = columnsController.getColumns(),
-                        column = columns && columns[columnIndex],
-                        columnID = column && column.isBand && column.index,
-                        $rows = that._getRowElements().not("." + GROUP_ROW_CLASS) || [];
-
-                    $.each($rows, function(rowIndex, row) {
-                        if(!$(row).hasClass(GROUP_ROW_CLASS)) {
-                            for(i = 0; i < visibleColumns.length; i++) {
-                                if(commonUtils.isNumeric(columnID) && columnsController.isParentBandColumn(visibleColumns[i].index, columnID) || visibleColumns[i].index === columnIndex) {
-                                    $rows.eq(rowIndex).children().eq(i).css({ opacity: value });
-                                    if(!commonUtils.isNumeric(columnID)) {
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                    });
+                    var $rows = this._getRowElements().not("." + GROUP_ROW_CLASS) || [];
+                    this._setRowOpacityCore($rows, this.getColumns(), columnIndex, value);
                 },
 
                 _getCellElementsCore: function(rowIndex) {
