58f46ab1dca2074628802826905d7a3206b2ec2f
salimov.alyar <alyar666@users.noreply.github.com>
2017-10-16 17:01:09 +0300

    dxFilterBuilder - Fix IE tests

--- testing/tests/DevExpress.ui.widgets/filterBuilderParts/commonTests.js
+++ testing/tests/DevExpress.ui.widgets/filterBuilderParts/commonTests.js
@@ -23,19 +23,20 @@ var clickByButtonAndSelectMenuItem = function($button, menuItemIndex) {
 
 QUnit.module("Rendering", function() {
     QUnit.test("markup init", function(assert) {
-        var etalon =
-        '<div id="container" class="dx-filterbuilder dx-widget">'
-            + '<div class="dx-filterbuilder-group">'
-                + '<div class="dx-filterbuilder-group-item">'
-                    + '<div class="dx-filterbuilder-text dx-filterbuilder-group-operation" tabindex="0">And</div>'
-                    + '<div class="dx-filterbuilder-action-icon dx-icon-plus dx-filterbuilder-action" tabindex="0"></div>'
+        var $etalon = $("<div/>").html(
+            '<div id="container" class="dx-filterbuilder dx-widget">'
+                + '<div class="dx-filterbuilder-group">'
+                    + '<div class="dx-filterbuilder-group-item">'
+                        + '<div class="dx-filterbuilder-text dx-filterbuilder-group-operation" tabindex="0">And</div>'
+                        + '<div class="dx-filterbuilder-action-icon dx-icon-plus dx-filterbuilder-action" tabindex="0"></div>'
+                    + '</div>'
+                    + '<div class="dx-filterbuilder-group-content"></div>'
                 + '</div>'
-                + '<div class="dx-filterbuilder-group-content"></div>'
             + '</div>'
-        + '</div>';
+        );
 
         var element = $("#container").dxFilterBuilder();
-        assert.equal(element.parent().html(), etalon);
+        assert.equal(element.parent().html(), $etalon.html());
     });
 
     QUnit.test("filterbuilder is created by different values", function(assert) {
@@ -60,62 +61,64 @@ QUnit.module("Rendering", function() {
     });
 
     QUnit.test("filter Content init by one condition", function(assert) {
-        var etalon =
-        '<div class=\"dx-filterbuilder-group\">'
-            + '<div class=\"dx-filterbuilder-group-item\">'
-                + '<div class=\"dx-filterbuilder-action-icon dx-icon-remove dx-filterbuilder-action\" tabindex=\"0\"></div>'
-                + '<div class="dx-filterbuilder-text dx-filterbuilder-group-operation" tabindex="0">Or</div>'
-                + '<div class="dx-filterbuilder-action-icon dx-icon-plus dx-filterbuilder-action" tabindex="0"></div>'
-            + '</div>'
-            + '<div class="dx-filterbuilder-group-content">'
-                + '<div class="dx-filterbuilder-group">'
-                    + '<div class=\"dx-filterbuilder-group-item\">'
-                        + '<div class=\"dx-filterbuilder-action-icon dx-icon-remove dx-filterbuilder-action\" tabindex=\"0\"></div>'
-                        + '<div class=\"dx-filterbuilder-text dx-filterbuilder-item-field\" tabindex=\"0\">Company Name</div>'
-                        + '<div class=\"dx-filterbuilder-text dx-filterbuilder-item-operation\" tabindex=\"0\">Equals</div>'
-                        + '<div class=\"dx-filterbuilder-text dx-filterbuilder-item-value\">'
-                            + '<div class=\"dx-filterbuilder-item-value-text\" tabindex=\"0\">K&amp;S Music</div>'
+        var $etalon = $("<div/>").html(
+            '<div class=\"dx-filterbuilder-group\">'
+                + '<div class=\"dx-filterbuilder-group-item\">'
+                    + '<div class=\"dx-filterbuilder-action-icon dx-icon-remove dx-filterbuilder-action\" tabindex=\"0\"></div>'
+                    + '<div class="dx-filterbuilder-text dx-filterbuilder-group-operation" tabindex="0">Or</div>'
+                    + '<div class="dx-filterbuilder-action-icon dx-icon-plus dx-filterbuilder-action" tabindex="0"></div>'
+                + '</div>'
+                + '<div class="dx-filterbuilder-group-content">'
+                    + '<div class="dx-filterbuilder-group">'
+                        + '<div class=\"dx-filterbuilder-group-item\">'
+                            + '<div class=\"dx-filterbuilder-action-icon dx-icon-remove dx-filterbuilder-action\" tabindex=\"0\"></div>'
+                            + '<div class=\"dx-filterbuilder-text dx-filterbuilder-item-field\" tabindex=\"0\">Company Name</div>'
+                            + '<div class=\"dx-filterbuilder-text dx-filterbuilder-item-operation\" tabindex=\"0\">Equals</div>'
+                            + '<div class=\"dx-filterbuilder-text dx-filterbuilder-item-value\">'
+                                + '<div class=\"dx-filterbuilder-item-value-text\" tabindex=\"0\">K&amp;S Music</div>'
+                            + '</div>'
                         + '</div>'
                     + '</div>'
                 + '</div>'
             + '</div>'
-        + '</div>';
+        );
 
         var element = $("#container").dxFilterBuilder({
             fields: fields,
             value: [[["CompanyName", "=", "K&S Music"], "Or"], "And"]
         });
-        assert.equal(element.find("." + FILTER_BUILDER_GROUP_CONTENT_CLASS).html(), etalon);
+        assert.equal(element.find("." + FILTER_BUILDER_GROUP_CONTENT_CLASS).html(), $etalon.html());
     });
 
     QUnit.test("filter Content init by several conditions", function(assert) {
-        var etalon =
-                '<div class="dx-filterbuilder-group">'
-                    + '<div class=\"dx-filterbuilder-group-item\">'
-                        + '<div class=\"dx-filterbuilder-action-icon dx-icon-remove dx-filterbuilder-action\" tabindex=\"0\"></div>'
-                        + '<div class=\"dx-filterbuilder-text dx-filterbuilder-item-field\" tabindex=\"0\">Company Name</div>'
-                        + '<div class=\"dx-filterbuilder-text dx-filterbuilder-item-operation\" tabindex=\"0\">Equals</div>'
-                        + '<div class=\"dx-filterbuilder-text dx-filterbuilder-item-value\">'
-                            + '<div class=\"dx-filterbuilder-item-value-text\" tabindex=\"0\">K&amp;S Music</div>'
-                        + '</div>'
+        var $etalon = $("<div/>").html(
+            '<div class="dx-filterbuilder-group">'
+                + '<div class=\"dx-filterbuilder-group-item\">'
+                    + '<div class=\"dx-filterbuilder-action-icon dx-icon-remove dx-filterbuilder-action\" tabindex=\"0\"></div>'
+                    + '<div class=\"dx-filterbuilder-text dx-filterbuilder-item-field\" tabindex=\"0\">Company Name</div>'
+                    + '<div class=\"dx-filterbuilder-text dx-filterbuilder-item-operation\" tabindex=\"0\">Equals</div>'
+                    + '<div class=\"dx-filterbuilder-text dx-filterbuilder-item-value\">'
+                        + '<div class=\"dx-filterbuilder-item-value-text\" tabindex=\"0\">K&amp;S Music</div>'
                     + '</div>'
                 + '</div>'
-                + '<div class="dx-filterbuilder-group">'
-                    + '<div class=\"dx-filterbuilder-group-item\">'
-                        + '<div class=\"dx-filterbuilder-action-icon dx-icon-remove dx-filterbuilder-action\" tabindex=\"0\"></div>'
-                        + '<div class=\"dx-filterbuilder-text dx-filterbuilder-item-field\" tabindex=\"0\">Zipcode</div>'
-                        + '<div class=\"dx-filterbuilder-text dx-filterbuilder-item-operation\" tabindex=\"0\">Equals</div>'
-                        + '<div class=\"dx-filterbuilder-text dx-filterbuilder-item-value\">'
-                            + '<div class=\"dx-filterbuilder-item-value-text\" tabindex=\"0\">98027</div>'
-                        + '</div>'
+            + '</div>'
+            + '<div class="dx-filterbuilder-group">'
+                + '<div class=\"dx-filterbuilder-group-item\">'
+                    + '<div class=\"dx-filterbuilder-action-icon dx-icon-remove dx-filterbuilder-action\" tabindex=\"0\"></div>'
+                    + '<div class=\"dx-filterbuilder-text dx-filterbuilder-item-field\" tabindex=\"0\">Zipcode</div>'
+                    + '<div class=\"dx-filterbuilder-text dx-filterbuilder-item-operation\" tabindex=\"0\">Equals</div>'
+                    + '<div class=\"dx-filterbuilder-text dx-filterbuilder-item-value\">'
+                        + '<div class=\"dx-filterbuilder-item-value-text\" tabindex=\"0\">98027</div>'
                     + '</div>'
-                + '</div>';
+                + '</div>'
+            + '</div>'
+        );
 
         var element = $("#container").dxFilterBuilder({
             fields: fields,
             value: [["CompanyName", "=", "K&S Music"], "Or", ["Zipcode", "=", "98027"]]
         });
-        assert.equal(element.find("." + FILTER_BUILDER_GROUP_CONTENT_CLASS).html(), etalon);
+        assert.equal(element.find("." + FILTER_BUILDER_GROUP_CONTENT_CLASS).html(), $etalon.html());
     });
 
     QUnit.test("value and operations depend on selected field", function(assert) {
