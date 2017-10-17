b763b56d2c0ca0ca859f09bd0af6e6b1bab313d2
eugen.zha <nightskylark@users.noreply.github.com>
2017-10-17 19:15:11 +0300

    Change import order in testing environment

--- testing/tests/DevExpress.core/config_predefined.tests.js
+++ testing/tests/DevExpress.core/config_predefined.tests.js
@@ -1,23 +1,15 @@
 "use strict";
 
-window.DevExpress = window.DevExpress || {};
-window.DevExpress.config = {
-    useJQuery: false
-};
+var config = require("core/config");
 
-define(function(require) {
-    var config = require("core/config");
+QUnit.module("predefined config");
 
-    QUnit.module("predefined config");
+QUnit.test("Get default value from DevExpress.config object", function(assert) {
+    var originalConfig = config();
 
-    QUnit.test("Get default value from DevExpress.config object", function(assert) {
-        var originalConfig = config();
-
-        try {
-            assert.strictEqual(originalConfig.useJQuery, false);
-        } finally {
-            config(originalConfig);
-        }
-    });
-
+    try {
+        assert.strictEqual(originalConfig.useJQuery, !QUnit.urlParams["nojquery"]);
+    } finally {
+        config(originalConfig);
+    }
 });
