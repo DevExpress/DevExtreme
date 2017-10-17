4f5616d80b1a8743adae39a6c8daa4e26b7a669f
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2017-10-17 14:50:30 +0300

    Fix case in ts declarations (#1599)

--- ts/viz-funnel.d.ts
+++ ts/viz-funnel.d.ts
@@ -5,162 +5,162 @@
 declare module DevExpress.viz.funnel {
 
     export interface Legend extends viz.core.BaseLegend {
-        /** @docid dxfunneloptions_legend_customizehint */
+        /** @docid dxFunneloptions_legend_customizehint */
         customizeHint?: (itemInfo: { item: funnelItem; text: string; }) => string;
 
-        /** @docid dxfunneloptions_legend_customizetext */
+        /** @docid dxFunneloptions_legend_customizetext */
         customizeText?: (itemInfo: { item: funnelItem; text: string; }) => string;
     }
 
-    /** @docid dxfunnelItem */
+    /** @docid dxFunnelItem */
     export interface funnelItem {
 
-    /** @docid dxfunnelItemfields_data */
+    /** @docid dxFunnelItemfields_data */
         data: Object;
 
-        /** @docid dxfunnelItemfields_argument */
+        /** @docid dxFunnelItemfields_argument */
         argument: any;
 
-        /** @docid dxfunnelItemfields_percent */
+        /** @docid dxFunnelItemfields_percent */
         percent: number;
 
-        /** @docid dxfunnelItemfields_value */
+        /** @docid dxFunnelItemfields_value */
         value: number;
 
-        /** @docid dxfunnelItemmethods_select */
+        /** @docid dxFunnelItemmethods_select */
         select(state: boolean): void;
 
-        /** @docid dxfunnelItemmethods_hover */
+        /** @docid dxFunnelItemmethods_hover */
         hover(state: boolean): void;
 
-        /** @docid dxfunnelItemmethods_getcolor */
+        /** @docid dxFunnelItemmethods_getcolor */
         getColor(): string;
 
-        /** @docid dxfunnelItemmethods_ishovered */
+        /** @docid dxFunnelItemmethods_ishovered */
         isHovered(): boolean;
 
-        /** @docid dxfunnelItemmethods_isselected */
+        /** @docid dxFunnelItemmethods_isselected */
         isSelected(): boolean;
 
-        /** @docid dxfunnelItemmethods_showtooltip */
+        /** @docid dxFunnelItemmethods_showtooltip */
         showTooltip(): void;
     }
-    /** @docid dxfunnel_options */
+    /** @docid dxFunnel_options */
     export interface dxFunnelOptions extends viz.core.BaseWidgetOptions, viz.core.RedrawOnResizeOptions, viz.core.TitleOptions, viz.core.LoadingIndicatorOptions, viz.core.ExportOptions {
-        /** @docid dxfunneloptions_adaptivelayout */
+        /** @docid dxFunneloptions_adaptivelayout */
         adaptiveLayout?: {
-            /** @docid dxfunneloptions_adaptivelayout_width */
+            /** @docid dxFunneloptions_adaptivelayout_width */
             width?: number;
-            /** @docid dxfunneloptions_adaptivelayout_height */
+            /** @docid dxFunneloptions_adaptivelayout_height */
             height?: number;
-            /** @docid dxfunneloptions_adaptivelayout_keeplabels */
+            /** @docid dxFunneloptions_adaptivelayout_keeplabels */
             keepLabels?: boolean;
         };
 
-        /** @docid dxfunneloptions_datasource */
+        /** @docid dxFunneloptions_datasource */
         dataSource?: any;
 
-        /** @docid dxfunneloptions_valuefield */
+        /** @docid dxFunneloptions_valuefield */
         valueField?: string;
 
-        /** @docid dxfunneloptions_colorfield */
+        /** @docid dxFunneloptions_colorfield */
         colorField?: string;
 
-        /** @docid dxfunneloptions_hoverenabled */
+        /** @docid dxFunneloptions_hoverenabled */
         hoverEnabled?: boolean;
 
-        /** @docid dxfunneloptions_argumentfield */
+        /** @docid dxFunneloptions_argumentfield */
         argumentField?: string;
 
-        /** @docid dxfunneloptions_selectionmode */
+        /** @docid dxFunneloptions_selectionmode */
         selectionMode?: string;
 
-        /** @docid dxfunneloptions_palette */
+        /** @docid dxFunneloptions_palette */
         palette?: any;
 
-        /** @docid dxfunneloptions_algorithm */
+        /** @docid dxFunneloptions_algorithm */
         algorithm?: string;
 
-        /** @docid dxfunneloptions_neckheight */
+        /** @docid dxFunneloptions_neckheight */
         neckHeight?: number;
 
-        /** @docid dxfunneloptions_neckwidth */
+        /** @docid dxFunneloptions_neckwidth */
         neckWidth?: number;
 
-        /** @docid dxfunneloptions_inverted */
+        /** @docid dxFunneloptions_inverted */
         inverted?: boolean;
 
-        /** @docid dxfunneloptions_sortdata */
+        /** @docid dxFunneloptions_sortdata */
         sortData?: boolean;
 
-        /** @docid dxfunneloptions_item */
+        /** @docid dxFunneloptions_item */
         item?: {
 
-            /** @docid dxfunneloptions_item_border */
+            /** @docid dxFunneloptions_item_border */
             border?: viz.core.Border;
 
-            /** @docid dxfunneloptions_item_hoverstyle */
+            /** @docid dxFunneloptions_item_hoverstyle */
             hoverStyle?: {
-                /** @docid dxfunneloptions_item_hoverstyle_border */
+                /** @docid dxFunneloptions_item_hoverstyle_border */
                 border?: viz.core.Border;
 
-                /** @docid dxfunneloptions_item_hoverstyle_hatching */
+                /** @docid dxFunneloptions_item_hoverstyle_hatching */
                 hatching?: viz.core.Hatching;
             };
 
-            /** @docid dxfunneloptions_item_selectionstyle */
+            /** @docid dxFunneloptions_item_selectionstyle */
             selectionStyle?: {
 
-                /** @docid dxfunneloptions_item_selectionstyle_border */
+                /** @docid dxFunneloptions_item_selectionstyle_border */
                 border?: viz.core.Border;
 
-                /** @docid dxfunneloptions_item_selectionstyle_hatching */
+                /** @docid dxFunneloptions_item_selectionstyle_hatching */
                 hatching?: viz.core.Hatching;
             };
         };
 
-        /** @docid dxfunneloptions_label */
+        /** @docid dxFunneloptions_label */
         label?: {
-            /** @docid dxfunneloptions_label_font */
+            /** @docid dxFunneloptions_label_font */
             font?: viz.core.Font;
 
-            /** @docid dxfunneloptions_label_position */
+            /** @docid dxFunneloptions_label_position */
             position?: string;
 
-            /** @docid dxfunneloptions_label_horizontaloffset */
+            /** @docid dxFunneloptions_label_horizontaloffset */
             horizontalOffset?: number;
 
-            /** @docid dxfunneloptions_label_horizontalalignment */
+            /** @docid dxFunneloptions_label_horizontalalignment */
             horizontalAlignment?: string;
 
-            /** @docid dxfunneloptions_label_connector */
+            /** @docid dxFunneloptions_label_connector */
             connector?: {
-                /** @docid dxfunneloptions_label_connector_color */
+                /** @docid dxFunneloptions_label_connector_color */
                 color?: string;
 
-                /** @docid dxfunneloptions_label_connector_visible */
+                /** @docid dxFunneloptions_label_connector_visible */
                 visible?: boolean;
 
-                /** @docid dxfunneloptions_label_connector_width */
+                /** @docid dxFunneloptions_label_connector_width */
                 width?: number;
 
-                /** @docid dxfunneloptions_label_connector_opacity */
+                /** @docid dxFunneloptions_label_connector_opacity */
                 opacity?: number;
             };
 
-            /** @docid dxfunneloptions_label_backgroundcolor */
+            /** @docid dxFunneloptions_label_backgroundcolor */
             backgroundColor?: string;
 
-            /** @docid dxfunneloptions_label_border */
+            /** @docid dxFunneloptions_label_border */
             border?: viz.core.DashedBorder;
 
-            /** @docid dxfunneloptions_label_visible */
+            /** @docid dxFunneloptions_label_visible */
             visible?: boolean;
 
-            /** @docid dxfunneloptions_label_showforzerovalues */
+            /** @docid dxFunneloptions_label_showforzerovalues */
             showForZeroValues?: boolean;
 
-            /** @docid dxfunneloptions_label_customizetext */
+            /** @docid dxFunneloptions_label_customizetext */
             customizeText?: (itemInfo: {
                 item: funnelItem;
                 valueText: string;
@@ -169,53 +169,53 @@ declare module DevExpress.viz.funnel {
                 percentText: string;
             }) => string;
 
-            /** @docid dxfunneloptions_label_format */
+            /** @docid dxFunneloptions_label_format */
             format?: any;
         };
-        /** @docid dxfunneloptions_legend */
+        /** @docid dxFunneloptions_legend */
         legend?: Legend;
 
-        /** @docid  dxfunneloptions_onitemclick */
+        /** @docid  dxFunneloptions_onitemclick */
         onItemClick?: any;
 
-        /** @docid  dxfunneloptions_onlegendclick */
+        /** @docid  dxFunneloptions_onlegendclick */
         onLegendClick?: any;
 
-        /** @docid  dxfunneloptions_onhoverchanged */
+        /** @docid  dxFunneloptions_onhoverchanged */
         onHoverChanged?: (e: {
             component: dxFunnel;
             element: Element;
             node: funnelItem;
         }) => void;
 
-        /** @docid  dxfunneloptions_onselectionchanged */
+        /** @docid  dxFunneloptions_onselectionchanged */
         onSelectionChanged?: (e: {
             component: dxFunnel;
             element: Element;
             node: funnelItem;
         }) => void;
 
-        /** @docid dxfunneloptions_tooltip */
+        /** @docid dxFunneloptions_tooltip */
         tooltip?: viz.core.Tooltip;
     }
 }
 
 declare module DevExpress.viz {
-    /** @docid dxfunnel */
+    /** @docid dxFunnel */
     export class dxFunnel extends viz.core.BaseWidget implements viz.core.LoadingIndicatorMethods {
         constructor(element: JQuery, options?: DevExpress.viz.funnel.dxFunnelOptions);
         constructor(element: Element, options?: DevExpress.viz.funnel.dxFunnelOptions);
 
-        /** @docid dxfunnelmethods_clearselection */
+        /** @docid dxFunnelmethods_clearselection */
         clearSelection(): void;
 
-        /** @docid dxfunnelmethods_getallitems */
+        /** @docid dxFunnelmethods_getallitems */
         getAllItems(): Array<DevExpress.viz.funnel.funnelItem>;
 
-        /** @docid dxfunnelmethods_hidetooltip */
+        /** @docid dxFunnelmethods_hidetooltip */
         hideTooltip(): void;
 
-        /** @docid dxfunnelmethods_getdatasource */
+        /** @docid dxFunnelmethods_getdatasource */
         getDataSource(): DevExpress.data.DataSource;
 
         showLoadingIndicator(): void;
