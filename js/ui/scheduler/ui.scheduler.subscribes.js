4fff1e07502190f1cd0190d135ffae47b4c2d62e
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2017-10-30 10:49:49 +0300

    Fix wrong appointments displaying on the agenda view in Scheduler (T566363) (#1760)

--- js/ui/scheduler/ui.scheduler.subscribes.js
+++ js/ui/scheduler/ui.scheduler.subscribes.js
@@ -374,8 +374,7 @@ var subscribes = {
         }
 
         if(updatedStartDate.getHours() < startDayHour) {
-            updatedStartDate.setHours(startDayHour);
-            updatedStartDate.setMinutes(0);
+            updatedStartDate.setHours(startDayHour, 0, 0, 0);
         }
 
         options.callback(updatedStartDate);
@@ -387,8 +386,7 @@ var subscribes = {
             updatedEndDate = endDate;
 
         if(endDate.getHours() >= endDayHour) {
-            updatedEndDate.setHours(endDayHour);
-            updatedEndDate.setMinutes(0);
+            updatedEndDate.setHours(endDayHour, 0, 0, 0);
         }
         options.callback(updatedEndDate);
     },
