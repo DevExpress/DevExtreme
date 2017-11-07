078eff258d9da522b4b0a1a05230f31daf151f42
Alyar <Alyar666@users.noreply.github.com>
2017-11-07 16:28:16 +0300

    dxList: Fix "Select All"  incorrectly works with a filtered data source when selectAllMode is "allPages" (T567757) (#1821)

--- js/ui/list/ui.list.edit.search.js
+++ js/ui/list/ui.list.edit.search.js
@@ -6,6 +6,20 @@ var ListEdit = require("./ui.list.edit"),
 var ListSearch = ListEdit.inherit(searchBoxMixin).inherit({
     _addWidgetPrefix: function(className) {
         return "dx-list-" + className;
+    },
+
+    _getCombinedFilter: function() {
+        var filter,
+            storeLoadOptions,
+            dataSource = this._dataSource;
+
+        if(dataSource) {
+            storeLoadOptions = { filter: dataSource.filter() };
+            dataSource._addSearchFilter(storeLoadOptions);
+            filter = storeLoadOptions.filter;
+        }
+
+        return filter;
     }
 });
 
