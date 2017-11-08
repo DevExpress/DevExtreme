0f643dbf0bbae596473ec4f4478aa47eb1913ddd
Eugen Zha <nightskylark@users.noreply.github.com>
2017-11-08 13:23:12 +0300

    Get rid of redundant scope refreshing on widget initialization in AngularJS (#1849)

--- js/integration/angular/component_registrator.js
+++ js/integration/angular/component_registrator.js
@@ -446,11 +446,11 @@ var ComponentBuilder = Class.inherit({
                 }
 
                 if(Config().wrapActionsBeforeExecute) {
-                    safeApply(function() {}, scope);
+                    this._applyAsync(function() {}, scope);
                 }
 
                 return disposeWatcher;
-            },
+            }.bind(this),
             templates: {
                 "dx-polymorph-widget": {
                     render: function(options) {
