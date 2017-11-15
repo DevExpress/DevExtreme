f0fa903b2e6ffddb049478438cba536f2c593e5b
dxvladislavvolkov <dxvladislavvolkov@users.noreply.github.com>
2017-11-15 13:33:02 +0300

    Add doc-comments for  the arguments of beforeSend option (#1950)

--- js/ui/pivot_grid/xmla_store/xmla_store.js
+++ js/ui/pivot_grid/xmla_store/xmla_store.js
@@ -824,11 +824,17 @@ exports.XmlaStore = Class.inherit((function() {
         */
 
         /**
-        * @name XmlaStoreOptions_beforeSend
-        * @publicName beforeSend
-        * @type function(request)
-        * @type_function_param1 request:object
-        */
+         * @name XmlaStoreOptions_beforeSend
+         * @publicName beforeSend
+         * @type function
+         * @type_function_param1 options:object
+         * @type_function_param1_field1 url:string
+         * @type_function_param1_field2 method:string
+         * @type_function_param1_field3 headers:object
+         * @type_function_param1_field4 xhrFields:object
+         * @type_function_param1_field5 data:string
+         * @type_function_param1_field6 dataType:object
+         */
         },
 
         getFields: function() {
