64c0d3f45b9523712fa7c8cf85b23419fd396901
Ksenia Mordalyuk <KsyushaM@users.noreply.github.com>
2017-11-23 13:29:04 +0300

    Fix navigation to the minimum date using the date navigator (T577389)

--- js/ui/scheduler/ui.scheduler.navigator.js
+++ js/ui/scheduler/ui.scheduler.navigator.js
@@ -295,7 +295,10 @@ var SchedulerNavigator = Widget.inherit({
         var min = this.option("min"),
             max = this.option("max");
 
-        this._prev.option("disabled", min && this._getNextDate(-1) <= min);
+        min = min ? dateUtils.trimTime(min) : min;
+        max = max ? dateUtils.trimTime(max) : max;
+
+        this._prev.option("disabled", min && this._getNextDate(-1) < min);
         this._next.option("disabled", max && this._getNextDate(1) >= max);
     },
 
