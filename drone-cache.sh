f237de6d7e0b826a156b70331e1259cc31df7f8a
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2017-11-24 00:36:43 +0300

    Simplify Drone cache (#2048)

--- drone-cache.sh
+++ drone-cache.sh
@@ -1,58 +1,39 @@
 #!/bin/bash
 
+MAIN_REPO="DevExpress/DevExtreme"
+MAIN_BRANCH=$(node -e "console.log(require('./package.json').version.split(/\./g).slice(0, 2).join('_'))")
+
 CACHE_DIRS="node_modules dotnet_packages"
-
-function read_fallback_branch {
-    node -e "console.log(require('./package.json').version.split(/\./g).slice(0, 2).join('_'))" 2> /dev/null || echo "unknown";
-}
-
-function try_upload {
-    local url="http://devextreme-ci-cache.s3.amazonaws.com/$1.tar.lz4"
-    if tar cf - $i | lz4 | curl -Lsf -X PUT -H "x-amz-acl: bucket-owner-full-control" --data-binary @- "$url"; then
-        echo "Uploaded: $url"
-        return 0
-    else
-        echo "Failed to upload: $url"
-        return 1
-    fi
-}
-
-function try_restore {
-    local url="http://devextreme-ci-cache.s3.amazonaws.com/$1.tar.lz4"
-    if curl -Lsf "$url" | lz4 -d | tar xf - 2>/dev/null; then
-        echo "Restored: $url"
-        return 0
-    else
-        echo "Failed to restore: $url"
-        return 1
-    fi
-}
-
-
-if [ -z "$DRONE_REPO" ] || [ -z "$DRONE_BRANCH" ]; then
-    echo "Missing required env"
-    exit 1
-fi
+CACHE_URL="http://devextreme-ci-cache.s3.amazonaws.com/$MAIN_BRANCH"
 
 if [ "$1" == "rebuild" ]; then
-
-    if [ "$DRONE_BUILD_EVENT" != "push" ]; then
-        echo "Skip on $DRONE_BUILD_EVENT"
+    if [ "$DRONE_BUILD_EVENT" != "push" ] || [ "$DRONE_REPO" != "$MAIN_REPO" ] || [ "$DRONE_BRANCH" != "$MAIN_BRANCH" ]; then
+        echo "Skip"
         exit 0
     fi
 
     for i in $CACHE_DIRS; do
-        [ -e $i ] && try_upload "$DRONE_REPO/$DRONE_BRANCH/$i"
+        if [ -e $i ]; then
+            if tar cf - $i | lz4 | curl -Lsf -X PUT -H "x-amz-acl: bucket-owner-full-control" --data-binary @- "$CACHE_URL/$i.tar.lz4"; then
+                echo "Uploaded: $i"
+            else
+                echo "Failed to upload: $i"
+            fi
+        else
+            echo "Does not exist: $i"
+        fi
     done
 
     exit 0
 fi
 
 if [ "$1" == "restore" ]; then
-    FALLBACK_BRANCH=$(read_fallback_branch)
-
     for i in $CACHE_DIRS; do
-        try_restore "$DRONE_REPO/$DRONE_BRANCH/$i" || try_restore "DevExpress/DevExtreme/$FALLBACK_BRANCH/$i"
+        if curl -Lsf "$CACHE_URL/$i.tar.lz4" | lz4 -d | tar xf - 2>/dev/null; then
+            echo "Restored: $i"
+        else
+            echo "Failed to restore: $i"
+        fi
     done
 
     exit 0
