47b56cd0fa40db9a8537a5603023927932d446bb
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2017-11-23 15:54:23 +0300

    See #2038

--- drone-cache.sh
+++ drone-cache.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
 
-CACHE_BUCKET=devextreme-ci-cache
 CACHE_DIRS="node_modules dotnet_packages"
+
+function read_fallback_branch {
+    node -e "console.log(require('./package.json').version.split(/\./g).slice(0, 2).join('_'))" 2> /dev/null || echo "unknown";
+}
+
+function try_upload {
+    local url="http://devextreme-ci-cache.s3.amazonaws.com/$1.tgz"
+    if tar cfz - $i | curl -Lsf -X PUT -H "x-amz-acl: bucket-owner-full-control" --data-binary @- "$url"; then
+        echo "Uploaded: $url"
+        return 0
+    else
+        echo "Failed to upload: $url"
+        return 1
+    fi
+}
+
+function try_restore {
+    local url="http://devextreme-ci-cache.s3.amazonaws.com/$1.tgz"
+    if curl -Lsf "$url" | tar xzf - 2>/dev/null; then
+        echo "Restored: $url"
+        return 0
+    else
+        echo "Failed to restore: $url"
+        return 1
+    fi
+}
+
 
 if [ -z "$DRONE_REPO" ] || [ -z "$DRONE_BRANCH" ]; then
     echo "Missing required env"
@@ -16,27 +42,17 @@ if [ "$1" == "rebuild" ]; then
     fi
 
     for i in $CACHE_DIRS; do
-        if [ -e $i ]; then
-            url="http://$CACHE_BUCKET.s3.amazonaws.com/$DRONE_REPO/$DRONE_BRANCH/$i.tgz"
-            if tar cfz - $i | curl -Lsf -X PUT -H "x-amz-acl: bucket-owner-full-control" --data-binary @- "$url"; then
-                echo "Uploaded: $url"
-            fi
-        else
-            echo "Does not exist: $i"
-        fi
+        [ -e $i ] && try_upload "$DRONE_REPO/$DRONE_BRANCH/$i"
     done
 
     exit 0
 fi
 
 if [ "$1" == "restore" ]; then
+    FALLBACK_BRANCH=$(read_fallback_branch)
+
     for i in $CACHE_DIRS; do
-        url="http://$CACHE_BUCKET.s3.amazonaws.com/$DRONE_REPO/$DRONE_BRANCH/$i.tgz"
-        if curl -Lsf "$url" | tar xzf - 2>/dev/null; then
-            echo "Restored: $url"
-        else
-            echo "Unable to restore: $url"
-        fi
+        try_restore "$DRONE_REPO/$DRONE_BRANCH/$i" || try_restore "DevExpress/DevExtreme/$FALLBACK_BRANCH/$i"
     done
 
     exit 0
