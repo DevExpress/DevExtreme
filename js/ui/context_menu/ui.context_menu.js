4fca778ef385f39a23b86f15021acdeb11513c2c
Eugeniy Maslyaev <mrjohn0011@users.noreply.github.com>
2017-12-07 12:06:29 +0300

    Remove extra space

--- js/ui/context_menu/ui.context_menu.js
+++ js/ui/context_menu/ui.context_menu.js
@@ -988,7 +988,7 @@ var ContextMenu = MenuBase.inherit((function() {
 
         /**
         * @name dxContextMenuMethods_toggle
-         * @publicName toggle(showing)
+        * @publicName toggle(showing)
         * @param1 showing:boolean
         * @return Promise<void>
         */
