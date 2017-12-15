8033344e24d5436810ac2cd26f501e035bcbb44f
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2017-12-15 13:49:02 +0300

    Refactoring

--- js/ui/responsive_box.js
+++ js/ui/responsive_box.js
@@ -479,7 +479,7 @@ var ResponsiveBox = CollectionWidget.inherit({
     },
 
     _needApplyAutoBaseSize: function(item) {
-        return !item.baseSize && (!item.minSize || item.minSize === "none") && (!item.maxSize || item.maxSize === "none");
+        return !item.baseSize && (!item.minSize) && (!item.maxSize || item.maxSize === "none");
     },
 
     _prepareBoxConfig: function(config) {
@@ -611,7 +611,7 @@ var ResponsiveBox = CollectionWidget.inherit({
             result.maxSize += sizeConfig.maxSize;
         }
 
-        result.minSize = result.minSize ? result.minSize : "none";
+        result.minSize = result.minSize ? result.minSize : 0;
         result.maxSize = result.maxSize ? result.maxSize : "none";
 
         this._isSingleColumnScreen() && (result.baseSize = 'auto');
