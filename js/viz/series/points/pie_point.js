df39997c43dee8c3dd74c1192e480fe9c0d45fd7
Pavel Gruba <pavel.gruba@devexpress.com>
2017-12-22 19:41:09 +0300

    Resolve labels overlapping for multiple pie series (T586462) (#2320)

--- js/viz/series/points/pie_point.js
+++ js/viz/series/points/pie_point.js
@@ -207,13 +207,25 @@ module.exports = _extend({}, symbolPoint, {
         return { x: x, y: y };
     },
 
-
     setLabelEllipsis: function() {
         var that = this,
-            bBox = that._label.getBoundingRect(),
-            coord = that._checkHorizontalLabelPosition(bBox, bBox, that._getVisibleArea());
+            label = that._label,
+            box = label.getBoundingRect(),
+            visibleArea = that._getVisibleArea(),
+            width = box.width;
 
-        that._label.fit(bBox.width - _abs(coord.x - bBox.x));
+        if(label.getLayoutOptions().position === "columns" && that.series.index > 0) {
+            width = visibleArea.maxX - that.centerX - that.radiusLabels;
+        } else {
+            if(box.x + width > visibleArea.maxX) {
+                width = visibleArea.maxX - box.x;
+            } else if(box.x < visibleArea.minX) {
+                width = box.x + width - visibleArea.minX;
+            }
+        }
+        if(width < box.width) {
+            label.fit(width);
+        }
     },
 
     setLabelTrackerData: function() {
