260be4eb6ff1e926232e2af9eb7d57968685ae2d
eugen.zha <nightskylark@users.noreply.github.com>
2017-12-27 14:51:17 +0300

    Publish `dxremove` event

--- js/core/remove_event.js
+++ js/core/remove_event.js
@@ -8,6 +8,14 @@ var registerEvent = require("../events/core/event_registrator");
 var eventName = "dxremove";
 var eventPropName = "dxRemoveEvent";
 
+/**
+  * @name ui events_dxremove
+  * @publicName dxremove
+  * @type eventType
+  * @type_function_param1 event:event
+  * @module events/remove
+*/
+
 beforeCleanData(function(elements) {
     elements = [].slice.call(elements);
     for(var i = 0; i < elements.length; i++) {
