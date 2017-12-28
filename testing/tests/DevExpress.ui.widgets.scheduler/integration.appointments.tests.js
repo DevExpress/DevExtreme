21156b2f43d1c358ace1328150e8b9f73924db9c
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2017-12-28 12:44:38 +0300

    Fix appointment collector displaying when ODataStore is used in Scheduler (T589783) (#2359)

--- testing/tests/DevExpress.ui.widgets.scheduler/integration.appointments.tests.js
+++ testing/tests/DevExpress.ui.widgets.scheduler/integration.appointments.tests.js
@@ -3727,6 +3727,63 @@ QUnit.test("DropDown appointment should be painted depend on resource color", fu
     assert.equal(this.getAppointmentColor(ddAppointments.eq(4), "border-left-color"), "#0000ff", "Appointment color is OK");
 });
 
+QUnit.test("DropDown appointment should be painted depend on resource color when resourses store is asynchronous", function(assert) {
+    var appointments = [
+        { startDate: new Date(2015, 2, 4), text: "a", endDate: new Date(2015, 2, 4, 0, 30), roomId: 1 },
+        { startDate: new Date(2015, 2, 4), text: "b", endDate: new Date(2015, 2, 4, 0, 30), roomId: 1 },
+
+        { startDate: new Date(2015, 2, 4), text: "c", endDate: new Date(2015, 2, 4, 0, 30), roomId: 1 },
+        { startDate: new Date(2015, 2, 4), text: "d", endDate: new Date(2015, 2, 4, 0, 30), roomId: 1 },
+        { startDate: new Date(2015, 2, 4), text: "e", endDate: new Date(2015, 2, 4, 0, 30), roomId: 2 },
+        { startDate: new Date(2015, 2, 4), text: "f", endDate: new Date(2015, 2, 4, 0, 30), roomId: 2 },
+        { startDate: new Date(2015, 2, 4), text: "g", endDate: new Date(2015, 2, 4, 0, 30), roomId: 2 }
+    ];
+    this.createInstance({
+        currentDate: new Date(2015, 2, 4),
+        views: ["month"],
+        width: 840,
+        currentView: "month",
+        firstDayOfWeek: 1,
+        resources: [
+            {
+                field: "roomId",
+                allowMultiple: true,
+                dataSource: new DataSource({
+                    store: new CustomStore({
+                        load: function() {
+                            var d = $.Deferred();
+                            setTimeout(function() {
+                                d.resolve([
+                                    { id: 1, text: "Room 1", color: "#ff0000" },
+                                    { id: 2, text: "Room 2", color: "#0000ff" }
+                                ]);
+                            }, 300);
+
+                            return d.promise();
+                        }
+                    })
+                })
+            }
+        ]
+    });
+
+    sinon.stub(this.instance.getRenderingStrategyInstance(), "_getMaxNeighborAppointmentCount").returns(4);
+
+    this.instance.option("dataSource", appointments);
+
+    this.clock.tick(300);
+    var dropDown = this.instance.$element().find(".dx-scheduler-dropdown-appointments").dxDropDownMenu("instance");
+
+    dropDown.open();
+    var ddAppointments = dropDown._list.$element().find(".dx-scheduler-dropdown-appointment");
+
+    assert.equal(this.getAppointmentColor(ddAppointments.eq(0), "border-left-color"), "#ff0000", "Appointment color is OK");
+    assert.equal(this.getAppointmentColor(ddAppointments.eq(1), "border-left-color"), "#ff0000", "Appointment color is OK");
+    assert.equal(this.getAppointmentColor(ddAppointments.eq(2), "border-left-color"), "#0000ff", "Appointment color is OK");
+    assert.equal(this.getAppointmentColor(ddAppointments.eq(3), "border-left-color"), "#0000ff", "Appointment color is OK");
+    assert.equal(this.getAppointmentColor(ddAppointments.eq(4), "border-left-color"), "#0000ff", "Appointment color is OK");
+});
+
 QUnit.test("DropDown appointments should not be duplicated when items option change (T503748)", function(assert) {
     this.createInstance({
         views: ['month'],
