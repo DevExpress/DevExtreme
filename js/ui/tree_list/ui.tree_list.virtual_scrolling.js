dae1d289d659add60d5a34753497df5418de89e1
Alyar <Alyar666@users.noreply.github.com>
2017-12-28 18:59:31 +0300

    dxTreeList: Fix impossibility of changing the expandedRowKeys option via API (T591390) (#2373)
    * dxTreeList: Fix impossibility of changing the expandedRowKeys option via API (T591390)
    
    * Eliminated the faults noted in the remarks

--- js/ui/tree_list/ui.tree_list.virtual_scrolling.js
+++ js/ui/tree_list/ui.tree_list.virtual_scrolling.js
@@ -5,7 +5,17 @@ var gridCore = require("./ui.tree_list.core"),
     virtualScrollingModule = require("../grid_core/ui.grid_core.virtual_scrolling"),
     extend = require("../../core/utils/extend").extend;
 
-var oldDefaultOptions = virtualScrollingModule.defaultOptions;
+var oldDefaultOptions = virtualScrollingModule.defaultOptions,
+    originalDataControllerExtender = virtualScrollingModule.extenders.controllers.data;
+
+virtualScrollingModule.extenders.controllers.data = extend({}, originalDataControllerExtender, {
+    _loadOnOptionChange: function() {
+        var virtualScrollController = this._dataSource && this._dataSource._virtualScrollController;
+
+        virtualScrollController && virtualScrollController.reset();
+        this.callBase();
+    }
+});
 
 gridCore.registerModule("virtualScrolling", extend({}, virtualScrollingModule, {
     defaultOptions: function() {
