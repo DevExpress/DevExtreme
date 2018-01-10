a7e5e319fc16284fd164dcd90e97c13f50f16e53
TikhonovaTatyana <tatyana.tikhonova@devexpress.com>
2018-01-10 12:35:35 +0300

    Delete Doc Comments - default for android below v 4 and windows 8 (#2404)

--- js/ui/list/ui.list.base.js
+++ js/ui/list/ui.list.base.js
@@ -435,7 +435,6 @@ var ListBase = CollectionWidget.inherit({
         /**
         * @name dxListOptions_useNativeScrolling
         * @publicName useNativeScrolling
-        * @custom_default_for_android_below_version_4 false
         * @custom_default_for_desktop false
         * @custom_default_for_mac_desktop true
         */
@@ -462,7 +461,6 @@ var ListBase = CollectionWidget.inherit({
                     /**
                     * @name dxListOptions_showScrollbar
                     * @publicName showScrollbar
-                    * @custom_default_for_android_below_version_4 "onHover"
                     * @custom_default_for_desktop "onHover"
                     */
                     showScrollbar: "onHover",
@@ -470,7 +468,6 @@ var ListBase = CollectionWidget.inherit({
                     /**
                     * @name dxListOptions_pageLoadMode
                     * @publicName pageLoadMode
-                    * @custom_default_for_android_below_version_4 "nextButton"
                     * @custom_default_for_desktop "nextButton"
                     */
                     pageLoadMode: "nextButton"
