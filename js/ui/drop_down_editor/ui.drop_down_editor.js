104ffd181cbff89c5cf9bcf99ad604b9864037e6
Eugeniy Maslyaev <mrjohn0011@users.noreply.github.com>
2018-01-11 16:20:55 +0300

    Extract isFocused to a method

--- js/ui/drop_down_editor/ui.drop_down_editor.js
+++ js/ui/drop_down_editor/ui.drop_down_editor.js
@@ -360,7 +360,7 @@ var DropDownEditor = TextBox.inherit({
     },
 
     _renderTemplatedField: function(fieldTemplate, data) {
-        var isFocused = this._input().is(":focus");
+        var isFocused = this._isFocused(this._input());
         this._resetFocus(isFocused);
 
         var $container = this._$container;
@@ -493,12 +493,16 @@ var DropDownEditor = TextBox.inherit({
         return this._input();
     },
 
+    _isFocused: function(element) {
+        return document.activeElement === $(element).get(0);
+    },
+
     _focusInput: function() {
         if(this.option("disabled")) {
             return false;
         }
 
-        if(!this._input().is(":focus")) {
+        if(!this._isFocused(this._input())) {
             eventsEngine.trigger(this._input(), "focus");
         }
 
