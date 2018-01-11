504db3037d1e5226126ed2a9cfb0f27c895944c6
volnyagin <vconst@users.noreply.github.com>
2018-01-11 15:19:40 +0300

    dxDataGrid: Fix touch click in the first data row if the grid is grouped (T593150)

--- testing/tests/DevExpress.ui.widgets.dataGrid/rowsView.tests.js
+++ testing/tests/DevExpress.ui.widgets.dataGrid/rowsView.tests.js
@@ -38,6 +38,7 @@ var $ = require("jquery"),
     support = require("core/utils/support"),
     browser = require("core/utils/browser"),
     pointerMock = require("../../helpers/pointerMock.js"),
+    nativePointerMock = require("../../helpers/nativePointerMock.js"),
     dataGridMocks = require("../../helpers/dataGridMocks.js"),
     numberLocalization = require("localization/number"),
     setupDataGridModules = dataGridMocks.setupDataGridModules,
@@ -3986,6 +3987,42 @@ QUnit.test("onCellHoverChanged event handling", function(assert) {
 
     //assert
     assert.strictEqual(onCellHoverChanged.eventType, "mouseout", "eventType");
+});
+
+QUnit.test('Touch click on cell should raise rowClick with correct target arguments (T593150)', function(assert) {
+    var rowClickCount = 0,
+        clock = sinon.useFakeTimers();
+
+    this.options.dataSource.group = "name";
+
+    this.options.grouping = { autoExpandAll: true };
+
+    this.options.onRowClick = function(e) {
+        rowClickCount++;
+        //assert
+        assert.equal(e.event.target, $targetTouchCell[0]);
+        assert.equal(e.event.currentTarget, $targetTouchCell.parent()[0]);
+    };
+
+    this.setupDataGridModules();
+
+    var testElement = $('#container');
+
+    this.rowsView.render(testElement);
+
+    //act
+    var $targetTouchCell = testElement.find('tbody > tr').eq(1).children().eq(1);
+    var $targetClickCell = testElement.find('tbody > tr').eq(0).children().eq(1);
+
+    nativePointerMock($targetTouchCell).start().touchStart().touchEnd();
+    nativePointerMock($targetClickCell).start().click(true);
+
+    clock.tick();
+
+    //assert
+    assert.equal(rowClickCount, 1);
+
+    clock.restore();
 });
 
 QUnit.testInActiveWindow('ScrollToPage when virtual scrolling mode', function(assert) {
