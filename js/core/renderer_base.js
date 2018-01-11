d22ece3db924286bcb2da6ff873cede3e8fa0dc0
volkov.vladislav <vladislav.volkov@devexpress.com>
2018-01-11 18:03:34 +0300

    Get window in renderer_base

--- js/core/renderer_base.js
+++ js/core/renderer_base.js
@@ -2,6 +2,7 @@
 
 var dataUtils = require("./element_data");
 var domAdapter = require("./dom_adapter");
+var window = domAdapter.getWindow();
 var typeUtils = require("./utils/type");
 var styleUtils = require("./utils/style");
 var sizeUtils = require("./utils/size");
