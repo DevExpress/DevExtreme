f7c4a8a2f4d073f8140968ca0deae18a8b8f8598
volkov.vladislav <vladislav.volkov@devexpress.com>
2018-01-11 16:49:38 +0300

    Add test

--- testing/tests/DevExpress.core/dom_adapter.tests.js
+++ testing/tests/DevExpress.core/dom_adapter.tests.js
@@ -1,6 +1,7 @@
 "use strict";
 
-var domAdapter = require("core/dom_adapter");
+var domAdapter = require("core/dom_adapter"),
+    isWindow = require("core/utils/type").isWindow;
 
 QUnit.module("DOM Adapter", {
     beforeEach: function() {
@@ -16,5 +17,9 @@ QUnit.test("insertElement", function(assert) {
     domAdapter.insertElement(this.container, target);
 
     assert.equal(this.container.childNodes.length, 1);
+});
+
+QUnit.test("getWindow", function(assert) {
+    assert.equal(isWindow(domAdapter.getWindow()), true);
 });
 
