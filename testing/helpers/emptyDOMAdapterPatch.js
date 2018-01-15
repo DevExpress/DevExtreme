27dbd726a3a6b9188bdd4dd3e5fb220a9418b20e
eugen.zha <nightskylark@users.noreply.github.com>
2018-01-15 18:51:49 +0300

    Restore DOMAdapter mock original methods before setting the sever-side strategy

--- testing/helpers/emptyDOMAdapterPatch.js
+++ testing/helpers/emptyDOMAdapterPatch.js
@@ -1,11 +1,19 @@
 "use strict";
 
 var domAdapter = require("core/dom_adapter");
+var domAdapterBackup = {};
 
 for(var method in domAdapter) {
+    domAdapterBackup[method] = domAdapter[method];
     delete domAdapter[method];
 }
 
 domAdapter.getWindow = function() {
     return null;
+};
+
+domAdapter.__restoreOriginal = function() {
+    for(var method in domAdapterBackup) {
+        domAdapter[method] = domAdapterBackup[method];
+    }
 };
--- testing/helpers/serverSideDOMAdapterPatch.js
+++ testing/helpers/serverSideDOMAdapterPatch.js
@@ -4,6 +4,8 @@ var domAdapter = require("core/dom_adapter");
 var serverSideWindowMock = require("./serverSideWindowMock.js");
 
 exports.set = function() {
+    domAdapter.__restoreOriginal();
+
     domAdapter.getWindow = function() {
         return serverSideWindowMock;
     };
