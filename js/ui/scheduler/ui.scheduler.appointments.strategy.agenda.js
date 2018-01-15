e3b0fc352a13563cea1472f4ac12759a91b9d962
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2018-01-15 11:08:14 +0300

    Fix "Provided date is not in valid range" error occuring in Scheduler (T593699) (#2467)

--- js/ui/scheduler/ui.scheduler.appointments.strategy.agenda.js
+++ js/ui/scheduler/ui.scheduler.appointments.strategy.agenda.js
@@ -15,6 +15,10 @@ var AgendaRenderingStrategy = BaseAppointmentsStrategy.inherit({
 
     getDeltaTime: noop,
 
+    keepAppointmentSettings: function() {
+        return true;
+    },
+
     getAppointmentGeometry: function(geometry) {
         return geometry;
     },
