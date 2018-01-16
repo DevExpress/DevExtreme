08608b26eb8f03bcfcec65b219f401ee072c7708
Eugen Zha <nightskylark@users.noreply.github.com>
2018-01-16 16:40:40 +0300

    Get rid of using `window.Node` on scripts loading (#2496)

--- js/core/utils/window.js
+++ js/core/utils/window.js
@@ -2,6 +2,7 @@
 
 var $ = require("../../core/renderer"),
     window = require("../../core/dom_adapter").getWindow(),
+    callOnce = require("../../core/utils/common").callOnce,
     eventsEngine = require("../../events/core/events_engine"),
     Callbacks = require("../../core/utils/callbacks");
 
@@ -39,11 +40,16 @@ var resizeCallbacks = (function() {
 
         callbacks.fire(changedDimension);
     };
-    prevSize = formatSize();
+
+    var setPrevSize = callOnce(function() {
+        prevSize = formatSize();
+    });
 
     callbacks.add = function() {
         var result = originalCallbacksAdd.apply(callbacks, arguments);
         var jqWindow = $(window);
+
+        setPrevSize();
 
         if(!resizeEventHandlerAttached && callbacks.has()) {
             eventsEngine.subscribeGlobal(jqWindow, "resize", handleResize);
