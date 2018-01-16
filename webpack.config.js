be437b5c47d7e10782d1860f7ad892671693a779
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2018-01-16 10:49:24 +0300

    Add babel to tests

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,8 @@ module.exports = {
             use: {
                 loader: 'babel-loader',
                 options: {
-                    presets: ['env']
+                    presets: ['env'],
+                    plugins: ['transform-es2015-modules-commonjs', 'add-module-exports']
                 }
             }
         }]
