4f75a0d4b92c700b1ad9302a1604474c1f73698c
eugen.zha <nightskylark@users.noreply.github.com>
2018-01-16 16:00:15 +0300

    Refactor with using callOnce utility

--- js/core/utils/window.js
+++ js/core/utils/window.js
@@ -2,7 +2,7 @@
 
 var $ = require("../../core/renderer"),
     window = require("../../core/dom_adapter").getWindow(),
-    noop = require("../../core/utils/common").noop,
+    callOnce = require("../../core/utils/common").callOnce,
     eventsEngine = require("../../events/core/events_engine"),
     Callbacks = require("../../core/utils/callbacks");
 
@@ -41,16 +41,14 @@ var resizeCallbacks = (function() {
         callbacks.fire(changedDimension);
     };
 
-    var setPrevSize = function() {
-        prevSize = formatSize();
-        setPrevSize = noop;
-    };
-
     callbacks.add = function() {
         var result = originalCallbacksAdd.apply(callbacks, arguments);
         var jqWindow = $(window);
 
-        setPrevSize();
+        callOnce(function() {
+            prevSize = formatSize();
+        });
+
         if(!resizeEventHandlerAttached && callbacks.has()) {
             eventsEngine.subscribeGlobal(jqWindow, "resize", handleResize);
             resizeEventHandlerAttached = true;
