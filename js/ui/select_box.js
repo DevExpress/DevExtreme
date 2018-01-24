816685c5e7e28b678cc81fd07572b3fd1ce9a30b
Eugeniy Maslyaev <mrjohn0011@users.noreply.github.com>
2018-01-24 16:52:34 +0300

    Fix unexpected onCustomItemCreating in dxSelectBox after selecting found item using keyboard (T597478) (#2624)

--- js/ui/select_box.js
+++ js/ui/select_box.js
@@ -121,7 +121,9 @@ var SelectBox = DropDownList.inherit({
                 } else {
                     if(this.option("acceptCustomValue")) {
                         e.preventDefault();
+                        return this.option("opened");
                     }
+
                     if(parent.enter.apply(this, arguments)) {
                         return this.option("opened");
                     }
