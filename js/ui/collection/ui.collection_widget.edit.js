b36cfcde7da699706f4776a80a8d95753bc54682
BingoRUS <bingofirst@gmail.com>
2018-01-25 17:30:53 +0300

    Release 17.1.8 Cherry-picked changesets:   0a52758 Dmitry Levkovskiy - Fix unexpected selection clearing on a dataSource changing (#1840)   234790c Alyar - dxList: Fix "Select All"  incorrectly works with a filtered data source when selectAllMode is "allPages" (T567757) (#1819)

--- js/ui/collection/ui.collection_widget.edit.js
+++ js/ui/collection/ui.collection_widget.edit.js
@@ -198,6 +198,10 @@ var CollectionWidget = BaseCollectionWidget.inherit({
         return !!(this._dataSource && this._dataSource.key());
     },
 
+    _getCombinedFilter: function() {
+        return this._dataSource && this._dataSource.filter();
+    },
+
     keyOf: function(item) {
         var key = item,
             store = this._dataSource && this._dataSource.store();
@@ -225,9 +229,7 @@ var CollectionWidget = BaseCollectionWidget.inherit({
                     that._updateSelectedItems(args.addedItems, args.removedItems);
                 }
             },
-            filter: function() {
-                return that._dataSource && that._dataSource.filter();
-            },
+            filter: that._getCombinedFilter.bind(that),
             totalCount: function() {
                 var items = that.option("items");
                 var dataSource = that._dataSource;
@@ -572,7 +574,10 @@ var CollectionWidget = BaseCollectionWidget.inherit({
                 }
                 break;
             case "dataSource":
-                this.option("selectedItemKeys", []);
+                if(!args.value || !args.value.length) {
+                    this.option("selectedItemKeys", []);
+                }
+
                 this.callBase(args);
                 break;
             case "selectedIndex":
