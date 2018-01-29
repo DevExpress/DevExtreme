3439eee15fa97d0294ed5db48ddf286f58db373c
alexanderPolosatov <alexanderPolosatov@users.noreply.github.com>
2018-01-29 13:28:00 +0300

    Fix memory leak in charts (T586649) (#2679)
    * fix mem leaks in axis
    
    * Add recursive cleaning of elements to vizRenderer
    
    * Extra checking was removed

--- testing/tests/DevExpress.viz.core/baseAxis.tests.js
+++ testing/tests/DevExpress.viz.core/baseAxis.tests.js
@@ -304,7 +304,10 @@ QUnit.test("Disposing", function(assert) {
 
     this.axis.dispose();
 
-    assert.ok(renderer.g.getCall(3).returnValue.dispose.called, "dispose is called");
+    assert.ok(renderer.g.getCall(0).returnValue.dispose.called, "axis group was cleared");
+    assert.ok(renderer.g.getCall(1).returnValue.dispose.called, "strips group was cleared");
+    assert.ok(renderer.g.getCall(3).returnValue.dispose.called, "elements group was cleared");
+    assert.ok(renderer.g.getCall(10).returnValue.dispose.called, "breaks group was cleared");
 });
 
 QUnit.test("calculateInterval - returns absolute difference of two numbers", function(assert) {
