25fb9f5f806d947c4046052adf5c9046d0e596ac
polosatov.alexander <alexanderPolosatov@users.noreply.github.com>
2018-01-30 15:43:40 +0300

    Fix slow labels overlapping issue (see T594902)

--- js/viz/chart_components/base_chart.js
+++ js/viz/chart_components/base_chart.js
@@ -779,17 +779,17 @@ var BaseChart = BaseWidget.inherit({
             points = series[i].getVisiblePoints();
 
             for(j = 0; j < points.length; j++) {
-                labels = labels.concat(points[j].getLabels());
+                labels.push.apply(labels, points[j].getLabels());
             }
         }
 
         for(i = 0; i < labels.length; i++) {
             currentLabel = labels[i];
-            currentLabelRect = currentLabel.getBoundingRect();
-
             if(!currentLabel.isVisible()) {
                 continue;
             }
+
+            currentLabelRect = currentLabel.getBoundingRect();
 
             for(j = i + 1; j < labels.length; j++) {
                 nextLabel = labels[j];
