db0321e238e9cf5831ee17727428196174c51625
zhernovkova.anna <anna.zhernovkova@devexpress.com>
2018-01-30 15:02:13 +0300

    Add test for viewport changing

--- testing/tests/DevExpress.serverSide/themes.tests.js
+++ testing/tests/DevExpress.serverSide/themes.tests.js
@@ -1,16 +1,29 @@
 "use strict";
-var fixture = document.getElementById("qunit-fixture");
-fixture.className = "dx-viewport";
+
+var themes = require("ui/themes"),
+    viewportUtils = require("core/utils/view_port");
+
 window.includeThemesLinks();
 
-var themes = require("ui/themes");
+var viewport = document.createElement("div");
+viewport.className = "dx-viewport";
+document.body.append(viewport);
+
+QUnit.module("themes", {
+    beforeEach: function() {
+        this._originalTheme = themes.current();
+    },
+    afterEach: function() {
+        themes.current(this._originalTheme);
+        viewportUtils.value(viewport);
+    }
+});
 
 QUnit.test("theme changing", function(assert) {
-    var originalTheme = themes.current();
     themes.current("android5.light");
 
     assert.equal(themes.current(), "android5.light");
-    assert.equal(fixture.classList.toString(), [
+    assert.equal(viewport.classList.toString(), [
         "dx-viewport",
         "dx-device-desktop",
         "dx-device-generic",
@@ -18,6 +31,25 @@ QUnit.test("theme changing", function(assert) {
         "dx-theme-android5-typography",
         "dx-color-scheme-light"
     ].join(" "));
+});
 
-    themes.current(originalTheme);
+QUnit.test("viewport changing", function(assert) {
+    themes.current("android5.light");
+
+    var fixture = document.getElementById("qunit-fixture");
+    var newViewport = document.createElement("div");
+    fixture.append(newViewport);
+    viewportUtils.value(newViewport);
+
+    assert.equal(viewportUtils.value()[0], newViewport);
+    assert.equal(viewport.classList.toString(), "dx-viewport");
+    assert.equal(newViewport.classList.toString(), [
+        "dx-device-desktop",
+        "dx-device-generic",
+        "dx-theme-android5",
+        "dx-theme-android5-typography",
+        "dx-color-scheme-light"
+    ].join(" "));
+
+    fixture.removeChild(newViewport);
 });
