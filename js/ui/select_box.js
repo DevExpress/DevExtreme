b228d4d063eac3a72cce8ff3ffcf058ed3ca6902
Eugeniy Maslyaev <mrjohn0011@users.noreply.github.com>
2018-02-01 18:23:36 +0300

    Refactoring

--- js/ui/select_box.js
+++ js/ui/select_box.js
@@ -576,14 +576,16 @@ var SelectBox = DropDownList.inherit({
         }
 
         var oldSelectedItem = this.option("selectedItem");
-        if(this._displayGetter(oldSelectedItem) !== this._searchValue()) {
-            this._renderInputValue().always((function(selectedItem) {
-                var newSelectedItem = commonUtils.ensureDefined(selectedItem, oldSelectedItem);
-                this._setSelectedItem(newSelectedItem);
-                this._updateField(newSelectedItem);
-                this._clearFilter();
-            }).bind(this));
+        if(this._displayGetter(oldSelectedItem) === this._searchValue()) {
+            return;
         }
+
+        this._renderInputValue().always((function(selectedItem) {
+            var newSelectedItem = commonUtils.ensureDefined(selectedItem, oldSelectedItem);
+            this._setSelectedItem(newSelectedItem);
+            this._updateField(newSelectedItem);
+            this._clearFilter();
+        }).bind(this));
     },
 
     _focusOutHandler: function(e) {
