1412c71e9ed82358c28ca9d60e9435db8e14ca56
eugen.zha <nightskylark@users.noreply.github.com>
2018-02-01 17:29:17 +0300

    Prepare test evironment for markup testing

--- testing/tests/DevExpress.serverSide/widgetsCreation.tests.js
+++ testing/tests/DevExpress.serverSide/widgetsCreation.tests.js
@@ -80,19 +80,3 @@ QUnit.test("Widgets", function(assert) {
         assert.ok(widgets[widget], widget);
     }
 });
-
-QUnit.module("Widget creation", {
-    beforeEach: function() {
-        var fixture = document.getElementById("qunit-fixture");
-        this.element = document.createElement("div");
-        fixture.appendChild(this.element);
-    },
-    afterEach: function() {
-        this.instance.dispose();
-    }
-});
-
-QUnit.test("Button", function(assert) {
-    this.instance = new widgets.Button(this.element);
-    assert.ok(this.instance);
-});
