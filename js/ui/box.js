d68bea35d7da6cdb1880f86cd5b7704b85977818
Dmitry Semenov <jsdmitry@users.noreply.github.com>
2018-02-02 16:20:08 +0300

    Fix a width of the popup when the form is located inside for IE and Edge browsers (T590137) (#2833)
    * Fix the flexBasis CSS property of the Box in the popup window
    
    * fix an editor container width of the simple item for IE and Edge browsers

--- js/ui/box.js
+++ js/ui/box.js
@@ -127,8 +127,7 @@ var FlexLayoutStrategy = Class.inherit({
                     width: "auto",
                     height: "auto",
                     display: styleUtils.stylePropPrefix("flexDirection") + "flex",
-                    flexDirection: $item.children().css("flexDirection") || "column",
-                    flexBasis: 0
+                    flexDirection: $item.children().css("flexDirection") || "column"
                 });
 
                 // NOTE: workaround for jQuery version < 1.11.1 (T181692)
