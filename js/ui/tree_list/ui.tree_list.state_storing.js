f3f15a24af5d567f1b4b3599607dce512226dd56
salimov.alyar <alyar666@users.noreply.github.com>
2018-02-06 14:46:20 +0300

    dxTreeList - support state storing

---
+++ js/ui/tree_list/ui.tree_list.state_storing.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var treeListCore = require("./ui.tree_list.core"),
+    extend = require("../../core/utils/extend").extend,
+    stateStoringModule = require("../grid_core/ui.grid_core.state_storing"),
+    origApplyState = stateStoringModule.extenders.controllers.stateStoring.applyState;
+
+treeListCore.registerModule("stateStoring", extend(true, {}, stateStoringModule, {
+    extenders: {
+        controllers: {
+            stateStoring: {
+                applyState: function(state) {
+                    origApplyState.apply(this, arguments);
+                    if(state.hasOwnProperty("expandedRowKeys")) {
+                        this.option("expandedRowKeys", state.expandedRowKeys);
+                    }
+                }
+            },
+            data: {
+                getUserState: function() {
+                    var state = this.callBase.apply(this, arguments);
+
+                    if(!this.option("autoExpandAll")) {
+                        state.expandedRowKeys = this.option("expandedRowKeys");
+                    }
+
+                    return state;
+                }
+            }
+        }
+    }
+}));
