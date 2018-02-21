a12991f564906b8b653d15d0e02ac6d5419f9b77
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2018-02-21 13:19:22 +0300

    Add some tests

--- testing/tests/DevExpress.serverSide/scrollView.tests.js
+++ testing/tests/DevExpress.serverSide/scrollView.tests.js
@@ -8,7 +8,8 @@ require("ui/scroll_view");
 var SCROLLABLE_CONTENT_CLASS = "dx-scrollable-content",
     SCROLLABLE_SCROLLBAR_CLASS = "dx-scrollable-scrollbar",
     SCROLLVIEW_TOP_POCKET_CLASS = "dx-scrollview-top-pocket",
-    SCROLLVIEW_BOTTOM_POCKET_CLASS = "dx-scrollview-bottom-pocket";
+    SCROLLVIEW_BOTTOM_POCKET_CLASS = "dx-scrollview-bottom-pocket",
+    SCROLLVIEW_LOADPANEL_CLASS = "dx-scrollview-loadpanel";
 
 QUnit.testStart(function() {
     var markup = '\
@@ -36,4 +37,11 @@ QUnit.test("scrollbar should not be rendered in scrollView on server", function(
         $scrollbar = $scrollView.find("." + SCROLLABLE_SCROLLBAR_CLASS);
 
     assert.equal($scrollbar.length, 0, "scrollView has no scrollbar on server");
+});
+
+QUnit.test("loadPanel should not be rendered in scrollView on server", function(assert) {
+    var $scrollView = $("#scrollView").dxScrollView(),
+        $loadPanel = $scrollView.find("." + SCROLLVIEW_LOADPANEL_CLASS);
+
+    assert.equal($loadPanel.length, 0, "scrollView has no loadPanel on server");
 });
