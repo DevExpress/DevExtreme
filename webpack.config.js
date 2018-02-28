7e878de66b475190e0530055933054e028e1317a
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2018-02-28 13:08:33 +0300

    Rework build flow to run babel once (#3043)

--- webpack.config.js
+++ webpack.config.js
@@ -19,14 +19,5 @@ module.exports = {
         'globalize/currency': 'window.Globalize',
         'globalize/date': 'window.Globalize',
         'globalize/message': 'window.Globalize'
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader'
-            }
-        }]
     }
 };
