7ce82858e049595c1d3214ac9a64664158d729db
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2018-03-02 14:34:22 +0300

    Revert "Fix menu test"
    This reverts commit 7f09634907319d9f0442295f5819e718cb89dcec.

--- testing/tests/DevExpress.ui.widgets/contextMenu.markup.tests.js
+++ testing/tests/DevExpress.ui.widgets/contextMenu.markup.tests.js
@@ -31,5 +31,5 @@ QUnit.test("context menu should have correct css class", function(assert) {
 QUnit.test("aria role", function(assert) {
     new ContextMenu(this.$element, {});
 
-    assert.equal(this.$element.attr("role"), undefined, "aria role is not present");
+    assert.equal(this.$element.attr("role"), "menu", "aria role is correct");
 });
