3c6887ca110389a843fcc467080b65d07f3c2998
Eugeniy Maslyaev <mrjohn0011@users.noreply.github.com>
2018-03-02 14:22:41 +0300

    Fix unexpected '10' number reverting on focusout in dxNumberBox (T607513, T605471, T606954)

--- js/ui/number_box/number_box.mask.js
+++ js/ui/number_box/number_box.mask.js
@@ -233,6 +233,24 @@ var NumberBoxMask = NumberBoxBase.inherit({
         }
     },
 
+    _getFormatForSign: function(text) {
+        var format = this._getFormatPattern(),
+            signParts = format.split(";"),
+            sign = number.getSign(text, format);
+
+        signParts[1] = signParts[1] || "-" + signParts[0];
+        return sign < 0 ? signParts[1] : signParts[0];
+    },
+
+    _removeStubs: function(text, excludeComma) {
+        var format = this._getFormatForSign(text),
+            thousandsSeparator = number.getThousandsSeparator(),
+            stubs = format.replace(/[#0.,]/g, ""),
+            regExp = new RegExp("[\-" + escapeRegExp((excludeComma ? "" : thousandsSeparator) + stubs) + "]", "g");
+
+        return text.replace(regExp, "");
+    },
+
     _getEditedText: function(text, selection, char) {
         var textBefore = text.slice(0, selection.start),
             textAfter = text.slice(selection.end),
@@ -246,11 +264,11 @@ var NumberBoxMask = NumberBoxBase.inherit({
             format = this._getFormatPattern(),
             isTextSelected = selection.start !== selection.end,
             parsed = number.parse(editedText, format),
-            maxPrecision = this._getMaxPrecision(format, parsed),
+            maxPrecision = this._getPrecisionLimits(format, editedText).max,
             isValueChanged = parsed !== this._parsedValue;
 
         var isDecimalPointRestricted = char === number.getDecimalSeparator() && maxPrecision === 0,
-            isUselessCharRestricted = !isTextSelected && !isValueChanged && char !== MINUS && !this._isValueIncomplete(editedText);
+            isUselessCharRestricted = !isTextSelected && !isValueChanged && char !== MINUS && !this._isValueIncomplete(editedText) && this._isStub(char);
 
         if(isDecimalPointRestricted || isUselessCharRestricted) {
             return undefined;
@@ -277,37 +295,22 @@ var NumberBoxMask = NumberBoxBase.inherit({
             return this.callBase(text);
         }
 
-        var formatParts = this._getFormatPattern().split(";")[0].split("."),
-            isRemoveKeyPressed = this._isDeleteKey(this._lastKey) || this._lastKey === "Backspace",
-            isFloatPartAllowed = formatParts.length === 2;
-
-        if(!isFloatPartAllowed || isRemoveKeyPressed) {
-            return false;
-        }
-
-        var clearedText = this._removeStubInText(text),
-            decimalSeparator = number.getDecimalSeparator(),
-            decimalSeparatorIndex = clearedText.indexOf(decimalSeparator),
-            separatorIsFirst = decimalSeparatorIndex === 0,
-            lastChar = clearedText.charAt(clearedText.length - 1),
-            onlyOneSeparatorExists = decimalSeparatorIndex === clearedText.length - 1;
-
-        if(separatorIsFirst) {
-            return false;
-        }
-        if(lastChar === decimalSeparator && onlyOneSeparatorExists) {
-            return true;
-        }
-        if(lastChar !== "0") {
-            return false;
-        }
-
         var caret = this._caret(),
-            floatLength = clearedText.length - decimalSeparatorIndex - 1,
-            maxPrecisionOverflow = floatLength >= this._getMaxPrecision(this._getFormatPattern(), clearedText),
-            textAfterCaret = this._getInputVal().slice(caret.start);
+            point = number.getDecimalSeparator(),
+            pointIndex = text.indexOf(point),
+            isCaretOnFloat = pointIndex > 0 && pointIndex < caret.start,
+            textParts = this._removeStubs(text, true).split(point);
 
-        return !maxPrecisionOverflow && (!textAfterCaret || this._isStub(textAfterCaret, true));
+        if(!isCaretOnFloat || textParts.length !== 2) {
+            return false;
+        }
+
+        var floatLength = textParts[1].length,
+            precision = this._getPrecisionLimits(this._getFormatPattern(), text),
+            isPrecisionInRange = inRange(floatLength, precision.min, precision.max),
+            endsWithZero = textParts[1].charAt(floatLength - 1) === "0";
+
+        return isPrecisionInRange && (endsWithZero || !floatLength);
     },
 
     _isValueInRange: function(value) {
@@ -422,12 +425,13 @@ var NumberBoxMask = NumberBoxBase.inherit({
         return this._parsedValue;
     },
 
-    _getMaxPrecision: function(format, value) {
-        var signParts = format.split(";"),
-            currentFormat = signParts[value >= 0 ? 0 : 1] || signParts[0],
-            floatPart = currentFormat.split(".")[1] || "";
+    _getPrecisionLimits: function(format, text) {
+        var currentFormat = this._getFormatForSign(text),
+            floatPart = (currentFormat.split(".")[1] || "").replace(/[^#0]/g, ""),
+            minPrecision = floatPart.replace(/^(0*)#*/, '$1').length,
+            maxPrecision = floatPart.length;
 
-        return floatPart.length;
+        return { min: minPrecision, max: maxPrecision };
     },
 
     _revertSign: function(e) {
@@ -534,16 +538,6 @@ var NumberBoxMask = NumberBoxBase.inherit({
             return (1 / oldValue) === (1 / newValue);
         }
         return this.callBase.apply(this, arguments);
-    },
-
-    _removeStubInText: function(text) {
-        var decimalSeparator = number.getDecimalSeparator(),
-            regExpString = "[^0-9" + decimalSeparator + "]+",
-            regExp = new RegExp(regExpString, "g");
-
-        text = text || this._getInputVal();
-
-        return text.replace(regExp, "");
     },
 
     _clearCache: function() {
