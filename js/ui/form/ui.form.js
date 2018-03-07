f07fa1824e3064a7c1ca31b8d9146d7c6cf9acbe
levkovskiy.dmitry <dmitry.levkovskiy@devexpress.com>
2018-03-07 13:11:10 +0300

    Add template parameter fields description

--- js/ui/form/ui.form.js
+++ js/ui/form/ui.form.js
@@ -326,6 +326,10 @@ var Form = Widget.inherit({
              * @publicName template
              * @type template|function
              * @type_function_param1 data:object
+             * @type_function_param1_field1 component:dxForm
+             * @type_function_param1_field2 dataField:string
+             * @type_function_param1_field3 editorOptions:object
+             * @type_function_param1_field4 editorType:string
              * @type_function_param2 itemElement:dxElement
              * @type_function_return string|Node|jQuery
              */
