f86a5c5c7955697713008fb3f27eec66330778cf
levkovskiy.dmitry <dmitry.levkovskiy@devexpress.com>
2018-03-19 16:35:27 +0300

    Fix touch events for FireFox (T602186)

--- js/events/core/events_engine.js
+++ js/events/core/events_engine.js
@@ -533,6 +533,11 @@ initEvent(normalizeEventArguments(function(src, config) {
 
     addProperty("which", calculateWhich, that);
 
+    if(src.type.startsWith("touch")) {
+        delete config.pageX;
+        delete config.pageY;
+    }
+
     extend(that, config);
 
     that.guid = ++guid;
