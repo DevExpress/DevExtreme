0ce6209e875e12abd11803701da1e25ec0f9e21b
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2018-03-20 16:48:58 +0300

    dxFilterBuilder: support SSR (#3358)

---
+++ testing/tests/DevExpress.serverSide/filterBuilder.tests.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var $ = require("jquery");
+
+QUnit.testStart(function() {
+    $("#qunit-fixture").html('<div id="container"></div>');
+});
+
+QUnit.module("Filter Builder markup");
+
+require("../DevExpress.ui.widgets/filterBuilderParts/markupTests.js");
