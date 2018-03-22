c0e72525181c1b8d69d2deeb1463d70c0b8d6e90
Dmitry Levkovskiy <DokaRus@users.noreply.github.com>
2018-03-22 14:04:04 +0300

    Add RangeSlider to the Form supported widget list (#3389)

--- js/ui/form/ui.form.layout_manager.js
+++ js/ui/form/ui.form.layout_manager.js
@@ -65,7 +65,8 @@ var FORM_EDITOR_BY_DEFAULT = "dxTextBox",
 
     SIMPLE_ITEM_TYPE = "simple",
 
-    DATA_OPTIONS = ["dataSource", "items"];
+    DATA_OPTIONS = ["dataSource", "items"],
+    EDITORS_WITH_ARRAY_VALUE = ["dxTagBox", "dxRangeSlider"];
 
 var LayoutManager = Widget.inherit({
     _getDefaultOptions: function() {
@@ -674,7 +675,7 @@ var LayoutManager = Widget.inherit({
             isDeepExtend = true,
             editorOptions;
 
-        if(options.editorType === "dxTagBox") {
+        if(EDITORS_WITH_ARRAY_VALUE.indexOf(options.editorType) !== -1) {
             defaultEditorOptions.value = defaultEditorOptions.value || [];
         }
 
