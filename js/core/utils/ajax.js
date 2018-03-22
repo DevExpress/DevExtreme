a5830547e40f9ca1d7a294748e019ec7b7f8d45a
volkov.vladislav <vladislav.volkov@devexpress.com>
2018-03-22 15:08:51 +0300

    Make Ajax injectable

--- js/core/utils/ajax.js
+++ js/core/utils/ajax.js
@@ -8,6 +8,7 @@ var window = windowUtils.getWindow();
 var extendFromObject = require("./extend").extendFromObject;
 var isDefined = require("./type").isDefined;
 var Promise = require("../polyfills/promise");
+var injector = require("./dependency_injector");
 var ajaxStrategy;
 
 var SUCCESS = "success",
@@ -240,120 +241,122 @@ var getRequestHeaders = function(options) {
     return headers;
 };
 
-var sendRequest = function(options) {
+module.exports = injector({
+    sendRequest: function(options) {
 
-    if(ajaxStrategy && !options.responseType && !options.upload) {
-        return ajaxStrategy(options);
-    }
+        if(this.ajaxStrategy && !options.responseType && !options.upload) {
+            return this.ajaxStrategy(options);
+        }
 
-    var xhr = httpRequest.getXhr(),
-        d = new Deferred(),
-        result = d.promise(),
-        async = isDefined(options.async) ? options.async : true,
-        dataType = options.dataType,
-        timeout = options.timeout || 0,
-        timeoutId;
+        var xhr = httpRequest.getXhr(),
+            d = options.deferred ? options.deferred : new Deferred(),
+            result = d.promise(),
+            async = isDefined(options.async) ? options.async : true,
+            dataType = options.dataType,
+            timeout = options.timeout || 0,
+            timeoutId;
 
-    options.crossDomain = isCrossDomain(options.url);
-    var needScriptEvaluation = dataType === "jsonp" || dataType === "script";
+        options.crossDomain = isCrossDomain(options.url);
+        var needScriptEvaluation = dataType === "jsonp" || dataType === "script";
 
-    if(options.cache === undefined) {
-        options.cache = !needScriptEvaluation;
-    }
+        if(options.cache === undefined) {
+            options.cache = !needScriptEvaluation;
+        }
 
-    var callbackName = getJsonpOptions(options),
-        headers = getRequestHeaders(options),
-        requestOptions = getRequestOptions(options, headers),
-        url = requestOptions.url,
-        parameters = requestOptions.parameters;
+        var callbackName = getJsonpOptions(options),
+            headers = getRequestHeaders(options),
+            requestOptions = getRequestOptions(options, headers),
+            url = requestOptions.url,
+            parameters = requestOptions.parameters;
 
-    if(callbackName) {
-        window[callbackName] = function(data) {
-            d.resolve(data, SUCCESS, xhr);
-        };
-    }
-
-    if(options.crossDomain && needScriptEvaluation) {
-        var reject = function() {
-                d.reject(xhr, ERROR);
-            },
-            resolve = function() {
-                if(dataType === "jsonp") return;
-                d.resolve(null, SUCCESS, xhr);
+        if(callbackName) {
+            window[callbackName] = function(data) {
+                d.resolve(data, SUCCESS, xhr);
             };
+        }
 
-        evalCrossDomainScript(url).then(resolve, reject);
-        return result;
-    }
+        if(options.crossDomain && needScriptEvaluation) {
+            var reject = function() {
+                    d.reject(xhr, ERROR);
+                },
+                resolve = function() {
+                    if(dataType === "jsonp") return;
+                    d.resolve(null, SUCCESS, xhr);
+                };
 
-    if(options.crossDomain && !("withCredentials" in xhr)) {
-        d.reject(xhr, ERROR);
-        return result;
-    }
+            evalCrossDomainScript(url).then(resolve, reject);
+            return result;
+        }
 
-    xhr.open(
-        getMethod(options),
-        url,
-        async,
-        options.username,
-        options.password);
+        if(options.crossDomain && !("withCredentials" in xhr)) {
+            d.reject(xhr, ERROR);
+            return result;
+        }
 
-    if(async) {
-        xhr.timeout = timeout;
-        timeoutId = setHttpTimeout(timeout, xhr, d);
-    }
+        xhr.open(
+            getMethod(options),
+            url,
+            async,
+            options.username,
+            options.password);
 
-    xhr["onreadystatechange"] = function(e) {
-        if(xhr.readyState === 4) {
-            clearTimeout(timeoutId);
-            if(isStatusSuccess(xhr.status)) {
-                if(hasContent(xhr.status)) {
-                    postProcess(d, xhr, dataType);
+        if(async) {
+            xhr.timeout = timeout;
+            timeoutId = setHttpTimeout(timeout, xhr, d);
+        }
+
+        xhr["onreadystatechange"] = function(e) {
+            if(xhr.readyState === 4) {
+                clearTimeout(timeoutId);
+                if(isStatusSuccess(xhr.status)) {
+                    if(hasContent(xhr.status)) {
+                        postProcess(d, xhr, dataType);
+                    } else {
+                        d.resolve(null, NO_CONTENT, xhr);
+                    }
                 } else {
-                    d.resolve(null, NO_CONTENT, xhr);
+                    d.reject(xhr, xhr.customStatus || ERROR);
                 }
-            } else {
-                d.reject(xhr, xhr.customStatus || ERROR);
+            }
+        };
+
+        if(options.upload) {
+            xhr.upload["onprogress"] = options.upload["onprogress"];
+            xhr.upload["onloadstart"] = options.upload["onloadstart"];
+            xhr.upload["onabort"] = options.upload["onabort"];
+        }
+
+        if(options.xhrFields) {
+            for(var field in options.xhrFields) {
+                xhr[field] = options.xhrFields[field];
             }
         }
-    };
 
-    if(options.upload) {
-        xhr.upload["onprogress"] = options.upload["onprogress"];
-        xhr.upload["onloadstart"] = options.upload["onloadstart"];
-        xhr.upload["onabort"] = options.upload["onabort"];
-    }
-
-    if(options.xhrFields) {
-        for(var field in options.xhrFields) {
-            xhr[field] = options.xhrFields[field];
+        if(options.responseType === "arraybuffer") {
+            xhr.responseType = options.responseType;
         }
-    }
 
-    if(options.responseType === "arraybuffer") {
-        xhr.responseType = options.responseType;
-    }
-
-    for(var name in headers) {
-        if(headers.hasOwnProperty(name) && isDefined(headers[name])) {
-            xhr.setRequestHeader(name, headers[name]);
+        for(var name in headers) {
+            if(headers.hasOwnProperty(name) && isDefined(headers[name])) {
+                xhr.setRequestHeader(name, headers[name]);
+            }
         }
+
+        if(options.beforeSend) {
+            options.beforeSend(xhr);
+        }
+
+        xhr.send(parameters);
+
+        result.abort = function() {
+            xhr.abort();
+        };
+
+        return result;
+    },
+
+    setStrategy: function(s) {
+        ajaxStrategy = s;
     }
+});
 
-    if(options.beforeSend) {
-        options.beforeSend(xhr);
-    }
-
-    xhr.send(parameters);
-
-    result.abort = function() {
-        xhr.abort();
-    };
-
-    return result;
-};
-
-exports.sendRequest = sendRequest;
-exports.setStrategy = function(s) {
-    ajaxStrategy = s;
-};
