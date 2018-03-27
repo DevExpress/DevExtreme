e82ec82a49655adb413e5f68bedb14d474fb754b
dmitry.levkovskiy <dmitry.levkovskiy@devexpress.com>
2018-03-27 14:04:16 +0300

    Update doccoment

--- js/ui/select_box.js
+++ js/ui/select_box.js
@@ -179,7 +179,6 @@ var SelectBox = DropDownList.inherit({
             * @type_function_param1 e:object
             * @type_function_param1_field4 text:string
             * @type_function_param1_field5 customItem:string|object|Promise<any>
-            * @type_function_return string|object|Promise<any>
             * @action
             * @default function(e) { if(!e.customItem) { e.customItem = e.text; } }
             */
