b6e5a3649fbd02d8ed4f0c38f5e672963ee197a7
volnyagin <vconst@users.noreply.github.com>
2018-04-02 16:29:27 +0300

    Fix summary after virtual scrolling rework

--- testing/tests/DevExpress.ui.widgets.dataGrid/dataGrid.tests.js
+++ testing/tests/DevExpress.ui.widgets.dataGrid/dataGrid.tests.js
@@ -3807,6 +3807,31 @@ QUnit.test("Horizontal scroll position of footer view is changed_T251448", funct
     assert.equal($headersView.scrollLeft(), 300, "scroll left of headers view");
 });
 
+QUnit.test("Total summary row should be rendered if scrolling mode is virtual", function(assert) {
+    // arrange
+    var clock = sinon.useFakeTimers(),
+        $dataGrid = $("#dataGrid").dxDataGrid({
+            width: 300,
+            dataSource: [{ id: 1 }],
+            scrolling: {
+                mode: "virtual"
+            },
+            summary: {
+                totalItems: [{
+                    column: "id",
+                    summaryType: "count"
+                }]
+            }
+        });
+
+    // act
+    clock.tick();
+
+    var $footerView = $dataGrid.find(".dx-datagrid-total-footer");
+    assert.ok($footerView.is(":visible"), "footer view is visible");
+    assert.ok($footerView.find(".dx-row").length, 1, "one footer row is rendered");
+});
+
 QUnit.test("Keep horizontal scroller position after refresh with native scrolling", function(assert) {
     var done = assert.async(),
         dataGrid,
