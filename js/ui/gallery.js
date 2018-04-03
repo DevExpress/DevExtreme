c1c9726cdcb7923e9cbf81265e65415879b5b0f5
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2018-04-03 10:09:31 +0300

    Fix items changing in a Gallery (T621610) (#3503)

--- js/ui/gallery.js
+++ js/ui/gallery.js
@@ -420,7 +420,7 @@ var Gallery = CollectionWidget.inherit({
         this._renderItemSizes();
         this._renderItemPositions();
         this._renderIndicator();
-        this._renderContainerPosition(this._calculateIndexOffset(selectedIndex));
+        this._renderContainerPosition(this._calculateIndexOffset(selectedIndex), true);
         this._renderItemVisibility();
     },
 
@@ -535,7 +535,8 @@ var Gallery = CollectionWidget.inherit({
         return (1 - this._actualItemWidth() * itemsPerPage) / (itemsPerPage + 1);
     },
 
-    _renderContainerPosition: function(offset, animate) {
+    _renderContainerPosition: function(offset, hideItems, animate) {
+        this._releaseInvisibleItems();
         offset = offset || 0;
 
         var that = this,
@@ -557,11 +558,10 @@ var Gallery = CollectionWidget.inherit({
             positionReady = new Deferred().resolveWith(that);
         }
 
-        if(this._deferredAnimate) {
-            positionReady.done(function() {
-                that._deferredAnimate.resolveWith(that);
-            });
-        }
+        positionReady.done(function() {
+            this._deferredAnimate && that._deferredAnimate.resolveWith(that);
+            hideItems && this._renderItemVisibility();
+        });
 
         return positionReady.promise();
     },
@@ -993,8 +993,6 @@ var Gallery = CollectionWidget.inherit({
         }
 
         this.option("selectedIndex", paginatedIndex);
-
-        this._renderItemVisibility();
     },
 
     _setFocusOnSelect: function() {
@@ -1059,7 +1057,6 @@ var Gallery = CollectionWidget.inherit({
         this._renderNavButtonsVisibility();
 
         this._renderSelectedItem();
-        this._renderItemVisibility();
 
         this._relocateItems(addedSelection[0], removedSelection[0]);
 
@@ -1074,7 +1071,7 @@ var Gallery = CollectionWidget.inherit({
         var indexOffset = this._calculateIndexOffset(newIndex, prevIndex);
 
 
-        this._renderContainerPosition(indexOffset, this.option("animationEnabled") && !withoutAnimation).done(function() {
+        this._renderContainerPosition(indexOffset, true, this.option("animationEnabled") && !withoutAnimation).done(function() {
             this._setFocusOnSelect();
             this._userInteraction = false;
             this._setupSlideShow();
@@ -1214,7 +1211,6 @@ var Gallery = CollectionWidget.inherit({
         }
 
         this.option("selectedIndex", itemIndex);
-        this._renderItemVisibility();
         return this._deferredAnimate.promise();
     },
 
