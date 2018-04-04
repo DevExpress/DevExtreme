f8f155bb1282d8e72f4b793ed24405ac32baa96a
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2018-04-04 16:33:56 +0300

    dxTreeList: add filterPanel

---
+++ js/ui/tree_list/ui.tree_list.filter_panel.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var core = require("./ui.tree_list.core"),
+    filterPanelModule = require("../grid_core/ui.grid_core.filter_panel");
+
+core.registerModule("filterPanel", filterPanelModule);
