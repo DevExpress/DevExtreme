9b8cf0d380a749e98c698b318ea47b85c685d965
Andrey Ovchinnikov <ovchinnikov@users.noreply.github.com>
2018-04-04 11:25:28 +0300

    Add capability to define eventsStrategy as a function

--- testing/tests/DevExpress.ui/eventsStrategy.tests.js
+++ testing/tests/DevExpress.ui/eventsStrategy.tests.js
@@ -53,6 +53,43 @@ QUnit.test("setup event strategy", function(assert) {
     $("#element").remove();
 });
 
+QUnit.test("setup event strategy as function", function(assert) {
+    assert.expect(6);
+
+    var eventName = "testEventName";
+    var checkEventMethod = function(name) {
+        if(name === eventName) {
+            assert.ok(true, "event subscribed");
+        }
+    };
+
+    var eventsStrategy = function() {
+        return {
+            on: checkEventMethod,
+            off: checkEventMethod,
+            fireEvent: checkEventMethod,
+            hasEvent: function(name) {
+                checkEventMethod(name);
+                return true;
+            },
+            dispose: function() {
+                assert.ok(true, "strategy disposed");
+            }
+        };
+    };
+
+    var instance = $("#element").dxWidget({
+        eventsStrategy: eventsStrategy
+    }).dxWidget("instance");
+
+    instance.on(eventName, function() {});
+    instance.off(eventName, function() {});
+    assert.ok(instance.hasEvent(eventName));
+    instance.fireEvent(eventName);
+
+    $("#element").remove();
+});
+
 QUnit.test("callbacks should have correct context", function(assert) {
     assert.expect(1);
 
