83c4ca74bd6d67a017dbf85f7e9de1b8819a459a
vostrikova.elena <elena.vostrikova@devexpress.com>
2018-04-04 15:48:25 +0300

    Fix doccomments

--- js/ui/pivot_grid/ui.pivot_grid.field_chooser.js
+++ js/ui/pivot_grid/ui.pivot_grid.field_chooser.js
@@ -676,7 +676,6 @@ var FieldChooser = BaseFieldChooser.inherit({
     /**
     * @name dxPivotGridFieldChooserMethods_applyChanges
     * @publicName applyChanges()
-    * @return Promise<void>
     */
     applyChanges: function() {
         var state = this.option("state");
@@ -690,7 +689,6 @@ var FieldChooser = BaseFieldChooser.inherit({
     /**
     * @name dxPivotGridFieldChooserMethods_cancelChanges
     * @publicName cancelChanges()
-    * @return Promise<void>
     */
     cancelChanges: function() {
         if(isDefined(this.option("state"))) {
