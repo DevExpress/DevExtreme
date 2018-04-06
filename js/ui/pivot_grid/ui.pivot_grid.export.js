a7f7dfdda9fe25f6f9e8e87fed68ddbe9de38bd3
Dmitry Semenov <jsdmitry@users.noreply.github.com>
2018-04-06 16:35:59 +0300

    add the ignoreExcelErrors to excel export

--- js/ui/pivot_grid/ui.pivot_grid.export.js
+++ js/ui/pivot_grid/ui.pivot_grid.export.js
@@ -28,6 +28,7 @@ exports.ExportMixin = extend({}, exportMixin, {
             proxyUrl: that.option("export.proxyUrl"),
             format: "EXCEL",
             rtlEnabled: that.option("rtlEnabled"),
+            ignoreErrors: that.option("export.ignoreExcelErrors"),
             exportingAction: that._actions.onExporting,
             exportedAction: that._actions.onExported,
             fileSavingAction: that._actions.onFileSaving
--- js/ui/pivot_grid/ui.pivot_grid.js
+++ js/ui/pivot_grid/ui.pivot_grid.js
@@ -464,7 +464,14 @@ var PivotGrid = Widget.inherit({
                  * @type string
                  * @default undefined
                  */
-                proxyUrl: undefined
+                proxyUrl: undefined,
+                /**
+                 * @name dxPivotGridOptions_export_ignoreExcelErrors
+                 * @publicName ignoreExcelErrors
+                 * @type boolean
+                 * @default true
+                 */
+                ignoreExcelErrors: true
             },
             /**
              * @name dxPivotGridOptions_showRowTotals
