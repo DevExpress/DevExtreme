0071be907eaf3e5eebb02d2b3a9ccf29106c9936
Alexander Komarov <sanrkom@gmail.com>
2018-05-03 12:26:29 +0300

    Add "Is" to names of operations in FilterBuilder

--- js/ui/filter_builder/filter_builder.js
+++ js/ui/filter_builder/filter_builder.js
@@ -424,7 +424,7 @@ var FilterBuilder = Widget.inherit({
                  * @type string
                  * @default "Between"
                  */
-                between: messageLocalization.format("dxDataGrid-filterRowOperationBetween"),
+                between: messageLocalization.format("dxFilterBuilder-filterOperationBetween"),
                 /**
                  * @name dxFilterBuilderOptions_filterOperationDescriptions_equal
                  * @publicName equal
@@ -581,7 +581,7 @@ var FilterBuilder = Widget.inherit({
     },
 
     _initCustomOperations: function() {
-        this._customOperations = utils.getMergedOperations(this.option("customOperations"), this.option("operationDescriptions.between"));
+        this._customOperations = utils.getMergedOperations(this.option("customOperations"), this.option("filterOperationDescriptions.between"));
     },
 
     _initModel: function() {
--- js/ui/grid_core/ui.grid_core.filter_builder.js
+++ js/ui/grid_core/ui.grid_core.filter_builder.js
@@ -134,8 +134,23 @@ module.exports = {
                     and: messageLocalization.format("dxFilterBuilder-and"),
                     or: messageLocalization.format("dxFilterBuilder-or"),
                     notAnd: messageLocalization.format("dxFilterBuilder-notAnd"),
-                    notOr: messageLocalization.format("dxFilterBuilder-notOr"),
+                    notOr: messageLocalization.format("dxFilterBuilder-notOr")
                 },
+                filterOperationDescriptions: {
+                    between: messageLocalization.format("dxFilterBuilder-filterOperationBetween"),
+                    equal: messageLocalization.format("dxFilterBuilder-filterOperationEquals"),
+                    notEqual: messageLocalization.format("dxFilterBuilder-filterOperationNotEquals"),
+                    lessThan: messageLocalization.format("dxFilterBuilder-filterOperationLess"),
+                    lessThanOrEqual: messageLocalization.format("dxFilterBuilder-filterOperationLessOrEquals"),
+                    greaterThan: messageLocalization.format("dxFilterBuilder-filterOperationGreater"),
+                    greaterThanOrEqual: messageLocalization.format("dxFilterBuilder-filterOperationGreaterOrEquals"),
+                    startsWith: messageLocalization.format("dxFilterBuilder-filterOperationStartsWith"),
+                    contains: messageLocalization.format("dxFilterBuilder-filterOperationContains"),
+                    notContains: messageLocalization.format("dxFilterBuilder-filterOperationNotContains"),
+                    endsWith: messageLocalization.format("dxFilterBuilder-filterOperationEndsWith"),
+                    isBlank: messageLocalization.format("dxFilterBuilder-filterOperationIsBlank"),
+                    isNotBlank: messageLocalization.format("dxFilterBuilder-filterOperationIsNotBlank")
+                }
             },
 
             /**
