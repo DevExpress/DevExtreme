a12dd36af9735f2df97d5f09f4d0bd4ea6168fcf
Konstantin Volnyagin <vconst@users.noreply.github.com>
2018-05-08 10:28:31 +0300

    DataGrid: Fix virtual scrolling bugs with large data (#3922)

--- testing/tests/DevExpress.ui.widgets.dataGrid/rowsView.tests.js
+++ testing/tests/DevExpress.ui.widgets.dataGrid/rowsView.tests.js
@@ -5168,6 +5168,7 @@ QUnit.module('Virtual scrolling', {
             rowsView._dataController._itemSizes = {};
             rowsView._dataController.getVirtualContentSize = x.getVirtualContentSize;
             rowsView._dataController.getContentOffset = x.getContentOffset;
+            rowsView._dataController.getItemSize = x.getItemSize;
             rowsView._dataController.viewportItemSize = x.viewportItemSize;
             rowsView._dataController.setContentSize = x.setContentSize;
             rowsView._dataController.setViewportPosition = x.setViewportPosition;
