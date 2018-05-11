aa5a57045cc1ecaa45b83013bcf2f0961bd78d04
Eugen Zha <nightskylark@users.noreply.github.com>
2018-05-11 15:14:04 +0300

    Get rid of cldr and globalize in vendor folder (#3962)

--- build/gulp/vendor.js
+++ build/gulp/vendor.js
@@ -2,18 +2,69 @@
 
 "use strict";
 
-var gulp = require('gulp');
-var merge = require('merge-stream');
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const merge = require('merge-stream');
+const context = require('./context.js');
+const compressionPipes = require('./compression-pipes.js');
+
+const PACKAGES_SOURCE = './node_modules';
+const DESTINATION_PATH = './artifacts/js';
+
+const VENDORS = [
+    {
+        path: '/angular/angular.js'
+    },
+    {
+        path: '/jquery/dist/jquery.js'
+    },
+    {
+        path: '/jszip/dist/jszip.js'
+    },
+    {
+        path: '/knockout/build/output/knockout-latest.js',
+        suffix: 'debug'
+    },
+    {
+        path: '/cldrjs/dist/cldr.js',
+        noUglyFile: true
+    },
+    {
+        path: '/globalize/dist/globalize.js',
+        noUglyFile: true
+    },
+    {
+        path: '/cldrjs/dist/cldr/@(event|supplemental|unresolved).js',
+        noUglyFile: true,
+        base: '/cldrjs/dist/'
+    },
+    {
+        path: '/globalize/dist/globalize/@(number|currency|date|message).js',
+        noUglyFile: true,
+        base: '/globalize/dist/'
+    }
+];
 
 gulp.task('vendor', function() {
-    return merge(
-        gulp.src([
-            './node_modules/angular/angular?(.min).js',
-            './vendor/cldr/**',
-            './vendor/globalize/**',
-            './node_modules/jquery/dist/jquery?(.min).js',
-            './node_modules/jszip/dist/jszip?(.min).js',
-            './node_modules/knockout/build/output/knockout-latest?(.debug).js'
-        ]).pipe(gulp.dest('artifacts/js'))
-    );
+    return merge.apply(this, VENDORS.map(function(vendor) {
+        let sourceConfig = vendor.base ? { base: PACKAGES_SOURCE + vendor.base } : null;
+        let source = gulp.src(PACKAGES_SOURCE + vendor.path, sourceConfig);
+        let stream = merge(source.pipe(gulp.dest(DESTINATION_PATH)));
+
+        if(context.uglify) {
+            if(vendor.noUglyFile) {
+                stream.add(source
+                    .pipe(compressionPipes.minify())
+                    .pipe(rename({ suffix: '.min' }))
+                    .pipe(gulp.dest(DESTINATION_PATH))
+                );
+            } else {
+                let path = PACKAGES_SOURCE + vendor.path.replace(/js$/, `${vendor.suffix || 'min'}.js`);
+                let minSource = gulp.src(path, sourceConfig);
+                stream.add(minSource.pipe(gulp.dest(DESTINATION_PATH)));
+            }
+        }
+
+        return stream;
+    }));
 });
