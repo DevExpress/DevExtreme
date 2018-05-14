3dc14da0e3c0b95734ebf7e622daca65f3d81b38
roman-simionov <rsimionov@gmail.com>
2018-05-14 18:29:34 +0300

    Fix formatting

--- js/ui/pivot_grid/xmla_store/xmla_store.js
+++ js/ui/pivot_grid/xmla_store/xmla_store.js
@@ -813,7 +813,7 @@ exports.XmlaStore = Class.inherit((function() {
 
     /**
     * @name XmlaStore
-* @publicName XmlaStore
+    * @publicName XmlaStore
     * @type object
     * @namespace DevExpress.data
     * @module ui/pivot_grid/xmla_store
@@ -824,36 +824,36 @@ exports.XmlaStore = Class.inherit((function() {
         ctor: function(options) {
             this._options = options;
 
-            /**
+        /**
         * @name XmlaStoreOptions_url
         * @publicName url
-            * @type string
-            */
+        * @type string
+        */
 
-            /**
+        /**
         * @name XmlaStoreOptions_catalog
         * @publicName catalog
-            * @type string
-            */
+        * @type string
+        */
 
-            /**
+        /**
         * @name XmlaStoreOptions_cube
         * @publicName cube
-            * @type string
-            */
+        * @type string
+        */
 
-            /**
+        /**
          * @name XmlaStoreOptions_beforeSend
          * @publicName beforeSend
-             * @type function
-             * @type_function_param1 options:object
-             * @type_function_param1_field1 url:string
-             * @type_function_param1_field2 method:string
-             * @type_function_param1_field3 headers:object
-             * @type_function_param1_field4 xhrFields:object
-             * @type_function_param1_field5 data:string
-             * @type_function_param1_field6 dataType:string
-             */
+         * @type function
+         * @type_function_param1 options:object
+         * @type_function_param1_field1 url:string
+         * @type_function_param1_field2 method:string
+         * @type_function_param1_field3 headers:object
+         * @type_function_param1_field4 xhrFields:object
+         * @type_function_param1_field5 data:string
+         * @type_function_param1_field6 dataType:string
+         */
         },
 
         getFields: function() {
