c2525bebcd6f1db656fc8c42458f66f9f52fbb36
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2018-05-18 14:44:24 +0300

    Fix IE & FF Scheduler tests

--- testing/tests/DevExpress.ui.widgets.scheduler/integration.workSpace.tests.js
+++ testing/tests/DevExpress.ui.widgets.scheduler/integration.workSpace.tests.js
@@ -1537,7 +1537,7 @@ QUnit.test("Group header should contain group header content with right height, 
         $headerContents = header.find(".dx-scheduler-group-header-content"),
         cellHeight = this.instance.$element().find(".dx-scheduler-date-table-cell").eq(1).outerHeight();
 
-    assert.equal($headerContents.eq(0).outerHeight(), 7 * cellHeight - 2 * DATE_TABLE_CELL_BORDER, "Group header content has right height");
+    assert.roughEqual($headerContents.eq(0).outerHeight(), 7 * cellHeight - 2 * DATE_TABLE_CELL_BORDER, 1, "Group header content has right height");
 });
 
 QUnit.test("WorkSpace should be refreshed after groups changed", function(assert) {
