24a92dc5fbdc8f92537fd77f5cc46429d557eb09
Alexander Komarov <sanrkom@gmail.com>
2018-05-24 13:23:01 +0300

    Fix tests

--- js/ui/grid_core/ui.grid_core.filter_builder.js
+++ js/ui/grid_core/ui.grid_core.filter_builder.js
@@ -60,9 +60,8 @@ var FilterBuilderView = modules.View.inherit({
             fields = this.getController("columns").getColumns(),
             customOperations = this.getController("filterSync").getCustomFilterOperations();
 
-        fields = fields.filter(function(item) {
-            return item.allowFiltering;
-        }).map(function(item) {
+        fields = fields.filter(item => item.allowFiltering && item.filterOperations)
+        .map(item => {
             var column = extend(true, {}, item);
             ["anyof", "noneof"].forEach(item => !column.filterOperations.includes(item) && column.filterOperations.push(item));
             return column;
