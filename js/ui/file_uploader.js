00441a32780c8d831342780c92f67d7ab5af4f33
levkovskiy.dmitry <dmitry.levkovskiy@devexpress.com>
2018-05-24 17:30:30 +0300

    Fix the FileUploader drag events handling (T631022)

--- js/ui/file_uploader.js
+++ js/ui/file_uploader.js
@@ -886,7 +886,7 @@ var FileUploader = Editor.inherit({
             return;
         }
 
-        this._dragEventsCount = 0;
+        this._dragEventsTargets = [];
 
         eventsEngine.on(this._$inputWrapper, eventUtils.addNamespace("dragenter", this.NAME), this._dragEnterHandler.bind(this));
         eventsEngine.on(this._$inputWrapper, eventUtils.addNamespace("dragover", this.NAME), this._dragOverHandler.bind(this));
@@ -907,7 +907,8 @@ var FileUploader = Editor.inherit({
             e.preventDefault();
         }
 
-        this._dragEventsCount++;
+        this._updateEventTargets(e);
+
         this.$element().addClass(FILEUPLOADER_DRAGOVER_CLASS);
     },
 
@@ -922,15 +923,26 @@ var FileUploader = Editor.inherit({
             e.preventDefault();
         }
 
-        this._dragEventsCount--;
+        this._updateEventTargets(e);
 
-        if(this._dragEventsCount <= 0) {
+        if(!this._dragEventsTargets.length) {
             this.$element().removeClass(FILEUPLOADER_DRAGOVER_CLASS);
         }
     },
 
+    _updateEventTargets: function(e) {
+        var targetIndex = this._dragEventsTargets.indexOf(e.target),
+            isTargetExists = targetIndex !== -1;
+
+        if(e.type === "dragenter") {
+            !isTargetExists && this._dragEventsTargets.push(e.target);
+        } else {
+            isTargetExists && this._dragEventsTargets.splice(targetIndex, 1);
+        }
+    },
+
     _dropHandler: function(e) {
-        this._dragEventsCount = 0;
+        this._dragEventsTargets = [];
         this.$element().removeClass(FILEUPLOADER_DRAGOVER_CLASS);
 
         if(this._useInputForDrop()) {
