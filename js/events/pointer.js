77ba15e6ef35dc5a46c8eace84324304ce5e796a
Alexander Bezborodov <alexander.bezborodov@devexpress.com>
2018-05-31 13:43:54 +0300

    Revert chart improvements for zooming/scrolling and behavior of the interaction with the page (#3080) (#4169)

--- js/events/pointer.js
+++ js/events/pointer.js
@@ -4,7 +4,6 @@ var support = require("../core/utils/support"),
     each = require("../core/utils/iterator").each,
     devices = require("../core/devices"),
     registerEvent = require("./core/event_registrator"),
-    domAdapter = require("../core/dom_adapter"),
     TouchStrategy = require("./pointer/touch"),
     MsPointerStrategy = require("./pointer/mspointer"),
     MouseStrategy = require("./pointer/mouse"),
@@ -87,20 +86,6 @@ var EventStrategy = (function() {
 each(EventStrategy.map, function(pointerEvent, originalEvents) {
     registerEvent(pointerEvent, new EventStrategy(pointerEvent, originalEvents));
 });
-
-if(support.touch) {
-    each([TouchStrategy.map["dxpointerdown"], TouchStrategy.map["dxpointermove"]], function(_, eventName) {
-        registerEvent(eventName, {
-            setup: function(element, data, namespaces, handler) {
-                if(namespaces.indexOf(TouchStrategy.POINTER_EVENTS_NAMESPACE) > -1) {
-                    domAdapter.listen(element, eventName, handler, { passive: false });
-                    return true;
-                }
-                return false;
-            }
-        });
-    });
-}
 
 module.exports = {
     down: "dxpointerdown",
