1f709b94312d39f72345aad515be22268ba8a956
Eugeniy Maslyaev <mrjohn0011@users.noreply.github.com>
2018-06-04 12:43:47 +0300

    Reformat incomplete values in the dxNumberBox if paste event is used (T641634)

--- js/ui/number_box/number_box.mask.js
+++ js/ui/number_box/number_box.mask.js
@@ -80,7 +80,6 @@ var NumberBoxMask = NumberBoxBase.inherit({
         }
     },
 
-
     _focusOutHandler: function(e) {
         this._focusOutOccurs = true;
         if(this._useMaskBehavior()) {
@@ -92,6 +91,7 @@ var NumberBoxMask = NumberBoxBase.inherit({
     },
 
     _updateFormattedValue: function() {
+        this._parsedValue = this._tryParse(this._getInputVal(), this._caret(), "");
         this._adjustParsedValue();
         this._setTextByParsedValue();
 
