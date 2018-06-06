9d0f1a9ef067f091948fa7a2523137f452f7a40c
Vasiliy Sokolov <vasiliy.sokolov@devexpress.com>
2018-06-06 11:55:04 +0300

    link items test

--- testing/tests/DevExpress.viz.sankey/sankey.items.link.tests.js
+++ testing/tests/DevExpress.viz.sankey/sankey.items.link.tests.js
@@ -1,2 +1,38 @@
 // TODO: Drawing link exactly between to rectangles
 // TODO: links colorMode
+
+"use strict";
+
+var common = require("./commonParts/common.js"),
+    createSankey = common.createSankey,
+    environment = common.environment,
+    themeModule = require("viz/themes");
+
+themeModule.registerTheme({
+    name: "test-theme",
+    sankey: {
+        nodes: {
+            border: {
+                visible: true,
+                color: "black"
+            }
+        }
+    } }, "generic.light");
+
+
+QUnit.module("Items: links", environment);
+
+QUnit.test("Creation", function(assert) {
+    var sankey = createSankey({
+            dataSource: [['A', 'Z', 1], ['B', 'Z', 1]],
+        }),
+        links = sankey.getAllItems().links;
+
+    assert.equal(links[0].connection.from, 'A');
+    assert.equal(links[0].connection.to, 'Z');
+    assert.equal(links[0].connection.weight, 1);
+
+    assert.equal(links[1].connection.from, 'B');
+    assert.equal(links[1].connection.to, 'Z');
+    assert.equal(links[1].connection.weight, 1);
+});
