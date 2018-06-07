5c5b0edbcc3a1adfc5427b26bf3a1cb5ce13a4aa
Alyar <Alyar666@users.noreply.github.com>
2018-06-07 17:51:51 +0300

    dxDataGrid: Fix incorrect restoring a selected filter operation after resetting the filter value when stateStoring is enabled (T629814) (#4283)

--- js/ui/grid_core/ui.grid_core.columns_controller.js
+++ js/ui/grid_core/ui.grid_core.columns_controller.js
@@ -957,6 +957,9 @@ module.exports = {
                                 column[fieldName] = userStateColumn[fieldName];
                             }
                         } else {
+                            if(fieldName === "selectedFilterOperation" && userStateColumn[fieldName]) {
+                                column.defaultSelectedFilterOperation = column[fieldName] || null;
+                            }
                             column[fieldName] = userStateColumn[fieldName];
                         }
                     }
@@ -2667,7 +2670,7 @@ module.exports = {
                     if(columnOptions.dataType) {
                         calculatedColumnOptions.userDataType = columnOptions.dataType;
                     }
-                    if(columnOptions.selectedFilterOperation) {
+                    if(columnOptions.selectedFilterOperation && !("defaultSelectedFilterOperation" in calculatedColumnOptions)) {
                         calculatedColumnOptions.defaultSelectedFilterOperation = columnOptions.selectedFilterOperation;
                     }
                     if(columnOptions.lookup) {
