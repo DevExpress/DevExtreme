22bdef5a877204917c062e13860260b9a02c8fb7
alexander-kotov-dx <alexander.kotov@devexpress.com>
2018-06-08 20:38:00 +0300

    Change event name dxpointerup to pointerEvents.up

--- js/ui/list/ui.list.edit.decorator.switchable.js
+++ js/ui/list/ui.list.edit.decorator.switchable.js
@@ -133,7 +133,7 @@ var SwitchableEditDecorator = EditDecorator.inherit({
             this._enablePositioning($itemElement);
             this._prepareDeleteReady($itemElement);
             this._animatePrepareDeleteReady($itemElement);
-            eventsEngine.off($itemElement, "dxpointerup");
+            eventsEngine.off($itemElement, pointerEvents.up);
         } else {
             this._forgetDeleteReady($itemElement);
             this._animateForgetDeleteReady($itemElement).done(this._disablePositioning.bind(this, $itemElement));
@@ -143,7 +143,7 @@ var SwitchableEditDecorator = EditDecorator.inherit({
     _enablePositioning: function($itemElement) {
         $itemElement.addClass(SWITCHABLE_MENU_ITEM_SHIELD_POSITIONING_CLASS);
         eventsEngine.on($itemElement, ACTIVE_EVENT_NAME, noop);
-        eventsEngine.one($itemElement, "dxpointerup", this._disablePositioning.bind(this, $itemElement));
+        eventsEngine.one($itemElement, pointerEvents.up, this._disablePositioning.bind(this, $itemElement));
     },
 
     _disablePositioning: function($itemElement) {
