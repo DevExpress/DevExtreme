69da02562c9fd5174bb3c966432f0d3a5f3cdefa
Konstantin Volnyagin <vconst@users.noreply.github.com>
2018-06-08 16:26:26 +0300

    Hide registerKeyHandler method for DataGrid/TreeList (T643261) (#4292)

--- js/ui/grid_core/ui.grid_core.keyboard_navigation.js
+++ js/ui/grid_core/ui.grid_core.keyboard_navigation.js
@@ -941,6 +941,13 @@ var KeyboardNavigationController = core.ViewController.inherit({
     }
 });
 
+/**
+* @name GridBaseMethods.registerKeyHandler
+* @publicName registerKeyHandler(key, handler)
+* @hidden
+* @inheritdoc
+*/
+
 module.exports = {
     defaultOptions: function() {
         return {
