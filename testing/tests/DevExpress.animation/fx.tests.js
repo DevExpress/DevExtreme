a0ef5defd30cbab30d4fb28bf3d21773be4304e1
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2018-06-18 15:39:11 +0300

    Fix list context menu position after page scrolling, part 2 (T611393) (#4410)

--- testing/tests/DevExpress.animation/fx.tests.js
+++ testing/tests/DevExpress.animation/fx.tests.js
@@ -97,6 +97,46 @@ QUnit.test("basic animation", function(assert) {
     this.clock.tick(20);
 });
 
+QUnit.test("basic animation when window was scrolled", function(assert) {
+    assert.expect(2);
+
+    var $container = $("#container"),
+        $wrapper = $("<div>").appendTo("body"),
+        $element = $("<div>").appendTo("body");
+
+    try {
+        $wrapper.css({ height: "150%", width: "150%", position: "absolute", top: "0", left: "0" });
+        $element.css({ height: 50, width: 50, top: "200px", background: 'blue' });
+
+        var initialTopPosition = $element.get(0).getBoundingClientRect().top,
+            initialLeftPosition = $element.get(0).getBoundingClientRect().left;
+
+        var done = assert.async();
+
+        window.scrollBy(200, 200);
+
+        this.animate($element, {
+            type: 'slide',
+            position: {
+                my: "right",
+                at: "right",
+                of: $container
+            },
+            duration: 200,
+            complete: function() {
+                assert.roughEqual($element.get(0).getBoundingClientRect().top, initialTopPosition - 200, 1.5, "position after animation is correct");
+                assert.roughEqual($element.get(0).getBoundingClientRect().left, initialLeftPosition - 200, 1, "position after animation is correct");
+                done();
+            }
+        });
+        this.clock.tick(250);
+    } finally {
+        window.scroll(0, 0);
+        $wrapper.remove();
+        $element.remove();
+    }
+});
+
 QUnit.test("draw callback", function(assert) {
     var $element = $("#test"),
         done = assert.async();
