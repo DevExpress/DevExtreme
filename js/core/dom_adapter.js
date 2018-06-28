4f4451a7c924d025f6c28f12cdbb7b4ad8707913
alexander-kotov-dx <alexander.kotov@devexpress.com>
2018-06-28 11:39:52 +0300

    Prevent passive event listeners problem (T630650)

--- js/core/dom_adapter.js
+++ js/core/dom_adapter.js
@@ -166,12 +166,12 @@ var nativeDOMAdapterStrategy = {
         return property in this._document;
     },
 
-    listen: function(element, event, callback, useCapture) {
+    listen: function(element, event, callback, options) {
         if(!element || !("addEventListener" in element)) {
             return noop;
         }
 
-        element.addEventListener(event, callback, useCapture);
+        element.addEventListener(event, callback, options);
 
         return function() {
             element.removeEventListener(event, callback);
