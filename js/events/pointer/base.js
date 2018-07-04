33bf141966aa8ea7b9873182fc71d4b146996e30
levkovskiy.dmitry <dmitry.levkovskiy@devexpress.com>
2018-07-04 17:17:54 +0300

    Get rid of memory leak in pointer events (T646729)

--- js/events/pointer/base.js
+++ js/events/pointer/base.js
@@ -52,6 +52,14 @@ var BaseStrategy = Class.inherit({
         return eventUtils.fireEvent(args);
     },
 
+    _setSelector: function(handleObj) {
+        this._selector = this.noBubble && handleObj ? handleObj.selector : null;
+    },
+
+    _getSelector: function() {
+        return this._selector;
+    },
+
     setup: function() {
         return true;
     },
@@ -59,10 +67,10 @@ var BaseStrategy = Class.inherit({
     add: function(element, handleObj) {
         if(this._handlerCount <= 0 || this.noBubble) {
             element = this.noBubble ? element : domAdapter.getDocument();
-            this._selector = this.noBubble ? handleObj.selector : null;
+            this._setSelector(handleObj);
 
             var that = this;
-            eventsEngine.on(element, this._originalEvents, this._selector, function(e) {
+            eventsEngine.on(element, this._originalEvents, this._getSelector(), function(e) {
                 that._handler(e);
             });
         }
@@ -72,7 +80,9 @@ var BaseStrategy = Class.inherit({
         }
     },
 
-    remove: function() {
+    remove: function(handleObj) {
+        this._setSelector(handleObj);
+
         if(!this.noBubble) {
             this._handlerCount--;
         }
@@ -86,7 +96,7 @@ var BaseStrategy = Class.inherit({
         element = this.noBubble ? element : domAdapter.getDocument();
 
         if(this._originalEvents !== "." + POINTER_EVENTS_NAMESPACE) {
-            eventsEngine.off(element, this._originalEvents, this._selector);
+            eventsEngine.off(element, this._originalEvents, this._getSelector());
         }
     },
 
