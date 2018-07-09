bc4938e1e250ac8ce78200deb858d8940a7527b7
Kseniya Mordalyuk <KsyushaM@users.noreply.github.com>
2018-07-09 17:19:27 +0300

    Add description for Recurrence Editor options (#4638)

---
+++ js/ui/recurrence_editor.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/**
+* @name dxRecurrenceEditor
+* @isEditor
+* @inherits Editor
+* @module ui/recurrence_editor
+* @export default
+*/
+module.exports = require("./scheduler/ui.scheduler.recurrence_editor");
