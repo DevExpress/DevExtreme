1579e89665c1a03982602608785275b1549fe219
Eugeniy Maslyaev <mrjohn0011@users.noreply.github.com>
2018-07-20 18:00:46 +0300

    Find items by keys in dxTreeView expander icon click handler (T656682) (#4765)

--- js/ui/tree_view/ui.tree_view.base.js
+++ js/ui/tree_view/ui.tree_view.base.js
@@ -1042,7 +1042,7 @@ var TreeViewBase = HierarchicalCollectionWidget.inherit({
 
         eventsEngine.off($icon, eventName);
         eventsEngine.on($icon, eventName, function(e) {
-            that._toggleExpandedState(node, undefined, e);
+            that._toggleExpandedState(node.internalFields.key, undefined, e);
         });
     },
 
