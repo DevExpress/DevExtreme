389b0493c577194b01616b0d1a8f9b8ae2d0f56a
Kseniya Mordalyuk <kseniya.mordalyuk@devexpress.com>
2018-07-25 13:45:23 +0300

    Fix unchecking default day from byday rule on Scheduler recurrence appointment form (T657610)

--- js/ui/scheduler/ui.scheduler.recurrence_editor.js
+++ js/ui/scheduler/ui.scheduler.recurrence_editor.js
@@ -814,7 +814,8 @@ var SchedulerRecurrenceEditor = Editor.inherit({
 
         this._changeRepeatCountValue();
         this._changeRepeatUntilValue();
-        this._changeCheckBoxesValue();
+        this._changeCheckBoxesValue(!!rules["byday"]);
+
         this._changeDayOfMonthValue();
         this._changeMonthOfYearValue();
     },
@@ -854,8 +855,8 @@ var SchedulerRecurrenceEditor = Editor.inherit({
         return this._recurrenceRule.rules().until || this._formatUntilDate(new Date());
     },
 
-    _changeCheckBoxesValue: function() {
-        if(!this._$repeatOnWeek) {
+    _changeCheckBoxesValue: function(byDayChanged) {
+        if(!this._$repeatOnWeek || !byDayChanged) {
             return;
         }
 
