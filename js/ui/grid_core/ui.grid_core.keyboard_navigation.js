872ddacf0ca7328d96c3ede717d3a6fadd16cb98
CORP\sermyazhko.anton <anton.sermyazhko@devexpress.com>
2018-08-02 16:00:02 +0300

    DataGrid - After apply the edit value and focus the editor do not display the revert button while the save process, if editing mode is cell (T657148)

--- js/ui/grid_core/ui.grid_core.keyboard_navigation.js
+++ js/ui/grid_core/ui.grid_core.keyboard_navigation.js
@@ -353,7 +353,6 @@ var KeyboardNavigationController = core.ViewController.inherit({
                 } else {
                     var $target = $(eventArgs.originalEvent.target);
                     eventsEngine.trigger($target, "blur");
-                    eventsEngine.trigger($target, "focus");
                     this._editingController.closeEditCell();
                     eventArgs.originalEvent.preventDefault();
                 }
