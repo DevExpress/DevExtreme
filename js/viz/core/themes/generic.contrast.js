2de979cef950f98ae1f47e6745983b31abcf7964
Vasiliy Sokolov <vasiliy.sokolov@devexpress.com>
2018-08-03 12:48:25 +0300

    Sankey diagram: generic.dark and generic.contrast themes settings

--- js/viz/core/themes/generic.contrast.js
+++ js/viz/core/themes/generic.contrast.js
@@ -288,5 +288,30 @@ registerTheme({
                 opacity: 1
             }
         }
+    },
+    sankey: {
+        label: {
+            font: {
+                color: WHITE
+            }
+        },
+        node: {
+            border: {
+                visible: true,
+                width: 1,
+                color: WHITE
+            }
+        },
+        link: {
+            opacity: 0.5,
+            border: {
+                visible: true,
+                width: 1,
+                color: WHITE
+            },
+            hoverStyle: {
+                opacity: 0.9
+            }
+        }
     }
 }, "generic.light");
