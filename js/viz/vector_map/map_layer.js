098804825521e5ef2dab6d9e3119dac3ff53c81d
polosatov.alexander <alexanderPolosatov@users.noreply.github.com>
2018-08-03 14:36:11 +0300

    Fix updating hovered layer issue

--- js/viz/vector_map/map_layer.js
+++ js/viz/vector_map/map_layer.js
@@ -1521,6 +1521,7 @@ MapLayerCollection.prototype = {
     dispose: function() {
         var that = this;
         that._clip.dispose();
+        that._layers.forEach(l => l.dispose());
         that._offTracker();
         that._params = that._offTracker = that._layers = that._layerByName = that._clip = that._background = that._container = null;
     },
@@ -1557,21 +1558,23 @@ MapLayerCollection.prototype = {
     },
 
     setOptions(options) {
+        const that = this;
         const optionList = options ? (_isArray(options) ? options : [options]) : [];
-        let layerByName = this._layerByName;
-        let layers = this._layers;
+        let layerByName = that._layerByName;
+        let layers = that._layers;
         const needToCreateLayers = optionList.length !== layers.length || layers.some((l, i) => {
             const name = getName(optionList, i);
             return _isDefined(name) && name !== l.proxy.name;
         });
 
         if(needToCreateLayers) {
-            layers.forEach(l => l.dispose());
-            this._layerByName = layerByName = {};
-            this._layers = layers = [];
+            that._params.tracker.reset();
+            that._layers.forEach(l => l.dispose());
+            that._layerByName = layerByName = {};
+            that._layers = layers = [];
             for(let i = 0, ii = optionList.length; i < ii; ++i) {
                 const name = getName(optionList, i) || ("map-layer-" + i);
-                const layer = layers[i] = new MapLayer(this._params, this._container, name, i);
+                const layer = layers[i] = new MapLayer(that._params, that._container, name, i);
                 layerByName[name] = layer;
             }
         }
