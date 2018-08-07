5edec3370e5be0d00bd5f1a3c356f87c8d2a965d
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2018-08-07 13:21:26 +0300

    Make templates of SlideOutView, DropDownEditors and Toolbar async (#4853)

---
+++ js/ui/shared/async_template_mixin.js
@@ -0,0 +1,26 @@
+"use strict";
+
+module.exports = {
+    _renderAsyncTemplate(template, args) {
+        const result = template && template.render(args);
+        if(!this._options.templatesRenderAsynchronously) {
+            args.onRendered.call(this);
+        }
+        return result;
+    },
+
+    _waitAsyncTemplates(callback) {
+        if(this._options.templatesRenderAsynchronously) {
+            this._asyncTemplatesTimer = setTimeout(function() {
+                callback.call(this);
+                this._cleanAsyncTemplatesTimer();
+            }.bind(this));
+        } else {
+            callback.call(this);
+        }
+    },
+
+    _cleanAsyncTemplatesTimer() {
+        clearTimeout(this._asyncTemplatesTimer);
+    }
+};
