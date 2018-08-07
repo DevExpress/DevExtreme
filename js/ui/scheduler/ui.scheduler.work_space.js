2d1cd39bdc733efea694148adea03eb87e14a390
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2018-08-07 14:31:40 +0300

    Fix errors occuring in Scheduler after navigating between months in ACST timezone (T661408) (#4951)

--- js/ui/scheduler/ui.scheduler.work_space.js
+++ js/ui/scheduler/ui.scheduler.work_space.js
@@ -2222,8 +2222,14 @@ var SchedulerWorkSpace = Widget.inherit({
     },
 
     getEndViewDate: function() {
-        var dateOfLastViewCell = this.getDateOfLastViewCell();
-        return new Date(dateOfLastViewCell.getTime() + this.getCellDuration() - 60000);
+        var dateOfLastViewCell = this.getDateOfLastViewCell(),
+            endDateOfLastViewCell = new Date(dateOfLastViewCell.getTime() + this.getCellDuration());
+
+        var daylightDiff = this.invoke("getDaylightOffset", dateOfLastViewCell, endDateOfLastViewCell) * toMs("minute") || 0;
+
+        endDateOfLastViewCell = new Date(endDateOfLastViewCell.getTime() + daylightDiff);
+
+        return new Date(endDateOfLastViewCell.getTime() - toMs("minute"));
     },
 
     getDateOfLastViewCell: function() {
