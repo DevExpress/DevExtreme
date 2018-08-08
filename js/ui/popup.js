ec298c0924daea42dfe08606e34acf3fe316e933
alexander-kotov-dx <alexander.kotov@devexpress.com>
2018-08-08 15:47:33 +0300

    Update compact mode for bottom toolbar popup buttons (T651511) (#4977)

--- js/ui/popup.js
+++ js/ui/popup.js
@@ -34,8 +34,6 @@ var POPUP_CLASS = "dx-popup",
 
     POPUP_BOTTOM_CLASS = "dx-popup-bottom",
 
-    POPUP_TOOLBAR_COMPACT_CLASS = "dx-popup-toolbar-compact",
-
     TEMPLATE_WRAPPER_CLASS = "dx-template-wrapper",
 
     ALLOWED_TOOLBAR_ITEM_ALIASES = ["cancel", "clear", "done"],
@@ -246,8 +244,7 @@ var Popup = Overlay.inherit({
 
             bottomTemplate: "bottom",
             useDefaultToolbarButtons: false,
-            useFlatToolbarButtons: false,
-            toolbarCompactMode: false
+            useFlatToolbarButtons: false
         });
     },
 
@@ -449,17 +446,17 @@ var Popup = Overlay.inherit({
         }
     },
 
-    _renderTemplateByType: function(optionName, data, $container) {
+    _renderTemplateByType: function(optionName, data, $container, additionalToolbarOptions) {
         var template = this._getTemplateByOption(optionName),
             toolbarTemplate = template instanceof EmptyTemplate;
 
         if(toolbarTemplate) {
-            var toolbarOptions = {
+            var toolbarOptions = extend(additionalToolbarOptions, {
                 items: data,
                 rtlEnabled: this.option("rtlEnabled"),
                 useDefaultButtons: this.option("useDefaultToolbarButtons"),
                 useFlatButtons: this.option("useFlatToolbarButtons")
-            };
+            });
 
             this._getTemplate("dx-polymorph-widget").render({
                 container: $container,
@@ -606,44 +603,11 @@ var Popup = Overlay.inherit({
         if(items.length) {
             this._$bottom && this._$bottom.remove();
             var $bottom = $("<div>").addClass(POPUP_BOTTOM_CLASS).insertAfter(this.$content());
-            this._$bottom = this._renderTemplateByType("bottomTemplate", items, $bottom).addClass(POPUP_BOTTOM_CLASS);
-            this.$bottomToolbarContentItems = this._$bottom.find(".dx-toolbar-items-container > div");
+            this._$bottom = this._renderTemplateByType("bottomTemplate", items, $bottom, { compactMode: true }).addClass(POPUP_BOTTOM_CLASS);
             this._toggleClasses();
         } else {
             this._$bottom && this._$bottom.detach();
         }
-    },
-
-    _getBottomToolbarWidth: function() {
-        var width = 0;
-
-        if(this.$bottomToolbarContentItems) {
-            this.$bottomToolbarContentItems.each(function(index, item) {
-                width += $(item).outerWidth();
-            });
-        }
-
-        return width;
-    },
-
-    _updateBottomToolbarCompactMode: function() {
-        if(this._$bottom) {
-            this._$bottom.removeClass(POPUP_TOOLBAR_COMPACT_CLASS);
-
-            if(this._$bottom.width() < this._getBottomToolbarWidth()) {
-                this._$bottom.addClass(POPUP_TOOLBAR_COMPACT_CLASS);
-            }
-        }
-    },
-
-    _show: function() {
-        var result = this.callBase.apply(this, arguments);
-
-        if(this.option("toolbarCompactMode")) {
-            this._updateBottomToolbarCompactMode();
-        }
-
-        return result;
     },
 
     _toggleClasses: function() {
@@ -776,7 +740,6 @@ var Popup = Overlay.inherit({
                 this._renderGeometry();
                 break;
             case "bottomTemplate":
-            case "toolbarCompactMode":
                 this._renderBottom();
                 this._renderGeometry();
                 break;
