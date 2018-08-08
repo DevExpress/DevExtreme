18c01deb9a6a338ef6d0d1849bc62e9af46db1b1
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2018-08-08 15:24:10 +0300

    Fix Scheduler tests in AUS Eastern Standard Time

--- js/ui/scheduler/utils.recurrence.js
+++ js/ui/scheduler/utils.recurrence.js
@@ -125,7 +125,11 @@ var dateGetterMap = {
 
         current.setHours(0, 0, 0);
         current.setDate(current.getDate() + diff);
-        daysFromYearStart = 1 + (current - new Date(current.getFullYear(), 0, 1)) / dayInMilliseconds;
+
+        var yearStart = new Date(current.getFullYear(), 0, 1),
+            timezoneDiff = (yearStart.getTimezoneOffset() - current.getTimezoneOffset()) * toMs("minute");
+
+        daysFromYearStart = 1 + (current - yearStart + timezoneDiff) / dayInMilliseconds;
 
         return Math.ceil(daysFromYearStart / 7);
     },
