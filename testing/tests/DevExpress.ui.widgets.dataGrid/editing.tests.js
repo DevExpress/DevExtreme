696e17468c8a6203e5d9f35723aeda3f669a1b98
Konstantin Volnyagin <vconst@users.noreply.github.com>
2018-08-08 10:36:48 +0300

    DataGrid: Fix error row if the showColumnHeaders option is false (T653307) (#4961)

--- testing/tests/DevExpress.ui.widgets.dataGrid/editing.tests.js
+++ testing/tests/DevExpress.ui.widgets.dataGrid/editing.tests.js
@@ -8503,6 +8503,7 @@ QUnit.testInActiveWindow("Show the revert button when an edit cell, server retur
     this.rowsView.render(testElement);
 
     this.applyOptions({
+        showColumnHeaders: true,
         editing: {
             mode: "cell"
         },
--- testing/tests/DevExpress.ui.widgets.dataGrid/errorHandling.tests.js
+++ testing/tests/DevExpress.ui.widgets.dataGrid/errorHandling.tests.js
@@ -81,6 +81,27 @@ QUnit.test("Render error row in column headers view", function(assert) {
     assert.strictEqual($errorRow.find("td").first().text(), "Test", "error message");
 });
 
+// T653307
+QUnit.test("Render error row in rows view if column headers are hidden", function(assert) {
+    // arrange
+    var that = this,
+        $testElement = $("#container"),
+        $firstRow;
+
+    this.options.showColumnHeaders = false;
+
+    that.rowsView.render($testElement);
+
+    // act
+    that.errorHandlingController.renderErrorRow("Test");
+
+    // assert
+    assert.equal($testElement.find('tbody > tr').length, 5, "row count");
+    $firstRow = $testElement.find('tbody > tr').first();
+    assert.ok($firstRow.hasClass("dx-error-row"), "first row is error row");
+    assert.strictEqual($firstRow.find("td").first().text(), "Test", "error message");
+});
+
 QUnit.test("Render error row in rows view", function(assert) {
     // arrange
     var that = this,
