b9528c92a47b027941cba0b84cac57229101e289
CORP\sermyazhko.anton <anton.sermyazhko@devexpress.com>
2018-08-23 15:58:11 +0300

    DataGrid - Do not lose the entered data in the editing form when expanding / collapsing another row (T660472)

--- js/ui/grid_core/ui.grid_core.master_detail.js
+++ js/ui/grid_core/ui.grid_core.master_detail.js
@@ -108,7 +108,8 @@ module.exports = {
                     },
                     _changeRowExpandCore: function(key) {
                         var that = this,
-                            expandIndex;
+                            expandIndex,
+                            editingController;
 
                         if(Array.isArray(key)) {
                             return that.callBase.apply(that, arguments);
@@ -120,6 +121,11 @@ module.exports = {
                                 that._expandedItems[expandIndex].visible = !visible;
                             } else {
                                 that._expandedItems.push({ key: key, visible: true });
+
+                                editingController = that.getController("editing");
+                                if(editingController) {
+                                    editingController.correctEditRowIndexAfterExpand(key);
+                                }
                             }
 
                             that.updateItems({
