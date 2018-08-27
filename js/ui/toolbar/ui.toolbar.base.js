597bbf2e543175443a1f6948193ce82621eb8789
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2018-08-27 14:52:54 +0300

    Make templates of SlideOutView, DropDownEditors and Toolbar async (#5194)

--- js/ui/toolbar/ui.toolbar.base.js
+++ js/ui/toolbar/ui.toolbar.base.js
@@ -6,6 +6,7 @@ var $ = require("../../core/renderer"),
     inArray = require("../../core/utils/array").inArray,
     extend = require("../../core/utils/extend").extend,
     each = require("../../core/utils/iterator").each,
+    AsyncTemplateMixin = require("../shared/async_template_mixin"),
     CollectionWidget = require("../collection/ui.collection_widget.edit"),
     BindableTemplate = require("../widget/bindable_template");
 
@@ -150,6 +151,10 @@ var ToolbarBase = CollectionWidget.inherit({
 
     _render: function() {
         this.callBase();
+        this._waitAsyncTemplates(this._renderAsync);
+    },
+
+    _renderAsync: function() {
         this._arrangeItems();
     },
 
@@ -365,6 +370,7 @@ var ToolbarBase = CollectionWidget.inherit({
     _renderEmptyMessage: commonUtils.noop,
 
     _clean: function() {
+        this._cleanAsyncTemplatesTimer();
         this._$toolbarItemsContainer.children().empty();
         this.$element().empty();
     },
@@ -422,7 +428,7 @@ var ToolbarBase = CollectionWidget.inherit({
     * @hidden
     * @inheritdoc
     */
-});
+}).include(AsyncTemplateMixin);
 
 registerComponent("dxToolbarBase", ToolbarBase);
 
