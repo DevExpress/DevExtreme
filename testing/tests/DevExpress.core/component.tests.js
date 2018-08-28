29a07e9dbeae75c11d546b63655ff21f28bf1b70
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2018-08-28 11:09:30 +0300

    Fix postponed operations calling in Component (#5203)

--- testing/tests/DevExpress.core/component.tests.js
+++ testing/tests/DevExpress.core/component.tests.js
@@ -1,6 +1,8 @@
 var $ = require("jquery"),
     noop = require("core/utils/common").noop,
     Component = require("core/component"),
+    Component = require("core/component"),
+    PostponedOperations = require("core/component").PostponedOperations,
     errors = require("core/errors"),
     devices = require("core/devices"),
     config = require("core/config");
@@ -183,6 +185,37 @@ QUnit.test("options api - 'onOptionChanged' changing", function(assert) {
     instance.option("option1", "value2");
 
     assert.equal(called, "new handler");
+});
+
+QUnit.test("component should initialize PostponedOperations", function(assert) {
+    var instance = new TestComponent({ a: 1 });
+
+    assert.ok(instance.postponedOperations instanceof PostponedOperations, "Componend initialize PostponedOperations");
+});
+
+QUnit.test("postponed operations should be called on endUpdate", function(assert) {
+    var instance = new TestComponent({ a: 1 }),
+        callPostponed = sinon.stub(instance.postponedOperations, "callPostponedOperations");
+
+    instance.endUpdate();
+    assert.ok(callPostponed.calledOnce, "Postponed operations are called");
+});
+
+QUnit.test("postponed operations should be called correctly without promises", function(assert) {
+    var instance = new TestComponent({ a: 1 });
+
+    var postponedOperation = function() {
+        return {
+            done: function() {
+                return true;
+            }
+        };
+    };
+
+    instance.postponedOperations.add("_firstPostponedOperation", postponedOperation, undefined);
+    delete instance.postponedOperations._postponedOperations._firstPostponedOperation.promises;
+    instance.endUpdate();
+    assert.ok(true, "Postponed operations are called correctly");
 });
 
 QUnit.test("component lifecycle, changing a couple of options", function(assert) {
