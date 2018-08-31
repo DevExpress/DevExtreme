0db2585f604d412317de22ea479e7f66b66016e8
Ilya Kharchenko <IlyaKhD@users.noreply.github.com>
2018-08-31 11:19:39 +0300

    Extract series types (#5250)
    - ChartSeries
    - PieChartSeries
    - PolarChartSeries

--- js/viz/docs/docrangeselector.js
+++ js/viz/docs/docrangeselector.js
@@ -661,7 +661,7 @@ var dxRangeSelector = {
         },
         /**
         * @name dxRangeSelectorOptions.chart.series
-        * @type dxChartOptions.series|Array<dxChartOptions.series>
+        * @type ChartSeries|Array<ChartSeries>
         * @default undefined
         * @notUsedInTheme
         */
@@ -682,7 +682,7 @@ var dxRangeSelector = {
             * @name dxRangeSelectorOptions.chart.seriesTemplate.customizeSeries
             * @type function(seriesName)
             * @type_function_param1 seriesName:any
-            * @type_function_return dxChartOptions.series
+            * @type_function_return ChartSeries
             */
             customizeSeries: function() { }
         },
