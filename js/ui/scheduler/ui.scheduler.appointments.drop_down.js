72b9e1c575a49b0c9c611ae5364e82de8dd13d3d
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2018-09-03 11:31:46 +0300

    Trying to fix

--- js/ui/scheduler/ui.scheduler.appointments.drop_down.js
+++ js/ui/scheduler/ui.scheduler.appointments.drop_down.js
@@ -150,11 +150,12 @@ var dropDownAppointments = Class.inherit({
                 buttonTemplate: this._createButtonTemplate(items.data.length, isCompact),
                 buttonWidth: config.buttonWidth,
                 closeOnClick: false,
-                onItemClick: function(args) {
-                    var mappedData = that.instance.fire("mapAppointmentFields", args);
+                onItemClick: (function(args) {
+                    var mappedData = this.instance.fire("mapAppointmentFields", args),
+                        result = extendFromObject(mappedData, args, false);
 
-                    that._appointmentClickAction(extendFromObject(mappedData, args, false));
-                },
+                    that._appointmentClickAction(this._clearExcessFields(result));
+                }).bind(this),
                 activeStateEnabled: false,
                 focusStateEnabled: this.instance.option("focusStateEnabled"),
                 itemTemplate: new FunctionTemplate(function(options) {
@@ -188,6 +189,14 @@ var dropDownAppointments = Class.inherit({
                 }
             });
         }
+    },
+
+    _clearExcessFields: function(data) {
+        delete data.itemData;
+        delete data.itemIndex;
+        delete data.itemElement;
+
+        return data;
     },
 
     _dragStartHandler: function($item, itemData, settings, $menu, e) {
