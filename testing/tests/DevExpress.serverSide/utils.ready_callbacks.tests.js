2ef253cbf06e0109b22bec73d8532e21fcbd9420
Andrey Ovchinnikov <ovchinnikov@users.noreply.github.com>
2018-09-04 14:26:23 +0300

    Clear ready callbacks after they were called (#5279)

---
+++ testing/tests/DevExpress.serverSide/utils.ready_callbacks.tests.js
@@ -0,0 +1,18 @@
+var readyCallbacks = require("core/utils/ready_callbacks");
+
+QUnit.module("readyCallbacks injection", {
+    afterEach: function() {
+        readyCallbacks.resetInjection();
+    }
+});
+
+QUnit.test("ready callbacks should be called only once", function(assert) {
+    let counter = 0;
+    readyCallbacks.add(() => counter++);
+
+    readyCallbacks.fire();
+    assert.equal(counter, 1);
+
+    readyCallbacks.fire();
+    assert.equal(counter, 1, "handler should be called only once");
+});
