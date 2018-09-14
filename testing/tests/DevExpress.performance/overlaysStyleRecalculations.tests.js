8c23acfc5089897371d7becca378585cbfebbcb0
Dmitry Levkovskiy <DokaRus@users.noreply.github.com>
2018-09-14 16:01:33 +0300

    Update Overlay style recalculation tests after #5398 (#5431)

--- testing/tests/DevExpress.performance/overlaysStyleRecalculations.tests.js
+++ testing/tests/DevExpress.performance/overlaysStyleRecalculations.tests.js
@@ -45,7 +45,7 @@ QUnit.performanceTest("dxOverlay should be rendered with minimum count of relayo
         });
     };
 
-    assert.measureStyleRecalculation(measureFunction, 5);
+    assert.measureStyleRecalculation(measureFunction, 7);
 });
 
 QUnit.performanceTest("showing dxOverlay should be with minimum count of relayouts", function(assert) {
@@ -59,7 +59,7 @@ QUnit.performanceTest("showing dxOverlay should be with minimum count of relayou
         overlay.show();
     };
 
-    assert.measureStyleRecalculation(measureFunction, 3);
+    assert.measureStyleRecalculation(measureFunction, 5);
 });
 
 QUnit.performanceTest("showing dxOverlay with shading should be with minimum count of relayouts", function(assert) {
@@ -73,7 +73,7 @@ QUnit.performanceTest("showing dxOverlay with shading should be with minimum cou
         overlay.show();
     };
 
-    assert.measureStyleRecalculation(measureFunction, 4);
+    assert.measureStyleRecalculation(measureFunction, 8);
 });
 
 QUnit.performanceTest("dxPopup should be rendered with minimum count of relayouts", function(assert) {
@@ -85,5 +85,5 @@ QUnit.performanceTest("dxPopup should be rendered with minimum count of relayout
         });
     };
 
-    assert.measureStyleRecalculation(measureFunction, 10);
+    assert.measureStyleRecalculation(measureFunction, 12);
 });
