483c95fe0442e9b4d43c7c078c917543ca6280dd
levkovskiy.dmitry <dmitry.levkovskiy@devexpress.com>
2018-09-25 17:26:12 +0300

    add one more test

--- testing/tests/DevExpress.ui.widgets.editors/richTextEditorParts/dropImageModule.tests.js
+++ testing/tests/DevExpress.ui.widgets.editors/richTextEditorParts/dropImageModule.tests.js
@@ -2,6 +2,7 @@ import $ from "jquery";
 
 import DropImage from "ui/rich_text_editor/modules/dropImage";
 import { createBlobFile } from "../../../helpers/fileHelper.js";
+import browser from "core/utils/browser";
 
 const IMAGE = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQYGWNgZGT8DwABDQEDEkMQNQAAAABJRU5ErkJggg==";
 
@@ -60,12 +61,24 @@ QUnit.module("DropImage module", moduleConfig, () => {
     });
 
     test("insert image on paste", (assert) => {
+        const clock = sinon.useFakeTimers();
         new DropImageMock(this.quillMock, this.options);
 
         const event = $.Event($.Event("paste", { clipboardData: { items: [this.file] } }));
         this.$element.trigger(event);
+        clock.tick();
 
         assert.equal(this.insertEmbedStub.callCount, 1, "File inserted");
         assert.deepEqual(this.insertEmbedStub.lastCall.args, [1, "image", IMAGE, "user"], "insert base64 image by user");
+        clock.restore();
+    });
+
+    test("dragover event", (assert) => {
+        new DropImage(this.quillMock, this.options);
+
+        const event = $.Event("dragover");
+        this.$element.trigger(event);
+
+        assert.equal(event.isDefaultPrevented(), !!browser.msie, "It should be prevented for MS browsers");
     });
 });
