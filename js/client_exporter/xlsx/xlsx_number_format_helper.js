2c210342f8a743bea08f0980b1daa958b9500d5f
Ignatov Dan <ignatovdan@gmail.com>
2018-10-01 13:38:31 +0300

    Move 'numFmt' node serialization code into single function

--- js/client_exporter/xlsx/xlsx_number_format_helper.js
+++ js/client_exporter/xlsx/xlsx_number_format_helper.js
@@ -2,6 +2,8 @@ import typeUtils from "../../core/utils/type";
 import xlsxTagHelper from './xlsx_tag_helper';
 
 const xlsxNumberFormatHelper = {
+    ID_PROPERTY_NAME: 'id',
+
     tryCreateTag: function(sourceObj) {
         let result = null;
         if(typeUtils.isDefined(sourceObj)) {
@@ -27,11 +29,12 @@ const xlsxNumberFormatHelper = {
         return !typeUtils.isDefined(tag) || !typeUtils.isDefined(tag.formatCode) || tag.formatCode === '';
     },
 
-    toXml: function(tag, addCustomAttributesCallback) {
+    toXml: function(tag) {
         // §18.8.30 numFmt (Number Format), 'ECMA-376 5th edition Part 1' (http://www.ecma-international.org/publications/standards/Ecma-376.htm)
-        const attributes = {};
-        addCustomAttributesCallback(attributes);
-        attributes.formatCode = tag.formatCode; // §21.2.2.71 formatCode (Format Code), §18.8.31 numFmts (Number Formats)
+        const attributes = {
+            'numFmtId': tag[xlsxNumberFormatHelper.ID_PROPERTY_NAME],
+            formatCode: tag.formatCode // §21.2.2.71 formatCode (Format Code), §18.8.31 numFmts (Number Formats)
+        };
         return xlsxTagHelper.toXml("numFmt", attributes);
     }
 };
