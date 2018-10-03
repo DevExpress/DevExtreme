61a38f4ac8edac0be89ad6d03e53f91c7031bb69
Andrey Lukyanov <lukyanovas@users.noreply.github.com>
2018-10-03 17:45:00 +0300

    Mark chart loadIndicator.show option as bindable (#5624)

--- js/viz/docs/docBaseWidget.js
+++ js/viz/docs/docBaseWidget.js
@@ -522,6 +522,7 @@ var BaseWidget = {
        * @name BaseWidgetOptions.loadingIndicator.show
        * @type boolean
        * @default false
+       * @fires BaseWidgetOptions.onOptionChanged
        */
         show: false,
         /**
