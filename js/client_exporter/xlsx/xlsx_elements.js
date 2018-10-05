3fe21ad8b8cfa360d1430fa204513d772430b803
Ignatov Dan <ignatovdan@gmail.com>
2018-10-05 12:38:51 +0300

    Fix typo

--- js/client_exporter/xlsx/xlsx_elements.js
+++ js/client_exporter/xlsx/xlsx_elements.js
@@ -47,7 +47,6 @@
 * @name XlsxCell.style.fill.patternFill.patternType
 * @type string
 * @acceptValues 'darkDown'|'darkGray'|'darkGrid'|'darkHorizontal'|'darkTrellis'|'darkUp'|'darkVertical'|'gray0625'|'gray125'|'lightDown'|'lightGray'|'lightGrid'|'lightHorizontal'|'lightTrellis'|'lightUp'|'lightVertical'|'mediumGray'|'none'|'solid'
-* @type Enums.XlsxPatternType
 */
 /**
 * @name XlsxCell.style.fill.patternFill.backgroundColor
