23714a2d6fd568939dbf59004cb0819701bd3e84
Ignatov Dan <ignatovdan@gmail.com>
2018-10-05 12:03:46 +0300

    Change the 'patternType' description from 'enum' to 'acceptValues'

--- js/client_exporter/xlsx/xlsx_elements.js
+++ js/client_exporter/xlsx/xlsx_elements.js
@@ -45,6 +45,8 @@
 */
 /**
 * @name XlsxCell.style.fill.patternFill.patternType
+* @type string
+* @acceptValues 'darkDown'|'darkGray'|'darkGrid'|'darkHorizontal'|'darkTrellis'|'darkUp'|'darkVertical'|'gray0625'|'gray125'|'lightDown'|'lightGray'|'lightGrid'|'lightHorizontal'|'lightTrellis'|'lightUp'|'lightVertical'|'mediumGray'|'none'|'solid'
 * @type Enums.XlsxPatternType
 */
 /**
