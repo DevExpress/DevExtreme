ae51ce780c393513001a5b9dd64ba17d36b9a41a
Ignatov Dan <ignatovdan@gmail.com>
2018-10-05 15:54:09 +0300

    Documentation: Add DataGrid.onXlsxCellPrepared entries (#5656)
    * Add DataGrid.onXlsxCellPrepared documentation entries
    
    * Change descriptions of simple possible values from 'enum' to 'acceptValues'
    
    * Change the 'patternType' description from 'enum' to 'acceptValues'
    
    * Postpone predefined number format descriptions.
    
    * Fix typo
    
    * Add cell.value and cell.dataType (the 'b' (Boolean), 'd' (Date) are not supported because they requires additional examples/tests)
    
    * Change 'acceptValues' to 'Enums'
    
    * Move export enums to docEnums.js

---
+++ js/client_exporter/xlsx/xlsx_elements.js
@@ -0,0 +1,144 @@
+/**
+* @name XlsxCell
+* @type object
+*/
+/**
+* @name XlsxCell.value
+* @type string|number|date|boolean
+*/
+/**
+* @name XlsxCell.dataType
+* @type Enums.XlsxCellDataType
+*/
+
+/**
+* @name XlsxCell.style
+* @type object
+*/
+
+/**
+* @name XlsxCell.style.numberFormat
+* @type object
+*/
+/**
+* @name XlsxCell.style.numberFormat.formatCode
+* @type string
+*/
+
+/**
+* @name XlsxCell.style.alignment
+* @type object
+*/
+/**
+* @name XlsxCell.style.alignment.horizontal
+* @type Enums.XlsxHorizontalAlignment
+*/
+/**
+* @name XlsxCell.style.alignment.vertical
+* @type Enums.XlsxVerticalAlignment
+*/
+/**
+* @name XlsxCell.style.alignment.wrapText
+* @type boolean
+*/
+
+/**
+* @name XlsxCell.style.fill
+* @type object
+*/
+/**
+* @name XlsxCell.style.fill.patternFill
+* @type object
+*/
+/**
+* @name XlsxCell.style.fill.patternFill.patternType
+* @type Enums.XlsxPatternType
+*/
+/**
+* @name XlsxCell.style.fill.patternFill.backgroundColor
+* @type XlsxColor
+*/
+/**
+* @name XlsxCell.style.fill.patternFill.foregroundColor
+* @type XlsxColor
+*/
+
+/**
+* @name XlsxCell.style.font
+* @type object
+*/
+/**
+* @name XlsxCell.style.font.size
+* @type number
+*/
+/**
+* @name XlsxCell.style.font.name
+* @type string
+*/
+/**
+* @name XlsxCell.style.font.bold
+* @type boolean
+*/
+/**
+* @name XlsxCell.style.font.italic
+* @type boolean
+*/
+/**
+* @name XlsxCell.style.font.underline
+* @type Enums.XlsxUnderlineType
+*/
+/**
+* @name XlsxCell.style.font.color
+* @type XlsxColor
+*/
+
+/**
+* @name XlsxColor
+* @type object
+*/
+/**
+* @name XlsxColor.rgb
+* @type string
+*/
+
+/**
+* @name XlsxGridCell
+* @type object
+*/
+/**
+* @name XlsxGridCell.rowType
+* @type string
+*/
+/**
+* @name XlsxGridCell.column
+* @type dxDataGridColumn
+*/
+/**
+* @name XlsxGridCell.value
+* @type any
+*/
+/**
+* @name XlsxGridCell.displayValue
+* @type string
+*/
+/**
+* @name XlsxGridCell.text
+* @type string
+*/
+
+/**
+* @name XlsxGridCell.row
+* @type object
+*/
+/**
+* @name XlsxGridCell.row.data
+* @type object
+*/
+/**
+* @name XlsxGridCell.row.key
+* @type any
+*/
+/**
+* @name XlsxGridCell.row.rowType
+* @type string
+*/
