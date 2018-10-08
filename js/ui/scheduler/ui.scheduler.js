ac525b68a3b336a693c0d8f5c25b433c9ffefcce
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2018-10-08 09:39:02 +0300

    Refactoring

--- js/ui/scheduler/ui.scheduler.js
+++ js/ui/scheduler/ui.scheduler.js
@@ -1505,11 +1505,11 @@ var Scheduler = Widget.inherit({
         var combinedDataAccessors = this._combineDataAccessors();
 
         this._appointmentModel = new SchedulerAppointmentModel(this._dataSource, {
-            startDateExpr: this.option("startDateExpr"),
-            endDateExpr: this.option("endDateExpr"),
-            allDayExpr: this.option("allDayExpr"),
-            recurrenceRuleExpr: this.option("recurrenceRuleExpr"),
-            recurrenceExceptionExpr: this.option("recurrenceExceptionExpr")
+            startDateExpr: this._dataAccessors.expr.startDateExpr,
+            endDateExpr: this._dataAccessors.expr.endDateExpr,
+            allDayExpr: this._dataAccessors.expr.allDayExpr,
+            recurrenceRuleExpr: this._dataAccessors.expr.recurrenceRuleExpr,
+            recurrenceExceptionExpr: this._dataAccessors.expr.recurrenceExceptionExpr
         }, combinedDataAccessors);
 
         this._appointmentModel = new SchedulerAppointmentModel(this._dataSource, combinedDataAccessors);
