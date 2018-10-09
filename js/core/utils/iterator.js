15e2b3fa3691e9df5009f7a00352cb21254d2054
eugen.zha <nightskylark@users.noreply.github.com>
2018-10-09 18:37:08 +0300

    Simplify iterator.map

--- js/core/utils/iterator.js
+++ js/core/utils/iterator.js
@@ -1,25 +1,15 @@
-var applyCallback = function(index, values, callback, result) {
-    var value = callback(values[index], index);
-
-    if(value != null) {
-        result.push(value);
-    }
-};
-
 var map = function(values, callback) {
-    var result = [];
-
-    if("length" in values) {
-        for(var index = 0; index < values.length; index++) {
-            applyCallback(index, values, callback, result);
-        }
-    } else {
-        for(var key in values) {
-            applyCallback(key, values, callback, result);
-        }
+    if(Array.isArray(values)) {
+        return values.map(callback);
     }
 
-    return result.slice(0);
+    let result = [];
+
+    for(let key in values) {
+        result.push(callback(values[key], key));
+    }
+
+    return result;
 };
 
 var each = function(values, callback) {
