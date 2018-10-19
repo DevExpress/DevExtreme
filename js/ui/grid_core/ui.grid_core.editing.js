ae956d14d1a22a884fd4c1b9976b4a1861a5b529
Konstantin Volnyagin <vconst@users.noreply.github.com>
2018-10-19 13:32:08 +0200

    DataGrid - Fix multiple remove event firing if a service returns error after a row is removed (T677915) (#5846)

--- js/ui/grid_core/ui.grid_core.editing.js
+++ js/ui/grid_core/ui.grid_core.editing.js
@@ -930,7 +930,9 @@ var EditingController = modules.ViewController.inherit((function() {
         },
 
         _removeEditDataItem: function(index) {
-            this._editData.splice(index, 1);
+            if(index >= 0) {
+                this._editData.splice(index, 1);
+            }
         },
 
         /**
@@ -1290,12 +1292,13 @@ var EditingController = modules.ViewController.inherit((function() {
                     $popupContent = that.getPopupContent();
                     dataController.dataErrorOccurred.fire(arg, $popupContent);
                     if(editMode !== EDIT_MODE_BATCH) {
+                        if(editData && editData.type === DATA_EDIT_DATA_REMOVE_TYPE) {
+                            that._removeEditDataItem(editIndex);
+                        }
                         break;
                     }
                 } else if(!cancel || !editData || editMode !== EDIT_MODE_BATCH && editData.type === DATA_EDIT_DATA_REMOVE_TYPE) {
-                    if(editIndex >= 0) {
-                        that._removeEditDataItem(editIndex);
-                    }
+                    that._removeEditDataItem(editIndex);
                     hasSavedData = !cancel;
                 }
             }
