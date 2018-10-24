da1f1942761d236d40b3fa895778d06f88143fa8
Ignatov Dan <ignatovdan@gmail.com>
2018-10-24 10:28:32 +0300

    Typo

--- js/client_exporter/xlsx/xlsx_color_helper.js
+++ js/client_exporter/xlsx/xlsx_color_helper.js
@@ -9,11 +9,7 @@ const xlsxColorHelper = {
         let result;
         if(source.length > 0 && source[0] === '#') {
             let colorCode = source.substr(1, source.length);
-            if(colorCode.length === 3) { // RGB
-                result = 'FF' + colorCode;
-            } else if(colorCode.length === 4) { // RGBA
-                result = colorCode[3] + colorCode.substr(0, 3);
-            } else if(colorCode.length === 6) { // RRGGBB
+            if(colorCode.length === 6) { // RRGGBB
                 result = 'FF' + colorCode;
             } else if(colorCode.length === 8) { // RRGGBBAA
                 result = colorCode[6] + colorCode[7] + colorCode.substr(0, 6);
