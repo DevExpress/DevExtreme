0740fec7633216f0a96a873252490374d4c2b429
Andrey Churkin <andrey.churkin@andreys-mbp.corp.devexpress.com>
2018-10-29 12:54:47 +0300

    Treview doesn't recalculate node selection states after 'searchValue' changing (close T669698)

--- testing/tests/DevExpress.ui.widgets/treeViewParts/optionChanged.js
+++ testing/tests/DevExpress.ui.widgets/treeViewParts/optionChanged.js
@@ -155,6 +155,36 @@ QUnit.test("searchValue from value to empty - update selection", function(assert
     assert.equal($items.length, 6, "6 items were rendered after filtration");
 });
 
+QUnit.test("Should recalculate selection after 'searchValue' changing", function(assert) {
+    var treeView = initTree({
+            searchValue: "b",
+            showCheckBoxesMode: "normal",
+            items: [{
+                id: "all",
+                text: "all",
+                items: [
+                    { id: "b", text: "b" },
+                    { id: "c", text: "c" }
+                ]
+            }]
+        }).dxTreeView("instance"),
+        $treeView = $(treeView.$element());
+
+    var checkSelection = function(isFirstItemSelected, isSecondItemSelected) {
+        var $items = $treeView.find(".dx-treeview-item"),
+            $checkboxes = $treeView.find(".dx-checkbox");
+
+        assert.strictEqual($items.length, 2, "2 items were rendered");
+        assert.strictEqual($checkboxes.eq(0).dxCheckBox("instance").option("value"), isFirstItemSelected);
+        assert.strictEqual($checkboxes.eq(1).dxCheckBox("instance").option("value"), isSecondItemSelected);
+    };
+
+    $treeView.find(".dx-checkbox").eq(1).trigger("dxclick");
+    checkSelection(true, true);
+    treeView.option("searchValue", "c");
+    checkSelection(false, false);
+});
+
 QUnit.test("searchValue - cut value - update selection", function(assert) {
     var data = $.extend(true, [], DATA[6]);
 
