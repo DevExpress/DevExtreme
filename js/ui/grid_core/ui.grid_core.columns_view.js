e4638383e2f021d975b0cd088bef254ecd11fc73
Alexander Komarov <sanrkom@gmail.com>
2018-10-31 16:49:21 +0300

    Revert changes after T681714 because they break bounces on real devices (#5987)

--- js/ui/grid_core/ui.grid_core.columns_view.js
+++ js/ui/grid_core/ui.grid_core.columns_view.js
@@ -124,7 +124,6 @@ exports.ColumnsView = modules.View.inherit(columnStateMixin).inherit({
         var options = extend({}, scrollingOptions, {
             direction: "both",
             bounceEnabled: false,
-            pushBackValue: 0,
             useKeyboard: false
         });
 
