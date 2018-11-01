99abc289ac3eaf5767030dc5243e2e2106c6b047
CORP\semenov <dmitry.semenov@devexpress.com>
2018-11-01 15:24:48 +0300

    fix import the Quill when the Quill is contained in the window

--- js/ui/html_editor/quill_importer.js
+++ js/ui/html_editor/quill_importer.js
@@ -1,12 +1,14 @@
 import Quill from "quill";
 import Errors from "../widget/ui.errors";
+import windowUtils from "../../core/utils/window";
 
 function getQuill() {
-    if(!Quill) {
+    const quill = windowUtils.hasWindow() && window.Quill || Quill;
+    if(!quill) {
         throw Errors.Error("E1041", "Quill");
     }
 
-    return Quill;
+    return quill;
 }
 
 export { getQuill };
