4f24620b4f3332cd42add30eee2939c086d19e89
CORP\semenov <dmitry.semenov@devexpress.com>
2018-11-01 15:43:43 +0300

    fix after review

--- js/ui/html_editor/quill_importer.js
+++ js/ui/html_editor/quill_importer.js
@@ -1,9 +1,10 @@
 import Quill from "quill";
 import Errors from "../widget/ui.errors";
-import windowUtils from "../../core/utils/window";
+import { getWindow } from "../../core/utils/window";
 
 function getQuill() {
-    const quill = windowUtils.hasWindow() && window.Quill || Quill;
+    const window = getWindow();
+    const quill = window && window.Quill || Quill;
     if(!quill) {
         throw Errors.Error("E1041", "Quill");
     }
