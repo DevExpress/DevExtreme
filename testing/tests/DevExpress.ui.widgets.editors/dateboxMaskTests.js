9a8698776ceb20aa8cd664058e3430d3403d2244
Eugeniy Maslyaev <mrjohn0011@users.noreply.github.com>
2018-11-07 15:54:16 +0300

    Don't revert a date part to the null in formatted dxDateBox (#6043)

--- testing/tests/DevExpress.ui.widgets.editors/dateboxMaskTests.js
+++ testing/tests/DevExpress.ui.widgets.editors/dateboxMaskTests.js
@@ -383,6 +383,16 @@ if(devices.real().deviceType === "desktop") {
             assert.deepEqual(this.keyboard.caret(), { start: 8, end: 10 }, "caret is good");
         });
 
+        QUnit.test("delete should not revert a part when the value is null", (assert) => {
+            this.instance.option("value", null);
+            this.keyboard.press("up");
+            assert.equal(this.instance.option("text"), "February 1 1970", "text has been changed");
+
+            this.keyboard.press("del");
+            assert.equal(this.instance.option("text"), "February 1 1970", "text has not been changed");
+            assert.deepEqual(this.keyboard.caret(), { start: 9, end: 10 }, "next group is selected");
+        });
+
         QUnit.test("backspace should revert group to initial value and go to the previous one", (assert) => {
             this.keyboard.press("right");
             this.keyboard.press("up");
