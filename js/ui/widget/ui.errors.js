838bbdd076489aeef6714725253ec9bf5af85459
dmitry.levkovskiy <dmitry.levkovskiy@devexpress.com>
2018-11-12 10:54:56 +0300

    Update description of the E1051 error

--- js/ui/widget/ui.errors.js
+++ js/ui/widget/ui.errors.js
@@ -211,7 +211,7 @@ module.exports = errorUtils(errors.ERROR_MESSAGES, {
     /**
      * @name ErrorsUIWidgets.E1051
      */
-    E1051: "The \"{0}\" converter is not found in the ConverterController",
+    E1051: "HtmlEditor's valueType is \"{0}\", but the {0} converter is not imported.",
 
     /**
     * @name ErrorsUIWidgets.W1001
