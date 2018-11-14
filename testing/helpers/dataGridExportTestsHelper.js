f0c37562ef4a3bd26f7dc7a13f0e83035db4378d
Ignatov Dan <ignatovdan@gmail.com>
2018-11-14 11:53:27 +0300

    DataGrid: Reorganize excel exporting tests (#6102)
    * Add 'options.pivotGridCell: { area, columnIndex, rowIndex }' info to 'customizeExcelCell(options)' similar to 'PivotGrid.onCellPrepared' (T502194)
    
    * Reorganize tests (intermediate)
    
    * Test 'dataFieldArea:column' configuration
    
    * Remove columnIndex/rowIndex initialization, it cannot correctly be calculated for multilevel headers at this step
    
    * The 'area' info is added
    
    * Renames
    
    * Synchronize DataGrid and PivotGrid export testing methods
    
    * Rename to getCustomizeExcelCellExpectedCells
    
    * Remove e.gridCell from customizeExcelCell
    
    * Remove e.component

--- testing/helpers/dataGridExportTestsHelper.js
+++ testing/helpers/dataGridExportTestsHelper.js
@@ -6,91 +6,47 @@ import $ from "jquery";
 import typeUtils from "core/utils/type";
 import { toComparable } from "core/utils/data";
 import { excel as excelCreator } from "exporter";
-import excel_creator from "exporter/excel_creator";
-import JSZipMock from "./jszipMock.js";
+import exportTestsHelper from "./exportTestsHelper.js";
 
-const INTERNAL_BASE_STYLE_XML1 = "<fonts count=\"2\"><font><sz val=\"11\" /><color theme=\"1\" /><name val=\"Calibri\" /><family val=\"2\" />" +
-    "<scheme val=\"minor\" /></font><font><b /><sz val=\"11\" /><color theme=\"1\" /><name val=\"Calibri\" />" +
-    "<family val=\"2\" /><scheme val=\"minor\" /></font></fonts>";
-const dataGridExportTestsHelper = {
+const dataGridExportTestsHelper = Object.create(exportTestsHelper);
 
-    BASE_STYLE_XML1: INTERNAL_BASE_STYLE_XML1,
-    BASE_STYLE_XML: INTERNAL_BASE_STYLE_XML1 + "<fills count=\"1\"><fill><patternFill patternType=\"none\" /></fill></fills>" + excelCreator.__internals.BASE_STYLE_XML2,
-    BASE_STYLE_XML2: excelCreator.__internals.BASE_STYLE_XML2,
-    WORKSHEET_HEADER_XML: excelCreator.__internals.WORKSHEET_HEADER_XML,
-    WORKSHEET_HEADER_XML1: excelCreator.__internals.WORKSHEET_HEADER_XML + '<sheetPr/><dimension ref="A1:C1"/>' +
-        '<sheetViews><sheetView tabSelected="1" workbookViewId="0"></sheetView></sheetViews>' +
-        '<sheetFormatPr defaultRowHeight="15" outlineLevelRow="0" x14ac:dyDescent="0.25"/>',
-    WORKSHEET_HEADER_WITH_PANE_XML: excelCreator.__internals.WORKSHEET_HEADER_XML + '<sheetPr/><dimension ref="A1:C1"/>' +
-        '<sheetViews><sheetView tabSelected="1" workbookViewId="0"><pane activePane="bottomLeft" state="frozen" ySplit="1" topLeftCell="A2" /></sheetView></sheetViews>' +
-        '<sheetFormatPr defaultRowHeight="15" outlineLevelRow="0" x14ac:dyDescent="0.25"/>',
+dataGridExportTestsHelper.runGeneralTest = function(assert, options, { styles = undefined, worksheet = undefined, sharedStrings = undefined, getCustomizeExcelCellExpectedCells = undefined } = {}) {
+    const that = this;
+    const done = assert.async();
+    const actualGridCells = [];
 
-    SHARED_STRINGS_HEADER_XML: excelCreator.__internals.XML_TAG + '<sst xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main"',
-    SHARED_STRINGS_EMPTY: excelCreator.__internals.XML_TAG + '<sst xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" count="0" uniqueCount="0"></sst>',
+    options.loadingTimeout = undefined;
+    options.export = options.export || {};
 
-    STYLESHEET_HEADER_XML: excelCreator.__internals.XML_TAG + '<styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main">',
-    STYLESHEET_STANDARDSTYLES: '<xf xfId="0" applyAlignment="1" fontId="1" applyNumberFormat="0" numFmtId="0"><alignment vertical="top" wrapText="1" horizontal="center" /></xf>' +
-        '<xf xfId="0" applyAlignment="1" fontId="1" applyNumberFormat="0" numFmtId="0"><alignment vertical="top" wrapText="1" horizontal="left" /></xf>' +
-        '<xf xfId="0" applyAlignment="1" fontId="1" applyNumberFormat="0" numFmtId="0"><alignment vertical="top" wrapText="1" horizontal="right" /></xf>',
-
-    STYLESHEET_FOOTER_XML: '<cellStyles count="1"><cellStyle name="Normal" xfId="0" builtinId="0" /></cellStyles></styleSheet>',
-
-    beforeEachTest: function() {
-        this.oldJSZip = excel_creator.ExcelCreator.JSZip;
-        excel_creator.ExcelCreator.JSZip = JSZipMock;
-    },
-
-    afterEachTest: function() {
-        excel_creator.ExcelCreator.JSZip = this.oldJSZip;
-    },
-
-    runGeneralTest: function(assert, gridOptions, { styles = undefined, worksheet = undefined, sharedStrings = undefined } = {}) {
-        const done = assert.async();
-        gridOptions.loadingTimeout = undefined;
-        gridOptions.onFileSaving = e => {
-            const zipMock = JSZipMock.lastCreatedInstance;
-
-            if(styles !== undefined) {
-                assert.strictEqual(zipMock.folder(excelCreator.__internals.XL_FOLDER_NAME).file(excelCreator.__internals.STYLE_FILE_NAME).content, styles, "styles");
+    if(getCustomizeExcelCellExpectedCells) {
+        const oldCustomizeExcelCell = options.export.customizeExcelCell;
+        options.export.customizeExcelCell = e => {
+            if(oldCustomizeExcelCell) {
+                oldCustomizeExcelCell(e);
             }
-            if(worksheet !== undefined) {
-                assert.strictEqual(zipMock.folder(excelCreator.__internals.XL_FOLDER_NAME).folder(excelCreator.__internals.WORKSHEETS_FOLDER).file(excelCreator.__internals.WORKSHEET_FILE_NAME).content, worksheet, "worksheet");
-            }
-            if(sharedStrings !== undefined) {
-                assert.strictEqual(zipMock.folder(excelCreator.__internals.XL_FOLDER_NAME).file(excelCreator.__internals.SHAREDSTRING_FILE_NAME).content, sharedStrings, "sharedStrings");
-            }
-
-            done();
-            e.cancel = true;
-        };
-
-        const dataGrid = $("#dataGrid").dxDataGrid(gridOptions).dxDataGrid("instance");
-
-        const getColumnWidthsHeadersOld = dataGrid.getView("columnHeadersView").getColumnWidths;
-        dataGrid.getView("columnHeadersView").getColumnWidths = function() {
-            const columnWidths = getColumnWidthsHeadersOld.apply(this);
-            return columnWidths.map(() => 100);
-        };
-
-        const getColumnWidthsRowsOld = dataGrid.getView("rowsView").getColumnWidths;
-        dataGrid.getView("rowsView").getColumnWidths = function() {
-            const columnWidths = getColumnWidthsRowsOld.apply(this);
-            return columnWidths.map(() => 100);
-        };
-        dataGrid.exportToExcel();
-    },
-
-    runCustomizeExcelCellTest: function(assert, gridOptions, getExpectedGridCellsCallback) {
-        const done = assert.async();
-        const actualGridCells = [];
-
-        gridOptions.export = gridOptions.export || {};
-        gridOptions.export.customizeExcelCell = e => {
             actualGridCells.push(e.gridCell);
         };
-        gridOptions.loadingTimeout = undefined;
-        gridOptions.onFileSaving = e => {
-            const expectedGridCells = getExpectedGridCellsCallback(e.component);
+    }
+
+    const oldOnFileSaving = options.onFileSaving;
+    options.onFileSaving = e => {
+        if(oldOnFileSaving) {
+            oldOnFileSaving(e);
+        }
+        const zipMock = that.getLastCreatedJSZipInstance();
+
+        if(styles !== undefined) {
+            assert.strictEqual(zipMock.folder(excelCreator.__internals.XL_FOLDER_NAME).file(excelCreator.__internals.STYLE_FILE_NAME).content, styles, "styles");
+        }
+        if(worksheet !== undefined) {
+            assert.strictEqual(zipMock.folder(excelCreator.__internals.XL_FOLDER_NAME).folder(excelCreator.__internals.WORKSHEETS_FOLDER).file(excelCreator.__internals.WORKSHEET_FILE_NAME).content, worksheet, "worksheet");
+        }
+        if(sharedStrings !== undefined) {
+            assert.strictEqual(zipMock.folder(excelCreator.__internals.XL_FOLDER_NAME).file(excelCreator.__internals.SHAREDSTRING_FILE_NAME).content, sharedStrings, "sharedStrings");
+        }
+
+        if(getCustomizeExcelCellExpectedCells) {
+            const expectedGridCells = getCustomizeExcelCellExpectedCells(e.component);
             assert.strictEqual(actualGridCells.length, expectedGridCells.length, 'actualGridCells.length');
             for(let i = 0; i < actualGridCells.length; i++) {
                 const actualGridCell = actualGridCells[i];
@@ -127,14 +83,30 @@ const dataGridExportTestsHelper = {
                     assert.strictEqual(actualGridCell.row.rowType, expectedGridCell.row.rowType, `row.rowType, ${i}`);
                 }
             }
+        }
 
-            done();
-            e.cancel = true;
-        };
+        done();
+        e.cancel = true;
+    };
 
-        const dataGrid = $("#dataGrid").dxDataGrid(gridOptions).dxDataGrid("instance");
-        dataGrid.exportToExcel();
-    },
+    const dataGrid = $("#dataGrid").dxDataGrid(options).dxDataGrid("instance");
+
+    const getColumnWidthsHeadersOld = dataGrid.getView("columnHeadersView").getColumnWidths;
+    dataGrid.getView("columnHeadersView").getColumnWidths = function() {
+        const columnWidths = getColumnWidthsHeadersOld.apply(this);
+        return columnWidths.map(() => 100);
+    };
+
+    const getColumnWidthsRowsOld = dataGrid.getView("rowsView").getColumnWidths;
+    dataGrid.getView("rowsView").getColumnWidths = function() {
+        const columnWidths = getColumnWidthsRowsOld.apply(this);
+        return columnWidths.map(() => 100);
+    };
+    dataGrid.exportToExcel();
+};
+
+dataGridExportTestsHelper.runCustomizeExcelCellTest = function(assert, gridOptions, getCustomizeExcelCellExpectedCells) {
+    dataGridExportTestsHelper.runGeneralTest(assert, gridOptions, { getCustomizeExcelCellExpectedCells });
 };
 
 export default dataGridExportTestsHelper;
