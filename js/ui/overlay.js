dda49a011f84c5cbf780382ec772691f3a8650ac
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2018-11-23 12:43:48 +0300

    Small refactoring in Overlay

--- js/ui/overlay.js
+++ js/ui/overlay.js
@@ -1013,6 +1013,12 @@ var Overlay = Widget.inherit({
         this._renderResize();
         this._renderScrollTerminator();
 
+        whenContentRendered.done(() => {
+            if(this.option("visible")) {
+                this._moveToContainer();
+            }
+        });
+
         return whenContentRendered.promise();
     },
 
@@ -1170,14 +1176,6 @@ var Overlay = Widget.inherit({
             left: isAllowedDrag ? position.left + shaderOffset.left + boundaryOffset.h : 0,
             right: isAllowedDrag ? -position.left - shaderOffset.left + deltaSize.width - boundaryOffset.h : 0
         };
-    },
-
-    _fireContentReadyAction: function() {
-        if(this.option("visible")) {
-            this._moveToContainer();
-        }
-
-        this.callBase.apply(this, arguments);
     },
 
     _moveFromContainer: function() {
