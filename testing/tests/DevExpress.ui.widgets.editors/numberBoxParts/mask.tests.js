3b655bf149197baf2c1cd396e96c03c4afb0eeeb
Eugeniy Maslyaev <mrjohn0011@users.noreply.github.com>
2018-11-23 13:30:20 +0300

    Allow dxNumberBox values without an integer part when there is no zeroes in the format (T687373)

--- testing/tests/DevExpress.ui.widgets.editors/numberBoxParts/mask.tests.js
+++ testing/tests/DevExpress.ui.widgets.editors/numberBoxParts/mask.tests.js
@@ -930,7 +930,7 @@ QUnit.test("value without integer part is not supported", function(assert) {
     });
 
     this.keyboard.type(".5");
-    assert.equal(this.input.val(), "$ 5", "point should be prevented");
+    assert.equal(this.input.val(), "$ .5", "point should be prevented");
 });
 
 QUnit.test("value with more than one point should not be incomplete", function(assert) {
