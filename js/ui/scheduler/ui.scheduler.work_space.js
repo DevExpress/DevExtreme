d8748d7ae144bc469dcfc4782c9e860c7e789e2b
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2018-11-23 14:46:52 +0300

    Support Scheduler workspace rendering for special cellDuration (T690834)

--- js/ui/scheduler/ui.scheduler.work_space.js
+++ js/ui/scheduler/ui.scheduler.work_space.js
@@ -1420,7 +1420,7 @@ var SchedulerWorkSpace = Widget.inherit({
 
     _getTimeCellDate: function(i) {
         var startViewDate = new Date(this.getStartViewDate()),
-            timeCellDuration = this.getCellDuration(),
+            timeCellDuration = Math.round(this.getCellDuration()),
             lastCellInDay = this._calculateDayDuration() / this.option("hoursInterval");
 
         startViewDate.setMilliseconds(startViewDate.getMilliseconds() + timeCellDuration * (i % lastCellInDay));
@@ -1498,7 +1498,7 @@ var SchedulerWorkSpace = Widget.inherit({
 
     calculateEndDate: function(startDate) {
         var result = new Date(startDate);
-        result.setMilliseconds(result.getMilliseconds() + this._getInterval());
+        result.setMilliseconds(result.getMilliseconds() + Math.round(this._getInterval()));
         return result;
     },
 
