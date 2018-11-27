bde8a0b61cd42ca63f79bc1f6c613e4633f70942
Ignatov Dan <ignatovdan@gmail.com>
2018-11-27 14:29:51 +0300

    Additional allowExporting/visible tests

--- testing/tests/DevExpress.ui.widgets.dataGrid/dataGrid.export.tests.js
+++ testing/tests/DevExpress.ui.widgets.dataGrid/dataGrid.export.tests.js
@@ -1466,16 +1466,10 @@ QUnit.test("Bands - show column headers & 'band.allowExporting: false'", functio
         '<sheetPr/><dimension ref="A1:C1"/>' +
         '<sheetViews><sheetView tabSelected="1" workbookViewId="0"><pane activePane="bottomLeft" state="frozen" ySplit="2" topLeftCell="A3" /></sheetView></sheetViews>' +
         '<sheetFormatPr defaultRowHeight="15" outlineLevelRow="0" x14ac:dyDescent="0.25"/>' +
-        '<cols>' +
-        '<col width="' + excelColumnWidthFrom_100 + '" min="1" max="1" />' +
-        '<col width="' + excelColumnWidthFrom_200 + '" min="2" max="2" />' +
-        '<col width="' + excelColumnWidthFrom_200 + '" min="3" max="3" />' +
-        '</cols>' +
+        '<cols><col width="' + excelColumnWidthFrom_100 + '" min="1" max="1" /></cols>' +
         '<sheetData>' +
-        '<row r="1" spans="1:3" outlineLevel="0" x14ac:dyDescent="0.25"><c r="A1" s="0" t="s"><v>0</v></c><c r="B1" s="0" t="s"><v>1</v></c><c r="C1" s="0" t="s" /></row>' +
-        '<row r="2" spans="1:3" outlineLevel="0" x14ac:dyDescent="0.25"><c r="A2" s="0" t="s" /><c r="B2" s="0" t="s"><v>2</v></c><c r="C2" s="0" t="s"><v>3</v></c></row>' +
+        '<row r="1" spans="1:3" outlineLevel="0" x14ac:dyDescent="0.25"><c r="A1" s="0" t="s" /></row>' +
         '</sheetData>' +
-        '<mergeCells count="2"><mergeCell ref="A1:A2" /><mergeCell ref="B1:C1" /></mergeCells>' +
         '</worksheet>';
 
     helper.runGeneralTest(
@@ -1626,10 +1620,6 @@ QUnit.test("Bands - show column headers & two bands & 'column.allowExporting: fa
         '</sheetData>' +
         '<mergeCells count="3"><mergeCell ref="A1:A2" /><mergeCell ref="B1:C1" /><mergeCell ref="D1:E1" /></mergeCells>' +
         '</worksheet>';
-    const sharedStrings = helper.SHARED_STRINGS_HEADER_XML + ' count="2" uniqueCount="2">' +
-        '<si><t>true</t></si>' +
-        '<si><t>false</t></si>' +
-        '</sst>';
 
     helper.runGeneralTest(
         assert,
@@ -1657,7 +1647,7 @@ QUnit.test("Bands - show column headers & two bands & 'column.allowExporting: fa
             dataSource: [],
             export: { enabled: true, ignoreExcelErrors: false },
         },
-        { worksheet, sharedStrings, fixedColumnWidth_100: false }
+        { worksheet, fixedColumnWidth_100: false }
     );
 });
 
