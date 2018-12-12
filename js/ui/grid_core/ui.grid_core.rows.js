240b359ea430020d7d999075997237e0e989ee3f
RomanTsukanov <roman.tsukanov@devexpress.com>
2018-12-12 10:11:00 +0300

    Fix the loadPanel.shading default value in Grids

--- js/ui/grid_core/ui.grid_core.rows.js
+++ js/ui/grid_core/ui.grid_core.rows.js
@@ -164,7 +164,7 @@ module.exports = {
                 /**
                 * @name GridBaseOptions.loadPanel.shading
                 * @type boolean
-                * @default true
+                * @default false
                 */
 
                 /**
