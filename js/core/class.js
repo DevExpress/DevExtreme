ac8e9b832a7373b2d684b1bc0deace8bd175d30e
Andrew Churkin (DevExpress) <Churkin.Andrey@corp.devexpress.com>
2018-12-21 17:03:56 +0300

    fix for ES6 classes

--- js/core/class.js
+++ js/core/class.js
@@ -44,6 +44,11 @@ var include = function() {
         name,
         i;
 
+    // NOTE: For ES6 classes. They don't have _includedCtors/_includedPostCtors
+    // properties and get them from the ancestor class.
+    classObj._includedCtors = classObj._includedCtors.slice(0);
+    classObj._includedPostCtors = classObj._includedPostCtors.slice(0);
+
     for(i = 0; i < arguments.length; i++) {
         argument = arguments[i];
         if(argument.ctor) {
