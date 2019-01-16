5bcedb8bd442b356a15b7fc9256e51a389258e2f
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2019-01-16 12:34:44 +0300

    Fix Scheduler workspace tests after #6579  (#6589)

--- testing/tests/DevExpress.ui.widgets.scheduler/workSpace.tests.js
+++ testing/tests/DevExpress.ui.widgets.scheduler/workSpace.tests.js
@@ -10,7 +10,8 @@ var $ = require("jquery"),
     resizeCallbacks = require("core/utils/resize_callbacks"),
     dateUtils = require("core/utils/date"),
     dateLocalization = require("localization/date"),
-    dragEvents = require("events/drag");
+    dragEvents = require("events/drag"),
+    memoryLeaksHelper = require("../../helpers/memoryLeaksHelper.js");
 
 require("common.css!");
 require("generic_light.css!");
@@ -1582,7 +1583,9 @@ QUnit.testStart(function() {
         assert.equal(cells.slice(1, 10).filter(".dx-state-focused").length, 9, " right cells are focused");
     });
 
-    QUnit.test("Pointeruip event subscriptions should be detached on dispose", function(assert) {
+    QUnit.test("Event subscriptions should be detached on dispose", function(assert) {
+        var originalEventSubscriptions = memoryLeaksHelper.getAllEventSubscriptions();
+
         var $element = $("#scheduler-work-space").dxSchedulerWorkSpaceMonth({
                 focusStateEnabled: true,
                 firstDayOfWeek: 1,
@@ -1598,7 +1601,7 @@ QUnit.testStart(function() {
 
         $element.dxSchedulerWorkSpaceMonth("instance").dispose();
 
-        assert.equal($._data(document, "events"), undefined, "Event subscriptions were detached");
+        assert.deepEqual(memoryLeaksHelper.getAllEventSubscriptions(), originalEventSubscriptions, "Subscribes after dispose are OK");
     });
 
     QUnit.test("Workspace should allow select/unselect cells with shift & right/left arrow", function(assert) {
