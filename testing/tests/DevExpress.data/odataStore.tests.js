6cf9e32f2c246a7857368328ea7570aace8da16c
dxvladislavvolkov <vladislav.volkov@devexpress.com>
2019-01-24 10:22:24 +0300

    Add test for oDataContext with global filterToLower

--- testing/tests/DevExpress.data/odataStore.tests.js
+++ testing/tests/DevExpress.data/odataStore.tests.js
@@ -2537,13 +2537,59 @@ QUnit.test("filterToLower equal false for ODataContext", function(assert) {
     var promises = [
         ctx.X.load({ filter: ["B", "contains", "O"] })
             .done(function(request) {
-                debugger;
                 assert.equal(request[0].data.$filter, "contains(tolower(B),'o')");
             }),
 
         ctx.Y.load({ filter: ["B", "contains", "O"] })
             .done(function(request) {
                 assert.equal(request[0].data.$filter, "contains(B,'O')");
+            })
+    ];
+
+    $.when.apply($, promises)
+        .fail(function() { assert.ok(false, MUST_NOT_REACH_MESSAGE); })
+        .always(done);
+});
+
+QUnit.test("oDataFilterToLower equal false for ODataContext", function(assert) {
+    assert.expect(3);
+
+    var done = assert.async();
+
+    config({ oDataFilterToLower: false });
+
+    ajaxMock.setup({
+        url: "odata.org/name",
+        callback: function(bag) {
+            this.responseText = { value: [bag] };
+        }
+    });
+
+    var ctx = new ODataContext({
+        version: 4,
+        url: "odata.org",
+        filterToLower: true,
+        entities: {
+            "X": { name: "name" },
+            "Y": { name: "name", filterToLower: false },
+            "Z": { name: "name", filterToLower: true }
+        }
+    });
+
+    var promises = [
+        ctx.X.load({ filter: ["B", "contains", "O"] })
+            .done(function(request) {
+                assert.equal(request[0].data.$filter, "contains(tolower(B),'o')");
+            }),
+
+        ctx.Y.load({ filter: ["B", "contains", "O"] })
+            .done(function(request) {
+                assert.equal(request[0].data.$filter, "contains(B,'O')");
+            }),
+
+        ctx.Z.load({ filter: ["B", "contains", "O"] })
+            .done(function(request) {
+                assert.equal(request[0].data.$filter, "contains(tolower(B),'o')");
             })
     ];
 
