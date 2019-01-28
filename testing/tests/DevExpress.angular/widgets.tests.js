84ffb9c3593116fe4873aa795f389aec6fce9bff
Alexander Ziborov <1420883+San4es@users.noreply.github.com>
2019-01-28 13:43:25 +0300

    Widgets - Get rid of useless escape (#6679)

--- testing/tests/DevExpress.angular/widgets.tests.js
+++ testing/tests/DevExpress.angular/widgets.tests.js
@@ -915,8 +915,8 @@ QUnit.module("box", {
 QUnit.test("innerBox with nested box item", function(assert) {
     var $markup = $("\
         <div dx-box=\"{}\">\
-            <div data-options=\"dxItem: {baseSize: 272, ratio: 0, box: {direction: \'col\'}}\">\
-                <div data-options=\"dxItem: {baseSize: \'auto\', ratio: 0}\"><h2>Box1</h2></div>\
+            <div data-options=\"dxItem: {baseSize: 272, ratio: 0, box: {direction: 'col'}}\">\
+                <div data-options=\"dxItem: {baseSize: 'auto', ratio: 0}\"><h2>Box1</h2></div>\
             </div>\
         </div>\
     ");
