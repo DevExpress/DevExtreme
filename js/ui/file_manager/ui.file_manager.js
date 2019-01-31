23622d52c37e1935d1f0d3dfd6122892b9fd7214
erokhin.maxim <maxim.erokhin@devexpress.com>
2019-01-31 18:25:42 +0300

    Add ability to apply CSS classes to the file manager

--- js/ui/file_manager/ui.file_manager.js
+++ js/ui/file_manager/ui.file_manager.js
@@ -2,6 +2,12 @@ import $ from "../../core/renderer";
 import Widget from "../widget/ui.widget";
 import registerComponent from "../../core/component_registrator";
 
+import DataGrid from "../data_grid/ui.data_grid";
+import TreeViewSearch from "../tree_view/ui.tree_view.search";
+
+const FIE_MANAGER_CLASS = "dx-filemanager";
+const FIE_MANAGER_DIRS_TREE_CLASS = FIE_MANAGER_CLASS + "-dirs-tree";
+
 var FileManager = Widget.inherit({
 
     _initTemplates: function() {
@@ -9,7 +15,39 @@ var FileManager = Widget.inherit({
 
     _initMarkup: function() {
         this.callBase();
-        $("<span>File Manager</span>").appendTo(this.$element());
+
+        this.$element().addClass(FIE_MANAGER_CLASS);
+
+        this._filesTreeView = this._createComponent($("<div>"), TreeViewSearch, {
+            items: this.generateFakeFoldersData()
+        });
+        this._filesTreeView.$element().addClass(FIE_MANAGER_DIRS_TREE_CLASS);
+        this.$element().append(this._filesTreeView.$element());
+
+        this._filesView = this._createComponent($("<div>"), DataGrid, {
+            columns: [ "FileName" ],
+            dataSource: this.generateFakeFilesData()
+        });
+        this.$element().append(this._filesView.$element());
+    },
+
+    generateFakeFoldersData: function() {
+        return [
+            {
+                text: "Folder 1",
+                items: [
+                    { text: "Folder 1.1" },
+                    { text: "Folder 1.2" } ] },
+            { text: "Folder 2" },
+            { text: "Folder 3" }
+        ];
+    },
+    generateFakeFilesData: function() {
+        return [
+            { FileName: "FileName 1.cs" },
+            { FileName: "FileName 2.cs" },
+            { FileName: "FileName 3.cs" }
+        ];
     }
 
 });
