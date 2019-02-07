d9cb1ce1cd1feb761eec8165448a50e1f674e6bc
Eugeniy Maslyaev <mrjohn0011@users.noreply.github.com>
2019-02-07 14:54:14 +0300

    Move z-index logic from the dxOverlay to the utility file (T689794)

--- js/ui/overlay/utils.js
+++ js/ui/overlay/utils.js
@@ -3,12 +3,10 @@ import { getWindow } from "../../core/utils/window";
 
 const WINDOW_HEIGHT_PERCENT = 0.9;
 
-const getElementMaxHeightByWindow = $element => {
+export const getElementMaxHeightByWindow = $element => {
     const window = getWindow(),
         offsetTop = $element.offset().top - $(window).scrollTop(),
         offsetBottom = $(window).innerHeight() - offsetTop - $element.outerHeight();
 
     return Math.max(offsetTop, offsetBottom) * WINDOW_HEIGHT_PERCENT;
 };
-
-export { getElementMaxHeightByWindow };
