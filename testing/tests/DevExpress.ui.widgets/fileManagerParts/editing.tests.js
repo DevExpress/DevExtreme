8c7122d57c8ff27e37fd3e0c9fb93479d5c59478
Vladimir Kovalev <vladimir.kovalev@devexpress.com>
2019-02-19 12:45:01 +0300

    Copy folder in folders area test

--- testing/tests/DevExpress.ui.widgets/fileManagerParts/editing.tests.js
+++ testing/tests/DevExpress.ui.widgets/fileManagerParts/editing.tests.js
@@ -182,4 +182,42 @@ QUnit.module("Editing operations", moduleConfig, () => {
         assert.ok($cells.eq(1).text(), "File 1-2.jpg", "file moved with target folder");
     });
 
+    test("copy folder in folders area", (assert) => {
+        var $folderNodes = this.$element.find(`.${internals.FOLDERS_TREE_VIEW_ITEM_CLASS}`);
+        var initialCount = $folderNodes.length;
+        var $folderNode = $folderNodes.eq(0);
+        assert.equal($folderNode.text(), "Folder 1", "has target folder");
+
+        $folderNode.trigger("dxclick");
+        this.clock.tick(400);
+
+        var $commandButton = this.$element.find(`.${internals.TOOLBAR_CLASS} .${internals.BUTTON_CLASS}:contains('Copy')`);
+        $commandButton.trigger("dxclick");
+        this.clock.tick(400);
+
+        $folderNodes = $(`.${internals.DIALOG_CLASS} .${internals.FOLDERS_TREE_VIEW_ITEM_CLASS}`);
+        $folderNodes.eq(2).trigger("dxclick");
+
+        var $okButton = $(`.${internals.POPUP_BOTTOM_CLASS} .${internals.BUTTON_CLASS}:contains('Select')`);
+        $okButton.trigger("dxclick");
+        this.clock.tick(400);
+
+        $folderNodes = this.$element.find(`.${internals.CONTAINER_CLASS} .${internals.FOLDERS_TREE_VIEW_ITEM_CLASS}`);
+        assert.equal($folderNodes.length, initialCount, "folders count not changed");
+        assert.ok($folderNodes.eq(0).text(), "Folder 1", "first folder is target folder");
+        assert.ok($folderNodes.eq(1).text(), "Folder 2", "second folder is not target folder");
+
+        var $folderToggles = this.$element.find(`.${internals.FOLDERS_TREE_VIEW_ITEM_TOGGLE_CLASS}`);
+        $folderToggles.eq(2).trigger("dxclick");
+
+        $folderNodes = this.$element.find(`.${internals.FOLDERS_TREE_VIEW_ITEM_CLASS}`);
+        assert.ok($folderNodes.eq(2).text(), "Folder 1", "target folder copied");
+        $folderNodes.eq(3).trigger("dxclick");
+        this.clock.tick(400);
+
+        var $cells = this.$element.find(`.${internals.GRID_DATA_ROW_CLASS} > td:first-child`);
+        assert.ok($cells.eq(0).text(), "File 1-1.txt", "file copied with target folder");
+        assert.ok($cells.eq(1).text(), "File 1-2.jpg", "file copied with target folder");
+    });
+
 });
