3e7d25042946b44d6a2313cd2045f4c1ada20f13
Andrey Ovchinnikov <ovchinnikov@users.noreply.github.com>
2019-02-22 17:38:10 +0300

    Change way to find knockout context for parent elements (#7026) (#7030)
    Change the way to find knockout context for parent elements

---
+++ js/integration/knockout/utils.js
@@ -0,0 +1,13 @@
+
+const ko = require("knockout");
+
+const getClosestNodeWithContext = (node) => {
+    var context = ko.contextFor(node);
+    if(!context && node.parentNode) {
+        return getClosestNodeWithContext(node.parentNode);
+    }
+
+    return node;
+};
+
+module.exports.getClosestNodeWithContext = getClosestNodeWithContext;
