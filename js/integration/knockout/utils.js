86def9ed59060eaaa2878bda0384e21eb10f0559
Alexander Ziborov <1420883+San4es@users.noreply.github.com>
2019-02-22 13:21:39 +0300

    [skip ci] getNodeWithContext -> getClosestNodeWithContext

--- js/integration/knockout/utils.js
+++ js/integration/knockout/utils.js
@@ -4,7 +4,7 @@ const ko = require("knockout");
 const getClosestNodeWithContext = (node) => {
     var context = ko.contextFor(node);
     if(!context && node.parentNode) {
-        return getNodeWithContext(node.parentNode);
+        return getClosestNodeWithContext(node.parentNode);
     }
 
     return node;
