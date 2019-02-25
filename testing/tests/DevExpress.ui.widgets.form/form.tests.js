f49b146ccdfef11be026a15dba74fb3e26d4009e
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2019-02-25 12:20:01 +0300

    Fix misaligned 'required field' marks in rtl mode (T714089) (#7046)

--- testing/tests/DevExpress.ui.widgets.form/form.tests.js
+++ testing/tests/DevExpress.ui.widgets.form/form.tests.js
@@ -1316,6 +1316,42 @@ QUnit.test("Align labels when layout is changed when small window size by defaul
     assert.equal($("." + internals.HIDDEN_LABEL_CLASS).length, 0, "hidden labels count");
 });
 
+QUnit.test("required mark aligned", (assert) => {
+    let $testContainer = $("#form").dxForm({
+        requiredMark: "!",
+        items: [{
+            dataField: "name",
+            isRequired: true
+        }]
+    });
+
+    let $labelsContent = $testContainer.find(`.${internals.FIELD_ITEM_LABEL_CONTENT_CLASS}`),
+        $requiredLabel = $labelsContent.find(`.${internals.FIELD_ITEM_LABEL_TEXT_CLASS}`),
+        $requiredMark = $labelsContent.find(`.${internals.FIELD_ITEM_REQUIRED_MARK_CLASS}`);
+
+    $labelsContent.width(200);
+
+    assert.equal($labelsContent.offset().left + $requiredLabel.width(), $requiredMark.offset().left, "position of requared mark is right");
+    assert.ok($requiredLabel.position().left < $requiredMark.position().left, "required mark should be after of the text");
+});
+
+QUnit.test("optional mark aligned", (assert) => {
+    let $testContainer = $("#form").dxForm({
+        optionalMark: "optMark",
+        showOptionalMark: true,
+        items: ["position"]
+    });
+
+    let $labelsContent = $testContainer.find(`.${internals.FIELD_ITEM_LABEL_CONTENT_CLASS}`),
+        $optionalLabel = $labelsContent.find(`.${internals.FIELD_ITEM_LABEL_TEXT_CLASS}`),
+        $optionalMark = $labelsContent.find(`.${internals.FIELD_ITEM_OPTIONAL_MARK_CLASS}`);
+
+    $labelsContent.width(200);
+
+    assert.equal($labelsContent.offset().left + $optionalLabel.width(), $optionalMark.offset().left, "position of optional mark is right");
+    assert.ok($optionalLabel.position().left < $optionalMark.position().left, "optional mark should be after of the text");
+});
+
 
 QUnit.module("Public API", {
     beforeEach: function() {
@@ -2445,6 +2481,45 @@ QUnit.test("Form redraw layout when colCount is 'auto' and an calculated colCoun
     assert.equal(refreshSpy.callCount, 1, "form has been redraw layout");
 });
 
+QUnit.module("Form when rtlEnabled is true");
+
+QUnit.test("required mark aligned when rtlEnabled option is set to true", (assert) => {
+    let $testContainer = $("#form").dxForm({
+        requiredMark: "!",
+        rtlEnabled: true,
+        items: [{
+            dataField: "name",
+            isRequired: true
+        }]
+    });
+
+    let $labelsContent = $testContainer.find(`.${internals.FIELD_ITEM_LABEL_CONTENT_CLASS}`),
+        $requiredLabel = $labelsContent.find(`.${internals.FIELD_ITEM_LABEL_TEXT_CLASS}`),
+        $requiredMark = $labelsContent.find(`.${internals.FIELD_ITEM_REQUIRED_MARK_CLASS}`);
+
+    $labelsContent.width(200);
+
+    assert.notEqual($labelsContent.offset().left, $requiredMark.offset().left, "position of requared mark is right");
+    assert.ok($requiredLabel.position().left > $requiredMark.position().left, "required mark should be before of the text");
+});
+
+QUnit.test("optional mark aligned when rtlEnabled option is set to true", (assert) => {
+    let $testContainer = $("#form").dxForm({
+        optionalMark: "optMark",
+        showOptionalMark: true,
+        rtlEnabled: true,
+        items: ["position"]
+    });
+
+    let $labelsContent = $testContainer.find(`.${internals.FIELD_ITEM_LABEL_CONTENT_CLASS}`),
+        $optionalLabel = $labelsContent.find(`.${internals.FIELD_ITEM_LABEL_TEXT_CLASS}`),
+        $optionalMark = $labelsContent.find(`.${internals.FIELD_ITEM_OPTIONAL_MARK_CLASS}`);
+
+    $labelsContent.width(200);
+
+    assert.notEqual($labelsContent.offset().left, $optionalMark.offset().left, "position of optional mark is right");
+    assert.ok($optionalLabel.position().left > $optionalMark.position().left, "optional mark should be before of the text");
+});
 
 QUnit.module("Events");
 
