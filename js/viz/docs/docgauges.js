bc60c7164718da53560deccffc185e05b17de66f
Roman Simionov <roman-simionov@users.noreply.github.com>
2019-02-28 14:56:12 +0300

    Add BarGauge legend doc (#7108)
    * Rename legend.format option to legend.itemTextFormat
    
    * Add BarGauge.legend options description
    
    * rename BarInfo->BarGaugeBarInfo

--- js/viz/docs/docgauges.js
+++ js/viz/docs/docgauges.js
@@ -731,6 +731,51 @@ var dxBarGauge = {
         }
     },
     /**
+    * @name dxBarGaugeOptions.legend
+    * @inherit BaseLegend
+    * @type object
+    */
+    legend: {
+        /**
+        * @name dxBarGaugeOptions.legend.itemTextFormat
+        * @extends CommonVizFormat
+        */
+        itemTextFormat: undefined,
+        /**
+        * @name BaseGaugeOptions.legend.visible
+        * @type boolean
+        * @inheritdoc
+        * @default false
+        */
+        visible: false,
+        /**
+        * @name BaseGaugeOptions.legend.customizeText
+        * @type function(arg)
+        * @type_function_param1 arg:object
+        * @type_function_param1_field1 item:BarGaugeBarInfo
+        * @type_function_param1_field2 text:string
+        * @type_function_return string
+        * @notUsedInTheme
+        */
+        customizeText: undefined,
+        /**
+        * @name BaseGaugeOptions.legend.customizeHint
+        * @type function(arg)
+        * @type_function_param1 arg:object
+        * @type_function_param1_field1 item:BarGaugeBarInfo
+        * @type_function_param1_field2 text:string
+        * @type_function_return string
+        */
+        customizeHint: undefined,
+        /**
+        * @name BaseGaugeOptions.legend.customizeItems
+        * @type function(items)
+        * @type_function_param1 items:Array<BarGaugeLegendItem>
+        * @type_function_return Array<BarGaugeLegendItem>
+        */
+        customizeItems: undefined
+    },
+    /**
     * @name dxBarGaugeOptions.resolveLabelOverlapping
     * @type Enums.BarGaugeResolveLabelOverlapping
     * @default 'hide'
@@ -797,4 +842,48 @@ var dxBarGauge = {
     * @action
     */
     onTooltipHidden: function() { }
+};
+
+/**
+* @name BarGaugeBarInfo
+* @type object
+*/
+var BarGaugeBarInfo = {
+    /**
+    * @name BarGaugeBarInfo.color
+    * @type string
+    */
+    color: "",
+    /**
+    * @name BarGaugeBarInfo.index
+    * @type number
+    */
+    index: 0,
+    /**
+    * @name BarGaugeBarInfo.value
+    * @type number
+    */
+    value: 0
+};
+
+/**
+* @name BarGaugeLegendItem
+* @type object
+*/
+var legendItem = {
+    /**
+    * @name BarGaugeLegendItem.text
+    * @type string
+    */
+    text: undefined,
+    /**
+    * @name BarGaugeLegendItem.item
+    * @type BarGaugeBarInfo
+    */
+    item: undefined,
+    /**
+    * @name BarGaugeLegendItem.visible
+    * @type boolean
+    */
+    visible: true
 };
