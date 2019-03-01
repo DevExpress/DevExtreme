3d15c381cd07e6ca2a38676199450aaffbe89c38
CORP\sermyazhko.anton <anton.sermyazhko@devexpress.com>
2019-03-01 12:26:38 +0300

    DataGrid should skip groupped rows by Tab key navigation in editing if cell or batch editing mode (T715092)

--- testing/tests/DevExpress.ui.widgets.dataGrid/keyboardNavigation.tests.js
+++ testing/tests/DevExpress.ui.widgets.dataGrid/keyboardNavigation.tests.js
@@ -3145,7 +3145,7 @@ QUnit.testInActiveWindow("Move focus to first data cell after tab key on group r
     });
 });
 
-QUnit.testInActiveWindow("DataGrid should not edit group cells after tab navigation from the editing cell (T714142)", function(assert) {
+QUnit.testInActiveWindow("DataGrid should skip group rows after tab navigation from the editing cell (T714142, T715092)", function(assert) {
     // arrange
     var $cell;
 
@@ -3191,8 +3191,8 @@ QUnit.testInActiveWindow("DataGrid should not edit group cells after tab navigat
     this.clock.tick();
 
     // assert
-    assert.notOk(this.editingController.isEditing(), "is editing");
-    assert.deepEqual(this.keyboardNavigationController._focusedCellPosition, { rowIndex: 1, columnIndex: 1 });
+    assert.ok(this.editingController.isEditing(), "is editing");
+    assert.deepEqual(this.keyboardNavigationController._focusedCellPosition, { rowIndex: 2, columnIndex: 1 });
 });
 
 QUnit.testInActiveWindow("Do not prevent default on 'shift+tab' if the current cell is the first", function(assert) {
