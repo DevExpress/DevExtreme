6dd766526dde95c4d3f27a2891b50a57c144c01e
groshenkovamarina <magroshenkova@gmail.com>
2019-03-15 12:30:20 +0300

    Add migration for old version

--- themebuilder/modules/metadata-loader.js
+++ themebuilder/modules/metadata-loader.js
@@ -1,11 +1,23 @@
+const semver = require("semver");
+
 class MetadataLoader {
     constructor() {
         this.fullMetadata = require("../data/metadata/dx-theme-builder-metadata");
+        this.migrationMetadata = require("../data/migration-metadata/migration-metadata");
     }
 
     load(theme, colorScheme) {
         return new Promise((resolve) => {
             let metadata = this.fullMetadata[theme + "_" + colorScheme.replace("-", "_") + "_metadata"];
+
+            if(semver.gte(this.version(), "18.2.8") && this.migrationMetadata[theme]) {
+                for(let i = 0; i < this.migrationMetadata[theme].length; i++) {
+                    metadata.push(
+                        { "Key": this.migrationMetadata[theme][i] }
+                    );
+                }
+            }
+
             resolve(metadata);
         });
     }
