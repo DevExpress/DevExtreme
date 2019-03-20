c73acc2e9886c4f8d60b903e0c5e7a35828b0240
Alexey Babich <alexey.babych@gmail.com>
2019-03-20 14:10:48 +0300

    Change version generation

--- build/gulp/context.js
+++ build/gulp/context.js
@@ -1,32 +1,58 @@
-var argv = require('yargs')
+const argv = require('yargs')
     .default('uglify', false)
     .argv;
 
-var productVersion = require('../../package.json').version,
-    scriptVersion = productVersion,
-    packageVersion = productVersion;
+const scriptVersion = require('../../package.json').version;
+const dxBuildLabel = process.env.DEVEXTREME_DXBUILD_LABEL;
+const dxBuildFlavor = process.env.DEVEXTREME_DXBUILD_FLAVOR;
+const dxBuildRevision = process.env.DEVEXTREME_DXBUILD_REVISION;
 
-var dxBuildLabel = process.env.DEVEXTREME_DXBUILD_LABEL,
-    dxBuildLabelFlavor = process.env.DEVEXTREME_DXBUILD_LABEL_FLAVOR,
-    dxBuildRevision = process.env.DEVEXTREME_DXBUILD_REVISION;
-
-if(dxBuildLabel) {
-    if(String(dxBuildLabel).replace(/_/g, '.') !== productVersion) {
-        throw 'DXBuild label does not match version in package.json';
-    }
-    if(dxBuildLabelFlavor) {
-        productVersion += '-' + dxBuildLabelFlavor;
-        packageVersion += '-' + dxBuildLabelFlavor;
-    }
-} else {
-    if(dxBuildRevision) {
-        productVersion += ' (build ' + dxBuildRevision + ')';
-        packageVersion = packageVersion.replace(/\d+$/, m => 1 + Number(m)) + '-pre-' + dxBuildRevision;
-    } else {
-        productVersion += '-dev';
-        packageVersion += '-dev';
-    }
+if(dxBuildLabel && String(dxBuildLabel).replace(/_/g, '.') !== scriptVersion) {
+    throw 'DXBuild label does not match version in package.json';
 }
+
+const getDefaultFlavor = (label, minor, revision) => {
+    return label
+        ? (minor <= 2 ? 'beta' : '')
+        : revision
+            ? (minor <= 1 ? 'alpha' : 'build')
+            : 'dev';
+};
+
+const getPackageVersion = (version, flavor, revision) => {
+    if(revision) {
+        version = version.replace(/\d+$/, m => 1 + Number(m));
+    }
+
+    return [version, flavor, revision]
+        .filter(v => v)
+        .join('-');
+};
+
+const getProductVersion = (version, revision) => {
+    return revision
+        ? version + ' (build ' + revision + ')'
+        : version;
+};
+
+const getRevision = (revision) => {
+    if(!revision) return '';
+
+    const date = new Date();
+    const padStart = (number) => ('0' + number).slice(-2);
+
+    const hours = padStart(date.getHours());
+    const minutes = padStart(date.getMinutes());
+    return revision + '-' + hours + minutes;
+};
+
+
+const minor = Number(scriptVersion.split('.')[2]);
+const revision = getRevision(dxBuildRevision);
+const flavor = dxBuildFlavor || getDefaultFlavor(dxBuildLabel, minor, revision);
+
+const packageVersion = getPackageVersion(scriptVersion, flavor, revision);
+const productVersion = getProductVersion(scriptVersion, revision);
 
 module.exports = {
     version: {
@@ -40,3 +66,4 @@ module.exports = {
     TRANSPILED_PATH: 'artifacts/transpiled',
     EULA_URL: 'https://js.devexpress.com/Licensing/'
 };
+
