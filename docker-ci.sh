32714d28f0a42635bacfd13acd9102aa37a594e5
Alexey Babich <alexey.babych@gmail.com>
2019-03-21 11:38:01 +0300

    try another case

--- docker-ci.sh
+++ docker-ci.sh
@@ -84,6 +84,7 @@ function run_test {
 }
 
 function run_themebuilder {
+    export DEVEXTREME_QUNIT_CI=false
     npm i
     dotnet build build/build-dotnet.sln
     npx gulp style-compiler-tb-assets
