e2f96062e7d2ceedf18e1001ffbe141460ec5691
Andrey Ovchinnikov <ovchinnikov@users.noreply.github.com>
2019-03-26 11:43:50 +0300

    Revert "Default template shouldn't be created from comments (T711311) (#7329) (#7352)" (#7430)
    This reverts commit 3c164541f6e16c99c64fffa4409d9b363173f9d6.

--- js/ui/widget/ui.widget.js
+++ js/ui/widget/ui.widget.js
@@ -36,7 +36,6 @@ var UI_FEEDBACK = "UIFeedback",
     FOCUS_NAMESPACE = "Focus",
     ANONYMOUS_TEMPLATE_NAME = "template",
     TEXT_NODE = 3,
-    COMMENT_NODE = 8,
     TEMPLATE_SELECTOR = "[data-options*='dxTemplate']",
     TEMPLATE_WRAPPER_CLASS = "dx-template-wrapper";
 
@@ -334,16 +333,15 @@ var Widget = DOMComponent.inherit({
     },
 
     _extractAnonymousTemplate: function() {
-        const templates = this.option("integrationOptions.templates"),
+        var templates = this.option("integrationOptions.templates"),
             anonymousTemplateName = this._getAnonymousTemplateName(),
             $anonymousTemplate = this.$element().contents().detach();
 
-        const $notJunkTemplateContent = $anonymousTemplate.filter(function(_, element) {
-                const isCommentNode = element.nodeType === COMMENT_NODE,
-                    isTextNode = element.nodeType === TEXT_NODE,
+        var $notJunkTemplateContent = $anonymousTemplate.filter(function(_, element) {
+                var isTextNode = element.nodeType === TEXT_NODE,
                     isEmptyText = $(element).text().trim().length < 1;
 
-                return !(isTextNode && isEmptyText) && !isCommentNode;
+                return !(isTextNode && isEmptyText);
             }),
             onlyJunkTemplateContent = $notJunkTemplateContent.length < 1;
 
