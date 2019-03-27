dc9085e5cc69441fd74f8cb21e92893d67dd4fca
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2019-03-27 14:52:26 +0300

    clean appointment_form file

--- js/ui/scheduler/ui.scheduler.appointment_form.js
+++ js/ui/scheduler/ui.scheduler.appointment_form.js
@@ -12,7 +12,6 @@ require("../text_area");
 require("../tag_box");
 
 var RECURRENCE_EDITOR_ITEM_CLASS = "dx-scheduler-recurrence-rule-item";
-// RECURRENCE_SWITCH_EDITOR_ITEM_CLASS = "dx-scheduler-recurrence-switch-item";
 
 var SchedulerAppointmentForm = {
 
@@ -199,32 +198,6 @@ var SchedulerAppointmentForm = {
                 itemType: "empty",
                 colSpan: 2
             },
-
-            // {
-            //     name: "repeatOnOff",
-            //     editorType: "dxSwitch",
-            //     colSpan: 2,
-            //     label: {
-            //         text: messageLocalization.format("dxScheduler-editorLabelRecurrence")
-            //     },
-            //     editorOptions: {
-            //         observer: schedulerInst,
-            //         onInitialized: function(args) {
-            //             var value = that._getRecurrenceRule(schedulerInst, that._appointmentForm);
-
-            //             schedulerInst.fire("recurrenceEditorVisibilityChanged", value);
-            //             args.component.option("value", value);
-            //         },
-            //         onValueChanged: function(args) {
-            //             var value = args.value,
-            //                 recEditor = that._appointmentForm.getEditor(dataExprs.recurrenceRuleExpr);
-
-            //             schedulerInst.fire("recurrenceEditorVisibilityChanged", value);
-            //             recEditor.option("visible", value);
-            //         }
-            //     },
-            //     cssClass: RECURRENCE_SWITCH_EDITOR_ITEM_CLASS
-            // },
             {
                 dataField: dataExprs.recurrenceRuleExpr,
                 editorType: "dxRecurrenceEditor",
