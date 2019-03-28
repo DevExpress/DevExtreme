fd18b1c5e144a5f4ede3213498d5861ee306eb87
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2019-03-28 10:27:20 +0300

    Tabs - fix behavior in scheduler after fix T706034 (#7461)

--- testing/tests/DevExpress.ui.widgets/tabs.width.tests.js
+++ testing/tests/DevExpress.ui.widgets/tabs.width.tests.js
@@ -7,9 +7,6 @@ import "common.css!";
 const TABS_ITEM_CLASS = "dx-tab",
     TABS_NAV_BUTTON_CLASS = "dx-tabs-nav-button";
 
-let toSelector = (cssClass) => `.${cssClass}`;
-
-
 QUnit.module("Width");
 
 class TabsWidthTestHelper {
@@ -45,19 +42,21 @@ class TabsWidthTestHelper {
         this.$container.remove();
     }
 
+    getTabItems() {
+        return this.$element.find(`.${TABS_ITEM_CLASS}`);
+    }
+
     createFixedTabs() {
         this._initializeInstanceTabs(400);
         this.checkFixedTabs();
     }
 
     checkFixedTabs() {
-        let tabItems = this.$element.find(toSelector(TABS_ITEM_CLASS));
-
         this.assert.equal(this.instance.option("width"), this.changeContainerWidth ? undefined : 400);
         this.assert.equal(this.$element.outerWidth(), 400);
-        this.assert.ok(tabItems.eq(0).width() > 190, tabItems.eq(0).width() + " > 190");
-        this.assert.ok(tabItems.eq(1).width() > 190, tabItems.eq(1).width() + " > 190");
-        this.assert.equal(this.$element.find("." + TABS_NAV_BUTTON_CLASS).length, 0, "nav buttons aren't rendered");
+        this.assert.ok(this.getTabItems().eq(0).width() > 190, this.getTabItems().eq(0).width() + " > 190");
+        this.assert.ok(this.getTabItems().eq(1).width() > 190, this.getTabItems().eq(1).width() + " > 190");
+        this.assert.equal(this.$element.find(`.${TABS_NAV_BUTTON_CLASS}`).length, 0, "nav buttons aren't rendered");
     }
 
     createStretchedTabs() {
@@ -66,13 +65,11 @@ class TabsWidthTestHelper {
     }
 
     checkStretchedTabs() {
-        let tabItems = this.$element.find(toSelector(TABS_ITEM_CLASS));
-
         this.assert.equal(this.instance.option("width"), this.changeContainerWidth ? undefined : 200);
         this.assert.equal(this.$element.outerWidth(), 200);
-        this.assert.ok(tabItems.eq(0).width() < 70, tabItems.eq(0).width() + " < 70");
-        this.assert.ok(tabItems.eq(1).width() > 130, tabItems.eq(1).width() + " > 130");
-        this.assert.equal(this.$element.find("." + TABS_NAV_BUTTON_CLASS).length, 0, "nav buttons aren't rendered");
+        this.assert.ok(this.getTabItems().eq(0).width() < 70, this.getTabItems().eq(0).width() + " < 70");
+        this.assert.ok(this.getTabItems().eq(1).width() > 130, this.getTabItems().eq(1).width() + " > 130");
+        this.assert.equal(this.$element.find(`.${TABS_NAV_BUTTON_CLASS}`).length, 0, "nav buttons aren't rendered");
     }
 
     createNavigationButtonsTabs() {
@@ -81,19 +78,17 @@ class TabsWidthTestHelper {
     }
 
     checkNavigationButtonsTabs() {
-        let tabItems = this.$element.find(toSelector(TABS_ITEM_CLASS));
-
         this.assert.equal(this.instance.option("width"), this.changeContainerWidth ? undefined : 100);
         this.assert.equal(this.$element.outerWidth(), 100);
 
         if(this.scrollingEnabled) {
-            this.assert.ok(tabItems.eq(0).width() < 70, tabItems.eq(0).width() + " < 70");
-            this.assert.ok(tabItems.eq(1).width() > 100, tabItems.eq(1).width() + " > 100");
-            this.assert.equal(this.$element.find("." + TABS_NAV_BUTTON_CLASS).length, 2, "nav buttons aren't rendered");
+            this.assert.ok(this.getTabItems().eq(0).width() < 70, this.getTabItems().eq(0).width() + " < 70");
+            this.assert.ok(this.getTabItems().eq(1).width() > 100, this.getTabItems().eq(1).width() + " > 100");
+            this.assert.equal(this.$element.find(`.${TABS_NAV_BUTTON_CLASS}`).length, 2, "nav buttons aren't rendered");
         } else {
-            this.assert.ok(tabItems.eq(0).width() < 55, tabItems.eq(0).width() + " < 55");
-            this.assert.ok(tabItems.eq(1).width() < 55, tabItems.eq(1).width() + " < 55");
-            this.assert.equal(this.$element.find("." + TABS_NAV_BUTTON_CLASS).length, 0, "nav buttons aren't rendered");
+            this.assert.ok(this.getTabItems().eq(0).width() < 55, this.getTabItems().eq(0).width() + " < 55");
+            this.assert.ok(this.getTabItems().eq(1).width() < 55, this.getTabItems().eq(1).width() + " < 55");
+            this.assert.equal(this.$element.find(`.${TABS_NAV_BUTTON_CLASS}`).length, 0, "nav buttons aren't rendered");
         }
     }
 
@@ -109,11 +104,11 @@ class TabsWidthTestHelper {
 
 [true, false, undefined].forEach((scrollingEnabled) => {
     ["container", "option"].forEach((setWidthApproach) => {
-        const config = ", scrollingEnabled=" + scrollingEnabled + ", change " + setWidthApproach + ".width";
+        const config = `, scrollingEnabled=${scrollingEnabled}, change ${setWidthApproach}.width`;
 
         QUnit.test("Show fixed tabs, resize to show stretched tabs" + config, function(assert) {
             let helper = new TabsWidthTestHelper(assert, scrollingEnabled, setWidthApproach);
-            helper.createFixedTabs();
+            helper.createFixedTabs(400);
             helper.setWidth(200);
             helper.checkStretchedTabs();
             helper.remove();
@@ -121,7 +116,7 @@ class TabsWidthTestHelper {
 
         QUnit.test("Show fixed tabs, resize to show navigation buttons" + config, function(assert) {
             let helper = new TabsWidthTestHelper(assert, scrollingEnabled, setWidthApproach);
-            helper.createFixedTabs();
+            helper.createFixedTabs(400);
             helper.setWidth(100);
             helper.checkNavigationButtonsTabs();
             helper.remove();
@@ -159,4 +154,60 @@ class TabsWidthTestHelper {
             helper.remove();
         });
     });
+});
+
+QUnit.test("Does not render navbuttons in certain scenario", (assert) => {
+    var styles = '<style>.dx-tabs{ max-width: 413px; } .dx-tab{ width: 100px; }</style>';
+
+    $("#qunit-fixture").html(styles);
+
+    let $container = $("<div>");
+    let $element = $("<div>");
+
+    $element.appendTo($container).dxTabs({
+        items: [
+            { text: "Timeline Day" },
+            { text: "Timeline Week" },
+            { text: "Timeline Work Week" },
+            { text: "Timeline Month" }
+        ],
+        scrollingEnabled: true,
+        showNavButtons: true,
+        width: "auto"
+    });
+    $container.appendTo("#qunit-fixture");
+
+    domUtils.triggerShownEvent($container);
+
+    assert.equal($element.find(`.${TABS_NAV_BUTTON_CLASS}`).length, 0);
+
+    $("#qunit-fixture").html("");
+});
+
+QUnit.test("Render navbuttons in certain scenario", (assert) => {
+    var styles = '<style>.dx-tabs{ max-width: 380px; } .dx-tab{ width: 100px; }</style>';
+
+    $("#qunit-fixture").html(styles);
+
+    let $container = $("<div>");
+    let $element = $("<div>");
+
+    $element.appendTo($container).dxTabs({
+        items: [
+            { text: "Timeline Day" },
+            { text: "Timeline Week" },
+            { text: "Timeline Work Week" },
+            { text: "Timeline Month" }
+        ],
+        scrollingEnabled: true,
+        showNavButtons: true,
+        width: "auto"
+    });
+    $container.appendTo("#qunit-fixture");
+
+    domUtils.triggerShownEvent($container);
+
+    assert.equal($element.find(`.${TABS_NAV_BUTTON_CLASS}`).length, 2);
+
+    $("#qunit-fixture").html("");
 });
