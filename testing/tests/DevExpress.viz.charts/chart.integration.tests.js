d13bfab3de0c1951d340c5e90c3ca2b66bb95f44
Alexander Bezborodov <alexander.bezborodov@devexpress.com>
2019-03-29 16:28:45 +0300

    dxViz: Fix layout tests after redesign axes

--- testing/tests/DevExpress.viz.charts/chart.integration.tests.js
+++ testing/tests/DevExpress.viz.charts/chart.integration.tests.js
@@ -2768,7 +2768,7 @@ QUnit.test("Alignment left. Rotate. Rotation angle is not a multiple of 90", fun
     var axis = chart.getArgumentAxis();
     var translateX = axis._majorTicks.map(t => t.label._settings.translateX);
 
-    translateX.forEach((tX, i) => assert.roughEqual(tX, 5 + 2.5 * i, 1.5));
+    translateX.forEach((tX, i) => assert.roughEqual(tX, 5 + 3 * i, 2.1));
 });
 
 QUnit.test("Alignment right. Rotate. Rotation angle is not a multiple of 90", function(assert) {
@@ -2784,7 +2784,7 @@ QUnit.test("Alignment right. Rotate. Rotation angle is not a multiple of 90", fu
     var axis = chart.getArgumentAxis();
     var translateX = axis._majorTicks.map(t => t.label._settings.translateX);
 
-    translateX.forEach((tX, i) => assert.roughEqual(tX, -12 - 2 * i, 2.1));
+    translateX.forEach((tX, i) => assert.roughEqual(tX, -12 - 3 * i, 2.1));
 });
 
 QUnit.test("Alignment left. Rotate. Rotation angle is a multiple of 90", function(assert) {
@@ -2816,5 +2816,5 @@ QUnit.test("Alignment right. Rotate. Rotation angle is a multiple of 90", functi
     var axis = chart.getArgumentAxis();
     var translateX = axis._majorTicks.map(t => t.label._settings.translateX);
 
-    translateX.forEach((tX, i) => assert.roughEqual(tX, 45, 1.5));
+    translateX.forEach((tX, i) => assert.roughEqual(tX, 43, 1.5));
 });
