23a3342af5df2db73bc7f5108542bf1cdaa3f76c
Alexey Babich <alexey.babych@gmail.com>
2019-03-29 12:42:40 +0300

    Compact customization for material

--- js/ui/scheduler/rendering_strategies/ui.scheduler.appointments.strategy.base.js
+++ js/ui/scheduler/rendering_strategies/ui.scheduler.appointments.strategy.base.js
@@ -724,7 +724,7 @@ var BaseRenderingStrategy = Class.inherit({
     },
 
     _isCompactTheme: function() {
-        return (themes.current() || "").split(".")[2] === "compact";
+        return (themes.current() || "").split(".").pop() === "compact";
     },
 
     _getAppointmentDefaultOffset: function() {
