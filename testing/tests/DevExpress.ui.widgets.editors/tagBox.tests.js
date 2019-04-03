aec8112b7d68634db91dcb0328fab25be865e9ae
Andrey Churkin <alam17@mail.ru>
2019-04-03 11:32:35 +0300

    TagBox should render function item template that returns default template's name (T726777) (#7514) (#7540)
    * TagBox should render function item template that returns default template's name (T726777) (#7500)
    
    * TagBox should render function item template that returns default template's name (T726777)
    
    * fix remark
    
    * refactoting
    
    * fix test

--- testing/tests/DevExpress.ui.widgets.editors/tagBox.tests.js
+++ testing/tests/DevExpress.ui.widgets.editors/tagBox.tests.js
@@ -5046,6 +5046,24 @@ QUnit.module("regression", {
         clock.restore();
     });
 
+    QUnit.test("should render function item template that returns default template's name (T726777)", (assert) => {
+        const tagBox = $("#tagBox").dxTagBox({
+            items: [{ text: "item1" }, { text: "item2" }],
+            itemTemplate: () => "item",
+            opened: true
+        }).dxTagBox("instance");
+        const checkItemsRender = () => {
+            const texts = ["item1", "item2"];
+            const listItems = tagBox._$list.find(".dx-list-item");
+
+            texts.forEach((text, index) => assert.strictEqual(listItems.eq(index).text(), text));
+        };
+
+        checkItemsRender();
+        tagBox.option("itemTemplate", () => "item");
+        checkItemsRender();
+    });
+
     QUnit.test("tagBox should not fail when asynchronous data source is used (T381326)", (assert) => {
         const data = [1, 2, 3, 4, 5];
         const timeToWait = 500;
