931dd2f4aaff67bdf25a9454d8dde31686724ded
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2019-04-04 13:45:53 +0300

    ButtonGroup - onItemClick event implement  (T727154) (#7559)

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -5568,6 +5568,8 @@ declare module DevExpress.ui {
         keyExpr?: string | Function;
         /** @name dxButtonGroup.Options.onSelectionChanged */
         onSelectionChanged?: ((e: { component?: dxButtonGroup, element?: DevExpress.core.dxElement, model?: any, addedItems?: Array<any>, removedItems?: Array<any> }) => any);
+        /** @name dxButtonGroup.Options.onItemClick */
+        onItemClick?: ((e: { component?: dxButtonGroup, element?: DevExpress.core.dxElement, model?: any, itemData?: any, itemElement?: DevExpress.core.dxElement, itemIndex?: number, event?: event }) => any);
         /** @name dxButtonGroup.Options.selectedItemKeys */
         selectedItemKeys?: Array<any>;
         /** @name dxButtonGroup.Options.selectedItems */
