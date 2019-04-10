31ee1aa7df662f966843fcfdddb4cc8de0e5a1ed
Roman Resh <romanresh@live.com>
2019-04-10 16:46:44 +0300

    diagram: scrollview, options, fix toolbar (#7617)
    * diagram: scrollview, options, fix toolbar
    
    * tests, prevent command executing on options update

--- js/ui/diagram/ui.diagram.commands.js
+++ js/ui/diagram/ui.diagram.commands.js
@@ -1,7 +1,7 @@
 import { getDiagram } from "./diagram_importer";
 
 const DiagramCommands = {
-    load: function() {
+    getToolbar: function() {
         const { DiagramCommand } = getDiagram();
         return [
             {
@@ -91,6 +91,99 @@ const DiagramCommands = {
             {
                 name: DiagramCommand.AutoLayoutTree,
                 text: "Layered Auto Layout"
+            }
+        ];
+    },
+    getOptions: function() {
+        const { DiagramCommand } = getDiagram();
+        return [
+            {
+                name: DiagramCommand.PageSize,
+                text: "Page Size",
+                widget: "dxSelectBox",
+                getValue: (v) => {
+                    const vParts = v.split("|");
+                    return {
+                        width: parseInt(vParts[0]),
+                        height: parseInt(vParts[1])
+                    };
+                },
+                setValue: (v) => `${v.width}|${v.height}`,
+                items: [
+                    {
+                        value: "12240|15840",
+                        title: "US-Letter (8,5\" x 11\")"
+                    },
+                    {
+                        value: "15817|24491",
+                        title: "US-Tabloid (27.9 cm x 43.2 cm)"
+                    },
+                    {
+                        value: "47679|67408",
+                        title: "A0 (84.1 cm x 118.9 cm)"
+                    },
+                    {
+                        value: "33676|47679",
+                        title: "A1 (59.4 cm x 84.1 cm)"
+                    },
+                    {
+                        value: "23811|33676",
+                        title: "A2 (42.0 cm x 59.4 cm)"
+                    },
+                    {
+                        value: "16838|23811",
+                        title: "A3 (29.7 cm x 42.0 cm)"
+                    },
+                    {
+                        value: "11906|16838",
+                        title: "A4 (21.0 cm x 29.7 cm)"
+                    },
+                    {
+                        value: "8391|11906",
+                        title: "A5 (14.8 cm x 21.0 cm)"
+                    },
+                    {
+                        value: "5953|8391",
+                        title: "A6 (10.5 cm x 14.8 cm)"
+                    },
+                    {
+                        value: "4195|5953",
+                        title: "A7 (7.4 cm x 10.5 cm)"
+                    }
+                ]
+            },
+            {
+                name: DiagramCommand.PageLandscape,
+                text: "Page Landscape",
+                widget: "dxCheckBox"
+            },
+            {
+                name: DiagramCommand.GridSize,
+                text: "Grid Size",
+                widget: "dxSelectBox",
+                items: [
+                    {
+                        value: 90,
+                        title: "0.16 cm"
+                    },
+                    {
+                        value: 180,
+                        title: "0.32 cm"
+                    },
+                    {
+                        value: 360,
+                        title: "0.64 cm"
+                    },
+                    {
+                        value: 720,
+                        title: "1.28 cm"
+                    }
+                ]
+            },
+            {
+                name: DiagramCommand.SnapToGrid,
+                text: "Snap to Grid",
+                widget: "dxCheckBox"
             }
         ];
     }
