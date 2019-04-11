664b91f79f95c8e2c7317beb74daaa351015720d
groshenkovamarina <marina.groshenkova@devexpress.com>
2019-04-11 15:27:58 +0300

    Update theme constants for new ThemeBuilder

--- themebuilder/modules/metadata-loader.js
+++ themebuilder/modules/metadata-loader.js
@@ -1,11 +1,21 @@
+const semver = require("semver");
+
 class MetadataLoader {
     constructor() {
         this.fullMetadata = require("../data/metadata/dx-theme-builder-metadata");
+        this.migrationMetadata = require("../data/migration-metadata/migration-metadata");
     }
 
     load(theme, colorScheme) {
         return new Promise((resolve) => {
-            let metadata = this.fullMetadata[theme + "_" + colorScheme.replace("-", "_") + "_metadata"];
+            let metadata = this.fullMetadata[theme + "_" + colorScheme.replace(/-/g, "_") + "_metadata"];
+
+            if(semver.gte(this.version(), "18.2.8") && this.migrationMetadata[theme]) {
+                this.migrationMetadata[theme].forEach(value => {
+                    metadata.push({ "Key": value });
+                });
+            }
+
             resolve(metadata);
         });
     }
---
+++ themebuilder/tests/migration-metadata-loader-spec.js
@@ -0,0 +1,46 @@
+const mock = require("mock-require");
+const assert = require("chai").assert;
+const MetadataLoader = require("../modules/metadata-loader");
+const metadata = require("./test-migration-metadata");
+
+const migrationData = {
+    generic: [
+        '@datagrid-base-background-color',
+        '@datagrid-border-color',
+        '@treelist-base-background-color',
+        '@treelist-border-color'
+    ]
+};
+
+const expectedMetadata = [{
+    "Name": "10. Font family",
+    "Key": "@base-font-family",
+    "Group": "base.typography",
+}, {
+    "Name": "20. Label color",
+    "Key": "@base-label-color",
+    "Group": "base.typography",
+}, {
+    "Key": "@datagrid-base-background-color"
+}, {
+    "Key": "@datagrid-border-color"
+}, {
+    "Key": "@treelist-base-background-color"
+}, {
+    "Key": "@treelist-border-color"
+}];
+
+describe("Migration metadataLoader", () => {
+    beforeEach(() => {
+        mock("../data/metadata/dx-theme-builder-metadata", metadata);
+        mock("../data/migration-metadata/migration-metadata", migrationData);
+    });
+
+    it("Load metadata", () => {
+        const metadataLoader = new MetadataLoader();
+        return metadataLoader.load("generic", "light").then(data => {
+            assert.deepEqual(data, expectedMetadata, "add missing constants works");
+        });
+    });
+});
+
