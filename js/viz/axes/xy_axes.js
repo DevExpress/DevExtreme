f6867964431119b17292d882f9ea056fff9e8154
Nikita Galushko <galnikrom@gmail.com>
2019-04-11 15:02:32 +0300

    fix

--- js/viz/axes/xy_axes.js
+++ js/viz/axes/xy_axes.js
@@ -939,7 +939,7 @@ module.exports = {
                 this._wrapped = title.element.setMaxSize(canvasLength, undefined, {
                     wordWrap: titleOptions.wordWrap || "none",
                     textOverflow: titleOptions.textOverflow || "ellipsis"
-                });
+                }) !== 1;
             } else {
                 !this._wrapped && title.element.restoreText();
             }
