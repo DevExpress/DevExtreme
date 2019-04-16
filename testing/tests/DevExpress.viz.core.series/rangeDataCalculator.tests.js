c1967a1026c71791505b006e01b56963fa251ac9
roman-simionov <rsimionov@gmail.com>
2019-04-16 20:15:23 +0300

    Fix datetime bar series in range selector (T733298)

--- testing/tests/DevExpress.viz.core.series/rangeDataCalculator.tests.js
+++ testing/tests/DevExpress.viz.core.series/rangeDataCalculator.tests.js
@@ -2698,6 +2698,42 @@ QUnit.test("Add interval if checkInterval in marginOptions", function(assert) {
     assert.equal(rangeData.max, 50, "max Visible Y");
 });
 
+QUnit.test("No errors when there is no axis viewport", function(assert) {
+    const data = [
+        { arg: new Date(1), val: 10 },
+        { arg: new Date(2), val: 20 },
+        { arg: new Date(3), val: 30 },
+        { arg: new Date(4), val: 40 },
+        { arg: new Date(5), val: 50 },
+        { arg: new Date(6), val: 60 }];
+
+    this.argumentAxis.getTranslator = function() {
+        return {
+            getBusinessRange() {
+                return {
+                    interval: 1,
+                    dataType: "datetime"
+                };
+            }
+        };
+    };
+    this.argumentAxis.getMarginOptions = () => { return { checkInterval: true }; };
+    this.argumentAxis.visualRange = () => { };
+
+    this.defaultOptions.showZero = false;
+    const series = createSeries(this.defaultOptions, { argumentAxis: this.argumentAxis });
+
+    series.updateData(data);
+    series.createPoints();
+
+    this.zoom(new Date(3), new Date(4));
+
+    const rangeData = series.getViewport();
+
+    assert.equal(rangeData.min, 10, "min Visible Y");
+    assert.equal(rangeData.max, 60, "max Visible Y");
+});
+
 QUnit.module("Get points in viewport", {
     beforeEach: function() {
         var argumentViewPort = [],
