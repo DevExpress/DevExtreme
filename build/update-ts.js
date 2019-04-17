788c055d5189543a70926bc50f1e177eb55dd445
Victor Shchelkunov <viterobk@users.noreply.github.com>
2019-04-17 14:26:16 +0300

    Remove the "update-ts" pre-commit hook

--- build/update-ts.js
+++
@@ -1,9 +0,0 @@
-const shell = require("shelljs");
-const TOOL_COMMAND = "devextreme-internal-declaration-tools";
-
-if(!shell.which(TOOL_COMMAND)) {
-    shell.echo(`${TOOL_COMMAND} command is not available`);
-    shell.exit(0);
-}
-
-shell.exit(shell.exec(TOOL_COMMAND).code);
