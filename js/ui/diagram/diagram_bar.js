600a715510498267f5ebc861b5a856c89783000e
Roman Resh <romanresh@live.com>
2019-04-17 10:55:49 +0300

    diagram - use dropdown button in toolbar + context menu (#7735)
    * use dropdown button in toolbar
    
    * diagram context menu

---
+++ js/ui/diagram/diagram_bar.js
@@ -0,0 +1,29 @@
+import { getDiagram } from "./diagram_importer";
+
+class DiagramBar {
+    constructor(owner) {
+        const { EventDispatcher } = getDiagram();
+        this.onChanged = new EventDispatcher(); // IBar.onChanged: EventDispatcher<IBarListener>
+        this._owner = owner;
+    }
+    raiseBarCommandExecuted(key, parameter) {
+        this.onChanged.raise("NotifyBarCommandExecuted", parseInt(key), parameter);
+    }
+
+    getCommandKeys() { // IBar.getCommandKeys(): DiagramCommand[]
+        throw "Not Implemented";
+    }
+    setItemValue(key, value) { // IBar.setItemValue(key: DiagramCommand, value: any)
+    }
+    setItemEnabled(key, enabled) { // IBar.setItemEnabled(key: DiagramCommand, enabled: boolean)
+    }
+    setItemVisible(key, enabled) { // IBar.setItemVisible(key: DiagramCommand, visible: boolean)
+    }
+    setEnabled(enabled) { // IBar.setEnabled(enabled: boolean)
+    }
+    isVisible() { // IBar.isVisible(): boolean
+        return true;
+    }
+}
+
+module.exports = DiagramBar;
