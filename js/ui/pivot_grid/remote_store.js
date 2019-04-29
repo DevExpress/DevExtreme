94b2a339c859fae6929e3752217214fd1ff13441
volnyagin <vconst@users.noreply.github.com>
2019-04-29 17:55:45 +0300

    PivotGrid: Fix expanding several rows and columns if paginate is enabled

--- js/ui/pivot_grid/remote_store.js
+++ js/ui/pivot_grid/remote_store.js
@@ -316,14 +316,20 @@ function getFiltersForDimension(fields) {
 }
 
 function getExpandedIndex(options, axis) {
-    if(axis === options.headerName) {
-        return options.path.length;
+    if(options.headerName) {
+        if(axis === options.headerName) {
+            return options.path.length;
+        } else if(options.oppositePath) {
+            return options.oppositePath.length;
+        }
     }
     return 0;
 }
 
 function getFiltersForExpandedDimension(options) {
-    return getFiltersByPath(options[options.headerName], options.path);
+    return getFiltersByPath(options[options.headerName], options.path).concat(
+        getFiltersByPath(options[options.headerName === "rows" ? "columns" : "rows"], options.oppositePath || [])
+    );
 }
 
 function getExpandedPathSliceFilter(options, dimensionName, level, firstCollapsedFieldIndex) {
