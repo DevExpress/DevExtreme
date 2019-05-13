a5c29e81a785732ba087a8a96d8e9681774b19ac
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2019-05-13 14:16:16 +0300

    Fix

--- js/ui/scheduler/ui.scheduler.appointments.strategy.base.js
+++ js/ui/scheduler/ui.scheduler.appointments.strategy.base.js
@@ -109,7 +109,7 @@ var BaseRenderingStrategy = Class.inherit({
             isRecurring = !!this.instance.fire("getField", "recurrenceRule", item);
 
         for(var j = 0; j < position.length; j++) {
-            var height = this.calculateAppointmentHeight(item, position[j]),
+            var height = this.calculateAppointmentHeight(item, position[j], isRecurring),
                 width = this.calculateAppointmentWidth(item, position[j], isRecurring),
                 resultWidth = width,
                 appointmentReduced = null,
@@ -468,7 +468,11 @@ var BaseRenderingStrategy = Class.inherit({
 
             fullDuration = this._adjustDurationByDaylightDiff(fullDuration, realStartDate, endDate);
 
-            endDate = new Date((viewStartDate.getTime() >= recurrencePartStartDate.getTime() ? recurrencePartStartDate.getTime() : viewStartDate.getTime()) + fullDuration);
+            endDate = new Date((viewStartDate.getTime() >= recurrencePartStartDate.getTime() ? recurrencePartStartDate.getTime() : viewStartDate.getTime()));
+
+            if(isRecurring) {
+                endDate = new Date(endDate.getTime() + fullDuration);
+            }
 
             if(!dateUtils.sameDate(realStartDate, endDate) && recurrencePartStartDate.getTime() < viewStartDate.getTime()) {
                 var headDuration = dateUtils.trimTime(endDate).getTime() - recurrencePartStartDate.getTime(),
