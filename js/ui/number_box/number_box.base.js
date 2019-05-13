8165141948c1245ff1dac6d01ffd3054d37fc31b
Eugeniy Maslyaev <mrjohn0011@users.noreply.github.com>
2019-05-13 16:30:13 +0300

    Use inputMode "decimal" attribute for dxNumberBox (T754627)

--- js/ui/number_box/number_box.base.js
+++ js/ui/number_box/number_box.base.js
@@ -152,6 +152,17 @@ var NumberBoxBase = TextEditor.inherit({
         return this.callBase().concat([{ name: "spins", Ctor: SpinButtons }]);
     },
 
+    _isMobileSupportInputMode: function() {
+        var version = parseFloat(browser.version);
+
+        return devices.real().platform !== "generic"
+            && !(
+                browser.chrome && version >= 66
+                || browser.safari && version >= 12
+                || browser.msie && version >= 75
+            );
+    },
+
     _defaultOptionsRules: function() {
         return this.callBase().concat([
             {
@@ -163,15 +174,7 @@ var NumberBoxBase = TextEditor.inherit({
                 }
             },
             {
-                device: function() {
-                    var version = parseFloat(browser.version);
-                    return devices.real().platform !== "generic"
-                        && !(
-                            browser.chrome && version >= 66
-                            || browser.safari && version >= 12
-                            || browser.msie && version >= 75
-                        );
-                },
+                device: this._isMobileSupportInputMode.bind(this),
                 options: {
                     /**
                      * @name dxNumberBoxOptions.mode
@@ -191,7 +194,7 @@ var NumberBoxBase = TextEditor.inherit({
     },
 
     _applyInputAttributes: function($input, customAttributes) {
-        $input.attr("inputmode", "numeric");
+        $input.attr("inputmode", "decimal");
         this.callBase($input, customAttributes);
     },
 
