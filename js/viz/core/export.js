f3e06f9367ddf6c4547f24c61328769cc78ff878
roman-simionov <rsimionov@gmail.com>
2019-05-16 17:05:59 +0300

    Fix uncaught exception in export (T740915)

--- js/viz/core/export.js
+++ js/viz/core/export.js
@@ -618,12 +618,30 @@ export const plugin = {
         _getExportMenuOptions() {
             return extend({}, this._getOption("export"), { rtl: this._getOption("rtlEnabled", true) });
         },
+
+        _disablePointerEvents() {
+            const pointerEventsValue = this._renderer.root.attr("pointer-events");
+
+            this._renderer.root.attr({
+                "pointer-events": "none"
+            });
+
+            return pointerEventsValue;
+        },
+
         exportTo(fileName, format) {
             const menu = this._exportMenu;
             const options = getExportOptions(this, this._getOption("export") || {}, fileName, format);
 
             menu && menu.hide();
-            clientExporter.export(this._renderer.root.element, options, getCreatorFunc(options.format));
+
+            const pointerEventsValue = this._disablePointerEvents();
+
+            clientExporter.export(this._renderer.root.element, options, getCreatorFunc(options.format)).done(() => {
+                this._renderer.root.attr({
+                    "pointer-events": pointerEventsValue
+                });
+            });
             menu && menu.show();
         },
         print() {
@@ -644,8 +662,14 @@ export const plugin = {
                 eventArgs.cancel = true;
             };
 
+            const pointerEventsValue = this._disablePointerEvents();
+
             menu && menu.hide();
-            clientExporter.export(this._renderer.root.element, options, getCreatorFunc(options.format));
+            clientExporter.export(this._renderer.root.element, options, getCreatorFunc(options.format)).done(() => {
+                this._renderer.root.attr({
+                    "pointer-events": pointerEventsValue
+                });
+            });
             menu && menu.show();
         }
     },
