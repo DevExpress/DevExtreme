e0b5575dd877a75085d8ac24e73b5dccf345e69f
vconst <vconst@users.noreply.github.com>
2019-05-17 17:46:57 +0300

    DataGrid: Fix page scrolling on an attempt to scroll the widget content (T737554) (#8093)

--- testing/tests/DevExpress.ui.widgets/scrollable.tests.js
+++ testing/tests/DevExpress.ui.widgets/scrollable.tests.js
@@ -2525,6 +2525,64 @@ QUnit.test("mousewheel for vertical direction", function(assert) {
     assert.equal(scrollable.scrollOffset().top, distance, "scrolled vertically");
 });
 
+// T737554
+QUnit.test("preventDefault should be called on immediate mousewheel at the end of content", function(assert) {
+    var $scrollable = $("#scrollable");
+    var lastWheelEventArgs;
+    $scrollable.on("dxmousewheel", function(e) {
+        lastWheelEventArgs = e;
+    });
+
+    $scrollable.dxScrollable({
+        useNative: false,
+        direction: "vertical",
+        inertiaEnabled: false,
+        bounceEnabled: false
+    });
+    var scrollable = $scrollable.dxScrollable("instance");
+
+    var $container = $scrollable.find("." + SCROLLABLE_CONTAINER_CLASS);
+
+    var pointer = pointerMock($container)
+        .start()
+        .wheel(-50);
+
+    pointer.wheel(-50);
+
+    assert.equal(scrollable.scrollOffset().top, 50, "scrolled vertically");
+    assert.strictEqual(lastWheelEventArgs.isDefaultPrevented(), true, "default is prevented for wheel event");
+});
+
+// T737554
+QUnit.test("preventDefault should not be called on delayed mousewheel at the end of content", function(assert) {
+    var $scrollable = $("#scrollable");
+    var lastWheelEventArgs;
+    $scrollable.on("dxmousewheel", function(e) {
+        lastWheelEventArgs = e;
+    });
+
+    $scrollable.dxScrollable({
+        useNative: false,
+        direction: "vertical",
+        inertiaEnabled: false,
+        bounceEnabled: false
+    });
+    var scrollable = $scrollable.dxScrollable("instance");
+
+    var $container = $scrollable.find("." + SCROLLABLE_CONTAINER_CLASS);
+
+    var pointer = pointerMock($container)
+        .start()
+        .wheel(-50);
+
+    this.clock.tick(500);
+
+    pointer.wheel(-50);
+
+    assert.equal(scrollable.scrollOffset().top, 50, "scrolled vertically");
+    assert.strictEqual(lastWheelEventArgs.isDefaultPrevented(), false, "default is prevented for wheel event");
+});
+
 QUnit.test("mousewheel calls update before validation", function(assert) {
     var distance = 10;
 
