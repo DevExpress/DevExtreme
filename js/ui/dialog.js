b59ee74e22f51e5e4b2a65f1f5b1cdd1a2b1b703
Vsevolod Filinov (DevExpress) <filinov.vsevolod@corp.devexpress.com>
2019-05-17 12:55:57 +0300

    disable drag of dx dialog without title (T740940)

--- js/ui/dialog.js
+++ js/ui/dialog.js
@@ -133,12 +133,12 @@ exports.custom = function(options) {
         });
     });
 
+    const showTitle = options.showTitle === undefined ? true : options.showTitle;
+
     const popupInstance = new Popup($element, extend({
         title: options.title || exports.title,
-        showTitle: function() {
-            const isTitle = options.showTitle === undefined ? true : options.showTitle;
-            return isTitle;
-        }(),
+        dragEnabled: showTitle,
+        showTitle: showTitle,
         height: "auto",
         width: function() {
             const isPortrait = $(window).height() > $(window).width(),
