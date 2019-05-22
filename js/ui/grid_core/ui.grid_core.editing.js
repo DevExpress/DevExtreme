b6a07cd72a65db13573dfb1a8faaee2a6e886afc
Alyar <Alyar666@users.noreply.github.com>
2019-05-22 18:08:02 +0300

    DataGrid - Fix the dependent cascading editor that is not updated when a master cell value is changed if showEditorAlways is enabled in batch mode (T741679) (#8137)

--- js/ui/grid_core/ui.grid_core.editing.js
+++ js/ui/grid_core/ui.grid_core.editing.js
@@ -1626,7 +1626,8 @@ var EditingController = modules.ViewController.inherit((function() {
                 $cellElement = $(options.cellElement),
                 editMode = getEditMode(that),
                 params,
-                columns;
+                columns,
+                isCustomSetCellValue = options.column.setCellValue !== options.column.defaultSetCellValue;
 
             if(rowKey === undefined) {
                 that._dataController.fireError("E1043");
@@ -1658,11 +1659,11 @@ var EditingController = modules.ViewController.inherit((function() {
                         return that.saveEditData();
                     } else if(editMode === EDIT_MODE_BATCH) {
                         columns = that._columnsController.getVisibleColumns();
-                        forceUpdateRow = columns.some((column) => column.calculateCellValue !== column.defaultCalculateCellValue);
+                        forceUpdateRow = isCustomSetCellValue || columns.some((column) => column.calculateCellValue !== column.defaultCalculateCellValue);
                     }
                 }
 
-                if(options.row && (forceUpdateRow || options.column.setCellValue !== options.column.defaultSetCellValue)) {
+                if(options.row && (forceUpdateRow || isCustomSetCellValue)) {
                     that._updateEditRow(options.row, forceUpdateRow);
                 }
             }
