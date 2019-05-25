ccf44d23f471695d935e083b471394c09b11e5fc
eugeniykiyashko <eugeniy.kiyashko@devexpress.com>
2019-05-26 00:48:36 +0300

    Revert "refactor selectAllWithSelectNodesRecursiveFalse.js"
    This reverts commit 9149e07dc278d1b70fdb2b436a38e10827fb84a4.

--- testing/tests/DevExpress.ui.widgets/treeViewParts/selectAllWithSelectNodesRecursiveFalse.js
+++ testing/tests/DevExpress.ui.widgets/treeViewParts/selectAllWithSelectNodesRecursiveFalse.js
@@ -1,24 +1,20 @@
+/* global DATA, initTree */
+
 import $ from "jquery";
-import { TreeViewTestWrapper, TreeViewDataHelper } from "../../../helpers/TreeViewTestHelper.js";
 
-let { module, test, } = QUnit;
-
-const dataHelper = new TreeViewDataHelper();
-const createInstance = (options) => new TreeViewTestWrapper(options);
-
-module("SelectAll mode with 'selectNodesRecursive' = 'false'", {
+QUnit.module("SelectAll mode with 'selectNodesRecursive' = 'false'", {
     beforeEach: function() {
-        let data = $.extend(true, [], dataHelper.data[5]);
+        var data = $.extend(true, [], DATA[5]);
         data[0].items[1].items[0].expanded = true;
         data[0].items[1].items[1].expanded = true;
-
-        this.treeView = createInstance({
+        var $treeView = initTree({
             items: $.extend(true, [], data),
             showCheckBoxesMode: "selectAll",
             selectNodesRecursive: false
         });
+        this.treeView = $treeView.dxTreeView("instance");
         this.checkAllItemsSelection = function(selection) {
-            var items = this.treeView.instance.option('items'),
+            var items = this.treeView.option('items'),
                 count = 0;
 
             count = items[0].selected === selection ? (count + 1) : count;
@@ -31,19 +27,17 @@ module("SelectAll mode with 'selectNodesRecursive' = 'false'", {
             return count;
         };
     }
-}, () => {
-    test("Select all items", function(assert) {
-        this.treeView.getSelectAllItem().dxCheckBox("instance").option("value", true);
+});
 
-        this.treeView.checkSelectedNodes([0, 1, 2, 3, 4, 5]);
-        assert.equal(this.checkAllItemsSelection(true), 6, "all items were selected");
-    });
+QUnit.test("Select all items", function(assert) {
+    var checkBox = this.treeView._$selectAllItem.dxCheckBox("instance");
+    checkBox.option("value", true);
+    assert.equal(this.checkAllItemsSelection(true), 6, "all items were selected");
+});
 
-    test("Unselect all items", function(assert) {
-        this.treeView.getSelectAllItem().dxCheckBox("instance").option("value", true);
-        this.treeView.getSelectAllItem().dxCheckBox("instance").option("value", false);
-
-        this.treeView.checkSelectedNodes([]);
-        assert.equal(this.checkAllItemsSelection(false), 6, "all items were unselected");
-    });
+QUnit.test("Unselect all items", function(assert) {
+    var checkBox = this.treeView._$selectAllItem.dxCheckBox("instance");
+    checkBox.option("value", true);
+    checkBox.option("value", false);
+    assert.equal(this.checkAllItemsSelection(false), 6, "all items were unselected");
 });
