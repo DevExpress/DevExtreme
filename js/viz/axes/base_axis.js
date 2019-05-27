6ce7a326d604ff03bc074b9c08b4e67591b4d5aa
roman-simionov <rsimionov@gmail.com>
2019-05-27 15:50:02 +0300

    Reset calculated whole range (T741577)

--- js/viz/axes/base_axis.js
+++ js/viz/axes/base_axis.js
@@ -1888,6 +1888,7 @@ Axis.prototype = {
     },
 
     setInitRange() {
+        this._initRange = {};
         if(Object.keys(this._options.wholeRange || {}).length === 0) {
             this._initRange = this.getZoomBounds();
         }
