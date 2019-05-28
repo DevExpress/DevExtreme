8ebcf75985dae35216690a27c2e6d88abc6eb524
alexey.kamyshin <alexey.kamyshin@devexpress.com>
2019-05-28 11:09:43 +0300

    Disable tests for mobile emulators

--- testing/tests/DevExpress.ui.widgets.scheduler/integration.appointments.tests.js
+++ testing/tests/DevExpress.ui.widgets.scheduler/integration.appointments.tests.js
@@ -45,6 +45,10 @@ function getOffset() {
     }
 }
 
+function isDeviceDesktop() {
+    return devices.current().deviceType === "desktop";
+}
+
 QUnit.module("Integration: Appointments", {
     beforeEach: function() {
         fx.off = true;
@@ -1720,6 +1724,7 @@ QUnit.test("Appointments should be repainted if the 'crossScrollingEnabled' is c
 });
 
 QUnit.test("Appointment should not twitch on drag start with horizontal dragging", function(assert) {
+    if(!isDeviceDesktop()) return;
     let resourcesData = [
         {
             text: "Samantha Bright",
@@ -1793,6 +1798,7 @@ QUnit.test("Appointment should not twitch on drag start with horizontal dragging
 });
 
 QUnit.test("Appointment should have correct position while horizontal dragging", function(assert) {
+    if(!isDeviceDesktop()) return;
     this.createInstance({
         height: 500,
         editing: true,
@@ -1821,6 +1827,7 @@ QUnit.test("Appointment should have correct position while horizontal dragging",
 });
 
 QUnit.test("Appointment should have correct position while horizontal dragging, crossScrollingEnabled = true (T732885)", function(assert) {
+    if(!isDeviceDesktop()) return;
     this.createInstance({
         height: 500,
         editing: true,
