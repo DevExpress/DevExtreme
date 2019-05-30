a6397d649dfc85175de030e7c43024d87b875261
CORP\sermyazhko.anton <anton.sermyazhko@devexpress.com>
2019-05-30 12:10:11 +0300

    Added regression testing on TestCafe

---
+++ testing/e2e/runner.js
@@ -0,0 +1,26 @@
+const createTestCafe = require('testcafe');
+const process = require('process');
+require('nconf').argv();
+
+var testCafe;
+createTestCafe('localhost', 1437, 1438)
+    .then(tc => {
+        testCafe = tc;
+        var testName = process.argv[2],
+            runner = testCafe.createRunner()
+                .browsers(["chrome"])
+                .src([
+                    "./testing/e2e/tests/dataGrid/keyboardNavigation.ts"
+                ]);
+
+        if(testName && testName.trim()) {
+            runner.filter(name => name === testName);
+        }
+
+        return runner.run({
+            quarantineMode: false
+        });
+    })
+    .then(failedCount => {
+        testCafe.close();
+    });
