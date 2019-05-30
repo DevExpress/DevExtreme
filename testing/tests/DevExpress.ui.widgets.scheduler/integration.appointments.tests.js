892a946ece63d99591e920840588bb48b16e4a0b
alexey.kamyshin <alexey.kamyshin@devexpress.com>
2019-05-30 10:06:38 +0300

    Adding single assert for "skipped tests"

--- testing/tests/DevExpress.ui.widgets.scheduler/integration.appointments.tests.js
+++ testing/tests/DevExpress.ui.widgets.scheduler/integration.appointments.tests.js
@@ -1736,7 +1736,10 @@ QUnit.test("Appointments should be repainted if the 'crossScrollingEnabled' is c
 });
 
 QUnit.test("Appointment should have correct position while horizontal dragging", function(assert) {
-    if(!isDeviceDesktop()) return;
+    if(!isDeviceDesktop()) {
+        assert.ok(true, 'Mobile device skip');
+        return;
+    }
     this.createInstance({
         height: 500,
         editing: true,
@@ -1766,7 +1769,10 @@ QUnit.test("Appointment should have correct position while horizontal dragging",
 });
 
 QUnit.test("Appointment should not twitch on drag start with horizontal dragging", function(assert) {
-    if(!isDeviceDesktop()) return;
+    if(!isDeviceDesktop()) {
+        assert.ok(true, 'Mobile device skip');
+        return;
+    }
     let resourcesData = [
         {
             text: "Samantha Bright",
@@ -1840,7 +1846,10 @@ QUnit.test("Appointment should not twitch on drag start with horizontal dragging
 });
 
 QUnit.test("Appointment should have correct position while horizontal dragging, crossScrollingEnabled = true (T732885)", function(assert) {
-    if(!isDeviceDesktop()) return;
+    if(!isDeviceDesktop()) {
+        assert.ok(true, 'Mobile device skip');
+        return;
+    }
     this.createInstance({
         height: 500,
         editing: true,
@@ -1869,7 +1878,10 @@ QUnit.test("Appointment should have correct position while horizontal dragging, 
 });
 
 QUnit.test("Appointment should have correct position while horizontal dragging in scrolled date table, crossScrollingEnabled = true", function(assert) {
-    if(!isDeviceDesktop()) return;
+    if(!isDeviceDesktop()) {
+        assert.ok(true, 'Mobile device skip');
+        return;
+    }
     this.createInstance({
         height: 500,
         width: 800,
