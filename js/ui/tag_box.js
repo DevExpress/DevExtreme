62d546aa5d25ad3661b88a01e6245be95a3e891b
Kseniya Mordalyuk <KsyushaM@users.noreply.github.com>
2019-06-06 12:52:23 +0300

    Fix tag text when display value is empty (T745958) (#8311)

--- js/ui/tag_box.js
+++ js/ui/tag_box.js
@@ -1011,10 +1011,10 @@ var TagBox = SelectBox.inherit({
     },
 
     _getItemModel: function(item, displayValue) {
-        if(typeUtils.isObject(item)) {
+        if(typeUtils.isObject(item) && displayValue) {
             return item;
         } else {
-            return displayValue;
+            return commonUtils.ensureDefined(displayValue, "");
         }
     },
 
