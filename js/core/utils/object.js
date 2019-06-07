290a931ff988c1a57411ce90b30d30a9d1810cd7
Alexander Ziborov <1420883+San4es@users.noreply.github.com>
2019-06-07 14:36:13 +0300

    deepExtendArraySafe - Fix object proto pollution

--- js/core/utils/object.js
+++ js/core/utils/object.js
@@ -71,7 +71,7 @@ var deepExtendArraySafe = function(target, changes, extendComplexObject, assignB
         prevValue = target[name];
         newValue = changes[name];
 
-        if(target === newValue) {
+        if(name === "__proto__" || target === newValue) {
             continue;
         }
 
