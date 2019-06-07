1a5d755e464a5cb852f72162266ef56a759103e1
ziborov.alexander <San4es@users.noreply.github.com>
2019-06-07 16:33:33 +0300

    Convert test to ES5
    (cherry picked from commit b34118d8a7b504a9443f3e8722c409393e0351eb)

--- testing/tests/DevExpress.core/utils.extend.tests.js
+++ testing/tests/DevExpress.core/utils.extend.tests.js
@@ -1,6 +1,8 @@
-import { extend } from "core/utils/extend";
+"use strict";
 
-QUnit.test("extend does not pollute object prototype", (assert) => {
-    extend(true, { }, JSON.parse(`{ "__proto__": { "pollution": true }}`));
+var extend = require("core/utils/extend").extend;
+
+QUnit.test("extend does not pollute object prototype", function(assert) {
+    extend(true, { }, JSON.parse("{ \"__proto__\": { \"pollution\": true }}"));
     assert.ok(!("pollution" in { }), "object prototype is not polluted");
 });
