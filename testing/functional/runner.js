083a25161438f26ce161a90c49593d6676bf8f39
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2019-06-07 11:55:42 +0300

    Drone CI - add functional tests (#8315)

--- testing/functional/runner.js
+++ testing/functional/runner.js
@@ -1,19 +1,20 @@
 const createTestCafe = require('testcafe');
 const process = require('process');
+const parseArgs = require('minimist');
 require('nconf').argv();
 
 var testCafe;
 createTestCafe('localhost', 1437, 1438)
     .then(tc => {
         testCafe = tc;
-        var testName = process.argv[2],
-            runner = testCafe.createRunner()
-                .browsers(["chrome"])
-                .src([
-                    "./testing/functional/tests/dataGrid/keyboardNavigation.ts"
-                ]);
 
-        if(testName && testName.trim()) {
+        var args = getArgs(),
+            testName = args.test.trim(),
+            runner = testCafe.createRunner()
+                .browsers(args.browsers.split(" "))
+                .src(["./testing/functional/tests/**/*.ts"]);
+
+        if(testName) {
             runner.filter(name => name === testName);
         }
 
@@ -23,4 +24,16 @@ createTestCafe('localhost', 1437, 1438)
     })
     .then(failedCount => {
         testCafe.close();
+        if(failedCount !== 0) {
+            process.exit(failedCount);
+        }
     });
+
+function getArgs() {
+    return parseArgs(process.argv.slice(1), {
+        default: {
+            browsers: "chrome",
+            test: ""
+        }
+    });
+}
