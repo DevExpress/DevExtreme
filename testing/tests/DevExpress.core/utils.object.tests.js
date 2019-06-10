7703f7d7268ffae777cfb765dd4300731a798111
Alexander Ziborov <1420883+San4es@users.noreply.github.com>
2019-06-10 10:58:49 +0300

    Use ES5 in object tests (#8328)

--- testing/tests/DevExpress.core/utils.object.tests.js
+++ testing/tests/DevExpress.core/utils.object.tests.js
@@ -180,6 +180,6 @@ QUnit.test("deepExtendArraySafe utility does not throw an error with 'null' deep
 });
 
 QUnit.test("deepExtendArraySafe utility does not pollute object prototype", function(assert) {
-    objectUtils.deepExtendArraySafe({ }, JSON.parse(`{ "__proto__": { "pollution": true }}`), true);
+    objectUtils.deepExtendArraySafe({ }, JSON.parse("{ \"__proto__\": { \"pollution\": true }}"), true);
     assert.ok(!("pollution" in { }), "object prototype is not polluted");
 });
