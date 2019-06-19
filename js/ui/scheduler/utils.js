e795ed67e43e7e56a6b1697feafc482ca6e405bf
Andrew Makarov <andrey.makarov@devexpress.com>
2019-06-19 18:07:55 +0300

    Refactoring

--- js/ui/scheduler/utils.js
+++ js/ui/scheduler/utils.js
@@ -1,6 +1,6 @@
 import devices from "../../core/devices";
 import browser from "../../core/utils/browser";
-import getWindow from "../../core/utils/window";
+import { getWindow } from "../../core/utils/window";
 
 export const utils = {
     isWebKitBrowserInZoom: () => devices.current().deviceType === "desktop" && browser.webkit && getWindow().devicePixelRatio > 1
