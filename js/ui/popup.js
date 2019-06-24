e4857d1b15741e250afbf9509942a24e2b09f4de
alexander-kotov-dx <alexander.kotov@devexpress.com>
2019-06-24 16:20:21 +0300

    small refactoring

--- js/ui/popup.js
+++ js/ui/popup.js
@@ -672,8 +672,8 @@ var Popup = Overlay.inherit({
         this._setHeightClasses(this.overlayContent(), currentHeightStrategyClass);
     },
 
-    _heightStrategyChangeOffset: function(currentHeightStrategyClass, popupHeightParts) {
-        return currentHeightStrategyClass === HEIGHT_STRATEGIES.flex ? -popupHeightParts.popupVerticalPaddings : 0;
+    _heightStrategyChangeOffset: function(currentHeightStrategyClass, popupVerticalPaddings) {
+        return currentHeightStrategyClass === HEIGHT_STRATEGIES.flex ? -popupVerticalPaddings : 0;
     },
 
     _chooseHeightStrategy: function(overlayContent) {
@@ -702,7 +702,7 @@ var Popup = Overlay.inherit({
                 + popupHeightParts.footer
                 + popupHeightParts.contentVerticalOffsets
                 + popupHeightParts.popupVerticalOffsets
-                + this._heightStrategyChangeOffset(currentHeightStrategyClass, popupHeightParts);
+                + this._heightStrategyChangeOffset(currentHeightStrategyClass, popupHeightParts.popupVerticalPaddings);
 
         if(currentHeightStrategyClass === HEIGHT_STRATEGIES.static) {
             if(!this._isAutoHeight() || contentMaxHeight || contentMinHeight) {
