d0e4d236d8150db1b77a47119ad0f9797481ce84
vconst <vconst@users.noreply.github.com>
2019-06-24 11:38:45 +0300

    DataGrid: Fix scrolling by content when all grouped rows are collapsed and a left column is fixed (T747718) (#8467)

--- js/ui/grid_core/ui.grid_core.column_fixing.js
+++ js/ui/grid_core/ui.grid_core.column_fixing.js
@@ -197,12 +197,19 @@ var baseFixedColumns = {
             isEmptyCell,
             transparentColumnIndex,
             alignByFixedColumnCellCount,
-            column = options.column;
+            column = options.column,
+            isFixedTableRendering = that._isFixedTableRendering,
+            isGroupCell = options.rowType === "group" && isDefined(column.groupIndex);
 
-        if(!that._isFixedTableRendering && that._isFixedColumns) {
+        // T747718
+        if(isFixedTableRendering && isGroupCell && !column.command) {
+            $cell.css("pointerEvents", "none");
+        }
+
+        if(!isFixedTableRendering && that._isFixedColumns) {
             isEmptyCell = column.fixed || column.command;
 
-            if(options.rowType === "group" && isDefined(column.groupIndex)) {
+            if(isGroupCell) {
                 isEmptyCell = false;
                 if(options.row.summaryCells && options.row.summaryCells.length) {
                     columns = that._columnsController.getVisibleColumns();
