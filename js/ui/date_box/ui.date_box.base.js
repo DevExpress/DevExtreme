db9f34018e4c57bcebfd6d94020c9ad47d58a2da
Eugeniy Maslyaev <mrjohn0011@users.noreply.github.com>
2019-06-26 17:18:15 +0300

    Don't apply non-changed validators in the dxDateBox when min/max changed (T539247)  (#8526)

--- js/ui/date_box/ui.date_box.base.js
+++ js/ui/date_box/ui.date_box.base.js
@@ -786,7 +786,7 @@ var DateBox = DropDownEditor.inherit({
                 break;
             case "min":
             case "max":
-                this._validateValue(this.dateOption("value"));
+                this._applyInternalValidation(this.dateOption("value"));
                 this._invalidate();
                 break;
             case "dateSerializationFormat":
