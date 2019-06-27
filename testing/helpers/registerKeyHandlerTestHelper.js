ec2cad4048bf9f7e2f0e6dd443aee5f287d5f183
eugeniykiyashko <eugeniy.kiyashko@devexpress.com>
2019-06-27 09:58:37 +0300

    TabPanel - The registerKeyHandler option's handler is not passed to tabs (T750382)

--- testing/helpers/registerKeyHandlerTestHelper.js
+++ testing/helpers/registerKeyHandlerTestHelper.js
@@ -2,13 +2,14 @@ import $ from "jquery";
 import keyboardMock from "./keyboardMock.js";
 
 const SUPPORTED_KEYS = ["backspace", "tab", "enter", "escape", "pageUp", "pageDown", "end", "home", "leftArrow", "upArrow", "rightArrow", "downArrow", "del", "space", "F", "A", "asterisk", "minus"];
+const TABPANEL = "dxTabPanel";
 
 const registerKeyHandlerTestHelper = {
     runTests: function(QUnit, WidgetName) {
 
         QUnit.module("RegisterKeyHandler", {
             beforeEach: () => {
-                this.handler = sinon.spy();
+                this.handler = sinon.spy(() => "checkHandler");
 
                 this.createWidget = (options = {}) => {
                     this.$widget = $("<div>")[WidgetName]($.extend({
@@ -19,9 +20,12 @@ const registerKeyHandlerTestHelper = {
                     this.widget = this.$widget[WidgetName]("instance");
                 };
 
-                this.checkKeyHandlerCall = (assert, key) => {
+                this.checkKeyHandlerCall = (assert, key, widget) => {
+                    let args = this.handler.firstCall.args[0];
+
                     assert.strictEqual(this.handler.callCount, 1, `key press ${key} button was handled`);
-                    assert.ok(this.$widget.is(this.handler.firstCall.args[0].target), "event.target");
+                    assert.deepEqual(widget._supportedKeys()[key], this.handler, "handled true event");
+                    assert.ok(widget.$element().is(args.target), "event.target");
                 };
             },
             afterEach: () => {
@@ -32,8 +36,10 @@ const registerKeyHandlerTestHelper = {
                 QUnit.test(`RegisterKeyHandler -> onInitialize - "${key}"`, (assert) => {
                     this.createWidget({ onInitialized: e => { e.component.registerKeyHandler(key, this.handler); } });
 
+                    if(WidgetName === TABPANEL) { this.$widget.attr("tabIndex", 1); }
+
                     keyboardMock(this.$widget).press(key);
-                    this.checkKeyHandlerCall(assert, key);
+                    this.checkKeyHandlerCall(assert, key, this.widget);
                 });
 
                 QUnit.test(`RegisterKeyHandler -> "${key}"`, (assert) => {
@@ -41,8 +47,16 @@ const registerKeyHandlerTestHelper = {
 
                     this.widget.registerKeyHandler(key, this.handler);
 
+                    if(WidgetName === TABPANEL) {
+                        keyboardMock(this.widget._tabs.$element()).press(key);
+                        this.checkKeyHandlerCall(assert, key, this.widget._tabs);
+                        this.handler.reset();
+
+                        this.$widget.attr("tabIndex", 1);
+                    }
+
                     keyboardMock(this.$widget).press(key);
-                    this.checkKeyHandlerCall(assert, key);
+                    this.checkKeyHandlerCall(assert, key, this.widget);
                 });
             });
         });
