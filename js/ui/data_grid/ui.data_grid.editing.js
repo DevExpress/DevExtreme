61f4429a7ab32d3a8a2473505bf4b96767fcc708
Alyar <Alyar666@users.noreply.github.com>
2019-07-02 12:09:29 +0300

    DataGrid: Fix edit form after expand/collapse grouped columns (T752381) (#8593)

--- js/ui/data_grid/ui.data_grid.editing.js
+++ js/ui/data_grid/ui.data_grid.editing.js
@@ -2,5 +2,22 @@ import "./ui.data_grid.editor_factory";
 
 import gridCore from "./ui.data_grid.core";
 import editingModule from "../grid_core/ui.grid_core.editing";
+import { extend } from '../../core/utils/extend';
 
-gridCore.registerModule("editing", editingModule);
+gridCore.registerModule("editing", extend(true, {}, editingModule, {
+    extenders: {
+        controllers: {
+            data: {
+                _changeRowExpandCore: function(key) {
+                    var editingController = this._editingController;
+
+                    if(Array.isArray(key)) {
+                        editingController && editingController.refresh();
+                    }
+
+                    this.callBase.apply(this, arguments);
+                }
+            }
+        }
+    }
+}));
