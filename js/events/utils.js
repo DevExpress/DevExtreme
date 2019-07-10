066856221939d21f558b572f20fb418bf06b223c
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2019-07-10 09:58:51 +0300

    DataGrid - A page scrolls after scrolling in the header filter on MAC (T733974) (#8510)

--- js/events/utils.js
+++ js/events/utils.js
@@ -109,6 +109,10 @@ const isMouseEvent = (e) => {
     return isNativeMouseEvent(e) || ((isPointerEvent(e) || isDxEvent(e)) && e.pointerType === "mouse");
 };
 
+const isDxMouseWheelEvent = (e) => {
+    return e && e.type === "dxmousewheel";
+};
+
 const isTouchEvent = (e) => {
     return isNativeTouchEvent(e) || ((isPointerEvent(e) || isDxEvent(e)) && e.pointerType === "touch");
 };
@@ -158,7 +162,7 @@ const needSkipEvent = (e) => {
     if($target.is(".dx-skip-gesture-event *, .dx-skip-gesture-event")) {
         return true;
     }
-    if(e.type === 'dxmousewheel') {
+    if(isDxMouseWheelEvent(e)) {
         const isContentEditableFocused = target.isContentEditable && $target.closest("div[contenteditable='true']").is(':focus');
         const isInputFocused = $target.is("input[type='number'], textarea, select") && $target.is(':focus');
         return isInputFocused || isContentEditableFocused;
@@ -242,6 +246,7 @@ module.exports = {
     eventSource: eventSource,
     isPointerEvent: isPointerEvent,
     isMouseEvent: isMouseEvent,
+    isDxMouseWheelEvent: isDxMouseWheelEvent,
     isTouchEvent: isTouchEvent,
     isKeyboardEvent: isKeyboardEvent,
 
