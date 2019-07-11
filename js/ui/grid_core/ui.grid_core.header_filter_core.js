879539ca9f37ec2545b1e3e4b7514a5388f14994
Konstantin Volnyagin <vconst@users.noreply.github.com>
2019-07-11 13:50:30 +0300

    DataGrid: Fix header filter popup unexpectedly close under certain conditions (T756320) (#8766)

--- js/ui/grid_core/ui.grid_core.header_filter_core.js
+++ js/ui/grid_core/ui.grid_core.header_filter_core.js
@@ -188,7 +188,7 @@ exports.HeaderFilterView = modules.View.inherit({
                 shading: false,
                 showTitle: false,
                 showCloseButton: false,
-                closeOnTargetScroll: true,
+                closeOnTargetScroll: false, // T756320
                 dragEnabled: false,
                 closeOnOutsideClick: true,
                 focusStateEnabled: false,
