ec3d0baa1139de81d16e32a21972b902820861bd
Alexey Kamyshin <aurrius.the.badger@gmail.com>
2019-07-11 09:13:34 +0200

    Revert "Enable Scheduler vertical scrolling with custom dataCellTemplate (T745074) (#8751)"
    This reverts commit 661ec88a5814022ca4e5cbc3ae735313c9eefa77.

--- js/ui/scheduler/workspaces/ui.scheduler.work_space.js
+++ js/ui/scheduler/workspaces/ui.scheduler.work_space.js
@@ -333,10 +333,6 @@ var SchedulerWorkSpace = Widget.inherit({
         return $cell.hasClass(ALL_DAY_TABLE_CELL_CLASS);
     },
 
-    _hasDataCellTemplate: function() {
-        return !!this.option("dataCellTemplate");
-    },
-
     _getGroupIndexByCell: function($cell) {
         var cellsInRow = this._getCellCount(),
             currentCellIndex = $cell.index() + 1,
