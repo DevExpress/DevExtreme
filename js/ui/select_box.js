1e120e8907f99ea68253ed1a63d2c7b465758037
Kseniya Mordalyuk <kseniya.mordalyuk@devexpress.com>
2019-07-11 17:07:45 +0300

    Close popup after Tab key click if search is enabled in SelectBox (T754157)

--- js/ui/select_box.js
+++ js/ui/select_box.js
@@ -11,6 +11,7 @@ var $ = require("../core/renderer"),
     getPublicElement = require("../core/utils/dom").getPublicElement,
     Deferred = deferredUtils.Deferred,
     errors = require("../core/errors"),
+    domAdapter = require("../core/dom_adapter"),
     inkRipple = require("./widget/utils.ink_ripple"),
     messageLocalization = require("../localization/message"),
     registerComponent = require("../core/component_registrator"),
@@ -659,6 +660,11 @@ var SelectBox = DropDownList.inherit({
 
     _shouldOpenPopup: function() {
         return this._needPassDataSourceToList();
+    },
+
+    _isFocused: function() {
+        var activeElement = domAdapter.getActiveElement();
+        return this.callBase() && $(activeElement).closest(this._input()).length > 0;
     },
 
     _renderValueChangeEvent: function() {
