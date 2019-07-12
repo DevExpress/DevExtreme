66874482d6ae067bdbeebe1da71175d1a49ddd3b
Dmitry Semenov <jsdmitry@users.noreply.github.com>
2019-07-12 14:12:01 +0300

    Fix tests on the check pointer events in the Edge browser (#8794)

--- testing/tests/DevExpress.ui.widgets/scrollable.tests.js
+++ testing/tests/DevExpress.ui.widgets/scrollable.tests.js
@@ -1897,6 +1897,7 @@ QUnit.module("Hoverable interaction", () => {
             ["vertical", "horizontal"].forEach((direction) => {
                 ["onScroll", "onHover", "always", "never"].forEach((showScrollbarMode) => {
                     QUnit.test(`ScrollBar hoverable - disabled: ${disabled}, showScrollbar: ${showScrollbarMode}, direction: ${direction}, onInitialize: ${onInitialize}`, (assert) => {
+                        const done = assert.async();
                         const $scrollable = $("#scrollable").dxScrollable({
                             useNative: false,
                             useSimulatedScrollbar: true,
@@ -1907,12 +1908,15 @@ QUnit.module("Hoverable interaction", () => {
                         });
 
                         const checkAsserts = (isHoverable) => {
-                            assert.strictEqual(scrollBar.option("hoverStateEnabled"), isHoverable, "scrollbar.hoverStateEnabled");
-                            assert.strictEqual($scrollBar.hasClass(SCROLLBAR_HOVERABLE_CLASS), isHoverable, `scrollbar hasn't ${SCROLLBAR_HOVERABLE_CLASS}`);
-                            if(browser.msie) {
-                                assert.ok($scrollBar.css("pointer-events"), "pointer-events prop is defined");
-                            }
-                            assert.strictEqual($scrollBar.css("pointer-events"), disabled ? "none" : "auto", "pointer-events");
+                            setTimeout(() => {
+                                assert.strictEqual(scrollBar.option("hoverStateEnabled"), isHoverable, "scrollbar.hoverStateEnabled");
+                                assert.strictEqual($scrollBar.hasClass(SCROLLBAR_HOVERABLE_CLASS), isHoverable, `scrollbar hasn't ${SCROLLBAR_HOVERABLE_CLASS}`);
+                                if(browser.msie) {
+                                    assert.ok($scrollBar.css("pointer-events"), "pointer-events prop is defined");
+                                }
+                                assert.strictEqual($scrollBar.css("pointer-events"), disabled ? "none" : "auto", "pointer-events");
+                                done();
+                            });
                         };
 
                         if(!onInitialize) {
