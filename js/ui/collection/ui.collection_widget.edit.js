186dbba2a72f639add4a071ece8f78878f33bea4
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2019-07-18 15:35:02 +0300

    List does not select items if the selectedItems contains a record which is not in the data source (T757809) (#8846)

--- js/ui/collection/ui.collection_widget.edit.js
+++ js/ui/collection/ui.collection_widget.edit.js
@@ -252,9 +252,14 @@ var CollectionWidget = BaseCollectionWidget.inherit({
                     options.userData = that._dataSource._userData;
                 }
                 var store = that._dataSource && that._dataSource.store();
-                return store ? store.load(options).done(function(items) {
-                    that._dataSource._applyMapFunction(items);
-                }) : new Deferred().resolve([]);
+
+                if(store) {
+                    return store.load(options).done(function(items) {
+                        that._dataSource._applyMapFunction(items);
+                    });
+                } else {
+                    return new Deferred().resolve(this.plainItems());
+                }
             },
             dataFields: function() {
                 return that._dataSource && that._dataSource.select();
