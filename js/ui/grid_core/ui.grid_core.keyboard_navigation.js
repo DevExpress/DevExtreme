152d58db926e9746e0a7dcfe8d002463ab1904f3
Igor Maltsev <igor_malzew@mail.ru>
2019-07-23 14:09:36 +0300

    DataGrid - added row should be scrolled to the top of the grid (T748046) (#8873)

--- js/ui/grid_core/ui.grid_core.keyboard_navigation.js
+++ js/ui/grid_core/ui.grid_core.keyboard_navigation.js
@@ -1701,6 +1701,10 @@ module.exports = {
                 init: function() {
                     this.callBase();
                     this._keyboardNavigationController = this.getController("keyboardNavigation");
+                },
+                _delayedInputFocus: function() {
+                    this._keyboardNavigationController._isNeedScroll = true;
+                    this.callBase.apply(this, arguments);
                 }
             },
             data: {
