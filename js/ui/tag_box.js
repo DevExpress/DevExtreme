2ab36ed0347d6e0affc9a9165f3c6f652159ba74
Dmitry Levkovskiy <DokaRus@users.noreply.github.com>
2019-07-25 10:53:34 +0300

    Fix LoadResult processing for CollectionWidget and TagBox (T800178) (#8924) (#8933)

--- js/ui/tag_box.js
+++ js/ui/tag_box.js
@@ -812,8 +812,13 @@ const TagBox = SelectBox.inherit({
             dataSource
                 .store()
                 .load({ filter, customQueryParams, expand })
-                .done(function(result) {
-                    const { data: items } = normalizeLoadResult(...arguments);
+                .done((data, extra) => {
+                    if(this._disposed) {
+                        d.reject();
+                        return;
+                    }
+
+                    const { data: items } = normalizeLoadResult(data, extra);
                     const mappedItems = dataSource._applyMapFunction(items);
                     d.resolve(mappedItems.filter(clientFilterFunction));
                 })
