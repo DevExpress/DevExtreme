012c32653348d80e37288b7c61793ee141398e38
Alexey Babich <alexey.babych@gmail.com>
2019-07-25 15:39:48 +0300

    Migrate to Gulp 4 (#8941)

--- build/gulp/style-compiler.js
+++ build/gulp/style-compiler.js
@@ -76,7 +76,7 @@ gulp.task('style-compiler-themes1', function(callback) {
 });
 
 gulp.task('style-compiler-themes-dev', function(callback) {
-    return gulp.watch("styles/**", ['style-compiler-themes']);
+    return gulp.watch("styles/**", gulp.series('style-compiler-themes'));
 });
 
 gulp.task('style-compiler-tb-assets', function(callback) {
