c3c4a75a751ec1a51ab2300bd3a01b3517e8d7dd
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2019-07-25 20:07:44 +0300

    TreeView, List - If the JAWS screen reader is used, keyboard navigation doesn't work (T747286) (#8811)
    * TreeView, List - If the JAWS screen reader is used, keyboard navigation doesn't work, and the reader doesn't pronounce the item text when a search bar is enabled (T747286)
    
    (cherry picked from commit ee84c2f6484efe3c5652525af183f3da252b3821)

--- testing/tests/DevExpress.ui.widgets/treeViewParts/accessibility.js
+++ testing/tests/DevExpress.ui.widgets/treeViewParts/accessibility.js
@@ -1,81 +1,113 @@
-/* global initTree */
+import TreeViewTestWrapper from "../../../helpers/TreeViewTestHelper.js";
 
+const createInstance = (options) => new TreeViewTestWrapper(options);
 const { module, test } = QUnit;
 
-module("aria accessibility", {
-    beforeEach: function() {
-        this.$element = initTree({
+class ariaAccessibilityTestHelper {
+    constructor(searchEnabled) {
+        this.treeView = createInstance({
             animationEnabled: false,
             items: [{ id: 1, text: "Item 1", expanded: true, items: [{ id: 3, text: "Item 11" }, { id: 4, text: "Item 12" }] }, { id: 2, text: "Item 2", expanded: false }],
             selectNodesRecursive: true,
-            showCheckBoxesMode: "normal"
+            showCheckBoxesMode: "normal",
+            searchEnabled: searchEnabled
+        });
+        this.element = this.treeView.instance;
+        this.$element = this.treeView.getElement();
+        this.$itemContainer = this.element._itemContainer();
+        this.clock = sinon.useFakeTimers();
+    }
+
+    checkAsserts(expectedValues) {
+        let { role, isActiveDescendant, tabIndex, $target } = expectedValues;
+
+        QUnit.assert.strictEqual($target.attr("role"), role, "role");
+        QUnit.assert.strictEqual(!!$target.attr("aria-activedescendant"), isActiveDescendant, "activedescendant");
+        QUnit.assert.strictEqual($target.attr("tabIndex"), tabIndex, "tabIndex");
+    }
+}
+
+module("aria accessibility", () => {
+    [true, false].forEach((searchEnabled) => {
+        QUnit.test(`aria role on initialize, searchEnabled: ${searchEnabled}`, () => {
+            let helper = new ariaAccessibilityTestHelper(searchEnabled);
+            const $itemContainer = helper.element._itemContainer(true);
+
+            helper.checkAsserts({ $target: searchEnabled ? $itemContainer : helper.$element, role: "tree", isActiveDescendant: true, tabIndex: '0' });
+            helper.checkAsserts({ $target: searchEnabled ? helper.$element : $itemContainer, role: undefined, isActiveDescendant: false, tabIndex: undefined });
         });
 
-        this.instance = this.$element.dxTreeView("instance");
-        this.clock = sinon.useFakeTimers();
-    },
+        QUnit.test(`aria role after initialize, searchEnabled: ${searchEnabled}`, () => {
+            let helper = new ariaAccessibilityTestHelper(searchEnabled);
+            helper.element.option("searchEnabled", !searchEnabled);
+            const $itemContainer = helper.element._itemContainer(true);
 
-    afterEach: function() {
-        this.$treeView = undefined;
-        this.instance = undefined;
-        this.clock.restore();
-    }
-}, () => {
-    test("aria expanded for items", function(assert) {
-        var $item1 = this.$element.find(".dx-treeview-node:eq(0)"),
-            $itemElements = this.instance.itemElements();
+            helper.checkAsserts({ $target: searchEnabled ? helper.$element : $itemContainer, role: "tree", isActiveDescendant: true, tabIndex: '0' });
+            helper.checkAsserts({ $target: searchEnabled ? $itemContainer : helper.$element, role: undefined, isActiveDescendant: false, tabIndex: undefined });
+        });
 
-        this.instance.collapseItem($itemElements[0]);
-        this.clock.tick(0);
+        test("aria expanded for items", function(assert) {
+            let helper = new ariaAccessibilityTestHelper(searchEnabled);
 
-        assert.equal($item1.attr("aria-expanded"), "false", "aria-expanded changing on item collapsing");
-    });
+            const $firstItem = helper.treeView.getNodes().eq(0);
 
-    test("aria selected for items via API", function(assert) {
-        var $item = this.$element.find(".dx-treeview-node:eq(0)"),
-            $itemElements = this.instance.itemElements();
+            helper.element.collapseItem(helper.element.itemElements()[0]);
+            helper.clock.tick(0);
 
-        this.instance.selectItem($itemElements[0]);
-        assert.equal($item.attr("aria-selected"), "true", "item is selected");
-    });
+            assert.equal($firstItem.attr("aria-expanded"), "false", "aria-expanded changing on item collapsing");
+        });
 
-    test("aria selected for items via UI", function(assert) {
-        var $nodes = this.$element.find(".dx-treeview-node"),
-            $checkboxes = this.$element.find(".dx-checkbox");
+        test("aria selected for items via API", function(assert) {
+            let helper = new ariaAccessibilityTestHelper(searchEnabled);
 
-        assert.equal($nodes.eq(0).attr("aria-selected"), "false", "item is unselected by default");
+            const $firstItem = helper.treeView.getNodes().eq(0);
 
-        $checkboxes.eq(0).trigger("dxclick");
-        assert.equal($nodes.eq(0).attr("aria-selected"), "true", "item is selected");
-        assert.equal($nodes.eq(1).attr("aria-selected"), "true", "item is selected");
+            helper.element.selectItem(helper.element.itemElements()[0]);
+            assert.equal($firstItem.attr("aria-selected"), "true", "item is selected");
+        });
 
-        $checkboxes.eq(1).trigger("dxclick");
-        assert.notOk($nodes.eq(0).attr("aria-selected"), "item is unselected");
-        assert.equal($nodes.eq(1).attr("aria-selected"), "false", "item is unselected");
-    });
+        test("aria selected for items via UI", function(assert) {
+            let helper = new ariaAccessibilityTestHelper(searchEnabled);
 
-    test("'Expanded' attr should be applied correctly when item was expanded on the second time", function(assert) {
-        var $item1 = this.$element.find(".dx-treeview-node:eq(0)"),
-            $itemElements = this.instance.itemElements();
+            let $nodes = helper.treeView.getNodes();
+            let $checkboxes = helper.treeView.getCheckBoxes();
 
-        this.instance.collapseItem($itemElements[0]);
-        this.clock.tick(0);
-        this.instance.expandItem($itemElements[0]);
-        this.clock.tick(0);
+            assert.equal($nodes.eq(0).attr("aria-selected"), "false", "item is unselected by default");
 
-        assert.equal($item1.attr("aria-expanded"), "true", "aria-expanded changing on item expanding");
-    });
+            $checkboxes.eq(0).trigger("dxclick");
+            assert.equal($nodes.eq(0).attr("aria-selected"), "true", "item is selected");
+            assert.equal($nodes.eq(1).attr("aria-selected"), "true", "item is selected");
 
-    test("'Expanded' attr should be applied correctly when item was expanded on the first time", function(assert) {
-        this.instance.option("items", [{ id: 1, text: "a", items: [{ id: 2, text: "b" }] }]);
+            $checkboxes.eq(1).trigger("dxclick");
+            assert.notOk($nodes.eq(0).attr("aria-selected"), "item is unselected");
+            assert.equal($nodes.eq(1).attr("aria-selected"), "false", "item is unselected");
+        });
 
-        var $item1 = this.$element.find(".dx-treeview-node:eq(0)"),
-            $itemElements = this.instance.itemElements();
+        test("'Expanded' attr should be applied correctly when item was expanded on the second time", function(assert) {
+            let helper = new ariaAccessibilityTestHelper(searchEnabled);
 
-        this.instance.expandItem($itemElements[0]);
-        this.clock.tick(0);
+            const $firstItem = helper.treeView.getNodes().eq(0);
+            const $itemElements = helper.element.itemElements();
 
-        assert.equal($item1.attr("aria-expanded"), "true", "aria-expanded changing on item expanding");
+            helper.element.collapseItem($itemElements[0]);
+            helper.clock.tick(0);
+            helper.element.expandItem($itemElements[0]);
+            helper.clock.tick(0);
+
+            assert.equal($firstItem.attr("aria-expanded"), "true", "aria-expanded changing on item expanding");
+        });
+
+        test("'Expanded' attr should be applied correctly when item was expanded on the first time", function(assert) {
+            let helper = new ariaAccessibilityTestHelper(searchEnabled);
+            helper.element.option("items", [{ id: 1, text: "a", items: [{ id: 2, text: "b" }] }]);
+
+            const $firstItem = helper.treeView.getNodes().eq(0);
+
+            helper.element.expandItem(helper.element.itemElements()[0]);
+            helper.clock.tick(0);
+
+            assert.equal($firstItem.attr("aria-expanded"), "true", "aria-expanded changing on item expanding");
+        });
     });
 });
 
