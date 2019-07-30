5d931d6437850806a58fd1763c174a9df3df6e54
babich-a <alexey.babych@gmail.com>
2019-07-30 09:24:43 +0300

    Make material themes

--- build/gulp/style-compiler.js
+++ build/gulp/style-compiler.js
@@ -1,11 +1,15 @@
 const gulp = require('gulp');
 const path = require('path');
+const rename = require('gulp-rename');
+const strip = require('gulp-strip-css-comments');
 const context = require('./context.js');
+const browsersList = require('../../package.json').browserslist;
+
 
 // less (new)
 const less = require('gulp-less');
 const LessAutoPrefix = require('less-plugin-autoprefix');
-const autoPrefix = new LessAutoPrefix({ browsers: ['last 2 versions'] }); // TODO read the right browsers list
+const autoPrefix = new LessAutoPrefix({ browsers: browsersList });
 
 
 function runStyleCompiler(command, params, callback) {
@@ -39,44 +43,45 @@ gulp.task('style-compiler-themes', function(callback) {
 
 const materialColors = [ 'blue', 'lime', 'orange', 'purple', 'teal' ];
 const materialLights = [ 'light', 'dark' ];
+const sizeSchemes = [ 'default', 'compact' ];
 const materialTasks = [];
 
 materialColors.forEach((color) => {
     materialLights.forEach((light) => {
-        const taskName = `material${color}${light}`;
-        materialTasks.push(taskName);
-        gulp.task(taskName, () => {
-            const cssArtifactsPath = path.join(process.cwd(), 'artifacts/css');
-            const lessSrcPath = path.join(process.cwd(), 'styles/bundles');
-
-            return gulp.src(path.join(lessSrcPath, 'material.less'))
-                .pipe(less({
-                    paths: [ path.join(process.cwd(), 'styles') ],
-                    plugins: [ autoPrefix ],
-                    modifyVars: {
-                        color,
-                        light
-                    }
-                }))
-                .pipe(gulp.dest(path.join(cssArtifactsPath, 'new', `dx.material.${color}.${light}.css`)));
+        sizeSchemes.forEach((size) => {
+            materialTasks.push({ color, light, size });
         });
     });
 });
 
-gulp.task('style-compiler-themes1', function(callback) {
-    // const cssArtifactsPath = path.join(process.cwd(), 'artifacts/css');
-    // const lessSrcPath = path.join(process.cwd(), 'styles/bundles');
+gulp.task('style-compiler-themes1', gulp.parallel(materialTasks.map((task) => {
+    const cssArtifactsPath = path.join(process.cwd(), 'artifacts/css');
+    const lessSrcPath = path.join(process.cwd(), 'styles/bundles');
+    const sizePostfix = task.size === 'default' ? '' : '.' + task.size;
+    const fileName = `dx.material.${task.color}.${task.light}${sizePostfix}.css`;
 
-    // return gulp.src(path.join(lessSrcPath, 'material.less'))
-    //     .pipe(less({
-    //         paths: [ path.join(process.cwd(), 'styles') ],
-    //         plugins: [ autoPrefix ]
-    //     }))
-    //     .pipe(gulp.dest(cssArtifactsPath));
-});
+    return function() {
+        return gulp
+            .src(path.join(lessSrcPath, 'material.less'))
+            .pipe(less({
+                paths: [ path.join(process.cwd(), 'styles') ],
+                plugins: [ autoPrefix ],
+                useFileCache: true,
+                modifyVars: task
+            }))
+            .pipe(strip())
+            .pipe(rename(fileName))
+            .pipe(gulp.dest(path.join(cssArtifactsPath, 'new'))); // TODO 'new' path changed
+    };
+    // TODO copy fonts and styles
+})));
 
 gulp.task('style-compiler-themes-dev', function(callback) {
     return gulp.watch("styles/**", gulp.series('style-compiler-themes'));
+});
+
+gulp.task('style-compiler-themes-dev1', function(callback) {
+    return gulp.watch("styles/**", gulp.series('style-compiler-themes1'));
 });
 
 gulp.task('style-compiler-tb-assets', function(callback) {
