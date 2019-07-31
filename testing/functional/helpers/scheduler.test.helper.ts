c4825e37ffacf7491e29390e1795fcd1e97ac9d3
Nara Lewinsky <33840321+naralewinsky@users.noreply.github.com>
2019-07-31 13:38:52 +0300

    dxScheduler – add TestCafe tests for DragAndDrop functionality (day view) (#8989)
    * Add TestCafe test for behaviour dragging between table rows in DayView-mode
    
    Appointments must be swapped in the Timeline
    
    * Add TestCafe test for behaviour dragging between table rows in DayView
    
    Refactoring code.
    
    * Add TestCafe test for behaviour dragging between table rows in DayView-mode
    
    Refactoring a code and the filesystem structure.
    
    * Add TestCafe test for behaviour dragging between table rows in DayView-mode
    
    Refactoring some code.
    
    * Add TestCafe test for behaviour dragging between table rows in DayView-mode
    
    Refactoring code.
    
    * Add TestCafe test for behaviour dragging between table rows in DayView-mode
    
    Refactoring code.
    
    * Add TestCafe test for behaviour dragging between table rows in DayView-mode
    
    Rename variables and change a code structure.
    
    * Add TestCafe test for behaviour dragging between table rows in DayView-mode
    
    Rename variables.
    
    * Appointments should be replaced on the timeline in DayView mode with maintaining their size and duration
    
    * Appointments should be replaced on the timeline in DayView-mode
    
    * Appointments should be replaced on the timeline in DayView mode with maintaining their size and duration
    
    * Appointments should be replaced on the timeline in DayView mode with maintaining their size and duration
    
    Fix 'expect(await...' problem.
    
    * Appointments should be replaced on the timeline in DayView mode
    
    Adding variable types for a wrapper class
    
    * ppointments should be replaced on the timeline in DayView mode with maintaining their size and duration
    
    Final.
    
    * Adding the index argument for the getAppointmentByTitle and remove the 'useColorAsDefault: true' from a createWidget.

--- testing/functional/helpers/scheduler.test.helper.ts
+++ testing/functional/helpers/scheduler.test.helper.ts
@@ -11,6 +11,30 @@ export default class SchedulerTestHelper {
         return this.scheduler.find(`.dx-scheduler-appointment`).nth(index);
     }
 
+    getAppointmentByTitle(title, index = 0) {
+        return this.scheduler.find(`.dx-scheduler-appointment`).withAttribute('title', title).nth(index);
+    }
+
+    getAppointmentBeginTime(appointment) {
+        return appointment.find('.dx-scheduler-appointment-content-date').nth(0).innerText;
+    }
+
+    getAppointmentFinalTime(appointment) {
+        return appointment.find('.dx-scheduler-appointment-content-date').nth(2).innerText;
+    }
+
+    getAppointmentHeight(appointment) {
+        return appointment.getStyleProperty('height');
+    }
+
+    getDateTableRow(rowIndex = 0) {
+        return this.scheduler.find(`.dx-scheduler-date-table-row`).nth(rowIndex);
+    }
+
+    getDateTableCell(rowIndex = 0, cellIndex = 0) {
+        return this.getDateTableRow(rowIndex).find(`.dx-scheduler-date-table-cell`).nth(cellIndex);
+    }
+
     isTooltipVisible() {
         return Selector(".dx-scheduler-appointment-tooltip-wrapper.dx-overlay-wrapper .dx-list").exists;
     }
