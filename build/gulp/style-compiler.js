b5fa61f8f7121691695d76eb91c6e672f50c9c10
babich-a <alexey.babych@gmail.com>
2019-08-02 12:04:17 +0300

    Full-working gulp tasks

--- build/gulp/style-compiler.js
+++ build/gulp/style-compiler.js
@@ -1,9 +1,12 @@
 const gulp = require('gulp');
 const path = require('path');
 const fs = require('fs');
-const strip = require('gulp-strip-css-comments');
+const replace = require('gulp-replace');
 const context = require('./context.js');
 const browsersList = require('../../package.json').browserslist;
+const starLicense = require('./header-pipes').starLicense;
+
+const cssArtifactsPath = path.join(process.cwd(), 'artifacts/css/new'); // TODO 'new' path changed
 
 
 // less (new)
@@ -30,8 +33,6 @@ function runStyleCompiler(command, params, callback) {
 }
 
 gulp.task('style-compiler-themes', function(callback) {
-    var cssArtifactsPath = path.join(process.cwd(), 'artifacts/css');
-
     runStyleCompiler(
         'themes', [
             '--version=' + context.version.product,
@@ -42,7 +43,6 @@ gulp.task('style-compiler-themes', function(callback) {
 });
 
 const compileBundle = (src) => {
-    const cssArtifactsPath = path.join(process.cwd(), 'artifacts/css');
     return gulp
         .src(src)
         .pipe(less({
@@ -50,8 +50,9 @@ const compileBundle = (src) => {
             plugins: [ autoPrefix ],
             useFileCache: true
         }))
-        .pipe(strip())
-        .pipe(gulp.dest(path.join(cssArtifactsPath, 'new'))); // TODO 'new' path changed
+        .pipe(replace(/\s*\/\*[\S\s]*?\*\//g, ''))
+        .pipe(starLicense())
+        .pipe(gulp.dest(cssArtifactsPath));
 };
 
 const getBundleTasks = (bundleGroup) => {
@@ -65,29 +66,38 @@ const getBundleTasks = (bundleGroup) => {
 gulp.task('style-compiler-material', gulp.parallel(getBundleTasks('material')));
 gulp.task('style-compiler-generic', gulp.parallel(getBundleTasks('generic')));
 gulp.task('style-compiler-ios7', gulp.parallel(getBundleTasks('ios7')));
+gulp.task('style-compiler-common', gulp.parallel(getBundleTasks('common')));
+
+gulp.task('copy-fonts-and-icons', () => {
+    return gulp
+        .src(['fonts/**/*', 'icons/**/*'], { base: '.' })
+        .pipe(gulp.dest(cssArtifactsPath));
+});
 
 gulp.task('style-compiler-themes1',
     gulp.parallel(
         'style-compiler-generic',
         'style-compiler-material',
-        'style-compiler-ios7')
-    // TODO copy fonts and styles
+        'style-compiler-ios7',
+        'style-compiler-common',
+        'copy-fonts-and-icons')
 );
 
 gulp.task('style-compiler-themes-dev', function(callback) {
-    return gulp.watch("styles/**", gulp.series('style-compiler-themes'));
+    return gulp.watch('styles/**', gulp.series('style-compiler-themes'));
 });
 
 gulp.task('style-compiler-themes-dev1', gulp.parallel(() => {
     const commonSources = [
-        "styles/widgets/base/**",
-        "styles/mixins.less",
-        "styles/theme.less"
+        'styles/widgets/base/**',
+        'styles/mixins.less',
+        'styles/theme.less'
     ];
 
-    gulp.watch(["styles/widgets/generic/**", "styles/bundles/generic/**"].concat(commonSources), gulp.series('style-compiler-generic'));
-    gulp.watch(["styles/widgets/material/**", "styles/bundles/material/**"].concat(commonSources), gulp.series('style-compiler-material'));
-    gulp.watch(["styles/widgets/ios7/**", "styles/bundles/ios7/**"].concat(commonSources), gulp.series('style-compiler-ios7'));
+    gulp.watch(['styles/widgets/generic/**', 'styles/bundles/generic/**'].concat(commonSources), gulp.series('style-compiler-generic'));
+    gulp.watch(['styles/widgets/material/**', 'styles/bundles/material/**'].concat(commonSources), gulp.series('style-compiler-material'));
+    gulp.watch(['styles/widgets/ios7/**', 'styles/bundles/ios7/**'].concat(commonSources), gulp.series('style-compiler-ios7'));
+    gulp.watch(['styles/widgets/common/**', 'styles/bundles/common/**', 'styles/mixins.less', 'styles/widgets/ui.less'], gulp.series('style-compiler-common'));
 }));
 
 gulp.task('style-compiler-tb-assets', function(callback) {
