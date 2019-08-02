2bd7352ed5274f24fcaaee944146d735e812f8db
babich-a <alexey.babych@gmail.com>
2019-08-02 14:45:14 +0300

    Turn gulp tasks on

--- build/gulp/style-compiler.js
+++ build/gulp/style-compiler.js
@@ -2,45 +2,15 @@ const gulp = require('gulp');
 const path = require('path');
 const fs = require('fs');
 const replace = require('gulp-replace');
+const less = require('gulp-less');
+const LessAutoPrefix = require('less-plugin-autoprefix');
+
 const context = require('./context.js');
 const browsersList = require('../../package.json').browserslist;
 const starLicense = require('./header-pipes').starLicense;
-
-const cssArtifactsPath = path.join(process.cwd(), 'artifacts/css/new'); // TODO 'new' path changed
-
-
-// less (new)
-const less = require('gulp-less');
-const LessAutoPrefix = require('less-plugin-autoprefix');
 const autoPrefix = new LessAutoPrefix({ browsers: browsersList });
 
-
-function runStyleCompiler(command, params, callback) {
-    var spawn = require('child_process').spawn;
-    var process = spawn(
-        'dotnet',
-        ['build/style-compiler/bin/style-compiler.dll', command].concat(params),
-        { stdio: 'inherit' }
-    );
-
-    process.on('exit', function(code) {
-        if(code === 0) {
-            callback();
-        } else {
-            callback('Style compiler failed');
-        }
-    });
-}
-
-gulp.task('style-compiler-themes', function(callback) {
-    runStyleCompiler(
-        'themes', [
-            '--version=' + context.version.product,
-            '--output-path=' + cssArtifactsPath
-        ],
-        callback
-    );
-});
+const cssArtifactsPath = path.join(process.cwd(), 'artifacts', 'css');
 
 const compileBundle = (src) => {
     return gulp
@@ -74,7 +44,7 @@ gulp.task('copy-fonts-and-icons', () => {
         .pipe(gulp.dest(cssArtifactsPath));
 });
 
-gulp.task('style-compiler-themes1',
+gulp.task('style-compiler-themes',
     gulp.parallel(
         'style-compiler-generic',
         'style-compiler-material',
@@ -83,11 +53,7 @@ gulp.task('style-compiler-themes1',
         'copy-fonts-and-icons')
 );
 
-gulp.task('style-compiler-themes-dev', function(callback) {
-    return gulp.watch('styles/**', gulp.series('style-compiler-themes'));
-});
-
-gulp.task('style-compiler-themes-dev1', gulp.parallel(() => {
+gulp.task('style-compiler-themes-dev', gulp.parallel(() => {
     const commonSources = [
         'styles/widgets/base/**',
         'styles/mixins.less',
@@ -98,7 +64,26 @@ gulp.task('style-compiler-themes-dev1', gulp.parallel(() => {
     gulp.watch(['styles/widgets/material/**', 'styles/bundles/material/**'].concat(commonSources), gulp.series('style-compiler-material'));
     gulp.watch(['styles/widgets/ios7/**', 'styles/bundles/ios7/**'].concat(commonSources), gulp.series('style-compiler-ios7'));
     gulp.watch(['styles/widgets/common/**', 'styles/bundles/common/**', 'styles/mixins.less', 'styles/widgets/ui.less'], gulp.series('style-compiler-common'));
+    gulp.watch(['fonts/**', 'icons/**'], gulp.series('copy-fonts-and-icons'));
 }));
+
+
+function runStyleCompiler(command, params, callback) {
+    var spawn = require('child_process').spawn;
+    var process = spawn(
+        'dotnet',
+        ['build/style-compiler/bin/style-compiler.dll', command].concat(params),
+        { stdio: 'inherit' }
+    );
+
+    process.on('exit', function(code) {
+        if(code === 0) {
+            callback();
+        } else {
+            callback('Style compiler failed');
+        }
+    });
+}
 
 gulp.task('style-compiler-tb-assets', function(callback) {
     var assetsPath = path.join(process.cwd(), 'themebuilder');
