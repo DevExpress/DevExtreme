9d167959ddbe1b9f80deca62d4d7c0acc2ae38da
Andrew Makarov <andrey.makarov@devexpress.com>
2019-08-07 11:35:17 +0300

    Add test

--- testing/tests/DevExpress.ui.widgets.scheduler/integration.appointmentTooltip.tests.js
+++ testing/tests/DevExpress.ui.widgets.scheduler/integration.appointmentTooltip.tests.js
@@ -67,6 +67,98 @@ QUnit.module("Integration: Appointment tooltip", {
     }
 });
 
+QUnit.test("After change view type, tooltip should be appear after click on appointment, group mode(T802158)", function(assert) {
+    const data = [
+        {
+            text: "Website Re-Design Plan",
+            priorityId: 2,
+            startDate: new Date(2018, 4, 21, 9, 30),
+            endDate: new Date(2018, 4, 21, 11, 30)
+        }, {
+            text: "Book Flights to San Fran for Sales Trip",
+            priorityId: 1,
+            startDate: new Date(2018, 4, 24, 10, 0),
+            endDate: new Date(2018, 4, 24, 12, 0),
+        }, {
+            text: "Install New Router in Dev Room",
+            priorityId: 1,
+            startDate: new Date(2018, 4, 21, 13),
+            endDate: new Date(2018, 4, 21, 15, 30)
+        }, {
+            text: "Approve Personal Computer Upgrade Plan",
+            priorityId: 2,
+            startDate: new Date(2018, 4, 22, 10, 0),
+            endDate: new Date(2018, 4, 22, 11, 0)
+        }, {
+            text: "Final Budget Review",
+            priorityId: 2,
+            startDate: new Date(2018, 4, 22, 12, 0),
+            endDate: new Date(2018, 4, 22, 13, 35)
+        }, {
+            text: "New Brochures",
+            priorityId: 2,
+            startDate: new Date(2018, 4, 21, 13, 0),
+            endDate: new Date(2018, 4, 21, 15, 15)
+        }, {
+            text: "Install New Database",
+            priorityId: 1,
+            startDate: new Date(2018, 4, 22, 9),
+            endDate: new Date(2018, 4, 22, 12, 15)
+        }, {
+            text: "Approve New Online Marketing Strategy",
+            priorityId: 2,
+            startDate: new Date(2018, 4, 23, 12, 0),
+            endDate: new Date(2018, 4, 23, 14, 0)
+        }, {
+            text: "Upgrade Personal Computers",
+            priorityId: 1,
+            startDate: new Date(2018, 4, 21, 9),
+            endDate: new Date(2018, 4, 21, 11, 30)
+        }
+    ];
+
+    const priorityData = [
+        {
+            text: "Low Priority",
+            id: 1,
+            color: "#1e90ff"
+        }, {
+            text: "High Priority",
+            id: 2,
+            color: "#ff9747"
+        }
+    ];
+
+    const defaultViews = ["week", "agenda", "month"];
+
+    this.createInstance({
+        dataSource: data,
+        views: defaultViews,
+        currentView: defaultViews[0],
+        crossScrollingEnabled: true,
+        currentDate: new Date(2018, 4, 21),
+        startDayHour: 9,
+        endDayHour: 16,
+        width: 800,
+        height: 600,
+        groups: ["priorityId"],
+        resources: [
+            {
+                fieldExpr: "priorityId",
+                allowMultiple: false,
+                dataSource: priorityData,
+                label: "Priority"
+            }
+        ]
+    });
+
+    defaultViews.forEach(view => {
+        this.scheduler.option("currentView", view);
+        this.scheduler.appointments.click();
+        assert.ok(this.scheduler.tooltip.isVisible(), `tooltip should be visible after click on item in ${view} view`);
+    });
+});
+
 QUnit.test("There is no need to check recurring appointment if editing.allowUpdating is false", function(assert) {
     this.createInstance({
         editing: {
