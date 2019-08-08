a8c67727bde06b168a685b35c6f0262404a7b333
Alexander Tkachenko <24324790+tar-z@users.noreply.github.com>
2019-08-08 15:57:54 +0300

    Update VizRange docs

--- js/viz/docs/docBaseWidget.js
+++ js/viz/docs/docBaseWidget.js
@@ -630,12 +630,14 @@ var VizRange = {
     * @name VizRange.startValue
     * @type number|date|string
     * @default undefined
+    * @fires BaseWidgetOptions.onOptionChanged
     */
     startValue: undefined,
     /**
     * @name VizRange.endValue
     * @type number|date|string
     * @default undefined
+    * @fires BaseWidgetOptions.onOptionChanged
     */
     endValue: undefined,
     /**
