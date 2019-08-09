bdc04bf0ced1bc2b0a7e9780a0b73adb486d6473
babich-a <alexey.babych@gmail.com>
2019-08-09 09:56:13 +0300

    Changes for DEVEXTREME_DOCKER_CI

--- gulpfile.js
+++ gulpfile.js
@@ -21,38 +21,32 @@ require('./build/gulp/ts');
 require('./build/gulp/localization');
 require('./build/gulp/style-compiler');
 
-gulp.task('parallel-build', (callback) => {
-    return multiProcess(['js-bundles-debug', 'js-bundles-prod', 'style-compiler-themes'], callback, true);
-});
-
+let styleTasks = ['style-compiler-themes', 'style-compiler-tb-assets'];
+let commonTasks = ['vectormap', 'aspnet', 'vendor', 'ts'];
+let parallelTasks = ['js-bundles-debug', 'js-bundles-prod', 'style-processing', 'common'];
 
 if(process.env['DEVEXTREME_QUNIT_CI']) {
     console.warn("Using QUnit CI mode for gulp default!");
-    gulp.task('default', gulp.series(
-        'clean',
-        'localization',
-        gulp.parallel([
-            'js-bundles-debug',
-            'vectormap',
-            'vendor'
-        ]),
-        'style-compiler-themes'
-    ));
-} else {
-    gulp.task('default', gulp.series(
-        'clean',
-        'localization',
-        'parallel-build',
-        gulp.parallel([
-            'vectormap',
-            'aspnet',
-            'vendor',
-            'ts'
-        ]),
-        'style-compiler-tb-assets',
-        'npm',
-        'themebuilder-npm'
-    ));
+    commonTasks = ['vectormap', 'vendor'];
+    styleTasks = ['style-compiler-themes'];
+    parallelTasks = ['js-bundles-debug', 'style-processing', 'common'];
 }
+
+gulp.task('common', gulp.parallel.apply(gulp, commonTasks));
+gulp.task('style-processing', gulp.parallel.apply(gulp, styleTasks));
+
+gulp.task('parallel-build',
+    process.env['DEVEXTREME_DOCKER_CI'] ?
+        gulp.parallel.apply(gulp, parallelTasks) :
+        (callback) => multiProcess(parallelTasks, callback, true));
+
+
+gulp.task('default', gulp.series(
+    'clean',
+    'localization',
+    'parallel-build',
+    'npm',
+    'themebuilder-npm'
+));
 
 gulp.task('dev', gulp.parallel('bundler-config-dev', 'js-bundles-dev', 'style-compiler-themes-dev'));
