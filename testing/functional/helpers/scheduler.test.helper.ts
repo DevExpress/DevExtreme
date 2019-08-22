2292e9d11e0b1db73999626babccd7640a7d2eb4
Nara Lewinsky <naralewinskee@gmail.com>
2019-08-22 11:29:55 +0300

    dxScheduler. TestCafe. Refactoring helper and tests

--- testing/functional/helpers/scheduler.test.helper.ts
+++ testing/functional/helpers/scheduler.test.helper.ts
@@ -36,51 +36,54 @@ export default class SchedulerTestHelper {
         };
     }
 
-    getDateTableRowElement(rowIndex = 0) {
-        return this.scheduler.find(`.dx-scheduler-date-table-row`).nth(rowIndex);
+    getDateTableRow(index = 0) {
+        return this.scheduler
+                   .find(`.dx-scheduler-date-table-row`)
+                   .nth(index);
     }
 
-    getDateTableCellElement(rowIndex = 0, cellIndex = 0) {
-        return this.getDateTableRowElement(rowIndex).find(`.dx-scheduler-date-table-cell`).nth(cellIndex);
+    getDateTableRowCell(rowIndex = 0, cellIndex = 0) {
+        return this.getDateTableRow(rowIndex)
+                   .find(`.dx-scheduler-date-table-cell`)
+                   .nth(cellIndex);
     }
 
-    getHeaderPanelCellElement(index = 0) {
-        return this.scheduler.find(`.dx-scheduler-header-panel-cell`).nth(index);
+    getAppointment(title, index = 0) {
+        return this.scheduler
+                   .find(`.dx-scheduler-appointment`)
+                   .withAttribute('title', title)
+                   .nth(index);
     }
 
-    getHeaderPanelCellElementByTitle(title, index = 0) {
-        return this.scheduler.find(`.dx-scheduler-header-panel-cell`).withAttribute('title', title).nth(index)
-    }
-
-    getAppointmentElement(index = 0) {
-        return this.scheduler.find(`.dx-scheduler-appointment`).nth(index);
-    }
-
-    getAppointmentElementByTitle(title, index = 0) {
-        return this.scheduler.find(`.dx-scheduler-appointment`).withAttribute('title', title).nth(index);
-    }
-
-    getAppointmentResizableHandleElement(appointment) {
+    getAppointmenttDate(appointment) {
+        const appointmentContentDate = appointment.find('.dx-scheduler-appointment-content-date');
         return {
-            left: appointment.find('.dx-resizable-handle-left'),
-            right: appointment.find('.dx-resizable-handle-right'),
-            top: appointment.find('.dx-resizable-handle-top'),
-            bottom: appointment.find('.dx-resizable-handle-bottom')
+            startTime:
+                appointmentContentDate.nth(0).innerText,
+            endTime:
+                appointmentContentDate.nth(2).innerText
         }
     }
 
-    getAppointmentElementTime(appointment) {
-        const appointmentContentDateElement = appointment.find('.dx-scheduler-appointment-content-date');
+    getAppointmentResizableHandle(appointment) {
         return {
-            startTime: appointmentContentDateElement.nth(0).innerText,
-            endTime: appointmentContentDateElement.nth(2).innerText
+            left:
+                appointment.find('.dx-resizable-handle-left'),
+            right:
+                appointment.find('.dx-resizable-handle-right'),
+            top:
+                appointment.find('.dx-resizable-handle-top'),
+            bottom:
+                appointment.find('.dx-resizable-handle-bottom')
         }
     }
 
-    getAppointmentElementSize(appointment) {
+    getAppointmentSize(appointment) {
         return {
-            height: appointment.getStyleProperty('height'),
-            width: appointment.getStyleProperty('width')
+            width:
+                appointment.getStyleProperty('width'),
+            height:
+                appointment.getStyleProperty('height')
         }
     }
 
