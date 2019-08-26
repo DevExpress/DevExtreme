e2df8a38be12c10ae0ace5efa5fcccddf071efaa
Alexey Kamyshin <aurrius.the.badger@gmail.com>
2019-08-26 10:28:16 +0300

    Fix position of appointment tail if it start previous week (#9409)

--- js/ui/scheduler/workspaces/ui.scheduler.work_space.js
+++ js/ui/scheduler/workspaces/ui.scheduler.work_space.js
@@ -2159,7 +2159,13 @@ var SchedulerWorkSpace = Widget.inherit({
 
         currentDayStart.setHours(this.option("startDayHour"), 0, 0, 0);
 
-        return ((date.getTime() - currentDayStart.getTime()) % cellDuration) / cellDuration;
+        let currentDateTime = date.getTime(),
+            currentDayStartTime = currentDayStart.getTime(),
+            minTime = this._firstViewDate.getTime();
+
+        return (currentDateTime > minTime)
+            ? ((currentDateTime - currentDayStartTime) % cellDuration) / cellDuration
+            : 0;
     },
 
     getCoordinatesByDateInGroup: function(date, appointmentResources, inAllDayRow) {
