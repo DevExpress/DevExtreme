4914480291958b3de89e3dc5cce4acfd5e87533f
Eugeniya Filippova <eugeniya.filippova@devexpress.com>
2019-08-26 09:12:27 +0300

    dxDiagram - Improve UI for shape image modification

--- js/ui/diagram/ui.diagram.dialogmanager.js
+++ js/ui/diagram/ui.diagram.dialogmanager.js
@@ -11,27 +11,33 @@ const DiagramDialogManager = {
         return this.dialogList ||
             (this.dialogList = [
                 {
-                    command: DiagramCommand.ChangeShapeImage,
-                    title: messageLocalization.format("dxDiagram-dialogChangeShapeImageTitle"),
-                    onGetContent: function(args) {
-                        const $uploader = $("<div>");
-                        args.component._createComponent($uploader, FileUploader, {
-                            selectButtonText: messageLocalization.format("dxDiagram-dialogChangeShapeImageSelectButton"),
-                            accept: "image/*",
-                            uploadMode: "useForm",
-                            onValueChanged: function(e) {
-                                const window = getWindow();
-                                var reader = new window.FileReader();
-                                reader.onload = function(e) {
-                                    args.component._commandParameter = e.target.result;
-                                };
-                                reader.readAsDataURL(e.value[0]);
-                            }
-                        });
-                        return $uploader;
-                    }
+                    command: DiagramCommand.InsertShapeImage,
+                    title: messageLocalization.format("dxDiagram-dialogInsertShapeImageTitle"),
+                    onGetContent: this.getChangeImageDialogContent
+                },
+                {
+                    command: DiagramCommand.EditShapeImage,
+                    title: messageLocalization.format("dxDiagram-dialogEditShapeImageTitle"),
+                    onGetContent: this.getChangeImageDialogContent
                 }
             ]);
+    },
+    getChangeImageDialogContent: function(args) {
+        const $uploader = $("<div>");
+        args.component._createComponent($uploader, FileUploader, {
+            selectButtonText: messageLocalization.format("dxDiagram-dialogEditShapeImageSelectButton"),
+            accept: "image/*",
+            uploadMode: "useForm",
+            onValueChanged: function(e) {
+                const window = getWindow();
+                var reader = new window.FileReader();
+                reader.onload = function(e) {
+                    args.component._commandParameter = e.target.result;
+                };
+                reader.readAsDataURL(e.value[0]);
+            }
+        });
+        return $uploader;
     },
     getDialogParameters(command) {
         var commandIndex = this.getConfigurations().map(c => c.command).indexOf(command);
