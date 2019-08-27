efbd312a5f5a090ea48c63bb83d11d319ec1db87
Jaan Toming <jtoming@gmail.com>
2019-08-27 14:26:10 +0300

    Make test easier

--- testing/tests/DevExpress.ui.widgets.dataGrid/dataGrid.tests.js
+++ testing/tests/DevExpress.ui.widgets.dataGrid/dataGrid.tests.js
@@ -7346,9 +7346,7 @@ QUnit.test("Toolbar should not be rerendered if editing.popup options were chang
         dataGrid = createDataGrid({
             loadingTimeout: undefined,
             dataSource: [],
-            onToolbarPreparing: function(e) {
-                onToolbarPreparingSpy();
-            },
+            onToolbarPreparing: onToolbarPreparingSpy,
             editing: {
                 mode: "popup"
             }
