DevExpress/DevExtreme

commit 95157a0ca98833380d9b9eea6e66da88fc077cf0

Improve polar chart axes layout (T808782, T717792, T808776) (#9505)
* Improve polar chart axes layout (T808782, T717792, T808776)

* remove extra line

js/viz/axes/base_axis.js
@@ -145,12 +145,11 @@ function updateTicksPosition(ticks, options, animate) {
 function updateGridsPosition(ticks, animate) {
     callAction(ticks, "updateGridPosition", animate);
 }
-
-function measureLabels(items) {
+const measureLabels = exports.measureLabels = function(items) {
     items.forEach(function(item) {
         item.labelBBox = item.label ? item.label.getBBox() : { x: 0, y: 0, width: 0, height: 0 };
     });
-}
+};
 
 function cleanUpInvalidTicks(ticks) {
     var i = ticks.length - 1;
@@ -263,6 +262,30 @@ function convertVisualRangeObject(visualRange, optionValue) {
 function getConstantLineSharpDirection(coord, axisCanvas) {
     return Math.max(axisCanvas.start, axisCanvas.end) !== coord ? 1 : -1;
 }
+
+const calculateCanvasMargins = exports.calculateCanvasMargins = function(bBoxes, canvas) {
+    const cLeft = canvas.left;
+    const cTop = canvas.top;
+    const cRight = canvas.width - canvas.right;
+    const cBottom = canvas.height - canvas.bottom;
+    return bBoxes
+        .reduce(function(margins, bBox) {
+            if(!bBox || bBox.isEmpty) {
+                return margins;
+            }
+            return {
+                left: _max(margins.left, cLeft - bBox.x),
+                top: _max(margins.top, cTop - bBox.y),
+                right: _max(margins.right, bBox.x + bBox.width - cRight),
+                bottom: _max(margins.bottom, bBox.y + bBox.height - cBottom)
+            };
+        }, {
+            left: 0,
+            right: 0,
+            top: 0,
+            bottom: 0
+        });
+};
 
 const Axis = exports.Axis = function(renderSettings) {
     var that = this;
@@ -517,6 +540,15 @@ Axis.prototype = {
         });
     },
 
+    _adjustLabelsCoord(offset, maxWidth, checkCanvas) {
+        const that = this;
+        that._majorTicks.forEach(function(tick) {
+            if(tick.label) {
+                tick.label.attr(that._getLabelAdjustedCoord(tick, offset + (tick.labelOffset || 0), maxWidth, checkCanvas));
+            }
+        });
+    },
+
     _adjustLabels: function(offset) {
         var that = this,
             maxSize = that._majorTicks.reduce(function(size, tick) {
@@ -530,11 +562,7 @@ Axis.prototype = {
             }, {}),
             additionalOffset = that._isHorizontal ? maxSize.height : maxSize.width;
 
-        that._majorTicks.forEach(function(tick) {
-            if(tick.label) {
-                tick.label.attr(that._getLabelAdjustedCoord(tick, offset + (tick.labelOffset || 0), maxSize.width));
-            }
-        });
+        that._adjustLabelsCoord(offset, maxSize.width);
 
         return offset + additionalOffset + (additionalOffset && that._options.label.indentFromAxis) + maxSize.offset;
     },
@@ -757,24 +785,9 @@ Axis.prototype = {
                         box.height = cBottom - cTop;
                     }
                     return box;
-                })(that._axisTitleGroup)),
-            margins = boxes
-                .reduce(function(margins, bBox) {
-                    if(!bBox || bBox.isEmpty) {
-                        return margins;
-                    }
-                    return {
-                        left: _max(margins.left, cLeft - bBox.x),
-                        top: _max(margins.top, cTop - bBox.y),
-                        right: _max(margins.right, bBox.x + bBox.width - cRight),
-                        bottom: _max(margins.bottom, bBox.y + bBox.height - cBottom)
-                    };
-                }, {
-                    left: 0,
-                    right: 0,
-                    top: 0,
-                    bottom: 0
-                });
+                })(that._axisTitleGroup));
+
+        const margins = calculateCanvasMargins(boxes, canvas);
 
         margins[position] += options.crosshairMargin;
 
@@ -1948,7 +1961,9 @@ Axis.prototype = {
         cleanUpInvalidTicks(that._minorTicks);
         cleanUpInvalidTicks(that._boundaryTicks);
 
-        that._updateAxisElementPosition();
+        if(this._axisElement) {
+            that._updateAxisElementPosition();
+        }
 
         updateTicksPosition(that._majorTicks, options.tick, animationEnabled);
         updateTicksPosition(that._minorTicks, options.minorTick, animationEnabled);
@@ -2436,9 +2451,7 @@ Axis.prototype = {
         if(step > 1 && boxes.some(checkLabels)) {
             that._applyLabelMode(mode, step, boxes, behavior);
         }
-        if(mode === "hide") {
-            that._checkBoundedLabelsOverlapping(step, majorTicks, boxes);
-        }
+        that._checkBoundedLabelsOverlapping(majorTicks, boxes, mode);
     },
 
     _applyLabelMode: function(mode, step, boxes, behavior, notRecastStep) {

js/viz/axes/polar_axes.js
@@ -7,6 +7,7 @@ var vizUtils = require("../core/utils"),
     tick = require("./tick").tick,
     polarAxes,
     _map = vizUtils.map,
+    baseAxisModule = require("./base_axis"),
 
     _math = Math,
     _abs = _math.abs,
@@ -139,6 +140,37 @@ circularAxes = polarAxes.circular = {
         }
     },
 
+    getMargins() {
+        const tickOptions = this._options.tick;
+        const tickOuterLength = Math.max(tickOptions.visible ? tickOptions.length / 2 + tickOptions.shift : 0, 0);
+        const radius = this.getRadius();
+        const { x, y } = this._center;
+        const labelBoxes = this._majorTicks.map(t => t.label && t.label.getBBox()).filter(b => b);
+
+        const canvas = extend({}, this._canvas, {
+            left: x - radius,
+            top: y - radius,
+            right: this._canvas.width - (x + radius),
+            bottom: this._canvas.height - (y + radius)
+        });
+
+        const margins = baseAxisModule.calculateCanvasMargins(labelBoxes, canvas);
+        Object.keys(margins).forEach(k => margins[k] = (margins[k] < tickOuterLength ? tickOuterLength : margins[k]));
+
+        return margins;
+    },
+
+    updateSize() {
+        const that = this;
+
+        baseAxisModule.Axis.prototype.updateSize.apply(that, arguments);
+
+        baseAxisModule.measureLabels(that._majorTicks);
+        that._adjustLabelsCoord(0, 0, true);
+
+        this._checkBoundedLabelsOverlapping(this._majorTicks, this._majorTicks.map(t=>t.labelBBox));
+    },
+
     _setVisualRange: _noop,
 
     allowToExtendVisualRange(isEnd) {
@@ -246,7 +278,7 @@ circularAxes = polarAxes.circular = {
         ];
     },
 
-    _getLabelAdjustedCoord: function(tick) {
+    _getLabelAdjustedCoord: function(tick, _offset, _maxWidth, checkCanvas) {
         var that = this,
             labelCoords = tick.labelCoords,
             labelY = labelCoords.y,
@@ -261,26 +293,54 @@ circularAxes = polarAxes.circular = {
             x = labelCoords.x + indentFromAxis * cos,
             y = labelY + (labelY - box.y - halfHeight) + indentFromAxis * sin;
 
+        let shiftX = 0;
+        let shiftY = 0;
+
         switch(getPolarQuarter(labelAngle)) {
             case 1:
-                x += halfWidth;
-                y += halfHeight * sin;
+                shiftX = halfWidth;
+                shiftY = halfHeight * sin;
                 break;
             case 2:
-                x += halfWidth * cos;
-                y += halfHeight;
+                shiftX = halfWidth * cos;
+                shiftY = halfHeight;
                 break;
             case 3:
-                x += -halfWidth;
-                y += halfHeight * sin;
+                shiftX = -halfWidth;
+                shiftY = halfHeight * sin;
                 break;
             case 4:
-                x += halfWidth * cos;
-                y += -halfHeight;
+                shiftX = halfWidth * cos;
+                shiftY = -halfHeight;
                 break;
         }
 
-        return { x: x, y: y };
+        if(checkCanvas) {
+            const canvas = that._canvas;
+            const boxShiftX = (x - labelCoords.x) + shiftX;
+            const boxShiftY = (y - labelCoords.y) + shiftY;
+
+            if(box.x + boxShiftX < canvas.originalLeft) {
+                shiftX -= box.x + boxShiftX - canvas.originalLeft;
+            }
+
+            if(box.x + box.width + boxShiftX > canvas.width - canvas.originalRight) {
+                shiftX -= box.x + box.width + boxShiftX - (canvas.width - canvas.originalRight);
+            }
+
+            if(box.y + boxShiftY < canvas.originalTop) {
+                shiftY -= box.y + boxShiftY - canvas.originalTop;
+            }
+
+            if(box.y + box.height + boxShiftY > canvas.height - canvas.originalBottom) {
+                shiftY -= box.y + box.height + boxShiftY - (canvas.height - canvas.originalBottom);
+            }
+        }
+
+        return {
+            x: x + shiftX,
+            y: y + shiftY
+        };
     },
 
     _getGridLineDrawer: function() {
@@ -355,9 +415,14 @@ circularAxes = polarAxes.circular = {
         return constants.getTicksCountInRange(that._majorTicks, "angle", _math.max(angle1, angle2));
     },
 
-    _checkBoundedLabelsOverlapping: function(step, majorTicks, boxes) {
-        var lastVisibleLabelIndex = _math.floor((boxes.length - 1) / step) * step,
-            labelOpt = this._options.label;
+    _checkBoundedLabelsOverlapping: function(majorTicks, boxes, mode) {
+        const labelOpt = this._options.label;
+        mode = mode || this._validateOverlappingMode(labelOpt.overlappingBehavior);
+        if(mode !== "hide") {
+            return;
+        }
+        const lastVisibleLabelIndex = majorTicks.reduce((lastVisibleLabelIndex, tick, index) => tick.label ? index : lastVisibleLabelIndex, null);
+
         if(!lastVisibleLabelIndex) {
             return;
         }
@@ -538,11 +603,15 @@ polarAxes.linear = {
 
     _getGridPoints: function(coords) {
         var pos = this.getCenter();
+        const radius = vizUtils.getDistance(pos.x, pos.y, coords.x, coords.y);
+        if(radius > this.getRadius()) {
+            return { cx: null, cy: null, r: null };
+        }
 
         return {
             cx: pos.x,
             cy: pos.y,
-            r: vizUtils.getDistance(pos.x, pos.y, coords.x, coords.y)
+            r: radius
         };
     },
 
@@ -661,6 +730,9 @@ polarAxes.linearSpider = _extend({}, polarAxes.linear, {
 
     _getGridPointsByRadius: function(radius) {
         var pos = this.getCenter();
+        if(radius > this.getRadius()) {
+            return { points: null };
+        }
 
         return {
             points: _map(this._spiderTicks, function(tick) {

testing/tests/DevExpress.viz.core/polarAxes.tests.js
@@ -706,6 +706,26 @@ QUnit.test("adjust labels", function(assert) {
     }
 });
 
+QUnit.test("adjust labels on updateSize", function(assert) {
+    this.options.label.visible = true;
+    this.options.visible = true;
+    const axis = this.createDrawnAxis();
+    const texts = this.renderer.text.getCalls().map(call => {
+        const text = call.returnValue;
+        text.attr.reset();
+        return text;
+    });
+
+    this.renderer.bBoxTemplate = { x: 30, y: 50, width: 40, height: 10 };
+
+    axis.updateSize(this.canvas);
+
+    texts.forEach(text => {
+        assert.equal(Math.round(text.attr.lastCall.args[0].x), 57);
+        assert.equal(Math.round(text.attr.lastCall.args[0].y), 70);
+    });
+});
+
 QUnit.test("draw labels, is not visible", function(assert) {
     this.createDrawnAxis();
 
@@ -1220,6 +1240,58 @@ QUnit.test("Update grid on second draw", function(assert) {
     });
 });
 
+QUnit.test("Update grid on second draw. Remove grid element if its value out of range", function(assert) {
+    this.generatedTicks = [500];
+
+    const axis = this.createDrawnAxis({ grid: { visible: true, color: "black", width: 1, opacity: 1 }, label: { overlappingBehavior: "ignore" } });
+
+    const grid = this.renderer.circle.lastCall.returnValue;
+    grid.stub("remove").reset();
+
+    axis.getTranslator().translate.withArgs(500, 0).returns(500);
+
+    axis.draw({
+        left: 10,
+        right: 80,
+        top: 0,
+        bottom: 0,
+        height: 100,
+        width: 100
+    });
+
+    assert.deepEqual(grid.remove.callCount, 1);
+});
+
+
+QUnit.test("Update grid on second draw. Remove grid element if its value out of range", function(assert) {
+    this.generatedTicks = [500];
+    this.renderSettings.drawingType = "linearSpider";
+    const axis = this.createSimpleAxis({ grid: { visible: true, color: "black", width: 1, opacity: 1 }, label: { overlappingBehavior: "ignore" } });
+
+    axis.setSpiderTicks([{ coords: { angle: -90 } }]);
+    axis.draw({
+        left: 10,
+        right: 80,
+        top: 0,
+        bottom: 0,
+        height: 100,
+        width: 100
+    });
+    const grid = this.renderer.path.lastCall.returnValue;
+    grid.stub("remove").reset();
+    axis.getTranslator().translate.withArgs(500, 0).returns(500);
+    axis.updateSize({
+        left: 10,
+        right: 80,
+        top: 0,
+        bottom: 0,
+        height: 100,
+        width: 100
+    });
+
+    assert.ok(grid.remove.callCount, 1);
+});
+
 QUnit.test("draw spider grid", function(assert) {
     this.renderSettings.drawingType = "linearSpider";
     var axis = this.createSimpleAxis({ grid: { visible: true, color: "black", width: 1, opacity: 1 }, label: { overlappingBehavior: "ignore" } });
@@ -1715,6 +1787,61 @@ QUnit.test("frequent tisks", function(assert) {
     assert.equal(text.getCall(7).returnValue.stub("remove").called, true, "text is removed");
     assert.equal(text.getCall(8).returnValue.stub("remove").called, true, "text is removed");
     assert.equal(text.getCall(9).returnValue.stub("remove").called, true, "text is removed");
+});
+
+QUnit.test("Check bounded ticks overlapping on updateSize - remove overlapped lavels if hide mode", function(assert) {
+    this.generatedTicks = [1, 2];
+    let bBoxes = [
+        { x: 0, y: 10, width: 10, height: 5 },
+        { x: 20, y: 10, width: 10, height: 5 },
+    ];
+    this.renderer.bBoxTemplate = function() {
+        return bBoxes.shift();
+    };
+
+    const axis = this.createDrawnAxis();
+
+    var text = this.renderer.text;
+
+    text.getCalls().forEach(c => c.returnValue.stub("remove").reset());
+    bBoxes = [
+        { x: 0, y: 10, width: 10, height: 5 },
+        { x: 5, y: 10, width: 10, height: 5 },
+    ];
+    axis.updateSize(this.canvas);
+
+    assert.equal(text.callCount, 2);
+    assert.equal(text.getCall(0).returnValue.remove.callCount, 0, "0 text is not removed");
+    assert.equal(text.getCall(1).returnValue.remove.callCount, 1, "1 text is removed");
+});
+
+QUnit.test("Check bounded ticks overlapping on updateSize - do not remove overlapped lavels if hide node", function(assert) {
+    this.generatedTicks = [1, 2];
+    let bBoxes = [
+        { x: 0, y: 10, width: 10, height: 5 },
+        { x: 20, y: 10, width: 10, height: 5 },
+    ];
+    this.renderer.bBoxTemplate = function() {
+        return bBoxes.shift();
+    };
+
+    const axis = this.createDrawnAxis({
+        visible: true,
+        label: { overlappingBehavior: "none" }
+    });
+
+    var text = this.renderer.text;
+
+    text.getCalls().forEach(c => c.returnValue.stub("remove").reset());
+    bBoxes = [
+        { x: 0, y: 10, width: 10, height: 5 },
+        { x: 5, y: 10, width: 10, height: 5 },
+    ];
+    axis.updateSize(this.canvas);
+
+    assert.equal(text.callCount, 2);
+    assert.equal(text.getCall(0).returnValue.remove.callCount, 0, "0 text is not removed");
+    assert.equal(text.getCall(1).returnValue.remove.callCount, 0, "1 text is not removed");
 });
 
 QUnit.module("Label overlapping, linear axis", $.extend({}, environment, {
@@ -2262,4 +2389,209 @@ QUnit.test("Do not apply margins two times", function(assert) {
     const tickGeneratorOptions = this.tickGenerator.lastCall.returnValue.lastCall.args[0];
     assert.equal(tickGeneratorOptions.min, -10);
     assert.equal(tickGeneratorOptions.max, 120);
+});
+
+
+QUnit.module("Get margins", $.extend(true, {}, environment, {
+    beforeEach: function() {
+        environment.beforeEach.call(this);
+        this.renderSettings.axisType = "polarAxes";
+        this.renderSettings.drawingType = "circular";
+        this.renderer.bBoxTemplate = { x: 0, y: 0, width: 0, height: 0, isEmpty: true };
+
+    },
+    afterEach: function() {
+        environment.afterEach.call(this);
+    },
+
+    setLabelBoxes(boxes) {
+        let i = 0;
+        this.renderer.bBoxTemplate = () => boxes[i++];
+    }
+}));
+
+QUnit.test("Without labels - returns canvas margins", function(assert) {
+    // arrange
+    const axis = this.createSimpleAxis({
+        label: {
+            visible: false
+        }
+    });
+    this.canvas = {
+        left: 0,
+        top: 0,
+        bottom: 0,
+        right: 0,
+        width: 600,
+        height: 606
+    };
+    axis.draw(this.canvas);
+    // act
+    const margins = axis.getMargins();
+    // assert
+    assert.strictEqual(margins.bottom, 0, "bottom");
+    assert.strictEqual(margins.left, 0, "left");
+    assert.strictEqual(margins.right, 0, "right");
+    assert.strictEqual(margins.top, 0, "right");
+});
+
+QUnit.test("With labels - calculate margins from labels", function(assert) {
+    this.generatedTicks = [1, 2, 3, 4];
+    // arrange
+    const axis = this.createSimpleAxis({
+        label: {
+            visible: true
+        }
+    });
+
+    this.canvas = {
+        left: 0,
+        top: 0,
+        bottom: 0,
+        right: 0,
+        width: 600,
+        height: 600
+    };
+    axis.draw(this.canvas);
+    this.setLabelBoxes([
+        { x: -10, width: 5, y: 10, height: 5 },
+        { x: 20, width: 5, y: 594, height: 20 },
+        { x: 594, width: 15, y: 10, height: 5 },
+        { x: 40, width: 5, y: -20, height: 5 }
+    ]);
+    // act
+    const margins = axis.getMargins();
+    // assert
+    assert.strictEqual(margins.bottom, 14, "bottom");
+    assert.strictEqual(margins.left, 10, "left");
+    assert.strictEqual(margins.right, 9, "right");
+    assert.strictEqual(margins.top, 20, "right");
+});
+
+QUnit.test("Without labels and with ticks - take into account length and shift", function(assert) {
+    this.generatedTicks = [1, 2, 3, 4];
+    // arrange
+    const axis = this.createSimpleAxis({
+        label: {
+            visible: false
+        },
+        tick: {
+            visible: true,
+            length: 12,
+            shift: 5
+        }
+    });
+
+    this.canvas = {
+        left: 0,
+        top: 0,
+        bottom: 0,
+        right: 0,
+        width: 600,
+        height: 600
+    };
+    axis.draw(this.canvas);
+    // act
+    const margins = axis.getMargins();
+    // assert
+    assert.strictEqual(margins.bottom, 11, "bottom");
+    assert.strictEqual(margins.left, 11, "left");
+    assert.strictEqual(margins.right, 11, "right");
+    assert.strictEqual(margins.top, 11, "right");
+});
+
+
+QUnit.test("Without labels and with inside ticks - do not take into ticks", function(assert) {
+    this.generatedTicks = [1, 2, 3, 4];
+    // arrange
+    const axis = this.createSimpleAxis({
+        label: {
+            visible: false
+        },
+        tick: {
+            visible: true,
+            length: 12,
+            shift: -8
+        }
+    });
+
+    this.canvas = {
+        left: 0,
+        top: 0,
+        bottom: 0,
+        right: 0,
+        width: 600,
+        height: 600
+    };
+    axis.draw(this.canvas);
+    // act
+    const margins = axis.getMargins();
+    // assert
+    assert.strictEqual(margins.bottom, 0, "bottom");
+    assert.strictEqual(margins.left, 0, "left");
+    assert.strictEqual(margins.right, 0, "right");
+    assert.strictEqual(margins.top, 0, "right");
+});
+
+
+QUnit.module("Circular axis. UpdateSize", $.extend({}, environment, {
+    beforeEach: function() {
+        environment.beforeEach.apply(this, arguments);
+        this.generatedTicks = [0];
+        this.translator.translate.returns(-90);
+
+        this.canvas = {
+            originalLeft: 5,
+            left: 20,
+            originalTop: 5,
+            top: 20,
+            originalBottom: 5,
+            bottom: 20,
+            right: 20,
+            originalRight: 5,
+            width: 200,
+            height: 200
+        };
+
+        this.options.startAngle = 0;
+        this.options.endAngle = 90;
+
+        this.renderSettings.axisType = "polarAxes";
+        this.renderSettings.drawingType = "circular";
+        this.options.min = 0;
+        this.options.max = 5000;
+        this.options.label = {
+            overlappingBehavior: "ignore",
+            visible: true,
+            alignment: "center"
+        };
+    },
+
+    act(bBox) {
+        const axis = this.createDrawnAxis();
+
+        this.renderer.bBoxTemplate = bBox;
+
+        axis.updateSize(this.canvas);
+        const { x, y } = this.renderer.text.lastCall.returnValue.attr.lastCall.args[0];
+        return [x, y];
+    }
+}));
+
+QUnit.test("adjust labels on updateSize - do not go out from left bound", function(assert) {
+    assert.deepEqual(this.act({ x: -15, y: 50, width: 40, height: 10 }), [40, 145]);
+});
+
+QUnit.test("adjust labels on updateSize - do not go out from right bound", function(assert) {
+    assert.deepEqual(this.act({ x: 155, y: 50, width: 110, height: 10 }), [-50, 145]);
+});
+
+QUnit.test("adjust labels on updateSize - do not go out from bottom bound", function(assert) {
+    this.translator.translate.returns(180);
+    assert.deepEqual(this.act({ x: 50, y: 180, width: 40, height: 30 }), [100, 165]);
+});
+
+QUnit.test("adjust labels on updateSize - do not go out from top bound", function(assert) {
+    this.translator.translate.returns(0);
+    assert.deepEqual(this.act({ x: 50, y: -100, width: 40, height: 60 }), [100, 125]);
 });

js/viz/axes/tick.js
@@ -78,7 +78,7 @@ function createTick(axis, renderer, tickOptions, gridOptions, skippedCategory, s
                 if(!lineElement) {
                     return;
                 }
-                if(settings.points === null) {
+                if(settings.points === null || settings.r === null) {
                     lineElement.remove();
                     return;
                 }

js/viz/axes/xy_axes.js
@@ -323,10 +323,6 @@ module.exports = {
         },
 
         _updateAxisElementPosition: function() {
-            if(!this._axisElement) {
-                return;
-            }
-
             var axisCoord = this._axisPosition,
                 canvas = this._getCanvasStartEnd();
 

js/viz/core/themes/generic.light.js
@@ -1509,8 +1509,8 @@ registerTheme({
             }
         },
         adaptiveLayout: {
-            width: 170,
-            height: 170,
+            width: 80,
+            height: 80,
             keepLabels: true
         },
         equalBarWidth: true,

js/viz/polar_chart.js
@@ -61,41 +61,49 @@ var dxPolarChart = AdvancedChart.inherit({
         return {};
     },
 
+    _calcCanvas: function() {
+        const canvas = extend({}, this._canvas);
+        const argumentAxis = this.getArgumentAxis();
+        const margins = argumentAxis.getMargins();
+        Object.keys(margins).forEach(margin => canvas[margin] = canvas[`original${margin[0].toUpperCase()}${margin.slice(1)}`] + margins[margin]);
+        return canvas;
+    },
+
     _renderAxes: function(drawOptions) {
         var that = this,
             valueAxis = that._getValueAxis(),
             argumentAxis = that.getArgumentAxis();
 
-        var canvas = that._calcCanvas(argumentAxis.measureLabels(extend({}, that._canvas), true));
-
-        argumentAxis.draw(canvas);
+        argumentAxis.draw(that._canvas);
         valueAxis.setSpiderTicks(argumentAxis.getSpiderTicks());
+
+        const canvas = that._calcCanvas();
+
+        argumentAxis.updateSize(canvas);
         valueAxis.draw(canvas);
+
+        return canvas;
     },
 
     _getValueAxis: function() {
         return this._valueAxes[0];
     },
 
-    _shrinkAxes: function(sizeShortage) {
+    _shrinkAxes: function(sizeStorage) {
         var valueAxis = this._getValueAxis(),
             argumentAxis = this.getArgumentAxis();
 
-        if(sizeShortage && (sizeShortage.width || sizeShortage.height)) {
+        if(sizeStorage && (sizeStorage.width || sizeStorage.height)
+        ) {
             argumentAxis.hideOuterElements();
-            argumentAxis.updateSize(this._canvas);
-            valueAxis.updateSize(this._canvas);
+            const canvas = this._calcCanvas();
+            argumentAxis.updateSize(canvas);
+            valueAxis.updateSize(canvas);
         }
     },
 
-    _calcCanvas: function(measure) {
-        var canvas = extend({}, this._canvas);
-
-        canvas.left += measure.width;
-        canvas.right += measure.width;
-        canvas.top += measure.height;
-        canvas.bottom += measure.height;
-        return canvas;
+    checkForMoreSpaceForPanesCanvas() {
+        return this.layoutManager.needMoreSpaceForPanesCanvas([{ canvas: this.getArgumentAxis().getCanvas() }], this._isRotated());
     },
 
     _getLayoutTargets: function() {

commit b25b6ee02bcfa5c0e28cd508928820c55e354022

Fix constant line regression after #9505

js/viz/axes/base_axis.js
@@ -2587,6 +2587,8 @@ Axis.prototype = {
 
     _visualRange: _noop,
 
+    _rotateConstantLine: _noop,
+
     applyVisualRangeSetter: _noop,
     // T642779,T714928
     getCategoriesSorter() {

js/viz/axes/constant_line.js
@@ -90,6 +90,7 @@ export default function createConstantLine(axis, options) {
             } else {
                 this.label && this.label.attr(axis._getConstantLineLabelsCoords(this.coord, this.labelOptions));
                 this.line && this.line.attr(axis._getConstantLineGraphicAttributes(this.coord));
+                axis._rotateConstantLine(this.line, this.coord);
             }
         },
 

js/viz/axes/polar_axes.js
@@ -234,10 +234,12 @@ circularAxes = polarAxes.circular = {
     },
 
     _createConstantLine: function(value, attr) {
-        var center = this.getCenter();
+        return this._createPathElement(this._getConstantLineGraphicAttributes(value).points, attr);
+    },
 
-        return this._createPathElement(this._getConstantLineGraphicAttributes(value).points, attr)
-            .rotate(value + this.getAngles()[0], center.x, center.y);
+    _rotateConstantLine(line, value) {
+        const { x, y } = this.getCenter();
+        line.rotate(value + this.getAngles()[0], x, y);
     },
 
     _getConstantLineLabelsCoords: function(value) {

testing/tests/DevExpress.viz.core/polarAxes.tests.js
@@ -4,6 +4,7 @@ import translator2DModule from "viz/translators/translator2d";
 import tickGeneratorModule from "viz/axes/tick_generator";
 import rangeModule from "viz/translators/range";
 import { Axis } from "viz/axes/base_axis";
+import { extend } from "core/utils/extend";
 
 var TranslatorStubCtor = new vizMocks.ObjectPool(translator2DModule.Translator2D),
     RangeStubCtor = new vizMocks.ObjectPool(rangeModule.Range);
@@ -887,6 +888,18 @@ QUnit.test("create constant lines with label, option 'startAngle' > 0", function
     assert.deepEqual(this.renderer.path.getCall(0).args, [[20, 50, 40, 50], "line"]);
     assert.deepEqual(this.renderer.path.getCall(0).returnValue.rotate.firstCall.args, [60, 20, 50]);
     assert.deepEqual(this.renderer.text.getCall(0).args, ["10", 25, 59]);
+});
+
+QUnit.test("Update constant line", function(assert) {
+    this.options.startAngle = 50;
+    const updateCanvas = extend({}, this.canvas);
+    this.canvas.width = 300;
+    const axis = this.createDrawnAxis({ constantLines: [{ value: 10, color: "green", label: { visible: true } }], label: { visible: false } });
+    axis.updateSize(updateCanvas);
+
+    assert.deepEqual(this.renderer.path.getCall(0).returnValue.attr.lastCall.args, [{ points: [20, 50, 40, 50] }]);
+    assert.deepEqual(this.renderer.path.getCall(0).returnValue.rotate.lastCall.args, [60, 20, 50]);
+    assert.deepEqual(this.renderer.text.getCall(0).returnValue.attr.lastCall.args, [{ x: 25, y: 59 }]);
 });
 
 QUnit.test("adjust constant line labels", function(assert) {