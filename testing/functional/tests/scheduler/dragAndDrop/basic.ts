9f8a2d824bcaabd7b8eff77ce66efda1bd3c4a06
Nara Lewinsky <naralewinskee@gmail.com>
2019-08-30 12:38:19 +0300

    dxScheduler. TestCafe. Some improvements in the code
    The problem with tabulation (replaced by spaces), exported variables in the widget (extra code), replacing the resource identifier from text to numeric type and other changes has been resolved.

--- testing/functional/tests/scheduler/dragAndDrop/basic.ts
+++ testing/functional/tests/scheduler/dragAndDrop/basic.ts
@@ -3,29 +3,29 @@ import { createScheduler } from './init/widget.setup';
 import url from '../../../helpers/getPageUrl';
 import Scheduler from '../../../model/scheduler';
 
-fixture `Drag-and-drop appointments in the Scheduler basic mode`
-	.page(url(__dirname, '../../container.html'));
+fixture `Drag-and-drop appointments in the Scheduler basic views`
+    .page(url(__dirname, '../../container.html'));
 
-['day', 'week', 'workWeek'].forEach((mode) => test(`Drag-n-drop in the "${mode}" mode`, async t => {
-	const scheduler = new Scheduler("#container");
-	const draggableAppointment = scheduler.getAppointment(`Brochure Design Review`);
+['day', 'week', 'workWeek'].forEach(view => test(`Drag-n-drop in the "${view}" view`, async t => {
+    const scheduler = new Scheduler("#container");
+    const draggableAppointment = scheduler.getAppointment(`Brochure Design Review`);
 
-	await t
-		.dragToElement(draggableAppointment.element, scheduler.getDateTableCell(4, 0))
-		.expect(draggableAppointment.size.height).eql(`50px`)
-		.expect(draggableAppointment.date.startTime).eql(`11:00 AM`)
-		.expect(draggableAppointment.date.endTime).eql(`11:30 AM`);
+    await t
+        .dragToElement(draggableAppointment.element, scheduler.getDateTableCell(4, 0))
+        .expect(draggableAppointment.size.height).eql(`50px`)
+        .expect(draggableAppointment.date.startTime).eql(`11:00 AM`)
+        .expect(draggableAppointment.date.endTime).eql(`11:30 AM`);
 
-}).before(() => createScheduler(mode, dataSource)));
+}).before(() => createScheduler(view, dataSource)));
 
-test(`Drag-n-drop in the "month" mode`, async t => {
-	const scheduler = new Scheduler("#container");
-	const draggableAppointment = scheduler.getAppointment(`Brochure Design Review`);
+test(`Drag-n-drop in the "month" view`, async t => {
+    const scheduler = new Scheduler("#container");
+    const draggableAppointment = scheduler.getAppointment(`Brochure Design Review`);
 
-	await t
-		.dragToElement(draggableAppointment.element, scheduler.getDateTableCell(0, 4))
-		.expect(draggableAppointment.size.height).eql(`19px`)
-		.expect(draggableAppointment.date.startTime).eql(`9:00 AM`)
-		.expect(draggableAppointment.date.endTime).eql(`9:30 AM`);
+    await t
+        .dragToElement(draggableAppointment.element, scheduler.getDateTableCell(0, 4))
+        .expect(draggableAppointment.size.height).eql(`19px`)
+        .expect(draggableAppointment.date.startTime).eql(`9:00 AM`)
+        .expect(draggableAppointment.date.endTime).eql(`9:30 AM`);
 
 }).before(() => createScheduler('month', dataSource));
--- testing/functional/tests/scheduler/resizeAppointments/init/widget.setup.ts
+++ testing/functional/tests/scheduler/resizeAppointments/init/widget.setup.ts
@@ -1,33 +1,32 @@
 import { createWidget } from "../../../../helpers/testHelper";
 
-export const widgetHeight = 833;
-export const widgetWidth = 1402;
-export const maxAppointmentsPerCell = 5;
-export const startDayHour = 9;
-export const currentDate = new Date(2019, 3, 1);
+const widgetHeight: number = 833;
+const widgetWidth: number = 1402;
+const maxAppointmentsPerCell: number = 5;
+const startDayHour: number = 9;
+const currentDate: Date = new Date(2019, 3, 1);
 
-export const resources = [
-	{ id: "0", color: "#e01e38" },
-	{ id: "1", color: "#f98322" },
-	{ id: "2", color: "#1e65e8" }
+const resources = [
+    { id: 0, color: "#e01e38" },
+    { id: 1, color: "#f98322" },
+    { id: 2, color: "#1e65e8" }
 ];
 
-export const createScheduler = (mode, dataSource, firstDayOfWeek = 1) =>
-	createWidget("dxScheduler", {
-		dataSource: dataSource,
-		resources: [
-			{
-				fieldExpr: "resourceId",
-				dataSource: resources,
-				label: "Color"
-			}
-		],
-		width: widgetWidth,
-		height: widgetHeight,
-		startDayHour: startDayHour,
-		firstDayOfWeek: firstDayOfWeek,
-		maxAppointmentsPerCell: maxAppointmentsPerCell,
-		views: [mode],
-		currentView: mode,
-		currentDate: currentDate
-	});
+export const createScheduler = (currentView, dataSource, firstDayOfWeek: number = 1) =>
+    createWidget("dxScheduler", {
+        dataSource: dataSource,
+        resources: [
+            {
+                fieldExpr: "resourceId",
+                dataSource: resources,
+                label: "Color"
+            }
+        ],
+        width: widgetWidth,
+        height: widgetHeight,
+        startDayHour: startDayHour,
+        firstDayOfWeek: firstDayOfWeek,
+        maxAppointmentsPerCell: maxAppointmentsPerCell,
+        currentView: currentView,
+        currentDate: currentDate
+    });
