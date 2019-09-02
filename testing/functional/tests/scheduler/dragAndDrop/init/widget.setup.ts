904caa21cf6fbe6295c7ad29b665c9b50a588a90
Nara Lewinsky <naralewinskee@gmail.com>
2019-09-02 14:12:19 +0300

    dxScheduler. TestCafe. Minor changes.

--- testing/functional/tests/scheduler/dragAndDrop/init/widget.setup.ts
+++ testing/functional/tests/scheduler/dragAndDrop/init/widget.setup.ts
@@ -1,32 +1,26 @@
 import { createWidget } from "../../../../helpers/testHelper";
 
-const widgetHeight: number = 833;
-const widgetWidth: number = 1666;
-const maxAppointmentsPerCell: number = 5;
-const startDayHour: number = 9;
-const currentDate: Date = new Date(2019, 3, 1);
-
 const resources = [
-    { id: 0, color: "#e01e38" },
-    { id: 1, color: "#f98322" },
-    { id: 2, color: "#1e65e8" }
+	{ id: 0, color: "#e01e38" },
+	{ id: 1, color: "#f98322" },
+	{ id: 2, color: "#1e65e8" }
 ];
 
-export const createScheduler = (currentView, dataSource, firstDayOfWeek = 1) =>
-    createWidget("dxScheduler", {
-        dataSource: dataSource,
-        resources: [
-            {
-                fieldExpr: "resourceId",
-                dataSource: resources,
-                label: "Color"
-            }
-        ],
-        width: widgetWidth,
-        height: widgetHeight,
-        startDayHour: startDayHour,
-        firstDayOfWeek: firstDayOfWeek,
-        maxAppointmentsPerCell: maxAppointmentsPerCell,
-        currentView: currentView,
-        currentDate: currentDate
-    });
+export const createScheduler = (currentView: string, dataSource: Array<any>, firstDayOfWeek: number = 1) =>
+	createWidget("dxScheduler", {
+		dataSource: dataSource,
+		resources: [
+			{
+				fieldExpr: "resourceId",
+				dataSource: resources,
+				label: "Color"
+			}
+		],
+		width: 833,
+		height: 1666,
+		startDayHour: 9,
+		firstDayOfWeek: firstDayOfWeek,
+		maxAppointmentsPerCell: 5,
+		currentView: currentView,
+		currentDate: new Date(2019, 3, 1)
+	});
