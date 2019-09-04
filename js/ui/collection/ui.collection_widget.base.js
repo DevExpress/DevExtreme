69cea590dfbb51ebd9034194221e1db078df40f0
Andrey Churkin <andrey.churkin@Andreys-MacBook-Pro.local>
2019-09-04 17:04:41 +0300

    List: should focus first item after changing selection mode (T811770)

--- js/ui/collection/ui.collection_widget.base.js
+++ js/ui/collection/ui.collection_widget.base.js
@@ -615,6 +615,12 @@ var CollectionWidget = Widget.inherit({
         }
     },
 
+    _invalidate: function() {
+        this.option("focusedElement", null);
+
+        return this.callBase.apply(this, arguments);
+    },
+
     _loadNextPage: function() {
         var dataSource = this._dataSource;
 
