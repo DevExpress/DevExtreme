3ac69ca682e586c86d30693e848bd4e4d4a1a74d
kruglikov.stepan <stepan.kruglikov@devexpress.com>
2019-09-06 16:34:43 +0300

    refactoring

--- js/ui/gantt/ui.gantt.dialogs.js
+++ js/ui/gantt/ui.gantt.dialogs.js
@@ -1,6 +1,5 @@
 import Popup from "../popup";
-import dxForm from "../form";
-import $ from "../../core/renderer";
+import Form from "../form";
 
 export class GanttDialog {
     constructor(owner, $element) {
@@ -15,10 +14,6 @@ export class GanttDialog {
         this.hide();
     }
 
-    _clean() {
-        delete this._popupInstance;
-    }
-
     show(name, parameters, callback) {
         this._callback = callback;
 
@@ -26,11 +21,13 @@ export class GanttDialog {
             return;
         }
         this._dialogInfo = new this.infoMap[name](parameters, this._apply.bind(this), this.hide.bind(this));
-        this._popupInstance.option("title", this._dialogInfo.getTitle());
-        this._popupInstance.option("toolbarItems", this._dialogInfo.getToolbarItems());
-        this._popupInstance.option("maxWidth", this._dialogInfo.getMaxWidth());
-        this._popupInstance.option("height", this._dialogInfo.getHeight());
-        this._popupInstance.option("contentTemplate", this._dialogInfo.getContentTemplate());
+        this._popupInstance.option({
+            title: this._dialogInfo.getTitle(),
+            toolbarItems: this._dialogInfo.getToolbarItems(),
+            maxWidth: this._dialogInfo.getMaxWidth(),
+            height: this._dialogInfo.getHeight(),
+            contentTemplate: this._dialogInfo.getContentTemplate()
+        });
         this._popupInstance.show();
     }
     hide() {
@@ -46,7 +43,7 @@ class DialogInfoBase {
         this._hideAction = hideAction;
     }
 
-    _getFormOptions() { return {}; }
+    _getFormItems() { return {}; }
     _updateParameters() {}
     _getOkToolbarItem() {
         return {
@@ -76,65 +73,59 @@ class DialogInfoBase {
     getMaxWidth() { return 400; }
     getHeight() { return "auto"; }
     getContentTemplate() {
-        return () => {
-            const element = $("<div>");
-            this._form = new dxForm(element, this._getFormOptions());
-            return element;
+        return (content) => {
+            this._form = new Form(content, {
+                formData: this._parameters,
+                items: this._getFormItems()
+            });
+            return content;
         };
     }
     getResult() {
-        const data = this._form.option("formData");
-        this._updateParameters(data);
+        const formData = this._form.option("formData");
+        this._updateParameters(formData);
         return this._parameters;
     }
 }
 
 class TaskEditDialogInfo extends DialogInfoBase {
     getTitle() { return "Task Information"; }
-    _getFormOptions() {
-        return {
-            items: [{
-                dataField: "title",
-                editorType: "dxTextBox",
-                label: { text: "Title", location: "left" },
-                editorOptions: {
-                    value: this._parameters.title
-                }
-            }, {
-                dataField: "start",
-                editorType: "dxDateBox",
-                label: { text: "Start", location: "left" },
-                editorOptions: {
-                    value: this._parameters.start,
-                    type: "datetime",
-                    width: "100%"
-                }
-            }, {
-                dataField: "end",
-                editorType: "dxDateBox",
-                label: { text: "End", location: "left" },
-                editorOptions: {
-                    value: this._parameters.end,
-                    type: "datetime",
-                    width: "100%"
-                }
-            }, {
-                dataField: "progress",
-                editorType: "dxNumberBox",
-                label: { text: "Progress", location: "left" },
-                editorOptions: {
-                    value: this._parameters.progress,
-                    showSpinButtons: true,
-                    min: 0,
-                    max: 100
-                }
-            }]
-        };
+    _getFormItems() {
+        return [{
+            dataField: "title",
+            editorType: "dxTextBox",
+            label: { text: "Title" }
+        }, {
+            dataField: "start",
+            editorType: "dxDateBox",
+            label: { text: "Start" },
+            editorOptions: {
+                type: "datetime",
+                width: "100%"
+            }
+        }, {
+            dataField: "end",
+            editorType: "dxDateBox",
+            label: { text: "End" },
+            editorOptions: {
+                type: "datetime",
+                width: "100%"
+            }
+        }, {
+            dataField: "progress",
+            editorType: "dxNumberBox",
+            label: { text: "Progress" },
+            editorOptions: {
+                showSpinButtons: true,
+                min: 0,
+                max: 100
+            }
+        }];
     }
-    _updateParameters(data) {
-        this._parameters.title = data.title;
-        this._parameters.start = data.start;
-        this._parameters.end = data.end;
-        this._parameters.progress = data.progress;
+    _updateParameters(formData) {
+        this._parameters.title = formData.title;
+        this._parameters.start = formData.start;
+        this._parameters.end = formData.end;
+        this._parameters.progress = formData.progress;
     }
 }
