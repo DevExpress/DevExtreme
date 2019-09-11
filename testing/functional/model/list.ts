0dd0828b62f11bd1653a2f0ab7c55d70d3469776
Andrey Churkin <alam17@mail.ru>
2019-09-11 15:17:12 +0300

    Grouped list can not reorder items (T727360) (#9673)
    * Grouped list can not reorder items (T727360)
    
    * add test
    
    * test refactoring
    
    * fix tests

--- testing/functional/model/list.ts
+++ testing/functional/model/list.ts
@@ -4,9 +4,12 @@ const CLASS = {
     checkbox: 'dx-checkbox',
     checkboxChecked: 'dx-checkbox-checked',
     focused: 'dx-state-focused',
+    group: 'dx-list-group',
+    groupHeader: 'dx-list-group-header',
     item: 'dx-list-item',
     radioButton: 'dx-radiobutton',
     radioButtonChecked: 'dx-radiobutton-checked',
+    reorderHandle: 'dx-list-reorder-handle',
     selectAllItem: 'dx-list-select-all'
 };
 
@@ -39,14 +42,34 @@ class Item {
     checkBox: CheckBox;
     isFocused: Promise<boolean>;
     radioButton: RadioButton;
+    reorderHandle: Selector;
+    text: Promise<string>;
 
     constructor (element: Selector) {
         this.element = element;
         this.checkBox = new CheckBox(element);
         this.isFocused = element.hasClass(CLASS.focused);
         this.radioButton = new RadioButton(element);
+        this.reorderHandle = element.find(`.${CLASS.reorderHandle}`);
+        this.text = element.textContent;
     }
 };
+
+class Group {
+    element: Selector;
+    header: Selector;
+    items: Selector;
+
+    constructor (element: Selector) {
+        this.element = element;
+        this.header =  element.find(`.${CLASS.groupHeader}`);
+        this.items = element.find(`.${CLASS.item}`);
+    }
+
+    getItem (index: number = 0): Item {
+        return new Item(this.items.nth(index));
+    }
+}
 
 export default class List {
     element: Selector;
@@ -61,5 +84,9 @@ export default class List {
 
     getItem (index: number = 0): Item {
         return new Item(this.items.nth(index));
+    }
+
+    getGroup(index: number = 0): Group {
+        return new Group(this.element.find(`.${CLASS.group}`).nth(index));
     }
 }
