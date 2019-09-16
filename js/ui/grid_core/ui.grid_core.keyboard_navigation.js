1a449b8a62d6557df6dd2f80ea069fbbc67cb8f2
igor_malzew <igor_malzew@mail.ru>
2019-09-16 14:32:16 +0300

    DataGrid - Provide a public flag in row that allows to identify a new item row

--- js/ui/grid_core/ui.grid_core.keyboard_navigation.js
+++ js/ui/grid_core/ui.grid_core.keyboard_navigation.js
@@ -958,7 +958,7 @@ var KeyboardNavigationController = core.ViewController.inherit({
 
         if(column.allowEditing) {
             let isDataRow = !row || row.rowType === "data";
-            isEditingAllowed = editingOptions.allowUpdating ? isDataRow : row && row.inserted;
+            isEditingAllowed = editingOptions.allowUpdating ? isDataRow : row && row.isNewRow;
         }
 
         if(!isEditingAllowed) {
