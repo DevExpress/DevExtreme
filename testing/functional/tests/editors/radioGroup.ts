3b833859204684b0e21caa2c0d13e51a2b33c3e3
Andrey Churkin <alam17@mail.ru>
2019-09-23 13:23:58 +0300

    RadioGroup: radio buttons placed into the template are selected after clicking the parent radio button (T816449) (#9815) (#9823)
    * RadioGroup: radio buttons placed into the template are selected after clicking the parent radio button
    
    * add test
    
    * rollback refactoring
    
    * refactoring
    
    * remove extra code

---
+++ testing/functional/tests/editors/radioGroup.ts
@@ -0,0 +1,62 @@
+import { Selector } from 'testcafe';
+import url from '../../helpers/getPageUrl';
+import RadioGroup from '../../model/radioGroup';
+import { convertCompilerOptionsFromJson } from 'typescript';
+
+fixture `Radio Group`
+    .page(url(__dirname, './pages/t816449.html'));
+
+test("Radio buttons placed into the template should not be selected after clicking the parent radio button (T816449)", async t => {
+    const parentGroup = new RadioGroup('#radio-group');
+    const firstChildGroup = new RadioGroup(parentGroup.getItem().content.child().nth(0));
+    const secondChildGroup = new RadioGroup(parentGroup.getItem(1).content.child());
+    const thirdChildGroup = new RadioGroup(parentGroup.getItem(2).content.child());
+
+    const checkGroup = async (group: RadioGroup, firstChecked: boolean = false, secondChecked: boolean  = false, thirdChecked: boolean  = false): Promise<void> => {
+        await t
+            .expect(group.getItem().radioButton.isChecked).eql(firstChecked)
+            .expect(group.getItem(1).radioButton.isChecked).eql(secondChecked)
+            .expect(group.getItem(2).radioButton.isChecked).eql(thirdChecked)
+    }
+
+    await checkGroup(parentGroup);
+    await checkGroup(firstChildGroup);
+    await checkGroup(secondChildGroup);
+    await checkGroup(thirdChildGroup);
+
+    await t.click(parentGroup.getItem().radioButton.element);
+    await checkGroup(parentGroup, true);
+    await checkGroup(firstChildGroup);
+    await checkGroup(secondChildGroup);
+    await checkGroup(thirdChildGroup);
+
+    await t.click(parentGroup.getItem(1).radioButton.element);
+    await checkGroup(parentGroup, false, true);
+    await checkGroup(firstChildGroup);
+    await checkGroup(secondChildGroup);
+    await checkGroup(thirdChildGroup);
+
+    await t.click(parentGroup.getItem(2).radioButton.element);
+    await checkGroup(parentGroup, false, false, true);
+    await checkGroup(firstChildGroup);
+    await checkGroup(secondChildGroup);
+    await checkGroup(thirdChildGroup);
+
+    await t.click(firstChildGroup.getItem().radioButton.element);
+    await checkGroup(parentGroup, false, false, true);
+    await checkGroup(firstChildGroup, true);
+    await checkGroup(secondChildGroup);
+    await checkGroup(thirdChildGroup);
+
+    await t.click(secondChildGroup.getItem(1).radioButton.element);
+    await checkGroup(parentGroup, false, false, true);
+    await checkGroup(firstChildGroup, true);
+    await checkGroup(secondChildGroup, false, true);
+    await checkGroup(thirdChildGroup);
+
+    await t.click(thirdChildGroup.getItem(2).radioButton.element);
+    await checkGroup(parentGroup, false, false, true);
+    await checkGroup(firstChildGroup, true);
+    await checkGroup(secondChildGroup, false, true);
+    await checkGroup(thirdChildGroup, false, false, true);
+});
