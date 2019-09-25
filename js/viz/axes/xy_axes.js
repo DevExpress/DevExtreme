97872607474eb050241c8aaec973e097b1bd841e
Roman Simionov <roman-simionov@users.noreply.github.com>
2019-09-25 16:39:51 +0300

    Fix axis.shift() (T813268) (#9881)

--- js/viz/axes/xy_axes.js
+++ js/viz/axes/xy_axes.js
@@ -1237,7 +1237,10 @@ module.exports = {
                 constantLinesGroups = that._axisConstantLineGroups;
 
             function shiftGroup(side, group) {
-                var attr = {},
+                var attr = {
+                        translateX: 0,
+                        translateY: 0
+                    },
                     shift = margins[side] ? margins[side] + axesSpacing : 0;
 
                 attr[isHorizontal ? "translateY" : "translateX"] = (side === "left" || side === "top" ? -1 : 1) * shift;
