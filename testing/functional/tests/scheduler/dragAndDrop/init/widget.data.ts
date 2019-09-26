f458e4d7173083d2b5eb13857d1c3339efdabd53
Nara Lewinsky <naralewinskee@gmail.com>
2019-09-26 11:00:04 +0300

    Fix focus behavior in appointment tooltips (T816316)
    Remove Id's from *.data.ts

--- testing/functional/tests/scheduler/dragAndDrop/init/widget.data.ts
+++ testing/functional/tests/scheduler/dragAndDrop/init/widget.data.ts
@@ -1,20 +1,17 @@
 export const dataSource = [
     {
-        Id: 1,
         text: 'Brochure Design Review',
         startDate: new Date(2019, 3, 1, 9, 0),
         endDate: new Date(2019, 3, 1, 9, 30),
         resourceId: 0
     },
     {
-        Id: 2,
         text: 'Update NDA Agreement',
         startDate: new Date(2019, 3, 1, 9, 0),
         endDate: new Date(2019, 3, 1, 10, 0),
         resourceId: 1
     },
     {
-        Id: 3,
         text: 'Staff Productivity Report',
         startDate: new Date(2019, 3, 1, 9, 0),
         endDate: new Date(2019, 3, 1, 10, 30),
@@ -24,49 +21,41 @@ export const dataSource = [
 
 export const appointmentCollectorData = [
     {
-        Id: 1,
         text: 'Website Re-Design Plan',
         startDate: new Date(2019, 3, 1, 9, 0),
         endDate: new Date(2019, 3, 1, 11, 30),
     },
     {
-        Id: 2,
         text: 'Book Flights to San Fran for Sales Trip',
         startDate: new Date(2019, 3, 1, 9, 0),
         endDate: new Date(2019, 3, 1, 11, 30),
     },
     {
-        Id: 3,
         text: 'Install New Router in Dev Room',
         startDate: new Date(2019, 3, 1, 9, 0),
         endDate: new Date(2019, 3, 1, 11, 30),
     },
     {
-        Id: 4,
         text: 'Approve Personal Computer Upgrade Plan',
         startDate: new Date(2019, 3, 1, 9, 0),
         endDate: new Date(2019, 3, 1, 11, 30),
     },
     {
-        Id: 5,
         text: 'Final Budget Review',
         startDate: new Date(2019, 3, 1, 9, 0),
         endDate: new Date(2019, 3, 1, 11, 30),
     },
     {
-        Id: 6,
         text: 'New Brochures',
         startDate: new Date(2019, 3, 1, 9, 0),
         endDate: new Date(2019, 3, 1, 11, 30),
     },
     {
-        Id: 7,
         text: 'Install New Database',
         startDate: new Date(2019, 3, 1, 9, 0),
         endDate: new Date(2019, 3, 1, 11, 30),
     },
     {
-        Id: 8,
         text: 'Approve New Online Marketing Strategy',
         startDate: new Date(2019, 3, 1, 9, 0),
         endDate: new Date(2019, 3, 1, 11, 30),
