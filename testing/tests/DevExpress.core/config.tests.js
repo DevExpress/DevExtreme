0a07ecf1dba194e6e4f25b3f27c85e39f5881e2a
zhavoronkov.evgeny <nightskylark@users.noreply.github.com>
2019-09-26 15:10:37 +0300

    Deprecate `decimalSeparator` and `thousandsSeparator` config props (#9885)

--- testing/tests/DevExpress.core/config.tests.js
+++ testing/tests/DevExpress.core/config.tests.js
@@ -1,6 +1,6 @@
-var config = require("core/config");
-
-require("bundles/modules/core");
+import config from "core/config";
+import errors from "core/errors";
+import "bundles/modules/core";
 
 QUnit.module("DevExtreme global config");
 
@@ -56,4 +56,31 @@ QUnit.test("default DevExpress.config contains 'useJQuery' with true value", fun
 QUnit.test("default DevExpress.config contains 'editorStylingMode' with undefined value", function(assert) {
     assert.ok("editorStylingMode" in config());
     assert.strictEqual(config().editorStylingMode, undefined);
+});
+
+QUnit.test("deprecated fields", function(assert) {
+    const originalLog = errors.log;
+    const log = [];
+
+    errors.log = (...args) => {
+        log.push(args);
+    };
+
+    try {
+        config({ decimalSeparator: "*" });
+        config({ thousandsSeparator: "*" });
+
+        assert.equal(log.length, 2);
+        assert.strictEqual(log[0][0], "W0003");
+        assert.strictEqual(log[0][1], "config");
+        assert.strictEqual(log[0][2], "decimalSeparator");
+        assert.strictEqual(log[0][3], "19.2");
+
+        assert.strictEqual(log[1][0], "W0003");
+        assert.strictEqual(log[1][1], "config");
+        assert.strictEqual(log[1][2], "thousandsSeparator");
+        assert.strictEqual(log[1][3], "19.2");
+    } finally {
+        errors.log = originalLog;
+    }
 });
