07c421dd24e001cb8f213b2846f8f222d490a4a7
Andrew Makarov <andrey.makarov@devexpress.com>
2019-10-02 10:18:16 +0300

    Fix test (#9981)

--- testing/tests/DevExpress.ui.widgets.scheduler/integration.RTL.tests.js
+++ testing/tests/DevExpress.ui.widgets.scheduler/integration.RTL.tests.js
@@ -1,5 +1,5 @@
 import fx from "animation/fx";
-import { createWrapper, initTestMarkup } from "./helpers.js";
+import { createWrapper, initTestMarkup, isDesktopEnvironment } from "./helpers.js";
 import translator from "animation/translator";
 
 import "ui/scheduler/ui.scheduler";
@@ -22,72 +22,83 @@ const moduleConfig = {
 };
 
 module("RTL", moduleConfig, () => {
-    test("Appointment should have correct position with multiple resources if rtlEnabled is true (T803275)", assert => {
-        const views = ["month", "week"];
+    if(isDesktopEnvironment()) {
+        test("Appointment should have correct position with multiple resources if rtlEnabled is true (T803275)", assert => {
+            const views = ["month", "week", "day"];
 
-        const expectedValues = {
-            month: [
-                {
-                    top: 257,
-                    left: 599
-                }, {
-                    top: 257,
-                    left: 250
-                }
-            ],
-            week: [
-                {
-                    top: 0,
-                    left: 539
-                }, {
-                    top: 0,
-                    left: 241
-                }
-            ]
-        };
-
-        const scheduler = createWrapper({
-            views: views,
-            currentView: views[0],
-            rtlEnabled: true,
-            dataSource: [{
-                text: "Apt1",
-                roomId: [1, 2],
-                startDate: new Date(2017, 4, 22),
-                endDate: new Date(2017, 4, 22, 1, 30)
-            }],
-            currentDate: new Date(2017, 4, 22),
-            groups: ['roomId'],
-            resources: [{
-                fieldExpr: "roomId",
-                dataSource: [
+            const expectedValues = {
+                month: [
                     {
-                        text: "Room 1",
-                        id: 1
+                        top: 257,
+                        left: 599
                     }, {
-                        text: "Room 2",
-                        id: 2
+                        top: 257,
+                        left: 250
                     }
                 ],
-            }],
-            width: 700,
-            height: 600
-        });
+                week: [
+                    {
+                        top: 0,
+                        left: 539
+                    }, {
+                        top: 0,
+                        left: 241
+                    }
+                ],
+                day: [
+                    {
+                        top: 0,
+                        left: 324
+                    }, {
+                        top: 0,
+                        left: 25
+                    }
+                ]
+            };
 
-        views.forEach(view => {
-            const { getAppointment } = scheduler.appointments;
-            const expectedValue = expectedValues[view];
+            const scheduler = createWrapper({
+                views: views,
+                currentView: views[0],
+                rtlEnabled: true,
+                dataSource: [{
+                    text: "Apt1",
+                    roomId: [1, 2],
+                    startDate: new Date(2017, 4, 22),
+                    endDate: new Date(2017, 4, 22, 1, 30)
+                }],
+                currentDate: new Date(2017, 4, 22),
+                groups: ['roomId'],
+                resources: [{
+                    fieldExpr: "roomId",
+                    dataSource: [
+                        {
+                            text: "Room 1",
+                            id: 1
+                        }, {
+                            text: "Room 2",
+                            id: 2
+                        }
+                    ],
+                }],
+                width: 700,
+                height: 600
+            });
 
-            scheduler.option("currentView", view);
+            views.forEach(view => {
+                const { getAppointment } = scheduler.appointments;
+                const expectedValue = expectedValues[view];
 
-            [getAppointment(0), getAppointment(1)].forEach((appointment, index) => {
-                const position = translator.locate(appointment);
+                scheduler.option("currentView", view);
 
-                assert.roughEqual(Math.round(position.left), expectedValue[index].left, 2.1, `left position of ${index} appointment should be correct in ${view} view`);
-                assert.roughEqual(Math.round(position.top), expectedValue[index].top, 2.1, `top position of ${index} appointment should be correct in ${view} view`);
+                [getAppointment(0), getAppointment(1)].forEach((appointment, index) => {
+                    const position = translator.locate(appointment);
+
+                    assert.roughEqual(Math.round(position.left), expectedValue[index].left, 2.1, `left position of ${index} appointment should be correct in ${view} view`);
+                    assert.roughEqual(Math.round(position.top), expectedValue[index].top, 2.1, `top position of ${index} appointment should be correct in ${view} view`);
+                });
             });
         });
-    });
+    }
 
     module("Task positions", () => {
         const createScheduler = view => {
