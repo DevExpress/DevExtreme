cb838e7b75e7731fb2b1d2c9fedcf9f3b3bac19f
Anton Kuznetsov (DevExpress) <anton.kuznetsov@devexpress.com>
2019-10-04 12:03:00 +0300

    refactoring

--- testing/tests/DevExpress.ui.widgets/tooltip.tests.js
+++ testing/tests/DevExpress.ui.widgets/tooltip.tests.js
@@ -1,8 +1,8 @@
-var $ = require("jquery"),
-    viewPort = require("core/utils/view_port").value,
-    fx = require("animation/fx"),
-    Tooltip = require("ui/tooltip"),
-    renderer = require("core/renderer");
+import $ from "jquery";
+import { value as viewPort } from "core/utils/view_port";
+import fx from "animation/fx";
+import Tooltip from "ui/tooltip";
+import renderer from "core/renderer";
 
 require("common.css!");
 
@@ -42,11 +42,11 @@ QUnit.test("render as tooltip", function(assert) {
 });
 
 QUnit.test("tooltip should render when target is core renderer object", function(assert) {
-    var target = renderer('#target');
-    var target2 = renderer('#target2');
+    const target = renderer('#target');
+    const target2 = renderer('#target2');
 
-    var $tooltip = $("#tooltip");
-    var $tooltip2 = $("#tooltip2");
+    const $tooltip = $("#tooltip");
+    const $tooltip2 = $("#tooltip2");
 
     new Tooltip($tooltip, {
         target: target,
@@ -67,7 +67,7 @@ QUnit.test("tooltip should render when target is core renderer object", function
 });
 
 QUnit.test("tooltip should render when target is selector", function(assert) {
-    var $tooltip = $("#tooltip");
+    const $tooltip = $("#tooltip");
 
     new Tooltip($tooltip, {
         target: '#defferedTarget',
