c2575036efbbd25fe4c9f73583acf1d701938052
EugeniyKiyashko <johnnykiyashko@gmail.com>
2019-10-07 00:54:11 +0300

    ContextMenu - test blinking on farm, rewrite test as functional

--- testing/functional/model/contextMenu.ts
+++ testing/functional/model/contextMenu.ts
@@ -1,53 +1,74 @@
 import Widget from './internal/widget';
-import { ClientFunction } from "testcafe";
+import { Selector, ClientFunction } from 'testcafe';
 
 const CLASS = {
-    checkbox: 'dx-checkbox',
-    checkboxChecked: 'dx-checkbox-checked',
-    disabled: 'dx-state-disabled',
-    focused: 'dx-state-focused',
-    group: 'dx-list-group',
-    groupHeader: 'dx-list-group-header',
-    item: 'dx-list-item',
-    radioButton: 'dx-radiobutton',
-    radioButtonChecked: 'dx-radiobutton-checked',
-    reorderHandle: 'dx-list-reorder-handle',
-    search: 'dx-list-search',
-    selectAllItem: 'dx-list-select-all'
+    overlay: "dx-overlay",
+    overlayWrapper: "dx-overlay-wrapper",
+    contextMenu: "dx-context-menu",
+    subMenu: "dx-submenu",
+    item: "dx-menu-item"
 };
 
-class Item {
+class Overlay {
     element: Selector;
-    isDisabled:  Promise<boolean>;
-    isFocused: Promise<boolean>;
-    isOpened: Promise<boolean>;
-    text: Promise<string>;
+    getOverlayInstance: ClientFunction<any>;
 
-    constructor (element: Selector) {
-        this.element = element;
+    constructor() {
+        this.element = Selector(`.${CLASS.overlay}`);
+
+        const { element } = this;
+        this.getOverlayInstance = ClientFunction(
+            () => $(element())['dxOverlay']('instance'),
+            { dependencies: { element }}
+        );
     }
-};
+
+    getOverlayOffset(): Promise<any> {
+        const getOverlayInstance: any = this.getOverlayInstance;
+        return ClientFunction(
+            ()=> {
+                const { offsetX, offsetY, pageX, pageY } = getOverlayInstance()._position.of;
+
+                return { offsetX: offsetX, offsetY: offsetY, pageX: pageX, pageY: pageY }
+            },
+            { dependencies: { getOverlayInstance } }
+        )();
+    }
+}
+
+class OverlayWrapper {
+    element: Selector;
+
+    constructor() {
+        this.element = Selector(`.${CLASS.overlayWrapper}`);
+    }
+
+    getVisibility(): Promise<string> {
+        const { element } = this;
+        const contextMenuClass = CLASS.contextMenu;
+        
+        return ClientFunction(()=> $(element()).find(`.${contextMenuClass}`).css("visibility"), {
+            dependencies: { element, contextMenuClass }
+        })();
+    }
+}
 
 export default class ContextMenu extends Widget {
     items: Selector;
-    getContextMenuInstance: ClientFunction<any>;
+    overlay: Overlay;
+    overlayWrapper: OverlayWrapper;
 
     name: string = 'dxContextMenu';
 
     constructor (id: string|Selector) {
         super(id);
 
-        this.items = this.element.find(`.${CLASS.item}`);
-        
-        const contextMenu =  this.element;
-
-        this.getContextMenuInstance = ClientFunction(
-            () => $(contextMenu())['dxContextMenu']('instance'),
-            { dependencies: { contextMenu }}
-        );
+        this.items = Selector(`.${CLASS.contextMenu}`).find(`.${CLASS.item}`);
+        this.overlay = new Overlay();
+        this.overlayWrapper = new OverlayWrapper();
     }
 
-    getItem (index: number = 0): Item {
-        return new Item(this.items.nth(index));
+    getItemCount() {
+        return this.items.count;
     }
 }
