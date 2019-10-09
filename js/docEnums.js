7fe8fcc3273fe5f6bd03d156b55095bfb1609a67
Roman Rodin <roman.rodin@devexpress.com>
2019-10-09 16:07:33 +0300

    Diagram - rename Ellipse shape + version up

--- js/docEnums.js
+++ js/docEnums.js
@@ -1092,7 +1092,7 @@
 
 /**
  * @typedef {string} Enums.DiagramShapeType
- * @enum {'text'|'rectangle'|'ellipsis'|'cross'|'triangle'|'diamond'|'heart'|'pentagon'|'octagon'|'star'|'arrowLeft'|'arrowTop'|'arrowRight'|'arrowBottom'|'arrowNorthSouth'|'arrowEastWest'|'process'|'decision'|'terminator'|'predefinedProcess'|'document'|'multipleDocuments'|'manualInput'|'preparation'|'data'|'database'|'hardDisk'|'internalStorage'|'paperTape'|'manualOperation'|'delay'|'storedData'|'display'|'merge'|'or'|'summingJunction'|'verticalContainer'|'horizontalContainer'|'cardWithImageOnLeft'|'cardWithImageOnTop'|'cardWithImageOnRight'}
+ * @enum {'text'|'rectangle'|'ellipse'|'cross'|'triangle'|'diamond'|'heart'|'pentagon'|'octagon'|'star'|'arrowLeft'|'arrowTop'|'arrowRight'|'arrowBottom'|'arrowNorthSouth'|'arrowEastWest'|'process'|'decision'|'terminator'|'predefinedProcess'|'document'|'multipleDocuments'|'manualInput'|'preparation'|'data'|'database'|'hardDisk'|'internalStorage'|'paperTape'|'manualOperation'|'delay'|'storedData'|'display'|'merge'|'or'|'summingJunction'|'verticalContainer'|'horizontalContainer'|'cardWithImageOnLeft'|'cardWithImageOnTop'|'cardWithImageOnRight'}
  */
 
 /**
