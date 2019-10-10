088b74539561d9fc861e854a6d31b82e734d13e7
Andrew Makarov <andrey.makarov@devexpress.com>
2019-10-10 16:19:55 +0300

    Update doc comment

--- js/ui/scheduler/ui.scheduler.js
+++ js/ui/scheduler/ui.scheduler.js
@@ -467,6 +467,7 @@ const Scheduler = Widget.inherit({
                 * @name dxSchedulerOptions.dropDownAppointmentTemplate
                 * @default "dropDownAppointment"
                 * @extends AppointmentTemplate
+                * @deprecated dxSchedulerOptions.dropDownAppointmentTemplate
                 */
             dropDownAppointmentTemplate: "dropDownAppointment",
 
@@ -1118,7 +1119,7 @@ const Scheduler = Widget.inherit({
              * @deprecated dxSchedulerOptions.onAppointmentFormOpening
              */
             onAppointmentFormCreated: { since: "18.2", alias: "onAppointmentFormOpening" },
-            dropDownAppointmentTemplate: { since: "20.1", message: "Use the 'dxSchedulerOptions.appointmentTooltipTemplate' option instead" }
+            dropDownAppointmentTemplate: { since: "20.1", message: "appointmentTooltipTemplate" }
         });
     },
 
