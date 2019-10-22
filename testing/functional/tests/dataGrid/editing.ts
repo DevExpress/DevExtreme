6aa33c8719d6b971d4e8c10618b75a3241e860f6
Anton Kuznetsov <anton.kuznetsov@devexpress.com>
2019-10-22 16:20:38 +0300

    merge

--- testing/functional/tests/dataGrid/editing.ts
+++ testing/functional/tests/dataGrid/editing.ts
@@ -2,6 +2,7 @@ import { pathToFileURL } from 'url';
 import { join } from  'path';
 import { createWidget } from '../../helpers/testHelper';
 import DataGrid from '../../model/dataGrid';
+import SelectBox from '../../model/selectBox';
 
 fixture `Editing`
     .page(pathToFileURL(join(__dirname, '../container.html')).href);
@@ -15,13 +16,37 @@ test("Tab key on editor should focus next cell if editing mode is cell", async t
         .pressKey("tab")
         .expect(dataGrid.getDataCell(1, 1).isFocused).ok();
 
-}).before(async () => {
-    await createWidget("dxDataGrid", {
-        dataSource: [{ name: "AaAaA", value: 1 }, { name: "aAaAa", value: 2 }],
-        editing: {
-            mode: "cell",
-            allowUpdating: true
-        },
-        columns: [{ dataField: "name", allowEditing: false }, { dataField: "value", showEditorAlways: true }]
-    });
+}).before(() => createWidget("dxDataGrid", {
+    dataSource: [{ name: "AaAaA", value: 1 }, { name: "aAaAa", value: 2 }],
+    editing: {
+        mode: "cell",
+        allowUpdating: true
+    },
+    columns: [{ dataField: "name", allowEditing: false }, { dataField: "value", showEditorAlways: true }]
+}));
+
+
+test("Value change on dataGrid row should be fired after clicking on editor (T823431)", async t => {
+    const dataGrid = new DataGrid("#container");
+    const selectBox = new SelectBox("#otherContainer");
+
+    await t
+        .click(dataGrid.getDataCell(0, 0).element)
+        .typeText(dataGrid.getDataCell(0, 0).element, "new_value")
+        .click(selectBox.dropDownButton)
+        .expect(dataGrid.getDataCell(0, 0).element.textContent).eql("new_value");
+
+}).before(() => {
+    return Promise.all([
+        createWidget("dxDataGrid", {
+            dataSource: [{ name: "old_value", value: 1 }],
+            editing: {
+                mode: "batch",
+                allowUpdating: true,
+                selectTextOnEditStart: true,
+                startEditAction: "click"
+            }
+        }),
+        createWidget("dxSelectBox", {}, false, "#otherContainer")
+    ]);
 });
