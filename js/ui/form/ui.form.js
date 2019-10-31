500a9ca4c7f513fa6f3c9d8e07e6c977b52f6499
Dmitry Semenov <jsdmitry@users.noreply.github.com>
2019-10-31 19:39:31 +0300

    Fix changing options of the tab in the tabbed item of the Form(T822602) (#10371)
    * fix changing options of the tab in the tabbed item of the Form
    
    * refactoring

--- js/ui/form/ui.form.js
+++ js/ui/form/ui.form.js
@@ -908,7 +908,7 @@ const Form = Widget.inherit({
             for(let i = 0; i < items.length; i++) {
                 const item = items[i];
                 const path = concatPaths(currentPath, createItemPathByIndex(i, isTabs));
-                const guid = this._itemsRunTimeInfo.add({ item, path });
+                const guid = this._itemsRunTimeInfo.add({ item, itemIndex: i, path });
 
                 if(isObject(item)) {
                     const itemCopy = extend({}, item);
@@ -1024,7 +1024,9 @@ const Form = Widget.inherit({
                     }
                 });
 
-                this._itemsRunTimeInfo && this._itemsRunTimeInfo.addLayoutManagerToItemByKey(layoutManager, itemData.guid);
+                if(this._itemsRunTimeInfo) {
+                    this._itemsRunTimeInfo.extendRunTimeItemInfoByKey(itemData.guid, { layoutManager });
+                }
 
                 if(alignItemLabels) {
                     this._alignLabelsInColumn.bind(this)({
@@ -1037,7 +1039,10 @@ const Form = Widget.inherit({
             }
         });
 
-        this._createComponent($tabPanel, TabPanel, tabPanelOptions);
+        const tabPanel = this._createComponent($tabPanel, TabPanel, tabPanelOptions);
+        if(item.tabs) {
+            item.tabs.forEach(tab => this._itemsRunTimeInfo.extendRunTimeItemInfoByKey(tab.guid, { widgetInstance: tabPanel }));
+        }
     },
 
     _itemGroupTemplate: function(item, e, $container) {
@@ -1077,7 +1082,7 @@ const Form = Widget.inherit({
                 cssItemClass: item.cssItemClass
             });
 
-            this._itemsRunTimeInfo && this._itemsRunTimeInfo.addLayoutManagerToItemByKey(layoutManager, item.guid);
+            this._itemsRunTimeInfo && this._itemsRunTimeInfo.extendRunTimeItemInfoByKey(item.guid, { layoutManager });
 
             colCount = layoutManager._getColCount();
             if(inArray(colCount, this._groupsColCount) === -1) {
