b02d78237cf821b877ee86f19f41416e93af9af7
Jaan Toming <jtoming@gmail.com>
2019-10-31 18:59:22 +0300

    FilterBuilder: Fix add condition popup was not hidden after scroll (T824147) (#10412)

--- js/ui/filter_builder/filter_builder.js
+++ js/ui/filter_builder/filter_builder.js
@@ -352,6 +352,8 @@ var FilterBuilder = Widget.inherit({
              */
             customOperations: [],
 
+            closePopupOnTargetScroll: true,
+
             /**
              * @name dxFilterBuilderCustomOperation
              * @type object
@@ -505,6 +507,8 @@ var FilterBuilder = Widget.inherit({
 
     _optionChanged: function(args) {
         switch(args.name) {
+            case "closePopupOnTargetScroll":
+                break;
             case "onEditorPreparing":
             case "onEditorPrepared":
             case "onValueChanged":
@@ -1152,6 +1156,7 @@ var FilterBuilder = Widget.inherit({
             },
             visible: true,
             focusStateEnabled: false,
+            closeOnTargetScroll: this.option("closePopupOnTargetScroll"),
             closeOnOutsideClick: true,
             onShown: options.popup.onShown,
             shading: false,
