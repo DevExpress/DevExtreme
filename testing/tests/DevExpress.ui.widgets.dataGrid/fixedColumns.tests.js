1f1b6d0f9229d61cb6ecde85b3800bfef91241ff
Anton Sermyazhko <pin361@gmail.com>
2019-11-13 10:42:35 +0300

    DataGrid - Fixed rows should be synchronized after resize column if wordWrapEnabled and grid height are set (T830739)
    # Conflicts:
    #	js/ui/grid_core/ui.grid_core.column_fixing.js

--- testing/tests/DevExpress.ui.widgets.dataGrid/fixedColumns.tests.js
+++ testing/tests/DevExpress.ui.widgets.dataGrid/fixedColumns.tests.js
@@ -230,7 +230,7 @@ QUnit.test("ColumnHeadersView - set column width for fixed table when no scroll"
     that.columnHeadersView.render($testElement);
 
     // act
-    that.columnHeadersView.setColumnWidths([50, 50, 50, 50, "auto"]);
+    that.columnHeadersView.setColumnWidths({ widths: [50, 50, 50, 50, "auto"] });
 
     // assert
     assert.equal($testElement.find(".dx-datagrid-headers").children().length, 2, "count content");
@@ -269,7 +269,7 @@ QUnit.test("ColumnHeadersView - set column width for fixed table when has scroll
     that.columnHeadersView.render($testElement);
 
     // act
-    that.columnHeadersView.setColumnWidths([100, 100, 100, 100, 100]);
+    that.columnHeadersView.setColumnWidths({ widths: [100, 100, 100, 100, 100] });
 
     // assert
     assert.equal($testElement.find(".dx-datagrid-headers").children().length, 2, "count content");
@@ -308,8 +308,8 @@ QUnit.test("ColumnHeadersView - set column width for fixed table when has scroll
     that.columnHeadersView.render($testElement);
 
     // act
-    that.columnHeadersView.setColumnWidths([50, 50, 50, "auto", 50]);
-    that.columnHeadersView.setColumnWidths([50, 150, 50, 50, 50]);
+    that.columnHeadersView.setColumnWidths({ widths: [50, 50, 50, "auto", 50] });
+    that.columnHeadersView.setColumnWidths({ widths: [50, 150, 50, 50, 50] });
 
     // assert
     assert.equal($testElement.find(".dx-datagrid-headers").children().length, 2, "count content");
@@ -349,7 +349,7 @@ QUnit.test("RowsView - set column width for fixed table when no scroll", functio
     that.rowsView.render($testElement);
 
     // act
-    that.rowsView.setColumnWidths([50, 50, 50, 50, "auto"]);
+    that.rowsView.setColumnWidths({ widths: [50, 50, 50, 50, "auto"] });
 
     // assert
     assert.equal($testElement.find(".dx-datagrid-rowsview").children(".dx-scrollable-wrapper").find(".dx-datagrid-content").length, 1, "has main content");
@@ -388,7 +388,7 @@ QUnit.test("RowsView - set column width for fixed table when has scroll", functi
     that.rowsView.render($testElement);
 
     // act
-    that.rowsView.setColumnWidths([100, 100, 100, 100, 100]);
+    that.rowsView.setColumnWidths({ widths: [100, 100, 100, 100, 100] });
 
     // assert
     assert.equal($testElement.find(".dx-datagrid-rowsview").children(".dx-scrollable-wrapper").find(".dx-datagrid-content").length, 1, "has main content");
@@ -3122,7 +3122,7 @@ QUnit.test("Fixed column widths should be correct when the group cell position i
     this.rowsView.render($testElement);
 
     // act
-    this.rowsView.setColumnWidths([100, 30, 150, 100]);
+    this.rowsView.setColumnWidths({ widths: [100, 30, 150, 100] });
     this.rowsView.resize();
 
     // assert
