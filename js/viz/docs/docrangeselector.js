663975713aaaf17e2bf3bdb835c1545e76143ac9
polosatov.alexander <alexanderPolosatov@users.noreply.github.com>
2019-11-14 13:29:27 +0300

    Fix type of rangeSelector.scale.label.customizeText callback args(T832367)

--- js/viz/docs/docrangeselector.js
+++ js/viz/docs/docrangeselector.js
@@ -168,7 +168,7 @@ var dxRangeSelector = {
             * @name dxRangeSelectorOptions.scale.label.customizeText
             * @type function(scaleValue)
             * @type_function_param1 scaleValue:object
-            * @type_function_param1_field1 value:Date|Number
+            * @type_function_param1_field1 value:Date|Number|string
             * @type_function_param1_field2 valueText:string
             * @type_function_return string
             * @notUsedInTheme
