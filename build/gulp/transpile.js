ced4e3e6c5c2e105369d9a3e8acebdee8ee601e4
Alexey Babich <alexey.babych@gmail.com>
2019-11-15 14:48:49 +0300

    Prevent the fail of transpile-watch task on error (#10616) (#10618)

--- build/gulp/transpile.js
+++ build/gulp/transpile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var watch = require('gulp-watch');
-
+var notify = require('gulp-notify');
+var plumber = require('gulp-plumber');
 var context = require('./context.js');
 
 var SRC = 'js/**/*.*';
@@ -17,6 +18,10 @@ gulp.task('transpile', ['bundler-config'], function() {
 
 gulp.task('transpile-watch', ['version-replace'], function() {
     return watch(SRC)
+        .pipe(plumber({
+            errorHandler: notify.onError('Error: <%= error.message %>')
+                .bind() // bind call is necessary to prevent firing 'end' event in notify.onError implementation
+        }))
         .pipe(babel())
         .pipe(gulp.dest(context.TRANSPILED_PATH));
 });
