116453ccff7667331e4ed4ded6389bad0139bee4
novsstation <57402891+novsstation@users.noreply.github.com>
2019-11-15 11:51:14 +0300

    Update testing/tests/DevExpress.ui.widgets/treeViewParts/lazyRendering.js
    Co-Authored-By: Dmitry Semenov <jsdmitry@users.noreply.github.com>

--- testing/tests/DevExpress.ui.widgets/treeViewParts/lazyRendering.js
+++ testing/tests/DevExpress.ui.widgets/treeViewParts/lazyRendering.js
@@ -27,7 +27,7 @@ QUnit.test("Only root nodes should be rendered by default", function(assert) {
 });
 
 QUnit.test("Expanding nodes should work with special charactes in id", function(assert) {
-    let testCases = ['!/#$%&\'()"+./:;<=>?@[]^`{|}~\\,', '____2______.jpg', 'E:\\test\\[gsdfgfd]  |  \'[some__file]', '!@#$%^&*()_+'];
+    const testIDs = ['!/#$%&\'()"+./:;<=>?@[]^`{|}~\\,', '____2______.jpg', 'E:\\test\\[gsdfgfd]  |  \'[some__file]', '!@#$%^&*()_+'];
     each(testCases, (index, testId) => {
         var $treeView = initTree({
             dataSource: [
