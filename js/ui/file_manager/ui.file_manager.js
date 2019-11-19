94e1af3adc64c52b977c07b45179680ab763dc97
Stanislav Klesarev <klesarevklesarevstas@gmail.com>
2019-11-19 15:38:27 +0300

    File Manager - 'Back' directory stores attributes of the represented directory (#10674)
    * 'Back' directory stores attributes of the represented directory
    
    * Code refactor

--- js/ui/file_manager/ui.file_manager.js
+++ js/ui/file_manager/ui.file_manager.js
@@ -22,8 +22,6 @@ import FileManagerEditingControl from "./ui.file_manager.editing";
 import FileManagerBreadcrumbs from "./ui.file_manager.breadcrumbs";
 import FileManagerAdaptivityControl from "./ui.file_manager.adaptivity";
 
-import { FileManagerItem } from "./file_provider/file_provider";
-
 const FILE_MANAGER_CLASS = "dx-filemanager";
 const FILE_MANAGER_WRAPPER_CLASS = FILE_MANAGER_CLASS + "-wrapper";
 const FILE_MANAGER_CONTAINER_CLASS = FILE_MANAGER_CLASS + "-container";
@@ -303,8 +301,10 @@ class FileManager extends Widget {
             : this._controller.getFiles(selectedDir);
 
         if(this.option("itemView.showParentFolder") && !selectedDir.fileItem.isRoot) {
-            let parentDirItem = new FileManagerItem(null, "..", true);
+            let parentDirItem = selectedDir.fileItem.createClone();
             parentDirItem.isParentFolder = true;
+            parentDirItem.name = "..";
+            parentDirItem.relativeName = "..";
             itemInfos = when(itemInfos)
                 .then(items => {
                     let itemInfosCopy = [...items];
