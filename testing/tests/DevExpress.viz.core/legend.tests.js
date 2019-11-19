a3be65ea3f553ca30662187bd3a1286048b7fb31
Alexander Ziborov <1420883+San4es@users.noreply.github.com>
2019-11-19 17:30:53 +0300

    Viz: Fix test assertion args (#10684)

--- testing/tests/DevExpress.viz.core/legend.tests.js
+++ testing/tests/DevExpress.viz.core/legend.tests.js
@@ -1920,7 +1920,7 @@ QUnit.test('Erase legend on update options', function(assert) {
     this.legend.update([]);
 
     assert.deepEqual(this.renderer.g.getCall(1).returnValue.remove.lastCall.args, [], 'group is removed');
-    assert.ok(this.renderer.g.getCall(1).returnValue.remove.lastCall.calledAfter(titleGroup.linkRemove.lastCall), [], 'group is removed');
+    assert.ok(this.renderer.g.getCall(1).returnValue.remove.lastCall.calledAfter(titleGroup.linkRemove.lastCall), 'group is removed');
 });
 
 QUnit.test('Check groups order', function(assert) {
