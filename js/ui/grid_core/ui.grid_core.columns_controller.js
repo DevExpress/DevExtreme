8479350a90257911e31402f91bb9b6194dd2c3b4
Jaan Toming <jtoming@gmail.com>
2019-11-19 16:04:50 +0300

    fixed arguments

--- js/ui/grid_core/ui.grid_core.columns_controller.js
+++ js/ui/grid_core/ui.grid_core.columns_controller.js
@@ -1370,7 +1370,7 @@ module.exports = {
                 }
             };
 
-            var columnOptionCore = function(that, column, optionName, value, notFireEvent, needUpdateIndexes) {
+            var columnOptionCore = function(that, column, optionName, value, notFireEvent) {
                 var optionGetter = dataCoreUtils.compileGetter(optionName),
                     columnIndex = column.index,
                     prevValue,
