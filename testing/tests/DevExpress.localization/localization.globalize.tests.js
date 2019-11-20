a771834b32c36d170297a575fa4ad16da469cd90
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2019-11-20 11:29:59 +0300

    ExcelJS - columns format support (#10651)

--- testing/tests/DevExpress.localization/localization.globalize.tests.js
+++ testing/tests/DevExpress.localization/localization.globalize.tests.js
@@ -419,4 +419,17 @@ define(function(require, exports, module) {
         assert.equal(numberLocalization.getThousandsSeparator(), "\xa0");
         Globalize.locale("en");
     });
+
+    var ExcelJSLocalizationFormatTests = require("../DevExpress.exporter/exceljsParts/exceljs.format.tests.js");
+
+    ExcelJSLocalizationFormatTests.default.runCurrencyTests([
+        { value: "USD", expected: "$#,##0_);\\($#,##0\\)" },
+        { value: "RUB", expected: "RUB#,##0_);\\(RUB#,##0\\)" },
+        { value: "JPY", expected: "¥#,##0_);\\(¥#,##0\\)" },
+        { value: "KPW", expected: "KPW#,##0_);\\(KPW#,##0\\)" },
+        { value: "LBP", expected: "LBP#,##0_);\\(LBP#,##0\\)" },
+        { value: "SEK", expected: "SEK#,##0_);\\(SEK#,##0\\)" }
+    ]);
 });
+
+
