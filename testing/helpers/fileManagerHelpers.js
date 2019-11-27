456dee75c92373b32fd56e1fb7a5c40eeb738894
Stanislav Klesarev <klesarevklesarevstas@gmail.com>
2019-11-27 14:29:52 +0300

    File Manager - Breadcrumbs height adjusted (#10863)
    * Breadcrumbs height is now theme-dependent
    
    * Rise fallen tests
    
    * Another fix of tests

--- testing/helpers/fileManagerHelpers.js
+++ testing/helpers/fileManagerHelpers.js
@@ -22,6 +22,7 @@ export const Consts = {
     FOLDERS_TREE_VIEW_ITEM_TOGGLE_CLASS: "dx-treeview-toggle-item-visibility",
     BREADCRUMBS_CLASS: "dx-filemanager-breadcrumbs",
     BREADCRUMBS_PARENT_DIRECOTRY_ITEM_CLASS: "dx-filemanager-breadcrumbs-parent-folder-item",
+    BREADCRUMBS_SEPARATOR_ITEM_CLASS: "dx-filemanager-breadcrumbs-separator-item",
     ITEMS_GRID_VIEW_CLASS: "dx-filemanager-files-view",
     FOCUSED_ITEM_CLASS: "dx-filemanager-focused-item",
     INACTIVE_AREA_CLASS: "dx-filemanager-inactive-area",
@@ -411,7 +412,7 @@ export class FileManagerBreadcrumbsWrapper {
     }
 
     getItems() {
-        return this._$element.find(`.${Consts.MENU_ITEM_WITH_TEXT_CLASS}`);
+        return this._$element.find(`.${Consts.MENU_ITEM_WITH_TEXT_CLASS}:not(.${Consts.BREADCRUMBS_SEPARATOR_ITEM_CLASS})`);
     }
 
     getItemByText(text) {
