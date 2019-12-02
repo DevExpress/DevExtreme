562bc8712f53847e90b7f27bbd7b35d873c54bb5
Anton Sermyazhko <pin361@gmail.com>
2019-12-02 15:35:38 +0300

    Docker script refactoring

--- docker-ci.sh
+++ docker-ci.sh
@@ -40,7 +40,6 @@ function run_test {
     local url="http://localhost:$port/run?notimers=true"
     local runner_pid
     local runner_result=0
-    local ios_user_agent="Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1"
 
     [ -n "$CONSTEL" ] && url="$url&constellation=$CONSTEL"
     [ -z "$JQUERY"  ] && url="$url&nojquery=true"
@@ -72,44 +71,86 @@ function run_test {
         ;;
 
         *)
-            google-chrome-stable --version
+            local chrome_command="google-chrome-stable \
+                --no-sandbox \
+                --disable-dev-shm-usage \
+                --disable-gpu \
+                --user-data-dir=/tmp/chrome";
 
             if [ "$HEADLESS" == "true" ]; then
-                google-chrome-stable \
-                    --no-sandbox \
-                    --disable-dev-shm-usage \
-                    --disable-gpu \
-                    --user-data-dir=/tmp/chrome \
+                echo "Headless mode"
+                chrome_command="$chrome_command \
                     --headless \
                     --remote-debugging-address=0.0.0.0 \
-                    --remote-debugging-port=9222 \
-                    $url &>headless-chrome.log &
-            elif [ "$IOS" == "true" ]; then
-                dbus-launch --exit-with-session google-chrome-stable \
-                    --no-sandbox \
-                    --disable-dev-shm-usage \
-                    --disable-gpu \
-                    --user-data-dir=/tmp/chrome \
+                    --remote-debugging-port=9222";
+            else
+                chrome_command="dbus-launch --exit-with-session $chrome_command \
                     --no-first-run \
                     --no-default-browser-check \
-                    --disable-translate \
-                    --user-agent="$ios_user_agent" \
+                    --disable-translate";
+            fi
+
+            if [ -n "$MOBILE_UA" ]; then
+                local user_agent
+
+                if [ "$MOBILE_UA" == "ios9" ]; then
+                    user_agent="Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1"
+                else
+                    exit 1
+                fi
+
+                echo "User agent: $user_agent"
+
+                chrome_command="$chrome_command \
+                    --user-agent=\"$user_agent\" \
                     --enable-viewport \
                     --touch-events \
                     --enable-overlay-scrollbar \
-                    --enable-features=OverlayScrollbar \
-                    $url &
-            else
-                dbus-launch --exit-with-session google-chrome-stable \
-                    --no-sandbox \
-                    --disable-dev-shm-usage \
-                    --disable-gpu \
-                    --user-data-dir=/tmp/chrome \
-                    --no-first-run \
-                    --no-default-browser-check \
-                    --disable-translate \
-                    $url &
+                    --enable-features=OverlayScrollbar"
             fi
+
+            google-chrome-stable --version
+
+            echo "Chrome cmd: $chrome_command"
+
+            $chrome_command $url &>chrome.log &
+
+            # if [ "$HEADLESS" == "true" ]; then
+            #   google-chrome-stable \
+            #     --no-sandbox \
+            #     --disable-dev-shm-usage \
+            #     --disable-gpu \
+            #     --user-data-dir=/tmp/chrome \
+            #     --headless \
+            #     --remote-debugging-address=0.0.0.0 \
+            #     --remote-debugging-port=9222 \
+            #     $url &>headless-chrome.log &
+            # elif [ "$IOS" == "true" ]; then
+            #     dbus-launch --exit-with-session google-chrome-stable \
+            #         --no-sandbox \
+            #         --disable-dev-shm-usage \
+            #         --disable-gpu \
+            #         --user-data-dir=/tmp/chrome \
+            #         --no-first-run \
+            #         --no-default-browser-check \
+            #         --disable-translate \
+            #         --user-agent="$ios_user_agent" \
+            #         --enable-viewport \
+            #         --touch-events \
+            #         --enable-overlay-scrollbar \
+            #         --enable-features=OverlayScrollbar \
+            #         $url &
+            # else
+            #     dbus-launch --exit-with-session google-chrome-stable \
+            #         --no-sandbox \
+            #         --disable-dev-shm-usage \
+            #         --disable-gpu \
+            #         --user-data-dir=/tmp/chrome \
+            #         --no-first-run \
+            #         --no-default-browser-check \
+            #         --disable-translate \
+            #         $url &
+            # fi
         ;;
 
     esac
