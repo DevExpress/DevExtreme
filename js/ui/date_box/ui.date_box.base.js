2f7701f334130f7c2589dacc4298b4a1492da0bd
kotov.alexander <alexander.kotov@devexpress.com>
2019-12-06 13:57:37 +0300

    Check all validation rules if internal validation options were changed for invalid state DateBox widget (T838294) (#10993)

--- js/ui/date_box/ui.date_box.base.js
+++ js/ui/date_box/ui.date_box.base.js
@@ -783,7 +783,11 @@ var DateBox = DropDownEditor.inherit({
                 break;
             case "min":
             case "max":
-                this._applyInternalValidation(this.dateOption("value"));
+                if(this.option("isValid")) {
+                    this._applyInternalValidation(this.dateOption("value"));
+                } else {
+                    this._validateValue(this.dateOption("value"));
+                }
                 this._invalidate();
                 break;
             case "dateSerializationFormat":
