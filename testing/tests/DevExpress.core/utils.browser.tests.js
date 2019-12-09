869d0c29b8799e53c8223df5af7e8ab47e3ea15d
BingoRUS <bingofirst@gmail.com>
2019-12-09 22:44:08 +0300

    Parse user agent of an unknown webkit browser correctly

--- testing/tests/DevExpress.core/utils.browser.tests.js
+++ testing/tests/DevExpress.core/utils.browser.tests.js
@@ -11,7 +11,8 @@ var userAgents = {
     chrome: "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
     chrome_ios: "Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/74.0.3729.157 Mobile/13B143 Safari/601.1.46",
     mozilla_ios: "Mozilla/5.0 (iPhone; CPU iPhone OS 12_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/18.2b15817 Mobile/15E148 Safari/605.1.15",
-    phantom: "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/538.1 (KHTML, like Gecko) PhantomJS/2.1.1 Safari/538.1"
+    phantom: "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/538.1 (KHTML, like Gecko) PhantomJS/2.1.1 Safari/538.1",
+    google_app_ios: "Mozilla/5.0 (iPhone; CPU iPhone OS 13_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) GSA/88.0.281793270 Mobile/15E148 Safari/604.1"
 };
 
 QUnit.module("browser");
@@ -25,27 +26,27 @@ QUnit.test("MSEdge is IE", function(assert) {
 });
 
 QUnit.test("browser is IE", function(assert) {
-    assert.ok(browser._fromUA(userAgents.ie).msie, "ie detected");
-
-    assert.equal(browser._fromUA(userAgents.ie).version, '9.0', "version was detect correctly");
+    var browserObject = browser._fromUA(userAgents.ie);
+    assert.ok(browserObject.msie, "ie detected");
+    assert.equal(browserObject.version, '9.0', "version was detect correctly");
 });
 
 QUnit.test("browser is IE11", function(assert) {
-    assert.ok(browser._fromUA(userAgents.ie11).msie, "ie11 detected");
-
-    assert.equal(browser._fromUA(userAgents.ie11).version, '11.0', "version was detect correctly");
+    var browserObject = browser._fromUA(userAgents.ie11);
+    assert.ok(browserObject.msie, "ie11 detected");
+    assert.equal(browserObject.version, '11.0', "version was detect correctly");
 });
 
-QUnit.test("browser is webkit", function(assert) {
-    assert.ok(browser._fromUA(userAgents.webkit).webkit, "webkit detected");
-
-    assert.equal(browser._fromUA(userAgents.webkit).version, '537.36', "version was detect correctly");
+QUnit.test("browser is generic webkit", function(assert) {
+    var browserObject = browser._fromUA(userAgents.webkit);
+    assert.ok(browserObject.webkit, "webkit detected");
+    assert.equal(browserObject.version, '43.0.2357.124', "version was detect correctly");
 });
 
 QUnit.test("browser is mozilla", function(assert) {
-    assert.ok(browser._fromUA(userAgents.mozilla).mozilla, "mozilla detected");
-
-    assert.equal(browser._fromUA(userAgents.mozilla).version, '38.0', "version was detect correctly");
+    var browserObject = browser._fromUA(userAgents.mozilla);
+    assert.ok(browserObject.mozilla, "mozilla detected");
+    assert.equal(browserObject.version, '38.0', "version was detect correctly");
 });
 
 QUnit.test("browser is safari", function(assert) {
@@ -89,4 +90,14 @@ QUnit.test("browser is mozilla (mobile)", function(assert) {
     assert.notOk(browserObject.safari, "firefox is not safari");
     assert.notOk(browserObject.chrome, "firefox is not chrome");
     assert.equal(browserObject.version, '18.2', "version was detect correctly");
+});
+
+QUnit.test("google app is chrome (mobile)", function(assert) {
+    var browserObject = browser._fromUA(userAgents.google_app_ios);
+
+    assert.notOk(browserObject.safari, "google app is not safari");
+    assert.notOk(browserObject.chrome, "google app is not chrome");
+    assert.ok(browserObject.webkit, "google app is webkit browser");
+    assert.ok(browserObject.unknown, "but google app is not known browser name");
+    assert.strictEqual(browserObject.version, "88.0.281793270", "google app version is correct");
 });
