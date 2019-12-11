d5bf17ccb376fe1e42efd278fbb0dc12be7c1c7f
kruglikov.stepan <stepan.kruglikov@devexpress.com>
2019-12-11 11:02:56 +0300

    Fix tests

--- testing/tests/DevExpress.ui.widgets/gantt.tests.js
+++ testing/tests/DevExpress.ui.widgets/gantt.tests.js
@@ -326,26 +326,26 @@ QUnit.module("Options", moduleConfig, () => {
         };
         this.createInstance(tasksOnlyOptions);
         this.clock.tick();
-        assert.equal(getFirstHeaderTitle(), "January", "is months scale type (auto)");
+        assert.equal(getFirstHeaderTitle(), "March", "is months scale type (auto)");
         this.instance.option("scaleType", "minutes");
-        assert.equal(getFirstHeaderTitle(), "10", "is minutes scale type");
+        assert.equal(getFirstHeaderTitle(), "30", "is minutes scale type");
         this.instance.option("scaleType", "hours");
-        assert.equal(getFirstHeaderTitle(), "12:00 AM", "is hours scale type");
+        assert.equal(getFirstHeaderTitle(), "9:00 PM", "is hours scale type");
         this.instance.option("scaleType", "days");
-        assert.equal(getFirstHeaderTitle(), "Sun, 17 Feb", "is days scale type");
+        assert.equal(getFirstHeaderTitle(), "Sat, 23 Feb", "is days scale type");
         this.instance.option("scaleType", "weeks");
-        assert.equal(getFirstHeaderTitle(), "Sun, 17 Feb - Sat, 23 Feb", "is weeks scale type");
+        assert.equal(getFirstHeaderTitle(), "Sun, 20 Jan - Sat, 26 Jan", "is weeks scale type");
         this.instance.option("scaleType", "months");
-        assert.equal(getFirstHeaderTitle(), "January", "is months scale type");
+        assert.equal(getFirstHeaderTitle(), "March", "is months scale type");
         this.instance.option("scaleType", "quarters");
         assert.equal(getFirstHeaderTitle(), "Q1", "is quarters scale type");
         this.instance.option("scaleType", "years");
-        assert.equal(getFirstHeaderTitle(), "2019", "is years scale type");
+        assert.equal(getFirstHeaderTitle(), "2008", "is years scale type");
 
         this.instance.option("tasks.dataSource", [{ "id": 0, "title": "t", "start": "2019-02-21", "end": "2019-02-26" }]);
-        assert.equal(getFirstHeaderTitle(), "2019", "is still years scale type");
+        assert.equal(getFirstHeaderTitle(), "2008", "is still years scale type");
         this.instance.option("scaleType", "auto");
-        assert.equal(getFirstHeaderTitle(), "Sun, 17 Feb", "is days scale type (auto)");
+        assert.equal(getFirstHeaderTitle(), "Sun, 10 Feb", "is days scale type (auto)");
     });
 });
 
@@ -497,9 +497,8 @@ QUnit.module("Dialogs", moduleConfig, () => {
         const $okButton = $dialog.find(".dx-popup-bottom").find(".dx-button").eq(0);
         $okButton.trigger("dxclick");
         this.clock.tick();
-        const $taskWrapper = this.$element.find(TASK_WRAPPER_SELECTOR).eq(0);
-        const firstTitle = $taskWrapper.children().children().first().text();
-        assert.equal(firstTitle, testTitle, "title text was modified");
+        const firstTreeListTitleText = this.$element.find(TREELIST_DATA_ROW_SELECTOR).first().find("td").eq(2).text();
+        assert.equal(firstTreeListTitleText, testTitle, "title text was modified");
 
         this.instance.option("editing.enabled", false);
         showTaskEditDialog(this.instance);
