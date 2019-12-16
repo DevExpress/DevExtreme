4811afe345db08d5b205411e4b94fdcd2f5aa28d
kotov.alexander <alexander.kotov@devexpress.com>
2019-12-16 18:46:26 +0300

    Update window's replacing for tests (#11128)

--- testing/tests/DevExpress.core/utils.resize_callbacks.tests.js
+++ testing/tests/DevExpress.core/utils.resize_callbacks.tests.js
@@ -1,22 +1,26 @@
-import $ from "jquery";
 import resizeCallbacks from "core/utils/resize_callbacks";
 import domAdapter from "core/dom_adapter";
 import windowUtils from "core/utils/window";
 
 QUnit.module('resizeCallbacks', {
     beforeEach: function() {
-        var test = this;
+        const test = this;
         test.__originalWindowElementGetter = windowUtils.getWindow;
         test.width = 400;
         test.height = 300;
 
         windowUtils.getWindow = function() {
-            return $.extend(test.__originalWindowElementGetter(), { innerHeight: test.height, innerWidth: test.width });
+            const fakeWindow = {};
+            fakeWindow.window = fakeWindow;
+            fakeWindow.innerHeight = test.height;
+            fakeWindow.innerWidth = test.width;
+
+            return fakeWindow;
         };
 
         test.__originalListener = domAdapter.listen;
 
-        var resizeHandlers = [];
+        const resizeHandlers = [];
         domAdapter.listen = function(element, event, handler) {
             if(element.window === element && event === "resize") {
                 resizeHandlers.push(handler);
