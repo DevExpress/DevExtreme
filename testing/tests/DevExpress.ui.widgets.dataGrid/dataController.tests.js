722d0f1584af7466e45a9f15c4023679d7f2d6e6
Alexander Ziborov <1420883+San4es@users.noreply.github.com>
2019-12-18 15:09:24 +0300

    DataGrid: Restore Sinon timers correctly in tests (#11173)

--- testing/tests/DevExpress.ui.widgets.dataGrid/dataController.tests.js
+++ testing/tests/DevExpress.ui.widgets.dataGrid/dataController.tests.js
@@ -3883,11 +3883,8 @@ QUnit.module('Virtual scrolling (ScrollingDataSource)', {
             return dataItems;
         };
 
-        this.clock = sinon.useFakeTimers();
-    }, afterEach: function() {
-        teardownModule.call(this);
-        this.clock.restore();
-    }
+    },
+    afterEach: teardownModule
 });
 
 QUnit.test('load first page', function(assert) {
@@ -4738,12 +4735,8 @@ QUnit.module('Infinite scrolling (ScrollingDataSource)', {
             });
             return dataItems;
         };
-
-        this.clock = sinon.useFakeTimers();
-    }, afterEach: function() {
-        teardownModule.call(this);
-        this.clock.restore();
-    }
+    },
+    afterEach: teardownModule
 });
 
 QUnit.test('not update pageSize on viewportSize', function(assert) {
