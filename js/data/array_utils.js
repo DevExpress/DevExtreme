f683295281d850d91fbaa189cb339365bd7c0d5f
volnyagin <vconst@users.noreply.github.com>
2019-12-24 17:26:46 +0300

    Refactoring

--- js/data/array_utils.js
+++ js/data/array_utils.js
@@ -45,18 +45,15 @@ function getItems(keyInfo, items, key, groupCount) {
     return items;
 }
 
-const LENGTH_KEY = '__DX_LENGTH__';
-
 function generateDataByKeyMap(keyInfo, array) {
-    if(keyInfo.key() && (!array._dataByKeyMap || array._dataByKeyMap[LENGTH_KEY] !== array.length)) {
+    if(keyInfo.key() && (!array._dataByKeyMap || array._dataByKeyMapLength !== array.length)) {
         var dataByKeyMap = {};
         for(var i = 0, arrayLength = array.length; i < arrayLength; i++) {
             dataByKeyMap[JSON.stringify(keyInfo.keyOf(array[i]))] = array[i];
         }
 
-        dataByKeyMap[LENGTH_KEY] = arrayLength;
-
         array._dataByKeyMap = dataByKeyMap;
+        array._dataByKeyMapLength = arrayLength;
     }
 }
 
