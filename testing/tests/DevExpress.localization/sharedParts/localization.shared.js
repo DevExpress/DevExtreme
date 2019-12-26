ddbf4baee9b01085209b69111a0d096af337ae7b
Ignatov Dan <ignatovdan@gmail.com>
2019-12-26 17:20:42 +0300

    Export - Apply localized currency format when localization is used (PivotGrid/DataGrid/T835933) (#11274)
    * Convert Intl/Globalize format string to OpenXml format
    
    * Add tests for ExcelJS (a low level format string should be used)
    
    * Avoid issues with 'Â' symbol when 2bytes UTF8 symbol was read as two 1byte symbols. Add commented links for export/exceljs scenarios.
    
    * Typos.
    
    * Avoid issues with 'Â' symbol when 2bytes UTF8 symbol was read as two 1byte symbols. Add commented links for export/exceljs scenarios.

--- testing/tests/DevExpress.localization/sharedParts/localization.shared.js
+++ testing/tests/DevExpress.localization/sharedParts/localization.shared.js
@@ -864,9 +864,6 @@ module.exports = function() {
     QUnit.test('format: several words', function(assert) {
         assert.equal(localization.formatNumber(0, { type: 'currency thousands', currency: undefined, precision: 0 }), '$0K');
     });
-    QUnit.test('getOpenXmlCurrencyFormat', function(assert) {
-        assert.equal(numberLocalization.getOpenXmlCurrencyFormat(), '$#,##0{0}_);\\($#,##0{0}\\)');
-    });
 
     QUnit.module('Localization custom functions');
 
