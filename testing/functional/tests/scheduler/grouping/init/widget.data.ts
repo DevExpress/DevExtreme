b5a0772a49174fd9f09bdf4b8e709c298d5d04bc
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2019-12-26 11:21:31 +0300

    Add functional test

---
+++ testing/functional/tests/scheduler/grouping/init/widget.data.ts
@@ -0,0 +1,144 @@
+export const dataSource = [
+    {
+        text: "Website Re-Design Plan",
+        priorityId: 2,
+        startDate: new Date(2018, 4, 21, 9, 30),
+        endDate: new Date(2018, 4, 21, 11, 30)
+    }, {
+        text: "Book Flights to San Fran for Sales Trip",
+        priorityId: 1,
+        startDate: new Date(2018, 4, 24, 10, 0),
+        endDate: new Date(2018, 4, 24, 12, 0),
+    }, {
+        text: "Install New Router in Dev Room",
+        priorityId: 1,
+        startDate: new Date(2018, 4, 20, 13),
+        endDate: new Date(2018, 4, 20, 15, 30)
+    }, {
+        text: "Approve Personal Computer Upgrade Plan",
+        priorityId: 2,
+        startDate: new Date(2018, 4, 22, 10, 0),
+        endDate: new Date(2018, 4, 22, 11, 0)
+    }, {
+        text: "Final Budget Review",
+        priorityId: 2,
+        startDate: new Date(2018, 4, 22, 12, 0),
+        endDate: new Date(2018, 4, 22, 13, 35)
+    }, {
+        text: "New Brochures",
+        priorityId: 2,
+        startDate: new Date(2018, 4, 21, 13, 0),
+        endDate: new Date(2018, 4, 21, 15, 15)
+    }, {
+        text: "Install New Database",
+        priorityId: 2,
+        startDate: new Date(2018, 4, 13, 9),
+        endDate: new Date(2018, 4, 15, 12, 15)
+    }, {
+        text: "Approve New Online Marketing Strategy",
+        priorityId: 2,
+        startDate: new Date(2018, 4, 23, 12, 0),
+        endDate: new Date(2018, 4, 23, 14, 0)
+    }, {
+        text: "Upgrade Personal Computers",
+        priorityId: 1,
+        startDate: new Date(2018, 4, 13, 9),
+        endDate: new Date(2018, 4, 13, 11, 30),
+        recurrenceRule: "FREQ=DAILY;COUNT=4"
+    }, {
+        text: "Prepare 2018 Marketing Plan",
+        priorityId: 2,
+        startDate: new Date(2018, 4, 24, 11, 0),
+        endDate: new Date(2018, 4, 24, 13, 30)
+    }, {
+        text: "Brochure Design Review",
+        priorityId: 1,
+        startDate: new Date(2018, 4, 23, 11, 0),
+        endDate: new Date(2018, 4, 23, 13, 30)
+    }, {
+        text: "Create Icons for Website",
+        priorityId: 2,
+        startDate: new Date(2018, 4, 25, 10, 0),
+        endDate: new Date(2018, 4, 25, 11, 30)
+    }, {
+        text: "Upgrade Server Hardware",
+        priorityId: 1,
+        startDate: new Date(2018, 4, 7, 9, 0),
+        endDate: new Date(2018, 4, 10, 15, 0)
+    }, {
+        text: "Submit New Website Design",
+        priorityId: 2,
+        startDate: new Date(2018, 4, 25, 16, 30),
+        endDate: new Date(2018, 4, 25, 18, 0)
+    }, {
+        text: "Launch New Website",
+        priorityId: 2,
+        startDate: new Date(2018, 4, 26, 12, 20),
+        endDate: new Date(2018, 4, 26, 14, 0)
+    }, {
+        text: "Google AdWords Strategy",
+        priorityId: 1,
+        startDate: new Date(2018, 4, 28, 9, 0, 0),
+        endDate: new Date(2018, 4, 28, 12, 0, 0)
+    }, {
+        text: "Rollout of New Website and Marketing Brochures",
+        priorityId: 1,
+        startDate: new Date(2018, 4, 28, 13, 0, 0),
+        endDate: new Date(2018, 4, 28, 15, 30, 0)
+    }, {
+        text: "Non-Compete Agreements",
+        priorityId: 2,
+        startDate: new Date(2018, 4, 29, 13, 0, 0),
+        endDate: new Date(2018, 4, 29, 15, 45, 0)
+    }, {
+        text: "Approve Hiring of John Jeffers",
+        priorityId: 2,
+        startDate: new Date(2018, 4, 29, 9, 0, 0),
+        endDate: new Date(2018, 4, 29, 12, 0, 0)
+    }, {
+        text: "Update NDA Agreement",
+        priorityId: 1,
+        startDate: new Date(2018, 4, 29, 11, 0, 0),
+        endDate: new Date(2018, 4, 29, 14, 15, 0)
+    }, {
+        text: "Update Employee Files with New NDA",
+        priorityId: 1,
+        startDate: new Date(2018, 5, 1, 9, 0, 0),
+        endDate: new Date(2018, 5, 1, 11, 45, 0)
+    }, {
+        text: "Submit Questions Regarding New NDA",
+        priorityId: 1,
+        startDate: new Date(2018, 4, 30, 10, 0, 0),
+        endDate: new Date(2018, 4, 30, 11, 30, 0)
+    }, {
+        text: "Submit Signed NDA",
+        priorityId: 1,
+        startDate: new Date(2018, 4, 30, 13, 0, 0),
+        endDate: new Date(2018, 4, 30, 15, 0, 0)
+    }, {
+        text: "Review Revenue Projections",
+        priorityId: 2,
+        startDate: new Date(2018, 4, 30, 11, 0, 0),
+        endDate: new Date(2018, 4, 30, 14, 0, 0)
+    }, {
+        text: "Comment on Revenue Projections",
+        priorityId: 2,
+        startDate: new Date(2018, 4, 28, 10, 0, 0),
+        endDate: new Date(2018, 4, 28, 13, 0, 0)
+    }, {
+        text: "Provide New Health Insurance Docs",
+        priorityId: 2,
+        startDate: new Date(2018, 5, 1, 12, 0, 0),
+        endDate: new Date(2018, 5, 1, 15, 0, 0)
+    }, {
+        text: "Review Changes to Health Insurance Coverage",
+        priorityId: 2,
+        startDate: new Date(2018, 4, 31, 9, 0, 0),
+        endDate: new Date(2018, 4, 31, 13, 0, 0)
+    }, {
+        text: "Review Training Course for any Ommissions",
+        priorityId: 1,
+        startDate: new Date(2018, 4, 31, 11, 0, 0),
+        endDate: new Date(2018, 4, 31, 14, 0, 0)
+    }
+];
