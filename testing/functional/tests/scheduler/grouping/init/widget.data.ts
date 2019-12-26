46eb5776604615ffc13e256d52eaa640f0b45342
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2019-12-26 14:45:40 +0300

    Fix allDay appointments rendering when groupByDate = true (T845632) (#11298)

---
+++ testing/functional/tests/scheduler/grouping/init/widget.data.ts
@@ -0,0 +1,18 @@
+export const dataSource = [
+    {
+        text: "Website Re-Design Plan",
+        priorityId: 2,
+        startDate: new Date(2018, 4, 21, 9, 30),
+        endDate: new Date(2018, 4, 21, 11, 30)
+    }, {
+        text: "Book Flights to San Fran for Sales Trip",
+        priorityId: 1,
+        startDate: new Date(2018, 4, 24, 10, 0),
+        endDate: new Date(2018, 4, 24, 12, 0),
+    }, {
+        text: "Install New Router in Dev Room",
+        priorityId: 1,
+        startDate: new Date(2018, 4, 20, 13),
+        endDate: new Date(2018, 4, 20, 15, 30)
+    }
+];
