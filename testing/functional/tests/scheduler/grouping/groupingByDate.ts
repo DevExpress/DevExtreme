3e6191be01b9120288cb25cb2cad411eedc88713
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2019-12-26 14:45:40 +0300

    Fix allDay appointments rendering when groupByDate = true (T845632) (#11298)

---
+++ testing/functional/tests/scheduler/grouping/groupingByDate.ts
@@ -0,0 +1,21 @@
+import { dataSource } from './init/widget.data';
+import { createScheduler } from './init/widget.setup';
+import url from '../../../helpers/getPageUrl';
+import Scheduler from '../../../model/scheduler';
+
+fixture `Drag-and-drop appointments into allDay panel in the grouped Scheduler `
+    .page(url(__dirname, '../../container.html'));
+
+test("Drag-n-drop between dateTable and allDay panel, groupByDate=true", async t => {
+    const scheduler = new Scheduler("#container");
+    const draggableAppointment = scheduler.getAppointment("Website Re-Design Plan");
+
+    await t
+        .dragToElement(draggableAppointment.element, scheduler.getAllDayTableCell(1))
+        .expect(draggableAppointment.size.width).eql("111px")
+        .expect(draggableAppointment.isAllDay).ok();
+
+}).before(() => createScheduler({
+    dataSource: dataSource,
+    groupByDate: true
+}));
