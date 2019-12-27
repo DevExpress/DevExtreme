eea4502483c2fe958703e2441c23c565f859cb7c
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2019-12-27 12:03:11 +0300

    small fix

--- js/ui/scheduler/ui.scheduler.subscribes.js
+++ js/ui/scheduler/ui.scheduler.subscribes.js
@@ -426,7 +426,7 @@ const subscribes = {
 
         if(endDate.getHours() >= endDayHour) {
             updatedEndDate.setHours(endDayHour, 0, 0, 0);
-        } else if(startDayHour > 0 && (endDate.getHours() * 60 + endDate.getMinutes() < (startDayHour * 60)) && !options.sameDate) {
+        } else if(!options.sameDate && startDayHour > 0 && (endDate.getHours() * 60 + endDate.getMinutes() < (startDayHour * 60))) {
             updatedEndDate = new Date(updatedEndDate.getTime() - toMs('day'));
             updatedEndDate.setHours(endDayHour, 0, 0, 0);
         }
