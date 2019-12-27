76c50e5a4bf05b84fbafffd91c0a1e730e16dfda
Andrey Ignatovskiy <43685423+LazyLahtak@users.noreply.github.com>
2019-12-27 16:27:12 +0300

    Renovation: add ES2020 features using babel's plugins (#11291)

--- docker-ci.sh
+++ docker-ci.sh
@@ -8,7 +8,7 @@ export DEVEXTREME_DOCKER_CI=true
 export NUGET_PACKAGES=$PWD/dotnet_packages
 
 function run_lint {
-    npm i eslint eslint-plugin-spellcheck stylelint stylelint-config-standard npm-run-all
+    npm i eslint eslint-plugin-spellcheck stylelint stylelint-config-standard npm-run-all babel-eslint
     npm run lint
 }
 
