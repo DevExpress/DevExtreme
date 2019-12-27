089e99c6d6403ddf0df72664a5628c62d0fcc4b3
Igor Maltsev <igor_malzew@mail.ru>
2019-12-27 15:08:32 +0300

    FilterBuilder - Deleting filter conditions leave empty nodes (T846991) (#11299)
    * FilterBuilder - Deleting filter conditions leave empty nodes (T846991)
    
    * Fix condition style

--- js/ui/filter_builder/filter_builder.js
+++ js/ui/filter_builder/filter_builder.js
@@ -881,7 +881,12 @@ var FilterBuilder = Widget.inherit({
 
         this._createRemoveButton(() => {
             utils.removeItem(parent, condition);
-            $item.remove();
+            const isSingleChild = $item.parent().children().length === 1;
+            if(isSingleChild) {
+                $item.parent().remove();
+            } else {
+                $item.remove();
+            }
             this._updateFilter();
         }).appendTo($item);
         this._createFieldButtonWithMenu(fields, condition, field).appendTo($item);
