4f8828be9f15e4665dd11faf697ed339b5dfc164
Reshetnikov.Roman <roman.reshetnikov@devexpress.com>
2020-01-09 18:03:13 +0300

    dxDiagram - itemType, public diagramitem

--- js/ui/diagram/diagram_item.js
+++ js/ui/diagram/diagram_item.js
@@ -1,7 +1,19 @@
 /**
     * @name dxDiagramItem
     * @type object
-*/
+    */
+/**
+    * @name dxDiagramItem.id
+    * @type String
+    */
+/**
+    * @name dxDiagramItem.dataItem
+    * @type object
+    */
+/**
+    * @name dxDiagramItem.itemType
+    * @type Enums.DiagramItemType
+    */
 
 
 /**
@@ -9,10 +21,30 @@
     * @inherits dxDiagramItem
     * @type object
     */
+/**
+    * @name dxDiagramShape.text
+    * @type String
+    */
+/**
+    * @name dxDiagramShape.type
+    * @type Enums.DiagramShapeType|String
+    */
 
 
 /**
     * @name dxDiagramConnector
     * @inherits dxDiagramItem
+    * @type object
+    */
+/**
+    * @name dxDiagramConnector.texts
+    * @type Array<String>
+    */
+/**
+    * @name dxDiagramConnector.fromKey
+    * @type object
+    */
+/**
+    * @name dxDiagramConnector.toKey
     * @type object
     */
