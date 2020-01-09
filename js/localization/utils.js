95feabc2e6bef847d284c1108eb0b36a01d1a3cc
dmitry.levkovskiy <dmitry.levkovskiy@devexpress.com>
2020-01-09 15:32:17 +0300

    Fix "fixedPoint" formatting with specific precision (T848392)

--- js/localization/utils.js
+++ js/localization/utils.js
@@ -1,19 +1,10 @@
-import { sign } from '../core/utils/math';
-
 const DECIMAL_BASE = 10;
-
-function roundByAbs(value) {
-    const valueSign = sign(value);
-
-    return valueSign * Math.round(Math.abs(value));
-}
 
 function adjustValue(value, precision) {
     const precisionMultiplier = Math.pow(DECIMAL_BASE, precision);
     const roundMultiplier = precisionMultiplier * DECIMAL_BASE;
-    const intermediateValue = roundByAbs(value * roundMultiplier) / DECIMAL_BASE;
 
-    return roundByAbs(intermediateValue) / precisionMultiplier;
+    return Math.round((value * roundMultiplier) / DECIMAL_BASE) / precisionMultiplier;
 }
 
 function toFixed(value, precision) {
