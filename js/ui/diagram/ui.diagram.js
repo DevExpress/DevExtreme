5ac996abd52877eeedb660b45a69e319250bf302
Roman Resh <roman.reshetnikov@devexpress.com>
2020-01-10 10:35:04 +0300

    dxDiagram - fixes, extend DiagramItem docs, publish itemType (#11417)
    * dxDiagram - itemType, public diagramitem
    
    * up version
    
    * diagram - update ts

--- js/ui/diagram/ui.diagram.js
+++ js/ui/diagram/ui.diagram.js
@@ -1469,6 +1469,7 @@ class Diagram extends Widget {
     _nativeShapeToDiagramShape(nativeShape) {
         return {
             dataItem: this._nodesOption && this._nodesOption.findItem(nativeShape.key),
+            itemType: 'shape',
             text: nativeShape.text,
             type: nativeShape.type
         };
@@ -1476,6 +1477,7 @@ class Diagram extends Widget {
     _nativeConnectorToDiagramConnector(nativeConnector) {
         return {
             dataItem: this._edgesOption && this._edgesOption.findItem(nativeConnector.key),
+            itemType: 'connector',
             texts: nativeConnector.texts,
             fromKey: nativeConnector.fromKey,
             toKey: nativeConnector.toKey
