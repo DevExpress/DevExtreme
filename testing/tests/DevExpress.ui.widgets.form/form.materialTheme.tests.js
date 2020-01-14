c3ed7708d258fc7ed7c0b3eba731c9f68918e891
Sergey Novikov <novsstation@yandex.ru>
2020-01-14 14:26:29 +0300

    Tests for all sizes

--- testing/tests/DevExpress.ui.widgets.form/form.materialTheme.tests.js
+++ testing/tests/DevExpress.ui.widgets.form/form.materialTheme.tests.js
@@ -20,71 +20,74 @@ QUnit.testStart(function() {
 
 QUnit.module('Form -> material theme');
 
-QUnit.test('Form have correct padding (T849353)', function(assert) {
-    const $form = $('#form').dxForm({
-        colCount: 1,
-        items: [ {
-            itemType: 'group',
-            caption: 'System Information',
-            items: [
-                'input1', {
-                    itemType: 'group',
-                    colCount: 4,
-                    items: [{
+['xs', 'sm', 'md', 'lg'].forEach(screenSize => {
+    QUnit.test(`Form have correct padding (T849353) -> ScreenSize: '${screenSize}'`, function(assert) {
+        const $form = $('#form').dxForm({
+            colCount: 1,
+            screenByWidth: () => { return screenSize; },
+            items: [ {
+                itemType: 'group',
+                caption: 'System Information',
+                items: [
+                    'dataField1', {
                         itemType: 'group',
-                        colSpan: 3,
-                        items:
-                            ['input2']
-                    }, 'input3']
-                }, {
-                    itemType: 'group',
-                    colCount: 2,
-                    items: ['input4', 'input5', {
-                        itemType: 'group',
-                        colCount: 1,
-                        colSpan: 2,
-                        items: ['input6']
+                        colCount: 4,
+                        items: [{
+                            itemType: 'group',
+                            colSpan: 3,
+                            items:
+                                ['dataField2']
+                        }, 'dataField3']
                     }, {
                         itemType: 'group',
                         colCount: 2,
-                        items: [ 'input7', {
+                        items: ['dataField4', 'dataField5', {
+                            itemType: 'group',
+                            colCount: 1,
+                            colSpan: 2,
+                            items: ['dataField6']
+                        }, {
                             itemType: 'group',
                             colCount: 2,
-                            items: ['input8', 'input9']
-                        } ]
-                    }, {
-                        itemType: 'group',
-                        colCount: 2,
-                        items: [ 'input10', 'input11' ]
-                    }] },
-                'input12']
-        }]
-    });
+                            items: [ 'dataField7', {
+                                itemType: 'group',
+                                colCount: 2,
+                                items: ['dataField8', 'dataField9']
+                            } ]
+                        }, {
+                            itemType: 'group',
+                            colCount: 2,
+                            items: [ 'dataField10', 'dataField11' ]
+                        }] },
+                    'dataField12']
+            }]
+        });
 
-    const $items = $form.find('input');
+        const $items = $form.find('input');
 
-    function getPadding(itemIndex1, itemIndex2) {
-        const item1Rect = $items.get(itemIndex1).getBoundingClientRect();
-        const item2Rect = $items.get(itemIndex2).getBoundingClientRect();
+        function getPadding(itemIndex1, itemIndex2) {
+            const item1Rect = $items.get(itemIndex1).getBoundingClientRect();
+            const item2Rect = $items.get(itemIndex2).getBoundingClientRect();
 
-        const paddingY = item1Rect.top - (item2Rect.top);
-        const paddingX = item1Rect.left - (item2Rect.left + item2Rect.width);
+            const paddingY = item1Rect.top - item2Rect.top;
+            const paddingX = item1Rect.left - item2Rect.right;
 
-        return { paddingX, paddingY };
-    }
-
-    const expectedPaddingY = getPadding(1, 0).paddingY;
-    const expectedPaddingX = getPadding(4, 3).paddingX;
-
-    $items.each(function(index, item) {
-        if(index !== 0) {
-            const padding = getPadding(index, index - 1);
-            if(padding.paddingY !== 0) {
-                assert.equal(padding.paddingY, expectedPaddingY, `paddingY between ${index - 1} and ${index} elements`);
-            } else {
-                assert.equal(padding.paddingX, expectedPaddingX, `paddingX between ${index - 1} and ${index} elements`);
-            }
+            return { paddingX, paddingY, isSameRow: item1Rect.top === item2Rect.top };
         }
+
+        const expectedPaddingY = getPadding(1, 0).paddingY;
+        const expectedPaddingX = getPadding(4, 3).paddingX;
+
+        $items.each(function(index, item) {
+            if(index !== 0) {
+                const padding = getPadding(index, index - 1);
+                if(padding.isSameRow) {
+                    assert.equal(padding.paddingX, expectedPaddingX, `paddingX between ${index - 1} and ${index} elements`);
+                } else {
+                    assert.equal(padding.paddingY, expectedPaddingY, `paddingY between ${index - 1} and ${index} elements`);
+                }
+            }
+        });
     });
 });
 
