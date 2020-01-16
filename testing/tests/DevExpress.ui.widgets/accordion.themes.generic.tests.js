ef36bda8635376837c46ea557f4ba679331e1162
ignatov <ignatovdan@gmail.com>
2020-01-16 17:19:23 +0300

    Accordion - center a dxButton icon in a custom itemTitleTemplate

---
+++ testing/tests/DevExpress.ui.widgets/accordion.themes.generic.tests.js
@@ -0,0 +1,5 @@
+import 'common.css!';
+import 'generic_light.css!';
+
+import { runThemesSharedTests } from './accordionParts/accordion.sharedThemes.tests.js';
+runThemesSharedTests('generic_light');
