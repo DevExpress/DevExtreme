46510d780d91af20c013d98370491674997bdc7b
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2020-01-16 13:25:31 +0300

    DataGrid - Splitting "keyboardNavigation.tests.js" into several tests by modules (#11523)

--- testing/tests/DevExpress.ui.widgets.dataGrid/keyboardNavigation.rowsView.tests.js
+++ testing/tests/DevExpress.ui.widgets.dataGrid/keyboardNavigation.rowsView.tests.js
@@ -147,173 +147,173 @@ QUnit.module('Rows view', {
     afterEach: function() {
         this.clock.restore();
     }
-});
+}, function() {
+    // T222258
+    QUnit.testInActiveWindow('Focused cell from free space row when view is rendered', function(assert) {
+        // arrange
+        const $container = $('#container');
+        let origUpdateFocus;
 
-// T222258
-QUnit.testInActiveWindow('Focused cell from free space row when view is rendered', function(assert) {
-    // arrange
-    const $container = $('#container');
-    let origUpdateFocus;
+        setupModules(this);
+        this.gridView.render($container);
+        this.keyboardNavigationController._focusedView = this.rowsView;
+        this.keyboardNavigationController._isNeedFocus = true;
 
-    setupModules(this);
-    this.gridView.render($container);
-    this.keyboardNavigationController._focusedView = this.rowsView;
-    this.keyboardNavigationController._isNeedFocus = true;
+        origUpdateFocus = this.keyboardNavigationController._updateFocus;
+        this.keyboardNavigationController._updateFocus = function() {
+            origUpdateFocus.apply(this, arguments);
 
-    origUpdateFocus = this.keyboardNavigationController._updateFocus;
-    this.keyboardNavigationController._updateFocus = function() {
-        origUpdateFocus.apply(this, arguments);
+            // assert
+            assert.ok(true);
+        };
+
+        // act
+        $($container.find('.dx-freespace-row').find('td').first()).trigger('dxpointerdown');
+        this.rowsView.renderCompleted.fire();
+    });
+
+    QUnit.testInActiveWindow('Cell is not focused when view is rendered if key is not pressed', function(assert) {
+        // arrange, act
+        let isCellFocused = false;
+        const $container = $('#container');
+
+        setupModules(this);
+
+        // act
+        this.gridView.render($container);
+        this.keyboardNavigationController._focusedView = this.rowsView;
+        this.keyboardNavigationController._isNeedScroll = true;
+        this.keyboardNavigationController._focusedCellPosition = { columnIndex: 0, rowIndex: 0 };
+        this.keyboardNavigationController._focus(this.gridView.element().find('td').eq(4));
+
+        this.clock.tick();
+
+        this.keyboardNavigationController._focusedCellPosition = { columnIndex: 0, rowIndex: 0 };
+        this.keyboardNavigationController._focus = function() {
+            isCellFocused = true;
+        };
+        this.rowsView.renderCompleted.fire();
+
+        this.clock.tick();
 
         // assert
-        assert.ok(true);
-    };
+        assert.ok(!isCellFocused);
+        assert.ok(!this.keyboardNavigationController._isNeedFocus);
+    });
 
-    // act
-    $($container.find('.dx-freespace-row').find('td').first()).trigger('dxpointerdown');
-    this.rowsView.renderCompleted.fire();
-});
+    QUnit.testInActiveWindow('Render rows view with keyboard navigation', function(assert) {
+        // arrange
+        const rowsView = this.createRowsView(this.items);
+        const testElement = $('#container');
 
-QUnit.testInActiveWindow('Cell is not focused when view is rendered if key is not pressed', function(assert) {
-    // arrange, act
-    let isCellFocused = false;
-    const $container = $('#container');
+        // act
+        rowsView.render(testElement);
 
-    setupModules(this);
+        // assert
+        assert.strictEqual(rowsView.element().attr('tabindex'), undefined, 'no tabindex on rowsView element');
+        assert.strictEqual(rowsView.element().find('td[tabIndex]').length, 1, 'cells with tabIndex attr count');
+    });
 
-    // act
-    this.gridView.render($container);
-    this.keyboardNavigationController._focusedView = this.rowsView;
-    this.keyboardNavigationController._isNeedScroll = true;
-    this.keyboardNavigationController._focusedCellPosition = { columnIndex: 0, rowIndex: 0 };
-    this.keyboardNavigationController._focus(this.gridView.element().find('td').eq(4));
+    // T391194, T380140
+    QUnit.testInActiveWindow('Tab from focused element before rowsview must focus first cell', function(assert) {
+        // arrange
+        const rowsView = this.createRowsView(this.items);
+        const testElement = $('.dx-datagrid');
 
-    this.clock.tick();
+        rowsView.render(testElement);
+        this.clock.tick();
 
-    this.keyboardNavigationController._focusedCellPosition = { columnIndex: 0, rowIndex: 0 };
-    this.keyboardNavigationController._focus = function() {
-        isCellFocused = true;
-    };
-    this.rowsView.renderCompleted.fire();
+        // act
+        const $focusable = testElement.find('[tabIndex]').first();
+        $focusable.focus();
+        this.clock.tick();
 
-    this.clock.tick();
+        // assert
+        assert.ok($focusable.is('td'), 'focusable is cell');
+        assert.strictEqual($focusable.text(), 'test1', 'focused cell text');
+        assert.strictEqual($focusable.index(), 0, 'focused cell columnIndex');
+        assert.strictEqual($focusable.parent().index(), 0, 'focused cell rowIndex');
+        assert.ok($focusable.is(':focus'), 'focused cell is focused');
+        assert.ok($focusable.hasClass('dx-focused'), 'focused cell has dx-focused class');
+    });
 
-    // assert
-    assert.ok(!isCellFocused);
-    assert.ok(!this.keyboardNavigationController._isNeedFocus);
-});
+    QUnit.testInActiveWindow('Skip invalid cell for moving to right', function(assert) {
+        // arrange
+        const rowsView = this.createRowsView(this.items, null, [{}, {}, {}, {}]);
+        const navigationController = this.dataGrid.keyboardNavigationController;
+        let $cell;
 
-QUnit.testInActiveWindow('Render rows view with keyboard navigation', function(assert) {
-    // arrange
-    const rowsView = this.createRowsView(this.items);
-    const testElement = $('#container');
-
-    // act
-    rowsView.render(testElement);
-
-    // assert
-    assert.strictEqual(rowsView.element().attr('tabindex'), undefined, 'no tabindex on rowsView element');
-    assert.strictEqual(rowsView.element().find('td[tabIndex]').length, 1, 'cells with tabIndex attr count');
-});
-
-// T391194, T380140
-QUnit.testInActiveWindow('Tab from focused element before rowsview must focus first cell', function(assert) {
-    // arrange
-    const rowsView = this.createRowsView(this.items);
-    const testElement = $('.dx-datagrid');
-
-    rowsView.render(testElement);
-    this.clock.tick();
-
-    // act
-    const $focusable = testElement.find('[tabIndex]').first();
-    $focusable.focus();
-    this.clock.tick();
-
-    // assert
-    assert.ok($focusable.is('td'), 'focusable is cell');
-    assert.strictEqual($focusable.text(), 'test1', 'focused cell text');
-    assert.strictEqual($focusable.index(), 0, 'focused cell columnIndex');
-    assert.strictEqual($focusable.parent().index(), 0, 'focused cell rowIndex');
-    assert.ok($focusable.is(':focus'), 'focused cell is focused');
-    assert.ok($focusable.hasClass('dx-focused'), 'focused cell has dx-focused class');
-});
-
-QUnit.testInActiveWindow('Skip invalid cell for moving to right', function(assert) {
-    // arrange
-    const rowsView = this.createRowsView(this.items, null, [{}, {}, {}, {}]);
-    const navigationController = this.dataGrid.keyboardNavigationController;
-    let $cell;
-
-    navigationController._isCellValid = function($cell) {
-        const cell = $cell[0];
-        return cell.cellIndex > 0 && cell.cellIndex < 2;
-    };
-    navigationController._focusedView = rowsView;
-    navigationController._focusedCellPosition = { rowIndex: 0, columnIndex: 1 };
-    rowsView.render($('#container'));
-
-    // assert, act
-    $cell = navigationController._getNextCell.call(navigationController, 'nextInRow');
-    assert.equal($cell[0].cellIndex, 3);
-});
-
-QUnit.testInActiveWindow('Skip invalid cell for moving to left', function(assert) {
-    // arrange
-    const rowsView = this.createRowsView(this.items, null, [{}, {}, {}, {}]);
-    const navigationController = this.dataGrid.keyboardNavigationController;
-    let $cell;
-
-    navigationController._isCellValid = function($cell) {
-        const cell = $cell[0];
-        return cell.cellIndex < 3 && cell.cellIndex !== 2 && cell.cellIndex !== 1 && cell.cellIndex >= 0;
-    };
-    navigationController._focusedView = rowsView;
-    navigationController._focusedCellPosition = { rowIndex: 0, columnIndex: 3 };
-    rowsView.render($('#container'));
-
-    // assert, act
-    $cell = navigationController._getNextCell.call(navigationController, 'previousInRow');
-    assert.equal($cell[0].cellIndex, 0);
-});
-
-QUnit.test('Focused state is not applied when element is not cell', function(assert) {
-    // arrange
-    const rowsView = this.createRowsView(this.items, null, [{}, {}, {}, {}]);
-    const $element = $('<div>');
-
-    this.dataGrid.getController('keyboardNavigation')._isCellValid = function($cell) {
-        return true;
-    };
-
-    rowsView.element = function() {
-        return {
-            attr: commonUtils.noop,
-            is: commonUtils.noop
+        navigationController._isCellValid = function($cell) {
+            const cell = $cell[0];
+            return cell.cellIndex > 0 && cell.cellIndex < 2;
         };
-    };
-    rowsView.getCellElements = function() {
-        return $element;
-    };
+        navigationController._focusedView = rowsView;
+        navigationController._focusedCellPosition = { rowIndex: 0, columnIndex: 1 };
+        rowsView.render($('#container'));
 
-    // act
-    rowsView.renderFocusState();
+        // assert, act
+        $cell = navigationController._getNextCell.call(navigationController, 'nextInRow');
+        assert.equal($cell[0].cellIndex, 3);
+    });
 
-    // assert
-    assert.ok(!$element.attr('tabIndex'));
-});
+    QUnit.testInActiveWindow('Skip invalid cell for moving to left', function(assert) {
+        // arrange
+        const rowsView = this.createRowsView(this.items, null, [{}, {}, {}, {}]);
+        const navigationController = this.dataGrid.keyboardNavigationController;
+        let $cell;
 
-QUnit.test('Apply custom tabIndex to rows view on click', function(assert) {
-    // arrange
-    const rowsView = this.createRowsView(this.items);
-    const testElement = $('#container');
+        navigationController._isCellValid = function($cell) {
+            const cell = $cell[0];
+            return cell.cellIndex < 3 && cell.cellIndex !== 2 && cell.cellIndex !== 1 && cell.cellIndex >= 0;
+        };
+        navigationController._focusedView = rowsView;
+        navigationController._focusedCellPosition = { rowIndex: 0, columnIndex: 3 };
+        rowsView.render($('#container'));
 
-    this.options.tabIndex = 5;
+        // assert, act
+        $cell = navigationController._getNextCell.call(navigationController, 'previousInRow');
+        assert.equal($cell[0].cellIndex, 0);
+    });
 
-    // act
-    rowsView.render(testElement);
+    QUnit.test('Focused state is not applied when element is not cell', function(assert) {
+        // arrange
+        const rowsView = this.createRowsView(this.items, null, [{}, {}, {}, {}]);
+        const $element = $('<div>');
 
-    const $cell = $(rowsView.element().find('td').first());
-    $cell.trigger(CLICK_EVENT);
-    assert.equal(rowsView.element().attr('tabIndex'), undefined, 'tabIndex of rowsView');
-    assert.equal($cell.attr('tabIndex'), 5, 'tabIndex of clicked cell');
+        this.dataGrid.getController('keyboardNavigation')._isCellValid = function($cell) {
+            return true;
+        };
+
+        rowsView.element = function() {
+            return {
+                attr: commonUtils.noop,
+                is: commonUtils.noop
+            };
+        };
+        rowsView.getCellElements = function() {
+            return $element;
+        };
+
+        // act
+        rowsView.renderFocusState();
+
+        // assert
+        assert.ok(!$element.attr('tabIndex'));
+    });
+
+    QUnit.test('Apply custom tabIndex to rows view on click', function(assert) {
+        // arrange
+        const rowsView = this.createRowsView(this.items);
+        const testElement = $('#container');
+
+        this.options.tabIndex = 5;
+
+        // act
+        rowsView.render(testElement);
+
+        const $cell = $(rowsView.element().find('td').first());
+        $cell.trigger(CLICK_EVENT);
+        assert.equal(rowsView.element().attr('tabIndex'), undefined, 'tabIndex of rowsView');
+        assert.equal($cell.attr('tabIndex'), 5, 'tabIndex of clicked cell');
+    });
 });
