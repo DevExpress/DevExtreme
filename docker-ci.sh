65d507ed7fda7c9885ac143cee2c8d9adcd92b8d
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2020-01-17 17:31:38 +0300

    Drone CI - Add performance tests (#11538)
    * Drone CI - Add performance tests
    
    * Correct test server url - Remove "jquery=" param

--- docker-ci.sh
+++ docker-ci.sh
@@ -44,6 +44,7 @@ function run_test {
     [ -n "$CONSTEL" ] && url="$url&constellation=$CONSTEL"
     [ -n "$MOBILE_UA" ] && url="$url&deviceMode=true"
     [ -z "$JQUERY"  ] && url="$url&nojquery=true"
+    [ -n "$PERF" ] && url="$url&include=DevExpress.performance&workerInWindow=true"
 
     if [ "$NO_HEADLESS" == "true" ]; then
         Xvfb :99 -ac -screen 0 1200x600x24 &
@@ -90,6 +91,16 @@ function run_test {
                     --no-first-run \
                     --no-default-browser-check \
                     --disable-translate";
+            fi
+
+            if [ "$PERF" == "true" ]; then
+                echo "Performance tests"
+                chrome_command="$chrome_command \
+                    --disable-popup-blocking \
+                    --remote-debugging-port=9223 \
+                    --enable-impl-side-painting \
+                    --enable-skia-benchmarking \
+                    --disable-web-security"
             fi
 
             if [ -n "$MOBILE_UA" ]; then
