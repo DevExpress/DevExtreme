5d3a66411a49db7a5b06bffa90917d0c372df919
Igor Maltsev <igor_malzew@mail.ru>
2020-01-17 19:13:01 +0300

    DataGrid - DateBox does not update a cell via a keyboard (T848039 / Refix T819067) (#11543)
    * DataGrid - DateBox does not update a cell via a keyboard (T848039 / Refix T819067)
    
    (cherry picked from commit 7f89663d715a6139fa6e57bb23db8b10af36aa88)
    
    * Fix test
    
    (cherry picked from commit ec59d115f3c908473471b81aead49f2e746e40d7)

--- testing/tests/DevExpress.ui.widgets.dataGrid/dataGrid.tests.js
+++ testing/tests/DevExpress.ui.widgets.dataGrid/dataGrid.tests.js
@@ -12836,7 +12836,8 @@ QUnit.testInActiveWindow('Datebox editor\'s enter key handler should be replaced
 
     // assert
     const dateBox = rowsViewWrapper.getEditor(0, 0).dxDateBox('instance');
-    assert.equal(dateBox._enterHandler, commonUtils.noop, 'dateBox enter key handler for mask is replaced');
+    const enterKeyHandler = dateBox._supportedKeys().enter;
+    assert.strictEqual(enterKeyHandler(), true, 'dateBox enter key handler is replaced');
 });
 
 QUnit.testInActiveWindow('Datebox editor\'s value should be selected from calendar by keyboard (T848039)', function(assert) {
