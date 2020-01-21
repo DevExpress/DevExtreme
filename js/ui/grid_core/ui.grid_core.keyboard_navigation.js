4984bc3a08d374493fa656f30d17cd1c5c59fff9
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2020-01-21 18:24:25 +0300

    DataGrid - Focused row should not being reseted after begin edit row if form edit mode (T851400)

--- js/ui/grid_core/ui.grid_core.keyboard_navigation.js
+++ js/ui/grid_core/ui.grid_core.keyboard_navigation.js
@@ -722,6 +722,12 @@ const KeyboardNavigationController = core.ViewController.inherit({
         const $parent = $target.parent();
         const isEditingRow = $parent.hasClass(EDIT_ROW_CLASS);
         const isInteractiveElement = $(event.target).is(INTERACTIVE_ELEMENTS_SELECTOR);
+        const isCommandButtonTarget = $(event.target).hasClass('dx-link');
+
+        if(isCommandButtonTarget) {
+            this.setCellFocusType();
+            return;
+        }
 
         if(this._isEventInCurrentGrid(event) && this._isCellValid($target, !isInteractiveElement)) {
             $target = this._isInsideEditForm($target) ? $(event.target) : $target;
@@ -884,7 +890,7 @@ const KeyboardNavigationController = core.ViewController.inherit({
     },
 
     _focus: function($cell, disableFocus, isInteractiveElement) {
-        const $row = ($cell && $cell.is('td')) ? $cell.parent() : $cell;
+        const $row = ($cell && !$cell.hasClass(ROW_CLASS)) ? $cell.closest(`.${ROW_CLASS}`) : $cell;
 
         if($row && isNotFocusedRow($row)) {
             return;
@@ -1569,8 +1575,7 @@ const KeyboardNavigationController = core.ViewController.inherit({
     },
 
     _getRowIndex: function($row) {
-        const that = this;
-        const focusedView = that._focusedView;
+        const focusedView = this._focusedView;
         let rowIndex = -1;
 
         if(focusedView) {
@@ -1578,7 +1583,7 @@ const KeyboardNavigationController = core.ViewController.inherit({
         }
 
         if(rowIndex >= 0) {
-            rowIndex += that._dataController.getRowIndexOffset();
+            rowIndex += this.getController('data').getRowIndexOffset();
         }
 
         return rowIndex;
@@ -1678,7 +1683,7 @@ const KeyboardNavigationController = core.ViewController.inherit({
     },
 
     _getCellElementFromTarget: function(target) {
-        return $(target).closest('.' + ROW_CLASS + '> td');
+        return $(target).closest(`.${ROW_CLASS} > td`);
     },
 
     _getRowsViewElement: function() {
@@ -1737,8 +1742,6 @@ module.exports = {
                  */
                 editOnKeyPress: false
             }
-
-
         };
     },
     controllers: {
