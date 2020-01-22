fcff387ff64be4c8bea019ec8ed5018a96899245
Andrey Ignatovskiy <43685423+LazyLahtak@users.noreply.github.com>
2020-01-22 15:06:51 +0300

    Back hasWindow variable in window (#11644)

--- js/core/utils/window.js
+++ js/core/utils/window.js
@@ -2,18 +2,20 @@
 
 const domAdapter = require('../dom_adapter');
 
-const hasWindow = () => typeof window !== 'undefined';
+const hasWindow = typeof window !== 'undefined';
 
-let windowObject = hasWindow() && window;
+let windowObject = hasWindow && window;
 
 if(!windowObject) {
     windowObject = {};
     windowObject.window = windowObject;
 }
 
+const hasWindowFn = () => hasWindow;
+
 const getWindow = () => windowObject;
 
-const hasProperty = (prop) => hasWindow() && prop in windowObject;
+const hasProperty = (prop) => hasWindowFn() && prop in windowObject;
 
 const defaultScreenFactorFunc = (width) => {
     if(width < 768) {
@@ -34,10 +36,10 @@ const getCurrentScreenFactor = (screenFactorCallback) => {
     return screenFactorFunc(windowWidth);
 };
 
-const getNavigator = () => hasWindow() ? windowObject.navigator : { userAgent: '' };
+const getNavigator = () => hasWindowFn() ? windowObject.navigator : { userAgent: '' };
 
 module.exports = {
-    hasWindow,
+    hasWindow: hasWindowFn,
     getWindow,
     hasProperty,
     defaultScreenFactorFunc,
