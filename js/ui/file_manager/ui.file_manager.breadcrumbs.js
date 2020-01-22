9ab0183cf01799fe801545c59f8e5f59b5747222
Stanislav Klesarev <klesarevklesarevstas@gmail.com>
2020-01-22 11:05:25 +0300

    Icon changed

--- js/ui/file_manager/ui.file_manager.breadcrumbs.js
+++ js/ui/file_manager/ui.file_manager.breadcrumbs.js
@@ -57,7 +57,7 @@ class FileManagerBreadcrumbs extends Widget {
 
         const result = [
             {
-                icon: 'arrowup',
+                icon: 'parentfolder',
                 directory: this._currentDirectory.parentDirectory,
                 isPathItem: true,
                 cssClass: FILE_MANAGER_BREADCRUMBS_PARENT_FOLDER_ITEM_CLASS
