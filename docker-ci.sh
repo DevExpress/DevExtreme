8e05b5ad10502d5ced746f9dff8b4b3884df5b05
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2020-01-23 10:53:38 +0300

    UpdateCI - Functional tests - Added testCafe quarantineMode parameter and applied it for Scheduler (#11646)
    * UpdateCI - Functional tests - Added testCafe quarantineMode parameter and applied it for scheduler.
    
    * A bit of refactoring
    
    * A bit of refactoring

--- docker-ci.sh
+++ docker-ci.sh
@@ -144,8 +144,9 @@ function run_test_functional {
     npm i
     npm run build
 
-    local args="--browsers chrome:headless";
-    [ "$COMPONENT" ] && args="$args --componentFolder $COMPONENT";
+    local args="--browsers=chrome:headless";
+    [ -n "$COMPONENT" ] && args="$args --componentFolder=$COMPONENT";
+    [ -n "$QUARANTINE_MODE" ] && args="$args --quarantineMode=true";
 
     npm run test-functional -- $args
 }
