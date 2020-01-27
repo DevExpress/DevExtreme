24b129fabf9ccad045cbf88ba7b98a8e52eb6529
Smirnova Yuliya <Krijovnick@users.noreply.github.com>
2020-01-27 13:10:16 +0300

    dxScheduler - fix recurrent appointment rendering with workWeek view (T853629) (#11660)

--- js/ui/scheduler/workspaces/ui.scheduler.work_space.js
+++ js/ui/scheduler/workspaces/ui.scheduler.work_space.js
@@ -2166,9 +2166,15 @@ const SchedulerWorkSpace = Widget.inherit({
             : 0;
     },
 
+    _isSkippedData: function() { return false; },
+
     getCoordinatesByDateInGroup: function(date, appointmentResources, inAllDayRow) {
         const indexes = this._getGroupIndexes(appointmentResources);
         const result = [];
+
+        if(this._isSkippedData(date)) {
+            return result;
+        }
 
         if(indexes.length) {
             for(let i = 0; i < indexes.length; i++) {
