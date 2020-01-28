697172ce7a5b64d677fd8d88ffe66c53bd28aebd
Stanislav Klesarev <klesarevklesarevstas@gmail.com>
2020-01-28 17:19:21 +0300

    File Manager - Change icon of filesView 'ParentFolder' item (#11634)
    * Icon changed
    
    * Breadcrumbs icon revert and filesView parentFolder icon change

--- js/ui/file_manager/ui.file_manager.js
+++ js/ui/file_manager/ui.file_manager.js
@@ -312,7 +312,7 @@ class FileManager extends Widget {
                     const itemInfosCopy = [...items];
                     itemInfosCopy.unshift({
                         fileItem: parentDirItem,
-                        icon: 'folder'
+                        icon: 'parentfolder'
                     });
                     return itemInfosCopy;
                 });
