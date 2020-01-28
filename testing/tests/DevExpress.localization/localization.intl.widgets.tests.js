5620e8e779e108851909d1d04c0e1d9c66b5d578
levkovskiy.dmitry <dmitry.levkovskiy@devexpress.com>
2020-01-28 11:15:15 +0300

    fix test

--- testing/tests/DevExpress.localization/localization.intl.widgets.tests.js
+++ testing/tests/DevExpress.localization/localization.intl.widgets.tests.js
@@ -1,5 +1,5 @@
-import dateLocalization from 'localization/date';
-import numberLocalization from 'localization/number';
+import 'localization/date';
+import 'localization/number';
 import { locale } from 'localization/core';
 
 import $ from 'jquery';
@@ -7,58 +7,25 @@ import 'ui/date_box';
 
 const TEXTEDITOR_INPUT_SELECTOR = '.dx-texteditor-input';
 
-const ROUNDING_BUG_NUMBERS = [4.645, -4.645, 35.855, -35.855];
-const ROUNDING_CORRECTION = {
-    '4.64': '4.65',
-    '-4.64': '-4.65',
-    '35.85': '35.86',
-    '-35.85': '-35.86'
-};
-
 const commonEnvironment = {
     beforeEach: function() {
         const markup = '<div id="dateBox"></div>';
 
-        $('#qunit').append(markup);
+        $('#qunit-fixture').append(markup);
     },
 
-    // afterEach: function() {
-    //     $('#qunit-fixture').empty();
-    // }
+    afterEach: function() {
+        $('#qunit-fixture').empty();
+    }
 };
 
-const patchPolyfillResults = () => {
-    dateLocalization.inject({
-        format: function(value, format) {
-            // NOTE: IntlPolyfill uses CLDR data, so it formats this format with ` at `, but real browser`s Intl uses `, ` separator.
-            let result = this.callBase.apply(this, arguments);
-            if(typeof result === 'string') {
-                result = result && result.replace(' at ', ', ');
-            }
-            return result;
-        }
-    });
 
-    numberLocalization.inject({
-        format: function(value, format) {
-            // NOTE: IntlPolifill rounding bug. In real Intl it works OK.
-            let result = this.callBase.apply(this, arguments);
-            if(ROUNDING_BUG_NUMBERS.indexOf(value) !== -1 && format.type === 'fixedPoint' && format.precision === 2 && !!ROUNDING_CORRECTION[result]) {
-                result = ROUNDING_CORRECTION[result];
-            }
-            return result;
-        }
-    });
-};
-
-QUnit.module('Intl localization', {
-    before: patchPolyfillResults
-}, () => {
+QUnit.module('Intl localization', () => {
     QUnit.module('DateBox', commonEnvironment, () => {
         QUnit.test('DateBox should not raise error when digits are not default arabic digits', function(assert) {
+            const currentLocale = locale();
             try {
                 locale('ar-u-nu-arab');
-
                 const $dateBox = $('#dateBox').dxDateBox({
                     value: new Date(2015, 10, 10),
                     type: 'date',
@@ -71,7 +38,7 @@ QUnit.module('Intl localization', {
             } catch(e) {
                 assert.ok(false, 'Error occured: ' + e.message);
             } finally {
-                // locale('en');
+                locale(currentLocale);
             }
         });
     });
