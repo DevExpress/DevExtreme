120614cf30324670ca064487467dcab9a4319f90
Dmitry Levkovskiy <DokaRus@users.noreply.github.com>
2020-01-30 18:21:23 +0300

    Add bundled tests for "onInitialized" and "onDisposing" events (#11762)
    * Add bundled tests for "onInitialized" and "onDisposing" events
    
    * add LayoutManager to tests
    
    Co-authored-by: AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>

---
+++ testing/tests/DevExpress.ui.widgets/init_dispose_widget_bundled_tests.js
@@ -0,0 +1,55 @@
+import GoogleStaticProvider from 'ui/map/provider.google_static';
+import fx from 'animation/fx';
+import { each } from 'core/utils/iterator';
+import 'bundles/modules/parts/widgets-all';
+
+const { module: testModule, test } = QUnit;
+
+testModule('Init and dispose', {
+    before: function() {
+        this.element = document.createElement('div');
+        document.getElementById('qunit-fixture').appendChild(this.element);
+    },
+    after: function() {
+        this.element.remove();
+    }
+}, function() {
+    fx.off = true;
+    GoogleStaticProvider.remapConstant('/mapURL?');
+
+    const getOptions = function(componentName, assert, done) {
+        const options = {
+            onInitialized: () => {
+                assert.ok(true, `${componentName} initialized`);
+            },
+            onDisposing: () => {
+                assert.ok(true, `${componentName} disposed`);
+                done();
+            }
+        };
+
+        switch(componentName) {
+            case 'dxValidator':
+                options.adapter = {};
+                break;
+            case 'dxMap':
+                options.provider = 'googleStatic';
+                break;
+        }
+
+        return options;
+    };
+
+    each(DevExpress.ui, function(componentName, componentConstructor) {
+        if(componentName.indexOf('dx') !== 0) {
+            return;
+        }
+
+        test(componentName, function(assert) {
+            assert.expect(2);
+            const done = assert.async();
+            const instance = new componentConstructor(this.element, getOptions(componentName, assert, done));
+            instance.dispose();
+        });
+    });
+});
