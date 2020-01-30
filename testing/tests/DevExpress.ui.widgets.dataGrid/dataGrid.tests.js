108bb3eb7c2f870110db506a2629dfb3b7b29e97
Jaan Toming <jtoming@gmail.com>
2020-01-30 12:03:10 +0300

    DataGrid: Fix selected checkboxes were not checked after page refresh if stateStoring is enabled and renderAsync is true (T857205)

--- testing/tests/DevExpress.ui.widgets.dataGrid/dataGrid.tests.js
+++ testing/tests/DevExpress.ui.widgets.dataGrid/dataGrid.tests.js
@@ -10124,6 +10124,51 @@ QUnit.test('cellTemplate should be rendered, asynchronously if column renderAsyn
     assert.equal(cellTemplateArgs[0].column.dataField, 'template', 'cell template column');
 });
 
+// T857205
+QUnit.test(' if renderAsync is true and state storing is used', function(assert) {
+    let selectedRowKeys = [1, 2];
+
+    const customLoad = sinon.spy(() => {
+        return {
+            selectedRowKeys: selectedRowKeys
+        };
+    });
+
+    // act
+    const grid = createDataGrid({
+        dataSource: [{ id: 1 }, { id: 2 }, { id: 3 }],
+        keyExpr: 'id',
+        loadingTimeout: undefined,
+        renderAsync: true,
+        filterRow: {
+            visible: true
+        },
+        selection: {
+            mode: 'multiple'
+        },
+        stateStoring: {
+            enabled: true,
+            type: 'custom',
+            customLoad
+        }
+    });
+
+    const $grid = grid.$element();
+    this.clock.tick();
+
+    const $selectCheckboxes = $grid.find('.dx-select-checkbox');
+    const $inputs = $selectCheckboxes.find('input');
+
+    // assert
+    assert.equal(customLoad.callCount, 1, 'customLoad was called once');
+
+    assert.deepEqual(grid.getSelectedRowKeys(), selectedRowKeys, 'selected row keys');
+
+    assert.equal($inputs.eq(1).prop('value'), 'true', 'first row checkbox');
+    assert.equal($inputs.eq(2).prop('value'), 'true', 'second row checkbox');
+    assert.equal($inputs.eq(3).prop('value'), 'false', 'third row checkbox');
+});
+
 QUnit.module('Assign options', baseModuleConfig);
 
 // B232542
