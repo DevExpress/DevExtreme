492641eb941563694ff893a6f2b5af1788c5b249
Andrew Makarov <andrey.makarov@devexpress.com>
2020-02-03 12:00:31 -0800

    Rename utils method. Add tests

--- testing/tests/DevExpress.ui.widgets.scheduler/helpers.js
+++ testing/tests/DevExpress.ui.widgets.scheduler/helpers.js
@@ -22,6 +22,18 @@ export class SchedulerTestWrapper {
 
         this.getTimePanel = () => $('.dx-scheduler-time-panel'),
 
+        this.timePanel = {
+            getElement: () => {
+                return $('.dx-scheduler-time-panel');
+            },
+            getTimeValues: () => {
+                const element = this.timePanel.getElement();
+                return element.find('.dx-scheduler-time-panel-cell > div').filter((i, el) => {
+                    return $(el).text() !== '';
+                }).map((i, el) => { return $(el).text(); });
+            }
+        },
+
         this.tooltip = {
             getOverlayContentElement: () => {
                 return this.isAdaptivity() ? this.tooltip.getContentElement().find('.dx-overlay-content') : $('.dx-scheduler-appointment-tooltip-wrapper .dx-overlay-content');
