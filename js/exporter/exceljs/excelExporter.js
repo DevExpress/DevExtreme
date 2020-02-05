91ecf5aa58de7010f0eed57d285ff1c32bb52069
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2020-02-05 16:52:45 +0300

    Revert "ExcelJS - add doс comments for arguments of the 'exportDataGrid' function (#11807)" (#11898)

--- js/exporter/exceljs/excelExporter.js
+++ js/exporter/exceljs/excelExporter.js
@@ -1,36 +1,3 @@
 import { exportDataGrid } from './exportDataGrid';
 
-/**
-* @name excelExporter
-* @section utils
-*/
-/**
-* @name CellAddress
-* @type object
-*/
-/**
-* @name ExportDataGridProps
-* @type object
-*/
-/**
-* @name CellsRange
-* @type object
-*/
-/**
-* @name ExportLoadPanel
-* @type object
-*/
-/**
-* @name ExcelDataGridCell
-* @type object
-*/
-/**
-* @name ExcelDataGridCell.groupSummaryItems.name
-* @type string
-*/
-/**
-* @name ExcelDataGridCell.groupSummaryItems.value
-* @type any
-*/
-
 export { exportDataGrid };
