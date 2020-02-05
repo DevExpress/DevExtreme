cb86ecca15db5e8eb697fd82a91b486bbcdc0b9a
Dmitry Levkovskiy <DokaRus@users.noreply.github.com>
2020-02-05 11:39:50 +0300

    Fix Calendar accessibility issues (#11846)

--- js/ui/calendar/ui.calendar.js
+++ js/ui/calendar/ui.calendar.js
@@ -491,10 +491,6 @@ const Calendar = Editor.inherit({
         this._renderSwipeable();
         this._renderFooter();
 
-        this.setAria({
-            'role': 'listbox',
-            'label': messageLocalization.format('dxCalendar-ariaWidgetName')
-        });
         this._updateAriaSelected();
         this._updateAriaId();
 
@@ -557,7 +553,7 @@ const Calendar = Editor.inherit({
             max: this._getMaxDate(),
             firstDayOfWeek: this.option('firstDayOfWeek'),
             value: this._dateOption('value'),
-            rtl: this.option('rtlEnabled'),
+            rtlEnabled: this.option('rtlEnabled'),
             disabled: this.option('disabled'),
             tabIndex: undefined,
             focusStateEnabled: this.option('focusStateEnabled'),
