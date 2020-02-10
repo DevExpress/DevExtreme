61d8c46835e34a5d3fea416b90b3f5c4626b688c
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2020-02-10 13:20:14 +0300

    CI: QUnit runner watchdog (#11938)

--- docker-ci.sh
+++ docker-ci.sh
@@ -161,6 +161,7 @@ function run_test {
 
     esac
 
+    start_runner_watchdog $runner_pid
     wait $runner_pid || runner_result=1
     exit $runner_result
 }
@@ -191,6 +192,22 @@ function run_test_functional {
 function run_test_scss {
     npm i
     npx gulp generate-scss
+}
+
+function start_runner_watchdog {
+    local last_suite_time_file="$PWD/testing/LastSuiteTime.txt"
+    local last_suite_time=unknown
+
+    while true; do
+        sleep 300
+
+        if [ ! -f $last_suite_time_file ] || [ $(cat $last_suite_time_file) == $last_suite_time ]; then
+            echo "Runner stalled"
+            kill -9 $1
+        else
+            last_suite_time=$(cat $last_suite_time_file)
+        fi
+    done &
 }
 
 echo "node $(node -v), npm $(npm -v), dotnet $(dotnet --version)"
