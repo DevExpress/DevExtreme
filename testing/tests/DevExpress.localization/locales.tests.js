992cc43891f36790b852c3a21582ff36b411d1f0
Roman Rodin <roman.rodin@devexpress.com>
2020-02-17 18:46:14 +0300

    Fix tests

--- testing/tests/DevExpress.localization/locales.tests.js
+++ testing/tests/DevExpress.localization/locales.tests.js
@@ -46,7 +46,6 @@ const compareLocales = function(first, second, assert) {
         'dxHtmlEditor-dialogLinkUrlField',
         'dxHtmlEditor-dialogLinkTextField',
         'dxDiagram-uiExport',
-        'dxDiagram-commandUndo',
         'dxDiagram-uiLayoutHorizontal',
         'dxDiagram-unitIn',
         'dxDiagram-unitCm',
