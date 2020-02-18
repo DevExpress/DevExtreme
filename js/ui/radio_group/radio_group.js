9dec86deb8b942f88434d47f3572b41498df1f60
kotov.alexander <alexander.kotov@devexpress.com>
2020-02-18 16:40:04 +0300

    Apply dataSource changes only after the widget's radios collection is ready (T861468) (#12043)

--- js/ui/radio_group/radio_group.js
+++ js/ui/radio_group/radio_group.js
@@ -7,6 +7,7 @@ import registerComponent from '../../core/component_registrator';
 import CollectionWidget from '../collection/ui.collection_widget.edit';
 import DataExpressionMixin from '../editor/ui.data_expression';
 import Editor from '../editor/editor';
+import { Deferred } from '../../core/utils/deferred';
 
 const RADIO_BUTTON_CHECKED_CLASS = 'dx-radiobutton-checked';
 const RADIO_BUTTON_CLASS = 'dx-radiobutton';
@@ -260,6 +261,7 @@ class RadioGroup extends Editor {
     }
 
     _renderRadios() {
+        this._areRadiosCreated = new Deferred();
         const $radios = $('<div>').appendTo(this.$element());
 
         this._radios = this._createComponent($radios, RadioCollection, {
@@ -278,6 +280,7 @@ class RadioGroup extends Editor {
             selectedItemKeys: [this.option('value')],
             tabIndex: this.option('tabIndex')
         });
+        this._areRadiosCreated.resolve();
     }
 
     _renderSubmitElement() {
@@ -302,7 +305,7 @@ class RadioGroup extends Editor {
     }
 
     _setCollectionWidgetOption() {
-        this._setWidgetOption('_radios', arguments);
+        this._areRadiosCreated.done(this._setWidgetOption.bind(this, '_radios', arguments));
     }
 
     _toggleActiveState($element, value, e) {
