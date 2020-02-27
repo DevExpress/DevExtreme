9bc5299abc50b0f0b6918c5c0701fbae94878f39
Andrey Ignatovskiy <lazylahtak@yandex.ru>
2020-02-27 14:52:29 +0300

    Use 'for' cycle instead spread iterable object

--- js/renovation/preact-wrapper/utils.js
+++ js/renovation/preact-wrapper/utils.js
@@ -1,13 +1,15 @@
 // NOTE: function for jQuery templates
 export const wrapElement = ($element, $wrapper) => {
-    const attributes = [...$wrapper.get(0).attributes];
-    attributes.forEach(({ name, value }) => {
+    const attributes = $wrapper.get(0).attributes;
+
+    for(let i = 0; i < attributes.length; i++) {
+        const { name, value } = attributes[i];
         if(name === 'class') {
             $element.addClass(value);
         } else {
             $element.attr(name, value);
         }
-    });
+    }
 
     const children = $wrapper.contents();
     $wrapper.replaceWith(children);
