beb1e64eb38a0a1bfc7a0925b9851ab2ddc49cbd
Andrey Ignatovskiy <lazylahtak@yandex.ru>
2020-02-27 15:16:21 +0300

    Use 'each' instead 'for'

--- js/renovation/preact-wrapper/utils.js
+++ js/renovation/preact-wrapper/utils.js
@@ -1,15 +1,15 @@
+import { each } from '../../core/utils/iterator';
+
 // NOTE: function for jQuery templates
 export const wrapElement = ($element, $wrapper) => {
     const attributes = $wrapper.get(0).attributes;
-
-    for(let i = 0; i < attributes.length; i++) {
-        const { name, value } = attributes[i];
+    each(attributes, (_, { name, value }) => {
         if(name === 'class') {
             $element.addClass(value);
         } else {
             $element.attr(name, value);
         }
-    }
+    });
 
     const children = $wrapper.contents();
     $wrapper.replaceWith(children);
