12a0c87b0ec1bd959dfde96698f994c075d90b4d
babich-a <alexey.babych@gmail.com>
2020-02-28 15:45:29 +0300

    fix fade replacer

--- build/gulp/scss/generator.js
+++ build/gulp/scss/generator.js
@@ -42,7 +42,7 @@ const replaceColorFunctions = (content) => {
     // change fade($color, 20%) to the color.change($color, $alpha: 0.20), $color can be other function
     // change fadein($color, 20%) to the color.adjust($color, $alpha: 0.20), $color can be other function
 
-    content = content.replace(/(fadein|fade)\(([$\d\w-#]*|[\w]*\(.*\)),\s*([\d.]+)%\)(;|,|\))/g, (match, func, color, percent, sign) => {
+    content = content.replace(/(fadein|fade)\(([$\d\w-#]*|[\w]*\(.*\)),\s*([\d.]+)%\)(;|,|\)|\s)/g, (match, func, color, percent, sign) => {
         const colorFunction = func === 'fade' ? 'change' : 'adjust';
         return `color.${colorFunction}(${color}, $alpha: ${percent / 100})${sign}`;
     });
