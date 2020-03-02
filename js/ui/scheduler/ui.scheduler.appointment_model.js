0f0ea633b2a46c9e5ec29cf273659bda638abffe
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2020-03-02 08:51:29 +0300

    Small fix

--- js/ui/scheduler/ui.scheduler.appointment_model.js
+++ js/ui/scheduler/ui.scheduler.appointment_model.js
@@ -492,7 +492,7 @@ class AppointmentModel {
     appointmentTakesSeveralDays(appointment) {
         const dataAccessors = this._dataAccessors;
         const startDate = dataAccessors.getter.startDate(appointment);
-        const endDate = new Date(dataAccessors.getter.endDate(appointment));
+        const endDate = dataAccessors.getter.endDate(appointment);
 
         const startDateCopy = dateUtils.trimTime(new Date(startDate));
         let endDateCopy = new Date(endDate);
