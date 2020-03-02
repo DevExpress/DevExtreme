e82e9dd88f07e6d257a864f9c694e184998b5fc8
Sergey Novikov <novsstation@yandex.ru>
2020-03-02 18:25:32 +0300

    More test cases

--- testing/tests/DevExpress.ui.widgets/treeViewParts/scrolling.tests.js
+++ testing/tests/DevExpress.ui.widgets/treeViewParts/scrolling.tests.js
@@ -1,8 +1,8 @@
 import TreeViewTestWrapper from '../../../helpers/TreeViewTestHelper.js';
 
 const configs = [];
-[false, true, undefined].forEach((expanded) => {
-    ['vertical', 'horizontal', 'both'].forEach((scrollDirection) => {
+['vertical', 'horizontal', 'both'].forEach((scrollDirection) => {
+    [false, true].forEach((expanded) => {
         configs.push({ expanded, scrollDirection });
     });
 });
@@ -11,7 +11,7 @@ const configs = [];
 QUnit.module('TreeView scrolling', () => {
     const ROOT_ID = -1;
     const TOTAL_ITEMS_COUNT = 21;
-    const LAST_ITEM_KEY = 20;
+    const LAST_ITEM_KEY = TOTAL_ITEMS_COUNT - 1;
 
     function createWrapper(scrollDirection, items) {
         return new TreeViewTestWrapper({
@@ -105,7 +105,7 @@ QUnit.module('TreeView scrolling', () => {
         });
     });
 
-    QUnit.test('scrollToItem(LAST_ITEM_KEY) -> scrollToItem(0)', function(assert) {
+    QUnit.test('allItems.expanded::true -> scrollToItem(lastItem.key) -> scrollToItem(firstItem.key)', function(assert) {
         const items = [];
         for(let i = 0; i < TOTAL_ITEMS_COUNT; i++) {
             items.push({ id: i, text: 'item' + i, parentId: i - 1, expanded: true });
@@ -124,21 +124,55 @@ QUnit.module('TreeView scrolling', () => {
         });
     });
 
-    QUnit.test('allItems.disabled: true -> scrollToItem(LAST_ITEM_KEY)', function(assert) {
+    QUnit.test('allItems.expanded::true -> scrollToItem(10) -> scrollToItem(10)', function(assert) {
         const items = [];
         for(let i = 0; i < TOTAL_ITEMS_COUNT; i++) {
-            items.push({ id: i, text: 'item' + i, parentId: ROOT_ID, disabled: true });
+            items.push({ id: i, text: 'item' + i, parentId: i - 1, expanded: true });
         }
 
-        const wrapper = createWrapper('horizontal', [{ id: 0, text: 'item1', parentId: ROOT_ID }]);
+        const wrapper = createWrapper('both', items);
+        let completionCallback = wrapper.instance.scrollToItem(10);
+
+        const done = assert.async();
+        completionCallback.done(() => {
+            completionCallback = wrapper.instance.scrollToItem(10);
+            completionCallback.done(() => {
+                wrapper.checkScrollPosition(320, 150);
+                done();
+            });
+        });
+    });
+
+    QUnit.test('allItems.disabled: true, allItems.expanded: false -> scrollToItem(lastItem.key)', function(assert) {
+        const items = [];
+        for(let i = 0; i < TOTAL_ITEMS_COUNT; i++) {
+            items.push({ id: i, text: 'item' + i, parentId: i - 1, disabled: true, expanded: false });
+        }
+
+        const wrapper = createWrapper('both', items);
         const completionCallback = wrapper.instance.scrollToItem(LAST_ITEM_KEY);
 
         const done = assert.async();
-        completionCallback
-            .fail(() => {
-                assert.ok('callback must fail');
-                done();
-            });
+        completionCallback.fail(() => {
+            assert.ok('callback must fail');
+            done();
+        });
+    });
+
+    QUnit.test('allItems.disabled: true, allItems.expanded: true -> scrollToItem(lastItem.key)', function(assert) {
+        const items = [];
+        for(let i = 0; i < TOTAL_ITEMS_COUNT; i++) {
+            items.push({ id: i, text: 'item' + i, parentId: i - 1, disabled: true, expanded: true });
+        }
+
+        const wrapper = createWrapper('both', items);
+        const completionCallback = wrapper.instance.scrollToItem(LAST_ITEM_KEY);
+
+        const done = assert.async();
+        completionCallback.done(() => {
+            wrapper.checkScrollPosition(572, 270);
+            done();
+        });
     });
 
     QUnit.test('scrollToItem(not exists key)', function(assert) {
@@ -146,10 +180,9 @@ QUnit.module('TreeView scrolling', () => {
         const completionCallback = wrapper.instance.scrollToItem(12345);
 
         const done = assert.async();
-        completionCallback
-            .fail(() => {
-                assert.ok('callback must fail');
-                done();
-            });
+        completionCallback.fail(() => {
+            assert.ok('callback must fail');
+            done();
+        });
     });
 });
