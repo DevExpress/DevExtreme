56119398f311badf82a6330e19d1abbb38c6a683
Jaan Toming <jtoming@gmail.com>
2020-03-02 17:41:18 +0300

    refix 2

--- js/ui/grid_core/ui.grid_core.columns_controller.js
+++ js/ui/grid_core/ui.grid_core.columns_controller.js
@@ -2547,7 +2547,16 @@ module.exports = {
 
                     return column.index;
                 },
-                getCustomizeTextByDataType: getCustomizeTextByDataType
+                getCustomizeTextByDataType: getCustomizeTextByDataType,
+                _getHeaderContentAlignment: function(columnAlignment) {
+                    const rtlEnabled = this.option('rtlEnabled');
+
+                    if(rtlEnabled) {
+                        return columnAlignment === 'left' ? 'right' : 'left';
+                    }
+
+                    return columnAlignment;
+                }
             };
         })())
     }
