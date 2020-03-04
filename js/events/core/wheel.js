46e9291f118101ab6c34d3c7452e3f963bf4c393
dmitry.levkovskiy <dmitry.levkovskiy@devexpress.com>
2020-03-04 17:07:57 +0300

    refactor dxmousewheel event

--- js/events/core/wheel.js
+++ js/events/core/wheel.js
@@ -1,47 +1,37 @@
-const $ = require('../../core/renderer');
-const eventsEngine = require('../../events/core/events_engine');
-const domAdapter = require('../../core/dom_adapter');
-const callOnce = require('../../core/utils/call_once');
-const registerEvent = require('./event_registrator');
-const eventUtils = require('../utils');
+import $ from '../../core/renderer';
+import eventsEngine from '../../events/core/events_engine';
+import registerEvent from './event_registrator';
+import { addNamespace, fireEvent } from '../utils';
+
 
 const EVENT_NAME = 'dxmousewheel';
 const EVENT_NAMESPACE = 'dxWheel';
-
-const getWheelEventName = callOnce(function() {
-    return domAdapter.hasDocumentProperty('onwheel') ? 'wheel' : 'mousewheel';
-});
+const NATIVE_EVENT_NAME = 'wheel';
 
 const wheel = {
 
     setup: function(element) {
         const $element = $(element);
-        eventsEngine.on($element, eventUtils.addNamespace(getWheelEventName(), EVENT_NAMESPACE), wheel._wheelHandler.bind(wheel));
+        eventsEngine.on($element, addNamespace(NATIVE_EVENT_NAME, EVENT_NAMESPACE), wheel._wheelHandler.bind(wheel));
     },
 
     teardown: function(element) {
-        eventsEngine.off(element, '.' + EVENT_NAMESPACE);
+        eventsEngine.off(element, `.${EVENT_NAMESPACE}`);
     },
 
-    _wheelHandler: function(e) {
-        const delta = this._getWheelDelta(e.originalEvent);
+    _wheelHandler: function({ originalEvent }) {
+        const { deltaY, deltaMode } = originalEvent;
 
-        eventUtils.fireEvent({
+        fireEvent({
             type: EVENT_NAME,
-            originalEvent: e,
-            delta: delta,
+            originalEvent,
+            deltaY: -deltaY,
+            deltaMode,
             pointerType: 'mouse'
         });
 
-        e.stopPropagation();
-    },
-
-    _getWheelDelta: function(event) {
-        return event.wheelDelta
-            ? event.wheelDelta
-            : -event.deltaY * 30;
+        originalEvent.stopPropagation();
     }
-
 };
 
 registerEvent(EVENT_NAME, wheel);
