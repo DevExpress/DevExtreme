9b768b3b84261575d21ae8a775ae33c5fab711f7
Sergey Novikov <novsstation@yandex.ru>
2020-03-04 15:50:58 +0300

    Full consistence of templates in accordion an tab_panel.js

--- js/ui/tab_panel.js
+++ js/ui/tab_panel.js
@@ -106,7 +106,7 @@ const TabPanel = MultiView.inherit({
                     $iconElement && $container.append($iconElement);
 
                     if(isDefined(data.title) && !isPlainObject(data.title)) {
-                        $container.append(data.title);
+                        $container.append($('<span>').text(data.title));
                     }
                 } else {
                     if(isDefined(data)) {
