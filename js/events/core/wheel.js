5d993638847ea0c050281d304c09b3b26efdda74
dmitry.levkovskiy <dmitry.levkovskiy@devexpress.com>
2020-03-06 13:16:34 +0300

    fix srolling for firefox

--- js/events/core/wheel.js
+++ js/events/core/wheel.js
@@ -8,8 +8,10 @@ const EVENT_NAME = 'dxmousewheel';
 const EVENT_NAMESPACE = 'dxWheel';
 const NATIVE_EVENT_NAME = 'wheel';
 
-const wheel = {
+const PIXEL_MODE = 0;
+const DELTA_MUTLIPLIER = 30;
 
+const wheel = {
     setup: function(element) {
         const $element = $(element);
         eventsEngine.on($element, addNamespace(NATIVE_EVENT_NAME, EVENT_NAMESPACE), wheel._wheelHandler.bind(wheel));
@@ -20,17 +22,30 @@ const wheel = {
     },
 
     _wheelHandler: function(e) {
-        const { deltaY, deltaMode } = e.originalEvent;
+        const { deltaMode, deltaY, deltaX, deltaZ } = e.originalEvent;
 
         fireEvent({
             type: EVENT_NAME,
             originalEvent: e,
-            delta: -deltaY,
+            delta: this._normalizeDelta(deltaY, deltaMode),
+            deltaX,
+            deltaY,
+            deltaZ,
             deltaMode,
             pointerType: 'mouse'
         });
 
         e.stopPropagation();
+    },
+
+    _normalizeDelta(delta, deltaMode) {
+        if(deltaMode === PIXEL_MODE) {
+            return -delta;
+        } else {
+            // Use multiplier to get rough delta value in px for the LINE or PAGE mode
+            // https://bugzilla.mozilla.org/show_bug.cgi?id=1392460
+            return -DELTA_MUTLIPLIER * delta;
+        }
     }
 };
 
