800d044182ead085820dc7e3fe4004b2b68576b8
Jaan Toming <jtoming@gmail.com>
2020-03-17 16:36:30 +0300

    DataGrid: Fix placeholder was wider than grid if it has horizontal scroll and fixed column (T868007)

--- js/ui/draggable.js
+++ js/ui/draggable.js
@@ -702,18 +702,6 @@ const Draggable = DOMComponentWithTemplate.inherit({
 
     _updateScrollable: function(e) {
         const that = this;
-        const $sourceElement = that._getSourceElement();
-
-        that._$scrollable = null;
-        $sourceElement.parents().toArray().some(function(element) {
-            const $element = $(element);
-
-            if(that._horizontalScrollHelper.isScrollable($element) || that._verticalScrollHelper.isScrollable($element)) {
-                that._$scrollable = $element;
-
-                return true;
-            }
-        });
 
         if(that.option('autoScroll')) {
             const $window = $(window);
