fbdd8beb2c178215b20bfbcc4a719627c69e85e1
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2020-03-19 11:19:50 +0300

    Remove excess comments

--- js/renovation/error-message.tsx
+++ js/renovation/error-message.tsx
@@ -1,9 +1,5 @@
 import { Component, ComponentBindings, JSXComponent, OneWay } from 'devextreme-generator/component_declaration/common';
 
-// const VALIDATION_SUMMARY_CLASS = 'dx-validationsummary';
-// const ITEM_CLASS = VALIDATION_SUMMARY_CLASS + '-item';
-// const ITEM_DATA_KEY = VALIDATION_SUMMARY_CLASS + '-item-data';
-
 export const viewFunction = ({ props: { message, className } }: ErrorMessage) => (
     <div className={`dx-validationsummary dx-validationsummary-item ${className}`}>
         {message}
