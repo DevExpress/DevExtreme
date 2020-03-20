ac049c6abbc11eca16dee5576a3a5c4754b1b7e0
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2020-03-20 22:43:15 +0300

    Fix timeline tests

--- testing/tests/DevExpress.ui.widgets.scheduler/timeline.tests.js
+++ testing/tests/DevExpress.ui.widgets.scheduler/timeline.tests.js
@@ -43,7 +43,7 @@ QUnit.module('Timeline Base', {
                 delete this.instance;
             }
 
-            this.instance = $('#scheduler-timeline').dxSchedulerTimeline().dxSchedulerTimeline('instance');
+            this.instance = $('#scheduler-timeline').dxSchedulerTimeline(options).dxSchedulerTimeline('instance');
             stubInvokeMethod(this.instance, options);
         };
 
