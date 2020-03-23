4af6a6a8b444c408a2e67ee821983fc62f02f8d4
NickMitrokhin <nikolai.mitrokhin@devexpress.com>
2020-03-23 11:24:06 +0300

    Change test

--- testing/tests/DevExpress.ui.widgets.dataGrid/dataGrid.tests.js
+++ testing/tests/DevExpress.ui.widgets.dataGrid/dataGrid.tests.js
@@ -19594,6 +19594,7 @@ QUnit.test('Row height should not be changed after validation', function(assert)
     const data = [
         { a: 'a', b: 'b', c: 'c' }
     ];
+    this.clock.restore();
 
     const grid = createDataGrid({
         dataSource: {
@@ -19649,18 +19650,18 @@ QUnit.test('Row height should not be changed after validation', function(assert)
                     }
                 }]
             }
-        ]
-    });
-
-    this.clock.tick();
-    const rowHeight = $(grid.getRowElement(0)).height();
-    this.clock.restore();
-
-    grid.cellValue(0, 1, '');
-    grid.saveEditData().done(() => {
-        assert.strictEqual($(grid.getRowElement(0)).height(), rowHeight, 'row height is not changed');
-
-        done();
+        ],
+        onContentReady: function(e) {
+            if(!e.component.__firstTime) {
+                e.component.__firstTime = true;
+                const rowHeight = $(grid.getRowElement(0)).height();
+                grid.cellValue(0, 1, '');
+                grid.saveEditData().done(() => {
+                    assert.strictEqual($(grid.getRowElement(0)).height(), rowHeight, 'row height is not changed');
+                    done();
+                });
+            }
+        }
     });
 });
 
