a432d62396d9f09b331cf4331ab16d8af7aa0eb1
Maxim Kudryavtsev <maxim_71rus_wow@mail.ru>
2020-03-23 11:04:36 +0300

    Add the ErrorMessage component (#12357)
    * Add validation declaration component
    
    * Edit tests
    
    * Remove excess comments
    
    * Fix tests
    
    * Fix test code coverage

---
+++ js/renovation/error-message.tsx
@@ -0,0 +1,20 @@
+import { Component, ComponentBindings, JSXComponent, OneWay } from 'devextreme-generator/component_declaration/common';
+
+export const viewFunction = ({ props: { message, className } }: ErrorMessage) => (
+    <div className={`dx-validationsummary dx-validationsummary-item ${className}`}>
+        {message}
+    </div>
+);
+
+@ComponentBindings()
+export class ErrorMessageInput {
+    @OneWay() className?: string = '';
+    @OneWay() message?: string = '';
+}
+
+// tslint:disable-next-line: max-classes-per-file
+@Component({
+    defaultOptionRules: null,
+    view: viewFunction,
+})
+export default class ErrorMessage extends JSXComponent<ErrorMessageInput> {}
