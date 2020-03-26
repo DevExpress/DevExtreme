7f0b601dfc5858bac62ee8ca83c95de88effc11b
NickMitrokhin <nikolai.mitrokhin@devexpress.com>
2020-03-26 15:56:12 +0300

    Refactors code

--- js/ui/grid_core/ui.grid_core.validating.js
+++ js/ui/grid_core/ui.grid_core.validating.js
@@ -234,7 +234,7 @@ const ValidatingController = modules.Controller.inherit((function() {
             let columnsController;
             let showEditorAlways = column.showEditorAlways;
 
-            if(isDefined(column.command) || !column.allowEditing || !column.validationRules || !Array.isArray(column.validationRules) || !column.validationRules.length) return;
+            if(isDefined(column.command) || !column.validationRules || !Array.isArray(column.validationRules) || !column.validationRules.length) return;
 
             editIndex = editingController.getIndexByKey(parameters.key, editingController._editData);
 
