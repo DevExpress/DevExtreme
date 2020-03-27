556a48f53a98dc6ad6c6321fd524501cd32267ad
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2020-03-27 13:29:25 +0300

    ExcelJS - change type definition excelCell to any (#12481)
    Co-authored-by: EugeniyKiyashko <johnnykiyashko@gmail.com>

--- js/excel_exporter.d.ts
+++ js/excel_exporter.d.ts
@@ -183,7 +183,7 @@ export interface ExportDataGridProps {
      * @type_function_param1_field2 excelCell:Object
      * @public
      */
-    customizeCell?: ((options: { gridCell?: ExcelDataGridCell, excelCell?: object}) => any);
+    customizeCell?: ((options: { gridCell?: ExcelDataGridCell, excelCell?: any}) => any);
     /**
      * @docid ExportDataGridProps.loadPanel
      * @type ExportLoadPanel
