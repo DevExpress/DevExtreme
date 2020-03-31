a297efb7f0179203b1497b219b9b0b08e02df50d
NickMitrokhin <nikolai.mitrokhin@devexpress.com>
2020-03-31 16:35:33 +0300

    Fixes the issue

--- js/ui/grid_core/ui.grid_core.editing.js
+++ js/ui/grid_core/ui.grid_core.editing.js
@@ -1773,6 +1773,7 @@ const EditingController = modules.ViewController.inherit((function() {
             const showEditorAlways = options.column.showEditorAlways;
             const isUpdateInCellMode = editMode === EDIT_MODE_CELL && options.row && !options.row.isNewRow;
             const focusPreviousEditingCell = showEditorAlways && !forceUpdateRow && isUpdateInCellMode && that.hasEditData() && !that.isEditCell(options.rowIndex, options.columnIndex);
+            let focusCellAfterRowUpdate = false;
 
             if(focusPreviousEditingCell) {
                 that._focusEditingCell();
@@ -1782,6 +1783,11 @@ const EditingController = modules.ViewController.inherit((function() {
 
             that._addEditData(params, options.row);
             that._updateEditButtons();
+
+            if(editMode === EDIT_MODE_CELL && isCustomSetCellValue) {
+                forceUpdateRow = true;
+                focusCellAfterRowUpdate = true;
+            }
 
             if(showEditorAlways && !forceUpdateRow) {
                 if(isUpdateInCellMode) {
@@ -1796,6 +1802,7 @@ const EditingController = modules.ViewController.inherit((function() {
 
             if(options.row && (forceUpdateRow || isCustomSetCellValue)) {
                 that._updateEditRow(options.row, forceUpdateRow, isCustomSetCellValue);
+                focusCellAfterRowUpdate && that._focusEditingCell();
             }
         },
         _updateEditRowCore: function(row, skipCurrentRow, isCustomSetCellValue) {
