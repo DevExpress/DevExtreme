eceaf273e913a74b3876b88d335e25572c19d70f
Nick Mitrokhin <NickMitrokhin@users.noreply.github.com>
2020-04-01 11:28:05 +0300

    DataGrid - Fixes updating a dependent invalid cell in the Cell editing mode(T872751) (#12517)

--- js/ui/grid_core/ui.grid_core.editing.js
+++ js/ui/grid_core/ui.grid_core.editing.js
@@ -1859,6 +1859,9 @@ const EditingController = modules.ViewController.inherit((function() {
             const showEditorAlways = options.column.showEditorAlways;
             const isUpdateInCellMode = editMode === EDIT_MODE_CELL && options.row && !options.row.isNewRow;
             const focusPreviousEditingCell = showEditorAlways && !forceUpdateRow && isUpdateInCellMode && that.hasEditData() && !that.isEditCell(options.rowIndex, options.columnIndex);
+            const columns = that._columnsController.getVisibleColumns();
+            const isCustomCalculateCellValue = columns.some((column) => column.calculateCellValue !== column.defaultCalculateCellValue);
+            let focusCellAfterRowUpdate = false;
 
             if(focusPreviousEditingCell) {
                 that._focusEditingCell();
@@ -1869,14 +1872,17 @@ const EditingController = modules.ViewController.inherit((function() {
             that._addEditData(params, options.row);
             that._updateEditButtons();
 
+            if(editMode === EDIT_MODE_CELL && (isCustomSetCellValue || isCustomCalculateCellValue)) {
+                forceUpdateRow = focusCellAfterRowUpdate = true;
+            }
+
             if(showEditorAlways && !forceUpdateRow) {
                 if(isUpdateInCellMode) {
                     that._editRowIndex = options.rowIndex + that._dataController.getRowIndexOffset();
                     that._editColumnIndex = options.columnIndex;
                     return that.saveEditData();
                 } else if(editMode === EDIT_MODE_BATCH) {
-                    const columns = that._columnsController.getVisibleColumns();
-                    forceUpdateRow = isCustomSetCellValue || columns.some((column) => column.calculateCellValue !== column.defaultCalculateCellValue);
+                    forceUpdateRow = isCustomSetCellValue || isCustomCalculateCellValue;
                 }
             }
 
@@ -1884,6 +1890,7 @@ const EditingController = modules.ViewController.inherit((function() {
             if(row) {
                 if(forceUpdateRow || isCustomSetCellValue) {
                     that._updateEditRow(row, forceUpdateRow, isCustomSetCellValue);
+                    focusCellAfterRowUpdate && that._focusEditingCell();
                 } else if(row.update) {
                     row.update();
                 }
