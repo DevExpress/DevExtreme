0ec2bd012facedea161f7b1da426d393048d3848
ksercs <ksercs0@gmail.com>
2020-04-06 10:53:29 +0300

    remove useless optionChanged handlers

--- js/ui/color_box/color_box.js
+++ js/ui/color_box/color_box.js
@@ -385,15 +385,6 @@ const ColorBox = DropDownEditor.inherit({
                     this._colorView.option(name, value);
                 }
                 break;
-            case 'applyValueMode':
-                this.callBase(args);
-                break;
-            case 'rtlEnabled':
-                if(this._colorView) {
-                    this._colorView.option(name, value);
-                }
-                this.callBase(args);
-                break;
             default:
                 this.callBase(args);
         }
