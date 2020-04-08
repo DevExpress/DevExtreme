b58a487533f06c1eaa6f47eb991f0c6943ea334b
Smirnova Yuliya <Krijovnick@users.noreply.github.com>
2020-04-08 13:00:56 +0300

    dxChart - fix render correct ticks and labels (T873310) (#12631)

--- testing/helpers/chartMocks.js
+++ testing/helpers/chartMocks.js
@@ -983,6 +983,7 @@ export const MockAxis = function(renderOptions) {
         createTicks: function() {
 
         },
+        estimateTickInterval: sinon.stub().returns(false),
         getMultipleAxesSpacing: function() {
             return this._options.mockAxesSpacing || 5;
         },
