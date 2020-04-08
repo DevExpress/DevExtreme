2f5397d8cd546e9bd4be06f093a6f1d60642f733
Dmitry Levkovskiy <DokaRus@users.noreply.github.com>
2020-04-08 18:09:39 +0300

    Autocomplete should trigger the "SelectionChanged" event on selecting an item from a data source (T875269) (#12625) (#12645)

--- js/ui/select_box.js
+++ js/ui/select_box.js
@@ -837,11 +837,6 @@ const SelectBox = DropDownList.inherit({
             case 'useInkRipple':
                 this._invalidate();
                 break;
-            case 'selectedItem':
-                if(args.previousValue !== args.value) {
-                    this.callBase(args);
-                }
-                break;
             case 'allowClearing':
                 break;
             default:
