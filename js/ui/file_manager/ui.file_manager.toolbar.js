8afbc2f69e98db27a1576470b3afa9ec3feabacb
Stanislav Klesarev <klesarevklesarevstas@gmail.com>
2020-04-09 17:59:06 +0300

    File Manager - Fix custom items visibility in file toolbar

--- js/ui/file_manager/ui.file_manager.toolbar.js
+++ js/ui/file_manager/ui.file_manager.toolbar.js
@@ -373,7 +373,7 @@ class FileManagerToolbar extends Widget {
 
     _fileToolbarHasEffectiveItems(fileItems) {
         const items = this._fileToolbar.option('items');
-        return items.some(({ name }) => name !== 'clear' && name !== 'refresh' && this._commandManager.isCommandAvailable(name, fileItems));
+        return items.some(item => this._isFileToolbarItemAvailable(item, fileItems));
     }
 
     _executeCommand(command) {
@@ -393,6 +393,13 @@ class FileManagerToolbar extends Widget {
         }
 
         return this._commandManager.isCommandAvailable(toolbarItem.name, fileItems);
+    }
+
+    _isFileToolbarItemAvailable(fileToolbarItem, fileItems) {
+        const name = fileToolbarItem.name;
+        const visible = fileToolbarItem.visible;
+        return !this._isDefaultItem(name) && ensureDefined(visible, true) ||
+            name !== 'clear' && name !== 'refresh' && this._commandManager.isCommandAvailable(name, fileItems);
     }
 
     _updateItemInToolbar(toolbar, commandName, options) {
