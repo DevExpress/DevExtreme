5e798feb03deef9cfc3fdeee0a6c7333a5b24405
Anton Kuznetsov <ksercs0@gmail.com>
2020-04-10 13:49:35 +0300

    NumberBox: it should be possible to set negative value when min is null (T876378) (#12653)

--- js/ui/number_box/number_box.base.js
+++ js/ui/number_box/number_box.base.js
@@ -270,11 +270,11 @@ const NumberBoxBase = TextEditor.inherit({
         const min = this.option('min');
         const max = this.option('max');
 
-        if(min !== undefined) {
+        if(typeUtils.isDefined(min)) {
             value = Math.max(min, value);
         }
 
-        if(max !== undefined) {
+        if(typeUtils.isDefined(max)) {
             value = Math.min(max, value);
         }
 
