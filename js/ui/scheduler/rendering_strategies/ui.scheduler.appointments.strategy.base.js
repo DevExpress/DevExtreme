8404e9b88649e3294f77263cb65b67b60cea429e
Yana <ms.yana.work@mail.ru>
2020-04-14 20:46:06 +0300

    small fix

--- js/ui/scheduler/rendering_strategies/ui.scheduler.appointments.strategy.base.js
+++ js/ui/scheduler/rendering_strategies/ui.scheduler.appointments.strategy.base.js
@@ -314,7 +314,7 @@ class BaseRenderingStrategy {
     _isItemsCross(item, currentItem, orientation) {
         const side_1 = Math.floor(item[orientation[0]]);
         const side_2 = Math.floor(item[orientation[1]]);
-        const isItemCross = Math.abs(item[orientation[2]] - currentItem[orientation[2]]) <= 1.01;
+        const isItemCross = Math.abs(item[orientation[2]] - currentItem[orientation[2]]) <= 1;
         return isItemCross && !!item.allDay === !!currentItem.allDay && (
             (side_1 <= currentItem[orientation[0]] && side_2 > currentItem[orientation[0]]) ||
                 (side_1 < currentItem[orientation[1]] && side_2 >= currentItem[orientation[1]] || (
