a2296e8cd3dc4ce5e693b611cf12bf84e9b0f4b8
Andrej Ignatovskij <lazylahtak@yandex.ru>
2020-04-15 14:23:41 +0300

    Correct NOTE

--- js/renovation/preact-wrapper/component.js
+++ js/renovation/preact-wrapper/component.js
@@ -81,8 +81,8 @@ export default class PreactWrapper extends DOMComponent {
         this._refresh();
     }
 
-    // NOTE: this method will be depricated
-    //       aria changes should be defined in declaration
+    // NOTE: this method will be deprecated
+    //       aria changes should be defined in declaration or passed through property
     setAria() {
         throw new Error('"setAria" method is deprecated, use "aria" property instead');
     }
