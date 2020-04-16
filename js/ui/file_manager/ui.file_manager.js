566b2c9331b7167c3e3c71f1a4ddf8612505178b
Stanislav Klesarev <klesarevklesarevstas@gmail.com>
2020-04-16 17:51:49 +0300

    Refactor

--- js/ui/file_manager/ui.file_manager.js
+++ js/ui/file_manager/ui.file_manager.js
@@ -620,14 +620,16 @@ class FileManager extends Widget {
             case 'toolbar':
                 {
                     const toolbarOptions = {};
-                    if(args.value.items) {
-                        toolbarOptions.generalItems = args.value.items;
+                    if(args.fullName === 'toolbar') {
+                        if(args.value.items) {
+                            toolbarOptions.generalItems = args.value.items;
+                        }
+                        if(args.value.fileSelectionItems) {
+                            toolbarOptions.fileItems = args.value.fileSelectionItems;
+                        }
                     }
                     if(args.fullName === 'toolbar.items') {
                         toolbarOptions.generalItems = args.value;
-                    }
-                    if(args.value.fileSelectionItems) {
-                        toolbarOptions.fileItems = args.value.fileSelectionItems;
                     }
                     if(args.fullName === 'toolbar.fileSelectionItems') {
                         toolbarOptions.fileItems = args.value;
