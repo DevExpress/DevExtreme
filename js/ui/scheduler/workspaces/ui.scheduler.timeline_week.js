bfd2d8c62670e24f56da6b958b828042e6527193
Yana <ms.yana.work@mail.ru>
2020-04-16 09:37:45 +0300

    Remove code

--- js/ui/scheduler/workspaces/ui.scheduler.timeline_week.js
+++ js/ui/scheduler/workspaces/ui.scheduler.timeline_week.js
@@ -2,8 +2,6 @@ const registerComponent = require('../../../core/component_registrator');
 const SchedulerTimeline = require('./ui.scheduler.timeline');
 
 const TIMELINE_CLASS = 'dx-scheduler-timeline-week';
-// const HEADER_ROW_CLASS = 'dx-scheduler-header-row';
-// const GROUP_ROW_CLSS = 'dx-scheduler-group-row';
 
 const SchedulerTimelineWeek = SchedulerTimeline.inherit({
     _getElementClass: function() {
@@ -12,25 +10,6 @@ const SchedulerTimelineWeek = SchedulerTimeline.inherit({
 
     _getCellCount: function() {
         return this.callBase() * this._getWeekDuration();
-    },
-
-    _setTableSizes: function() {
-        this.callBase();
-        // const cellWidth = this.getCellWidth();
-        // const minWidth = this.getWorkSpaceMinWidth();
-        // let cellCount = this.$element().find('.' + HEADER_ROW_CLASS).last().find('th').length;
-
-        // if(this._isGroupedByDate()) {
-        //     cellCount = this.$element().find('.' + GROUP_ROW_CLSS).last().find('th').length;
-        // }
-
-        // let width = cellWidth * cellCount;
-
-        // if(width < minWidth) {
-        //     width = minWidth;
-        // }
-
-        // this._$headerPanel.width(width);
     },
 
     _getHeaderPanelCellWidth: function($headerRow) {
