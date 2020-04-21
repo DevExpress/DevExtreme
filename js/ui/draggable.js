7052d5f4ea089e6d7dc7ccbb44f1f8bad8bf6189
volnyagin <vconst@users.noreply.github.com>
2020-04-21 14:16:08 +0300

    DataGrid: Fix row dragging if remote data with slow connection is used (T867087)

--- js/ui/draggable.js
+++ js/ui/draggable.js
@@ -881,7 +881,6 @@ const Draggable = DOMComponent.inherit({
             case 'contentTemplate':
             case 'container':
             case 'clone':
-                this._resetDragElement();
                 break;
             case 'allowMoveByClick':
             case 'dragDirection':
