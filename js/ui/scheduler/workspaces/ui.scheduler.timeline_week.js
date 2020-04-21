423ca673de7857ddda5b1726f5a3dfffe3ef50e7
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2020-04-21 14:53:57 +0300

    Fix Scheduler timelineWeek rendering with groupByDate & crossScrolling (T853642) (#12735)
    (cherry picked from commit b57c5e32d215606b847918d145735aa0b22075e5)

--- js/ui/scheduler/workspaces/ui.scheduler.timeline_week.js
+++ js/ui/scheduler/workspaces/ui.scheduler.timeline_week.js
@@ -2,7 +2,6 @@ const registerComponent = require('../../../core/component_registrator');
 const SchedulerTimeline = require('./ui.scheduler.timeline');
 
 const TIMELINE_CLASS = 'dx-scheduler-timeline-week';
-const HEADER_ROW_CLASS = 'dx-scheduler-header-row';
 
 const SchedulerTimelineWeek = SchedulerTimeline.inherit({
     _getElementClass: function() {
@@ -11,21 +10,6 @@ const SchedulerTimelineWeek = SchedulerTimeline.inherit({
 
     _getCellCount: function() {
         return this.callBase() * this._getWeekDuration();
-    },
-
-    _setTableSizes: function() {
-        this.callBase();
-        const cellWidth = this.getCellWidth();
-        const minWidth = this.getWorkSpaceMinWidth();
-        const $headerCells = this.$element().find('.' + HEADER_ROW_CLASS).last().find('th');
-
-        let width = cellWidth * $headerCells.length;
-
-        if(width < minWidth) {
-            width = minWidth;
-        }
-
-        this._$headerPanel.width(width);
     },
 
     _getHeaderPanelCellWidth: function($headerRow) {
