d78ab1a01c1aa3c562633129d703d38a48fcf7bc
Vladimir Kovalev <47112293+vladkovl@users.noreply.github.com>
2020-04-29 14:46:07 +0300

    FileUploader - Accessibility - The file input element does not have the aria-* attributes and does not allow to set one (T876739) (#12894)

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -3733,6 +3733,8 @@ declare module DevExpress.ui {
         chunkSize?: number;
         /** @name dxFileUploader.Options.focusStateEnabled */
         focusStateEnabled?: boolean;
+        /** @name dxFileUploader.Options.inputAttr */
+        inputAttr?: any;
         /** @name dxFileUploader.Options.invalidFileExtensionMessage */
         invalidFileExtensionMessage?: string;
         /** @name dxFileUploader.Options.invalidMaxFileSizeMessage */
