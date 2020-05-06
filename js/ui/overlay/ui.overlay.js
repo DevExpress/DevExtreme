a6d15323ccff7623f7d9612f7ef9f83dc11d1b89
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2020-05-06 15:11:07 +0300

    ScrollView - The scrolling operation is interrupted on touch devices (T886654) (#12917) (#12926)

--- js/ui/overlay/ui.overlay.js
+++ js/ui/overlay/ui.overlay.js
@@ -1003,7 +1003,7 @@ var Overlay = Widget.inherit({
             const originalEvent = e.originalEvent.originalEvent;
             e._cancelPreventDefault = true;
 
-            if(originalEvent && originalEvent.type !== 'mousemove') {
+            if(originalEvent && originalEvent.type !== 'mousemove' && e.cancelable !== false) {
                 e.preventDefault();
             }
         });
