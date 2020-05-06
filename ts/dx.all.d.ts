3f3b9d19ef769159811f360641771bdabbb20da1
Alexander Bezborodov <alexander.bezborodov@devexpress.com>
2020-05-06 15:34:41 +0300

    dxChart: Add the 'position' option (axis' label) to TS

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -7856,6 +7856,8 @@ declare module DevExpress.viz {
         indentFromAxis?: number;
         /** @name dxChart.Options.commonAxisSettings.label.overlappingBehavior */
         overlappingBehavior?: 'rotate' | 'stagger' | 'none' | 'hide';
+        /** @name dxChart.Options.commonAxisSettings.label.position */
+        position?: 'inside' | 'outside' | 'bottom' | 'left' | 'right' | 'top';
         /** @name dxChart.Options.commonAxisSettings.label.rotationAngle */
         rotationAngle?: number;
         /** @name dxChart.Options.commonAxisSettings.label.staggeringSpacing */
