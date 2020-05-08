f20f126f92509a916250f1d20979eff3b75657fe
babich-a <alexey.babych@gmail.com>
2020-05-08 09:38:57 +0300

    new lines

--- themebuilder-scss/src/modules/builder.ts
+++ themebuilder-scss/src/modules/builder.ts
@@ -1,8 +1,5 @@
 
-// const themes = require('./themes.js');
 import normalize from './config-normalizer';
-// const Compiler = require('./compiler');
-// const compiler = new Compiler();
 
 export class Builder {
     constructor() {}
@@ -14,7 +11,7 @@ export class Builder {
         // css - compiler (+),
         // swatchSelector - compiler (+),
         // version - metadata (+),
-        // widgets - HZ
-        // unusedWidgets - HZ
+        // widgets - ?
+        // unusedWidgets - ?
     }
 }
