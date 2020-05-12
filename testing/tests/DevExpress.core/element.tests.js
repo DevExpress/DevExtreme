c0396e689ca8ca16fbdc12e81964ce9cb1cd0df3
eugen_zha <eugen.zha@gmail.com>
2020-05-12 22:47:14 +0300

    Update element tests

--- testing/tests/DevExpress.core/element.tests.js
+++ testing/tests/DevExpress.core/element.tests.js
@@ -1,16 +1,17 @@
 import { getPublicElement, setPublicElementWrapper } from 'core/element';
 
 QUnit.module('Public element', () => {
-    QUnit.test('Should unwrap element to clear node vy default', function(assert) {
-        const expectedPublicElement = {};
-        const mockElement = { get: (i) => i === 0 && expectedPublicElement };
+    QUnit.test('Should unwrap element using current strategy', function(assert) {
+        const unwrappedElement = {};
+        const mockElement = { get: (i) => i === 0 && unwrappedElement };
+        const expectedPublicElement = QUnit.urlParams['nojquery'] ? unwrappedElement : mockElement;
         assert.strictEqual(getPublicElement(mockElement), expectedPublicElement);
     });
 
     QUnit.test('Should use new strategy to unwrap public element', function(assert) {
-        const expectedPublicElement = {};
-        const mockElement = expectedPublicElement;
-        setPublicElementWrapper((element) => element);
+        const expectedPublicElement = { };
+        const mockElement = { unwrappedElement: expectedPublicElement };
+        setPublicElementWrapper((element) => element.unwrappedElement);
         assert.strictEqual(getPublicElement(mockElement), expectedPublicElement);
     });
 });
