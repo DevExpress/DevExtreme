a4cdfbe0617af64a23eab5726cec6ef0237b986d
Alexey Babich <alexey.babych@gmail.com>
2020-05-12 14:50:35 +0300

    Add swatches support for scss Themebuilder (#12965)

--- build/gulp/style-compiler.js
+++ build/gulp/style-compiler.js
@@ -9,7 +9,7 @@ const autoPrefix = require('gulp-autoprefixer');
 const parseArguments = require('minimist');
 const fs = require('fs');
 
-const dataUri = require('./gulp-data-uri');
+const dataUri = require('./gulp-data-uri').gulpPipe;
 const generator = require('../../themebuilder/modules/metadata-generator');
 const cleanCssOptions = require('../../themebuilder/modules/clean-css-options');
 const context = require('./context');
