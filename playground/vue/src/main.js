abb9bc70922a12f70a1337590b77d6ecf763ab75
roman-simionov <roman-simionov@users.noreply.github.com>
2020-05-19 19:23:53 +0300

    disable eslint

--- playground/vue/src/main.js
+++ playground/vue/src/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import Vue from 'vue';
 import App from './App.vue';
 
@@ -6,3 +7,4 @@ Vue.config.productionTip = false;
 new Vue({
     render: h => h(App),
 }).$mount('#app');
+/* eslint-enable */
