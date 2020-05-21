6e9f797a16ba0b07ce88ab8d7c13ec92eceaf28f
Andrew Makarov <andrey.makarov@devexpress.com>
2020-05-21 11:49:12 +0300

    Support filter in resource data source

--- js/ui/scheduler/ui.scheduler.resource_manager.js
+++ js/ui/scheduler/ui.scheduler.resource_manager.js
@@ -25,6 +25,7 @@ export default class ResourceManager {
         } else {
             return new DataSourceModule.DataSource({
                 store: DataSourceModule.normalizeDataSourceOptions(dataSource).store,
+                filter: dataSource.filter,
                 pageSize: 0
             });
         }
