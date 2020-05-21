d59d14d98e12966065797f439f4cac8c18d2954a
ksercs <ksercs0@gmail.com>
2020-05-21 11:54:22 +0300

    onPullRefresh test refactoring

--- testing/tests/DevExpress.ui.widgets.editors/lookup.tests.js
+++ testing/tests/DevExpress.ui.widgets.editors/lookup.tests.js
@@ -3540,7 +3540,7 @@ QUnit.module('Events', {
             }
         }).dxLookup('instance');
 
-        instance._list._pullDownHandler();
+        instance._list.option('onPullRefresh')();
     });
 
     QUnit.test('onPullRefresh handler should be passed to the list - subscription by "on" method', function(assert) {
@@ -3562,7 +3562,7 @@ QUnit.module('Events', {
             assert.ok(true, 'onPullRefresh is fired');
         });
 
-        instance._list._pullDownHandler();
+        instance._list.option('onPullRefresh')();
     });
 
     QUnit.test('change "onScroll" handler runtime', function(assert) {
