10341a7b0da9d7752ff0f3cdbb275808f27a4704
Alexey Babich <alexey.babych@gmail.com>
2020-05-22 15:01:14 +0300

    Remove scss themebuilder code from 20.1 (#13172)

--- themebuilder-scss/src/modules/base-parameters.ts
+++
@@ -1,1 +0,0 @@
-export default ['@base-accent', '@base-text-color', '@base-bg', '@base-border-color', '@base-border-radius'];
--- themebuilder-scss/tests/modules/pre-compiler.test.ts
+++
@@ -1,8 +0,0 @@
-
-import PreCompiler from '../../src/modules/pre-compiler';
-
-describe('PreCompiler class tests', () => {
-  test('createSassForSwatch', () => {
-    expect(PreCompiler.createSassForSwatch('test-theme-name', 'sass')).toBe('.dx-swatch-test-theme-name { sass };');
-  });
-});
