3a5d8ee66fe3cae9d582c6d1b574532517a4c641
babich-a <alexey.babych@gmail.com>
2020-05-26 16:45:39 +0300

    Extractor class

---
+++ themebuilder-scss/src/modules/bootstrap-extractor.ts
@@ -0,0 +1,86 @@
+import * as sass from 'sass';
+import less from 'less';
+import { promises as fs } from 'fs';
+import bootstrap3meta from '../data/bootstrap-metadata/bootstrap-metadata';
+import bootstrap4meta from '../data/bootstrap-metadata/bootstrap4-metadata';
+
+export default class BootstrapExtractor {
+  compiler: (input: string) => Promise<string>;
+
+  sourceProcessor: () => Promise<string>;
+
+  meta: { [key:string]: string }
+
+  input: string;
+
+  constructor(source: string, version: number) {
+      this.input = source;
+    if (version === 3) {
+      this.compiler = BootstrapExtractor.lessRender;
+      this.sourceProcessor = this.lessProcessor;
+      this.meta = bootstrap3meta;
+    } else {
+      this.compiler = BootstrapExtractor.sassRender;
+      this.sourceProcessor = this.sassProcessor;
+      this.meta = bootstrap4meta;
+    }
+  }
+
+  static readSassFile(fileName: string): Promise<string> {
+    const path = require.resolve('bootstrap/scss/' + fileName);
+    return fs.readFile(path, 'utf8');
+  }
+
+  static sassRender(input: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      sass.render({ data: input }, (error, result) => {
+        error ? reject(error.message) : resolve(result.css.toString());
+      });
+    });
+  }
+
+  static lessRender(input: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      less.render(input, (error, result) => {
+        error ? reject(error.message) : resolve(result.css);
+      });
+    });
+  }
+
+  async sassProcessor(): Promise<string> {
+    const functions = await BootstrapExtractor.readSassFile('_functions.scss');
+    const variables = await BootstrapExtractor.readSassFile('_variables.scss');
+    return functions + this.input + variables;
+  }
+
+  lessProcessor(): Promise<string> {
+    return Promise.resolve(this.input);
+  }
+
+  getServiceCode(): string {
+    let variables = Object.keys(this.meta)
+      .map(key => `${key}: ${this.meta[key]};`)
+      .join('');
+
+    return `dx-empty {${variables}}`;
+  }
+
+  async compile(): Promise<string> {
+    const source = await this.sourceProcessor() + this.getServiceCode();
+    return this.compiler(source);
+  }
+
+  async extract(): Promise<Array<ConfigMetaItem>> {
+    const css = await this.compile();
+    const ruleRegex = /([\w-]*):\s(.*);/g;
+    const result: Array<ConfigMetaItem> = [];
+
+    let match = ruleRegex.exec(css);
+    while (match !== null) {
+      result.push({ key: `$${match[1]}`, value: match[2] });
+      match = ruleRegex.exec(css);
+    }
+
+    return result;
+  }
+}
---
+++ themebuilder-scss/tests/modules/bootstrap-extractor.test.ts
@@ -0,0 +1,107 @@
+import BootstrapExtractor from '../../src/modules/bootstrap-extractor';
+import { readFileSync } from 'fs';
+
+describe('BootstrapExtractor', () => {
+  test('constructor set the right compiler and processor', () => {
+    const lessExtractor = new BootstrapExtractor('', 3);
+    const sassExtractor = new BootstrapExtractor('', 4);
+    const defaultExtractor = new BootstrapExtractor('', 10);
+
+    expect(lessExtractor.compiler).toBe(BootstrapExtractor.lessRender);
+    expect(sassExtractor.compiler).toBe(BootstrapExtractor.sassRender);
+    expect(defaultExtractor.compiler).toBe(BootstrapExtractor.sassRender);
+
+    expect(lessExtractor.sourceProcessor).toBe(lessExtractor.lessProcessor);
+    expect(sassExtractor.sourceProcessor).toBe(sassExtractor.sassProcessor);
+    expect(defaultExtractor.sourceProcessor).toBe(defaultExtractor.sassProcessor);
+  });
+
+  test('sassRender', async () => {
+    const sass = '$var: red; div { color: $var;}';
+    const css = 'div {\n  color: red;\n}';
+
+    expect(await BootstrapExtractor.sassRender(sass)).toBe(css);
+
+    expect(BootstrapExtractor.sassRender('0'))
+      .rejects
+      .toMatch(/^expected \"{\"\./);
+  });
+
+  test('lessRender', async () => {
+    const less = '@var: red; div { color: @var;}';
+    const css = 'div {\n  color: red;\n}\n';
+
+    expect(await BootstrapExtractor.lessRender(less)).toBe(css);
+
+    expect(BootstrapExtractor.lessRender('0'))
+      .rejects
+      .toBe('Unrecognised input. Possibly missing something');
+  });
+
+  test('sassProcessor', async () => {
+    const testSassString = 'test string';
+    const extractor = new BootstrapExtractor(testSassString, 4);
+    const functionsPath = require.resolve('bootstrap/scss/_functions.scss');
+    const variablesPath = require.resolve('bootstrap/scss/_variables.scss');
+    const functions = readFileSync(functionsPath);
+    const variables = readFileSync(variablesPath);
+
+    expect(await extractor.sassProcessor())
+      .toBe(functions + testSassString + variables);
+  });
+
+  test('lessProcessor', async () => {
+    const testLessString = 'test string';
+    const extractor = new BootstrapExtractor(testLessString, 3);
+
+    expect(await extractor.lessProcessor()).toBe(testLessString);
+  });
+
+  test('getServiceCode', async () => {
+    const extractor = new BootstrapExtractor('', 3);
+    extractor.meta = {
+        'test-key-var1': '$var1',
+        'test-key-var2': '$var2',
+    }
+    const expectedStyleString = 'dx-empty {test-key-var1: $var1;test-key-var2: $var2;}';
+    expect(extractor.getServiceCode()).toBe(expectedStyleString);
+  });
+
+  test('compile (bootstrap 3)', async () => {
+    const input = '@var1: test1;@var2: test2;';
+    const extractor = new BootstrapExtractor(input, 3);
+    extractor.meta = { 'dx-var1': '@var1', 'dx-var2': '@var2' };
+
+    expect(await extractor.compile()).toBe('dx-empty {\n  dx-var1: test1;\n  dx-var2: test2;\n}\n');
+  });
+
+  test('compile (bootstrap 4)', async () => {
+    const input = '$var1: test1;$var2: test2;';
+    const extractor = new BootstrapExtractor(input, 4);
+    extractor.meta = { 'dx-var1': '$var1', 'dx-var2': '$var2' };
+
+    expect(await extractor.compile()).toBe('dx-empty {\n  dx-var1: test1;\n  dx-var2: test2;\n}');
+  });
+
+  test('extract (bootstrap 3)', async () => {
+    const input = '@var1: test1;@var2: test2;';
+    const extractor = new BootstrapExtractor(input, 3);
+    extractor.meta = { 'dx-var1': '@var1', 'dx-var2': '@var2' };
+
+    expect(await extractor.extract()).toEqual([
+      { key: '$dx-var1', value: 'test1' },
+      { key: '$dx-var2', value: 'test2' }
+    ]);
+  });
+
+  test('extract (bootstrap 4)', async () => {
+    const input = '$var1: test1;$var2: test2;';
+    const extractor = new BootstrapExtractor(input, 4);
+    extractor.meta = { 'dx-var1': '$var1', 'dx-var2': '$var2' };
+
+    expect(await extractor.extract()).toEqual([
+      { key: '$dx-var1', value: 'test1' },
+      { key: '$dx-var2', value: 'test2' }
+    ]);
+  });
+});
