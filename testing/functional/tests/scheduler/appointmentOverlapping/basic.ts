a1a6e2caa3c102292eed1b58b84d5855300f2882
Yana Yarovaya <Yarovaya@users.noreply.github.com>
2020-06-01 10:46:53 +0300

    Fix allDay appointments overlapping in Scheduler week view (T893803) (#13231)

--- testing/functional/tests/scheduler/appointmentOverlapping/basic.ts
+++ testing/functional/tests/scheduler/appointmentOverlapping/basic.ts
@@ -21,11 +21,23 @@ test("Multi-day appointment should not overlap other appointments when specific 
 
 test("Simple appointment should not overlap allDay appointment when specific width is set, 'auto' mode (T864456)", async t => {
     const scheduler = new Scheduler("#container");
-    const { element } = scheduler.getAppointment("Appointment 5");
+    const { element } = scheduler.getAppointment("Appointment 4");
 
     await t
         .expect(scheduler.getAppointmentCollectorCount()).eql(0)
-        .expect(await element.clientTop).eql(0)
+        .expect(await element.getBoundingClientRectProperty("top")).eql(114)
+
+}).before(() => createScheduler({
+    dataSource: allDayData
+}));
+
+test("Crossing allDay appointments should not overlap each other (T893674)", async t => {
+    const scheduler = new Scheduler("#container");
+    const firstAppointment  = scheduler.getAppointment("Appointment 1");
+    const secondAppointment  = scheduler.getAppointment("Appointment 2");
+
+    await t
+        .expect(await firstAppointment.element.getBoundingClientRectProperty("top")).notEql(await secondAppointment.element.getBoundingClientRectProperty("top"))
 
 }).before(() => createScheduler({
     dataSource: allDayData
