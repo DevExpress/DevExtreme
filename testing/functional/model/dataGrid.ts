f97128d9816142435343b46e47393382d71c0e67
Nick Mitrokhin <NickMitrokhin@users.noreply.github.com>
2020-06-02 15:27:55 +0300

    DataGrid - Fixes loosing focus on master row expand (T892203) (#13267)

--- testing/functional/model/dataGrid.ts
+++ testing/functional/model/dataGrid.ts
@@ -203,6 +203,7 @@ class DataRow extends DxElement {
     isFocusedRow: Promise<boolean>;
     isSelected: Promise<boolean>;
     isEdited: Promise<boolean>;
+    isExpanded: Promise<boolean>;
 
     constructor(element: Selector, widgetName: string) {
         super(element);
@@ -211,6 +212,7 @@ class DataRow extends DxElement {
         this.isFocusedRow = this.element.hasClass(CLASS.focusedRow);
         this.isSelected = this.element.hasClass(CLASS.selection);
         this.isEdited = this.element.hasClass(CLASS.editedRow);
+        this.isExpanded = this.element.find(`.${CLASS.commandExpand} .${addWidgetPrefix(this.widgetName, CLASS.groupExpanded)}`).exists;
     }
 
     getDataCell(index: number): DataCell {
@@ -342,7 +344,7 @@ export default class DataGrid extends Widget {
     }
 
     getDataRow(index: number): DataRow {
-        return new DataRow(this.element.find(`.${CLASS.dataRow}:nth-child(${++index})`), this.name);
+        return new DataRow(this.element.find(`.${CLASS.dataRow}[aria-rowindex='${++index}']`), this.name);
     }
 
     getDataCell(rowIndex: number, columnIndex: number): DataCell {
