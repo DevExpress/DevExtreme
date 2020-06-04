4b0326b5bcb6676457b6df00e0d23ddbec11ba4c
Evgeniy Aryamnov <earyamnow@mail.ru>
2020-06-04 14:35:13 +0300

    Fix types

--- js/renovation/scheduler/appointment-tooltip/types.tsx
+++ js/renovation/scheduler/appointment-tooltip/types.tsx
@@ -11,6 +11,17 @@ export type AppointmentItem = {
 };
 export type AppointmentItemSettings = {
   targetedAppointmentData?: dxSchedulerAppointment;
+  originalAppointmentStartDate?: Date;
+  originalAppointmentEndDate?: Date;
+  startDate?: Date;
+  endDate?: Date;
+  direction?: 'vertical' | 'horizontal';
+  allDay?: boolean;
+  isCompact?: boolean;
+  virtual?: boolean;
+  groupIndex?: number;
+  appointmentReduced?: boolean;
+  sortedIndex?: number;
 };
 export type FormattedContent = {
   text: string;
