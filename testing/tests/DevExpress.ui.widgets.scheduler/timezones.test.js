bcc660c80143657d5945c14cf881e3d71bc18a7e
Andrew Makarov <andrey.makarov@devexpress.com>
2020-06-07 16:49:43 +0000

    Fix tests

--- testing/tests/DevExpress.ui.widgets.scheduler/timezones.test.js
+++ testing/tests/DevExpress.ui.widgets.scheduler/timezones.test.js
@@ -35,7 +35,7 @@ const data = [{
     startDateTimeZone: 'America/Los_Angeles', // -7
     endDateTimeZone: 'America/Los_Angeles' // -7
 }, {
-    text: 'app_1_local',
+    text: 'app_local',
     startDate: new Date('2017-05-22T16:00:00.000Z'), // local offset
     endDate: new Date('2017-05-22T16:30:00.000Z') // local offset
 }];
@@ -105,8 +105,8 @@ module('Appointment popup', moduleConfig, () => {
             startDate: (new Date(2017, 4, 22, 5)).valueOf(),
             endDate: (new Date(2017, 4, 22, 5, 30)).valueOf()
         }, {
-            startDate: (new Date(2017, 4, 22, 19)).valueOf(),
-            endDate: (new Date(2017, 4, 22, 19, 30)).valueOf()
+            startDate: (new Date('2017-05-22T16:00:00.000Z')).valueOf(),
+            endDate: (new Date('2017-05-22T16:30:00.000Z')).valueOf()
         }];
 
         cases.forEach((testCase, index) => {
