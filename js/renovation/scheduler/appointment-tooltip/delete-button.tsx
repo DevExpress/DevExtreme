9dc0ced5672d4e076f05b87833633b1d4b3bf732
Evgeniy Aryamnov <earyamnow@mail.ru>
2020-06-15 15:14:47 +0300

    Test DeleteButton

--- js/renovation/scheduler/appointment-tooltip/delete-button.tsx
+++ js/renovation/scheduler/appointment-tooltip/delete-button.tsx
@@ -1,12 +1,14 @@
 import {
-  Component, JSXComponent,
+  Component, JSXComponent, OneWay, ComponentBindings,
 } from 'devextreme-generator/component_declaration/common';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { h } from 'preact';
 import Button, { ButtonProps } from '../../button';
 import { TOOLTIP_APPOINTMENT_ITEM_DELETE_BUTTON } from './consts';
 
 export const viewFunction = (viewModel: DeleteButton) => (
   <Button
-    className={TOOLTIP_APPOINTMENT_ITEM_DELETE_BUTTON}
+    className={`${TOOLTIP_APPOINTMENT_ITEM_DELETE_BUTTON} ${viewModel.props.className}`}
     icon="trash"
     stylingMode="text"
     onClick={viewModel.props.onClick}
@@ -15,8 +17,13 @@ export const viewFunction = (viewModel: DeleteButton) => (
   />
 );
 
+@ComponentBindings()
+export class DeleteButtonProps extends ButtonProps {
+  @OneWay() className?: string = '';
+}
+
 @Component({
   defaultOptionRules: null,
   view: viewFunction,
 })
-export default class DeleteButton extends JSXComponent(ButtonProps) {}
+export default class DeleteButton extends JSXComponent(DeleteButtonProps) {}
--- testing/jest/scheduler/appointment-tooltip/delete-button.tests.tsx
+++ testing/jest/scheduler/appointment-tooltip/delete-button.tests.tsx
@@ -1,42 +1,42 @@
 import { h } from 'preact';
-import { mount, ReactWrapper } from 'enzyme';
-import DeleteButton from '../../../../js/renovation/scheduler/appointment-tooltip/delete-button';
-import Button, { ButtonProps } from '../../../../js/renovation/button';
-
-jest.mock('../../../../js/renovation/button', () => () => null);
+import { shallow } from 'enzyme';
+import { DeleteButtonProps, viewFunction as DeleteButtonView } from '../../../../js/renovation/scheduler/appointment-tooltip/delete-button';
+import Button from '../../../../js/renovation/button';
 
 describe('DeleteButton', () => {
   describe('View', () => {
-    const render = (props = {}): ReactWrapper => {
-      window.h = h;
-      return mount(<DeleteButton {...props} />).childAt(0);
-    };
-    const defaultProps: ButtonProps = {
+    const defaultProps: DeleteButtonProps = {
+      ...(new DeleteButtonProps()),
       onClick: jest.fn(),
     };
 
     it('should render component correctly', () => {
-      const tree = render(defaultProps);
+      const tree = shallow(<DeleteButtonView props={{ ...defaultProps }} />);
 
-      expect(tree.is('.dx-tooltip-appointment-item-delete-button-container'))
-        .toBe(true);
-      expect(tree.children())
-        .toHaveLength(1);
-      expect(tree.childAt(0).type())
+      expect(tree.type())
         .toBe(Button);
-    });
-
-    it('should pass correct properties to button', () => {
-      const tree = render(defaultProps);
-
-      const button = tree.find(Button);
-      expect(button.props())
+      expect(tree.props())
         .toMatchObject({
           icon: 'trash',
           stylingMode: 'text',
           onClick: defaultProps.onClick,
-          className: 'dx-tooltip-appointment-item-delete-button',
         });
+    });
+
+    it('should combine `className` with predefined classes', () => {
+      const tree = shallow(<DeleteButtonView props={{ ...defaultProps, className: 'custom-class' }} />);
+
+      expect(tree.hasClass('dx-tooltip-appointment-item-delete-button')).toBe(true);
+      expect(tree.hasClass('custom-class')).toBe(true);
+    });
+
+    it('should spread restAttributes', () => {
+      const tree = shallow(<DeleteButtonView
+        props={{ ...defaultProps }}
+        restAttributes={{ customAttribute: 'customAttribute' }}
+      />);
+
+      expect(tree.prop('customAttribute')).toBe('customAttribute');
     });
   });
 });
