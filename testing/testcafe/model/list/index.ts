99eab3c9772ccc1822d0e06889eb94e8da47bfcc
Andrey Churkin <alam17@mail.ru>
2020-06-15 18:54:59 +0300

    Renovation: move testcafe tests to the 'testcafe' folder (#13423)
    * Renovation: move testcafe tests to the 'testcafe' folder
    
    * fix build
    
    * fix tests
    
    Co-authored-by: Andrey Churkin <andrey.churkin@Andreys-MacBook-Pro.local>
    Co-authored-by: Andrey Churkin <andrey.churkin@deryagina-nb-10.corp.devexpress.com>

---
+++ testing/testcafe/model/list/index.ts
@@ -0,0 +1,37 @@
+import Widget from '../internal/widget';
+import ListItem from './item';
+import ListGroup from './group';
+
+const CLASS = {
+    group: 'dx-list-group',
+    item: 'dx-list-item',
+    search: 'dx-list-search',
+    selectAllItem: 'dx-list-select-all',
+
+    // Custom classes
+    nestedItem: 'nested-item'
+};
+
+export default class List extends Widget {
+    items: Selector;
+    searchInput: Selector;
+    selectAll: ListItem;
+
+    name: string = 'dxList';
+
+    constructor(id: string|Selector) {
+        super(id);
+
+        this.items = this.element.find(`.${CLASS.item}:not(.${CLASS.nestedItem})`);
+        this.searchInput = this.element.find(`.${CLASS.search} input`);
+        this.selectAll = new ListItem(this.element.find(`.${CLASS.selectAllItem}`));
+    }
+
+    getItem(index: number = 0): ListItem {
+        return new ListItem(this.items.nth(index));
+    }
+
+    getGroup(index: number = 0): ListGroup {
+        return new ListGroup(this.element.find(`.${CLASS.group}`).nth(index));
+    }
+}
