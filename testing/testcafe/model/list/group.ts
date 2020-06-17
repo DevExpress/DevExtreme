7b492f2835cd5c5e9dad6adcdbaf2776eec8add7
Andrey Churkin <alam17@mail.ru>
2020-06-17 21:16:13 +0300

    ESLint: testcafe tests formating

--- testing/testcafe/model/list/group.ts
+++ testing/testcafe/model/list/group.ts
@@ -1,25 +1,27 @@
 import ListItem from './item';
 
 const CLASS = {
-    groupHeader: 'dx-list-group-header',
-    item: 'dx-list-item',
+  groupHeader: 'dx-list-group-header',
+  item: 'dx-list-item',
 
-    // Custom classes
-    nestedItem: 'nested-item'
+  // Custom classes
+  nestedItem: 'nested-item',
 };
 
 export default class ListGroup {
-    element: Selector;
-    header: Selector;
-    items: Selector;
+  element: Selector;
 
-    constructor(element: Selector) {
-        this.element = element;
-        this.header = element.find(`.${CLASS.groupHeader}`);
-        this.items = element.find(`.${CLASS.item}:not(.${CLASS.nestedItem})`);
-    }
+  header: Selector;
 
-    getItem(index: number = 0): ListItem {
-        return new ListItem(this.items.nth(index));
-    }
+  items: Selector;
+
+  constructor(element: Selector) {
+    this.element = element;
+    this.header = element.find(`.${CLASS.groupHeader}`);
+    this.items = element.find(`.${CLASS.item}:not(.${CLASS.nestedItem})`);
+  }
+
+  getItem(index = 0): ListItem {
+    return new ListItem(this.items.nth(index));
+  }
 }
--- testing/testcafe/tests/scheduler/recurrences/basic.ts
+++ testing/testcafe/tests/scheduler/recurrences/basic.ts
@@ -3,23 +3,24 @@ import createScheduler from './init/widget.setup';
 import url from '../../../helpers/getPageUrl';
 import Scheduler from '../../../model/scheduler';
 
-fixture `Rendering of the recurrence appointments in  Scheduler `
-    .page(url(__dirname, '../../container.html'));
+fixture`Rendering of the recurrence appointments in  Scheduler `
+  .page(url(__dirname, '../../container.html'));
 
-test("Drag-n-drop recurrence appointment between dateTable and allDay panel", async t => {
-    const scheduler = new Scheduler("#container");
-    const draggableAppointment = scheduler.getAppointment("Simple recurrence appointment");
+test('Drag-n-drop recurrence appointment between dateTable and allDay panel', async (t) => {
+  const scheduler = new Scheduler('#container');
+  const draggableAppointment = scheduler.getAppointment('Simple recurrence appointment');
 
-    await t
-        .dragToElement(draggableAppointment.element, scheduler.getAllDayTableCell(0), {
-            speed: 0.1
-        })
-        .expect(scheduler.getAppointmentCount()).eql(7)
-        .expect(draggableAppointment.size.width).eql("114px")
-        .expect(draggableAppointment.isAllDay).ok();
-
+  await t
+    .dragToElement(draggableAppointment.element, scheduler.getAllDayTableCell(0), {
+      speed: 0.1,
+    })
+    .expect(scheduler.getAppointmentCount()).eql(7)
+    .expect(draggableAppointment.size.width)
+    .eql('114px')
+    .expect(draggableAppointment.isAllDay)
+    .ok();
 }).before(() => createScheduler({
-    dataSource: dataSource,
-    startDayHour: 1,
-    recurrenceEditMode: "series"
+  dataSource,
+  startDayHour: 1,
+  recurrenceEditMode: 'series',
 }));
