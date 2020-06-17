9c00f95576815cf6032746fec36e1825506e0642
Nick Mitrokhin <NickMitrokhin@users.noreply.github.com>
2020-06-17 14:44:56 +0300

    DataGrid - Fixes updating cell value in a last cell on Tab (T898356) (#13435)

--- testing/testcafe/tests/dataGrid/keyboardNavigation.ts
+++ testing/testcafe/tests/dataGrid/keyboardNavigation.ts
@@ -1073,3 +1073,152 @@ test('The expand cell should not lose focus on expanding a master row (T892203)'
         enabled: true
     }
 }));
+
+['Batch', 'Cell'].forEach(editMode => {
+    test(`${editMode} mode - Cells in a new row should be updated on Tab (T898356)`, async t => {
+        const dataGrid = new DataGrid('#container');
+        const addRowButton = dataGrid.getHeaderPanel().getAddRowButton();
+        const cell00 = dataGrid.getDataCell(0, 0);
+        const editor00 = cell00.getEditor();
+        const cell01 = dataGrid.getDataCell(0, 1);
+        const editor01 = cell01.getEditor();
+
+        await t
+            .click(addRowButton)
+
+            .expect(cell00.isFocused).ok()
+            .expect(editor00.element.focused).ok()
+
+            .typeText(editor00.element, '1')
+            .pressKey('tab')
+
+            .expect(cell00.isFocused).notOk()
+            .expect(dataGrid.apiGetCellValue(0, 0)).eql('1')
+            .expect(cell01.isFocused).ok()
+            .expect(editor01.element.focused).ok()
+
+            .typeText(editor01.element, '2')
+            .pressKey('tab')
+
+            .expect(cell01.isFocused).notOk()
+            .expect(dataGrid.apiGetCellValue(0, 1)).eql('2');
+
+    }).before(() => createWidget('dxDataGrid', {
+        dataSource: [],
+        editing: {
+            mode: editMode.toLowerCase(),
+            allowAdding: true
+        },
+        columns: ['a', 'b']
+    }));
+
+    test(`${editMode} mode - Cells in a new row should be updated on Shift+Tab (T898356)`, async t => {
+        const dataGrid = new DataGrid('#container');
+        const addRowButton = dataGrid.getHeaderPanel().getAddRowButton();
+        const cell00 = dataGrid.getDataCell(0, 0);
+        const editor00 = cell00.getEditor();
+        const cell01 = dataGrid.getDataCell(0, 1);
+        const editor01 = cell01.getEditor();
+
+        await t
+            .click(addRowButton)
+            .click(cell01.element)
+
+            .expect(cell01.isFocused).ok()
+            .expect(editor01.element.focused).ok()
+
+            .typeText(editor01.element, '2')
+            .pressKey('shift+tab')
+
+            .expect(cell01.isFocused).notOk()
+            .expect(dataGrid.apiGetCellValue(0, 1)).eql('2')
+            .expect(cell00.isFocused).ok()
+            .expect(editor00.element.focused).ok()
+
+            .typeText(editor00.element, '1')
+            .pressKey('shift+tab')
+
+            .expect(cell00.isFocused).notOk()
+            .expect(dataGrid.apiGetCellValue(0, 0)).eql('1');
+
+    }).before(() => createWidget('dxDataGrid', {
+        dataSource: [],
+        editing: {
+            mode: editMode.toLowerCase(),
+            allowAdding: true
+        },
+        columns: ['a', 'b']
+    }));
+
+    test(`${editMode} mode - Cells in a modified row should be updated on Tab (T898356)`, async t => {
+        const dataGrid = new DataGrid('#container');
+        const cell00 = dataGrid.getDataCell(0, 0);
+        const editor00 = cell00.getEditor();
+        const cell01 = dataGrid.getDataCell(0, 1);
+        const editor01 = cell01.getEditor();
+
+        await t
+            .click(cell00.element)
+
+            .expect(cell00.isFocused).ok()
+            .expect(editor00.element.focused).ok()
+
+            .typeText(editor00.element, '1')
+            .pressKey('tab')
+
+            .expect(cell00.isFocused).notOk()
+            .expect(dataGrid.apiGetCellValue(0, 0)).eql('11')
+            .expect(cell01.isFocused).ok()
+            .expect(editor01.element.focused).ok()
+
+            .typeText(editor01.element, '2')
+            .pressKey('tab')
+
+            .expect(cell01.isFocused).notOk()
+            .expect(dataGrid.apiGetCellValue(0, 1)).eql('22');
+
+    }).before(() => createWidget('dxDataGrid', {
+        dataSource: [{ a: '1', b: '2' }],
+        editing: {
+            mode: editMode.toLowerCase(),
+            allowUpdating: true
+        },
+        columns: ['a', 'b']
+    }));
+
+    test(`${editMode} mode - Cells in a modified row should be updated on Shift+Tab (T898356)`, async t => {
+        const dataGrid = new DataGrid('#container');
+        const cell00 = dataGrid.getDataCell(0, 0);
+        const editor00 = cell00.getEditor();
+        const cell01 = dataGrid.getDataCell(0, 1);
+        const editor01 = cell01.getEditor();
+
+        await t
+            .click(cell01.element)
+
+            .expect(cell01.isFocused).ok()
+            .expect(editor01.element.focused).ok()
+
+            .typeText(editor01.element, '2')
+            .pressKey('shift+tab')
+
+            .expect(cell01.isFocused).notOk()
+            .expect(dataGrid.apiGetCellValue(0, 1)).eql('22')
+            .expect(cell00.isFocused).ok()
+            .expect(editor00.element.focused).ok()
+
+            .typeText(editor00.element, '1')
+            .pressKey('shift+tab')
+
+            .expect(cell00.isFocused).notOk()
+            .expect(dataGrid.apiGetCellValue(0, 0)).eql('11');
+
+    }).before(() => createWidget('dxDataGrid', {
+        dataSource: [{ a: '1', b: '2' }],
+        editing: {
+            mode: editMode.toLowerCase(),
+            allowUpdating: true
+        },
+        columns: ['a', 'b']
+    }));
+});
