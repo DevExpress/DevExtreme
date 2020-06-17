11c4d52b1dc582c0f8ea4f73660d72e150cc6694
Mike Vitik <vitikm@gmail.com>
2020-06-17 18:15:58 +0300

    Fix click to page count should navigate to last page

--- testing/jest/pager/pages-small.tests.tsx
+++ testing/jest/pager/pages-small.tests.tsx
@@ -1,17 +1,17 @@
-import { h } from 'preact';
+import { h, createRef } from 'preact';
 import { shallow } from 'enzyme';
 import PagesSmall, { viewFunction as PagesSmallComponent } from '../../../js/renovation/pager/pages-small';
 import getElementComputedStyle from '../../../js/renovation/pager/utils/get-computed-style';
 import NumberBox from '../../../js/renovation/number-box';
 
-jest.mock('../../../js/renovation/number-box', () => { });
+jest.mock('../../../js/renovation/number-box', jest.fn());
+jest.mock('../../../js/renovation/pager/page', jest.fn());
 jest.mock('../../../js/renovation/pager/utils/get-computed-style');
 
 describe('Small pager pages', () => {
   // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
   const render = (props) => {
-    const tree = shallow(<PagesSmallComponent {...props} /> as any);
-    // const pageIndexNumberBoxContainer = root.childAt(0);
+    const tree = shallow<Element>(<PagesSmallComponent {...props} /> as any);
     const pageIndexNumberBox = tree.childAt(0);
     const span = tree.childAt(1);
     const maxPage = tree.childAt(2);
@@ -25,33 +25,25 @@ describe('Small pager pages', () => {
   };
 
   it('View', () => {
-    const {
-      pageIndexRef = {} as NumberBox,
-      valueChange,
-      width,
-      value,
-      props,
-    } = new PagesSmall({
-      pageCount: 100, pageIndex: 2, rtlEnabled: true, pagesCountText: 'of',
-    });
+    const props = {
+      valueChange: jest.fn(),
+      width: 40,
+      value: 3,
+      pageIndexRef: createRef() as NumberBox,
+      selectLastPageIndex: jest.fn(),
+      props: { pageCount: 100, pagesCountText: 'of', rtlEnabled: true },
+    } as Partial<PagesSmall>;
     const {
       tree, pageIndexNumberBox, span, maxPage,
-    } = render({
-      pageIndexRef,
-      valueChange,
-      width,
-      value,
-      props,
-    });
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    expect((tree.props() as any).className).toBe('dx-light-pages');
+    } = render(props);
+    expect(tree.props().className).toBe('dx-light-pages');
 
     expect(pageIndexNumberBox.props()).toEqual({
-      className: 'dx-page-index', max: 100, min: 1, value: 3, rtlEnabled: true, valueChange, width: 40,
+      className: 'dx-page-index', max: 100, min: 1, value: 3, rtlEnabled: true, valueChange: props.valueChange, width: 40,
     });
     expect(span.html()).toBe('<span class="dx-info  dx-info-text">of</span>');
     expect(maxPage.props()).toEqual({
-      children: [], index: 99, selected: false, className: 'dx-pages-count',
+      children: [], index: 99, selected: false, className: 'dx-pages-count', onClick: props.selectLastPageIndex,
     });
   });
   it('updateWidth effect', () => {
@@ -62,6 +54,16 @@ describe('Small pager pages', () => {
     component.updateWidth();
     expect(getElementComputedStyle).toBeCalledWith(numberBoxElement);
     expect(component.width).toBe(19 + 10 * 3);
+  });
+  it('selectLastPageIndex', () => {
+    const pageIndexChangeHandler = jest.fn();
+    const component = new PagesSmall({
+      pageCount: 3,
+      pageIndex: 2,
+      pageIndexChange: pageIndexChangeHandler,
+    });
+    component.selectLastPageIndex();
+    expect(pageIndexChangeHandler).toBeCalledWith(2);
   });
   it('setValue', () => {
     const pageIndexChangeHandler = jest.fn();
