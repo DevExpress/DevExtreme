1fbf5621b544e33b7464f5c41b0f133d0ff5c661
Mike Vitik <vitikm@gmail.com>
2020-06-17 13:23:18 +0300

    workaround for angular-integration

--- js/events/core/events_engine.d.ts
+++ js/events/core/events_engine.d.ts
@@ -1,6 +1,8 @@
 type EventsEngineType = {
     on: (element, eventName, handler) => void;
     off: (element, eventName, handler) => void;
+    set: (eventEngine) => void;
 };
 declare const eventsEngine: EventsEngineType;
+export declare function set(eventEngine): void;
 export default eventsEngine;
