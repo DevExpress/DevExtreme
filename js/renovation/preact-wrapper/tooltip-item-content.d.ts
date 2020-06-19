64dabe15557b16fa0f840c058230c8c653487d8c
Evgeniy Aryamnov <earyamnow@mail.ru>
2020-06-19 17:46:43 +0300

    Add d.ts

---
+++ js/renovation/preact-wrapper/tooltip-item-content.d.ts
@@ -0,0 +1,7 @@
+/* eslint-disable no-underscore-dangle */
+import Component from './component';
+
+export default class Button extends Component {
+    _setOptionsByReference(): any;
+}
+/* eslint-enable no-underscore-dangle */
