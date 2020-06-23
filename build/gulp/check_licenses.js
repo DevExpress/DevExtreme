6d39fa54c73f43484ee3f66a80bde858d90682a3
Yana <ms.yana.work@mail.ru>
2020-06-23 14:31:21 +0300

    Revert

--- build/gulp/check_licenses.js
+++ build/gulp/check_licenses.js
@@ -3,8 +3,8 @@ const gulp = require('gulp');
 const lazyPipe = require('lazypipe');
 const named = require('vinyl-named');
 // const context = require('./context.js');
-const path = require('path');
-const artifactsPath = path.join(__dirname, '..', '..', '..', '..', 'artifacts');
+// const path = require('path');
+// const artifactsPath = path.join(__dirname, '..', '..', '..', '..', 'artifacts');
 
 const rruleName = 'rrule.js';
 const rruleUrl = 'https://github.com/jakubroztocil/rrule';
@@ -20,7 +20,9 @@ const checkRruleLicenseComment = lazyPipe()
     });
 
 gulp.task('check-rrule-license-header', function() {
-    const file = path.join(artifactsPath, 'js', 'dx.all.js');
-    return gulp.src(file)
+    // const file = path.join(artifactsPath, 'js', 'dx.all.js');
+    // return gulp.src(context.TRANSPILED_PATH + '/bundles/dx.all.js')
+    //     .pipe(checkRruleLicenseComment());
+    return gulp.src('artifacts/js/dx.all.js')
         .pipe(checkRruleLicenseComment());
 });
