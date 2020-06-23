4c02992f495d0f4c22871d84ac8814797d7873d1
Yana <ms.yana.work@mail.ru>
2020-06-23 21:07:07 +0300

    Test only dx.all.js

--- build/gulp/check_licenses.js
+++ build/gulp/check_licenses.js
@@ -2,9 +2,6 @@
 const gulp = require('gulp');
 const lazyPipe = require('lazypipe');
 const named = require('vinyl-named');
-// const context = require('./context.js');
-// const path = require('path');
-// const artifactsPath = path.join(__dirname, '..', '..', '..', '..', 'artifacts');
 
 const rruleName = 'rrule.js';
 const rruleUrl = 'https://github.com/jakubroztocil/rrule';
@@ -20,9 +17,6 @@ const checkRruleLicenseComment = lazyPipe()
     });
 
 gulp.task('check-rrule-license-header', function() {
-    // const file = path.join(artifactsPath, 'js', 'dx.all.js');
-    // return gulp.src(context.TRANSPILED_PATH + '/bundles/dx.all.js')
-    //     .pipe(checkRruleLicenseComment());
     return gulp.src('artifacts/js/dx.all.js')
         .pipe(checkRruleLicenseComment());
 });
--- gulpfile.js
+++ gulpfile.js
@@ -59,9 +59,9 @@ function createMainBatch() {
 }
 
 function createDefaultBatch() {
-    const tasks = [ 'clean', 'localization', createMainBatch(), 'check-rrule-license-header'];
+    const tasks = [ 'clean', 'localization', createMainBatch()];
     if(!TEST_CI) {
-        tasks.push('npm', 'themebuilder-npm');
+        tasks.push('npm', 'themebuilder-npm', 'check-rrule-license-header');
     }
     return gulp.series(tasks);
 }
