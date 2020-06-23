f2baf8a25a341b15d54269b4c179f10997fb5623
Evgeniy Aryamnov <earyamnow@mail.ru>
2020-06-23 09:31:27 +0300

    Fix

--- js/core/utils/deferred.d.ts
+++ js/core/utils/deferred.d.ts
@@ -10,3 +10,18 @@ export interface Promise<T> {
     state(): string;
     promise(target?: any): Promise<T>;
 }
+
+export interface Deferred<T> {
+    state(): string;
+    always(alwaysCallback?: PromiseCallback<T>): Deferred<T>;
+    done(doneCallback?: PromiseCallback<T>): Deferred<T>;
+    fail(failCallback?: PromiseCallback<T>): Deferred<T>;
+    progress(progressCallback?: PromiseCallback<T>): Deferred<T>;
+    notify(value?: any, ...args: any[]): Deferred<T>;
+    notifyWith(context: any, args?: any[]): Deferred<T>;
+    reject(value?: any, ...args: any[]): Deferred<T>;
+    rejectWith(context: any, args?: any[]): Deferred<T>;
+    resolve(value?: T, ...args: any[]): Deferred<T>;
+    resolveWith(context: any, args?: T[]): Deferred<T>;
+    promise(target?: any): Promise<T>;
+}
