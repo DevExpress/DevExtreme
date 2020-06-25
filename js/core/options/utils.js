fce417d7404ed2a6ab4598101c09674964f3e36e
Roman Simionov <roman-simionov@users.noreply.github.com>
2020-06-25 12:10:21 +0300

    Make createDefaultOptionRules export named (#13574)

--- js/core/options/utils.js
+++ js/core/options/utils.js
@@ -36,6 +36,4 @@ export const getNestedOptionValue = function(optionsObject, name) {
     return cachedGetters[name](optionsObject, { functionsAsIs: true });
 };
 
-export default function createDefaultOptionRules(options = []) {
-    return options;
-}
+export const createDefaultOptionRules = (options = []) => options;
