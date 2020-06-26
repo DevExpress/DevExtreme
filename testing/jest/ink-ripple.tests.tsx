3dff7828aa46f0d4b03b76e2b5edac05d7662370
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2020-06-26 16:31:59 +0300

    Fix lint error

--- testing/jest/ink-ripple.tests.tsx
+++ testing/jest/ink-ripple.tests.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { h } from 'preact';
 import { shallow } from 'enzyme';
 import InkRipple, { viewFunction } from '../../js/renovation/ink-ripple';
