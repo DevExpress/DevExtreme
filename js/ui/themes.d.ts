9d9adf27acd45b59e71a3491dce2910f154d1e75
Pavel Gruba <pavel.gruba@devexpress.com>
2020-06-29 11:39:35 +0300

    Revert themes declaration

--- js/ui/themes.d.ts
+++ js/ui/themes.d.ts
@@ -34,13 +34,4 @@ export default class themes {
      * @public
      */
     static ready(callback: Function): void;
-    /**
-     * @docid ui.themesmethods.isMaterial
-     * @publicName isMaterial(themeName)
-     * @param1 themeName: string
-     * @static
-     * @prevFileNamespace DevExpress.ui
-     * @hidden
-     */
-    static isMaterial(themeName: string): boolean
 }
