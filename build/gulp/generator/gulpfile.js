7a85aa63e1b8cc593542e4de783463c4fc844d72
roman-simionov <roman-simionov@users.noreply.github.com>
2020-07-06 11:19:41 +0300

    Revert "Generate preact components into artifacts only (#13694)"
    This reverts commit d734a1e52370799be30f02f769a3e5d4d39820af.

--- build/gulp/generator/gulpfile.js
+++ build/gulp/generator/gulpfile.js
@@ -8,13 +8,14 @@ const merge = require('merge-stream');
 const { generateComponents } = require('devextreme-generator/component-compiler');
 const generator = require('devextreme-generator/preact-generator').default;
 const ts = require('gulp-typescript');
+const lint = require('gulp-eslint');
 const plumber = require('gulp-plumber');
 const gulpIf = require('gulp-if');
 const babel = require('gulp-babel');
 const notify = require('gulp-notify');
 const watch = require('gulp-watch');
 
-const SRC = ['js/renovation/**/*.tsx', '!js/renovation/**/*.j.tsx'];
+const SRC = ['js/renovation/**/*.tsx'];
 const DEST = 'js/renovation/';
 const BUNDLES_PARTS = 'js/bundles/modules/parts/';
 const COMPAT_TESTS_PARTS = 'testing/jest/compatibility/';
@@ -27,22 +28,6 @@ const knownErrors = [
     'Cannot find module \'preact/compat\''
 ];
 
-function generateJQueryComponents() {
-    generator.options = {
-        defaultOptionsModule: 'js/core/options/utils',
-        jqueryComponentRegistratorModule: 'js/core/component_registrator',
-        jqueryBaseComponentModule: 'js/renovation/preact-wrapper/component',
-        generateJQueryOnly: true
-    };
-
-    return gulp.src(SRC)
-        .pipe(generateComponents(generator))
-        .pipe(plumber(()=>null))
-        .pipe(gulp.dest(DEST));
-}
-
-const context = require('../context.js');
-
 function generatePreactComponents() {
     const tsProject = ts.createProject('build/gulp/generator/ts-configs/preact.tsconfig.json');
 
@@ -52,7 +37,7 @@ function generatePreactComponents() {
         jqueryBaseComponentModule: 'js/renovation/preact-wrapper/component'
     };
 
-    return gulp.src(SRC, { base: 'js' })
+    return gulp.src(SRC)
         .pipe(generateComponents(generator))
         .pipe(plumber(()=>null))
         .pipe(tsProject({
@@ -63,8 +48,15 @@ function generatePreactComponents() {
             },
             finish() {}
         }))
-        .pipe(babel())
-        .pipe(gulp.dest(context.TRANSPILED_PATH));
+        .pipe(gulpIf(file => file.extname === '.js',
+            lint({
+                quiet: true,
+                fix: true,
+                useEslintrc: true
+            })
+        ))
+        .pipe(lint.format())
+        .pipe(gulp.dest(DEST));
 }
 
 function processRenovationMeta() {
@@ -97,7 +89,7 @@ function processRenovationMeta() {
             .pipe(gulp.dest(COMPAT_TESTS_PARTS))
     );
 }
-gulp.task('generate-components', gulp.series(generateJQueryComponents, generatePreactComponents, processRenovationMeta));
+gulp.task('generate-components', gulp.series(generatePreactComponents, processRenovationMeta));
 
 function addGenerationTask(
     frameworkName,
