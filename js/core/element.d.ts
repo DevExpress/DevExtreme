0a0720f544c946e14a13390d2d72b105d60ac1df
ovchinnikov.av <ov4innikov.av@gmail.com>
2020-07-06 15:39:40 +0300

    Delete svg element

--- js/core/element.d.ts
+++ js/core/element.d.ts
@@ -5,12 +5,3 @@
  * @prevFileNamespace DevExpress.core
  */
 export type dxElement = HTMLElement & JQuery;
-
-/**
- * @docid dxSvgElement
- * @type SVGElement|jQuery
- * @hidden
- * @prevFileNamespace DevExpress.core
- */
-export type dxSvgElement = SVGElement & JQuery;
-
