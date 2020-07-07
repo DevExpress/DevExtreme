43bf13362fc53d0454d29cc6182d3aa9a9a38b85
Mike Vitik <vitikm@gmail.com>
2020-07-07 12:30:41 +0300

    Renovated pager QUnit tests

---
+++ testing/helpers/renovationHelper.js
@@ -0,0 +1,40 @@
+import registerComponent from 'core/component_registrator';
+import { name as getName } from 'core/utils/public_component';
+import { act } from 'preact/test-utils';
+
+const createModuleConfig = (oldWidget, renovatedWidget, config) => {
+    const widgetName = getName(oldWidget);
+    return {
+        beforeEach: function() {
+            const renovatedWidgetWrapper = renovatedWidget.inherit({
+                ctor: function() {
+                    let res;
+                    act(() => {
+                        res = this.callBase.apply(this, arguments);
+                    });
+                    return res;
+                },
+                option: function() {
+                    let res;
+                    act(() => {
+                        res = this.callBase.apply(this, arguments);
+                    });
+                    return res;
+                }
+            });
+            renovatedWidgetWrapper.getInstance = renovatedWidget.getInstance;
+            registerComponent(widgetName, renovatedWidgetWrapper);
+            config.beforeEach && config.beforeEach.apply(this);
+        },
+        afterEach: function() {
+            config.afterEach && config.afterEach.apply(this);
+            registerComponent(widgetName, oldWidget);
+        }
+    };
+};
+
+export const getQUnitModuleForTestingRenovationWidget = (oldWidget, newWidget) => (name, config, tests) => {
+    QUnit.module(name, config, () => tests(false));
+    const newConfig = createModuleConfig(oldWidget, newWidget, config);
+    QUnit.module(`Renovated ${name}`, newConfig, () => tests(true));
+};
