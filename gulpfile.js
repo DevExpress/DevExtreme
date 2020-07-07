b5837ba5e1c19d81924656a16c3a412c669820c4
Yana <ms.yana.work@mail.ru>
2020-07-07 12:35:01 +0300

    Remove rrule from NOTICE

--- gulpfile.js
+++ gulpfile.js
@@ -59,8 +59,7 @@ function createMainBatch() {
 function createDefaultBatch() {
     const tasks = [ 'clean', 'localization', createMainBatch() ];
     if(!TEST_CI) {
-        tasks.push('npm', 'themebuilder-npm');
-        tasks.push('check-license-notices');
+        tasks.push('npm', 'themebuilder-npm', 'check-license-notices');
     }
     return gulp.series(tasks);
 }
