96bf8a2b505b7a7766524c25b70ea97f5d0a5faa
dmitry.levkovskiy <dmitry.levkovskiy@devexpress.com>
2020-07-07 15:20:13 +0300

    Fix vAlign for Form in WkWebView-based browser (T898735) (#13727)

--- js/ui/text_box/ui.text_editor.base.js
+++ js/ui/text_box/ui.text_editor.base.js
@@ -8,6 +8,7 @@ import { extend } from '../../core/utils/extend';
 import { inArray } from '../../core/utils/array';
 import { each } from '../../core/utils/iterator';
 import { current, isMaterial } from '../themes';
+import devices from '../../core/devices';
 import Editor from '../editor/editor';
 import { addNamespace } from '../../events/utils';
 import { normalizeKeyName } from '../../events/utils';
@@ -323,10 +324,25 @@ const TextEditorBase = Editor.inherit({
     },
 
     _applyInputAttributes: function($input, customAttributes) {
-        $input.attr('autocomplete', 'off')
-            .attr(customAttributes)
+        const inputAttributes = extend(this._getDefaultAttributes(), customAttributes);
+        $input
+            .attr(inputAttributes)
             .addClass(TEXTEDITOR_INPUT_CLASS)
             .css('minHeight', this.option('height') ? '0' : '');
+    },
+
+    _getDefaultAttributes: function() {
+        const defaultAttributes = {
+            autocomplete: 'off'
+        };
+
+        if(devices.real().ios) {
+            // WA to fix vAlign (T898735)
+            // https://bugs.webkit.org/show_bug.cgi?id=142968
+            defaultAttributes.placeholder = '&nbsp;';
+        }
+
+        return defaultAttributes;
     },
 
     _updateButtons: function(names) {
