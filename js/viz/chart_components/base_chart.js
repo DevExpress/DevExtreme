7bdfb7bfeb79a998d048589106bc3a913024bfc2
polosatov.alexander <alexanderPolosatov@users.noreply.github.com>
2020-07-08 16:30:09 +0300

    Fix panes updating issue(T906065) (#13746)

--- js/viz/chart_components/base_chart.js
+++ js/viz/chart_components/base_chart.js
@@ -1019,6 +1019,7 @@ const BaseChart = BaseWidget.inherit({
         argumentAxis: 'AXES_AND_PANES',
         commonAxisSettings: 'AXES_AND_PANES',
         panes: 'AXES_AND_PANES',
+        commonPaneSettings: 'AXES_AND_PANES',
         defaultPane: 'AXES_AND_PANES',
         useAggregation: 'AXES_AND_PANES',
         containerBackgroundColor: 'AXES_AND_PANES',
