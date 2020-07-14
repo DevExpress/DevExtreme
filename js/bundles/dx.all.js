5489420c5334063479d0eb793c565cf2a84bd70f
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2020-07-14 16:52:52 +0300

    Fix import

--- js/bundles/dx.all.js
+++ js/bundles/dx.all.js
@@ -2,7 +2,7 @@ import './modules/parts/widgets-all';
 import './modules/parts/viz';
 import DevExpress from './modules/core';
 import { EventsStrategy } from '../core/events_strategy';
-import { Options } from '../core/options';
+import { Options } from '../core/options/index';
 
 DevExpress.integration = {};
 
