d363122c56dfb87b1ecb57a2a438daadcf9eae25
groshenkovamarina <marina.groshenkova@devexpress.com>
2020-07-14 14:55:00 +0300

    dxLookup - Fix dependence of usePopover and itemCenteringEnabled options

--- js/ui/lookup.js
+++ js/ui/lookup.js
@@ -635,7 +635,7 @@ const Lookup = DropDownList.inherit({
     },
 
     _getPopupHeight: function() {
-        if(this._list && this._list.itemElements() && this.option('itemCenteringEnabled')) {
+        if(this._list && this._list.itemElements()) {
             return this._calculateListHeight(this.option('grouped')) +
                 (this._$searchWrapper ? this._$searchWrapper.outerHeight() : 0) +
                 (this._popup._$bottom ? this._popup._$bottom.outerHeight() : 0) +
@@ -646,12 +646,12 @@ const Lookup = DropDownList.inherit({
     },
 
     _getPopupWidth: function() {
-        return this.option('itemCenteringEnabled') ? $(this.element()).outerWidth() : $(window).width() * 0.8;
+        return $(this.element()).outerWidth();
     },
 
     _renderPopup: function() {
         if(this.option('usePopover') && !this.option('fullScreen')) {
-            if(this.option('_scrollToSelectedItemEnabled') && this.option('itemCenteringEnabled')) {
+            if(this.option('_scrollToSelectedItemEnabled')) {
                 this.callBase();
             } else {
                 this._renderPopover();
@@ -730,10 +730,14 @@ const Lookup = DropDownList.inherit({
 
         result.maxHeight = function() { return $(window).height(); };
 
-        if(this.option('_scrollToSelectedItemEnabled') && this.option('itemCenteringEnabled')) {
-            result.position = {
+        if(this.option('_scrollToSelectedItemEnabled')) {
+            result.position = this.option('itemCenteringEnabled') ? {
                 my: 'left top',
                 at: 'left top',
+                of: this.element()
+            } : {
+                my: 'left top',
+                at: 'left bottom',
                 of: this.element()
             };
         }
@@ -1125,8 +1129,9 @@ const Lookup = DropDownList.inherit({
             case '_scrollToSelectedItemEnabled':
                 break;
             case 'itemCenteringEnabled':
-                if(this.option('_scrollToSelectedItemEnabled') && value) {
-                    this.option('usePopover', false);
+                if(this.option('_scrollToSelectedItemEnabled')) {
+                    this.option('position', undefined);
+                    this._renderPopup();
                 }
                 break;
             default:
