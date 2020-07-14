9e1b300b8c0c239cb13560b649903c87eac89524
Dmitry Levkovskiy <DokaRus@users.noreply.github.com>
2020-07-14 12:29:02 +0300

    Refactoring dxGallery event listeners to resolve issue with Safari (T877000) (#13777)
    * Refactoring dxGallery event listeners to resolve issue with Safari (T877000)
    
    * tiny cleanup

---
+++ testing/testcafe/model/gallery/index.ts
@@ -0,0 +1,22 @@
+import Widget from '../internal/widget';
+import GalleryIndicatorItem from './indicatorItem';
+
+const CLASS = {
+  indicatorItem: 'dx-gallery-indicator-item'
+};
+
+export default class List extends Widget {
+  indicatorItems: Selector;
+
+  name = 'dxGallery';
+
+  constructor(id: string|Selector) {
+    super(id);
+
+    this.indicatorItems = this.element.find(`.${CLASS.indicatorItem}`);
+  }
+
+  getIndicatorItem(index = 0): GalleryIndicatorItem {
+    return new GalleryIndicatorItem(this.indicatorItems.nth(index));
+  }
+}
