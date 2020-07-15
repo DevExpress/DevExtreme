634646f3a1c1b1eb0e4f6b6e65fd0f798f4120b9
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2020-07-15 15:04:24 +0300

    Scheduler - Renovation - TimePanel (#13807)
    * Scheduler - Renovate TimePanel
    
    * Add tests
    
    * Add tests
    
    Co-authored-by: Anton Sermyazhko <pin361@gmail.com>

---
+++ js/renovation/scheduler/workspaces/base/time_panel/cell.tsx
@@ -0,0 +1,34 @@
+import {
+  Component, ComponentBindings, JSXComponent, OneWay,
+} from 'devextreme-generator/component_declaration/common';
+
+export const viewFunction = (viewModel: TimePanelCell) => (
+  <td
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    {...viewModel.restAttributes}
+    className={
+      `dx-scheduler-time-panel-cell dx-scheduler-cell-sizes-vertical
+      dx-scheduler-first-group-cell dx-scheduler-last-group-cell
+      ${viewModel.props.className}`
+    }
+  >
+    <div>
+      {viewModel.props.text}
+    </div>
+  </td>
+);
+
+@ComponentBindings()
+export class TimePanelCellProps {
+  @OneWay() startDate?: Date = new Date();
+
+  @OneWay() text?: string = '';
+
+  @OneWay() className?: string;
+}
+
+@Component({
+  defaultOptionRules: null,
+  view: viewFunction,
+})
+export class TimePanelCell extends JSXComponent(TimePanelCellProps) {}
