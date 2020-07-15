fa88f488d6c5ce86b41db2aa254369e1cfbfae96
Sergey Novikov <57402891+novsstation@users.noreply.github.com>
2020-07-15 14:49:49 +0300

    Menu - onItemRendered is not executed for sub menu items (T906117) (#13787)
    * Bugfix T906117
    
    * Tests for bugfix
    
    * Fix naming of the tests
    
    * fix the component and element properties of the event arg
    
    * Simplified fix
    
    * Remove unnecessary method
    
    * PR feedback

--- js/ui/menu/ui.menu.js
+++ js/ui/menu/ui.menu.js
@@ -50,7 +50,7 @@ const DEFAULT_DELAY = {
     'hide': 300
 };
 
-const ACTIONS = ['onSubmenuShowing', 'onSubmenuShown', 'onSubmenuHiding', 'onSubmenuHidden', 'onItemContextMenu', 'onItemClick', 'onSelectionChanged'];
+const ACTIONS = ['onSubmenuShowing', 'onSubmenuShown', 'onSubmenuHiding', 'onSubmenuHidden', 'onItemContextMenu', 'onItemClick', 'onSelectionChanged', 'onItemRendered'];
 
 class Menu extends MenuBase {
 
@@ -525,7 +525,7 @@ class Menu extends MenuBase {
             },
             onSelectionChanged: this._nestedItemOnSelectionChangedHandler.bind(this),
             onItemClick: this._nestedItemOnItemClickHandler.bind(this),
-            onItemRendered: this.option('onItemRendered'),
+            onItemRendered: this._nestedItemOnItemRenderedHandler.bind(this),
             onLeftFirstItem: isMenuHorizontal ? null : this._moveMainMenuFocus.bind(this, PREVITEM_OPERATION),
             onLeftLastItem: isMenuHorizontal ? null : this._moveMainMenuFocus.bind(this, NEXTITEM_OPERATION),
             onCloseRootSubmenu: this._moveMainMenuFocus.bind(this, isMenuHorizontal ? PREVITEM_OPERATION : null),
@@ -609,6 +609,10 @@ class Menu extends MenuBase {
 
     _nestedItemOnItemClickHandler(e) {
         this._actions['onItemClick'](e);
+    }
+
+    _nestedItemOnItemRenderedHandler(e) {
+        this._actions['onItemRendered'](e);
     }
 
     _attachSubmenuHandlers($rootItem, submenu) {
