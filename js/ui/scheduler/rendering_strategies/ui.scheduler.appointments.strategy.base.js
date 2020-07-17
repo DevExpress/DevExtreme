9fd6c0e5cfbf4f51861d11e2bd0970e162c8b0c4
Andrew Makarov <andrey.makarov@devexpress.com>
2020-07-17 17:50:59 +0300

    Fix tests

--- js/ui/scheduler/rendering_strategies/ui.scheduler.appointments.strategy.base.js
+++ js/ui/scheduler/rendering_strategies/ui.scheduler.appointments.strategy.base.js
@@ -564,7 +564,7 @@ class BaseRenderingStrategy {
             }
         }
 
-        const currentViewEndTime = new Date(new Date(endDate).setHours(this.instance.option('endDayHour'), 0, 0));
+        const currentViewEndTime = new Date(new Date(endDate).setHours(this.instance._getCurrentViewOption('endDayHour'), 0, 0));
 
         if(endDate.getTime() > currentViewEndTime.getTime()) {
             endDate = currentViewEndTime;
