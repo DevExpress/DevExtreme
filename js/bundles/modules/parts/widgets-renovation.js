efe466c2e14b57a750b70d6b44561f699ff86853
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2020-07-22 12:10:26 +0300

    Update widgets-renovation.js
    Remove internal components

--- js/bundles/modules/parts/widgets-renovation.js
+++ js/bundles/modules/parts/widgets-renovation.js
@@ -8,8 +8,6 @@ renovation.dxrButton = require('../../../renovation/ui/button.j').default;
 renovation.dxrWidget = require('../../../renovation/ui/common/widget.j').default;
 renovation.dxrGridPager = require('../../../renovation/pager/grid_pager.j').default;
 renovation.dxrDataGrid = require('../../../renovation/data_grid/data_grid.j').default;
-renovation.dxrDayDateTableLayout = require('../../../renovation/ui/scheduler/workspaces/day/date_table/layout.j').default;
-renovation.dxrTimePanelTableLayout = require('../../../renovation/ui/scheduler/workspaces/base/time_panel/layout.j').default;
 renovation.dxrTooltipItemLayout = require('../../../renovation/ui/scheduler/appointment_tooltip/item_layout.j').default;
 /// BUNDLER_PARTS_END
 module.exports = renovation;
