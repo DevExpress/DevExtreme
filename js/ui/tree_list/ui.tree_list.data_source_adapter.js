42ce857c16b4c64ca19cd7042b132ce8fb247eca
Jaan Toming <jtoming@gmail.com>
2020-07-24 16:58:04 +0300

    TreeList: Fix error was thrown if rootValue is defined and node's parentId is null (T915695) (#13917)

--- js/ui/tree_list/ui.tree_list.data_source_adapter.js
+++ js/ui/tree_list/ui.tree_list.data_source_adapter.js
@@ -135,7 +135,7 @@ let DataSourceAdapterTreeList = DataSourceAdapter.inherit((function() {
             const key = this._keyGetter(item);
             let parentId = this._parentIdGetter(item);
 
-            parentId = isDefined(parentId) ? parentId : rootValue;
+            parentId = parentId !== undefined ? parentId : rootValue;
             const parentNode = nodeByKey[parentId] = nodeByKey[parentId] || { key: parentId, children: [] };
 
             const node = nodeByKey[key] = nodeByKey[key] || { key: key, children: [] };
