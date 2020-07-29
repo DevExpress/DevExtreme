af621e2bf51b4aa7861d4d0f5e265f421bcd6d8d
Roman Rodin <roman.rodin@devexpress.com>
2020-07-29 16:11:48 +0300

    Diagram - move to export default from the core package

--- js/ui/diagram/diagram.importer.js
+++ js/ui/diagram/diagram.importer.js
@@ -1,10 +1,9 @@
 import Errors from '../widget/ui.errors';
-import * as Diagram from 'devexpress-diagram';
+import Diagram from 'devexpress-diagram';
 
 export function getDiagram() {
-    if(!Diagram.DiagramCommand) {
+    if(!Diagram) {
         throw Errors.Error('E1041', 'devexpress-diagram');
     }
-
     return Diagram;
 }
