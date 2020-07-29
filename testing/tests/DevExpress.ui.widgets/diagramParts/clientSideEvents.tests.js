628174bd4adc070fa7e8c4d26903be313a04f574
Roman Rodin <roman.rodin@devexpress.com>
2020-07-29 16:45:40 +0300

    Diagram - Data in the store is already updated in ArrayStore.onUpdating (T901080)

--- testing/tests/DevExpress.ui.widgets/diagramParts/clientSideEvents.tests.js
+++ testing/tests/DevExpress.ui.widgets/diagramParts/clientSideEvents.tests.js
@@ -45,18 +45,20 @@ QUnit.module('ClientSideEvents', {
         assert.equal(selectedItems[0].text, 'A new ticket');
     });
     test('click on bound diagram', function(assert) {
+        const nodes = [
+            { key: '123', text: 'mytext', foo: 'bar' },
+            { key: '345', text: 'myconnector' }
+        ];
+        const edges = [
+            { key: '1', from: '123', to: '345' }
+        ];
         this.instance.option('nodes.keyExpr', 'key');
         this.instance.option('nodes.textExpr', 'text');
         this.instance.option('edges.keyExpr', 'key');
         this.instance.option('edges.fromKey', 'from');
         this.instance.option('edges.toKey', 'to');
-        this.instance.option('nodes.dataSource', [
-            { key: '123', text: 'mytext', foo: 'bar' },
-            { key: '345', text: 'myconnector' }
-        ]);
-        this.instance.option('edges.dataSource', [
-            { key: '1', from: '123', to: '345' }
-        ]);
+        this.instance.option('nodes.dataSource', nodes);
+        this.instance.option('edges.dataSource', edges);
         let clickedItem;
         let dblClickedItem;
         this.instance.option('onItemClick', function(e) {
@@ -69,16 +71,19 @@ QUnit.module('ClientSideEvents', {
         assert.equal(clickedItem.dataItem.key, '123');
         assert.equal(clickedItem.dataItem.foo, 'bar');
         assert.equal(clickedItem.text, 'mytext');
+        assert.equal(clickedItem.dataItem.key, nodes[0].key);
         assert.equal(dblClickedItem, undefined);
 
         this.instance._diagramInstance.onNativeAction.raise('notifyItemDblClick', this.instance._diagramInstance.model.findShapeByDataKey('123').toNative());
         assert.equal(dblClickedItem.dataItem.key, '123');
         assert.equal(dblClickedItem.dataItem.foo, 'bar');
         assert.equal(dblClickedItem.text, 'mytext');
+        assert.equal(dblClickedItem.dataItem.key, nodes[0].key);
 
         this.instance._diagramInstance.onNativeAction.raise('notifyItemClick', this.instance._diagramInstance.model.findConnectorByDataKey('1').toNative());
         assert.equal(clickedItem.dataItem.key, '1');
         assert.equal(clickedItem.fromKey, '123');
         assert.equal(clickedItem.toKey, '345');
+        assert.equal(clickedItem.dataItem.key, edges[0].key);
     });
 });
