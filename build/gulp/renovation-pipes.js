bf97eb04c51c6a496c1bd97221d719ed976610f1
maximkudriavtsev <maxim_71rus_wow@mail.ru>
2020-07-31 17:17:52 +0300

    Replace scheduler options

--- build/gulp/renovation-pipes.js
+++ build/gulp/renovation-pipes.js
@@ -3,6 +3,7 @@
 const lazyPipe = require('lazypipe');
 const gulpEach = require('gulp-each');
 const gulpIf = require('gulp-if');
+const replace = require('gulp-replace');
 const renovatedComponents = require('../../js/bundles/modules/parts/renovation');
 
 const renovatedFileNames = renovatedComponents.map(component => component.name);
@@ -25,5 +26,10 @@ module.exports = {
                 const fileContext = 'import Widget from "../renovation/' + component.pathInRenovationFolder + '";export default Widget;';
                 callback(null, fileContext);
             }));
+        })
+        .pipe(function() { // Workaround for scheduler `renovateRender` property.
+            return gulpIf(function(file) {
+                return file.basename === 'ui.scheduler.work_space.js';
+            }, replace('renovateRender: false', 'renovateRender: true'));
         }),
 };
