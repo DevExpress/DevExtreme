82bcc24aa4610fd9d27483fcdabdaf80490e9747
Vladimir Kuznetsov <kuznetsov.vladimir.n@gmail.com>
2020-07-31 16:37:27 +0300

    Remove doc comments
    (cherry picked from commit 21cd44dbf6d532797ff405b8b4cc17a062fb32d0)

--- js/pdf_exporter.js
+++ js/pdf_exporter.js
@@ -1,14 +1,5 @@
 import { exportDataGrid } from './exporter/pdfjs/export_data_grid';
 
-/**
-* @name pdfExporter
-* @section utils
-*/
-/**
-* @name ExportDataGridProps
-* @namespace DevExpress.pdfExporter
-* @type object
-*/
 
 export {
     exportDataGrid
