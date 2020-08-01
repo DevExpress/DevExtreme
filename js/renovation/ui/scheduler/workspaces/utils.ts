194a5daaf1af5b4b0ffbdd666455f12c155b6dcf
Anton Sermyazhko <pin361@gmail.com>
2020-08-01 18:19:05 +0300

    Extend 'utils'

--- js/renovation/ui/scheduler/workspaces/utils.ts
+++ js/renovation/ui/scheduler/workspaces/utils.ts
@@ -1,4 +1,5 @@
 import { combineClasses } from '../../../utils/combine_classes';
+import { GroupedViewData } from './types.d';
 
 export const getKeyByDateAndGroup = (date: Date, group?: object): string => {
   let key = date.toString();
@@ -26,7 +27,16 @@ export const addHeightToStyle = (
 export const getGroupCellClasses = (
   isFirstCell: boolean,
   isLastCell: boolean,
+  className = '',
 ): string => combineClasses({
   'dx-scheduler-first-group-cell': isFirstCell,
   'dx-scheduler-last-group-cell': isLastCell,
+  [className]: true,
 });
+
+export const getIsGroupedAllDayPanel = (viewData: GroupedViewData): boolean => {
+  const { groupedData } = viewData;
+  const isAllDayPanel = !!groupedData[0]?.allDayPanel;
+
+  return isAllDayPanel && groupedData.length > 1;
+};
