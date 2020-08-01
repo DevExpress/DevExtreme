e97d6d510518adea26faa62af53f63134ebefbdb
Anton Sermyazhko <pin361@gmail.com>
2020-08-02 00:20:39 +0300

    DateTableBody - Add 'AllDayPanelTableBody'

--- js/renovation/ui/scheduler/workspaces/base/date_table/__tests__/table_body.test.tsx
+++ js/renovation/ui/scheduler/workspaces/base/date_table/__tests__/table_body.test.tsx
@@ -5,6 +5,18 @@ import { DateTableRow as Row } from '../row';
 import * as utilsModule from '../../../utils';
 
 const getKeyByDateAndGroup = jest.spyOn(utilsModule, 'getKeyByDateAndGroup');
+jest.mock('devextreme-generator/component_declaration/common', () => ({
+  ...require.requireActual('devextreme-generator/component_declaration/common'),
+  Fragment: ({ children }) => <div>{children}</div>,
+}));
+jest.mock('../row', () => ({
+  ...require.requireActual('../row'),
+  DateTableRow: ({ children }) => <div>{children}</div>,
+}));
+jest.mock('../all_day_panel/table_body', () => ({
+  ...require.requireActual('../all_day_panel/table_body'),
+  AllDayPanelTableBody: (props) => <div {...props} />,
+}));
 
 describe('DateTableBody', () => {
   describe('Render', () => {
@@ -12,7 +24,8 @@ describe('DateTableBody', () => {
       groupedData: [{
         dateTable: [
           [{ startDate: new Date(2020, 6, 9, 0), endDate: new Date(2020, 6, 9, 0, 30), groups: 1 }],
-          [{ startDate: new Date(2020, 6, 9, 0, 30), endDate: new Date(2020, 6, 9, 1), groups: 2 }],
+          [{ startDate: new Date(2020, 6, 9, 1), endDate: new Date(2020, 6, 9, 1, 30), groups: 2 }],
+          [{ startDate: new Date(2020, 6, 9, 2), endDate: new Date(2020, 6, 9, 2, 30), groups: 3 }],
         ],
       }],
     };
@@ -34,36 +47,45 @@ describe('DateTableBody', () => {
       const rows = render({}).find(Row);
 
       expect(rows)
-        .toHaveLength(2);
+        .toHaveLength(3);
     });
 
     it('should render cells and pass correct props to them', () => {
       const tableBody = render({});
+      const assert = (
+        cells: any,
+        index: number,
+        isFirstCell: boolean,
+        isLastCell: boolean,
+      ): void => {
+        const cell = cells.at(index);
+
+        expect(cell.prop('isFirstCell'))
+          .toBe(isFirstCell);
+        expect(cell.prop('isLastCell'))
+          .toBe(isLastCell);
+        expect(cell.props())
+          .toMatchObject({
+            startDate: viewData.groupedData[0].dateTable[index][0].startDate,
+            endDate: viewData.groupedData[0].dateTable[index][0].endDate,
+            groups: viewData.groupedData[0].dateTable[index][0].groups,
+          });
+      };
 
       const cells = tableBody.find(cellTemplate);
       expect(cells)
-        .toHaveLength(2);
+        .toHaveLength(3);
 
-      expect(cells.at(0).props())
-        .toMatchObject({
-          startDate: viewData.groupedData[0].dateTable[0][0].startDate,
-          endDate: viewData.groupedData[0].dateTable[0][0].endDate,
-          groups: viewData.groupedData[0].dateTable[0][0].groups,
-        });
-
-      expect(cells.at(1).props())
-        .toMatchObject({
-          startDate: viewData.groupedData[0].dateTable[1][0].startDate,
-          endDate: viewData.groupedData[0].dateTable[1][0].endDate,
-          groups: viewData.groupedData[0].dateTable[1][0].groups,
-        });
+      assert(cells, 0, true, false);
+      assert(cells, 1, false, false);
+      assert(cells, 2, false, true);
     });
 
     it('should call getKeyByDateAndGroup with correct parameters', () => {
       render({});
 
       expect(getKeyByDateAndGroup)
-        .toHaveBeenCalledTimes(4);
+        .toHaveBeenCalledTimes(6);
 
       expect(getKeyByDateAndGroup)
         .toHaveBeenNthCalledWith(
@@ -75,6 +97,7 @@ describe('DateTableBody', () => {
           2, viewData.groupedData[0].dateTable[0][0].startDate,
           viewData.groupedData[0].dateTable[0][0].groups,
         );
+
       expect(getKeyByDateAndGroup)
         .toHaveBeenNthCalledWith(
           3, viewData.groupedData[0].dateTable[1][0].startDate,
@@ -84,6 +107,35 @@ describe('DateTableBody', () => {
         .toHaveBeenNthCalledWith(
           4, viewData.groupedData[0].dateTable[1][0].startDate,
           viewData.groupedData[0].dateTable[1][0].groups,
+        );
+
+      expect(getKeyByDateAndGroup)
+        .toHaveBeenNthCalledWith(
+          5, viewData.groupedData[0].dateTable[2][0].startDate,
+          viewData.groupedData[0].dateTable[2][0].groups,
+        );
+      expect(getKeyByDateAndGroup)
+        .toHaveBeenNthCalledWith(
+          6, viewData.groupedData[0].dateTable[2][0].startDate,
+          viewData.groupedData[0].dateTable[2][0].groups,
+        );
+    });
+
+    it('should call `getIsGroupedAllDayPanel` with correct parameters', () => {
+      (getIsGroupedAllDayPanel as jest.Mock).mockReturnValue(true);
+
+      const tableBody = render({});
+
+      expect(tableBody.find(AllDayPanelTableBody).exists())
+        .toBe(true);
+
+      expect(getIsGroupedAllDayPanel)
+        .toHaveBeenCalledTimes(1);
+
+      expect(getIsGroupedAllDayPanel)
+        .toHaveBeenNthCalledWith(
+          1,
+          viewData,
         );
     });
   });
