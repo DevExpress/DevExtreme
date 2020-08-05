34206df57669e006f62ce9f3d270dcf9e93b2f36
Dmitry Levkovskiy <DokaRus@users.noreply.github.com>
2020-08-05 12:44:01 +0300

    Describe DropDownButtonItem "onClick" field (T903650) (#14104)

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -3487,6 +3487,8 @@ declare module DevExpress.ui {
     }
     /** @name dxDropDownButtonItem */
     export interface dxDropDownButtonItem extends dxListItem {
+        /** @name dxDropDownButtonItem.onClick */
+        onClick?: ((e: { component?: dxDropDownButton, element?: DevExpress.core.dxElement, model?: any, jQueryEvent?: JQueryEventObject, event?: DevExpress.events.event }) => any) | string;
     }
     /** @name dxDropDownEditor.Options */
     export interface dxDropDownEditorOptions<T = dxDropDownEditor> extends dxTextBoxOptions<T> {
