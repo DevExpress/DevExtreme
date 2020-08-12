b9b0ecb15fd9707b4b95204e1945907fbde948be
Krijovnick <yulianabest7@yandex.ru>
2020-08-12 18:16:44 +0300

    add test

--- js/viz/components/legend.js
+++ js/viz/components/legend.js
@@ -709,7 +709,6 @@ extend(legendPrototype, {
     },
 
     _createLines: function(lines, layoutOptions) {
-
         this._items.forEach((item, i) => {
             const tableLine = getLines(lines, layoutOptions, i);
             const labelBox = {
@@ -725,7 +724,7 @@ extend(legendPrototype, {
                 height: item.markerBBox.height,
                 element: item.marker,
                 pos: {
-                    horizontal: labelBox.pos.vertical === TOP || labelBox.pos.vertical === BOTTOM ? CENTER : LEFT,
+                    horizontal: labelBox.pos.vertical === TOP || labelBox.pos.vertical === BOTTOM ? CENTER : labelBox.pos.horizontal,
                     vertical: CENTER
                 },
                 bBox: { width: item.markerBBox.width, height: item.markerBBox.height, x: item.markerBBox.x, y: item.markerBBox.y },
