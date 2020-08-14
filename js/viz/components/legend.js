db9bc657a0bf8b619d5a66cee895017f2fc93290
Smirnova Yuliya <Krijovnick@users.noreply.github.com>
2020-08-14 18:28:15 +0300

    Revert "dxChart - fix items alignment for legend with template (T918106) (#14210)" (#14240)
    This reverts commit cfba893a43d75bf9a8c23ff67225083164c7ebdf.

--- js/viz/components/legend.js
+++ js/viz/components/legend.js
@@ -705,6 +705,7 @@ extend(legendPrototype, {
     },
 
     _createLines: function(lines, layoutOptions) {
+
         this._items.forEach((item, i) => {
             const tableLine = getLines(lines, layoutOptions, i);
             const labelBox = {
@@ -720,7 +721,7 @@ extend(legendPrototype, {
                 height: item.markerBBox.height,
                 element: item.marker,
                 pos: {
-                    horizontal: labelBox.pos.vertical === TOP || labelBox.pos.vertical === BOTTOM ? CENTER : labelBox.pos.horizontal,
+                    horizontal: CENTER,
                     vertical: CENTER
                 },
                 bBox: { width: item.markerBBox.width, height: item.markerBBox.height, x: item.markerBBox.x, y: item.markerBBox.y },
