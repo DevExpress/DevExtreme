249272307b11c698ba4ddfe3561407bbff383c34
Mike Vitik <vitikm@gmail.com>
2020-08-17 14:12:46 +0300

    Remove gridpager Add gridCompatibility option Refactor pager props forwarding

--- js/bundles/modules/parts/renovation.js
+++ js/bundles/modules/parts/renovation.js
@@ -5,5 +5,5 @@ module.exports = [
     { name: 'Widget', pathInRenovationFolder: 'ui/widget.j' },
     { name: 'TooltipItemLayout', pathInRenovationFolder: 'ui/scheduler/appointment-tooltip/item-layout.j' },
     // { name: 'DataGrid', pathInRenovationFolder: 'ui/data_grid/data_grid.j' },
-    { name: 'Pager', pathInRenovationFolder: 'ui/pager/grid_pager.j' },
+    { name: 'Pager', pathInRenovationFolder: 'ui/pager/pager.j' },
 ];
