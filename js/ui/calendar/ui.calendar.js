c5cd19c4a511a5faf712129a30e9b8c8b475ca09
Dmitry Levkovskiy <DokaRus@users.noreply.github.com>
2020-08-18 12:12:39 +0300

    Calendar views should sync root disabled state (T923199) (#14275)
    * Calendar views should sync root disabled state (T923199)
    
    * remove useless optionChanged handler

--- js/ui/calendar/ui.calendar.js
+++ js/ui/calendar/ui.calendar.js
@@ -618,7 +618,7 @@ const Calendar = Editor.inherit({
         const $view = $('<div>').appendTo(this._$viewsWrapper);
         const config = this._viewConfig(date);
 
-        return new specificView($view, config);
+        return this._createComponent($view, specificView, config);
     },
 
     _viewConfig: function(date) {
@@ -631,8 +631,6 @@ const Calendar = Editor.inherit({
             max: this._getMaxDate(),
             firstDayOfWeek: this.option('firstDayOfWeek'),
             value: this._dateOption('value'),
-            rtlEnabled: this.option('rtlEnabled'),
-            disabled: this.option('disabled'),
             tabIndex: undefined,
             focusStateEnabled: this.option('focusStateEnabled'),
             hoverStateEnabled: this.option('hoverStateEnabled'),
@@ -1197,10 +1195,6 @@ const Calendar = Editor.inherit({
                 this.option('currentDate', typeUtils.isDefined(value) ? new Date(value) : new Date());
                 this._updateViewsValue(value);
                 this._setSubmitValue(value);
-                this.callBase(args);
-                break;
-            case 'disabled':
-                this._view.option('disabled', value);
                 this.callBase(args);
                 break;
             case 'onCellClick':
