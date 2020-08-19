09c9a3c90bd921e49fa2bf240da3ef82e77ded5c
Andrew Makarov <andrey.makarov@devexpress.com>
2020-08-19 11:30:04 +0300

    dxScheduler - refactoring after implement timezone engine (#14214)
    * Move createSettings to separate file
    
    * Add appointmentSettingsGenerator.js
    
    * Refactoring tests
    
    * Fix TODO description
    
    * Remove unnecessary tests
    
    * Refactoring updateScrollPosition
    
    * Merge with https://github.com/DevExpress/DevExtreme/pull/14234
    
    * Fix 'this'
    
    * fix merge conflicts
    
    * Fix merge

--- js/ui/scheduler/appointmentAdapter.js
+++ js/ui/scheduler/appointmentAdapter.js
@@ -98,7 +98,7 @@ class AppointmentAdapter {
     }
 
     calculateDate(date, appointmentTimeZone, pathTimeZoneConversion) {
-        if(!date) { // TODO: E1032 should be trown only for startDate above
+        if(!date) { // TODO: E1032 should be thrown only for startDate above
             return undefined;
         }
 
