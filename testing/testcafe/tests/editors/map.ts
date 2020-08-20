6a56c505bda96f84987a3c4112e1f2ee4c16768c
ksercs <ksercs0@gmail.com>
2020-08-20 17:10:19 +0300

    fix lint errors

--- testing/testcafe/tests/editors/map.ts
+++ testing/testcafe/tests/editors/map.ts
@@ -5,10 +5,10 @@ fixture`Map`
   .page(url(__dirname, './pages/T914315.html'));
 
 test('Map should not raise errors when it is disposed immediately after creating (T914315)', async (t) => {
-    const $editButton = Selector('.dx-command-edit').child();
+  const $editButton = Selector('.dx-command-edit').child();
 
-    await t
-        .click($editButton)
-        .wait(500)
-        .expect(true).ok();
+  await t
+    .click($editButton)
+    .wait(500)
+    .expect(true).ok();
 });
