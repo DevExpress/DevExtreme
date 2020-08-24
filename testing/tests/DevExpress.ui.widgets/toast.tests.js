dd30bfe1b3d62b3021f07aaf48ed97edeff974ef
ksercs <ksercs0@gmail.com>
2020-08-24 16:48:48 +0300

    one more try

--- testing/tests/DevExpress.ui.widgets/toast.tests.js
+++ testing/tests/DevExpress.ui.widgets/toast.tests.js
@@ -84,27 +84,20 @@ QUnit.module('general', moduleConfig, () => {
     });
 
     QUnit.test('position on mobile devices', function(assert) {
-        const originalRealDevice = devices.real();
         const originalCurrentDevice = devices.current();
 
         try {
-            devices.real({ deviceType: 'phone' });
             devices.current({ deviceType: 'phone' });
             fx.off = false;
 
-            this.instance.option({
-                message: 'test42',
-                position: { my: 'bottom center', at: 'bottom center', offset: '0 0' }
-            });
-
-            fx.off = true;
+            this.instance = new Toast(this.$element);
             this.instance.show();
+            this.clock.tick(5000);
 
             const $content = this.instance.$content();
             assert.roughEqual($content.offset().top + $content.outerHeight(), window.visualViewport.height, 1.01);
             assert.roughEqual($content.outerWidth(), window.visualViewport.width, 1.01);
         } finally {
-            devices.real(originalRealDevice);
             devices.current(originalCurrentDevice);
         }
     });
