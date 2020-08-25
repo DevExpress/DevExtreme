5f1ec4d305ffdcc132f6f62f920f1285b1b1e514
Evgeniy Aryamnov <earyamnow@mail.ru>
2020-08-25 15:27:22 +0300

    Refactor tests

--- js/renovation/ui/scheduler/workspaces/base/date_table/all_day_panel/__tests__/layout.test.tsx
+++ js/renovation/ui/scheduler/workspaces/base/date_table/all_day_panel/__tests__/layout.test.tsx
@@ -2,25 +2,32 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { viewFunction as LayoutView, AllDayPanelLayout } from '../layout';
 import { AllDayPanelTableBody } from '../table_body';
-import { GroupedViewData } from '../../../../types.d';
 import * as utilsModule from '../../../../utils';
 
 const addHeightToStyle = jest.spyOn(utilsModule, 'addHeightToStyle');
 
 describe('AllDayPanelLayout ', () => {
+  const viewData = {
+    groupedData: [{
+      allDayPanel: [{
+        startDate: new Date(2020, 6, 9),
+        endDate: new Date(2020, 6, 10),
+        text: '',
+        index: 0,
+      }],
+      dateTable: [[]],
+    }],
+    cellCountInGroupRow: 1,
+  };
+  const allDayPanelData = viewData.groupedData[0].allDayPanel;
+
   describe('Render', () => {
-    const viewData = {
-      groupedData: [
-        { allDayPanel: [{ startDate: new Date(2020, 6, 9, 0) }] },
-        { allDayPanel: [{ startDate: new Date(2020, 6, 9, 1) }] },
-      ],
-    };
     const render = (viewModel) => shallow(
       <LayoutView
+        allDayPanelData={allDayPanelData}
         {...viewModel}
         props={{
           visible: true,
-          viewData,
           ...viewModel.props,
         }}
       />,
@@ -33,21 +40,39 @@ describe('AllDayPanelLayout ', () => {
         .toBe('customAttribute');
     });
 
-    it('should render correctly', () => {
-      const layout = render({ classes: 'some-class' });
+    it('should render components correctly', () => {
+      const dataCellTemplate = () => null;
+      const style = { height: 100 };
+      const layout = render({
+        classes: 'some-class',
+        style,
+        restAttributes: { style: { height: 500 } },
+        props: { dataCellTemplate },
+      });
 
       expect(layout.hasClass('some-class'))
         .toBe(true);
+      expect(layout.prop('style'))
+        .toEqual(style);
 
       const allDayTable = layout.find('.dx-scheduler-all-day-table');
+
       expect(allDayTable.exists())
         .toBe(true);
-
-      const tableBodies = allDayTable.find(AllDayPanelTableBody);
-      expect(tableBodies.exists())
+      expect(allDayTable.hasClass('dx-scheduler-all-day-table'))
         .toBe(true);
-      expect(tableBodies)
+
+      const tableBody = allDayTable.find(AllDayPanelTableBody);
+
+      expect(tableBody.exists())
+        .toBe(true);
+      expect(tableBody)
         .toHaveLength(1);
+      expect(tableBody.props())
+        .toMatchObject({
+          viewData: allDayPanelData,
+          dataCellTemplate,
+        });
     });
 
     it('should not be rendered if "visible" is false', () => {
@@ -57,27 +82,15 @@ describe('AllDayPanelLayout ', () => {
       expect(allDayTable.exists())
         .toBe(false);
     });
-
-    it('should pass "style" to the root', () => {
-      const layout = render({ style: { height: 100 } });
-
-      expect(layout.prop('style'))
-        .toStrictEqual({ height: 100 });
-    });
   });
 
   describe('Logic', () => {
     describe('Getters', () => {
       it('allDayPanelData', () => {
-        const viewData = {
-          groupedData: [{
-            allDayPanel: [{ startDate: new Date(2020, 6, 9, 0) }],
-          }],
-        } as GroupedViewData;
         const layout = new AllDayPanelLayout({ viewData });
 
         expect(layout.allDayPanelData)
-          .toStrictEqual([{ startDate: new Date(2020, 6, 9, 0) }]);
+          .toStrictEqual(viewData.groupedData[0].allDayPanel);
       });
 
       it('style', () => {
@@ -92,7 +105,7 @@ describe('AllDayPanelLayout ', () => {
       });
 
       describe('classes', () => {
-        it('should not add dx-hidden class if "visible" is false', () => {
+        it('should not add dx-hidden class if "visible" is true', () => {
           const layout = new AllDayPanelLayout({
             className: 'some-class',
             visible: true,
