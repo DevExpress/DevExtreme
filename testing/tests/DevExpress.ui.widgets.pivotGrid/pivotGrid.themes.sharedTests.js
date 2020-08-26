ddd1d9997a6fe0048ebd12b278c97275917b5bc8
EugeniyKiyashko <johnnykiyashko@gmail.com>
2020-08-26 13:24:02 +0300

    PivotGrid - Cell text in a row header goes beyond cell borders in Material theme(T922737)

--- testing/tests/DevExpress.ui.widgets.pivotGrid/pivotGrid.themes.sharedTests.js
+++ testing/tests/DevExpress.ui.widgets.pivotGrid/pivotGrid.themes.sharedTests.js
@@ -17,67 +17,57 @@ export const runThemesSharedTests = function(moduleNamePostfix) {
         }
     }, function() {
         // T922737
-        [true, false].forEach((rtlEnabled) => {
-            QUnit.test(`Cell text contains inside a region of cell: rtlEnabled - ${rtlEnabled}`, function(assert) {
-                const pivotGridOptions = {
-                    dataSource: {
-                        fields: [{
-                            width: 120,
-                            dataField: 'region',
-                            area: 'row',
-                            expanded: true
-                        }, {
-                            dataField: 'city',
-                            area: 'row',
-                        }, {
-                            dataField: 'city',
-                            area: 'column'
-                        }],
-                        store: [{
-                            'region': 'North America word1',
-                            'city': 'New York'
-                        }]
-                    }
-                };
-
-                const pivotGridElement = document.getElementById('PivotGrid');
-                new PivotGrid(pivotGridElement, {
-                    ...pivotGridOptions,
-                    width: 400,
-                    height: 400,
-                    showRowGrandTotals: true,
-                    rtlEnabled
-                });
-                this.clock.tick();
-
-                const cellElement = pivotGridElement.querySelector(`.${PIVOTGRID_EXPANDED_CLASS}`);
-                const cellTextElement = cellElement.lastChild;
-
-                const cellRect = cellElement.getBoundingClientRect();
-                const cellTextRect = cellTextElement.getBoundingClientRect();
-
-                assert.strictEqual(cellRect.width > cellTextRect.width, true, 'width');
-                assert.strictEqual(cellRect.right > cellTextRect.right, true, 'right');
-                assert.strictEqual(cellRect.left < cellTextRect.left, true, 'left');
-                assert.strictEqual(cellRect.top < cellTextRect.top, true, 'top');
-                assert.strictEqual(cellRect.bottom > cellTextRect.bottom, true, 'bottom');
-
-
-                const totalCellElement = pivotGridElement.querySelector(`.${PIVOTGRID_TOTAL_CLASS}`);
-                const grandTotalCellElement = pivotGridElement.querySelector(`.${PIVOTGRID_ROW_TOTAL_CLASS}.${PIVOTGRID_GRAND_TOTAL_CLASS}.dx-last-cell`);
-
-                const totalCellRect = totalCellElement.getBoundingClientRect();
-                const totalTextRect = totalCellElement.lastChild.getBoundingClientRect();
-                const grandTotalRect = grandTotalCellElement.getBoundingClientRect();
-                const grandTotalTextRect = grandTotalCellElement.lastChild.getBoundingClientRect();
-
-                if(rtlEnabled) {
-                    assert.roughEqual(totalCellRect.right - totalTextRect.right, grandTotalRect.right - grandTotalTextRect.right, 0.1, 'total & grandTotal cells have the same padding');
-                } else {
-                    assert.roughEqual(totalTextRect.left - totalCellRect.left, grandTotalTextRect.left - grandTotalRect.left, 0.1, 'total & grandTotal cells have the same padding');
+        QUnit.test('Cell text contains inside a region of cell', function(assert) {
+            const pivotGridOptions = {
+                dataSource: {
+                    fields: [{
+                        width: 120,
+                        dataField: 'region',
+                        area: 'row',
+                        expanded: true
+                    }, {
+                        dataField: 'city',
+                        area: 'row',
+                    }, {
+                        dataField: 'city',
+                        area: 'column'
+                    }],
+                    store: [{
+                        'region': 'North America word1',
+                        'city': 'New York'
+                    }]
                 }
-            });
-        });
+            };
 
+            const pivotGridElement = document.getElementById('PivotGrid');
+            new PivotGrid(pivotGridElement, {
+                ...pivotGridOptions,
+                width: 400,
+                height: 400,
+                showRowGrandTotals: true
+            });
+            this.clock.tick();
+
+            const cellElement = pivotGridElement.querySelector(`.${PIVOTGRID_EXPANDED_CLASS}`);
+            const cellTextElement = cellElement.lastChild;
+
+            const cellRect = cellElement.getBoundingClientRect();
+            const cellTextRect = cellTextElement.getBoundingClientRect();
+            assert.strictEqual(cellRect.width > cellTextRect.width, true, 'width');
+            assert.strictEqual(cellRect.right >= cellTextRect.right - parseFloat(window.getComputedStyle(cellTextElement).paddingRight), true, 'right');
+            assert.strictEqual(cellRect.left < cellTextRect.left, true, 'left');
+            assert.strictEqual(cellRect.top < cellTextRect.top, true, 'top');
+            assert.strictEqual(cellRect.bottom > cellTextRect.bottom, true, 'bottom');
+
+            const totalCellElement = pivotGridElement.querySelector(`.${PIVOTGRID_TOTAL_CLASS}`);
+            const grandTotalCellElement = pivotGridElement.querySelector(`.${PIVOTGRID_ROW_TOTAL_CLASS}.${PIVOTGRID_GRAND_TOTAL_CLASS}.dx-last-cell`);
+
+            const totalCellRect = totalCellElement.getBoundingClientRect();
+            const totalTextRect = totalCellElement.lastChild.getBoundingClientRect();
+            const grandTotalRect = grandTotalCellElement.getBoundingClientRect();
+            const grandTotalTextRect = grandTotalCellElement.lastChild.getBoundingClientRect();
+
+            assert.roughEqual(totalTextRect.left - totalCellRect.left, grandTotalTextRect.left - grandTotalRect.left, 0.1, 'total & grandTotal cells have the same padding');
+        });
     });
 };
