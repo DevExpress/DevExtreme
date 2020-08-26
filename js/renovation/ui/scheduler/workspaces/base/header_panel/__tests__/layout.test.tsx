cb15fa7d5fec5dc9450a1e3b6b03303a314b5f74
Evgeniy Aryamnov <earyamnow@mail.ru>
2020-08-26 15:27:44 +0300

    Test header-panel layout

--- js/renovation/ui/scheduler/workspaces/base/header_panel/__tests__/layout.test.tsx
+++ js/renovation/ui/scheduler/workspaces/base/header_panel/__tests__/layout.test.tsx
@@ -8,14 +8,21 @@ const getKeyByDateAndGroup = jest.spyOn(utilsModule, 'getKeyByDateAndGroup');
 describe('HeaderPanelLayoutBase', () => {
   describe('Render', () => {
     const cellTemplate = () => null;
-    const viewCellsData = [[
-      {
-        startDate: new Date(2020, 6, 9), endDate: new Date(2020, 6, 10), today: true, groups: 1,
-      },
-      {
-        startDate: new Date(2020, 6, 10), endDate: new Date(2020, 6, 11), today: false, groups: 1,
-      },
-    ]];
+    const viewCellsData = [[{
+      startDate: new Date(2020, 6, 9),
+      endDate: new Date(2020, 6, 10),
+      today: true,
+      groups: { id: 1 },
+      groupIndex: 1,
+      index: 0,
+    }, {
+      startDate: new Date(2020, 6, 10),
+      endDate: new Date(2020, 6, 11),
+      today: false,
+      groups: { id: 1 },
+      groupIndex: 1,
+      index: 1,
+    }]];
     const render = (viewModel) => shallow(LayoutView({
       ...viewModel,
       props: { cellTemplate, ...viewModel.props, viewCellsData },
@@ -56,8 +63,11 @@ describe('HeaderPanelLayoutBase', () => {
         .toHaveLength(2);
     });
 
-    it('should render cells and pass correct props to them', () => {
-      const layout = render({});
+    it('should render cells and pass correct props to them in basic case', () => {
+      const dateCellTemplate = () => null;
+      const layout = render({
+        props: { dateCellTemplate },
+      });
 
       const cells = layout.find(cellTemplate);
       expect(cells)
@@ -68,12 +78,41 @@ describe('HeaderPanelLayoutBase', () => {
           startDate: viewCellsData[0][0].startDate,
           endDate: viewCellsData[0][0].endDate,
           today: viewCellsData[0][0].today,
+          groups: viewCellsData[0][0].groups,
+          groupIndex: viewCellsData[0][0].groupIndex,
+          index: viewCellsData[0][0].index,
+          dateCellTemplate,
         });
       expect(cells.at(1).props())
         .toMatchObject({
           startDate: viewCellsData[0][1].startDate,
           endDate: viewCellsData[0][1].endDate,
           today: viewCellsData[0][1].today,
+          groups: viewCellsData[0][1].groups,
+          groupIndex: viewCellsData[0][1].groupIndex,
+          index: viewCellsData[0][1].index,
+          dateCellTemplate,
+        });
+    });
+
+    it('should not pass groups and groupInex to cells in case of Vertical Gruping', () => {
+      const layout = render({
+        isVerticalGroupOrientation: true,
+      });
+
+      const cells = layout.find(cellTemplate);
+      expect(cells)
+        .toHaveLength(2);
+
+      expect(cells.at(0).props())
+        .toMatchObject({
+          groups: undefined,
+          groupIndex: undefined,
+        });
+      expect(cells.at(1).props())
+        .toMatchObject({
+          groups: undefined,
+          groupIndex: undefined,
         });
     });
 
