ac1ac4db49794dd37ef703a608fc097b4edbd23f
babich-a <alexey.babych@gmail.com>
2020-08-26 22:10:40 +0300

    Set the same behavior for Edge as for IE in datagrid tests

--- testing/helpers/grid/getPointsByColumnsHelper.js
+++ testing/helpers/grid/getPointsByColumnsHelper.js
@@ -2,8 +2,7 @@ import browser from 'core/utils/browser';
 
 export function roundPoints(pointsByColumnsResult) {
     return pointsByColumnsResult.map(point => {
-        const isIE11 = browser.msie && parseInt(browser.version) <= 11;
-        point.x = isIE11 ? Math.floor(point.x) : point.x;
+        point.x = browser.msie ? Math.floor(point.x) : point.x;
         return point;
     });
 }
