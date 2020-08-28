9ffcbd3ea702a425216a20dca97e2fb270f7c33d
ksercs <ksercs0@gmail.com>
2020-08-28 15:08:46 +0300

    remove useless selectbox 'autocompletionEnabled' option

--- js/ui/select_box.js
+++ js/ui/select_box.js
@@ -162,14 +162,6 @@ const SelectBox = DropDownList.inherit({
             showSelectionControls: false,
 
             /**
-            * @name dxSelectBoxOptions.autocompletionEnabled
-            * @type boolean
-            * @default true
-            * @hidden
-            */
-            autocompletionEnabled: true,
-
-            /**
             * @name dxSelectBoxOptions.allowClearing
             * @type boolean
             * @default true
@@ -783,8 +775,7 @@ const SelectBox = DropDownList.inherit({
     },
 
     _shouldSubstitutionBeRendered: function() {
-        return this.option('autocompletionEnabled')
-            && !this._preventSubstitution
+        return !this._preventSubstitution
             && this.option('searchEnabled')
             && !this.option('acceptCustomValue')
             && this.option('searchMode') === 'startswith';
@@ -832,7 +823,6 @@ const SelectBox = DropDownList.inherit({
     _optionChanged: function(args) {
         switch(args.name) {
             case '_isAdaptablePopupPosition':
-            case 'autocompletionEnabled':
                 break;
             case 'onCustomItemCreating':
                 this._initCustomItemCreatingAction();
