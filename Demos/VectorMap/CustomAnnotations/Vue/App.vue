17937360180a6eba622bdcd02b994263d9c7f4db
Smirnova Yuliya <Krijovnick@users.noreply.github.com>
2020-09-04 10:52:06 +0300

    VectorMap demo fix lint (#105)

--- Demos/VectorMap/CustomAnnotations/Vue/App.vue
+++ Demos/VectorMap/CustomAnnotations/Vue/App.vue
@@ -8,18 +8,18 @@
       :data-source="usa"
     />
     <DxCommonAnnotationSettings
-        type="custom"
-        template="annotationTemplate"
-      />
-      <DxAnnotation
-        v-for="state in statesData"
-        :coordinates="state.coordinates"
-        :data="state.data"
-        :key="state.data.name"
-      />
-      <template #annotationTemplate="{ data }">
-        <AnnotationTemplate :annotation="data"/>
-      </template>
+      type="custom"
+      template="annotationTemplate"
+    />
+    <DxAnnotation
+      v-for="state in statesData"
+      :coordinates="state.coordinates"
+      :data="state.data"
+      :key="state.data.name"
+    />
+    <template #annotationTemplate="{ data }">
+      <AnnotationTemplate :annotation="data"/>
+    </template>
   </DxVectorMap>
 </template>
 <script>
@@ -44,22 +44,22 @@ export default {
     DxCommonAnnotationSettings,
     AnnotationTemplate
   },
-  methods: {
-    customizeAnnotation: (annotationItem) => {
-      if (annotationItem.data.name === 'Illinois') {
-        annotationItem.offsetY = -80;
-        annotationItem.offsetX = -100;
-      }
-      
-      return annotationItem;
-    }
-  },
   data() {
     return {
       statesData,
       usa: mapsData.usa,
       bounds: [-118, 55, -80, 23]
     };
+  },
+  methods: {
+    customizeAnnotation: (annotationItem) => {
+      if (annotationItem.data.name === 'Illinois') {
+        annotationItem.offsetY = -80;
+        annotationItem.offsetX = -100;
+      }
+
+      return annotationItem;
+    }
   }
 };
 </script>
