0f23219fbac3e63ef0be9a445a1edfb967775693
Stanislav Klesarev <klesarevklesarevstas@gmail.com>
2020-09-04 12:39:30 +0300

    Keeps input files

--- js/ui/file_uploader.js
+++ js/ui/file_uploader.js
@@ -279,6 +279,9 @@ class FileUploader extends Editor {
         const files = this._$fileInput.prop('files');
 
         if(files && !files.length) {
+            if(this.option('value').length && this._useInputForDrop()) {
+                this._$fileInput.prop('files', this._createFileList(this.option('value')));
+            }
             return;
         }
 
@@ -288,6 +291,13 @@ class FileUploader extends Editor {
         if(this.option('uploadMode') === 'instantly') {
             this._uploadFiles();
         }
+    }
+
+    _createFileList(files) {
+        const dataTransfer = new window.DataTransfer();
+        files.forEach(file => dataTransfer.items.add(file));
+
+        return dataTransfer.files;
     }
 
     _shouldFileListBeExtended() {
