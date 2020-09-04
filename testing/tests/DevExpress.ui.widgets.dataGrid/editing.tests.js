856d8e2392c7c8c83929210b109a410dfce85aad
NickMitrokhin <nikolai.mitrokhin@devexpress.com>
2020-09-04 14:07:08 +0300

    Added a test

--- testing/tests/DevExpress.ui.widgets.dataGrid/editing.tests.js
+++ testing/tests/DevExpress.ui.widgets.dataGrid/editing.tests.js
@@ -8526,6 +8526,31 @@ QUnit.module('Editing with real dataController', {
         assert.notOk($secondCell.hasClass('dx-datagrid-invalid'), 'the second cell is rendered as valid');
     });
 
+    QUnit.test('Cell should be prepared before creating an editor (T928363)', function(assert) {
+        // arrange
+        let isEditorCell = false;
+        const rowsView = this.rowsView;
+        const $testElement = $('#container');
+        this.options = {
+            dataSource: [{ field1: 'test' }],
+            editing: {
+                mode: 'batch'
+            },
+            onEditorPreparing: function(e) {
+                isEditorCell = $(e.editorElement).closest('td').hasClass('dx-editor-cell');
+            }
+        };
+
+        this.editorFactoryController.init();
+        rowsView.render($testElement);
+        this.clock.tick();
+
+        this.editCell(0, 0);
+        this.clock.tick();
+
+        assert.ok(isEditorCell, 'cell is rendered for an editor');
+    });
+
     QUnit.module('Editing state', {
         beforeEach: function() {
             this.options.dataSource = this.options.dataSource.store;
