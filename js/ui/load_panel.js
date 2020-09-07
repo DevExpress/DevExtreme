a9c1fb9b0ba8b9a5e9b91d8e25d06f66088d5e6a
Anton Kuznetsov <ksercs0@gmail.com>
2020-09-07 15:43:38 +0300

    LoadPanel: indicatorSrc option change handler refactoring (#14467) (#14475)

--- js/ui/load_panel.js
+++ js/ui/load_panel.js
@@ -183,8 +183,11 @@ const LoadPanel = Overlay.inherit({
             return;
         }
 
-        this._$indicator = $('<div>').addClass(LOADPANEL_INDICATOR_CLASS)
-            .appendTo(this._$contentWrapper);
+        if(!this._$indicator) {
+            this._$indicator = $('<div>')
+                .addClass(LOADPANEL_INDICATOR_CLASS)
+                .appendTo(this._$contentWrapper);
+        }
 
         this._createComponent(this._$indicator, LoadIndicator, {
             indicatorSrc: this.option('indicatorSrc')
@@ -214,11 +217,7 @@ const LoadPanel = Overlay.inherit({
                 this._togglePaneVisible();
                 break;
             case 'indicatorSrc':
-                if(this._$indicator) {
-                    this._createComponent(this._$indicator, LoadIndicator, {
-                        indicatorSrc: this.option('indicatorSrc')
-                    });
-                }
+                this._renderLoadIndicator();
                 break;
             default:
                 this.callBase(args);
