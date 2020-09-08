849f35edb0fa4fa2bad7dd60c705635a9eaacda8
Roman Rodin <roman.rodin@devexpress.com>
2020-09-08 10:51:28 +0300

    Diagram demos - remove trailing spaces (#113)

--- Demos/Diagram/OperationRestrictions/React/data.js
+++ Demos/Diagram/OperationRestrictions/React/data.js
@@ -1,47 +1,47 @@
-﻿var orgItems = [{  
+﻿var orgItems = [{
   'id':'106',
   'name':'Development',
   'type':'root'
-}, {  
+}, {
   'id':'107',
   'name':'WinForms Team',
   'type': 'team',
   'parentId': '106'
-}, {  
+}, {
   'id':'109',
   'name':'Javascript Team',
   'type': 'team',
   'parentId': '106'
-}, {  
+}, {
   'id':'110',
   'name':'ASP.NET Team',
   'type': 'team',
   'parentId': '106'
-}, {  
+}, {
   'id':'112',
   'name': 'Ana Trujillo',
   'parentId': '107'
-}, {  
+}, {
   'id':'113',
   'name':'Antonio Moreno',
   'parentId': '107'
-}, {  
+}, {
   'id':'115',
   'name':'Christina Berglund',
   'parentId': '109'
-}, {  
+}, {
   'id':'116',
   'name':'Hanna Moos',
   'parentId': '109'
-}, {  
+}, {
   'id':'119',
   'name':'Laurence Lebihan',
  'parentId': '110'
-}, {  
+}, {
   'id':'122',
   'name': 'Patricio Simpson',
   'parentId': '110'
-}, {  
+}, {
   'id':'123',
   'name':'Francisco Chang',
   'parentId': '110'
