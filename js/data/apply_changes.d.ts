4686459ea9217d86d9ddf88c9abd2caff2b6a34a
NickMitrokhin <nikolai.mitrokhin@devexpress.com>
2020-09-09 11:19:51 +0300

    Refactoring the source code

--- js/data/apply_changes.d.ts
+++ js/data/apply_changes.d.ts
@@ -3,7 +3,7 @@
  * @publicName applyChanges(data, changes, keyName, options)
  * @param1 data:Array<any>
  * @param2 changes:Array<any>
- * @param3 keyName?:string
+ * @param3 keyName:string
  * @param4 options?:any
  * @return Array<any>
  * @namespace DevExpress.data
@@ -12,6 +12,6 @@
  * @prevFileNamespace DevExpress.data
  * @public
  */
-declare function applyChanges(data: Array<any>, changes: Array<{ type: string, data?: any, key?: any }>, keyName?: string, options?: any): Array<any>;
+declare function applyChanges(data: Array<any>, changes: Array<any>, keyName: string, options?: any): Array<any>;
 
 export default applyChanges;
--- js/data/apply_changes.js
+++ js/data/apply_changes.js
@@ -1,91 +1,53 @@
 import { isDefined } from '../core/utils/type';
-import { extend } from '../core/utils/extend';
-import { equalByValue } from '../core/utils/common';
+import { deepExtendArraySafe } from '../core/utils/object';
 import { errors } from './errors';
-import Guid from '../core/guid';
-
-const getItemIndexByKey = function(key, items, keyName) {
-    let index = -1;
-
-    if(key !== undefined && Array.isArray(items)) {
-        keyName = arguments.length <= 2 ? 'key' : keyName;
-        for(let i = 0; i < items.length; i++) {
-            const item = isDefined(keyName) ? items[i][keyName] : items[i];
-
-            if(equalByValue(key, item)) {
-                index = i;
-                break;
-            }
-        }
-    }
-
-    return index;
-};
-
-const insertItemToArray = function(items, item, keyName) {
-    if(isDefined(item)) {
-        const newItem = extend({}, item);
-        if(isDefined(keyName) && !isDefined(newItem[keyName])) {
-            newItem[keyName] = String(new Guid());
-        }
-        items.push(newItem);
-    } else {
-        errors.log('Inserted data is not defined');
-    }
-};
-
-const updateItemInArray = function(items, item, key, keyName) {
-    if(isDefined(keyName)) {
-        const index = getItemIndexByKey(key, items, keyName);
-        if(index >= 0) {
-            items[index] = extend({}, items[index], item);
-        } else {
-            errors.log(`Item with the following key is not found: ${JSON.stringify(key)}`);
-        }
-    } else {
-        errors.log('keyName is not defined');
-    }
-};
-
-const removeItemFromArray = function(items, key, keyName) {
-    if(isDefined(keyName)) {
-        const index = getItemIndexByKey(key, items, keyName);
-        index >= 0 && items.splice(index, 1);
-    } else {
-        errors.log('keyName is not defined');
-    }
-};
+import ArrayStore from './array_store';
 
 const applyChanges = function(data, changes, keyName, options) {
     const { immutable = true } = isDefined(options) ? options : {};
-    let result = null;
+    let result = [];
 
     if(!isDefined(data)) {
         return result;
     }
 
-    result = immutable === true ? [...data] : data;
+    result = immutable === true ? deepExtendArraySafe(result, data, true) : data;
 
     if(!isDefined(changes)) {
         return result;
     }
 
+    if(!isDefined(keyName) || keyName.trim().length === 0) {
+        errors.Error('W100000');
+        return result;
+    }
+
+    const store = new ArrayStore({
+        key: keyName,
+        data: result
+    });
+
     changes.forEach((change) => {
         switch(change.type) {
             case 'insert': {
-                insertItemToArray(result, change.data);
+                store.insert(change.data);
                 break;
             }
             case 'update': {
-                updateItemInArray(result, change.data, change.key, keyName);
+                store.update(change.key, change.data);
                 break;
             }
             case 'remove': {
-                removeItemFromArray(result, change.key, keyName);
+                store.remove(change.key);
                 break;
             }
         }
     });
+
+    store.load().done((res) => {
+        result = res;
+    });
+
     return result;
 };
 
