8ab7eba76f532296d04c582e06ae4dfb29927bdf
Evgeniy Aryamnov <earyamnow@mail.ru>
2020-09-11 09:17:15 +0300

    Fix test

--- testing/tests/DevExpress.ui.widgets.scheduler/virtual_scrolling.tests.js
+++ testing/tests/DevExpress.ui.widgets.scheduler/virtual_scrolling.tests.js
@@ -154,12 +154,14 @@ module('Virtual Scrolling model', {
 
     test('It should call _getTotalRowCount with correct parameters', function(assert) {
         const getTotalRowCountSpy = sinon.spy(this.worksSpaceMock, '_getTotalRowCount');
-        const _isVerticalGroupedWorkSpaceSpy = sinon.spy(this.worksSpaceMock, '_isVerticalGroupedWorkSpace');
+        const isVerticalGroupedWorkSpaceSpy = sinon.spy(this.worksSpaceMock, '_isVerticalGroupedWorkSpace');
+        const isGroupedAllDayPanelSpy = sinon.spy(this.worksSpaceMock, 'isGroupedAllDayPanel');
 
         this.virtualScrolling._updateState(0);
 
-        assert.ok(_isVerticalGroupedWorkSpaceSpy.called, '_isVerticalGroupedWorkSpaceSpy was called');
+        assert.ok(isVerticalGroupedWorkSpaceSpy.called, '_isVerticalGroupedWorkSpaceSpy was called');
         assert.ok(getTotalRowCountSpy.called, 'getTotalRowCountSpy was called');
+        assert.notOk(isGroupedAllDayPanelSpy.called, 'isGroupedAllDayPanel was not called');
         assert.equal(getTotalRowCountSpy.getCall(0).args[0], 0, 'Correct first parameter');
         assert.equal(getTotalRowCountSpy.getCall(0).args[1], false, 'Correct second parameter');
     });
