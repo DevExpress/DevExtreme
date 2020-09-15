3c760cd5922ee4b63050e80721181e06bc97cc62
Evgeniy Aryamnov <earyamnow@mail.ru>
2020-09-15 17:32:42 +0300

    Refactor

--- js/ui/scheduler/workspaces/view_data_provider.js
+++ js/ui/scheduler/workspaces/view_data_provider.js
@@ -107,65 +107,59 @@ class ViewDataGenerator {
         const {
             cellCount,
             cellDataGetters,
-            horizontalGroupsCount,
             rowCountInGroup,
-            groupOrientation,
         } = options;
         const viewCellsData = [];
 
         for(let i = 0; i < renderRowCount; ++i) {
             const rowIndex = startRowIndex + rowOffset + i;
 
-            viewCellsData.push([]);
-            for(let columnIndex = 0; columnIndex < cellCount; ++columnIndex) {
-
-                const cellDataValue = { };
-
-                cellDataGetters.forEach(getter => {
-                    const cellValue = getter(undefined, rowIndex, columnIndex).value;
-                    Object.assign(cellDataValue, cellValue);
-                });
-
-                cellDataValue.index = this._calculateCellIndex(
-                    horizontalGroupsCount, groupOrientation, this._workspace.isGroupedByDate(),
-                    rowIndex % rowCountInGroup, columnIndex, cellCount,
-                );
-                cellDataValue.key = this._getKeyByDateAndGroup(cellDataValue.startDate, cellDataValue.groupIndex);
-
-                viewCellsData[i].push(cellDataValue);
-            }
+            const rowIndexInGroup = rowIndex % rowCountInGroup;
+            viewCellsData.push(this._generateCellsRow(
+                options, cellDataGetters, rowIndex, cellCount, rowIndexInGroup,
+            ));
         }
 
         return viewCellsData;
     }
 
     _generateAllDayPanelData(options, groupIndex, rowCount, cellCount) {
-        if(!this.workspace._isShowAllDayPanel()) {
+        const workSpace = this.workspace;
+        if(!workSpace._isShowAllDayPanel()) {
             return null;
         }
 
-        const { horizontalGroupsCount, groupOrientation } = options;
         const rowIndex = Math.max(groupIndex * rowCount, 0);
 
-        const allDayPanel = [];
+        return this._generateCellsRow(
+            options, [workSpace._getAllDayCellData.bind(workSpace)],
+            rowIndex, cellCount, 0, groupIndex,
+        );
+    }
+
+    _generateCellsRow(options, cellDataGetters, rowIndex, cellCount, rowIndexInGroup, groupIndex) {
+        const cellsRow = [];
+        const {
+            horizontalGroupsCount,
+            groupOrientation,
+        } = options;
 
         for(let columnIndex = 0; columnIndex < cellCount; ++columnIndex) {
-            const cellDataValue = this.workspace._getAllDayCellData(undefined, rowIndex, columnIndex, groupIndex).value;
+            const cellDataValue = cellDataGetters.reduce((data, getter) => ({
+                ...data,
+                ...getter(undefined, rowIndex, columnIndex, groupIndex).value
+            }), {});
 
             cellDataValue.index = this._calculateCellIndex(
                 horizontalGroupsCount, groupOrientation, this._workspace.isGroupedByDate(),
-                0, columnIndex, cellCount,
+                rowIndexInGroup, columnIndex, cellCount,
             );
             cellDataValue.key = this._getKeyByDateAndGroup(cellDataValue.startDate, cellDataValue.groupIndex);
 
-            allDayPanel.push(cellDataValue);
+            cellsRow.push(cellDataValue);
         }
 
-        return allDayPanel;
-    }
-
-    _createCellsRow() {
-        // TODO
+        return cellsRow;
     }
 
     _calculateCellIndex(horizontalGroupsCount, groupOrientation, isGroupedByDate, rowIndex, columnIndex, columnsNumber) {
