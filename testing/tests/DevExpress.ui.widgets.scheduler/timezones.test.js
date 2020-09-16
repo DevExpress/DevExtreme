f139e462e49c382283a5b307b3a5de8b86df4bd0
Andrew Makarov <andrey.makarov@devexpress.com>
2020-09-16 18:13:13 +0300

    Add base tests

--- testing/tests/DevExpress.ui.widgets.scheduler/timezones.test.js
+++ testing/tests/DevExpress.ui.widgets.scheduler/timezones.test.js
@@ -28,20 +28,20 @@ const moduleConfig = {
 
 const data = [{
     text: 'app_1_Los_Angeles',
-    startDate: new Date('2017-05-22T05:00:00.000Z'), // local offset
-    endDate: new Date('2017-05-22T05:30:00.000Z'), // local offset
+    startDate: new Date('2017-05-22T05:00:00.000Z'),
+    endDate: new Date('2017-05-22T05:30:00.000Z'),
     startDateTimeZone: timeZones.LosAngeles, // -7
     endDateTimeZone: timeZones.LosAngeles // -7
 }, {
     text: 'app_2_Los_Angeles',
-    startDate: new Date('2017-05-22T12:00:00.000Z'), // local offset
-    endDate: new Date('2017-05-22T12:30:00.000Z'), // local offset
+    startDate: new Date('2017-05-22T12:00:00.000Z'),
+    endDate: new Date('2017-05-22T12:30:00.000Z'),
     startDateTimeZone: timeZones.LosAngeles, // -7
     endDateTimeZone: timeZones.LosAngeles // -7
 }, {
     text: 'app_local',
-    startDate: new Date('2017-05-22T16:00:00.000Z'), // local offset
-    endDate: new Date('2017-05-22T16:30:00.000Z') // local offset
+    startDate: new Date('2017-05-22T16:00:00.000Z'),
+    endDate: new Date('2017-05-22T16:30:00.000Z')
 }];
 
 const createScheduler = (options = {}) => {
@@ -56,18 +56,198 @@ const createScheduler = (options = {}) => {
 
 module('Not native date DST', moduleConfig, () => {
     module('summer time', () => {
+        const from1amTo2amCase = {
+            text: 'Recurrence start from 1 a.m. to 2 a.m.',
+            startDate: new Date('2020-03-01T09:00:00.000Z'),
+            endDate: new Date('2020-03-01T10:00:00.000Z'),
+            expectedTexts: [
+                '1:00 AM - 2:00 AM',
+                '1:00 AM - 2:00 AM',
+                '1:00 AM - 2:00 AM',
+                '1:00 AM - 3:00 AM',
+                '1:00 AM - 2:00 AM',
+                '1:00 AM - 2:00 AM',
+                '1:00 AM - 2:00 AM'
+            ]
+        };
 
+        const from1amTo3amCase = {
+            text: 'Recurrence start from 1 a.m. to 3 a.m.',
+            startDate: new Date('2020-03-01T09:00:00.000Z'),
+            endDate: new Date('2020-03-01T11:00:00.000Z'),
+            expectedTexts: [
+                '1:00 AM - 3:00 AM',
+                '1:00 AM - 3:00 AM',
+                '1:00 AM - 3:00 AM',
+                '1:00 AM - 3:00 AM',
+                '1:00 AM - 3:00 AM',
+                '1:00 AM - 3:00 AM',
+                '1:00 AM - 3:00 AM'
+            ]
+        };
+
+        const from2amTo3amCase = {
+            text: 'Recurrence start from 2 a.m. to 3 a.m.',
+            startDate: new Date('2020-03-01T10:00:00.000Z'),
+            endDate: new Date('2020-03-01T11:00:00.000Z'),
+            expectedTexts: [
+                '2:00 AM - 3:00 AM',
+                '2:00 AM - 3:00 AM',
+                '2:00 AM - 3:00 AM',
+                '3:00 AM - 4:00 AM',
+                '2:00 AM - 3:00 AM',
+                '2:00 AM - 3:00 AM',
+                '2:00 AM - 3:00 AM'
+            ]
+        };
+
+        const from6amTo7amCase = {
+            text: 'Recurrence start from 6 a.m. to 7 a.m.',
+            startDate: new Date('2020-03-01T16:00:00.000Z'),
+            endDate: new Date('2020-03-01T17:00:00.000Z'),
+            expectedTexts: [
+                '8:00 AM - 9:00 AM',
+                '8:00 AM - 9:00 AM',
+                '8:00 AM - 9:00 AM',
+                '8:00 AM - 9:00 AM',
+                '8:00 AM - 9:00 AM',
+                '8:00 AM - 9:00 AM',
+                '8:00 AM - 9:00 AM'
+            ]
+        };
+
+        [from1amTo2amCase, from1amTo3amCase, from2amTo3amCase, from6amTo7amCase].forEach(testCase => {
+            test(testCase.text, function(assert) {
+                const scheduler = createScheduler({
+                    dataSource: [{
+                        startDate: testCase.startDate,
+                        endDate: testCase.endDate,
+                        text: 'Test',
+                        recurrenceRule: 'FREQ=DAILY',
+                    }],
+                    firstDayOfWeek: 4,
+                    timeZone: timeZones.LosAngeles,
+                    currentDate: new Date(2020, 2, 8)
+                });
+
+                const count = scheduler.appointments.getAppointmentCount();
+                for(let i = 0; i < count; i++) {
+                    const expectedText = testCase.expectedTexts[i];
+                    const appointmentText = scheduler.appointments.getDateText(i);
+                    assert.equal(appointmentText, expectedText, `appointment date text should be equal ${expectedText}`);
+
+                    scheduler.appointments.click(i);
+
+                    const tooltipText = scheduler.tooltip.getDateText();
+                    assert.equal(tooltipText, expectedText, `tooltip date text should be equal ${expectedText}`);
+
+                    scheduler.instance.hideAppointmentTooltip();
+                }
+
+                assert.expect(testCase.expectedTexts.length * 2);
+            });
+        });
     });
 
     module('winter time', () => {
+        const from1amTo2amCase = {
+            text: 'Recurrence start from 1 a.m. to 2 a.m.',
+            startDate: '2020-10-28T08:00:00.000Z',
+            endDate: '2020-10-28T09:00:00.000Z',
+            expectedTexts: [
+                '1:00 AM - 2:00 AM',
+                '1:00 AM - 2:00 AM',
+                '1:00 AM - 2:00 AM',
+                '1:00 AM - 2:00 AM',
+                '1:00 AM - 2:00 AM',
+                '1:00 AM - 2:00 AM',
+                '1:00 AM - 2:00 AM'
+            ]
+        };
 
+        const from2amTo3amCase = {
+            text: 'Recurrence start from 2 a.m. to 3 a.m.',
+            startDate: '2020-10-28T09:00:00.000Z',
+            endDate: '2020-10-28T10:00:00.000Z',
+            expectedTexts: [
+                '2:00 AM - 3:00 AM',
+                '2:00 AM - 3:00 AM',
+                '2:00 AM - 3:00 AM',
+                '2:00 AM - 3:00 AM',
+                '2:00 AM - 3:00 AM',
+                '2:00 AM - 3:00 AM',
+                '2:00 AM - 3:00 AM',
+            ]
+        };
+
+        const from1amTo3amCase = {
+            text: 'Recurrence start from 1 a.m. to 3 a.m.',
+            startDate: '2020-10-28T08:00:00.000Z',
+            endDate: '2020-10-28T10:00:00.000Z',
+            expectedTexts: [
+                '1:00 AM - 3:00 AM',
+                '1:00 AM - 3:00 AM',
+                '1:00 AM - 3:00 AM',
+                '1:00 AM - 3:00 AM',
+                '1:00 AM - 3:00 AM',
+                '1:00 AM - 3:00 AM',
+                '1:00 AM - 3:00 AM',
+            ]
+        };
+
+        const from6amTo7amCase = {
+            text: 'Recurrence start from 6 a.m. to 7 a.m.',
+            startDate: '2020-10-28T13:00:00.000Z',
+            endDate: '2020-10-28T14:00:00.000Z',
+            expectedTexts: [
+                '6:00 AM - 7:00 AM',
+                '6:00 AM - 7:00 AM',
+                '6:00 AM - 7:00 AM',
+                '6:00 AM - 7:00 AM',
+                '6:00 AM - 7:00 AM',
+                '6:00 AM - 7:00 AM',
+                '6:00 AM - 7:00 AM'
+            ]
+        };
+
+        [from1amTo2amCase, from2amTo3amCase, from1amTo3amCase, from6amTo7amCase].forEach(testCase => {
+            test(testCase.text, function(assert) {
+                const scheduler = createScheduler({
+                    dataSource: [{
+                        startDate: testCase.startDate,
+                        endDate: testCase.endDate,
+                        text: 'Test',
+                        recurrenceRule: 'FREQ=DAILY',
+                    }],
+                    firstDayOfWeek: 4,
+                    timeZone: timeZones.LosAngeles,
+                    currentDate: new Date(2020, 10, 1),
+                });
+
+                const count = scheduler.appointments.getAppointmentCount();
+                for(let i = 0; i < count; i++) {
+                    const expectedText = testCase.expectedTexts[i];
+                    const appointmentText = scheduler.appointments.getDateText(i);
+                    assert.equal(appointmentText, expectedText, `appointment date text should be equal ${expectedText}`);
+
+                    scheduler.appointments.click(i);
+
+                    const tooltipText = scheduler.tooltip.getDateText();
+                    assert.equal(tooltipText, expectedText, `tooltip date text should be equal ${expectedText}`);
+
+                    scheduler.instance.hideAppointmentTooltip();
+                }
+
+                assert.expect(testCase.expectedTexts.length * 2);
+            });
+        });
     });
 });
 
 module('Scheduler grid', moduleConfig, () => {
-    module('timezone = "America/New_York', () => {
+    module(`timezone = "${timeZones.NewYork}"`, () => {
         test('startDate and endDate of appointments should valid', function(assert) {
-            const scheduler = createScheduler({ timeZone: 'America/New_York' }); // -4 offset
+            const scheduler = createScheduler({ timeZone: timeZones.NewYork }); // -4 offset
 
             ['1:00 AM - 1:30 AM', '8:00 AM - 8:30 AM', '12:00 PM - 12:30 PM'].forEach((expected, index) => {
                 const dateText = scheduler.appointments.getDateText(index);
@@ -97,7 +277,7 @@ module('Appointment popup', moduleConfig, () => {
 
         cases.forEach((testCase, index) => {
             test('StartDate and endDate should be valid', function(assert) {
-                const scheduler = createScheduler({ timeZone: 'America/New_York' }); // -4 offset
+                const scheduler = createScheduler({ timeZone: timeZones.NewYork }); // -4 offset
 
                 scheduler.appointments.dblclick(index);
 
