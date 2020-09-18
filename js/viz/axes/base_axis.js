5417466ef48be3ef0179c8a2b114d5d814057887
Smirnova Yuliya <Krijovnick@users.noreply.github.com>
2020-09-18 10:05:56 +0300

    dxChart - fix chart with the breaks with visualRange and scrolling/zooming (T927605) (#14622)

--- js/viz/axes/base_axis.js
+++ js/viz/axes/base_axis.js
@@ -1326,7 +1326,7 @@ Axis.prototype = {
         }
         that._seriesData.sortCategories(that.getCategoriesSorter(argCategories));
 
-        that._seriesData.breaks = that._initialBreaks = that._getScaleBreaks(options, that._seriesData, that._series, that.isArgumentAxis);
+        that._seriesData.userBreaks = that._getScaleBreaks(options, that._seriesData, that._series, that.isArgumentAxis);
 
         that._translator.updateBusinessRange(that._getViewportRange());
     },
@@ -1532,7 +1532,11 @@ Axis.prototype = {
                     minVisible: start,
                     maxVisible: end
                 }, that._series, that.isArgumentAxis);
-                ticks = generateTicks(tickInterval, false, start, end, breaks).ticks;
+                const filteredBreaks = that._filterBreaks(breaks, {
+                    minVisible: start,
+                    maxVisible: end
+                }, options.breakStyle);
+                ticks = generateTicks(tickInterval, false, start, end, filteredBreaks).ticks;
             }
         }
 
@@ -1556,9 +1560,11 @@ Axis.prototype = {
         that._isSynchronized = false;
         that.updateCanvas(canvas);
 
+        const range = that._getViewportRange();
+        that._initialBreaks = range.breaks = this._seriesData.breaks = that._filterBreaks(this._seriesData.userBreaks, range, options.breakStyle);
+
         that._estimatedTickInterval = that._getTicks(that.adjustViewport(this._seriesData), _noop, true).tickInterval; // tickInterval calculation
 
-        const range = that._getViewportRange();
         const margins = this._calculateValueMargins();
 
         range.addRange({
@@ -2203,7 +2209,7 @@ Axis.prototype = {
 
         const viewPort = that.getViewport();
 
-        that._seriesData.breaks = that._initialBreaks = that._getScaleBreaks(that._options, {
+        that._seriesData.userBreaks = that._getScaleBreaks(that._options, {
             minVisible: viewPort.startValue,
             maxVisible: viewPort.endValue
         }, that._series, that.isArgumentAxis);
@@ -2686,7 +2692,7 @@ Axis.prototype = {
     _getScreenDelta: function() {
         const that = this;
         const canvas = that._getCanvasStartEnd();
-        const breaks = that._seriesData ? that._seriesData.breaks : [];
+        const breaks = that._seriesData ? that._seriesData.breaks || [] : [];
         const breaksLength = breaks.length;
         const screenDelta = _abs(canvas.start - canvas.end);
 
@@ -2694,6 +2700,8 @@ Axis.prototype = {
     },
 
     _getScaleBreaks: function() { return []; },
+
+    _filterBreaks: function() { return []; },
 
     _adjustTitle: _noop,
 
