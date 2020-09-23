8a58ccfec4ef749b8d13f152342063c7a1f9a701
Andrew Makarov <andrey.makarov@devexpress.com>
2020-09-23 13:50:51 +0300

    Replace on 'Brazil/Acre'

--- testing/tests/DevExpress.ui.widgets.scheduler/utils.test.js
+++ testing/tests/DevExpress.ui.widgets.scheduler/utils.test.js
@@ -19,8 +19,8 @@ module('Time zone utils', {}, () => {
     });
 
     test('isEqualLocalTimeZone', function(assert) {
-        const result = timeZoneUtils.isEqualLocalTimeZone('Canada/Yukon');
+        const result = timeZoneUtils.isEqualLocalTimeZone('Brazil/Acre');
 
-        assert.notOk(result, 'local time zone shouldn\'t equal \'Canada/Yukon\'');
+        assert.notOk(result, 'local time zone shouldn\'t equal \'Brazil/Acre\'');
     });
 });
