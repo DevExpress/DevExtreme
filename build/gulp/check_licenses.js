0131ad50052169cbe1cf00e3ddf2c5058283ac60
Yana <ms.yana.work@mail.ru>
2020-09-24 08:32:59 +0300

    Improve checking

--- build/gulp/check_licenses.js
+++ build/gulp/check_licenses.js
@@ -5,12 +5,20 @@ const named = require('vinyl-named');
 
 const checkRruleLicenseNotice = lazyPipe()
     .pipe(named, function(file) {
-        const name = 'rrule.js';
+        const name = 'rrule.js - Library for working with recurrence rules for calendar dates.';
         const url = 'https://github.com/jakubroztocil/rrule';
+        const copyright = 'Copyright 2010, Jakub Roztocil and Lars Schoning';
         const licenseUrl = 'https://github.com/jakubroztocil/rrule/blob/master/LICENCE';
-        const re = new RegExp('\\*!\\s*.*' + name + '[\\s\\S]*' + url + '[\\s\\S]*' + '.*Copyright[\\s\\S]*' + licenseUrl);
+        const licenseType = 'Licenced under the BSD licence.';
+        const separator = '\\s*\\*\\s';
 
-        if(file.contents.toString().search(re) === -1) {
+        const fileContent = file.contents.toString();
+
+        // const re1 = new RegExp('\\*!\\s*.*' + name + '\\s*\\*\\s' + url + '\\s*\\*\\s*\\*\\s' + copyright + '\\s*\\*\\s' + licenceType + '\\s*\\*\\s' + licenseUrl);
+        // const re = new RegExp('\\*!\\s*.*' + name + separator + url + separator + '*\\*\\s' + copyright + separator + licenseType + separator + licenseUrl);
+
+        const re = new RegExp(`\\*!\\s*.*${name}${separator}${url}${separator}*\\*\\s${copyright}${separator}${licenseType}${separator}${licenseUrl}`);
+        if(fileContent.search(re) === -1) {
             throw new Error(`RRule license header wasn't found in ${file.stem}`);
         }
     });
