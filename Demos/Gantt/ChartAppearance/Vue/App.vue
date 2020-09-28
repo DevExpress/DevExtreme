29cda6ca7f1dede9c632640b4e7f3732e8a96c70
Artem Kobilinskii <a.kobilinsky@gmail.com>
2020-09-28 15:26:49 +0300

    dxGantt: Fix linter issues with Vue and Angular (#175)
    * dxGantt: Fix Linters
    
    * dxGantt: Fix linter issue
    
    * dxGantt: Fix linter issue by moving styles to classes
    
    * dxGantt: Fix linter issues with Vue and Angular
    
    Co-authored-by: CORP\kobilinsky.artem <artem.kobilinsky@devexpress.com>

--- Demos/Gantt/ChartAppearance/Vue/App.vue
+++ Demos/Gantt/ChartAppearance/Vue/App.vue
@@ -110,16 +110,15 @@ export default {
   },
   methods: {
     getTaskTooltipContentTemplate(model) {
-      var timeEstimate = Math.abs(model.start - model.end) / 36e5;
-      var timeLeft = Math.floor((100 - model.progress) / 100 * timeEstimate);
+      const timeEstimate = Math.abs(model.start - model.end) / 36e5;
+      const timeLeft = Math.floor((100 - model.progress) / 100 * timeEstimate);
 
-      return "<div style='font-size:14px'>" + model.title + "</div>"                
-          + "<p style='font-size:10px'>" + "<span> Estimate: </span>" + timeEstimate + "<span> hours </span>" + "</p>"
-          + "<p style='font-size:10px'>" + "<span> Left: </span>" + timeLeft + "<span> hours </span>" + "</p>";
+      return `<div class="template-header"> ${model.title} </div>`
+      + `<p class="template-item"> <span> Estimate: </span> ${timeEstimate} <span> hours </span> </p>`
+      + `<p class="template-item"> <span> Left: </span> ${timeLeft} <span> hours </span> </p>`;
     },
     onShowCustomTaskTooltip(e) {
-        e.value ? this.taskTooltipContentTemplate = this.getTaskTooltipContentTemplate 
-                : this.taskTooltipContentTemplate = "";
+      e.value ? this.taskTooltipContentTemplate = this.getTaskTooltipContentTemplate : this.taskTooltipContentTemplate = '';
     }
   }
 };
@@ -149,5 +148,13 @@ export default {
 
   .option:last-child {
     margin-right: 0;
+  }
+
+  .template-header {
+    font-size: 14px;
+  }
+
+  .template-item {
+      font-size: 10px;
   }
 </style>
