6b5b31361f555d1b01c9a87edf70aee659020725
Artem Kobilinskii <a.kobilinsky@gmail.com>
2020-09-28 14:38:44 +0300

    dxGantt: Fix linter issue by moving styles to classes (#173)
    * dxGantt: Fix Linters
    
    * dxGantt: Fix linter issue
    
    * dxGantt: Fix linter issue by moving styles to classes
    
    Co-authored-by: CORP\kobilinsky.artem <artem.kobilinsky@devexpress.com>

--- Demos/Gantt/ChartAppearance/React/App.js
+++ Demos/Gantt/ChartAppearance/React/App.js
@@ -111,9 +111,9 @@ class App extends React.Component {
     const timeEstimate = Math.abs(model.start - model.end) / 36e5;
     const timeLeft = Math.floor((100 - model.progress) / 100 * timeEstimate);
 
-    return `<div style="font-size:14px"> ${model.title} </div>`
-    + `<p style="font-size:10px"> <span> Estimate: </span> ${timeEstimate} <span> hours </span> </p>`
-    + `<p style="font-size:10px"> <span> Left: </span> ${timeLeft} <span> hours </span> </p>`;
+    return `<div class="template-header"> ${model.title} </div>`
+    + `<p class="template-item"> <span> Estimate: </span> ${timeEstimate} <span> hours </span> </p>`
+    + `<p class="template-item"> <span> Left: </span> ${timeLeft} <span> hours </span> </p>`;
   }
 }
 
