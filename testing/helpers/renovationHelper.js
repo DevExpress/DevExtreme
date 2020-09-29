a7ef7ade283bc5b1660a918076f2eede2463e92a
Maxim Kudryavtsev <maksim.kudryavtsev@devexpress.com>
2020-09-29 16:32:59 +0300

    Add capability to fix QUnit tests with renovation (#14682)
    The `setupRenovation` function wraps methods into `Preact.act` to avoid async code.

--- testing/helpers/renovationHelper.js
+++ testing/helpers/renovationHelper.js
@@ -2,6 +2,45 @@ import registerComponent from 'core/component_registrator';
 import { name as getName } from 'core/utils/public_component';
 import { act } from 'preact/test-utils';
 
+const WRAPPED_METHOD_NAMES = {
+    ctor: true,
+    option: true,
+    focus: true,
+    repaint: true,
+    render: true,
+    _createComponent: true,
+};
+
+function functionWrapper() {
+    let res;
+    const that = this;
+    act(() => {
+        res = that.callBase.apply(that, arguments);
+    });
+    return res;
+}
+
+const skipRenovationAsyncMethods = (widget) => {
+    const wrappedMethods = Object.keys(WRAPPED_METHOD_NAMES).reduce((methods, methodName) => {
+        methods[methodName] = functionWrapper;
+        return methods;
+    }, {});
+
+    return widget.inherit(wrappedMethods);
+};
+
+export const setupRenovation = (widget) => {
+    const widgetName = getName(widget);
+
+    QUnit.moduleStart(() => {
+        registerComponent(widgetName, skipRenovationAsyncMethods(widget));
+    });
+
+    QUnit.moduleDone(() => {
+        registerComponent(widgetName, widget);
+    });
+};
+
 export const createRenovationModuleConfig = (oldWidget, renovatedWidget, config = {}) => {
     const widgetName = getName(oldWidget);
     if(oldWidget.IS_RENOVATED_WIDGET !== undefined) {
