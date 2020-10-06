337ac0eaf2761f18a19fbbcd4eab8d4892193727
volkov.vladislav <dxvladislavvolkov@users.noreply.github.com>
2020-10-06 16:21:00 +0300

    Fix linter (#196)
    Co-authored-by: dxvladislavvolkov <vladislavvolkov@Vladislavs-MacBook-Pro.local>

--- Demos/TreeList/KeyboardNavigation/Vue/index.js
+++ Demos/TreeList/KeyboardNavigation/Vue/index.js
@@ -3,4 +3,4 @@ import App from './App.vue';
 import config from 'devextreme-vue/core/config';
 config({ useLegacyTemplateEngine: false });
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
