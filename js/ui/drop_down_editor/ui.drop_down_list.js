544453e4a597196d736224e65adf3ae53d6f284c
Anton Kuznetsov <ksercs0@gmail.com>
2020-10-06 11:24:48 +0300

    SelectBox should be opened on instant re-click after click on clearButton (T935717) (#14812) (#14820)

--- js/ui/drop_down_editor/ui.drop_down_list.js
+++ js/ui/drop_down_editor/ui.drop_down_list.js
@@ -435,6 +435,7 @@ const DropDownList = DropDownEditor.inherit({
 
         this._clearFilter();
         this._clearSelectedItem();
+        this._preventFiltering = true;
     },
 
     _listItemElements: function() {
