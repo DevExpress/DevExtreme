3cafd085b679a5226b2c63950a86d42409ff0004
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2020-10-07 12:51:48 +0300

    Scheduler - Correct data for virtual scrolling demo (#205)

--- Demos/Scheduler/VirtualScrolling/Angular/app/app.service.ts
+++ Demos/Scheduler/VirtualScrolling/Angular/app/app.service.ts
@@ -5,6 +5,7 @@ export class Appointment {
     startDate: Date;
     endDate: Date;
     allDay?: boolean;
+    resourceId?: number;
 }
 
 export class Resource {
@@ -13,46 +14,91 @@ export class Resource {
     color: string;
 }
 
+const dataSet: Appointment[] = [{
+    text: "Website Re-Design Plan",
+    startDate: new Date(2021, 8, 6, 9, 30),
+    endDate: new Date(2021, 8, 6, 11, 30)
+}, {
+    text: "Book Flights to San Fran for Sales Trip",
+    startDate: new Date(2021, 8, 6, 12, 0),
+    endDate: new Date(2021, 8, 6, 13, 0),
+    allDay: true
+}, {
+    text: "Install New Router in Dev Room",
+    startDate: new Date(2021, 8, 6, 14, 30),
+    endDate: new Date(2021, 8, 6, 15, 30)
+}, {
+    text: "Approve Personal Computer Upgrade Plan",
+    startDate: new Date(2021, 8, 7, 10, 0),
+    endDate: new Date(2021, 8, 7, 11, 0)
+}, {
+    text: "Final Budget Review",
+    startDate: new Date(2021, 8, 7, 12, 0),
+    endDate: new Date(2021, 8, 7, 13, 35)
+}, {
+    text: "New Brochures",
+    startDate: new Date(2021, 8, 7, 14, 30),
+    endDate: new Date(2021, 8, 7, 15, 45)
+}, {
+    text: "Install New Database",
+    startDate: new Date(2021, 8, 8, 9, 45),
+    endDate: new Date(2021, 8, 8, 11, 15)
+}, {
+    text: "Approve New Online Marketing Strategy",
+    startDate: new Date(2021, 8, 8, 12, 0),
+    endDate: new Date(2021, 8, 8, 14, 0)
+}, {
+    text: "Upgrade Personal Computers",
+    startDate: new Date(2021, 8, 8, 15, 15),
+    endDate: new Date(2021, 8, 8, 16, 30)
+}, {
+    text: "Customer Workshop",
+    startDate: new Date(2021, 8, 9, 11, 0),
+    endDate: new Date(2021, 8, 9, 12, 0),
+    allDay: true
+}, {
+    text: "Prepare 2015 Marketing Plan",
+    startDate: new Date(2021, 8, 9, 11, 0),
+    endDate: new Date(2021, 8, 9, 13, 30)
+}, {
+    text: "Brochure Design Review",
+    startDate: new Date(2021, 8, 9, 14, 0),
+    endDate: new Date(2021, 8, 9, 15, 30)
+}, {
+    text: "Create Icons for Website",
+    startDate: new Date(2021, 8, 10, 10, 0),
+    endDate: new Date(2021, 8, 10, 11, 30)
+}, {
+    text: "Upgrade Server Hardware",
+    startDate: new Date(2021, 8, 10, 14, 30),
+    endDate: new Date(2021, 8, 10, 16, 0)
+}, {
+    text: "Submit New Website Design",
+    startDate: new Date(2021, 8, 10, 16, 30),
+    endDate: new Date(2021, 8, 10, 18, 0)
+}, {
+    text: "Launch New Website",
+    startDate: new Date(2021, 8, 10, 12, 20),
+    endDate: new Date(2021, 8, 10, 14, 0)
+}
+];
+
+const colors = [
+    'rgba(88, 116, 255, 0.8)',
+    'rgba(234, 128, 252, 0.8)',
+    'rgba(198, 86, 144, 0.85)',
+    'rgba(254, 194, 0, 0.7)'
+];
 
 @Injectable()
 export class Service {
-    private colors: string[];
-    private texts: string[];
-
-    constructor() {
-        this.colors = [
-            'rgba(63, 81, 181, 0.7)',
-            'rgba(234, 128, 252, 0.7)',
-            'rgba(223, 82, 134, 0.7)',
-            'rgba(254, 194, 0, 0.7)'
-        ];
-        this.texts = [
-            "Website Re-Design Plan",
-            "Book Flights to San Fran for Sales Trip",
-            "Install New Router in Dev Room",
-            "Approve Personal Computer Upgrade Plan",
-            "Final Budget Review",
-            "New Brochures",
-            "Install New Database",
-            "Approve New Online Marketing Strategy",
-            "Upgrade Personal Computers",
-            "Customer Workshop",
-            "Prepare 2015 Marketing Plan",
-            "Brochure Design Review",
-            "Create Icons for Website",
-            "Upgrade Server Hardware",
-            "Submit New Website Design",
-            "Launch New Website",
-          ];
-    }
-
     get resourcesAmount() { return 100; }
 
     generateResources(): Resource[] {
         const resources = [];
 
-        for(var i = 0; i < this.resourcesAmount; ++i) {
-            const color = this.colors[i % this.colors.length];
+        for(let i = 0; i < this.resourcesAmount; ++i) {
+            const color = colors[i % colors.length];
 
             resources.push({
                 id: i,
@@ -65,40 +111,19 @@ export class Service {
     };
 
     generateAppointments(): Appointment[] {
-        const startDay = 6;
-        const endDay = 14;
-        const startDayHour = 9;
-        const endDayHour = 18;
-
-        const appointments = [];
+        const data = [];
 
         for(let resourceId = 0; resourceId < this.resourcesAmount; ++resourceId) {
-            for(let dayIndex = startDay; dayIndex <= endDay; ++dayIndex) {
-                for(let count = 0; count < 10; ++count) {
-                    const hour = this.getRandomInt(startDayHour, endDayHour);
-                    const minutes = this.getRandomInt(15, 44);
-                    const minutesBeforeHour = 60 - minutes;
-                    const minMinutes = Math.min(minutes, minutesBeforeHour);
-                    const maxMinutes = Math.max(minutes, minutesBeforeHour);
-                    const appointmentTime = this.getRandomInt(minMinutes, maxMinutes) * 60 * 1000;
-                    const startDate = new Date(2021, 8, dayIndex, hour, minutes);
-                    const endDate = new Date(startDate.getTime() + appointmentTime);
-
-                    const item = {
-                        text: this.texts[this.getRandomInt(0, this.texts.length)],
-                        resourceId: resourceId,
-                        startDate: startDate,
-                        endDate: endDate,
-                    };
-                    appointments.push(item);
-                }
-            }
+            dataSet.forEach(item => {
+                data.push({
+                    text: item.text,
+                    startDate: item.startDate,
+                    endDate: item.endDate,
+                    resourceId
+                });
+            });
         }
 
-        return appointments;
+        return data;
     }
-
-    private getRandomInt(min, max): number {
-        return Math.floor(min + Math.random() * (max + 1 - min));
-    };
 }
