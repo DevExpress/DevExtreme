62b37ef32ad3932bb16a5c09ecc73ecabce5c7e8
Artem Kobilinskii <a.kobilinsky@gmail.com>
2020-10-08 16:17:17 +0300

    dxGantt: Fix Linters (#221)
    * dxGantt: Fix Linters
    
    * dxGantt: Fix linter issue
    
    * dxGantt: Fix linter issue by moving styles to classes
    
    * dxGantt: Fix linter issues with Vue and Angular
    
    * dxGantt: Add ContextMenu demo
    
    * dxGantt: Improve and fix ContextMenu demos
    
    * dxGantt: Fix linter
    
    Co-authored-by: CORP\kobilinsky.artem <artem.kobilinsky@devexpress.com>

--- Demos/Gantt/Toolbar/Vue/App.vue
+++ Demos/Gantt/Toolbar/Vue/App.vue
@@ -55,8 +55,8 @@
     >
       <div>
         The DevExtreme HTML5 JavaScript <b>Gantt</b> allows you to display the task flow and dependencies between tasks over a certain period.
-        <br/>
-        <br/>
+        <br>
+        <br>
         You can move and modify tasks (a task name, duration or progress, for example) directly from the chart.
         Adjust the timescale to display tasks in smaller or greater time intervals, from hours to years.
         Hold the CTRL key and rotate your mouse's scroll wheel to zoom (in or out) to browse data across various levels of detail.
