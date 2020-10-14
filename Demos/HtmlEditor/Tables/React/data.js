6d2172c2a5a5f3f0473becab560d4ae863de5f6c
Dmitry Levkovskiy <DokaRus@users.noreply.github.com>
2020-10-14 16:51:13 +0300

    Update "HtmlEditor - Tables" demo content and remove "hidden" tag (#266)

--- Demos/HtmlEditor/Tables/React/data.js
+++ Demos/HtmlEditor/Tables/React/data.js
@@ -1,27 +1,27 @@
 export const markup = `
     <h2>
-        Simple table
+        Hardware Upgrade
     </h2>
     <br>
     <table>
         <tr style="font-weight: bold;">
-            <td style="text-align: left;">Task</td>
+            <td style="text-align: left;">Task Subject</td>
             <td style="text-align: center;">Status</td>
             <td style="text-align: right;">Progress</td>
         </tr>
         <tr style="text-align: center;">
-            <td style="text-align: left;">Remove deprecated parts</td>
+            <td style="text-align: left;">Upgrade Server Hardware</td>
             <td style="text-align: center;">In Progress</td>
             <td style="text-align: right;">45%</td>
         </tr>
         <tr style="text-align: center;">
-            <td style="text-align: left;">Fix inconsistent appearance</td>
-            <td style="text-align: center; color: red;">Paused</td>
+            <td style="text-align: left;">Upgrade Personal Computers</td>
+            <td style="text-align: center; color: red;">Need Assistance</td>
             <td style="text-align: right;">80%</td>
         </tr>
         <tr style="text-align: center;">
-            <td style="text-align: left;">Update Toolbar module</td>
-            <td style="text-align: center; color: lime;">Finished</td>
+            <td style="text-align: left;">Approve Personal Computer Upgrade Plan</td>
+            <td style="text-align: center; color: lime;">Completed</td>
             <td style="text-align: right;">100%</td>
         </tr>
     </table>
