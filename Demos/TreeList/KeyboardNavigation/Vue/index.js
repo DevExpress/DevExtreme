4e924619452fa5c564a804acaa4221a7814569d8
volkov.vladislav <dxvladislavvolkov@users.noreply.github.com>
2020-10-14 11:59:03 +0300

    Get rid of useLegacyTemplateEngine flag (#263)
    Co-authored-by: dxvladislavvolkov <vladislavvolkov@Vladislavs-MacBook-Pro.local>

--- Demos/TreeList/KeyboardNavigation/Vue/index.js
+++ Demos/TreeList/KeyboardNavigation/Vue/index.js
@@ -1,6 +1,4 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import config from 'devextreme-vue/core/config';
-config({ useLegacyTemplateEngine: false });
 
 createApp(App).mount('#app');
