920f7fdd347146fa36d5e44d497794d2f33ffe7e
ksercs <ksercs0@gmail.com>
2020-10-21 22:56:20 +0300

    total test fix

--- testing/tests/DevExpress.animation/position.tests.js
+++ testing/tests/DevExpress.animation/position.tests.js
@@ -966,12 +966,8 @@ const testCollision = (name, fixtureName, params, expectedHorzDist, expectedVert
         const $what = $('#what').height(300);
         const initialInnerHeight = window.innerHeight;
         const initialOuterHeight = window.outerHeight;
-        let heightStub;
-        if(QUnit.urlParams['nojquery']) {
-            heightStub = sinon.stub(renderer.fn, 'height').returns(1000);
-        } else {
-            heightStub = sinon.stub($.prototype, 'height').returns(1000);
-        }
+
+        const heightStub = sinon.stub(renderer.fn, 'height').returns(1000);
 
         try {
             window.innerHeight = 500;
