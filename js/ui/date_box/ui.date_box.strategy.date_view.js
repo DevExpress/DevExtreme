5d5f5bd5eff3936303ee063d9c829618ee1ab4cc
Anton Kuznetsov <ksercs0@gmail.com>
2020-10-27 16:13:29 +0300

    DateBox with rollers should have popup width 100% on mobile ios (#15061) (#15069)

--- js/ui/date_box/ui.date_box.strategy.date_view.js
+++ js/ui/date_box/ui.date_box.strategy.date_view.js
@@ -29,7 +29,6 @@ const DateViewStrategy = DateBoxStrategy.inherit({
         return {
             toolbarItems: this.dateBox._popupToolbarItemsConfig(),
             onInitialized: config.onInitialized,
-            width: 'auto',
 
             defaultOptionsRules: [
                 {
