f1e8f9c6286ea9ac48a31c54af3d4480a4e3edee
ksercs <ksercs0@gmail.com>
2020-10-28 17:13:02 +0300

    check scroll behavior in hidden scope

--- js/core/utils/position.js
+++ js/core/utils/position.js
@@ -42,7 +42,7 @@ const getScrollBehavior = callOnce(function() {
     /* Append a RTL scrollable 1px square containing a 2px-wide child and check
        the initial scrollLeft and whether it's possible to set a negative one.*/
     document.body.insertAdjacentHTML('beforeend', `<div style='direction: rtl;
-       position: absolute; left: 0; top: 0; overflow: hidden; width: 1px;
+       position: absolute; left: 0; top: -1; overflow: hidden; width: 1px;
        height: 1px;'><div style='width: 2px; height: 1px;'></div></div>`);
 
     const scroller = document.body.lastElementChild;
