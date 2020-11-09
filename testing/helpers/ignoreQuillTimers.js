cb4430ae9cf48f74d0c40bf8b417953e53e0c662
Dmitry Levkovskiy <DokaRus@users.noreply.github.com>
2020-11-09 15:16:25 +0300

    Fix blinking HtmlEditor tests (#14431) (#15172)

--- testing/helpers/ignoreQuillTimers.js
+++ testing/helpers/ignoreQuillTimers.js
@@ -1,12 +1,15 @@
 QUnit.timersDetector.ignoreRules.register(function(args) {
     const timerType = args.timerType;
     const callback = String(args.callback).replace(/\s/g, '');
-    const stack = args.stack;
+    const stack = args.stack.toLowerCase();
 
     if(
         timerType === 'timeouts' &&
-        /handleDOM.*quill.js/.test(stack) &&
-        callback.indexOf('function(){[nativecode]}') > -1
+        /handledom.*quill|register.*quill/.test(stack) &&
+        (
+            callback.indexOf('function(){[nativecode]}') > -1 ||
+            callback.toLowerCase().indexOf('firefox') > -1
+        )
     ) {
         return true;
     }
