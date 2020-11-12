f0be773c16b69d78bccfee203e103a7858bcb4f5
CORP\kobilinsky.artem <artem.kobilinsky@devexpress.com>
2020-11-12 15:00:19 +0300

    dxGantt: Add allowTaskResourceUpdate property. Update version to 2.0.5

--- testing/tests/DevExpress.ui.widgets/gantt.tests.js
+++ testing/tests/DevExpress.ui.widgets/gantt.tests.js
@@ -318,6 +318,7 @@ QUnit.module('Options', moduleConfig, () => {
             allowResourceAdding: false,
             allowResourceDeleting: false,
             allowResourceUpdating: false,
+            allowTaskResourceUpdating: false
         });
         coreEditingSettings = getGanttViewCore(this.instance).settings.editing;
         assert.equal(coreEditingSettings.enabled, true, 'editing allowed');
@@ -329,6 +330,7 @@ QUnit.module('Options', moduleConfig, () => {
         assert.equal(coreEditingSettings.allowResourceInsert, false, 'resource adding is prohibited');
         assert.equal(coreEditingSettings.allowResourceDelete, false, 'resource deleting is prohibited');
         assert.equal(coreEditingSettings.allowResourceUpdate, false, 'resource updating is prohibited');
+        assert.equal(coreEditingSettings.allowTaskResourceUpdate, false, 'task resource updating is prohibited');
         this.instance.option('editing.enabled', false);
         coreEditingSettings = getGanttViewCore(this.instance).settings.editing;
         assert.equal(coreEditingSettings.enabled, false, 'editing is prohibited');
@@ -721,6 +723,33 @@ QUnit.module('Dialogs', moduleConfig, () => {
 
         popupTitleText = $dialog.find('.dx-popup-title').text();
         assert.equal(expectedTaskEditTitleText, popupTitleText, 'taskEditPopup title shown again');
+    });
+    test('assign resource dxTagBox is disabled when allowTaskResourceUpdating is false', function(assert) {
+        this.createInstance(allSourcesOptions);
+        this.instance.option('editing.enabled', true);
+        this.instance.option('editing.allowTaskResourceUpdating', false);
+
+        this.instance.option('selectedRowKey', 1);
+        this.clock.tick();
+
+        showTaskEditDialog(this.instance);
+        const $dialog = $('body').find(POPUP_SELECTOR);
+        const tagBox = $dialog.find('.dx-tag-container > .dx-texteditor-input');
+        assert.ok(tagBox.attr('aria-readOnly'), 'resource tagBox is readOnly');
+    });
+    test('show edit resource dialog button is disabled when allowResourceAdding and allowResourceDeleting are false ', function(assert) {
+        this.createInstance(allSourcesOptions);
+        this.instance.option('editing.enabled', true);
+        this.instance.option('editing.allowResourceAdding', false);
+        this.instance.option('editing.allowResourceDeleting', false);
+
+        this.instance.option('selectedRowKey', 1);
+        this.clock.tick();
+
+        showTaskEditDialog(this.instance);
+        const $dialog = $('body').find(POPUP_SELECTOR);
+        const button = $dialog.find('.dx-texteditor-buttons-container > .dx-button');
+        assert.ok(button.attr('aria-disabled'), 'button is disabled');
     });
 });
 
