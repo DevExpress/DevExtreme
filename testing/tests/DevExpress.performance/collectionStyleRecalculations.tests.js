3a0836cdb7c9f6bf564b3daf7026611dcb831201
babich-a <alexey.babych@gmail.com>
2020-11-13 16:08:37 +0300

    Add generic theme to performance tests

--- testing/tests/DevExpress.performance/collectionStyleRecalculations.tests.js
+++ testing/tests/DevExpress.performance/collectionStyleRecalculations.tests.js
@@ -1,6 +1,7 @@
 require('../../helpers/qunitPerformanceExtension.js');
 
 require('common.css!');
+require('generic_light.css!');
 
 const $ = require('jquery');
 const resizeCallbacks = require('core/utils/resize_callbacks');
