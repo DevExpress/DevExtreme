8ed21107ff5009b4a4a6bb5f6ee10220766fbfc4
Anton Kuznetsov <ksercs0@gmail.com>
2020-11-14 01:32:16 +0300

    Fix tests after #15199 (#15245)

--- testing/tests/DevExpress.ui.widgets/popover.tests.js
+++ testing/tests/DevExpress.ui.widgets/popover.tests.js
@@ -1046,6 +1046,7 @@ QUnit.module('positioning', () => {
                 .css('height', '2000px')
                 .appendTo('body');
 
+            window.scrollTo(0, $target.offset().top);
             new Popover($popover, {
                 target: $target,
                 visible: true,
@@ -1054,10 +1055,11 @@ QUnit.module('positioning', () => {
 
             const $shader = $('.dx-overlay-shader');
 
-            assert.strictEqual($shader.height(), $(window).height(), 'shader height is equal to window height');
-            assert.strictEqual($shader.width(), $(window).width(), 'shader width is equal to window width');
+            assert.roughEqual($shader.height(), $(window).height(), 1.01, 'shader height is equal to window height');
+            assert.roughEqual($shader.width(), $(window).width(), 1.01, 'shader width is equal to window width');
         } finally {
             fixtures.simple.drop();
+            window.scrollTo(0, 0);
         }
     });
 
