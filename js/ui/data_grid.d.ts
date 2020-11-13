9566933cf3f7c7b9c8677541a1e3897025bed725
Alyar <Alyar666@users.noreply.github.com>
2020-11-13 14:23:24 +0300

    DataGrid: scrolling.showScrollbar - fix default value for desktop (#15256)

--- js/ui/data_grid.d.ts
+++ js/ui/data_grid.d.ts
@@ -906,6 +906,7 @@ export interface GridBaseScrolling {
     scrollByThumb?: boolean;
     /**
      * @docid GridBaseOptions.scrolling.showScrollbar
+     * @default 'onHover' [for](desktop)
      * @type Enums.ShowScrollbarMode
      * @default 'onScroll'
      * @prevFileNamespace DevExpress.ui
