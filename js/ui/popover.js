9788ade16631b29578d4761adc6fe1a28681277d
Anton Kuznetsov <ksercs0@gmail.com>
2020-11-14 12:43:16 +0300

    Popover wrapper should have correct position and dimensions when page is scrolled (T945429) (#15199) (#15224)

--- js/ui/popover.js
+++ js/ui/popover.js
@@ -212,7 +212,8 @@ const Popover = Popup.inherit({
             closeOnTargetScroll: true,
             arrowPosition: '',
             arrowOffset: 0,
-            boundaryOffset: { h: 10, v: 10 }
+            boundaryOffset: { h: 10, v: 10 },
+            _fixedPosition: true
 
             /**
             * @name dxPopoverOptions.focusStateEnabled
