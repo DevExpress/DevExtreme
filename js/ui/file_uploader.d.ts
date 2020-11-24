da563f8cccba20eda858a904d1125f65d2e9d0f6
Stanislav Klesarev <klesarevklesarevstas@gmail.com>
2020-11-24 13:47:37 +0300

    Update ts once more

--- js/ui/file_uploader.d.ts
+++ js/ui/file_uploader.d.ts
@@ -101,7 +101,7 @@ export interface dxFileUploaderOptions extends EditorOptions<dxFileUploader> {
      * @docid dxFileUploaderOptions.labelText
      * @type string
      * @default "or Drop file here"
-     * @default "" [for](msie|desktop)
+     * @default "" [for](Internet_Explorer_10|Internet_Explorer_9)
      * @prevFileNamespace DevExpress.ui
      * @public
      */
