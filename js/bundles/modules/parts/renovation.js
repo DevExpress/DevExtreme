ee9fa529b13dec399040fb79456bec553c0a0eae
Evgeniy Aryamnov <earyamnow@mail.ru>
2020-11-26 12:38:31 +0300

    Delete from bundle

--- js/bundles/modules/parts/renovation.js
+++ js/bundles/modules/parts/renovation.js
@@ -4,7 +4,6 @@ module.exports = [
     { name: 'CheckBox', pathInRenovationFolder: 'ui/check_box.j' },
     { name: 'Widget', pathInRenovationFolder: 'ui/widget.j' },
     { name: 'ScrollView', pathInRenovationFolder: 'ui/scroll_view.j' },
-    { name: 'TooltipItemLayout', pathInRenovationFolder: 'ui/scheduler/appointment-tooltip/item-layout.j' },
     // { name: 'DataGrid', pathInRenovationFolder: 'ui/data_grid/data_grid.j' },
     { name: 'Pager', pathInRenovationFolder: 'ui/pager/pager.j' },
 ];
