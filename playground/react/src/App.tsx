181aebdc9f908f71152f224b7901ce98e4c88048
Krijovnick <yulianabest7@yandex.ru>
2020-11-30 17:34:41 +0300

    reset example

--- playground/react/src/App.tsx
+++ playground/react/src/App.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import Tooltip from './artifacts/react/renovation/viz/core/tooltip';
+import Button from './artifacts/react/renovation/ui/button';
 
 function App() {
-    const [MousePosition, setMousePosition] = React.useState({
-        left: 0,
-        top: 0
-    })
-    function handleMouseMove(ev) { setMousePosition({left: ev.pageX, top: ev.pageY}); }
     return (
-        <svg width="500" height="600">
-            <rect width="500" height="500" fill='white' onPointerMove={(ev)=> handleMouseMove(ev)} strokeWidth={1} stroke='black'></rect>
-            <Tooltip 
-                text={'Tooltip Text'} x={MousePosition.left} y={MousePosition.top} 
-                canvas={{left: 0, top: 0, width: 500, height: 500, right: 0, bottom: 0}}
-                offset={8}>
-            </Tooltip>
-        </svg>
+        <Button
+            text={'Click Me!'}
+            icon={'download'}
+            onClick={() => alert('Clicked!')}
+        ></Button>
     );
 }
 
