c8173c1d879413a0ad1cbc8972e868d13394d621
Alexander Bezborodov <alexander.bezborodov@devexpress.com>
2020-12-01 17:06:37 +0300

    Viz - Bullet renovation - Fix spaces count

--- js/renovation/viz/core/common/types.d.ts
+++ js/renovation/viz/core/common/types.d.ts
@@ -1,8 +1,8 @@
 export interface Canvas { left?: number; top?: number; right?: number; bottom?: number; width?: number; height?: number }
 export interface Size { width?: number; height?: number }
 export interface RecalculateCoordinates {
-    canvas: Canvas; anchorX: number; anchorY: number;
-    size: Size; offset: number; arrowLength: number;
+  canvas: Canvas; anchorX: number; anchorY: number;
+  size: Size; offset: number; arrowLength: number;
 }
 export interface Coordinates { x: number; y: number; anchorX: number; anchorY: number }
 export interface Margin { top?: number; left?: number; bottom?: number; right?: number }
