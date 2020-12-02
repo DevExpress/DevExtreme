6e6b1d2a021816e11b807f6f1420e497fd4af849
Mike Vitik <vitikm@gmail.com>
2020-12-02 15:19:01 +0300

    fix renovation tests not work because *.j.js files aren't generated

--- build/gulp/generator/gulpfile.js
+++ build/gulp/generator/gulpfile.js
@@ -102,7 +102,8 @@ function generatePreactComponents(distPath = './', babelConfig = transpileConfig
             .pipe(gulpIf(isDefault, gulp.dest(context.TRANSPILED_PROD_PATH)))
             .pipe(gulpIf(renovation, gulp.dest(context.TRANSPILED_PROD_RENOVATION_PATH)))
             // dev is true only for 'generate-components-watch'. 'generate-components-watch' used in launch for qunit
-            .pipe(gulpIf(dev, gulp.dest(context.TRANSPILED_RENOVATION_PATH)))
+            // without renovation flag ui/renovation is clear after build:r
+            .pipe(gulpIf(renovation || dev, gulp.dest(context.TRANSPILED_RENOVATION_PATH)))
             .pipe(gulpIf(esmPackage, gulp.dest(path.join(context.TRANSPILED_PROD_ESM_PATH, distPath))))
             .on('end', function() {
                 done(!dev && errors.length || undefined);
