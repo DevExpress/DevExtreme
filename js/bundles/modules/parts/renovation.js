7ca35e584b223ac8df4097ed0c887fdf6b9e4d27
Mike Vitik <vitikm@gmail.com>
2020-12-03 12:38:08 +0300

    fix after merge

--- js/bundles/modules/parts/renovation.js
+++ js/bundles/modules/parts/renovation.js
@@ -2,10 +2,10 @@
 // pathToWrapper should contains named export - WrapperWidget. WrapperWidget is predefined.
 // Default export doesnt work
 module.exports = [
-    { name: 'Button', pathInRenovationFolder: 'ui/button.j' },
-    { name: 'CheckBox', pathInRenovationFolder: 'ui/check_box.j' },
-    { name: 'Widget', pathInRenovationFolder: 'ui/widget.j' },
-    // { name: 'ScrollView', pathInRenovationFolder: 'ui//scroll_view/scroll_view.j' },
-    // { name: 'DataGrid', pathInRenovationFolder: 'ui/data_grid/data_grid.j' },
-    { name: 'Pager', pathInRenovationFolder: 'ui/pager/pager.j' },
+    { name: 'Button', pathInRenovationFolder: 'ui/button.j', pathInJSFolder: 'ui/button.js' },
+    { name: 'CheckBox', pathInRenovationFolder: 'ui/check_box.j', pathInJSFolder: 'ui/check_box.js' },
+    // { name: 'Widget', pathInRenovationFolder: 'ui/widget.j' },
+    // { name: 'ScrollView', pathInRenovationFolder: 'ui/scroll_view.j', pathInJSFolder: 'ui/scroll_view.js' },
+    // { name: 'DataGrid', pathInRenovationFolder: 'ui/data_grid/data_grid.j', pathInJSFolder: 'ui/button.js' },
+    { name: 'Pager', pathInRenovationFolder: 'ui/pager/pager.j', pathInJSFolder: 'ui/pager.js', pathToWrapper: '../../../testing/helpers/renovationPagerHelper.js' },
 ];
