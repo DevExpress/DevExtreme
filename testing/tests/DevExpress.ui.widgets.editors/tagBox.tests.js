671f3537a95f68cbdcfcca9a2e6cf583f6e39e33
ksercs <ksercs0@gmail.com>
2020-12-07 16:20:03 +0300

    add tick

--- testing/tests/DevExpress.ui.widgets.editors/tagBox.tests.js
+++ testing/tests/DevExpress.ui.widgets.editors/tagBox.tests.js
@@ -1030,6 +1030,7 @@ QUnit.module('the "text" option', moduleSetup, () => {
         const keyboard = new keyboardMock($input);
 
         keyboard.type('i');
+        this.clock.tick(TIME_TO_WAIT);
 
         const $firstItem = $(tagBox.content()).find(`.${LIST_ITEM_CLASS}`).eq(0);
         $firstItem.click();
