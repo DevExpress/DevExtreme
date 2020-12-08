d7b3953a88b00c4735b0b10afb7fa1037d6e3f78
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2020-12-08 11:52:18 +0300

    Export to PDF - add definition for loadPanel option (#15585)

--- js/excel_exporter.d.ts
+++ js/excel_exporter.d.ts
@@ -1,5 +1,6 @@
 import dxDataGrid, { dxDataGridColumn } from './ui/data_grid';
 import dxPivotGrid, { dxPivotGridPivotGridCell } from './ui/pivot_grid';
+import { ExportLoadPanel } from './exporter/export_load_panel';
 
 export interface ExcelDataGridCell {
     /**
@@ -101,63 +102,6 @@ export interface CellRange {
      * @public
      */
     to?: CellAddress;
-}
-
-export interface ExportLoadPanel {
-    /**
-     * @docid ExportLoadPanel.enabled
-     * @type boolean
-     * @default true
-     */
-    enabled?: boolean;
-    /**
-     * @docid ExportLoadPanel.text
-     * @type string
-     * @default "Exporting..."
-     */
-    text?: string;
-    /**
-     * @docid ExportLoadPanel.width
-     * @type number
-     * @default 200
-     */
-    width?: number;
-    /**
-     * @docid ExportLoadPanel.height
-     * @type number
-     * @default 90
-     */
-    height?: number;
-    /**
-     * @docid ExportLoadPanel.showIndicator
-     * @type boolean
-     * @default true
-     */
-    showIndicator?: boolean;
-    /**
-     * @docid ExportLoadPanel.indicatorSrc
-     * @type string
-     * @default ""
-     */
-    indicatorSrc?: string;
-    /**
-     * @docid ExportLoadPanel.showPane
-     * @type boolean
-     * @default true
-     */
-    showPane?: boolean;
-    /**
-     * @docid ExportLoadPanel.shading
-     * @type boolean
-     * @default false
-     */
-    shading?: boolean;
-    /**
-     * @docid ExportLoadPanel.shadingColor
-     * @type string
-     * @default ''
-     */
-    shadingColor?: string;
 }
 
 /**
