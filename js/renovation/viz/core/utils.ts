8ad1e8140d91eeee3e3f66b66d27dd046a17afa8
Alexander Bezborodov <alexander.bezborodov@devexpress.com>
2020-12-11 18:09:58 +0300

    Viz Renovation - Fix base widget canvas calculating

--- js/renovation/viz/core/utils.ts
+++ js/renovation/viz/core/utils.ts
@@ -1,5 +1,16 @@
 import { ConfigContextValue } from '../../ui/common/config_context';
 import globalConfig from '../../../core/config';
+import getElementComputedStyle from '../../ui/pager/utils/get_computed_style';
+import { toNumber } from '../../ui/pager/utils/get_element_width';
+
+export function getElementWidth(element: Element | undefined): number {
+  const style = getElementComputedStyle(element);
+  return toNumber(style?.width) - toNumber(style?.paddingLeft) - toNumber(style?.paddingRight);
+}
+export function getElementHeight(element: Element | undefined): number {
+  const style = getElementComputedStyle(element);
+  return toNumber(style?.height) - toNumber(style?.paddingTop) - toNumber(style?.paddingBottom);
+}
 
 export const sizeIsValid = (value): boolean => !!(value ?? (value > 0));
 
