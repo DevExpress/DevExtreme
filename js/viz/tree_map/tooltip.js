e7a4546451398eb85d2c0735d52300c00cf967ac
Smirnova Yuliya <Krijovnick@users.noreply.github.com>
2020-12-11 18:18:15 +0300

    Viz - fix showing tooltip (T956585) (#15668)

--- js/viz/tree_map/tooltip.js
+++ js/viz/tree_map/tooltip.js
@@ -40,6 +40,9 @@ proto._showTooltip = function(index, coords) {
         return;
     }
     const callback = (result) => {
+        if(result === undefined) {
+            return;
+        }
         if(result) {
             that._moveTooltip(node, coords);
         } else {
