9f40e27f310393d763101ed4b70f630105d57fe5
Dmitry Levkovskiy <DokaRus@users.noreply.github.com>
2020-12-22 09:37:29 +0300

    Fix an unexpected behavior when using keyboard navigation in an empty TagBox (T959418) (#15787)
    (cherry picked from commit c766e75259fd3e426458d1b893a272ecae951968)

--- js/ui/tag_box.js
+++ js/ui/tag_box.js
@@ -50,6 +50,7 @@ const TagBox = SelectBox.inherit({
     _supportedKeys: function() {
         const parent = this.callBase();
         const sendToList = options => this._list._keyboardHandler(options);
+        const rtlEnabled = this.option('rtlEnabled');
 
         return extend({}, parent, {
             backspace: function(e) {
@@ -123,13 +124,11 @@ const TagBox = SelectBox.inherit({
                 }
             },
             leftArrow: function(e) {
-                if(!this._isCaretAtTheStart()) {
-                    return;
-                }
-
-                const rtlEnabled = this.option('rtlEnabled');
-
-                if(this._isEditable() && rtlEnabled && !this._$focusedTag) {
+                if(
+                    !this._isCaretAtTheStart() ||
+                    this._isEmpty() ||
+                    this._isEditable() && rtlEnabled && !this._$focusedTag
+                ) {
                     return;
                 }
 
@@ -140,13 +139,11 @@ const TagBox = SelectBox.inherit({
                 !this.option('multiline') && this._scrollContainer(direction);
             },
             rightArrow: function(e) {
-                if(!this._isCaretAtTheStart()) {
-                    return;
-                }
-
-                const rtlEnabled = this.option('rtlEnabled');
-
-                if(this._isEditable() && !rtlEnabled && !this._$focusedTag) {
+                if(
+                    !this._isCaretAtTheStart() ||
+                    this._isEmpty() ||
+                    this._isEditable() && !rtlEnabled && !this._$focusedTag
+                ) {
                     return;
                 }
 
@@ -163,6 +160,10 @@ const TagBox = SelectBox.inherit({
         e.preventDefault();
         e.stopPropagation();
         this._saveValueChangeEvent(e);
+    },
+
+    _isEmpty: function() {
+        return this._getValue().length === 0;
     },
 
     _updateTagsContainer: function($element) {
