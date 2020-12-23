a2a932eb327ae2db1d797e219845c9a27d730d7e
Sergey Novikov <novsstation@yandex.ru>
2020-12-23 18:08:00 +0300

    Fix code coverage

--- js/renovation/ui/scroll_view/scrollbar.tsx
+++ js/renovation/ui/scroll_view/scrollbar.tsx
@@ -4,15 +4,12 @@ import {
 } from 'devextreme-generator/component_declaration/common';
 
 import { Widget } from '../common/widget';
-import { combineClasses } from '../../utils/combine_classes';
 import { ScrollbarProps } from './scrollbar_props';
 
 export const viewFunction = ({
-  cssClasses,
   restAttributes,
 }: Scrollbar): JSX.Element => (
   <Widget
-    classes={cssClasses}
     // eslint-disable-next-line react/jsx-props-no-spreading
     {...restAttributes}
   >
@@ -24,10 +21,4 @@ export const viewFunction = ({
   view: viewFunction,
 })
 export class Scrollbar extends JSXComponent<ScrollbarProps>() {
-  // eslint-disable-next-line class-methods-use-this
-  get cssClasses(): string {
-    const classesMap = {
-    };
-    return combineClasses(classesMap);
-  }
 }
