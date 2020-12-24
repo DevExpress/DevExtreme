1273d44d4d2764221bd42573b1b31bb894466f3c
Evgeniy Aryamnov <earyamnow@mail.ru>
2020-12-24 10:03:27 +0300

    Add testcafe test

--- testing/testcafe/tests/scheduler/virtualScrollingCellSelection/virtualScrollingCellSelection.ts
+++ testing/testcafe/tests/scheduler/virtualScrollingCellSelection/virtualScrollingCellSelection.ts
@@ -178,3 +178,24 @@ test('Selection should work correctly while scrolling when appointments are grou
     groupOrientation: 'vertical',
   }],
 }));
+
+test('Selection should work in month view', async (t) => {
+  await t
+    .dragToElement(scheduler.getDateTableCell(0, 0), scheduler.getDateTableCell(0, 1));
+
+  await checkSelectionWhenFocusedInViewport(t, scheduler, 2, 0, 1);
+
+  await scrollTo(1500);
+  await t
+    .expect(scheduler.dateTableCells.filter('.dx-state-focused').count)
+    .eql(0);
+
+  await scrollTo(0);
+  await checkSelectionWhenFocusedInViewport(t, scheduler, 2, 0, 1);
+}).before(() => createScheduler({
+  views: [{
+    type: 'month',
+    intervalCount: 30,
+  }],
+  currentView: 'month',
+}));
