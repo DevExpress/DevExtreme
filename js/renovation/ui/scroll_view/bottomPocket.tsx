35bf31969d74a3e0423e47b5cc9a22035698bce4
EugeniyKiyashko <johnnykiyashko@gmail.com>
2020-12-25 17:32:07 +0300

    remove unnessary parts

--- js/renovation/ui/scroll_view/bottomPocket.tsx
+++ js/renovation/ui/scroll_view/bottomPocket.tsx
@@ -1,10 +1,7 @@
 import {
   JSXComponent,
-  OneWay,
   ComponentBindings,
   Component,
-  Ref,
-  RefObject,
 } from 'devextreme-generator/component_declaration/common';
 
 import {
@@ -15,30 +12,23 @@ const SCROLLVIEW_REACHBOTTOM_CLASS = 'dx-scrollview-scrollbottom';
 const SCROLLVIEW_REACHBOTTOM_INDICATOR_CLASS = 'dx-scrollview-scrollbottom-indicator';
 const SCROLLVIEW_REACHBOTTOM_TEXT_CLASS = 'dx-scrollview-scrollbottom-text';
 
-export const viewFunction = (viewModel: BottomPocket): JSX.Element => {
-  const {
-    bottomPocketRef,
-  } = viewModel;
-
-  return (
-    <div className={SCROLLVIEW_BOTTOM_POCKET_CLASS} ref={bottomPocketRef}>
-      <div className={SCROLLVIEW_REACHBOTTOM_CLASS}>
-        <div className={SCROLLVIEW_REACHBOTTOM_INDICATOR_CLASS} />
-        <div className={SCROLLVIEW_REACHBOTTOM_TEXT_CLASS} />
-      </div>
+export const viewFunction = (): JSX.Element => (
+  <div className={SCROLLVIEW_BOTTOM_POCKET_CLASS}>
+    <div className={SCROLLVIEW_REACHBOTTOM_CLASS}>
+      <div className={SCROLLVIEW_REACHBOTTOM_INDICATOR_CLASS} />
+      <div className={SCROLLVIEW_REACHBOTTOM_TEXT_CLASS} />
     </div>
-  );
-};
+  </div>
+);
 
 @ComponentBindings()
 export class BottomPocketProps {
-  @OneWay() someProp = false;
 }
 
 @Component({
+  defaultOptionRules: null,
   view: viewFunction,
 })
 
 export class BottomPocket extends JSXComponent(BottomPocketProps) {
-  @Ref() bottomPocketRef!: RefObject<HTMLDivElement>;
 }
--- js/renovation/ui/scroll_view/topPocket.tsx
+++ js/renovation/ui/scroll_view/topPocket.tsx
@@ -1,10 +1,7 @@
 import {
   JSXComponent,
-  OneWay,
   ComponentBindings,
   Component,
-  Ref,
-  RefObject,
 } from 'devextreme-generator/component_declaration/common';
 
 import {
@@ -16,31 +13,24 @@ const SCROLLVIEW_PULLDOWN_IMAGE_CLASS = 'dx-scrollview-pull-down-image';
 const SCROLLVIEW_PULLDOWN_INDICATOR_CLASS = 'dx-scrollview-pull-down-indicator';
 const SCROLLVIEW_PULLDOWN_TEXT_CLASS = 'dx-scrollview-pull-down-text';
 
-export const viewFunction = (viewModel: TopPocket): JSX.Element => {
-  const {
-    topPocketRef,
-  } = viewModel;
-
-  return (
-    <div className={SCROLLVIEW_TOP_POCKET_CLASS} ref={topPocketRef}>
-      <div className={SCROLLVIEW_PULLDOWN}>
-        <div className={SCROLLVIEW_PULLDOWN_IMAGE_CLASS} />
-        <div className={SCROLLVIEW_PULLDOWN_INDICATOR_CLASS} />
-        <div className={SCROLLVIEW_PULLDOWN_TEXT_CLASS} />
-      </div>
+export const viewFunction = (): JSX.Element => (
+  <div className={SCROLLVIEW_TOP_POCKET_CLASS}>
+    <div className={SCROLLVIEW_PULLDOWN}>
+      <div className={SCROLLVIEW_PULLDOWN_IMAGE_CLASS} />
+      <div className={SCROLLVIEW_PULLDOWN_INDICATOR_CLASS} />
+      <div className={SCROLLVIEW_PULLDOWN_TEXT_CLASS} />
     </div>
-  );
-};
+  </div>
+);
 
 @ComponentBindings()
 export class TopPocketProps {
-  @OneWay() someProp = false;
 }
 
 @Component({
+  defaultOptionRules: null,
   view: viewFunction,
 })
 
 export class TopPocket extends JSXComponent(TopPocketProps) {
-  @Ref() topPocketRef!: RefObject<HTMLDivElement>;
 }
