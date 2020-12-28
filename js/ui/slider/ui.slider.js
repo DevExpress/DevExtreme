7e88330df4198f224f1c98084705ae3807911e26
Anton Kuznetsov <ksercs0@gmail.com>
2020-12-29 02:59:42 +0300

    Editors: resolve todos (#15874)

--- js/ui/slider/ui.slider.js
+++ js/ui/slider/ui.slider.js
@@ -10,7 +10,6 @@ import eventsEngine from '../../events/core/events_engine';
 import Swipeable from '../../events/gesture/swipeable';
 import pointerEvents from '../../events/pointer';
 import { addNamespace, isMouseEvent, isTouchEvent, eventData } from '../../events/utils/index';
-import { triggerShownEvent } from '../../events/visibility_change';
 import numberLocalization from '../../localization/number';
 import { isMaterial, current as currentTheme } from '../themes';
 import TrackBar from '../track_bar';
@@ -615,7 +614,6 @@ const Slider = TrackBar.inherit({
                 this.callBase(args);
                 this._renderHandle();
                 this._repaintHandle();
-                triggerShownEvent(this.$element()); // TODO: move firing dxshown event to Widget
                 break;
             case 'min':
             case 'max':
