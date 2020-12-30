ba27c029c0fe9511b71589b9ccb75887224a4ca3
Alexey Babich <alexey.babych@gmail.com>
2020-12-30 14:25:19 +0300

    Remove accordion from dx.common.css (#15904) (#15915)

--- testing/tests/DevExpress.ui.widgets/accordion.tests.js
+++ testing/tests/DevExpress.ui.widgets/accordion.tests.js
@@ -1,5 +1,6 @@
 import fx from 'animation/fx';
 import 'common.css!';
+import 'generic_light.css!';
 import config from 'core/config';
 import { deferUpdate, noop } from 'core/utils/common';
 import { extend } from 'core/utils/extend';
@@ -440,7 +441,7 @@ QUnit.module('widget options', moduleSetup, () => {
             assert.ok(!$item.hasClass(ACCORDION_ITEM_OPENED_CLASS), 'content is hidden before animation is started');
 
             $($title).trigger('dxclick');
-            assert.roughEqual($item.height(), $title.outerHeight(), 0.1, 'height of the item is equal to the title height');
+            assert.roughEqual($item.outerHeight(), $title.outerHeight(), 0.1, 'height of the item is equal to the title height');
             this.clock.tick(1000);
 
             assert.ok($item.height() > $title.outerHeight(), 'height is not 0 when animation is complete');
@@ -723,7 +724,7 @@ QUnit.module('widget options changed', moduleSetup, () => {
             assert.ok(!$item.hasClass(ACCORDION_ITEM_OPENED_CLASS), 'content is hidden before animation is started');
 
             $($title).trigger('dxclick');
-            assert.roughEqual($item.height(), $title.outerHeight(), 0.1, 'height of the item is equal to the title height');
+            assert.roughEqual($item.outerHeight(), $title.outerHeight(), 0.1, 'height of the item is equal to the title height');
             this.clock.tick(1000);
 
             assert.ok($item.height() > $title.outerHeight(), 'height is not 0 when animation is complete');
@@ -1359,12 +1360,12 @@ QUnit.module('optionChanged', moduleSetup, () => {
             instance.option('items[1].visible', true);
             item1 = item1GetterFunc();
             assert.strictEqual(item1.hasClass(HIDDEN_CLASS), false, 'item1 is visible');
-            assert.roughEqual(item1.height(), 21, 1.001, 'item1 has valid height');
+            assert.roughEqual(item1.outerHeight(), 43, 1.001, 'item1 has valid height');
 
             instance.option('items[1].visible', false);
             item1 = item1GetterFunc();
             assert.strictEqual(item1.hasClass(HIDDEN_CLASS), true, 'item1 is hidden');
-            assert.strictEqual(item1.height(), 0, 'item1 has zero height');
+            assert.strictEqual(item1.outerHeight(), 0, 'item1 has zero height');
         });
     });
 });
