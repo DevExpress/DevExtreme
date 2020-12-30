efb5ecf667a1064a0110430241c79343fcb7adb9
babich-a <alexey.babych@gmail.com>
2020-12-30 15:22:36 +0300

    Try return old code

--- build/gulp/gulp-data-uri.js
+++ build/gulp/gulp-data-uri.js
@@ -5,12 +5,12 @@ const path = require('path');
 const fs = require('fs');
 const sass = require('sass');
 const dataUriRegex = /data-uri\((?:'(image\/svg\+xml;charset=UTF-8)',\s)?['"]?([^)'"]+)['"]?\)/g;
-const repositoryRoot = path.join(__dirname, '..', '..');
+// const repositoryRoot = path.join(__dirname, '..', '..');
 
-const getFilePath = (fileName) => {
-    const relativePath = path.join(repositoryRoot, fileName);
-    return path.resolve(relativePath);
-};
+// const getFilePath = (fileName) => {
+//     const relativePath = path.join(repositoryRoot, fileName);
+//     return path.resolve(relativePath);
+// };
 
 const svg = (buffer, svgEncoding) => {
     const encoding = svgEncoding || 'image/svg+xml;charset=UTF-8';
@@ -24,7 +24,8 @@ const img = (buffer, ext) => {
 };
 
 const handler = (_, svgEncoding, fileName) => {
-    const filePath = getFilePath(fileName);
+    const relativePath = path.join(__dirname, '..', '..', fileName);
+    const filePath = path.resolve(relativePath);
     const ext = filePath.split('.').pop();
     const data = fs.readFileSync(filePath);
     const buffer = Buffer.from(data);
@@ -40,21 +41,21 @@ const sassFunction = (args) => {
     return new sass.types.String(handler(null, encoding, url));
 };
 
-const getImagesFromContent = (content) => {
-    const result = [];
-    let match;
+// const getImagesFromContent = (content) => {
+//     const result = [];
+//     let match;
 
-    while((match = dataUriRegex.exec(content)) !== null) {
-        const imagePath = getFilePath(match[2]);
-        result.push(imagePath);
-    }
+//     while((match = dataUriRegex.exec(content)) !== null) {
+//         const imagePath = getFilePath(match[2]);
+//         result.push(imagePath);
+//     }
 
-    return result;
-};
+//     return result;
+// };
 
 module.exports = {
     gulpPipe: () => replace(dataUriRegex, handler),
-    getImagesFromContent,
+    // getImagesFromContent,
     resolveDataUri: (content) => content.replace(dataUriRegex, handler),
     sassFunctions: {
         'data-uri($args...)': sassFunction
--- testing/styles/unused-images.test.ts
+++ testing/styles/unused-images.test.ts
@@ -1,22 +1,22 @@
-import { readFileSync } from 'fs';
-import { resolve } from 'path';
-import { sync } from 'glob';
-import { getImagesFromContent } from '../../build/gulp/gulp-data-uri';
+// import { readFileSync } from 'fs';
+// import { resolve } from 'path';
+// import { sync } from 'glob';
+// import { getImagesFromContent } from '../../build/gulp/gulp-data-uri';
 
-test('There are no unused images in repository', () => {
-  const fullImagesFileList = sync('images/widgets/**/*.*')
-    .map((fileName) => resolve(fileName).toLowerCase())
-    .sort();
+// test('There are no unused images in repository', () => {
+//   const fullImagesFileList = sync('images/widgets/**/*.*')
+//     .map((fileName) => resolve(fileName).toLowerCase())
+//     .sort();
 
-  const usedImagesFileList = sync('scss/**/*.*')
-    .map((fileName) => {
-      const fileContent = readFileSync(resolve(fileName));
-      const imageNames = getImagesFromContent(fileContent);
-      return imageNames.map((imageName) => resolve(imageName).toLowerCase());
-    })
-    .flat()
-    .filter((value, index, self) => self.indexOf(value) === index)
-    .sort();
+//   const usedImagesFileList = sync('scss/**/*.*')
+//     .map((fileName) => {
+//       const fileContent = readFileSync(resolve(fileName));
+//       const imageNames = getImagesFromContent(fileContent);
+//       return imageNames.map((imageName) => resolve(imageName).toLowerCase());
+//     })
+//     .flat()
+//     .filter((value, index, self) => self.indexOf(value) === index)
+//     .sort();
 
-  expect(fullImagesFileList).toEqual(usedImagesFileList);
-});
+//   expect(fullImagesFileList).toEqual(usedImagesFileList);
+// });
