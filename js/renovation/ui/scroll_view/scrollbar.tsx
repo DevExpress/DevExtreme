681e24495c52ebf770427819cda19df84901158c
EugeniyKiyashko <johnnykiyashko@gmail.com>
2021-01-11 17:26:06 +0300

    Scrollable - add scrollbar styles

--- js/renovation/ui/scroll_view/scrollbar.tsx
+++ js/renovation/ui/scroll_view/scrollbar.tsx
@@ -1,22 +1,29 @@
 import {
   Component,
   JSXComponent,
+  InternalState,
 } from 'devextreme-generator/component_declaration/common';
 
 import { combineClasses } from '../../utils/combine_classes';
 
 import { ScrollbarProps } from './scrollbar_props';
+import {
+  ScrollDirection,
+} from './scrollable_utils';
 
 const SCROLLABLE_SCROLLBAR_CLASS = 'dx-scrollable-scrollbar';
-const SCROLLBAR_SCROLL_CLASS = 'dx-scrollable-scroll';
-const SCROLLBAR_SCROLL_CONTENT_CLASS = 'dx-scrollable-scroll-content';
+const SCROLLABLE_SCROLL_CLASS = 'dx-scrollable-scroll';
+const SCROLLABLE_SCROLL_CONTENT_CLASS = 'dx-scrollable-scroll-content';
+
+const THUMB_MIN_SIZE = 15;
 
 export const viewFunction = (viewModel: Scrollbar): JSX.Element => {
-  const { cssClasses } = viewModel;
+  const { cssClasses, styles } = viewModel;
+
   return (
     <div className={cssClasses}>
-      <div className={SCROLLBAR_SCROLL_CLASS}>
-        <div className={SCROLLBAR_SCROLL_CONTENT_CLASS} />
+      <div className={SCROLLABLE_SCROLL_CLASS} style={styles}>
+        <div className={SCROLLABLE_SCROLL_CONTENT_CLASS} />
       </div>
     </div>
   );
@@ -27,6 +34,8 @@ export const viewFunction = (viewModel: Scrollbar): JSX.Element => {
   view: viewFunction,
 })
 export class Scrollbar extends JSXComponent<ScrollbarProps>() {
+  @InternalState() baseContainerToContentRatio = 0;
+
   get cssClasses(): string {
     const { direction } = this.props;
 
@@ -35,5 +44,28 @@ export class Scrollbar extends JSXComponent<ScrollbarProps>() {
       [`dx-scrollbar-${direction}`]: true,
     };
     return combineClasses(classesMap);
+  }
+
+  get styles(): { [key: string]: string | number } {
+    const style = this.restAttributes.style || {};
+
+    return {
+      ...style,
+      display: this.needScrollbar() ? '' : 'none',
+      [`${this.getDimension()}`]: THUMB_MIN_SIZE,
+    };
+  }
+
+  private isHidden(): boolean {
+    return this.props.visibilityMode === 'never';
+  }
+
+  private needScrollbar(): boolean {
+    return !this.isHidden() && (this.baseContainerToContentRatio < 1);
+  }
+
+  private getDimension(): string {
+    const { isHorizontal } = new ScrollDirection(this.props.direction);
+    return isHorizontal ? 'width' : 'height';
   }
 }
