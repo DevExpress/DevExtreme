279dc80fd4a628454ae6f7b4fa7ba150551875ff
Andrew Makarov <andrewmakarov@inbox.ru>
2021-01-12 19:14:41 +0300

    Add appointmentContent class

---
+++ js/ui/scheduler/appointments/appointmentContent/index.js
@@ -0,0 +1,4 @@
+import { AppointmentContent } from './appointmentContent';
+import { AgendaAppointmentContent } from './agendaAppointmentContent';
+
+export { AppointmentContent, AgendaAppointmentContent };
