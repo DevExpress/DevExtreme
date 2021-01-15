4e805fa908a786b94602124861ac59987ee51b24
MikeVitik <vitikm@gmail.com>
2021-01-15 13:17:17 +0300

    DataGrid:The paging info section is not rendered under certain conditions (T962160) (#15995)

--- js/renovation/ui/pager/utils/get_element_width.ts
+++ js/renovation/ui/pager/utils/get_element_width.ts
@@ -1,9 +1,14 @@
 import getElementComputedStyle from '../../../utils/get_computed_style';
 import { toNumber } from '../../../utils/type_conversion';
 
+export function getElementStyle(name: keyof CSSStyleDeclaration, element?: Element): number {
+  const computedStyle = getElementComputedStyle(element) || {};
+  return toNumber(computedStyle[name]);
+}
+
 export function getElementWidth(element: Element | undefined): number {
-  return toNumber(getElementComputedStyle(element)?.width);
+  return getElementStyle('width', element);
 }
 export function getElementMinWidth(element: Element | undefined): number {
-  return toNumber(getElementComputedStyle(element)?.minWidth);
+  return getElementStyle('minWidth', element);
 }
