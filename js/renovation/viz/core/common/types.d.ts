41e361f36513c4594a0f4a9e6fa076fb3e83f6d8
Krijovnick <yulianabest7@yandex.ru>
2021-01-18 16:26:23 +0300

    fix

--- js/renovation/viz/core/common/types.d.ts
+++ js/renovation/viz/core/common/types.d.ts
@@ -9,9 +9,7 @@ export type Margin = { top?: number; left?: number; bottom?: number; right?: num
 export type InitialBorder = { color: string; width: number; dashStyle: string; opacity?: number; visible: boolean };
 export type Border = { stroke?: string, strokeWidth?: number, strokeOpacity?: number, dashStyle?: string };
 export type CustomizedOptions = { text?: string | null, html?: string | null, color?: string, borderColor?: string, fontColor?: string };
-export type CustomizeTooltipFn = (info: any) => {
-    text?: string | null; html?: string | null; color?: string; borderColor?: string; fontColor?: string;
-};
+export type CustomizeTooltipFn = (info: any) => CustomizedOptions;
 export type Location = 'center' | 'edge';
 export interface TooltipData {
   value?: number | Date | string,
