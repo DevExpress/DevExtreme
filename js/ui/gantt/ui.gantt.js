5f9df949b3aee470d6df9bdc1a37382dd613258c
kobilinsky.artem <artem.kobilinsky@devexpress.com>
2021-01-18 18:41:20 +0300

    dxGantt: fix tests

--- js/ui/gantt/ui.gantt.js
+++ js/ui/gantt/ui.gantt.js
@@ -78,6 +78,10 @@ class Gantt extends Widget {
         this._refreshDataSource(GANTT_RESOURCE_ASSIGNMENTS);
     }
 
+    _refresh() {
+        this._isGanttRendered = false;
+        super._refresh();
+    }
     _renderContent() {
         this._isMainElementVisible = this.$element().is(':visible');
         if(this._isMainElementVisible && !this._isGanttRendered) {
@@ -1357,10 +1361,6 @@ class Gantt extends Widget {
     }
     updateDimensions() {
         this._setInnerElementsWidth();
-    }
-    repaint() {
-        this._isGanttRendered = false;
-        super.repaint();
     }
 
     _optionChanged(args) {
