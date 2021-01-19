0d0c59d95ff2b518ea349ad32de1d9e4b8a77c7c
churkin <alam17@mail.ru>
2021-01-19 14:31:38 +0300

    Tree Shaking: Optimize esm source code

--- build/gulp/transpile-config.js
+++ build/gulp/transpile-config.js
@@ -20,9 +20,13 @@ module.exports = {
     }),
 
     esm: Object.assign({}, common, {
-        presets: [['@babel/preset-env', { modules: false }]],
+        // eslint-disable-next-line spellcheck/spell-checker
+        presets: [['@babel/preset-env', { modules: false, targets: { esmodules: true } }]],
         plugins: common.plugins.concat(
-            [['@babel/plugin-transform-runtime', { useESModules: true }]]
+            [['@babel/plugin-transform-runtime', {
+                useESModules: true,
+                version: '7.5.0' // https://github.com/babel/babel/issues/10261#issuecomment-514687857
+            }]]
         )
     })
 };
