4127375b1047bdcf3afa9776ed34a5dc5cde36dc
Artem Kobilinskii <a.kobilinsky@gmail.com>
2021-01-19 10:05:03 +0300

    dxGantt: fix gantt render in hidden container (T958809) (#16046)
    * dxGantt: Fix FullScreen issue (T960368)
    
    * dxGantt: fix tests
    
    * dxGantt: fix tests
    
    * dxGantt: fix tests
    
    * dxGantt: fix tests
    
    * dxGantt: remove unused parameter
    
    * dxGantt: remove unused parameter
    
    * dxGantt: fix tests
    
    * dxGantt: fix gantt render in hidden container (T958809)
    
    * dxGantt: fix tests
    
    Co-authored-by: kobilinsky.artem <artem.kobilinsky@devexpress.com>

--- testing/tests/DevExpress.ui.widgets/gantt.tests.js
+++ testing/tests/DevExpress.ui.widgets/gantt.tests.js
@@ -3050,3 +3050,53 @@ QUnit.module('FullScreen Mode', moduleConfig, () => {
         fullScreenCommand.execute();
     });
 });
+
+QUnit.module('Repaint', moduleConfig, () => {
+    test('should render treeList after repaint()', function(assert) {
+        this.createInstance(tasksOnlyOptions);
+        this.clock.tick();
+        this.instance.repaint();
+        this.clock.tick();
+        const treeListElements = this.$element.find(TREELIST_SELECTOR);
+        assert.strictEqual(treeListElements.length, 1);
+    });
+    test('should render task wrapper for each task after repaint()', function(assert) {
+        this.createInstance(allSourcesOptions);
+        this.clock.tick();
+        this.instance.repaint();
+        this.clock.tick();
+        const elements = this.$element.find(TASK_WRAPPER_SELECTOR);
+        assert.equal(elements.length, tasks.length - 1);
+    });
+    test('should store task changes after repaint() ', function(assert) {
+        this.createInstance(allSourcesOptions);
+        this.instance.option('editing.enabled', true);
+        this.instance.option('selectedRowKey', 1);
+        this.clock.tick();
+        showTaskEditDialog(this.instance);
+        this.clock.tick();
+        const $dialog = $('body').find(POPUP_SELECTOR);
+        assert.equal($dialog.length, 1, 'dialog is shown');
+
+        const $inputs = $dialog.find(INPUT_TEXT_EDITOR_SELECTOR);
+        assert.equal($inputs.eq(0).val(), tasks[0].title, 'title text is shown');
+        assert.equal((new Date($inputs.eq(1).val())).getTime(), tasks[0].start.getTime(), 'start task text is shown');
+        assert.equal((new Date($inputs.eq(2).val())).getTime(), tasks[0].end.getTime(), 'end task text is shown');
+        assert.equal($inputs.eq(3).val(), tasks[0].progress + '%', 'progress text is shown');
+
+        const testTitle = 'text';
+        const titleTextBox = $dialog.find('.dx-textbox').eq(0).dxTextBox('instance');
+        titleTextBox.option('value', testTitle);
+        const $okButton = $dialog.find('.dx-popup-bottom').find('.dx-button').eq(0);
+        $okButton.trigger('dxclick');
+        this.clock.tick();
+        let firstTreeListTitleText = this.$element.find(TREELIST_DATA_ROW_SELECTOR).first().find('td').eq(2).text();
+        assert.equal(firstTreeListTitleText, testTitle, 'title text was modified');
+
+        this.instance.repaint();
+        this.clock.tick();
+        firstTreeListTitleText = this.$element.find(TREELIST_DATA_ROW_SELECTOR).first().find('td').eq(2).text();
+        assert.equal(firstTreeListTitleText, testTitle, 'title text is the same after repaint()');
+    });
+});
+
