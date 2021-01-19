9f0cce1fe1353a65600981b246330b237203d863
Evgeniy Aryamnov <earyamnow@mail.ru>
2021-01-19 17:09:40 +0300

    Add test for group-panel

--- js/renovation/ui/scheduler/workspaces/base/group_panel/__tests__/group_panel.test.tsx
+++ js/renovation/ui/scheduler/workspaces/base/group_panel/__tests__/group_panel.test.tsx
@@ -76,66 +76,67 @@ describe('GroupPanel Vertical Layout', () => {
       });
 
       describe('groupsRenderData', () => {
-        it('should transform grouping data into group items', () => {
-          const groups = [{
-            name: 'group 1',
-            items: [{
-              text: 'item 1', id: 1, color: 'color 1',
-            }, {
-              text: 'item 2', id: 2, color: 'color 2',
-            }],
-            data: [{
-              text: 'item 1', id: 1, color: 'color 1',
-            }, {
-              text: 'item 2', id: 2, color: 'color 2',
-            }],
+        const groupsBase = [{
+          name: 'group 1',
+          items: [{
+            text: 'item 1', id: 1, color: 'color 1',
           }, {
-            name: 'group 2',
-            items: [{
-              text: 'item 3', id: 1, color: 'color 3',
-            }, {
-              text: 'item 4', id: 2, color: 'color 4',
-            }],
-            data: [{
-              text: 'item 3', id: 1, color: 'color 3',
-            }, {
-              text: 'item 4', id: 2, color: 'color 4',
-            }],
-          }];
+            text: 'item 2', id: 2, color: 'color 2',
+          }],
+          data: [{
+            text: 'item 1', id: 1, color: 'color 1',
+          }, {
+            text: 'item 2', id: 2, color: 'color 2',
+          }],
+        }, {
+          name: 'group 2',
+          items: [{
+            text: 'item 3', id: 1, color: 'color 3',
+          }, {
+            text: 'item 4', id: 2, color: 'color 4',
+          }],
+          data: [{
+            text: 'item 3', id: 1, color: 'color 3',
+          }, {
+            text: 'item 4', id: 2, color: 'color 4',
+          }],
+        }];
+
+        it('should transform grouping data into group items', () => {
           const groupPanel = new GroupPanel({
-            groups,
+            groups: groupsBase,
           });
 
           expect(groupPanel.groupsRenderData)
             .toEqual([[{
-              ...groups[0].items[0],
-              data: groups[0].data[0],
-              resourceName: groups[0].name,
+              ...groupsBase[0].items[0],
+              data: groupsBase[0].data[0],
+              resourceName: groupsBase[0].name,
               key: '0_group 1_1',
             }, {
-              ...groups[0].items[1],
-              data: groups[0].data[1],
-              resourceName: groups[0].name,
+              ...groupsBase[0].items[1],
+              data: groupsBase[0].data[1],
+              resourceName: groupsBase[0].name,
               key: '0_group 1_2',
             }], [{
-              ...groups[1].items[0],
-              data: groups[1].data[0],
-              resourceName: groups[1].name,
+              ...groupsBase[1].items[0],
+              data: groupsBase[1].data[0],
+              resourceName: groupsBase[1].name,
               key: '0_group 2_1',
             }, {
-              ...groups[1].items[1],
-              data: groups[1].data[1],
-              resourceName: groups[1].name,
+              ...groupsBase[1].items[1],
+              data: groupsBase[1].data[1],
+              resourceName: groupsBase[1].name,
               key: '0_group 2_2',
             }, {
-              ...groups[1].items[0],
-              data: groups[1].data[0],
-              resourceName: groups[1].name,
+              ...groupsBase[1].items[0],
+              data: groupsBase[1].data[0],
+              resourceName: groupsBase[1].name,
               key: '1_group 2_1',
             }, {
-              ...groups[1].items[1],
-              data: groups[1].data[1],
-              resourceName: groups[1].name,
+              ...groupsBase[1].items[1],
+              data: groupsBase[1].data[1],
+              resourceName: groupsBase[1].name,
               key: '1_group 2_2',
             }]]);
         });
@@ -148,7 +149,7 @@ describe('GroupPanel Vertical Layout', () => {
             }, {
               text: 'item 2', id: 2, color: 'color 2',
             }],
-          }];
+          }] as any;
           const groupPanel = new GroupPanel({
             groups,
           });
@@ -162,6 +163,101 @@ describe('GroupPanel Vertical Layout', () => {
               ...groups[0].items[1],
               resourceName: groups[0].name,
               key: '0_group 1_2',
+            }]]);
+        });
+
+        it('should transform grouping data into group items corectly when appointments are groupped by date', () => {
+          const groupPanel = new GroupPanel({
+            groups: groupsBase,
+            groupByDate: true,
+            columnCountPerGroup: 2,
+          });
+
+          expect(groupPanel.groupsRenderData)
+            .toEqual([[{
+              ...groupsBase[0].items[0],
+              data: groupsBase[0].data[0],
+              resourceName: groupsBase[0].name,
+              key: '0_group 1_1_group_by_date_0',
+              isFirstGroupCell: true,
+              isLastGroupCell: false,
+            }, {
+              ...groupsBase[0].items[1],
+              data: groupsBase[0].data[1],
+              resourceName: groupsBase[0].name,
+              key: '0_group 1_2_group_by_date_0',
+              isFirstGroupCell: false,
+              isLastGroupCell: true,
+            }, {
+              ...groupsBase[0].items[0],
+              data: groupsBase[0].data[0],
+              resourceName: groupsBase[0].name,
+              key: '0_group 1_1_group_by_date_1',
+              isFirstGroupCell: true,
+              isLastGroupCell: false,
+            }, {
+              ...groupsBase[0].items[1],
+              data: groupsBase[0].data[1],
+              resourceName: groupsBase[0].name,
+              key: '0_group 1_2_group_by_date_1',
+              isFirstGroupCell: false,
+              isLastGroupCell: true,
+            }], [{
+              ...groupsBase[1].items[0],
+              data: groupsBase[1].data[0],
+              resourceName: groupsBase[1].name,
+              key: '0_group 2_1_group_by_date_0',
+              isFirstGroupCell: true,
+              isLastGroupCell: false,
+            }, {
+              ...groupsBase[1].items[1],
+              data: groupsBase[1].data[1],
+              resourceName: groupsBase[1].name,
+              key: '0_group 2_2_group_by_date_0',
+              isFirstGroupCell: false,
+              isLastGroupCell: false,
+            }, {
+              ...groupsBase[1].items[0],
+              data: groupsBase[1].data[0],
+              resourceName: groupsBase[1].name,
+              key: '1_group 2_1_group_by_date_0',
+              isFirstGroupCell: false,
+              isLastGroupCell: false,
+            }, {
+              ...groupsBase[1].items[1],
+              data: groupsBase[1].data[1],
+              resourceName: groupsBase[1].name,
+              key: '1_group 2_2_group_by_date_0',
+              isFirstGroupCell: false,
+              isLastGroupCell: true,
+            }, {
+              ...groupsBase[1].items[0],
+              data: groupsBase[1].data[0],
+              resourceName: groupsBase[1].name,
+              key: '0_group 2_1_group_by_date_1',
+              isFirstGroupCell: true,
+              isLastGroupCell: false,
+            }, {
+              ...groupsBase[1].items[1],
+              data: groupsBase[1].data[1],
+              resourceName: groupsBase[1].name,
+              key: '0_group 2_2_group_by_date_1',
+              isFirstGroupCell: false,
+              isLastGroupCell: false,
+            }, {
+              ...groupsBase[1].items[0],
+              data: groupsBase[1].data[0],
+              resourceName: groupsBase[1].name,
+              key: '1_group 2_1_group_by_date_1',
+              isFirstGroupCell: false,
+              isLastGroupCell: false,
+            }, {
+              ...groupsBase[1].items[1],
+              data: groupsBase[1].data[1],
+              resourceName: groupsBase[1].name,
+              key: '1_group 2_2_group_by_date_1',
+              isFirstGroupCell: false,
+              isLastGroupCell: true,
             }]]);
         });
       });
