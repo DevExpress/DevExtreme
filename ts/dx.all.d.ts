c215146f0afd581f4202c7b77e6057e3e9f02552
Andrew Makarov <andrewmakarov@inbox.ru>
2021-01-19 20:04:23 +0300

    Cherry pick

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -10498,7 +10498,7 @@ declare module DevExpress.ui {
         /**
          * [descr:dxSchedulerAppointment.endDate]
          */
-        endDate?: Date;
+        endDate?: Date | String;
         /**
          * [descr:dxSchedulerAppointment.endDateTimeZone]
          */
@@ -10518,7 +10518,7 @@ declare module DevExpress.ui {
         /**
          * [descr:dxSchedulerAppointment.startDate]
          */
-        startDate?: Date;
+        startDate?: Date | String;
         /**
          * [descr:dxSchedulerAppointment.startDateTimeZone]
          */
