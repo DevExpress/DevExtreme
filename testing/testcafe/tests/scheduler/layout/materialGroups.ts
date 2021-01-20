d0ef0a4a783c1e48438cffe3063495e994a0bde1
Andrew Makarov <andrewmakarov@inbox.ru>
2021-01-21 00:43:09 +0300

    CreateDataSource function move to utils.ts

--- testing/testcafe/tests/scheduler/layout/materialGroups.ts
+++ testing/testcafe/tests/scheduler/layout/materialGroups.ts
@@ -1,44 +1,10 @@
 import { compareScreenshot } from '../../../helpers/screenshot-comparer';
 import createWidget from '../../../helpers/createWidget';
 import url from '../../../helpers/getPageUrl';
+import { createDataSetForScreenShotTests } from './utils';
 
 fixture`Scheduler: Material theme layout`
   .page(url(__dirname, './material.html'));
-
-const createDataSetForScreenShotTests = () => {
-  const result: {
-    text: string;
-    startDate: Date;
-    endDate: Date;
-    priorityId: number;
-    allDay?: boolean;
-  }[] = [];
-  // eslint-disable-next-line no-plusplus
-  for (let day = 1; day < 25; day++) {
-    result.push({
-      text: '1 appointment',
-      startDate: new Date(2020, 6, day, 0),
-      endDate: new Date(2020, 6, day, 1),
-      priorityId: 0,
-    });
-
-    result.push({
-      text: '2 appointment',
-      startDate: new Date(2020, 6, day, 1),
-      endDate: new Date(2020, 6, day, 2),
-      priorityId: 1,
-    });
-
-    result.push({
-      text: '3 appointment',
-      startDate: new Date(2020, 6, day, 3),
-      endDate: new Date(2020, 6, day, 5),
-      allDay: true,
-      priorityId: 0,
-    });
-  }
-  return result;
-};
 
 const createScheduler = async (view: string, groupOrientation: string) => {
   await createWidget('dxScheduler', {
