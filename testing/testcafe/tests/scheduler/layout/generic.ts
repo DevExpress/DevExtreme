d6e30ee9e460058473476d90c00a98e8b6322417
Andrew Makarov <andrewmakarov@inbox.ru>
2021-01-20 13:29:20 +0300

    Revert tests

---
+++ testing/testcafe/tests/scheduler/layout/generic.ts
@@ -0,0 +1,96 @@
+import { compareScreenshot } from '../../../helpers/screenshort-comparer';
+import createWidget from '../../../helpers/createWidget';
+import url from '../../../helpers/getPageUrl';
+import Scheduler from '../../../model/scheduler';
+
+fixture`Scheduler: Generic theme layout`
+  .page(url(__dirname, '../../container.html'));
+
+const createDataSetForScreenShotTests = () => {
+  const result: {
+    text: string;
+    startDate: Date;
+    endDate: Date;
+    priorityId: number;
+    allDay?: boolean;
+  }[] = [];
+    // eslint-disable-next-line no-plusplus
+  for (let day = 1; day < 25; day++) {
+    result.push({
+      text: '1 appointment',
+      startDate: new Date(2020, 6, day, 0),
+      endDate: new Date(2020, 6, day, 1),
+      priorityId: 0,
+    });
+
+    result.push({
+      text: '2 appointment',
+      startDate: new Date(2020, 6, day, 1),
+      endDate: new Date(2020, 6, day, 2),
+      priorityId: 1,
+    });
+
+    result.push({
+      text: '3 appointment',
+      startDate: new Date(2020, 6, day, 3),
+      endDate: new Date(2020, 6, day, 5),
+      allDay: true,
+      priorityId: 0,
+    });
+  }
+
+  return result;
+};
+
+const createScheduler = async (view: string, resourcesValue?: unknown[]) => {
+  await createWidget('dxScheduler', {
+    dataSource: createDataSetForScreenShotTests(),
+    currentDate: new Date(2020, 6, 15),
+    views: [view],
+    currentView: view,
+    resources: resourcesValue,
+    height: 600,
+  }, true);
+};
+
+const resources = [{
+  fieldExpr: 'priorityId',
+  dataSource: [
+    {
+      text: 'Low Priority',
+      id: 0,
+      color: '#24ff50',
+    }, {
+      text: 'High Priority',
+      id: 1,
+      color: '#ff9747',
+    },
+  ],
+  label: 'Priority',
+}];
+
+[undefined, resources].forEach((resourcesValue) => {
+  ['agenda', 'day', 'week', 'workWeek', 'month'].forEach((view) => {
+    test(`Base views layout test in generic theme with resources(view='${view})', resource=${!!resourcesValue}`, async (t) => {
+      const scheduler = new Scheduler('#container');
+
+      await t.click(scheduler.getAppointment('1 appointment', 0).element, { speed: 0.5 });
+      await t.expect(scheduler.appointmentTooltip.isVisible()).ok();
+
+      await t.expect(await compareScreenshot(t, `generic-layout-with-resource(view=${view}-resource=${!!resourcesValue}).png`)).ok();
+    }).before(() => createScheduler(view, resourcesValue));
+  });
+});
+
+[undefined, resources].forEach((resourcesValue) => {
+  ['timelineDay', 'timelineWeek', 'timelineWorkWeek', 'timelineMonth'].forEach((view) => {
+    test(`Timeline views layout test in generic theme with resources(view='${view})', resource=${!!resourcesValue}`, async (t) => {
+      const scheduler = new Scheduler('#container');
+
+      await t.click(scheduler.getAppointment('1 appointment', 0).element, { speed: 0.5 });
+      await t.expect(scheduler.appointmentTooltip.isVisible()).ok();
+
+      await t.expect(await compareScreenshot(t, `generic-layout-with-resource(view=${view}-resource=${!!resourcesValue}).png`)).ok();
+    }).before(() => createScheduler(view, resourcesValue));
+  });
+});
