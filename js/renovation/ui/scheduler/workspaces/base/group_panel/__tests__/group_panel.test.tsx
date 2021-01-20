9fe7957e8a8b8d4fa97ec452676b4c32462a9888
Evgeniy Aryamnov <earyamnow@mail.ru>
2021-01-20 09:32:50 +0300

    Transfer getGroupsRenderedData to utils

--- js/renovation/ui/scheduler/workspaces/base/group_panel/__tests__/group_panel.test.tsx
+++ js/renovation/ui/scheduler/workspaces/base/group_panel/__tests__/group_panel.test.tsx
@@ -6,6 +6,11 @@ import {
 import { GroupPanelVerticalLayout } from '../vertical/layout';
 import { VERTICAL_GROUP_ORIENTATION, HORIZONTAL_GROUP_ORIENTATION } from '../../../../consts';
 import { GroupPanelHorizontalLayout } from '../horizontal/layout';
+import { getGroupsRenderData } from '../utils';
+
+jest.mock('../utils', () => ({
+  getGroupsRenderData: jest.fn(() => 'data'),
+}));
 
 describe('GroupPanel Vertical Layout', () => {
   describe('Render', () => {
@@ -75,190 +80,21 @@ describe('GroupPanel Vertical Layout', () => {
         });
       });
 
-      describe('groupsRenderData', () => {
-        const groupsBase = [{
-          name: 'group 1',
-          items: [{
-            text: 'item 1', id: 1, color: 'color 1',
-          }, {
-            text: 'item 2', id: 2, color: 'color 2',
-          }],
-          data: [{
-            text: 'item 1', id: 1, color: 'color 1',
-          }, {
-            text: 'item 2', id: 2, color: 'color 2',
-          }],
-        }, {
-          name: 'group 2',
-          items: [{
-            text: 'item 3', id: 1, color: 'color 3',
-          }, {
-            text: 'item 4', id: 2, color: 'color 4',
-          }],
-          data: [{
-            text: 'item 3', id: 1, color: 'color 3',
-          }, {
-            text: 'item 4', id: 2, color: 'color 4',
-          }],
-        }];
-
-        it('should transform grouping data into group items', () => {
-          const groupPanel = new GroupPanel({
-            groups: groupsBase,
-          });
-
-          expect(groupPanel.groupsRenderData)
-            .toEqual([[{
-              ...groupsBase[0].items[0],
-              data: groupsBase[0].data[0],
-              resourceName: groupsBase[0].name,
-              key: '0_group 1_1',
-            }, {
-              ...groupsBase[0].items[1],
-              data: groupsBase[0].data[1],
-              resourceName: groupsBase[0].name,
-              key: '0_group 1_2',
-            }], [{
-              ...groupsBase[1].items[0],
-              data: groupsBase[1].data[0],
-              resourceName: groupsBase[1].name,
-              key: '0_group 2_1',
-            }, {
-              ...groupsBase[1].items[1],
-              data: groupsBase[1].data[1],
-              resourceName: groupsBase[1].name,
-              key: '0_group 2_2',
-            }, {
-              ...groupsBase[1].items[0],
-              data: groupsBase[1].data[0],
-              resourceName: groupsBase[1].name,
-              key: '1_group 2_1',
-            }, {
-              ...groupsBase[1].items[1],
-              data: groupsBase[1].data[1],
-              resourceName: groupsBase[1].name,
-              key: '1_group 2_2',
-            }]]);
-        });
-
-        it('should work when data parameter is undefined', () => {
-          const groups = [{
-            name: 'group 1',
-            items: [{
-              text: 'item 1', id: 1, color: 'color 1',
-            }, {
-              text: 'item 2', id: 2, color: 'color 2',
-            }],
-          }] as any;
+      describe('groupsRenderedData', () => {
+        it('should call getGroupsRenderedData with correct parameters', () => {
+          const groups = [];
           const groupPanel = new GroupPanel({
             groups,
-          });
-
-          expect(groupPanel.groupsRenderData)
-            .toEqual([[{
-              ...groups[0].items[0],
-              resourceName: groups[0].name,
-              key: '0_group 1_1',
-            }, {
-              ...groups[0].items[1],
-              resourceName: groups[0].name,
-              key: '0_group 1_2',
-            }]]);
-        });
-
-        it('should transform grouping data into group items corectly when appointments are groupped by date', () => {
-          const groupPanel = new GroupPanel({
-            groups: groupsBase,
             groupByDate: true,
-            columnCountPerGroup: 2,
+            columnCountPerGroup: 72,
           });
 
           expect(groupPanel.groupsRenderData)
-            .toEqual([[{
-              ...groupsBase[0].items[0],
-              data: groupsBase[0].data[0],
-              resourceName: groupsBase[0].name,
-              key: '0_group 1_1_group_by_date_0',
-              isFirstGroupCell: true,
-              isLastGroupCell: false,
-            }, {
-              ...groupsBase[0].items[1],
-              data: groupsBase[0].data[1],
-              resourceName: groupsBase[0].name,
-              key: '0_group 1_2_group_by_date_0',
-              isFirstGroupCell: false,
-              isLastGroupCell: true,
-            }, {
-              ...groupsBase[0].items[0],
-              data: groupsBase[0].data[0],
-              resourceName: groupsBase[0].name,
-              key: '0_group 1_1_group_by_date_1',
-              isFirstGroupCell: true,
-              isLastGroupCell: false,
-            }, {
-              ...groupsBase[0].items[1],
-              data: groupsBase[0].data[1],
-              resourceName: groupsBase[0].name,
-              key: '0_group 1_2_group_by_date_1',
-              isFirstGroupCell: false,
-              isLastGroupCell: true,
-            }], [{
-              ...groupsBase[1].items[0],
-              data: groupsBase[1].data[0],
-              resourceName: groupsBase[1].name,
-              key: '0_group 2_1_group_by_date_0',
-              isFirstGroupCell: true,
-              isLastGroupCell: false,
-            }, {
-              ...groupsBase[1].items[1],
-              data: groupsBase[1].data[1],
-              resourceName: groupsBase[1].name,
-              key: '0_group 2_2_group_by_date_0',
-              isFirstGroupCell: false,
-              isLastGroupCell: false,
-            }, {
-              ...groupsBase[1].items[0],
-              data: groupsBase[1].data[0],
-              resourceName: groupsBase[1].name,
-              key: '1_group 2_1_group_by_date_0',
-              isFirstGroupCell: false,
-              isLastGroupCell: false,
-            }, {
-              ...groupsBase[1].items[1],
-              data: groupsBase[1].data[1],
-              resourceName: groupsBase[1].name,
-              key: '1_group 2_2_group_by_date_0',
-              isFirstGroupCell: false,
-              isLastGroupCell: true,
-            }, {
-              ...groupsBase[1].items[0],
-              data: groupsBase[1].data[0],
-              resourceName: groupsBase[1].name,
-              key: '0_group 2_1_group_by_date_1',
-              isFirstGroupCell: true,
-              isLastGroupCell: false,
-            }, {
-              ...groupsBase[1].items[1],
-              data: groupsBase[1].data[1],
-              resourceName: groupsBase[1].name,
-              key: '0_group 2_2_group_by_date_1',
-              isFirstGroupCell: false,
-              isLastGroupCell: false,
-            }, {
-              ...groupsBase[1].items[0],
-              data: groupsBase[1].data[0],
-              resourceName: groupsBase[1].name,
-              key: '1_group 2_1_group_by_date_1',
-              isFirstGroupCell: false,
-              isLastGroupCell: false,
-            }, {
-              ...groupsBase[1].items[1],
-              data: groupsBase[1].data[1],
-              resourceName: groupsBase[1].name,
-              key: '1_group 2_2_group_by_date_1',
-              isFirstGroupCell: false,
-              isLastGroupCell: true,
-            }]]);
+            .toBe('data');
+          expect(getGroupsRenderData)
+            .toBeCalledWith(
+              groups, 72, true,
+            );
         });
       });
     });
--- js/renovation/ui/scheduler/workspaces/base/group_panel/group_panel.tsx
+++ js/renovation/ui/scheduler/workspaces/base/group_panel/group_panel.tsx
@@ -8,56 +8,8 @@ import { GroupPanelProps } from './group_panel_props';
 import { GroupPanelVerticalLayout } from './vertical/layout';
 import { GroupPanelHorizontalLayout } from './horizontal/layout';
 import { GroupPanelLayoutProps } from './group_panel_layout_props';
-import {
-  GroupRenderItem,
-  Group,
-  GroupItem,
-} from '../../types.d';
-
-const getGroupsRenderData = (
-  groups: Group[],
-  columnCountPerGroup: number,
-  groupByDate: boolean,
-): GroupRenderItem[][] => {
-  let repeatCount = 1;
-  let groupRenderItems = groups.map((group: Group) => {
-    const result = [] as GroupRenderItem[];
-    const { name: resourceName, items, data } = group;
-
-    for (let i = 0; i < repeatCount; i += 1) {
-      result.push(...items.map(({ id, text, color }: GroupItem, index: number) => ({
-        id,
-        text,
-        color,
-        key: `${i}_${resourceName}_${id}`,
-        resourceName,
-        data: data?.[index],
-      })));
-    }
-
-    repeatCount *= items.length;
-    return result;
-  });
-
-  if (groupByDate) {
-    groupRenderItems = [...(new Array(columnCountPerGroup))]
-      .reduce((currentGroupItems, _, index) => groupRenderItems.map((groupsRow, rowIndex) => {
-        const currentRow = currentGroupItems[rowIndex] || [];
-
-        return [
-          ...currentRow,
-          ...groupsRow.map((item, columnIndex) => ({
-            ...item,
-            key: `${item.key}_group_by_date_${index}`,
-            isFirstGroupCell: columnIndex === 0,
-            isLastGroupCell: columnIndex === groupsRow.length - 1,
-          })),
-        ];
-      }), []);
-  }
-
-  return groupRenderItems;
-};
+import { GroupRenderItem } from '../../types.d';
+import { getGroupsRenderData } from './utils';
 
 export const viewFunction = ({
   layout: Layout,
