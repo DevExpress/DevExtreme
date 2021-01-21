24dea633d99b4790aaa0ae6a334876b98c8afac9
EugeniyKiyashko <johnnykiyashko@gmail.com>
2021-01-21 17:17:22 +0300

    remove unnessary any type

--- js/renovation/ui/scroll_view/scrollbar.tsx
+++ js/renovation/ui/scroll_view/scrollbar.tsx
@@ -43,7 +43,7 @@ export const viewFunction = (viewModel: Scrollbar): JSX.Element => {
 
   return (
     <Widget
-      rootElementRef={scrollbarRef as any}
+      rootElementRef={scrollbarRef}
       classes={cssClasses}
       activeStateEnabled={activeStateEnabled}
       hoverStateEnabled={hoverStateEnabled}
