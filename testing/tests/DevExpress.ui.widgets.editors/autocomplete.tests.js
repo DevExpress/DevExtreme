a4474778cc58ff6f9970df42fb8ea327620198f6
ksercs <ksercs0@gmail.com>
2021-01-22 15:40:27 +0300

    add tests for Autocomplete & fix bug

--- testing/tests/DevExpress.ui.widgets.editors/autocomplete.tests.js
+++ testing/tests/DevExpress.ui.widgets.editors/autocomplete.tests.js
@@ -45,6 +45,7 @@ QUnit.testStart(() => {
 const TEXTEDITOR_INPUT_CLASS = 'dx-texteditor-input';
 const LIST_CLASS = 'dx-list';
 const LIST_ITEM_CLASS = 'dx-list-item';
+const CLEAR_BUTTON_AREA_SELECTOR = '.dx-clear-button-area';
 const FOCUSED_STATE_SELECTOR = '.dx-state-focused';
 const KEY_DOWN = 'ArrowDown';
 const KEY_UP = 'ArrowUp';
@@ -1519,23 +1520,6 @@ QUnit.module('regressions', {
         assert.strictEqual(selectionChangedStub.lastCall.args[0].selectedItem, null);
     });
 
-    QUnit.test('clear button should save valueChangeEvent', function(assert) {
-        const valueChangedHandler = sinon.spy();
-
-        this.instance.option({
-            items: ['item 1'],
-            value: 'item 1',
-            onValueChanged: valueChangedHandler,
-            showClearButton: true
-        });
-
-        const $clearButton = this.element.find('.dx-clear-button-area');
-        $clearButton.trigger('dxclick');
-
-        assert.equal(valueChangedHandler.callCount, 1, 'valueChangedHandler has been called');
-        assert.equal(valueChangedHandler.getCall(0).args[0].event.type, 'dxclick', 'event is correct');
-    });
-
     QUnit.test('item initialization scenario', function(assert) {
         const instance = $('#autocomplete').dxAutocomplete({
             items: ['a', 'b', 'c']
@@ -1636,26 +1620,40 @@ QUnit.module('widget sizing render', {
     });
 });
 
-QUnit.module('correct event should be passed to valueChanged', {
+QUnit.module('valueChanged should receive correct event', {
     beforeEach: function() {
         fx.off = true;
-        this.valueChangedHandler = sinon.spy();
         this.clock = sinon.useFakeTimers();
+
+        this.handler = sinon.stub();
         this.$element = $('#widget').dxAutocomplete({
             items: ['11', '22'],
             searchTimeout: 0,
-            onValueChanged: this.valueChangedHandler
+            onValueChanged: this.handler,
+            valueChangeEvent: ''
         });
         this.instance = this.$element.dxAutocomplete('instance');
         this.$input = this.$element.find(`.${TEXTEDITOR_INPUT_CLASS}`);
         this.keyboard = keyboardMock(this.$input);
         this.getListItems = () => $(`.${LIST_ITEM_CLASS}`);
+
+        this.testProgramChange = (assert) => {
+            this.instance.option('value', 'new value');
+
+            const callCount = this.handler.callCount;
+            const event = this.handler.getCall(callCount - 1).args[0].event;
+            assert.strictEqual(event, undefined, 'event is undefined');
+        };
     },
     afterEach: function() {
         fx.off = false;
         this.clock.restore();
     }
 }, () => {
+    QUnit.test('on program change', function(assert) {
+        this.testProgramChange(assert);
+    });
+
     QUnit.test('on item select using click (T963574)', function(assert) {
         this.keyboard.type('1');
 
@@ -1663,22 +1661,11 @@ QUnit.module('correct event should be passed to valueChanged', {
         const $firstItem = $listItems.eq(0);
         $firstItem.trigger('dxclick');
 
-        const valueChangedEvent = this.valueChangedHandler.getCall(1).args[0].event;
-        assert.strictEqual(valueChangedEvent.type, 'dxclick', 'event type is correct');
-        assert.strictEqual(valueChangedEvent.target, $firstItem.get(0), 'event target is correct');
-    });
+        const event = this.handler.getCall(0).args[0].event;
+        assert.strictEqual(event.type, 'dxclick', 'event type is correct');
+        assert.strictEqual(event.target, $firstItem.get(0), 'event target is correct');
 
-    QUnit.test('on runtime change after item select using click', function(assert) {
-        this.keyboard.type('1');
-
-        const $listItems = this.getListItems();
-        const $firstItem = $listItems.eq(0);
-        $firstItem.trigger('dxclick');
-
-        this.instance.option('value', '33');
-
-        const valueChangedEvent = this.valueChangedHandler.getCall(2).args[0].event;
-        assert.strictEqual(valueChangedEvent, undefined, 'event is undefined');
+        this.testProgramChange(assert);
     });
 
     QUnit.test('on item select using enter', function(assert) {
@@ -1690,20 +1677,93 @@ QUnit.module('correct event should be passed to valueChanged', {
         const $listItems = this.getListItems();
         const $firstItem = $listItems.eq(0);
 
-        const valueChangedEvent = this.valueChangedHandler.getCall(1).args[0].event;
-        assert.strictEqual(valueChangedEvent.type, 'keydown', 'event type is correct');
-        assert.strictEqual(valueChangedEvent.target.get(0), $firstItem.get(0), 'event target is correct');
+        const event = this.handler.getCall(0).args[0].event;
+        assert.strictEqual(event.type, 'keydown', 'event type is correct');
+        assert.strictEqual(event.target, $firstItem.get(0), 'event target is correct');
+
+        this.testProgramChange(assert);
     });
 
-    QUnit.test('on runtime change after item select using enter', function(assert) {
+    QUnit.test('on item select using tab', function(assert) {
         this.keyboard
             .type('1')
             .press('down')
-            .press('enter');
+            .press('tab');
 
-        this.instance.option('value', '33');
+        const event = this.handler.getCall(0).args[0].event;
+        assert.strictEqual(event.type, 'keydown', 'event type is correct');
+        assert.strictEqual(event.target, this.$input.get(0), 'event target is correct');
 
-        const valueChangedEvent = this.valueChangedHandler.getCall(2).args[0].event;
-        assert.strictEqual(valueChangedEvent, undefined, 'event is undefined');
+        this.testProgramChange(assert);
+    });
+
+    QUnit.test('on input', function(assert) {
+        this.instance.option('valueChangeEvent', 'input');
+
+        this.keyboard.type('a');
+
+        const event = this.handler.getCall(0).args[0].event;
+        assert.strictEqual(event.type, 'input', 'event type is correct');
+        assert.strictEqual(event.target, this.$input.get(0), 'event target is correct');
+
+        this.testProgramChange(assert);
+    });
+
+    QUnit.test('on change', function(assert) {
+        this.instance.option('valueChangeEvent', 'change');
+
+        this.keyboard
+            .type('a')
+            .change();
+
+        const event = this.handler.getCall(0).args[0].event;
+        assert.strictEqual(event.type, 'change', 'event type is correct');
+        assert.strictEqual(event.target, this.$input.get(0), 'event target is correct');
+
+        this.testProgramChange(assert);
+    });
+
+    QUnit.test('on focusout', function(assert) {
+        this.instance.option('valueChangeEvent', 'focusout');
+
+        this.keyboard
+            .type('a')
+            .blur();
+
+        const event = this.handler.getCall(0).args[0].event;
+        assert.strictEqual(event.type, 'focusout', 'event type is correct');
+        assert.strictEqual(event.target, this.$input.get(0), 'event target is correct');
+
+        this.testProgramChange(assert);
+    });
+
+    QUnit.test('on keyup', function(assert) {
+        this.instance.option('valueChangeEvent', 'keyup');
+
+        this.keyboard
+            .type('a')
+            .keyUp('a');
+
+        const event = this.handler.getCall(0).args[0].event;
+        assert.strictEqual(event.type, 'keyup', 'event type is correct');
+        assert.strictEqual(event.target, this.$input.get(0), 'event target is correct');
+
+        this.testProgramChange(assert);
+    });
+
+    QUnit.test('on clearing using clearButton', function(assert) {
+        this.instance.option({
+            value: '11',
+            showClearButton: true
+        });
+
+        const $clearButton = this.$element.find(CLEAR_BUTTON_AREA_SELECTOR);
+        $clearButton.trigger('dxclick');
+
+        const event = this.handler.getCall(1).args[0].event;
+        assert.strictEqual(event.type, 'dxclick', 'event type is correct');
+        assert.strictEqual(event.target, $clearButton.get(0), 'event target is correct');
+
+        this.testProgramChange(assert);
     });
 });
