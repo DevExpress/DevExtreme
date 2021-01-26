f69b07891f04f0fa6b9fb824ba2a36e829f08c05
VictorSavushkin <victor.savushkin@devexpress.com>
2021-01-26 22:01:49 +0300

    Diagram add Container tests

--- testing/tests/DevExpress.ui.widgets/diagramParts/clientSideEvents.tests.js
+++ testing/tests/DevExpress.ui.widgets/diagramParts/clientSideEvents.tests.js
@@ -43,7 +43,7 @@ QUnit.module('ClientSideEvents', {
         for(const key in clickedItem) {
             if(Object.prototype.hasOwnProperty.call(clickedItem, key)) count++;
         }
-        assert.equal(count, 9);
+        assert.equal(count, 12);
     });
     test('getItemByKey of unbound diagram', function(assert) {
         this.instance._diagramInstance.commandManager.getCommand(DiagramCommand.Import).execute(Consts.SIMPLE_DIAGRAM);
@@ -66,7 +66,45 @@ QUnit.module('ClientSideEvents', {
         for(const key in apiItem) {
             if(Object.prototype.hasOwnProperty.call(apiItem, key)) count++;
         }
-        assert.equal(count, 9);
+        assert.equal(count, 12);
+    });
+    test('getItemById of unbound diagram (Container)', function(assert) {
+        this.instance._diagramInstance.commandManager.getCommand(DiagramCommand.Import).execute(Consts.SIMPLE_DIAGRAM_WITH_CONTAINER);
+        // child
+        let apiItem = this.instance.getItemById('5');
+        assert.equal(apiItem.id, '5');
+        assert.equal(apiItem.key, undefined);
+        assert.equal(apiItem.text, 'Laurence Lebihan');
+        assert.equal(apiItem.dataItem, undefined);
+        assert.equal(apiItem.position.x, 1);
+        assert.equal(apiItem.position.y, 1.5);
+        assert.equal(apiItem.size.width, 1);
+        assert.equal(apiItem.size.height, 0.75);
+        assert.equal(apiItem.attachedConnectorIds.length, 0);
+        assert.equal(apiItem.containerId, '1');
+
+        // container
+        apiItem = this.instance.getItemById('1');
+        assert.equal(apiItem.id, '1');
+        assert.equal(apiItem.key, undefined);
+        assert.equal(apiItem.text, 'ASP.NET Team');
+        assert.equal(apiItem.type, 'verticalContainer');
+        assert.equal(apiItem.dataItem, undefined);
+        assert.equal(apiItem.position.x, 0.75);
+        assert.equal(apiItem.position.y, 1);
+        assert.equal(apiItem.size.width, 1.5);
+        assert.equal(apiItem.size.height, 4);
+        assert.equal(apiItem.attachedConnectorIds.length, 0);
+        assert.equal(apiItem.containerChildItemIds.length, 1);
+        assert.equal(apiItem.containerChildItemIds[0], '5');
+        assert.equal(apiItem.containerId, '');
+        assert.equal(apiItem.containerExpanded, true);
+
+        let count = 0;
+        for(const key in apiItem) {
+            if(Object.prototype.hasOwnProperty.call(apiItem, key)) count++;
+        }
+        assert.equal(count, 12);
     });
     test('selectionchanged on unbound diagram', function(assert) {
         this.instance._diagramInstance.commandManager.getCommand(DiagramCommand.Import).execute(Consts.SIMPLE_DIAGRAM);
@@ -112,7 +150,7 @@ QUnit.module('ClientSideEvents', {
         for(const key in clickedItem) {
             if(Object.prototype.hasOwnProperty.call(clickedItem, key)) count++;
         }
-        assert.equal(count, 9);
+        assert.equal(count, 12);
         assert.equal(dblClickedItem, undefined);
 
         this.instance._diagramInstance.onNativeAction.raise('notifyItemDblClick', this.instance._diagramInstance.model.findShapeByDataKey('123').toNative(DiagramUnit.In));
@@ -154,7 +192,7 @@ QUnit.module('ClientSideEvents', {
         for(const key in apiItem) {
             if(Object.prototype.hasOwnProperty.call(apiItem, key)) count++;
         }
-        assert.equal(count, 9);
+        assert.equal(count, 12);
     });
     test('getItemById of bound diagram', function(assert) {
         const nodes = [
@@ -182,7 +220,7 @@ QUnit.module('ClientSideEvents', {
         for(const key in apiItem) {
             if(Object.prototype.hasOwnProperty.call(apiItem, key)) count++;
         }
-        assert.equal(count, 9);
+        assert.equal(count, 12);
     });
 });
 
