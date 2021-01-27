7990b7d9f822928d5e3dd710452bd207657ea1c2
Alyar666 <alyar.salimov@devexpress.com>
2021-01-27 23:14:15 +0300

    Renovated DataGrid: Fix error when there are grouped columns in jQuery approach

--- js/core/templates/function_template.d.ts
+++ js/core/templates/function_template.d.ts
@@ -1,7 +1,9 @@
+import { dxElement } from "../element";
+
 export class FunctionTemplate {
   render(template: {
     container: unknown,
     model?: object,
     transclude?: boolean
-  }): void;
+  }): dxElement;
 }
