c9f21f89c1317a2cabe36fff3cd7601ac50fe657
Alexander Bezborodov <alexander.bezborodov@devexpress.com>
2021-01-27 15:38:27 +0300

    Viz Renovation - Fix tooltip cloud size calculation (#16178)

--- js/renovation/viz/core/base_widget.tsx
+++ js/renovation/viz/core/base_widget.tsx
@@ -25,6 +25,7 @@ import {
 } from './utils';
 import { resolveRtlEnabled, resolveRtlEnabledDefinition } from '../../utils/resolve_rtl';
 import { getNextDefsSvgId, getFuncIri } from './renderers/utils';
+import { isUpdatedFlatObject } from '../common/utils';
 
 const DEFAULT_CANVAS = {
   left: 0, top: 0, right: 0, bottom: 0, width: 0, height: 0,
@@ -170,7 +171,7 @@ export class BaseWidget extends JSXComponent(BaseWidgetProps) {
     });
 
     if (isDefined(newCanvas.height) && isDefined(newCanvas.width)
-    && Object.keys(newCanvas).some((key) => newCanvas[key] !== canvas?.[key])) {
+    && isUpdatedFlatObject(canvas, newCanvas)) {
       this.props.canvas = newCanvas;
     }
   }
