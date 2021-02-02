91cb6fa1b67f26d27184a322feec6fa43af41d18
Stanislav Klesarev <stanislav.klesarev@devexpress.com>
2021-02-02 13:25:31 +0300

    File Manager - Fix tree view nodes expanded state after folder delete (T946436) (#15789)
    * Add test
    
    * Unify tests
    
    * Fix ticket problem
    
    * Finally fix treeView different behavior with/without jQuery
    
    * Extend new test
    
    * Fix for remote-like providers
    
    * Update test
    
    * Revert "Fix for remote-like providers"
    
    This reverts commit cd0c25724ebd122362580bcea2198b3fd1bcc75a.
    
    * Reduce number of requests
    
    * Add and update tests
    
    * TODO: CHECK!!!
    
    * Temp commit
    
    * Add tests
    
    * Disable test for ie+jQuery 3
    
    * Presumably fix tests
    
    * Enable wrongly disabled test
    
    * Apply requested changes

--- js/ui/file_manager/file_items_controller.js
+++ js/ui/file_manager/file_items_controller.js
@@ -127,7 +127,12 @@ export default class FileItemsController {
 
     _getCurrentItemsInternal(onlyFiles) {
         const currentDirectory = this.getCurrentDirectory();
-        return onlyFiles ? this.getFiles(currentDirectory) : this.getDirectoryContents(currentDirectory);
+        const getItemsPromise = this.getDirectoryContents(currentDirectory);
+        return getItemsPromise.then(items => {
+            const separatedItems = this._separateItemsByType(items);
+            currentDirectory.fileItem.hasSubDirectories = !!separatedItems.folders.length;
+            return onlyFiles ? separatedItems.files : items;
+        });
     }
 
     getDirectories(parentDirectoryInfo, skipNavigationOnError) {
@@ -135,9 +140,11 @@ export default class FileItemsController {
             .then(itemInfos => itemInfos.filter(info => info.fileItem.isDirectory));
     }
 
-    getFiles(parentDirectoryInfo) {
-        return this.getDirectoryContents(parentDirectoryInfo)
-            .then(itemInfos => itemInfos.filter(info => !info.fileItem.isDirectory));
+    _separateItemsByType(itemInfos) {
+        const folders = [];
+        const files = [];
+        itemInfos.forEach(info => info.fileItem.isDirectory ? folders.push(info) : files.push(info));
+        return { folders, files };
     }
 
     getDirectoryContents(parentDirectoryInfo, skipNavigationOnError) {
