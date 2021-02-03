a568b4a1776e2fd9c0581b346f1bd703907320ba
Alyar666 <alyar.salimov@devexpress.com>
2021-02-03 17:05:20 +0300

    Eliminated the faults noted in the remarks

--- js/ui/tree_list/ui.tree_list.core.js
+++ js/ui/tree_list/ui.tree_list.core.js
@@ -4,10 +4,10 @@ import modules from '../grid_core/ui.grid_core.modules';
 extend(exports, modules, {
     modules: [],
 
-    foreachNodes: function(nodes, callBack, alwaysHasChildren) {
+    foreachNodes: function(nodes, callBack, ignoreHasChildren) {
         for(let i = 0; i < nodes.length; i++) {
-            if(callBack(nodes[i]) !== false && (alwaysHasChildren || nodes[i].hasChildren) && nodes[i].children.length) {
-                this.foreachNodes(nodes[i].children, callBack, alwaysHasChildren);
+            if(callBack(nodes[i]) !== false && (ignoreHasChildren || nodes[i].hasChildren) && nodes[i].children.length) {
+                this.foreachNodes(nodes[i].children, callBack, ignoreHasChildren);
             }
         }
     }
