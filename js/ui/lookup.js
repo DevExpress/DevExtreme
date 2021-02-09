45855e650060f7b20821dedc451055c71f51ec0c
groshenkovamarina <marina.groshenkova@devexpress.com>
2021-02-09 15:21:36 +0300

    Lookup - Fix drop-down closing when parent container scroll in Material theme (T968552)

--- js/ui/lookup.js
+++ js/ui/lookup.js
@@ -744,6 +744,8 @@ const Lookup = DropDownList.inherit({
                 at: 'left bottom',
                 of: this.element()
             };
+
+            result.closeOnTargetScroll = true;
         }
 
         each(['position', 'animation', 'popupWidth', 'popupHeight'], (function(_, optionName) {
