9baaa4201edc8626b91d0bee49366802acdcffd0
Stanislav Klesarev <klesarevklesarevstas@gmail.com>
2021-02-10 15:29:35 +0300

    Add more suitable solution

--- js/ui/file_manager/file_items_controller.js
+++ js/ui/file_manager/file_items_controller.js
@@ -201,11 +201,14 @@ export default class FileItemsController {
         const tempDirInfo = this._createDirInfoByName(name, parentDirectoryInfo);
         const actionInfo = this._createEditActionInfo('create', tempDirInfo, parentDirectoryInfo);
         return this._processEditAction(actionInfo,
-            () => this._fileProvider.createDirectory(parentDirectoryInfo.fileItem, name),
-            () => {
-                this._resetDirectoryState(parentDirectoryInfo, true);
-                parentDirectoryInfo.fileItem.hasSubDirectories = true;
-            });
+            () => this._fileProvider.createDirectory(parentDirectoryInfo.fileItem, name)
+                .done(info => {
+                    if(!parentDirectoryInfo.fileItem.isRoot()) {
+                        parentDirectoryInfo.fileItem.hasSubDirectories = true;
+                    }
+                    return info;
+                }),
+            () => this._resetDirectoryState(parentDirectoryInfo, true));
     }
 
     renameItem(fileItemInfo, name) {
