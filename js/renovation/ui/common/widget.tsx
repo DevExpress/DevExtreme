b29d3e06d2f07379e9d1d775e30adf8f946b6698
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2021-02-10 15:58:01 +0300

    Widget - call onDimensionChanged handler on window resize (#16330)

--- js/renovation/ui/common/widget.tsx
+++ js/renovation/ui/common/widget.tsx
@@ -29,6 +29,7 @@ import { EffectReturn } from '../../utils/effect_return.d';
 import { ConfigContextValue, ConfigContext } from '../../common/config_context';
 import { ConfigProvider } from '../../common/config_provider';
 import { resolveRtlEnabled, resolveRtlEnabledDefinition } from '../../utils/resolve_rtl';
+import resizeCallbacks from '../../../core/utils/resize_callbacks';
 
 const getAria = (args: object): { [name: string]: string } => Object.keys(args).reduce((r, key) => {
   if (args[key]) {
@@ -310,6 +311,18 @@ export class Widget extends JSXComponent(WidgetProps) {
     if (onDimensionChanged) {
       resize.on(this.widgetRef, onDimensionChanged, { namespace });
       return (): void => resize.off(this.widgetRef, { namespace });
+    }
+
+    return undefined;
+  }
+
+  @Effect() windowResizeEffect(): EffectReturn {
+    const { onDimensionChanged } = this.props;
+
+    if (onDimensionChanged) {
+      resizeCallbacks.add(onDimensionChanged);
+
+      return (): void => { resizeCallbacks.remove(onDimensionChanged); };
     }
 
     return undefined;
