1c2a23861a52d29f1a91627f9479da575eefaac8
Dmitry Levkovskiy <DokaRus@users.noreply.github.com>
2021-02-10 19:49:28 +0300

    Fix the mismatch between the submitted value of the masked TextBox and the actual value (T969736) (#16343) (#16354)

--- js/ui/text_box/ui.text_editor.mask.js
+++ js/ui/text_box/ui.text_editor.mask.js
@@ -308,18 +308,22 @@ const TextEditorMask = TextEditorBase.inherit({
 
     _renderValue: function() {
         if(this._maskRulesChain) {
-            const text = this._maskRulesChain.text();
-
             this._showMaskPlaceholder();
 
             if(this._$hiddenElement) {
                 const value = this._maskRulesChain.value();
-                const hiddenElementValue = this._isMaskedValueMode() ? text : value;
+                const submitElementValue = !stringUtils.isEmpty(value) ?
+                    this._getPreparedValue() :
+                    '';
 
-                this._$hiddenElement.val(!stringUtils.isEmpty(value) ? hiddenElementValue : '');
+                this._$hiddenElement.val(submitElementValue);
             }
         }
         return this.callBase();
+    },
+
+    _getPreparedValue: function() {
+        return this._convertToValue().replace(/\s+$/, '');
     },
 
     _valueChangeEventHandler: function(e) {
@@ -330,7 +334,7 @@ const TextEditorMask = TextEditorBase.inherit({
 
         this._saveValueChangeEvent(e);
 
-        this.option('value', this._convertToValue().replace(/\s+$/, ''));
+        this.option('value', this._getPreparedValue());
     },
 
     _isControlKeyFired: function(e) {
