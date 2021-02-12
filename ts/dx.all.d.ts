10c59449bad7e86f7063340943c344b010ee3e3c
gaev.alexey <alexey.gayev@devexpress.com>
2021-02-12 13:33:18 +0300

    DxGantt - Scroll to date

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -8019,6 +8019,10 @@ declare module DevExpress.ui {
          */
         insertTask(data: any): void;
         /**
+         * [descr:dxGantt.scrollToDate(date)]
+         */
+        scrollToDate(date: Date | number | string): void;
+        /**
          * [descr:dxGantt.unassignResourceFromTask(resourceKey, taskKey)]
          */
         unassignResourceFromTask(resourceKey: any, taskKey: any): void;
