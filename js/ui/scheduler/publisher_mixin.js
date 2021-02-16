b4d00a9a0ef67d977558d149c5ee3565f9ab8f3b
andrewmakarov <>
2021-02-16 18:44:36 +0300

    Remove ui.scheduler prefix

---
+++ js/ui/scheduler/publisher_mixin.js
@@ -0,0 +1,16 @@
+const publisherMixin = {
+    notifyObserver: function(subject, args) {
+        const observer = this.option('observer');
+        if(observer) {
+            observer.fire(subject, args);
+        }
+    },
+    invoke: function() {
+        const observer = this.option('observer');
+
+        if(observer) {
+            return observer.fire.apply(observer, arguments);
+        }
+    }
+};
+export default publisherMixin;
