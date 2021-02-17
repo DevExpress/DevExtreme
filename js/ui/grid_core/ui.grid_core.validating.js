979bd928475e65f4555ce603b6c687034f8e69b9
NickMitrokhin <nikolai.mitrokhin@devexpress.com>
2021-02-17 12:14:08 +0300

    Fixes the issue

--- js/ui/grid_core/ui.grid_core.validating.js
+++ js/ui/grid_core/ui.grid_core.validating.js
@@ -402,7 +402,7 @@ const ValidatingController = modules.Controller.inherit((function() {
 
         isCurrentValidatorProcessing: function({ rowKey, columnIndex }) {
             return this._currentCellValidator && this._currentCellValidator.option('validationGroup').key === rowKey
-                    && this._currentCellValidator.option('dataGetter')().column.index === columnIndex;
+                && this._currentCellValidator.option('dataGetter')().column.index === columnIndex;
         },
 
         validateCell: function(validator) {
@@ -1124,6 +1124,20 @@ module.exports = {
                         !hideBorder && this._rowsView.element() && this._rowsView.updateFreeSpaceRowHeight();
                     },
 
+                    _getColumnByCell: function($cell) {
+                        if(!$cell) {
+                            return null;
+                        }
+
+                        const rowsView = this.getView('rowsView');
+                        const $fixedTableElement = rowsView.getFixedTableElement && rowsView.getFixedTableElement();
+                        const columnController = this.getController('columns');
+                        const cellInFixedTable = $fixedTableElement && $cell.closest($fixedTableElement).length;
+                        const columns = cellInFixedTable ? columnController.getFixedColumns() : columnController.getVisibleColumns();
+
+                        return columns[$cell.index()];
+                    },
+
                     focus: function($element, hideBorder) {
                         if(!arguments.length) return this.callBase();
 
@@ -1142,7 +1156,7 @@ module.exports = {
                         const editData = rowOptions ? editingController.getEditDataByKey(rowOptions.key) : null;
                         let validationResult;
                         const $cell = $focus && $focus.is('td') ? $focus : null;
-                        const column = $cell && this.getController('columns').getVisibleColumns()[$cell.index()];
+                        const column = this._getColumnByCell($cell);
                         const validatingController = this.getController('validating');
 
                         if(validator) {
