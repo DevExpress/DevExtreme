b8984bf3957fe224dfc4e33f70b12fb4cb473cb8
babich-a <alexey.babych@gmail.com>
2021-03-02 12:17:50 +0300

    Update jest and packages

--- themebuilder-scss/jest.config.js
+++ themebuilder-scss/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     'globals': {
         'ts-jest': {
-            tsConfig: 'tests/tsconfig.json',
+            tsconfig: 'tests/tsconfig.json',
         },
         'DART_TEST': false,
     },
