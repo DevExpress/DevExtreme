248fa63a77357c9aca43a4988355c8d4a0eaa831
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2021-03-03 18:10:25 +0300

    Update TS bundle

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -6,14 +6,6 @@ interface JQueryPromise<T> {
 interface JQueryEventObject {
     cancel?: boolean;
 }
-interface PromiseLike<T> {
-}
-interface Promise<T> {
-    then<TResult1 = T, TResult2 = never>(
-        onfulfilled?: ((value: T, extraParameters: T) => TResult1 | PromiseLike<TResult1>) | undefined | null,
-        onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null
-    ): Promise<TResult1 | TResult2>;
-}
 /* #EndGlobalDeclaration */
 /* #StartJQueryAugmentation */
 interface JQuery {
