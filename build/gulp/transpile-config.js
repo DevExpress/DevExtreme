beebac60ced66ba6a6ae0f4f353a599b7b98edcf
volnyagin <vconst@users.noreply.github.com>
2021-03-04 10:49:02 +0300

    Use @babel/plugin-transform-runtime for cjs code

--- build/gulp/transpile-config.js
+++ build/gulp/transpile-config.js
@@ -6,6 +6,10 @@ const common = {
         '@babel/plugin-proposal-optional-chaining',
         ['babel-plugin-inferno', { 'imports': true }],
         'transform-object-assign',
+        ['@babel/plugin-transform-runtime', {
+            useESModules: true,
+            version: '7.5.0' // https://github.com/babel/babel/issues/10261#issuecomment-514687857
+        }]
     ],
     ignore: ['**/*.json', '**/sinon.js'],
 };
@@ -16,18 +20,13 @@ module.exports = {
         plugins: common.plugins.concat([
             ['add-module-exports', { addDefaultProperty: true }],
             ['@babel/plugin-transform-modules-commonjs', { strict: true }],
+            ['@babel/plugin-transform-spread', { loose: true }],
             ['@babel/plugin-transform-classes', { loose: true }]
         ])
     }),
 
     esm: Object.assign({}, common, {
         // eslint-disable-next-line spellcheck/spell-checker
-        presets: [['@babel/preset-env', { modules: false, targets: { esmodules: true } }]],
-        plugins: common.plugins.concat(
-            [['@babel/plugin-transform-runtime', {
-                useESModules: true,
-                version: '7.5.0' // https://github.com/babel/babel/issues/10261#issuecomment-514687857
-            }]]
-        )
+        presets: [['@babel/preset-env', { modules: false, targets: { esmodules: true } }]]
     })
 };
