f21e1cd37e8c4993eb9a1a8a9d20c93b4ffc7499
ilya.kharchenko <14272298+IlyaKhD@users.noreply.github.com>
2021-03-04 19:17:19 +0300

    Fix Promise augmentation (#16546)

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -10,7 +10,7 @@ interface PromiseLike<T> {
 }
 interface Promise<T> {
     then<TResult1 = T, TResult2 = never>(
-        onfulfilled?: ((value: T, extraParameters: T) => TResult1 | PromiseLike<TResult1>) | undefined | null,
+        onfulfilled?: ((value: T, extraParameters?: T) => TResult1 | PromiseLike<TResult1>) | undefined | null,
         onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null
     ): Promise<TResult1 | TResult2>;
 }
