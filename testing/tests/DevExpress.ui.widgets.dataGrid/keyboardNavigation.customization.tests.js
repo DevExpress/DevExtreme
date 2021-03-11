e9ffa9ce8946d59eb19903ea7ca45af93b8d48b2
volnyagin <vconst@users.noreply.github.com>
2021-03-11 14:13:01 +0300

    Fix dispose in IE11

--- testing/tests/DevExpress.ui.widgets.dataGrid/keyboardNavigation.customization.tests.js
+++ testing/tests/DevExpress.ui.widgets.dataGrid/keyboardNavigation.customization.tests.js
@@ -71,7 +71,7 @@ QUnit.module('Customize keyboard navigation', {
         this.clock = sinon.useFakeTimers();
     },
     afterEach: function() {
-        this.dispose();
+        this.dispose && this.dispose();
         this.clock.restore();
     }
 }, function() {
