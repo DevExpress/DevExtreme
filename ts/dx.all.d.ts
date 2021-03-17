72ab7290681caaf3310d0601138bcd19cdd582cf
EugeniyKiyashko <johnnykiyashko@gmail.com>
2021-03-17 12:31:39 +0300

    ScrollView - Parameter names are no the same in docs

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -10076,11 +10076,7 @@ declare module DevExpress.ui {
         /**
          * [descr:dxScrollable.scrollBy(distance)]
          */
-        scrollBy(distance: number): void;
-        /**
-         * [descr:dxScrollable.scrollBy(distanceObject)]
-         */
-        scrollBy(distanceObject: any): void;
+        scrollBy(distance: number | any): void;
         /**
          * [descr:dxScrollable.scrollHeight()]
          */
@@ -10096,13 +10092,9 @@ declare module DevExpress.ui {
         /**
          * [descr:dxScrollable.scrollTo(targetLocation)]
          */
-        scrollTo(targetLocation: number): void;
+        scrollTo(targetLocation: number | any): void;
         /**
-         * [descr:dxScrollable.scrollTo(targetLocationObject)]
-         */
-        scrollTo(targetLocation: any): void;
-        /**
-         * [descr:dxScrollable.scrollToElement(targetLocation)]
+         * [descr:dxScrollable.scrollToElement(element)]
          */
         scrollToElement(element: Element | JQuery): void;
         /**
