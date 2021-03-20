b21c85f0827f9ffd1ce59f2baa2d1aad6710997d
Ignatov Dan <ignatovdan@gmail.com>
2021-03-20 14:07:55 +0300

    Drawer - Simplify 'drawer.renderPosition' internals (#16606) (#16739)

--- js/ui/drawer/ui.drawer.rendering.strategy.shrink.js
+++ js/ui/drawer/ui.drawer.rendering.strategy.shrink.js
@@ -1,50 +1,42 @@
 import { animation } from './ui.drawer.rendering.strategy';
 import DrawerStrategy from './ui.drawer.rendering.strategy';
 import $ from '../../core/renderer';
-import { extend } from '../../core/utils/extend';
 import { camelize } from '../../core/utils/inflector';
 
 class ShrinkStrategy extends DrawerStrategy {
-
-    _slidePositionRendering(config, _, animate) {
-        if(animate) {
-            const animationConfig = extend(config.defaultAnimationConfig, {
-                $element: config.$panel,
-                margin: config.panelOffset,
-                duration: this.getDrawerInstance().option('animationDuration'),
-                direction: config.direction
-            });
-
-            animation.margin(animationConfig);
-        } else {
-            config.$panel.css('margin' + camelize(config.direction, true), config.panelOffset);
-        }
-    }
-
-    _expandPositionRendering(config, _, animate) {
+    _internalRenderPosition(changePositionUsingFxAnimation) {
         const drawer = this.getDrawerInstance();
+        const direction = drawer.calcTargetPosition();
+        const $panel = $(drawer.content());
+        const panelSize = this._getPanelSize(drawer.option('opened'));
+        const panelOffset = this._getPanelOffset(drawer.option('opened'));
+        const revealMode = drawer.option('revealMode');
 
-        if(animate) {
-            const animationConfig = extend(config.defaultAnimationConfig, {
-                $element: config.$panel,
-                size: config.size,
-                duration: drawer.option('animationDuration'),
-                direction: config.direction
-            });
-            animation.size(animationConfig);
+        if(changePositionUsingFxAnimation) {
+            if(revealMode === 'slide') {
+                animation.margin({
+                    complete: () => { this._elementsAnimationCompleteHandler(); },
+                    $element: $panel,
+                    duration: drawer.option('animationDuration'),
+                    direction: direction,
+                    margin: panelOffset
+                });
+            } else if(revealMode === 'expand') {
+                animation.size({
+                    complete: () => { this._elementsAnimationCompleteHandler(); },
+                    $element: $panel,
+                    duration: drawer.option('animationDuration'),
+                    direction: direction,
+                    size: panelSize
+                });
+            }
         } else {
-            if(drawer.isHorizontalDirection()) {
-                $(config.$panel).css('width', config.size);
-            } else {
-                $(config.$panel).css('height', config.size);
+            if(revealMode === 'slide') {
+                $panel.css('margin' + camelize(direction, true), panelOffset);
+            } else if(revealMode === 'expand') {
+                $panel.css(drawer.isHorizontalDirection() ? 'width' : 'height', panelSize);
             }
         }
-    }
-
-    _getPositionRenderingConfig(isDrawerOpened) {
-        return extend(super._getPositionRenderingConfig(isDrawerOpened), {
-            panelOffset: this._getPanelOffset(isDrawerOpened)
-        });
     }
 
     isViewContentFirst(position, isRtl) {
