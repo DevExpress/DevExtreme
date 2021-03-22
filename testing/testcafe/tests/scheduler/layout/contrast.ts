1c9922c4e32272a76397c4c752ae3173d406321d
Evgeniy Aryamnov <earyamnow@mail.ru>
2021-03-22 18:17:56 +0300

    Fix test

--- testing/testcafe/tests/scheduler/layout/contrast.ts
+++ testing/testcafe/tests/scheduler/layout/contrast.ts
@@ -6,19 +6,22 @@ import { createDataSetForScreenShotTests } from './utils';
 fixture`Scheduler: Contrast theme layout`
   .page(url(__dirname, './contrast.html'));
 
-const createScheduler = async (view: string, isRenovatedRender: boolean): Promise<void> => {
+const createScheduler = async (options: Record<string, unknown>): Promise<void> => {
   await createWidget('dxScheduler', {
     dataSource: createDataSetForScreenShotTests(),
     currentDate: new Date(2020, 6, 15),
-    views: [view],
-    currentView: view,
     height: 600,
-    renovateRender: isRenovatedRender,
+    ...options,
   }, true);
 };
 
 ['day', 'week'].forEach((view) => {
   test(`Base views layout test in contrat theme(view='${view})`, async (t) => {
     await t.expect(await compareScreenshot(t, `contrast-layout-(view=${view}).png`)).ok();
-  }).before(() => createScheduler(view, true));
+  }).before(() => createScheduler({
+    views: [view],
+    currentView: view,
+    renovateRender: true,
+    showAllDayPanel: false,
+  }));
 });
