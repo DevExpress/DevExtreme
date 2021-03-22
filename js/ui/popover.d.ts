c4d6f2d082c86fe85b91ccc9d7471328fb233361
Zedwag <zedwag@gmail.com>
2021-03-22 18:47:41 +0300

    add default targer value description for popover

--- js/ui/popover.d.ts
+++ js/ui/popover.d.ts
@@ -107,6 +107,7 @@ export interface dxPopoverOptions<T = dxPopover> extends dxPopupOptions<T> {
     showTitle?: boolean;
     /**
      * @docid
+     * @default Window
      * @prevFileNamespace DevExpress.ui
      * @public
      */
