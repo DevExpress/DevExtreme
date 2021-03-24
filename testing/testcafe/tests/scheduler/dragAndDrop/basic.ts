cbc74ac0317db8f158c379da504ba35952494871
Andrew Makarov <andrey.makarov@devexpress.com>
2021-03-24 21:48:36 +0300

    Fix tests

--- testing/testcafe/tests/scheduler/dragAndDrop/basic.ts
+++ testing/testcafe/tests/scheduler/dragAndDrop/basic.ts
@@ -41,7 +41,7 @@ test('Drag-n-drop when browser has horizontal scroll', async (t) => {
   const draggableAppointment = scheduler.getAppointment('Staff Productivity Report');
 
   await t
-    .dragToElement(draggableAppointment.element, scheduler.getAllDayTableCell(6))
+    .dragToElement(draggableAppointment.element, scheduler.getAllDayTableCell(6), { speed: 0.5 })
     .expect(draggableAppointment.isAllDay).eql(true);
 }).before(() => createScheduler({
   views: ['week'],
