48cba509e9f233a7e88283e71c3f110530a24109
andrewmakarov <>
2021-03-29 14:10:48 +0300

    Use default dragging for workspace

--- testing/tests/DevExpress.ui.widgets.scheduler/appointments.tests.js
+++ testing/tests/DevExpress.ui.widgets.scheduler/appointments.tests.js
@@ -148,11 +148,12 @@ const moduleOptions = {
         };
 
         this.instance = $('#scheduler-appointments').dxSchedulerAppointments({
-            observer: observer,
-            _draggingMode: 'default'
+            observer: observer
         }).dxSchedulerAppointments('instance');
 
-        this.workspaceInstance = $('#scheduler-work-space').dxSchedulerWorkSpaceWeek({}).dxSchedulerWorkSpaceWeek('instance');
+        this.workspaceInstance = $('#scheduler-work-space').dxSchedulerWorkSpaceWeek({
+            _draggingMode: 'default'
+        }).dxSchedulerWorkSpaceWeek('instance');
         this.workspaceInstance.getWorkArea().append(this.instance.$element());
 
         const schedulerMock = {
