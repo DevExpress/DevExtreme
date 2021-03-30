544df39ce85f902ef157e4aa19085075692298ae
andrewmakarov <>
2021-03-30 20:03:43 +0300

    Add test

--- testing/testcafe/tests/scheduler/dragAndDrop/outlookDragging/base.ts
+++ testing/testcafe/tests/scheduler/dragAndDrop/outlookDragging/base.ts
@@ -83,3 +83,38 @@ test('Basic drag-n-drop movements with mouse offset', async (t) => {
   height: 600,
   width: 1000,
 }));
+
+test('Basic drag-n-drop movements within the cell', async (t) => {
+  const scheduler = new Scheduler('#container');
+  const draggableAppointment = scheduler.getAppointment('Website Re-Design Plan');
+
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+  await t
+    .drag(draggableAppointment.element, 50, 0)
+    .expect(await takeScreenshot('drag-n-drop-within-cell-to-right.png', scheduler.workSpace))
+    .ok()
+
+    .drag(draggableAppointment.element, -50, 0)
+    .expect(await takeScreenshot('drag-n-drop-within-cell-to-left.png', scheduler.workSpace))
+    .ok()
+
+    .drag(draggableAppointment.element, 0, 30)
+    .expect(await takeScreenshot('drag-n-drop-within-cell-to-bottom.png', scheduler.workSpace))
+    .ok()
+
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(() => createWidget('dxScheduler', {
+  dataSource: [{
+    text: 'Website Re-Design Plan',
+    startDate: new Date(2021, 4, 24, 10),
+    endDate: new Date(2021, 4, 24, 12, 30),
+  }],
+  views: ['week'],
+  currentView: 'week',
+  currentDate: new Date(2021, 4, 27),
+  startDayHour: 9,
+  height: 600,
+  width: 1000,
+}));
