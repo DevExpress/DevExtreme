00329a9b6a1b0e3990ac06e5a0dbb1a9fe1ff63c
Aryamnov Eugeniy <51905811+AryamnovEugeniy@users.noreply.github.com>
2021-04-06 08:54:41 +0300

    dxScheduler - fix layout on small screens (T981358) (#16812)

---
+++ testing/testcafe/tests/scheduler/layout/adaptiveGenericRTL.ts
@@ -0,0 +1,107 @@
+import { compareScreenshot } from '../../../helpers/screenshot-comparer';
+import createWidget from '../../../helpers/createWidget';
+import url from '../../../helpers/getPageUrl';
+import Scheduler from '../../../model/scheduler';
+import {
+  createDataSetForScreenShotTests,
+  resourceDataSource,
+  views,
+  verticalViews,
+  horizontalViews,
+} from './utils';
+
+fixture`Scheduler: Adaptive Generic theme layout in RTL`
+  .page(url(__dirname, '../../container.html'));
+
+const createScheduler = async (
+  additionalProps: Record<string, unknown>,
+): Promise<void> => {
+  await createWidget('dxScheduler', {
+    dataSource: createDataSetForScreenShotTests(),
+    currentDate: new Date(2020, 6, 15),
+    height: 600,
+    rtlEnabled: true,
+    ...additionalProps,
+  }, true);
+};
+
+[false, true].forEach((crossScrollingEnabled) => {
+  // TODO: There is a bug in RTL in timeline views even without adaptivity which breaks markup.
+  // We should test timeline views too after we reowrk our markup
+  const lastVerticalView = crossScrollingEnabled ? 4 : 8;
+
+  const verticalViewsForRTL = verticalViews.slice(0, lastVerticalView);
+
+  test(`Adaptive views layout test in generic theme, crossScrollingEnabled=${crossScrollingEnabled} in RTL`, async (t) => {
+    const scheduler = new Scheduler('#container');
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const view of views) {
+      await scheduler.option('currentView', view);
+
+      await t.expect(
+        await compareScreenshot(t, `adaptive-generic-layout(view=${view}-crossScrollingEnabled=${!!crossScrollingEnabled}-rtl).png`),
+      ).ok();
+    }
+  }).before(async (t) => {
+    await t.resizeWindow(400, 600);
+
+    await createScheduler({
+      views,
+      currentView: 'day',
+      crossScrollingEnabled,
+    });
+  }).after(async (t) => {
+    await t.resizeWindow(1200, 800);
+  });
+
+  test(`Adaptive views layout test in generic theme, crossScrollingEnabled=${crossScrollingEnabled} when horizontal grouping and RTL are used`, async (t) => {
+    const scheduler = new Scheduler('#container');
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const view of views) {
+      await scheduler.option('currentView', view);
+
+      await t.expect(
+        await compareScreenshot(t, `adaptive-generic-layout(view=${view}-crossScrollingEnabled=${!!crossScrollingEnabled}-horizontal-grouping-rtl).png`),
+      ).ok();
+    }
+  }).before(async (t) => {
+    await t.resizeWindow(400, 600);
+
+    await createScheduler({
+      views: horizontalViews,
+      currentView: 'day',
+      crossScrollingEnabled,
+      groups: ['priorityId'],
+      resources: resourceDataSource,
+    });
+  }).after(async (t) => {
+    await t.resizeWindow(1200, 800);
+  });
+
+  test(`Adaptive views layout test in generic theme, crossScrollingEnabled=${crossScrollingEnabled} when vertical grouping and RTL are used`, async (t) => {
+    const scheduler = new Scheduler('#container');
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const view of verticalViewsForRTL) {
+      await scheduler.option('currentView', view.type);
+
+      await t.expect(
+        await compareScreenshot(t, `adaptive-generic-layout(view=${view.type}-crossScrollingEnabled=${!!crossScrollingEnabled}-vertical-grouping-rtl).png`),
+      ).ok();
+    }
+  }).before(async (t) => {
+    await t.resizeWindow(400, 600);
+
+    await createScheduler({
+      views: verticalViewsForRTL,
+      currentView: 'day',
+      crossScrollingEnabled,
+      groups: ['priorityId'],
+      resources: resourceDataSource,
+    });
+  }).after(async (t) => {
+    await t.resizeWindow(1200, 800);
+  });
+});
