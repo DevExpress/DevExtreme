5bd868715ab247a4530c00fcb62fcd6ab177609b
Denis Gradoboev <idenis.gradoboev@yandex.com>
2021-04-16 13:50:29 +0300

    Fix tests

--- testing/tests/DevExpress.ui.widgets.scheduler/integration.workSpace.tests.js
+++ testing/tests/DevExpress.ui.widgets.scheduler/integration.workSpace.tests.js
@@ -2073,24 +2073,33 @@ QUnit.module('Markup', () => {
     });
 });
 
-module('Day of the week order', () => {
-    test('First day of week should be correct in "en-GB" locale (T988896)', function(assert) {
+
+[
+    {
+        view: 'month'
+    },
+    {
+        view: 'week'
+    }
+].forEach(({ view }) => {
+    QUnit.test(`First day of week should be correct in "en-GB" locale (T988896) in ${view} view`, function(assert) {
         const locale = localization.locale();
         localization.locale('en-GB');
 
         const scheduler = createWrapper({
-            views: ['month'],
-            currentView: 'month',
+            views: [view],
+            currentView: view,
             currentDate: new Date(2021, 4, 27),
             firstDayOfWeek: 0,
         });
 
-        const firstDayOfWeek = scheduler.workSpace.getOrdinaryHeaderPanelCells().eq(0).text();
-        const lastDayOfWeek = scheduler.workSpace.getOrdinaryHeaderPanelCells().eq(6).text();
+        const firstDayOfWeek = scheduler.workSpace.getOrdinaryHeaderPanelCells().eq(0).text().substr(0, 3);
+        const lastDayOfWeek = scheduler.workSpace.getOrdinaryHeaderPanelCells().eq(6).text().substr(0, 3);
 
         assert.equal(firstDayOfWeek, 'Sun', 'First day of week is sunday');
-        assert.equal(lastDayOfWeek, 'Sat', 'First day of week is saturday');
+        assert.equal(lastDayOfWeek, 'Sat', 'Last day of week is saturday');
 
         localization.locale(locale);
     });
 });
+
