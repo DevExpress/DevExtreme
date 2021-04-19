ac326c056896d595ceb11d36830e43dd15044b13
ignatov <ignatovdan@gmail.com>
2021-04-19 13:23:29 +0300

    visibilityChange -> domUtils
    (cherry picked from commit 87fd96415a4b3d7051e8410f92eea4ca241f502d)

--- testing/tests/DevExpress.ui.widgets/drawer.tests.js
+++ testing/tests/DevExpress.ui.widgets/drawer.tests.js
@@ -839,7 +839,7 @@ QUnit.module('Drawer behavior', () => {
 
                             QUnit.test(`dxResize event: {opened: false} -> {opened: true} for {${openedStateMode}, ${revealMode}, ${position}, shading: ${shading}, minSize: ${minSize}, animation: ${animationEnabled}}`, function(assert) {
                                 const done = assert.async();
-                                const triggerResizeEventInitial = visibilityChange.triggerResizeEvent;
+                                const triggerResizeEventInitial = domUtils.triggerResizeEvent;
                                 let resizeCallCount = 0;
 
                                 const drawer = $('#drawer').dxDrawer({
@@ -856,7 +856,7 @@ QUnit.module('Drawer behavior', () => {
                                     minSize: minSize
                                 }).dxDrawer('instance');
 
-                                visibilityChange.triggerResizeEvent = ($element) => {
+                                domUtils.triggerResizeEvent = ($element) => {
                                     resizeCallCount++;
                                     assert.strictEqual(resizeCallCount, 1, 'resize event should be triggered once');
                                     assert.equal($element, drawer.viewContent(), 'ViewContent element is expected');
@@ -870,7 +870,7 @@ QUnit.module('Drawer behavior', () => {
                                     assert.strictEqual(viewRect.width, expectedViewRect.width, 'ViewContent width');
                                     assert.strictEqual(viewRect.height, expectedViewRect.height, 'ViewContent height');
 
-                                    visibilityChange.triggerResizeEvent = triggerResizeEventInitial;
+                                    domUtils.triggerResizeEvent = triggerResizeEventInitial;
                                     done();
                                 };
 
