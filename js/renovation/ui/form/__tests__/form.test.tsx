aa298f44f6025cfd6890d06ee5f039ba3eea05a4
Sergey Novikov <novsstation@yandex.ru>
2021-04-19 18:33:57 +0300

    PR feedback

--- js/renovation/ui/form/__tests__/form.test.tsx
+++ js/renovation/ui/form/__tests__/form.test.tsx
@@ -10,22 +10,6 @@ describe('Form', () => {
 
     expect(form.props()).toEqual({});
   });
-
-  describe('Getters', () => {
-    describe('cssClasses', () => {
-      it('Check has dx-form class', () => {
-        const form = new Form({});
-        expect(form.cssClasses).toEqual(expect.stringMatching('dx-form'));
-      });
-    });
-
-    describe('aria', () => {
-      it('Check has form role', () => {
-        const form = new Form({});
-        expect(form.aria).toEqual({ role: 'form' });
-      });
-    });
-  });
 });
 
 describe('Form > Getters', () => {
