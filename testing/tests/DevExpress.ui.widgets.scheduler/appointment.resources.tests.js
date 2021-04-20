bc7cc18ac30bdebc5c7a61c5a3067dccd044a8cc
Evgeniy Aryamnov <earyamnow@mail.ru>
2021-04-20 17:30:09 +0300

    Refactor appointment.resources.tests, appointment.scroll.tests and appointment.templates.tests

--- testing/tests/DevExpress.ui.widgets.scheduler/appointment.resources.tests.js
+++ testing/tests/DevExpress.ui.widgets.scheduler/appointment.resources.tests.js
@@ -1,10 +1,8 @@
-import $ from 'jquery';
 import fx from 'animation/fx';
-import Color from 'color';
 import { DataSource } from 'data/data_source/data_source';
 import {
-    SchedulerTestWrapper,
-    initTestMarkup
+    initTestMarkup,
+    createWrapper
 } from '../../helpers/scheduler/helpers.js';
 
 import 'ui/scheduler/ui.scheduler';
@@ -23,35 +21,7 @@ const APPOINTMENT_CLASS = 'dx-scheduler-appointment';
 module('Integration: Appointment resources', {
     beforeEach: function() {
         fx.off = true;
-        this.createInstance = function(options) {
-            this.instance = $('#scheduler').dxScheduler($.extend(options,
-                {
-                    height: options && options.height || 600
-                })
-            ).dxScheduler('instance');
-
-            this.clock.tick(300);
-            this.instance.focus();
-
-            this.scheduler = new SchedulerTestWrapper(this.instance);
-        };
-        this.getAppointmentColor = function($task, checkedProperty) {
-            checkedProperty = checkedProperty || 'backgroundColor';
-            return new Color($task.css(checkedProperty)).toHex();
-        };
         this.clock = sinon.useFakeTimers();
-        this.tasks = [
-            {
-                text: 'Task 1',
-                startDate: new Date(2015, 1, 9, 1, 0),
-                endDate: new Date(2015, 1, 9, 2, 0)
-            },
-            {
-                text: 'Task 2',
-                startDate: new Date(2015, 1, 9, 11, 0),
-                endDate: new Date(2015, 1, 9, 12, 0)
-            }
-        ];
     },
     afterEach: function() {
         fx.off = false;
@@ -62,34 +32,7 @@ module('Integration: Appointment resources', {
         'standard',
         'virtual'
     ].forEach(scrollingMode => {
-        module(`Scrolling mode ${scrollingMode}`, {
-            beforeEach: function() {
-                const createInstance = this.createInstance.bind(this);
-                this.createInstance = options => {
-                    options = options || {};
-                    $.extend(
-                        true,
-                        options,
-                        {
-                            scrolling: {
-                                mode: scrollingMode
-                            }
-                        }
-                    );
-
-                    createInstance(options);
-
-                    if(scrollingMode === 'virtual') {
-                        const virtualScrollingDispatcher = this.instance.getWorkSpace().virtualScrollingDispatcher;
-                        if(virtualScrollingDispatcher) {
-                            virtualScrollingDispatcher.renderer.getRenderTimeout = () => -1;
-                        }
-                    }
-                };
-
-                this.scrollTo = args => this.instance.getWorkSpace().getScrollable().scrollTo(args);
-            }
-        }, () => {
+        module(`Scrolling mode ${scrollingMode}`, () => {
             test('Task with resources should contain a right data attr', function(assert) {
                 const data = new DataSource({
                     store: [
@@ -99,7 +42,7 @@ module('Integration: Appointment resources', {
                     ]
                 });
 
-                this.createInstance({
+                const scheduler = createWrapper({
                     currentDate: new Date(2015, 1, 9),
                     currentView: 'week',
                     resources: [{
@@ -110,10 +53,12 @@ module('Integration: Appointment resources', {
                         dataSource: [{ id: 1, text: 'c', color: 'blue' }, { id: 2, text: 'd', color: 'white' }]
                     }],
                     dataSource: data,
-                    width: 700
+                    width: 700,
+                    height: 600,
+                    scrolling: { mode: scrollingMode },
                 });
 
-                const tasks = this.instance.$element().find('.' + APPOINTMENT_CLASS);
+                const tasks = scheduler.instance.$element().find('.' + APPOINTMENT_CLASS);
 
                 assert.ok(tasks.eq(0).attr('data-ownerid-2'));
                 assert.ok(tasks.eq(0).attr('data-roomid-1'));
@@ -134,7 +79,7 @@ module('Integration: Appointment resources', {
                     ]
                 });
 
-                this.createInstance({
+                const scheduler = createWrapper({
                     currentDate: new Date(2015, 1, 9),
                     currentView: 'week',
                     resources: [{
@@ -142,10 +87,12 @@ module('Integration: Appointment resources', {
                         dataSource: [{ id: 1, text: 'a', color: 'red' }, { id: 2, text: 'b', color: 'green' }],
                     }],
                     dataSource: data,
-                    width: 700
+                    width: 700,
+                    height: 600,
+                    scrolling: { mode: scrollingMode },
                 });
 
-                const tasks = this.instance.$element().find('.' + APPOINTMENT_CLASS);
+                const tasks = scheduler.instance.$element().find('.' + APPOINTMENT_CLASS);
 
                 assert.ok(tasks.eq(0).attr('data-owner__32____32__id-2'));
             });
