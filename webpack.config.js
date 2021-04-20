66c59718223001b35f3ea80577a1503c15384bb6
Andrey Ignatovskiy <43685423+LazyLahtak@users.noreply.github.com>
2021-04-20 10:40:14 +0300

    Remove excessive webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -33,8 +33,7 @@ module.exports = {
             'process.env': {
                 'NODE_ENV': JSON.stringify('production')
             }
-        }),
-        new webpack.optimize.UglifyJsPlugin()
+        })
     ],
     resolve: {
         alias: {
