456b8aa28f9df352f4a3151a19e14027c412ccce
Sergey Novikov <novsstation@yandex.ru>
2021-04-20 11:05:40 +0300

    Simple implementation of layoutmanager class

---
+++ js/renovation/ui/form/form_layout_manager.tsx
@@ -0,0 +1,33 @@
+import {
+  Component,
+  JSXComponent,
+} from '@devextreme-generator/declarations';
+
+import { combineClasses } from '../../utils/combine_classes';
+import { Widget } from '../common/widget';
+import { FormLayoutManagerProps } from './form_layout_manager_props';
+
+export const viewFunction = (viewModel: FormLayoutManager): JSX.Element => {
+  const { cssClasses, restAttributes } = viewModel;
+  return (
+    <Widget
+      classes={cssClasses}
+      {...restAttributes} // eslint-disable-line react/jsx-props-no-spreading
+    />
+  );
+};
+
+@Component({
+  defaultOptionRules: null,
+  jQuery: { register: true },
+  view: viewFunction,
+})
+
+export class FormLayoutManager extends JSXComponent<FormLayoutManagerProps>() {
+  // eslint-disable-next-line class-methods-use-this
+  get cssClasses(): string {
+    return combineClasses({
+      'dx-layout-manager': true,
+    });
+  }
+}
