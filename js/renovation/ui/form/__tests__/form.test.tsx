c9e91c77f86346b57cfa68e49605b6277801732b
Sergey Novikov <novsstation@yandex.ru>
2021-04-20 12:47:14 +0300

    New tests for form

--- js/renovation/ui/form/__tests__/form.test.tsx
+++ js/renovation/ui/form/__tests__/form.test.tsx
@@ -3,15 +3,51 @@ import React from 'react';
 import each from 'jest-each';
 import { FormProps } from '../form_props';
 import { Form } from '../form';
+import { Scrollable } from '../../scroll_view/scrollable';
 
-describe('Form', () => {
-  it('InitialProps', () => {
-    const props = new FormProps();
+it('Form > InitialProps', () => {
+  const props = new FormProps();
+  const form = mount<Form>(<Form {...props} />);
+
+  expect(form.props()).toEqual({
+    scrollingEnabled: false,
+    useNativeScrolling: undefined,
+  });
+});
+
+describe('Form > Markup', () => {
+  it('Scrollable is not rendered, if scrollingEnabled = false', () => {
+    const props = { scrollingEnabled: false } as FormProps;
     const form = mount<Form>(<Form {...props} />);
 
-    expect(form.props()).toEqual({
-      scrollingEnabled: false,
-      useNativeScrolling: undefined,
+    const scrollable = form.find(Scrollable);
+    expect(scrollable.exists()).toBe(false);
+  });
+
+  it('Scrollable is rendered, if scrollingEnabled = true', () => {
+    const props = { scrollingEnabled: true } as FormProps;
+    const form = mount<Form>(<Form {...props} />);
+
+    const scrollable = form.find(Scrollable);
+    expect(scrollable.exists()).toBe(true);
+  });
+
+  each([false, true, undefined, null]).describe('scrollingEnabled: %o', (useNativeScrolling) => {
+    it('Scrollable params is valid', () => {
+      const props = { scrollingEnabled: true, useNativeScrolling } as FormProps;
+      const form = mount<Form>(<Form {...props} />);
+
+      const scrollable = form.find(Scrollable);
+      expect(scrollable.props()).toEqual({
+        useNative: !!useNativeScrolling,
+        useSimulatedScrollbar: !useNativeScrolling,
+        useKeyboard: false,
+        direction: 'both',
+        bounceEnabled: false,
+        aria: { role: 'form' },
+        classes: 'dx-form',
+        'rest-attributes': 'restAttributes',
+      });
     });
   });
 });
