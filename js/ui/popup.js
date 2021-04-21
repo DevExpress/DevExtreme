8be4d1b205ee0ec5037191c28e6fc4c9770416b5
Zedwag <zedwag@gmail.com>
2021-04-21 10:47:21 +0300

    Popup: return old safari support code

--- js/ui/popup.js
+++ js/ui/popup.js
@@ -18,6 +18,7 @@ import {
 } from '../core/utils/size';
 import { getBoundingRect } from '../core/utils/position';
 import { isDefined } from '../core/utils/type';
+import { compare as compareVersions } from '../core/utils/version';
 import { getWindow, hasWindow } from '../core/utils/window';
 import { triggerResizeEvent } from '../events/visibility_change';
 import messageLocalization from '../localization/message';
@@ -59,6 +60,7 @@ const BUTTON_TEXT_MODE = 'text';
 const BUTTON_CONTAINED_MODE = 'contained';
 
 const IS_IE11 = (browser.msie && parseInt(browser.version) === 11);
+const IS_OLD_SAFARI = browser.safari && compareVersions(browser.version, [11]) < 0;
 const HEIGHT_STRATEGIES = { static: '', inherit: POPUP_CONTENT_INHERIT_HEIGHT_CLASS, flex: POPUP_CONTENT_FLEX_HEIGHT_CLASS };
 
 const getButtonPlace = name => {
@@ -540,7 +542,7 @@ const Popup = Overlay.inherit({
 
         if(this._isAutoHeight() && this.option('autoResizeEnabled')) {
             if(isAutoWidth) {
-                if(!IS_IE11) {
+                if(!IS_IE11 || IS_OLD_SAFARI) {
                     currentHeightStrategyClass = HEIGHT_STRATEGIES.inherit;
                 }
             } else {
