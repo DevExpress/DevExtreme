93922bf47a091f82684a1f19bf315374c952703c
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2021-04-26 19:55:13 +0300

    Replace DxPromise type with PromiseLike where possible

--- js/data/custom_store.d.ts
+++ js/data/custom_store.d.ts
@@ -1,7 +1,3 @@
-import {
-    DxPromise
-} from '../core/utils/deferred';
-
 import Store, {
     StoreOptions
 } from './abstract_store';
@@ -18,7 +14,7 @@ export interface CustomStoreOptions extends StoreOptions<CustomStore> {
      * @prevFileNamespace DevExpress.data
      * @public
      */
-    byKey?: ((key: any | string | number) => DxPromise<any>);
+    byKey?: ((key: any | string | number) => PromiseLike<any>);
     /**
      * @docid
      * @default true
@@ -33,7 +29,7 @@ export interface CustomStoreOptions extends StoreOptions<CustomStore> {
      * @prevFileNamespace DevExpress.data
      * @public
      */
-    insert?: ((values: any) => DxPromise<any>);
+    insert?: ((values: any) => PromiseLike<any>);
     /**
      * @docid
      * @type_function_param1 options:LoadOptions
@@ -41,7 +37,7 @@ export interface CustomStoreOptions extends StoreOptions<CustomStore> {
      * @prevFileNamespace DevExpress.data
      * @public
      */
-    load?: ((options: LoadOptions) => DxPromise<any> | Array<any>);
+    load?: ((options: LoadOptions) => PromiseLike<any> | Array<any>);
     /**
      * @docid
      * @type string
@@ -58,7 +54,7 @@ export interface CustomStoreOptions extends StoreOptions<CustomStore> {
      * @prevFileNamespace DevExpress.data
      * @public
      */
-    remove?: ((key: any | string | number) => DxPromise<void>);
+    remove?: ((key: any | string | number) => PromiseLike<void>);
     /**
      * @docid
      * @type_function_param1 loadOptions:object
@@ -68,7 +64,7 @@ export interface CustomStoreOptions extends StoreOptions<CustomStore> {
      * @prevFileNamespace DevExpress.data
      * @public
      */
-    totalCount?: ((loadOptions: { filter?: any, group?: any }) => DxPromise<number>);
+    totalCount?: ((loadOptions: { filter?: any, group?: any }) => PromiseLike<number>);
     /**
      * @docid
      * @type_function_param1 key:object|string|number
@@ -77,7 +73,7 @@ export interface CustomStoreOptions extends StoreOptions<CustomStore> {
      * @prevFileNamespace DevExpress.data
      * @public
      */
-    update?: ((key: any | string | number, values: any) => DxPromise<any>);
+    update?: ((key: any | string | number, values: any) => PromiseLike<any>);
     /**
      * @docid
      * @default undefined
--- js/ui/scheduler.d.ts
+++ js/ui/scheduler.d.ts
@@ -9,10 +9,6 @@ import {
     template
 } from '../core/templates/template';
 
-import {
-    DxPromise
-} from '../core/utils/deferred';
-
 import DataSource, {
     DataSourceOptions
 } from '../data/data_source';
@@ -63,7 +59,7 @@ export type AppointmentAddedEvent = EventInfo<dxScheduler> & {
 /** @public */
 export type AppointmentAddingEvent = EventInfo<dxScheduler> & {
   readonly appointmentData: any;
-  cancel: boolean | DxPromise<boolean>;
+  cancel: boolean | PromiseLike<boolean>;
 }
 
 /** @public */
@@ -90,7 +86,7 @@ export type AppointmentDeletedEvent = EventInfo<dxScheduler> & {
 /** @public */
 export type AppointmentDeletingEvent = EventInfo<dxScheduler> & {
   readonly appointmentData: any;
-  cancel: boolean | DxPromise<boolean>;
+  cancel: boolean | PromiseLike<boolean>;
 }
 
 /** @public */
@@ -115,7 +111,7 @@ export type AppointmentUpdatedEvent = EventInfo<dxScheduler> & {
 export type AppointmentUpdatingEvent = EventInfo<dxScheduler> & {
   readonly oldData: any;
   readonly newData: any;
-  cancel?: boolean | DxPromise<boolean>;
+  cancel?: boolean | PromiseLike<boolean>;
 }
 
 /** @public */
