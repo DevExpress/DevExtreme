ef6147b521afdfe69875ca3e9ee11c2c5f29deed
Sergey Novikov <57402891+novsstation@users.noreply.github.com>
2021-04-26 17:28:29 +0300

    Form - Render scrollable (#17152)
    * Render scrollable
    
    * New tests for form
    
    * PR feedback
    
    * Fix generator errors about types mismatch
    
    * Remove using of the useSimulatedScrollbar from form
    
    * PR feedback
    
    * Fix tests
    
    * Fix showing scrolling for android
    
    * PR feedback
    
    * PR feedback
    
    * PR feedback
    
    * Fix tests
    
    * Small types refactoring
    
    * Update scrollable.tsx
    
    Fix props after merging

--- js/renovation/ui/form/form.tsx
+++ js/renovation/ui/form/form.tsx
@@ -7,15 +7,40 @@ import { FormProps } from './form_props';
 
 import { combineClasses } from '../../utils/combine_classes';
 import { Widget } from '../common/widget';
+import { Scrollable } from '../scroll_view/scrollable';
 
 export const viewFunction = (viewModel: Form): JSX.Element => {
-  const { aria, cssClasses, restAttributes } = viewModel;
-  return (
-    <Widget
-      aria={aria}
-      classes={cssClasses}
-      {...restAttributes} // eslint-disable-line react/jsx-props-no-spreading
-    />
+  const aria = { role: 'form' };
+  const cssClasses = combineClasses({ 'dx-form': true });
+  const {
+    props: {
+      scrollingEnabled,
+      useNativeScrolling,
+    },
+    restAttributes,
+  } = viewModel;
+  return (scrollingEnabled
+    ? (
+      <Scrollable
+        aria={aria}
+        classes={cssClasses}
+        useNative={!!useNativeScrolling}
+        useSimulatedScrollbar={!useNativeScrolling}
+        useKeyboard={false}
+        direction="both"
+        bounceEnabled={false}
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        // TODO: {...restAttributes} waits for the https://trello.com/c/er8aTcsZ/2711-renovation-react-some-events-from-restattributes-may-have-the-same-type-as-react-exists-events
+      />
+    )
+    : (
+      <Widget
+        aria={aria}
+        classes={cssClasses}
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        {...restAttributes}
+      />
+    )
   );
 };
 
@@ -26,17 +51,5 @@ export const viewFunction = (viewModel: Form): JSX.Element => {
 })
 
 export class Form extends JSXComponent<FormProps>() {
-  // eslint-disable-next-line class-methods-use-this
-  get aria(): Record<string, string> {
-    return {
-      role: 'form',
-    };
-  }
 
-  // eslint-disable-next-line class-methods-use-this
-  get cssClasses(): string {
-    return combineClasses({
-      'dx-form': true,
-    });
-  }
 }
