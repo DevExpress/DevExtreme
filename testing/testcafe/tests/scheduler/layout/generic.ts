521ef3674e383e8cc2c4b60a54dd4de1261ddb15
andrewmakarov <>
2021-04-26 18:25:32 +0300

    Fix intervalCount=1 case

--- testing/testcafe/tests/scheduler/layout/generic.ts
+++ testing/testcafe/tests/scheduler/layout/generic.ts
@@ -60,48 +60,50 @@ const resources = [{
   });
 });
 
-['horizontal', 'vertical'].forEach((groupOrientation) => {
-  [true, false].forEach((showAllDayPanel) => {
-    const testName = `Day view with interval and crossScrollingEnabled, groupOrientation='${groupOrientation}', showAllDayPanel='${showAllDayPanel}'
+[1, 2].forEach((intervalCount) => {
+  ['horizontal', 'vertical'].forEach((groupOrientation) => {
+    [true, false].forEach((showAllDayPanel) => {
+      const testName = `Day view with interval and crossScrollingEnabled, groupOrientation='${groupOrientation}', showAllDayPanel='${showAllDayPanel}'
       layout test`;
 
-    test(testName, async (t) => {
-      const scheduler = new Scheduler('#container');
-      const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+      test(testName, async (t) => {
+        const scheduler = new Scheduler('#container');
+        const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-      const pngName = `day-view-with-interval-crossScrollingEnabled-groupOrientation=${groupOrientation}-showAllDayPanel=${showAllDayPanel}.png`;
+        const pngName = `day-view-with-interval-crossScrollingEnabled-groupOrientation=${groupOrientation}-showAllDayPanel=${showAllDayPanel}.png`;
 
-      await t
-        .expect(await takeScreenshot(pngName, scheduler.workSpace))
-        .ok()
+        await t
+          .expect(await takeScreenshot(pngName, scheduler.workSpace))
+          .ok()
 
-        .expect(compareResults.isValid())
-        .ok(compareResults.errorMessages());
-    }).before(() => createWidget('dxScheduler', {
-      resources: [{
-        fieldExpr: 'roomId',
-        dataSource: [{
-          text: 'Room 1',
-          id: 1,
-        }, {
-          text: 'Room 2',
-          id: 2,
+          .expect(compareResults.isValid())
+          .ok(compareResults.errorMessages());
+      }).before(() => createWidget('dxScheduler', {
+        resources: [{
+          fieldExpr: 'roomId',
+          dataSource: [{
+            text: 'Room 1',
+            id: 1,
+          }, {
+            text: 'Room 2',
+            id: 2,
+          }],
+          label: 'Room',
         }],
-        label: 'Room',
-      }],
-      dataSource: [],
-      views: [{
-        name: 'dayView',
-        type: 'day',
-        intervalCount: 2,
-        groupOrientation,
-      }],
-      currentView: 'dayView',
-      currentDate: new Date(2021, 4, 25),
-      height: 600,
-      groups: ['roomId'],
-      showAllDayPanel,
-      crossScrollingEnabled: true,
-    }));
+        dataSource: [],
+        views: [{
+          name: 'dayView',
+          type: 'day',
+          intervalCount,
+          groupOrientation,
+        }],
+        currentView: 'dayView',
+        currentDate: new Date(2021, 4, 25),
+        height: 600,
+        groups: ['roomId'],
+        showAllDayPanel,
+        crossScrollingEnabled: true,
+      }));
+    });
   });
 });
