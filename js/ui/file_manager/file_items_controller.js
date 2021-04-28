6755287240107e39c52cdcb4317e558828e65bf7
Stanislav Klesarev <klesarevklesarevstas@gmail.com>
2021-04-28 13:00:14 +0300

    File Manager - Fixes IE tests (additionall T976976 fix)

--- js/ui/file_manager/file_items_controller.js
+++ js/ui/file_manager/file_items_controller.js
@@ -70,8 +70,7 @@ export default class FileItemsController {
     updateProvider(fileProvider, currentPath) {
         this._resetCurrentDirectory();
         this._setProvider(fileProvider);
-        this.refresh();
-        return this.setCurrentPath(currentPath);
+        return this.refresh().then(() => this.setCurrentPath(currentPath));
     }
 
     _createFileProvider(fileProvider) {
