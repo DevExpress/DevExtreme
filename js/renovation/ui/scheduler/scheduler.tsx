0260bfa0751fc25c5af0a94dacbe4edcd2dfdb0e
andrewmakarov <>
2021-04-29 17:22:54 +0300

    Init files

---
+++ js/renovation/ui/scheduler/scheduler.tsx
@@ -0,0 +1,33 @@
+import {
+  Component,
+  JSXComponent,
+} from '@devextreme-generator/declarations';
+
+import { SchedulerProps } from './props';
+
+// import { combineClasses } from '../../utils/combine_classes';
+import { Widget } from '../common/widget';
+
+export const viewFunction = (viewModel: Scheduler): JSX.Element => {
+  const { restAttributes } = viewModel;
+  return (
+    <Widget
+      {...restAttributes} // eslint-disable-line react/jsx-props-no-spreading
+    />
+  );
+};
+
+@Component({
+  defaultOptionRules: null,
+  jQuery: { register: true },
+  view: viewFunction,
+})
+
+export class Scheduler extends JSXComponent<SchedulerProps>() {
+  // eslint-disable-next-line class-methods-use-this
+  // get cssClasses(): string {
+  //   return combineClasses({
+  //     'dx-form': true,
+  //   });
+  // }
+}
