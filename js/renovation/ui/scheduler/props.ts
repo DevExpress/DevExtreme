22f48ae35005391a6f6fba6cb7cc703f51632c6b
andrewmakarov <>
2021-04-30 16:00:45 +0300

    Complete describing props

--- js/renovation/ui/scheduler/props.ts
+++ js/renovation/ui/scheduler/props.ts
@@ -1,11 +1,11 @@
 /* eslint-disable max-classes-per-file */
 import {
   ComponentBindings,
-  // OneWay,
-  // Event,
-  // TwoWay,
-  // Nested,
-  // Template,
+  OneWay,
+  Event,
+  TwoWay,
+  Nested,
+  Template,
 } from '@devextreme-generator/declarations';
 
 import type { template } from '../../../core/templates/template';
@@ -42,268 +42,382 @@ import type { UserDefinedElement, DxElement } from '../../../core/element'; // e
 
 @ComponentBindings()
 export class SchedulerProps {
+  @OneWay()
   adaptivityEnabled?: boolean;
 
-  allDayExpr?: string;
+  @Nested()
+  appointmentDragging?: AppointmentDragging;
 
+  @OneWay()
+  cellDuration?: number;
+
+  @OneWay()
+  crossScrollingEnabled?: boolean;
+
+  @TwoWay()
+  currentDate?: Date | number | string;
+
+  @TwoWay()
+  currentView?: 'agenda' | 'day' | 'month' | 'timelineDay' | 'timelineMonth' | 'timelineWeek' | 'timelineWorkWeek' | 'week' | 'workWeek';
+
+  @OneWay()
+  dataSource?: string | dxSchedulerAppointment[] | DataSource | DataSourceOptions;
+
+  @OneWay()
+  dateSerializationFormat?: string;
+
+  @OneWay()
+  descriptionExpr?: string;
+
+  @Nested()
+  editing?: AppointmentEditing;
+
+  @OneWay()
+  endDayHour?: number;
+
+  @OneWay()
+  firstDayOfWeek?: 0 | 1 | 2 | 3 | 4 | 5 | 6;
+
+  @OneWay()
+  focusStateEnabled?: boolean;
+
+  @OneWay()
+  groupByDate?: boolean;
+
+  @OneWay()
+  groups?: string[];
+
+  @OneWay()
+  indicatorUpdateInterval?: number;
+
+  @OneWay()
+  max?: Date | number | string;
+
+  @OneWay()
+  maxAppointmentsPerCell?: number | 'auto' | 'unlimited';
+
+  @OneWay()
+  min?: Date | number | string;
+
+  @OneWay()
+  noDataText?: string;
+
+  @OneWay()
+  recurrenceEditMode?: 'dialog' | 'occurrence' | 'series';
+
+  @OneWay()
+  remoteFiltering?: boolean;
+
+  @Nested()
+  resources?: Resource[];
+
+  @Nested()
+  scrolling?: dxSchedulerScrolling;
+
+  @OneWay()
+  selectedCellData?: unknown[];
+
+  @OneWay()
+  shadeUntilCurrentTime?: boolean;
+
+  @OneWay()
+  showAllDayPanel?: boolean;
+
+  @OneWay()
+  showCurrentTimeIndicator?: boolean;
+
+  @OneWay()
+  startDayHour?: number;
+
+  @OneWay()
+  timeZone?: string;
+
+  @OneWay()
+  useDropDownViewSwitcher?: boolean;
+
+  @Nested()
+  views?: ('day' | 'week' | 'workWeek' | 'month' | 'timelineDay' | 'timelineWeek' | 'timelineWorkWeek' | 'timelineMonth' | 'agenda' | View)[];
+
+  /* Events */
+
+  @Event()
+  customizeDateNavigatorText?: ((info: DateNavigatorTextInfo) => string);
+
+  @Event()
+  onAppointmentAdded?: ((e: AppointmentDraggingAddEvent) => void);
+
+  @Event()
+  onAppointmentAdding?: ((e: AppointmentAddingEvent) => void);
+
+  @Event()
+  onAppointmentClick?: ((e: AppointmentClickEvent) => void) | string;
+
+  @Event()
+  onAppointmentContextMenu?: ((e: AppointmentContextMenuEvent) => void) | string;
+
+  @Event()
+  onAppointmentDblClick?: ((e: AppointmentDblClickEvent) => void) | string;
+
+  @Event()
+  onAppointmentDeleted?: ((e: AppointmentDeletedEvent) => void);
+
+  @Event()
+  onAppointmentDeleting?: ((e: AppointmentDeletingEvent) => void);
+
+  @Event()
+  onAppointmentFormOpening?: ((e: AppointmentFormOpeningEvent) => void);
+
+  @Event()
+  onAppointmentRendered?: ((e: AppointmentRenderedEvent) => void);
+
+  @Event()
+  onAppointmentUpdated?: ((e: AppointmentUpdatedEvent) => void);
+
+  @Event()
+  onAppointmentUpdating?: ((e: AppointmentUpdatingEvent) => void);
+
+  @Event()
+  onCellClick?: ((e: CellClickEvent) => void) | string;
+
+  @Event()
+  onCellContextMenu?: ((e: CellContextMenuEvent) => void) | string;
+
+  /* Templates */
+
+  @Template()
+  timeCellTemplate?:
+  // eslint-disable-next-line max-len
+  template | ((itemData: unknown, itemIndex: number, itemElement: DxElement) => string | UserDefinedElement);
+
+  @Template()
+  resourceCellTemplate?:
+  // eslint-disable-next-line max-len
+  template | ((itemData: unknown, itemIndex: number, itemElement: DxElement) => string | UserDefinedElement);
+
+  @Template()
+  dropDownAppointmentTemplate?:
+  // eslint-disable-next-line max-len
+  template | ((itemData: unknown, itemIndex: number, contentElement: DxElement) => string | UserDefinedElement);
+
+  @Template()
   appointmentCollectorTemplate?:
   // eslint-disable-next-line max-len
   template|((data: AppointmentCollectorTemplateData, collectorElement: DxElement) => string | UserDefinedElement);
 
-  appointmentDragging?: AppointmentDragging;
-
+  @Template()
   appointmentTemplate?:
   // eslint-disable-next-line max-len
   template | ((model: AppointmentTemplateData, itemIndex: number, contentElement: DxElement) => string | UserDefinedElement);
 
+  @Template()
+  dateCellTemplate?:
+  // eslint-disable-next-line max-len
+  template | ((itemData: unknown, itemIndex: number, itemElement: DxElement) => string | UserDefinedElement);
+
+  @Template()
   appointmentTooltipTemplate?:
   // eslint-disable-next-line max-len
   template| ((model: AppointmentTemplateData, itemIndex: number, contentElement: DxElement) => string | UserDefinedElement);
 
-  cellDuration?: number;
-
-  crossScrollingEnabled?: boolean;
-
-  currentDate?: Date | number | string;
-
-  currentView?: 'agenda' | 'day' | 'month' | 'timelineDay' | 'timelineMonth' | 'timelineWeek' | 'timelineWorkWeek' | 'week' | 'workWeek';
-
-  customizeDateNavigatorText?: ((info: DateNavigatorTextInfo) => string);
-
+  @Template()
   dataCellTemplate?:
   // eslint-disable-next-line max-len
   template | ((itemData: unknown, itemIndex: number, itemElement: DxElement) => string | UserDefinedElement);
 
-  dataSource?: string | dxSchedulerAppointment[] | DataSource | DataSourceOptions;
+  /* Field expressions */
 
-  dateCellTemplate?:
-  // eslint-disable-next-line max-len
-  template | ((itemData: unknown, itemIndex: number, itemElement: DxElement) => string | UserDefinedElement);
-
-  dateSerializationFormat?: string;
-
-  descriptionExpr?: string;
-
-  dropDownAppointmentTemplate?:
-  // eslint-disable-next-line max-len
-  template | ((itemData: unknown, itemIndex: number, contentElement: DxElement) => string | UserDefinedElement);
-
-  editing?: AppointmentEditing;
-
-  endDateExpr?: string;
-
-  endDateTimeZoneExpr?: string;
-
-  endDayHour?: number;
-
-  firstDayOfWeek?: 0 | 1 | 2 | 3 | 4 | 5 | 6;
-
-  focusStateEnabled?: boolean;
-
-  groupByDate?: boolean;
-
-  groups?: string[];
-
-  indicatorUpdateInterval?: number;
-
-  max?: Date | number | string;
-
-  maxAppointmentsPerCell?: number | 'auto' | 'unlimited';
-
-  min?: Date | number | string;
-
-  noDataText?: string;
-
-  onAppointmentAdded?: ((e: AppointmentDraggingAddEvent) => void);
-
-  onAppointmentAdding?: ((e: AppointmentAddingEvent) => void);
-
-  onAppointmentClick?: ((e: AppointmentClickEvent) => void) | string;
-
-  onAppointmentContextMenu?: ((e: AppointmentContextMenuEvent) => void) | string;
-
-  onAppointmentDblClick?: ((e: AppointmentDblClickEvent) => void) | string;
-
-  onAppointmentDeleted?: ((e: AppointmentDeletedEvent) => void);
-
-  onAppointmentDeleting?: ((e: AppointmentDeletingEvent) => void);
-
-  onAppointmentFormOpening?: ((e: AppointmentFormOpeningEvent) => void);
-
-  onAppointmentRendered?: ((e: AppointmentRenderedEvent) => void);
-
-  onAppointmentUpdated?: ((e: AppointmentUpdatedEvent) => void);
-
-  onAppointmentUpdating?: ((e: AppointmentUpdatingEvent) => void);
-
-  onCellClick?: ((e: CellClickEvent) => void) | string;
-
-  onCellContextMenu?: ((e: CellContextMenuEvent) => void) | string;
-
-  recurrenceEditMode?: 'dialog' | 'occurrence' | 'series';
-
+  @OneWay()
   recurrenceExceptionExpr?: string;
 
+  @OneWay()
   recurrenceRuleExpr?: string;
 
-  remoteFiltering?: boolean;
-
-  resourceCellTemplate?:
-  // eslint-disable-next-line max-len
-  template | ((itemData: unknown, itemIndex: number, itemElement: DxElement) => string | UserDefinedElement);
-
-  resources?: Resource[];
-
-  scrolling?: dxSchedulerScrolling;
-
-  selectedCellData?: unknown[];
-
-  shadeUntilCurrentTime?: boolean;
-
-  showAllDayPanel?: boolean;
-
-  showCurrentTimeIndicator?: boolean;
-
+  @OneWay()
   startDateExpr?: string;
 
+  @OneWay()
   startDateTimeZoneExpr?: string;
 
-  startDayHour?: number;
+  @OneWay()
+  endDateExpr?: string;
 
+  @OneWay()
+  endDateTimeZoneExpr?: string;
+
+  @OneWay()
+  allDayExpr?: string;
+
+  @OneWay()
   textExpr?: string;
-
-  timeCellTemplate?:
-  // eslint-disable-next-line max-len
-  template | ((itemData: unknown, itemIndex: number, itemElement: DxElement) => string | UserDefinedElement);
-
-  timeZone?: string;
-
-  useDropDownViewSwitcher?: boolean;
-
-  views?: ('day' | 'week' | 'workWeek' | 'month' | 'timelineDay' | 'timelineWeek' | 'timelineWorkWeek' | 'timelineMonth' | 'agenda' | View)[];
 }
 
 @ComponentBindings()
 export class Resource {
+  @OneWay()
   allowMultiple?: boolean;
 
-  colorExpr?: string;
-
+  @OneWay()
   dataSource?: string | unknown[] | DataSource | DataSourceOptions;
 
-  displayExpr?: string | ((resource: unknown) => string);
-
-  fieldExpr?: string;
-
+  @OneWay()
   label?: string;
 
+  @OneWay()
   useColorAsDefault?: boolean;
 
+  /* Field expressions */
+
+  @OneWay()
   valueExpr?: string | ((data: unknown) => string);
+
+  @OneWay()
+  colorExpr?: string;
+
+  @OneWay()
+  displayExpr?: string | ((resource: unknown) => string);
+
+  @OneWay()
+  fieldExpr?: string;
 }
 
 @ComponentBindings()
 export class View {
+  @OneWay()
   agendaDuration?: number;
 
+  @OneWay()
+  cellDuration?: number;
+
+  @OneWay()
+  endDayHour?: number;
+
+  @OneWay()
+  firstDayOfWeek?: 0 | 1 | 2 | 3 | 4 | 5 | 6;
+
+  @OneWay()
+  groupByDate?: boolean;
+
+  @OneWay()
+  groupOrientation?: 'horizontal' | 'vertical';
+
+  @OneWay()
+  groups?: string[];
+
+  @OneWay()
+  intervalCount?: number;
+
+  @OneWay()
+  maxAppointmentsPerCell?: number | 'auto' | 'unlimited';
+
+  @OneWay()
+  name?: string;
+
+  @OneWay()
+  startDate?: Date | number | string;
+
+  @OneWay()
+  startDayHour?: number;
+
+  @OneWay()
+  type?: 'agenda' | 'day' | 'month' | 'timelineDay' | 'timelineMonth' | 'timelineWeek' | 'timelineWorkWeek' | 'week' | 'workWeek';
+
+  @Nested()
+  scrolling?: dxSchedulerScrolling;
+
+  @Template()
   appointmentCollectorTemplate?:
   // eslint-disable-next-line max-len
   template | ((data: AppointmentCollectorTemplateData, collectorElement: DxElement) => string | UserDefinedElement);
 
+  @Template()
   appointmentTemplate?:
   // eslint-disable-next-line max-len
   template | ((model: AppointmentTemplateData, itemIndex: number, contentElement: DxElement) => string | UserDefinedElement);
 
+  @Template()
   appointmentTooltipTemplate?:
   // eslint-disable-next-line max-len
   template | ((model: AppointmentTooltipTemplateData, itemIndex: number, contentElement: DxElement) => string | UserDefinedElement);
 
-  cellDuration?: number;
-
+  @Template()
   dataCellTemplate?:
   // eslint-disable-next-line max-len
   template | ((itemData: unknown, itemIndex: number, itemElement: DxElement) => string | UserDefinedElement);
 
+  @Template()
   dateCellTemplate?:
   // eslint-disable-next-line max-len
   template | ((itemData: unknown, itemIndex: number, itemElement: DxElement) => string | UserDefinedElement);
 
+  @Template()
   dropDownAppointmentTemplate?:
   // eslint-disable-next-line max-len
   template | ((itemData: unknown, itemIndex: number, contentElement: DxElement) => string | UserDefinedElement);
 
-  endDayHour?: number;
-
-  firstDayOfWeek?: 0 | 1 | 2 | 3 | 4 | 5 | 6;
-
-  groupByDate?: boolean;
-
-  groupOrientation?: 'horizontal' | 'vertical';
-
-  groups?: string[];
-
-  intervalCount?: number;
-
-  maxAppointmentsPerCell?: number | 'auto' | 'unlimited';
-
-  name?: string;
-
+  @Template()
   resourceCellTemplate?:
   // eslint-disable-next-line max-len
   template | ((itemData: unknown, itemIndex: number, itemElement: DxElement) => string | UserDefinedElement);
 
-  startDate?: Date | number | string;
-
-  startDayHour?: number;
-
+  @Template()
   timeCellTemplate?:
   // eslint-disable-next-line max-len
   template | ((itemData: unknown, itemIndex: number, itemElement: DxElement) => string | UserDefinedElement);
-
-  type?: 'agenda' | 'day' | 'month' | 'timelineDay' | 'timelineMonth' | 'timelineWeek' | 'timelineWorkWeek' | 'week' | 'workWeek';
-
-  scrolling?: dxSchedulerScrolling;
 }
 
 @ComponentBindings()
 export class AppointmentEditing {
+  @OneWay()
   allowAdding?: boolean;
 
+  @OneWay()
   allowDeleting?: boolean;
 
+  @OneWay()
   allowDragging?: boolean;
 
+  @OneWay()
   allowResizing?: boolean;
 
+  @OneWay()
   allowTimeZoneEditing?: boolean;
 
+  @OneWay()
   allowUpdating?: boolean;
 
+  @OneWay()
   allowEditingTimeZones?: boolean;
 }
 
 @ComponentBindings()
 export class AppointmentDragging {
-  appointmentDragging?: {
+  @OneWay()
+  autoScroll?: boolean;
 
-    autoScroll?: boolean;
+  @OneWay()
+  data?: unknown;
 
-    data?: unknown;
+  @OneWay()
+  group?: string;
 
-    group?: string;
+  @OneWay()
+  scrollSensitivity?: number;
 
-    onAdd?: ((e: AppointmentDraggingAddEvent) => void);
+  @OneWay()
+  scrollSpeed?: number;
 
-    onDragEnd?: ((e: AppointmentDraggingEndEvent) => void);
+  @Event()
+  onAdd?: ((e: AppointmentDraggingAddEvent) => void);
 
-    onDragMove?: ((e: AppointmentDraggingMoveEvent) => void);
+  @Event()
+  onDragEnd?: ((e: AppointmentDraggingEndEvent) => void);
 
-    onDragStart?: ((e: AppointmentDraggingStartEvent) => void);
+  @Event()
+  onDragMove?: ((e: AppointmentDraggingMoveEvent) => void);
 
-    onRemove?: ((e: AppointmentDraggingRemoveEvent) => void);
+  @Event()
+  onDragStart?: ((e: AppointmentDraggingStartEvent) => void);
 
-    scrollSensitivity?: number;
-
-    scrollSpeed?: number;
-  };
+  @Event()
+  onRemove?: ((e: AppointmentDraggingRemoveEvent) => void);
 }
