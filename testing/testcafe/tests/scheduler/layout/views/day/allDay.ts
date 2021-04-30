354b2c5b78b35a73d7aac1fcb1760dd6f1efe6ff
Andrew Makarov <andrey.makarov@devexpress.com>
2021-04-30 17:23:28 +0300

    dxScheduler - refactoring screenshot test (#17341)
    * Cherry pick
    
    * Remove unused file
    
    Co-authored-by: andrewmakarov <>

--- testing/testcafe/tests/scheduler/layout/views/day/allDay.ts
+++ testing/testcafe/tests/scheduler/layout/views/day/allDay.ts
@@ -1,22 +1,22 @@
-import { createScreenshotsComparer } from '../../../helpers/screenshot-comparer';
-import createWidget from '../../../helpers/createWidget';
-import url from '../../../helpers/getPageUrl';
-import Scheduler from '../../../model/scheduler';
+import Scheduler from '../../../../../model/scheduler';
+import { createScreenshotsComparer } from '../../../../../helpers/screenshot-comparer';
+import createWidget from '../../../../../helpers/createWidget';
+import url from '../../../../../helpers/getPageUrl';
 
-fixture`Scheduler: Generic theme layout`
-  .page(url(__dirname, '../../container.html'));
+fixture`Layout:Views:Day:AllDay`
+  .page(url(__dirname, '../../../../container.html'));
 
 [1, 2].forEach((intervalCount) => {
   ['horizontal', 'vertical'].forEach((groupOrientation) => {
     [true, false].forEach((showAllDayPanel) => {
-      const testName = `Day view with interval and crossScrollingEnabled, groupOrientation='${groupOrientation}', showAllDayPanel='${showAllDayPanel}', intervalCount='${intervalCount}'
+      const testName = `Day view with interval and crossScrollingEnabled(groupOrientation='${groupOrientation}', showAllDayPanel='${showAllDayPanel}', intervalCount='${intervalCount}')
       layout test`;
 
       test(testName, async (t) => {
         const scheduler = new Scheduler('#container');
         const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-        const pngName = `day-view-with-interval-crossScrollingEnabled-groupOrientation=${groupOrientation}-showAllDayPanel=${showAllDayPanel}-intervalCount=${intervalCount}.png`;
+        const pngName = `day-view-interval-orientation=${groupOrientation}-allDay=${showAllDayPanel}-interval=${intervalCount}.png`;
 
         await t
           .expect(await takeScreenshot(pngName, scheduler.workSpace))
