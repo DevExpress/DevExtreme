9f3647fc131490e05f8206e9254089af65bd6b6c
Alyar666 <28081959Ci>
2021-05-04 19:54:02 +0300

    DataGrid / TreeList: Fix row focus after filtering (T993300)

--- js/ui/tree_list/ui.tree_list.focus.js
+++ js/ui/tree_list/ui.tree_list.focus.js
@@ -92,7 +92,6 @@ core.registerModule('focus', extend(true, {}, focusModule, {
 
                     that.expandAscendants(key).done(function() {
                         dataSource.load({
-                            filter: that.getCombinedFilter(),
                             sort: that.getController('columns').getSortDataSourceParameters(!dataSource.remoteOperations().sorting),
                             parentIds: []
                         }).done(function(nodes) {
