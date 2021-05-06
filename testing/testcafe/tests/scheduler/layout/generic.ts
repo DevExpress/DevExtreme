5a0866ec66bf4ed6cb9f90bfd96a87e4099b2a72
Evgeniy Aryamnov <earyamnow@mail.ru>
2021-05-06 19:40:12 +0300

    Refactor

--- testing/testcafe/tests/scheduler/layout/generic.ts
+++ testing/testcafe/tests/scheduler/layout/generic.ts
@@ -59,21 +59,3 @@ const resources = [{
     }).before(() => createScheduler(view, resourcesValue));
   });
 });
-
-test('Header cells should be aligned with date-table cells in timeline-month when current date changes', async (t) => {
-  const scheduler = new Scheduler('#container');
-
-  await scheduler.option('currentDate', new Date(2020, 11, 1));
-
-  await t.expect(
-    await compareScreenshot(t, 'virtual-scrolling-timeline-month-change-current-date.png'),
-  ).ok();
-}).before(async () => {
-  await createWidget('dxScheduler', {
-    currentDate: new Date(2020, 10, 1),
-    currentView: 'timelineMonth',
-    height: 600,
-    views: ['timelineMonth'],
-    crossScrollingEnabled: true,
-  }, true);
-});
