1ad2b38ce4dd2a6ba6f83d1b752083cc49d4781c
andrewmakarov <>
2021-05-11 23:18:08 +0300

    Add methods to declaration

--- js/renovation/ui/scheduler/scheduler.tsx
+++ js/renovation/ui/scheduler/scheduler.tsx
@@ -1,12 +1,15 @@
 import {
   Component,
   JSXComponent,
+  // Method,
 } from '@devextreme-generator/declarations';
 
 import { SchedulerProps } from './props';
 
 // import { combineClasses } from '../../utils/combine_classes';
 import { Widget } from '../common/widget';
+// import { UserDefinedElement } from '../../../core/element'; // eslint-disable-line import/named
+// import DataSource from '../../../data/data_source';
 
 export const viewFunction = (viewModel: Scheduler): JSX.Element => {
   const { restAttributes } = viewModel;
@@ -27,5 +30,66 @@ export class Scheduler extends JSXComponent(SchedulerProps) {
   //   return combineClasses({
   //     'dx-form': true,
   //   });
+  // }
+  // @Method()
+  // addAppointment(appointment: any): void {
+
+  // }
+
+  // @Method()
+  // deleteAppointment(appointment: any): void {
+
+  // }
+
+  // @Method()
+  // getDataSource(): DataSource {
+  //   return new DataSource([]); // TODO
+  // }
+
+  // @Method()
+  // getEndViewDate(): Date {
+  //   return new Date(); // TODO
+  // }
+
+  // @Method()
+  // getStartViewDate(): Date {
+  //   return new Date(); // TODO
+  // }
+
+  // @Method()
+  // hideAppointmentPopup(saveChanges?: boolean): void {
+
+  // }
+
+  // @Method()
+  // hideAppointmentTooltip(): void {
+
+  // }
+
+  // @Method()
+  // scrollTo(date: Date, group?: object, allDay?: boolean): void {
+
+  // }
+
+  // @Method()
+  // scrollToTime(hours: number, minutes: number, date?: Date): void {
+
+  // }
+
+  // @Method()
+  // showAppointmentPopup(appointmentData?: any, createNewAppointment?: boolean,
+  // currentAppointmentData?: any): void {
+
+  // }
+
+  // @Method()
+  // showAppointmentTooltip(appointmentData: any, target: string | UserDefinedElement,
+  // currentAppointmentData?: any): void {
+
+  // }
+
+  // @Method()
+  // updateAppointment(target: any, appointment: any): void {
+
   // }
 }
