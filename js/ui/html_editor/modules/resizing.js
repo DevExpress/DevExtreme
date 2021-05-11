454671b23e4221b894cb048ae25913f65de3d9cb
alexander-kotov-dx <alexander.kotov@devexpress.com>
2021-05-11 14:07:50 +0300

    Revert changes from the resizing module to fix bundled tests

--- js/ui/html_editor/modules/resizing.js
+++ js/ui/html_editor/modules/resizing.js
@@ -7,7 +7,6 @@ import devices from '../../../core/devices';
 import Resizable from '../../resizable';
 import { getBoundingRect } from '../../../core/utils/position';
 import Quill from 'devextreme-quill';
-import BaseHtmlEditorModule from './base';
 
 const DX_RESIZE_FRAME_CLASS = 'dx-resize-frame';
 const DX_TOUCH_DEVICE_CLASS = 'dx-touch-device';
@@ -19,14 +18,17 @@ const MOUSEDOWN_EVENT = addNamespace('mousedown', MODULE_NAMESPACE);
 
 const FRAME_PADDING = 1;
 
-export default class ResizingModule extends BaseHtmlEditorModule {
+export default class ResizingModule {
+
     constructor(quill, options) {
-        super(quill, options);
         this.quill = quill;
+        this.editorInstance = options.editorInstance;
+
         this.allowedTargets = options.allowedTargets || ['image'];
         this.enabled = !!options.enabled;
         this._hideFrameWithContext = this.hideFrame.bind(this);
         this._framePositionChangedHandler = this._prepareFramePositionChangedHandler();
+
 
         if(this.enabled) {
             this._attachEvents();
