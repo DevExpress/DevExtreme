6f77e796200bdcf97a910a5b3acd70e545940016
Stanislav Klesarev <klesarevklesarevstas@gmail.com>
2021-05-13 17:38:06 +0300

    File Manager - Fix selection resetting for single selection mode (T972613)

--- js/ui/file_manager/ui.file_manager.item_list.details.js
+++ js/ui/file_manager/ui.file_manager.item_list.details.js
@@ -339,6 +339,10 @@ class FileManagerDetailsItemList extends FileManagerItemListBase {
         }
     }
 
+    _resetFocus() {
+        this._setFocusedItemKey(undefined);
+    }
+
     _createThumbnailColumnCell(container, cellInfo) {
         this._getItemThumbnailContainer(cellInfo.data).appendTo(container);
     }
@@ -386,7 +390,7 @@ class FileManagerDetailsItemList extends FileManagerItemListBase {
 
             const selectedItems = [];
             const selectedKeys = [];
-            if(fileItemInfo) {
+            if(fileItemInfo && !this._isParentDirectoryItem(fileItemInfo)) {
                 selectedItems.push(fileItemInfo.fileItem);
                 selectedKeys.push(fileItemInfo.fileItem.key);
             }
