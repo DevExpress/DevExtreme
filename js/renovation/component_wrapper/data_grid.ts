657d4930bde49a5233505f9ff2acf71d51937377
volnyagin <vconst@users.noreply.github.com>
2021-05-14 14:29:58 +0300

    Renovated DataGrid: Rename IDataGrid to DataGridForComponentWrapper

--- js/renovation/component_wrapper/data_grid.ts
+++ js/renovation/component_wrapper/data_grid.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import Component from './common/component';
-import type { IDataGrid } from '../ui/grids/data_grid/common/types';
+import type { DataGridForComponentWrapper } from '../ui/grids/data_grid/common/types';
 import gridCore from '../../ui/data_grid/ui.data_grid.core';
 import { updatePropsImmutable } from "./utils/update-props-immutable";
 
@@ -12,39 +12,39 @@ export default class DataGridWrapper extends Component {
     static registerModule = gridCore.registerModule.bind(gridCore);
 
     beginUpdate() {
-        const gridInstance = (this.viewRef as IDataGrid)?.getComponentInstance();
+        const gridInstance = (this.viewRef as DataGridForComponentWrapper)?.getComponentInstance();
 
         super.beginUpdate();
         gridInstance?.beginUpdate();
     }
 
     endUpdate() {
-        const gridInstance = (this.viewRef as IDataGrid)?.getComponentInstance();
+        const gridInstance = (this.viewRef as DataGridForComponentWrapper)?.getComponentInstance();
 
         super.endUpdate();
         gridInstance?.endUpdate();
     }
 
     isReady() {
-        const gridInstance = (this.viewRef as IDataGrid)?.getComponentInstance();
+        const gridInstance = (this.viewRef as DataGridForComponentWrapper)?.getComponentInstance();
 
         return gridInstance?.isReady();
     }
 
     getView(name) {
-        const gridInstance = (this.viewRef as IDataGrid)?.getComponentInstance();
+        const gridInstance = (this.viewRef as DataGridForComponentWrapper)?.getComponentInstance();
 
         return gridInstance?.getView(name);
     }
 
     getController(name) {
-        const gridInstance = (this.viewRef as IDataGrid)?.getComponentInstance();
+        const gridInstance = (this.viewRef as DataGridForComponentWrapper)?.getComponentInstance();
 
         return gridInstance?.getController(name);
     }
 
     state(state) {
-        const gridInstance = (this.viewRef as IDataGrid)?.getComponentInstance();
+        const gridInstance = (this.viewRef as DataGridForComponentWrapper)?.getComponentInstance();
 
         return gridInstance?.state(state);
     }
@@ -57,14 +57,14 @@ export default class DataGridWrapper extends Component {
         super._optionChanging(fullName, value, prevValue);
         if(this.viewRef) {
             const name = fullName.split(/[.[]/)[0];
-            const prevProps = { ...(this.viewRef as IDataGrid).prevProps };
+            const prevProps = { ...(this.viewRef as DataGridForComponentWrapper).prevProps };
             updatePropsImmutable(prevProps, this.option(), name, fullName);
-            (this.viewRef as IDataGrid).prevProps = prevProps;
+            (this.viewRef as DataGridForComponentWrapper).prevProps = prevProps;
         }
     }
 
     _optionChanged(e): void {
-        const gridInstance = (this.viewRef as IDataGrid)?.getComponentInstance?.();
+        const gridInstance = (this.viewRef as DataGridForComponentWrapper)?.getComponentInstance?.();
         if (e.fullName === 'dataSource' && e.value === gridInstance?.option('dataSource')) {
             gridInstance?.option('dataSource', e.value);
         }
