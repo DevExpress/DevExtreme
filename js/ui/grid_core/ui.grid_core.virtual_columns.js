a49cde3cecf6de7b4d6495015b384d2a61380ace
Alyar666 <28081959Ci>
2021-05-17 20:52:26 +0300

    DataGrid: Fix summary item flickering when the grid is scrolled if the rowRenderingMode option is set to virtual (T996914)

--- js/ui/grid_core/ui.grid_core.virtual_columns.js
+++ js/ui/grid_core/ui.grid_core.virtual_columns.js
@@ -24,7 +24,7 @@ const VirtualScrollingRowsViewExtender = {
     }
 };
 
-const HeaderFooterViewExtender = {
+const HeaderViewExtender = {
     _renderCore: function() {
         const that = this;
         const scrollLeft = that._scrollLeft;
@@ -250,8 +250,7 @@ module.exports = {
             columns: ColumnsControllerExtender
         },
         views: {
-            columnHeadersView: HeaderFooterViewExtender,
-            footerView: HeaderFooterViewExtender,
+            columnHeadersView: HeaderViewExtender,
             rowsView: VirtualScrollingRowsViewExtender
         }
     }
