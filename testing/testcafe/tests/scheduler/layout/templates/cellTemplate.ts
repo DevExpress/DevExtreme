638b04e0e0b572205747cfe51a43c4d438db5560
andrewmakarov <>
2021-05-19 00:00:16 +0300

    For check screenshots

--- testing/testcafe/tests/scheduler/layout/templates/cellTemplate.ts
+++ testing/testcafe/tests/scheduler/layout/templates/cellTemplate.ts
@@ -13,7 +13,7 @@ fixture`Layout:Templates:CellTemplate`
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
     await t
-      .expect(await takeScreenshot(`data-cell-template-currentView=${currentView}`, scheduler.workSpace))
+      .expect(await takeScreenshot(`data-cell-template-currentView=${currentView}`, scheduler.element.find('.dx-scheduler-header')))
       .ok()
 
       .expect(compareResults.isValid())
