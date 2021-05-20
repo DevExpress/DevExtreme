e8fb3fa41fd5e73f2fae32a4c90a8ef9afcf84ee
CORP\zaborshchikov.e <jacksonrr3@yandex.ru>
2021-05-20 11:58:17 +0300

    add package.json

--- testing/test_bundlers/rollup.config.js
+++ testing/test_bundlers/rollup.config.js
@@ -3,14 +3,14 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-    input: './testing/test_bundlers/src/index_esm.js',
+    input: '../../testing/test_bundlers/src/index_esm.js',
     output: {
-        file: './artifacts/test_bundlers/dist_rollup/main.js',
+        file: '../../artifacts/test_bundlers/dist_rollup/main.js',
         exports: 'auto',
         format: 'es'
     },
     plugins: [nodeResolve({
-        moduleDirectories: ['./artifacts/npm', 'node_modules'],
+        moduleDirectories: ['../../artifacts/npm', 'node_modules'],
         preferBuiltins: false,
     }),
     commonjs()
