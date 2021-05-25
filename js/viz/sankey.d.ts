4945f633806250965f1f76d3da22fafcbcbb3bda
Smirnova Yuliya <Krijovnick@users.noreply.github.com>
2021-05-25 17:54:05 +0300

    Sankey - fix doc comment (#17636)

--- js/viz/sankey.d.ts
+++ js/viz/sankey.d.ts
@@ -725,7 +725,7 @@ export interface dxSankeyNode {
     showTooltip(): void;
     /**
      * @docid
-     * @deprecated
+     * @deprecated dxSankeyNode.label
      * @public
      */
     title?: string;
