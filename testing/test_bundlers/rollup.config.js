65099c2e5fad54613be124e6ef194311b0e5eb6d
CORP\zaborshchikov.e <jacksonrr3@yandex.ru>
2021-05-27 14:52:30 +0300

    complete rollup config

--- testing/test_bundlers/rollup.config.js
+++ testing/test_bundlers/rollup.config.js
@@ -1,10 +1,12 @@
 /* eslint-disable spellcheck/spell-checker */
+/* global  __dirname */
+
+import path from 'path';
 
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-// import nodePolyfills from 'rollup-plugin-node-polyfills';
-// import alias from '@rollup/plugin-alias';
-import replace from '@rollup/plugin-replace';
+import alias from '@rollup/plugin-alias';
+// import replace from '@rollup/plugin-replace';
 
 
 export default {
@@ -12,48 +14,38 @@ export default {
     output: {
         file: './dist/dist_rollup/bundle_esm.js',
         // exports: 'auto',
-        format: 'cjs'
+        format: 'es'
     },
+    external: [ 'rrule' ],
     plugins: [
-        replace({
-            'preventAssignment': true,
-            'process.env.NODE_ENV': JSON.stringify('development')
+        alias({
+            jszip: path.join(__dirname, './node_modules/jszip/dist/jszip.min.js'),
+            globalize$: path.join(__dirname, './node_modules/globalize/dist/globalize.js'),
+            globalize: path.join(__dirname, './node_modules/globalize/dist/globalize'),
+            cldr$: path.join(__dirname, './node_modules/cldrjs/dist/cldr.js'),
+            cldr: path.join(__dirname, './node_modules/cldrjs/dist/cldr')
         }),
         nodeResolve({
+            // mainFields: ['jsnext:main', 'module'],
             moduleDirectories: ['./node_modules/', './node_modules/globalize/dist'],
-            // moduleDirectories: ['./node_modules/'],
-            preferBuiltins: false
+            browser: true
         }),
-        // commonjs(),
+        // nodeResolve(),
         commonjs({
-            sourceMap: true,
-            exclude: [ '../../artifacts/npm/devextreme/node_modules/rrule/dist/esm/src/rrule.js'],
-            // esmExternals: ['./rrule'],
-            // defaultIsModuleExports: true,
-            // requireReturnsDefault: 'preferred',
-            // ignoreDynamicRequires: true,
             include: [
-                '../../artifacts/npm/devextreme/node_modules/jszip/lib/index.js',
-                '../../artifacts/npm/devextreme/node_modules/devexpress-gantt/dist/dx-gantt.js',
-                '../../artifacts/npm/devextreme/node_modules/devextreme-quill/dist/dx-quill.js',
-                '../../node_modules/globalize/dist/node-main.js',
-                '../../artifacts/npm/devextreme/node_modules/rrule/dist/esm/src/rrule.js'
+                './node_modules/rxjs/**',
+                './node_modules/jszip/**',
+                './node_modules/devexpress-gantt/**',
+                './node_modules/devextreme-quill/**',
+                './node_modules/globalize/**',
+                // './node_modules/devextreme/**',
+                // './node_modules/devextreme-angular/**',
+                './node_modules/luxon/**'
             ],
             dynamicRequireTargets: [
-                '../../artifacts/npm/devextreme/node_modules/jszip/lib/index.js',
-                '../../artifacts/npm/devextreme/node_modules/devexpress-gantt/dist/dx-gantt.js',
-                '../../artifacts/npm/devextreme/node_modules/devextreme-quill/dist/dx-quill.js',
-                '../../node_modules/globalize/dist/node-main.js',
-                '../../artifacts/npm/devextreme/node_modules/rrule/dist/esm/src/rrule.js',
-                '../../artifacts/npm/devextreme/node_modules/luxon/src/impl/locale.js',
-                '../../artifacts/npm/devextreme/node_modules/luxon/src/datetime.js'
-            ],
-        })
-        // alias({
-        //     globalize$: './node_modules/globalize/dist/globalize.js',
-        //     globalize: './node_modules/globalize/dist/globalize',
-        //     cldr$: './node_modules/cldrjs/dist/cldr.js',
-        //     cldr: './node_modules/cldrjs/dist/cldr'
-        // })
+                './node_modules/rrule/dist/esm/src/rrule.js'
+            ]
+        }),
+
     ]
 };
--- testing/test_bundlers/vite.config.js
+++ testing/test_bundlers/vite.config.js
@@ -1,25 +1,24 @@
 /* eslint-disable spellcheck/spell-checker */
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
+import rollupConfig from './rollup.config';
 
 module.exports = {
     root: './src',
 
     build: {
-        rollupOptions: {
-            input: './src/modules_esm.js',
-            output: {
-                file: '../../artifacts/test_bundlers/dist_vite/bundle_esm.js',
-                exports: 'auto',
-                format: 'es'
-            },
-            plugins: [
-                nodeResolve({
-                    moduleDirectories: ['node_modules', './node_modules/globalize/dist'],
-                    preferBuiltins: false
-                }),
-                commonjs()
-            ]
-        }
+        rollupOptions: rollupConfig
+        //     input: './src/modules_esm.js',
+        //     output: {
+        //         file: '../../artifacts/test_bundlers/dist_vite/bundle_esm.js',
+        //         exports: 'auto',
+        //         format: 'es'
+        //     },
+        //     plugins: [
+        //         nodeResolve({
+        //             moduleDirectories: ['node_modules', './node_modules/globalize/dist'],
+        //             preferBuiltins: false
+        //         }),
+        //         commonjs()
+        //     ]
+        // }
     }
 };
