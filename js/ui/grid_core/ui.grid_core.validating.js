10ff53a4c9322085598acc3220f1f42820c1cb7f
Nick Mitrokhin <NickMitrokhin@users.noreply.github.com>
2021-06-01 19:30:18 +0300

    virtual scrolling: enables infinite mode (#17650) (#17651)

--- js/ui/grid_core/ui.grid_core.validating.js
+++ js/ui/grid_core/ui.grid_core.validating.js
@@ -54,6 +54,7 @@ const VALIDATION_STATUS = {
 const EDIT_DATA_INSERT_TYPE = 'insert';
 const EDIT_DATA_REMOVE_TYPE = 'remove';
 const VALIDATION_CANCELLED = 'cancel';
+const NEW_SCROLLING_MODE = 'scrolling.newMode';
 
 const validationResultIsValid = function(result) {
     return isDefined(result) && result !== VALIDATION_CANCELLED;
@@ -635,8 +636,11 @@ export const validatingModule = {
                     let result = this.callBase.apply(this, arguments);
                     const { key, pageIndex } = change;
                     const validationData = this.getController('validating')._getValidationData(key);
+                    const scrollingMode = this.option('scrolling.mode');
+                    const virtualMode = scrollingMode === 'virtual';
+                    const appendMode = scrollingMode === 'infinite';
 
-                    if(result && !validationData?.isValid && this.option('scrolling.mode') !== 'virtual') {
+                    if(result && !validationData?.isValid && !virtualMode && !(appendMode && this.option(NEW_SCROLLING_MODE))) {
                         result = pageIndex === this._pageIndex;
                     }
 
