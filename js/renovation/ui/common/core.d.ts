565af5400640184ab62ea196f22a4001cf9e77d6
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2021-06-02 15:37:06 +0300

    Fix compilation

--- js/renovation/ui/common/core.d.ts
+++ js/renovation/ui/common/core.d.ts
@@ -1,21 +1,22 @@
+/* eslint-disable @typescript-eslint/method-signature-style */
 declare module '../../../core/component' {
   interface Component<TProperties> { // eslint-disable-line @typescript-eslint/no-unused-vars
+    _optionsByReference: Record<string, any>;
+    _deprecatedOptions: Record<string, any>;
     _options: {
-      silent: (path: any, value: any) => void;
+      silent(path: any, value: any): void;
     };
-    _createActionByOption: (optionName: string, config: Record<string, any>) => (
+    _createActionByOption(optionName: string, config: Record<string, any>): (
       (...args: any[]) => any
     );
-    _dispose: () => void;
-    _getDefaultOptions: () => Record<string, any>;
-    _init: () => void;
-    _initializeComponent: () => void;
-    _optionChanging: (name: string, value: unknown, prevValue: unknown) => void;
-    _optionChanged: (args: { name: string; value: unknown }) => void;
-    _setOptionsByReference: () => void;
-    _optionsByReference: Record<string, any>;
-    _setDeprecatedOptions: () => void;
-    _deprecatedOptions: Record<string, any>;
+    _dispose(): void;
+    _getDefaultOptions(): Record<string, any>;
+    _init(): void;
+    _initializeComponent(): void;
+    _optionChanging(name: string, value: unknown, prevValue: unknown): void;
+    _optionChanged(args: { name: string; value: unknown }): void;
+    _setOptionsByReference(): void;
+    _setDeprecatedOptions(): void;
   }
 }
 
