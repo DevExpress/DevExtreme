132c009719c7a90343b42e0d22771103efe5a90c
volnyagin <vconst@users.noreply.github.com>
2021-06-03 16:32:36 +0300

    Fix angularjs tests

--- js/renovation/component_wrapper/data_grid.ts
+++ js/renovation/component_wrapper/data_grid.ts
@@ -57,10 +57,9 @@ export default class DataGridWrapper extends Component {
 
     _optionChanging(fullName: string, prevValue: unknown, value: unknown): void {
         super._optionChanging(fullName, prevValue, value);
-        if(this.viewRef) {
+        if(this.viewRef && prevValue !== value) {
             const name = fullName.split(/[.[]/)[0];
             const prevProps = { ...(this.viewRef as DataGridForComponentWrapper).prevProps };
-
             updatePropsImmutable(prevProps, this.option(), name, fullName);
             (this.viewRef as DataGridForComponentWrapper).prevProps = prevProps;
         }
@@ -126,7 +125,10 @@ export default class DataGridWrapper extends Component {
             'onInitialized', 
             'integrationOptions', 
             'adaptColumnWidthByRatio', 
-            'useLegacyKeyboardNavigation'
+            'useLegacyKeyboardNavigation',
+            'templatesRenderAsynchronously',
+            'forceApplyBindings',
+            'nestedComponentOptions',
         ]);
     }
 }
