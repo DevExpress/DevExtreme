ad37b0232763fdb3416682afdc45443053928bfb
CORP\zaborshchikov.e <jacksonrr3@yandex.ru>
2021-06-03 14:56:58 +0300

    update prepare script

--- testing/bundlers/prepare_src.js
+++ testing/bundlers/prepare_src.js
@@ -9,11 +9,11 @@ const globalizeEsm = 'import Globalize from \'globalize\';\n';
 const prefix = 'devextreme/';
 
 const modulesMetadataFilePath = '../../build/gulp/modules_metadata.json';
-
 const modulesCjsFilePath = './src/modules_cjs.js';
 const modulesEsmFilePAth = './src/modules_esm.js';
 
 const excludeModules = ['ui/set_template_engine', 'core/element', 'data/utils', 'viz/export', 'ui/diagram', 'ui/overlay'];
+
 const cjsImports = [ globalizeCjs ];
 const esmImports = [ globalizeEsm ];
 
@@ -21,7 +21,6 @@ const getModuleFromObj = (module, cjsImports, esmImports) => {
     if(module.exports) {
         Object.keys(module.exports).map((expr) => {
             if(!(module.exports[expr].exportAs === 'type')) {
-
                 const path = module.exports[expr].path;
                 let importItem = path.slice(path.lastIndexOf('.') + 1);
 
@@ -42,9 +41,7 @@ const getModuleFromObj = (module, cjsImports, esmImports) => {
 };
 
 try {
-    let modules_metadata = fs.readFileSync(modulesMetadataFilePath);
-    modules_metadata = JSON.parse(modules_metadata);
-    modules_metadata.forEach((module) => {
+    JSON.parse(fs.readFileSync(modulesMetadataFilePath)).forEach((module) => {
         if(!module.isInternal &&
            !(excludeModules.includes(module.name))) {
             getModuleFromObj(module, cjsImports, esmImports);
