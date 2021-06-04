4f441abe50fab13cb43a821f3146f0ac981f2f00
Andrey Ignatovskiy <43685423+LazyLahtak@users.noreply.github.com>
2021-06-04 10:48:30 +0300

    Use Effects instead mount and update to fit with other components

--- js/renovation/component_wrapper/common/template_wrapper.ts
+++ js/renovation/component_wrapper/common/template_wrapper.ts
@@ -1,4 +1,4 @@
-import { BaseInfernoComponent } from '@devextreme/vdom';
+import { InfernoComponent, InfernoEffect } from '@devextreme/vdom';
 // eslint-disable-next-line spellcheck/spell-checker
 import { findDOMfromVNode } from 'inferno';
 import { replaceWith } from '../../../core/utils/dom';
@@ -8,7 +8,7 @@ import { getPublicElement } from '../../../core/element';
 import { removeDifferentElements } from '../utils/utils';
 import Number from '../../../core/polyfills/number';
 import { FunctionTemplate } from '../../../core/templates/function_template';
-import noop from '../../utils/noop';
+import { EffectReturn } from '../../utils/effect_return';
 
 export interface TemplateModel {
   data: Record<string, unknown>;
@@ -21,56 +21,62 @@ interface TemplateWrapperProps {
   transclude?: boolean;
 }
 
-export class TemplateWrapper extends BaseInfernoComponent<TemplateWrapperProps> {
-  clearRenderedContent: () => void = noop;
-
+export class TemplateWrapper extends InfernoComponent<TemplateWrapperProps> {
   constructor(props: TemplateWrapperProps) {
     super(props);
     this.renderTemplate = this.renderTemplate.bind(this);
   }
 
-  renderTemplate(): void {
+  renderTemplate(): EffectReturn {
     // eslint-disable-next-line spellcheck/spell-checker
-    const node = findDOMfromVNode(this.$LI, true) as Element;
-    const parentNode = node.parentNode as Element;
+    const node = findDOMfromVNode(this.$LI, true);
+    if (node) {
+      const { parentNode } = node;
+      if (parentNode) {
+        const $parent = $(parentNode as Element);
+        const $children = $parent.contents();
 
-    const $parent = $(parentNode);
-    const $children = $parent.contents();
+        const {
+          data, index,
+        } = this.props.model ?? { data: {} };
 
-    const {
-      data, index,
-    } = this.props.model ?? { data: {} };
+        Object.keys(data).forEach((name) => {
+          if (data[name] && domAdapter.isNode(data[name])) {
+            data[name] = getPublicElement($(data[name] as Element));
+          }
+        });
 
-    Object.keys(data).forEach((name) => {
-      if (data[name] && domAdapter.isNode(data[name])) {
-        data[name] = getPublicElement($(data[name] as Element));
+        const $result = $(this.props.template.render({
+          container: getPublicElement($parent),
+          transclude: this.props.transclude,
+          ...!this.props.transclude ? { model: data } : {},
+          ...!this.props.transclude && Number.isFinite(index) ? { index } : {},
+        }));
+
+        replaceWith($(node), $result);
+
+        return (): void => {
+          // NOTE: order is important
+          removeDifferentElements($children, $parent.contents());
+          parentNode.appendChild(node);
+        };
       }
-    });
+    }
 
-    const $result = $(this.props.template.render({
-      container: getPublicElement($parent),
-      transclude: this.props.transclude,
-      ...!this.props.transclude ? { model: data } : {},
-      ...!this.props.transclude && Number.isFinite(index) ? { index } : {},
-    }));
-
-    replaceWith($(node), $result);
-
-    this.clearRenderedContent = (): void => {
-      // NOTE: order is important
-      removeDifferentElements($children, $parent.contents());
-      parentNode.appendChild(node);
-    };
+    return undefined;
   }
 
-  componentDidMount(): void {
-    this.renderTemplate();
+  createEffects(): InfernoEffect[] {
+    return [new InfernoEffect(this.renderTemplate, [this.props.template, this.props.model])];
   }
 
-  componentDidUpdate(): void {
-    this.clearRenderedContent();
-    this.renderTemplate();
+  updateEffects(): void {
+    this._effects[0].update([this.props.template, this.props.model]);
   }
+
+  // NOTE: Prevent nodes clearing on unmount.
+  //       Nodes will be destroyed by inferno on markup update
+  componentWillUnmount(): void { }
 
   render(): JSX.Element | null {
     return null;
