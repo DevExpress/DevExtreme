8f7f20398b91e3d8f72594f0b0f26a24f72755b5
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2021-06-04 15:31:58 +0300

    Scheduler - Screenshot tester should wait appointments to appear from both schedulers (#959)

--- Demos/Scheduler/SignalRService/test-code.js
+++ Demos/Scheduler/SignalRService/test-code.js
@@ -1,7 +1,9 @@
 ﻿new Promise(resolve => {
     var i = 0;
     const interval = setInterval(() => {
-        if(i > 50 || $('.dx-scheduler-appointment-title').length) {
+        const appointmentCountInScheduler1 = $('#scheduler1 .dx-scheduler-appointment').length;
+        const appointmentCountInScheduler2 = $('#scheduler2 .dx-scheduler-appointment').length;
+        if(i > 50 || (appointmentCountInScheduler1 > 0 && appointmentCountInScheduler2 > 0)) {
             clearInterval(interval);
             resolve();
         }
