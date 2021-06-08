b65fc3aef20208b69021cc7cfb608ec7bc420d52
Andrey Ignatovskiy <43685423+LazyLahtak@users.noreply.github.com>
2021-06-08 13:38:14 +0300

    Add test

--- js/renovation/component_wrapper/common/__tests__/component.test.tsx
+++ js/renovation/component_wrapper/common/__tests__/component.test.tsx
@@ -825,6 +825,22 @@ describe('templates and slots', () => {
 
     expect(() => $('#component').dxTemplatedTestWidget({ template })).not.toThrowError();
   });
+
+  it('should have default templates for jQuery', () => {
+    const instance = $('#component')
+      .dxTemplatedTestWidget()
+      .dxTemplatedTestWidget('instance');
+
+    const templateNames = instance._propsInfo.templates;
+    const defaultTemplates = instance._templatesInfo;
+    const innerOptions = instance._props;
+    const publicOptions = instance.option();
+
+    templateNames.forEach((name) => {
+      expect(innerOptions[name]).toBe(defaultTemplates[name]);
+      expect(publicOptions[name]).toBe(null);
+    });
+  });
 });
 
 describe('events/actions', () => {
