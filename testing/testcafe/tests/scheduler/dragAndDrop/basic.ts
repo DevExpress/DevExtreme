f5c0899e5d4732e386cc1f425a2b9abfadb5b0e1
Denis Gradoboev <idenis.gradoboev@yandex.com>
2021-06-08 15:16:42 +0300

    Add test for dnd from another area

--- testing/testcafe/tests/scheduler/dragAndDrop/basic.ts
+++ testing/testcafe/tests/scheduler/dragAndDrop/basic.ts
@@ -1,3 +1,4 @@
+import { Selector } from 'testcafe';
 import { dataSource } from './init/widget.data';
 import createScheduler from './init/widget.setup';
 import url from '../../../helpers/getPageUrl';
@@ -119,4 +120,28 @@ test('Drag recurrent appointment occurrence from collector (T832887)', async (t)
     recurrenceRule: 'FREQ=DAILY',
   }],
   currentDate: new Date(2019, 2, 26),
+}));
+
+fixture`Drag-n-drop from another draggable area`
+  .page(url(__dirname, './pages/containerWithDnD.html'));
+
+test.only('Drag-n-drop an appointment when "cellDuration" changes dynamically', async (t) => {
+  const scheduler = new Scheduler('#container');
+
+  scheduler.option('cellDuration', 10);
+
+  await t
+    .dragToElement(Selector('.item'), scheduler.getDateTableCell(0, 0))
+    .expect(scheduler.getAppointmentByIndex(0).date.time)
+    .eql('9:00 AM - 9:10 AM');
+}).before(async () => createScheduler({
+  views: ['week'],
+  currentView: 'week',
+  appointmentDragging: {
+    group: 'group',
+    onAdd(e) {
+      e.component.addAppointment(e.itemData);
+      e.itemElement.remove();
+    },
+  },
 }));
