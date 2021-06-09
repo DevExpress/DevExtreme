3a49048bcbef1e862f147df04cbe904746cd4905
Evgeniy Aryamnov <earyamnow@mail.ru>
2021-06-09 16:40:50 +0300

    Add testcafe test

--- testing/testcafe/tests/scheduler/dragAndDrop/appointmentCollector.ts
+++ testing/testcafe/tests/scheduler/dragAndDrop/appointmentCollector.ts
@@ -1,7 +1,9 @@
 import { appointmentCollectorData } from './init/widget.data';
 import createScheduler from './init/widget.setup';
+import createWidget from '../../../helpers/createWidget';
 import url from '../../../helpers/getPageUrl';
 import Scheduler from '../../../model/scheduler';
+import { createScreenshotsComparer } from '../../../helpers/screenshot-comparer';
 
 fixture`Drag-and-drop behaviour for the appointment tooltip`
   .page(url(__dirname, '../../container.html'));
@@ -58,4 +60,46 @@ test('Drag-n-drop in same table cell', async (t) => {
   dataSource: appointmentCollectorData,
   maxAppointmentsPerCell: 2,
   width: 1000,
+}));
+
+test('Drag-n-drop to the cell below should work in month view (T1005115)', async (t) => {
+  const scheduler = new Scheduler('#container');
+  const collector = scheduler.getAppointmentCollector('1 more');
+  const { appointmentTooltip } = scheduler;
+  const appointmentTooltipItem = appointmentTooltip.getListItem('Approve Personal Computer Upgrade Plan');
+
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+  await t
+    .click(collector.element)
+    .dragToElement(
+      appointmentTooltipItem.element,
+      scheduler.getDateTableCell(1, 3),
+      { speed: 0.1 },
+    )
+
+    .expect(await takeScreenshot('drag-n-drop-from-tooltip-to-cell-below-in-month.png', scheduler.workSpace))
+    .ok()
+
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => createWidget('dxScheduler', {
+  currentDate: new Date(2019, 3, 1),
+  views: ['month'],
+  currentView: 'month',
+  dataSource: [{
+    text: 'Website Re-Design Plan',
+    startDate: new Date(2019, 3, 3, 9, 30),
+    endDate: new Date(2019, 3, 3, 11, 30),
+  }, {
+    text: 'Approve Personal Computer Upgrade Plan',
+    startDate: new Date(2019, 3, 3, 10, 0),
+    endDate: new Date(2019, 3, 3, 11, 0),
+  }, {
+    text: 'Install New Database',
+    startDate: new Date(2019, 3, 3, 9, 45),
+    endDate: new Date(2019, 3, 3, 11, 15),
+  }],
+  maxAppointmentsPerCell: 2,
+  height: 800,
 }));
