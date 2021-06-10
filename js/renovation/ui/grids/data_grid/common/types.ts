79ce53a597081ccdb1b43591cf0bbea4e11b6792
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2021-06-10 13:06:04 +0300

    Extract private augmentation for Component (#17750)

--- js/renovation/ui/grids/data_grid/common/types.ts
+++ js/renovation/ui/grids/data_grid/common/types.ts
@@ -1,14 +1,15 @@
 import dxDataGrid from '../../../../../ui/data_grid';
+import { ComponentExt } from '../../../common/component';
 import type { DataGridProps } from './data_grid_props';
 
-export interface GridInstance extends dxDataGrid {
+export interface GridInstance extends dxDataGrid, ComponentExt {
   isReady: () => boolean;
-  getView: (name: string) => any;
-  getController: (name: string) => any;
+  getView: (name: string) => any; // eslint-disable-line @typescript-eslint/no-explicit-any
+  getController: (name: string) => any; // eslint-disable-line @typescript-eslint/no-explicit-any
   resize: () => void;
   updateDimensions: (checkSize?: boolean) => void;
   isScrollbarVisible: () => boolean;
-  getTopVisibleRowData: () => any;
+  getTopVisibleRowData: () => any; // eslint-disable-line @typescript-eslint/no-explicit-any
   getScrollbarWidth: (isHorizontal: boolean) => number;
 }
 
