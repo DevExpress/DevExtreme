65d60a506bed06152ec108aecdf6210eb9b7a6b3
alexander-kotov-dx <alexander.kotov@devexpress.com>
2021-06-10 20:11:14 +0300

    module refactoring

--- js/ui/html_editor/modules/tableResizing.js
+++ js/ui/html_editor/modules/tableResizing.js
@@ -1,7 +1,7 @@
 import $ from '../../../core/renderer';
 import eventsEngine from '../../../events/core/events_engine';
 import { isDefined } from '../../../core/utils/type';
-// import { addNamespace } from '../../../events/utils/index';
+import { addNamespace } from '../../../events/utils/index';
 import _windowResizeCallbacks from '../../../core/utils/resize_callbacks';
 import { move } from '../../../animation/translator';
 import { getBoundingRect } from '../../../core/utils/position';
@@ -16,11 +16,9 @@ const DEFAULT_MIN_COLUMN_WIDTH = 40;
 
 const DRAGGABLE_ELEMENT_OFFSET = 1;
 
-// const MODULE_NAMESPACE = 'dxHtmlTableResizingModule';
+const MODULE_NAMESPACE = 'dxHtmlTableResizingModule';
 
-// const KEYDOWN_EVENT = addNamespace('keydown', MODULE_NAMESPACE);
-// const SCROLL_EVENT = addNamespace('scroll', MODULE_NAMESPACE);
-// const MOUSEDOWN_EVENT = addNamespace('mousedown', MODULE_NAMESPACE);
+const POINTERDOWN_EVENT = addNamespace('dxpointerdown', MODULE_NAMESPACE);
 
 export default class TableResizingModule extends BaseModule {
     constructor(quill, options) {
@@ -36,68 +34,57 @@ export default class TableResizingModule extends BaseModule {
                 this._attachResizerTimeout = setTimeout(() => {
 
                     const $tables = this._findTables();
-                    // _createResizeFrames
                     if($tables.length) {
-                        // this._createTableResizeFrame();
                         this._createResizeFrames($tables);
-
-
                         this._updateFramesPositions();
                         this._updateFramesSeparators();
 
                         quill.on('text-change', () => {
-                            this._updateFramesPositions(this._$columnResizeFrame);
+                            this._updateFramesPositions();
                         });
                     }
                 });
             });
 
             _windowResizeCallbacks.add(() => {
-                setTimeout(() => {
-                    // this._fixColumnsWidth(this._tableResizeFrames[0].$table);
+                this._windowResizeTimeout = setTimeout(() => {
                     this._updateFramesPositions();
                     this._updateFramesSeparators();
-                }, 100);
-
+                });
             });
         }
     }
 
-    // _isAllowedTarget(targetElement) {
-    //     return this._isTableElement(targetElement);
-    // }
-
-    // _isTableElement(targetElement) {
-    //     let result = false;
-    //     ['TABLE', 'TBODY', 'THEAD', 'TFOOT', 'TD', 'TR', 'TH'].forEach((tagName) => {
-    //         if(targetElement.tagName.toUpperCase() === tagName) {
-    //             result = true;
-    //             return false;
-    //         }
-    //     });
-
-    //     return result;
-    // }
-
-    // _findTableRoot(targetElement) {
-    //     return $(targetElement).parents('table').get(0);
-    // }
     _findTables() {
         return $(this.editorInstance._getQuillContainer()).find('table');
+    }
+
+    _createResizeFrames($tables) {
+        $tables.each((index, $item) => {
+            this._tableResizeFrames[index] = {
+                $frame: this._createTableResizeFrame($item),
+                $table: $($item),
+                index: index
+            };
+        });
+    }
+
+    _createTableResizeFrame() {
+        return $('<div>')
+            .addClass(DX_COLUMN_RESIZE_FRAME_CLASS)
+            .appendTo(this.editorInstance._getQuillContainer());
+
     }
 
     _updateFramesPositions() {
         each(this._tableResizeFrames, (index, tableResizeFrame) => {
             this._updateFramePosition(tableResizeFrame.$table, tableResizeFrame.$frame);
         });
-
     }
 
     _updateFramePosition($table, $frame) {
-        // const $currentTable = $table || $(this.editorInstance._getQuillContainer()).find('table').get(0);
         const { height, width, top: targetTop, left: targetLeft } = getBoundingRect($table.get(0));
         const { top: containerTop, left: containerLeft } = getBoundingRect(this.quill.root);
-        // const borderWidth = this._getBorderWidth();
 
         $frame
             .css({
@@ -109,90 +96,82 @@ export default class TableResizingModule extends BaseModule {
         move($frame, { left: 0, top: 0 });
     }
 
-    _createResizeFrames($tables) {
-        $tables.each((index, $item) => {
-            this._tableResizeFrames[index] = {
-                $frame: this._createTableResizeFrame($item),
-                $table: $($item),
-                index: index
-            };
-
-            // $('<div>').addClass('test00000').appendTo($item);
-
-
-        });
-    }
-
-    _createTableResizeFrame() {
-        // console.log('_createTableResizeFrame');
-        return $('<div>')
-            .addClass(DX_COLUMN_RESIZE_FRAME_CLASS)
-            .appendTo(this.editorInstance._getQuillContainer());
-
-    }
-
     _updateFramesSeparators() {
         each(this._tableResizeFrames, (index, frame) => {
-
             this._updateFrameSeparators(frame, 'vertical');
             this._updateFrameSeparators(frame, 'horizontal');
         });
+    }
+
+    _isDraggable($element) {
+        return $element.hasClass('dx-draggable') && $element.is(':visible');
+    }
+
+    _removeOldDraggable($currentLineSeparator, lineResizerClass) {
+        if(this._isDraggable($currentLineSeparator)) {
+            const draggable = $($currentLineSeparator).dxDraggable('instance');
+            draggable.dispose();
+            $($currentLineSeparator).addClass(lineResizerClass);
+        }
+    }
+
+    _getDirectionInfo(direction) {
+        if(direction === 'vertical') {
+            return {
+                lineResizerClass: DX_ROW_RESIZER_CLASS,
+                getSizeFunction: 'outerHeight',
+                positionCoordinate: 'top',
+                positionStyleProperty: 'height',
+                positionCoordinateName: 'y'
+            };
+        } else {
+            return {
+                lineResizerClass: DX_COLUMN_RESIZER_CLASS,
+                getSizeFunction: 'outerWidth',
+                positionCoordinate: 'left',
+                positionStyleProperty: 'width',
+                positionCoordinateName: 'x'
+            };
+        }
     }
 
     _updateFrameSeparators(frame, direction) {
         const $determinantElements = this._getTableDeterminantElements(frame.$table, direction);
         const determinantElementsCount = $determinantElements.length;
         const determinantElementsSeparatorsCount = determinantElementsCount - 1;
-        const lineResizerClass = direction === 'vertical' ? DX_ROW_RESIZER_CLASS : DX_COLUMN_RESIZER_CLASS;
-        const getSizeFunction = direction === 'vertical' ? 'outerHeight' : 'outerWidth';
-        const positionCoordinate = direction === 'vertical' ? 'top' : 'left';
-        // const controlledElements = this._getControlledElements(); // th or td
-        const $lineSeparators = frame.$frame.find(`.${lineResizerClass}`);
+        const directionInfo = this._getDirectionInfo(direction);
+        const lineSeparators = frame.$frame.find(`.${directionInfo.lineResizerClass}`);
         const styleOptions = {
             transform: 'none'
         };
 
-        // this._updateFramePosition(this._$columnResizeFrame);
-
         let currentPosition = 0;
-        for(let i = 0; i <= determinantElementsSeparatorsCount; i++) { //  headers
-            currentPosition += $determinantElements.eq(i)[getSizeFunction]();
-            // $lineSeparators[i] = $lineSeparators[i]
+        for(let i = 0; i <= determinantElementsSeparatorsCount; i++) {
+            currentPosition += $determinantElements.eq(i)[directionInfo.getSizeFunction]();
 
-            if(!isDefined($lineSeparators[i])) {
-                $lineSeparators[i] = $('<div>')
-                    .addClass(lineResizerClass)
+            if(!isDefined(lineSeparators[i])) {
+                lineSeparators[i] = $('<div>')
+                    .addClass(directionInfo.lineResizerClass)
                     .appendTo(frame.$frame)
                     .get(0);
             }
 
-            const $currentLineSeparator = $($lineSeparators[i]);
+            const $currentLineSeparator = $(lineSeparators[i]);
 
-            if($currentLineSeparator.hasClass('dx-draggable') && $currentLineSeparator.is(':visible')) {
-                const draggable = $($currentLineSeparator).dxDraggable('instance');
+            this._removeOldDraggable($currentLineSeparator, directionInfo.lineResizerClass);
 
-                draggable.dispose();
-
-                $($currentLineSeparator).addClass(lineResizerClass);
-            }
-
-            styleOptions[positionCoordinate] = currentPosition - DRAGGABLE_ELEMENT_OFFSET;
-
+            styleOptions[directionInfo.positionCoordinate] = currentPosition - DRAGGABLE_ELEMENT_OFFSET;
             $($currentLineSeparator).css(styleOptions);
-            // console.log(direction);
-            // console.log(styleOptions);
 
-            this._attachColumnSeparatorEvents($lineSeparators[i], $determinantElements, i, frame, direction);
-
+            const attachSeparatorData = {
+                lineSeparator: lineSeparators[i],
+                index: i,
+                $determinantElements,
+                frame,
+                direction
+            };
+            this._attachColumnSeparatorEvents(attachSeparatorData);
         }
-
-        // for(let i = 0; i <= columnsResizingElementsCount; i++) { //  headers
-        //     $lineSeparators[i] = $('<div>')
-        //         .addClass('dx-table-column-resizer')
-        //         .appendTo(this._$columnResizeFrame);
-        // }
-
-        // eventsEngine.on(this.quill.root, SCROLL_EVENT, this._framePositionChangedHandler);
     }
 
     _getTableDeterminantElements($table, direction) {
@@ -203,126 +182,71 @@ export default class TableResizingModule extends BaseModule {
         }
     }
 
-    _attachColumnSeparatorEvents(lineSeparator, $determinantElements, index, frame, direction) {
-
-        eventsEngine.on(lineSeparator, 'dxpointerdown', () => {
-            // if(this._currentDraggableElement) {
-            // const isTheSameSeparator = this._currentDraggableElement._$element.get(0) === $($columnSeparator).get(0);
-            // if(!isTheSameSeparator) {
-            // const element = this._currentDraggableElement._$element;
-            // const leftStyle = $(element).css('left');
-            // this._currentDraggableElement.dispose();
-            // $(element).css('left', leftStyle).addClass(DX_COLUMN_RESIZER_CLASS);
-            // } else {
-            //     return;
-            // }
-            // }
-
-            this._createDraggableElement(lineSeparator, $determinantElements, index, frame, direction);
+    _attachColumnSeparatorEvents(options) {
+        eventsEngine.on(options.lineSeparator, POINTERDOWN_EVENT, () => {
+            this._createDraggableElement(options);
         });
     }
 
-    _createDraggableElement(lineSeparator, $determinantElements, index, frame, direction) {
-        let nextLineSize;
-        // let startDragPosition;
-        let startLineSize;
-
-        // console.log(direction);
-
-        const getSizeFunction = direction === 'vertical' ? 'outerHeight' : 'outerWidth';
-        const positionStyleProperty = direction === 'vertical' ? 'height' : 'width';
-        // const positionCoordinate = direction === 'vertical' ? 'top' : 'left';
-        const positionCoordinateName = direction === 'vertical' ? 'y' : 'x';
+    _createDraggableElement({ lineSeparator, $determinantElements, index, frame, direction }) {
+        const directionInfo = this._getDirectionInfo(direction);
         const currentDirection = direction;
-
-        // const boundaryConfig = ;
 
         this._currentDraggableElement = this.editorInstance._createComponent(lineSeparator, Draggable, {
             contentTemplate: null,
-            // boundary: frame.$frame,
+            boundary: this.editorInstance._getQuillContainer(),
             allowMoveByClick: false,
             dragDirection: direction,
             onDragMove: ({ component, event }) => {
-                // debugger;
-                // const newPosition = startDragPosition + event.offset[positionCoordinateName];
-                // console.log('move ' + newPosition);
-                // console.log(newPosition);
-
-                const currentLineNewSize = startLineSize + event.offset[positionCoordinateName];
+                const currentLineNewSize = this._startLineSize + event.offset[directionInfo.positionCoordinateName];
 
                 if(currentDirection === 'horizontal') {
-
-                    const nextColumnNewSize = nextLineSize && nextLineSize - event.offset[positionCoordinateName];
+                    const nextColumnNewSize = this._nextLineSize && this._nextLineSize - event.offset[directionInfo.positionCoordinateName];
                     const isCurrentColumnHasEnoughPlace = currentLineNewSize >= this._minColumnWidth;
-                    const isNextColumnHasEnoughPlace = !nextLineSize || nextColumnNewSize >= this._minColumnWidth;
+                    const isNextColumnHasEnoughPlace = !this._nextLineSize || nextColumnNewSize >= this._minColumnWidth;
 
                     if(isCurrentColumnHasEnoughPlace && isNextColumnHasEnoughPlace) {
-                        $determinantElements.eq(index).attr(positionStyleProperty, currentLineNewSize + 'px');
+                        $determinantElements.eq(index).attr(directionInfo.positionStyleProperty, currentLineNewSize + 'px');
 
-                        if(nextLineSize) {
-                            $determinantElements.eq(index + 1).attr(positionStyleProperty, nextColumnNewSize + 'px');
+                        if(this._nextLineSize) {
+                            $determinantElements.eq(index + 1).attr(directionInfo.positionStyleProperty, nextColumnNewSize + 'px');
                         }
                     }
                 } else {
-                    // const isCurrentRowHasEnoughPlace = currentLineNewSize >= this._minRowHeight;
-
                     const newHeight = Math.max(currentLineNewSize, this._minRowHeight);
-                    // console.log(newHeight);
-
-                    // if(isCurrentRowHasEnoughPlace) {
-                    $determinantElements.eq(index).attr(positionStyleProperty, newHeight + 'px');
-                    // }
+                    $determinantElements.eq(index).attr(directionInfo.positionStyleProperty, newHeight + 'px');
                 }
 
                 this._updateFramePosition(frame.$table, frame.$frame);
-
-
-                // console.log(event.offset[positionCoordinateName]);
-                // this._updateByDrag = true;
-                // const $alphaChannelHandle = this._$alphaChannelHandle;
-                // const alphaChannelHandlePosition = locate($alphaChannelHandle).left + this._alphaChannelHandleWidth / 2;
-                // this._saveValueChangeEvent(event);
-                // this._calculateColorTransparencyByScaleWidth(alphaChannelHandlePosition);
             },
             onDragStart: () => {
-                // startDragPosition = parseInt($(lineSeparator).css(positionCoordinate).replace('px', ''));
-                this._fixColumnsWidth(frame.$table);
-                frame.$table.css('width', 'auto');
-                startLineSize = parseInt($($determinantElements[index])[getSizeFunction]());
-                nextLineSize = 0;
+                this._fixColumnsWidth(frame);
+                this._startLineSize = parseInt($($determinantElements[index])[directionInfo.getSizeFunction]());
+                this._nextLineSize = 0;
                 if($determinantElements[index + 1]) {
-                    nextLineSize = parseInt($($determinantElements[index + 1])[getSizeFunction]());
+                    this._nextLineSize = parseInt($($determinantElements[index + 1])[directionInfo.getSizeFunction]());
                 }
-
-                // if($columns[index - 1]) {
-                //     this._previousColumnWidth = parseInt($($columns[index - 1]).outerWidth());
-                // }
-
-                // console.log('start ' + this._startDragPosition);
             },
             onDragEnd: () => {
-                // this._updateFrameSeparators(frame, 'horizontal'); // set positions only
-                // this._updateFrameSeparators(frame, 'vertical');
-                // this._currentDraggableElement.dxDraggable('instance')._resetDragElement();
                 this._updateFramesPositions();
                 this._updateFramesSeparators();
             }
         });
     }
 
-    _fixColumnsWidth($table) {
-        const determinantElements = this._getTableDeterminantElements($table);
+    _fixColumnsWidth(frame) {
+        const determinantElements = this._getTableDeterminantElements(frame.$table);
 
         each(determinantElements, (index, element) => {
             const columnWidth = $(element).outerWidth();
             $(element).attr('width', columnWidth >= DEFAULT_MIN_COLUMN_WIDTH ? columnWidth : DEFAULT_MIN_COLUMN_WIDTH);
         });
+
+        frame.$table.css('width', 'auto');
     }
 
     clean() {
-        // this._detachEvents();
         clearTimeout(this._attachResizerTimeout);
-        // this._$resizeFrame.remove();
-        // this._$resizeFrame = undefined;
+        clearTimeout(this._windowResizeTimeout);
     }
 }
