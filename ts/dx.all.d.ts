fa69198e023f3898f5b24fda095bf98e1e86e42d
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2021-06-17 23:19:08 +0300

    slideOut, slideOutView: fix doc comments for toggleMenuVisibility method (#17930)

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -17536,7 +17536,7 @@ declare module DevExpress.ui {
      * [descr:dxSlideOut.toggleMenuVisibility(showing)]
      */
     toggleMenuVisibility(
-      showing: boolean
+      showing?: boolean
     ): DevExpress.core.utils.DxPromise<void>;
   }
   module dxSlideOut {
@@ -17681,9 +17681,11 @@ declare module DevExpress.ui {
      */
     showMenu(): DevExpress.core.utils.DxPromise<void>;
     /**
-     * [descr:dxSlideOutView.toggleMenuVisibility()]
+     * [descr:dxSlideOutView.toggleMenuVisibility(showing)]
      */
-    toggleMenuVisibility(): DevExpress.core.utils.DxPromise<void>;
+    toggleMenuVisibility(
+      showing?: boolean
+    ): DevExpress.core.utils.DxPromise<void>;
   }
   module dxSlideOutView {
     export type DisposingEvent = DevExpress.events.EventInfo<dxSlideOutView>;
