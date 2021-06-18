1446a8c27133d9777df7a5088c428fe3e4981993
Andrey Churkin <alam17@mail.ru>
2021-06-18 14:25:33 +0300

    Renovation: add ResizableContainer component (#17378)
    * Renovation: add ResizableContainer component
    
    * fix remark

--- js/events/core/events_engine.d.ts
+++ js/events/core/events_engine.d.ts
@@ -1,8 +1,9 @@
 type EventsEngineType = {
-    on: (element: any, eventName: any, handler: any) => void;
+    on: (element: any, eventName: any, handler: any, options?: any) => void;
     off: (element: any, eventName: any, handler: any) => void;
     set: (eventEngine: any) => void;
-};
+}
+
 declare const eventsEngine: EventsEngineType;
 export declare function set(eventEngine: any): void;
 export default eventsEngine;
