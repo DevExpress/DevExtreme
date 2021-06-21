ecf8c96ceb7b5644e0d64fd52686956ced2f81c3
Alexey Serov <alexey.serov@devexpress.com>
2021-06-21 10:25:40 +0300

    update type.d.ts

--- js/core/utils/type.d.ts
+++ js/core/utils/type.d.ts
@@ -1,12 +1,10 @@
-export declare function type(object: any): string;
-
 export declare function isBoolean(object: any): object is boolean;
 
 export declare function isDate(object: any): object is Date;
 
 export declare function isString(object: any): object is string;
 
-export declare function type(object: any): 'object' | 'array' | 'string' | 'date' | 'null';
+export declare function type(object: any): 'object' | 'array' | 'string' | 'date' | 'null' | string;
 
 export declare function isNumeric(object: any): object is number;
 
@@ -18,11 +16,11 @@ export declare function isDefined<T>(object: T): object is NonNullable<T>;
 
 export declare function isObject<T = {}>(object: unknown): object is object;
 
-export declare function isEmptyObject<T = {}>(object: T): boolean;
+export declare function isEmptyObject(object: any): boolean;
 
 export declare function isPlainObject(object: any): object is { [key: string]: any };
 
-export declare function isPrimitive<T>(value: T): value is Exclude<T, Function>&Exclude<T, object>&Exclude<T, any[]>;
+export declare function isPrimitive(value: any): value is string | number |  bigint | boolean | undefined | symbol | null;
 
 export declare function isWindow(object: any): object is (Window & typeof globalThis);
 
