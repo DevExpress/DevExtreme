59568c4fc4075adabf1a12aa30c9fe9d255c0ff4
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2021-06-26 01:18:21 +0300

    fix gant tests

--- js/ui/list/ui.list.base.js
+++ js/ui/list/ui.list.base.js
@@ -1049,7 +1049,7 @@ export const ListBase = CollectionWidget.inherit({
     scrollToItem: function(itemElement) {
         const $item = this._editStrategy.getItemElement(itemElement);
 
-        this._scrollView.scrollToElement($item.get(0));
+        this._scrollView.scrollToElement($item?.get(0));
     },
 
     _dimensionChanged: function() {
