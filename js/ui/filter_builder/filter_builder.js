6318ed951ded28c75c7753d096424feb0cfdb9d6
Alexander Bezborodov <alexander.bezborodov@devexpress.com>
2021-06-25 13:55:10 +0300

    Security: Replace HTML insertion with text insertion where the markup is not needed(XSS) (#18081)

--- js/ui/filter_builder/filter_builder.js
+++ js/ui/filter_builder/filter_builder.js
@@ -321,7 +321,7 @@ const FilterBuilder = Widget.inherit({
                     onItemClick: (e) => {
                         if(groupMenuItem !== e.itemData) {
                             setGroupValue(criteria, e.itemData.value);
-                            $operationButton.html(e.itemData.text);
+                            $operationButton.text(e.itemData.text);
                             groupMenuItem = e.itemData;
                             this._updateFilter();
                         }
