0d72c6fd48ee83d2f9551b146a1b2aea862240da
Alexander Kozlovskiy (DevExpress) <kozlovskiy.alexander@corp.devexpress.com>
2021-06-28 00:14:35 +0300

    Fix bug with excess datasource load when search non-existant item (T1001808)

--- js/ui/collection/ui.collection_widget.base.js
+++ js/ui/collection/ui.collection_widget.base.js
@@ -555,6 +555,7 @@ const CollectionWidget = Widget.inherit({
     },
 
     _dataSourceChangedHandler: function(newItems) {
+        !newItems.length && this.option('selectedItems', []);
         const items = this.option('items');
         if(this._initialized && items && this._shouldAppendItems()) {
             this._renderedItemsCount = items.length;
