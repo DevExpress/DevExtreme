c252f20ad9cb984a0a4eb37fa6b5485a936eb0db
kotov.alexander <alexander.kotov@devexpress.com>
2021-06-30 15:04:02 +0300

    HtmlEditor - support base table resizing (#17875)

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -13162,6 +13162,10 @@ declare module DevExpress.ui {
      */
     mediaResizing?: dxHtmlEditorMediaResizing;
     /**
+     * [descr:dxHtmlEditorOptions.tableResizing]
+     */
+    tableResizing?: dxHtmlEditorTableResizing;
+    /**
      * [descr:dxHtmlEditorOptions.mentions]
      */
     mentions?: Array<dxHtmlEditorMention>;
@@ -13197,6 +13201,24 @@ declare module DevExpress.ui {
      * [descr:dxHtmlEditorOptions.stylingMode]
      */
     stylingMode?: 'outlined' | 'underlined' | 'filled';
+  }
+  /**
+   * [descr:dxHtmlEditorTableResizing]
+   * @deprecated Attention! This type is for internal purposes only. If you used it previously, please describe your scenario in the following GitHub Issue, and we will suggest a public alternative: {@link https://github.com/DevExpress/DevExtreme/issues/17885|Internal Types}.
+   */
+  export interface dxHtmlEditorTableResizing {
+    /**
+     * [descr:dxHtmlEditorTableResizing.minColumnWidth]
+     */
+    minColumnWidth?: number;
+    /**
+     * [descr:dxHtmlEditorTableResizing.minRowHeight]
+     */
+    minRowHeight?: number;
+    /**
+     * [descr:dxHtmlEditorTableResizing.enabled]
+     */
+    enabled?: boolean;
   }
   /**
    * [descr:dxHtmlEditorToolbar]
