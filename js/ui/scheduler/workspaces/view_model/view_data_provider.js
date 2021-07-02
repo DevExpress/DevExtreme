fe0a4eb64c5de9b54dded27f0aa1c52e13442003
Evgeniy Aryamnov <earyamnow@mail.ru>
2021-07-02 13:25:27 +0300

    Fix tests

--- js/ui/scheduler/workspaces/view_model/view_data_provider.js
+++ js/ui/scheduler/workspaces/view_model/view_data_provider.js
@@ -7,17 +7,19 @@ import { ViewDataGeneratorTimelineMonth } from './view_data_generator_timeline_m
 
 const getViewDataGeneratorByViewType = (viewType) => {
     switch(viewType) {
+        case VIEWS.MONTH:
+            return new ViewDataGeneratorMonth();
+        case VIEWS.TIMELINE_MONTH:
+            return new ViewDataGeneratorTimelineMonth();
         case VIEWS.DAY:
         case VIEWS.WEEK:
         case VIEWS.WORK_WEEK:
         case VIEWS.TIMELINE_DAY:
         case VIEWS.TIMELINE_WEEK:
         case VIEWS.TIMELINE_WORK_WEEK:
+        default:
             return new ViewDataGenerator();
-        case VIEWS.MONTH:
-            return new ViewDataGeneratorMonth();
-        case VIEWS.TIMELINE_MONTH:
-            return new ViewDataGeneratorTimelineMonth();
+
     }
 };
 
