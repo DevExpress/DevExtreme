4a430287e359e7cbae34377915a89dc55907c30f
ksercs <ksercs0@gmail.com>
2021-07-06 14:54:39 +0300

    support IE

--- testing/tests/DevExpress.ui.widgets.editors/dropDownBox.tests.js
+++ testing/tests/DevExpress.ui.widgets.editors/dropDownBox.tests.js
@@ -378,7 +378,7 @@ QUnit.module('common', moduleConfig, () => {
 
                 byKey: (key) => {
                     const deferred = $.Deferred();
-                    const filter = () => this.items.find(item => item.id === key);
+                    const filter = () => this.items.filter(item => item.id === key)[0];
                     if(this.callCount === 0) {
                         setTimeout(() => {
                             deferred.resolve(filter());
