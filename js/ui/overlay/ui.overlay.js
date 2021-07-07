30fb2eca4b8adacd4e1019165da6642aed1ff233
ksercs <ksercs0@gmail.com>
2021-07-07 20:09:48 +0300

    fix SSR

--- js/ui/overlay/ui.overlay.js
+++ js/ui/overlay/ui.overlay.js
@@ -355,6 +355,10 @@ const Overlay = Widget.inherit({
     },
 
     _initContentResizeObserver: function() {
+        if(!hasWindow()) {
+            return;
+        }
+
         this._contentResizeObserver = new window.ResizeObserver(() => {
             if(!this._shouldSkipContentResizeHandler) {
                 this._dimensionChanged();
@@ -364,11 +368,11 @@ const Overlay = Widget.inherit({
     },
 
     _observeContentResize: function() {
-        this._contentResizeObserver.observe(this._$content.get(0));
+        this._contentResizeObserver?.observe(this._$content.get(0));
     },
 
     _unobserveContentResize: function() {
-        this._contentResizeObserver.unobserve(this._$content.get(0));
+        this._contentResizeObserver?.unobserve(this._$content.get(0));
     },
 
     _initMarkup() {
@@ -1357,7 +1361,7 @@ const Overlay = Widget.inherit({
 
         this._toggleSafariScrolling();
         zIndexPool.remove(this._zIndex);
-        this._contentResizeObserver.disconnect();
+        this._contentResizeObserver?.disconnect();
         this._contentResizeObserver = undefined;
         this._$wrapper.remove();
         this._$content.remove();
