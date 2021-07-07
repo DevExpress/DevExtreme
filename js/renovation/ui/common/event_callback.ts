3dd79220e5d8a3947fd4aee79e42885cd09350fd
ignatov <ignatovdan@gmail.com>
2021-07-07 17:33:41 +0300

    Add Toolbar.item.widget property wrapper

--- js/renovation/ui/common/event_callback.ts
+++ js/renovation/ui/common/event_callback.ts
@@ -1,2 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-type-alias
 export type EventCallback<T = undefined> = T extends undefined ? () => void : (value: T) => void;
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars-experimental */
+/* eslint-disable @typescript-eslint/no-extraneous-class */
+class WorkaroundForVue {
+  // TODO: empty class as a WA for https://github.com/DevExpress/devextreme-renovation/issues/725
+}
