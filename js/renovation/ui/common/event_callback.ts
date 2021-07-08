b4c4cbce3a502a02be82049147a907b48d4299bd
ignatov <ignatovdan@gmail.com>
2021-07-08 09:23:08 +0300

    Avoid eslint rules

--- js/renovation/ui/common/event_callback.ts
+++ js/renovation/ui/common/event_callback.ts
@@ -1,9 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-type-alias
 export type EventCallback<T = undefined> = T extends undefined ? () => void : (value: T) => void;
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-unused-vars-experimental */
-/* eslint-disable @typescript-eslint/no-extraneous-class */
-class WorkaroundForVue {
+export class WorkaroundForVue {
   // TODO: empty class as a WA for https://github.com/DevExpress/devextreme-renovation/issues/725
+  public dummy = '';
 }
