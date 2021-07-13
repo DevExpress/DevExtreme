9f0d43ec04ddb78ba5a91d083c65c91fb3ab592d
Alyar <Alyar666@users.noreply.github.com>
2021-07-13 16:40:40 +0300

    Renovated DataGrid: provide default values for options and add angular wrapper tests (#18286)

--- testing/tests/DevExpress.ui.widgets.dataGrid/dataGrid.tests.js
+++ testing/tests/DevExpress.ui.widgets.dataGrid/dataGrid.tests.js
@@ -1634,7 +1634,7 @@ QUnit.module('Assign options', baseModuleConfig, () => {
         dataGrid.option('columnChooser', { mode: 'select' });
         assert.equal(headerPanel._getToolbarOptions.callCount, 3, 'Toolbar items are updated after columnChooser options change');
 
-        dataGrid.option('export', { allowExportSelectedData: false });
+        dataGrid.option('export', { allowExportSelectedData: true });
         assert.equal(headerPanel._getToolbarOptions.callCount, 4, 'Toolbar items are updated after export options change');
 
         dataGrid.option('groupPanel', { emptyPanelText: 'test' });
@@ -1728,12 +1728,13 @@ QUnit.module('Assign options', baseModuleConfig, () => {
     QUnit.test('paging change', function(assert) {
         // arrange, act
         const dataGrid = createDataGrid({
-            loadingTimeout: null,
             dataSource: {
                 store: [{ value: 1 }, { value: 2 }, { value: 3 }, { value: 4 }, { value: 5 }],
                 pageSize: 3
             }
         });
+
+        this.clock.tick();
 
         const changedSpy = sinon.spy();
         const loadingSpy = sinon.spy();
@@ -1746,6 +1747,8 @@ QUnit.module('Assign options', baseModuleConfig, () => {
             pageIndex: 1,
             pageSize: 2
         });
+
+        this.clock.tick();
 
         // assert
         assert.strictEqual(changedSpy.callCount, 1, 'changed is called');
