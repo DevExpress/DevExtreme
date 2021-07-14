85da055407a9443310a310bc7f7b11b8df85bb48
Alexander Kozlovskiy (DevExpress) <kozlovskiy.alexander@corp.devexpress.com>
2021-07-14 13:39:43 +0300

    add dx-dropdowneditor-overlay class to ddeditors popup wrapper

--- js/ui/drop_down_editor/ui.drop_down_editor.js
+++ js/ui/drop_down_editor/ui.drop_down_editor.js
@@ -570,6 +570,7 @@ const DropDownEditor = TextBox.inherit({
             width: () => getElementWidth(this.$element()),
             height: 'auto',
             shading: false,
+            wrapperAttr: { class: 'dx-dropdowneditor-overlay' },
             closeOnTargetScroll: true,
             closeOnOutsideClick: this._closeOutsideDropDownHandler.bind(this),
             animation: {
