dc37c73dbdbc518c767343d26a2f28d14d83c890
Sergey Novikov <novsstation@yandex.ru>
2021-07-15 13:07:42 +0300

    Fix code coverage

--- js/renovation/ui/form/wrapper/__tests__/form.test.tsx
+++ js/renovation/ui/form/wrapper/__tests__/form.test.tsx
@@ -4,11 +4,16 @@ import { Form, viewFunction as FormView } from '../form';
 import { FormProps } from '../form_props';
 import { DomComponentWrapper } from '../../../common/dom_component_wrapper';
 import LegacyForm from '../../../../../ui/form';
+import { SimpleItem } from '../simple_item';
+import { EmptyItem } from '../empty_item';
+import { GroupItem } from '../group_item';
+import { TabbedItem } from '../tabbed_item';
 
 describe('Form', () => {
   describe('View', () => {
     it('default render', () => {
       const componentProps = new FormProps();
+      componentProps.items = ['testField', new SimpleItem(), new EmptyItem(), new GroupItem(), new TabbedItem()];
       const props = {
         props: componentProps,
         restAttributes: { 'rest-attributes': 'true' },
