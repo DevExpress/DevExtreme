c19462358a24b40a8986412dc5833a796fe1e34f
ksercs <ksercs0@gmail.com>
2021-07-15 15:42:55 +0300

    add testcafe tests for dimension change during animtion

--- testing/testcafe/model/popup.ts
+++ testing/testcafe/model/popup.ts
@@ -2,15 +2,19 @@ import Widget from './internal/widget';
 
 const CLASS = {
   content: 'dx-overlay-content',
+  wrapper: 'dx-overlay-wrapper',
 };
 export default class Popup extends Widget {
   name = 'dxPopup';
 
   content: Selector;
 
+  wrapper: Selector;
+
   constructor(id: string) {
     super(id);
 
     this.content = this.element.find(`.${CLASS.content}`);
+    this.wrapper = this.element.find(`.${CLASS.wrapper}`);
   }
 }
