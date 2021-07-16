9481a870ba202980a7cbeabad6f888926fe660c6
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2021-07-16 14:43:06 +0300

    Scheduler - CurrentTimeIndicator demo - Increase timeout in test code before screenshot (#1073)

--- Demos/Scheduler/CurrentTimeIndicator/test-code.js
+++ Demos/Scheduler/CurrentTimeIndicator/test-code.js
@@ -1,13 +1,13 @@
 testUtils.importAnd(() => 'devextreme/ui/scheduler', () => DevExpress.ui.dxScheduler, function (dxScheduler) {
     var today = new Date();
     today.setHours(11, 35, 0, 0);
-    
+
     const indicatorTime = new Date(today.setDate(today.getDate() - today.getDay() + 3));
-    
+
     return testUtils.postponeUntilFound('.dx-scheduler').then(x => {
         var instance = dxScheduler.getInstance(document.querySelector('.dx-scheduler'));
         instance.option('indicatorTime', indicatorTime);
-    }).then(testUtils.postpone(1000)).then(()=>{
+    }).then(testUtils.postpone(2000)).then(()=>{
         dxScheduler.getInstance(document.querySelector('.dx-scheduler')).scrollTo(indicatorTime);
-    }).then(testUtils.postpone(1000));
+    }).then(testUtils.postpone(2000));
 });
