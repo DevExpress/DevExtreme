4eb684459faf3e1b8d4ad816aa58258a5774f3e5
andrewmakarov <>
2021-07-16 08:57:36 +0300

    Refactoring tests

--- testing/testcafe/tests/scheduler/layout/focus/focus.ts
+++ testing/testcafe/tests/scheduler/layout/focus/focus.ts
@@ -73,3 +73,37 @@ test('Appointment should be focus on click', async (t) => {
     width: 600,
   }, true);
 });
+
+test('Appointment should be get focus after popup hided', async (t) => {
+  const scheduler = new Scheduler('#container');
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+  await t
+    .doubleClick(scheduler.getAppointment('A').element, { speed: 0.1 })
+    .expect(await takeScreenshot('popup-focused-after-show.png', scheduler.element))
+    .ok();
+
+  await t
+    .click(scheduler.appointmentPopup.cancelButton, { speed: 0.1 })
+    .expect(await takeScreenshot('appointment-focused-after-hide-popup.png', scheduler.element))
+    .ok();
+
+  await t
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => {
+  await createWidget('dxScheduler', {
+    dataSource: [{
+      text: 'A',
+      startDate: new Date(2021, 4, 27, 0, 30),
+      endDate: new Date(2021, 4, 27, 1),
+      recurrenceRule: 'FREQ=DAILY',
+    }],
+    recurrenceEditMode: 'series',
+    views: ['week'],
+    currentView: 'week',
+    currentDate: new Date(2021, 4, 27),
+    height: 600,
+    width: 600,
+  }, true);
+});
