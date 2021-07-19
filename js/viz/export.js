0935a0c894b2fcc9994e5e744ddd6d51e1a86e61
CORP\zaborshchikov.e <jacksonrr3@yandex.ru>
2021-07-19 15:44:49 +0300

    improve code style

--- js/viz/export.js
+++ js/viz/export.js
@@ -1,8 +1,1 @@
-import * as Export from './core/export';
-
-export const exportFromMarkup = Export.exportFromMarkup;
-export const getMarkup = Export.getMarkup;
-export const exportWidgets = Export.exportWidgets;
-export const combineMarkups = Export.combineMarkups;
-export const ExportMenu = Export.ExportMenu;
-export const plugin = Export.plugin;
+export * from './core/export';
