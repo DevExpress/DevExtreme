82c7ef00f24422fa9f71f91f2c04bf8de0a97049
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2021-07-21 12:08:46 +0300

    fix expectations

--- testing/tests/DevExpress.exporter/exceljsParts/exceljs.pivotGrid.tests.js
+++ testing/tests/DevExpress.exporter/exceljsParts/exceljs.pivotGrid.tests.js
@@ -1627,7 +1627,9 @@ QUnit.module('Scenarios', moduleConfig, () => {
     });
 
     QUnit.test('Check header/data/total cell style/data type, pivot.rtlEnabled = true', function(assert) {
+        const clock = sinon.useFakeTimers();
         const done = assert.async();
+
         const ds = {
             fields: [
                 { area: 'row', dataField: 'row1', dataType: 'string', expanded: true, showTotals: true },
@@ -1692,8 +1694,13 @@ QUnit.module('Scenarios', moduleConfig, () => {
             helper.checkOutlineLevel([0, 0, 0, 0, 0], topLeft.row);
             helper.checkAutoFilter(false, { from: topLeft, to: topLeft }, { 'rightToLeft': true, state: 'frozen', ySplit: topLeft.row + 1, xSplit: topLeft.column + 1 });
             helper.checkCellRange(cellRange, { row: 5, column: 5 }, topLeft);
+
+            clock.restore();
             done();
         });
+
+        clock.tick(400);
+
     });
 
     QUnit.test('Export [string x string x number] with format: currency', function(assert) {
