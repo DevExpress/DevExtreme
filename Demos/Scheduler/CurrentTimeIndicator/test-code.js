57a981b603a23d06a561dfc00eb11878913d7997
Evgeniy.Zaborshchikov <49640784+jacksonrr3@users.noreply.github.com>
2021-07-27 19:00:01 +0300

    Fix React test: Scheduler-CurrentTimeIndicator (#1095)

--- Demos/Scheduler/CurrentTimeIndicator/test-code.js
+++ Demos/Scheduler/CurrentTimeIndicator/test-code.js
@@ -1,13 +1,14 @@
 testUtils.importAnd(() => 'devextreme/ui/scheduler', () => DevExpress.ui.dxScheduler, function (dxScheduler) {
-    var today = new Date();
+    const today = new Date();
     today.setHours(11, 35, 0, 0);
 
     const indicatorTime = new Date(today.setDate(today.getDate() - today.getDay() + 3));
 
-    return testUtils.postponeUntilFound('.dx-scheduler').then(x => {
-        var instance = dxScheduler.getInstance(document.querySelector('.dx-scheduler'));
+    return testUtils.postponeUntilFound('.dx-scheduler').then(() => {
+        const instance = dxScheduler.getInstance(document.querySelector('.dx-scheduler'));
         instance.option('indicatorTime', indicatorTime);
-    }).then(testUtils.postpone(2000)).then(()=>{
-        dxScheduler.getInstance(document.querySelector('.dx-scheduler')).scrollTo(indicatorTime);
-    }).then(testUtils.postpone(2000));
+        testUtils.postpone(2000).then(() => {
+            instance.scrollTo(indicatorTime);
+        })
+    })
 });
