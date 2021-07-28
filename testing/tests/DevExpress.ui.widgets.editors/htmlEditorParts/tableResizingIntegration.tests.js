e5b9a0d2dfff878e0d0212851d6e4e268a803e11
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2021-07-28 20:20:23 +0300

    fix editor test

--- testing/tests/DevExpress.ui.widgets.editors/htmlEditorParts/tableResizingIntegration.tests.js
+++ testing/tests/DevExpress.ui.widgets.editors/htmlEditorParts/tableResizingIntegration.tests.js
@@ -1202,7 +1202,7 @@ module('Table resizing integration', {
             $table = this.$element.find('table');
 
             $table.find('tr').eq(0).find('td').each((i, columnElement) => {
-                assert.roughEqual($(columnElement).outerWidth(), expectedColumnsWidths[i], 2.01, 'Column has expected width, index = ' + i);
+                assert.roughEqual($(columnElement).outerWidth(), expectedColumnsWidths[i], 2.51, 'Column has expected width, index = ' + i);
             });
 
             assert.roughEqual($table.outerWidth(), tableWidth, 2.01, 'Table width is not changed');
