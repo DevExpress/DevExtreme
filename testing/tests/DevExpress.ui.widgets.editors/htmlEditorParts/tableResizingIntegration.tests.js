2224a993e202d451157742c143bb17ef7f26bc82
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2021-07-28 20:41:08 +0300

    fix editor test

--- testing/tests/DevExpress.ui.widgets.editors/htmlEditorParts/tableResizingIntegration.tests.js
+++ testing/tests/DevExpress.ui.widgets.editors/htmlEditorParts/tableResizingIntegration.tests.js
@@ -1205,7 +1205,7 @@ module('Table resizing integration', {
                 assert.roughEqual($(columnElement).outerWidth(), expectedColumnsWidths[i], 2.51, 'Column has expected width, index = ' + i);
             });
 
-            assert.roughEqual($table.outerWidth(), tableWidth, 2.01, 'Table width is not changed');
+            assert.roughEqual($table.outerWidth(), tableWidth, 2.51, 'Table width is not changed');
         });
 
         test('Column resizers should be updated after a column insert', function(assert) {
