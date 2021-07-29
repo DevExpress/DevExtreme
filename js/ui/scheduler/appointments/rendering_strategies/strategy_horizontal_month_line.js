8d834fb42d723d1b1786bd86314adfb294a69280
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2021-07-29 18:45:36 +0300

    Scheduler - Refactoring

--- js/ui/scheduler/appointments/rendering_strategies/strategy_horizontal_month_line.js
+++ js/ui/scheduler/appointments/rendering_strategies/strategy_horizontal_month_line.js
@@ -8,6 +8,8 @@ const MILLISECONDS_IN_MINUTE = 60000;
 const ZERO_APPOINTMENT_DURATION_IN_DAYS = 1;
 
 class HorizontalMonthLineRenderingStrategy extends HorizontalAppointmentsStrategy {
+    get viewDataProvider() { return this.options.viewDataProvider; }
+
     calculateAppointmentWidth(appointment, position) {
         const startDate = dateUtils.trimTime(position.info.appointment.startDate);
         const { normalizedEndDate } = position.info.appointment;
@@ -16,9 +18,13 @@ class HorizontalMonthLineRenderingStrategy extends HorizontalAppointmentsStrateg
 
         let width = this.cropAppointmentWidth(duration * cellWidth, cellWidth);
 
-        if(this.instance.isVirtualScrolling()) {
-            const workSpace = this.instance.getWorkSpace();
-            const skippedDays = workSpace.viewDataProvider.getSkippedDaysCount(position.groupIndex, startDate, normalizedEndDate, duration);
+        if(this.isVirtualScrolling) {
+            const skippedDays = this.viewDataProvider.getSkippedDaysCount(
+                position.groupIndex,
+                startDate,
+                normalizedEndDate,
+                duration
+            );
 
             width -= skippedDays * cellWidth;
         }
