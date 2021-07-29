1fa3c26bafe21a882c8eefe545f2041227774b16
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2021-07-29 20:15:30 +0300

    tests: fix acceptable margins of error when checking values (#18506)

--- testing/tests/DevExpress.exporter/exceljsParts/exceljs.tests.js
+++ testing/tests/DevExpress.exporter/exceljsParts/exceljs.tests.js
@@ -1,6 +1,5 @@
 import $ from 'jquery';
 import browser from 'core/utils/browser';
-import devices from 'core/devices';
 import localization from 'localization';
 import ja from 'localization/messages/ja.json!';
 import messageLocalization from 'localization/message';
@@ -442,8 +441,6 @@ const moduleConfig = {
         });
 
         QUnit.test('Header - 2 column, column.width: auto', function(assert) {
-            const currentDevice = devices.current();
-            const isWinPhone = currentDevice.deviceType === 'phone' && currentDevice.platform === 'generic';
             const done = assert.async();
 
             const dataGrid = $('#dataGrid').dxDataGrid({
@@ -452,13 +449,7 @@ const moduleConfig = {
             }).dxDataGrid('instance');
 
             exportDataGrid(getOptions(this, dataGrid, null, true)).then(() => {
-                let expectedWidths = [3.71, 67.71, undefined];
-                if(browser.mozilla) {
-                    expectedWidths = [3.85, 67.57, undefined];
-                } else if(browser.msie && !isWinPhone) {
-                    expectedWidths = [3.77, 67.65, undefined];
-                }
-                helper.checkColumnWidths(expectedWidths, topLeft.column);
+                helper.checkColumnWidths([3.8, 67.6, undefined], topLeft.column);
                 done();
             });
         });
