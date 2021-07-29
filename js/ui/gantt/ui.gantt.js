c2c78a6056f5c0868eb0f9d7093976f83aae1ebc
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2021-07-29 12:54:01 +0300

    gantt: revert optionchanged_bundled tests fix

--- js/ui/gantt/ui.gantt.js
+++ js/ui/gantt/ui.gantt.js
@@ -702,8 +702,6 @@ class Gantt extends Widget {
                 super._optionChanged(args);
                 this._sizeHelper?.setGanttHeight(this._$element.height());
                 break;
-            case 'sorting':
-                break;
             default:
                 super._optionChanged(args);
         }
