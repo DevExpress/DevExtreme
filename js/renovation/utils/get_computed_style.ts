c12b51dea26de5dbd433cad33ad094f97a992be0
Andrey Churkin <alam17@mail.ru>
2021-07-30 17:19:04 +0300

    ESLint: prohibit to use global window variable (#18485)

--- js/renovation/utils/get_computed_style.ts
+++ js/renovation/utils/get_computed_style.ts
@@ -1,3 +1,7 @@
+import { getWindow } from '../../core/utils/window';
+
+const window = getWindow();
+
 export default function getElementComputedStyle(el: Element | undefined | null):
 CSSStyleDeclaration | null {
   return el ? window.getComputedStyle?.(el) : null;
