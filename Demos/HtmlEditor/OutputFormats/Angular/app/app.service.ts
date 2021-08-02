a26eb0b508eb4504b99a852edc52aedba9b6c645
kotov.alexander <alexander.kotov@devexpress.com>
2021-08-02 13:36:37 +0300

    HtmlEditor output format demo - prevent value format change after the first edititng operation (#1113)

--- Demos/HtmlEditor/OutputFormats/Angular/app/app.service.ts
+++ Demos/HtmlEditor/OutputFormats/Angular/app/app.service.ts
@@ -1,13 +1,6 @@
 import { Injectable } from '@angular/core';
 
-let markup: string = `
-    <h2>
-        <img src="../../../../images/widgets/HtmlEditor.svg" alt="HtmlEditor">
-        Formatted Text Editor (HTML Editor)
-    </h2>
-    <br>
-    <p>DevExtreme JavaScript HTML Editor is a client-side WYSIWYG text editor that allows its users to format textual and visual content and store it as HTML or Markdown.</p>
-`;
+let markup: string = '<h2><img src="../../../../images/widgets/HtmlEditor.svg" alt="HtmlEditor"> Formatted Text Editor (HTML Editor)</h2><p><br></p><p>DevExtreme JavaScript HTML Editor is a client-side WYSIWYG text editor that allows its users to format textual and visual content and store it as HTML or Markdown.</p>';
 
 @Injectable()
 export class Service {
