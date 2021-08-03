b0e8faddcfde4adde3e33aee7a2523bb94f2cc48
andrewmakarov <andrewmakarov@inbox.ru>
2021-08-03 16:27:56 +0300

    Remove unused code

--- testing/testcafe/tests/scheduler/dragAndDrop/outlookDragging/base.ts
+++ testing/testcafe/tests/scheduler/dragAndDrop/outlookDragging/base.ts
@@ -8,7 +8,6 @@ fixture`Outlook dragging base tests`
 
 test('Basic drag-n-drop movements in groups', async (t) => {
   const scheduler = new Scheduler('#container');
-  // T
 
   const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
   const draggableAppointment = scheduler.getAppointment('Website Re-Design Plan');
