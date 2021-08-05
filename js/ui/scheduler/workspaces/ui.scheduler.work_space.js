6be07d306be96a3bb4680d5a1e8f51a08c2f52c0
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2021-08-05 10:32:16 +0300

    change on public method
    (cherry picked from commit 08efbd02920efc0124ae0c259faf00dbaa9bab13)

--- js/ui/scheduler/workspaces/ui.scheduler.work_space.js
+++ js/ui/scheduler/workspaces/ui.scheduler.work_space.js
@@ -2401,7 +2401,7 @@ class SchedulerWorkSpace extends WidgetObserver {
     }
 
     getScrollableContainer() {
-        return this._dateTableScrollable._container();
+        return $(this._dateTableScrollable.container());
     }
 
     getHeaderPanelHeight() {
