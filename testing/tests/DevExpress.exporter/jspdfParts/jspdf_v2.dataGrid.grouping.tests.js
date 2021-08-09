ba6ef66086b951fc933db0d6fc4c303b1eec46c4
Sergey Novikov <novsstation@yandex.ru>
2021-08-09 14:40:16 +0300

    More test cases

--- testing/tests/DevExpress.exporter/jspdfParts/jspdf_v2.dataGrid.grouping.tests.js
+++ testing/tests/DevExpress.exporter/jspdfParts/jspdf_v2.dataGrid.grouping.tests.js
@@ -50,6 +50,41 @@ const JSPdfGroupingTests = {
                 });
             });
 
+            QUnit.test('1 level - 1 group - [{f1, groupIndex: 0}, f2, f3] - height auto', function(assert) {
+                const done = assert.async();
+                const doc = createMockPdfDoc();
+
+                const dataGrid = createDataGrid({
+                    columns: [
+                        { dataField: 'f1', groupIndex: 0 },
+                        { dataField: 'f2' },
+                        { dataField: 'f3' },
+                    ],
+                    dataSource: [
+                        { f1: 'f1', f2: 'f1_2', f3: 'f1_3' },
+                        { f1: 'f1', f2: 'f2_2', f3: 'f2_3' },
+                    ],
+                });
+
+                const onRowExporting = () => { };
+
+                const expectedLog = [
+                    'text,F2,10,24.2,{baseline:middle}', 'setLineWidth,1', 'rect,10,15,90,18.4',
+                    'text,F3,100,24.2,{baseline:middle}', 'setLineWidth,1', 'rect,100,15,80,18.4',
+                    'text,F1: f1,10,42.6,{baseline:middle}', 'setLineWidth,1', 'rect,10,33.4,170,18.4',
+                    'text,f1_2,20,61,{baseline:middle}', 'setLineWidth,1', 'rect,20,51.8,80,18.4',
+                    'text,f1_3,100,61,{baseline:middle}', 'setLineWidth,1', 'rect,100,51.8,80,18.4',
+                    'text,f2_2,20,79.4,{baseline:middle}', 'setLineWidth,1', 'rect,20,70.2,80,18.4',
+                    'text,f2_3,100,79.4,{baseline:middle}', 'setLineWidth,1', 'rect,100,70.2,80,18.4'
+                ];
+
+                exportDataGrid(doc, dataGrid, { topLeft: { x: 10, y: 15 }, columnWidths: [ 90, 80 ], onRowExporting }).then(() => {
+                    // doc.save();
+                    assert.deepEqual(doc.__log, expectedLog);
+                    done();
+                });
+            });
+
             QUnit.test('1 level - 2 group - [{f1, groupIndex: 0}, f2, f3]', function(assert) {
                 const done = assert.async();
                 const doc = createMockPdfDoc();
@@ -90,6 +125,42 @@ const JSPdfGroupingTests = {
                     'text,F1: f2_1,10,85,{baseline:middle}', 'setLineWidth,1', 'rect,10,75,170,20',
                     'text,f2_2,20,107,{baseline:middle}', 'setLineWidth,1', 'rect,20,95,80,24',
                     'text,f2_3,100,107,{baseline:middle}', 'setLineWidth,1', 'rect,100,95,80,24'
+                ];
+
+                exportDataGrid(doc, dataGrid, { topLeft: { x: 10, y: 15 }, columnWidths: [ 90, 80 ], onRowExporting }).then(() => {
+                    // doc.save();
+                    assert.deepEqual(doc.__log, expectedLog);
+                    done();
+                });
+            });
+
+            QUnit.test('1 level - 2 group - [{f1, groupIndex: 0}, f2, f3] - height auto', function(assert) {
+                const done = assert.async();
+                const doc = createMockPdfDoc();
+
+                const dataGrid = createDataGrid({
+                    columns: [
+                        { dataField: 'f1', groupIndex: 0 },
+                        { dataField: 'f2' },
+                        { dataField: 'f3' },
+                    ],
+                    dataSource: [
+                        { f1: 'f1_1', f2: 'f1_2', f3: 'f1_3' },
+                        { f1: 'f2_1', f2: 'f2_2', f3: 'f2_3' },
+                    ],
+                });
+
+                const onRowExporting = () => { };
+
+                const expectedLog = [
+                    'text,F2,10,24.2,{baseline:middle}', 'setLineWidth,1', 'rect,10,15,90,18.4',
+                    'text,F3,100,24.2,{baseline:middle}', 'setLineWidth,1', 'rect,100,15,80,18.4',
+                    'text,F1: f1_1,10,42.6,{baseline:middle}', 'setLineWidth,1', 'rect,10,33.4,170,18.4',
+                    'text,f1_2,20,61,{baseline:middle}', 'setLineWidth,1', 'rect,20,51.8,80,18.4',
+                    'text,f1_3,100,61,{baseline:middle}', 'setLineWidth,1', 'rect,100,51.8,80,18.4',
+                    'text,F1: f2_1,10,79.4,{baseline:middle}', 'setLineWidth,1', 'rect,10,70.2,170,18.4',
+                    'text,f2_2,20,97.8,{baseline:middle}', 'setLineWidth,1', 'rect,20,88.6,80,18.4',
+                    'text,f2_3,100,97.8,{baseline:middle}', 'setLineWidth,1', 'rect,100,88.6,80,18.4'
                 ];
 
                 exportDataGrid(doc, dataGrid, { topLeft: { x: 10, y: 15 }, columnWidths: [ 90, 80 ], onRowExporting }).then(() => {
@@ -149,6 +220,43 @@ const JSPdfGroupingTests = {
                 });
             });
 
+            QUnit.test('2 level - 1 group - [{f1, groupIndex: 0}, {f2, groupIndex: 1}, f3, f4] - height auto', function(assert) {
+                const done = assert.async();
+                const doc = createMockPdfDoc();
+
+                const dataGrid = createDataGrid({
+                    columns: [
+                        { dataField: 'f1', groupIndex: 0 },
+                        { dataField: 'f2', groupIndex: 1 },
+                        { dataField: 'f3' },
+                        { dataField: 'f4' },
+                    ],
+                    dataSource: [
+                        { f1: 'f1', f2: 'f2', f3: 'f1_3', f4: 'f1_4' },
+                        { f1: 'f1', f2: 'f2', f3: 'f2_3', f4: 'f2_4' },
+                    ],
+                });
+
+                const onRowExporting = () => { };
+
+                const expectedLog = [
+                    'text,F3,10,24.2,{baseline:middle}', 'setLineWidth,1', 'rect,10,15,90,18.4',
+                    'text,F4,100,24.2,{baseline:middle}', 'setLineWidth,1', 'rect,100,15,80,18.4',
+                    'text,F1: f1,10,42.6,{baseline:middle}', 'setLineWidth,1', 'rect,10,33.4,170,18.4',
+                    'text,F2: f2,20,61,{baseline:middle}', 'setLineWidth,1', 'rect,20,51.8,160,18.4',
+                    'text,f1_3,30,79.4,{baseline:middle}', 'setLineWidth,1', 'rect,30,70.2,70,18.4',
+                    'text,f1_4,100,79.4,{baseline:middle}', 'setLineWidth,1', 'rect,100,70.2,80,18.4',
+                    'text,f2_3,30,97.8,{baseline:middle}', 'setLineWidth,1', 'rect,30,88.6,70,18.4',
+                    'text,f2_4,100,97.8,{baseline:middle}', 'setLineWidth,1', 'rect,100,88.6,80,18.4'
+                ];
+
+                exportDataGrid(doc, dataGrid, { topLeft: { x: 10, y: 15 }, columnWidths: [ 90, 80 ], onRowExporting }).then(() => {
+                    // doc.save();
+                    assert.deepEqual(doc.__log, expectedLog);
+                    done();
+                });
+            });
+
             QUnit.test('2 level - 2 groups - [{f1, groupIndex: 0}, {f2, groupIndex: 1}, f3, f4]', function(assert) {
                 const done = assert.async();
                 const doc = createMockPdfDoc();
@@ -193,6 +301,44 @@ const JSPdfGroupingTests = {
                     'text,F2: f2_2,20,105,{baseline:middle}', 'setLineWidth,1', 'rect,20,95,160,20',
                     'text,f2_3,30,127,{baseline:middle}', 'setLineWidth,1', 'rect,30,115,70,24',
                     'text,f2_4,100,127,{baseline:middle}', 'setLineWidth,1', 'rect,100,115,80,24'
+                ];
+
+                exportDataGrid(doc, dataGrid, { topLeft: { x: 10, y: 15 }, columnWidths: [ 90, 80 ], onRowExporting }).then(() => {
+                    // doc.save();
+                    assert.deepEqual(doc.__log, expectedLog);
+                    done();
+                });
+            });
+
+            QUnit.test('2 level - 2 groups - [{f1, groupIndex: 0}, {f2, groupIndex: 1}, f3, f4] - height auto', function(assert) {
+                const done = assert.async();
+                const doc = createMockPdfDoc();
+
+                const dataGrid = createDataGrid({
+                    columns: [
+                        { dataField: 'f1', groupIndex: 0 },
+                        { dataField: 'f2', groupIndex: 1 },
+                        { dataField: 'f3' },
+                        { dataField: 'f4' },
+                    ],
+                    dataSource: [
+                        { f1: 'f1', f2: 'f1_2', f3: 'f1_3', f4: 'f1_4' },
+                        { f1: 'f1', f2: 'f2_2', f3: 'f2_3', f4: 'f2_4' },
+                    ],
+                });
+
+                const onRowExporting = () => { };
+
+                const expectedLog = [
+                    'text,F3,10,24.2,{baseline:middle}', 'setLineWidth,1', 'rect,10,15,90,18.4',
+                    'text,F4,100,24.2,{baseline:middle}', 'setLineWidth,1', 'rect,100,15,80,18.4',
+                    'text,F1: f1,10,42.6,{baseline:middle}', 'setLineWidth,1', 'rect,10,33.4,170,18.4',
+                    'text,F2: f1_2,20,61,{baseline:middle}', 'setLineWidth,1', 'rect,20,51.8,160,18.4',
+                    'text,f1_3,30,79.4,{baseline:middle}', 'setLineWidth,1', 'rect,30,70.2,70,18.4',
+                    'text,f1_4,100,79.4,{baseline:middle}', 'setLineWidth,1', 'rect,100,70.2,80,18.4',
+                    'text,F2: f2_2,20,97.8,{baseline:middle}', 'setLineWidth,1', 'rect,20,88.6,160,18.4',
+                    'text,f2_3,30,116.2,{baseline:middle}', 'setLineWidth,1', 'rect,30,107,70,18.4',
+                    'text,f2_4,100,116.2,{baseline:middle}', 'setLineWidth,1', 'rect,100,107,80,18.4'
                 ];
 
                 exportDataGrid(doc, dataGrid, { topLeft: { x: 10, y: 15 }, columnWidths: [ 90, 80 ], onRowExporting }).then(() => {
