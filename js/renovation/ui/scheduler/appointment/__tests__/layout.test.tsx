07c45c4d4761ea791de4c6b70df9d61609a4eb21
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2021-08-11 14:06:44 +0300

    Extend jest test

--- js/renovation/ui/scheduler/appointment/__tests__/layout.test.tsx
+++ js/renovation/ui/scheduler/appointment/__tests__/layout.test.tsx
@@ -38,30 +38,33 @@ describe('AppointmentLayout', () => {
           },
         },
       };
+      const viewModel0 = {
+        ...defaultViewModel,
+        geometry: {
+          ...defaultViewModel.geometry,
+          left: 10,
+        },
+      };
+      const viewModel1 = {
+        ...defaultViewModel,
+        geometry: {
+          ...defaultViewModel.geometry,
+          top: 20,
+        },
+      };
+      const viewModel2 = {
+        ...defaultViewModel,
+        geometry: {
+          ...defaultViewModel.geometry,
+          width: 1220,
+        },
+      };
       const layout = render({
         props: {
           items: [
-            {
-              ...defaultViewModel,
-              geometry: {
-                ...defaultViewModel.geometry,
-                left: 10,
-              },
-            },
-            {
-              ...defaultViewModel,
-              geometry: {
-                ...defaultViewModel.geometry,
-                top: 20,
-              },
-            },
-            {
-              ...defaultViewModel,
-              geometry: {
-                ...defaultViewModel.geometry,
-                width: 1220,
-              },
-            },
+            viewModel0,
+            viewModel1,
+            viewModel2,
           ],
         },
       });
@@ -72,14 +75,23 @@ describe('AppointmentLayout', () => {
       expect(layout.children().length)
         .toEqual(3);
 
-      expect(layout.children().get(0).key)
+      let appointment = layout.children().get(0);
+      expect(appointment.key)
         .toEqual('1-1628157600000-1628164800000_11-4-10-20');
+      expect(appointment.props.viewModel)
+        .toBe(viewModel0);
 
-      expect(layout.children().get(1).key)
+      appointment = layout.children().get(1);
+      expect(appointment.key)
         .toEqual('1-1628157600000-1628164800000_2-22-10-20');
+      expect(appointment.props.viewModel)
+        .toBe(viewModel1);
 
-      expect(layout.children().get(2).key)
+      appointment = layout.children().get(2);
+      expect(appointment.key)
         .toEqual('1-1628157600000-1628164800000_2-4-1220-20');
+      expect(appointment.props.viewModel)
+        .toBe(viewModel2);
     });
   });
 });
