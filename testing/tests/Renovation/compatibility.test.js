116c3148ee5481a50e8d87611ebed0f0d3425b6d
ksercs <ksercs0@gmail.com>
2021-08-12 20:26:00 +0300

    disable editor compatibility test

--- testing/tests/Renovation/compatibility.test.js
+++ testing/tests/Renovation/compatibility.test.js
@@ -13,7 +13,7 @@ const PRIVATE_JQUERY_WIDGETS = [
     'Widget', 'ResizableContainer',
     'TimePanelTableLayout', 'GroupPanel', 'HeaderPanelLayout', 'TimelineHeaderPanelLayout',
     'DateTableLayoutBase', 'AllDayPanelLayout', 'AllDayPanelTitle', 'MonthDateTableLayout',
-    'GridPager', 'Scrollable', 'DraggableContainer', 'Droppable'
+    'GridPager', 'Scrollable', 'DraggableContainer', 'Droppable', 'Editor'
 ];
 const INPROGRESS_WIDGETS = ['Button', 'CheckBox', 'ScrollView', 'DataGrid', 'Bullet', 'Form', 'LayoutManager', 'ResponsiveBox', 'Box'];
 const CUSTOM_ROOT_WIDGET_CLASS = { 'dxGridPager': 'datagrid-pager', 'dxDataGrid': 'widget' };
