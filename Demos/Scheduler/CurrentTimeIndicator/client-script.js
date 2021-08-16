0b866d80da47fa5488c1a62646d7114cfa4f8c47
Denis Gradoboev <idenis.gradoboev@yandex.com>
2021-08-16 16:20:54 +0500

    dxScheduler - Revert setting date in currentTimeIndicator demo to current date (#1140)

--- Demos/Scheduler/CurrentTimeIndicator/client-script.js
+++ Demos/Scheduler/CurrentTimeIndicator/client-script.js
@@ -1,1 +1,15 @@
-MockDate.set(new Date("2021/07/28 11:35"));
+const getWednesdayThisWeek = () => {
+    const WEDNESDAY_INDEX = 3;
+
+    const date = new Date();
+
+    const currentDay = date.getDay();
+    const distance = WEDNESDAY_INDEX - currentDay;
+    date.setDate(date.getDate() + distance);
+
+    date.setHours(11, 35, 0, 0);
+
+    return date;
+}
+
+MockDate.set(getWednesdayThisWeek());
