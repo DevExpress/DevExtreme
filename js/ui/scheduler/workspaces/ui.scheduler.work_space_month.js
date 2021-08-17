40674242db2525d3f59190603f694c9d5c84c7a5
Evgeniy Aryamnov <earyamnow@mail.ru>
2021-08-17 11:44:49 +0300

    Fix another import

--- js/ui/scheduler/workspaces/ui.scheduler.work_space_month.js
+++ js/ui/scheduler/workspaces/ui.scheduler.work_space_month.js
@@ -8,7 +8,7 @@ import dxrMonthDateTableLayout from '../../../renovation/ui/scheduler/workspaces
 import {
     getViewStartByOptions,
     getCellText,
-} from './utils/month';
+} from '../../../renovation/ui/scheduler/view_model/to_test/views/utils/month';
 import { calculateDayDuration, formatWeekday } from '../../../renovation/ui/scheduler/view_model/to_test/views/utils/base';
 import { VIEWS } from '../constants';
 
