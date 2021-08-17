8bb048f1c4a8d24d50aa29b00203f9ccc087e604
Evgeniy.Zaborshchikov <49640784+jacksonrr3@users.noreply.github.com>
2021-08-17 16:29:25 +0300

    Fix React and Vue tests: HtmlEditor-OutputFormats (#1142)

--- Demos/HtmlEditor/OutputFormats/test-code.js
+++ Demos/HtmlEditor/OutputFormats/test-code.js
@@ -1,4 +1,6 @@
-testUtils.findElements('.value-content').forEach(x=>{
-    var text = x.textContent;
-    x.textContent = text.replace(/src\s*=\s*"(.+?)"/, 'src="images/widgets/HtmlEditor.svg"');
-});
+testUtils.postponeUntilFound('.value-content').then(() => {
+    testUtils.findElements('.value-content').forEach(x=>{
+        var text = x.textContent;
+        x.textContent = text.replace(/src\s*=\s*"(.+?)"/, 'src="images/widgets/HtmlEditor.svg"');
+    });
+})
