fc4a63c21c138bfe900edc8bc4dfb19ca2418076
Evgeniy Aryamnov <earyamnow@mail.ru>
2021-08-19 15:30:48 +0300

    Fix date.d.ts

--- js/localization/date.d.ts
+++ js/localization/date.d.ts
@@ -5,8 +5,8 @@ type Format = 'abbreviated' | 'short' | 'narrow';
 interface DateLocalization {
   firstDayOfWeekIndex(): number;
   format(date: Date | undefined, format: LocalizationFormat): string | Date | undefined;
-  getDayNames(format: Format): string[];
-  getMonthNames(format: Format): string[];
+  getDayNames(format?: Format): string[];
+  getMonthNames(format?: Format): string[];
 }
 declare const dateLocalization: DateLocalization;
 export default dateLocalization;
