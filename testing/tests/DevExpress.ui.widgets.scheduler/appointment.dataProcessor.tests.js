1e647c7ea9fa4eb5a9308bb8912fde0a8dc90838
Andrew Makarov <andrewmakarov@inbox.ru>
2021-08-23 10:53:32 +0300

    dxScheduler - refactoring filtering by dates(T1021963) (#18831)
    * Refactoring filtering by dates
    
    * Add tests
    
    * Refactoring tests
    
    * Add etalons
    
    * Add testSpeed=0.1
    
    * Fix tests - add .png to etalon file name
    
    * Fix server-side filtering
    
    * Fix tests

--- testing/tests/DevExpress.ui.widgets.scheduler/appointment.dataProcessor.tests.js
+++ testing/tests/DevExpress.ui.widgets.scheduler/appointment.dataProcessor.tests.js
@@ -90,7 +90,7 @@ module('Server side filtering', () => {
         });
         const dateFilter = [
             [
-                ['endDate', '>', new Date(2015, 1, 9, 0)],
+                ['endDate', '>=', new Date(2015, 1, 9, 0)],
                 ['startDate', '<', new Date(2015, 1, 11)]
             ],
             'or',
@@ -99,6 +99,7 @@ module('Server side filtering', () => {
                 ['startDate', new Date(2015, 1, 9)]
             ]
         ];
+
         appointmentDataProvider.filterByDate(new Date(2015, 1, 9, 0), new Date(2015, 1, 10, 13), true);
 
         let expectedFilter = [dateFilter, [
@@ -181,7 +182,7 @@ module('Server side filtering', () => {
     test('Appointment filterByDate should filter dataSource correctly without copying dateFilter', function(assert) {
         const dateFilter = [
             [
-                ['endDate', '>', new Date(2015, 1, 9, 0)],
+                ['endDate', '>=', new Date(2015, 1, 9, 0)],
                 ['startDate', '<', new Date(2015, 1, 11)]
             ],
             'or',
@@ -250,7 +251,7 @@ module('Server side filtering', () => {
 
             const expectedFilter = [[
                 [
-                    ['endDate', '>', new Date(2015, 1, 10)],
+                    ['endDate', '>=', new Date(2015, 1, 10)],
                     ['startDate', '<', new Date(2015, 1, 11)]
                 ],
                 'or',
@@ -300,7 +301,7 @@ module('Server side filtering', () => {
 
             const expectedFilter = [[
                 [
-                    ['endDate', '>', '2015-02-10T00:00:00'],
+                    ['endDate', '>=', '2015-02-10T00:00:00'],
                     ['startDate', '<', '2015-02-11T00:00:00']
                 ],
                 'or',
@@ -458,7 +459,7 @@ module('Server side filtering', () => {
         appointmentDataProvider.filterByDate(new Date(2015, 1, 11), new Date(2015, 1, 11, 11), true);
         dataSource.load();
 
-        assert.equal(dataSource.items().length, 0, 'filterByDate works correctly');
+        assert.equal(dataSource.items().length, 1, 'filterByDate works correctly');
     });
 
     test('Appointment filterByDate should correctly filter items with recurrenceRule, if recurrenceRuleExpr!=null', function(assert) {
