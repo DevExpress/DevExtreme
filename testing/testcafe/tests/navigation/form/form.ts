9d25aef5671029f098dc1f823d49649bdaacd345
Ignatov Dan <ignatovdan@gmail.com>
2021-08-24 20:35:58 +0300

    Patch file names

--- testing/testcafe/tests/navigation/form/form.ts
+++ testing/testcafe/tests/navigation/form/form.ts
@@ -6,31 +6,16 @@ import createWidget from '../../../helpers/createWidget';
 fixture`Form`
   .page(url(__dirname, '../../container.html'));
 
-test('Empty', async (t) => {
-  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
-
-  await t
-    .expect(await takeScreenshot('Empty.png', Selector('#container')))
-    .ok()
-    .expect(compareResults.isValid())
-    .ok(compareResults.errorMessages());
-}).before(async () => createWidget('dxForm', {
-  width: 100,
-  height: 100,
-}));
-
-// TODO: https://js.devexpress.com/Documentation/21_2/ApiReference/UI_Components/dxForm/Configuration/#labelLocation
 // TODO: https://js.devexpress.com/Documentation/21_2/ApiReference/UI_Components/dxForm/Item_Types/GroupItem/
 // TODO: https://js.devexpress.com/Documentation/21_2/ApiReference/UI_Components/dxForm/Item_Types/TabbedItem/
 // TODO: https://js.devexpress.com/Documentation/21_2/ApiReference/UI_Components/dxForm/Item_Types/ButtonItem/
 // TODO: https://js.devexpress.com/Documentation/21_2/ApiReference/UI_Components/dxForm/Item_Types/EmptyItem/
-// TODO: https://js.devexpress.com/Documentation/21_2/ApiReference/UI_Components/dxForm/Configuration/#rtlEnabled
 
 [false, true].forEach((rtlEnabled) => {
   ['left', 'right', 'top'].forEach((labelLocation) => {
     [1, 2, 3].forEach((colCount) => {
       [1, 2, 3, 4, 5, 6].forEach((itemNumber) => {
-        const testName = `TextBox_${itemNumber},colCount_${colCount}`;
+        const testName = `TextBox_${itemNumber},colCount_${colCount},labelLocation_${labelLocation},rtlEnabled_${rtlEnabled}`;
         test(testName, async (t) => {
           const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
