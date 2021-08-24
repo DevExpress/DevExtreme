8837dc2fc3847ff8ad5e1243ab33666f181928af
Ignatov Dan <ignatovdan@gmail.com>
2021-08-24 19:54:40 +0300

    Add rtlEnabled

--- testing/testcafe/tests/navigation/form/form.ts
+++ testing/testcafe/tests/navigation/form/form.ts
@@ -26,27 +26,28 @@ test('Empty', async (t) => {
 // TODO: https://js.devexpress.com/Documentation/21_2/ApiReference/UI_Components/dxForm/Item_Types/EmptyItem/
 // TODO: https://js.devexpress.com/Documentation/21_2/ApiReference/UI_Components/dxForm/Configuration/#rtlEnabled
 
-['left', 'right', 'top'].forEach((labelLocation) => {
-// eslint-disable-next-line no-plusplus
-  for (let colCount = 1; colCount <= 3; colCount++) {
-  // eslint-disable-next-line no-plusplus
-    for (let itemNumber = 1; itemNumber <= 6; itemNumber++) {
-      const testName = `TextBox_${itemNumber},colCount_${colCount}`;
-      test(testName, async (t) => {
-        const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+[false, true].forEach((rtlEnabled) => {
+  ['left', 'right', 'top'].forEach((labelLocation) => {
+    [1, 2, 3].forEach((colCount) => {
+      [1, 2, 3, 4, 5, 6].forEach((itemNumber) => {
+        const testName = `TextBox_${itemNumber},colCount_${colCount}`;
+        test(testName, async (t) => {
+          const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-        await t
-          .expect(await takeScreenshot(`${testName}.png`, Selector('#container')))
-          .ok()
-          .expect(compareResults.isValid())
-          .ok(compareResults.errorMessages());
-      }).before(async () => createWidget('dxForm', {
-        width: 500,
-        height: 500,
-        colCount,
-        labelLocation,
-        items: Array(itemNumber).fill(null).map((_, i) => ({ dataField: `TextBox_${i}` })),
-      }));
-    }
-  }
+          await t
+            .expect(await takeScreenshot(`${testName}.png`, Selector('#container')))
+            .ok()
+            .expect(compareResults.isValid())
+            .ok(compareResults.errorMessages());
+        }).before(async () => createWidget('dxForm', {
+          width: 500,
+          height: 500,
+          colCount,
+          rtlEnabled,
+          labelLocation,
+          items: Array(itemNumber).fill(null).map((_, i) => ({ dataField: `TextBox_${i}` })),
+        }));
+      });
+    });
+  });
 });
