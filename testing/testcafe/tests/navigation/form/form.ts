860593a5441252d5b3c4bfd3802f328fa3c8b79c
Ignatov Dan <ignatovdan@gmail.com>
2021-08-24 19:20:08 +0300

    Typo, add new configs for colCount

--- testing/testcafe/tests/navigation/form/form.ts
+++ testing/testcafe/tests/navigation/form/form.ts
@@ -10,7 +10,7 @@ test('Empty', async (t) => {
   const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
   await t
-    .expect(await takeScreenshot('Empty', Selector('#container')))
+    .expect(await takeScreenshot('Empty.png', Selector('#container')))
     .ok()
     .expect(compareResults.isValid())
     .ok(compareResults.errorMessages());
@@ -19,23 +19,33 @@ test('Empty', async (t) => {
   height: 100,
 }));
 
+// TODO: https://js.devexpress.com/Documentation/21_2/ApiReference/UI_Components/dxForm/Configuration/#labelLocation
+// TODO: https://js.devexpress.com/Documentation/21_2/ApiReference/UI_Components/dxForm/Item_Types/GroupItem/
+// TODO: https://js.devexpress.com/Documentation/21_2/ApiReference/UI_Components/dxForm/Item_Types/TabbedItem/
+// TODO: https://js.devexpress.com/Documentation/21_2/ApiReference/UI_Components/dxForm/Item_Types/ButtonItem/
+// TODO: https://js.devexpress.com/Documentation/21_2/ApiReference/UI_Components/dxForm/Item_Types/EmptyItem/
+// TODO: https://js.devexpress.com/Documentation/21_2/ApiReference/UI_Components/dxForm/Configuration/#rtlEnabled
+
 // eslint-disable-next-line no-plusplus
 for (let colCount = 1; colCount <= 3; colCount++) {
-  const testName = `TextBox_1,colCount_${colCount}`;
-  test(testName, async (t) => {
-    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+  // eslint-disable-next-line no-plusplus
+  for (let itemNumber = 1; itemNumber <= 6; itemNumber++) {
+    const testName = `TextBox_${itemNumber},colCount_${colCount}`;
+    test(testName, async (t) => {
+      const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-    await t
-      .expect(await takeScreenshot(`${testName}.png`, Selector('#container')))
-      .ok()
-      .expect(compareResults.isValid())
-      .ok(compareResults.errorMessages());
-  }).before(async () => createWidget('dxForm', {
-    width: 100,
-    height: 100,
-    colCount: 1,
-    items: [{
-      dataField: `TextBox_${colCount}`,
-    }],
-  }));
+      await t
+        .expect(await takeScreenshot(`${testName}.png`, Selector('#container')))
+        .ok()
+        .expect(compareResults.isValid())
+        .ok(compareResults.errorMessages());
+    }).before(async () => createWidget('dxForm', {
+      width: 500,
+      height: 500,
+      colCount: 1,
+      items: [{
+        dataField: `TextBox_${itemNumber}`,
+      }],
+    }));
+  }
 }
