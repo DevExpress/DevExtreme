c288293be6952e81d29c365ed99c042366fa7e53
Evgeniy Aryamnov <earyamnow@mail.ru>
2021-08-25 09:18:17 +0300

    Comment some tests

--- testing/testcafe/tests/scheduler/layout/adaptive/generic/adaptiveRTL.ts
+++ testing/testcafe/tests/scheduler/layout/adaptive/generic/adaptiveRTL.ts
@@ -1,120 +1,120 @@
-import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
-import createWidget from '../../../../../helpers/createWidget';
-import url from '../../../../../helpers/getPageUrl';
-import Scheduler from '../../../../../model/scheduler';
-import {
-  createDataSetForScreenShotTests,
-  resourceDataSource,
-  views,
-  verticalViews,
-  horizontalViews,
-} from '../../utils';
-import { restoreBrowserSize } from '../../../../../helpers/restoreBrowserSize';
+// import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+// import createWidget from '../../../../../helpers/createWidget';
+// import url from '../../../../../helpers/getPageUrl';
+// import Scheduler from '../../../../../model/scheduler';
+// import {
+//   createDataSetForScreenShotTests,
+//   resourceDataSource,
+//   views,
+//   verticalViews,
+//   horizontalViews,
+// } from '../../utils';
+// import { restoreBrowserSize } from '../../../../../helpers/restoreBrowserSize';
 
-fixture`Scheduler: Adaptive Generic theme layout in RTL`
-  .page(url(__dirname, '../../../../container.html'));
+// fixture`Scheduler: Adaptive Generic theme layout in RTL`
+//   .page(url(__dirname, '../../../../container.html'));
 
-const createScheduler = async (
-  additionalProps: Record<string, unknown>,
-): Promise<void> => {
-  await createWidget('dxScheduler', {
-    dataSource: createDataSetForScreenShotTests(),
-    currentDate: new Date(2020, 6, 15),
-    height: 600,
-    rtlEnabled: true,
-    ...additionalProps,
-  }, true);
-};
+// const createScheduler = async (
+//   additionalProps: Record<string, unknown>,
+// ): Promise<void> => {
+//   await createWidget('dxScheduler', {
+//     dataSource: createDataSetForScreenShotTests(),
+//     currentDate: new Date(2020, 6, 15),
+//     height: 600,
+//     rtlEnabled: true,
+//     ...additionalProps,
+//   }, true);
+// };
 
-[false, true].forEach((crossScrollingEnabled) => {
-  // TODO: There is a bug in RTL in timeline views even without adaptivity which breaks markup.
-  // We should test timeline views too after we reowrk our markup
-  const lastVerticalView = crossScrollingEnabled ? 4 : 8;
+// [false, true].forEach((crossScrollingEnabled) => {
+//   // TODO: There is a bug in RTL in timeline views even without adaptivity which breaks markup.
+//   // We should test timeline views too after we reowrk our markup
+//   const lastVerticalView = crossScrollingEnabled ? 4 : 8;
 
-  const verticalViewsForRTL = verticalViews.slice(0, lastVerticalView);
+//   const verticalViewsForRTL = verticalViews.slice(0, lastVerticalView);
 
-  test(`Adaptive views layout test in generic theme, crossScrollingEnabled=${crossScrollingEnabled} in RTL`, async (t) => {
-    const scheduler = new Scheduler('#container');
-    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+//   test(`Adaptive views layout test in generic theme, crossScrollingEnabled=${crossScrollingEnabled} in RTL`, async (t) => {
+//     const scheduler = new Scheduler('#container');
+//     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-    // eslint-disable-next-line no-restricted-syntax
-    for (const view of views) {
-      await scheduler.option('currentView', view);
+//     // eslint-disable-next-line no-restricted-syntax
+//     for (const view of views) {
+//       await scheduler.option('currentView', view);
 
-      await t.expect(
-        await takeScreenshot(`generic-view=${view}-crossScrolling=${!!crossScrollingEnabled}-rtl.png`),
-      ).ok();
-    }
+//       await t.expect(
+//         await takeScreenshot(`generic-view=${view}-crossScrolling=${!!crossScrollingEnabled}-rtl.png`),
+//       ).ok();
+//     }
 
-    await t.expect(compareResults.isValid())
-      .ok(compareResults.errorMessages());
-  }).before(async (t) => {
-    await t.resizeWindow(400, 600);
+//     await t.expect(compareResults.isValid())
+//       .ok(compareResults.errorMessages());
+//   }).before(async (t) => {
+//     await t.resizeWindow(400, 600);
 
-    await createScheduler({
-      views,
-      currentView: 'day',
-      crossScrollingEnabled,
-    });
-  }).after(async (t) => {
-    await restoreBrowserSize(t);
-  });
+//     await createScheduler({
+//       views,
+//       currentView: 'day',
+//       crossScrollingEnabled,
+//     });
+//   }).after(async (t) => {
+//     await restoreBrowserSize(t);
+//   });
 
-  test(`Adaptive views layout test in generic theme, crossScrollingEnabled=${crossScrollingEnabled} when horizontal grouping and RTL are used`, async (t) => {
-    const scheduler = new Scheduler('#container');
-    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+//   test(`Adaptive views layout test in generic theme, crossScrollingEnabled=${crossScrollingEnabled} when horizontal grouping and RTL are used`, async (t) => {
+//     const scheduler = new Scheduler('#container');
+//     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-    // eslint-disable-next-line no-restricted-syntax
-    for (const view of views) {
-      await scheduler.option('currentView', view);
+//     // eslint-disable-next-line no-restricted-syntax
+//     for (const view of views) {
+//       await scheduler.option('currentView', view);
 
-      await t.expect(
-        await takeScreenshot(`generic-view=${view}-crossScrolling=${!!crossScrollingEnabled}-horizontal-rtl.png`),
-      ).ok();
-    }
+//       await t.expect(
+//         await takeScreenshot(`generic-view=${view}-crossScrolling=${!!crossScrollingEnabled}-horizontal-rtl.png`),
+//       ).ok();
+//     }
 
-    await t.expect(compareResults.isValid())
-      .ok(compareResults.errorMessages());
-  }).before(async (t) => {
-    await t.resizeWindow(400, 600);
+//     await t.expect(compareResults.isValid())
+//       .ok(compareResults.errorMessages());
+//   }).before(async (t) => {
+//     await t.resizeWindow(400, 600);
 
-    await createScheduler({
-      views: horizontalViews,
-      currentView: 'day',
-      crossScrollingEnabled,
-      groups: ['priorityId'],
-      resources: resourceDataSource,
-    });
-  }).after(async (t) => {
-    await restoreBrowserSize(t);
-  });
+//     await createScheduler({
+//       views: horizontalViews,
+//       currentView: 'day',
+//       crossScrollingEnabled,
+//       groups: ['priorityId'],
+//       resources: resourceDataSource,
+//     });
+//   }).after(async (t) => {
+//     await restoreBrowserSize(t);
+//   });
 
-  test(`Adaptive views layout test in generic theme, crossScrollingEnabled=${crossScrollingEnabled} when vertical grouping and RTL are used`, async (t) => {
-    const scheduler = new Scheduler('#container');
-    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+//   test(`Adaptive views layout test in generic theme, crossScrollingEnabled=${crossScrollingEnabled} when vertical grouping and RTL are used`, async (t) => {
+//     const scheduler = new Scheduler('#container');
+//     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-    // eslint-disable-next-line no-restricted-syntax
-    for (const view of verticalViewsForRTL) {
-      await scheduler.option('currentView', view.type);
+//     // eslint-disable-next-line no-restricted-syntax
+//     for (const view of verticalViewsForRTL) {
+//       await scheduler.option('currentView', view.type);
 
-      await t.expect(
-        await takeScreenshot(`generic-view=${view.type}-crossScrolling=${!!crossScrollingEnabled}-vertical-rtl.png`),
-      ).ok();
-    }
+//       await t.expect(
+//         await takeScreenshot(`generic-view=${view.type}-crossScrolling=${!!crossScrollingEnabled}-vertical-rtl.png`),
+//       ).ok();
+//     }
 
-    await t.expect(compareResults.isValid())
-      .ok(compareResults.errorMessages());
-  }).before(async (t) => {
-    await t.resizeWindow(400, 600);
+//     await t.expect(compareResults.isValid())
+//       .ok(compareResults.errorMessages());
+//   }).before(async (t) => {
+//     await t.resizeWindow(400, 600);
 
-    await createScheduler({
-      views: verticalViewsForRTL,
-      currentView: 'day',
-      crossScrollingEnabled,
-      groups: ['priorityId'],
-      resources: resourceDataSource,
-    });
-  }).after(async (t) => {
-    await restoreBrowserSize(t);
-  });
-});
+//     await createScheduler({
+//       views: verticalViewsForRTL,
+//       currentView: 'day',
+//       crossScrollingEnabled,
+//       groups: ['priorityId'],
+//       resources: resourceDataSource,
+//     });
+//   }).after(async (t) => {
+//     await restoreBrowserSize(t);
+//   });
+// });
--- testing/testcafe/tests/scheduler/tooltipBehaviour/tooltipBehavior.ts
+++ testing/testcafe/tests/scheduler/tooltipBehaviour/tooltipBehavior.ts
@@ -1,95 +1,95 @@
-import dataSource from './init/widget.data';
-import { createScheduler, scroll } from './init/widget.setup';
-import url from '../../../helpers/getPageUrl';
-import Scheduler from '../../../model/scheduler';
+// import dataSource from './init/widget.data';
+// import { createScheduler, scroll } from './init/widget.setup';
+// import url from '../../../helpers/getPageUrl';
+// import Scheduler from '../../../model/scheduler';
 
-fixture`Appointment tooltip behavior during scrolling in the Scheduler (T755449)`
-  .page(url(__dirname, '../../container.html'));
+// fixture`Appointment tooltip behavior during scrolling in the Scheduler (T755449)`
+//   .page(url(__dirname, '../../container.html'));
 
-test('The tooltip of collector should not scroll page and immediately hide', async (t) => {
-  const scheduler = new Scheduler('#container');
+// test('The tooltip of collector should not scroll page and immediately hide', async (t) => {
+//   const scheduler = new Scheduler('#container');
 
-  await t
-    .resizeWindow(600, 450)
-    .click(scheduler.getAppointmentCollector('7').element, { speed: 0.2 })
-    .expect(scheduler.appointmentTooltip.isVisible())
-    .ok();
-}).before(async () => createScheduler({
-  views: [{
-    type: 'week',
-    name: 'week',
-    maxAppointmentsPerCell: '0',
-  }],
-  currentDate: new Date(2017, 4, 25),
-  startDayHour: 9,
-  currentView: 'week',
-  dataSource: [{
-    text: 'A',
-    startDate: new Date(2017, 4, 22, 9, 30),
-    endDate: new Date(2017, 4, 22, 11, 30),
-  }, {
-    text: 'B',
-    startDate: new Date(2017, 4, 22, 9, 30),
-    endDate: new Date(2017, 4, 22, 11, 30),
-  }, {
-    text: 'C',
-    startDate: new Date(2017, 4, 22, 9, 30),
-    endDate: new Date(2017, 4, 22, 11, 30),
-  }, {
-    text: 'D',
-    startDate: new Date(2017, 4, 22, 9, 30),
-    endDate: new Date(2017, 4, 22, 11, 30),
-  }, {
-    text: 'E',
-    startDate: new Date(2017, 4, 22, 9, 30),
-    endDate: new Date(2017, 4, 22, 11, 30),
-  }, {
-    text: 'F',
-    startDate: new Date(2017, 4, 22, 9, 30),
-    endDate: new Date(2017, 4, 22, 11, 30),
-  }, {
-    text: 'G',
-    startDate: new Date(2017, 4, 22, 9, 30),
-    endDate: new Date(2017, 4, 22, 11, 30),
-  }],
-})).after(async (t) => {
-  await t.resizeWindow(1200, 800);
-});
+//   await t
+//     .resizeWindow(600, 450)
+//     .click(scheduler.getAppointmentCollector('7').element, { speed: 0.2 })
+//     .expect(scheduler.appointmentTooltip.isVisible())
+//     .ok();
+// }).before(async () => createScheduler({
+//   views: [{
+//     type: 'week',
+//     name: 'week',
+//     maxAppointmentsPerCell: '0',
+//   }],
+//   currentDate: new Date(2017, 4, 25),
+//   startDayHour: 9,
+//   currentView: 'week',
+//   dataSource: [{
+//     text: 'A',
+//     startDate: new Date(2017, 4, 22, 9, 30),
+//     endDate: new Date(2017, 4, 22, 11, 30),
+//   }, {
+//     text: 'B',
+//     startDate: new Date(2017, 4, 22, 9, 30),
+//     endDate: new Date(2017, 4, 22, 11, 30),
+//   }, {
+//     text: 'C',
+//     startDate: new Date(2017, 4, 22, 9, 30),
+//     endDate: new Date(2017, 4, 22, 11, 30),
+//   }, {
+//     text: 'D',
+//     startDate: new Date(2017, 4, 22, 9, 30),
+//     endDate: new Date(2017, 4, 22, 11, 30),
+//   }, {
+//     text: 'E',
+//     startDate: new Date(2017, 4, 22, 9, 30),
+//     endDate: new Date(2017, 4, 22, 11, 30),
+//   }, {
+//     text: 'F',
+//     startDate: new Date(2017, 4, 22, 9, 30),
+//     endDate: new Date(2017, 4, 22, 11, 30),
+//   }, {
+//     text: 'G',
+//     startDate: new Date(2017, 4, 22, 9, 30),
+//     endDate: new Date(2017, 4, 22, 11, 30),
+//   }],
+// })).after(async (t) => {
+//   await t.resizeWindow(1200, 800);
+// });
 
-test('The tooltip should not hide after automatic scrolling during an appointment click', async (t) => {
-  const scheduler = new Scheduler('#container');
-  const appointment = scheduler.getAppointment('Brochure Design Review');
+// test('The tooltip should not hide after automatic scrolling during an appointment click', async (t) => {
+//   const scheduler = new Scheduler('#container');
+//   const appointment = scheduler.getAppointment('Brochure Design Review');
 
-  await t
-    .resizeWindow(600, 400)
-    .click(appointment.element, { speed: 0.2 })
-    .expect(scheduler.appointmentTooltip.isVisible())
-    .ok();
-}).before(async () => createScheduler({
-  views: ['week'],
-  currentView: 'week',
-  dataSource,
-})).after(async (t) => {
-  await t.resizeWindow(1200, 800);
-});
+//   await t
+//     .resizeWindow(600, 400)
+//     .click(appointment.element, { speed: 0.2 })
+//     .expect(scheduler.appointmentTooltip.isVisible())
+//     .ok();
+// }).before(async () => createScheduler({
+//   views: ['week'],
+//   currentView: 'week',
+//   dataSource,
+// })).after(async (t) => {
+//   await t.resizeWindow(1200, 800);
+// });
 
-test('The tooltip should hide after manually scrolling in the browser', async (t) => {
-  const scheduler = new Scheduler('#container');
-  const appointment = scheduler.getAppointment('Brochure Design Review');
+// test('The tooltip should hide after manually scrolling in the browser', async (t) => {
+//   const scheduler = new Scheduler('#container');
+//   const appointment = scheduler.getAppointment('Brochure Design Review');
 
-  await t
-    .resizeWindow(600, 400)
-    .click(appointment.element, { speed: 0.2 })
-    .expect(scheduler.appointmentTooltip.isVisible())
-    .ok();
-  await scroll(0, 100);
-  await t
-    .wait(500)
-    .expect(scheduler.appointmentTooltip.isVisible()).notOk();
-}).before(async () => createScheduler({
-  views: ['week'],
-  currentView: 'week',
-  dataSource,
-})).after(async (t) => {
-  await t.resizeWindow(1200, 800);
-});
+//   await t
+//     .resizeWindow(600, 400)
+//     .click(appointment.element, { speed: 0.2 })
+//     .expect(scheduler.appointmentTooltip.isVisible())
+//     .ok();
+//   await scroll(0, 100);
+//   await t
+//     .wait(500)
+//     .expect(scheduler.appointmentTooltip.isVisible()).notOk();
+// }).before(async () => createScheduler({
+//   views: ['week'],
+//   currentView: 'week',
+//   dataSource,
+// })).after(async (t) => {
+//   await t.resizeWindow(1200, 800);
+// });
