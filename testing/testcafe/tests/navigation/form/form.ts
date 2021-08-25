7b20c1755c18ff7337199158bdb4e13ca5692107
Ignatov Dan <ignatovdan@gmail.com>
2021-08-25 12:23:42 +0300

    Test screenshot size

---
+++ testing/testcafe/tests/navigation/form/form.ts
@@ -0,0 +1,20 @@
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import { Selector } from 'testcafe';
+import url from '../../../helpers/getPageUrl';
+import createWidget from '../../../helpers/createWidget';
+
+fixture`Form`
+  .page(url(__dirname, '../../container.html'));
+
+test('Empty form', async (t) => {
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+  await t
+    .expect(await takeScreenshot('Empty Form', Selector('#container')))
+    .ok()
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => createWidget('dxForm', {
+  width: 100,
+  height: 100,
+}));
