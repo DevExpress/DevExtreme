21d6bf9c7dded1d84e192e363de4232b984a3e94
Alexander Z <alexander.zelevinskiy@devexpress.com>
2021-08-26 16:31:40 +0300

    PdfExportGanttProps

--- js/pdf_exporter.d.ts
+++ js/pdf_exporter.d.ts
@@ -1,6 +1,7 @@
 import { DxPromise } from './core/utils/deferred';
 import dxDataGrid, { Column } from './ui/data_grid';
 import { ExportLoadPanel } from './exporter/export_load_panel';
+import dxGantt from './ui/gantt';
 
 /**
  * @docid
@@ -116,3 +117,77 @@ export interface PdfExportDataGridProps {
  * @public
  */
 export function exportDataGrid(options: PdfExportDataGridProps): DxPromise<void>;
+
+/**
+ * @docid
+ * @namespace DevExpress.pdfExporter
+ */
+ export interface PdfExportGanttProps {
+  /**
+   * @docid
+   * @type_function_param1 options:object
+   * @type_function_return object
+   * @public
+   */
+  createDocumentMethod?: ((options: any) => object);
+  /**
+   * @docid
+   * @default undefined
+   * @public
+   */
+  jsPDFDocument?: object;
+  /**
+   * @docid
+   * @default undefined
+   * @public
+   */
+  component?: dxGantt;
+  /**
+   * @docid
+   * @default undefined
+   * @public
+   */
+  format?: string | object;
+  /**
+   * @docid
+   * @default true
+   * @public
+   */
+  landscape?: boolean;
+  /**
+   * @docid
+   * @default undefined
+   * @public
+   */
+  fileName?: string;
+  /**
+   * @docid
+   * @default undefined
+   * @public
+   */
+  margins?: object;
+   /**
+   * @docid
+   * @type Enums.GanttPdfExportMode
+   * @public
+   */
+  exportMode?: 'all' | 'treeList' | 'chart';
+   /**
+   * @docid
+   * @type Enums.GanttPdfExportDateRange|object
+   * @public
+   */
+  dateRange?: 'all' | 'visible' | object;
+}
+
+/**
+ * @docid pdfExporter.exportGantt
+ * @publicName exportGantt(options)
+ * @param1 options:PdfExportGanttProps
+ * @return Promise<any>
+ * @namespace DevExpress.pdfExporter
+ * @module pdf_exporter
+ * @static
+ * @public
+ */
+ export function exportGantt(options: PdfExportGanttProps): DxPromise<any>;
