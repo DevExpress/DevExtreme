daa2f86a3270aeb8d8d2bb15ef7b848740b4fd31
Alyar <Alyar666@users.noreply.github.com>
2021-08-27 17:02:46 +0300

    DataGrid: Fix the column edit button display after collapsing and expanding a grouped row (T1022630) (#18889)

--- js/ui/grid_core/ui.grid_core.editing.js
+++ js/ui/grid_core/ui.grid_core.editing.js
@@ -2137,7 +2137,7 @@ export const editingModule = {
                 },
                 _updateItemsCore: function(change) {
                     this.callBase(change);
-                    this._updateEditRow(this.items());
+                    this._updateEditRow(this.items(true));
                 },
                 _applyChangeUpdate: function(change) {
                     this._updateEditRow(change.items);
