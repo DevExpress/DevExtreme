50bc0bdd40a7c00634513159b945e5cf74c1ff60
Evgeniy Aryamnov <earyamnow@mail.ru>
2021-08-27 09:49:35 +0300

    Add screenshot test

---
+++ testing/testcafe/tests/scheduler/appointments/dataSource.ts
@@ -0,0 +1,23 @@
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import Scheduler from '../../../model/scheduler';
+import url from '../../../helpers/getPageUrl';
+
+fixture`DataSource`
+  .page(url(__dirname, './pages/containerWithArrayStore.html'));
+
+test('Appointment key should be deleted when removing an appointment from series (T1024213)', async (t) => {
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+  const scheduler = new Scheduler('#scheduler');
+
+  await t
+    .doubleClick(scheduler.getAppointmentByIndex(1).element)
+    .click(scheduler.appointmentPopup.doneButton)
+
+    .wait(10000)
+    .expect(await takeScreenshot('exclude-appointment-from-series-via-form-editing.png', scheduler.workSpace))
+    .ok()
+
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+});
