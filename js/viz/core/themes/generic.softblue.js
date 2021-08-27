1b57b8ac9e9e77b6017c31d7076c83bc8c3be9aa
Alexander Bezborodov <alexander.bezborodov@devexpress.com>
2021-08-27 13:21:57 +0300

    dxViz - Fix registration of viz themes for ES6 modularity (T1021540) (#18964)

--- js/viz/core/themes/generic.softblue.js
+++ js/viz/core/themes/generic.softblue.js
@@ -1,4 +1,3 @@
-import { registerTheme } from '../../themes';
 const ACCENT_COLOR = '#7ab8eb';
 const BACKGROUND_COLOR = '#fff';
 const TITLE_COLOR = '#333';
@@ -6,129 +5,135 @@ const SUBTITLE_COLOR = '#99a1a8';
 const TEXT_COLOR = '#707070';
 const BORDER_COLOR = '#e8eaeb';
 
-registerTheme({
-    name: 'generic.softblue',
-    defaultPalette: 'Soft Blue',
+export default [{
+    theme: {
+        name: 'generic.softblue',
+        defaultPalette: 'Soft Blue',
 
-    backgroundColor: BACKGROUND_COLOR,
-    primaryTitleColor: TITLE_COLOR,
-    secondaryTitleColor: SUBTITLE_COLOR,
-    gridColor: BORDER_COLOR,
-    axisColor: TEXT_COLOR,
-    'export': {
         backgroundColor: BACKGROUND_COLOR,
-        font: {
-            color: TITLE_COLOR
-        },
-        button: {
-            'default': {
-                color: TITLE_COLOR,
-                borderColor: '#c9d0d4',
-                backgroundColor: BACKGROUND_COLOR
+        primaryTitleColor: TITLE_COLOR,
+        secondaryTitleColor: SUBTITLE_COLOR,
+        gridColor: BORDER_COLOR,
+        axisColor: TEXT_COLOR,
+        'export': {
+            backgroundColor: BACKGROUND_COLOR,
+            font: {
+                color: TITLE_COLOR
             },
-            hover: {
-                color: TITLE_COLOR,
-                borderColor: '#a7b2b9',
-                backgroundColor: '#e6e6e6'
-            },
-            focus: {
-                color: TITLE_COLOR,
-                borderColor: '#82929b',
-                backgroundColor: '#e6e6e6'
-            },
-            active: {
-                color: TITLE_COLOR,
-                borderColor: '#82929b',
-                backgroundColor: '#d4d4d4'
+            button: {
+                'default': {
+                    color: TITLE_COLOR,
+                    borderColor: '#c9d0d4',
+                    backgroundColor: BACKGROUND_COLOR
+                },
+                hover: {
+                    color: TITLE_COLOR,
+                    borderColor: '#a7b2b9',
+                    backgroundColor: '#e6e6e6'
+                },
+                focus: {
+                    color: TITLE_COLOR,
+                    borderColor: '#82929b',
+                    backgroundColor: '#e6e6e6'
+                },
+                active: {
+                    color: TITLE_COLOR,
+                    borderColor: '#82929b',
+                    backgroundColor: '#d4d4d4'
+                }
             }
-        }
-    },
-    legend: {
-        font: {
-            color: TEXT_COLOR
-        }
-    },
-    tooltip: {
-        color: BACKGROUND_COLOR,
-        border: {
-            color: BORDER_COLOR
         },
-        font: {
-            color: TITLE_COLOR
-        }
-    },
-    'chart:common': {
-        commonSeriesSettings: {
-            label: {
+        legend: {
+            font: {
+                color: TEXT_COLOR
+            }
+        },
+        tooltip: {
+            color: BACKGROUND_COLOR,
+            border: {
+                color: BORDER_COLOR
+            },
+            font: {
+                color: TITLE_COLOR
+            }
+        },
+        'chart:common': {
+            commonSeriesSettings: {
+                label: {
+                    border: {
+                        color: BORDER_COLOR
+                    }
+                }
+            }
+        },
+        'chart:common:annotation': {
+            color: BACKGROUND_COLOR,
+            border: {
+                color: BORDER_COLOR
+            },
+            font: {
+                color: TITLE_COLOR
+            }
+        },
+        chart: {
+            commonPaneSettings: {
                 border: {
                     color: BORDER_COLOR
                 }
-            }
-        }
-    },
-    'chart:common:annotation': {
-        color: BACKGROUND_COLOR,
-        border: {
-            color: BORDER_COLOR
-        },
-        font: {
-            color: TITLE_COLOR
-        }
-    },
-    chart: {
-        commonPaneSettings: {
-            border: {
-                color: BORDER_COLOR
+            },
+            commonAxisSettings: {
+                breakStyle: { color: '#cfd2d3' }
             }
         },
-        commonAxisSettings: {
-            breakStyle: { color: '#cfd2d3' }
-        }
-    },
-    rangeSelector: {
-        scale: {
-            breakStyle: { color: '#cfd2d3' },
-            tick: {
-                opacity: 0.12
+        rangeSelector: {
+            scale: {
+                breakStyle: { color: '#cfd2d3' },
+                tick: {
+                    opacity: 0.12
+                }
+            },
+            selectedRangeColor: ACCENT_COLOR,
+            sliderMarker: {
+                color: ACCENT_COLOR
+            },
+            sliderHandle: {
+                color: ACCENT_COLOR,
+                opacity: 0.5
             }
         },
-        selectedRangeColor: ACCENT_COLOR,
-        sliderMarker: {
+        sparkline: {
+            pointColor: BACKGROUND_COLOR,
+            minColor: '#f0ad4e',
+            maxColor: '#d9534f'
+        },
+        treeMap: {
+            group: {
+                color: BORDER_COLOR,
+                label: {
+                    font: {
+                        color: SUBTITLE_COLOR
+                    }
+                }
+            }
+        },
+        bullet: {
             color: ACCENT_COLOR
         },
-        sliderHandle: {
-            color: ACCENT_COLOR,
-            opacity: 0.5
-        }
-    },
-    sparkline: {
-        pointColor: BACKGROUND_COLOR,
-        minColor: '#f0ad4e',
-        maxColor: '#d9534f'
-    },
-    treeMap: {
-        group: {
-            color: BORDER_COLOR,
-            label: {
-                font: {
-                    color: SUBTITLE_COLOR
+        gauge: {
+            valueIndicators: {
+                'rangebar': {
+                    color: ACCENT_COLOR
+                },
+                'textcloud': {
+                    color: ACCENT_COLOR
                 }
             }
         }
     },
-    bullet: {
-        color: ACCENT_COLOR
+    baseThemeName: 'generic.light'
+}, {
+    theme: {
+        name: 'generic.softblue.compact'
     },
-    gauge: {
-        valueIndicators: {
-            'rangebar': {
-                color: ACCENT_COLOR
-            },
-            'textcloud': {
-                color: ACCENT_COLOR
-            }
-        }
-    }
-}, 'generic.light');
-
-registerTheme({ name: 'generic.softblue.compact' }, 'generic.softblue');
+    baseThemeName: 'generic.softblue'
+}];
