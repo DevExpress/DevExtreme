b0422800d2a5585269ef811a9663053aca76c749
babich-a <alexey.babych@gmail.com>
2021-08-30 11:43:18 +0300

    Fix diagram toolbar and floating panel after material upgrade

--- js/ui/diagram/ui.diagram.floating_panel.js
+++ js/ui/diagram/ui.diagram.floating_panel.js
@@ -122,6 +122,7 @@ class DiagramFloatingPanel extends DiagramPanel {
             minWidth: this._getPopupMinWidth(),
             minHeight: this._getPopupMinHeight(),
             position: this._getPopupPosition(),
+            showCloseButton: true,
             copyRootClassesToWrapper: true,
             onContentReady: function() {
                 that._renderPopupContent(that._popup.content());
