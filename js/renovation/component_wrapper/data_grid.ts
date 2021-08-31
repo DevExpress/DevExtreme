28383a9e4e0ebbf413c7315e803e49d823a4d6a5
Roman Semenov <roman.semenov@devexpress.com>
2021-08-31 07:38:59 +0300

    fix wrapper

--- js/renovation/component_wrapper/data_grid.ts
+++ js/renovation/component_wrapper/data_grid.ts
@@ -4,6 +4,7 @@ import { getPathParts } from '../../core/utils/data';
 import Component, { ComponentWrapperProps } from './common/component';
 import type { DataGridForComponentWrapper, GridInstance } from '../ui/grids/data_grid/common/types';
 import gridCore from '../../ui/data_grid/ui.data_grid.core';
+import { Column } from '../../ui/data_grid';
 import { updatePropsImmutable } from './utils/update_props_immutable';
 import type { TemplateComponent, Option } from './common/types';
 import type { ExcelCellInfo, Export, OptionChangedEvent } from '../../ui/data_grid';
@@ -22,7 +23,14 @@ componentRegistratorCallbacks.add((name, componentClass) => {
   }
 });
 
-export default class DataGridWrapper extends Component {
+export default class DataGridWrapper
+  <TRowData,
+   TKeyExpr extends string | string[],
+   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+   TKey=TKeyExpr extends keyof TRowData ? TRowData[TKeyExpr] : any,
+   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+   TColumns extends Column<TRowData, TKey, any>[]=Column<TRowData, TKey, any>[],
+  > extends Component {
   static registerModule = gridCore.registerModule.bind(gridCore);
 
   _onInitialized!: Function;
@@ -69,8 +77,10 @@ export default class DataGridWrapper extends Component {
     return this._getInternalInstance()?.isReady();
   }
 
-  _getInternalInstance(): GridInstance {
-    return (this.viewRef as DataGridForComponentWrapper)?.getComponentInstance();
+  _getInternalInstance(): GridInstance<TRowData, TKeyExpr, TKey, TColumns> {
+    return (
+      this.viewRef as DataGridForComponentWrapper<TRowData, TKeyExpr, TKey, TColumns>
+    )?.getComponentInstance();
   }
 
   _fireContentReady(): void {}
@@ -83,7 +93,11 @@ export default class DataGridWrapper extends Component {
     super._optionChanging(fullName, prevValue, value);
     if (this.viewRef && prevValue !== value) {
       const name = getPathParts(fullName)[0];
-      const prevProps = { ...(this.viewRef as DataGridForComponentWrapper).prevProps };
+      const prevProps = {
+        ...(
+          this.viewRef as DataGridForComponentWrapper<TRowData, TKeyExpr, TKey, TColumns>
+        ).prevProps,
+      };
 
       if (name === 'integrationOptions') {
         return;
@@ -104,7 +118,9 @@ export default class DataGridWrapper extends Component {
         updatePropsImmutable(prevProps, this.option(), name, fullName);
       }
 
-      (this.viewRef as DataGridForComponentWrapper).prevProps = prevProps;
+      (
+        this.viewRef as DataGridForComponentWrapper<TRowData, TKeyExpr, TKey, TColumns>
+      ).prevProps = prevProps;
     }
   }
 
@@ -135,11 +151,11 @@ export default class DataGridWrapper extends Component {
     // eslint-disable-next-line no-param-reassign
     options.onInitialized = this._onInitialized;
 
-    const exportOptions = options.export as Export;
+    const exportOptions = options.export as Export<TRowData, TKey>;
     const originalCustomizeExcelCell = exportOptions?.customizeExcelCell;
 
     if (originalCustomizeExcelCell) {
-      exportOptions.customizeExcelCell = (e: ExcelCellInfo): void => {
+      exportOptions.customizeExcelCell = (e: ExcelCellInfo<TRowData, TKey>): void => {
         // eslint-disable-next-line
         (e as any).component = this;
 
@@ -169,7 +185,7 @@ export default class DataGridWrapper extends Component {
     }
   }
 
-  _internalOptionChangedHandler(e: OptionChangedEvent): void {
+  _internalOptionChangedHandler(e: OptionChangedEvent<TRowData, TKey>): void {
     const isSecondLevelOption = e.name !== e.fullName;
 
     if (isSecondLevelOption && e.value !== e.previousValue) {
