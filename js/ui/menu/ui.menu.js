6c89f4094dccdec24c2bdefc016d688cdeec0f44
Ignatov Dan <ignatovdan@gmail.com>
2021-09-02 09:58:29 +0300

    Menu - Avoid "Cannot use 'in' operator to search for 'length' in true" error when adaptivityEnabled is true (#18986)

--- js/ui/menu/ui.menu.js
+++ js/ui/menu/ui.menu.js
@@ -957,8 +957,9 @@ class Menu extends MenuBase {
                 super._optionChanged(args);
                 break;
             default:
-                if(this._isAdaptivityEnabled()) {
-                    this._treeView.option(args.name, args.value);
+                if(this._isAdaptivityEnabled() && ((args.name === args.fullName) || (args.name === 'items'))) {
+                    // TODO: if(args.name === 'items') this._treeView.option('items', this.option('items')) or treeView.repaint() ?
+                    this._treeView.option(args.fullName, args.value);
                 }
                 super._optionChanged(args);
         }
