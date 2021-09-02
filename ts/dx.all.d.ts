16ddf91a03895c834c2546ec81e4c6b0a335cab8
Artem Kobilinskii <a.kobilinsky@gmail.com>
2021-09-02 15:11:37 +0300

    dxGantt: Add new api (#19032)
    * dxGantt: Rename toolbar items
    
    * dxGantt: update d.ts
    
    * dxGantt: Add new api
    
    * dxGantt: Update version
    
    Co-authored-by: kobilinsky.artem <artem.kobilinsky@devexpress.com>

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -12581,6 +12581,22 @@ declare module DevExpress.ui {
      * [descr:dxGantt.showDependencies(value)]
      */
     showDependencies(value: boolean): void;
+    /**
+     * [descr:dxGantt.zoomIn()]
+     */
+    zoomIn(): void;
+    /**
+     * [descr:dxGantt.zoomOut()]
+     */
+    zoomOut(): void;
+    /**
+     * [descr:dxGantt.unassignAllResourcesFromTask(taskKey)]
+     */
+    unassignAllResourcesFromTask(taskKey: any): void;
+    /**
+     * [descr:dxGantt.showTaskDetailsDialog(taskKey)]
+     */
+    showTaskDetailsDialog(taskKey: any): void;
   }
   module dxGantt {
     export type ContentReadyEvent = DevExpress.events.EventInfo<dxGantt>;
