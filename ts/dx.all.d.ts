49196a4b881eb12d6725e05f49b238ce357f8948
VictorSavushkin <victor.savushkin@devexpress.com>
2021-09-06 13:20:54 +0300

    Diagram - fix for docs

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -9159,7 +9159,7 @@ declare module DevExpress.ui {
       /**
        * [descr:dxDiagramOptions.edges.customDataExpr]
        */
-      customDataExpr?: string | ((data: any, value: any) => any);
+      customDataExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.edges.dataSource]
        */
@@ -9171,59 +9171,59 @@ declare module DevExpress.ui {
       /**
        * [descr:dxDiagramOptions.edges.fromExpr]
        */
-      fromExpr?: string | ((data: any, value: any) => any);
+      fromExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.edges.fromLineEndExpr]
        */
-      fromLineEndExpr?: string | ((data: any, value: any) => any);
+      fromLineEndExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.edges.fromPointIndexExpr]
        */
-      fromPointIndexExpr?: string | ((data: any, value: any) => any);
+      fromPointIndexExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.edges.keyExpr]
        */
-      keyExpr?: string | ((data: any, value: any) => any);
+      keyExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.edges.lineTypeExpr]
        */
-      lineTypeExpr?: string | ((data: any, value: any) => any);
+      lineTypeExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.edges.lockedExpr]
        */
-      lockedExpr?: string | ((data: any, value: any) => any);
+      lockedExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.edges.pointsExpr]
        */
-      pointsExpr?: string | ((data: any, value: any) => any);
+      pointsExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.edges.styleExpr]
        */
-      styleExpr?: string | ((data: any, value: any) => any);
+      styleExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.edges.textExpr]
        */
-      textExpr?: string | ((data: any, value: any) => any);
+      textExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.edges.textStyleExpr]
        */
-      textStyleExpr?: string | ((data: any, value: any) => any);
+      textStyleExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.edges.toExpr]
        */
-      toExpr?: string | ((data: any, value: any) => any);
+      toExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.edges.toLineEndExpr]
        */
-      toLineEndExpr?: string | ((data: any, value: any) => any);
+      toLineEndExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.edges.toPointIndexExpr]
        */
-      toPointIndexExpr?: string | ((data: any, value: any) => any);
+      toPointIndexExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.edges.zIndexExpr]
        */
-      zIndexExpr?: string | ((data: any, value: any) => any);
+      zIndexExpr?: string | ((data: any, value?: any) => any);
     };
     /**
      * [descr:dxDiagramOptions.export]
@@ -9286,15 +9286,15 @@ declare module DevExpress.ui {
       /**
        * [descr:dxDiagramOptions.nodes.containerChildrenExpr]
        */
-      containerChildrenExpr?: string | ((data: any, value: any) => any);
+      containerChildrenExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.nodes.containerKeyExpr]
        */
-      containerKeyExpr?: string | ((data: any, value: any) => any);
+      containerKeyExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.nodes.customDataExpr]
        */
-      customDataExpr?: string | ((data: any, value: any) => any);
+      customDataExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.nodes.dataSource]
        */
@@ -9306,59 +9306,59 @@ declare module DevExpress.ui {
       /**
        * [descr:dxDiagramOptions.nodes.heightExpr]
        */
-      heightExpr?: string | ((data: any, value: any) => any);
+      heightExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.nodes.imageUrlExpr]
        */
-      imageUrlExpr?: string | ((data: any, value: any) => any);
+      imageUrlExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.nodes.itemsExpr]
        */
-      itemsExpr?: string | ((data: any, value: any) => any);
+      itemsExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.nodes.keyExpr]
        */
-      keyExpr?: string | ((data: any, value: any) => any);
+      keyExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.nodes.leftExpr]
        */
-      leftExpr?: string | ((data: any, value: any) => any);
+      leftExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.nodes.lockedExpr]
        */
-      lockedExpr?: string | ((data: any, value: any) => any);
+      lockedExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.nodes.parentKeyExpr]
        */
-      parentKeyExpr?: string | ((data: any, value: any) => any);
+      parentKeyExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.nodes.styleExpr]
        */
-      styleExpr?: string | ((data: any, value: any) => any);
+      styleExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.nodes.textExpr]
        */
-      textExpr?: string | ((data: any, value: any) => any);
+      textExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.nodes.textStyleExpr]
        */
-      textStyleExpr?: string | ((data: any, value: any) => any);
+      textStyleExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.nodes.topExpr]
        */
-      topExpr?: string | ((data: any, value: any) => any);
+      topExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.nodes.typeExpr]
        */
-      typeExpr?: string | ((data: any, value: any) => any);
+      typeExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.nodes.widthExpr]
        */
-      widthExpr?: string | ((data: any, value: any) => any);
+      widthExpr?: string | ((data: any, value?: any) => any);
       /**
        * [descr:dxDiagramOptions.nodes.zIndexExpr]
        */
-      zIndexExpr?: string | ((data: any, value: any) => any);
+      zIndexExpr?: string | ((data: any, value?: any) => any);
     };
     /**
      * [descr:dxDiagramOptions.hasChanges]
