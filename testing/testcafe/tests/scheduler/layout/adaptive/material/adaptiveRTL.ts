d6cc9d99d570ab70fb3a5dfe51239906091a4e73
Evgeniy Aryamnov <earyamnow@mail.ru>
2021-09-06 09:42:18 +0300

    Refactor tests

--- testing/testcafe/tests/scheduler/layout/adaptive/material/adaptiveRTL.ts
+++ testing/testcafe/tests/scheduler/layout/adaptive/material/adaptiveRTL.ts
@@ -27,12 +27,6 @@ const createScheduler = async (
 };
 
 [false, true].forEach((crossScrollingEnabled) => {
-  // TODO: There is a bug in RTL in timeline views even without adaptivity which breaks markup.
-  // We should test timeline views too after we reowrk our markup
-  const lastVerticalView = crossScrollingEnabled ? 4 : 8;
-
-  const verticalViewsForRTL = verticalViews.slice(0, lastVerticalView);
-
   test(`Adaptive views layout test in material theme, crossScrollingEnabled=${crossScrollingEnabled} in RTL`, async (t) => {
     const scheduler = new Scheduler('#container');
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
@@ -94,7 +88,7 @@ const createScheduler = async (
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
     // eslint-disable-next-line no-restricted-syntax
-    for (const view of verticalViewsForRTL) {
+    for (const view of verticalViews) {
       await scheduler.option('currentView', view.type);
 
       // Another bug in RTL in month view
@@ -111,7 +105,7 @@ const createScheduler = async (
     await t.resizeWindow(400, 600);
 
     await createScheduler({
-      views: verticalViewsForRTL,
+      views: verticalViews,
       currentView: 'day',
       crossScrollingEnabled,
       groups: ['priorityId'],
