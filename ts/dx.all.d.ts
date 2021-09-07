80e1f91f810db0f56dc25cf9e69166a61226dfaf
Zedwag <zedwag@gmail.com>
2021-09-07 12:32:07 +0300

    ProgressBar: add false value type

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -16193,7 +16193,7 @@ declare module DevExpress.ui {
     /**
      * [descr:dxProgressBarOptions.value]
      */
-    value?: number;
+    value?: number | false;
   }
   /**
    * [descr:dxRadioGroup]
