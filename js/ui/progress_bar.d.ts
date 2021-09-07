5fb41b7ef88bd37cfdbad567bd81664d99ae5297
Zedwag <zedwag@gmail.com>
2021-09-07 16:22:58 +0300

    fix

--- js/ui/progress_bar.d.ts
+++ js/ui/progress_bar.d.ts
@@ -45,7 +45,7 @@ export interface dxProgressBarOptions extends dxTrackBarOptions<dxProgressBar> {
     statusFormat?: string | ((ratio: number, value: number) => string);
     /**
      * @docid dxProgressBarOptions.value
-     * @type number
+     * @type number|boolean
      * @default 0
      * @prevFileNamespace DevExpress.ui
      * @public
