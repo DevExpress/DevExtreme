9a8bcbcb7f9de2b127d21f12498876778f34f282
Alexey Babich <alexey.babych@gmail.com>
2021-09-07 20:31:37 +0300

    Enable 'no-restricted-properties' eslint rule (#1270)

--- Demos/RangeSelector/LogarithmicScale/Knockout/index.js
+++ Demos/RangeSelector/LogarithmicScale/Knockout/index.js
@@ -3,7 +3,7 @@ window.onload = function () {
   const max = 100;
 
   for (let i = 0; i < max; i++) {
-    dataSource.push({ arg: Math.pow(10, i * 0.1), val: Math.log(i + 1) / Math.log(0.5) + (Math.random() - 0.5) * (100 / (i + 1)) + 10 });
+    dataSource.push({ arg: 10 ** i * 0.1, val: Math.log(i + 1) / Math.log(0.5) + (Math.random() - 0.5) * (100 / (i + 1)) + 10 });
   }
 
   const viewModel = {
