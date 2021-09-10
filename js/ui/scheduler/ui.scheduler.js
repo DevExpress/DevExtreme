080d347a9ee70bbef0d91ab42dae69b67f3c8dc6
Aryamnov Eugeniy <51905811+AryamnovEugeniy@users.noreply.github.com>
2021-09-10 11:42:17 +0300

    dxScheduler - rerender the Scheduler only once when a template is changed (T1028189​​​​​​​) (#19158)

--- js/ui/scheduler/ui.scheduler.js
+++ js/ui/scheduler/ui.scheduler.js
@@ -481,7 +481,6 @@ class Scheduler extends Widget {
             case 'resourceCellTemplate':
             case 'dataCellTemplate':
             case 'timeCellTemplate':
-                this._updateOption('workSpace', name, value);
                 this.repaint();
                 break;
             case 'groups':
