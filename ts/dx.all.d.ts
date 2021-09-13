d599e7bb6406ce9ef84189362b34ed3edbc572be
ilya.kharchenko <14272298+IlyaKhD@users.noreply.github.com>
2021-09-13 12:58:00 +0300

    Add DefaultOptionsRule type (#19075)

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -824,7 +824,7 @@ declare module DevExpress {
      * [descr:DOMComponent.defaultOptions(rule)]
      */
     static defaultOptions<TProperties = DevExpress.DOMComponent.Properties>(
-      rule: Partial<DevExpress.core.Rule<TProperties>>
+      rule: DevExpress.core.DefaultOptionsRule<TProperties>
     ): void;
 
     /**
@@ -1461,6 +1461,16 @@ declare module DevExpress.core {
    */
   interface Condition extends JQueryEventObject {}
   /**
+   * @deprecated Attention! This type is for internal purposes only. If you used it previously, please describe your scenario in the following GitHub Issue, and we will suggest a public alternative: {@link https://github.com/DevExpress/DevExtreme/issues/17885|Internal Types}.
+   */
+  export type DeepPartial<T> = {
+    [P in keyof T]?: DeepPartial<T[P]>;
+  };
+  export type DefaultOptionsRule<T> = {
+    device?: Device | Device[] | ((device: Device) => boolean);
+    options: DeepPartial<T>;
+  };
+  /**
    * [descr:dxElement]
    * @deprecated [depNote:dxElement]
    * @deprecated Attention! This type is for internal purposes only. If you used it previously, please describe your scenario in the following GitHub Issue, and we will suggest a public alternative: {@link https://github.com/DevExpress/DevExtreme/issues/17885|Internal Types}.
@@ -1530,19 +1540,6 @@ declare module DevExpress.core {
    * @deprecated Attention! This type is for internal purposes only. If you used it previously, please describe your scenario in the following GitHub Issue, and we will suggest a public alternative: {@link https://github.com/DevExpress/DevExtreme/issues/17885|Internal Types}.
    */
   interface PromiseType<T> extends JQueryPromise<T> {}
-  /**
-   * @deprecated Attention! This type is for internal purposes only. If you used it previously, please describe your scenario in the following GitHub Issue, and we will suggest a public alternative: {@link https://github.com/DevExpress/DevExtreme/issues/17885|Internal Types}.
-   */
-  export type RecursivePartial<T> = {
-    [P in keyof T]?: RecursivePartial<T[P]>;
-  };
-  /**
-   * @deprecated Attention! This type is for internal purposes only. If you used it previously, please describe your scenario in the following GitHub Issue, and we will suggest a public alternative: {@link https://github.com/DevExpress/DevExtreme/issues/17885|Internal Types}.
-   */
-  export type Rule<T> = {
-    device: ((device: Device) => boolean) | Device | Device[];
-    options: RecursivePartial<T>;
-  };
   /**
    * [descr:template]
    */
@@ -23556,7 +23553,7 @@ declare module DevExpress.viz {
      * @deprecated Attention! This type is for internal purposes only. If you used it previously, please describe your scenario in the following GitHub Issue, and we will suggest a public alternative: {@link https://github.com/DevExpress/DevExtreme/issues/17885|Internal Types}.
      */
     static defaultOptions<TProperties>(
-      rule: Partial<DevExpress.core.Rule<TProperties>>
+      rule: DevExpress.core.DefaultOptionsRule<TProperties>
     ): void;
     /**
      * [descr:BaseWidget.exportTo(fileName, format)]
