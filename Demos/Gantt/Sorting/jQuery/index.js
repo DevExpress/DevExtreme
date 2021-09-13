567e4119745482246b3c1b4ed755d8e3f00216fd
Artem Kobilinskii <a.kobilinsky@gmail.com>
2021-09-13 16:00:06 +0300

    dxGantt: Add Sorting demo (#1317)
    * dxGantt: Add Sorting demo
    
    * dxGantt: fix linters
    
    * dxGantt: fix linters
    
    * dxGantt: update etalon
    
    * dxGantt: update etalon
    
    Co-authored-by: kobilinsky.artem <artem.kobilinsky@devexpress.com>

---
+++ Demos/Gantt/Sorting/jQuery/index.js
@@ -0,0 +1,34 @@
+$(() => {
+  $('#gantt').dxGantt({
+    rootValue: -1,
+    tasks: {
+      dataSource: tasks,
+    },
+    dependencies: {
+      dataSource: dependencies,
+    },
+    resources: {
+      dataSource: resources,
+    },
+    resourceAssignments: {
+      dataSource: resourceAssignments,
+    },
+    editing: {
+      enabled: true,
+    },
+    columns: [{
+      dataField: 'title',
+      caption: 'Subject',
+      width: 300,
+      sortOrder: 'asc',
+    }, {
+      dataField: 'start',
+      caption: 'Start Date',
+    }, {
+      dataField: 'end',
+      caption: 'End Date',
+    }],
+    scaleType: 'weeks',
+    taskListWidth: 500,
+  });
+});
