1ed5bf1953bc31c3fe242048bb53bbfb46550de0
andrewmakarov <andrewmakarov@inbox.ru>
2021-09-14 19:40:05 +0300

    Clean up resource manager

--- js/ui/scheduler/resources/resourceManager.js
+++ js/ui/scheduler/resources/resourceManager.js
@@ -1,36 +1,23 @@
-import { wrapToArray } from '../../../core/utils/array';
 import { isDefined } from '../../../core/utils/type';
 import { compileGetter, compileSetter } from '../../../core/utils/data';
 import { when, Deferred } from '../../../core/utils/deferred';
 
 import { AgendaResourceProcessor } from './agendaResourceProcessor';
 import {
-    getCellGroups,
     getDisplayExpr,
     getFieldExpr,
     getValueExpr,
     getWrappedDataSource,
-    getResourceColor,
     filterResources,
-    getPaintedResources,
 } from './utils';
 
 export class ResourceManager {
     constructor(resources) {
         this.loadedResources = [];
         this.resourceLoaderMap = new Map();
-        this._dataAccessors = {
-            getter: {},
-            setter: {}
-        };
         this.agendaProcessor = new AgendaResourceProcessor();
 
         this.setResources(resources);
-    }
-
-    getDataAccessors(fieldName, type) {
-        const actions = this._dataAccessors[type];
-        return actions[fieldName];
     }
 
     setResources(resources = []) {
@@ -51,10 +38,6 @@ export class ResourceManager {
         });
 
         this.agendaProcessor.initializeState(resources);
-    }
-
-    getResources() {
-        return this._resources || [];
     }
 
     isLoaded() {
@@ -120,29 +103,5 @@ export class ResourceManager {
 
     _createPlainResourcesByAppointmentAsync(rawAppointment) {
         return this.agendaProcessor.createListAsync(rawAppointment);
-    }
-
-    getAppointmentColor({ groupIndex, itemData, groups }) {
-        const paintedResources = getPaintedResources(this.getResources(), groups);
-
-        if(paintedResources) {
-            const field = getFieldExpr(paintedResources);
-
-            const cellGroups = getCellGroups(groupIndex, this.loadedResources);
-            const resourceValues = wrapToArray(this.getDataAccessors(field, 'getter')(itemData));
-
-            let groupId = resourceValues[0];
-
-            for(let i = 0; i < cellGroups.length; i++) {
-                if(cellGroups[i].name === field) {
-                    groupId = cellGroups[i].id;
-                    break;
-                }
-            }
-
-            return getResourceColor(this.getResources(), this.resourceLoaderMap, field, groupId);
-        }
-
-        return new Deferred().resolve().promise();
     }
 }
