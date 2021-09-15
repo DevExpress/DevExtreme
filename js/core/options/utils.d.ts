b5742b24ec403f52d60aa3e5301261d1645d1f25
Elizaveta Galuza (DevExpress) <galuza.elizaveta@corp.devexpress.com>
2021-09-15 18:20:20 +0300

    Remove type Rule

--- js/core/options/utils.d.ts
+++ js/core/options/utils.d.ts
@@ -6,9 +6,6 @@ import {
     DefaultOptionsRule,
 } from '../options';
 
-// TODO: Remove after https://trello.com/c/me612NxO/2872-rename-rule-to-defauloptionrule is ready
-export type Rule<T> = DefaultOptionsRule<T>;
-
 export function convertRulesToOptions<T>(rules: DefaultOptionsRule<T>[]): T;
 
 export function normalizeOptions(options: string | object, value): { [name: string]: string };
@@ -19,5 +16,4 @@ export function getFieldName(fullName: string): string;
 
 export function getParentName(fullName: string): string;
 
-// TODO: Remove Required<T> after https://trello.com/c/me612NxO/2872-rename-rule-to-defauloptionrule is ready
-export function createDefaultOptionRules<T>(options?: DefaultOptionsRule<T>[]): Required<DefaultOptionsRule<T>>[];
+export function createDefaultOptionRules<T>(options?: DefaultOptionsRule<T>[]): DefaultOptionsRule<T>[];
