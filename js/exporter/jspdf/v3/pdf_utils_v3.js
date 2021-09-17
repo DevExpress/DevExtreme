12ac29c83301b63aa081c305f10ef9066b43894e
kuznetsov.vladimir <vladimir.kuznetsov@devexpress.com>
2021-09-17 10:39:20 +0300

    Refactoring of 'calculateTextWidth' and 'calculateTextHeight' methods

--- js/exporter/jspdf/v3/pdf_utils_v3.js
+++ js/exporter/jspdf/v3/pdf_utils_v3.js
@@ -5,24 +5,27 @@ function round(value) {
     return Math.round(value * 1000) / 1000; // checked with browser zoom - 500%
 }
 
-function getTextLines(doc, text, padding, font, { wordWrapEnabled, columnWidth }) {
+function getTextLines(doc, text, font, { wordWrapEnabled, textWidth }) {
     if(wordWrapEnabled) {
-        const cellContentWidth = columnWidth - (padding.left + padding.right);
         // it also splits text by '\n' automatically
-        return doc.splitTextToSize(text, cellContentWidth, {
+        return doc.splitTextToSize(text, textWidth, {
             fontSize: font?.size || doc.getFontSize()
         });
     }
     return text.split('\n');
 }
 
-function calculateTextHeight(doc, text, padding, font, { wordWrapEnabled, columnWidth }) {
+function calculateTextWidth(columnWidth, padding) {
+    return columnWidth - (padding.left + padding.right);
+}
+
+function calculateTextHeight(doc, text, font, { wordWrapEnabled, textWidth }) {
     const height = doc.getTextDimensions(text, {
         fontSize: font?.size || doc.getFontSize()
     }).h;
 
-    const linesCount = getTextLines(doc, text, padding, font, { wordWrapEnabled, columnWidth }).length;
-    return height * linesCount * doc.getLineHeightFactor() + (padding.top + padding.bottom);
+    const linesCount = getTextLines(doc, text, font, { wordWrapEnabled, textWidth }).length;
+    return height * linesCount * doc.getLineHeightFactor();
 }
 
 function calculateRowHeight(doc, cells, columnWidths) {
@@ -41,8 +44,9 @@ function calculateRowHeight(doc, cells, columnWidths) {
         const font = cells[cellIndex].pdfCell.font;
         const wordWrapEnabled = cells[cellIndex].pdfCell.wordWrapEnabled;
         const columnWidth = columnWidths[cellIndex];
+        const textWidth = calculateTextWidth(columnWidth, cellPadding);
         if(isDefined(cellText)) {
-            const cellHeight = calculateTextHeight(doc, cellText, cellPadding, font, { wordWrapEnabled, columnWidth });
+            const cellHeight = calculateTextHeight(doc, cellText, font, { wordWrapEnabled, textWidth }) + cellPadding.top + cellPadding.bottom;
             if(rowHeight < cellHeight) {
                 rowHeight = cellHeight;
             }
@@ -64,17 +68,16 @@ function drawRect(doc, x, y, width, height, style) {
 }
 
 function drawTextInRect(doc, text, padding, rect, wordWrapEnabled, jsPdfTextOptions) {
-    const textArray = getTextLines(doc, text, padding, doc.getFont(), { wordWrapEnabled, columnWidth: rect.w });
-    const linesCount = textArray.length;
-
-    const emptyPadding = { top: 0, right: 0, bottom: 0, left: 0 }; // for calculating height of one line only
-    const heightOfOneLine = calculateTextHeight(doc, textArray[0], emptyPadding, doc.getFont(), { wordWrapEnabled: false });
     const textRect = {
         x: rect.x + padding.left,
         y: rect.y + padding.top,
         w: rect.w - (padding.left + padding.right),
         h: rect.h - (padding.top + padding.bottom)
     };
+    const textArray = getTextLines(doc, text, doc.getFont(), { wordWrapEnabled, textWidth: textRect.w });
+    const linesCount = textArray.length;
+
+    const heightOfOneLine = calculateTextHeight(doc, textArray[0], doc.getFont(), { wordWrapEnabled: false });
 
     // TODO: check lineHeightFactor - https://github.com/MrRio/jsPDF/issues/3234
     const y = textRect.y + (textRect.h / 2)
@@ -85,4 +88,4 @@ function drawTextInRect(doc, text, padding, rect, wordWrapEnabled, jsPdfTextOpti
     doc.text(textArray.join('\n'), round(textRect.x), round(y), textOptions);
 }
 
-export { calculateRowHeight, calculateTextHeight, drawLine, drawRect, drawTextInRect };
+export { calculateRowHeight, calculateTextHeight, calculateTextWidth, drawLine, drawRect, drawTextInRect };
