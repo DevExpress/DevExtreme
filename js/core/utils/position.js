43e3f3b61e1bb56ed4c067da56630377d9ef8a78
jacksonrr3 <jacksonrr3@yandex.ru>
2021-09-21 12:22:26 +0300

    get rid of exeption handler

--- js/core/utils/position.js
+++ js/core/utils/position.js
@@ -32,7 +32,17 @@ const getBoundingRect = (element) => {
             height: element.outerHeight
         };
     }
-    return element.getBoundingClientRect();
+
+    return element.getBoundingClientRect
+        ? element.getBoundingClientRect()
+        : {
+            width: 0,
+            height: 0,
+            bottom: 0,
+            top: 0,
+            left: 0,
+            right: 0
+        };
 };
 
 export {
