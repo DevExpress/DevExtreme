6048510c2425d61c17315b0df9fea323dfff90f3
Alexey Serov <alexey.serov@devexpress.com>
2021-09-21 10:26:53 +0300

    asign generic parameters

--- js/data/data_source_aliases.d.ts
+++ js/data/data_source_aliases.d.ts
@@ -5,10 +5,10 @@ import Store from './abstract_store';
  * @docid
  * @type string|Store|DataSourceOptions|Array<any>
  * */
-export type DataSourceFactory<T> = string | Array<T> | Store | DataSourceOptions;
+export type DataSourceFactory<T> = string | Array<T> | Store<any, T> | DataSourceOptions<any, T>;
 
 /**
  * @docid
  * @type Store|DataSource|DataSourceOptions|string|Array<any>
  * */
-export type DataSourceDefinition<T> = DataSourceFactory<T> | DataSource;
+export type DataSourceDefinition<T> = DataSourceFactory<T> | DataSource<any, T>;
