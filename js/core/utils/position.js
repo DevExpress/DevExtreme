75fd08235555aca533642f06637423ce0108bae9
jacksonrr3 <jacksonrr3@yandex.ru>
2021-09-22 15:35:06 +0300

    delete element === undefineg handler

--- js/core/utils/position.js
+++ js/core/utils/position.js
@@ -15,18 +15,7 @@ const getBoundingRect = (element) => {
         };
     }
 
-    if(element && element.getBoundingClientRect) {
-        return element.getBoundingClientRect();
-    }
-
-    return {
-        width: 0,
-        height: 0,
-        bottom: 0,
-        top: 0,
-        left: 0,
-        right: 0
-    };
+    return element.getBoundingClientRect();
 };
 
 export {
