f6680400b2b4e5c0612084d3f0da2faea998ee08
Alexander Bezborodov (DevExpress) <bezborodov.alexander@corp.devexpress.com>
2021-09-24 13:20:32 +0300

    DataSource - add type inference, Component - define constructor args

--- js/data/data_source.d.ts
+++ js/data/data_source.d.ts
@@ -2,11 +2,8 @@ import {
  FilterDescriptor, GroupDescriptor, SelectDescriptor, SortDescriptor, LoadOptions, SearchOperation,
 } from './index';
 import { DxPromise } from '../core/utils/deferred';
-import Store from './abstract_store';
+import Store, { StoreOptions } from './abstract_store';
 import { CustomStoreOptions } from './custom_store';
-import { ArrayStoreOptions } from './array_store';
-import { LocalStoreOptions } from './local_store';
-import { ODataStoreOptions } from './odata/store';
 
 /** @namespace DevExpress.data */
 export interface DataSourceOptions<TKey = any, TSourceValue = any, TValue = TSourceValue, TMappedValue = TValue> {
@@ -50,12 +47,14 @@ export interface DataSourceOptions<TKey = any, TSourceValue = any, TValue = TSou
     /**
      * @docid
      * @type_function_param1 error:Object
+     * @type_function_param1_field1 message:string
      * @action
      * @public
      */
     onLoadError?: ((error: { readonly message?: string }) => void);
     /**
      * @docid
+     * @type_function_param1 isLoading:boolean
      * @action
      * @public
      */
@@ -132,14 +131,16 @@ export interface DataSourceOptions<TKey = any, TSourceValue = any, TValue = TSou
      * @type Store|StoreOptions|Array<any>
      */
     store?: Array<TSourceValue> |
-        Store<TKey, TSourceValue> |
-        ArrayStoreOptions<TKey, TSourceValue> & { type: 'array' } |
-        LocalStoreOptions<TKey, TSourceValue> & { type: 'local' } |
-        ODataStoreOptions<TKey, TSourceValue> & { type: 'odata' } |
-        CustomStoreOptions<TKey, TSourceValue>;
+        Store<TSourceValue, TKeyExpr, TKey> |
+        ArrayStoreOptions<TSourceValue, TKeyExpr, TKey> & { type: 'array' } |
+        LocalStoreOptions<TSourceValue, TKeyExpr, TKey> & { type: 'local' } |
+        ODataStoreOptions<TSourceValue, TKeyExpr, TKey> & { type: 'odata' } |
+        CustomStoreOptions<TSourceValue, TKeyExpr, TKey>;
 }
 /**
  * @docid
+ * @module data/data_source
+ * @export default
  * @public
  */
 export default class DataSource<TKey = any, TValue = any> {
@@ -150,6 +151,7 @@ export default class DataSource<TKey = any, TValue = any> {
     /**
      * @docid
      * @publicName cancel(operationId)
+     * @return boolean
      * @public
      */
     cancel(operationId: number): boolean;
@@ -190,24 +192,28 @@ export default class DataSource<TKey = any, TValue = any> {
     /**
      * @docid
      * @publicName isLastPage()
+     * @return boolean
      * @public
      */
     isLastPage(): boolean;
     /**
      * @docid
      * @publicName isLoaded()
+     * @return boolean
      * @public
      */
     isLoaded(): boolean;
     /**
      * @docid
      * @publicName isLoading()
+     * @return boolean
      * @public
      */
     isLoading(): boolean;
     /**
      * @docid
      * @publicName items()
+     * @return Array<any>
      * @public
      */
     items(): Array<any>;
@@ -318,12 +324,14 @@ export default class DataSource<TKey = any, TValue = any> {
     /**
      * @docid
      * @publicName requireTotalCount()
+     * @return boolean
      * @public
      */
     requireTotalCount(): boolean;
     /**
      * @docid
      * @publicName requireTotalCount(value)
+     * @param1 value:boolean
      * @public
      */
     requireTotalCount(value: boolean): void;
@@ -344,24 +352,28 @@ export default class DataSource<TKey = any, TValue = any> {
     /**
      * @docid
      * @publicName searchOperation()
+     * @return string
      * @public
      */
     searchOperation(): string;
     /**
      * @docid
      * @publicName searchOperation(op)
+     * @param1 op:string
      * @public
      */
     searchOperation(op: string): void;
     /**
      * @docid
      * @publicName searchValue()
+     * @return any
      * @public
      */
     searchValue(): any;
     /**
      * @docid
      * @publicName searchValue(value)
+     * @param1 value:any
      * @public
      */
     searchValue(value: any): void;
@@ -399,7 +411,7 @@ export default class DataSource<TKey = any, TValue = any> {
      * @return object
      * @public
      */
-    store(): Store<TKey, TValue>;
+    store(): Store<TValue, TKeyExpr, TKey> | StoreOptions<TValue, TKeyExpr, TKey> | Array<TValue>;
     /**
      * @docid
      * @publicName totalCount()
