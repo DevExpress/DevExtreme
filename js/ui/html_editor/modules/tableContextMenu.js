95e98c2c17e34a8181fd2512ca624c4791ed0f82
kotov.alexander <alexander.kotov@devexpress.com>
2021-09-24 18:24:33 +0300

    HtmlEditor - support editing of tables and cells properties (#19351)

--- js/ui/html_editor/modules/tableContextMenu.js
+++ js/ui/html_editor/modules/tableContextMenu.js
@@ -4,8 +4,9 @@ import BaseModule from './base';
 import eventsEngine from '../../../events/core/events_engine';
 import { addNamespace } from '../../../events/utils/index';
 import ContextMenu from '../../context_menu';
+import { showCellPropertiesForm, showTablePropertiesForm } from '../ui/tableForms';
+import localizationMessage from '../../../localization/message';
 import { getTableOperationHandler } from '../utils/table_helper';
-
 
 const MODULE_NAMESPACE = 'dxHtmlTableContextMenu';
 
@@ -48,27 +49,43 @@ if(Quill) {
             return this.editorInstance._createComponent($container, ContextMenu, menuConfig);
         }
 
+        showTableProperties(e) {
+            const $table = $(this._targetElement).closest('table');
+            this._contextMenu.hide();
+            this._popupForm = showTablePropertiesForm(this.editorInstance, $table);
+            this._targetElement = null;
+        }
+
+        showCellProperties(e) {
+            const $cell = $(this._targetElement).closest('th, td');
+            this._contextMenu.hide();
+            this._popupForm = showCellPropertiesForm(this.editorInstance, $cell);
+            this._targetElement = null;
+        }
+
         _getMenuConfig(options) {
             return {
                 target: this._quillContainer,
                 showEvent: null,
                 dataSource: [
                     { text: 'Insert', items: [
-                        { text: 'Insert Header Row', icon: 'header', onClick: getTableOperationHandler(this.quill, 'insertHeaderRow') },
-                        { text: 'Insert Row Above', icon: 'insertrowabove', onClick: getTableOperationHandler(this.quill, 'insertRowAbove') },
-                        { text: 'Insert Row Below', icon: 'insertrowbelow', onClick: getTableOperationHandler(this.quill, 'insertRowBelow') },
-                        { text: 'Insert Column Left', icon: 'insertcolumnleft', beginGroup: true, onClick: getTableOperationHandler(this.quill, 'insertColumnLeft') },
-                        { text: 'Insert Column Right', icon: 'insertcolumnright', onClick: getTableOperationHandler(this.quill, 'insertColumnRight') },
+                        { text: localizationMessage.format('dxHtmlEditor-insertHeaderRow'), icon: 'header', onClick: getTableOperationHandler(this.quill, 'insertHeaderRow') },
+                        { text: localizationMessage.format('dxHtmlEditor-insertRowAbove'), icon: 'insertrowabove', onClick: getTableOperationHandler(this.quill, 'insertRowAbove') },
+                        { text: localizationMessage.format('dxHtmlEditor-insertRowBelow'), icon: 'insertrowbelow', onClick: getTableOperationHandler(this.quill, 'insertRowBelow') },
+                        { text: localizationMessage.format('dxHtmlEditor-insertColumnLeft'), icon: 'insertcolumnleft', beginGroup: true, onClick: getTableOperationHandler(this.quill, 'insertColumnLeft') },
+                        { text: localizationMessage.format('dxHtmlEditor-insertColumnRight'), icon: 'insertcolumnright', onClick: getTableOperationHandler(this.quill, 'insertColumnRight') },
                     ] },
                     {
                         text: 'Delete',
                         items: [
-                            { text: 'Delete Column', icon: 'deletecolumn', onClick: getTableOperationHandler(this.quill, 'deleteColumn') },
-                            { text: 'Delete Row', icon: 'deleterow', onClick: getTableOperationHandler(this.quill, 'deleteRow') },
-                            { text: 'Delete Table', icon: 'deletetable', onClick: getTableOperationHandler(this.quill, 'deleteTable') }
+                            { text: localizationMessage.format('dxHtmlEditor-deleteColumn'), icon: 'deletecolumn', onClick: getTableOperationHandler(this.quill, 'deleteColumn') },
+                            { text: localizationMessage.format('dxHtmlEditor-deleteRow'), icon: 'deleterow', onClick: getTableOperationHandler(this.quill, 'deleteRow') },
+                            { text: localizationMessage.format('dxHtmlEditor-deleteTable'), icon: 'deletetable', onClick: getTableOperationHandler(this.quill, 'deleteTable') }
                         ]
-                    }
-                ],
+                    },
+                    { text: localizationMessage.format('dxHtmlEditor-cellProperties'), icon: 'cellproperties', onClick: (e) => { this.showCellProperties(e); } },
+                    { text: localizationMessage.format('dxHtmlEditor-tableProperties'), icon: 'tableproperties', onClick: (e) => { this.showTableProperties(e); } }
+                ]
             };
         }
 
@@ -87,6 +104,7 @@ if(Quill) {
         _prepareContextMenuHandler() {
             return (event) => {
                 if(this._isTableTarget(event.target)) {
+                    this._targetElement = event.target;
                     this._setContextMenuPosition(event);
                     this._contextMenu.show();
                     event.preventDefault();
@@ -101,7 +119,7 @@ if(Quill) {
                     my: 'left top',
                     at: 'left top',
                     collision: 'fit flip',
-                    offset: { x: event.clientX - startPosition.x, y: event.clientY - startPosition.y }
+                    offset: { x: event.clientX - startPosition.left, y: event.clientY - startPosition.top }
                 }
             });
         }
@@ -118,7 +136,10 @@ if(Quill) {
         }
 
         prepareCleanCallback() {
-            return () => { this._detachEvents(); };
+            return () => {
+                this._detachEvents();
+                this._popupForm?.dispose();
+            };
         }
     };
 }
