6436e459d88f897ab9de1f8cda0ffaa633cea5d7
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2021-09-27 18:40:05 +0300

    Scheduler - Get rid of using instance in appointment rendering strategy (#19374)
    * RowCount
    
    * appointmentCountPerCell, appointmentOffset
    
    * allowResizing, allowAllDayResizing
    
    * cellDurationInMinute
    
    * getCellHeight, groupCount, appointmentTakesAllDay
    
    * remove getRenderingStrategy
    
    * Rename getWorkspaceGroupWidth -> getGroupWidth
    
    * Move getCellDuration to the renovated code
    
    * Correct typo

--- js/ui/scheduler/appointments/rendering_strategies/strategy_horizontal.js
+++ js/ui/scheduler/appointments/rendering_strategies/strategy_horizontal.js
@@ -23,7 +23,7 @@ class HorizontalRenderingStrategy extends BaseAppointmentsStrategy {
 
         duration = this._adjustDurationByDaylightDiff(duration, startDate, normalizedEndDate);
 
-        const cellDuration = this.instance.getAppointmentDurationInMinutes() * toMs('minute');
+        const cellDuration = this.cellDurationInMinutes * toMs('minute');
         const durationInCells = duration / cellDuration;
         const width = this.cropAppointmentWidth(durationInCells * cellWidth, cellWidth);
 
@@ -91,7 +91,7 @@ class HorizontalRenderingStrategy extends BaseAppointmentsStrategy {
         let deltaTime = 0;
         const deltaWidth = args.width - initialSize.width;
 
-        deltaTime = toMs('minute') * Math.round(deltaWidth / this.cellWidth * this.instance.getAppointmentDurationInMinutes());
+        deltaTime = toMs('minute') * Math.round(deltaWidth / this.cellWidth * this.cellDurationInMinutes);
 
         return deltaTime;
     }
