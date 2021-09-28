0dc1bb20ad7febe54151f8ff2684dbb8bc36d4bf
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2021-09-28 17:43:11 +0300

    Fix formatting

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -1784,9 +1784,9 @@ declare module DevExpress.data {
     /**
      * [descr:DataSource.on(events)]
      */
-    on(events: {
-      [key in DevExpress.data.DataSource.EventName]?: Function;
-    }): this;
+    on(
+      events: { [key in DevExpress.data.DataSource.EventName]?: Function }
+    ): this;
     /**
      * [descr:DataSource.pageIndex()]
      */
@@ -2463,9 +2463,11 @@ declare module DevExpress.data {
     /**
      * [descr:PivotGridDataSource.on(events)]
      */
-    on(events: {
-      [key in DevExpress.data.PivotGridDataSource.EventName]?: Function;
-    }): this;
+    on(
+      events: {
+        [key in DevExpress.data.PivotGridDataSource.EventName]?: Function;
+      }
+    ): this;
     /**
      * [descr:PivotGridDataSource.reload()]
      */
