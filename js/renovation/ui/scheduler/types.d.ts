a869bca1bbbb519c085da6e7a3668b815c85d6b2
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2021-09-28 18:12:29 +0300

    Correct DataAccessorType

--- js/renovation/ui/scheduler/types.d.ts
+++ js/renovation/ui/scheduler/types.d.ts
@@ -2,7 +2,7 @@ export type GroupOrientation = 'vertical' | 'horizontal';
 export type ViewType = 'agenda' | 'day' | 'month' | 'timelineDay' | 'timelineMonth' | 'timelineWeek' | 'timelineWorkWeek' | 'week' | 'workWeek';
 
 export interface DataAccessorType {
-  getter: Record<string, (data: unknown) => Date>;
+  getter: Record<string, (data: unknown) => Date | string>;
   setter: unknown;
   expr: unknown;
 }
