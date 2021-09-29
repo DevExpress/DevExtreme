2852645d8aea83d8e61c23b6c05f79046e153a82
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2021-09-29 12:50:00 +0300

    fix jest coverage

--- jest.config.js
+++ jest.config.js
@@ -21,9 +21,7 @@ module.exports = {
     coveragePathIgnorePatterns: [
         './js/renovation/ui/grids/data_grid/datagrid_component.ts',
         './js/renovation/ui/scheduler/workspaces/utils.ts',
-        './js/renovation/ui/scroll_view/utils/get_element_offset.ts',
-        './js/renovation/ui/scroll_view/utils/get_element_style.ts',
-        './js/renovation/ui/scroll_view/utils/get_translate_values.ts',
+        './js/renovation/ui/scroll_view/',
         './js/renovation/utils/get_computed_style.ts',
         './js/renovation/utils/noop.ts',
         './js/renovation/utils/render_template.ts',
