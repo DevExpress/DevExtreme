fa39952035a216b90202ca94eb703663e9fe10e9
VictorSavushkin <36996384+VictorSavushkin@users.noreply.github.com>
2021-09-29 15:53:43 +0300

    Diagram - add useNativeScrolling (T1026930) (#19435)
    * Diagram - Fullscreen mode doesn't work if a diagram is placed inside a tabpanel widget or in popup (T1027296)
    
    * Diagram Version up for (T1029074 dup, T1029773)
    
    * Diagram Version Up for bugs T1026141, T1024093
    
    * Diagram - HistoryToolbar is not adjusted on full screen toggling
    
    * Diagram - add useNativeScrolling (T1026930)

--- js/ui/diagram.d.ts
+++ js/ui/diagram.d.ts
@@ -878,6 +878,13 @@ export interface dxDiagramOptions extends WidgetOptions<dxDiagram> {
     /**
      * @docid
      * @default true
+     * @default false &for(desktop except Mac)
+     * @public
+     */
+     useNativeScrolling?: boolean;
+    /**
+     * @docid
+     * @default true
      * @public
      */
     snapToGrid?: boolean;
