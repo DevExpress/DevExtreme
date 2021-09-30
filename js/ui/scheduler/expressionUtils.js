6b4ce5b333e824e1e3cdb6ebe8d3cf03c8f7323c
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2021-09-30 16:16:00 +0300

    Correct validation in getter / setter

--- js/ui/scheduler/expressionUtils.js
+++ js/ui/scheduler/expressionUtils.js
@@ -5,14 +5,16 @@ export const ExpressionUtils = {
     getField: (key, field, obj) => {
         const dataAccessors = getAppointmentDataProvider(key).getDataAccessors();
 
-        if(isDefined(dataAccessors.getter[field])) {
-            return dataAccessors.getter[field](obj);
+        if(!isDefined(dataAccessors.getter[field])) {
+            return;
         }
+
+        return dataAccessors.getter[field](obj);
     },
     setField: (key, field, obj, value) => {
         const { dataAccessors } = getAppointmentDataProvider(key);
 
-        if(isDefined(dataAccessors.setter[field])) {
+        if(!isDefined(dataAccessors.setter[field])) {
             return;
         }
 
