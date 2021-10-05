0d70acd28c1f0e4af7ae8b01b4266c60ca6cea29
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2021-10-05 15:00:54 +0300

    revert some changes

--- js/renovation/ui/scroll_view/utils/get_relative_offset.ts
+++ js/renovation/ui/scroll_view/utils/get_relative_offset.ts
@@ -5,9 +5,15 @@ export function getRelativeOffset(
 
   let currentElement = sourceElement;
 
-  while (currentElement?.offsetParent && !currentElement.classList.contains(targetElementClass)) {
-    offset.left += currentElement.offsetLeft;
-    offset.top += currentElement.offsetTop;
+  while (currentElement && !currentElement.classList.contains(targetElementClass)) {
+    const parentOffsetElement = currentElement.offsetParent as HTMLElement;
+
+    const currentElementRect = currentElement.getBoundingClientRect();
+    const parentOffsetElementRect = parentOffsetElement.getBoundingClientRect();
+
+    offset.left += currentElementRect.left - parentOffsetElementRect.left;
+    offset.top += currentElementRect.top - parentOffsetElementRect.top;
+
     currentElement = currentElement.offsetParent as HTMLElement;
   }
 
