37e530fad7bd1afd7695be4d692eb617ebcaf046
ksercs <ksercs0@gmail.com>
2021-10-05 14:06:47 +0300

    Move popup and its position controller to a new directory

--- js/ui/overlay/overlay_position_controller.js
+++ js/ui/overlay/overlay_position_controller.js
@@ -302,67 +302,6 @@ class OverlayPositionController {
     }
 }
 
-class PopupPositionController extends OverlayPositionController {
-    constructor({
-        fullScreen, forceApplyBindings,
-        ...args
-    }) {
-        super(args);
-
-        this._props = {
-            ...this._props,
-            fullScreen,
-            forceApplyBindings
-        };
-
-        this._lastPositionBeforeFullScreen = undefined;
-    }
-
-    set fullScreen(fullScreen) {
-        this._props.fullScreen = fullScreen;
-
-        if(fullScreen) {
-            this._fullScreenEnabled();
-        } else {
-            this._fullScreenDisabled();
-        }
-    }
-
-    positionContent() {
-        if(this._props.fullScreen) {
-            move(this._$content, { top: 0, left: 0 });
-            this.detectVisualPositionChange();
-        } else {
-            this._props.forceApplyBindings?.();
-
-            if(!this._shouldRenderContentInitialPosition && this._lastPositionBeforeFullScreen) {
-                move(this._$content, this._lastPositionBeforeFullScreen);
-                this._lastPositionBeforeFullScreen = undefined;
-                this.detectVisualPositionChange();
-            } else {
-                super.positionContent();
-            }
-        }
-    }
-
-    _getWrapperCoveredElement() {
-        if(this._props.fullScreen) {
-            return $(window);
-        }
-
-        return super._getWrapperCoveredElement();
-    }
-
-    _fullScreenEnabled() {
-        this.restorePositionOnNextRender(false);
-        this._lastPositionBeforeFullScreen = this._visualPosition;
-    }
-
-    _fullScreenDisabled() {
-        this.restorePositionOnNextRender(false);
-    }
-}
-
 class PopoverPositionController extends OverlayPositionController {
     constructor({ shading, $arrow, ...args }) {
         super(args);
@@ -466,7 +405,6 @@ class PopoverPositionController extends OverlayPositionController {
 export {
     OVERLAY_POSITION_ALIASES,
     OverlayPositionController,
-    PopupPositionController,
     POPOVER_POSITION_ALIASES,
     PopoverPositionController
 };
