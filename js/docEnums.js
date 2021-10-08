ded73e4b0fd24f8c5d89e2f4a1b2f521ce8a684e
alexander-kotov-dx <alexander.kotov@devexpress.com>
2021-10-08 15:35:16 +0300

    fix enums

--- js/docEnums.js
+++ js/docEnums.js
@@ -1021,7 +1021,7 @@
 
 /**
  * @typedef {string} Enums.HtmlEditorContextMenuItem
- * @enum {'background'|'bold'|'color'|'font'|'italic'|'link'|'image'|'strike'|'subscript'|'superscript'|'underline'|'blockquote'|'increaseIndent'|'decreaseIndent'|'orderedList'|'bulletList'|'alignLeft'|'alignCenter'|'alignRight'|'alignJustify'|'codeBlock'|'variable'|'undo'|'redo'|'clear'|'insertTable'|'insertHeaderRow'|'insertRowAbove'|'insertRowBelow'|'insertColumnLeft'|'insertColumnRight'|'deleteColumn'|'deleteRow'|'deleteTable'|'cellProperties|'tableProperties'}
+ * @enum {'background'|'bold'|'color'|'font'|'italic'|'link'|'image'|'strike'|'subscript'|'superscript'|'underline'|'blockquote'|'increaseIndent'|'decreaseIndent'|'orderedList'|'bulletList'|'alignLeft'|'alignCenter'|'alignRight'|'alignJustify'|'codeBlock'|'variable'|'undo'|'redo'|'clear'|'insertTable'|'insertHeaderRow'|'insertRowAbove'|'insertRowBelow'|'insertColumnLeft'|'insertColumnRight'|'deleteColumn'|'deleteRow'|'deleteTable'|'cellProperties'|'tableProperties'}
  */
 
 /**
