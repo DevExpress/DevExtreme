c84d7ffc75f589594a81c77c214bcca03cb26c85
ksercs <ksercs0@gmail.com>
2021-10-11 18:37:44 +0300

    SelectBox: fix search after "reset" method call (T1021888)

--- js/ui/drop_down_editor/ui.drop_down_list.js
+++ js/ui/drop_down_editor/ui.drop_down_list.js
@@ -440,7 +440,6 @@ const DropDownList = DropDownEditor.inherit({
 
         this._clearFilter();
         this._clearSelectedItem();
-        this._preventFiltering = true;
     },
 
     _listItemElements: function() {
