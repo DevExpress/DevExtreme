a7b96636d596a88585ec3f02ed6ed035e44cc5ed
ksercs <ksercs0@gmail.com>
2021-10-12 18:01:02 +0300

    SelectBox: search should be cancelled after item click even if it is in progress (T1027535)

--- js/ui/select_box.js
+++ js/ui/select_box.js
@@ -574,6 +574,7 @@ const SelectBox = DropDownList.inherit({
 
         if(shouldCancelSearch) {
             const isPopupVisible = this._popup?._hideAnimationProcessing;
+            this._clearSearchTimer();
             if(isPopupVisible) {
                 this._shouldCancelSearch(true);
             } else {
