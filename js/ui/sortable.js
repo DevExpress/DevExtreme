a16480592089fe9044a52c98ff63c02a9fd242c3
volnyagin <vconst@users.noreply.github.com>
2021-10-13 09:52:16 +0300

    Sortable: Fix dragging to empty sortable if html has overflow auto style (T1034893)

--- js/ui/sortable.js
+++ js/ui/sortable.js
@@ -262,7 +262,7 @@ const Sortable = Draggable.inherit({
             const itemPoints = this.option('itemPoints');
             const itemPoint = itemPoints?.filter(item => item.index === toIndex)[0];
 
-            if(itemPoint) {
+            if(itemPoint && itemPoint.top !== undefined) {
                 const isVertical = this._isVerticalOrientation();
                 if(isVertical) {
                     return top <= itemPoint.top && itemPoint.top <= bottom;
