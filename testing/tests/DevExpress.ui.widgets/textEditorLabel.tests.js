e8ce4c864d4f5f4afd2efb742ce059479fcdb2b9
ksercs <ksercs0@gmail.com>
2021-10-14 17:04:00 +0300

    label tests pure refactoring

--- testing/tests/DevExpress.ui.widgets/textEditorLabel.tests.js
+++ testing/tests/DevExpress.ui.widgets/textEditorLabel.tests.js
@@ -3,97 +3,108 @@ import { TextEditorLabel } from 'ui/text_box/ui.text_editor.label';
 import { getWidth } from 'core/utils/size';
 
 const TEXTEDITOR_LABEL_CLASS = 'dx-texteditor-label';
+
 const LABEL_BEFORE_SELECTOR = '.dx-label-before';
 const LABEL_SELECTOR = '.dx-label';
 const LABEL_AFTER_SELECTOR = '.dx-label-after';
 
 QUnit.testStart(function() {
-    const markup =
-        '<div id="textEditor"></div>';
+    const markup = '<div id="textEditor"></div>';
 
     $('#qunit-fixture').html(markup);
 });
 
-QUnit.module('texteditor_label', {
+QUnit.module('textEditorLabel', {
     beforeEach: function() {
-        this.editor = $('#textEditor');
-        this.label = new TextEditorLabel({
-            $editor: this.editor,
+        this.$editor = $('#textEditor');
+        this.labelInitialConfig = {
+            $editor: this.$editor,
             text: 'Label',
             mark: '*',
+            mode: 'static',
             beforeWidth: 7,
             containerWidth: 180
-        });
+        };
+        this.init = (options) => {
+            this.label = new TextEditorLabel($.extend({}, this.labelInitialConfig, options));
+            this.$label = this.label.$element();
+            this.getBeforeElement = () => this.$label.find(LABEL_BEFORE_SELECTOR);
+            this.getLabelElement = () => this.$label.find(LABEL_SELECTOR);
+            this.getSpan = () => this.getLabelElement().find('span');
+        };
+        this.reinit = (options) => {
+            this.label = null;
+            this.$label.remove();
+
+            this.init(options);
+        };
+
+        this.init({});
     }
 }, () => {
-    QUnit.module('label murkup', () => {
-        QUnit.test('check label root element', function(assert) {
-            assert.equal(this.label.$element().length, 1, 'root element is created');
-            assert.ok(this.label.$element().hasClass(TEXTEDITOR_LABEL_CLASS), 'right root element class');
+    QUnit.module('render', () => {
+        QUnit.test('base markup', function(assert) {
+            assert.ok(this.getBeforeElement().length, 'label has before element');
+            assert.ok(this.getLabelElement().length, 'label has internal element');
+            assert.ok(this.$label.find(LABEL_AFTER_SELECTOR).length, 'label has after element');
+
+            assert.ok(this.getSpan().length, 'internal label has span');
         });
 
-        QUnit.test('check label before element', function(assert) {
-            assert.equal(this.label.$element().find(LABEL_BEFORE_SELECTOR).length, 1, 'label has before element');
+        QUnit.test('root element should have dx-texteditor-label class', function(assert) {
+            assert.ok(this.$label.hasClass(TEXTEDITOR_LABEL_CLASS));
         });
 
-        QUnit.test('check label after element', function(assert) {
-            assert.equal(this.label.$element().find(LABEL_AFTER_SELECTOR).length, 1, 'label has after element');
+        QUnit.test('span should have text equal to text prop', function(assert) {
+            assert.strictEqual(this.getSpan().text(), this.labelInitialConfig.text, 'text is correct');
         });
 
-
-        QUnit.test('check label element', function(assert) {
-            assert.equal(this.label.$element().find(LABEL_SELECTOR).length, 1, 'label has internal element');
+        QUnit.test('span should have data-mark requal to mark prop', function(assert) {
+            assert.strictEqual(this.getSpan().attr('data-mark'), this.labelInitialConfig.mark, 'mark is correct');
         });
 
+        QUnit.test('label before element should have width equal to beforeWidth prop', function(assert) {
+            const beforeWidth = getWidth(this.getBeforeElement());
 
-        QUnit.test('check label text', function(assert) {
-            assert.equal(this.label.$element().text(), 'Label', 'right label text');
+            assert.strictEqual(beforeWidth, this.labelInitialConfig.beforeWidth, 'before element width is correct');
         });
 
+        QUnit.test('label internal element should have width equal to containerWidth prop', function(assert) {
+            const labelWidth = getWidth(this.getLabelElement());
 
-        QUnit.test('check label mark', function(assert) {
-            assert.equal(this.label.$element().find('span').attr('data-mark'), '*', 'right label mark');
+            assert.strictEqual(labelWidth, this.labelInitialConfig.containerWidth, 'label internal element width is correct');
         });
-
-        QUnit.test('check label before width', function(assert) {
-            assert.equal(getWidth(this.label.$element().find(LABEL_BEFORE_SELECTOR)), 7, 'right label before width');
-        });
-
-
-        QUnit.test('check label width', function(assert) {
-            assert.equal(getWidth(this.label.$element().find(LABEL_SELECTOR)), 180, 'right label width');
-        });
-
     });
+
     QUnit.module('public methods', () => {
-        QUnit.test('check label text after update', function(assert) {
+        QUnit.test('updateText', function(assert) {
+            const newText = 'LabelNew';
+            this.label.updateText(newText);
 
-            this.label.updateText('LabelNew');
-
-            assert.strictEqual(this.label.$element().text(), 'LabelNew', 'right label text');
+            assert.strictEqual(this.getSpan().text(), newText, 'text is updated');
         });
 
+        QUnit.test('updateMark', function(assert) {
+            const newMark = ':';
+            this.label.updateMark(newMark);
 
-        QUnit.test('check label mark after update', function(assert) {
-
-            this.label.updateMark(':');
-
-            assert.strictEqual(this.label.$element().find('span').attr('data-mark'), ':', 'right label mark');
+            assert.strictEqual(this.getSpan().attr('data-mark'), newMark, 'span mark is updated');
         });
 
+        QUnit.test('updateBeforeWidth', function(assert) {
+            const newBeforeWidth = 200;
+            this.label.updateBeforeWidth(newBeforeWidth);
 
-        QUnit.test('check label before element width after update', function(assert) {
-
-            this.label.updateBeforeWidth(200);
-
-            assert.strictEqual(getWidth(this.label.$element().find(LABEL_BEFORE_SELECTOR)), 200, 'right before width');
+            const labelBeforeWidth = getWidth(this.getBeforeElement());
+            assert.strictEqual(labelBeforeWidth, newBeforeWidth, 'before element width is updated');
         });
 
-        QUnit.test('check label element width after update', function(assert) {
+        QUnit.test('updateWidth', function(assert) {
+            const newContainerWidth = 300;
+            this.label.updateWidth(newContainerWidth);
 
-            this.label.updateWidth(300);
-
-            assert.strictEqual(getWidth(this.label.$element().find(LABEL_SELECTOR)), 300, 'right label width');
+            const labelWidth = getWidth(this.getLabelElement());
+            assert.strictEqual(labelWidth, newContainerWidth, 'label width is updated');
         });
     });
 });
