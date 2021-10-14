ecd3b488e018add7a0768d82d1fad52b4a6b364b
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2021-10-14 16:27:14 +0300

    Scheduler - Renovation - All day appointments render (#19679)
    * Scheduler - Renovation - Rrendering of the all day appointments
    
    * Set local dates in test

--- js/renovation/ui/scheduler/view_model/appointments/appointments.ts
+++ js/renovation/ui/scheduler/view_model/appointments/appointments.ts
@@ -1,23 +1,20 @@
 import type { Appointment } from '../../../../../ui/scheduler';
 import { AppointmentsModelType } from '../../model/types';
 import { AppointmentViewModelGenerator } from '../../../../../ui/scheduler/appointments/viewModelGenerator';
-import { AppointmentViewModel } from '../../appointment/types';
-import { AppointmentViewModelType } from './types';
+import { AppointmentsViewModelType } from '../../appointment/types';
 
 export const getAppointmentsViewModel = (
   model: AppointmentsModelType,
   filteredItems: Appointment[],
-): AppointmentViewModel[] => {
+): AppointmentsViewModelType => {
   const appointmentViewModel = new AppointmentViewModelGenerator();
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  const result = appointmentViewModel.generate(
+  return appointmentViewModel.generate(
     filteredItems,
     {
       ...model,
       isRenovatedAppointments: true,
     },
-  ) as AppointmentViewModelType;
-
-  return result.viewModel;
+  ) as AppointmentsViewModelType;
 };
