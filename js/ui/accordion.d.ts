14bda2619210b33409cd90488a84a931a4ec9579
Pavel Gruba <pavel.gruba@devexpress.com>
2021-10-14 12:50:37 +0300

    Fix options types dicscovering

--- js/ui/accordion.d.ts
+++ js/ui/accordion.d.ts
@@ -66,6 +66,7 @@ export type SelectionChangedEvent<TItem extends ItemLike = any, TKey = any> = Ev
 /**
  * @deprecated use Properties instead
  * @namespace DevExpress.ui
+ * @public
  */
 export interface dxAccordionOptions<
     TItem extends ItemLike = any,
