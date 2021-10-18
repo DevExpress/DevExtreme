6d6b2baa8d541c79294cf3be75c06674d2a2cf12
tsukanov_roman <roman.tsukanov@devexpress.com>
2021-10-18 09:31:19 +0300

    Fix the declaration

--- js/ui/data_grid.d.ts
+++ js/ui/data_grid.d.ts
@@ -1785,7 +1785,7 @@ export interface ScrollingBase {
     /**
      * @docid GridBaseOptions.scrolling.scrollByContent
      * @default true
-     * @default false [for](non-touch_devices)
+     * @default false &for(non-touch_devices)
      * @public
      */
     scrollByContent?: boolean;
