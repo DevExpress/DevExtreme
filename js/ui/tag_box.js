83cea65f146343edf9a4c08bd068f812cd4e0033
groshenkovamarina <marina.groshenkova@devexpress.com>
2021-10-19 13:59:57 +0300

    Editor's label - Extract in a separate component

--- js/ui/tag_box.js
+++ js/ui/tag_box.js
@@ -224,6 +224,10 @@ const TagBox = SelectBox.inherit({
         return this.callBase();
     },
 
+    _getLabelContainer: function() {
+        return this._$tagsContainer;
+    },
+
     _scrollContainer: function(direction) {
         if(this.option('multiline') || !hasWindow()) {
             return;
