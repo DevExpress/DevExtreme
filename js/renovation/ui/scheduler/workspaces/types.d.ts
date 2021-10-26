2a3965f71cddb8a78ffcb8005fbf1257a9bdcf91
Evgeniy Aryamnov <earyamnow@mail.ru>
2021-10-26 14:42:59 +0300

    dxScheduler - optimize grid viewModel generation

--- js/renovation/ui/scheduler/workspaces/types.d.ts
+++ js/renovation/ui/scheduler/workspaces/types.d.ts
@@ -1,4 +1,5 @@
-import { BaseTemplateProps, GroupOrientation } from '../types';
+import { BaseTemplateProps, GroupOrientation, ViewType } from '../types';
+import { GetDateForHeaderText } from '../view_model/to_test/views/types';
 
 export interface ViewCellData {
   startDate: Date;
@@ -50,6 +51,21 @@ interface GroupedViewDataBase {
 
 export interface GroupedViewData extends GroupedViewDataBase {
   groupedData: ViewData[];
+}
+
+interface CellPositionData {
+  rowIndex: number;
+  columnIndex: number;
+}
+
+interface CellInfo {
+  data: ViewCellData;
+  position: CellPositionData;
+}
+
+export interface ViewDataMap {
+  dateTableMap: CellInfo[][];
+  allDayPanelMap: CellInfo[];
 }
 
 export interface TimePanelData extends GroupedViewDataBase {
@@ -152,8 +168,36 @@ interface ViewOptions {
   groupOrientation: GroupOrientation;
 }
 
+export interface ViewDataProviderOptions {
+  startRowIndex: number;
+  startCellIndex: number;
+  groupOrientation: GroupOrientation;
+  groupByDate: boolean;
+  groups: Group[];
+  isProvideVirtualCellsWidth: boolean;
+  isAllDayPanelVisible: boolean;
+  selectedCells?: unknown;
+  focusedCell?: unknown;
+  headerCellTextFormat: string | ((date: Date) => string);
+  getDateForHeaderText: GetDateForHeaderText;
+  startDayHour: number;
+  endDayHour: number;
+  cellDuration: number;
+  viewType: ViewType;
+  intervalCount: number;
+  hoursInterval: number;
+  currentDate: Date;
+  startDate?: Date;
+  firstDayOfWeek: number;
+
+  isGenerateTimePanelData: boolean;
+  isGenerateWeekDaysHeaderData: boolean;
+}
+
 // TODO: tempporary
 export interface ViewDataProviderType {
+  completeViewDataMap: ViewCellData[][];
+  viewDataMap: ViewDataMap;
   timePanelData: TimePanelData;
   viewData: GroupedViewData;
   dateHeaderData: DateHeaderData;
@@ -171,6 +215,171 @@ export interface ViewDataProviderType {
   getIntervalDuration: (intervalCount: number) => number;
   getStartViewDate: () => Date;
   getViewOptions: () => ViewOptions;
+  setViewOptions: (options: ViewDataProviderOptions) => void;
+  createGroupedDataMapProvider: () => void;
+}
+
+interface CompleteViewDataGenerationOptions {
+  currentDate: Date;
+  startDate?: Date;
+  startDayHour: number;
+  endDayHour: number;
+  groupByDate: boolean;
+  groups: Group[];
+  intervalCount: number;
+  firstDayOfWeek: number;
+  hoursInterval: number;
+  cellDuration: number;
+  startViewDate: Date;
+  groupOrientation: GroupOrientation;
+  isVerticalGrouping: boolean;
+  isHorizontalGrouping: boolean;
+  isGroupedByDate: boolean;
+  isAllDayPanelVisible: boolean;
+  viewType: ViewType;
+  interval: number;
+}
+
+interface ViewDataMapGenerationOptions {
+  startRowIndex: number;
+  startCellIndex: number;
+  isVerticalGrouping: boolean;
+  isAllDayPanelVisible: boolean;
+}
+
+interface ViewDataGenerationOptions {
+  topVirtualRowHeight: number;
+  bottomVirtualRowHeight: number;
+  leftVirtualCellWidth: number;
+  rightVirtualCellWidth: number;
+  startRowIndex: number;
+  startCellIndex: number;
+  isProvideVirtualCellsWidth: boolean;
+  isVerticalGrouping: boolean;
+  isAllDayPanelVisible: boolean;
+  isGroupedAllDayPanel: boolean;
+}
+
+type GenerateViewDataMap = (
+  completeViewDateMap: ViewCellData[][],
+  options: ViewDataMapGenerationOptions,
+) => ViewDataMap;
+
+type GetViewDataFromMap = (
+  completeViewDateMap: ViewCellData[][],
+  viewDataMap: ViewDataMap,
+  options: ViewDataGenerationOptions,
+) => GroupedViewData;
+
+// TODO: use TS in ViewDataGenerators
+export interface ViewDataGeneratorType {
+  getCompleteViewDataMap: (options: CompleteViewDataGenerationOptions) => ViewCellData[][];
+  generateViewDataMap: GenerateViewDataMap;
+  getViewDataFromMap: GetViewDataFromMap;
+  getInterval: (hoursInterval: number) => number;
+}
+
+interface CompleteDateHeaderMapGenerationOptions {
+  isGenerateWeekDaysHeaderData: boolean;
+  isGroupedByDate: boolean;
+  groups: Group[];
+  groupOrientation: GroupOrientation;
+  startDayHour: number;
+  endDayHour: number;
+  hoursInterval: number;
+  isHorizontalGrouping: boolean;
+  intervalCount: number;
+  today?: Date;
+  headerCellTextFormat: string | ((date: Date) => string);
+  getDateForHeaderText: GetDateForHeaderText;
+  interval: number;
+  startViewDate: Date;
+  currentDate: Date;
+  viewType: ViewType;
+}
+
+interface DateHeaderDataGenerationOptions {
+  isGenerateWeekDaysHeaderData: boolean;
+  cellWidth: number;
+  isProvideVirtualCellsWidth: boolean;
+  startDayHour: number;
+  endDayHour: number;
+  hoursInterval: number;
+  startCellIndex: number;
+  cellCount?: number;
+  groups: Group[];
+  groupOrientation: GroupOrientation;
+  isGroupedByDate: boolean;
+}
+
+type GetCompleteDateHeaderMap = (
+  options: CompleteDateHeaderMapGenerationOptions,
+  completeViewData: ViewCellData[][],
+) => DateHeaderCellData[][];
+
+type GenerateDateHeaderData = (
+  completeDateHeaderMap: DateHeaderCellData[][],
+  completeViewDataMap: ViewCellData[][],
+  options: DateHeaderDataGenerationOptions,
+) => DateHeaderData;
+
+// TODO: use TS in DateHeaderDataGenerator
+export interface DateHeaderDataGeneratorType {
+  getCompleteDateHeaderMap: GetCompleteDateHeaderMap;
+  generateDateHeaderData: GenerateDateHeaderData;
+}
+
+interface CompleteTimePanelMapGenerationOptions {
+  startViewDate: Date;
+  cellDuration: number;
+  startDayHour: number;
+  endDayHour: number;
+  isVerticalGrouping: boolean;
+  intervalCount: number;
+  currentDate: Date;
+  viewType: ViewType;
+  hoursInterval: number;
+}
+
+interface TimePanelDataGenerationOptions {
+  startRowIndex: number;
+  rowCount?: number;
+  topVirtualRowHeight: number;
+  bottomVirtualRowHeight: number;
+  isGroupedAllDayPanel: boolean;
+  isVerticalGrouping: boolean;
+  isAllDayPanelVisible: boolean;
+}
+
+type GetCompleteTimePanelMap = (
+  options: CompleteTimePanelMapGenerationOptions,
+  completeViewData: ViewCellData[][],
+) => ViewCellData[];
+
+type GenerateTimePanelData = (
+  completeTimePanelMap: ViewCellData[],
+  options: TimePanelDataGenerationOptions,
+) => TimePanelData;
+
+// TODO: use TS in TimePanelDataGenerator
+export interface TimePanelDataGeneratorType {
+  getCompleteTimePanelMap: GetCompleteTimePanelMap;
+  generateTimePanelData: GenerateTimePanelData;
+}
+
+export interface WorkSpaceGenerationOptions {
+  intervalCount: number;
+  groups: Group[];
+  groupByDate: boolean;
+  groupOrientation: GroupOrientation;
+  startDayHour: number;
+  endDayHour: number;
+  currentDate: Date;
+  startDate?: Date;
+  firstDayOfWeek: number;
+  hoursInterval: number;
+  type: ViewType;
+  cellDuration: number;
 }
 
 export interface CellsMetaData {
