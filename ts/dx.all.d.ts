7c9b10221750e0e400527ff9c10a7f9289c0a19b
Igggr <igor.smirnov@devexpress.com>
2021-10-27 18:01:45 +0300

    Add 'auto' in DiagramDataLayoutType

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -4413,7 +4413,7 @@ declare module DevExpress.types {
   /**
    * @deprecated Attention! This type is for internal purposes only. If you used it previously, please describe your scenario in the following GitHub Issue, and we will suggest a public alternative: {@link https://github.com/DevExpress/DevExtreme/issues/17885|Internal Types}.
    */
-  export type DiagramDataLayoutType = 'off' | 'tree' | 'layered';
+  export type DiagramDataLayoutType = 'auto' | 'off' | 'tree' | 'layered';
   /**
    * @deprecated Attention! This type is for internal purposes only. If you used it previously, please describe your scenario in the following GitHub Issue, and we will suggest a public alternative: {@link https://github.com/DevExpress/DevExtreme/issues/17885|Internal Types}.
    */
