baae409e2cdddd800b5dc820df1eae46707a2e1b
Alexander Kozlovskiy <zedwag@gmail.com>
2021-10-28 14:07:54 +0300

    DropDownEditors: popup should have correct position when editor is in the bottom right (T1034931) (#19889)

--- js/ui/overlay/ui.overlay.js
+++ js/ui/overlay/ui.overlay.js
@@ -1213,8 +1213,11 @@ const Overlay = Widget.inherit({
 
             resetPosition(this._$content);
 
+            const wrapperOverflow = this._$wrapper.css('overflow');
+            this._$wrapper.css('overflow', 'hidden');
             const position = this._transformStringPosition(this._position, POSITION_ALIASES);
             const resultPosition = positionUtils.setup(this._$content, position);
+            this._$wrapper.css('overflow', wrapperOverflow);
 
             forceRepaint(this._$content);
 
