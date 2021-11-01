9402de201cf5edf89d650ffa0e0c18dcfa7fa424
ksercs <ksercs0@gmail.com>
2021-11-01 12:34:15 +0300

    update empty char

--- js/ui/text_box/ui.text_editor.mask.rule.js
+++ js/ui/text_box/ui.text_editor.mask.rule.js
@@ -4,7 +4,7 @@ import { inArray } from '../../core/utils/array';
 import { isFunction } from '../../core/utils/type';
 import { noop } from '../../core/utils/common';
 
-const EMPTY_CHAR = '_empty_';
+const EMPTY_CHAR = '(_empty_)';
 
 const BaseMaskRule = Class.inherit({
 
