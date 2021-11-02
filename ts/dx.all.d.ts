88227cc89992e079b0a9b57dda1cfd9261b4f7ac
ilyakhd <ilyakhd@users.noreply.github.com>
2021-11-02 10:39:34 +0300

    Make fx constant public

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -934,7 +934,6 @@ declare module DevExpress {
   }
   /**
    * [descr:fx]
-   * @deprecated Attention! This type is for internal purposes only. If you used it previously, please describe your scenario in the following GitHub Issue, and we will suggest a public alternative: {@link https://github.com/DevExpress/DevExtreme/issues/17885|Internal Types}.
    */
   export const fx: {
     /**
