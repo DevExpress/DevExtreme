8d76c45fd9a5fe78298806907ca4c8f7a7172223
Roman Semenov <roman.semenov@devexpress.com>
2021-11-02 17:23:51 +0300

    fix review

--- js/ui/sortable.js
+++ js/ui/sortable.js
@@ -764,7 +764,7 @@ const Sortable = Draggable.inherit({
         return left;
     },
 
-    _movePlaceholder: function(_isFromHandler) {
+    _movePlaceholder: function() {
         const that = this;
         const $placeholderElement = that._$placeholderElement || that._createPlaceholder();
         if(!$placeholderElement) {
