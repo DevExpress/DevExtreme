714902e50d4eecb21dd340fa161cabffabc3867f
polosatov.alexander <alexander.polosatov@devexpress.com>
2021-11-09 18:03:52 +0300

    Viz export. Fix PDF document size on scaled screen(T1041447)

--- testing/tests/DevExpress.exporter/imageCreator.tests.js
+++ testing/tests/DevExpress.exporter/imageCreator.tests.js
@@ -7,6 +7,9 @@ const testingMarkupEnd = '</svg>';
 import browser from 'core/utils/browser';
 import svgUtils from 'core/utils/svg';
 import { Deferred } from 'core/utils/deferred';
+import { getWindow } from 'core/utils/window';
+
+const window = getWindow();
 
 const pathNameByUrl = (url) => {
     const a = document.createElement('a');
@@ -20,6 +23,8 @@ const pathNameByUrl = (url) => {
 function setupCanvasStub(drawnElements, paths) {
     const prototype = window.CanvasRenderingContext2D.prototype;
     const canvasPrototype = window.HTMLCanvasElement.prototype;
+
+    sinon.spy(exporter.image.creator, '_createCanvas');
 
     // image
     sinon.stub(prototype, 'drawImage', function(img, x, y, width, height) {
@@ -304,6 +309,8 @@ function teardownCanvasStub() {
     const prototype = window.CanvasRenderingContext2D.prototype;
     const canvasPrototype = window.HTMLCanvasElement.prototype;
 
+    exporter.image.creator._createCanvas.restore();
+
     // image
     prototype.drawImage.restore();
     canvasPrototype.toDataURL.restore();
@@ -361,6 +368,45 @@ QUnit.module('Svg to image to canvas', {
     },
     afterEach: function() {
         teardownCanvasStub();
+    }
+});
+
+QUnit.test('Canvas size', function(assert) {
+    const done = assert.async();
+    const imageBlob = exporter.image.getData(testingMarkupStart + testingMarkupEnd, {
+        format: 'png', width: 500,
+        height: 250, margin: 10
+    });
+
+    $.when(imageBlob).done(function() {
+        const createCanvas = exporter.image.creator._createCanvas;
+
+        assert.strictEqual(createCanvas.callCount, 1);
+        assert.deepEqual(createCanvas.getCall(0).args, [500, 250, 10]);
+        done();
+    });
+});
+
+QUnit.test('Canvas size. Scaled screen', function(assert) {
+    const done = assert.async();
+    const srcPixelRatio = window.devicePixelRatio;
+    window.devicePixelRatio = 2;
+
+    try {
+        const imageBlob = exporter.image.getData(testingMarkupStart + testingMarkupEnd, {
+            format: 'png', width: 500,
+            height: 250, margin: 10
+        });
+
+        $.when(imageBlob).done(function() {
+            const createCanvas = exporter.image.creator._createCanvas;
+
+            assert.strictEqual(createCanvas.callCount, 1);
+            assert.deepEqual(createCanvas.getCall(0).args, [1000, 500, 10]);
+            done();
+        });
+    } finally {
+        window.devicePixelRatio = srcPixelRatio;
     }
 });
 
@@ -455,13 +501,14 @@ QUnit.test('Transformation of canvas context args (T892041, T1020859)', function
     }
 
     const done = assert.async();
+    const srcPixelRatio = window.devicePixelRatio;
+    window.devicePixelRatio = 2;
     const context = window.CanvasRenderingContext2D.prototype;
     const imageBlob = imageCreator.getData(testingMarkupStart + '<polygon points=\'220,10 300,210 170,250 123,234\' style=\'fill:lime;stroke:purple;stroke-width:1\'/>' + testingMarkupEnd,
         {
             width: 560,
             height: 290,
-            format: 'png',
-            pixelRatio: 2
+            format: 'png'
         });
 
     assert.expect(1);
@@ -470,6 +517,7 @@ QUnit.test('Transformation of canvas context args (T892041, T1020859)', function
             assert.deepEqual(context.setTransform.getCall(0).args, [2, 0, 0, 2, 0, 0], 'setTransform');
         } finally {
             done();
+            window.devicePixelRatio = srcPixelRatio;
         }
     });
 });
