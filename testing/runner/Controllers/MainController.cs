b809036695a16b04708cc2284a352d636646ff6b
Alexey Serov <alexey.serov@devexpress.com>
2021-11-09 19:56:06 +0300

    remove detailed log

--- testing/runner/Controllers/MainController.cs
+++ testing/runner/Controllers/MainController.cs
@@ -107,15 +107,13 @@ namespace Runner.Controllers
         [HttpPost]
         public void NotifyTestStarted(string name) {
             lock (IO_SYNC) {
-                // ConsoleHelper.Logger.WriteLine($"       [ run] {name}");
-                ConsoleHelper.WriteLine($"       [ run] {name}");
+                ConsoleHelper.Logger.WriteLine($"       [ run] {name}");
             }
         }
         [HttpPost]
         public void NotifyTestCompleted(string name, bool passed) {
             lock (IO_SYNC) {
-                // ConsoleHelper.Logger.WriteLine($"       [{(passed ? "  ok" : "fail")}] {name}");
-                ConsoleHelper.WriteLine($"       [{(passed ? "  ok" : "fail")}] {name}");
+                ConsoleHelper.Logger.WriteLine($"       [{(passed ? "  ok" : "fail")}] {name}");
             }
         }
         [HttpPost]
