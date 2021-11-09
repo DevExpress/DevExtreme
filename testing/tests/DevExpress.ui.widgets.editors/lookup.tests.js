debdff248d44c168dc54925ef5913f90ec109061
ksercs <ksercs0@gmail.com>
2021-11-09 13:00:15 +0300

    fix lookup aria tests

--- testing/tests/DevExpress.ui.widgets.editors/lookup.tests.js
+++ testing/tests/DevExpress.ui.widgets.editors/lookup.tests.js
@@ -3181,6 +3181,7 @@ if(devices.real().deviceType === 'desktop') {
     [true, false].forEach((searchEnabled) => {
         QUnit.module(`Aria accessibility, searchEnabled: ${searchEnabled}`, {
             beforeEach: function() {
+                this.isMac = devices.real().mac;
                 helper = new ariaAccessibilityTestHelper({
                     createWidget: ($element, options) => new Lookup($element,
                         $.extend({
@@ -3204,7 +3205,11 @@ if(devices.real().deviceType === 'desktop') {
                 helper.checkAttributes(helper.$widget, { 'aria-owns': helper.widget._popupContentId }, 'widget');
                 helper.checkAttributes(helper.widget._popup.$content(), { id: helper.widget._popupContentId }, 'popupContent');
                 if($input.length) {
-                    helper.checkAttributes($input, { autocomplete: 'off', type: 'text', spellcheck: 'false', tabindex: '0', role: 'textbox' }, 'input');
+                    const expectedAttributes = { autocomplete: 'off', type: 'text', spellcheck: 'false', tabindex: '0', role: 'textbox' };
+                    if(this.isMac) {
+                        expectedAttributes.placeholder = ' ';
+                    }
+                    helper.checkAttributes($input, expectedAttributes, 'input');
                 }
 
                 helper.widget.option('searchEnabled', !searchEnabled);
@@ -3213,7 +3218,11 @@ if(devices.real().deviceType === 'desktop') {
                 helper.checkAttributes(helper.$widget, { 'aria-owns': helper.widget._popupContentId }, 'widget');
                 helper.checkAttributes(helper.widget._popup.$content(), { id: helper.widget._popupContentId }, 'popupContent');
                 if($input.length) {
-                    helper.checkAttributes($input, { autocomplete: 'off', type: 'text', spellcheck: 'false', role: 'textbox' }, 'input');
+                    const expectedAttributes = { autocomplete: 'off', type: 'text', spellcheck: 'false', role: 'textbox' };
+                    if(this.isMac) {
+                        expectedAttributes.placeholder = ' ';
+                    }
+                    helper.checkAttributes($input, expectedAttributes, 'input');
                 }
             });
 
