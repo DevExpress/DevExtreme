000b48e68230eacd6db527017a434b20d89ed359
Alexey Serov <alexey.serov@devexpress.com>
2021-11-09 09:35:34 +0300

    "runner stalled" after 3 minutes

--- docker-ci.sh
+++ docker-ci.sh
@@ -173,7 +173,7 @@ function start_runner_watchdog {
     local last_suite_time=unknown
 
     while true; do
-        sleep 600
+        sleep 180
 
         if [ ! -f $last_suite_time_file ] || [ $(cat $last_suite_time_file) == $last_suite_time ]; then
             echo "Runner stalled"
