26b9a82c24505d38df9247c3edee4f3868e6e1e8
Konstantin Volnyagin <vconst@users.noreply.github.com>
2021-11-11 18:03:22 +0300

    DataGrid: Fix showing several revert buttons and validation messages if startEditAction is 'dblclick' and isHighlighted is assigned to true (T1041287) (#20016)

--- js/ui/grid_core/ui.grid_core.validating.js
+++ js/ui/grid_core/ui.grid_core.validating.js
@@ -1022,7 +1022,7 @@ export default {
                             return;
                         }
 
-                        let $tooltipElement = $container.find('.' + this.addWidgetPrefix(REVERT_TOOLTIP_CLASS));
+                        let $tooltipElement = this._rowsView.element().find('.' + this.addWidgetPrefix(REVERT_TOOLTIP_CLASS));
                         const $overlayContainer = $container.closest(`.${this.addWidgetPrefix(CONTENT_CLASS)}`);
 
                         $tooltipElement && $tooltipElement.remove();
@@ -1117,10 +1117,14 @@ export default {
                             errorMessageText += (errorMessageText.length ? '<br/>' : '') + encodeHtml(message);
                         });
 
+                        const invalidMessageClass = this.addWidgetPrefix(WIDGET_INVALID_MESSAGE_CLASS);
+
+                        this._rowsView.element().find('.' + invalidMessageClass).remove();
+
                         const $overlayElement = $('<div>')
                             .addClass(INVALID_MESSAGE_CLASS)
                             .addClass(INVALID_MESSAGE_ALWAYS_CLASS)
-                            .addClass(this.addWidgetPrefix(WIDGET_INVALID_MESSAGE_CLASS))
+                            .addClass(invalidMessageClass)
                             .html(errorMessageText)
                             .appendTo($cell);
 
