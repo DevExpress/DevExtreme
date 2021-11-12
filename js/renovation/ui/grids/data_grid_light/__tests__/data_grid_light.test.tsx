d568587178401c68978dc2bfe1d68ec53e02f06b
volnyagin <vconst@users.noreply.github.com>
2021-11-12 18:19:44 +0300

    DataGrid - Create basic DataGridLight

---
+++ js/renovation/ui/grids/data_grid_light/__tests__/data_grid_light.test.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { mount } from 'enzyme';
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import { DataGridLight, viewFunction as DataGridView, DataGridLightProps } from '../data_grid_light';
+import { Widget } from '../../../common/widget';
+
+describe('DataGridLight', () => {
+  describe('View', () => {
+    it('default render', () => {
+      const props = {
+        props: new DataGridLightProps(),
+        aria: { role: 'presentation' },
+        classes: '',
+        restAttributes: { 'rest-attributes': 'true' },
+      } as Partial<DataGridLight>;
+      const tree = mount(<DataGridView {...props as any} /> as any);
+
+      expect(tree.find(Widget).props()).toMatchObject({
+        aria: { role: 'presentation' },
+        classes: '',
+        'rest-attributes': 'true',
+      });
+    });
+
+    it('render with dataSource and 1 column', () => {
+      const props = new DataGridLightProps();
+      props.dataSource = [{ id: 1 }, { id: 2 }];
+      props.columns = ['id'];
+      const viewProps = { props } as Partial<DataGridLight>;
+      const tree = mount(<DataGridView {...viewProps as any} /> as any);
+
+      expect(tree.find('tr')).toHaveLength(3);
+      expect(tree.find('td')).toHaveLength(3);
+      expect(tree.find('td').at(0).children().text()).toEqual('id');
+      expect(tree.find('td').at(1).text()).toEqual('1');
+      expect(tree.find('td').at(2).text()).toEqual('2');
+    });
+
+    it('render with dataSource and 2 columns', () => {
+      const props = new DataGridLightProps();
+      props.dataSource = [{ id: 1, name: 'name 1' }];
+      props.columns = ['id', 'name'];
+      const viewProps = { props } as Partial<DataGridLight>;
+      const tree = mount(<DataGridView {...viewProps as any} /> as any);
+
+      expect(tree.find('tr')).toHaveLength(2);
+      expect(tree.find('td')).toHaveLength(4);
+      expect(tree.find('td').at(0).children().text()).toEqual('id');
+      expect(tree.find('td').at(1).children().text()).toEqual('name');
+      expect(tree.find('td').at(2).text()).toEqual('1');
+      expect(tree.find('td').at(3).text()).toEqual('name 1');
+    });
+  });
+
+  describe('Logic', () => {
+    describe('Getters', () => {
+      describe('aria', () => {
+        it('should have role "presentation"', () => {
+          expect(new DataGridLight({}).aria).toEqual({ role: 'presentation' });
+        });
+      });
+
+      describe('cssClasses', () => {
+        it('should have empty cssClasses', () => {
+          expect(new DataGridLight({}).cssClasses).toEqual('');
+        });
+      });
+    });
+  });
+});
---
+++ js/renovation/ui/grids/data_grid_light/data_grid_light.tsx
@@ -0,0 +1,103 @@
+import {
+  Component, JSXComponent, ComponentBindings, OneWay,
+} from '@devextreme-generator/declarations';
+
+import { Widget } from '../../common/widget';
+import { BaseWidgetProps } from '../../common/base_props';
+
+export const viewFunction = (viewModel: DataGridLight): JSX.Element => (
+  <Widget // eslint-disable-line jsx-a11y/no-access-key
+    accessKey={viewModel.props.accessKey}
+    activeStateEnabled={viewModel.props.activeStateEnabled}
+    aria={viewModel.aria}
+    classes={viewModel.cssClasses}
+    disabled={viewModel.props.disabled}
+    focusStateEnabled={viewModel.props.focusStateEnabled}
+    height={viewModel.props.height}
+    hint={viewModel.props.hint}
+    hoverStateEnabled={viewModel.props.hoverStateEnabled}
+    rtlEnabled={viewModel.props.rtlEnabled}
+    tabIndex={viewModel.props.tabIndex}
+    visible={viewModel.props.visible}
+    width={viewModel.props.width}
+    {...viewModel.restAttributes} // eslint-disable-line react/jsx-props-no-spreading
+  >
+    <div className="dx-datagrid dx-gridbase-container" role="grid" aria-label="Data grid">
+      <div className="dx-datagrid-headers dx-datagrid-nowrap" role="presentation">
+        <div className="dx-datagrid-content dx-datagrid-scroll-container" role="presentation">
+          <table className="dx-datagrid-table dx-datagrid-table-fixed" role="presentation">
+            <tbody role="presentation">
+              <tr className="dx-row dx-column-lines dx-header-row" role="row">
+                {viewModel.props.columns.map((dataField, index) => (
+                  <td
+                    aria-selected="false"
+                    role="columnheader" // eslint-disable-line jsx-a11y/no-noninteractive-element-to-interactive-role
+                    aria-colindex={index + 1}
+                    id={`dx-col-${index + 1}`}
+                    aria-label={`Column ${dataField}`}
+                    className="dx-datagrid-action dx-cell-focus-disabled"
+                    aria-sort="none"
+                    tabIndex={0}
+                  >
+                    <div className="dx-datagrid-text-content dx-text-content-alignment-left" role="presentation">
+                      {dataField}
+                    </div>
+                  </td>
+                ))}
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="dx-datagrid-rowsview dx-datagrid-nowrap" role="presentation">
+        <div className="dx-datagrid-content">
+          <table className="dx-datagrid-table dx-datagrid-table-fixed" role="presentation">
+            <tbody role="presentation">
+              {viewModel.props.dataSource.map((item, rowIndex) => (
+                <tr
+                  className="dx-row dx-data-row dx-column-lines"
+                  role="row"
+                  aria-selected="false"
+                  aria-rowindex={rowIndex + 1}
+                >
+                  {viewModel.props.columns.map((dataField, index) => (
+                    <td
+                      aria-describedby={`dx-col-${index + 1}`}
+                      aria-selected="false"
+                      role="gridcell"
+                      aria-colindex={index + 1}
+                    >
+                      {String(item[dataField])}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </Widget>
+);
+
+@ComponentBindings()
+export class DataGridLightProps extends BaseWidgetProps {
+  @OneWay()
+  dataSource: Record<string, unknown>[] = [];
+
+  @OneWay()
+  columns: string[] = [];
+}
+
+@Component({
+  defaultOptionRules: null,
+  jQuery: { register: false },
+  view: viewFunction,
+})
+export class DataGridLight extends JSXComponent(DataGridLightProps) {
+  readonly aria = {
+    role: 'presentation',
+  };
+
+  readonly cssClasses = '';
+}
