09174d7ca025ebd207f11ebc81ff06eb67558e5a
Pavel Gruba <pavel.gruba@devexpress.com>
2021-11-12 12:16:07 +0300

    Update tools version

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -668,7 +668,6 @@ declare module DevExpress {
      */
     resetOption(optionName: string): void;
   }
-
   /**
    * @deprecated Attention! This type is for internal purposes only. If you used it previously, please describe your scenario in the following GitHub Issue, and we will suggest a public alternative: {@link https://github.com/DevExpress/DevExtreme/issues/17885|Internal Types}.
    */
