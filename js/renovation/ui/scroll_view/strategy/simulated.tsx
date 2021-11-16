d7f8eeffd9411853d1b89ce36d7341bf1d6f4e39
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2021-11-16 22:42:14 +0300

    Revert "try new solution for working with scrollLocation values"
    This reverts commit 4cc4ede6186b9415657e13c3e705ab14b7324da3.

--- js/renovation/ui/scroll_view/strategy/simulated.tsx
+++ js/renovation/ui/scroll_view/strategy/simulated.tsx
@@ -724,23 +724,20 @@ export class ScrollableSimulated extends JSXComponent<ScrollableSimulatedProps>(
 
     const { fullScrollProp, location, needFireScroll } = eventData;
 
-    // we pass scrollTop value the 687.875px to container
-    // after that container's scrollTop property store the 687.333px
     this.containerRef.current![fullScrollProp] = location;
-    const actualNativeLocation = this.containerRef.current![fullScrollProp];
 
     if (fullScrollProp === 'scrollLeft') {
-      this.hScrollLocation = -actualNativeLocation;
+      this.hScrollLocation = -location;
     } else {
-      this.vScrollLocation = -actualNativeLocation;
+      this.vScrollLocation = -location;
     }
 
-    const scrollDelta = Math.abs(this.savedScrollOffset[fullScrollProp] - actualNativeLocation);
+    const scrollDelta = Math.abs(this.savedScrollOffset[fullScrollProp] - location);
 
     if (needFireScroll && scrollDelta >= 1) {
       this.onScroll();
     }
-    this.savedScrollOffset[fullScrollProp] = actualNativeLocation;
+    this.savedScrollOffset[fullScrollProp] = location;
   }
 
   get hScrollOffsetMax(): number {
