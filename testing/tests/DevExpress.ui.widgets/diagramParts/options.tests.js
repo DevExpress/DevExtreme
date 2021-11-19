f9cd70a9fbdb4f4634f2728b50c9a5228f140735
VictorSavushkin <36996384+VictorSavushkin@users.noreply.github.com>
2021-11-19 16:19:41 +0300

    Diagram - fix zoomLevel issue (T1044759) (#20189)

--- testing/tests/DevExpress.ui.widgets/diagramParts/options.tests.js
+++ testing/tests/DevExpress.ui.widgets/diagramParts/options.tests.js
@@ -45,6 +45,15 @@ QUnit.module('Options', {
         this.instance._diagramInstance.commandManager.getCommand(DiagramCommand.ZoomLevel).execute(1.5);
         assert.equal(this.instance.option('zoomLevel'), 1.5);
         assert.ok(this.onOptionChanged.called);
+
+        // T1044759
+        this.instance.repaint();
+        this.instance.option('zoomLevel', 1.2);
+        assert.equal(this.instance.option('zoomLevel'), 1.2);
+        assert.ok(this.onOptionChanged.called);
+        this.instance.fitToWidth();
+        assert.equal(this.instance.option('zoomLevel'), 1);
+        assert.ok(this.onOptionChanged.called);
     });
     test('should change zoomLevel object property', function(assert) {
         assert.equal(this.instance._diagramInstance.settings.zoomLevel, 1);
