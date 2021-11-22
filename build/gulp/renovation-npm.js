a94de4ee25f5c2542758bd177da4852d2bfd75be
alexserov <jexaser@gmail.com>
2021-11-22 13:47:08 +0300

    add package.json and readme.md

--- build/gulp/renovation-npm.js
+++ build/gulp/renovation-npm.js
@@ -2,20 +2,43 @@
 
 const gulp = require('gulp');
 const del = require('del');
+const merge = require('merge-stream');
+const ctx = require('./context.js');
+const { version } = require('../../package.json');
+const replace = require('gulp-replace');
+
+function copyServiceFiles(dist) {
+    return () => merge(
+        gulp
+            .src('package.json')
+            .pipe(replace(version, ctx.version.package))
+            .pipe(gulp.dest(dist)),
+        gulp
+            .src('README.md')
+            .pipe(gulp.dest(dist))
+    );
+}
+function cleanNpmFramework(destination) {
+    return function cleanNpmFramework(done) {
+        del.sync(destination);
+        done();
+    };
+}
+function copyFrameworkArtifacts(source, destination) {
+    return () => merge(
+        gulp.src(source)
+            .pipe(gulp.dest(destination))
+    );
+}
 
 function addCompilationTask(frameworkName) {
     const frameworkSrc = `artifacts/${frameworkName}/renovation/**/*`;
     const npmDestFolder = `artifacts/npm-${frameworkName}`;
     const generateSeries = [
-        function cleanNpmFramework(cb) {
-            del.sync(npmDestFolder);
-            cb();
-        },
+        cleanNpmFramework(npmDestFolder),
         `generate-${frameworkName}`,
-        function copyFrameworkArtifacts() {
-            return gulp.src(frameworkSrc)
-                .pipe(gulp.dest(npmDestFolder));
-        }
+        copyFrameworkArtifacts(frameworkSrc, npmDestFolder),
+        copyServiceFiles(npmDestFolder)
     ];
     gulp.task(`renovation-npm-${frameworkName}`, gulp.series(...generateSeries));
 }
