64f13037a9b756c2c44ef6efa3b08cea6c9700d9
alexserov <jexaser@gmail.com>
2021-11-22 15:43:49 +0300

    replace external imports with devextreme package refs

--- build/gulp/renovation-npm.js
+++ build/gulp/renovation-npm.js
@@ -6,6 +6,71 @@ const merge = require('merge-stream');
 const ctx = require('./context.js');
 const { version } = require('../../package.json');
 const replace = require('gulp-replace');
+const through = require('through2');
+const { parse, print } = require('recast');
+const { visit, namedTypes, builders } = require('ast-types');
+const path = require('path');
+const tsparser = require("recast/parsers/typescript");
+const jsparser = require("recast/parsers/babel")
+
+function performRecastReplacements(rootFolderPath) {
+    return through.obj((file, enc, callback) => {
+        if (file.isNull())
+            return callback(null, file);
+        
+        const fileExt = path.extname(file.path);
+        const absoluteRootFolderPath = path.resolve(process.cwd(), rootFolderPath);
+
+        const isJs = fileExt === '.js';
+        const isTs = fileExt === '.ts';
+        if (!isJs && !isTs)
+            return callback(null, file);
+
+        const ast = parse(file.contents.toString(), {
+            parser: isJs ? jsparser : tsparser,
+            sourceFileName: file.path
+        });
+
+        let needsPrint = false;
+        const fileDir = path.dirname(file.path);
+        const devextremeFolder = path.dirname(absoluteRootFolderPath);
+        visit(ast, {
+            visitImportDeclaration(nodePath) {
+                const importFrom = nodePath.node.source.value;
+                if (importFrom.startsWith('.')) {
+                    //relative module path
+                    const absoluteModulePath = path.resolve(fileDir, importFrom);
+                    if (absoluteModulePath.startsWith(absoluteRootFolderPath)) {
+                        return false;
+                    }
+                    const newPath = path.relative(devextremeFolder, absoluteModulePath).replace(/\\/g, '/');
+
+                    const args = [
+                        nodePath.node.specifiers,
+                        builders.stringLiteral(`devextreme/${newPath}`),
+                        nodePath.node.importKind
+                    ];
+                    if (!args[2]) {
+                        args.splice(2, 1);
+                    }
+                    nodePath.replace(builders.importDeclaration(...args));
+                    needsPrint = true;
+                    return false;
+                } else {
+                    //package module import
+                    return false;
+                }
+            }
+        })
+
+        if (!needsPrint) {
+            return callback(null, file);
+        }
+        const result = print(ast, { quote: 'single', lineTerminator: '\n' }).code;
+        file.contents = Buffer.from(result, enc);
+        return callback(null, file);
+    });
+}
 
 function copyServiceFiles(dist) {
     return () => merge(
@@ -26,13 +91,14 @@ function cleanNpmFramework(destination) {
 }
 function copyFrameworkArtifacts(source, destination) {
     return () => merge(
-        gulp.src(source)
+        gulp.src(source + "/**/*")
+            .pipe(performRecastReplacements(source))
             .pipe(gulp.dest(destination))
     );
 }
 
 function addCompilationTask(frameworkName) {
-    const frameworkSrc = `artifacts/${frameworkName}/renovation/**/*`;
+    const frameworkSrc = `artifacts/${frameworkName}/renovation`;
     const npmDestFolder = `artifacts/npm-${frameworkName}`;
     const generateSeries = [
         cleanNpmFramework(npmDestFolder),
