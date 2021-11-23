abf3392266930cd66677790d7dea856040956101
alexserov <jexaser@gmail.com>
2021-11-23 20:22:38 +0300

    add fw test repos

---
+++ testing/renovation-npm/react-app/src/App.js
@@ -0,0 +1,14 @@
+import 'devextreme/dist/css/dx.light.css';
+import Button from '@devextreme/react/ui/button';
+
+function App() {
+  return (
+    <div className="App">
+       <Button
+                text="Click me"
+            />
+    </div>
+  );
+}
+
+export default App;
