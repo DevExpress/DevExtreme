2cf2d48b80b7acad51e9434c6333c1a90ff4b152
Alexey Babich <alexey.babych@gmail.com>
2021-11-23 14:40:27 +0300

    Set less strong cascad for padding in datagrid/treelist (T1045321) (#20187)
    * Set less strong cascad for padding in datagrid/treelist
    
    * Fix master-detail padding for both themes
    
    * Fix tests
    
    * Fix etalon

---
+++ testing/testcafe/tests/dataGrid/masterDetail.ts
@@ -0,0 +1,80 @@
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import url from '../../helpers/getPageUrl';
+import createWidget, { disposeWidgets } from '../../helpers/createWidget';
+import { changeTheme } from '../../helpers/changeTheme';
+
+fixture.disablePageReloads`Master detail`
+  .page(url(__dirname, '../container.html'))
+  .afterEach(async () => disposeWidgets());
+
+['material.blue.light', 'generic.light'].forEach((theme) => {
+  test(`Checkbox align right in masterdetail (T1045321) ${theme}`, async (t) => {
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+    // assert
+    await t
+      .expect(await takeScreenshot(`T1045321-${theme}.png`, '#container'))
+      .ok()
+      .expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
+  }).before(async () => {
+    await changeTheme(theme);
+    return createWidget('dxDataGrid', {
+      dataSource: [{
+        ID: 1,
+        Prefix: 'Mr.',
+      }],
+      keyExpr: 'ID',
+      showBorders: true,
+      selection: {
+        mode: 'multiple',
+      },
+      columns: [
+        {
+          dataField: 'Prefix',
+          caption: 'Title',
+          width: 400,
+        },
+      ],
+      masterDetail: {
+        autoExpandAll: true,
+        enabled: true,
+        template(container) {
+          ($('<div>') as any)
+            .dxTreeList({
+              columnAutoWidth: true,
+              showBorders: true,
+              selection: {
+                mode: 'multiple',
+              },
+              dataSource: [{
+                ID: 1,
+                Title: 'CEO',
+                Hire_Date: '1995-01-15',
+              }],
+              rootValue: -1,
+              keyExpr: 'ID',
+              parentIdExpr: 'Head_ID',
+              columns: [
+                {
+                  dataField: 'Title',
+                  caption: 'Position',
+                  width: 200,
+                },
+                {
+                  dataField: 'Hire_Date',
+                  dataType: 'date',
+                  width: 200,
+                },
+              ],
+              showRowLines: true,
+            })
+            .appendTo(container);
+        },
+      },
+    });
+  }).after(async () => {
+    await disposeWidgets();
+    await changeTheme('generic.light');
+  });
+});
