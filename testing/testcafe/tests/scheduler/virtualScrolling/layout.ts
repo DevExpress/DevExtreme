e2a90b5663c8ee19ddf9c716ee98c2d90629e67d
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2021-11-23 17:28:00 +0300

    scrollable: add necessary masks for scheduler screenshot tests (#20204)
    (cherry picked from commit 3ce94fbc09598b22965cb3f569756e0f1202c65a)

--- testing/testcafe/tests/scheduler/virtualScrolling/layout.ts
+++ testing/testcafe/tests/scheduler/virtualScrolling/layout.ts
@@ -43,11 +43,6 @@ test('Virtual scrolling layout in scheduler views', async (t) => {
     const view = views[i];
 
     await scheduler.option('currentView', view.type);
-
-    await t.expect(
-      await takeScreenshot(`virtual-scrolling-${view.type}-before-scroll.png`),
-    ).ok();
-
     await scrollTo(scrollConfig[i].firstDate);
 
     await t.expect(
@@ -79,11 +74,6 @@ test('Virtual scrolling layout in scheduler views when horizontal grouping is en
     const view = views[i];
 
     await scheduler.option('currentView', view.type);
-
-    await t.expect(
-      await takeScreenshot(`virtual-scrolling-${view.type}-before-scroll-horizontal-grouping.png`),
-    ).ok();
-
     await scrollTo(scrollConfig[i].firstDate, { resourceId: 6 });
 
     await t.expect(
@@ -117,10 +107,6 @@ test('Virtual scrolling layout in scheduler views when grouping by date is enabl
     const view = views[i];
 
     await scheduler.option('currentView', view.type);
-
-    await t.expect(
-      await takeScreenshot(`virtual-scrolling-${view.type}-before-scroll-grouping-by-date.png`),
-    ).ok();
 
     await scrollTo(scrollConfig[i].firstDate, { resourceId: 3 });
 
