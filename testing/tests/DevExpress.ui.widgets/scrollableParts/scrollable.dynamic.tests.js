afb88d95db23d043c37d9df8921b4d8104033554
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2021-11-26 13:26:18 +0300

    scrollable: refactor qunit tests for renovated widget (#20241)

--- testing/tests/DevExpress.ui.widgets/scrollableParts/scrollable.dynamic.tests.js
+++ testing/tests/DevExpress.ui.widgets/scrollableParts/scrollable.dynamic.tests.js
@@ -3,7 +3,6 @@ import { getTranslateValues } from 'renovation/ui/scroll_view/utils/get_translat
 import animationFrame from 'animation/frame';
 import resizeCallbacks from 'core/utils/resize_callbacks';
 import pointerMock from '../../../helpers/pointerMock.js';
-import Scrollable from 'ui/scroll_view/ui.scrollable';
 
 import 'generic_light.css!';
 
@@ -16,7 +15,6 @@ import {
 const INERTIA_TIMEOUT = 100;
 
 const GESTURE_LOCK_KEY = 'dxGestureLock';
-const isRenovation = !!Scrollable.IS_RENOVATED_WIDGET;
 
 const moduleConfig = {
     beforeEach: function() {
@@ -319,6 +317,7 @@ QUnit.test('stop bounce on click', function(assert) {
     const $scrollable = $('#scrollable').dxScrollable({
         useNative: false,
         onEnd: function() {
+            assert.ok(false, 'shouldn\'t fire end action');
         }
     });
     const $content = $scrollable.find(`.${SCROLLABLE_CONTENT_CLASS}`);
@@ -351,6 +350,7 @@ QUnit.test('stop inertia bounce on after mouse up', function(assert) {
     const $scrollable = $('#scrollable').dxScrollable({
         useNative: false,
         onEnd: function() {
+            assert.ok(false, 'shouldn\'t fire end action');
         }
     });
     const $content = $scrollable.find(`.${SCROLLABLE_CONTENT_CLASS}`);
@@ -546,8 +546,6 @@ QUnit.test('velocity calculated correctly when content height less than containe
 
 [true, false].forEach((useNative) => {
     QUnit.test(`window resize should call update, useNative: ${useNative}`, function(assert) {
-        assert.expect(1);
-
         const $scrollable = $('#scrollable');
 
         const updateHandler = sinon.spy();
@@ -561,7 +559,7 @@ QUnit.test('velocity calculated correctly when content height less than containe
 
         resizeCallbacks.fire();
 
-        assert.equal(updateHandler.callCount, isRenovation ? 0 : 1, 'onUpdate handler was fired once');
+        assert.equal(updateHandler.callCount, 1, 'onUpdate handler was fired once');
     });
 
 
