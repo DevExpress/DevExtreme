edc9d4876c39cb599411b57d3b042dde850e45f1
ilyakhd <ilyakhd@users.noreply.github.com>
2021-11-30 22:18:30 +0300

    Fix EventObject type declaration (class changed to type)

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -3187,7 +3187,7 @@ declare module DevExpress.events {
   /**
    * [descr:EventObject]
    */
-  export class EventObject {
+  export type EventObject = {
     /**
      * [descr:EventObject.currentTarget]
      */
@@ -3231,7 +3231,7 @@ declare module DevExpress.events {
      * [descr:EventObject.stopPropagation()]
      */
     stopPropagation(): void;
-  }
+  };
   /**
    * [descr:handler(event, extraParameters)]
    * @deprecated Attention! This type is for internal purposes only. If you used it previously, please describe your scenario in the following GitHub Issue, and we will suggest a public alternative: {@link https://github.com/DevExpress/DevExtreme/issues/17885|Internal Types}.
