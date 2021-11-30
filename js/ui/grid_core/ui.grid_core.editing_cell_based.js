c96bb26aa382f092b473747645460b99976862ea
NickMitrokhin <nikolai.mitrokhin@devexpress.com>
2021-11-30 14:48:51 +0300

    small refactoring 2

--- js/ui/grid_core/ui.grid_core.editing_cell_based.js
+++ js/ui/grid_core/ui.grid_core.editing_cell_based.js
@@ -208,10 +208,6 @@ export default {
                         column
                     };
 
-                    if(!item) {
-                        return;
-                    }
-
                     if(item.key === undefined) {
                         this._dataController.fireError('E1043');
                         return;
