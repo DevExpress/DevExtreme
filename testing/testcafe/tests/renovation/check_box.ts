4a4b4ff10707b37aff47d40ec954efbe3d30b182
ksercs <ksercs0@gmail.com>
2021-12-01 17:35:40 +0300

    don't stop tests after first fail

--- testing/testcafe/tests/renovation/check_box.ts
+++ testing/testcafe/tests/renovation/check_box.ts
@@ -1,4 +1,4 @@
-import { compareScreenshot } from 'devextreme-screenshot-comparer';
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import { Selector } from 'testcafe';
 import { multiPlatformTest } from '../../helpers/multi-platform-test';
 
@@ -9,20 +9,24 @@ fixture('CheckBox');
 test('CheckBox value', async (t, { screenshotComparerOptions }) => {
   const checkBox = Selector('.dx-checkbox');
 
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
   await t
     .expect(checkBox.hasClass('dx-checkbox-indeterminate')).eql(true)
-    .expect(await compareScreenshot(t, 'check_box_indeterminate.png', null, screenshotComparerOptions))
-    .eql(true);
+    .expect(await takeScreenshot('check_box_indeterminate.png', null, screenshotComparerOptions)).ok()
 
-  await t
     .click(checkBox)
-    .expect(checkBox.hasClass('dx-checkbox-checked')).eql(true)
-    .expect(await compareScreenshot(t, 'check_box_checked.png', null, screenshotComparerOptions))
-    .eql(true);
+    .expect(checkBox.hasClass('dx-checkbox-checked'))
+    .eql(true)
+    .expect(await takeScreenshot('check_box_checked.png', null, screenshotComparerOptions))
+    .ok()
 
-  await t
     .click(checkBox)
-    .expect(checkBox.hasClass('dx-checkbox-checked')).eql(false)
-    .expect(await compareScreenshot(t, 'check_box_unchecked.png', null, screenshotComparerOptions))
-    .eql(true);
+    .expect(checkBox.hasClass('dx-checkbox-checked'))
+    .eql(false)
+    .expect(await takeScreenshot('check_box_unchecked.png', null, screenshotComparerOptions))
+    .ok()
+
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
 });
