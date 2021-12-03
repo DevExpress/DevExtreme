9d5bc9ac7ee9061da0feb85ff059e0ec376221d2
Evgeniy Aryamnov <earyamnow@mail.ru>
2021-12-03 16:24:32 +0300

    dxScheduler - delete component instance from renovated scheduler

--- js/renovation/ui/scheduler/__tests__/scheduler.test.tsx
+++ js/renovation/ui/scheduler/__tests__/scheduler.test.tsx
@@ -553,142 +553,80 @@ describe('Scheduler', () => {
     });
 
     describe('Methods', () => {
-      it('dispose should pass call to instance', () => {
-        const scheduler = new Scheduler(new SchedulerProps());
-        const dispose = jest.fn();
+      describe('Public API', () => {
+        it('Non-implemented methods should not throw errors', () => {
+          const scheduler = new Scheduler(new SchedulerProps());
 
-        scheduler.instance = {
-          dispose,
-        } as any;
+          expect(() => scheduler.addAppointment({
+            startDate: new Date(2021, 5, 15, 12),
+            endDate: new Date(2021, 5, 15, 14),
+            text: 'temp',
+          }))
+            .not.toThrow();
+          expect(() => scheduler.deleteAppointment({
+            startDate: new Date(2021, 5, 15, 12),
+            endDate: new Date(2021, 5, 15, 14),
+            text: 'temp',
+          }))
+            .not.toThrow();
+          expect(() => scheduler.updateAppointment({
+            startDate: new Date(2021, 5, 15, 12),
+            endDate: new Date(2021, 5, 15, 14),
+            text: 'temp',
+          }, {
+            startDate: new Date(2021, 5, 15, 12),
+            endDate: new Date(2021, 5, 15, 14),
+            text: 'changed',
+          }))
+            .not.toThrow();
 
-        scheduler.dispose()();
+          expect(() => scheduler.showAppointmentPopup())
+            .not.toThrow();
+          expect(() => scheduler.showAppointmentTooltip({}, ''))
+            .not.toThrow();
+          expect(() => scheduler.hideAppointmentPopup())
+            .not.toThrow();
 
-        expect(dispose).toBeCalledTimes(1);
-      });
-
-      it('getComponentInstance should pass call to instance', () => {
-        const scheduler = new Scheduler(new SchedulerProps());
-        const mockInstance = {};
-
-        scheduler.instance = mockInstance as any;
-        expect(scheduler.getComponentInstance()).toMatchObject(mockInstance);
-      });
-
-      it('*Appointment\'s methods should pass call to instance', () => {
-        const addAppointment = jest.fn();
-        const deleteAppointment = jest.fn();
-        const updateAppointment = jest.fn();
-
-        const scheduler = new Scheduler(new SchedulerProps());
-
-        scheduler.instance = {
-          addAppointment,
-          deleteAppointment,
-          updateAppointment,
-        } as any;
-
-        scheduler.addAppointment({
-          startDate: new Date(2021, 5, 15, 12),
-          endDate: new Date(2021, 5, 15, 14),
-          text: 'temp',
+          expect(() => scheduler.scrollTo(new Date()))
+            .not.toThrow();
+          expect(() => scheduler.scrollToTime(12, 12))
+            .not.toThrow();
         });
-        expect(addAppointment).toHaveBeenCalled();
 
-        scheduler.deleteAppointment({
-          startDate: new Date(2021, 5, 15, 12),
-          endDate: new Date(2021, 5, 15, 14),
-          text: 'temp',
+        it('getDataSource should return an instance of DataSource', () => {
+          const scheduler = new Scheduler(new SchedulerProps());
+
+          expect(scheduler.getDataSource())
+            .toEqual(expect.any(DataSource));
         });
-        expect(deleteAppointment).toHaveBeenCalled();
 
-        scheduler.updateAppointment({
-          startDate: new Date(2021, 5, 15, 12),
-          endDate: new Date(2021, 5, 15, 14),
-          text: 'temp',
-        }, {
-          startDate: new Date(2021, 5, 15, 12),
-          endDate: new Date(2021, 5, 15, 14),
-          text: 'changed',
+        it('hideAppointmentTooltip should hide tooltip', () => {
+          const scheduler = new Scheduler(new SchedulerProps());
+
+          scheduler.tooltipVisible = true;
+          scheduler.hideAppointmentTooltip();
+
+          expect(scheduler.tooltipVisible)
+            .toBe(false);
         });
-        expect(updateAppointment).toHaveBeenCalled();
-      });
 
-      it('getDataSource should pass call to instance', () => {
-        const getDataSource = jest.fn();
-        const scheduler = new Scheduler(new SchedulerProps());
+        it('getEndViewDate and getStartViewDate should work correctly', () => {
+          const scheduler = new Scheduler({
+            ...new SchedulerProps(),
+            currentDate: new Date(2021, 11, 3),
+          });
 
-        scheduler.instance = {
-          getDataSource,
-        } as any;
+          scheduler.workSpaceViewModel = {
+            viewDataProvider: {
+              getLastCellEndDate: () => new Date(2021, 11, 4),
+            },
+          } as any;
 
-        scheduler.getDataSource();
-
-        expect(getDataSource).toHaveBeenCalled();
-      });
-
-      it('*appointmentPopup and *appointmentTooltip should pass call to instance', () => {
-        const hideAppointmentPopup = jest.fn();
-        const hideAppointmentTooltip = jest.fn();
-
-        const showAppointmentPopup = jest.fn();
-        const showAppointmentTooltip = jest.fn();
-
-        const scheduler = new Scheduler(new SchedulerProps());
-
-        scheduler.instance = {
-          hideAppointmentPopup,
-          hideAppointmentTooltip,
-          showAppointmentPopup,
-          showAppointmentTooltip,
-        } as any;
-
-        scheduler.hideAppointmentPopup();
-        expect(hideAppointmentPopup).toHaveBeenCalled();
-
-        scheduler.hideAppointmentTooltip();
-        expect(hideAppointmentTooltip).toHaveBeenCalled();
-
-        scheduler.showAppointmentPopup();
-        expect(showAppointmentPopup).toHaveBeenCalled();
-
-        scheduler.showAppointmentTooltip({}, '');
-        expect(showAppointmentTooltip).toHaveBeenCalled();
-      });
-
-      it('getEndViewDate and getStartViewDate should pass call to instance', () => {
-        const getEndViewDate = jest.fn();
-        const getStartViewDate = jest.fn();
-
-        const scheduler = new Scheduler(new SchedulerProps());
-
-        scheduler.instance = {
-          getEndViewDate,
-          getStartViewDate,
-        } as any;
-
-        scheduler.getEndViewDate();
-        expect(getEndViewDate).toHaveBeenCalled();
-
-        scheduler.getStartViewDate();
-        expect(getStartViewDate).toHaveBeenCalled();
-      });
-
-      it('scroll* methods should pass call to instance', () => {
-        const scrollTo = jest.fn();
-        const scrollToTime = jest.fn();
-
-        const scheduler = new Scheduler(new SchedulerProps());
-
-        scheduler.instance = {
-          scrollTo,
-          scrollToTime,
-        } as any;
-
-        scheduler.scrollTo(new Date());
-        expect(scrollTo).toHaveBeenCalled();
-
-        scheduler.scrollToTime(12, 12);
-        expect(scrollToTime).toHaveBeenCalled();
+          expect(scheduler.getStartViewDate())
+            .toEqual(new Date(2021, 11, 3));
+          expect(scheduler.getEndViewDate())
+            .toEqual(new Date(2021, 11, 4));
+        });
       });
 
       it('dataAccessors should be correctly created', () => {
