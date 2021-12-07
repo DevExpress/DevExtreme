1c7af71ce449fc6b075d0c284ff8128625dc6740
MikeVitik <vitikm@gmail.com>
2021-12-07 16:27:16 +0300

    fix jest test

--- js/renovation/ui/grids/data_grid/data_grid.tsx
+++ js/renovation/ui/grids/data_grid/data_grid.tsx
@@ -523,7 +523,7 @@ export class DataGrid extends JSXComponent(DataGridProps) implements DataGridFor
 
   @Effect({ run: 'once' })
   setupInstance(): void {
-    const element = this.widgetElementRef.current as HTMLElement;
+    const element = this.widgetElementRef?.current as HTMLElement;
     // TODO Vitik: Not only optionChanged should be rewrited.
     // All other events should be re-raised by renovated grid.
 
