f6ba0a433cad33f91ad8a2efa2207cbd08629b48
alexserov@gmail.com <jexaser@gmail.com>
2021-12-08 14:21:42 +0300

    external format fix

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -972,7 +972,7 @@ declare module DevExpress {
   /**
    * @deprecated Attention! This type is for internal purposes only. If you used it previously, please describe your scenario in the following GitHub Issue, and we will suggest a public alternative: {@link https://github.com/DevExpress/DevExtreme/issues/17885|Internal Types}.
    */
-  type ExternalFormat = any;
+  type ExternalFormat = never;
   /**
    * @deprecated Attention! This type is for internal purposes only. If you used it previously, please describe your scenario in the following GitHub Issue, and we will suggest a public alternative: {@link https://github.com/DevExpress/DevExtreme/issues/17885|Internal Types}.
    */
