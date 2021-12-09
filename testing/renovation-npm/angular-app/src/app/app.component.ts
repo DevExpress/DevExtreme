d5e38fbf09caa4422c7fe397c313c3b377394747
dxvladislavvolkov <vladislavvolkov@vladislavs-mbp.corp.devexpress.com>
2021-12-09 15:52:48 +0300

    Build components using angular 12

--- testing/renovation-npm/angular-app/src/app/app.component.ts
+++ testing/renovation-npm/angular-app/src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-
+/* eslint-disable */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // eslint-disable-next-line spellcheck/spell-checker
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-app';
-  helloWorld() {
-    alert(1);
-  }
+  title = 'app12';
 }
