b5f8062b938845efb58852a494a429228887e44a
Roman Semenov <roman.semenov@devexpress.com>
2021-12-13 12:32:38 +0300

    fix

--- js/ui/grid_core/ui.grid_core.editing_form_based.js
+++ js/ui/grid_core/ui.grid_core.editing_form_based.js
@@ -289,6 +289,7 @@ export const editingFormBasedModule = {
                     this._rowsView.renderTemplate($container, template, cellOptions, !!$container.closest(getWindow().document).length).done(() => {
                         this._rowsView._updateCell($container, cellOptions);
                     });
+                    this._rowsView.renderDelayedTemplates();
                     return cellOptions;
                 },
 
