73aca6c057ff7a5856661401f13d791ca39815ed
Anton Kuznetsov <anton.kuznetsov@devexpress.com>
2021-12-13 19:12:51 +0300

    Validator: fix editor accessibility attributes render for renovation and framework wrappers (T1048128, T1049206) (#20495)

--- js/ui/editor/editor.js
+++ js/ui/editor/editor.js
@@ -121,6 +121,7 @@ const Editor = Widget.inherit({
 
         this.callBase();
         this._renderValidationState();
+        this.option('_onMarkupRendered')?.();
     },
 
     _raiseValueChangeAction: function(value, previousValue) {
