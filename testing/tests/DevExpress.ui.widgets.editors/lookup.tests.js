82b7ad2680a49927a63eb1ad12d60492d4a99931
Anton Kuznetsov <anton.kuznetsov@devexpress.com>
2021-12-21 04:49:37 +0300

    TextEditor: placeholder should be rendered only once on markup initialization (T1054252) (#20608)

--- testing/tests/DevExpress.ui.widgets.editors/lookup.tests.js
+++ testing/tests/DevExpress.ui.widgets.editors/lookup.tests.js
@@ -72,7 +72,6 @@ const LIST_ITEM_SELECTED_CLASS = 'dx-list-item-selected';
 const LIST_GROUP_HEADER_CLASS = 'dx-list-group-header';
 
 const LOOKUP_SEARCH_CLASS = 'dx-lookup-search';
-const LOOKUP_SEARCH_WRAPPER_CLASS = 'dx-lookup-search-wrapper';
 const LOOKUP_FIELD_CLASS = 'dx-lookup-field';
 const CLEAR_BUTTON_CLASS = 'dx-popup-clear';
 const APPLY_BUTTON_CLASS = 'dx-popup-done';
@@ -95,6 +94,14 @@ const getList = function() {
     return $('.dx-list').dxList('instance');
 };
 
+const getSearchBox = (lookup) => {
+    return lookup._$searchBox;
+};
+
+const getSearchWrapper = (lookup) => {
+    return $(lookup.content()).find('.dx-lookup-search-wrapper');
+};
+
 QUnit.module('Lookup', {
     beforeEach: function() {
         fx.off = true;
@@ -114,7 +121,7 @@ QUnit.module('Lookup', {
             this.$list = $('.dx-list');
             this.list = this.$list.dxList('instance');
 
-            this.$search = $(this.instance._$searchBox);
+            this.$search = getSearchBox(this.instance);
             this.search = this.instance._searchBox;
         };
     },
@@ -1557,7 +1564,7 @@ QUnit.module('options', {
         }).dxLookup('instance');
 
         const popup = instance._popup;
-        const $search = instance._$searchBox;
+        const $search = getSearchBox(instance);
 
         assert.ok($(popup.$wrapper()).hasClass('dx-lookup-popup-search'));
         assert.ok($search.is(':visible'), 'default value');
@@ -1565,6 +1572,18 @@ QUnit.module('options', {
         instance.option('searchEnabled', false);
         assert.ok(!$(popup.$wrapper()).hasClass('dx-lookup-popup-search'));
         assert.ok($search.is(':hidden'), 'hidden');
+    });
+
+    QUnit.test('excess main input placeholder should not be rendered when deferRendering=false (T1054252)', function(assert) {
+        const lookup = $('#lookup').dxLookup({
+            deferRendering: false
+        }).dxLookup('instance');
+
+        const $searchWrapper = getSearchWrapper(lookup);
+        const $placeholders = $searchWrapper.find('.dx-placeholder');
+
+        assert.strictEqual($placeholders.length, 1, 'search wrapper contains the single placeholder');
+        assert.strictEqual($placeholders.eq(0).attr('data-dx_placeholder'), 'Search', 'it is search placeholder');
     });
 
     QUnit.test('cleanSearchOnOpening', function(assert) {
@@ -1951,7 +1970,7 @@ QUnit.module('options', {
             opened: true
         }).dxLookup('instance');
 
-        assert.equal($(instance.content()).find('.' + LOOKUP_SEARCH_WRAPPER_CLASS).length, 0, 'search wrapper is not rendered');
+        assert.equal(getSearchWrapper(instance).length, 0, 'search wrapper is not rendered');
     });
 
     QUnit.test('search wrapper should be rendered if the \'searchEnabled\' option is true', function(assert) {
@@ -1960,7 +1979,7 @@ QUnit.module('options', {
             opened: true
         }).dxLookup('instance');
 
-        assert.equal($(instance.content()).find('.' + LOOKUP_SEARCH_WRAPPER_CLASS).length, 1, 'search wrapper is rendered');
+        assert.equal(getSearchWrapper(instance).length, 1, 'search wrapper is rendered');
     });
 
     QUnit.test('clear button option runtime change', function(assert) {
@@ -2589,7 +2608,7 @@ QUnit.module('focus policy', {
 
         instance.option('opened', true);
 
-        const $searchBox = instance._$searchBox;
+        const $searchBox = getSearchBox(instance);
         assert.ok($searchBox.hasClass(FOCUSED_CLASS), '\'focus\' method focus searchBox with opened overlay');
     });
 
@@ -2672,7 +2691,7 @@ QUnit.module('keyboard navigation', {
         const instance = $element.dxLookup('instance');
 
         assert.ok(instance.option('opened'));
-        assert.ok(instance._$searchBox.hasClass(FOCUSED_CLASS), 'searchBox has focus after open popup');
+        assert.ok(getSearchBox(instance).hasClass(FOCUSED_CLASS), 'searchBox has focus after open popup');
     });
 
     QUnit.testInActiveWindow('lookup-list should be focused after \'down\' key pressing', function(assert) {
@@ -2689,7 +2708,7 @@ QUnit.module('keyboard navigation', {
         });
         const instance = $element.dxLookup('instance');
 
-        const keyboard = keyboardMock(instance._$searchBox.find('.dx-texteditor-input'));
+        const keyboard = keyboardMock(getSearchBox(instance).find('.dx-texteditor-input'));
         keyboard.keyDown('down');
 
         assert.ok(instance._$list.find('.dx-list-item').first().hasClass(FOCUSED_CLASS), 'list-item is focused after down key pressing');
@@ -2732,7 +2751,7 @@ QUnit.module('keyboard navigation', {
         });
         const instance = $element.dxLookup('instance');
 
-        const keyboard = keyboardMock(instance._$searchBox.find('.dx-texteditor-input'));
+        const keyboard = keyboardMock(getSearchBox(instance).find('.dx-texteditor-input'));
         keyboard.keyDown('down');
         keyboard.keyDown('down');
         keyboard.keyDown('enter');
@@ -2754,7 +2773,7 @@ QUnit.module('keyboard navigation', {
         });
         const instance = $element.dxLookup('instance');
 
-        const keyboard = keyboardMock(instance._$searchBox.find('.dx-texteditor-input'));
+        const keyboard = keyboardMock(getSearchBox(instance).find('.dx-texteditor-input'));
         keyboard.keyDown('down');
         keyboard.keyDown('down');
         keyboard.keyDown('space');
@@ -2825,7 +2844,7 @@ QUnit.module('keyboard navigation', {
             focusStateEnabled: true,
             searchEnabled: true
         }).dxLookup('instance');
-        const keyboard = keyboardMock(instance._$searchBox.find('.dx-texteditor-input'));
+        const keyboard = keyboardMock(getSearchBox(instance).find('.dx-texteditor-input'));
 
         assert.ok(instance.option('opened'), 'overlay opened');
 
