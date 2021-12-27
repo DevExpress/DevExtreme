53c512d04a3399e3a48104d6d98e997d842d1f09
Alexander Bezborodov (DevExpress) <bezborodov.alexander@corp.devexpress.com>
2021-12-27 19:10:50 +0300

    fix lint ts task and npm installation

--- build/gulp/test_typescript.js
+++ build/gulp/test_typescript.js
@@ -5,7 +5,7 @@ const exec = require('child_process').exec;
 
 gulp.task('test-ts',
     (callback) => {
-        exec(`cd testing/typescript && npm i && npm run test-ts`, (e, out, err) => {
+        exec(`cd testing/typescript && npm i --no-audit --no-fund && npm run test-ts`, (e, out, err) => {
             console.log(out, err);
             callback(e);
         });
