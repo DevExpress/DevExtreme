03941da5ffea5b6725db06ff602cec038542ae07
volnyagin <vconst@users.noreply.github.com>
2021-12-28 12:17:05 +0300

    DataGrid: Fix editor value for lookup column in form editing mode if editorType is 'dxTagBox' (T1054830)

--- js/ui/grid_core/ui.grid_core.editing_form_based.js
+++ js/ui/grid_core/ui.grid_core.editing_form_based.js
@@ -267,6 +267,8 @@ export const editingFormBasedModule = {
                     const editorType = getEditorType(item);
                     const rowData = detailCellOptions?.row.data;
                     const form = formTemplateOptions.component;
+                    const { label, labelMark, labelMode } = formTemplateOptions.editorOptions || {};
+
                     const cellOptions = extend({}, detailCellOptions, {
                         data: rowData,
                         cellElement: null,
@@ -275,7 +277,9 @@ export const editingFormBasedModule = {
                         id: form.getItemID(item.name || item.dataField),
                         column: extend({}, column, {
                             editorType: editorType,
-                            editorOptions: extend({}, formTemplateOptions.editorOptions, column.editorOptions, item.editorOptions)
+                            editorOptions: extend({
+                                label, labelMark, labelMode
+                            }, column.editorOptions, item.editorOptions)
                         }),
                         columnIndex: column.index,
                         setValue: !isReadOnly && column.allowEditing && function(value) {
