8465748f0fc27014e658a3b208df997bfc4dc7d7
jacksonrr3 <jacksonrr3@yandex.ru>
2021-12-28 14:30:35 +0300

    add inject property

--- js/core/utils/ajax.d.ts
+++ js/core/utils/ajax.d.ts
@@ -1,5 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-empty-interface, @typescript-eslint/no-unused-vars
-export interface JQueryXHR { }
+export interface JQueryXHR {
+  inject(Object): void ;
+}
 
 declare const ajax: JQueryXHR;
 export default ajax;
--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -1513,7 +1513,9 @@ declare module DevExpress.core {
   /**
    * @deprecated Attention! This type is for internal purposes only. If you used it previously, please describe your scenario in the following GitHub Issue, and we will suggest a public alternative: {@link https://github.com/DevExpress/DevExtreme/issues/17885|Internal Types}.
    */
-  export interface JQueryXHR {}
+  export interface JQueryXHR {
+    inject(Object): void;
+  }
   /**
    * @deprecated Attention! This type is for internal purposes only. If you used it previously, please describe your scenario in the following GitHub Issue, and we will suggest a public alternative: {@link https://github.com/DevExpress/DevExtreme/issues/17885|Internal Types}.
    */
