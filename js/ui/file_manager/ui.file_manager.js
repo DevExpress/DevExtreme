e748aab5d6ab819e7b77abec53a4a1af7873a9e5
Stanislav Klesarev <klesarevklesarevstas@gmail.com>
2021-12-30 17:49:26 +0300

    Revert "Add fix"
    This reverts commit ae74ff0e193302e0059fa651b0eae67faa5c733b.

--- js/ui/file_manager/ui.file_manager.js
+++ js/ui/file_manager/ui.file_manager.js
@@ -557,7 +557,7 @@ class FileManager extends Widget {
                 }
                 const pathKeys = this._lockCurrentPathProcessing ? undefined : this.option('currentPathKeys');
                 this._controller.updateProvider(args.value, pathKeys)
-                    .always(() => this._providerUpdateDeferred.resolve())
+                    .then(() => this._providerUpdateDeferred.resolve())
                     .always(() => this.repaint());
                 break;
             }
