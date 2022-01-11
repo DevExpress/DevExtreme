e9ee6e69ab448300463f430737ce8ef1d5b41d3e
Alexander Bezborodov (DevExpress) <bezborodov.alexander@corp.devexpress.com>
2022-01-11 20:26:49 +0300

    Update tests by guidelines

--- testing/typescript/data/custom_store.ts
+++ testing/typescript/data/custom_store.ts
@@ -1,34 +1,48 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable no-new */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars-experimental */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 // eslint-disable-next-line import/no-extraneous-dependencies
 import $ from 'jquery';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import CustomStore from '../../../js/data/custom_store';
 
-
-export function loadByFetchWithModification(url: string): CustomStore {
-  return new CustomStore({
-    load: () => fetch(url)
-      .then((response) => response.json())
-      .then((data) => ({
-        data: data.data,
-        totalCount: data.totalCount,
-        summary: data.summary,
-        groupCount: data.groupCount,
-      })),
+export async function infersTItemFromComplexLoadResult() {
+  const store = new CustomStore({
+    // eslint-disable-next-line @typescript-eslint/require-await
+    load: async () => ({
+      data: [{ a: 1 }, { a: 2 }, { a: 3 }],
+      totalCount: 10,
+      summary: [10],
+      groupCount: 3,
+    }),
   });
+
+  // If TItem inferred incorrectly, the assignment below will result to compilation error.
+  // However, if TItem inferred as any, it still will be broken,
+  // so we may try adding some logic to avoid it
+  const value: { a: number } = await store.byKey('stub');
 }
 
-export function loadByAjax(url: string, data: string): CustomStore {
-  return new CustomStore({
+export function loadAcceptsAjaxResult() {
+  new CustomStore({
     load: (): JQueryXHR => $.ajax({
-      url, cache: false, dataType: 'json', data,
+      url: 'url', cache: false, dataType: 'json', data: 'data',
     }),
   });
 }
 
-export function loadByHttpClientAngular(http: HttpClient, url: string): CustomStore {
-  return new CustomStore({
-    load: () => http.get(url).toPromise(),
+export function loadAcceptsAngularHttpClient() {
+  const http: HttpClient = new HttpClient({} as HttpHandler);
+  new CustomStore({
+    load: () => http.get('url').toPromise(),
+  });
+}
+
+export function loadAcceptsPromiseOfObject() {
+  new CustomStore({
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    load: () => new Promise<object>(undefined),
   });
 }
