03e81ada05bacb0b664da6e7c8cc281483d816b6
Andrew Makarov <andrewmakarov@inbox.ru>
2022-01-12 13:44:47 +0300

    dxScheduler - mock date for time indicator demo (#1630)
    * Mock date for time indicator demo
    
    * Change date
    
    * Replace etalon
    
    * Remove mask

--- Demos/Scheduler/CurrentTimeIndicator/client-script.js
+++ Demos/Scheduler/CurrentTimeIndicator/client-script.js
@@ -1,15 +1,1 @@
-const getWednesdayThisWeek = () => {
-  const WEDNESDAY_INDEX = 3;
-
-  const date = new Date();
-
-  const currentDay = date.getDay();
-  const distance = WEDNESDAY_INDEX - currentDay;
-  date.setDate(date.getDate() + distance);
-
-  date.setHours(11, 35, 0, 0);
-
-  return date;
-};
-
-MockDate.set(getWednesdayThisWeek());
+MockDate.set(new Date(2020, 6, 15, 16));
