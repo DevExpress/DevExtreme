2212f458acac72f9aa542dd8a2110ba420565e60
Roman Semenov <roman.semenov@devexpress.com>
2022-01-12 16:11:48 +0300

    DataGridLight - add selection (#20711)
    * realization
    
    * test
    
    * fix lint
    
    * fix
    
    * fix tests
    
    * fix
    
    * fix tests
    
    * fix review
    
    * fix
    
    * fix
    
    * fix tests

--- js/renovation/ui/grids/data_grid_light/widgets/data_row.tsx
+++ js/renovation/ui/grids/data_grid_light/widgets/data_row.tsx
@@ -1,42 +1,55 @@
 import {
-  Component, JSXComponent, ComponentBindings, OneWay,
+  Component, JSXComponent, ComponentBindings, OneWay, Effect, InternalState, Consumer,
 } from '@devextreme-generator/declarations';
+import { combineClasses } from '../../../../utils/combine_classes';
+import { createGetter, PluginsContext, Plugins } from '../../../../utils/plugin/context';
+import { Column, RowData } from '../types';
+
+export type DataRowPropertiesGetterType = (data: RowData) => Record<string, unknown>;
+export const DataRowPropertiesGetter = createGetter<DataRowPropertiesGetterType>(() => ({}));
+export type DataRowClassesGetterType = (data: RowData) => Record<string, boolean>;
+export const DataRowClassesGetter = createGetter<DataRowClassesGetterType>(() => ({}));
 
 export const viewFunction = (viewModel: DataRow): JSX.Element => (
   <tr
-    className="dx-row dx-data-row dx-column-lines"
+    className={viewModel.cssClasses}
     role="row"
     aria-selected="false"
-    // TODO uncomment after https://trello.com/c/kVXfSWI7
-    // aria-rowindex={viewModel.props.rowIndex + 1}
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    {...viewModel.additionalParams}
   >
-    {viewModel.cellTexts.map((text, index) => (
-      <td
-        // eslint-disable-next-line react/no-array-index-key
-        key={index}
-        // TODO uncomment after https://trello.com/c/kVXfSWI7
-        // aria-describedby={`dx-col-${index + 1}`}
-        aria-selected="false"
-        role="gridcell"
-        // TODO uncomment after https://trello.com/c/kVXfSWI7
-        // aria-colindex={index + 1}
-      >
-        {text}
-      </td>
-    ))}
+    {viewModel.props.columns.map((column, index) => {
+      const { cellTemplate: CellTemplate, dataField } = column;
+      return (
+        <td
+          // eslint-disable-next-line react/no-array-index-key
+          key={index}
+          // TODO uncomment after https://trello.com/c/kVXfSWI7
+          // aria-describedby={`dx-col-${index + 1}`}
+          aria-selected="false"
+          role="gridcell"
+        >
+          {
+            CellTemplate
+              ? <CellTemplate data={viewModel.props.data} />
+              : dataField && `${viewModel.props.data[dataField]}`
+          }
+        </td>
+      );
+    })}
   </tr>
 );
 
 @ComponentBindings()
 export class DataRowProps {
   @OneWay()
-  data: Record<string, unknown> = {};
+  data: RowData = {};
 
   @OneWay()
   rowIndex = 0;
 
   @OneWay()
-  columns: string[] = [];
+  columns: Column[] = [];
 }
 
 @Component({
@@ -44,8 +57,35 @@ export class DataRowProps {
   view: viewFunction,
 })
 export class DataRow extends JSXComponent(DataRowProps) {
-  get cellTexts(): string[] {
-    const { columns, data } = this.props;
-    return columns.map((dataField) => String(data[dataField]));
+  @Consumer(PluginsContext)
+  plugins = new Plugins();
+
+  get cssClasses(): string {
+    return combineClasses({
+      'dx-row': true,
+      'dx-data-row': true,
+      'dx-column-lines': true,
+      ...this.additionalClasses,
+    });
+  }
+
+  @InternalState()
+  additionalParams: Record<string, unknown> = {};
+
+  @InternalState()
+  additionalClasses: Record<string, boolean> = {};
+
+  @Effect()
+  watchAdditionalParams(): () => void {
+    return this.plugins.watch(DataRowPropertiesGetter, (getter) => {
+      this.additionalParams = getter(this.props.data);
+    });
+  }
+
+  @Effect()
+  watchAdditionalClasses(): () => void {
+    return this.plugins.watch(DataRowClassesGetter, (getter) => {
+      this.additionalClasses = getter(this.props.data);
+    });
   }
 }
