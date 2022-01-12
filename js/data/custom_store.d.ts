9736f496d4937f1eff9200c444cb37cfcc7ad2e3
Alexander Bezborodov <alexander.bezborodov@devexpress.com>
2022-01-12 16:11:48 +0300

    TS - Add ts testing for regression cases (T1052277, T1051918) (#20685)

--- js/data/custom_store.d.ts
+++ js/data/custom_store.d.ts
@@ -13,6 +13,20 @@ export type GroupItem<
     TItem = any,
 > = { key: any | string | number; items: Array<TItem> | Array<GroupItem> | null; count?: number; summary?: Array<any> };
 
+/** @public */
+export type ResolvedData<
+    TItem = any,
+> =
+  | Object
+  | Array<TItem>
+  | Array<GroupItem>
+  | {
+      data: Array<TItem> | Array<GroupItem>;
+      totalCount?: number;
+      summary?: Array<any>;
+      groupCount?: number;
+    };
+
 /**
  * @namespace DevExpress.data
  * @deprecated Use Options instead
@@ -48,15 +62,8 @@ export interface CustomStoreOptions<
      * @public
      */
     load: ((options: LoadOptions<TItem>) =>
-      | DxPromise<
-        | Array<TItem>
-        | Array<GroupItem>
-        | {
-            data: Array<TItem> | Array<GroupItem>;
-            totalCount?: number;
-            summary?: Array<any>;
-            groupCount?: number;
-          }>
+      | DxPromise<ResolvedData<TItem>>
+      | PromiseLike<ResolvedData<TItem>>
       | Array<GroupItem>
       | Array<TItem>);
     /**
