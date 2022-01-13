b4c6d35b6e76da1b06c43c4e9842dfceaad65fde
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2022-01-13 12:14:23 +0300

    try fix

--- testing/testcafe/model/scrollView/scrollView.ts
+++ testing/testcafe/model/scrollView/scrollView.ts
@@ -1,12 +1,12 @@
+import type Scrollable from './internal/scrollable';
 import type { PlatformType } from '../../helpers/multi-platform-test/platform-type';
 import { ScrollableFactory } from './internal/scrollable';
 
 const getScrollView = (
   platform: PlatformType,
-) => class ScrollView extends ScrollableFactory[platform] {
+// eslint-disable-next-line @typescript-eslint/no-extra-parens
+) => class ScrollView extends (ScrollableFactory[platform] as typeof Scrollable) {
   name = 'dxScrollView';
-
-  platform = platform;
 };
 
 export const ScrollViewFactory = {
