bd410b80849d42a9a7f3fd9a84d4d927231bc7f3
Stanislav Klesarev <klesarevklesarevstas@gmail.com>
2022-01-14 12:53:42 +0300

    Fix +1 -1

--- js/ui/file_manager/ui.file_manager.js
+++ js/ui/file_manager/ui.file_manager.js
@@ -47,7 +47,7 @@ class FileManager extends Widget {
 
     _init() {
         super._init();
-        this._providerUpdateDeferred = new Deferred().resolve();
+        this._providerUpdateDeferred = null;
         this._lockCurrentPathProcessing = false;
 
         this._controller = new FileItemsController({
@@ -523,22 +523,28 @@ class FileManager extends Widget {
 
         switch(name) {
             case 'currentPath':
-                this._lockCurrentPathProcessing = true;
-                this.log('# set path option');
-                this._providerUpdateDeferred.then(() => {
-                    this._lockCurrentPathProcessing = false;
-                    this.log('# set path option -> call');
-                    return this._controller.setCurrentPath(args.value);
-                });
+                {
+                    const updateFunc = () => {
+                        this._lockCurrentPathProcessing = false;
+                        this.log('# set path option -> call');
+                        return this._controller.setCurrentPath(args.value);
+                    };
+                    this._lockCurrentPathProcessing = true;
+                    this.log('# set path option');
+                    this._providerUpdateDeferred ? this._providerUpdateDeferred.then(updateFunc) : updateFunc();
+                }
                 break;
             case 'currentPathKeys':
-                this._lockCurrentPathProcessing = true;
-                this.log('# set pathKeys option');
-                this._providerUpdateDeferred.then(() => {
-                    this._lockCurrentPathProcessing = false;
-                    this.log('# set pathKeys option -> call');
-                    this._controller.setCurrentPathByKeys(args.value);
-                });
+                {
+                    const updateFunc = () => {
+                        this._lockCurrentPathProcessing = false;
+                        this.log('# set pathKeys option -> call');
+                        this._controller.setCurrentPathByKeys(args.value);
+                    };
+                    this._lockCurrentPathProcessing = true;
+                    this.log('# set pathKeys option');
+                    this._providerUpdateDeferred ? this._providerUpdateDeferred.then(updateFunc) : updateFunc();
+                }
                 break;
             case 'selectedItemKeys':
                 if(!this._lockSelectionProcessing && this._itemView) {
@@ -561,6 +567,7 @@ class FileManager extends Widget {
                 const pathKeys = this._lockCurrentPathProcessing ? undefined : this.option('currentPathKeys');
                 this._controller.updateProvider(args.value, pathKeys)
                     .then(() => this._providerUpdateDeferred.resolve())
+                    .always(() => this._providerUpdateDeferred = null)
                     .always(() => this.repaint());
                 break;
             }
