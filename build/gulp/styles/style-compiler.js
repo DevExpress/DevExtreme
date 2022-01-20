e3cb91a1f3b636bc08dbe72f346a36eaae3b5391
NickMitrokhin <nikolai.mitrokhin@devexpress.com>
2022-01-20 17:26:41 +0300

    adds building compact themes

--- build/gulp/styles/style-compiler.js
+++ build/gulp/styles/style-compiler.js
@@ -20,9 +20,11 @@ const cssArtifactsPath = join(process.cwd(), 'artifacts', 'css');
 
 const DEFAULT_DEV_BUNDLE_NAMES = [
     'light',
+    'light.compact',
     'dark',
     'contrast',
     'material.blue.light',
+    'material.blue.light.compact',
 ];
 
 const getBundleSourcePath = name => `scss/bundles/dx.${name}.scss`;
