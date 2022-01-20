49eb41972e86c6f0539471f6ae4c1d436118a885
NickMitrokhin <nikolai.mitrokhin@devexpress.com>
2022-01-20 13:06:14 +0300

    fixes page size selector

--- js/renovation/ui/pager/utils/calculate_values_fitted_width.ts
+++ js/renovation/ui/pager/utils/calculate_values_fitted_width.ts
@@ -1,4 +1,9 @@
+import { isMaterial, isCompact, current as currentTheme } from '../../../../ui/themes';
+
 export const oneDigitWidth = 10;
 export function calculateValuesFittedWidth(minWidth: number, values: number[]): number {
-  return minWidth + oneDigitWidth * Math.max(...values).toString().length;
+  const themeName = currentTheme();
+  const padding = isMaterial(themeName) && !isCompact(themeName) ? 22 : 0;
+
+  return minWidth + padding + oneDigitWidth * Math.max(...values).toString().length;
 }
