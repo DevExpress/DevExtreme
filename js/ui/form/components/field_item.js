4803b2883b23fc25392785e756cba1f82cccd8ac
groshenkovamarina <marina.groshenkova@devexpress.com>
2022-01-20 22:05:09 +0300

    Editors Label - Fix editor doesn't provide accessibility information about it's label (Form)

--- js/ui/form/components/field_item.js
+++ js/ui/form/components/field_item.js
@@ -121,7 +121,7 @@ export function renderFieldItem({
         try {
             widgetInstance = createComponentCallback($div, item.editorType, editorOptions);
             widgetInstance.setAria('describedby', helpID);
-            widgetInstance.setAria('labelledby', labelID);
+            if(labelID) widgetInstance.setAria('labelledby', labelID);
             widgetInstance.setAria('required', isRequired);
         } catch(e) {
             errors.log('E1035', e.message);
