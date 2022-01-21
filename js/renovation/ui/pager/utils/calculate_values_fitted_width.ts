f2d8769b6d9098d07c67904c88b16bc682137d98
NickMitrokhin <nikolai.mitrokhin@devexpress.com>
2022-01-21 18:16:36 +0300

    refix

--- js/renovation/ui/pager/utils/calculate_values_fitted_width.ts
+++ js/renovation/ui/pager/utils/calculate_values_fitted_width.ts
@@ -1,9 +1,4 @@
-import { isMaterial, isCompact, current as currentTheme } from '../../../../ui/themes';
-
 export const oneDigitWidth = 10;
 export function calculateValuesFittedWidth(minWidth: number, values: number[]): number {
-  const themeName = currentTheme();
-  const padding = isMaterial(themeName) && !isCompact(themeName) ? 22 : 0;
-
-  return minWidth + padding + oneDigitWidth * Math.max(...values).toString().length;
+  return minWidth + oneDigitWidth * Math.max(...values).toString().length;
 }
