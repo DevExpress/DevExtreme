61a213d03399012c5277d245cde359c5ab8c577f
Dmitry Levkovskiy <DokaRus@users.noreply.github.com>
2022-01-21 19:46:37 +0300

    HtmlEditor: fix sync of active formatting in the toolbar (T922236) (#20872)

--- js/ui/html_editor/modules/toolbar.js
+++ js/ui/html_editor/modules/toolbar.js
@@ -38,6 +38,7 @@ if(Quill) {
     const SELECTION_CHANGE_EVENT = 'selection-change';
 
     const USER_ACTION = 'user';
+    const SILENT_ACTION = 'silent';
 
     const FORMAT_HOTKEYS = {
         66: 'bold',
@@ -70,17 +71,20 @@ if(Quill) {
 
             this._toolbarWidgets = new WidgetCollector();
             this._formatHandlers = getFormatHandlers(this);
-            // this._formatHandlers = new FormatHelper(this);
             this._tableFormats = getTableFormats(quill);
 
             if(isDefined(options.items)) {
                 this._addCallbacks();
                 this._renderToolbar();
 
-                this.quill.on('editor-change', (eventName) => {
-                    const isSelectionChanged = eventName === SELECTION_CHANGE_EVENT;
+                this.quill.on('editor-change', (eventName, newValue, oldValue, eventSource) => {
+                    const isSilentMode = eventSource === SILENT_ACTION && isEmptyObject(this.quill.getFormat());
 
-                    this._updateToolbar(isSelectionChanged);
+                    if(!isSilentMode) {
+                        const isSelectionChanged = eventName === SELECTION_CHANGE_EVENT;
+
+                        this._updateToolbar(isSelectionChanged);
+                    }
                 });
             }
         }
