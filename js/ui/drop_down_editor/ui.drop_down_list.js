6a1d4ec8f29d81db9d3a2ba3240dfdc69d3c4125
Zedwag <zedwag@gmail.com>
2022-01-21 10:08:43 +0300

    Implement suggestions

--- js/ui/drop_down_editor/ui.drop_down_list.js
+++ js/ui/drop_down_editor/ui.drop_down_list.js
@@ -485,19 +485,15 @@ const DropDownList = DropDownEditor.inherit({
         this._setAriaTargetForList();
         this._list.option('_listAttributes', { 'role': 'combobox' });
 
-        this._renderPreventBlur(this._$list);
+        this._renderPreventBlurOnListClick();
         this._setListFocusedElementOptionChange();
     },
 
-    _renderPreventBlur: function($target, exclusionElements = []) {
+    _renderPreventBlurOnListClick: function() {
         const eventName = addNamespace('mousedown', 'dxDropDownList');
 
-        eventsEngine.off($target, eventName);
-        eventsEngine.on($target, eventName, function(e) {
-            if(!exclusionElements.includes(e.target)) {
-                e.preventDefault();
-            }
-        }.bind(this));
+        eventsEngine.off(this._$list, eventName);
+        eventsEngine.on(this._$list, eventName, (e) => e.preventDefault());
     },
 
     _renderOpenedState: function() {
