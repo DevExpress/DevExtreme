0b2e15589563aa90f15cddae01ead23fda723b24
Roman Semenov <roman.semenov@devexpress.com>
2022-01-25 13:55:50 +0300

    fix totalCount
    (cherry picked from commit 87e187199ea366c2caa16d966bfca797c82529a0)

--- js/ui/grid_core/ui.grid_core.data_controller.js
+++ js/ui/grid_core/ui.grid_core.data_controller.js
@@ -253,7 +253,9 @@ export const dataControllerModule = {
                     storeLoadOptions.filter = this.combinedFilter(storeLoadOptions.filter);
 
                     if(storeLoadOptions.filter?.length === 1 && storeLoadOptions.filter[0] === '!') {
-                        e.data = { data: [], totalCount: 0 };
+                        e.data = [];
+                        e.extra = e.extra || {};
+                        e.extra.totalCount = 0;
                     }
 
                     if(!columnsController.isDataSourceApplied()) {
