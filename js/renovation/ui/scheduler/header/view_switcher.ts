72cc459738a93c4685a5390f4b9e13d4157cc123
DendyGrobovshik <idenis.gradoboev@yandex.com>
2022-01-26 12:54:31 +0300

    Fix this issue in renovated code

--- js/renovation/ui/scheduler/header/view_switcher.ts
+++ js/renovation/ui/scheduler/header/view_switcher.ts
@@ -32,29 +32,40 @@ export const getDropDownViewSwitcher = (
   selectedView: string,
   views: ItemView[],
   setCurrentView: (view: ItemView) => void,
-): ToolbarItem => ({
-  widget: 'dxDropDownButton',
-  locateInMenu: 'never',
-  cssClass: VIEW_SWITCHER_CLASS,
-  options: {
-    items: views,
-    useSelectMode: true,
-    keyExpr: 'name',
-    selectedItemKey: selectedView,
-    displayExpr: 'text',
-    elementAttr: {
-      class: VIEW_SWITCHER_DROP_DOWN_BUTTON_CLASS,
-    },
-    onItemClick: (e) => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      setCurrentView(e.itemData);
-    },
-    dropDownOptions: {
-      width: 'max-content',
-      wrapperAttr: {
-        class: VIEW_SWITCHER_DROP_DOWN_BUTTON_CONTENT_CLASS,
+): ToolbarItem => {
+  const isOneView = views.length === 1 && views[0].name === selectedView;
+
+  return {
+    widget: 'dxDropDownButton',
+    locateInMenu: 'never',
+    cssClass: VIEW_SWITCHER_CLASS,
+    options: {
+      items: views,
+      useSelectMode: true,
+      keyExpr: 'name',
+      selectedItemKey: selectedView,
+      displayExpr: 'text',
+      showArrowIcon: !isOneView,
+      elementAttr: {
+        class: VIEW_SWITCHER_DROP_DOWN_BUTTON_CLASS,
+      },
+      onItemClick: (e) => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        setCurrentView(e.itemData);
+      },
+      dropDownOptions: {
+        onShowing: (e) => {
+          if (isOneView) {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+            e.cancel = true;
+          }
+        },
+        width: 'max-content',
+        wrapperAttr: {
+          class: VIEW_SWITCHER_DROP_DOWN_BUTTON_CONTENT_CLASS,
+        },
       },
     },
-  },
-  ...item,
-} as ToolbarItem);
+    ...item,
+  } as ToolbarItem;
+};
