32a6777763ffa9975411e9ed262ef16e8c0a34b4
Alexey Babich <alexey.babych@gmail.com>
2022-01-26 18:27:58 +0300

    Fix list dependencies for dxList (T1061555) (#20956) (#20966)
    * Fix list dependencies for dxList
    
    * Add test

--- themebuilder-scss/tests/metadata/dependency-collector.test.ts
+++ themebuilder-scss/tests/metadata/dependency-collector.test.ts
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import cabinet from 'filing-cabinet';
+import { dependencies as idealDependencies } from '../data/dependencies';
 import DependencyCollector, { filePathMap } from '../../src/metadata/dependency-collector';
+import { dependencies as builtDependencies } from '../../src/data/metadata/dx-theme-builder-metadata';
 
 const simpleDependencies: ScriptsDependencyTree = {
   dependencies: {
@@ -231,5 +233,11 @@ describe('validation', () => {
   test('validate - index file has less widgets than in dependencies', () => {
     dependencyCollector.themes = ['lesstheme'];
     expect(() => dependencyCollector.validate()).toThrow('Some public widgets (lesstheme) has no // STYLE comment in source code or private widget has one');
+  });
+});
+
+describe('Integration test', () => {
+  test('Check if dependensies is good', () => {
+    expect(builtDependencies).toEqual(idealDependencies);
   });
 });
