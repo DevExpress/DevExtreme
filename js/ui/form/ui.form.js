3dcdf5caf1c730b0ea460dad437c5bfce88e40a1
Ignatov Dan <ignatovdan@gmail.com>
2022-02-04 11:22:51 +0300

    Manually reconnect DxValidationSummary to editors (T1036547) (#21031)

--- js/ui/form/ui.form.js
+++ js/ui/form/ui.form.js
@@ -578,6 +578,9 @@ const Form = Widget.inherit({
                 const nestedItemsRunTimeInfo = component.getItemsRunTimeInfo();
                 this._itemsRunTimeInfo.removeItemsByItems(nestedItemsRunTimeInfo);
             },
+            onFieldItemRendered: () => {
+                this._validationSummary?._initGroupRegistration();
+            }
         });
     },
 
@@ -792,7 +795,7 @@ const Form = Widget.inherit({
         if(ITEM_OPTIONS_FOR_VALIDATION_UPDATING.indexOf(optionName) > -1) {
             ValidationEngine.addGroup(this._getValidationGroup());
             if(this.option('showValidationSummary')) {
-                this._validationSummary && this._validationSummary._initGroupRegistration();
+                this._validationSummary?._initGroupRegistration();
             }
         }
     },
