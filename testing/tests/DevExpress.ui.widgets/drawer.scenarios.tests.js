e892e3c2b835eed7b0c4e94d00d08173aa1a7b28
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2022-02-16 15:53:07 +0300

    drawer: fixing freezes test task (#21129)

--- testing/tests/DevExpress.ui.widgets/drawer.scenarios.tests.js
+++ testing/tests/DevExpress.ui.widgets/drawer.scenarios.tests.js
@@ -15,11 +15,29 @@ import 'ui/tree_view';
 import 'generic_light.css!';
 
 import dxDrawer from 'ui/drawer';
+import fx from 'animation/fx';
 
-QUnit.testStart(() => {
-    $('#qunit-fixture').html(drawerTesters.markup);
-    // $('#qunit-tests').prepend(drawerTesters.markup);
-});
+const moduleConfig = {
+    before: function() {
+        this.$fixture = $('#qunit-fixture');
+    },
+    beforeEach: function() {
+        this.$element = $(drawerTesters.markup);
+        this.$fixture.html(this.$element);
+        this.clock = sinon.useFakeTimers();
+        clearStack();
+        fx.off = true;
+    },
+    afterEach: function() {
+        fx.off = false;
+        this.$element.dxDrawer('instance').dispose();
+        this.$element.remove();
+        delete this.$element;
+        this.clock.restore();
+        this.clock = undefined;
+        clearStack();
+    }
+};
 
 const openedStateModes = ['shrink', 'push', 'overlap'];
 const configs = [];
@@ -35,20 +53,10 @@ openedStateModes.forEach(openedStateMode => {
     });
 });
 
-QUnit.module('zIndex conflicts', {
-    beforeEach() {
-        this.clock = sinon.useFakeTimers();
-        clearStack();
-    },
-    afterEach() {
-        this.clock.restore();
-        this.clock = undefined;
-        clearStack();
-    }
-}, () => {
+QUnit.module('zIndex conflicts', moduleConfig, () => {
     openedStateModes.forEach(openedStateMode => {
-        function checkShaderZIndex(assert) {
-            const $drawer = $('#' + drawerTesters.drawerElementId).dxDrawer({
+        function checkShaderZIndex($drawer) {
+            $drawer.dxDrawer({
                 shading: true, opened: true, width: 600, height: 600, position: 'left',
                 openedStateMode: openedStateMode,
                 template: function() {
@@ -59,7 +67,7 @@ QUnit.module('zIndex conflicts', {
             const $shader = $drawer.find('.dx-drawer-shader');
             const shaderZIndex = window.getComputedStyle($shader[0]).zIndex;
             if(!isNumeric(shaderZIndex)) {
-                assert.ok(false, `test is designed for shader ZIndex numeric value but '${shaderZIndex}' was found. Redesign this test for another approach.`);
+                QUnit.assert.ok(false, `test is designed for shader ZIndex numeric value but '${shaderZIndex}' was found. Redesign this test for another approach.`);
             }
 
             let recursionLevel = 0;
@@ -70,34 +78,37 @@ QUnit.module('zIndex conflicts', {
                     return;
                 }
                 if(isNumeric(currentElementStyle.zIndex) && Number(currentElementStyle.zIndex) > Number(shaderZIndex)) {
-                    assert.ok(false, `shader has '${shaderZIndex}' z-index but there z-index is greater: ${$element.prop('tagName')}(z-Index: ${currentElementStyle.zIndex}, id:${$element.attr('id')})`);
+                    QUnit.assert.ok(false, `shader has '${shaderZIndex}' z-index but there z-index is greater: ${$element.prop('tagName')}(z-Index: ${currentElementStyle.zIndex}, id:${$element.attr('id')})`);
                 }
                 recursionLevel++;
                 $element.children().each((_, child) => recursiveCheckZIndex($(child)));
                 recursionLevel--;
             };
-            recursiveCheckZIndex($($drawer.find('#view')));
-            assert.ok(true, 'one assert to fit the "at least one assertion" rule');
+            recursiveCheckZIndex($drawer.find('#view'));
+            QUnit.assert.ok(true, 'one assert to fit the "at least one assertion" rule');
         }
 
         QUnit.test(`(${openedStateMode}) ColorBox_inner`, function(assert) {
-            $('<div>').appendTo('#view').dxColorBox({
+            const $view = this.$element.find('#view');
+            $('<div>').appendTo($view).dxColorBox({
                 value: '#f05b41'
             });
 
-            checkShaderZIndex(assert);
+            checkShaderZIndex(this.$element);
         });
 
         QUnit.test(`(${openedStateMode}) DataGrid_inner`, function(assert) {
-            $('<div>').appendTo('#view').dxDataGrid({
+            const $view = this.$element.find('#view');
+            $('<div>').appendTo($view).dxDataGrid({
                 editing: { mode: 'row', allowUpdating: true }, dataSource: [{ date: new Date(2010, 10, 10), str: 'qwe' }]
             });
 
-            checkShaderZIndex(assert);
+            checkShaderZIndex(this.$element);
         });
 
         QUnit.test(`(${openedStateMode}) FileManager_inner`, function(assert) {
-            $('<div>').appendTo('#view').dxFileManager({
+            const $view = this.$element.find('#view');
+            $('<div>').appendTo($view).dxFileManager({
                 currentPath: 'Documents/Projects',
                 fileSystemProvider: [
                     {
@@ -116,44 +127,49 @@ QUnit.module('zIndex conflicts', {
             });
 
             this.clock.tick(400);
-            checkShaderZIndex(assert);
+            checkShaderZIndex(this.$element);
         });
 
         QUnit.test(`(${openedStateMode}) Menu_inner`, function(assert) {
-            $('<div>').appendTo('#view').dxMenu({
+            const $view = this.$element.find('#view');
+            $('<div>').appendTo($view).dxMenu({
                 dataSource: [{ text: 'item1', items: [{ text: 'item1/item1' }, { text: 'item1/item2' }] }]
             });
 
-            checkShaderZIndex(assert);
+            checkShaderZIndex(this.$element);
         });
 
         QUnit.test(`(${openedStateMode}) SelectBox_inner`, function(assert) {
-            $('<div>').appendTo('#view').dxSelectBox({
+            const $view = this.$element.find('#view');
+            $('<div>').appendTo($view).dxSelectBox({
                 dataSource: ['item1', 'item2']
             });
 
-            checkShaderZIndex(assert);
+            checkShaderZIndex(this.$element);
         });
 
         QUnit.test(`(${openedStateMode}) TabPanel_inner`, function(assert) {
-            $('<div>').appendTo('#view').dxTabPanel({
+            const $view = this.$element.find('#view');
+            $('<div>').appendTo($view).dxTabPanel({
                 selectedIndex: 1,
                 items: [{ title: 'Tab1', text: 'This is Tab1' }, { title: 'Tab2', text: 'This is Tab2' }, { title: 'Tab3', text: 'This is Tab3' }]
             });
 
-            checkShaderZIndex(assert);
+            checkShaderZIndex(this.$element);
         });
 
         QUnit.test(`(${openedStateMode}) TextBox_inner`, function(assert) {
-            $('<div>').appendTo('#view').dxTextBox({
+            const $view = this.$element.find('#view');
+            $('<div>').appendTo($view).dxTextBox({
                 value: 'value'
             });
 
-            checkShaderZIndex(assert);
+            checkShaderZIndex(this.$element);
         });
 
         QUnit.test(`(${openedStateMode}) TreeView_inner`, function(assert) {
-            $('<div>').appendTo('#view').dxTreeView({
+            const $view = this.$element.find('#view');
+            $('<div>').appendTo($view).dxTreeView({
                 showCheckBoxesMode: 'normal',
                 items: [{
                     id: '1', text: 'item1', expanded: true,
@@ -166,23 +182,13 @@ QUnit.module('zIndex conflicts', {
                 }]
             });
 
-            checkShaderZIndex(assert);
+            checkShaderZIndex(this.$element);
         });
     });
 });
 
 configs.forEach(config => {
-    QUnit.module(`Scenarios (${config.openedStateMode}, ${config.position}, ${config.revealMode}, shading: ${config.shading}, minSize: ${config.minSize})`, {
-        beforeEach() {
-            this.clock = sinon.useFakeTimers();
-            clearStack();
-        },
-        afterEach() {
-            this.clock.restore();
-            this.clock = undefined;
-            clearStack();
-        }
-    }, () => {
+    QUnit.module(`Scenarios (${config.openedStateMode}, ${config.position}, ${config.revealMode}, shading: ${config.shading}, minSize: ${config.minSize})`, moduleConfig, () => {
 
         function configIs(openedStateMode, position, revealMode) {
             const isPosition = Array.isArray(position) ? (position.indexOf(config.position) >= 0) : (config.position === position || !position);
@@ -192,7 +198,8 @@ configs.forEach(config => {
 
         function testOrSkip(name, skip, callback) {
             if(skip()) {
-                QUnit.skip(name + ' - NOT SUPPORTED', function() {});
+                // eslint-disable-next-line qunit/no-commented-tests
+                // QUnit.skip(name + ' - NOT SUPPORTED', function() {});
             } else {
                 QUnit.test(name, callback);
             }
@@ -201,7 +208,8 @@ configs.forEach(config => {
         function testOverlap(name, skip, callback) {
             if(config.openedStateMode === 'overlap') {
                 if(skip()) {
-                    QUnit.skip(name + ' - NOT SUPPORTED', function() {});
+                    // eslint-disable-next-line qunit/no-commented-tests
+                    // QUnit.skip(name + ' - NOT SUPPORTED', function() {});
                 } else {
                     QUnit.test(name, callback);
                 }
@@ -522,7 +530,6 @@ configs.forEach(config => {
 
             drawerTesters[config.position].checkOpened(assert, drawer, drawerElement);
         });
-
     });
 });
 
@@ -543,17 +550,7 @@ changeOpenedStateModeConfigs.forEach(config => {
             config, targetOptions);
     }
 
-    QUnit.module(`Change openedStateMode (position: ${config.position}, revealMode: ${config.revealMode}, minSize: ${config.minSize})`, {
-        beforeEach() {
-            this.clock = sinon.useFakeTimers();
-            clearStack();
-        },
-        afterEach() {
-            this.clock.restore();
-            this.clock = undefined;
-            clearStack();
-        }
-    }, () => {
+    QUnit.module(`Change openedStateMode (position: ${config.position}, revealMode: ${config.revealMode}, minSize: ${config.minSize})`, moduleConfig, () => {
         QUnit.test('opened: false, push -> shrink', function(assert) {
             const drawerElement = document.getElementById(drawerTesters.drawerElementId);
             const drawer = new dxDrawer(drawerElement, getFullDrawerOptions({
@@ -714,17 +711,7 @@ changeOpenedStateModeConfigs.forEach(config => {
     });
 });
 
-QUnit.module('Scenarios', {
-    beforeEach() {
-        this.clock = sinon.useFakeTimers();
-        clearStack();
-    },
-    afterEach() {
-        this.clock.restore();
-        this.clock = undefined;
-        clearStack();
-    }
-}, () => {
+QUnit.module('Scenarios', moduleConfig, () => {
     QUnit.test('push, left, opened: false, hidden child, AngularJS, T956751', function(assert) {
         const drawerElement = document.getElementById(drawerTesters.drawerElementId);
 
