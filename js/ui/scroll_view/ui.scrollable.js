8cdc7c4044325b6ed0c9bb64c780c611d9e97e25
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2022-02-17 12:01:20 +0300

    fix optionChanged tests
    (cherry picked from commit 3cb57c96a3694eee3cb73abc17e754876eb13a56)

--- js/ui/scroll_view/ui.scrollable.js
+++ js/ui/scroll_view/ui.scrollable.js
@@ -259,6 +259,7 @@ const Scrollable = DOMComponent.inherit({
                 this._strategy && this._strategy.disabledChanged();
                 break;
             case 'updateManually':
+            case 'scrollByContent':
                 break;
             case 'width':
                 this.callBase(args);
