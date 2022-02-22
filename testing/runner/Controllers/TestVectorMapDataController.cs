f6ff95cf71cac0b4a6feba39f8e47d53e9eedea8
Alexander Bezborodov (DevExpress) <bezborodov.alexander@corp.devexpress.com>
2022-02-22 14:02:50 +0300

    TestRunner - Update .Net version to 3.1

--- testing/runner/Controllers/TestVectorMapDataController.cs
+++ testing/runner/Controllers/TestVectorMapDataController.cs
@@ -42,9 +42,9 @@ namespace Runner.Controllers
             }
         }
 
-        IHostingEnvironment _env;
+        IWebHostEnvironment _env;
 
-        public TestVectorMapDataController(IHostingEnvironment env)
+        public TestVectorMapDataController(IWebHostEnvironment env)
         {
             _env = env;
             ConsoleHelper.Logger.SetWorkingFolder(env.ContentRootPath);
