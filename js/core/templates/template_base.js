975fe909ab1b38ff3a2ab469f3a93bb9ed51d913
ivanblinov2k17 <ivanblinov2015@mail.ru>
2022-02-24 18:05:56 +0300

    add renovation option

--- js/core/templates/template_base.js
+++ js/core/templates/template_base.js
@@ -15,7 +15,7 @@ export class TemplateBase {
         delete options.onRendered;
 
         let $result;
-        if(options.transclude && this._element) {
+        if(options.transclude && this._element && options.renovated) {
             $result = $('<div>').append(this._element).contents();
         } else {
             $result = this._renderCore(options);
