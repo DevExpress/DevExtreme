028a3547a8d9749facadcb89dbd1a13cdd9e948a
Igggr <igor.smirnov@devexpress.com>
2022-03-04 10:45:37 +0300

    Use only RecursivePropertyType

--- testing/helpers/type_helpers/type_helpers.ts
+++ testing/helpers/type_helpers/type_helpers.ts
@@ -1,20 +1,29 @@
 /* eslint-disable @typescript-eslint/no-type-alias */
-export type PropertyType<T, TProp extends PropertyKey> =
-  T extends (infer T2)[]
-    ? PropertyType<T2, TProp>
-    : T extends Partial<Record<TProp, infer TValue>>
-      ? TValue
-      : never;
-
-export type DeepPropertyType<T, TProps extends PropertyKey[]> =
+export type RecursivePropertyType<T, TProps extends string[]> =
   TProps extends [infer TProp]
-    ? PropertyType<T, TProp & PropertyKey>
-    : (TProps extends [infer TProp, ... infer Rest]
-      ? (Rest extends PropertyKey[]
-        ? DeepPropertyType<PropertyType<T, TProp & PropertyKey>, Rest>
+    ? (
+      T extends (infer T2)[]
+        ? RecursivePropertyType<T2, TProps>
+        : (
+          TProp extends string
+            ? (
+              T extends Partial<Record<TProp, infer TValue>>
+                ? TValue
+                : never
+            )
+            : never
+        )
+    )
+    : (
+      TProps extends [infer TProp, ... infer Rest]
+        ? (
+          TProp extends string
+            ? Rest extends string[]
+              ? RecursivePropertyType<RecursivePropertyType<T, [TProp]>, Rest>
+              : never
+            : never
+        )
         : never
-      )
-      : never
     );
 
 export type Equals<X, Y> =
