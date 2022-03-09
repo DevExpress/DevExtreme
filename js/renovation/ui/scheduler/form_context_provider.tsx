454f5e2e09b06f209df564ccd6b65529835668c7
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2022-03-09 19:04:57 +0300

    Appointment edit form - Add form cotext

---
+++ js/renovation/ui/scheduler/form_context_provider.tsx
@@ -0,0 +1,34 @@
+import {
+  ComponentBindings,
+  JSXComponent,
+  OneWay,
+  Slot,
+  Provider,
+  Component,
+} from '@devextreme-generator/declarations';
+import {
+  FormContext,
+  IFormContext,
+} from './form_context';
+
+export const viewFunction = (
+  viewModel: FormContextProvider,
+): JSX.Element => viewModel.props.children;
+
+@ComponentBindings()
+export class FormContextProviderProps {
+  @OneWay() formContextValue!: IFormContext;
+
+  @Slot() children!: JSX.Element;
+}
+
+@Component({
+  defaultOptionRules: null,
+  view: viewFunction,
+})
+export class FormContextProvider extends JSXComponent<FormContextProviderProps, 'formContextValue' | 'children'>() {
+  @Provider(FormContext)
+  get formContextValue(): IFormContext {
+    return this.props.formContextValue;
+  }
+}
