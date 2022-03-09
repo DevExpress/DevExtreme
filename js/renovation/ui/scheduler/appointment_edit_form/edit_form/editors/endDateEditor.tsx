57d7f9ebfae37d72723ee95514dd8482b832fd9e
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2022-03-09 19:02:13 +0300

    Appointment edit form - Editors

---
+++ js/renovation/ui/scheduler/appointment_edit_form/edit_form/editors/endDateEditor.tsx
@@ -0,0 +1,59 @@
+import {
+  Component,
+  ComponentBindings,
+  JSXComponent,
+  OneWay,
+} from '@devextreme-generator/declarations';
+import { FormDate, normalizeNewEndDate } from '../utils/normalizeDate';
+import { DateEditor } from './dateEditor';
+
+export const viewFunction = ({
+  props: {
+    value,
+    disabled,
+    firstDayOfWeek,
+    isAllDay,
+  },
+  valueChange,
+}: EndDateEditor): JSX.Element => (
+  <DateEditor
+    value={value}
+    valueChange={valueChange}
+    firstDayOfWeek={firstDayOfWeek}
+    disabled={disabled}
+    isAllDay={isAllDay}
+  />
+);
+
+@ComponentBindings()
+export class EndDateEditorProps {
+  @OneWay() value!: Date;
+
+  @OneWay() firstDayOfWeek!: number | undefined;
+
+  @OneWay() isAllDay!: boolean;
+
+  @OneWay() disabled?: boolean;
+
+  @OneWay() startDate: FormDate;
+
+  @OneWay() endDate: FormDate;
+
+  @OneWay() dateChange!: (date: Date) => void;
+}
+
+@Component({ view: viewFunction })
+export class EndDateEditor extends JSXComponent<
+EndDateEditorProps, 'value' | 'firstDayOfWeek' | 'isAllDay' | 'dateChange'>() {
+  valueChange(newDate: Date): Date {
+    const result = normalizeNewEndDate(
+      newDate,
+      this.props.startDate,
+      this.props.endDate,
+    );
+
+    this.props.dateChange(newDate);
+
+    return result;
+  }
+}
