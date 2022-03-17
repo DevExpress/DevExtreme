e6907cc41fcc9c072916c08a0a65e531aef639bd
Zedwag <zedwag@gmail.com>
2022-03-17 19:12:56 +0400

    Popover should hide on position.of parents scroll (T1075342)

--- js/ui/popover/ui.popover.js
+++ js/ui/popover/ui.popover.js
@@ -373,6 +373,10 @@ const Popover = Popup.inherit({
         return this._positionController._getContainerPosition();
     },
 
+    _hideOnParentScrollTarget: function() {
+        return $(this._positionController._position.of || $());
+    },
+
     _getSideByLocation: function(location) {
         const isFlippedByVertical = location.v.flip;
         const isFlippedByHorizontal = location.h.flip;
