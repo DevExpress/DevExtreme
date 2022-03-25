37d5e4fff1db7f29fd68a4d4be296432c01a818a
ekiyashko <johnnykiyashko@gmail.com>
2022-03-25 10:02:01 +0400

    pdf export: add warning about using old exportDataGrid method

--- js/exporter/jspdf/export_data_grid.js
+++ js/exporter/jspdf/export_data_grid.js
@@ -1,5 +1,8 @@
 import { isDefined, isObject } from '../../core/utils/type';
+import errors from '../../core/errors';
 import { Export } from './current/export';
+
+const HOW_TO_MIGRATE_ARTICLE = 'https://supportcenter.devexpress.com/ticket/details/t1077554';
 
 function _getFullOptions(options) {
     if(!(isDefined(options) && isObject(options))) {
@@ -10,6 +13,9 @@ function _getFullOptions(options) {
     }
     if(!(isDefined(options.jsPDFDocument) && isObject(options.jsPDFDocument))) {
         throw Error('The "jsPDFDocument" field must contain a jsPDF instance.');
+    }
+    if(isDefined(options.autoTableOptions)) {
+        errors.log('W0001', 'Export', 'autoTableOptions', '22.1', `You can migrate from exporting to PDF with the AutoTable plugin to a new export system. See the following topic for more information: ${HOW_TO_MIGRATE_ARTICLE}`);
     }
     return Export.getFullOptions(options);
 }
