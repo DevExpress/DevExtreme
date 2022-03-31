116630ad6bcfda9f6b31a1ff75445d69e0416176
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2022-03-31 09:53:02 +0400

    Revert "skip tests"
    This reverts commit 583ea05a2d5d20678e61a7db4e3a3645fa97aa2d.

--- testing/testcafe/tests/navigation/scrollView/scrollView.ts
+++ testing/testcafe/tests/navigation/scrollView/scrollView.ts
@@ -1,47 +1,46 @@
-/* eslint-disable max-len */
-// import { Selector } from 'testcafe';
-// import url from '../../../helpers/getPageUrl';
-// import createWidget from '../../../helpers/createWidget';
-// import ScrollView from '../../../model/scrollView/scrollView';
-// import { appendElementTo } from '../helpers/domUtils';
-// import { ScrollableDirection } from '../../../../../js/renovation/ui/scroll_view/common/types';
+import { Selector } from 'testcafe';
+import url from '../../../helpers/getPageUrl';
+import createWidget from '../../../helpers/createWidget';
+import ScrollView from '../../../model/scrollView/scrollView';
+import { appendElementTo } from '../helpers/domUtils';
+import { ScrollableDirection } from '../../../../../js/renovation/ui/scroll_view/common/types';
 
-// fixture`ScrollView`
-//   .page(url(__dirname, '../../container.html'));
+fixture`ScrollView`
+  .page(url(__dirname, '../../container.html'));
 
-// // T817096
-// [150, 300].forEach((scrollableContentSize) => {
-//   (['vertical', 'horizontal'] as ScrollableDirection[]).forEach((direction) => {
-//     ['onHover', 'always', 'onScroll', 'never'].forEach((showScrollbar) => {
-//       const scrollableContainerSize = 200;
-//       const scrollBarVisibleAfterMouseEnter = (showScrollbar === 'always' || showScrollbar === 'onHover') && scrollableContentSize > scrollableContainerSize;
-//       const scrollBarVisibleAfterMouseLeave = showScrollbar === 'always' && scrollableContentSize > scrollableContainerSize;
+// T817096
+[150, 300].forEach((scrollableContentSize) => {
+  (['vertical', 'horizontal'] as ScrollableDirection[]).forEach((direction) => {
+    ['onHover', 'always', 'onScroll', 'never'].forEach((showScrollbar) => {
+      const scrollableContainerSize = 200;
+      const scrollBarVisibleAfterMouseEnter = (showScrollbar === 'always' || showScrollbar === 'onHover') && scrollableContentSize > scrollableContainerSize;
+      const scrollBarVisibleAfterMouseLeave = showScrollbar === 'always' && scrollableContentSize > scrollableContainerSize;
 
-//       test(`Scroll visibility on mouseEnter/mouseLeave, showScrollbar: '${showScrollbar}', direction: '${direction}', content ${scrollableContentSize < scrollableContainerSize ? 'less' : 'more'} than container`, async (t) => {
-//         const scrollView = new ScrollView('#scrollView', { direction });
+      test(`Scroll visibility on mouseEnter/mouseLeave, showScrollbar: '${showScrollbar}', direction: '${direction}', content ${scrollableContentSize < scrollableContainerSize ? 'less' : 'more'} than container`, async (t) => {
+        const scrollView = new ScrollView('#scrollView', { direction });
 
-//         await appendElementTo('body', 'button', 'mouseLeaveButton', {
-//           width: '150px', height: '50px', backgroundColor: 'grey',
-//         });
+        await appendElementTo('body', 'button', 'mouseLeaveButton', {
+          width: '150px', height: '50px', backgroundColor: 'grey',
+        });
 
-//         await t.expect(scrollView.scrollbar.isScrollVisible()).eql(scrollBarVisibleAfterMouseLeave);
-//         await t.hover(scrollView.getContainer());
-//         await t.expect(scrollView.scrollbar.isScrollVisible()).eql(scrollBarVisibleAfterMouseEnter);
-//         await t.click(Selector('#mouseLeaveButton'));
-//         await t.expect(scrollView.scrollbar.isScrollVisible()).eql(scrollBarVisibleAfterMouseLeave);
-//       }).before(async () => {
-//         await appendElementTo('#container', 'div', 'innerScrollViewContent', {
-//           width: `${scrollableContentSize}px`, height: `${scrollableContentSize}px`, backgroundColor: 'steelblue',
-//         });
+        await t.expect(scrollView.scrollbar.isScrollVisible()).eql(scrollBarVisibleAfterMouseLeave);
+        await t.hover(scrollView.getContainer());
+        await t.expect(scrollView.scrollbar.isScrollVisible()).eql(scrollBarVisibleAfterMouseEnter);
+        await t.click(Selector('#mouseLeaveButton'));
+        await t.expect(scrollView.scrollbar.isScrollVisible()).eql(scrollBarVisibleAfterMouseLeave);
+      }).before(async () => {
+        await appendElementTo('#container', 'div', 'innerScrollViewContent', {
+          width: `${scrollableContentSize}px`, height: `${scrollableContentSize}px`, backgroundColor: 'steelblue',
+        });
 
-//         return createWidget('dxScrollView', {
-//           width: scrollableContainerSize,
-//           height: scrollableContainerSize,
-//           useNative: false,
-//           direction,
-//           showScrollbar,
-//         });
-//       });
-//     });
-//   });
-// });
+        return createWidget('dxScrollView', {
+          width: scrollableContainerSize,
+          height: scrollableContainerSize,
+          useNative: false,
+          direction,
+          showScrollbar,
+        });
+      });
+    });
+  });
+});
