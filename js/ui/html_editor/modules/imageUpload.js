c9b28b5ee33c5b6583047515142f19cd70e6cbff
CORP\kotov.alexander <alexander.kotov@devexpress.com>
2022-04-05 10:32:46 +0400

    base working proto

--- js/ui/html_editor/modules/imageUpload.js
+++ js/ui/html_editor/modules/imageUpload.js
@@ -2,7 +2,8 @@ import Quill from 'devextreme-quill';
 import BaseModule from './base';
 import eventsEngine from '../../../events/core/events_engine';
 import $ from '../../../core/renderer';
-// import { getFormatHandlers, getDefaultClickHandler, ICON_MAP } from '../utils/toolbar_helper';
+// import { getFormatHandlers, getDefaultClickHandler, ICON_MAP } from '../utils/toolbar_helper'
+import { urlUpload } from '../utils/image_uploader_helper';
 import { addNamespace } from '../../../events/utils/index';
 import FileUploader from '../../file_uploader';
 
@@ -20,13 +21,15 @@ if(Quill) {
 
             super(quill, options);
 
+            this.options = options;
+
             this._quillContainer = this.editorInstance._getQuillContainer();
 
             this.addCleanCallback(this.prepareCleanCallback());
             // this._formatHandlers = getFormatHandlers(this);
             // this._tableFormats = getTableFormats(quill);
 
-            const useServerUpload = options.fileUploadMode !== 'base64';
+            const useServerUpload = this.options.fileUploadMode !== 'base64';
 
             if(useServerUpload) {
                 this._enableDragAndDropUploading(quill);
@@ -34,7 +37,7 @@ if(Quill) {
         }
 
         _enableDragAndDropUploading(quill) {
-            this._fileUploader = this._initFileUploader();
+            this._initFileUploader();
 
             quill.getModule('uploader').removeDropHandler();
             this._attachEvents();
@@ -45,9 +48,15 @@ if(Quill) {
                 .addClass(HIDDEN_FILE_UPLOADER_CLASS)
                 .appendTo(this._quillContainer);
 
-            const fileUploaderOptions = {};
+            const fileUploaderOptions = {
+                accept: 'image/*',
+                uploadUrl: this.options.uploadUrl,
+                uploadMode: 'instantly',
+            };
 
             this.editorInstance._createComponent($container, FileUploader, fileUploaderOptions);
+
+            this._fileUploader = $container.dxFileUploader('instance');
 
 
             return $container;
@@ -67,7 +76,21 @@ if(Quill) {
             // debugger;
             e.preventDefault();
             e.stopPropagation();
+            // debugger;
+            this.saveValueChangeEvent(e);
 
+            const dataTransfer = e.originalEvent.dataTransfer;
+            // const hasFiles = dataTransfer?.files?.length;
+            const file = dataTransfer.files[0];
+
+            const imageUrl = this.options.uploadDirectory + file.name;
+
+            const selection = this.quill.getSelection();
+            const pasteIndex = selection ? selection.index : this.quill.getLength();
+
+            this._fileUploader.upload(file);
+
+            urlUpload(this.quill, pasteIndex, { src: imageUrl });
 
             // e.originalEvent.preventDefault();
             // e.originalEvent.stopPropagation();
