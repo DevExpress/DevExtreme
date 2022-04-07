27145cec0e69436d6e566e4918d9339c48e7d7ba
Alexander Bezborodov (DevExpress) <bezborodov.alexander@corp.devexpress.com>
2022-04-07 16:00:02 +0400

    Localization - disable native Intl for some tests

--- testing/tests/DevExpress.localization/localization.nativeIntl.base.test.js
+++ testing/tests/DevExpress.localization/localization.nativeIntl.base.test.js
@@ -5,11 +5,11 @@ locale('en');
 
 QUnit.module('Localization date', () => {
     QUnit.test('object syntax, fractionalSecondDigits is set in format (T1079944)', function(assert) {
-        const date = new Date(Date.UTC(2012, 11, 20, 3, 0, 0, 123));
-        const options = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+        const date = new Date(Date.UTC(2010, 0, 1, 0, 1, 2, 345));
+        const options = { minute: '2-digit', second: '2-digit' };
 
-        assert.equal(dateLocalization.format(date, options), '12/20/2012, 07:00:00 AM');
-        assert.equal(dateLocalization.format(date, { ...options, fractionalSecondDigits: '3' }), '12/20/2012, 07:00:00.123 AM');
-        assert.equal(dateLocalization.format(date, { ...options, fractionalSecondDigits: '1' }), '12/20/2012, 07:00:00.1 AM');
+        assert.equal(dateLocalization.format(date, options), '01:02');
+        assert.equal(dateLocalization.format(date, { ...options, fractionalSecondDigits: '3' }), '01:02.345');
+        assert.equal(dateLocalization.format(date, { ...options, fractionalSecondDigits: '1' }), '01:02.3');
     });
 });
