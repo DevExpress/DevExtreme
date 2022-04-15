8195c10f344a55da1db12ee23d940d022af00fef
zelevinskiy <alexander.zelevinskiy@devexpress.com>
2022-04-15 16:03:28 +0400

    Diagram - Fix touch bugs (T1064072, T1058682, etc)

--- js/ui/diagram/ui.diagram.context_menu.js
+++ js/ui/diagram/ui.diagram.context_menu.js
@@ -7,7 +7,6 @@ import DiagramMenuHelper from './ui.diagram.menu_helper';
 
 import DiagramBar from './diagram.bar';
 import { getDiagram } from './diagram.importer';
-import { hasWindow, getWindow } from '../../core/utils/window';
 
 const DIAGRAM_TOUCHBAR_CLASS = 'dx-diagram-touchbar';
 const DIAGRAM_TOUCHBAR_OVERLAY_CLASS = 'dx-diagram-touchbar-overlay';
@@ -92,21 +91,10 @@ class DiagramContextMenuWrapper extends Widget {
     _hide() {
         this._$contextMenuTargetElement.hide();
         this._contextMenuInstance.hide();
-        delete this._isTouchMode;
     }
     _isTouchBarMode() {
-        if(this._isTouchMode !== undefined) {
-            return this._isTouchMode;
-        }
         const { Browser } = getDiagram();
-        if(Browser.TouchUI) {
-            return true;
-        }
-        if(!hasWindow()) {
-            return false;
-        }
-        const window = getWindow();
-        return window.navigator && window.navigator.maxTouchPoints > 0;
+        return Browser.TouchUI;
     }
     _onItemClick(itemData) {
         let processed = false;
