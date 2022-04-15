04bc18a2695760e5e09c71eed6bbe06f869bc8d6
Anton Sermyazhko <pin361@gmail.com>
2022-04-15 10:16:34 +0300

    Fix updating showAllDayAppointmnets by option

--- js/ui/scheduler/ui.scheduler.js
+++ js/ui/scheduler/ui.scheduler.js
@@ -731,7 +731,7 @@ class Scheduler extends Widget {
                 this._updateOption('workSpace', args.fullName, value);
                 break;
             case 'showAllDayAppointments':
-                this._updateOption('showAllDayAppointments', name, value);
+                this._workSpace.option('showAllDayAppointments', value);
                 break;
             case 'renovateRender':
                 this._updateOption('workSpace', name, value);
@@ -1600,7 +1600,7 @@ class Scheduler extends Widget {
             schedulerHeight: this.option('height'),
             schedulerWidth: this.option('width'),
             onSelectedCellsClick: this.showAddAppointmentPopup.bind(this),
-            onVirtualScrollingUpdated: this._renderAppointments.bind(this),
+            onRenderAppointments: this._renderAppointments.bind(this),
             getHeaderHeight: () => utils.DOM.getHeaderHeight(this._header),
             onScrollEnd: () => this._appointments.updateResizableArea(),
 
