c93fd8f3d237fff34e6c61fb1174e162f71b5d78
Roman Semenov <roman.semenov@devexpress.com>
2022-04-18 09:56:29 +0400

    fix tests

--- testing/tests/DevExpress.ui.widgets.dataGrid/filterRow.tests.js
+++ testing/tests/DevExpress.ui.widgets.dataGrid/filterRow.tests.js
@@ -2235,7 +2235,7 @@ QUnit.module('Filter Row with real dataController and columnsController', {
             { column1: 1, column2: 1 },
             { column1: 2, column2: 2 },
         ];
-        this.options.filterRow.showRelevantValues = true;
+        this.options.syncLookupFilterValues = true;
 
         setupDataGridModules(this, ['data', 'columns', 'columnHeaders', 'filterRow', 'editorFactory'], {
             initViews: true
@@ -2266,6 +2266,65 @@ QUnit.module('Filter Row with real dataController and columnsController', {
 
         // assert
         assert.strictEqual(dropDownList2.find('.dx-item').length, 2);
+        assert.strictEqual(dropDownList2.find('.dx-item:eq(1)').text(), 'value1');
+    });
+
+    QUnit.test('Lookup select box should not show only relevant values if syncLookupFilterValues = false', function(assert) {
+        // arrange
+        const $testElement = $('#container');
+
+        this.options.columns = [{
+            dataField: 'column1',
+            allowFiltering: true,
+            lookup: {
+                dataSource: [{ id: 1, value: 'value1' }, { id: 2, value: 'value2' }],
+                valueExpr: 'id',
+                displayExpr: 'value'
+            }
+        }, {
+            dataField: 'column2',
+            allowFiltering: true,
+            lookup: {
+                dataSource: [{ id: 1, value: 'value1' }, { id: 2, value: 'value2' }],
+                valueExpr: 'id',
+                displayExpr: 'value'
+            }
+        }];
+        this.options.dataSource = [
+            { column1: 1, column2: 1 },
+            { column1: 2, column2: 2 },
+        ];
+        this.options.syncLookupFilterValues = false;
+
+        setupDataGridModules(this, ['data', 'columns', 'columnHeaders', 'filterRow', 'editorFactory'], {
+            initViews: true
+        });
+        this.columnHeadersView.render($testElement);
+
+        // act
+        const dropDown1 = $('.dx-dropdowneditor-button:eq(0)');
+        const dropDown2 = $('.dx-dropdowneditor-button:eq(1)');
+
+        dropDown1.trigger('dxclick');
+        dropDown2.trigger('dxclick');
+
+        // assert
+        const dropDownList1 = $('.dx-list:eq(0)');
+        const dropDownList2 = $('.dx-list:eq(1)');
+
+        assert.strictEqual(dropDownList1.find('.dx-item').length, 3);
+        assert.strictEqual(dropDownList1.find('.dx-item:eq(1)').text(), 'value1');
+        assert.strictEqual(dropDownList1.find('.dx-item:eq(2)').text(), 'value2');
+
+        assert.strictEqual(dropDownList2.find('.dx-item').length, 3);
+        assert.strictEqual(dropDownList2.find('.dx-item:eq(1)').text(), 'value1');
+        assert.strictEqual(dropDownList2.find('.dx-item:eq(2)').text(), 'value2');
+
+        // act
+        dropDownList1.find('.dx-item:eq(1)').trigger('dxclick');
+
+        // assert
+        assert.strictEqual(dropDownList2.find('.dx-item').length, 3);
         assert.strictEqual(dropDownList2.find('.dx-item:eq(1)').text(), 'value1');
     });
 
