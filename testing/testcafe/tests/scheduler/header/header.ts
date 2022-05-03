ef5fe3534a31aa6fb61fa0d873bdb5becd7aeaea
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2022-05-03 14:45:36 +0300

    Scheduler - Fix view switcher incorrectly switch between differently typed views (T1080992) (#21637)

--- testing/testcafe/tests/scheduler/header/header.ts
+++ testing/testcafe/tests/scheduler/header/header.ts
@@ -1,10 +1,8 @@
-import { Selector } from 'testcafe';
-import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import createWidget from '../../../helpers/createWidget';
 import url from '../../../helpers/getPageUrl';
 import Scheduler from '../../../model/scheduler';
 
-fixture`Hotkeys for appointments update and navigation`
+fixture`Scheduler header`
   .page(url(__dirname, '../../container.html'));
 
 test('dateNavigator buttons should not be selected after clicking', async (t) => {
@@ -44,89 +42,3 @@ test('dateNavigator buttons should have "contained" styling mode with generic th
   views: ['day'],
   height: 580,
 }));
-
-fixture`Scheduler: Adaptive material theme layout`
-  .page(url(__dirname, '../../containerMaterial.html'));
-
-test('dateNavigator buttons should have "text" styling mode with material theme', async (t) => {
-  const { toolbar } = new Scheduler('#container');
-
-  await t
-    .expect(toolbar.navigator.prevButton.hasClass('dx-button-mode-text'))
-    .ok()
-
-    .expect(toolbar.navigator.caption.hasClass('dx-button-mode-text'))
-    .ok()
-
-    .expect(toolbar.navigator.nextButton.hasClass('dx-button-mode-text'))
-    .ok();
-}).before(async () => createWidget('dxScheduler', {
-  currentView: 'day',
-  views: ['day'],
-  height: 580,
-}));
-
-test('viewSwitcher dropdown button popup should have a specified class', async (t) => {
-  const { toolbar } = new Scheduler('#container');
-  const dropDownButton = toolbar.viewSwitcher.getDropDownButton();
-
-  const viewSwitcherDropDownButtonContent = '.dx-scheduler-view-switcher-dropdown-button-content';
-
-  await t
-    .click(dropDownButton.element)
-
-    .expect(Selector(viewSwitcherDropDownButtonContent).count)
-    .eql(1);
-}).before(async () => createWidget('dxScheduler', {
-  currentView: 'day',
-  views: ['day', 'week'],
-  height: 580,
-}));
-
-test('The toolbar should not display if the config is empty', async (t) => {
-  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
-
-  const scheduler = new Scheduler('#container');
-
-  await t
-    .expect(await takeScreenshot('scheduler-with-empty-toolbar-config.png'))
-    .ok();
-
-  await scheduler.option('toolbar', [{ defaultElement: 'viewSwitcher' }]);
-
-  await t
-    .expect(await takeScreenshot('scheduler-with-non-empty-toolbar-config.png'))
-    .ok();
-
-  await t
-    .expect(compareResults.isValid())
-    .ok(compareResults.errorMessages());
-}).before(async () => createWidget('dxScheduler', {
-  currentDate: new Date(2020, 2, 2),
-  currentView: 'day',
-  views: ['day'],
-  height: 580,
-  toolbar: [],
-}, true));
-
-test('The viewSwitcher should not drop down if only one view', async (t) => {
-  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
-
-  const { toolbar } = new Scheduler('#container');
-  const dropDownButton = toolbar.viewSwitcher.getDropDownButton();
-
-  await t
-    .click(dropDownButton.element)
-
-    .expect(await takeScreenshot('drop-down-with-one-view.png', toolbar.element))
-    .ok()
-
-    .expect(compareResults.isValid())
-    .ok(compareResults.errorMessages());
-}).before(async () => createWidget('dxScheduler', {
-  currentDate: new Date(2020, 2, 2),
-  currentView: 'day',
-  views: ['day'],
-  useDropDownViewSwitcher: true,
-  height: 580,
-}, true));
