da9ec3a300e63934fb0d14cff3429bb4a09a0a49
Sergey <sergey.novikov@devexpress.com>
2022-05-06 17:05:16 +0400

    Check multiple themes

--- testing/testcafe/tests/navigation/form/form.simpleItem.ts
+++ testing/testcafe/tests/navigation/form/form.simpleItem.ts
@@ -1,6 +1,10 @@
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import { ClientFunction } from 'testcafe';
 import url from '../../../helpers/getPageUrl';
 import createWidget from '../../../helpers/createWidget';
+import { changeTheme } from '../../../helpers/changeTheme';
+
+const waitFont = ClientFunction(() => (window as any).DevExpress.ui.themes.waitWebFont('Item123somevalu*op ', 400));
 
 fixture`Form`
   .page(url(__dirname, '../../container.html'));
@@ -30,29 +34,33 @@ fixture`Form`
   });
 });
 
-test('widget alignment (T1086611)', async (t) => {
-  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+['generic.light', 'material.blue.light'].forEach((theme) => {
+  test('widget alignment (T1086611)', async (t) => {
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+    await changeTheme(theme);
+    await waitFont();
 
-  await t
-    .expect(await takeScreenshot('widget alignment (T1086611).png', '#container'))
-    .ok()
-    .expect(compareResults.isValid())
-    .ok(compareResults.errorMessages());
-}).before(async () => createWidget('dxForm', {
-  colCount: 2,
-  with: 1000,
-  formData: {},
-  items: [{
-    dataField: 'FirstName',
-    editorType: 'dxTextBox',
-  }, {
-    dataField: 'Position',
-    editorType: 'dxSelectBox',
-  }, {
-    dataField: 'BirthDate',
-    editorType: 'dxDateBox',
-  }, {
-    dataField: 'Notes',
-    editorType: 'dxTextArea',
-  }],
-}));
+    await t
+      .expect(await takeScreenshot('widget alignment (T1086611).png', '#container'))
+      .ok()
+      .expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
+  }).before(async () => createWidget('dxForm', {
+    colCount: 2,
+    with: 1000,
+    formData: {},
+    items: [{
+      dataField: 'FirstName',
+      editorType: 'dxTextBox',
+    }, {
+      dataField: 'Position',
+      editorType: 'dxSelectBox',
+    }, {
+      dataField: 'BirthDate',
+      editorType: 'dxDateBox',
+    }, {
+      dataField: 'Notes',
+      editorType: 'dxTextArea',
+    }],
+  }));
+});
