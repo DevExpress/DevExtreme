c4128972882a7b7d4aac3f54bfa5a35e048df6f7
Sergey <sergey.novikov@devexpress.com>
2022-05-06 18:02:45 +0400

    theme is not applied

--- testing/testcafe/tests/navigation/form/form.simpleItem.ts
+++ testing/testcafe/tests/navigation/form/form.simpleItem.ts
@@ -34,33 +34,35 @@ fixture`Form`
   });
 });
 
-['generic.light', 'material.blue.light'].forEach((theme) => {
-  test('widget alignment (T1086611)', async (t) => {
-    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
-    await changeTheme(theme);
-    await waitFont();
+['left', 'right', 'top'].forEach((labelLocation) => {
+  ['generic.light', 'material.blue.light'].forEach((theme) => {
+    test('widget alignment (T1086611)', async (t) => {
+      const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+      await changeTheme(theme);
+      await waitFont();
 
-    await t
-      .expect(await takeScreenshot('widget alignment (T1086611).png', '#container'))
-      .ok()
-      .expect(compareResults.isValid())
-      .ok(compareResults.errorMessages());
-  }).before(async () => createWidget('dxForm', {
-    colCount: 2,
-    with: 1000,
-    formData: {},
-    items: [{
-      dataField: 'FirstName',
-      editorType: 'dxTextBox',
-    }, {
-      dataField: 'Position',
-      editorType: 'dxSelectBox',
-    }, {
-      dataField: 'BirthDate',
-      editorType: 'dxDateBox',
-    }, {
-      dataField: 'Notes',
-      editorType: 'dxTextArea',
-    }],
-  }));
+      await t
+        .expect(await takeScreenshot(`widget alignment (T1086611)_${labelLocation}_${theme}.png`, '#container'))
+        .ok()
+        .expect(compareResults.isValid())
+        .ok(compareResults.errorMessages());
+    }).before(async () => createWidget('dxForm', {
+      colCount: 2,
+      with: 1000,
+      formData: {},
+      items: [{
+        dataField: 'FirstName',
+        editorType: 'dxTextBox',
+      }, {
+        dataField: 'Position',
+        editorType: 'dxSelectBox',
+      }, {
+        dataField: 'BirthDate',
+        editorType: 'dxDateBox',
+      }, {
+        dataField: 'Notes',
+        editorType: 'dxTextArea',
+      }],
+    }));
+  });
 });
