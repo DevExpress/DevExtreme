25ddfc6055e46b4d301bac7cb6d906227e5b703e
Oleg Kipchatov <35765711+OlegKipchatov@users.noreply.github.com>
2022-05-17 20:36:11 +0400

    Add type for dxLoadPanel.contentTemaplate

--- js/ui/load_panel.js
+++ js/ui/load_panel.js
@@ -58,6 +58,7 @@ const LoadPanel = Overlay.inherit({
 
             /**
             * @name dxLoadPanelOptions.contentTemplate
+            * @type template
             * @hidden
             */
 
