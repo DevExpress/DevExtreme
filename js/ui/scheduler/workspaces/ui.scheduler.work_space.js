2c5f85ce92db74ffe6516fa1e8ec542439030498
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2022-05-30 18:18:06 +0400

    Scheduler - Fix broken layout if virtual scrolling and width or height is set in percentage (T1091980) (#21783)
    # Conflicts:
    #	js/ui/scheduler/workspaces/ui.scheduler.virtual_scrolling.js

--- js/ui/scheduler/workspaces/ui.scheduler.work_space.js
+++ js/ui/scheduler/workspaces/ui.scheduler.work_space.js
@@ -2202,6 +2202,8 @@ class SchedulerWorkSpace extends WidgetObserver {
             getSchedulerWidth: () => this.option('schedulerWidth'),
             getViewHeight: () => this.$element().height ? this.$element().height() : getHeight(this.$element()),
             getViewWidth: () => this.$element().width ? this.$element().width() : getWidth(this.$element()),
+            getWindowHeight: () => getWindow().innerHeight,
+            getWindowWidth: () => getWindow().innerWidth,
             getScrolling: () => this.option('scrolling'),
             getScrollableOuterWidth: this.getScrollableOuterWidth.bind(this),
             getScrollable: this.getScrollable.bind(this),
