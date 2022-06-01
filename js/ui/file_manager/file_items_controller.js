d3734e008d1bf544d0b96e1b51e5d1b077068d28
Stanislav Klesarev <stanislav.klesarev@devexpress.com>
2022-06-01 16:40:17 +0300

    FileManager - Fix the getItems method invocation count after exception (T1085224) (#21786)
    * FileManager - Fix the getItems method invocation count after exception (T1085224)
    
    * Perform requested changes and add tests
    
    * Rename _tryUnlockController -> _tryEndLoading

--- js/ui/file_manager/file_items_controller.js
+++ js/ui/file_manager/file_items_controller.js
@@ -13,6 +13,7 @@ import { find } from '../../core/utils/array';
 import { extend } from '../../core/utils/extend';
 import { equalByValue } from '../../core/utils/common';
 import { isDefined, isObject, isPromise } from '../../core/utils/type';
+import Guid from '../../core/guid';
 
 const DEFAULT_ROOT_FILE_SYSTEM_ITEM_NAME = 'Files';
 
@@ -31,6 +32,7 @@ export default class FileItemsController {
 
         this._defaultIconMap = this._createDefaultIconMap();
 
+        this.startSingleLoad();
         this._setSecurityController();
         this._setProvider(options.fileProvider);
         this._initialize();
@@ -235,9 +237,14 @@ export default class FileItemsController {
                 .resolve(parentDirectoryInfo.items)
                 .promise();
         }
+        if(this._singleOperationLockId && parentDirectoryInfo.itemsSingleLoadErrorId === this._singleOperationLockId) {
+            this._changeDirectoryOnError(parentDirectoryInfo, skipNavigationOnError, true);
+            return new Deferred().reject().promise();
+        }
 
         const dirKey = parentDirectoryInfo.getInternalKey();
         let loadItemsDeferred = this._loadedItems[dirKey];
+
         if(loadItemsDeferred) {
             return loadItemsDeferred;
         }
@@ -250,6 +257,11 @@ export default class FileItemsController {
                 );
                 parentDirectoryInfo.itemsLoaded = true;
                 return parentDirectoryInfo.items;
+            }, () => {
+                if(this._singleOperationLockId && parentDirectoryInfo.itemsSingleLoadErrorId !== this._singleOperationLockId) {
+                    parentDirectoryInfo.itemsSingleLoadErrorId = this._singleOperationLockId;
+                }
+                return [];
             });
 
         this._loadedItems[dirKey] = loadItemsDeferred;
@@ -485,6 +497,12 @@ export default class FileItemsController {
 
     _handleItemLoadError(parentDirectoryInfo, errorInfo, skipNavigationOnError) {
         parentDirectoryInfo = this._getActualDirectoryInfo(parentDirectoryInfo);
+        this._raiseGetItemsError(parentDirectoryInfo, errorInfo);
+        this._changeDirectoryOnError(parentDirectoryInfo, skipNavigationOnError);
+        return new Deferred().reject().promise();
+    }
+
+    _raiseGetItemsError(parentDirectoryInfo, errorInfo) {
         const actionInfo = this._createEditActionInfo('getItems', parentDirectoryInfo, parentDirectoryInfo);
         this._raiseEditActionStarting(actionInfo);
         this._raiseEditActionResultAcquired(actionInfo);
@@ -494,13 +512,17 @@ export default class FileItemsController {
             fileItem: parentDirectoryInfo.fileItem,
             index: 0
         });
-        this._resetDirectoryState(parentDirectoryInfo);
-        parentDirectoryInfo.expanded = false;
-        if(!skipNavigationOnError) {
-            this.setCurrentDirectory(parentDirectoryInfo.parentDirectory);
-        }
+    }
 
-        return new Deferred().reject().promise();
+    _changeDirectoryOnError(dirInfo, skipNavigationOnError, isActualDirectoryRequired) {
+        if(isActualDirectoryRequired) {
+            dirInfo = this._getActualDirectoryInfo(dirInfo);
+        }
+        this._resetDirectoryState(dirInfo);
+        dirInfo.expanded = false;
+        if(!skipNavigationOnError) {
+            this.setCurrentDirectory(dirInfo.parentDirectory);
+        }
     }
 
     _processEditAction(actionInfo, beforeAction, action, afterAction, completeAction) {
@@ -609,6 +631,14 @@ export default class FileItemsController {
         return this._executeDataLoad(() => {
             return this._refreshDeferred = this._refreshInternal();
         }, 'refresh');
+    }
+
+    startSingleLoad() {
+        this._singleOperationLockId = new Guid().toString();
+    }
+
+    endSingleLoad() {
+        delete this._singleOperationLockId;
     }
 
     _refreshInternal() {
@@ -853,7 +883,7 @@ export default class FileItemsController {
 
     _tryCallAction(actionName) {
         const args = Array.prototype.slice.call(arguments, 1);
-        if(this._options[actionName]) {
+        if(this._isInitialized && this._options[actionName]) {
             this._options[actionName](...args);
         }
     }
