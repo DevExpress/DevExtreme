1a4278fd6edc842690f27aa1016e02fd44a02e90
Roman Semenov <roman.semenov@devexpress.com>
2022-06-08 22:32:38 +0300

    DataGrid - The component returns undefined data when the Shift key is used for multiple selection in a certain scenario (T1092804) (#21815)
    * test
    
    * fix
    
    * fix review

--- js/ui/selection/selection.js
+++ js/ui/selection/selection.js
@@ -116,11 +116,17 @@ export default Class.inherit({
         const allowLoadByRange = this.options.allowLoadByRange?.();
         let indexOffset;
         let focusedItemNotInLoadedRange = false;
+        let shiftFocusedItemNotInLoadedRange = false;
+
+        const itemIsNotInLoadedRange = (index) => index >= 0 && !items.filter(it => it.loadIndex === index).length;
 
         if(allowLoadByRange) {
             indexOffset = item.loadIndex - itemIndex;
             itemIndex = item.loadIndex;
-            focusedItemNotInLoadedRange = this._focusedItemIndex >= 0 && !items.filter(it => it.loadIndex === this._focusedItemIndex).length;
+            focusedItemNotInLoadedRange = itemIsNotInLoadedRange(this._focusedItemIndex);
+            if(isDefined(this._shiftFocusedItemIndex)) {
+                shiftFocusedItemNotInLoadedRange = itemIsNotInLoadedRange(this._shiftFocusedItemIndex);
+            }
         }
 
         if(!this.isSelectable() || !this.isDataItem(item)) {
@@ -133,7 +139,7 @@ export default Class.inherit({
         keys = keys || {};
 
         if(keys.shift && this.options.mode === 'multiple' && this._focusedItemIndex >= 0) {
-            if(focusedItemNotInLoadedRange) {
+            if(focusedItemNotInLoadedRange || shiftFocusedItemNotInLoadedRange) {
                 isSelectedItemsChanged = itemIndex !== this._shiftFocusedItemIndex || this._focusedItemIndex !== this._shiftFocusedItemIndex;
 
                 if(isSelectedItemsChanged) {
