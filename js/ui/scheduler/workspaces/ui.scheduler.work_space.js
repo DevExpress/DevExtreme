4cbffed86a70c1837c42ca65ceba19196aa86b16
Ilya Vinogradov <ilya.vinogradov@devexpress.com>
2022-06-14 14:01:36 +0400

    T1094033 Scheduler: code review fixes. Remove redundant check in workspace drop event handler.

--- js/ui/scheduler/workspaces/ui.scheduler.work_space.js
+++ js/ui/scheduler/workspaces/ui.scheduler.work_space.js
@@ -985,18 +985,13 @@ class SchedulerWorkSpace extends WidgetObserver {
         eventsEngine.on(element, DragEventNames.ENTER, DRAG_AND_DROP_SELECTOR, { checkDropTarget: onCheckDropTarget }, onDragEnter);
         eventsEngine.on(element, DragEventNames.LEAVE, removeClasses);
         eventsEngine.on(element, DragEventNames.DROP, DRAG_AND_DROP_SELECTOR, () => {
-            if(!this.dragBehavior) {
-                this.removeDroppableCellClass();
+            if(!this.dragBehavior?.dragBetweenComponentsPromise) {
+                this.dragBehavior.removeDroppableClasses();
                 return;
             }
 
-            if(!this.dragBehavior.dragBetweenComponentsPromise) {
-                this.dragBehavior?.removeDroppableClasses();
-                return;
-            }
-
-            this.dragBehavior.dragBetweenComponentsPromise.then(() => {
-                this.dragBehavior?.removeDroppableClasses();
+            this.dragBehavior.dragBetweenComponentsPromise?.then(() => {
+                this.dragBehavior.removeDroppableClasses();
             });
         });
 
