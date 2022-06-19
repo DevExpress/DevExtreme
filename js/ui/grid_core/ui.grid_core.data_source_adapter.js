50a5f2e96bc4efa2c67cc87d75ad8fded9325ed0
Roman Semenov <roman.semenov@devexpress.com>
2022-06-19 23:03:00 +0300

    fix after review

--- js/ui/grid_core/ui.grid_core.data_source_adapter.js
+++ js/ui/grid_core/ui.grid_core.data_source_adapter.js
@@ -173,6 +173,7 @@ export default gridCore.Controller.inherit((function() {
         },
         push: function(changes, fromStore) {
             const store = this.store();
+            const isReshapeMode = this.option('editing.refreshMode') === 'reshape';
 
             if(this._needClearStoreDataCache()) {
                 this._cachedStoreData = undefined;
@@ -190,7 +191,7 @@ export default gridCore.Controller.inherit((function() {
                 });
             }
 
-            if(!fromStore) {
+            if(!fromStore && !isReshapeMode) {
                 this._applyBatch(changes);
             }
 
@@ -226,7 +227,6 @@ export default gridCore.Controller.inherit((function() {
             const groupCount = gridCore.normalizeSortingInfo(this.group()).length;
             const totalCount = this.totalCount();
             const isVirtualMode = this.option('scrolling.mode') === 'virtual';
-            const isReshapeMode = this.option('editing.refreshMode') === 'reshape';
 
             changes = changes.filter(function(change) {
                 return !dataSource.paginate() || change.type !== 'insert' || change.index !== undefined;
@@ -251,7 +251,7 @@ export default gridCore.Controller.inherit((function() {
                 useInsertIndex: true
             });
 
-            if(this._currentTotalCount > 0 || !isReshapeMode && isVirtualMode && totalCount === oldItemCount) {
+            if(this._currentTotalCount > 0 || isVirtualMode && totalCount === oldItemCount) {
                 this._skipCorrection += getItemCount() - oldItemCount;
             }
 
