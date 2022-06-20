df6847afd5e40cfccd56dafb69a5b3dbc3d6eb61
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2022-06-20 11:55:52 +0400

    fix rounding error

--- testing/tests/DevExpress.ui.widgets.editors/lookup.tests.js
+++ testing/tests/DevExpress.ui.widgets.editors/lookup.tests.js
@@ -3505,7 +3505,7 @@ QUnit.module('default options', {
 
             const $popup = $('.dx-popup-content');
 
-            assert.roughEqual($popup.height(), 112, 1, 'popup height if DataSource without items and `searchEnabled: true`');
+            assert.roughEqual($popup.height(), 112, 1.01, 'popup height if DataSource without items and `searchEnabled: true`');
         } finally {
             $lookup.remove();
             themes.isMaterial = origIsMaterial;
