f9010aabaac5950e3f56364590be82ed72268708
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2022-06-20 12:49:53 +0400

    Revert "Remove from webpack.config"
    This reverts commit 69432a9ab1e04521bcc51ab1d7f4d6b0c48ac3d6.

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ module.exports = {
         'jquery': 'window.jQuery',
         'jszip': 'window.JSZip',
         'knockout': 'window.ko',
+        'angular': 'window.angular',
         'globalize': 'window.Globalize',
         'globalize/number': 'window.Globalize',
         'globalize/currency': 'window.Globalize',
