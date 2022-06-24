9fc8460fe1e1eeedd9a5045566ae4bade1fdbb9a
Ilya Vinogradov <ilya.vinogradov@devexpress.com>
2022-06-24 16:34:40 +0400

    Scheduler: Fix T1093753 cherry-pick to 21_2 version.

--- js/ui/scheduler/appointments/dataProvider/appointmentFilter.js
+++ js/ui/scheduler/appointments/dataProvider/appointmentFilter.js
@@ -107,6 +107,7 @@ class FilterMaker {
 export class AppointmentFilterBaseStrategy {
     constructor(options) {
         this.options = options;
+        this.dataSource = this.options.dataSource;
         this.dataAccessors = this.options.dataAccessors;
 
         this._init();
@@ -269,12 +270,7 @@ export class AppointmentFilterBaseStrategy {
                 }
             }
 
-            const appointmentTakesAllDay = getAppointmentTakesAllDay(
-                appointment,
-                viewStartDayHour,
-                viewEndDayHour,
-                this.allDayPanelMode,
-            );
+            const appointmentTakesAllDay = getAppointmentTakesAllDay(appointment, viewStartDayHour, viewEndDayHour);
             const appointmentTakesSeveralDays = getAppointmentTakesSeveralDays(appointment);
             const isAllDay = appointment.allDay;
             const isLongAppointment = appointmentTakesSeveralDays || appointmentTakesAllDay;
