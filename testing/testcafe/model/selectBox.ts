ebd563faecae78b09c5a52498cd1b6208c1bf2a3
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2022-06-26 13:37:02 +0400

    fix selectbox types

--- testing/testcafe/model/selectBox.ts
+++ testing/testcafe/model/selectBox.ts
@@ -1,18 +1,5 @@
-import { Selector } from 'testcafe';
 import DropDownList from './internal/dropDownList';
-import { getComponentInstance } from '../helpers/multi-platform-test';
 
-const CLASS = {
-  selectBox: 'dx-selectbox',
-};
 export default class SelectBox extends DropDownList {
   name = 'dxSelectBox';
-
-  getInstance: () => Promise<unknown>;
-
-  constructor(id: string) {
-    super(id);
-
-    this.getInstance = getComponentInstance('jquery', Selector(`.${CLASS.selectBox}`));
-  }
 }
