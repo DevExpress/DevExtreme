a8995e3a5b616191c3df98946f9dd78d7e3ea20c
Roman Semenov <roman.semenov@devexpress.com>
2022-06-27 11:20:43 +0300

    add test for legacy
    (cherry picked from commit 84d2f52de205d4d7a0546223f4ba67744ad6da38)

--- testing/tests/DevExpress.ui.widgets.treeList/virtualScrolling.integration.tests.js
+++ testing/tests/DevExpress.ui.widgets.treeList/virtualScrolling.integration.tests.js
@@ -12,27 +12,33 @@ QUnit.testStart(function() {
 
 QUnit.module('Virtual Scrolling', baseModuleConfig, () => {
     // T1097528
-    QUnit.test('Last row should not disappear after adding new row when refreshMode is repaint', function(assert) {
+    [true, false].forEach(legacyMode => {
+        QUnit.test('Last row should not disappear after adding new row when refreshMode is repaint', function(assert) {
         // arrange
-        const treeList = $('#treeList').dxTreeList({
-            dataSource: [{ id: 1 }, { id: 2 }],
-            keyExpr: 'id',
-            height: 100,
-            editing: {
-                refreshMode: 'repaint'
-            },
-        }).dxTreeList('instance');
+            const treeList = $('#treeList').dxTreeList({
+                dataSource: [{ id: 1 }, { id: 2 }],
+                keyExpr: 'id',
+                height: 100,
+                scrolling: {
+                    virtual: true,
+                    legacyMode
+                },
+                editing: {
+                    refreshMode: 'repaint'
+                },
+            }).dxTreeList('instance');
 
-        this.clock.tick(500);
+            this.clock.tick(500);
 
-        // assert
-        assert.strictEqual(treeList.getVisibleRows().length, 2, 'visible rows count');
+            // assert
+            assert.strictEqual(treeList.getVisibleRows().length, 2, 'visible rows count');
 
-        // act
-        treeList.addRow();
-        treeList.saveEditData();
+            // act
+            treeList.addRow();
+            treeList.saveEditData();
 
-        // assert
-        assert.strictEqual(treeList.getVisibleRows().length, 3, 'visible rows count');
+            // assert
+            assert.strictEqual(treeList.getVisibleRows().length, 3, 'visible rows count');
+        });
     });
 });
