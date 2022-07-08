147aa5edeb0c1d360ccffd1d764ef96cc73e94d9
Zedwag <zedwag@gmail.com>
2022-07-08 20:19:21 +0800

    add testcafe test

--- testing/testcafe/tests/editors/validationMessage.ts
+++ testing/testcafe/tests/editors/validationMessage.ts
@@ -1,6 +1,7 @@
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import { ClientFunction, Selector } from 'testcafe';
 import url from '../../helpers/getPageUrl';
+import { changeTheme } from '../../helpers/changeTheme';
 import CheckBox from '../../model/checkBox';
 import { restoreBrowserSize } from '../../helpers/restoreBrowserSize';
 import createWidget from '../../helpers/createWidget';
@@ -77,4 +78,29 @@ test('Validation Message position should be correct after change visibility of p
   }, true, '#textbox');
 }).after(async (t) => {
   await restoreBrowserSize(t);
+});
+
+fixture`ValidationMessagePosition`
+  .page(url(__dirname, './pages/validation.html'));
+
+const setPosition = ClientFunction(
+  (position) => (window as any).createEditorsInTheme(position),
+);
+
+const positions = ['top', 'right', 'bottom', 'left'];
+const themes = ['generic.light', 'generic.light.compact', 'material.blue.light', 'material.blue.light.compact'];
+themes.forEach((theme) => {
+  positions.forEach((position) => {
+    test(`Editors ValidationMessage position is correct (${position}, ${theme})`, async (t) => {
+      const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+      await changeTheme(theme);
+      await setPosition(position);
+
+      await t
+        .expect(await takeScreenshot(`editor-validation-position-${position}-${theme}.png`, '#container'))
+        .ok()
+        .expect(compareResults.isValid())
+        .ok(compareResults.errorMessages());
+    });
+  });
 });
