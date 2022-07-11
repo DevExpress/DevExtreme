568d7f0ef4b3a01746e9421883f8203376433804
Zedwag <zedwag@gmail.com>
2022-07-12 00:53:46 +0800

    fix defaultOptions test

--- testing/tests/DevExpress.ui/defaultOptions.tests.js
+++ testing/tests/DevExpress.ui/defaultOptions.tests.js
@@ -198,7 +198,7 @@ testComponentDefaults(ValidationMessage,
 
         mode: 'auto',
         validationErrors: undefined,
-        position: 'top',
+        positionSide: 'top',
         boundary: undefined,
         offset: { h: 0, v: 0 }
     }
