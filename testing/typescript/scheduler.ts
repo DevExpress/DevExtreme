6fb164d6877839c4014ace24c31aa00a1e424cf3
ilya.kharchenko <14272298+IlyaKhD@users.noreply.github.com>
2022-07-15 23:21:05 +0400

    Allow using custom properties in Scheduler's appointment (T1062511) (#22168)

---
+++ testing/typescript/scheduler.ts
@@ -0,0 +1,13 @@
+/* eslint-disable import/named */
+
+import {
+  Appointment,
+} from '../../js/ui/scheduler';
+
+import {
+  ANY,
+} from './consts';
+
+const appt: Appointment = {
+  CustomProp: ANY,
+};
