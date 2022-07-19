dde3e47ffe4588f78190a676f98dfc15afb2d64e
Anton Kuznetsov <ksercs0@gmail.com>
2022-07-20 02:32:11 +0800

    TagBox: refactor to improve selection performance (#22179) (#22189)

--- testing/tests/DevExpress.core/utils.array.tests.js
+++ testing/tests/DevExpress.core/utils.array.tests.js
@@ -2,19 +2,6 @@ import arrayUtils from 'core/utils/array';
 
 QUnit.module('array utils');
 
-QUnit.testInActiveWindow('intersection', function(assert) {
-    // Edge cases
-    assert.deepEqual(arrayUtils.intersection(), []);
-    assert.deepEqual(arrayUtils.intersection([]), []);
-    assert.deepEqual(arrayUtils.intersection([], []), []);
-    assert.deepEqual(arrayUtils.intersection('a', 'b'), []);
-
-    // Normal cases
-    assert.deepEqual(arrayUtils.intersection([1, 2], [1]), [1]);
-    assert.deepEqual(arrayUtils.intersection([1], [1, 2]), [1]);
-    assert.deepEqual(arrayUtils.intersection([1, 2], [1, 2]), [1, 2]);
-});
-
 QUnit.test('merge', function(assert) {
     const array1 = [ 1, 2 ];
     const array2 = [ 3, 4 ];
