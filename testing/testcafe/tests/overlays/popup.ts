14e7b16bd310f077314edfdc2db35e9ffc6ef35f
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2022-07-20 17:17:22 +0400

    Overlays: refactor testcafe tests

--- testing/testcafe/tests/overlays/popup.ts
+++ testing/testcafe/tests/overlays/popup.ts
@@ -1,15 +1,16 @@
-import { Selector } from 'testcafe';
+import { Selector, t } from 'testcafe';
 import url from '../../helpers/getPageUrl';
-import Popup from '../../model/popup';
 import asyncForEach from '../../helpers/asyncForEach';
+import createWidget from '../../helpers/createWidget';
+import { appendElementTo, setStyleAttribute } from '../navigation/helpers/domUtils';
 
 fixture`Popup`
-  .page(url(__dirname, './pages/T920408.html'))
-  .beforeEach(async (t) => { await t.wait(10000); });
+  .page(url(__dirname, '../container.html'));
 
-test('Popup should be centered regarding the container even if container is animated (T920408)', async (t) => {
-  const outerPopup = new Popup('#popup');
-  const wrapper = outerPopup.content.find('.dx-overlay-wrapper');
+test('Popup should be centered regarding the container even if container is animated (T920408)', async () => {
+  await t.wait(500);
+
+  const wrapper = Selector('#content .dx-overlay-wrapper');
   const content = wrapper.find('.dx-overlay-content');
 
   const wrapperRect: { bottom: number; top: number; left: number; right: number } = {
@@ -36,11 +37,31 @@ test('Popup should be centered regarding the container even if container is anim
   await t
     .expect(wrapperHorizontalCenter)
     .within(contentHorizontalCenter - 0.5, contentHorizontalCenter + 0.5);
+}).before(async () => {
+  await appendElementTo('#container', 'div', 'content', {});
+  await setStyleAttribute(Selector('#content'), 'width: 100%; height: 100%;');
+  await createWidget('dxPopup', {
+    width: 600,
+    height: 400,
+    visible: true,
+  });
+
+  await appendElementTo('#container', 'div', 'innerContainer', {});
+  await t.wait(500);
+
+  return createWidget('dxPopup', {
+    position: { of: '#content' },
+    container: '#content',
+    visible: true,
+    width: 100,
+    height: 100,
+  }, false, '#innerContainer');
 });
 
-test('Popup wrapper left top corner should be the same as the container right left corner even if container is animated', async (t) => {
-  const outerPopup = new Popup('#popup');
-  const wrapper = outerPopup.content.find('.dx-overlay-wrapper');
+test('Popup wrapper left top corner should be the same as the container right left corner even if container is animated', async () => {
+  await t.wait(500);
+
+  const wrapper = Selector('#content .dx-overlay-wrapper');
   const container = wrapper.parent();
 
   const wrapperRect: { top: number; left: number } = { top: 0, left: 0 };
@@ -58,118 +79,31 @@ test('Popup wrapper left top corner should be the same as the container right le
   await t
     .expect(wrapperRect.left)
     .within(containerRect.left - 0.5, containerRect.left + 0.5);
+}).before(async () => {
+  await appendElementTo('#container', 'div', 'content', {});
+  await setStyleAttribute(Selector('#content'), 'width: 100%; height: 100%;');
+  await createWidget('dxPopup', {
+    width: 600,
+    height: 400,
+    visible: true,
+  });
+
+  await appendElementTo('#container', 'div', 'innerContainer', {});
+  await t.wait(500);
+
+  return createWidget('dxPopup', {
+    position: { of: '#content' },
+    container: '#content',
+    visible: true,
+    width: 100,
+    height: 100,
+  }, false, '#innerContainer');
 });
 
-fixture`Popup T946851`
-  .page(url(__dirname, './pages/T946851.html'))
-  .beforeEach(async (t) => { await t.wait(10000); });
-
-test('There should not be any errors when position.of is html (T946851)', async (t) => {
+test('There should not be any errors when position.of is html (T946851)', async () => {
   await t
     .expect(true).ok();
-});
-
-fixture`Popup drag borders`
-  .page(url(__dirname, './pages/popupDrag/containerBorders.html'));
-
-test('Popup can not be dragged outside of the container (window)', async (t) => {
-  const popup = new Popup('#popup');
-  const { content, toolbar } = popup;
-
-  const popupRect: { bottom: number; top: number; left: number; right: number } = {
-    bottom: 0, top: 0, left: 0, right: 0,
-  };
-
-  await t
-    .resizeWindow(700, 700)
-    .drag(toolbar, -10000, -10000);
-
-  await asyncForEach(['bottom', 'left', 'top', 'right'], async (prop) => {
-    popupRect[prop] = await content.getBoundingClientRectProperty(prop);
-  });
-
-  await t
-    .expect(popupRect.top)
-    .eql(0);
-
-  await t
-    .expect(popupRect.left)
-    .eql(0);
-
-  await t
-    .drag(toolbar, 10000, 10000);
-
-  await asyncForEach(['bottom', 'left', 'top', 'right'], async (prop) => {
-    popupRect[prop] = await content.getBoundingClientRectProperty(prop);
-  });
-
-  await t
-    .expect(popupRect.bottom)
-    .eql(700);
-
-  await t
-    .expect(popupRect.right)
-    .eql(700);
-});
-
-fixture`Popup drag in small container`
-  .page(url(__dirname, './pages/popupDrag/containerSmallerThanContent.html'));
-
-test('Popup can not be dragged if content bigger than container', async (t) => {
-  const content = Selector('.dx-overlay-content');
-  const toolbar = Selector('.dx-popup-title');
-
-  const popupPosition: { top: number; left: number } = {
-    top: 0, left: 0,
-  };
-
-  const newPopupPosition: { top: number; left: number } = {
-    top: 0, left: 0,
-  };
-
-  await asyncForEach(['left', 'top'], async (prop) => {
-    popupPosition[prop] = await content.getBoundingClientRectProperty(prop);
-  });
-
-  await t
-    .drag(toolbar, 50, 50);
-
-  await asyncForEach(['left', 'top'], async (prop) => {
-    newPopupPosition[prop] = await content.getBoundingClientRectProperty(prop);
-  });
-
-  await t
-    .expect(popupPosition.top)
-    .eql(newPopupPosition.top);
-
-  await t
-    .expect(popupPosition.left)
-    .eql(newPopupPosition.left);
-});
-
-fixture`Popup drag with enabled dragOutsideBoundary option`
-  .page(url(__dirname, './pages/popupDrag/dragOutsideBoundary.html'));
-
-test('Popup can be dragged outside of the container if dragOutsideBoundary is enabled', async (t) => {
-  const popup = new Popup('#popup');
-  const { content, toolbar } = popup;
-
-  const popupPosition: { top: number; left: number } = {
-    top: 0, left: 0,
-  };
-
-  await t
-    .drag(toolbar, -10000, -10000);
-
-  await asyncForEach(['left', 'top'], async (prop) => {
-    popupPosition[prop] = await content.getBoundingClientRectProperty(prop);
-  });
-
-  await t
-    .expect(popupPosition.top)
-    .lt(0);
-
-  await t
-    .expect(popupPosition.left)
-    .lt(0);
-});
+}).before(async () => createWidget('dxPopup', {
+  position: { of: 'html' },
+  visible: true,
+}));
--- testing/testcafe/tests/overlays/resizeObserverIntegration.ts
+++ testing/testcafe/tests/overlays/resizeObserverIntegration.ts
@@ -1,56 +1,22 @@
-import { Selector } from 'testcafe';
+import { Selector, ClientFunction } from 'testcafe';
 import url from '../../helpers/getPageUrl';
 import Popup from '../../model/popup';
 import asyncForEach from '../../helpers/asyncForEach';
-
-[
-  { pageName: 'popupDimensionChangeDuringAnimation.html', messageSuffix: 'content dimension is changed during animation' },
-  { pageName: 'popupDimensionOptionChangeDuringAnimation.html', messageSuffix: 'popup dimension option is changed during animation' },
-  { pageName: 'popupContentDimensionChange.html', messageSuffix: 'content dimension is changed' },
-].forEach(({ pageName, messageSuffix }) => {
-  fixture`Popup`
-    .page(url(__dirname, `./pages/resizeObserverIntegration/${pageName}`))
-    .beforeEach(async (t) => { await t.wait(10000); });
-
-  test(`Popup should be centered regarding the container even if ${messageSuffix}`, async (t) => {
-    const popup = new Popup('#popup');
-    const { wrapper, content } = popup;
-
-    const wrapperRect: { bottom: number; top: number; left: number; right: number } = {
-      bottom: 0, top: 0, left: 0, right: 0,
-    };
-    const contentRect: { bottom: number; top: number; left: number; right: number } = {
-      bottom: 0, top: 0, left: 0, right: 0,
-    };
-
-    await asyncForEach(['bottom', 'left', 'right', 'top'], async (prop) => {
-      wrapperRect[prop] = await wrapper.getBoundingClientRectProperty(prop);
-      contentRect[prop] = await content.getBoundingClientRectProperty(prop);
-    });
-
-    const wrapperVerticalCenter = (wrapperRect.bottom + wrapperRect.top) / 2;
-    const wrapperHorizontalCenter = (wrapperRect.left + wrapperRect.right) / 2;
-    const contentVerticalCenter = (contentRect.bottom + contentRect.top) / 2;
-    const contentHorizontalCenter = (contentRect.left + contentRect.right) / 2;
-
-    await t
-      .expect(wrapperVerticalCenter)
-      .within(contentVerticalCenter - 0.5, contentVerticalCenter + 0.5);
-
-    await t
-      .expect(wrapperHorizontalCenter)
-      .within(contentHorizontalCenter - 0.5, contentHorizontalCenter + 0.5);
-  });
-});
+import createWidget from '../../helpers/createWidget';
+import { setStyleAttribute, appendElementTo } from '../navigation/helpers/domUtils';
+import { restoreBrowserSize } from '../../helpers/restoreBrowserSize';
 
 fixture`Popup`
-  .page(url(__dirname, './pages/resizeObserverIntegration/windowResize.html'));
+  .page(url(__dirname, '../container.html'));
 
-test('popup should be repositioned after window resize', async (t) => {
-  await t.resizeWindow(200, 200);
+test('Popup should be centered regarding the container even if content dimension is changed during animation', async (t) => {
+  const popup = new Popup('#container');
 
-  const popup = new Popup('#popup');
-  const { wrapper, content } = popup;
+  await popup.show();
+  await setStyleAttribute(Selector('#content'), 'width: 300px; height: 300px;');
+
+  const wrapper = popup.getWrapper();
+  const content = popup.getContent();
 
   const wrapperRect: { bottom: number; top: number; left: number; right: number } = {
     bottom: 0, top: 0, left: 0, right: 0,
@@ -76,15 +42,139 @@ test('popup should be repositioned after window resize', async (t) => {
   await t
     .expect(wrapperHorizontalCenter)
     .within(contentHorizontalCenter - 0.5, contentHorizontalCenter + 0.5);
+}).before(async () => createWidget('dxPopup', {
+  width: 'auto',
+  height: 'auto',
+  contentTemplate: () => $('<div>').attr({ id: 'content' }).css({ width: 100, height: 100 }),
+}));
+
+test('Popup should be centered regarding the container even if popup dimension option is changed during animation', async (t) => {
+  const popup = new Popup('#container');
+
+  await popup.show();
+  await setStyleAttribute(Selector('#content'), 'width: 300px; height: 300px;');
+
+  const wrapper = popup.getWrapper();
+  const content = popup.getContent();
+
+  const wrapperRect: { bottom: number; top: number; left: number; right: number } = {
+    bottom: 0, top: 0, left: 0, right: 0,
+  };
+  const contentRect: { bottom: number; top: number; left: number; right: number } = {
+    bottom: 0, top: 0, left: 0, right: 0,
+  };
+
+  await asyncForEach(['bottom', 'left', 'right', 'top'], async (prop) => {
+    wrapperRect[prop] = await wrapper.getBoundingClientRectProperty(prop);
+    contentRect[prop] = await content.getBoundingClientRectProperty(prop);
+  });
+
+  const wrapperVerticalCenter = (wrapperRect.bottom + wrapperRect.top) / 2;
+  const wrapperHorizontalCenter = (wrapperRect.left + wrapperRect.right) / 2;
+  const contentVerticalCenter = (contentRect.bottom + contentRect.top) / 2;
+  const contentHorizontalCenter = (contentRect.left + contentRect.right) / 2;
+
+  await t
+    .expect(wrapperVerticalCenter)
+    .within(contentVerticalCenter - 0.5, contentVerticalCenter + 0.5);
+
+  await t
+    .expect(wrapperHorizontalCenter)
+    .within(contentHorizontalCenter - 0.5, contentHorizontalCenter + 0.5);
+}).before(async () => createWidget('dxPopup', {
+  width: 'auto',
+  height: 'auto',
+  contentTemplate: () => $('<div>').attr({ id: 'content' }).css({ width: 100, height: 100 }),
+}));
+
+test('Popup should be centered regarding the container even if content dimension is changed', async (t) => {
+  const popup = new Popup('#container');
+
+  await popup.show();
+  await setStyleAttribute(Selector('#content'), 'width: 300px; height: 300px;');
+
+  const wrapper = popup.getWrapper();
+  const content = popup.getContent();
+
+  const wrapperRect: { bottom: number; top: number; left: number; right: number } = {
+    bottom: 0, top: 0, left: 0, right: 0,
+  };
+  const contentRect: { bottom: number; top: number; left: number; right: number } = {
+    bottom: 0, top: 0, left: 0, right: 0,
+  };
+
+  await asyncForEach(['bottom', 'left', 'right', 'top'], async (prop) => {
+    wrapperRect[prop] = await wrapper.getBoundingClientRectProperty(prop);
+    contentRect[prop] = await content.getBoundingClientRectProperty(prop);
+  });
+
+  const wrapperVerticalCenter = (wrapperRect.bottom + wrapperRect.top) / 2;
+  const wrapperHorizontalCenter = (wrapperRect.left + wrapperRect.right) / 2;
+  const contentVerticalCenter = (contentRect.bottom + contentRect.top) / 2;
+  const contentHorizontalCenter = (contentRect.left + contentRect.right) / 2;
+
+  await t
+    .expect(wrapperVerticalCenter)
+    .within(contentVerticalCenter - 0.5, contentVerticalCenter + 0.5);
+
+  await t
+    .expect(wrapperHorizontalCenter)
+    .within(contentHorizontalCenter - 0.5, contentHorizontalCenter + 0.5);
+}).before(async () => createWidget('dxPopup', {
+  width: 'auto',
+  height: 'auto',
+  contentTemplate: () => $('<div>').attr({ id: 'content' }).css({ width: 100, height: 100 }),
+  animation: null,
+}));
+
+test('popup should be repositioned after window resize', async (t) => {
+  const popup = new Popup('#container');
+
+  const wrapper = popup.getWrapper();
+  const content = popup.getContent();
+
+  const wrapperRect: { bottom: number; top: number; left: number; right: number } = {
+    bottom: 0, top: 0, left: 0, right: 0,
+  };
+  const contentRect: { bottom: number; top: number; left: number; right: number } = {
+    bottom: 0, top: 0, left: 0, right: 0,
+  };
+
+  await asyncForEach(['bottom', 'left', 'right', 'top'], async (prop) => {
+    wrapperRect[prop] = await wrapper.getBoundingClientRectProperty(prop);
+    contentRect[prop] = await content.getBoundingClientRectProperty(prop);
+  });
+
+  const wrapperVerticalCenter = (wrapperRect.bottom + wrapperRect.top) / 2;
+  const wrapperHorizontalCenter = (wrapperRect.left + wrapperRect.right) / 2;
+  const contentVerticalCenter = (contentRect.bottom + contentRect.top) / 2;
+  const contentHorizontalCenter = (contentRect.left + contentRect.right) / 2;
+
+  await t
+    .expect(wrapperVerticalCenter)
+    .within(contentVerticalCenter - 0.5, contentVerticalCenter + 0.5);
+
+  await t
+    .expect(wrapperHorizontalCenter)
+    .within(contentHorizontalCenter - 0.5, contentHorizontalCenter + 0.5);
+}).before(async (t) => {
+  await t.resizeWindow(200, 200);
+
+  return createWidget('dxPopup', {
+    animation: null,
+    visible: true,
+    width: 100,
+    height: 100,
+  });
+}).after(async (t) => {
+  await restoreBrowserSize(t);
 });
 
-fixture`Popup width/height animation`
-  .page(url(__dirname, './pages/resizeObserverIntegration/popupSizesAnimation.html'))
-  .beforeEach(async (t) => { await t.wait(10000); });
-
 test('Popup dimensions should be correct after width or height animation', async (t) => {
-  const popup = new Popup('#popup');
-  const { content } = popup;
+  const popup = new Popup('#container');
+  const content = popup.getContent();
+
+  await t.wait(500);
 
   const contentRect: { width: number; height: number } = {
     width: 0, height: 0,
@@ -101,13 +191,22 @@ test('Popup dimensions should be correct after width or height animation', async
   await t
     .expect(contentRect.height)
     .eql(300);
-});
-
-fixture`Resize during animation`
-  .page(url(__dirname, './pages/resizeObserverIntegration/shownAfterResizeDuringAnimation.html'))
-  .beforeEach(async (t) => { await t.wait(10000); });
+}).before(async () => createWidget('dxPopup', {
+  visible: true,
+  animation: {
+    show: {
+      from: { width: '10px', height: '10px' },
+      to: { width: '300px', height: '300px' },
+    },
+  },
+}));
 
 test('Showing and shown events should be raised only once even after resize during animation', async (t) => {
+  const popup = new Popup('#container');
+
+  await popup.show();
+  await setStyleAttribute(Selector('#content'), 'width: 300px; height: 300px;');
+
   const shownCallCount = Number(await Selector('#shown_call_count').innerText);
   const showingCallCount = Number(await Selector('#showing_call_count').innerText);
 
@@ -117,4 +216,21 @@ test('Showing and shown events should be raised only once even after resize duri
   await t
     .expect(showingCallCount)
     .eql(1);
+}).before(async () => {
+  await appendElementTo('body', 'div', 'shown_call_count', {});
+  await appendElementTo('body', 'div', 'showing_call_count', {});
+
+  return createWidget('dxPopup', {
+    width: 'auto',
+    height: 'auto',
+    contentTemplate: () => $('<div>').attr({ id: 'content' }).css({ width: 100, height: 100 }),
+    onShown: ClientFunction(() => {
+      const callCount = +$('#shown_call_count').text();
+      $('#shown_call_count').text(callCount + 1);
+    }),
+    onShowing: ClientFunction(() => {
+      const callCount = +$('#showing_call_count').text();
+      $('#showing_call_count').text(callCount + 1);
+    }),
+  });
 });
