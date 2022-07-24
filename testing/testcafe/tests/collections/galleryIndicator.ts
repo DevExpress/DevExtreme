280ce01689dea0fd4700dfe679834105b9a04f35
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2022-07-24 16:06:33 +0400

    Overlays: refactor testcafe tests (#22205)

--- testing/testcafe/tests/collections/galleryIndicator.ts
+++ testing/testcafe/tests/collections/galleryIndicator.ts
@@ -5,10 +5,9 @@ import Gallery from '../../model/gallery';
 const YELLOW_PIXEL = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAMSURBVBhXYzi8wA8AA9sBsq0bEHsAAAAASUVORK5CYII=';
 const BLACK_PIXEL = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAMSURBVBhXY1hSWg4AA1EBkakDs38AAAAASUVORK5CYII=';
 const RED_PIXEL = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAMSURBVBhXY/i5aQsABQcCYPaWuk8AAAAASUVORK5CYII=';
-const createGallery = async (options): Promise<void> => createWidget('dxGallery', options, true);
 
 fixture`Click on indicator`
-  .page(url(__dirname, '../container.html'));
+  .page(url(__dirname, '../../container.html'));
 
 test('click on indicator item should change selected item', async (t) => {
   const gallery = new Gallery('#container');
@@ -17,8 +16,8 @@ test('click on indicator item should change selected item', async (t) => {
   await t
     .click(secondIndicatorItem.element)
     .expect(secondIndicatorItem.isSelected).ok();
-}).before(async () => createGallery({
+}).before(async () => createWidget('dxGallery', {
   height: 300,
   showIndicator: true,
   items: [BLACK_PIXEL, RED_PIXEL, YELLOW_PIXEL],
-}));
+}, true));
