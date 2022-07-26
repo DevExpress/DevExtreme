28e3ddc8e724887ecbd39c8b6f952ad6b5894abf
Alyar <>
2022-07-26 13:19:50 +0300

    Eliminated the faults

--- testing/tests/DevExpress.ui.widgets.dataGrid/virtualScrolling.tests.js
+++ testing/tests/DevExpress.ui.widgets.dataGrid/virtualScrolling.tests.js
@@ -951,21 +951,11 @@ QUnit.module('VirtualScrollingController. New mode', {
         assert.deepEqual(viewportParams, { skip: DEFAULT_TOTAL_ITEMS_COUNT - viewportSize, take: viewportSize }, 'viewport params');
     });
 
-    QUnit.test('getItemIndexPosition with three arguments', function(assert) {
-        // arrange
-        mockDataSource.totalItemsCount.returns(100000000);
-        this.scrollController.viewportItemSize(50);
-        this.scrollController.setContentItemSizes([new Array(15)].map(() => 50));
-
-        // act, assert
-        assert.strictEqual(this.scrollController.getItemIndexByPosition(10500, 1000, 500), 1010, 'item index');
-    });
-
     QUnit.test('viewportSize should be correct when there is a lot of data', function(assert) {
         // arrange
         mockDataSource.totalItemsCount.returns(100000000);
         this.scrollController.viewportItemSize(50);
-        this.scrollController.setContentItemSizes([new Array(15)].map(() => 50));
+        this.scrollController.setContentItemSizes([...new Array(15)].map(() => 50));
 
         // act
         this.scrollController.viewportHeight(500, 10000);
