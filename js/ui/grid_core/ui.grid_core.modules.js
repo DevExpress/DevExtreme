8f5af3ff0a73ccc20e0c882b3d770ac61cda905c
Roman Semenov <roman.semenov@devexpress.com>
2022-07-28 15:28:54 +0300

    fix jquery
    (cherry picked from commit 5f5f4d42eff467e863f0840e600a6b772e673b43)

--- js/ui/grid_core/ui.grid_core.modules.js
+++ js/ui/grid_core/ui.grid_core.modules.js
@@ -278,7 +278,7 @@ const View = ModuleItem.inherit({
     },
 
     focus: function(preventScroll) {
-        this.element().focus({ preventScroll });
+        this.element().get(0).focus({ preventScroll: preventScroll });
     }
 });
 
