a45d074b7d08af645e3fedf0e99665804c913999
Alyar <Alyar666@users.noreply.github.com>
2022-07-29 17:20:18 +0300

    DataGrid: Fix row focusing when keyboard navigation is disabled (T1105542) (#22264)
    * DataGrid: Fix row focusing when keyboard navigation is disabled (T1105542)
    
    * Fix test for IOS
    
    Co-authored-by: Alyar <>

--- testing/tests/DevExpress.ui.widgets.dataGrid/focus.integration.tests.js
+++ testing/tests/DevExpress.ui.widgets.dataGrid/focus.integration.tests.js
@@ -1552,6 +1552,33 @@ QUnit.module('Initialization', baseModuleConfig, () => {
         assert.ok(true, 'no errors');
     });
 
+    // T1105542
+    QUnit.test('Row should be focused after clicking on it when keyboardNavigation.enabled is false', function(assert) {
+        // arrange
+        const dataGrid = $('#dataGrid').dxDataGrid({
+            loadingTimeout: null,
+            dataSource: [{ id: 1, field: 'some1' }, { id: 2, field: 'some2' }],
+            keyExpr: 'id',
+            keyboardNavigation: { enabled: false },
+            focusedRowEnabled: true,
+            columns: [{
+                dataField: 'id',
+                allowEditing: false,
+            }, 'field']
+        }).dxDataGrid('instance');
+        const $cellElement = $(dataGrid.getCellElement(0, 0));
+
+        // act
+        $cellElement.trigger({
+            type: 'dxclick',
+            originalEvent: $.Event('dxclick', { target: $cellElement })
+        });
+
+        // assert
+        assert.equal(dataGrid.option('focusedRowIndex'), 0, 'focusedRowIndex');
+        assert.equal(dataGrid.option('focusedRowKey'), 1, 'focusedRowKey');
+        assert.ok($(dataGrid.getRowElement(0)).hasClass('dx-row-focused'), 'Focused row');
+    });
 });
 QUnit.module('Virtual row rendering', baseModuleConfig, () => {
     // T809900
