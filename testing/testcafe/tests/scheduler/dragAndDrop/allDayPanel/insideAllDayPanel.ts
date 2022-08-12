8c7e7beabd9cf40e54ca08c936827e792a9a055a
Anton Sermyazhko <anton.sermyazhko@devexpress.com>
2022-08-12 10:10:56 +0400

    Add test

--- testing/testcafe/tests/scheduler/dragAndDrop/allDayPanel/insideAllDayPanel.ts
+++ testing/testcafe/tests/scheduler/dragAndDrop/allDayPanel/insideAllDayPanel.ts
@@ -67,3 +67,43 @@ test('Drag-n-drop appointment without allDay field', async (t) => {
   views: ['week'],
   currentView: 'week',
 }));
+
+test('Drag-n-drop appointment after change allDay field', async (t) => {
+  const scheduler = new Scheduler('#container');
+  const draggableAppointment = scheduler.getAppointment('appt');
+
+  await t
+    .doubleClick(draggableAppointment.element)
+    .expect(scheduler.appointmentPopup.element.exists)
+    .ok()
+    .click(scheduler.appointmentPopup.allDay.element)
+    .expect(scheduler.appointmentPopup.allDay.on)
+    .ok()
+    .click(scheduler.appointmentPopup.doneButton);
+
+  await t
+    .dragToElement(draggableAppointment.element, scheduler.getAllDayTableCell(4))
+    .expect(Math.floor(await draggableAppointment.element.getBoundingClientRectProperty('height')))
+    .eql(24)
+    .expect(draggableAppointment.date.time)
+    .eql('12:00 AM - 12:01 AM');
+
+  await t
+    .doubleClick(draggableAppointment.element)
+    .expect(scheduler.appointmentPopup.element.exists)
+    .ok()
+    .expect(scheduler.appointmentPopup.allDay.on)
+    .ok();
+}).before(async () => createWidget('dxScheduler', {
+  currentDate: new Date(2021, 4, 11),
+  width: 700,
+  height: 500,
+  endDayHour: 3,
+  dataSource: [{
+    startDate: new Date(2021, 4, 10),
+    endDate: new Date(2021, 4, 12),
+    text: 'appt',
+  }],
+  views: ['week'],
+  currentView: 'week',
+}));
