ebcef78694528b47053ef91d500d3e88251faf03
Aliullov Vlad <91639107+GoodDayForSurf@users.noreply.github.com>
2022-08-23 05:02:14 -0700

    Fix export argument type (T1108890) (#22414)

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -36164,13 +36164,13 @@ declare module DevExpress.viz {
    * [descr:viz.exportWidgets(widgetInstances)]
    */
   export function exportWidgets(
-    widgetInstances: Array<Array<DOMComponent>>
+    widgetInstances: VizWidget | Array<VizWidget> | Array<Array<VizWidget>>
   ): void;
   /**
    * [descr:viz.exportWidgets(widgetInstances, options)]
    */
   export function exportWidgets(
-    widgetInstances: Array<Array<DOMComponent>>,
+    widgetInstances: VizWidget | Array<VizWidget> | Array<Array<VizWidget>>,
     options: {
       fileName?: string;
       format?: 'GIF' | 'JPEG' | 'PDF' | 'PNG' | 'SVG';
@@ -36246,7 +36246,9 @@ declare module DevExpress.viz {
   /**
    * [descr:viz.getMarkup(widgetInstances)]
    */
-  export function getMarkup(widgetInstances: Array<DOMComponent>): string;
+  export function getMarkup(
+    widgetInstances: VizWidget | Array<VizWidget> | Array<Array<VizWidget>>
+  ): string;
   /**
    * [descr:viz.getPalette(paletteName)]
    */
@@ -36602,6 +36604,16 @@ declare module DevExpress.viz {
         years?: number;
       }
     | TimeIntervalType;
+  export type VizWidget =
+    | dxChart
+    | dxPieChart
+    | dxFunnel
+    | dxSankey
+    | dxTreeMap
+    | dxVectorMap
+    | dxSparkline
+    | dxBullet
+    | dxBarGauge;
 }
 declare module DevExpress.viz.dxBarGauge {
   export type LegendItem = BarGaugeLegendItem;
