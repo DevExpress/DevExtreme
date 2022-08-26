2068b133c169250988596596d8b14d192669a155
marker dao ® <mark_nikolsky@outlook.com>
2022-08-26 20:30:55 +0400

    add testcafe scenario

--- testing/testcafe/tests/editors/tagBox/label.ts
+++ testing/testcafe/tests/editors/tagBox/label.ts
@@ -2,8 +2,10 @@ import { compareScreenshot } from 'devextreme-screenshot-comparer';
 import { changeTheme } from '../../../helpers/changeTheme';
 import url from '../../../helpers/getPageUrl';
 import createWidget from '../../../helpers/createWidget';
+import { setAttribute } from '../../navigation/helpers/domUtils';
 
 const stylingMods = ['outlined', 'underlined', 'filled'];
+const labelMods = ['static', 'floating', 'hidden'];
 const themes = ['generic.light', 'material.blue.light'];
 
 fixture`TagBox_Label`
@@ -38,6 +40,40 @@ themes.forEach((theme) => {
         ...componentOption,
         multiline: true,
       }, true, '#otherContainer');
+    });
+
+    labelMods.forEach((labelMode) => {
+      test(`Label shouldn't be cutted for dxTagBox ${theme} in stylingMode=${stylingMode}, labelMode=${labelMode} (T1104913)`, async (t) => {
+        await t.click('#container');
+
+        await t.expect(await compareScreenshot(t, `label-tag-box-styleMode=${stylingMode},labelMode=${labelMode},theme=${theme.replace(/\./g, '-')}.png`)).ok();
+      }).before(async (t) => {
+        await t.resizeWindow(300, 400);
+        await changeTheme(theme);
+
+        await setAttribute('#container', 'style', 'top: 250px');
+
+        return createWidget('dxTagBox', {
+          width: 200,
+          label: 'Label text',
+          labelMode,
+          stylingMode,
+          dataSource: {
+            load() {
+              return new Promise((resolve) => {
+                resolve([
+                  { text: 'item_1' },
+                  { text: 'item_2' },
+                  { text: 'item_3' },
+                  { text: 'item_4' },
+                ]);
+              });
+            },
+            paginate: true,
+            pageSize: 20,
+          },
+        }, true, '#container');
+      });
     });
   });
 });
