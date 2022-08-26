a1a10f34100cb209e9c361a7bf1532366277bcd4
Artemiy Dmitrochenko <54763823+xSev07@users.noreply.github.com>
2022-08-26 11:15:58 +0400

    Scheduler (T1108755): fixes agenda throws a TypeError when changing the data source of resources (#22392)
    fixed type error when changing data source
    
    changed scheduler appointment model for getting resource value by resource label
    
    Co-authored-by: artemiy.dmitrochenko <artemiy.dmitrochenko@devexpress.com>
    (cherry picked from commit 75fb48123d9bf0f3dbc97b2daf26712f2dc1ee0c)

--- testing/testcafe/tests/scheduler/agenda/editing.ts
+++ testing/testcafe/tests/scheduler/agenda/editing.ts
@@ -38,3 +38,56 @@ test('It should be possible to delete an appointment', async (t) => {
     height: 600,
   }, true);
 });
+
+test('It should be possible to change the data source of agenda resources', async (t) => {
+  const scheduler = new Scheduler('#container');
+
+  await t
+    .expect(scheduler.getAppointmentResourceByIndex(0, 'Owner'))
+    .eql('Samantha Bright')
+    .expect(scheduler.getAppointmentResourceByIndex(1, 'Owner'))
+    .eql('Todd Hoffman');
+
+  await scheduler.option('resources[0].dataSource', [{
+    text: 'Todd Hoffman',
+    id: 2,
+  },
+  ]);
+
+  await t
+    .expect(scheduler.getAppointmentResourceByIndex(0, 'Owner'))
+    .notOk()
+    .expect(scheduler.getAppointmentResourceByIndex(1, 'Owner'))
+    .eql('Todd Hoffman');
+}).before(async () => {
+  await createWidget('dxScheduler', {
+    dataSource: [
+      {
+        text: 'New Brochures',
+        ownerId: [1],
+        startDate: new Date(2021, 1, 1, 18, 30),
+        endDate: new Date(2021, 1, 1, 21, 15),
+      }, {
+        text: 'Website Re-Design Plan',
+        ownerId: [2],
+        startDate: new Date(2021, 1, 1, 23, 45),
+        endDate: new Date(2021, 1, 2, 18, 15),
+      },
+    ],
+    views: ['agenda'],
+    currentView: 'agenda',
+    currentDate: new Date(2021, 1, 1),
+    resources: [{
+      fieldExpr: 'ownerId',
+      dataSource: [{
+        text: 'Samantha Bright',
+        id: 1,
+      }, {
+        text: 'Todd Hoffman',
+        id: 2,
+      },
+      ],
+      label: 'Owner',
+    }],
+  });
+});
