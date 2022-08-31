fb790eb0109881226b195d5101325f629e0c6123
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2022-08-31 17:14:39 +0400

    Fix old aliases

--- ts/aliases.d.ts
+++ ts/aliases.d.ts
@@ -1,8 +1,8 @@
 declare module DevExpress {
     /** @deprecated Use DevExpress.events.EventObject instead */
-    export type dxEvent = DevExpress.events.EventObject
+    export type dxEvent = DevExpress.events.EventObject;
     /** @deprecated Use DevExpress.events.event instead */
-    export type event = DevExpress.events.event
+    export type event = DevExpress.events.event;
 }
 
 declare module DevExpress.viz {
@@ -15,114 +15,195 @@ declare module DevExpress.viz {
 
     /** @deprecated Use DevExpress.viz instead */
     export module charts {
+        /** @deprecated */
         export type dxChartOptions = DevExpress.viz.dxChartOptions;
+        /** @deprecated */
         export type dxChartArgumentAxis = DevExpress.viz.dxChartArgumentAxis;
+        /** @deprecated */
         export type dxChartArgumentAxisConstantLines  = DevExpress.viz.dxChartArgumentAxisConstantLines;
+        /** @deprecated */
         export type dxChartArgumentAxisConstantLinesLabel = DevExpress.viz.dxChartArgumentAxisConstantLinesLabel;
+        /** @deprecated */
         export type dxChartArgumentAxisConstantLineStyle = DevExpress.viz.dxChartArgumentAxisConstantLineStyle;
+        /** @deprecated */
         export type dxChartArgumentAxisConstantLineStyleLabel = DevExpress.viz.dxChartArgumentAxisConstantLineStyleLabel;
+        /** @deprecated */
         export type dxChartArgumentAxisLabel = DevExpress.viz.dxChartArgumentAxisLabel;
+        /** @deprecated */
         export type dxChartArgumentAxisStrips = DevExpress.viz.dxChartArgumentAxisStrips;
+        /** @deprecated */
         export type dxChartArgumentAxisStripsLabel = DevExpress.viz.dxChartArgumentAxisStripsLabel;
+        /** @deprecated */
         export type dxChartArgumentAxisTitle = DevExpress.viz.dxChartArgumentAxisTitle;
+        /** @deprecated */
         export type dxChartCommonAxisSettings = DevExpress.viz.dxChartCommonAxisSettings;
+        /** @deprecated */
         export type dxChartCommonAxisSettingsConstantLineStyle = DevExpress.viz.dxChartCommonAxisSettingsConstantLineStyle;
+        /** @deprecated */
         export type dxChartCommonAxisSettingsConstantLineStyleLabel = DevExpress.viz.dxChartCommonAxisSettingsConstantLineStyleLabel;
+        /** @deprecated */
         export type dxChartCommonAxisSettingsLabel = DevExpress.viz.dxChartCommonAxisSettingsLabel;
+        /** @deprecated */
         export type dxChartCommonAxisSettingsStripStyle = DevExpress.viz.dxChartCommonAxisSettingsStripStyle;
+        /** @deprecated */
         export type dxChartCommonAxisSettingsStripStyleLabel = DevExpress.viz.dxChartCommonAxisSettingsStripStyleLabel;
+        /** @deprecated */
         export type dxChartCommonAxisSettingsTitle = DevExpress.viz.dxChartCommonAxisSettingsTitle;
+        /** @deprecated */
         export type dxChartCommonPaneSettings = DevExpress.viz.dxChartCommonPaneSettings;
+        /** @deprecated */
         export type dxChartCommonSeriesSettings = DevExpress.viz.dxChartCommonSeriesSettings;
+        /** @deprecated */
         export type dxChartLegend = DevExpress.viz.dxChartLegend;
+        /** @deprecated */
         export type dxChartPanes = DevExpress.viz.dxChartPanes;
+        /** @deprecated */
         export type dxChartSeries = DevExpress.viz.dxChartSeries;
+        /** @deprecated */
         export type dxChartTooltip = DevExpress.viz.dxChartTooltip;
+        /** @deprecated */
         export type dxChartValueAxis = DevExpress.viz.dxChartValueAxis;
+        /** @deprecated */
         export type dxChartValueAxisConstantLines = DevExpress.viz.dxChartValueAxisConstantLines;
+        /** @deprecated */
         export type dxChartValueAxisConstantLinesLabel = DevExpress.viz.dxChartValueAxisConstantLinesLabel;
+        /** @deprecated */
         export type dxChartValueAxisConstantLineStyle = DevExpress.viz.dxChartValueAxisConstantLineStyle;
+        /** @deprecated */
         export type dxChartValueAxisConstantLineStyleLabel = DevExpress.viz.dxChartValueAxisConstantLineStyleLabel;
+        /** @deprecated */
         export type dxChartValueAxisLabel = DevExpress.viz.dxChartValueAxisLabel;
+        /** @deprecated */
         export type dxChartValueAxisStrips = DevExpress.viz.dxChartValueAxisStrips;
+        /** @deprecated */
         export type dxChartValueAxisStripsLabel = DevExpress.viz.dxChartValueAxisStripsLabel;
+        /** @deprecated */
         export type dxChartValueAxisTitle = DevExpress.viz.dxChartValueAxisTitle;
 
+        /** @deprecated */
         export type dxPieChartOptions = DevExpress.viz.dxPieChartOptions;
+        /** @deprecated */
         export type dxPieChartAdaptiveLayout = DevExpress.viz.dxPieChartAdaptiveLayout;
+        /** @deprecated */
         export type dxPieChartLegend = DevExpress.viz.dxPieChartLegend;
+        /** @deprecated */
         export type dxPieChartSeries = DevExpress.viz.dxPieChartSeries;
 
+        /** @deprecated */
         export type dxPolarChartOptions = DevExpress.viz.dxPolarChartOptions;
+        /** @deprecated */
         export type dxPolarChartAdaptiveLayout = DevExpress.viz.dxPolarChartAdaptiveLayout;
+        /** @deprecated */
         export type dxPolarChartArgumentAxis = DevExpress.viz.dxPolarChartArgumentAxis;
+        /** @deprecated */
         export type dxPolarChartArgumentAxisConstantLines = DevExpress.viz.dxPolarChartArgumentAxisConstantLines;
+        /** @deprecated */
         export type dxPolarChartArgumentAxisConstantLinesLabel = DevExpress.viz.dxPolarChartArgumentAxisConstantLinesLabel;
+        /** @deprecated */
         export type dxPolarChartArgumentAxisLabel = DevExpress.viz.dxPolarChartArgumentAxisLabel;
+        /** @deprecated */
         export type dxPolarChartArgumentAxisStrips = DevExpress.viz.dxPolarChartArgumentAxisStrips;
+        /** @deprecated */
         export type dxPolarChartArgumentAxisStripsLabel = DevExpress.viz.dxPolarChartArgumentAxisStripsLabel;
+        /** @deprecated */
         export type dxPolarChartCommonAxisSettings = DevExpress.viz.dxPolarChartCommonAxisSettings;
+        /** @deprecated */
         export type dxPolarChartCommonAxisSettingsConstantLineStyle = DevExpress.viz.dxPolarChartCommonAxisSettingsConstantLineStyle;
+        /** @deprecated */
         export type dxPolarChartCommonAxisSettingsConstantLineStyleLabel = DevExpress.viz.dxPolarChartCommonAxisSettingsConstantLineStyleLabel;
+        /** @deprecated */
         export type dxPolarChartCommonAxisSettingsLabel = DevExpress.viz.dxPolarChartCommonAxisSettingsLabel;
+        /** @deprecated */
         export type dxPolarChartCommonAxisSettingsStripStyle = DevExpress.viz.dxPolarChartCommonAxisSettingsStripStyle;
+        /** @deprecated */
         export type dxPolarChartCommonAxisSettingsStripStyleLabel = DevExpress.viz.dxPolarChartCommonAxisSettingsStripStyleLabel;
+        /** @deprecated */
         export type dxPolarChartCommonAxisSettingsTick = DevExpress.viz.dxPolarChartCommonAxisSettingsTick;
+        /** @deprecated */
         export type dxPolarChartCommonSeriesSettings = DevExpress.viz.dxPolarChartCommonSeriesSettings;
+        /** @deprecated */
         export type dxPolarChartLegend = DevExpress.viz.dxPolarChartLegend;
+        /** @deprecated */
         export type dxPolarChartSeries = DevExpress.viz.dxPolarChartSeries;
+        /** @deprecated */
         export type dxPolarChartTooltip = DevExpress.viz.dxPolarChartTooltip;
+        /** @deprecated */
         export type dxPolarChartValueAxis = DevExpress.viz.dxPolarChartValueAxis;
+        /** @deprecated */
         export type dxPolarChartValueAxisConstantLines = DevExpress.viz.dxPolarChartValueAxisConstantLines;
+        /** @deprecated */
         export type dxPolarChartValueAxisConstantLinesLabel = DevExpress.viz.dxPolarChartValueAxisConstantLinesLabel;
+        /** @deprecated */
         export type dxPolarChartValueAxisLabel = DevExpress.viz.dxPolarChartValueAxisLabel;
+        /** @deprecated */
         export type dxPolarChartValueAxisStrips = DevExpress.viz.dxPolarChartValueAxisStrips;
+        /** @deprecated */
         export type dxPolarChartValueAxisStripsLabel = DevExpress.viz.dxPolarChartValueAxisStripsLabel;
+        /** @deprecated */
         export type dxPolarChartValueAxisTick = DevExpress.viz.dxPolarChartValueAxisTick;
     }
 
     /** @deprecated Use DevExpress.viz instead */
     export module funnel {
+        /** @deprecated */
         export type dxFunnelOptions = DevExpress.viz.dxFunnelOptions;
+        /** @deprecated */
         export type dxFunnelTooltip = DevExpress.viz.dxFunnelTooltip;
     }
 
     /** @deprecated Use DevExpress.viz instead */
     export module gauges {
+        /** @deprecated */
         export type dxCircularGaugeOptions = DevExpress.viz.dxCircularGaugeOptions;
+        /** @deprecated */
         export type dxCircularGaugeRangeContainer = DevExpress.viz.dxCircularGaugeRangeContainer;
+        /** @deprecated */
         export type dxCircularGaugeScale = DevExpress.viz.dxCircularGaugeScale;
+        /** @deprecated */
         export type dxCircularGaugeScaleLabel = DevExpress.viz.dxCircularGaugeScaleLabel;
 
+        /** @deprecated */
         export type dxLinearGaugeOptions = DevExpress.viz.dxLinearGaugeOptions;
+        /** @deprecated */
         export type dxLinearGaugeRangeContainer = DevExpress.viz.dxLinearGaugeRangeContainer;
+        /** @deprecated */
         export type dxLinearGaugeScale = DevExpress.viz.dxLinearGaugeScale;
+        /** @deprecated */
         export type dxLinearGaugeScaleLabel = DevExpress.viz.dxLinearGaugeScaleLabel;
 
+        /** @deprecated */
         export type dxBarGaugeOptions = DevExpress.viz.dxBarGaugeOptions;
+        /** @deprecated */
         export type dxBarGaugeTooltip = DevExpress.viz.dxBarGaugeTooltip;
     }
 
     /** @deprecated Use DevExpress.viz instead */
     export module rangeSelector {
+        /** @deprecated */
         export type dxRangeSelectorOptions = DevExpress.viz.dxRangeSelectorOptions;
     }
 
     /** @deprecated Use DevExpress.viz instead */
     export module sparklines {
+        /** @deprecated */
         export type dxSparklineOptions = DevExpress.viz.dxSparklineOptions;
+        /** @deprecated */
         export type dxBulletOptions = DevExpress.viz.dxBulletOptions;
     }
 
     /** @deprecated Use DevExpress.viz instead */
     export module map {
+        /** @deprecated */
         export type dxVectorMapOptions = DevExpress.viz.dxVectorMapOptions;
+        /** @deprecated */
         export type dxVectorMapTooltip = DevExpress.viz.dxVectorMapTooltip;
     }
 
     /** @deprecated Use DevExpress.viz instead */
     export module treeMap {
+        /** @deprecated */
         export type dxTreeMapOptions = DevExpress.viz.dxTreeMapOptions;
+        /** @deprecated */
         export type dxTreeMapTooltip = DevExpress.viz.dxTreeMapTooltip;
     }
 }
