ae5b25a37048dfcbc2623b7b58efca90e368b783
eldar.iusupzhanov <eldar.iusupzhanov@devexpress.com>
2022-09-06 11:35:31 +0400

    fixes
    (cherry picked from commit 470f7e8f46d1050a21e40e19d70eb8442a8a4597)

--- js/ui/data_grid.d.ts
+++ js/ui/data_grid.d.ts
@@ -1462,6 +1462,11 @@ export interface Pager {
      * @default "auto"
      */
     visible?: boolean | Mode;
+    /**
+     * @docid GridBaseOptions.pager.ariaLabel
+     * @default "Navigation"
+     */
+    ariaLabel?: string;
 }
 
 /**
