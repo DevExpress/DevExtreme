80b1920bb5af78d0e320b0a38003f7224f80f25d
Artemiy Dmitrochenko <54763823+xSev07@users.noreply.github.com>
2022-09-06 12:04:49 +0400

    Scheduler: extends load options in DataSource (#22544)
    Added view dates to load options
    
    Co-authored-by: artemiy.dmitrochenko <artemiy.dmitrochenko@devexpress.com>

--- testing/testcafe/tests/scheduler/dataSource/load.ts
+++ testing/testcafe/tests/scheduler/dataSource/load.ts
@@ -1,3 +1,4 @@
+import { ClientFunction } from 'testcafe';
 import Scheduler from '../../../model/scheduler';
 import createWidget from '../../../helpers/createWidget';
 import url from '../../../helpers/getPageUrl';
@@ -45,3 +46,86 @@ test('it should correctly load items with post processing', async (t) => {
   },
   true,
 ));
+
+declare global {
+  interface Window {
+    testOptions: {
+      startDate: Date;
+      endDate: Date;
+    };
+  }
+}
+
+const getWindow = ClientFunction(() => window.testOptions);
+
+[true, false].forEach((groupByDate) => {
+  test(`it should have start and end date in load options groupByDate=${groupByDate}`, async (t) => {
+    const win = await getWindow();
+    await t
+      .expect(win.startDate)
+      .eql(new Date(2021, 4, 9))
+      .expect(win.endDate)
+      .eql(new Date(2021, 4, 15, 2, 59));
+  }).before(async () => createWidget(
+    'dxScheduler',
+    {
+      dataSource: {
+        load: (loadOptions) => {
+          const { startDate, endDate } = loadOptions;
+
+          // added dates to global scope because there isn't another acceptable way to test them
+          window.testOptions = {
+            startDate,
+            endDate,
+          };
+        },
+      },
+      currentDate: new Date(2021, 4, 11),
+      width: 700,
+      height: 500,
+      startDayHour: 0,
+      endDayHour: 3,
+      groupByDate,
+      views: ['week'],
+      currentView: 'week',
+    },
+    true,
+  ));
+
+  test(`it should have dates in load options when view dates changing. groupByDate=${groupByDate}`, async (t) => {
+    const { toolbar } = new Scheduler('#container');
+
+    await t
+      .click(toolbar.navigator.nextButton);
+    const win = await getWindow();
+    await t
+      .expect(win.startDate)
+      .eql(new Date(2021, 4, 16))
+      .expect(win.endDate)
+      .eql(new Date(2021, 4, 22, 2, 59));
+  }).before(async () => createWidget(
+    'dxScheduler',
+    {
+      dataSource: {
+        load: (loadOptions) => {
+          const { startDate, endDate } = loadOptions;
+
+          // added dates to global scope because there isn't another acceptable way to test them
+          window.testOptions = {
+            startDate,
+            endDate,
+          };
+        },
+      },
+      currentDate: new Date(2021, 4, 11),
+      width: 700,
+      height: 500,
+      startDayHour: 0,
+      endDayHour: 3,
+      groupByDate,
+      views: ['week'],
+      currentView: 'week',
+    },
+    true,
+  ));
+});
