1c671db284189812ba3c1253cc19641be87b594f
eldar.iusupzhanov <eldar.iusupzhanov@devexpress.com>
2022-09-06 11:41:50 +0400

    ariaLabel to label
    (cherry picked from commit 93b19bea2890655691bdfb50d4b6e4599034ff2a)

--- js/ui/data_grid.d.ts
+++ js/ui/data_grid.d.ts
@@ -1419,10 +1419,10 @@ export interface Pager {
      */
     visible?: boolean | Mode;
     /**
-     * @docid GridBaseOptions.pager.ariaLabel
+     * @docid GridBaseOptions.pager.label
      * @default "Navigation"
      */
-    ariaLabel?: string;
+    label?: string;
 }
 
 export interface SearchPanel {
