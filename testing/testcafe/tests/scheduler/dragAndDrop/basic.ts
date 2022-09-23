4ef9d9357758ac45a4db74ccd3658d4bfd85a188
Ilya Vinogradov <48182348+williamvinogradov@users.noreply.github.com>
2022-09-23 04:44:57 -0700

    Scheduler: Fix drag-n-drop an appointment to the left column (#22704)
    Co-authored-by: Ilya Vinogradov <ilya.vinogradov@devexpress.com>

--- testing/testcafe/tests/scheduler/dragAndDrop/basic.ts
+++ testing/testcafe/tests/scheduler/dragAndDrop/basic.ts
@@ -1,3 +1,4 @@
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import { dataSource } from './init/widget.data';
 import createScheduler from './init/widget.setup';
 import url from '../../../helpers/getPageUrl';
@@ -121,3 +122,36 @@ test('Drag recurrent appointment occurrence from collector (T832887)', async (t)
   }],
   currentDate: new Date(2019, 2, 26),
 }));
+
+test('Drag-n-drop the appointment to the left column to the cell that has the same time', async (t) => {
+  const scheduler = new Scheduler('#container');
+  const screenshotZone = scheduler.workSpace;
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+  const draggableAppointment = scheduler.getAppointment('Test appointment');
+
+  await t
+    .dragToElement(
+      draggableAppointment.element,
+      scheduler.getDateTableCell(2, 2), { speed: 0.5 },
+    );
+
+  await takeScreenshot('drag-n-drop-appointment-to-left-column.png', screenshotZone);
+
+  await t.expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => {
+  await createScheduler({
+    timeZone: 'Etc/GMT',
+    dataSource: [{
+      text: 'Test appointment',
+      startDate: new Date('2022-09-08T10:00:00.000Z'),
+      endDate: new Date('2022-09-08T10:30:00.000Z'),
+    }],
+    views: ['week'],
+    currentView: 'week',
+    currentDate: new Date('2022-09-09T10:00:00.000Z'),
+    startDayHour: 9,
+    width: 600,
+    height: 600,
+  });
+});
