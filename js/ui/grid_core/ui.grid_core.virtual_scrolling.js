6d426e2eae3483095896d1354ed9fb23e2bc67bd
evgenii.ovchinnikov <9976332+yuyugene@users.noreply.github.com>
2022-10-11 19:12:51 +0400

    DataGrid - Reinitialize viewportParams and pageLoadSize on filter application (T1118229) (#22826)
    DataGrid - Reinitialize viewportParams and pageLoadSize on filter application (T1118229)

--- js/ui/grid_core/ui.grid_core.virtual_scrolling.js
+++ js/ui/grid_core/ui.grid_core.virtual_scrolling.js
@@ -1554,6 +1554,10 @@ export const virtualScrollingModule = {
                         this._itemCount = 0;
                         this._allItems = null;
                         this.callBase.apply(this, arguments);
+                    },
+                    _applyFilter: function() {
+                        this._dataSource?.loadPageCount(1);
+                        this.callBase.apply(this, arguments);
                     }
                 };
 
