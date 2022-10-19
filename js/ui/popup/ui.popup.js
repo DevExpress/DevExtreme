a75e4378011636459f89f1881e6b68a97d34d56c
marker dao ® | Roman Ty <mark_nikolsky@outlook.com>
2022-10-19 12:21:54 +0400

    Rename const

--- js/ui/popup/ui.popup.js
+++ js/ui/popup/ui.popup.js
@@ -259,13 +259,13 @@ const Popup = Overlay.inherit({
 
     _init: function() {
         const popupWrapperClassExternal = this.option('_wrapperClassExternal');
-        const wrapperClasses = [POPUP_WRAPPER_CLASS, popupWrapperClassExternal].filter(Boolean).join(' ');
+        const popupWrapperClasses = [POPUP_WRAPPER_CLASS, popupWrapperClassExternal].filter(Boolean).join(' ');
 
         this.callBase();
         this._updateResizeCallbackSkipCondition();
 
         this.$element().addClass(POPUP_CLASS);
-        this.$wrapper().addClass(wrapperClasses);
+        this.$wrapper().addClass(popupWrapperClasses);
         this._$popupContent = this._$content
             .wrapInner($('<div>').addClass(POPUP_CONTENT_CLASS))
             .children().eq(0);
