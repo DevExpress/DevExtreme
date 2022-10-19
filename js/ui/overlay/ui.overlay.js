0065e327039c9664d60f24cae4590ad0c11b2125
ksercs <ksercs0@gmail.com>
2022-10-19 17:45:56 +0400

    Overlay: reposition content after async template is rendered (T1114344)

--- js/ui/overlay/ui.overlay.js
+++ js/ui/overlay/ui.overlay.js
@@ -942,6 +942,9 @@ const Overlay = Widget.inherit({
             transclude,
             onRendered: () => {
                 whenContentRendered.resolve();
+                if(this.option('templatesRenderAsynchronously')) {
+                    this._dimensionChanged();
+                }
             }
         });
 
