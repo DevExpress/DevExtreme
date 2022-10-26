a43a319b986da7049e803636cacb04f522d6d918
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2022-10-26 15:44:22 +0300

    refactor calendar

--- testing/tests/DevExpress.ui.widgets.editors/datebox.tests.js
+++ testing/tests/DevExpress.ui.widgets.editors/datebox.tests.js
@@ -1471,6 +1471,7 @@ QUnit.module('widget sizing render', {}, () => {
         const $element = $('#dateBox').appendTo($parent);
         const component = $('#dateBox').dxDateBox({
             width: undefined,
+            pickerType: 'calendar',
             showDropDownButton: false
         }).dxDateBox('instance');
         const { width: initialWidth } = $element.get(0).getBoundingClientRect();
