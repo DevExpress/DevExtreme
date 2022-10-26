fda2bec23da965e2fd758b9e72de0fef1201aa32
Gagik Harutyunyan <53352872+gagharutyunyan1993@users.noreply.github.com>
2022-10-26 20:26:20 +0400

    optimized toMilliseconds function

--- js/renovation/ui/common/utils/date/toMilliseconds.ts
+++ js/renovation/ui/common/utils/date/toMilliseconds.ts
@@ -9,17 +9,18 @@ type TimeUnit =
   | 'quarter'
   | 'year';
 
+const timeIntervals: Record<TimeUnit, number> = {
+  millisecond: 1,
+  second: 1000,
+  minute: 1000 * 60,
+  hour: 1000 * 60 * 60,
+  day: 1000 * 60 * 60 * 24,
+  week: 1000 * 60 * 60 * 24 * 7,
+  month: 1000 * 60 * 60 * 24 * 30,
+  quarter: 1000 * 60 * 60 * 24 * 30 * 3,
+  year: 1000 * 60 * 60 * 24 * 365,
+};
+
 export function toMilliseconds(value: TimeUnit): number {
-  const timeIntervals: { [key: string]: number } = {
-    millisecond: 1,
-    second: 1000,
-    minute: 1000 * 60,
-    hour: 1000 * 60 * 60,
-    day: 1000 * 60 * 60 * 24,
-    week: 1000 * 60 * 60 * 24 * 7,
-    month: 1000 * 60 * 60 * 24 * 30,
-    quarter: 1000 * 60 * 60 * 24 * 30 * 3,
-    year: 1000 * 60 * 60 * 24 * 365,
-  };
   return timeIntervals[value];
 }
