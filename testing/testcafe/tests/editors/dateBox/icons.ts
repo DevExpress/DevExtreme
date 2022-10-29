114b868b82815392ef6667190a94971eaabff605
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2022-10-29 09:33:08 +0300

    replace etalons

--- testing/testcafe/tests/editors/dateBox/icons.ts
+++ testing/testcafe/tests/editors/dateBox/icons.ts
@@ -1,14 +1,18 @@
+/* eslint-disable no-restricted-syntax */
 import { compareScreenshot } from 'devextreme-screenshot-comparer';
 import { changeTheme } from '../../../helpers/changeTheme';
 import url from '../../../helpers/getPageUrl';
 import createWidget from '../../../helpers/createWidget';
 import DateBox from '../../../model/dateBox';
+import Guid from '../../../../../js/core/guid';
+import { appendElementTo } from '../../navigation/helpers/domUtils';
 
 const TIME_TO_WAIT = 500;
 
-const stylingMods = ['outlined', 'underlined', 'filled'];
+const stylingModes = ['outlined', 'underlined', 'filled'];
 const themes = ['generic.light', 'material.blue.light'];
 const pickerTypes = ['calendar', 'list', 'native', 'rollers'];
+const labelModes = ['static', 'floating', 'hidden'];
 const types = ['date', 'datetime', 'time'];
 
 fixture`DateBox_Icon`
@@ -18,14 +22,43 @@ fixture`DateBox_Icon`
   });
 
 themes.forEach((theme) => {
-  stylingMods.forEach((stylingMode) => {
+  test(`DateBox styles ${theme}`, async (t) => {
+    await t.expect(await compareScreenshot(t, `datebox-styling,theme=${theme.replace(/\./g, '-')}.png`, '#container')).ok();
+  }).before(async () => {
+    await changeTheme(theme);
+
+    for (const stylingMode of stylingModes) {
+      for (const type of types) {
+        for (const pickerType of pickerTypes) {
+          for (const rtlEnabled of [true, false]) {
+            for (const labelMode of labelModes) {
+              const id = `${new Guid()}`;
+
+              await appendElementTo('#container', 'div', id, { });
+              await createWidget('dxDateBox', {
+                label: 'label text',
+                labelMode,
+                stylingMode,
+                showClearButton: true,
+                pickerType,
+                type,
+                rtlEnabled,
+                value: new Date(2021, 9, 17, 16, 34),
+              }, false, `#${id}`);
+            }
+          }
+        }
+      }
+    }
+  });
+
+  stylingModes.forEach((stylingMode) => {
     pickerTypes.forEach((pickerType) => {
       types.forEach((type) => {
         [true, false].forEach((rtlEnabled) => {
           test(`Icon for dxDateBox ${theme} stylingMode=${stylingMode} pickerType=${pickerType} type=${type} rtlEnabled=${rtlEnabled}`, async (t) => {
             const dateBox = new DateBox('#container');
             const { dropDownEditorButton } = dateBox;
-            await t.expect(await compareScreenshot(t, `db-icon-stMode=${stylingMode},pType=${pickerType},type=${type},rtl=${rtlEnabled},theme=${theme.replace(/\./g, '-')}.png`)).ok();
 
             if (pickerType !== 'native') {
               await t
