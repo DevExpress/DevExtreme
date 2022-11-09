e3297c916d28438fba3ddce7c5b9361b00d73fea
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2022-11-10 02:42:33 +0400

    Button: container should be stretched if the font-size of text is large

--- testing/testcafe/tests/editors/button/common.ts
+++ testing/testcafe/tests/editors/button/common.ts
@@ -3,8 +3,12 @@ import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import url from '../../../helpers/getPageUrl';
 import createWidget from '../../../helpers/createWidget';
 import { changeTheme } from '../../../helpers/changeTheme';
-import { appendElementTo } from '../../navigation/helpers/domUtils';
+import { appendElementTo, insertStylesheetRule, deleteStylesheetRule } from '../../navigation/helpers/domUtils';
 import Guid from '../../../../../js/core/guid';
+
+const BUTTON_CLASS = 'dx-button';
+const BUTTON_TEXT_CLASS = 'dx-button-text';
+const ICON_CLASS = 'dx-icon';
 
 const stylingModes = ['text', 'outlined', 'contained'];
 const types = ['back', 'danger', 'default', 'normal', 'success'];
@@ -16,6 +20,7 @@ fixture`Button`
   });
 
 const themes = ['generic.light', 'generic.light.compact', 'material.blue.light', 'material.blue.light.compact'];
+
 themes.forEach((theme) => {
   test(`Buttons render (${theme})`, async (t) => {
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
@@ -25,39 +30,26 @@ themes.forEach((theme) => {
       .ok()
       .expect(compareResults.isValid())
       .ok(compareResults.errorMessages());
-  }).before(async () => {
-    await changeTheme(theme);
 
-    for (const stylingMode of stylingModes) {
-      for (const type of types) {
-        for (const text of ['Button Text', '']) {
-          for (const icon of ['home', undefined]) {
-            for (const rtlEnabled of [true, false]) {
-              const id = `${new Guid()}`;
-
-              await appendElementTo('#container', 'div', id, { });
-              await createWidget('dxButton', {
-                stylingMode,
-                text,
-                type,
-                rtlEnabled,
-                icon,
-              }, false, `#${id}`);
-            }
-          }
-        }
-      }
-    }
-  });
-
-  test(`Buttons render with overflow (${theme})`, async (t) => {
-    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+    await insertStylesheetRule(`.${BUTTON_TEXT_CLASS} { width: 70px }`, 0);
 
     await t
       .expect(await takeScreenshot(`buttons-render-with-overflow-theme=${theme.replace(/\./g, '-')}.png`, '#container'))
       .ok()
       .expect(compareResults.isValid())
       .ok(compareResults.errorMessages());
+
+    await deleteStylesheetRule(0);
+
+    await insertStylesheetRule(`.${BUTTON_TEXT_CLASS}, .${BUTTON_CLASS} .${ICON_CLASS} { font-size: 26px }`, 0);
+
+    await t
+      .expect(await takeScreenshot(`buttons-stretch-of-large-text-theme=${theme.replace(/\./g, '-')}.png`, '#container'))
+      .ok()
+      .expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
+
+    await deleteStylesheetRule(0);
   }).before(async () => {
     await changeTheme(theme);
 
@@ -70,7 +62,6 @@ themes.forEach((theme) => {
 
               await appendElementTo('#container', 'div', id, { });
               await createWidget('dxButton', {
-                width: 70,
                 stylingMode,
                 text,
                 type,
