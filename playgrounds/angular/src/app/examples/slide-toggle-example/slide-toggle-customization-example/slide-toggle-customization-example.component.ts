5381b2753878b9bbc7890e56cf7b940689144c1d
iBat <git@bulychev.pro>
2022-11-09 10:40:00 +0400

    fix angular playground

--- playgrounds/angular/src/app/examples/slide-toggle-example/slide-toggle-customization-example/slide-toggle-customization-example.component.ts
+++ playgrounds/angular/src/app/examples/slide-toggle-example/slide-toggle-customization-example/slide-toggle-customization-example.component.ts
@@ -1,40 +1,40 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { TTextPosition } from '@devexpress/core/lib/components/slideToggle';
+import { TTextPosition } from '@devexpress/core/lib/esm/components/slideToggle';
 import { BehaviorSubject, take } from 'rxjs';
 import { CustomIndicatorComponent, CustomTextComponent } from '../custom-components';
 
 
 @Component({
-  selector: 'app-slide-toggle-customization-example',
-  templateUrl: './slide-toggle-customization-example.component.html',
-  styleUrls: ['./slide-toggle-customization-example.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-slide-toggle-customization-example',
+    templateUrl: './slide-toggle-customization-example.component.html',
+    styleUrls: ['./slide-toggle-customization-example.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SlideToggleCustomizationExampleComponent {
-  private controlConfig = new BehaviorSubject({
-    text: 'Component',
-    textPosition: 'right' as TTextPosition,
-  });
-
-  controlConfig$ = this.controlConfig.asObservable();
-  indicatorView = CustomIndicatorComponent;
-  textView = CustomTextComponent;
-
-  changeControlText(event: Event): void {
-    this.controlConfig.pipe(take(1)).subscribe((config) => {
-      this.controlConfig.next({
-        ...config,
-        text: (event.target as HTMLInputElement)?.value
-      });
-    })
-  }
-
-  changeControlTextPosition(event: Event): void {
-    this.controlConfig.pipe(take(1)).subscribe((config) => {
-      this.controlConfig.next({
-        ...config,
-        textPosition: ((event.target as HTMLSelectElement)?.value || 'right' as unknown) as TTextPosition,
-      })
+    private controlConfig = new BehaviorSubject({
+        text: 'Component',
+        textPosition: 'right' as TTextPosition
     });
-  }
+
+    controlConfig$ = this.controlConfig.asObservable();
+    indicatorView = CustomIndicatorComponent;
+    textView = CustomTextComponent;
+
+    changeControlText(event: Event): void {
+        this.controlConfig.pipe(take(1)).subscribe((config) => {
+            this.controlConfig.next({
+                ...config,
+                text: (event.target as HTMLInputElement)?.value
+            });
+        });
+    }
+
+    changeControlTextPosition(event: Event): void {
+        this.controlConfig.pipe(take(1)).subscribe((config) => {
+            this.controlConfig.next({
+                ...config,
+                textPosition: ((event.target as HTMLSelectElement)?.value || 'right' as unknown) as TTextPosition
+            });
+        });
+    }
 }
