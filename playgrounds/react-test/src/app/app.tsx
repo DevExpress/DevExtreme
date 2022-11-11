07f03a34ed66ab76fddaacc988dbc58c9d2bcb3e
iBat <git@bulychev.pro>
2022-11-11 15:19:15 +0400

    NX generated test projects

---
+++ playgrounds/react-test/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from "./app.module.scss";
+import NxWelcome from "./nx-welcome";
+
+export function App() {
+    return (
+        <>
+            <NxWelcome title="react-test" />
+            <div />
+        </>
+    );
+}
+
+export default App;
