80544e81b94d68b87e79efc3afcf3bae4d7e4412
Alyar <Alyar666@users.noreply.github.com>
2022-11-14 12:27:18 +0300

    DataGrid: Refix flickering/blinking on paging, sorting and other data operations in React 18 (T1107403, T1112852, T1100603) (#23196)
    Co-authored-by: Alyar <>

--- testing/tests/DevExpress.knockout/dataGrid.tests.js
+++ testing/tests/DevExpress.knockout/dataGrid.tests.js
@@ -244,7 +244,6 @@ QUnit.module('Work with knockout', {
             getCellTextCallCount++;
             return options.rowIndex + ' - ' + options.text;
         };
-        this.viewModel.gridOptions.renderAsync = undefined;
         this.viewModel.gridOptions.columns[0] = {
             dataField: 'field1',
             cssClass: 'test-cell',
