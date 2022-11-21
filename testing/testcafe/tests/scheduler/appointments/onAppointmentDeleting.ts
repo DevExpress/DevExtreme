5d78010cf187aaed9d683eebc17b372f070705ce
Gagik Harutyunyan <gagharutyunyan1993@gmail.com>
2022-11-21 18:53:00 +0400

    refactored test logic

--- testing/testcafe/tests/scheduler/appointments/onAppointmentDeleting.ts
+++ testing/testcafe/tests/scheduler/appointments/onAppointmentDeleting.ts
@@ -1,6 +1,10 @@
+import { ClientFunction } from 'testcafe';
 import Scheduler from '../../../model/scheduler';
 import createWidget from '../../../helpers/createWidget';
 import url from '../../../helpers/getPageUrl';
+
+fixture`Display arguments in appointment templates and events`
+  .page(url(__dirname, '../../container.html'));
 
 const data = [
   {
@@ -10,51 +14,68 @@ const data = [
   },
 ];
 
-const createScheduler = (deletingOption: (e) => void) => ({
-  dataSource: data,
-  views: ['day'],
-  currentView: 'day',
-  currentDate: new Date(2021, 3, 27),
-  startDayHour: 1,
-  endDayHour: 7,
-  height: 600,
-  cellDuration: 30,
-  onAppointmentDeleting: deletingOption,
+[{
+  cancel: false,
+  expectedCount: 0,
+}, {
+  cancel: true,
+  expectedCount: 1,
+}].forEach(({ cancel, expectedCount }) => {
+  test('based on the boolean value must delete or leave the appointment', async (t) => {
+    const scheduler = new Scheduler('#container');
+    const appointment = scheduler.getAppointment('Brochure Design Review');
+    const { appointmentTooltip } = scheduler;
+
+    await t
+      .click(appointment.element)
+      .expect(appointmentTooltip.isVisible())
+      .ok()
+      .click(appointmentTooltip.deleteButton)
+      .expect(scheduler.getAppointmentCount())
+      .eql(expectedCount);
+  }).before(async () => createWidget('dxScheduler', {
+    dataSource: data,
+    views: ['day'],
+    currentView: 'day',
+    currentDate: new Date(2021, 3, 27),
+    startDayHour: 1,
+    endDayHour: 7,
+    height: 600,
+    cellDuration: 30,
+    onAppointmentDeleting: ClientFunction((e) => {
+      e.cancel = cancel;
+    }, {
+      dependencies: { cancel },
+    }),
+  }, true));
+
+  test('should leave or delete appointment if the Promise has been resolved', async (t) => {
+    const scheduler = new Scheduler('#container');
+    const appointment = scheduler.getAppointment('Brochure Design Review');
+    const { appointmentTooltip } = scheduler;
+
+    await t
+      .click(appointment.element)
+      .expect(appointmentTooltip.isVisible())
+      .ok()
+      .click(appointmentTooltip.deleteButton)
+      .expect(scheduler.getAppointmentCount())
+      .eql(expectedCount);
+  }).before(async () => createWidget('dxScheduler', {
+    dataSource: data,
+    views: ['day'],
+    currentView: 'day',
+    currentDate: new Date(2021, 3, 27),
+    startDayHour: 1,
+    endDayHour: 7,
+    height: 600,
+    cellDuration: 30,
+    onAppointmentDeleting: ClientFunction((e) => {
+      e.cancel = new Promise((resolve) => {
+        resolve(cancel);
+      });
+    }, {
+      dependencies: { cancel },
+    }),
+  }, true));
 });
-
-const testOptions = (count: number) => async (t) => {
-  const scheduler = new Scheduler('#container');
-  const appointment = scheduler.getAppointment('Brochure Design Review');
-  const { appointmentTooltip } = scheduler;
-
-  await t
-    .click(appointment.element, { speed: 0.1 })
-    .expect(appointmentTooltip.exists)
-    .ok()
-    .click(appointmentTooltip.deleteButton, { speed: 0.1 })
-    .expect(scheduler.getAppointmentCount())
-    .eql(count);
-};
-
-fixture`Display* arguments in appointment templates and events`
-  .page(url(__dirname, '../../container.html'));
-
-test('should delete appointment if mode is "false"', testOptions(0)).before(async () => createWidget('dxScheduler', createScheduler((e) => {
-  e.cancel = false;
-}), true));
-
-test('should delete appointment if mode is "true"', testOptions(1)).before(async () => createWidget('dxScheduler', createScheduler((e) => {
-  e.cancel = true;
-}), true));
-
-test('should delete appointment if the Promise has been resolved', testOptions(0)).before(async () => createWidget('dxScheduler', createScheduler((e) => {
-  e.cancel = new Promise((resolve) => {
-    resolve(false);
-  });
-}), true));
-
-test('should leave appointment if the Promise has been resolved', testOptions(1)).before(async () => createWidget('dxScheduler', createScheduler((e) => {
-  e.cancel = new Promise((resolve) => {
-    resolve(true);
-  });
-}), true));
