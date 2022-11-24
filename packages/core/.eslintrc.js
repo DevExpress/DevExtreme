f4a470e7860ebf6d4afe9d397ed078820aa743f5
iBat <git@bulychev.pro>
2022-11-24 17:37:59 +0400

    adjust eslint settings

--- packages/core/.eslintrc.js
+++ packages/core/.eslintrc.js
@@ -1,14 +1,8 @@
 module.exports = {
-  extends: 'devextreme/typescript',
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     createDefaultProgram: true,
     project: './tsconfig.package.json',
     tsconfigRootDir: __dirname,
-  },
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'error',
-    'import/exports-last': 'error',
   },
   overrides: [
     {
