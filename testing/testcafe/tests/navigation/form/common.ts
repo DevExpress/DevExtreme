a898c0f03afbfb20c777e206e7ea0b0e66ec895c
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2022-11-24 23:57:49 +0400

    refactor

--- testing/testcafe/tests/navigation/form/common.ts
+++ testing/testcafe/tests/navigation/form/common.ts
@@ -2,17 +2,18 @@ import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import url from '../../../helpers/getPageUrl';
 import createWidget from '../../../helpers/createWidget';
 import { changeTheme } from '../../../helpers/changeTheme';
+import { getThemePostfix } from '../../../helpers/getPostfix';
 
 fixture`Form`
   .page(url(__dirname, '../../container.html'));
 
 ['generic.light', 'generic.dark', 'generic.contrast'].forEach((theme) => {
   // T882067
-  test(`Color of the marks,theme=${theme}`, async (t) => {
+  test(`Color of the mark,theme=${theme}`, async (t) => {
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
     await t
-      .expect(await takeScreenshot(`Form_color_of_the_marks,theme=${theme.replace(/\./g, '-')}.png`, '#container'))
+      .expect(await takeScreenshot(`Form color of the mark${getThemePostfix(theme)}.png`, '#container'))
       .ok()
       .expect(compareResults.isValid())
       .ok(compareResults.errorMessages());
