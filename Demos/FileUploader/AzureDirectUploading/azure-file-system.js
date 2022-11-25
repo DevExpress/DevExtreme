7cbb69e886d2dba5cdf3487e1d3b0f8361b76793
Stanislav Klesarev <stanislav.klesarev@devexpress.com>
2022-11-25 12:03:06 +0400

    T1125133 - FileUploader - fix azure-file-system file (#2266)

--- Demos/FileUploader/AzureDirectUploading/azure-file-system.js
+++ Demos/FileUploader/AzureDirectUploading/azure-file-system.js
@@ -64,7 +64,7 @@ var AzureFileSystem = function(azureGateway) {
     var moveFile = function(sourcePath, destinationPath) {
         return gateway.copyBlob(sourcePath, destinationPath)
             .then(function() {
-                gateway.deleteBlob(sourcePath);
+                return gateway.deleteBlob(sourcePath);
             });
     };
 
@@ -74,7 +74,7 @@ var AzureFileSystem = function(azureGateway) {
         return executeActionForEachEntry(prefix, function(entry) {
             return copyEntry(entry, prefix, destinationKey)
                 .then(function() {
-                    gateway.deleteBlob(entry.name);
+                    return gateway.deleteBlob(entry.name);
                 });
         });
     };
@@ -140,7 +140,7 @@ var AzureFileSystem = function(azureGateway) {
     var compareDataObjects = function(obj1, obj2) {
         if(obj1.isDirectory === obj2.isDirectory) {
             var name1 = obj1.name.toLowerCase();
-            var name2 = obj1.name.toLowerCase();
+            var name2 = obj2.name.toLowerCase();
             if(name1 < name2) {
                 return -1;
             } else {
@@ -284,7 +284,7 @@ var AzureGateway = function(endpointUrl, onRequestExecuted) {
             '<BlockList>'
         ];
 
-        for(var i = 0; i < blockCount; i += 1) {
+        for(var i = 0; i < blockCount; i++) {
             var blockContent = '  <Latest>' + getBlockId(i) + '</Latest>';
             contentParts.push(blockContent);
         }
@@ -344,7 +344,7 @@ var AzureGateway = function(endpointUrl, onRequestExecuted) {
 
         var queryString = commandQueryString || "";
         if(restQueryString) {
-            queryString += queryString ? "&" + restQueryString : restQueryString;    
+            queryString += queryString ? "&" + restQueryString : restQueryString;
         }
 
         ajaxArgs.url = queryString ? urlPath + "?" + queryString : urlPath;
