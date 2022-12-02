959e3a46b82d0710e0e6453e207a89f4c74b8627
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2022-12-02 14:45:36 +0400

    Fix useCoreState compilation

--- packages/react/src/internal/hooks/use-core-state.ts
+++ packages/react/src/internal/hooks/use-core-state.ts
@@ -1,7 +1,7 @@
-import { ObjectType, StateManager } from '@devexpress/core';
+import { UnknownRecord, StateManager } from '@devexpress/core';
 import { useEffect, useState } from 'react';
 
-export function useCoreState<TState extends ObjectType>(
+export function useCoreState<TState extends UnknownRecord>(
   stateManager: StateManager<TState>,
 ): TState {
   const [state, setState] = useState(stateManager.getState());
