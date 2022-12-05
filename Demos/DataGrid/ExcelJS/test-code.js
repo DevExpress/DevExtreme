518577dc3fb29c4ba950d6ba39fe703d0af32503
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2022-12-05 13:43:32 +0300

    DataGrid export: remove hidden demo (#2303)

--- Demos/DataGrid/ExcelJS/test-code.js
+++
@@ -1,4 +0,0 @@
-testUtils.postponeUntil(() => {
-  const header = document.querySelector('#dx-col-2 > div.dx-datagrid-text-content.dx-text-content-alignment-right');
-  return header && header.innerText === 'Invoice Number';
-}, 100, 3000);
