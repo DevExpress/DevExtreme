8053bfd319988502daa0d2cd947eb806eee25932
Ilya Vinogradov <ilya.vinogradov@devexpress.com>
2022-12-06 17:18:06 +0400

    Renovation Angular: Remove redundant config files.

--- packages/angular/rollup/rollup.config.js
+++
@@ -1,9 +0,0 @@
-import { getRollupConfig } from "./rollup.utils";
-
-const OUTPUT_DIR = './lib';
-const COMPONENTS = [
-  'slideToggle',
-  'pager',
-];
-
-export default getRollupConfig(COMPONENTS, OUTPUT_DIR);
