c1c754d87fc125d726d3f4ea309f105c0bd0b9cd
Ilya Vinogradov <ilya.vinogradov@devexpress.com>
2022-12-06 17:18:26 +0400

    Renovation Angular: Add simple library.

---
+++ packages/angular/projects/angular/src/components/radio-group/radio-group.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { RadioGroupComponent } from './radio-group.component';
+
+@NgModule({
+  declarations: [RadioGroupComponent],
+  exports: [RadioGroupComponent],
+})
+export class RadioGroupModule {
+}
