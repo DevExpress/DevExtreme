2c1ac7ecc0961d86671da9fb1c4052bc93021461
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2022-12-08 21:17:41 +0400

    Update dx.all.d.ts

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -24665,7 +24665,6 @@ declare module DevExpress.ui {
     syncLookupFilterValues?: boolean;
   };
   /**
-   * [descr:GridBaseOptions]
    * @deprecated Attention! This type is for internal purposes only. If you used it previously, please submit a ticket to our {@link https://supportcenter.devexpress.com/ticket/create Support Center}. We will check if there is an alternative solution.
    */
   interface GridBaseOptionsBlank<
