984e89f0d489b7ee831f8a46d5df60b518f24f2b
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2022-12-08 21:16:39 +0400

    Update dx.all.d.ts

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -3504,7 +3504,6 @@ declare module DevExpress.common.grids {
     syncLookupFilterValues?: boolean;
   };
   /**
-   * [descr:GridBaseOptions]
    * @deprecated Attention! This type is for internal purposes only. If you used it previously, please submit a ticket to our {@link https://supportcenter.devexpress.com/ticket/create Support Center}. We will check if there is an alternative solution.
    */
   interface GridBaseOptionsBlank<
