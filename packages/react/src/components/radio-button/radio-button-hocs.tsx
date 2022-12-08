4e8c0662cc0110d2b38473eb49de079ed7ba5d06
MikeVitik <vitikm@gmail.com>
2022-12-08 16:00:28 +0400

    Refactoring react for generate infero

---
+++ packages/react/src/components/radio-button/radio-button-hocs.tsx
@@ -0,0 +1,67 @@
+import {
+  ChangeEventHandler,
+  ComponentType,
+  useState,
+} from 'react';
+import { useCoreState } from '../../internal/hooks';
+import { RadioButtonInternal } from './radio-button-internal';
+import {
+  CoreBoundRadioButtonProps,
+  RadioButtonRenderProps,
+  RadioButtonRenderType,
+  RadioTemplateProps,
+} from './types';
+
+function withUncontrolledBehavior(RadioButton: RadioButtonRenderType) {
+  return ({ defaultChecked, ...props }: RadioButtonRenderProps) => {
+    const [internalChecked, setInternalChecked] = useState(defaultChecked || false);
+    const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+      setInternalChecked(event.target.checked);
+      event.preventDefault();
+      event.stopPropagation();
+      props.onChange?.(event);
+    };
+    const renderRadioComponent = (
+      RadioComponent: ComponentType<RadioTemplateProps>,
+    ) => <RadioComponent checked={internalChecked} />;
+    return (
+      <RadioButton
+          // eslint-disable-next-line react/jsx-props-no-spreading
+        {...props}
+        onChange={handleChange}
+        renderRadioComponent={renderRadioComponent}
+      />
+    );
+  };
+}
+function withRadioGroup(RadioButton: RadioButtonRenderType) {
+  return ({
+    radioGroupCore: { dispatcher, stateManager },
+    value,
+    ...props
+  }: CoreBoundRadioButtonProps) => {
+    const coreState = useCoreState(stateManager);
+
+    const checked = coreState.value === value;
+    const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+      event.preventDefault();
+      event.stopPropagation();
+      dispatcher.dispatch('updateValue', {
+        value,
+      });
+    };
+
+    return (
+      <RadioButton
+          // eslint-disable-next-line react/jsx-props-no-spreading
+        {...props}
+        value={value}
+        checked={checked}
+        onChange={handleChange}
+      />
+    );
+  };
+}
+
+export const CoreBoundRadioButton = withRadioGroup(RadioButtonInternal);
+export const UncontrolledRadioButton = withUncontrolledBehavior(RadioButtonInternal);
