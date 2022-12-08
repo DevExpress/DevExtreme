fbb41726120e2d93cb5c7e690d1d83f22b0f9640
MikeVitik <vitikm@gmail.com>
2022-12-08 16:00:28 +0400

    Refactoring react for generate infero

---
+++ packages/react/src/components/radio-button/radio-button-internal.tsx
@@ -0,0 +1,49 @@
+import {
+  LabelTemplateProps,
+  RadioButtonRenderProps,
+  RadioTemplateProps,
+} from './types';
+
+export function DefaultRadioTemplate({ checked = false }: RadioTemplateProps) {
+  return <span>{checked ? '◉' : '◎'}</span>;
+}
+
+export function DefaultLabelTemplate({ label }: LabelTemplateProps) {
+  return <span>{label}</span>;
+}
+
+export function RadioButtonInternal({
+  name, value, checked, defaultChecked, onClick, onChange, label, radioTemplate,
+  labelTemplate, inputId, inputRef, renderRadioComponent,
+}: RadioButtonRenderProps): JSX.Element {
+  const RadioComponent = radioTemplate || DefaultRadioTemplate;
+  const LabelComponent = labelTemplate || DefaultLabelTemplate;
+
+  return (
+    <span>
+      <label
+        htmlFor={inputId}
+        style={{ cursor: 'pointer', userSelect: 'none' }}
+      >
+        <input
+          ref={inputRef}
+          id={inputId}
+          name={name}
+          style={{ display: 'none' }}
+          type="radio"
+          value={value}
+          checked={checked}
+          defaultChecked={defaultChecked}
+          onClick={onClick}
+          onChange={onChange}
+        />
+        {renderRadioComponent ? (
+          renderRadioComponent(RadioComponent)
+        ) : (
+          <RadioComponent checked={checked || false} />
+        )}
+        {label && <LabelComponent label={label} />}
+      </label>
+    </span>
+  );
+}
---
+++ packages/react/src/internal/hooks/use-id.ts
@@ -0,0 +1,15 @@
+import { useMemo } from 'react';
+
+function makeid(length = 5) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i += 1) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+export function useId() {
+  const id = useMemo(() => makeid(), []);
+  return id;
+}
