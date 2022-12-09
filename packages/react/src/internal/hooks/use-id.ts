69f1fcac72ed824b01722038df4f5c5792abd0db
MikeVitik <vitikm@gmail.com>
2022-12-09 16:26:29 +0400

    Update packages/react/src/internal/hooks/use-id.ts
    Co-authored-by: ilya.kharchenko <14272298+IlyaKhD@users.noreply.github.com>

--- packages/react/src/internal/hooks/use-id.ts
+++ packages/react/src/internal/hooks/use-id.ts
@@ -1,15 +1,13 @@
 import { useMemo } from 'react';
 
-function makeid(length = 5) {
-  let result = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const charactersLength = characters.length;
-  for (let i = 0; i < length; i += 1) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
+function createIdGenerator() {
+  let counter = BigInt(0); 
+  return () => (counter++).toString(36);
 }
+
+const  makeId = createIdGenerator();
+
 export function useId() {
-  const id = useMemo(() => makeid(), []);
+  const id = useMemo(() => makeId(), []);
   return id;
 }
