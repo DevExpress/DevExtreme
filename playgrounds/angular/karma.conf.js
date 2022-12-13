6c96f21ece298f49440340ae04886af9fdcab609
const314 <constantin.panchenko@gmail.com>
2022-12-13 17:29:01 +0400

    disable linter for configs in angular playground

--- playgrounds/angular/karma.conf.js
+++ playgrounds/angular/karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+/* eslint-disable */
 
 module.exports = function (config) {
   config.set({
