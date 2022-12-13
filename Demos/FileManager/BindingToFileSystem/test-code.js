11a17cb8bfaa4f0144c6fda28c1196dd7da8a69a
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2022-12-13 14:11:12 +0400

    Fix FileManager-BindingToFileSystem vis test (#2313)

--- Demos/FileManager/BindingToFileSystem/test-code.js
+++ Demos/FileManager/BindingToFileSystem/test-code.js
@@ -5,4 +5,7 @@ testUtils.postponeUntilFound('td[aria-describedby="dx-col-3"]').then(() => {
   testUtils.findElements('td[aria-describedby="dx-col-3"]').forEach((x) => {
     x.innerText = '01/12/1999';
   });
+  testUtils.findElements('td[aria-describedby="dx-col-4"]').forEach((x) => {
+    x.innerText = '';
+  });
 });
