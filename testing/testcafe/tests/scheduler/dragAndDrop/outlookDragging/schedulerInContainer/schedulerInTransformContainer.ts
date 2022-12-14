d5ceb9f906df0af7e6ef9db641bc6f2183c16ca6
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2022-12-15 00:23:06 +0400

    update outlook dragging tests

--- testing/testcafe/tests/scheduler/dragAndDrop/outlookDragging/schedulerInContainer/schedulerInTransformContainer.ts
+++ testing/testcafe/tests/scheduler/dragAndDrop/outlookDragging/schedulerInContainer/schedulerInTransformContainer.ts
@@ -1,47 +1,56 @@
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import { appendElementTo, deleteStylesheetRule, insertStylesheetRule } from '../../../../navigation/helpers/domUtils';
 import createWidget, { disposeWidgets } from '../../../../../helpers/createWidget';
 import url from '../../../../../helpers/getPageUrl';
 import Scheduler from '../../../../../model/scheduler';
 
 fixture.disablePageReloads`Outlook dragging, for case scheduler in container with transform style`
-  .page(url(__dirname, './containerWithTransform.html'))
+  .page(url(__dirname, '../../../../container.html'))
   .afterEach(async () => disposeWidgets());
 
 test('Dragging should be work right in case dxScheduler placed in container with transform style', async (t) => {
-  const scheduler = new Scheduler('.dx-scheduler');
+  const scheduler = new Scheduler('#scheduler');
 
   const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
   const draggableAppointment = scheduler.getAppointment('Website Re-Design Plan');
 
   await t
-    .drag(draggableAppointment.element, 0, 120, { speed: 0.1 })
+    .drag(draggableAppointment.element, 0, 120, { speed: 0.5 })
     .expect(await takeScreenshot('dxScheduler-placed-in-transform-container-drag-to-bottom.png'))
     .ok()
 
-    .drag(draggableAppointment.element, 0, -170, { speed: 0.1 })
+    .drag(draggableAppointment.element, 0, -170, { speed: 0.5 })
     .expect(await takeScreenshot('dxScheduler-placed-in-transform-container-drag-to-top.png'))
     .ok()
 
-    .drag(draggableAppointment.element, 100, 0, { speed: 0.1 })
+    .drag(draggableAppointment.element, 100, 0, { speed: 0.5 })
     .expect(await takeScreenshot('dxScheduler-placed-in-transform-container-drag-to-right.png'))
     .ok()
 
-    .drag(draggableAppointment.element, -230, 0, { speed: 0.1 })
+    .drag(draggableAppointment.element, -230, 0, { speed: 0.5 })
     .expect(await takeScreenshot('dxScheduler-placed-in-transform-container-drag-to-left.png'))
     .ok()
 
     .expect(compareResults.isValid())
     .ok(compareResults.errorMessages());
-}).before(async () => createWidget('dxScheduler', {
-  dataSource: [{
-    text: 'Website Re-Design Plan',
-    startDate: new Date(2021, 2, 24, 11),
-    endDate: new Date(2021, 2, 24, 12),
-  }],
-  views: ['workWeek'],
-  currentView: 'workWeek',
-  currentDate: new Date(2021, 2, 22),
-  startDayHour: 9,
-  height: 600,
-  width: 800,
-}));
+}).before(async () => {
+  await appendElementTo('#container', 'div', 'scheduler');
+  await insertStylesheetRule('#container { margin-top: 100px; margin-left: 100px; transform: translate(0px, 0px); }', 0);
+
+  return createWidget('dxScheduler', {
+    dataSource: [{
+      text: 'Website Re-Design Plan',
+      startDate: new Date(2021, 2, 24, 11),
+      endDate: new Date(2021, 2, 24, 12),
+    }],
+    views: ['workWeek'],
+    currentView: 'workWeek',
+    currentDate: new Date(2021, 2, 22),
+    startDayHour: 9,
+    height: 600,
+    width: 800,
+  }, false, '#scheduler');
+}).after(async () => {
+  await disposeWidgets();
+  await deleteStylesheetRule(0);
+});
