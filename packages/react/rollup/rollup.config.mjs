d8358ecb7380bf703139572cbbc876e0fa8988dd
iBat <git@bulychev.pro>
2022-12-15 17:33:04 +0400

    CI: move noEmitOnError option to rollup watch config

--- packages/react/rollup/rollup.config.mjs
+++ packages/react/rollup/rollup.config.mjs
@@ -1,42 +1,40 @@
 import typescript from '@rollup/plugin-typescript';
+import {
+  checkExternalPackage,
+  checkWatchMode,
+  FORMAT_EXTENSIONS,
+} from '../../../build/rollup/utils.js';
 
-const OUTPUT_DIR = './lib'
-const FORMAT_EXTENSIONS = {
-    esm: 'mjs',
-    cjs: 'cjs',
-}
-
-function checkExternalPackage(id) {
-    return ['@devextreme'].includes(id.split('/')[0]);
-}
+const OUTPUT_DIR = './lib';
 
 function getBundleConfig(outputDir, format) {
-    return {
-        input: 'src/index.ts',
-        output: {
-            dir: outputDir,
-            entryFileNames: `[name].${FORMAT_EXTENSIONS[format]}`,
-            format,
-            sourcemap: true
+  return {
+    input: 'src/index.ts',
+    output: {
+      dir: outputDir,
+      entryFileNames: `[name].${FORMAT_EXTENSIONS[format]}`,
+      format,
+      sourcemap: true,
+    },
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.package.json',
+        compilerOptions: {
+          noEmitOnError: checkWatchMode(),
+          outDir: outputDir,
         },
-        plugins: [
-            typescript({
-                tsconfig: './tsconfig.package.json',
-                compilerOptions: {
-                    outDir: outputDir,
-                },
-                outputToFilesystem: true
-            })
-        ],
-        external: checkExternalPackage,
-    };
+        outputToFilesystem: true,
+      }),
+    ],
+    external: checkExternalPackage,
+  };
 }
 
 function getRollupConfig(outputDir) {
-    return [
-        getBundleConfig(outputDir, 'esm'),
-        getBundleConfig(outputDir, 'cjs'),
-    ];
+  return [
+    getBundleConfig(outputDir, 'esm'),
+    getBundleConfig(outputDir, 'cjs'),
+  ];
 }
 
-export default getRollupConfig(OUTPUT_DIR)
+export default getRollupConfig(OUTPUT_DIR);
