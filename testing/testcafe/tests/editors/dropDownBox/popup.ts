c01334a0397b839a3d7796e4d65aa9eb8ae59a6a
marker dao ® | Roman Ty <mark_nikolsky@outlook.com>
2022-12-20 18:18:04 +0700

    fix(@tests): Replace etalon

--- testing/testcafe/tests/editors/dropDownBox/popup.ts
+++ testing/testcafe/tests/editors/dropDownBox/popup.ts
@@ -1,13 +1,14 @@
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import url from '../../../helpers/getPageUrl';
 import createWidget from '../../../helpers/createWidget';
+import { safeSizeTest } from '../../../helpers/safeSizeTest';
 
 const BUTTON_CLASS = 'dx-dropdowneditor-button';
 
 fixture`Drop Down Button's Popup`
   .page(url(__dirname, '../../container.html'));
 
-test('Popup should have correct height when DropDownBox is opend first time (T1130045)', async (t) => {
+safeSizeTest('Popup should have correct height when DropDownBox is opened first time (T1130045)', async (t) => {
   const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
   await t
