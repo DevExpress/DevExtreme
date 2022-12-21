6ae0404572f240dce148ea97e674e238cf49ab04
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2022-12-21 14:01:55 +0400

    Refactor

--- packages/interim/src/renovation/component_wrapper/common/template_wrapper.ts
+++ packages/interim/src/renovation/component_wrapper/common/template_wrapper.ts
@@ -63,12 +63,10 @@ function recordMutations<TReturn>(target: Node, func: () => TReturn): [
   const observer = new MutationObserver(() => {});
 
   observer.observe(target, { childList: true, attributes: true, subtree: true });
-
-  const result = func();
-  var mutations = observer.takeRecords();
+  const result: [TReturn, MutationRecord[]] = [func(), observer.takeRecords()];
   observer.disconnect();
 
-  return [result, mutations];
+  return result;
 }
 
 function buildTemplateContent(props: TemplateWrapperProps, container: Element): ChildNode[] {
@@ -96,17 +94,21 @@ function buildTemplateContent(props: TemplateWrapperProps, container: Element): 
     return [];
   }
 
-  const result = isDxElementWrapper(rendered)
+  return isDxElementWrapper(rendered)
     ? rendered.toArray()
     : [getPublicElement($(rendered))]
+}
 
-  return result;
+function noop(): void {}
+
+function detectTemplateBehavior(container: Node, content: Node[]) {
+  return content.length === 0 || (content.length === 1 && content[0] === container)
+    ? 'mutates-container'
+    : 'returns-content';
 }
 
 export class TemplateWrapper extends InfernoComponent<TemplateWrapperProps> {
-  __node!: Element;
-  __parent!: HTMLElement;
-  __templateCleaner: (() => void) | undefined;
+  __templateCleaner: (() => void) = noop;
 
   constructor(props: TemplateWrapperProps) {
     super(props);
@@ -115,49 +117,41 @@ export class TemplateWrapper extends InfernoComponent<TemplateWrapperProps> {
 
   renderTemplate(): void {
     const node = findDOMfromVNode(this.$LI, true);
-    if(node?.parentElement == null || node?.parentNode == null) {
-      throw new Error('Template must have parent node');
+    if(node?.parentElement == null) {
+      throw new Error('Template must have parent element');
     }
 
-    this.__node = node;
-    this.__parent = node.parentElement;
-
-    this.updateContent(this.props);
-  }
-
-  shouldUpdate(nextProps: TemplateWrapperProps): boolean {
-    const { model } = this.props;
-    const { model: nextModel } = nextProps;
-
-    return model?.isEqual
-      ? !model.isEqual(model.data, nextModel?.data)
-      : defaultComparer(this.props, nextProps);
-  }
-
-  shouldComponentUpdate(nextProps: TemplateWrapperProps): boolean {
-    if(this.shouldUpdate(nextProps)) {
-      this.updateContent(nextProps);
-    }
-
-    return false;
-  }
-
-  updateContent(props: TemplateWrapperProps): void {
-    const container = getPublicElement($(this.__parent));
+    const container = getPublicElement($(node.parentElement));
 
     const [ content, mutations ] = recordMutations(
       container,
-      () => buildTemplateContent(props, container),
+      () => buildTemplateContent(this.props, container),
     );
 
-    this.__templateCleaner?.();
+    this.__templateCleaner();
 
-    if (content.length === 0 || (content.length === 1 && content[0] === container)) {
-      this.__templateCleaner = () => mutations.forEach(revertMutation);
-    } else {
-      this.__node.after(...content);
-      this.__templateCleaner = () => content.forEach(el => el?.remove());
+    switch(detectTemplateBehavior(container, content)) {
+
+      case 'mutates-container':
+        this.__templateCleaner = () => mutations.forEach(revertMutation);
+        break;
+
+      case 'returns-content':
+        node.after(...content);
+        this.__templateCleaner = () => content.forEach(el => el?.remove());
+        break;
+
+      default:
+        this.__templateCleaner = noop;
     }
+  }
+
+  shouldComponentUpdate(nextProps: TemplateWrapperProps): boolean {
+    const { model } = this.props;
+
+    return model?.isEqual
+      ? !model.isEqual(model.data, nextProps.model?.data)
+      : defaultComparer(this.props, nextProps);
   }
 
   createEffects(): InfernoEffect[] {
@@ -171,7 +165,7 @@ export class TemplateWrapper extends InfernoComponent<TemplateWrapperProps> {
   // NOTE: Prevent nodes clearing on unmount.
   //       Nodes will be destroyed by inferno on markup update
   componentWillUnmount(): void {
-    this.__templateCleaner?.();
+    this.__templateCleaner();
   }
 
   render(): JSX.Element | null {
