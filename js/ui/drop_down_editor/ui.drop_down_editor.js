90bd7b1bd4b95a97fc596ed9a78ec62e34c22eeb
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2022-12-21 12:53:47 +0400

    update comment

--- js/ui/drop_down_editor/ui.drop_down_editor.js
+++ js/ui/drop_down_editor/ui.drop_down_editor.js
@@ -625,8 +625,8 @@ const DropDownEditor = TextBox.inherit({
     },
 
     _dimensionChanged: function() {
-        // This uses for hiding of popup on window resize if editor was hidden (T1133813)
-        // The better way to use the resize observable to close the popup after hiding the editor
+        // This part hides the popup on window resize if an editor was hidden (T1133813)
+        // Use the resize observer to close the popup after hiding the editor instead
         if(hasWindow() && !this.$element().is(':visible')) {
             this.close();
             return;
