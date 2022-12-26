c65feb4da35062dae0c09fd562a54d737d24ae73
iBat <git@bulychev.pro>
2022-12-26 15:59:08 +0400

    CI: fix build

--- packages/interim/rollup/rollup.config.mjs
+++ packages/interim/rollup/rollup.config.mjs
@@ -1,14 +1,39 @@
+import typescript from '@rollup/plugin-typescript';
 import {
-  getBundleConfig, getDtsConfig,
+  checkExternalPackage,
+  checkWatchMode,
+  FORMAT_EXTENSIONS,
 } from '../../../build/rollup/utils.js';
 
-const OUTPUT_DIR = './lib';
+const OUTPUT_DIR = './lib'
+
+function getBundleConfig(outputDir, format) {
+  return {
+    input: 'src/index.ts',
+    output: {
+      dir: outputDir,
+      entryFileNames: `[name].${FORMAT_EXTENSIONS[format]}`,
+      format,
+      sourcemap: true
+    },
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.package.json',
+        compilerOptions: {
+          noEmitOnError: checkWatchMode(),
+          outDir: outputDir,
+        },
+        outputToFilesystem: true
+      })
+    ],
+    external: checkExternalPackage,
+  };
+}
 
 function getRollupConfig(outputDir) {
   return [
     getBundleConfig(outputDir, 'esm'),
     getBundleConfig(outputDir, 'cjs'),
-    getDtsConfig(outputDir, true),
   ];
 }
 
