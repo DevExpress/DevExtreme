73d48a4aa26f5d1c1d42c23b3221e974a125a01e
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2022-12-29 21:08:39 +0400

    Add index

--- packages/jquery/src/component-wrapper.js
+++ packages/jquery/src/component-wrapper.js
@@ -2,8 +2,8 @@ import { ComponentWrapper as BaseComponentWrapper } from '@devextreme/interim';
 
 export class ComponentWrapper extends BaseComponentWrapper {
 
-  _buildTemplateArgs(data) {
-    return super._buildTemplateArgs({ data });
+  _buildTemplateArgs(data, index) {
+    return super._buildTemplateArgs({ data }, index);
   }
 
 }
