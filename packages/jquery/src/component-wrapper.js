25dd3347825142b6cec359ab9e940bc37f4a4133
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2022-12-30 14:38:46 +0400

    Fix lint

--- packages/jquery/src/component-wrapper.js
+++ packages/jquery/src/component-wrapper.js
@@ -1,7 +1,6 @@
 import { ComponentWrapper as BaseComponentWrapper } from '@devextreme/interim';
 
 export class ComponentWrapper extends BaseComponentWrapper {
-
   _createTemplateComponent(...args) {
     const template = super._createTemplateComponent(...args);
 
