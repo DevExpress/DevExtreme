1e2d456d8254b4124116651ae42fdec0a425287c
MikeVitik <vitikm@gmail.com>
2022-12-30 17:45:25 +0400

    Add examples for using theme in react

--- playgrounds/react/src/app.tsx
+++ playgrounds/react/src/app.tsx
@@ -3,8 +3,11 @@ import { Route, Routes } from 'react-router-dom';
 import { RadioButtonExample } from './examples/radio-button/radio-button-example';
 import { RadioGroupCompatibleExample } from './examples/radio-group-compatible/radio-group-compatible-example';
 import { RadioGroupExample } from './examples/radio-group/radio-group-example';
+import { DynamicThemeExample } from './examples/themes/dynamic-theme-example';
+import { ThemeExample } from './examples/themes/theme-example';
 import { Home } from './home';
 
+import '@devextreme/styles/lib/themes/light.css';
 import './app.css';
 
 export function App() {
@@ -14,6 +17,8 @@ export function App() {
       <Route path="/radio-group" element={<RadioGroupExample />} />
       <Route path="/radio-group-compatible" element={<RadioGroupCompatibleExample />} />
       <Route path="/radio-button" element={<RadioButtonExample />} />
+      <Route path="/theme" element={<ThemeExample />} />
+      <Route path="/dynamic-theme" element={<DynamicThemeExample />} />
     </Routes>
   );
 }
