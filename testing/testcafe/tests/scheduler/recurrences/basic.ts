a1dc65dca0513bae4ac96885d929ea7105f31bcf
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2023-01-03 12:01:38 +0400

    refactor

--- testing/testcafe/tests/scheduler/recurrences/basic.ts
+++ testing/testcafe/tests/scheduler/recurrences/basic.ts
@@ -14,9 +14,9 @@ test('Drag-n-drop recurrence appointment between dateTable and allDay panel', as
   await t.expect(await compareScreenshot(t, 'basic-recurrence-appointment-init.png')).ok();
 
   await t
-    .dragToElement(draggableAppointment.element, scheduler.getAllDayTableCell(0))
+    .dragToElement(draggableAppointment.element, scheduler.getAllDayTableCell(0), { speed: 0.5 })
     .expect(scheduler.getAppointmentCount()).eql(7)
-    .expect((await draggableAppointment.element.boundingClientRect).width)
+    .expect((await scheduler.getAppointment('Simple recurrence appointment').element.boundingClientRect).width)
     .eql(114)
     .expect(draggableAppointment.isAllDay)
     .ok();
