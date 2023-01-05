93d7810d5b3bfefa25250a48c82812d878ec60ef
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2023-01-05 14:28:43 +0400

    Add missing declarations to core/utils/common

--- js/core/utils/common.d.ts
+++ js/core/utils/common.d.ts
@@ -1,1 +1,5 @@
-export function noop();
+export function noop(): void;
+
+export function ensureDefined<T>(value: T, defaultValue: T): NonNullable<T>;
+
+export function equalByValue(object1: any, object2: any, depth?: number, strict?: boolean): boolean;
