e00c42bca89fb7c6093b361e06abd81f43c9f253
Ilya Vinogradov <ilya.vinogradov@devexpress.com>
2023-01-05 22:39:31 +0400

    PivotGrid(T1138119): Fix fieldChoose popup area items drag-n-drop.

---
+++ js/ui/pivot_grid/sortable/const.js
@@ -0,0 +1,15 @@
+export const SortableConst = {
+    IEFieldWidthCorrection: 1,
+    targets: {
+        drag: 'drag',
+    },
+    classes: {
+        areaBox: 'dx-area-box',
+        areaField: 'dx-area-field',
+        fieldsContainer: 'dx-pivotgrid-fields-container',
+        widget: 'dx-widget',
+    },
+    attrs: {
+        treeListItem: 'tree-list-item'
+    },
+};
--- js/ui/pivot_grid/ui.pivot_grid.field_chooser.js
+++ js/ui/pivot_grid/ui.pivot_grid.field_chooser.js
@@ -15,6 +15,7 @@ const DIV = '<div>';
 const hasWindow = hasWindowFn();
 
 import './data_source';
+import { SortableConst } from './sortable/index';
 
 const FIELDCHOOSER_CLASS = 'dx-pivotgridfieldchooser';
 const FIELDCHOOSER_CONTAINER_CLASS = 'dx-pivotgridfieldchooser-container';
@@ -88,6 +89,11 @@ function compareItems(a, b) {
 
 function getScrollable(container) {
     return container.find('.dx-scrollable').dxScrollable('instance');
+}
+
+function configureTreeViewScrollable(treeView) {
+    const treeViewScrollable = treeView.getScrollable();
+    treeViewScrollable.option('useNative', false);
 }
 
 const FieldChooser = BaseFieldChooser.inherit({
@@ -445,15 +451,19 @@ const FieldChooser = BaseFieldChooser.inherit({
             searchEnabled: that.option('allowSearch'),
             searchTimeout: that.option('searchTimeout'),
             itemTemplate: function(itemData, itemIndex, itemElement) {
+                const $item = $('<div>')
+                    .toggleClass('dx-area-field', !itemData.items)
+                    .attr(SortableConst.attrs.treeListItem, true)
+                    .data('field', itemData.field)
+                    .appendTo(itemElement);
+
                 if(itemData.icon) {
-                    getImageContainer(itemData.icon).appendTo(itemElement);
+                    getImageContainer(itemData.icon).appendTo($item);
                 }
 
                 $('<span>')
-                    .toggleClass('dx-area-field', !itemData.items)
-                    .data('field', itemData.field)
                     .text(itemData.text)
-                    .appendTo(itemElement);
+                    .appendTo($item);
             },
             onItemCollapsed: function(e) {
                 const index = that._expandedPaths.indexOf(e.itemData.path);
@@ -512,6 +522,7 @@ const FieldChooser = BaseFieldChooser.inherit({
                 });
             }
         });
+        configureTreeViewScrollable(treeView);
 
         const dataChanged = function() {
             let scrollable = getScrollable(container);
@@ -573,7 +584,9 @@ const FieldChooser = BaseFieldChooser.inherit({
             };
             that._dataChangedHandlers.push(render);
             render();
-            $fieldsContainer.dxScrollable();
+            $fieldsContainer.dxScrollable({
+                useNative: false,
+            });
         } else {
             $areaContainer.addClass('dx-all-fields');
             $fieldsContainer.addClass('dx-treeview-border-visible');
@@ -593,6 +606,7 @@ const FieldChooser = BaseFieldChooser.inherit({
 
     resetTreeView: function() {
         const treeView = this.$element().find('.dx-treeview').dxTreeView('instance');
+        configureTreeViewScrollable(treeView);
 
         if(treeView) {
             treeView.option('searchValue', '');
