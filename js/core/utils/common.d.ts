c967acc9aa596df660c5370927e05e0e9d57da5e
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2023-01-05 14:48:56 +0400

    Add declaration for deferRender

--- js/core/utils/common.d.ts
+++ js/core/utils/common.d.ts
@@ -1,5 +1,9 @@
+import { DeferredObj } from './deferred';
+
 export function noop(): void;
+
+export function deferRender<T>(func: () => T, deferred?: DeferredObj<T>): T | Promise<T> | DeferredObj<T>;
 
 export function ensureDefined<T>(value: T, defaultValue: T): NonNullable<T>;
 
-export function equalByValue(object1: any, object2: any, depth?: number, strict?: boolean): boolean;
+export function equalByValue(object1: unknown, object2: unknown, depth?: number, strict?: boolean): boolean;
