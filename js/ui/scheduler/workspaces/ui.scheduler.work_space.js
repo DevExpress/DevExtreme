b4eed87dc5fd5cd5ebbc717786a2cb91752667f3
Gagik Harutyunyan <53352872+gagharutyunyan1993@users.noreply.github.com>
2023-01-05 16:08:51 +0400

    replaced updateDimensions to _renderView

--- js/ui/scheduler/workspaces/ui.scheduler.work_space.js
+++ js/ui/scheduler/workspaces/ui.scheduler.work_space.js
@@ -2606,6 +2606,9 @@ class SchedulerWorkSpace extends WidgetObserver {
         }
 
         this.renderWorkSpace();
+        if(this.isRenovatedRender()) {
+            this.virtualScrollingDispatcher.updateDimensions();
+        }
 
         this._updateGroupTableHeight();
         this.updateHeaderEmptyCellWidth();
