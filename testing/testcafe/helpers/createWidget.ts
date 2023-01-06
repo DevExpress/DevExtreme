f6d6d1ad3abdd3f8df3adb3d008ed077af8fc9f9
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2023-01-06 16:22:31 +0400

    switch off animation and don't use dispose widgets

--- testing/testcafe/helpers/createWidget.ts
+++ testing/testcafe/helpers/createWidget.ts
@@ -54,9 +54,16 @@ export type WidgetName =
 export default async function createWidget(
   widgetName: WidgetName,
   options: unknown,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   disableAnimation = false,
   selector = '#container',
 ): Promise<void> {
+  // if (disableAnimation) {
+  await ClientFunction(() => {
+    (window as any).DevExpress.fx.off = true;
+  })();
+  // }
+
   await ClientFunction(() => {
     const widgetOptions = typeof options === 'function' ? options() : options;
     (window as any).widget = $(`${selector}`)[widgetName](widgetOptions)[widgetName]('instance');
@@ -69,28 +76,22 @@ export default async function createWidget(
               selector,
             },
   })();
-
-  if (disableAnimation) {
-    await ClientFunction(() => {
-      (window as any).DevExpress.fx.off = true;
-    })();
-  }
 }
 
 export async function disposeWidgets(): Promise<void> {
-  await ClientFunction(() => {
-    const widgetSelector = '.dx-widget';
-    const $elements = $(widgetSelector)
-      .filter((_, element) => $(element).parents(widgetSelector).length === 0);
-    $elements.each((_, element) => {
-      const $widgetElement = $(element);
-      const widgetNames = $widgetElement.data().dxComponents;
-      widgetNames?.forEach((name) => {
-        if ($widgetElement.hasClass('dx-widget')) {
-          ($widgetElement as any)[name]('dispose');
-        }
-      });
-      $widgetElement.empty();
-    });
-  })();
+  // await ClientFunction(() => {
+  //   const widgetSelector = '.dx-widget';
+  //   const $elements = $(widgetSelector)
+  //     .filter((_, element) => $(element).parents(widgetSelector).length === 0);
+  //   $elements.each((_, element) => {
+  //     const $widgetElement = $(element);
+  //     const widgetNames = $widgetElement.data().dxComponents;
+  //     widgetNames?.forEach((name) => {
+  //       if ($widgetElement.hasClass('dx-widget')) {
+  //         ($widgetElement as any)[name]('dispose');
+  //       }
+  //     });
+  //     $widgetElement.empty();
+  //   });
+  // })();
 }
