456800347b1529b75f837ce08cd5dd76fca19adf
marker dao ® | Roman Ty <mark_nikolsky@outlook.com>
2023-01-11 15:27:33 +0700

    fix(@tests): Remove viewport class from body && Refactor test

--- testing/testcafe/tests/navigation/treeView/material/treeView.ts
+++ testing/testcafe/tests/navigation/treeView/material/treeView.ts
@@ -1,21 +1,28 @@
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import { Selector } from 'testcafe';
+import { changeTheme } from '../../../../helpers/changeTheme';
+import { setClassAttribute, removeClassAttribute } from '../../helpers/domUtils';
 import url from '../../../../helpers/getPageUrl';
 import createWidget from '../../../../helpers/createWidget';
-import { changeTheme } from '../../../../helpers/changeTheme';
 // eslint-disable-next-line import/extensions
 import { products } from './data.js';
+
+const materialWrapperClasses = [
+  'dx-viewport',
+  'dx-theme-material',
+  'dx-theme-material-typography',
+  'dx-color-scheme-blue-dark',
+].join(' ');
 
 fixture`T1138605-TreeView`
   .page(url(__dirname, '../../../container.html'));
 
 // T1138605
 ['material.blue.dark', 'material.blue.dark.compact'].forEach((theme) => {
-  test(`T1138605-${theme}`, async (t) => {
+  test(`TreeView ${theme}`, async (t) => {
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
     const scrollableContainer = Selector('.dx-scrollable-container');
-
     await t.scroll(scrollableContainer, 'bottom');
 
     await t
@@ -24,6 +31,7 @@ fixture`T1138605-TreeView`
       .expect(compareResults.isValid())
       .ok(compareResults.errorMessages());
   }).before(async () => {
+    await setClassAttribute(Selector('body'), materialWrapperClasses);
     await changeTheme(theme);
 
     return createWidget('dxTreeView', {
@@ -33,5 +41,6 @@ fixture`T1138605-TreeView`
     });
   }).after(async () => {
     await changeTheme('generic.light');
+    await removeClassAttribute(Selector('body'), materialWrapperClasses);
   });
 });
