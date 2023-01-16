dd145068ec61e2abc89c0fa757c3039c9490a0b4
Ilya Vinogradov <ilya.vinogradov@devexpress.com>
2023-01-17 00:08:43 +0400

    R2 VM Core: Implement store + selector.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,24 +4,24 @@ export type {
 } from './middlewares';
 
 export type {
-  StateManager,
-  Dispatcher,
-} from './state-manager';
-
-export type {
   Handlers,
 } from './reducer';
 
 export type {
   Selector,
-} from './view-model';
+} from './selector';
 
 export type {
-  ViewModelManager,
-} from './view-model-manager';
+  Store,
+  StoreState,
+} from './store';
 
 export {
-  createCore,
-} from './create-core';
+  createSelector,
+} from './selector';
+
+export {
+  createStore,
+} from './store';
 
 export * from './utils';
