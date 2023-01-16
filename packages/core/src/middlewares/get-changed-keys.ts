8e378675c6de9cd6808782e8a49dd3ba37a9a90b
Ilya Vinogradov <ilya.vinogradov@devexpress.com>
2023-01-17 00:07:47 +0400

    R2 VM Core: Remove unused entity + tests.

--- packages/core/src/middlewares/get-changed-keys.ts
+++
@@ -1,8 +0,0 @@
-import { getKeys, UnknownRecord } from '../utils';
-
-export function getChangedKeys<TState extends UnknownRecord>(
-  prev: TState,
-  next: TState,
-): (keyof TState)[] {
-  return getKeys(next).filter((key) => next[key] !== prev[key]);
-}
