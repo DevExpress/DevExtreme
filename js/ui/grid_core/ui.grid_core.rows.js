24814618653a832a9b00dbe78b0c30a12f5a9de2
MikeVitik <vitikm@gmail.com>
2023-01-17 16:37:05 +0400

    Review all _renderCore and return deferred then need (#23682)

--- js/ui/grid_core/ui.grid_core.rows.js
+++ js/ui/grid_core/ui.grid_core.rows.js
@@ -687,11 +687,12 @@ export const rowsModule = {
                     this.setAria('role', 'presentation', $element);
 
                     const $table = this._renderTable({ change: change });
-                    this._updateContent($table, change);
+                    const deferred = this._updateContent($table, change);
 
                     this.callBase(change);
 
                     this._lastColumnWidths = null;
+                    return deferred;
                 },
 
                 _getRows: function(change) {
