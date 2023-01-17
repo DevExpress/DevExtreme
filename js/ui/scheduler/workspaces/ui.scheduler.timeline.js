9591a1b5230e328518ba06741d37f8b17d561a7b
Gagik Harutyunyan <53352872+gagharutyunyan1993@users.noreply.github.com>
2023-01-17 13:01:05 +0400

    commit to restart tests

--- js/ui/scheduler/workspaces/ui.scheduler.timeline.js
+++ js/ui/scheduler/workspaces/ui.scheduler.timeline.js
@@ -345,6 +345,7 @@ class SchedulerTimeline extends SchedulerWorkSpace {
         }
 
         this.renderWorkSpace();
+
         if(this.isRenovatedRender()) {
             this.virtualScrollingDispatcher.updateDimensions();
         }
