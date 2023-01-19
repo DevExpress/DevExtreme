6f666611f69d3debf4db94a73c70c1751aa31504
Roman Semenov <roman.semenov@devexpress.com>
2023-01-19 20:37:10 +0400

    fix
    (cherry picked from commit e7feeacd6d73b287ef7d0cb4c7129c26ed5b9bc2)

--- js/ui/pivot_grid/data_source.js
+++ js/ui/pivot_grid/data_source.js
@@ -458,7 +458,9 @@ export default Class.inherit((function() {
                 } else {
                     fieldsDictionary[fieldKey] = mergedField = field;
                 }
-                extend(mergedField, { dataType: dataTypes[field.dataField] });
+                if(dataTypes[field.dataField]) {
+                    mergedField.dataType ||= dataTypes[field.dataField];
+                }
                 delete fieldsDictionary[fieldKey];
                 removedFields[fieldKey] = storeField;
 
