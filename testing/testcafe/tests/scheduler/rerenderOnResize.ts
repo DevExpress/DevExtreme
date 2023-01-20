bb2864a68703ea7528297295cbe0a6ec43ef5913
Gagik Harutyunyan <53352872+gagharutyunyan1993@users.noreply.github.com>
2023-01-20 10:34:47 +0400

    test commit to re-run tests

--- testing/testcafe/tests/scheduler/rerenderOnResize.ts
+++ testing/testcafe/tests/scheduler/rerenderOnResize.ts
@@ -54,7 +54,6 @@ safeSizeTest('Appointment should not re-rendered on window resize when width and
   const { element } = scheduler.getAppointment('test');
 
   await setStyleAttribute(element, 'background-color: red;');
-  await t
-    .resizeWindow(300, 300);
+  await t.resizeWindow(300, 300);
   await t.expect(await getStyleAttribute(element)).eql('transform: translate(0px, 26px); width: 200px; height: 74px; background-color: red;');
 }).before(async () => createScheduler('#container', { width: 600, height: 400 }));
