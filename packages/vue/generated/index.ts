3fb40e396bd318d253a739dabc4e8053bd254b73
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2023-01-24 18:36:33 +0400

    Add vue-wrappers to playground

---
+++ packages/vue/generated/index.ts
@@ -0,0 +1,1 @@
+export { DxRadioButton } from "./radio-button";
---
+++ packages/vue/generated/radio-button.ts
@@ -0,0 +1,55 @@
+import RadioButton, { Properties } from "../../jquery/src/radio-button";
+import { createComponent } from "devextreme-vue/core/index";
+
+type AccessibleOptions = Pick<Properties,
+  "checked" |
+  "defaultChecked" |
+  "label" |
+  "labelTemplate" |
+  "name" |
+  "onClick" |
+  "onSelected" |
+  "radioTemplate"
+>;
+
+interface DxRadioButton extends AccessibleOptions {
+  readonly instance?: RadioButton;
+}
+const DxRadioButton = createComponent({
+  props: {
+    checked: Boolean,
+    defaultChecked: Boolean,
+    label: String,
+    labelTemplate: Function,
+    name: {},
+    onClick: Function,
+    onSelected: Function,
+    radioTemplate: Function
+  },
+  emits: {
+    "update:isActive": null,
+    "update:hoveredElement": null,
+    "update:checked": null,
+    "update:defaultChecked": null,
+    "update:label": null,
+    "update:labelTemplate": null,
+    "update:name": null,
+    "update:onClick": null,
+    "update:onSelected": null,
+    "update:radioTemplate": null,
+  },
+  computed: {
+    instance(): RadioButton {
+      return (this as any).$_instance;
+    }
+  },
+  beforeCreate() {
+    (this as any).$_WidgetClass = RadioButton;
+    (this as any).$_hasAsyncTemplate = true;
+  }
+});
+
+export default DxRadioButton;
+export {
+  DxRadioButton
+};
