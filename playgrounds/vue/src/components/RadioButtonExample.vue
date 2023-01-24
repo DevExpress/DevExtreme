3bb53b062b46a4f129d34c4fdb3093bb27563632
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2023-01-24 18:34:24 +0400

    Clean up playground

---
+++ playgrounds/vue/src/components/RadioButtonExample.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { DxRadioButton } from '../../../../packages/vue/generated/radio-button';
+</script>
+
+<template>
+  <main>
+    <DxRadioButton label="Select Me"/>
+  </main>
+</template>
