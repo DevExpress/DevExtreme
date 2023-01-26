300db1d0e140f408b14a02866419de3f94675677
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2023-01-26 12:21:47 +0400

    Enhance example

--- playgrounds/vue/src/components/RadioButtonExample.vue
+++ playgrounds/vue/src/components/RadioButtonExample.vue
@@ -1,9 +1,32 @@
 <script setup lang="ts">
-import { DxRadioButton } from '../../../../packages/vue/generated/radio-button';
+import { DxRadioButton } from '../../../../packages/vue/src/radio-button';
+</script>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      checked: false
+    }
+  },
+  methods: {
+    switchChecked() {
+      this.checked = !this.checked;
+    }
+  }
+}
 </script>
 
 <template>
   <main>
-    <DxRadioButton label="Select Me"/>
+    <DxRadioButton label="Check Me" :checked="checked" labelTemplate="labelTemplate" radioTemplate="radioTemplate">
+      <template #labelTemplate="{ data: { label } }">
+        {{ label.toUpperCase() }}
+      </template>
+      <template #radioTemplate="{ data: { checked } }">
+        {{ checked ? '✔' : '∅' }}
+      </template>
+    </DxRadioButton>
+      <button @click="switchChecked()">Switch Value</button>
   </main>
 </template>
