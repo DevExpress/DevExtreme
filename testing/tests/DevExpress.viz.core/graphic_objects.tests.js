09859efac6c78d4e7c0f5f12d2af57573b28293b
Shpileva Yuliya <krijovnick@gmail.com>
2023-01-27 10:45:43 +0400

    add tests for graphic_objects

---
+++ testing/tests/DevExpress.viz.core/graphic_objects.tests.js
@@ -0,0 +1,38 @@
+import { registerPattern, registerGradient, getGraphicObjects } from 'viz/graphic_objects';
+import utils from 'viz/core/utils';
+
+
+QUnit.module('Graphic objects', {
+    beforeEach: function() {
+        this.getNextDefsStub = sinon.stub();
+        this.getNextDefsStub.onCall(0).returns('DevExpressId_1');
+        this.getNextDefsStub.onCall(1).returns('DevExpressId_2');
+        utils.getNextDefsSvgId = this.getNextDefsStub;
+    }
+});
+
+QUnit.test('should register pattern', function(assert) {
+    const id_1 = registerPattern({ key: 'test_key_1' });
+    const id_2 = registerPattern({ key: 'test_key_2' });
+
+    assert.equal(this.getNextDefsStub.callCount, 2);
+    assert.equal(id_1, 'DevExpressId_1');
+    assert.equal(id_2, 'DevExpressId_2');
+    assert.deepEqual(getGraphicObjects(), {
+        'DevExpressId_1': { options: { key: 'test_key_1' }, element: 'pattern' },
+        'DevExpressId_2': { options: { key: 'test_key_2' }, element: 'pattern' }
+    });
+});
+
+QUnit.test('should register gradient', function(assert) {
+    const id_1 = registerGradient('gradient_type', { key: 'test_key_1' });
+    const id_2 = registerGradient('gradient_type', { key: 'test_key_2' });
+
+    assert.equal(this.getNextDefsStub.callCount, 2);
+    assert.equal(id_1, 'DevExpressId_1');
+    assert.equal(id_2, 'DevExpressId_2');
+    assert.deepEqual(getGraphicObjects(), {
+        'DevExpressId_1': { options: { key: 'test_key_1' }, element: 'gradient_type' },
+        'DevExpressId_2': { options: { key: 'test_key_2' }, element: 'gradient_type' }
+    });
+});
