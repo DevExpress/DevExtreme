63bdfd80b98c24019a122b038d8e2b664563c5b7
Alexander Bulychev <git@bulychev.pro>
2023-01-30 13:32:48 +0400

    WIP: reanimate devextreme workflows

--- packages/devextreme/build/gulp/vendor.js
+++ packages/devextreme/build/gulp/vendor.js
@@ -12,56 +12,56 @@ const DESTINATION_CSS_PATH = './artifacts/css';
 
 const JS_VENDORS = [
     {
-        path: '/angular/angular.js'
+        path: 'angular/angular.js'
     },
     {
-        path: '/exceljs/dist/exceljs.js'
+        path: 'exceljs/dist/exceljs.js'
     },
     {
-        path: '/file-saver/dist/FileSaver.js'
+        path: 'file-saver/dist/FileSaver.js'
     },
     {
-        path: '/jquery/dist/jquery.js'
+        path: 'jquery/dist/jquery.js'
     },
     {
-        path: '/jspdf/dist/jspdf.umd.js'
+        path: 'jspdf/dist/jspdf.umd.js'
     },
     {
-        path: '/jspdf-autotable/dist/jspdf.plugin.autotable.js'
+        path: 'jspdf-autotable'
     },
     {
-        path: '/jszip/dist/jszip.js'
+        path: 'jszip/dist/jszip.js'
     },
     {
-        path: '/knockout/build/output/knockout-latest.js',
+        path: 'knockout/build/output/knockout-latest.js',
         suffix: 'debug'
     },
     {
-        path: '/cldrjs/dist/cldr.js',
+        path: 'cldrjs/dist/cldr.js',
         noUglyFile: true
     },
     {
-        path: '/globalize/dist/globalize.js',
+        path: 'globalize/dist/globalize.js',
         noUglyFile: true
     },
     {
-        path: '/cldrjs/dist/cldr/@(event|supplemental|unresolved).js',
+        path: ['event', 'supplemental', 'unresolved'].map(f => `cldrjs/dist/cldr/${f}.js`),
         noUglyFile: true,
         base: '/cldrjs/dist/'
     },
     {
-        path: '/globalize/dist/globalize/@(number|currency|date|message).js',
+        path: ['number', 'currency', 'date', 'message'].map(f => `globalize/dist/globalize/${f}.js`),
         noUglyFile: true,
         base: '/globalize/dist/'
     },
     {
-        path: '/devexpress-diagram/dist/dx-@(diagram|diagram.min).js'
+        path: ['diagram', 'diagram.min'].map(f => `devexpress-diagram/dist/dx-${f}.js`)
     },
     {
-        path: '/devexpress-gantt/dist/dx-@(gantt|gantt.min).js'
+        path: ['gantt', 'gantt.min'].map(f => `devexpress-gantt/dist/dx-${f}.js`)
     },
     {
-        path: '/devextreme-quill/dist/dx-@(quill|quill.min).js'
+        path: ['quill', 'quill.min'].map(f => `devextreme-quill/dist/dx-${f}.js`)
     }
 ];
 
@@ -77,7 +77,8 @@ const CSS_VENDORS = [
 gulp.task('vendor-js', function() {
     return merge.apply(this, JS_VENDORS.map(function(vendor) {
         const sourceConfig = vendor.base ? { base: PACKAGES_SOURCE + vendor.base } : null;
-        const stream = gulp.src(PACKAGES_SOURCE + vendor.path, sourceConfig)
+        const vendorPaths = Array.isArray(vendor.path) ? vendor.path : [vendor.path];
+        const stream = gulp.src(vendorPaths.map(p => require.resolve(p)), sourceConfig)
             .pipe(gulp.dest(DESTINATION_JS_PATH));
 
         if(vendor.noUglyFile) {
@@ -87,7 +88,7 @@ gulp.task('vendor-js', function() {
                 .pipe(gulp.dest(DESTINATION_JS_PATH));
         }
 
-        const path = PACKAGES_SOURCE + vendor.path.replace(/js$/, `${vendor.suffix || 'min'}.js`);
+        const path = vendorPaths.map(p => require.resolve(p.replace(/(min\.)?js$/, `${vendor.suffix || 'min'}.js`)));
 
         return merge(stream, gulp.src(path, sourceConfig)
             .pipe(gulp.dest(DESTINATION_JS_PATH))
