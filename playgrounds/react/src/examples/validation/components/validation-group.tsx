1ad57010d618f0812e9ba17381d10560c6adfef5
const314 <constantin.panchenko@gmail.com>
2023-01-31 19:40:52 +0400

    validation group registration

--- playgrounds/react/src/examples/validation/components/validation-group.tsx
+++ playgrounds/react/src/examples/validation/components/validation-group.tsx
@@ -1,12 +1,25 @@
-import { PropsWithChildren, useMemo, useRef } from 'react';
+import { ValidationResult } from '@devextreme/interim';
+import {
+  ForwardedRef, forwardRef, PropsWithChildren, useContext, useImperativeHandle, useMemo, useRef,
+} from 'react';
+import { ValidationEngineContext } from '../contexts/validation-engine-context';
 import { ValidationGroupContext } from '../contexts/validation-group-context';
 
 interface ValidationGroupProps extends PropsWithChildren {
   name?: string
 }
 
-export function ValidationGroup({ name, children }: ValidationGroupProps) {
+function ValidationGroupComponent(
+  { name, children }: ValidationGroupProps,
+  imperativeRef: ForwardedRef<ValidationGroupRef>,
+) {
   const groupRef = useRef(Symbol(name || 'validation-group'));
+  const validationEngine = useContext(ValidationEngineContext);
+
+  useImperativeHandle(imperativeRef, () => ({
+    validate: () => (validationEngine.validateGroup(groupRef.current)),
+  }), [validationEngine]);
+
   const validationGroupContext = useMemo(() => ({ name, group: groupRef.current }), [name]);
   return (
     <ValidationGroupContext.Provider value={validationGroupContext}>
@@ -14,3 +27,10 @@ export function ValidationGroup({ name, children }: ValidationGroupProps) {
     </ValidationGroupContext.Provider>
   );
 }
+
+const ValidationGroup = forwardRef(ValidationGroupComponent);
+export interface ValidationGroupRef {
+  validate: () => ValidationResult
+}
+
+export { ValidationGroup };
--- playgrounds/react/src/examples/validation/components/validator.tsx
+++ playgrounds/react/src/examples/validation/components/validator.tsx
@@ -1,29 +1,59 @@
-import { ValidationRule } from '@devextreme/interim';
+import { ValidationResult, ValidationRule } from '@devextreme/interim';
 import {
-  PropsWithChildren, useContext, useEffect, useMemo, useRef,
+  PropsWithChildren, useCallback, useContext, useEffect, useMemo, useRef,
 } from 'react';
 import { EditorContext } from '../contexts/editor-context';
 import { ValidationEngineContext } from '../contexts/validation-engine-context';
 import { ValidationGroupContext } from '../contexts/validation-group-context';
 import { ValidatorContext } from '../contexts/validator-context';
 
+interface ValidatorImpl {
+  validate: () => ValidationResult,
+  validationRules?: ValidationRule[],
+  reset: () => void,
+  on: (event: string, callback: () => void) => void,
+  off: (event: string, callback: () => void) => void,
+}
+
 export function Validator({
   validationGroup: validationGroupProp,
   children,
 }: PropsWithChildren & { validationGroup?: string }) {
-  const rules = useRef<ValidationRule[]>([]);
   const validationGroupContext = useContext(ValidationGroupContext);
   const editorContext = useContext(EditorContext);
   const validationEngine = useContext(ValidationEngineContext);
+  const rules = useRef<ValidationRule[]>([]);
+  const performValidation = useCallback(() => {
+    if (editorContext) {
+      const { editorName, editorValue } = editorContext;
+      return validationEngine.validate(editorValue, rules.current, editorName);
+    }
+    return null;
+  },
+  [editorContext?.editorName, editorContext?.editorValue]);
+
+  const validator = useRef<ValidatorImpl>({
+    validationRules: rules.current,
+    validate: performValidation,
+    reset: () => {},
+    on: () => {},
+    off: () => {},
+  });
+
+  useEffect(() => {
+    const validationGroup = validationGroupProp ?? validationGroupContext;
+    validationEngine.registerValidatorInGroup(validationGroup, validator.current);
+    return () => validationEngine.removeRegisteredValidator(validationGroup, validator.current);
+  }, [validationGroupProp ?? validationGroupContext]);
+
+  useEffect(() => {
+    validator.current.validate = performValidation;
+  }, [performValidation]);
 
   useEffect(() => {
     if (editorContext) {
-      const { editorName, editorValue, notifyErrorRaised } = editorContext;
-      const validationGroup = validationGroupProp
-        ? { name: validationGroupProp } : validationGroupContext;
-      console.log(validationGroup);
-      const validationResult = validationEngine.validate(editorValue, rules.current, editorName);
-      console.log(validationResult);
+      const { notifyErrorRaised } = editorContext;
+      const validationResult = validator.current.validate();
       notifyErrorRaised(
         validationResult.isValid
           ? [] : validationResult.brokenRules.map((rule: ValidationRule) => rule.message),
