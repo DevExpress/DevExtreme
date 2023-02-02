9847b2b572401c29144cfb710ad283eba3da68ed
marker dao ® | roman <mark_nikolsky@outlook.com>
2023-02-02 21:44:14 +0700

    Overlay: Skip check for container existence if container is not defined (T1143527)

--- testing/tests/DevExpress.ui.widgets/overlay.tests.js
+++ testing/tests/DevExpress.ui.widgets/overlay.tests.js
@@ -339,15 +339,18 @@ testModule('render', moduleConfig, () => {
         assert.ok(overlayContainer.parent().hasClass(VIEWPORT_CLASS), 'overlay\'s container is the viewport\'s child');
     });
 
-    test('Overlay does not fail if swatch is undefined (render before documentReady, T713615)', function(assert) {
+    test('Overlay does not fail if swatch is undefined (render before documentReady, T713615, T1143527)', function(assert) {
         const stub = sinon.stub(swatch, 'getSwatchContainer', () => {
-            stub.restore();
             return undefined;
         });
 
-        const container = $('#container');
-        container.dxOverlay({ visible: true }).dxOverlay('instance');
-        assert.expect(0);
+        try {
+            $('#container').dxOverlay({ visible: true });
+
+            assert.expect(0);
+        } finally {
+            stub.restore();
+        }
     });
 
     QUnit.module('Breaking change t1123711 - warning W1021', () => {
