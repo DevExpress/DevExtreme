be9a683600dc0824d1112e8c414ab1700c607204
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2023-02-06 21:13:14 +0400

    Fix int

--- packages/core/src/__test__/integration/uncontrolled-component.test.ts
+++ packages/core/src/__test__/integration/uncontrolled-component.test.ts
@@ -1,11 +1,11 @@
-import { createSelector, createStore } from "../../index";
+import { createSelector, createStore } from '../../index';
 
 const PROP1_DEFAULT = 'prop1-default';
 const PROP1_PARAM = 'param1';
 
 type State = {
   prop1: string;
-}
+};
 
 function createUncontrolledComponent({
   onProp1Change,
@@ -14,7 +14,6 @@ function createUncontrolledComponent({
   onProp1Change?(v: string): void,
   prop1Default?: string,
 } = {}) {
-
   const store = createStore<State>({
     prop1: prop1Default,
   }, {
@@ -23,32 +22,29 @@ function createUncontrolledComponent({
       changeCallback(value: string): void {
         onProp1Change?.(value);
       },
-    }
+    },
   });
 
   const selector1 = createSelector(
     (state: State) => ({ ...state, param1: PROP1_PARAM }),
-    ({ prop1, param1 }) => `selected1-${param1}-${prop1}`
-  )
+    ({ prop1, param1 }) => `selected1-${param1}-${prop1}`,
+  );
 
   let selected1 = selector1(store.getState());
 
-  store.subscribe((state) => selected1 = selector1(state));
+  store.subscribe((state) => { selected1 = selector1(state); });
 
   return {
     selected1,
-  }
+  };
 }
 
 describe('uncontrolled component', () => {
-
   it('provides selected value', () => {
-
     const uncontrolledComponent = createUncontrolledComponent({
-      prop1Default: 'abc'
+      prop1Default: 'abc',
     });
 
     expect(uncontrolledComponent.selected1).toBe('selected1-param1-abc');
-  })
-
+  });
 });
