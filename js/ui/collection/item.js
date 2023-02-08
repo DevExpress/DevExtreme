d7418e4e915a652a96d44643bd71e92e99442f9b
marker dao ® | Roman Ty <mark_nikolsky@outlook.com>
2023-02-08 16:59:18 +0700

    fix(CollectionWidgetItem): Use setAria for setting aria-disabled

--- js/ui/collection/item.js
+++ js/ui/collection/item.js
@@ -6,7 +6,6 @@ import { attachInstanceToElement, getInstanceByElement } from '../../core/utils/
 const INVISIBLE_STATE_CLASS = 'dx-state-invisible';
 const DISABLED_STATE_CLASS = 'dx-state-disabled';
 const ITEM_CONTENT_PLACEHOLDER_CLASS = 'dx-item-content-placeholder';
-const DISABLED_STATE_ATTR = 'aria-disabled';
 
 const forcibleWatcher = function(watchMethod, fn, callback) {
     const filteredCallback = (function() {
@@ -78,7 +77,7 @@ const CollectionItem = Class.inherit({
 
     _renderDisabled: function(value, oldValue) {
         this._$element.toggleClass(DISABLED_STATE_CLASS, !!value);
-        this._$element.attr(DISABLED_STATE_ATTR, !!value);
+        this.setAria('disabled', !!value, this._$element);
 
         this._updateOwnerFocus(value);
     },
