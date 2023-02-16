04afbf27b74895799580dc267853c89d6f85b445
ilya.kharchenko <14272298+IlyaKhD@users.noreply.github.com>
2023-02-17 01:39:28 +1200

    Synс with mono branch (2e8a2a5) (#23891)

--- js/core/utils/common.d.ts
+++ js/core/utils/common.d.ts
@@ -9,3 +9,5 @@ export function ensureDefined<T>(value: T, defaultValue: T): NonNullable<T>;
 export function equalByValue(object1: unknown, object2: unknown, depth?: number, strict?: boolean): boolean;
 
 export function deferUpdate<T>(func: () => T, deferred?: DeferredObj<T>): T | Promise<T> | DeferredObj<T>;
+
+export function escapeRegExp(string: string): string;
