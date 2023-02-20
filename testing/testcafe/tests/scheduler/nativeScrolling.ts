d023a3e4acca7d84c2eb971e14313f8d05d0b597
Gagik Harutyunyan <53352872+gagharutyunyan1993@users.noreply.github.com>
2023-02-20 17:39:54 +0400

    added screenshot test for bug

--- testing/testcafe/tests/scheduler/nativeScrolling.ts
+++ testing/testcafe/tests/scheduler/nativeScrolling.ts
@@ -1,4 +1,6 @@
 import { ClientFunction } from 'testcafe';
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import { changeTheme } from '../../helpers/changeTheme';
 import createWidget from '../../helpers/createWidget';
 import url from '../../helpers/getPageUrl';
 import Scheduler from '../../model/scheduler';
@@ -57,3 +59,36 @@ test('ScrollToTime works correctly with timelineDay and timelineWeek view (T7499
   groups: ['priority'],
   height: 580,
 }));
+
+const resourcesData = [...Array(20).keys()].map((num: number) => ({
+  text: `Name ${num}`,
+  id: num,
+}));
+
+test('The group panel and date table stay in sync during scrolling on material themes (T1146448)', async (t) => {
+  const scheduler = new Scheduler('#container');
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+  await scheduler.scrollTo(new Date(2021, 1, 2), { ownerId: 19 });
+
+  await t
+    .expect(await takeScreenshot('material-theme-scrolling-vertical', scheduler.workSpace))
+    .ok()
+
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => {
+  await changeTheme('material.blue.light');
+
+  return createWidget('dxScheduler', {
+    dataSource: [],
+    views: ['timelineWeek'],
+    currentView: 'timelineWeek',
+    groups: ['ownerId'],
+    currentDate: new Date(2021, 1, 2),
+    resources: [{ fieldExpr: 'ownerId', dataSource: resourcesData, label: 'Owner' }],
+    height: 600,
+  });
+}).after(async () => {
+  await changeTheme('generic.light');
+});
