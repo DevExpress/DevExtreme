923e202f202d057a76b79d3b7085001dc27bdcb4
Ilya Vinogradov <ilya.vinogradov@devexpress.com>
2023-02-23 16:32:43 +0400

    TreeList(T1147695): Add missed renaming after code review.

--- js/ui/grid_core/keyboard_navigation/dom.js
+++ js/ui/grid_core/keyboard_navigation/dom.js
@@ -1,12 +1,12 @@
 import { ATTRIBUTES } from './const';
 
-const isCommandCell = ($cell) => $cell.attr(ATTRIBUTES.dragCell) !== undefined;
+const isDragCell = ($cell) => $cell.attr(ATTRIBUTES.dragCell) !== undefined;
 
 const getCellToFocus = ($cellElements, columnIndex) => $cellElements
     .filter(`[${ATTRIBUTES.ariaColIndex}="${columnIndex + 1}"]:not([${ATTRIBUTES.dragCell}])`)
     .first();
 
 export const GridCoreKeyboardNavigationDom = {
-    isCommandCell,
+    isDragCell,
     getCellToFocus,
 };
