1ad6a4e5671df65547d25059c4e20df59b751f96
Ilya Vinogradov <ilya.vinogradov@devexpress.com>
2023-02-23 15:12:35 +0400

    GridCore(T1147695): Add e2e test for custom buttons scenarios.

--- testing/testcafe/tests/dataGrid/keyboardNavigation/markup.screenshots.ts
+++ testing/testcafe/tests/dataGrid/keyboardNavigation/markup.screenshots.ts
@@ -12,8 +12,8 @@ const DATA_GRID_SELECTOR = '#container';
 test('Focused cells should look correctly', async (t) => {
   const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
   const dataGrid = new DataGrid(DATA_GRID_SELECTOR);
-  const headerCellToFocus = dataGrid.getHeaders().getHeaderRow(0).getHeaderCell(1);
-  const dataCellToFocus = dataGrid.getDataCell(0, 1);
+  const headerCellToFocus = dataGrid.getHeaders().getHeaderRow(0).getHeaderCell(0);
+  const dataCellToFocus = dataGrid.getDataCell(0, 0);
 
   await t.click(headerCellToFocus.element)
     .pressKey('tab');
@@ -29,29 +29,78 @@ test('Focused cells should look correctly', async (t) => {
   dataSource: [
     {
       id: 1,
-      parentId: 0,
       columnA: 'A_0',
       columnB: 'B_0',
     },
     {
       id: 2,
-      parentId: 0,
       columnA: 'A_1',
       columnB: 'B_1',
     },
     {
       id: 3,
-      parentId: 0,
       columnA: 'A_2',
       columnB: 'B_2',
     },
   ],
   keyExpr: 'id',
-  parentIdExpr: 'parentId',
   columns: ['id', 'columnA', 'columnB'],
-  rowDragging: {
-    allowReordering: true,
+  sorting: {
+    mode: 'none',
   },
+}));
+
+test('Focused custom buttons should look correctly', async (t) => {
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+  const dataGrid = new DataGrid(DATA_GRID_SELECTOR);
+  const headerCellToFocus = dataGrid.getHeaders().getHeaderRow(0).getHeaderCell(3);
+
+  await t.click(headerCellToFocus.element)
+    .pressKey('tab');
+  await takeScreenshot('data-grid_keyboard-navigation-custom-buttons-cell-focused.png');
+
+  await t.pressKey('tab');
+  await takeScreenshot('data-grid_keyboard-navigation-custom-button-focused.png');
+
+  await t.expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => createWidget('dxDataGrid', {
+  dataSource: [
+    {
+      id: 1,
+      columnA: 'A_0',
+      columnB: 'B_0',
+    },
+    {
+      id: 2,
+      columnA: 'A_1',
+      columnB: 'B_1',
+    },
+    {
+      id: 3,
+      columnA: 'A_2',
+      columnB: 'B_2',
+    },
+  ],
+  keyExpr: 'id',
+  columns: [
+    {
+      type: 'buttons',
+      buttons: [
+        {
+          hint: 'button_1',
+          icon: 'edit',
+        },
+        {
+          hint: 'button_2',
+          icon: 'remove',
+        },
+      ],
+    },
+    'id',
+    'columnA',
+    'columnB',
+  ],
   sorting: {
     mode: 'none',
   },
