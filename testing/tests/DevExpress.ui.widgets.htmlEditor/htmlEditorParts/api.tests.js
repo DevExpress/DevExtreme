d0f93662d75682fc26855ea38bb929077e04549e
polosatov.alexander <alexanderPolosatov@users.noreply.github.com>
2023-02-24 15:15:03 +0400

    HtmlEditor: Fix history buttons state after call clearHistory method(T1133799) (#23918)

--- testing/tests/DevExpress.ui.widgets.htmlEditor/htmlEditorParts/api.tests.js
+++ testing/tests/DevExpress.ui.widgets.htmlEditor/htmlEditorParts/api.tests.js
@@ -7,6 +7,8 @@ import { isObject } from 'core/utils/type';
 const { test, module: testModule } = QUnit;
 
 const TOOLBAR_FORMAT_WIDGET_CLASS = 'dx-htmleditor-toolbar-format';
+const DISABLED_STATE_CLASS = 'dx-state-disabled';
+const BUTTON_CLASS = 'dx-button';
 
 const moduleConfig = {
     beforeEach: function() {
@@ -254,19 +256,52 @@ testModule('API', moduleConfig, () => {
         assert.strictEqual(this.instance.option('value'), '<p>cbaTest 1</p><p>Test 2</p><p>Test 3</p>', 'redo operation');
     });
 
-    test('clearHistory', function(assert) {
+    test('value should not change on undo after "clearHistory" call', function(assert) {
         this.createEditor();
         this.instance.insertText(0, 'a');
-        this.clock.tick(1000);
-        this.instance.insertText(0, 'b');
-        this.clock.tick(1000);
-        this.instance.insertText(0, 'c');
         this.clock.tick(1000);
 
         this.instance.clearHistory();
         this.instance.undo();
 
-        assert.strictEqual(this.instance.option('value'), '<p>cbaTest 1</p><p>Test 2</p><p>Test 3</p>', 'history is empty');
+        assert.strictEqual(this.instance.option('value'), '<p>aTest 1</p><p>Test 2</p><p>Test 3</p>', 'value is actual');
+    });
+
+    test('value should not change on redo after "clearHistory" call', function(assert) {
+        this.createEditor();
+        this.instance.insertText(0, 'a');
+        this.clock.tick(1000);
+        this.instance.undo();
+
+        this.instance.clearHistory();
+        this.instance.redo();
+
+        assert.strictEqual(this.instance.option('value'), '<p>Test 1</p><p>Test 2</p><p>Test 3</p>', 'value is actual');
+    });
+
+    test('undo button should have disabled state after call "clearHistory"', function(assert) {
+        this.options.toolbar = { items: ['undo'] };
+        this.createEditor();
+        this.instance.insertText(0, 'a');
+        this.clock.tick(1000);
+        const $undoButton = $('#htmlEditor').find(`.${BUTTON_CLASS}`);
+
+        this.instance.clearHistory();
+
+        assert.strictEqual($undoButton.hasClass(DISABLED_STATE_CLASS), true, 'undo button disabled');
+    });
+
+    test('redo button should have disabled state after call "undo"->"clearHistory"', function(assert) {
+        this.options.toolbar = { items: ['redo'] };
+        this.createEditor();
+        this.instance.insertText(0, 'a');
+        this.clock.tick(1000);
+        const $redoButton = $('#htmlEditor').find(`.${BUTTON_CLASS}`);
+
+        this.instance.undo();
+        this.instance.clearHistory();
+
+        assert.strictEqual($redoButton.hasClass(DISABLED_STATE_CLASS), true, 'redo button disabled');
     });
 
     test('registerModule', function(assert) {
