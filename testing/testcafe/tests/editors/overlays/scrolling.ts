9c86918ad68d7ab4f603333c3b221796185e94b4
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2023-02-28 12:38:55 +0400

    remove body style attribute after test

--- testing/testcafe/tests/editors/overlays/scrolling.ts
+++ testing/testcafe/tests/editors/overlays/scrolling.ts
@@ -6,6 +6,7 @@ import url from '../../../helpers/getPageUrl';
 import createWidget from '../../../helpers/createWidget';
 import {
   appendElementTo, getComputedPropertyValue, getDocumentScrollTop, insertStylesheetRulesToPage,
+  removeAttribute,
 } from '../../../helpers/domUtils';
 import Popup from '../../../model/overlay/index';
 
@@ -16,7 +17,7 @@ if (!isMaterial()) {
   [false, true].forEach((shading) => {
     [false, true].forEach((enableBodyScroll) => {
       [false, true].forEach((fullScreen) => {
-        test(`Overlay with native scrolling, shading: ${shading}, enableBodyScroll: ${enableBodyScroll}, fullScreen: ${fullScreen}`, async (t) => {
+        test(`Overlay native scrolling, shading: ${shading}, enableBodyScroll: ${enableBodyScroll}, fullScreen: ${fullScreen}`, async (t) => {
           const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
           const overlay = new Popup('#overlay');
@@ -59,13 +60,12 @@ if (!isMaterial()) {
             .expect(getDocumentScrollTop())
             .eql(258);
 
-          await testScreenshot(t, takeScreenshot, `Overlay with native scrolling,shading=${shading},enableBodyScroll=${enableBodyScroll},fullScreen=${fullScreen}.png`);
+          await testScreenshot(t, takeScreenshot, `Overlay native scrolling,shading=${shading},enableBodyScroll=${enableBodyScroll},fullScreen=${fullScreen}.png`);
 
           await overlay.hide();
 
           await checkBodyStyles({ paddingRight: '10px', overflow: 'auto' });
           await t
-
             .expect(getDocumentScrollTop())
             .eql(258);
 
@@ -120,7 +120,7 @@ if (!isMaterial()) {
             <div class="room">\
               <div class="caption">Rooms</div>\
               <div class="features">\
-                <div>Climate control    </div>\
+                <div>Climate control</div>\
                 <div>Air conditioning</div>\
                 <div>Coffee/tea maker</div>\
                 <div>Iron/ironing</div>\
@@ -136,6 +136,8 @@ if (!isMaterial()) {
               $content.html(overlayContent);
             },
           }, '#overlay');
+        }).after(async () => {
+          await removeAttribute('body', 'style');
         });
       });
     });
