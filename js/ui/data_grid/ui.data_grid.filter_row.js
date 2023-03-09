361e419ad4684ce4ad3cc8bf4b381083da8f6a6a
Roman Semenov <roman.semenov@devexpress.com>
2023-03-09 10:59:01 +0400

    update missing import

--- js/ui/data_grid/ui.data_grid.filter_row.js
+++ js/ui/data_grid/ui.data_grid.filter_row.js
@@ -1,4 +1,4 @@
 import core from './ui.data_grid.core';
-import { filterRowModule } from '../grid_core/filtering/filter_row';
+import { filterRowModule } from '../grid_core/filtering/module_filter_row';
 
 core.registerModule('filterRow', filterRowModule);
