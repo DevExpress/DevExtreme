18865dc8797d0b6ca71cd227c5e80dcdcfba7464
Ilya Vinogradov <48182348+williamvinogradov@users.noreply.github.com>
2023-03-21 13:46:22 +0400

    Pivot grid:  Refactoring sortable separated modules. (#24034)
    Co-authored-by: Ilya Vinogradov <ilya.vinogradov@devexpress.com>

---
+++ js/__internal/grids/pivot_grid/field_chooser/const.ts
@@ -0,0 +1,56 @@
+export const ATTRIBUTES = {
+  treeViewItem: 'tree-view-item',
+  allowScrolling: 'allow-scrolling',
+  itemGroup: 'item-group',
+};
+
+export const CLASSES = {
+  area: {
+    self: 'dx-area',
+    box: 'dx-area-box',
+    caption: 'dx-area-caption',
+    icon: 'dx-area-icon',
+    field: 'dx-area-field',
+    fieldContainer: 'dx-area-field-container',
+    fieldContent: 'dx-area-field-content',
+    fieldList: 'dx-area-fields',
+    fieldListHeader: 'dx-area-fields-header',
+  },
+  pivotGrid: {
+    dragAction: 'dx-pivotgrid-drag-action',
+    fieldsContainer: 'dx-pivotgrid-fields-container',
+  },
+  fieldChooser: {
+    self: 'dx-pivotgridfieldchooser',
+    container: 'dx-pivotgridfieldchooser-container',
+    contextMenu: 'dx-pivotgridfieldchooser-context-menu',
+  },
+  layout: {
+    zero: 'dx-layout-0',
+    second: 'dx-layout-2',
+  },
+  treeView: {
+    self: 'dx-treeview',
+    borderVisible: 'dx-treeview-border-visible',
+  },
+  scrollable: {
+    self: 'dx-scrollable',
+  },
+  allFields: 'dx-all-fields',
+  col: 'dx-col',
+  headerFilter: 'dx-header-filter',
+  row: 'dx-row',
+  widget: 'dx-widget',
+};
+
+export const ICONS = {
+  measure: 'measure',
+  hierarchy: 'hierarchy',
+  dimension: 'dimension',
+};
+
+export const SORTABLE_CONST = {
+  targets: {
+    drag: 'drag',
+  },
+};
