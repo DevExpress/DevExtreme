04403256296996bf7f06a28d662e3fc4077d1f38
marker dao ® | roman <mark_nikolsky@outlook.com>
2023-03-22 18:16:57 +0700

    Update js/viz/chart.d.ts
    Co-authored-by: ilya.kharchenko <14272298+IlyaKhD@users.noreply.github.com>
    Signed-off-by: marker dao ® | roman <mark_nikolsky@outlook.com>

--- js/viz/chart.d.ts
+++ js/viz/chart.d.ts
@@ -233,7 +233,7 @@ export interface baseLabelObject {
      * @return object
      * @public
      */
-    shift(x: number, y: number): baseLabelObject;
+    shift(x: number, y: number): this;
     /**
      * @docid
      * @publicName hide()
