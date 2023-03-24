d9b61d3368d7163ef50c703a17746e50842b4a73
Roman Semenov <roman.semenov@devexpress.com>
2023-03-24 17:10:54 +0400

    PivotGrid - Unnecessary separator appears in the header filter in material themes (T1149159) (#24100)

--- testing/testcafe/model/pivotGrid/columnHeaderArea.ts
+++ testing/testcafe/model/pivotGrid/columnHeaderArea.ts
@@ -1,6 +1,15 @@
+import { Selector } from 'testcafe';
+
 const CLASSES = {
   columnHeader: 'dx-column-header',
   action: 'dx-pivotgrid-action',
+  columnIndicators: 'dx-column-indicators',
+  headerFilter: {
+    icon: 'dx-header-filter',
+    menu: 'dx-header-filter-menu',
+  },
+  popup: 'dx-popup-normal',
+  scrollable: 'dx-scrollable-container',
 };
 
 export default class ColumnHeaderArea {
@@ -12,5 +21,21 @@ export default class ColumnHeaderArea {
 
   getAction(idx = 0): Selector {
     return this.element.find(`.${CLASSES.action}`).nth(idx);
+  }
+
+  // todo: move header filter to seperate file
+
+  // popup is contained in viewport
+  // eslint-disable-next-line class-methods-use-this
+  getHeaderFilterMenu(): Selector {
+    return Selector(`.${CLASSES.headerFilter.menu}`);
+  }
+
+  getHeaderFilterIcon(idx = 0): Selector {
+    return this.getAction(idx).find(`.${CLASSES.headerFilter.icon}`);
+  }
+
+  getHeaderFilterScrollable(): Selector {
+    return this.getHeaderFilterMenu().find(`.${CLASSES.scrollable}`);
   }
 }
