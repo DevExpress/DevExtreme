61fe550acc0fb03bdb4c2a99efe6fc3df4b85967
timbset <timbset@gmail.com>
2023-03-27 18:07:11 +0400

    Simplify gantt tests

--- testing/tests/DevExpress.ui.widgets/gantt.missingModules.tests.js
+++ testing/tests/DevExpress.ui.widgets/gantt.missingModules.tests.js
@@ -1,1 +1,13 @@
-import './ganttParts/importGantt.tests.js';
+/* global System */
+System.addImportMap({
+    imports: {
+        'devexpress-gantt': '/testing/helpers/noGantt.js'
+    }
+});
+
+System.register(['./ganttParts/importGantt.tests.js'], function() {
+    return {
+        setters: [function() {}],
+        execute: function() {}
+    };
+});
--- testing/tests/DevExpress.ui.widgets/ganttParts/importGantt.tests.js
+++ testing/tests/DevExpress.ui.widgets/ganttParts/importGantt.tests.js
@@ -1,29 +1,12 @@
-/* global System */
+import { getGanttViewCore as getGantt } from 'ui/gantt/gantt_importer';
 
-System.addImportMap({
-    imports: {
-        'devexpress-gantt': '/testing/helpers/noGantt.js'
-    }
-});
-
-System.register(['ui/gantt/gantt_importer'], function() {
-    let getGantt;
-
-    return {
-        setters: [function(_ganttImporter) {
-            getGantt = _ganttImporter.getGanttViewCore;
-        }],
-        execute: function() {
-            QUnit.module('Import devexpress-gantt', function() {
-                QUnit.test('throw an error if the devexpress-gantt script isn\'t referenced', function(assert) {
-                    assert.throws(
-                        function() { getGantt(); },
-                        function(e) {
-                            return /(E1041)[\s\S]*(devexpress-gantt)/.test(e.message);
-                        }
-                    );
-                });
-            });
-        }
-    };
+QUnit.module('Import devexpress-gantt', function() {
+    QUnit.test('throw an error if the devexpress-gantt script isn\'t referenced', function(assert) {
+        assert.throws(
+            function() { getGantt(); },
+            function(e) {
+                return /(E1041)[\s\S]*(devexpress-gantt)/.test(e.message);
+            }
+        );
+    });
 });
