643254faf2159c9bb3786b6b236aa0d9598e3f01
Andrey Dolzhikov <andrey.dolzhikov@devexpress.com>
2023-03-29 15:54:46 +0300

    https://trello.com/c/d83PRrX8/6847-add-tests-for-a-case-from-the-t1140411-bug

---
+++ testing/testcafe/tests/dataSources/testData.ts
@@ -0,0 +1,12 @@
+export const getData = (rowCount: number, colCount: number): Record<string, string>[] => {
+  const items: Record<string, string>[] = [];
+  for (let i = 0; i < rowCount; i += 1) {
+    const item: Record<string, string> = {};
+    for (let j = 0; j < colCount; j += 1) {
+      item[`field_${j}`] = `val_${i}_${j}`;
+    }
+    items.push(item);
+  }
+
+  return items;
+};
