f14706ad7896fc108b1e22def17b571311f2b53e
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2023-03-30 11:56:20 +0400

    change test scenario

--- testing/testcafe/tests/editors/button/floatingAction.ts
+++ testing/testcafe/tests/editors/button/floatingAction.ts
@@ -9,24 +9,20 @@ import { appendElementTo, setStyleAttribute } from '../../../helpers/domUtils';
 const OVERLAY_CONTENT_CLASS = 'dx-overlay-content';
 const FA_MAIN_BUTTON_CLASS = 'dx-fa-button-main';
 
-fixture.disablePageReloads`FloatingAction`
-  .page(url(__dirname, '../../container.html'))
-  .beforeEach(async () => {
-    await ClientFunction(() => {
-      (window as any).DevExpress.config({
-        floatingActionButtonConfig: {
-          icon: 'edit',
-          shading: false,
-          position: {
-            of: '#container',
-            my: 'right bottom',
-            at: 'right bottom',
-            offset: '-16 -16',
-          },
-        },
-      });
-    })();
+const setGlobalConfig = ClientFunction(() => {
+  (window as any).DevExpress.config({
+    floatingActionButtonConfig: {
+      icon: 'edit',
+      shading: false,
+      position: {
+        of: '#container',
+      },
+    },
   });
+});
+
+fixture.disablePageReloads`FloatingAction`
+  .page(url(__dirname, '../../container.html'));
 
 for (const label of ['Add Row', undefined]) {
   for (const icon of ['home', undefined]) {
@@ -34,6 +30,7 @@ for (const label of ['Add Row', undefined]) {
       const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
       await testScreenshot(t, takeScreenshot, `FAB with one speed dial action button,label=${label},icon=${icon}.png`, {
+        element: '#container',
         shouldTestInCompact: true,
         compactCallBack: async () => {
           await ClientFunction(() => {
@@ -49,46 +46,28 @@ for (const label of ['Add Row', undefined]) {
       await setStyleAttribute(Selector('#container'), 'width: 300px; height: 300px;');
       await appendElementTo('#container', 'div', 'speed-dial-action');
 
+      await setGlobalConfig();
+
+      await ClientFunction(() => {
+        (window as any).DevExpress.config({
+          floatingActionButtonConfig: {
+            icon: 'edit',
+            shading: false,
+            position: {
+              of: '#container',
+              my: 'right bottom',
+              at: 'right bottom',
+              offset: '-16 -16',
+            },
+          },
+        });
+      })();
+
       await createWidget('dxSpeedDialAction', {
         label,
         icon,
         visible: true,
       }, '#speed-dial-action');
-    });
-
-    test(`FAB with two speed dial action buttons, label: ${label}, icon: ${icon}`, async (t) => {
-      const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
-
-      await testScreenshot(t, takeScreenshot, `FAB with speed dial action buttons,label=${label},icon=${icon}.png`, {
-        shouldTestInCompact: true,
-        compactCallBack: async () => {
-          await ClientFunction(() => {
-            (window as any).DevExpress.ui.repaintFloatingActionButton();
-          })();
-        },
-      });
-
-      await t
-        .expect(compareResults.isValid())
-        .ok(compareResults.errorMessages());
-    }).before(async () => {
-      await setStyleAttribute(Selector('#container'), 'width: 300px; height: 300px;');
-      await appendElementTo('#container', 'div', 'speed-dial-action');
-      await appendElementTo('#container', 'div', 'speed-dial-action-trash');
-
-      await createWidget('dxSpeedDialAction', {
-        label,
-        icon,
-        index: 1,
-        visible: true,
-      }, '#speed-dial-action');
-
-      await createWidget('dxSpeedDialAction', {
-        label: 'Remove Row',
-        icon: 'trash',
-        index: 2,
-        visible: true,
-      }, '#speed-dial-action-trash');
     });
 
     test(`FAB with two speed dial action buttons after opening, label: ${label}, icon: ${icon}`, async (t) => {
@@ -98,6 +77,7 @@ for (const label of ['Add Row', undefined]) {
         .click(Selector(`.${FA_MAIN_BUTTON_CLASS} .${OVERLAY_CONTENT_CLASS}`));
 
       await testScreenshot(t, takeScreenshot, `FAB with two speed dial actions after opening,label=${label},icon=${icon}.png`, {
+        element: '#container',
         shouldTestInCompact: true,
         compactCallBack: async () => {
           await ClientFunction(() => {
@@ -117,6 +97,8 @@ for (const label of ['Add Row', undefined]) {
       await appendElementTo('#container', 'div', 'speed-dial-action');
       await appendElementTo('#container', 'div', 'speed-dial-action-trash');
 
+      await setGlobalConfig();
+
       await createWidget('dxSpeedDialAction', {
         label,
         icon,
@@ -133,3 +115,41 @@ for (const label of ['Add Row', undefined]) {
     });
   }
 }
+
+test('FAB with two speed dial action buttons', async (t) => {
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+  await testScreenshot(t, takeScreenshot, 'FAB with two speed dial action buttons.png', {
+    element: '#container',
+    shouldTestInCompact: true,
+    compactCallBack: async () => {
+      await ClientFunction(() => {
+        (window as any).DevExpress.ui.repaintFloatingActionButton();
+      })();
+    },
+  });
+
+  await t
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => {
+  await setStyleAttribute(Selector('#container'), 'width: 300px; height: 300px;');
+  await appendElementTo('#container', 'div', 'speed-dial-action');
+  await appendElementTo('#container', 'div', 'speed-dial-action-trash');
+
+  await setGlobalConfig();
+
+  await createWidget('dxSpeedDialAction', {
+    label: 'Add row',
+    icon: 'plus',
+    index: 1,
+    visible: true,
+  }, '#speed-dial-action');
+
+  await createWidget('dxSpeedDialAction', {
+    label: 'Remove Row',
+    icon: 'trash',
+    index: 2,
+    visible: true,
+  }, '#speed-dial-action-trash');
+});
