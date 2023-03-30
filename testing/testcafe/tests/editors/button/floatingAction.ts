feafc2abcfddeef6dff1cf09e35ed4ea1842b715
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2023-03-30 10:33:06 +0400

    tests refactor

--- testing/testcafe/tests/editors/button/floatingAction.ts
+++ testing/testcafe/tests/editors/button/floatingAction.ts
@@ -9,15 +9,15 @@ import createWidget from '../../../helpers/createWidget';
 const OVERLAY_CONTENT_CLASS = 'dx-overlay-content';
 const FA_MAIN_BUTTON_CLASS = 'dx-fa-button-main';
 
-fixture`FloatingAction`
+fixture.disablePageReloads`FloatingAction`
   .page(url(__dirname, '../../container.html'));
 
 for (const label of ['Add Row', undefined]) {
   for (const icon of ['home', undefined]) {
-    safeSizeTest(`One speed dial action button render, label: ${label}, icon: ${icon}`, async (t) => {
+    safeSizeTest(`FAB with dne speed dial action button, label: ${label}, icon: ${icon}`, async (t) => {
       const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-      await testScreenshot(t, takeScreenshot, `One speed dial action button render,label=${label},icon=${icon}.png`, {
+      await testScreenshot(t, takeScreenshot, `FAB with one speed dial action button,label=${label},icon=${icon}.png`, {
         shouldTestInCompact: true,
         compactCallBack: async () => {
           await ClientFunction(() => {
@@ -37,10 +37,10 @@ for (const label of ['Add Row', undefined]) {
       });
     });
 
-    safeSizeTest(`Two speed dial action buttons render, label: ${label}, icon: ${icon}`, async (t) => {
+    safeSizeTest(`FAB with two speed dial action buttons, label: ${label}, icon: ${icon}`, async (t) => {
       const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-      await testScreenshot(t, takeScreenshot, `Two speed dial action buttons render,label=${label},icon=${icon}.png`, {
+      await testScreenshot(t, takeScreenshot, `FAB with speed dial action buttons,label=${label},icon=${icon}.png`, {
         shouldTestInCompact: true,
         compactCallBack: async () => {
           await ClientFunction(() => {
@@ -49,7 +49,32 @@ for (const label of ['Add Row', undefined]) {
         },
       });
 
-      await testScreenshot(t, takeScreenshot, `Two speed dial actions after click,label=${label},icon=${icon}.png`, {
+      await t
+        .expect(compareResults.isValid())
+        .ok(compareResults.errorMessages());
+    }, [300, 300]).before(async () => {
+      await createWidget('dxSpeedDialAction', {
+        label,
+        icon,
+        index: 1,
+        visible: true,
+      });
+
+      await createWidget('dxSpeedDialAction', {
+        label: 'Remove Row',
+        icon: 'trash',
+        index: 2,
+        visible: true,
+      }, '#otherContainer');
+    });
+
+    safeSizeTest(`FAB with two speed dial action buttons after opening, label: ${label}, icon: ${icon}`, async (t) => {
+      const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+      await t
+        .click(Selector(`.${FA_MAIN_BUTTON_CLASS} .${OVERLAY_CONTENT_CLASS}`));
+
+      await testScreenshot(t, takeScreenshot, `FAB with two speed dial actions after opening,label=${label},icon=${icon}.png`, {
         shouldTestInCompact: true,
         compactCallBack: async () => {
           await ClientFunction(() => {
