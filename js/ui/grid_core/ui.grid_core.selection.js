03641a13e3d0691e903c575b2e5df30f48609d81
Roman Semenov <roman.semenov@devexpress.com>
2023-04-05 00:14:19 +0400

    DataGrid - All rows are selected when deleting a row with deferred selection and refreshMode set to refresh/repaint (T1153564) (#24148)
    (cherry picked from commit b523ee4e7753ca8111185348e998ac89a410992d)

--- js/ui/grid_core/ui.grid_core.selection.js
+++ js/ui/grid_core/ui.grid_core.selection.js
@@ -13,6 +13,7 @@ import holdEvent from '../../events/hold';
 import Selection from '../selection/selection';
 import { Deferred } from '../../core/utils/deferred';
 import errors from '../widget/ui.errors';
+import { equalByValue } from '../../core/utils/common';
 
 const EDITOR_CELL_CLASS = 'dx-editor-cell';
 const ROW_CLASS = 'dx-row';
@@ -115,7 +116,19 @@ const SelectionController = gridCore.Controller.inherit((function() {
         },
 
         _handleDataPushed: function(changes) {
-            const removedKeys = changes.filter(change => change.type === 'remove').map(change => change.key);
+            let removedKeys = changes
+                .filter(change => change.type === 'remove')
+                .map(change => change.key);
+
+            if(this.option('selection.deferred')) {
+                const selectedKeys = this._dataController.items()
+                    .filter(item => item.isSelected)
+                    .map(item => item.key);
+
+                removedKeys = removedKeys
+                    .filter(key => selectedKeys.find(selectedKey => equalByValue(selectedKey, key)));
+            }
+
             removedKeys.length && this.deselectRows(removedKeys);
         },
 
