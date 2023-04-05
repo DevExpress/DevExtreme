cf63555597278a63512b2d7fb43603358eb85a57
marker dao ® | Roman Ty <mark_nikolsky@outlook.com>
2023-04-05 23:33:19 +0700

    feat(tabs: tests): Add TestCafe tests

--- testing/testcafe/tests/navigation/tabs/common.ts
+++ testing/testcafe/tests/navigation/tabs/common.ts
@@ -1,9 +1,12 @@
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import { Selector } from 'testcafe';
 import { testScreenshot } from '../../../helpers/themeUtils';
 import url from '../../../helpers/getPageUrl';
 import createWidget from '../../../helpers/createWidget';
 import { Item } from '../../../../../js/ui/tabs.d';
 import { appendElementTo, setAttribute } from '../../../helpers/domUtils';
+
+const TAB_SELECTOR = '.dx-tabs-wrapper .dx-tab';
 
 fixture.disablePageReloads`Tabs_common`
   .page(url(__dirname, '../../container.html'));
@@ -28,4 +31,58 @@ test('Tabs icon alignment', async (t) => {
   ] as Item[];
 
   return createWidget('dxTabs', { dataSource }, '#tabs');
+});
+
+[true, false].forEach((selectOnFocus) => {
+  test('Tabs item states', async (t) => {
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+    await testScreenshot(t, takeScreenshot, `Tabs without focus and selectOnFocus: ${selectOnFocus}.png`, { element: '#tabs' });
+
+    await t.pressKey('tab');
+    await testScreenshot(t, takeScreenshot, `Tabs when its available item has focus and selectOnFocus: ${selectOnFocus}.png`, { element: '#tabs' });
+
+    await t.pressKey('right');
+    await testScreenshot(t, takeScreenshot, `Tabs when its disabled item has focus and selectOnFocus: ${selectOnFocus}.png`, { element: '#tabs' });
+
+    const thirdItem = Selector(`${TAB_SELECTOR}:nth-child(3)`);
+    const fourthItem = Selector(`${TAB_SELECTOR}:nth-child(4)`);
+
+    await t
+      .pressKey('right')
+      .dispatchEvent(thirdItem, 'mousedown');
+
+    await testScreenshot(t, takeScreenshot, `Tabs when third item has active state and selectOnFocus: ${selectOnFocus}.png`, { element: '#tabs' });
+
+    await t
+      .dispatchEvent(thirdItem, 'mouseup')
+      .click(thirdItem)
+      .hover(fourthItem);
+
+    await testScreenshot(t, takeScreenshot, `Tabs when third item is selected, fourth item has hover state and selectOnFocus: ${selectOnFocus}.png`, { element: '#tabs' });
+
+    await t
+      .expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
+  }).before(async () => {
+    await appendElementTo('#container', 'div', 'tabs');
+    await setAttribute('#container', 'style', 'width: 500px; height: 600px;');
+
+    const dataSource = [
+      { text: 'John Heart' },
+      { text: 'Marina Thomas', disabled: true },
+      { text: 'Robert Reagan' },
+      { text: 'Greta Sims' },
+      { text: 'Olivia Peyton' },
+    ] as Item[];
+
+    const tabsOptions = {
+      dataSource,
+      selectOnFocus,
+      width: 450,
+      showNavButtons: true,
+    };
+
+    return createWidget('dxTabs', tabsOptions, '#tabs');
+  });
 });
