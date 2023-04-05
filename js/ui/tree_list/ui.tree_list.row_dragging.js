dd12093993594cdec70e0d18039fcd8b42aafbc5
Roman Semenov <roman.semenov@devexpress.com>
2023-04-05 16:59:03 +0400

    GridCore - move to ts (part 1) (#24183)
    Co-authored-by: Ilya Vinogradov <ilya.vinogradov@devexpress.com>

--- js/ui/tree_list/ui.tree_list.row_dragging.js
+++ js/ui/tree_list/ui.tree_list.row_dragging.js
@@ -1,4 +1,4 @@
 import gridCore from './ui.tree_list.core';
-import { rowDraggingModule } from '../grid_core/row_dragging/module';
+import { rowDraggingModule } from '../grid_core/ui.grid_core.row_dragging';
 
 gridCore.registerModule('rowDragging', rowDraggingModule);
