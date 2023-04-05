bdf91b0748c88e39d0830e37aed6674b3bfc3a8a
marker dao ® | Roman Ty <mark_nikolsky@outlook.com>
2023-04-06 00:46:30 +0700

    refactor(tabs: tests): Rename var && Fix descriptions

--- testing/testcafe/tests/navigation/tabs/common.ts
+++ testing/testcafe/tests/navigation/tabs/common.ts
@@ -6,7 +6,7 @@ import createWidget from '../../../helpers/createWidget';
 import { Item } from '../../../../../js/ui/tabs.d';
 import { appendElementTo, setAttribute } from '../../../helpers/domUtils';
 
-const TAB_SELECTOR = '.dx-tabs-wrapper .dx-tab';
+const TAB_CLASS = 'dx-tab';
 
 fixture.disablePageReloads`Tabs_common`
   .page(url(__dirname, '../../container.html'));
@@ -37,29 +37,29 @@ test('Tabs icon alignment', async (t) => {
   test('Tabs item states', async (t) => {
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-    await testScreenshot(t, takeScreenshot, `Tabs without focus and selectOnFocus: ${selectOnFocus}.png`, { element: '#tabs' });
+    await testScreenshot(t, takeScreenshot, `Tabs without focus and selectOnFocus=${selectOnFocus}.png`, { element: '#tabs' });
 
     await t.pressKey('tab');
-    await testScreenshot(t, takeScreenshot, `Tabs when its available item has focus and selectOnFocus: ${selectOnFocus}.png`, { element: '#tabs' });
+    await testScreenshot(t, takeScreenshot, `Tabs when its available item has focus and selectOnFocus=${selectOnFocus}.png`, { element: '#tabs' });
 
     await t.pressKey('right');
-    await testScreenshot(t, takeScreenshot, `Tabs when its disabled item has focus and selectOnFocus: ${selectOnFocus}.png`, { element: '#tabs' });
+    await testScreenshot(t, takeScreenshot, `Tabs when its disabled item has focus and selectOnFocus=${selectOnFocus}.png`, { element: '#tabs' });
 
-    const thirdItem = Selector(`${TAB_SELECTOR}:nth-child(3)`);
-    const fourthItem = Selector(`${TAB_SELECTOR}:nth-child(4)`);
+    const thirdItem = Selector(`.${TAB_CLASS}:nth-child(3)`);
+    const fourthItem = Selector(`.${TAB_CLASS}:nth-child(4)`);
 
     await t
       .pressKey('right')
       .dispatchEvent(thirdItem, 'mousedown');
 
-    await testScreenshot(t, takeScreenshot, `Tabs when third item has active state and selectOnFocus: ${selectOnFocus}.png`, { element: '#tabs' });
+    await testScreenshot(t, takeScreenshot, `Tabs when third item has active state and selectOnFocus=${selectOnFocus}.png`, { element: '#tabs' });
 
     await t
       .dispatchEvent(thirdItem, 'mouseup')
       .click(thirdItem)
       .hover(fourthItem);
 
-    await testScreenshot(t, takeScreenshot, `Tabs when third item is selected, fourth item has hover state and selectOnFocus: ${selectOnFocus}.png`, { element: '#tabs' });
+    await testScreenshot(t, takeScreenshot, `Tabs when third item is selected, fourth item has hover state and selectOnFocus=${selectOnFocus}.png`, { element: '#tabs' });
 
     await t
       .expect(compareResults.isValid())
