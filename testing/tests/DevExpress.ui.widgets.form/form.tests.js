ff64cc0003c12bc0dfd6941174a36603c2a9f73a
AlexanderMoiseev <moiseevalexandr1@gmail.com>
2023-04-06 18:30:55 +0700

    inline config

--- testing/tests/DevExpress.ui.widgets.form/form.tests.js
+++ testing/tests/DevExpress.ui.widgets.form/form.tests.js
@@ -4033,7 +4033,7 @@ function getColsCountFromDOM($form) {
 }
 
 QUnit.test('Setting "colCountByScreen" property of group with dot notation should update layout', function(assert) {
-    const config = {
+    const $form = $('#form').dxForm({
         screenByWidth: () => 'md',
         items: [
             {
@@ -4042,9 +4042,8 @@ QUnit.test('Setting "colCountByScreen" property of group with dot notation shoul
                 items: [{ dataField: 'name' }, { dataField: 'email' }, { dataField: 'age' }]
             }
         ],
-    };
+    });
 
-    const $form = $('#form').dxForm(config);
     const instance = $form.dxForm('instance');
 
     instance.option('items[0].colCountByScreen.md', 2);
