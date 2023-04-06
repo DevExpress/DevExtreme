e56fef98f5d044c3aa1c8cee7c947d0335fc2d66
Shpileva Yuliya <Krijovnick@users.noreply.github.com>
2023-04-06 18:21:53 +0400

    PieChart Doughnut jQuery demo: refactor centerTemplate method to support CSP (#2525)

--- Demos/Charts/DoughnutWithCustomLabelInCenter/jQuery/index.js
+++ Demos/Charts/DoughnutWithCustomLabelInCenter/jQuery/index.js
@@ -29,17 +29,56 @@ $(() => {
         .getVisiblePoints()
         .reduce((s, p) => s + p.originalValue, 0);
       const { country } = pieChart.getAllSeries()[0].getVisiblePoints()[0].data;
-      const content = $(`<svg><circle cx="100" cy="100" fill="#eee" r="${pieChart.getInnerRadius() - 6}"></circle>`
-                    + `<image x="70" y="58" width="60" height="40" href="../../../../images/flags/${country.replace(/\s/, '').toLowerCase()}.svg"/>`
-                    + '<text text-anchor="middle" style="font-size: 18px" x="100" y="120" fill="#494949">'
-                    + `<tspan x="100" >${country}</tspan>`
-                    + `<tspan x="100" dy="20px" style="font-weight: 600">${
-                      formatNumber(total)
-                    }</tspan></text></svg>`);
 
-      container.appendChild(content.get(0));
+      const circle = createElement('circle', {
+        cx: 100,
+        cy: 100,
+        fill: '#eee',
+        r: pieChart.getInnerRadius() - 6,
+      });
+      const image = createElement('image', {
+        x: 70,
+        y: 58,
+        width: 60,
+        height: 40,
+        href: `../../../../images/flags/${country.replace(/\s/, '').toLowerCase()}.svg`,
+      });
+      const text = createText({
+        x: 100,
+        y: 120,
+        fill: '#494949',
+        'text-anchor': 'middle',
+        'font-size': 18,
+      }, [country, formatNumber(total)]);
+
+      container.appendChild(circle);
+      container.appendChild(image);
+      container.appendChild(text);
     },
   };
+
+  function createElement(type, attributes) {
+    const element = document.createElementNS('http://www.w3.org/2000/svg', type);
+    Object.keys(attributes).forEach((attributeName) => {
+      element.setAttribute(attributeName, attributes[attributeName]);
+    });
+    return element;
+  }
+
+  function createText(attributes, contents) {
+    const text = createElement('text', attributes);
+
+    const tspan1 = createElement('tspan', { x: attributes.x, dy: 0 });
+    tspan1.textContent = contents[0];
+
+    const tspan2 = createElement('tspan', { x: attributes.x, dy: 20, 'font-weight': 600 });
+    tspan2.textContent = contents[1];
+
+    text.appendChild(tspan1);
+    text.appendChild(tspan2);
+
+    return text;
+  }
 
   $('#countries')
     .dxPieChart($.extend({}, commonSettings, {
