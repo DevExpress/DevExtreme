1d15adc6f12cf1fde0f7ac4a039639cc6a2bafac
Zedwag <zedwag@gmail.com>
2023-04-10 08:47:55 -0400

    create component folder

---
+++ js/ui/date_range_box/ui.date_range_box.js
@@ -0,0 +1,39 @@
+import registerComponent from '../../core/component_registrator';
+import { extend } from '../../core/utils/extend';
+import DateBox from '../date_box';
+
+// STYLE dateBox
+
+class DateRangeBox extends DateBox {
+    _getDefaultOptions() {
+        return extend(super._getDefaultOptions(), {
+            endDate: null,
+            startDate: null,
+            value: [null, null],
+
+            /**
+            * @name dxDateRangeBoxOptions.adaptivityEnabled
+            * @hidden
+            */
+
+            /**
+            * @name dxDateRangeBoxOptions.interval
+            * @hidden
+            */
+
+            /**
+             * @name dxDateRangeBoxOptions.showAnalogClock
+             * @hidden
+             */
+
+            /**
+            * @name dxDateRangeBoxOptions.type
+            * @hidden
+            */
+        });
+    }
+}
+
+registerComponent('dxDateRangeBox', DateRangeBox);
+
+export default DateRangeBox;
