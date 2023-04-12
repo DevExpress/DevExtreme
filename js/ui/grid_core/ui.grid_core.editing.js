492a79e6dd37683a2a681f6d685cc069ae61c4d3
Alyar <>
2023-04-13 01:46:25 +0400

    Fix tests

--- js/ui/grid_core/ui.grid_core.editing.js
+++ js/ui/grid_core/ui.grid_core.editing.js
@@ -2574,7 +2574,7 @@ export const editingModule = {
                 _renderCore: function(change) {
                     this.callBase.apply(this, arguments);
 
-                    return this._waitAsyncTemplates(change, true).done(() => {
+                    return this.waitAsyncTemplates(change, true).done(() => {
                         this._editingController._focusEditorIfNeed();
                     });
                 }
