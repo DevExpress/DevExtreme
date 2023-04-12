aa783299b8b77264f3e184995d5f6f44a4529da7
Andrew Makarov <andrewmakarov@inbox.ru>
2023-04-12 15:00:17 +0400

    Use allDay expression

---
+++ testing/testcafe/tests/scheduler/layout/appointments/allDay/allDayExpr.ts
@@ -0,0 +1,52 @@
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import Scheduler from '../../../../../model/scheduler';
+import createWidget from '../../../../../helpers/createWidget';
+import url from '../../../../../helpers/getPageUrl';
+
+fixture.disablePageReloads`Layout:Appointments:allDayExpr`
+  .page(url(__dirname, '../../../../container.html'));
+
+[{
+  config: {
+    allDayExpr: 'AllDay',
+  },
+  data: {
+    AllDay: true,
+  },
+}, {
+  config: {
+  },
+  data: {
+    AllDay: true,
+  },
+}].forEach(({ config, data }) => {
+  test('All day appointment should be render valid in case without endDate property(T1155630)', async (t) => {
+    const { toolbar, workSpace } = new Scheduler('#container');
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+    await t
+      .expect(await takeScreenshot(`week-all-day-expr-${config.allDayExpr}.png`, workSpace)).ok();
+
+    await t
+      .click(toolbar.viewSwitcher.getButton('timelineWeek').element);
+
+    await t
+      .expect(await takeScreenshot(`timelineWeek-all-day-expr-${config.allDayExpr}.png`, workSpace)).ok();
+
+    await t.expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
+  }).before(async () => {
+    await createWidget('dxScheduler', {
+      dataSource: [{
+        text: 'MY EVENT',
+        startDate: new Date(2023, 2, 19, 23, 45),
+        ...data,
+      }],
+      views: ['week', 'timelineWeek'],
+      currentView: 'week',
+      currentDate: new Date(2023, 2, 21),
+      height: 600,
+      ...config,
+    });
+  });
+});
