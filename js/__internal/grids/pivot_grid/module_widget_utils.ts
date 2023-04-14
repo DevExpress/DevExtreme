c16aa746e225a125bbab52f434843eade937e8c4
timbset <timbset@gmail.com>
2023-04-14 19:02:54 +0400

    Revert CSP fix in grids

--- js/__internal/grids/pivot_grid/module_widget_utils.ts
+++ js/__internal/grids/pivot_grid/module_widget_utils.ts
@@ -368,22 +368,10 @@ const getScrollbarWidth = (
 const calculateScrollbarWidth = callOnce(() => {
   const document = domAdapter.getDocument();
 
-  // TODO: use some built-in CSS class not to use inline styles
   document.body.insertAdjacentHTML(
     'beforeend',
-    // TODO: rename ID
-    '<div id="name-it-well"></div>',
+    '<div style=\'position: absolute; overflow: scroll; width: 100px; height: 100px; top: -9999px;\'></div>',
   );
-
-  const element = document.getElementById('name-it-well');
-
-  if (element != null) {
-    element.style.position = 'absolute';
-    element.style.overflow = 'scroll';
-    element.style.width = '100px';
-    element.style.height = '100px';
-    element.style.top = '-9999px';
-  }
 
   const scrollbar = document.body.lastElementChild;
   const scrollbarWidth = getScrollbarWidth(scrollbar);
