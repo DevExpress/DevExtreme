e21f8b3cd2c5f73a73dc767af51026ce451b4523
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2023-04-15 07:47:00 +0400

    DateRangeBox: use buttonsCollection to render drop down button

--- testing/tests/DevExpress.ui.widgets.editors/dateRangeBox.tests.js
+++ testing/tests/DevExpress.ui.widgets.editors/dateRangeBox.tests.js
@@ -1,6 +1,8 @@
 import $ from 'jquery';
+import config from 'core/config';
 import DateRangeBox from 'ui/date_range_box';
 import DateBox from 'ui/date_box';
+import { isRenderer } from 'core/utils/type';
 
 import 'generic_light.css!';
 
@@ -11,9 +13,17 @@ QUnit.testStart(() => {
     $('#qunit-fixture').html(markup);
 });
 
+const WIDGET_CLASS = 'dx-widget';
+const BUTTON_CLASS = 'dx-button';
+const DROP_DOWN_EDITOR_BUTTON_CLASS = 'dx-dropdowneditor-button';
+const DROP_DOWN_EDITOR_BUTTONS_CONTAINER_CLASS = 'dx-texteditor-buttons-container';
+
 const getStartDateBoxInstance = dateRangeBoxInstance => dateRangeBoxInstance.getStartDateBox();
 
 const getEndDateBoxInstance = dateRangeBoxInstance => dateRangeBoxInstance.getEndDateBox();
+
+const getButtonsContainers = $element => $element.find(`> .${DROP_DOWN_EDITOR_BUTTONS_CONTAINER_CLASS}`);
+const getButtons = $element => $element.find(`.${DROP_DOWN_EDITOR_BUTTON_CLASS}`);
 
 const moduleConfig = {
     beforeEach: function() {
@@ -83,6 +93,7 @@ QUnit.module('DateRangeBox Initialization', moduleConfig, () => {
                 disabledDates: null,
                 displayFormat: null,
                 dropDownOptions: {},
+                dropDownButtonTemplate: 'dropDownButton',
                 endDate: null,
                 focusStateEnabled: true,
                 height: undefined,
@@ -171,7 +182,7 @@ QUnit.module('DateRangeBox Initialization', moduleConfig, () => {
 
             const expectedOptions = {
                 ...expectedDateBoxOptions,
-                showClearButton: true,
+                showClearButton: false,
             };
             const endDateBox = getEndDateBoxInstance(this.instance);
 
@@ -179,5 +190,210 @@ QUnit.module('DateRangeBox Initialization', moduleConfig, () => {
                 assert.deepEqual(value, endDateBox.option(key), `${key} default value is correct`);
             });
         });
+    });
+
+    QUnit.module('DateBox\'s option dependency from DateRangeBox options ', () => {
+        const initialDateRangeBoxOptions = {
+            showClearButton: true,
+            showDropDownButton: true,
+            buttons: ['dropDown'],
+            // TODO: extend this list of options
+        };
+
+        QUnit.test('StartDateBox has expected settings', function(assert) {
+            this.reinit(initialDateRangeBoxOptions);
+
+            const expectedOptions = {
+                showClearButton: true,
+                showDropDownButton: false,
+                buttons: undefined,
+            };
+            const startDateBox = getStartDateBoxInstance(this.instance);
+
+            Object.entries(initialDateRangeBoxOptions).forEach(([key]) => {
+                assert.deepEqual(expectedOptions[key], startDateBox.option(key), `${key} value is correct`);
+            });
+        });
+
+        QUnit.test('EndDateBox has expected settings', function(assert) {
+            this.reinit(initialDateRangeBoxOptions);
+
+            const expectedOptions = {
+                showClearButton: true,
+                showDropDownButton: false,
+                buttons: undefined,
+            };
+            const endDateBox = getEndDateBoxInstance(this.instance);
+
+            Object.entries(initialDateRangeBoxOptions).forEach(([key]) => {
+                assert.deepEqual(expectedOptions[key], endDateBox.option(key), `${key} value is correct`);
+            });
+        });
+    });
+});
+
+QUnit.module('DropDownButton', moduleConfig, () => {
+    QUnit.test('Drop down button should be rendered if showDropDownButton is true', function(assert) {
+        this.reinit({
+            showDropDownButton: true
+        });
+
+        assert.strictEqual(getButtons(this.$element).length, 1, 'drop down button was rendered');
+    });
+
+    QUnit.test('Drop down button should not be rendered if showDropDownButton is false', function(assert) {
+        this.reinit({
+            showDropDownButton: false
+        });
+
+        assert.strictEqual(getButtons(this.$element).length, 0, 'drop down button was not rendered');
+    });
+
+    QUnit.test('Drop button should not be rendered if buttons is [] and showDropDownButton is true', function(assert) {
+        this.reinit({
+            showDropDownButton: true,
+            buttons: []
+        });
+
+        assert.strictEqual(getButtons(this.$element).length, 0, 'drop down button was not rendered');
+    });
+
+    QUnit.test('Drop button should not be rendered if buttons is ["dropDown"] and showDropDownButton is false', function(assert) {
+        this.reinit({
+            showDropDownButton: false,
+            buttons: ['dropDown']
+        });
+
+        assert.strictEqual(getButtons(this.$element).length, 0, 'drop down button was not rendered');
+    });
+
+    QUnit.test('Drop button should be rendered if buttons is ["dropDown"]', function(assert) {
+        this.reinit({
+            buttons: ['dropDown']
+        });
+
+        assert.strictEqual(getButtons(this.$element).length, 1, 'drop down button was rendered');
+    });
+
+    QUnit.test('Drop button template should be rendered correctly', function(assert) {
+        const buttonTemplate = function(buttonData, contentElement) {
+            assert.strictEqual(isRenderer(contentElement), !!config().useJQuery, 'contentElement is correct');
+
+            return '<div>Template</div>';
+        };
+
+        this.reinit({
+            dropDownButtonTemplate: buttonTemplate
+        });
+
+        assert.strictEqual(getButtons(this.$element).text(), 'Template', 'template was rendered');
+    });
+
+    QUnit.test('Drop button template should be changed correctly in runtime', function(assert) {
+        const buttonTemplate = function(buttonData, contentElement) {
+            assert.strictEqual(isRenderer(contentElement), !!config().useJQuery, 'contentElement is correct');
+
+            return '<div>Template</div>';
+        };
+
+        this.instance.option('dropDownButtonTemplate', buttonTemplate);
+
+        assert.strictEqual(getButtons(this.$element).text(), 'Template', 'template was rendered');
+    });
+
+    QUnit.test('render custom action button in after section in right order, drop down button first', function(assert) {
+        this.reinit({
+            buttons: ['dropDown', {
+                name: 'home',
+                location: 'after',
+                options: {
+                    icon: 'home',
+                    elementAttr: {
+                        class: 'custom-button'
+                    }
+                },
+            }]
+        });
+
+        const $buttonsContainer = getButtonsContainers(this.$element);
+
+        assert.strictEqual($buttonsContainer.length, 1, 'one buttons container was rendered');
+
+        const $buttons = $buttonsContainer.find(`.${WIDGET_CLASS}`);
+        assert.strictEqual($buttons.length, 2, 'two items was rendered');
+        assert.strictEqual($buttons.eq(0).hasClass(DROP_DOWN_EDITOR_BUTTON_CLASS), true, 'drop down button was rendered in right order');
+        assert.strictEqual($buttons.eq(1).hasClass('custom-button'), true, 'custom button was rendered in right order');
+    });
+
+    QUnit.test('render custom action button in after section in right order, custom button first ', function(assert) {
+        this.reinit({
+            buttons: [{
+                name: 'home',
+                location: 'after',
+                options: {
+                    icon: 'home',
+                    elementAttr: {
+                        class: 'custom-button'
+                    }
+                },
+            }, 'dropDown']
+        });
+
+        const $buttonsContainer = getButtonsContainers(this.$element);
+
+        assert.strictEqual($buttonsContainer.length, 1, 'one buttons container was rendered');
+
+        const $buttons = $buttonsContainer.find(`.${WIDGET_CLASS}`);
+        assert.strictEqual($buttons.length, 2, 'two items was rendered');
+        assert.strictEqual($buttons.eq(0).hasClass('custom-button'), true, 'custom button was rendered in right order');
+        assert.strictEqual($buttons.eq(1).hasClass(DROP_DOWN_EDITOR_BUTTON_CLASS), true, 'drop down button was rendered in right order');
+    });
+
+    QUnit.test('render custom action button in before section', function(assert) {
+        this.reinit({
+            buttons: [{
+                name: 'home',
+                location: 'before',
+                options: {
+                    icon: 'home',
+                    elementAttr: {
+                        class: 'custom-button'
+                    }
+                },
+            }, 'dropDown']
+        });
+
+        const $buttonsContainers = getButtonsContainers(this.$element);
+
+        assert.strictEqual($buttonsContainers.length, 2, 'two buttons containers were rendered');
+
+        const $beforeSectionButton = $buttonsContainers.eq(0).find(`.${WIDGET_CLASS}`);
+        assert.strictEqual($beforeSectionButton.length, 1, 'one button was rendered in before section');
+        assert.strictEqual($beforeSectionButton.eq(0).hasClass('custom-button'), true, 'custom button was rendered in right order');
+
+        assert.strictEqual(getButtons($buttonsContainers.eq(1)).length, 1, 'drop down button was rendered in after section');
+    });
+
+    QUnit.test('getButton("dropDown") method should return instance of default drop down button', function(assert) {
+        this.reinit({});
+
+        assert.deepEqual(this.instance.getButton('dropDown'), getButtons(this.$element).dxButton('instance'));
+    });
+
+    QUnit.test('getButton("home") method should return instance of custom action button', function(assert) {
+        this.reinit({
+            name: 'home',
+            location: 'before',
+            options: {
+                icon: 'home',
+                elementAttr: {
+                    class: 'custom-button'
+                }
+            },
+        });
+
+        const $homeButton = getButtonsContainers(this.$element).find(`.${BUTTON_CLASS}`);
+
+        assert.deepEqual(this.instance.getButton('home'), $homeButton.dxButton('instance'));
     });
 });
