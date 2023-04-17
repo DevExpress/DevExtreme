a8301735e0fcdabbb9af1007e7c53f8c6e14b174
marker dao ® | roman <mark_nikolsky@outlook.com>
2023-04-17 16:53:19 +0400

    Tabs: Fix states design issues in Generic

--- testing/testcafe/tests/navigation/tabPanel/common.ts
+++ testing/testcafe/tests/navigation/tabPanel/common.ts
@@ -2,6 +2,7 @@ import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import { testScreenshot } from '../../../helpers/themeUtils';
 import url from '../../../helpers/getPageUrl';
 import createWidget from '../../../helpers/createWidget';
+import TabPanel from '../../../model/tabPanel';
 import { Item } from '../../../../../js/ui/tab_panel.d';
 
 fixture.disablePageReloads`TabPanel_common`
@@ -137,6 +138,7 @@ test('TabPanel borders without scrolling', async (t) => {
 
 test('TabPanel when its disabled item has focus', async (t) => {
   const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+  const tabPanel = new TabPanel('#container');
 
   await testScreenshot(t, takeScreenshot, 'TabPanel without focus.png', { element: '#container' });
 
@@ -145,6 +147,20 @@ test('TabPanel when its disabled item has focus', async (t) => {
 
   await t.pressKey('right');
   await testScreenshot(t, takeScreenshot, 'TabPanel when its disabled item has focus.png', { element: '#container' });
+
+  await t.pressKey('right');
+
+  const thirdItem = tabPanel.getItem(2);
+  const fourthItem = tabPanel.getItem(3);
+
+  await t.dispatchEvent(thirdItem.element, 'mousedown');
+  await testScreenshot(t, takeScreenshot, 'TabPanel when 3 item has active state.png', { element: '#container' });
+
+  await t
+    .dispatchEvent(thirdItem.element, 'mouseup')
+    .hover(fourthItem.element);
+
+  await testScreenshot(t, takeScreenshot, 'TabPanel when 4 item has hover state.png', { element: '#container' });
 
   await t
     .expect(compareResults.isValid())
@@ -155,9 +171,9 @@ test('TabPanel when its disabled item has focus', async (t) => {
       title: 'John Heart',
       text: 'John Heart',
     }, {
-      disabled: true,
       title: 'Olivia Peyton',
       text: 'Olivia Peyton',
+      disabled: true,
     }, {
       title: 'Robert Reagan',
       text: 'Robert Reagan',
@@ -173,8 +189,44 @@ test('TabPanel when its disabled item has focus', async (t) => {
   const tabPanelOptions = {
     dataSource,
     height: 120,
-    width: 350,
+    width: 450,
     showNavButtons: true,
+  };
+
+  return createWidget('dxTabPanel', tabPanelOptions);
+});
+
+test('Tab borders in TabPanel with expanded tabs', async (t) => {
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+  await t
+    .pressKey('tab')
+    .pressKey('right')
+    .pressKey('right');
+
+  await testScreenshot(t, takeScreenshot, 'TabPanel with expanded tabs.png', { element: '#container' });
+
+  await t
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => {
+  const dataSource = [
+    {
+      title: 'John Heart',
+      text: 'John Heart',
+    }, {
+      title: 'Olivia Peyton',
+      text: 'Olivia Peyton',
+    }, {
+      title: 'Robert Reagan',
+      text: 'Robert Reagan',
+    },
+  ] as Item[];
+
+  const tabPanelOptions = {
+    dataSource,
+    height: 120,
+    width: 450,
   };
 
   return createWidget('dxTabPanel', tabPanelOptions);
