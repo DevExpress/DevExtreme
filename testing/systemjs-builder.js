0ae059d48734c2e51964ea99e54598fea8739723
timbset <timbset@gmail.com>
2023-04-17 16:04:14 +0400

    Fix SystemJS builder on Windows

--- testing/systemjs-builder.js
+++ testing/systemjs-builder.js
@@ -1,5 +1,5 @@
-const path = require('path');
-const fs = require('fs');
+const path = require('node:path/posix');
+const fs = require('node:fs');
 const babel = require('@babel/core');
 const parseArguments = require('minimist');
 
