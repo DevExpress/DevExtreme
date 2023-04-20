ee22ce88ba52306685875e08d6235529537cffc0
timbset <timbset@gmail.com>
2023-04-21 02:21:42 +0400

    Fix some widgets (3/3) tests

--- testing/tests/DevExpress.ui.widgets/list.tests.js
+++ testing/tests/DevExpress.ui.widgets/list.tests.js
@@ -4,30 +4,25 @@ import 'generic_light.css!';
 
 QUnit.testStart(() => {
     const markup =
-        '<style>\
-            .dx-scrollable-native-win8.dx-scrollable-native.dx-scrollview .dx-scrollable-container .dx-scrollview-content {\
-                height: auto !important;\
-            }\
-            \
-            .dx-list-item {\
-                height: 40px;\
-            }\
-            \
-            .dx-icon-remove {\
-                width: 20px;\
-            }\
-            \
-            .dx-inkripple {\
-                position: absolute;\
-                overflow: hidden;\
-            }\
-        </style>\
-        \
-        <div id="list"></div>\
-        \
-        <div id="templated-list">\
-            <div data-options="dxTemplate: { name: \'item\' }">Item Template</div>\
-        </div>';
+        `<style nonce="qunit-test">
+            .dx-scrollable-native-win8.dx-scrollable-native.dx-scrollview .dx-scrollable-container .dx-scrollview-content {
+                height: auto !important;
+            }
+            .dx-list-item {
+                height: 40px;
+            }
+            .dx-icon-remove {
+                width: 20px;
+            }
+            .dx-inkripple {
+                position: absolute;
+                overflow: hidden;
+            }
+        </style>
+        <div id="list"></div>
+        <div id="templated-list">
+            <div data-options="dxTemplate: { name: 'item' }">Item Template</div>
+        </div>`;
 
     $('#qunit-fixture').html(markup);
 });
