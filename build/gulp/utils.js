4558c68cfc82269f04d2997e89405bb59b2c957f
Alexander Bulychev <git@bulychev.pro>
2023-04-20 17:45:46 +0400

    CI: rework .npmignore

--- build/gulp/utils.js
+++ build/gulp/utils.js
@@ -2,6 +2,7 @@
 
 const gulp = require('gulp');
 const env = require('./env-variables');
+const Vinyl = require('vinyl');
 
 gulp.task('skippedTask', done => done());
 
@@ -12,12 +13,28 @@ const runTaskByCondition = (condition, task) => {
     return (done) => done ? done() : gulp.series('skippedTask');
 };
 
+const stringSrc = (filename, str) => {
+    const src = require('stream').Readable({ objectMode: true });
+
+    src._read = function() {
+        this.push(new Vinyl({
+            cwd: '',
+            path: filename,
+            contents: Buffer.from(str, 'utf-8')
+        }));
+        this.push(null);
+    };
+
+    return src;
+};
+
 const isEsmPackage = env.BUILD_ESM_PACKAGE;
 
 const packageDir = 'devextreme';
 
 module.exports = {
     packageDir,
+    stringSrc,
     isEsmPackage,
     runTaskByCondition,
     ifEsmPackage: (task) => runTaskByCondition(isEsmPackage, task),
