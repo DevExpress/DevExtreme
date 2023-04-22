f347e6edcf2ff1a856e13da191138f048b1868ac
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2023-04-22 04:13:45 +0400

    remove testcafe test. This scenario is in daterangebox.stategy tests

--- testing/testcafe/tests/editors/dateRangeBox/focus.ts
+++ testing/testcafe/tests/editors/dateRangeBox/focus.ts
@@ -88,52 +88,6 @@ test('DateRangeBox & Start DateBox should have focus class after click on drop d
   value: ['2022/09/17', '2022/10/24'],
 }));
 
-test('DateRangeBox & EndDateBox should have focus class after select start date and end date in calendar', async (t) => {
-  const dateRangeBox = new DateRangeBox('#container');
-
-  await t
-    .expect(dateRangeBox.isFocused)
-    .notOk()
-    .expect(dateRangeBox.getStartDateBox().isFocused)
-    .notOk()
-    .expect(dateRangeBox.getEndDateBox().isFocused)
-    .notOk();
-
-  await t
-    .click(Selector('body'))
-    .pressKey('tab');
-
-  await dateRangeBox.option('opened', true);
-
-  await t
-    .expect(dateRangeBox.isFocused)
-    .ok()
-    .expect(dateRangeBox.getStartDateBox().isFocused)
-    .ok()
-    .expect(dateRangeBox.getEndDateBox().isFocused)
-    .notOk();
-
-  await t
-    .click(dateRangeBox.getCalendarCell(20))
-    .expect(dateRangeBox.isFocused)
-    .ok()
-    .expect(dateRangeBox.getStartDateBox().isFocused)
-    .notOk()
-    .expect(dateRangeBox.getEndDateBox().isFocused)
-    .ok();
-
-  await t
-    .click(dateRangeBox.getCalendarCell(140))
-    .expect(dateRangeBox.isFocused)
-    .ok()
-    .expect(dateRangeBox.getStartDateBox().isFocused)
-    .notOk()
-    .expect(dateRangeBox.getEndDateBox().isFocused)
-    .ok();
-}).before(async () => createWidget('dxDateRangeBox', {
-  value: [null, null],
-}));
-
 test('DateRangeBox & StartDateBox should be focused if dateRangeBox open by click on drop down button and endDateBox was focused', async (t) => {
   const dateRangeBox = new DateRangeBox('#container');
 
