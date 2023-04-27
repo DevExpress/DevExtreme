3aca2665646d7bcf489b33c0275220f948bbfedd
Roman Semenov <roman.semenov@devexpress.com>
2023-04-27 17:00:13 +0400

    DataGrid - The defaultSortIndex property was removed in v22.2.5 (T1160139) (#24411)

--- js/common/grids.d.ts
+++ js/common/grids.d.ts
@@ -377,6 +377,7 @@ export interface ColumnBase<TRowData = any> {
   /**
    * @docid GridBaseColumn.sortIndex
    * @default undefined
+   * @fires GridBaseOptions.onOptionChanged
    * @public
    */
   sortIndex?: number;
