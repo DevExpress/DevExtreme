d7d050381fd5303835f231b65a6c4ee52bacc3bd
Andrey Dolzhikov <andrey.dolzhikov@devexpress.com>
2023-05-03 14:13:48 +0300

    fix

--- testing/tests/DevExpress.ui.widgets.scheduler/desktopTooltip.tests.js
+++ testing/tests/DevExpress.ui.widgets.scheduler/desktopTooltip.tests.js
@@ -115,7 +115,7 @@ QUnit.test('contentTemplate passed to createComponent should work correct', func
 
         assert.equal(stubCreateComponent.getCall(1).args[0][0].nodeName, 'DIV');
         assert.equal(stubCreateComponent.getCall(1).args[1], List);
-        assert.equal(Object.keys(stubCreateComponent.getCall(1).args[2]).length, 6);
+        assert.equal(Object.keys(stubCreateComponent.getCall(1).args[2]).length, 7);
         assert.equal(stubCreateComponent.getCall(1).args[2].dataSource, dataList);
         assert.equal(stubCreateComponent.getCall(1).args[2].showScrollbar, 'onHover');
         assert.ok(stubCreateComponent.getCall(1).args[2].onContentReady);
