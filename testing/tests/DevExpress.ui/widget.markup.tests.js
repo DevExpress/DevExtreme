e2032e408cb7e611a30698321fc1d118f6f93fa4
Lykoi18 <aiden.ghost666@gmail.com>
2023-05-04 12:15:29 +0400

    Fix serverSide tests

--- testing/tests/DevExpress.ui/widget.markup.tests.js
+++ testing/tests/DevExpress.ui/widget.markup.tests.js
@@ -16,11 +16,13 @@ require('generic_light.css!');
     QUnit.testStart(function() {
         const markup = '\
             <div id="widget"></div>\
-            <div id="widthRootStyle" style="width: 300px;"></div>\
-            <div id="widthRootStylePercent" style="width: 50%;"></div>\
+            <div id="widthRootStyle"></div>\
+            <div id="widthRootStylePercent"></div>\
             </div>';
 
         $('#qunit-fixture').html(markup);
+        $('#widthRootStyle').css('width', '300px');
+        $('#widthRootStylePercent').css('width', '50%');
     });
 
     QUnit.module('Widget markup');
