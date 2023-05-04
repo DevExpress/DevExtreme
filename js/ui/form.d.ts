de7d933386c3159f2972a288aecf7497da1fb42b
const314 <constantin.panchenko@gmail.com>
2023-05-04 19:27:26 +0400

    temporary comment EventsIntegrityCheckingHelper for form to fix build error in R1

--- js/ui/form.d.ts
+++ js/ui/form.d.ts
@@ -808,12 +808,14 @@ export type Properties = dxFormOptions;
 /** @deprecated use Properties instead */
 export type Options = dxFormOptions;
 
-type EventProps<T> = Extract<keyof T, `on${any}`>;
-type CheckedEvents<TProps, TEvents extends { [K in EventProps<TProps>]: (e: any) => void } & Record<Exclude<keyof TEvents, keyof TProps>, never>> = TEvents;
+// TODO: temporary commented out to fix jquery generation error in R1
 
-type FilterOutHidden<T> = Omit<T, 'onFocusIn' | 'onFocusOut'>;
+// type EventProps<T> = Extract<keyof T, `on${any}`>;
+// type CheckedEvents<TProps, TEvents extends { [K in EventProps<TProps>]: (e: any) => void } & Record<Exclude<keyof TEvents, keyof TProps>, never>> = TEvents;
 
-type EventsIntegrityCheckingHelper = CheckedEvents<FilterOutHidden<Properties>, Required<Events>>;
+// type FilterOutHidden<T> = Omit<T, 'onFocusIn' | 'onFocusOut'>;
+
+// type EventsIntegrityCheckingHelper = CheckedEvents<FilterOutHidden<Properties>, Required<Events>>;
 
 type Events = {
 /**
