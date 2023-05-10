b6e969070d5483a4b05ac439978e6d1a5b000d7d
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2023-05-10 11:41:22 +0400

    DateRangeBox: popup should not be reopened after click on inputs

--- js/ui/date_range_box/ui.multiselect_date_box.js
+++ js/ui/date_range_box/ui.multiselect_date_box.js
@@ -1,3 +1,4 @@
+import $ from '../../core/renderer';
 import DateBox from '../date_box/ui.date_box.mask';
 import RangeCalendarStrategy from './strategy/rangeCalendar';
 
@@ -13,6 +14,45 @@ class MultiselectDateBox extends DateBox {
 
         this.close();
         this.option('focusStateEnabled') && this.focus();
+    }
+
+    _openHandler(e) {
+        if(this._strategy.dateRangeBox.option('opened')) {
+            return;
+        }
+
+        super._openHandler(e);
+    }
+
+    _closeOutsideDropDownHandler(e) {
+        const { target } = e;
+        const [startDateInput, endDateInput] = this._strategy.dateRangeBox.field();
+
+        return super._closeOutsideDropDownHandler(e) && !($(target).is(startDateInput) || $(target).is(endDateInput));
+    }
+
+    _focusInHandler(e) {
+        super._focusInHandler(e);
+
+        // const { target } = e;
+        // const [startDateInput, endDateInput] = this._strategy.dateRangeBox.field();
+
+        // if(!this._strategy.dateRangeBox.getStartDateBox()._strategy._widget) {
+        //     return;
+        // }
+
+        // const value = this._strategy.dateRangeBox.getStartDateBox()._strategy._widget.option('values');
+
+        // if($(target).is(startDateInput)) {
+        //     this._strategy.setActiveStartDateBox();
+        //     this._strategy._widget.option('_currentSelection', 'startDate');
+        //     this._strategy._widget._setViewsMinOption(value[0]);
+        // }
+        // if($(target).is(endDateInput)) {
+        //     this._strategy.dateRangeBox.getStartDateBox()._strategy.setActiveEndDateBox();
+        //     this._strategy.dateRangeBox.getStartDateBox()._strategy._widget.option('_currentSelection', 'endDate');
+        //     this._strategy.dateRangeBox.getStartDateBox()._strategy._widget._setViewsMaxOption(value[1]);
+        // }
     }
 }
 
