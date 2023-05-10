0be0c077ce0b2a4a7a70168e129cf93d5f9aed6c
marker dao ® <mark_nikolsky@outlook.com>
2023-05-10 13:08:34 +0400

    feat(collectionWidget): Revert pointerdown event && Add get event name method

--- js/ui/collection/ui.collection_widget.base.js
+++ js/ui/collection/ui.collection_widget.base.js
@@ -691,22 +691,26 @@ const CollectionWidget = Widget.inherit({
         this._attachContextMenuEvent();
     },
 
+    _getPointerEventName() {
+        return pointerEvents.down;
+    },
+
     _attachClickEvent: function() {
         const itemSelector = this._itemSelector();
         const clickEventNamespace = addNamespace(clickEventName, this.NAME);
-        const pointerUpEventNamespace = addNamespace(pointerEvents.up, this.NAME);
+        const pointerEventNamespace = addNamespace(pointerEvents.up, this.NAME);
         const that = this;
 
-        const pointerUpAction = new Action(function(args) {
+        const pointerAction = new Action(function(args) {
             const event = args.event;
             that._itemPointerDownHandler(event);
         });
 
         eventsEngine.off(this._itemContainer(), clickEventNamespace, itemSelector);
-        eventsEngine.off(this._itemContainer(), pointerUpEventNamespace, itemSelector);
+        eventsEngine.off(this._itemContainer(), pointerEventNamespace, itemSelector);
         eventsEngine.on(this._itemContainer(), clickEventNamespace, itemSelector, (function(e) { this._itemClickHandler(e); }).bind(this));
-        eventsEngine.on(this._itemContainer(), pointerUpEventNamespace, itemSelector, function(e) {
-            pointerUpAction.execute({
+        eventsEngine.on(this._itemContainer(), pointerEventNamespace, itemSelector, function(e) {
+            pointerAction.execute({
                 element: $(e.target),
                 event: e
             });
