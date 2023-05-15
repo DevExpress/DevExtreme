108ebc51c874cf1179a9ebdb1e88b25a46f6490f
Eldar Iusupzhanov <eldar.iusupzhanov@devexpress.com>
2023-05-15 14:59:57 +0800

    GridCore: Move doc comments - part 2.2

--- js/ui/grid_core/ui.grid_core.columns_controller.js
+++ js/ui/grid_core/ui.grid_core.columns_controller.js
@@ -1,2 +1,21 @@
 /** @deprecated */
 export * from '../../__internal/grids/grid_core/columns_controller/module';
+
+/**
+ * @name dxDataGridColumn.grouped
+ * @type boolean
+ * @hidden
+ * @default false
+ */
+/**
+ * @name dxDataGridColumn.resized
+ * @type function
+ * @hidden
+ * @default undefined
+ */
+/**
+ * @name dxDataGridOptions.regenerateColumnsByVisibleItems
+ * @type boolean
+ * @hidden
+ * @default false
+ */
