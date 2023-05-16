93bec1884b6220b389512de60e6c429270ca1910
AlexanderMoiseev <moiseevalexandr1@gmail.com>
2023-05-16 18:46:03 +0800

    Dialog: fix width for phones (#24633)

--- js/ui/dialog.js
+++ js/ui/dialog.js
@@ -176,9 +176,9 @@ export const custom = function(options) {
         .addClass(DX_DIALOG_ROOT_CLASSNAME);
 
     function show() {
-        if(devices.real().platform === 'android') {
+        if(devices.real().deviceType === 'phone') {
             const isPortrait = getHeight(window) > getWidth(window);
-            const width = isPortrait ? '80%' : '60%';
+            const width = isPortrait ? '90%' : '60%';
             popupInstance.option({ width });
         }
 
