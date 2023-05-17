6049cddb8e3acaed9f971bb16026109d9a0fbb3d
Shpileva Yuliya <Krijovnick@users.noreply.github.com>
2023-05-17 09:07:44 +0400

    PieChart: add utils file in demo with custom styles (#2602)

--- Demos/Charts/PieWithCustomStyles/React/App.js
+++ Demos/Charts/PieWithCustomStyles/React/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import PieChart, {
   Series, Label, Connector, Export,
 } from 'devextreme-react/pie-chart';
-import { registerGradient, registerPattern } from 'devextreme/common/charts';
+import {
+  getStrokePattern, getSquarePattern, getLinearGradient, getRadialGradient, getPatternImage,
+} from './utils.js';
 import { data } from './data.js';
 
 class App extends React.Component {
@@ -28,94 +30,6 @@ class App extends React.Component {
 
 function customizeText(info) {
   return info.argument;
-}
-
-const imagePatternSize = 12;
-const shapePatternSize = 6;
-
-function hexToRgb(hex, opacity = 1) {
-  const hexColorParts = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return `rgba(${parseInt(hexColorParts[1], 16)}, ${parseInt(hexColorParts[2], 16)}, ${parseInt(hexColorParts[3], 16)}, ${opacity})`;
-}
-
-function getGradient(type, color1, color2) {
-  return registerGradient(type, {
-    colors: [{
-      offset: '20%',
-      color: color1,
-    }, {
-      offset: '90%',
-      color: color2,
-    }],
-  });
-}
-
-function getLinearGradient(color) { return getGradient('linear', color, hexToRgb(color, 0.5)); }
-
-function getRadialGradient(color) { return getGradient('radial', hexToRgb(color, 0.5), color); }
-
-function getPatternImage(color) {
-  return registerPattern({
-    width: imagePatternSize,
-    height: imagePatternSize,
-    template: (container) => {
-      const rect = createRect(imagePatternSize, color);
-      const image = document.createElementNS('http://www.w3.org/2000/svg', 'image');
-      image.setAttribute('x', 0);
-      image.setAttribute('y', 0);
-      image.setAttribute('width', imagePatternSize);
-      image.setAttribute('height', imagePatternSize);
-      image.setAttribute('href', '../../../../images/Charts/PieWithCustomStyles/diamond.svg');
-      image.setAttribute('opacity', 0.6);
-
-      container.appendChild(rect);
-      container.appendChild(image);
-    },
-  });
-}
-
-function getStrokePattern(color) {
-  return registerPattern({
-    width: shapePatternSize,
-    height: shapePatternSize,
-    template: (container) => {
-      const halfSize = shapePatternSize / 2;
-      const oneAndAHalfSize = shapePatternSize * 1.5;
-      const d = `M ${halfSize} ${-halfSize} L ${-halfSize} ${halfSize} M 0 ${shapePatternSize} L ${shapePatternSize} 0 M ${oneAndAHalfSize} ${halfSize} L ${halfSize} ${oneAndAHalfSize}`;
-
-      const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-
-      path.setAttribute('stroke', color);
-      path.setAttribute('stroke-width', 2);
-      path.setAttribute('d', d);
-      container.appendChild(path);
-    },
-  });
-}
-
-function getSquarePattern(color) {
-  return registerPattern({
-    width: shapePatternSize,
-    height: shapePatternSize,
-    template: (container) => {
-      const rect = createRect(shapePatternSize, null, color, 2);
-      container.appendChild(rect);
-    },
-  });
-}
-
-function createRect(size, fill, stroke, strokeWidth) {
-  const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-
-  rect.setAttribute('x', 0);
-  rect.setAttribute('y', 0);
-  rect.setAttribute('width', size);
-  rect.setAttribute('height', size);
-  rect.setAttribute('fill', fill);
-  rect.setAttribute('stroke', stroke);
-  rect.setAttribute('stroke-width', strokeWidth);
-
-  return rect;
 }
 
 function customizePoint(point) {
--- Demos/Charts/PieWithCustomStyles/Vue/App.vue
+++ Demos/Charts/PieWithCustomStyles/Vue/App.vue
@@ -26,11 +26,14 @@ import DxPieChart, {
   DxConnector,
   DxExport,
 } from 'devextreme-vue/pie-chart';
-import { registerGradient, registerPattern } from 'devextreme/common/charts';
+import {
+  getStrokePattern,
+  getSquarePattern,
+  getLinearGradient,
+  getRadialGradient,
+  getPatternImage,
+} from './utils.js';
 import { data } from './data.js';
-
-const imagePatternSize = 12;
-const shapePatternSize = 6;
 
 export default {
   components: {
@@ -51,19 +54,19 @@ export default {
       let fillId;
       switch (point.argument) {
         case 'Stripes':
-          fillId = this.getStrokePattern(color);
+          fillId = getStrokePattern(color);
           break;
         case 'Grid':
-          fillId = this.getSquarePattern(color);
+          fillId = getSquarePattern(color);
           break;
         case 'Linear Gradient':
-          fillId = this.getLinearGradient(color);
+          fillId = getLinearGradient(color);
           break;
         case 'Radial Gradient':
-          fillId = this.getRadialGradient(color);
+          fillId = getRadialGradient(color);
           break;
         case 'Image':
-          fillId = this.getPatternImage(color);
+          fillId = getPatternImage(color);
           break;
         default:
           break;
@@ -74,90 +77,6 @@ export default {
 
     customizeText(info) {
       return info.argument;
-    },
-
-    hexToRgb(hex, opacity = 1) {
-      const hexColorParts = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-      return `rgba(${parseInt(hexColorParts[1], 16)}, ${parseInt(hexColorParts[2], 16)}, ${parseInt(hexColorParts[3], 16)}, ${opacity})`;
-    },
-
-    getGradient(type, color1, color2) {
-      return registerGradient(type, {
-        colors: [{
-          offset: '20%',
-          color: color1,
-        }, {
-          offset: '90%',
-          color: color2,
-        }],
-      });
-    },
-
-    getLinearGradient(color) { return this.getGradient('linear', color, this.hexToRgb(color, 0.5)); },
-
-    getRadialGradient(color) { return this.getGradient('radial', this.hexToRgb(color, 0.5), color); },
-
-    getPatternImage(color) {
-      return registerPattern({
-        width: imagePatternSize,
-        height: imagePatternSize,
-        template: (container) => {
-          const rect = this.createRect(imagePatternSize, color);
-          const image = document.createElementNS('http://www.w3.org/2000/svg', 'image');
-          image.setAttribute('x', 0);
-          image.setAttribute('y', 0);
-          image.setAttribute('width', imagePatternSize);
-          image.setAttribute('height', imagePatternSize);
-          image.setAttribute('href', '../../../../images/Charts/PieWithCustomStyles/diamond.svg');
-          image.setAttribute('opacity', '0.6');
-
-          container.appendChild(rect);
-          container.appendChild(image);
-        },
-      });
-    },
-
-    getStrokePattern(color) {
-      return registerPattern({
-        width: shapePatternSize,
-        height: shapePatternSize,
-        template: (container) => {
-          const halfSize = shapePatternSize / 2;
-          const oneAndAHalfSize = shapePatternSize * 1.5;
-          const d = `M ${halfSize} ${-halfSize} L ${-halfSize} ${halfSize} M 0 ${shapePatternSize} L ${shapePatternSize} 0 M ${oneAndAHalfSize} ${halfSize} L ${halfSize} ${oneAndAHalfSize}`;
-          const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-
-          path.setAttribute('stroke', color);
-          path.setAttribute('stroke-width', 2);
-          path.setAttribute('d', d);
-          container.appendChild(path);
-        },
-      });
-    },
-
-    getSquarePattern(color) {
-      return registerPattern({
-        width: shapePatternSize,
-        height: shapePatternSize,
-        template: (container) => {
-          const rect = this.createRect(shapePatternSize, null, color, 2);
-          container.appendChild(rect);
-        },
-      });
-    },
-
-    createRect(size, fill, stroke, strokeWidth) {
-      const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-
-      rect.setAttribute('x', 0);
-      rect.setAttribute('y', 0);
-      rect.setAttribute('width', size);
-      rect.setAttribute('height', size);
-      rect.setAttribute('fill', fill);
-      rect.setAttribute('stroke', stroke);
-      rect.setAttribute('stroke-width', strokeWidth);
-
-      return rect;
     },
   },
 };
