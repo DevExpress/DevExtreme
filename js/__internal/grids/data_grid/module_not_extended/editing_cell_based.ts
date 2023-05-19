174401ce84d62041ab39cb9929864be974c17797
Eldar Iusupzhanov <eldar.iusupzhanov@devexpress.com>
2023-05-19 18:55:41 +0800

    GridCore: Add re-export js modules - part 2.3

--- js/__internal/grids/data_grid/module_not_extended/editing_cell_based.ts
+++ js/__internal/grids/data_grid/module_not_extended/editing_cell_based.ts
@@ -1,4 +1,4 @@
-import editingCellBasedModule from '@js/ui/grid_core/ui.grid_core.editing_cell_based';
+import { editingCellBasedModule } from '@js/ui/grid_core/ui.grid_core.editing_cell_based';
 import gridCore from '../module_core';
 
 gridCore.registerModule('editingCellBased', editingCellBasedModule);
