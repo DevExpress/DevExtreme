3e158f3b452b96d8cbb1b564c0e373299f938a90
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2023-05-22 15:16:10 +0400

    DateRangeBox: focus start date input and raise `input` event by click on clear button (#24686)

--- js/ui/date_range_box/ui.multiselect_date_box.js
+++ js/ui/date_range_box/ui.multiselect_date_box.js
@@ -24,6 +24,14 @@ class MultiselectDateBox extends DateBox {
         super._openHandler(e);
     }
 
+    _clearValueHandler(e) {
+        this.option('text', '');
+        e.stopPropagation();
+
+        this._saveValueChangeEvent(e);
+        this._clearValue();
+    }
+
     _renderOpenedState() {
         const opened = this._strategy.dateRangeBox.getStartDateBox()?.option('opened') ?? this.option('opened');
 
