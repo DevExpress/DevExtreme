68edf2f26f6a00570871971415dbe87e5ce207d8
Alexander Bulychev <git@bulychev.pro>
2023-05-30 15:18:16 +0400

    fix tests

--- testing/tests/DevExpress.localization/validation.tests.js
+++ testing/tests/DevExpress.localization/validation.tests.js
@@ -41,7 +41,7 @@ QUnit.test('Invalid message localization, formatted', function(assert) {
         type: 'numeric'
     }], 'Salaire');
 
-    assert.equal(result.brokenRule.message, 'La valeur du champ Salaire doit être un nombre', 'France localization should be used');
+    assert.equal(result.brokenRule.message, 'Salaire doit être un nombre', 'France localization should be used');
 });
 
 QUnit.test('T212840: Numeric - invalid, with default culture-agnostic behaviour', function(assert) {
