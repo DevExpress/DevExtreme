12f8dce4a948288f73d703eedad6be56add0f208
marker dao ® <mark_nikolsky@outlook.com>
2023-06-09 17:06:15 +0400

    feat(VV API): Add tests

--- testing/tests/DevExpress.core/utils.visual_viewport.tests.js
+++ testing/tests/DevExpress.core/utils.visual_viewport.tests.js
@@ -1,4 +1,4 @@
-import { hasWindow } from 'core/utils/window';
+import { hasWindow, getWindow } from 'core/utils/window';
 import {
     visualViewportEventMap,
     hasVisualViewport,
@@ -6,38 +6,81 @@ import {
     subscribeOnVisualViewportEvent,
 } from 'core/utils/visual_viewport';
 
+const setVisualViewport = (value, configurable) => {
+    Object.defineProperty(getWindow(), 'visualViewport', {
+        get: () => value,
+        configurable,
+    });
+};
+
+const getOriginalVisualViewport = () => {
+    const { visualViewport } = getWindow();
+
+    return visualViewport;
+};
+
 QUnit.module('VisualViewport API', () => {
     QUnit.test('VisualViewport exists if there is window object', function(assert) {
         assert.strictEqual(hasVisualViewport(), hasWindow());
     });
 
     QUnit.test('getVisualViewportSizes returns correct values', function(assert) {
-        const visualViewportSizes = getVisualViewportSizes();
-        const visualViewportSizesKeys = Object.keys(visualViewportSizes);
-
-        const expectedResults = {
-            offsetLeft: 0,
-            offsetTop: 0,
-            pageLeft: 0,
-            pageTop: 0,
-            scale: 1,
+        const sizes = {
+            height: 7,
+            offsetLeft: 1,
+            offsetTop: 2,
+            pageLeft: 3,
+            pageTop: 4,
+            scale: 5,
+            width: 6,
         };
 
-        assert.strictEqual(visualViewportSizesKeys.length, 7, 'correct size properties count');
+        const originalVisualViewport = getOriginalVisualViewport();
 
-        visualViewportSizesKeys.forEach(property => {
-            if(property === 'width' || property === 'height') {
-                assert.strictEqual(typeof visualViewportSizes[property], 'number', `correct ${property} property type`);
+        try {
+            setVisualViewport(sizes, true);
 
-                return;
-            }
+            const visualViewportSizes = getVisualViewportSizes();
+            const visualViewportSizesKeys = Object.keys(visualViewportSizes);
 
-            assert.strictEqual(visualViewportSizes[property], expectedResults[property], `correct ${property} property value`);
-        });
+            assert.strictEqual(visualViewportSizesKeys.length, 7, 'correct size properties count');
+
+            visualViewportSizesKeys.forEach(property => {
+                assert.strictEqual(visualViewportSizes[property], sizes[property], `correct ${property} property value`);
+            });
+        } finally {
+            setVisualViewport(originalVisualViewport, false);
+        }
     });
 
     QUnit.test('subscribeOnVisualViewportEvent returns a function', function(assert) {
-        assert.strictEqual(typeof subscribeOnVisualViewportEvent(visualViewportEventMap.resize, () => {}), 'function');
-        assert.strictEqual(typeof subscribeOnVisualViewportEvent(visualViewportEventMap.scroll, () => {}), 'function');
+        try {
+            this.resizeUnScribeCallback = subscribeOnVisualViewportEvent(visualViewportEventMap.resize, () => {});
+            this.scrollUnScribeCallback = subscribeOnVisualViewportEvent(visualViewportEventMap.scroll, () => {});
+
+            assert.strictEqual(typeof this.resizeUnScribeCallback, 'function');
+            assert.strictEqual(typeof this.scrollUnScribeCallback, 'function');
+        } finally {
+            this.resizeUnScribeCallback && this.resizeUnScribeCallback();
+            this.scrollUnScribeCallback && this.scrollUnScribeCallback();
+
+            this.resizeUnScribeCallback = null;
+            this.scrollUnScribeCallback = null;
+        }
+    });
+
+    QUnit.test('VisualViewport resize fires VisualViewport resize callback the correct number of times', function(assert) {
+        let counter = 0;
+
+        const resizeUnScribeCallback = subscribeOnVisualViewportEvent(visualViewportEventMap.resize, () => counter++);
+        const visualViewport = getOriginalVisualViewport();
+
+        visualViewport.dispatchEvent(new Event('resize'));
+        assert.strictEqual(counter, 1, 'callback was fired once');
+
+        resizeUnScribeCallback();
+
+        visualViewport.dispatchEvent(new Event('resize'));
+        assert.strictEqual(counter, 1, 'there are no callbacks');
     });
 });
