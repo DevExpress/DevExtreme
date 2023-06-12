6c7005ce7102684c57b1005082e879b73a7e2f6c
Eldar Iusupzhanov <ferrarijed@gmail.com>
2023-06-12 21:39:09 +0800

    write test

--- testing/tests/DevExpress.ui.widgets.dataGrid/editing.tests.js
+++ testing/tests/DevExpress.ui.widgets.dataGrid/editing.tests.js
@@ -2831,6 +2831,23 @@ QUnit.module('Editing', {
 
         assert.notOk(spy.called);
     });
+
+    QUnit.test('Not refocus cell after editRowKey was reset', function(assert) {
+        // arrange
+        $.extend(this.options.editing, {
+            allowUpdating: true,
+            mode: 'cell',
+        });
+
+        const spy = sinon.spy(this.editingController, '_focusEditingCell');
+
+        // act
+        this.editingController._refocusEditCell = true;
+        this.editingController._resetEditRowKey();
+
+        // assert
+        assert.notOk(spy.called);
+    });
 });
 
 QUnit.module('Editing with real dataController', {
