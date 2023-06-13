9158e1912aa577723b0333d206691d52a7b9f1c9
alexander.polosatov <alexander.polosatov@devexpress.com>
2023-06-13 11:07:49 +0400

    test

--- testing/testcafe/tests/editors/radioGroup/common.ts
+++ testing/testcafe/tests/editors/radioGroup/common.ts
@@ -1,6 +1,9 @@
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import { testScreenshot } from '../../../helpers/themeUtils';
 import url from '../../../helpers/getPageUrl';
 import RadioGroup from '../../../model/radioGroup';
 import createWidget from '../../../helpers/createWidget';
+import { appendElementTo } from '../../../helpers/domUtils';
 
 fixture.disablePageReloads`Radio Group`
   .page(url(__dirname, '../../container.html'));
@@ -64,10 +67,30 @@ test('Radio buttons placed into the template should not be selected after clicki
   await checkGroup(firstChildGroup, true);
   await checkGroup(secondChildGroup, false, true);
   await checkGroup(thirdChildGroup, false, false, true);
-}).before(async () => createWidget('dxRadioGroup', {
-  items: [{}, {}, {}],
-  itemTemplate: () => ($('<div>') as any).dxRadioGroup({
-    dataSource: [{}, {}, {}],
-    layout: 'horizontal',
-  }),
-}));
+}).before(async () => {
+  await appendElementTo('#container', 'div', 'template');
+  await createWidget('dxRadioGroup', {
+    items: [{}, {}, {}],
+    itemTemplate: () => ($('<div>') as any).dxRadioGroup({
+      dataSource: [{}, {}, {}],
+      layout: 'horizontal',
+    }),
+  }, '#template');
+});
+
+test('component in scaled container(T1165339)', async (t) => {
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+  await testScreenshot(t, takeScreenshot, 'RadioGroup horizontal validation.png', { element: '#scaled' });
+
+  await t
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => {
+  await appendElementTo('#container', 'div', 'scaled', { transform: 'scale(0.7)' });
+
+  await createWidget('dxRadioGroup', {
+    value: true,
+    text: 'in scaled container',
+  }, '#scaled');
+});
