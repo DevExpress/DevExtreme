6e4adfae87ba33281f04a8833f6cf2eeae3a610c
Lykoi18 <aiden.ghost666@gmail.com>
2023-06-13 20:20:00 +0400

    Remove systemjs-builder dependency and refactor

--- build/gulp/systemjs.js
+++ build/gulp/systemjs.js
@@ -6,12 +6,6 @@ const gulp = require('gulp');
 
 const root = path.resolve(__dirname, '../..');
 
-gulp.task('transpile-systemjs-builder', (callback) => {
-    spawn('node', [path.resolve(root, 'testing/systemjs-builder.js'), '--transpile=builder'], { stdio: 'inherit' })
-        .on('error', callback)
-        .on('close', code => code ? callback(new Error(code)) : callback());
-});
-
 gulp.task('transpile-systemjs-modules', (callback) => {
     spawn('node', [path.resolve(root, 'testing/systemjs-builder.js'), '--transpile=modules'], { stdio: 'inherit' })
         .on('error', callback)
@@ -42,13 +36,10 @@ gulp.task('transpile-systemjs-js-vendors', (callback) => {
         .on('close', code => code ? callback(new Error(code)) : callback());
 });
 
-gulp.task('transpile-systemjs', gulp.series(
-    'transpile-systemjs-builder',
-    gulp.parallel(
-        'transpile-systemjs-modules',
-        'transpile-systemjs-modules-renovation',
-        'transpile-systemjs-testing',
-        'transpile-systemjs-css',
-        'transpile-systemjs-js-vendors'
-    )
+gulp.task('transpile-systemjs', gulp.parallel(
+    'transpile-systemjs-modules',
+    'transpile-systemjs-modules-renovation',
+    'transpile-systemjs-testing',
+    'transpile-systemjs-css',
+    'transpile-systemjs-js-vendors'
 ));
