44988b8beaaf7e7378a713caa2e74d92ec125684
alexander.polosatov <alexander.polosatov@devexpress.com>
2023-06-13 16:18:05 +0400

    remove additional container

--- testing/testcafe/tests/editors/radioGroup/common.ts
+++ testing/testcafe/tests/editors/radioGroup/common.ts
@@ -1,15 +1,16 @@
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import { Selector } from 'testcafe';
 import { testScreenshot } from '../../../helpers/themeUtils';
 import url from '../../../helpers/getPageUrl';
 import RadioGroup from '../../../model/radioGroup';
 import createWidget from '../../../helpers/createWidget';
-import { appendElementTo } from '../../../helpers/domUtils';
+import { setStyleAttribute } from '../../../helpers/domUtils';
 
 fixture.disablePageReloads`Radio Group`
   .page(url(__dirname, '../../container.html'));
 
 test('Radio buttons placed into the template should not be selected after clicking the parent radio button (T816449)', async (t) => {
-  const parentGroup = new RadioGroup('#template');
+  const parentGroup = new RadioGroup('#container');
   const firstChildGroup = new RadioGroup(parentGroup.getItem().content.child().nth(0));
   const secondChildGroup = new RadioGroup(parentGroup.getItem(1).content.child());
   const thirdChildGroup = new RadioGroup(parentGroup.getItem(2).content.child());
@@ -67,16 +68,13 @@ test('Radio buttons placed into the template should not be selected after clicki
   await checkGroup(firstChildGroup, true);
   await checkGroup(secondChildGroup, false, true);
   await checkGroup(thirdChildGroup, false, false, true);
-}).before(async () => {
-  await appendElementTo('#container', 'div', 'template');
-  await createWidget('dxRadioGroup', {
-    items: [{}, {}, {}],
-    itemTemplate: () => ($('<div>') as any).dxRadioGroup({
-      dataSource: [{}, {}, {}],
-      layout: 'horizontal',
-    }),
-  }, '#template');
-});
+}).before(async () => createWidget('dxRadioGroup', {
+  items: [{}, {}, {}],
+  itemTemplate: () => ($('<div>') as any).dxRadioGroup({
+    dataSource: [{}, {}, {}],
+    layout: 'horizontal',
+  }),
+}));
 
 test('Dot of Radio button placed in scaled container should have valid centering(T1165339)', async (t) => {
   const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
@@ -87,10 +85,10 @@ test('Dot of Radio button placed in scaled container should have valid centering
     .expect(compareResults.isValid())
     .ok(compareResults.errorMessages());
 }).before(async () => {
-  await appendElementTo('#container', 'div', 'scaled', { transform: 'scale(0.7)' });
+  await setStyleAttribute(Selector('#container'), 'transform: scale(0.7);');
 
   await createWidget('dxRadioGroup', {
     items: ['One', 'Two', 'Three'],
     value: 'Two',
-  }, '#scaled');
+  }, '#container');
 });
