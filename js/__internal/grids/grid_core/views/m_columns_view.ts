5e1f646084b7b940a1f14124a1590b1971be1953
Eldar Iusupzhanov <ferrarijed@gmail.com>
2023-06-14 15:57:38 +0800

    fix _getCellElement

--- js/__internal/grids/grid_core/views/m_columns_view.ts
+++ js/__internal/grids/grid_core/views/m_columns_view.ts
@@ -1152,7 +1152,7 @@ export class ColumnsView extends viewWithColumnStateMixin {
 
     const $cell = $cells.eq(columnVisibleIndex);
 
-    return $cell;
+    return $cell.length > 0 ? $cell : undefined;
   }
 
   _getRowElement(rowIndex) {
