1f00f58483f8be283c1091be22ce71125fef33d5
Andrew Makarov <andrewmakarov@inbox.ru>
2023-06-20 20:55:17 +0400

    Add e2e test

--- testing/testcafe/tests/scheduler/header/dateNavigator.ts
+++ testing/testcafe/tests/scheduler/header/dateNavigator.ts
@@ -1,3 +1,4 @@
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import createWidget from '../../../helpers/createWidget';
 import url from '../../../helpers/getPageUrl';
 import Scheduler from '../../../model/scheduler';
@@ -32,3 +33,47 @@ fixture.disablePageReloads`Date navigator`
     height: 600,
   }));
 });
+
+test('Current date in Calendar should be respond on prev and next buttons of navigator', async (t) => {
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+  const { toolbar } = new Scheduler('#container');
+
+  await t
+    .click(toolbar.navigator.caption);
+
+  await t
+    .expect(await takeScreenshot('initial-calendar-state.png'))
+    .ok();
+
+  await t
+    .click(toolbar.navigator.nextButton)
+    .click(toolbar.navigator.nextButton)
+    .click(toolbar.navigator.nextButton);
+  await t
+    .click(toolbar.navigator.caption);
+
+  await t
+    .expect(await takeScreenshot('calendar-state-after-next-button-click.png'))
+    .ok();
+
+  await t
+    .click(toolbar.navigator.prevButton)
+    .click(toolbar.navigator.prevButton);
+  await t
+    .click(toolbar.navigator.caption);
+
+  await t
+    .expect(await takeScreenshot('calendar-state-after-prev-button-click.png'))
+    .ok();
+
+  await t
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => createWidget('dxScheduler', {
+  dataSource: [],
+  views: ['week', 'month'],
+  currentView: 'week',
+  currentDate: new Date(2021, 2, 28),
+}));
+
+// await toolbar.calendar.getView();
