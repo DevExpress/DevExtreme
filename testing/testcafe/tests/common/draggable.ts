c05c97681dc6995bc53c180dbb3969da2edcadb7
Andrey Dolzhikov <andrey.dolzhikov@devexpress.com>
2023-06-21 19:17:50 +0300

    extract method

--- testing/testcafe/tests/common/draggable.ts
+++ testing/testcafe/tests/common/draggable.ts
@@ -6,6 +6,37 @@ import Scrollable from '../../model/scrollView/scrollable';
 fixture.disablePageReloads`Draggable`
   .page(url(__dirname, '../container.html'));
 
+const init = ClientFunction(() => {
+  $('<div>', {
+    id: 'scrollview',
+    width: 400,
+    height: 400,
+  })
+    .css({
+      position: 'absolute',
+      top: 0,
+      padding: 20,
+      background: '#f18787',
+    })
+    .appendTo('#container');
+
+  $('<div>', {
+    id: 'scrollview-content',
+    height: 500,
+    width: 500,
+  }).appendTo('#scrollview');
+
+  $('<div>', {
+    id: 'drag-me',
+  })
+    .css({
+      'background-color': 'blue',
+      display: 'inline-block',
+    })
+    .appendTo('#scrollview-content');
+  $('#drag-me').append('DRAG ME!!!');
+});
+
 test('dxDraggable element should not loose its position on dragging with auto-scroll inside ScrollView (T1169590)', async (t) => {
   const draggable = Selector('#drag-me');
   const scrollable = new Scrollable('#scrollview');
@@ -32,37 +63,6 @@ test('dxDraggable element should not loose its position on dragging with auto-sc
     .expect((await draggable().boundingClientRect).left)
     .gt(400);
 }).before(async () => {
-  const init = ClientFunction(() => {
-    $('<div>', {
-      id: 'scrollview',
-      width: 400,
-      height: 400,
-    })
-      .css({
-        position: 'absolute',
-        top: 0,
-        padding: 20,
-        background: '#f18787',
-      })
-      .appendTo('#container');
-
-    $('<div>', {
-      id: 'scrollview-content',
-      height: 500,
-      width: 500,
-    }).appendTo('#scrollview');
-
-    $('<div>', {
-      id: 'drag-me',
-    })
-      .css({
-        'background-color': 'blue',
-        display: 'inline-block',
-      })
-      .appendTo('#scrollview-content');
-    $('#drag-me').append('DRAG ME!!!');
-  });
-
   await init();
   await createWidget('dxScrollView', {
     direction: 'both',
