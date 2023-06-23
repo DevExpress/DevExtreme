64321ee14b24e76bdb012e7ba84435add63fe994
alexander.polosatov <alexander.polosatov@devexpress.com>
2023-06-23 12:35:59 +0400

    fix misc tests

--- testing/tests/DevExpress.common/charts.tests.js
+++ testing/tests/DevExpress.common/charts.tests.js
@@ -1,4 +1,5 @@
-import { registerPattern, registerGradient, getGraphicObjects } from 'common/charts';
+import { registerPattern, registerGradient } from 'common/charts';
+import graphicObjects from '__internal/common/m_charts';
 import utils from 'viz/core/utils';
 
 
@@ -20,7 +21,7 @@ QUnit.test('should register pattern', function(assert) {
     assert.equal(this.getNextDefsStub.callCount, 2);
     assert.equal(id_1, 'DevExpressId_1');
     assert.equal(id_2, 'DevExpressId_2');
-    assert.deepEqual(getGraphicObjects(), {
+    assert.deepEqual(graphicObjects.getGraphicObjects(), {
         'DevExpressId_1': { key: 'test_key_1', type: 'pattern' },
         'DevExpressId_2': { key: 'test_key_2', type: 'pattern' }
     });
@@ -33,7 +34,7 @@ QUnit.test('should register gradient', function(assert) {
     assert.equal(this.getNextDefsStub.callCount, 2);
     assert.equal(id_1, 'DevExpressId_1');
     assert.equal(id_2, 'DevExpressId_2');
-    assert.deepEqual(getGraphicObjects(), {
+    assert.deepEqual(graphicObjects.getGraphicObjects(), {
         'DevExpressId_1': { key: 'test_key_1', type: 'gradient_type' },
         'DevExpressId_2': { key: 'test_key_2', type: 'gradient_type' }
     });
