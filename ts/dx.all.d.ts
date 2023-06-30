34be686c8f9782a19ca1ecd347aac331bfab64dc
Ilya Vinogradov <ilya.vinogradov@devexpress.com>
2023-06-30 19:55:09 +0400

    Scheduler(T1171988): Update dx.all.d.ts bundle.

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -23227,6 +23227,7 @@ declare module DevExpress.ui {
         readonly fromComponent?: dxSortable | dxDraggable;
         readonly toComponent?: dxSortable | dxDraggable;
         readonly toData?: any;
+        readonly toItemData?: any;
       };
     /**
      * @deprecated Attention! This type is for internal purposes only. If you used it previously, please submit a ticket to our {@link https://supportcenter.devexpress.com/ticket/create Support Center}. We will check if there is an alternative solution.
