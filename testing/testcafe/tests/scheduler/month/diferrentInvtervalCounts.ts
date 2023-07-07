d2a080d5526bcabd8c4dede071206e34e16f1c40
Roman Semenov <roman.semenov@devexpress.com>
2023-07-07 07:37:50 +0400

    fix test
    set test to default
    
    fix testcafe tests

--- testing/testcafe/tests/scheduler/month/diferrentInvtervalCounts.ts
+++ testing/testcafe/tests/scheduler/month/diferrentInvtervalCounts.ts
@@ -10,8 +10,10 @@ test('Interval count: 1, February of 2021', async (t) => {
   const scheduler = new Scheduler('#container');
   await t.expect(await compareScreenshot(t, 'month-february-2021.png', scheduler.workSpace)).ok();
 }).before(async () => createWidget('dxScheduler', {
-  views: ['month'],
-  intervalCount: 1,
+  views: [{
+    type: 'month',
+    intervalCount: 1,
+  }],
   currentView: 'month',
   firstDayOfWeek: 1,
   currentDate: new Date(2021, 1, 1),
@@ -20,11 +22,14 @@ test('Interval count: 1, February of 2021', async (t) => {
 test('Interval count: 12', async (t) => {
   const scheduler = new Scheduler('#container');
   await scheduler.scrollTo(new Date(2024, 6, 1)); // scroll to last row
+
   await t.expect(await compareScreenshot(t, 'month-interval-count-12.png', scheduler.workSpace)).ok();
 }).before(async () => createWidget('dxScheduler', {
-  views: ['month'],
-  intervalCount: 1,
+  views: [{
+    type: 'month',
+    intervalCount: 12,
+  }],
+  height: 600,
   currentView: 'month',
-  firstDayOfWeek: 1,
   currentDate: new Date(2023, 6, 1),
 }));
