f52c2af02d79c17f9984595505ffb1625f2a038b
Eldar Iusupzhanov <ferrarijed@gmail.com>
2023-07-11 15:25:14 +0600

    Scheduler - Move to TS - part 1: fix elint & imports

--- packages/devextreme/js/__internal/scheduler/appointments/data_provider/m_appointment_data_provider.ts
+++ packages/devextreme/js/__internal/scheduler/appointments/data_provider/m_appointment_data_provider.ts
@@ -1,137 +1,151 @@
-import config from '../../../../core/config';
-import { AppointmentDataSource } from './appointmentDataSource';
-import { AppointmentFilterBaseStrategy, AppointmentFilterVirtualStrategy } from './appointmentFilter';
-import combineRemoteFilter from '../../../../renovation/ui/scheduler/utils/filtering/remote';
+import config from '@js/core/config';
+import combineRemoteFilter from '@js/renovation/ui/scheduler/utils/filtering/remote';
+
+import { AppointmentDataSource } from './m_appointment_data_source';
+import { AppointmentFilterBaseStrategy, AppointmentFilterVirtualStrategy } from './m_appointment_filter';
 
 const FilterStrategies = {
-    virtual: 'virtual',
-    standard: 'standard'
+  virtual: 'virtual',
+  standard: 'standard',
 };
 
 export class AppointmentDataProvider {
-    constructor(options) {
-        this.options = options;
-        this.dataSource = this.options.dataSource;
-        this.dataAccessors = this.options.dataAccessors;
-        this.timeZoneCalculator = this.options.timeZoneCalculator;
+  options: any;
 
-        this.appointmentDataSource = new AppointmentDataSource(this.dataSource);
+  dataSource: any;
 
-        this.initFilterStrategy();
+  dataAccessors: any;
+
+  timeZoneCalculator: any;
+
+  appointmentDataSource: AppointmentDataSource;
+
+  filterStrategy: any;
+
+  constructor(options) {
+    this.options = options;
+    this.dataSource = this.options.dataSource;
+    this.dataAccessors = this.options.dataAccessors;
+    this.timeZoneCalculator = this.options.timeZoneCalculator;
+
+    this.appointmentDataSource = new AppointmentDataSource(this.dataSource);
+
+    this.initFilterStrategy();
+  }
+
+  get keyName() {
+    return this.appointmentDataSource.keyName;
+  }
+
+  get isDataSourceInit() {
+    return !!this.dataSource;
+  }
+
+  get filterStrategyName() {
+    return this.options.getIsVirtualScrolling()
+      ? FilterStrategies.virtual
+      : FilterStrategies.standard;
+  }
+
+  getFilterStrategy() {
+    if (!this.filterStrategy || this.filterStrategy.strategyName !== this.filterStrategyName) {
+      this.initFilterStrategy();
     }
 
-    get keyName() {
-        return this.appointmentDataSource.keyName;
+    return this.filterStrategy;
+  }
+
+  initFilterStrategy() {
+    const filterOptions = {
+      resources: this.options.resources,
+      dataAccessors: this.dataAccessors,
+      startDayHour: this.options.startDayHour,
+      endDayHour: this.options.endDayHour,
+      showAllDayPanel: this.options.showAllDayPanel,
+      timeZoneCalculator: this.options.timeZoneCalculator,
+      //
+      loadedResources: this.options.getLoadedResources,
+      supportAllDayRow: this.options.getSupportAllDayRow,
+      viewType: this.options.getViewType,
+      viewDirection: this.options.getViewDirection,
+      dateRange: this.options.getDateRange,
+      groupCount: this.options.getGroupCount,
+      viewDataProvider: this.options.getViewDataProvider,
+      allDayPanelMode: this.options.allDayPanelMode,
+    };
+
+    this.filterStrategy = this.filterStrategyName === FilterStrategies.virtual
+      ? new AppointmentFilterVirtualStrategy(filterOptions)
+      : new AppointmentFilterBaseStrategy(filterOptions);
+  }
+
+  setDataSource(dataSource) {
+    this.dataSource = dataSource;
+    this.initFilterStrategy();
+    this.appointmentDataSource.setDataSource(this.dataSource);
+  }
+
+  updateDataAccessors(dataAccessors) {
+    this.dataAccessors = dataAccessors;
+    this.initFilterStrategy();
+  }
+
+  // Filter mapping
+  filter(preparedItems) {
+    return this.getFilterStrategy().filter(preparedItems);
+  }
+
+  // TODO rename to the setRemoteFilter
+  filterByDate(min, max, remoteFiltering, dateSerializationFormat) {
+    if (!this.dataSource || !remoteFiltering) {
+      return;
     }
 
-    get isDataSourceInit() {
-        return !!this.dataSource;
-    }
+    const dataSourceFilter = this.dataSource.filter();
+    const filter = combineRemoteFilter({
+      dataSourceFilter,
+      dataAccessors: this.dataAccessors,
+      min,
+      max,
+      dateSerializationFormat,
+      forceIsoDateParsing: config().forceIsoDateParsing,
+    });
 
-    get filterStrategyName() {
-        return this.options.getIsVirtualScrolling()
-            ? FilterStrategies.virtual
-            : FilterStrategies.standard;
-    }
+    this.dataSource.filter(filter);
+  }
 
-    getFilterStrategy() {
-        if(!this.filterStrategy || this.filterStrategy.strategyName !== this.filterStrategyName) {
-            this.initFilterStrategy();
-        }
+  hasAllDayAppointments(filteredItems, preparedItems) {
+    return this.getFilterStrategy().hasAllDayAppointments(filteredItems, preparedItems);
+  }
 
-        return this.filterStrategy;
-    }
+  filterLoadedAppointments(filterOption, preparedItems) {
+    return this.getFilterStrategy().filterLoadedAppointments(filterOption, preparedItems);
+  }
 
-    initFilterStrategy() {
-        const filterOptions = {
-            resources: this.options.resources,
-            dataAccessors: this.dataAccessors,
-            startDayHour: this.options.startDayHour,
-            endDayHour: this.options.endDayHour,
-            showAllDayPanel: this.options.showAllDayPanel,
-            timeZoneCalculator: this.options.timeZoneCalculator,
-            //
-            loadedResources: this.options.getLoadedResources,
-            supportAllDayRow: this.options.getSupportAllDayRow,
-            viewType: this.options.getViewType,
-            viewDirection: this.options.getViewDirection,
-            dateRange: this.options.getDateRange,
-            groupCount: this.options.getGroupCount,
-            viewDataProvider: this.options.getViewDataProvider,
-            allDayPanelMode: this.options.allDayPanelMode,
-        };
+  calculateAppointmentEndDate(isAllDay, startDate) {
+    return this.getFilterStrategy().calculateAppointmentEndDate(isAllDay, startDate);
+  }
 
-        this.filterStrategy = this.filterStrategyName === FilterStrategies.virtual
-            ? new AppointmentFilterVirtualStrategy(filterOptions)
-            : new AppointmentFilterBaseStrategy(filterOptions);
-    }
+  // Appointment data source mappings
+  cleanState() { this.appointmentDataSource.cleanState(); }
 
-    setDataSource(dataSource) {
-        this.dataSource = dataSource;
-        this.initFilterStrategy();
-        this.appointmentDataSource.setDataSource(this.dataSource);
-    }
+  getUpdatedAppointment() { return this.appointmentDataSource._updatedAppointment; }
 
-    updateDataAccessors(dataAccessors) {
-        this.dataAccessors = dataAccessors;
-        this.initFilterStrategy();
-    }
+  getUpdatedAppointmentKeys() { return this.appointmentDataSource._updatedAppointmentKeys; }
 
-    // Filter mapping
-    filter(preparedItems) {
-        return this.getFilterStrategy().filter(preparedItems);
-    }
+  add(rawAppointment) {
+    return this.appointmentDataSource.add(rawAppointment);
+  }
 
-    // TODO rename to the setRemoteFilter
-    filterByDate(min, max, remoteFiltering, dateSerializationFormat) {
-        if(!this.dataSource || !remoteFiltering) {
-            return;
-        }
+  update(target, rawAppointment) {
+    return this.appointmentDataSource.update(target, rawAppointment);
+  }
 
-        const dataSourceFilter = this.dataSource.filter();
-        const filter = combineRemoteFilter({
-            dataSourceFilter,
-            dataAccessors: this.dataAccessors,
-            min,
-            max,
-            dateSerializationFormat,
-            forceIsoDateParsing: config().forceIsoDateParsing
-        });
+  remove(rawAppointment) {
+    return this.appointmentDataSource.remove(rawAppointment);
+  }
 
-        this.dataSource.filter(filter);
-    }
-
-
-    hasAllDayAppointments(filteredItems, preparedItems) {
-        return this.getFilterStrategy().hasAllDayAppointments(filteredItems, preparedItems);
-    }
-
-    filterLoadedAppointments(filterOption, preparedItems) {
-        return this.getFilterStrategy().filterLoadedAppointments(filterOption, preparedItems);
-    }
-
-    calculateAppointmentEndDate(isAllDay, startDate) {
-        return this.getFilterStrategy().calculateAppointmentEndDate(isAllDay, startDate);
-    }
-
-    // Appointment data source mappings
-    cleanState() { this.appointmentDataSource.cleanState(); }
-    getUpdatedAppointment() { return this.appointmentDataSource._updatedAppointment; }
-    getUpdatedAppointmentKeys() { return this.appointmentDataSource._updatedAppointmentKeys; }
-
-    add(rawAppointment) {
-        return this.appointmentDataSource.add(rawAppointment);
-    }
-
-    update(target, rawAppointment) {
-        return this.appointmentDataSource.update(target, rawAppointment);
-    }
-
-    remove(rawAppointment) {
-        return this.appointmentDataSource.remove(rawAppointment);
-    }
-
-    destroy() {
-        this.appointmentDataSource.destroy();
-    }
+  destroy() {
+    this.appointmentDataSource.destroy();
+  }
 }
