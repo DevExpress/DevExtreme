3f0b18bc76479314dfca68b36c5db9c524fe3001
Alexander Bulychev <git@bulychev.net>
2023-07-11 18:05:11 +0400

    enable rest workflows

--- packages/devextreme/testing/systemjs-builder.js
+++ packages/devextreme/testing/systemjs-builder.js
@@ -100,6 +100,7 @@ const transpileFile = async(sourcePath, targetPath) => {
     const code = fs.readFileSync(sourcePath)
         .toString()
         .replaceAll(path.normalize('/testing/helpers/'), path.normalize('/artifacts/transpiled-testing/helpers/'))
+        // TODO see TODO at packages/devextreme/testing/tests/DevExpress.viz.vectorMap.utils/tests.js and remove this lines after fix
         .replaceAll(
             path.normalize('../../../artifacts/js/vectormap-utils/dx.vectormaputils.js'),
             path.normalize('../../../../artifacts/js/vectormap-utils/dx.vectormaputils.js'));
