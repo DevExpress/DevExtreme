96d95f89a0557bef0717ca68bef39fc726f36b05
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2023-07-13 16:04:14 +0400

    Fix expectations in themebuilder tests

--- packages/devextreme-themebuilder/tests/data/compilation-results/no-changes-css.ts
+++ packages/devextreme-themebuilder/tests/data/compilation-results/no-changes-css.ts
@@ -1,9 +1,1 @@
-export default `.dx-accordion {
-  background-color: "Helvetica Neue","Segoe UI",helvetica,verdana,sans-serif;
-  color: #337ab7;
-  background-image: url(icons/icons.woff2);
-}
-.dx-accordion .from-base {
-  background-color: transparent;
-  color: #337ab7;
-}`;
+export default '.dx-accordion{background-color:"Helvetica Neue","Segoe UI",helvetica,verdana,sans-serif;color:#337ab7;background-image:url(icons/icons.woff2)}.dx-accordion .from-base{background-color:transparent;color:#337ab7}';
