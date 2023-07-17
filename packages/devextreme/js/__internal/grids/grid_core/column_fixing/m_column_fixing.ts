185e0d81e061f20922168ab998aed3ab0f4be9ed
Alyar <>
2023-07-17 04:59:53 +0400

    DataGrid: Fix new rows rendering when scrolling horizontally then collapsing a row (T1176161)

--- packages/devextreme/js/__internal/grids/grid_core/column_fixing/m_column_fixing.ts
+++ packages/devextreme/js/__internal/grids/grid_core/column_fixing/m_column_fixing.ts
@@ -150,10 +150,12 @@ const baseFixedColumns = {
 
     if (this._isFixedColumns) {
       const change = options?.change;
+      const $dataRows = this._getRowElements(this._fixedTableElement);
+      const needPartialUpdate = change?.virtualColumnsScrolling && $dataRows.length === change?.items?.length;
 
       this._isFixedTableRendering = true;
 
-      if (change?.virtualColumnsScrolling && this.option('scrolling.legacyMode') !== true) {
+      if (needPartialUpdate && this.option('scrolling.legacyMode') !== true) {
         this._partialUpdateFixedTable(fixedColumns);
         this._isFixedTableRendering = false;
       } else {
