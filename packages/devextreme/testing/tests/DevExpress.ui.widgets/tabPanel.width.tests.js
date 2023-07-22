f14c12165daa691ac0ae3952f741a39028b87628
marker dao ® <mark_nikolsky@outlook.com>
2023-07-22 17:58:17 +0400

    fix(tabPanel): Fix styling in with nav buttons mode && Fix flex-grow

--- packages/devextreme/testing/tests/DevExpress.ui.widgets/tabPanel.width.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.widgets/tabPanel.width.tests.js
@@ -62,10 +62,10 @@ QUnit.module('Tabs width', () => {
 
             if(options.width > 250) {
                 this.assert.strictEqual(getOuterWidth(this._getTabItem(0)), 140, getOuterWidth(this._getTabItem(0)) + ' = 140');
-                this.assert.strictEqual(getOuterWidth(this._getTabItem(1)) > 140, true, getOuterWidth(this._getTabItem(1)) + ' > 140');
+                this.assert.strictEqual(getOuterWidth(this._getTabItem(1)), 140, getOuterWidth(this._getTabItem(1)) + ' = 140');
             } else {
                 this.assert.strictEqual(getOuterWidth(this._getTabItem(0)), 140, getOuterWidth(this._getTabItem(0)) + ' = 140');
-                this.assert.strictEqual(getOuterWidth(this._getTabItem(1)) > 140, true, getOuterWidth(this._getTabItem(1)) + ' > 140');
+                this.assert.strictEqual(getOuterWidth(this._getTabItem(1)), 140, getOuterWidth(this._getTabItem(1)) + ' = 140');
             }
 
             this.assert.equal(this.$tabPanel.find(`.${TABS_NAV_BUTTON_CLASS}`).length, options.expectNavButtons, `${options.expectNavButtons} navigation buttons should be rendered`);
