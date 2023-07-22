2e5506e1cb84c4a02813c01a3746ffbd242dab59
marker dao ® <mark_nikolsky@outlook.com>
2023-07-22 22:27:47 +0400

    feat(tabPanel): Add addShadowDomStyles();

--- packages/devextreme/testing/tests/DevExpress.ui.widgets/tabPanel.width.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.widgets/tabPanel.width.tests.js
@@ -4,6 +4,7 @@ import 'generic_light.css!';
 import { triggerResizeEvent, triggerShownEvent } from 'events/visibility_change';
 import $ from 'jquery';
 import 'ui/tab_panel';
+import { addShadowDomStyles } from 'core/utils/shadow_dom';
 
 const TABS_ITEM_CLASS = 'dx-tab';
 const TABS_NAV_BUTTON_CLASS = 'dx-tabs-nav-button';
@@ -31,6 +32,7 @@ QUnit.module('Tabs width', () => {
             }).dxTabPanel('instance');
 
             this.$container.appendTo('#qunit-fixture');
+            addShadowDomStyles($('#qunit-fixture'));
 
             if(!this._isOptionApproach()) {
                 this.setContainerWidth(width);
