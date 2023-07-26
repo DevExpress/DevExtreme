b7c12b331c60ff63f4847c231cd4b1efc29bb818
Ilya Vinogradov <48182348+williamvinogradov@users.noreply.github.com>
2023-07-26 20:36:46 +0400

    TS Build: Remove old R0 checks & commands. (#25182)
    Co-authored-by: Ilya Vinogradov <ilya.vinogradov@devexpress.com>

--- packages/devextreme/build/gulp/generator/generator-options.js
+++ packages/devextreme/build/gulp/generator/generator-options.js
@@ -1,16 +1,11 @@
 'use strict';
 
-const BASE_GENERATOR_OPTIONS = {
-    defaultOptionsModule: 'js/core/options/utils',
-};
-
 const BASE_GENERATOR_OPTIONS_WITH_JQUERY = {
-    ...BASE_GENERATOR_OPTIONS,
+    defaultOptionsModule: 'js/core/options/utils',
     jqueryComponentRegistratorModule: 'js/core/component_registrator',
     jqueryBaseComponentModule: 'js/renovation/component_wrapper/common/component'
 };
 
 module.exports = {
-    BASE_GENERATOR_OPTIONS,
     BASE_GENERATOR_OPTIONS_WITH_JQUERY
 };
