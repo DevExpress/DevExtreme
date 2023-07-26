1881cce7f7245e40bd669aafcdbd4fbf80938b6b
Roman Semenov <roman.semenov@devexpress.com>
2023-07-26 20:27:03 +0400

    Scheduler - All Day Panel works incorrectly with certain appointments (T1177505) (#25189)

--- testing/testcafe/tests/scheduler/appointments/allDay/allDay.ts
+++ testing/testcafe/tests/scheduler/appointments/allDay/allDay.ts
@@ -2,6 +2,7 @@ import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import { insertStylesheetRulesToPage } from '../../../../helpers/domUtils';
 import createWidget from '../../../../helpers/createWidget';
 import url from '../../../../helpers/getPageUrl';
+import Scheduler from '../../../../model/scheduler';
 
 fixture.disablePageReloads`Scheduler - All day appointments`
   .page(url(__dirname, '../../../container.html'));
@@ -83,6 +84,52 @@ test('it should skip weekend days in timelineWorkWeek', async (t) => {
       maxAppointmentsPerCell: 'unlimited',
       currentView: 'timelineWorkWeek',
       currentDate: new Date(2021, 3, 2),
+    },
+  );
+});
+
+test('should work correctly for unsorted dataSource', async (t) => {
+  const scheduler = new Scheduler('#container');
+
+  const {
+    takeScreenshot,
+    compareResults,
+  } = createScreenshotsComparer(t);
+
+  await t
+    .expect(await takeScreenshot('allDay-unsorted-datasource.png', scheduler.workSpace))
+    .ok();
+
+  await t
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => {
+  await createWidget(
+    'dxScheduler',
+    {
+      dataSource: [{
+        id: 3,
+        text: '3',
+        startDate: new Date('2020-11-23T00:00:00.000'),
+        endDate: new Date('2020-11-28T00:00:00.000'),
+        allDay: true,
+      }, {
+        id: 5,
+        text: '5',
+        startDate: new Date('2020-11-27T00:00:00.000'),
+        endDate: new Date('2020-11-27T00:00:00.000'),
+        allDay: true,
+      }, {
+        id: 1,
+        text: '1',
+        startDate: new Date('2020-11-25T22:20:00.000'),
+        endDate: new Date('2020-11-26T12:30:00.000'),
+      }],
+      views: ['week'],
+      currentView: 'week',
+      showAllDayPanel: true,
+      currentDate: new Date(2020, 10, 25),
+      height: 600,
     },
   );
 });
