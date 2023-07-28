9ab4a3093ba9d9bd50de8f21afda98fbf0687335
Mark Allen Ramirez <markallenramirez@gmail.com>
2023-07-28 13:11:07 +0800

    test

---
+++ packages/devextreme/testing/testcafe/tests/dataGrid/columnHeaders.ts
@@ -0,0 +1,19 @@
+import { a11yCheck } from '../../helpers/accessibilityUtils';
+import url from '../../helpers/getPageUrl';
+import createWidget from '../../helpers/createWidget';
+
+fixture.disablePageReloads`Column Headers`
+  .page(url(__dirname, '../container.html'));
+
+test('Checking column headers via aXe', async (t) => {
+  await a11yCheck(t);
+}).before(async () => createWidget('dxDataGrid', {
+  dataSource: [{
+    id: 1,
+    field1: 'field1',
+    field2: 'field2'
+  }],
+  keyExpr: 'id',
+  columns: ['field1', 'field2'],
+  showBorders: true,
+}));
