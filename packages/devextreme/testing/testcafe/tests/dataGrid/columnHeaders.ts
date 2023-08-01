525b78777a942a4260ea389947cdaa08202fc842
Mark Allen Ramirez <markallenramirez@gmail.com>
2023-08-01 11:31:07 +0800

    add test material

--- packages/devextreme/testing/testcafe/tests/dataGrid/columnHeaders.ts
+++ packages/devextreme/testing/testcafe/tests/dataGrid/columnHeaders.ts
@@ -1,19 +1,27 @@
 import { a11yCheck } from '../../helpers/accessibilityUtils';
 import url from '../../helpers/getPageUrl';
 import createWidget from '../../helpers/createWidget';
+import { changeTheme } from '../../helpers/changeTheme';
 
 fixture.disablePageReloads`Column Headers`
   .page(url(__dirname, '../container.html'));
 
-test('Checking column headers via aXe', async (t) => {
-  await a11yCheck(t);
-}).before(async () => createWidget('dxDataGrid', {
-  dataSource: [{
-    id: 1,
-    field1: 'field1',
-    field2: 'field2',
-  }],
-  keyExpr: 'id',
-  columns: ['field1', 'field2'],
-  showBorders: true,
-}));
+['generic.light', 'material.blue.light'].forEach((theme) => {
+  test(`Checking column headers via aXe - ${theme}`, async (t) => {
+    await a11yCheck(t);
+  }).before(async () => {
+    await changeTheme(theme);
+    return createWidget('dxDataGrid', {
+      dataSource: [{
+        id: 1,
+        field1: 'field1',
+        field2: 'field2',
+      }],
+      keyExpr: 'id',
+      columns: ['field1', 'field2'],
+      showBorders: true,
+    })
+  }).after(async () => {
+    await changeTheme('generic.light');
+  });
+});
