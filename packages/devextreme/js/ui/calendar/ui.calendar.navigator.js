4ff94ca056c0b1e782fb000cf7519e25177de501
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2023-08-03 22:19:58 +0400

    revert calendar changes

--- packages/devextreme/js/ui/calendar/ui.calendar.navigator.js
+++ packages/devextreme/js/ui/calendar/ui.calendar.navigator.js
@@ -66,7 +66,7 @@ class Navigator extends Widget {
 
         this._prevButton = this._createComponent($('<div>'),
             Button, {
-                focusStateEnabled: true,
+                focusStateEnabled: false,
                 icon: rtlEnabled ? 'chevronright' : 'chevronleft',
                 onClick: (e) => { this._clickAction({ direction: -direction, event: e }); },
                 type,
