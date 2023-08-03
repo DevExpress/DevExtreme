2e7b02ef12811dc9b3a4e891b356d15a2abfab70
Constantine Panchenko <constantin.panchenko@gmail.com>
2023-08-03 12:47:21 +0400

    Typescript to Javascript demos conversion tool (#2723)

--- configs/React/config.js
+++ configs/React/config.js
@@ -1,8 +1,17 @@
 window.config = {
-  transpiler: 'plugin-babel',
+  transpiler: 'ts',
+  typescriptOptions: {
+    module: 'system',
+    emitDecoratorMetadata: true,
+    experimentalDecorators: true,
+    jsx: 'react',
+  },
   meta: {
     'react': {
       'esModule': true,
+    },
+    'typescript': {
+      'exports': 'ts',
     },
     'devextreme/localization.js': {
       'esModule': true,
@@ -28,6 +37,8 @@ window.config = {
   },
   defaultExtension: 'js',
   map: {
+    'ts': 'npm:plugin-typescript/lib/plugin.js',
+    'typescript': 'npm:typescript/lib/typescript.js',
     'react': 'npm:react/umd/react.development.js',
     'react-dom': 'npm:react-dom/umd/react-dom.development.js',
     'prop-types': 'npm:prop-types/prop-types.js',
