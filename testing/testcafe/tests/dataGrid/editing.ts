1280c01dd1b60498c09fd94e1e2637a2e1daa5a0
Alyar <Alyar666@users.noreply.github.com>
2023-08-07 20:00:50 +0300

    DataGrid: Fix unstable test (#25278)
    Co-authored-by: Alyar <>

--- testing/testcafe/tests/dataGrid/editing.ts
+++ testing/testcafe/tests/dataGrid/editing.ts
@@ -2316,8 +2316,15 @@ test('Popup EditForm screenshot', async (t) => {
   test(`The disabled state should be correct for a custom button when given as a SVG image (${theme})`, async (t) => {
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
     const dataGrid = new DataGrid('#container');
+    const commandCell = dataGrid.getDataRow(0).getCommandCell(2);
+    const firstCustomIcon = commandCell.getButton(2);
+    const secondCustomIcon = commandCell.getButton(3);
 
     await t
+      .expect(firstCustomIcon.clientWidth)
+      .eql(20)
+      .expect(secondCustomIcon.clientWidth)
+      .eql(20)
       .expect(await takeScreenshot(`T1179114-grid-edit-custom-button-in-${theme.split('.')[0]}-theme-when-useicons-is-${useIcons}.png`, dataGrid.element))
       .ok()
       .expect(compareResults.isValid())
@@ -2340,6 +2347,7 @@ test('Popup EditForm screenshot', async (t) => {
       },
       columns: ['Id', 'name', {
         type: 'buttons',
+        width: 200,
         buttons: [
           {
             name: 'delete',
