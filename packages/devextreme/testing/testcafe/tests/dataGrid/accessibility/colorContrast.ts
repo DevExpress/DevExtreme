37efcbe40b697ed996af4cbd03ea3a59b63756a5
Alyar <>
2023-08-10 15:35:04 +0400

    Add test for sorting and group panel

--- packages/devextreme/testing/testcafe/tests/dataGrid/accessibility/colorContrast.ts
+++ packages/devextreme/testing/testcafe/tests/dataGrid/accessibility/colorContrast.ts
@@ -2,6 +2,7 @@ import { a11yCheck } from '../../../helpers/accessibilityUtils';
 import url from '../../../helpers/getPageUrl';
 import createWidget from '../../../helpers/createWidget';
 import DataGrid from '../../../model/dataGrid';
+import { getData } from '../helpers/generateDataSourceData';
 import { Themes } from '../helpers/themes';
 import { changeTheme } from '../../../helpers/changeTheme';
 
@@ -31,6 +32,45 @@ const DATA_GRID_SELECTOR = '#container';
 
     return createWidget('dxDataGrid', {
       dataSource: [],
+    });
+  }).after(async () => {
+    await changeTheme('generic.light');
+  });
+
+  test(`Sorting and group panel in ${theme}`, async (t) => {
+    const dataGrid = new DataGrid(DATA_GRID_SELECTOR);
+
+    await t
+      .expect(dataGrid.isReady())
+      .ok();
+
+    await a11yCheck(t, DATA_GRID_SELECTOR, {
+      runOnly: 'color-contrast',
+    });
+  }).before(async () => {
+    await changeTheme(theme);
+
+    return createWidget('dxDataGrid', {
+      dataSource: getData(10, 5),
+      keyExpr: 'field_0',
+      groupPanel: {
+        visible: true,
+      },
+      columns: [
+        'field_0',
+        'field_1',
+        'field_2',
+        {
+          dataField: 'field_3',
+          sortOrder: 'asc',
+          sortIndex: 0,
+        },
+        {
+          dataField: 'field_4',
+          sortOrder: 'desc',
+          sortIndex: 1,
+        },
+      ],
     });
   }).after(async () => {
     await changeTheme('generic.light');
