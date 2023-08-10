45584a1089b0ddff109b54d2d66e741c04f0e7db
Alyar <>
2023-08-10 15:38:37 +0400

    Add test for paging

--- packages/devextreme/testing/testcafe/tests/dataGrid/accessibility/colorContrast.ts
+++ packages/devextreme/testing/testcafe/tests/dataGrid/accessibility/colorContrast.ts
@@ -75,4 +75,82 @@ const DATA_GRID_SELECTOR = '#container';
   }).after(async () => {
     await changeTheme('generic.light');
   });
+
+  test(`Paging with displayMode is 'full' in ${theme}`, async (t) => {
+    const dataGrid = new DataGrid(DATA_GRID_SELECTOR);
+
+    await t
+      .expect(dataGrid.isReady())
+      .ok();
+
+    await a11yCheck(t, DATA_GRID_SELECTOR, {
+      runOnly: 'color-contrast',
+    });
+  }).before(async () => {
+    await changeTheme(theme);
+
+    return createWidget('dxDataGrid', {
+      dataSource: getData(100, 5),
+      keyExpr: 'field_0',
+      columns: [
+        'field_0',
+        'field_1',
+        'field_2',
+        'field_3',
+        'field_4',
+      ],
+      paging: {
+        pageSize: 5,
+      },
+      pager: {
+        visible: true,
+        allowedPageSizes: [5, 10, 'all'],
+        showPageSizeSelector: true,
+        showInfo: true,
+        showNavigationButtons: true,
+        displayMode: 'full',
+      },
+    });
+  }).after(async () => {
+    await changeTheme('generic.light');
+  });
+
+  test(`Paging with displayMode is 'compact' in ${theme}`, async (t) => {
+    const dataGrid = new DataGrid(DATA_GRID_SELECTOR);
+
+    await t
+      .expect(dataGrid.isReady())
+      .ok();
+
+    await a11yCheck(t, DATA_GRID_SELECTOR, {
+      runOnly: 'color-contrast',
+    });
+  }).before(async () => {
+    await changeTheme(theme);
+
+    return createWidget('dxDataGrid', {
+      dataSource: getData(100, 5),
+      keyExpr: 'field_0',
+      columns: [
+        'field_0',
+        'field_1',
+        'field_2',
+        'field_3',
+        'field_4',
+      ],
+      paging: {
+        pageSize: 5,
+      },
+      pager: {
+        visible: true,
+        allowedPageSizes: [5, 10, 'all'],
+        showPageSizeSelector: true,
+        showInfo: true,
+        showNavigationButtons: true,
+        displayMode: 'compact',
+      },
+    });
+  }).after(async () => {
+    await changeTheme('generic.light');
+  });
 });
