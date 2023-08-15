5d1accf290ff6a58d3ef1c5867ed53e05105a86d
Roman Semenov <roman.semenov@devexpress.com>
2023-08-15 10:19:40 +0400

    stabilize test

--- packages/devextreme/testing/tests/DevExpress.ui.widgets.scheduler/workSpace.markup.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.widgets.scheduler/workSpace.markup.tests.js
@@ -1243,22 +1243,24 @@ QUnit.module('Workspace Month markup', monthModuleConfig, () => {
     });
 
     QUnit.test('WorkSpace Month view has right count of rows with view option intervalCount', function(assert) {
+        this.instance.option('currentDate', '2023-06-01');
         this.instance.option('intervalCount', 2);
 
         let rows = this.instance.$element().find('.dx-scheduler-date-table-row');
-        assert.equal(rows.length, 9, 'view has right rows count');
+        assert.equal(rows.length, 10, 'view has right rows count');
 
         this.instance.option('intervalCount', 4);
 
         rows = this.instance.$element().find('.dx-scheduler-date-table-row');
-        assert.equal(rows.length, 18, 'view has right rows count');
+        assert.equal(rows.length, 19, 'view has right rows count');
     });
 
     QUnit.test('WorkSpace Month view has right count of cells with view option intervalCount', function(assert) {
+        this.instance.option('currentDate', '2023-06-01');
         this.instance.option('intervalCount', 2);
 
         const rows = this.instance.$element().find('.dx-scheduler-date-table-cell');
-        assert.equal(rows.length, 7 * 9, 'view has right cells count');
+        assert.equal(rows.length, 7 * 10, 'view has right cells count');
     });
 
     QUnit.test('WorkSpace Month view with option intervalCount has cells with special firstDayOfMonth class', function(assert) {
