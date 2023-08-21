299a2726c0fdc31cf1b3260a339235a17720db1c
Roman Semenov <roman.semenov@devexpress.com>
2023-08-21 06:09:18 +0400

    test

---
+++ packages/devextreme/testing/testcafe/tests/dataGrid/filterPanel.ts
@@ -0,0 +1,47 @@
+import { Selector } from 'testcafe';
+import url from '../../helpers/getPageUrl';
+import createWidget from '../../helpers/createWidget';
+import DataGrid from '../../model/dataGrid';
+
+fixture.disablePageReloads`filterPanel`
+  .page(url(__dirname, '../container.html'));
+
+const POPUP_CLASS = '.dx-popup';
+const INVISIBLE_CLASS = '.dx-state-invisible';
+
+function getVisiblePopups(): Selector {
+  return Selector(`${POPUP_CLASS}:not(${INVISIBLE_CLASS})`);
+}
+
+// T1182854
+test('editor\'s popup inside filterBuilder is opening & closing right (T1182854)', async (t) => {
+  const dataGrid = new DataGrid('#container');
+
+  const filterBuilder = (
+    await dataGrid.getFilterPanel().openFilterBuilderPopup(t)
+  ).getFilterBuilder();
+
+  await t.expect(getVisiblePopups().count).eql(1); // only filterPanel popup
+
+  await t.click(filterBuilder.getField().getValueText());
+  await t.expect(getVisiblePopups().count).eql(2); // filterPanel popup + editor popup
+
+  await t.debug();
+
+  await t.click(filterBuilder.getField().getValueText());
+  await t.expect(getVisiblePopups().count).eql(1); // only filterPanel popup
+
+  await t.debug();
+
+  await t.click(filterBuilder.getField().getValueText());
+  await t.expect(getVisiblePopups().count).eql(2); // filterPanel popup + editor popup
+
+  await t.debug();
+}).before(async () => createWidget('dxDataGrid', {
+  dataSource: [{ column1: 'first' }],
+  columns: ['column1'],
+  filterValue: ['column1', 'anyof', []],
+  filterPanel: {
+    visible: true,
+  },
+}));
