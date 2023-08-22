9851e0e898dbcd75fe6d5369510a5b6077971d8b
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2023-08-22 13:16:09 +0400

    Add test cases for invalid tokens

--- packages/devextreme/js/__internal/common/crypto/license_validation.test.ts
+++ packages/devextreme/js/__internal/common/crypto/license_validation.test.ts
@@ -1,7 +1,7 @@
 import { parseToken } from './license_validation';
 
 describe('license token', () => {
-  const testCases = [
+  const validCases = [
     {
       token: 'ewogICJmb3JtYXQiOiAxLAogICJjdXN0b21lcklkIjogImIxMTQwYjQ2LWZkZTEtNDFiZC1hMjgwLTRkYjlmOGU3ZDliZCIsCiAgIm1heFZlcnNpb25BbGxvd2VkIjogMjMxCn0=.DiDceRbil4IzXl5av7pNkKieyqHHhRf+CM477zDu4N9fyrhkQsjRourYvgVfkbSm+EQplkXhlMBc3s8Vm9n+VtPaMbeWXis92cdW/6HiT+Dm54xw5vZ5POGunKRrNYUzd9zTbYcz0bYA/dc/mHFeUdXA0UlKcx1uMaXmtJrkK74=',
       payload: {
@@ -22,14 +22,7 @@ describe('license token', () => {
       },
     },
   ];
-
-  testCases.map((c) => c.token).forEach((token) => it(`verifies payload: '${token}'`, () => {
-    const license = parseToken(token);
-
-    expect(license.kind).toBe('verified');
-  }));
-
-  testCases.forEach(({ token, payload: expected }) => it(`decodes payload '${token}'`, () => {
+  validCases.forEach(({ token, payload: expected }) => it(`verifies and decodes payload '${token}'`, () => {
     const license = parseToken(token);
 
     expect(license.kind).toBe('verified');
@@ -37,6 +30,64 @@ describe('license token', () => {
       expect(license.payload).toEqual(expected);
     }
   }));
+
+  const invalidSignatureCases = [
+    'ewogICJmb3JtYXQiOiAxLAogICJjdXN0b21lcklkIjogImIxMTQwYjQ2LWZkZTEtNDFiZC1hMjgwLTRkYjlmOGU3ZDliZCIsCiAgIm1heFZlcnNpb25BbGxvd2VkIjogMjMxCn0=.NVsilC5uWlD5QGS6bocLMlsVVK0VpZXYwU2DstUiLRpEI79/onuR8dGWasCLBo4PORDHPkNA/Ej8XeCHzJ0EkXRRZ7E2LrP/xlEfHRXTruvW4IEbZt3LiwJBt6/isLz+wzXtYtjV7tpE07/Y0TFoy+mWpHoU11GVtwKh6weRxkg=',
+  ];
+  invalidSignatureCases.forEach((token) => it(`fails if payload is not verified: '${token}'`, () => {
+    const license = parseToken(token);
+
+    expect(license.kind).toBe('corrupted');
+
+    if (license.kind === 'corrupted') {
+      expect(license.error).toBe('verification');
+    }
+  }));
+
+  it('fails if payload is invalid JSON', () => {
+    const license = parseToken('YWJj.vjx6wAI9jVkHJAnKcsuYNZ5UvCq3UhypQ+0f+kZ37/Qc1uj4BM6//Kfi4SVsXGOaOTFYWgzesROnHCp3jZRqphJwal4yXHD1sGFi6FEdB4MgdgNZvsZSnxNWLs/7s07CzuHLTpJrAG7sTdHVkQWZNnSCKjzV7909c/Stl9+hkLo=');
+
+    expect(license.kind).toBe('corrupted');
+
+    if (license.kind === 'corrupted') {
+      expect(license.error).toBe('deserialization');
+    }
+  });
+
+  it('fails if payload is invalid Base64', () => {
+    const license = parseToken('ewogICJmb3JtYXQiOiAxLAogICJjdXN0b21lcklkIjogIjM3Yjg4ZjBmLWQ0MmMtNDJiZS05YjhkLTU1ZGMwYzUzYzAxZiIsCiAgIm1heFZlcnNpb25BbGxvd2VkIjogMjIxCn0-.EnP/RDKg0eSyaPU1eDUFll1lqOdYbhN3u73LhN1op8vjNwA0P1vKiT1DfQRmXudlleGWgDkLA2OmJYUER8j7I3LSFf3hLkBAoWoBErgveTb2zkbz8P1i9lE+XmzIXeYHyZBYUt0IPkNfajF9zzbSDDin1CvW7pnADi0vIeZ5ICQ=');
+
+    expect(license.kind).toBe('corrupted');
+
+    if (license.kind === 'corrupted') {
+      expect(license.error).toBe('decoding');
+    }
+  });
+
+  const incompletePayloadCases = [
+    'ewogICJmb3JtYXQiOiAxLAogICJtYXhWZXJzaW9uQWxsb3dlZCI6IDIzMQp9.WH30cajUFcKqw/fwt4jITM/5tzVwPpbdbezhhdBi5oeOvU06zKY0J4M8gQy8GQ++RPYVCAo2md6vI9D80FD2CC4w+hpQLJNJJgNUHYPrgG6CX1yAB3M+NKHsPP9S71bXAgwvignb5uPo0R5emQzr4RKDhWQMKtgqEcRe+yme2mU=',
+    'ewogICJjdXN0b21lcklkIjogImIxMTQwYjQ2LWZkZTEtNDFiZC1hMjgwLTRkYjlmOGU3ZDliZCIsCiAgIm1heFZlcnNpb25BbGxvd2VkIjogMjMxCn0=.ok32DBaAgf3ijLmNQb+A0kUV2AiSivqvZJADdF607qqlAaduAVnotJtgdwm/Ib3MErfaGrDohCYoFMnKQevkRxFkA7tK3kOBnTZPUnZY0r3wyulMQmr4Qo+Sjf/fyXs4IYpGsC7/uJjgrCos8uzBegfmgfM93XSt6pKl9+c5xvc=',
+    'ewogICJmb3JtYXQiOiAxLAogICJjdXN0b21lcklkIjogImIxMTQwYjQ2LWZkZTEtNDFiZC1hMjgwLTRkYjlmOGU3ZDliZCIKfQ==.resgTqmazrorRNw7mmtV31XQnmTSw0uLEArsmpzCjWMQJLocBfAjpFvKBf+SAG9q+1iOSFySj64Uv2xBVqHnyeNVBRbouOKOnAB8RpkKvN4sc5SDc8JAG5TkwPVSzK/VLBpQxpqbxlcrRfHwz9gXqQoPt4/ZVATn285iw3DW0CU=',
+  ];
+  incompletePayloadCases.forEach((token) => it(`fails if payload misses required fields: '${token}'`, () => {
+    const license = parseToken(token);
+
+    expect(license.kind).toBe('corrupted');
+
+    if (license.kind === 'corrupted') {
+      expect(license.error).toBe('payload');
+    }
+  }));
+
+  it('fails if payload has unsupported version', () => {
+    const license = parseToken('ewogICJmb3JtYXQiOiAyLAogICJjdXN0b21lcklkIjogImIxMTQwYjQ2LWZkZTEtNDFiZC1hMjgwLTRkYjlmOGU3ZDliZCIsCiAgIm1heFZlcnNpb25BbGxvd2VkIjogMjMxCn0=.tTBymZMROsYyMiP6ldXFqGurbzqjhSQIu/pjyEUJA3v/57VgToomYl7FVzBj1asgHpadvysyTUiX3nFvPxbp166L3+LB3Jybw9ueMnwePu5vQOO0krqKLBqRq+TqHKn7k76uYRbkCIo5UajNfzetHhlkin3dJf3x2K/fcwbPW5A=');
+
+    expect(license.kind).toBe('corrupted');
+
+    if (license.kind === 'corrupted') {
+      expect(license.error).toBe('version');
+    }
+  });
 
   [
     '', '.', 'a', 'a.', '.a', '.a.', '.a.', '.a.b', 'a.b.', '.a.b.',
@@ -46,7 +97,7 @@ describe('license token', () => {
     expect(license.kind).toBe('corrupted');
 
     if (license.kind === 'corrupted') {
-      expect(license.error).toBe('format');
+      expect(license.error).toBe('general');
     }
   }));
 });
--- packages/devextreme/js/__internal/common/crypto/license_validation.ts
+++ packages/devextreme/js/__internal/common/crypto/license_validation.ts
@@ -5,41 +5,72 @@ export interface License {
   readonly maxVersionAllowed: number;
 }
 
+interface Payload extends Partial<License> {
+  readonly format?: number;
+}
+
 export type Token = {
   readonly kind: 'verified';
   readonly payload: License;
 } | {
   readonly kind: 'corrupted';
-  readonly error: 'format' | 'verification';
+  readonly error: 'general' | 'verification' | 'decoding' | 'deserialization' | 'payload' | 'version';
 };
 
 const SPLITIER = '.';
+const FORMAT = 1;
 
-const FORMAT_ERROR: Token = { kind: 'corrupted', error: 'format' };
+const GENERAL_ERROR: Token = { kind: 'corrupted', error: 'general' };
 const VERIFICATION_ERROR: Token = { kind: 'corrupted', error: 'verification' };
+const DECODING_ERROR: Token = { kind: 'corrupted', error: 'decoding' };
+const DESERIALIZATION_ERROR: Token = { kind: 'corrupted', error: 'deserialization' };
+const PAYLOAD_ERROR: Token = { kind: 'corrupted', error: 'payload' };
+const VERSION_ERROR: Token = { kind: 'corrupted', error: 'version' };
 
 export function parseToken(encodedToken: string | undefined): Token {
   if (encodedToken === undefined) {
-    return FORMAT_ERROR;
+    return GENERAL_ERROR;
   }
 
   const parts = encodedToken.split(SPLITIER);
 
   if (parts.length !== 2 || parts[0].length === 0 || parts[1].length === 0) {
-    return FORMAT_ERROR;
+    return GENERAL_ERROR;
   }
-
-  const payload = JSON.parse(atob(parts[0]));
 
   if (!verify({ text: parts[0], signature: parts[1] })) {
     return VERIFICATION_ERROR;
   }
 
+  let decodedPayload = '';
+  try {
+    decodedPayload = atob(parts[0]);
+  } catch {
+    return DECODING_ERROR;
+  }
+
+  let payload: Payload = {};
+  try {
+    payload = JSON.parse(decodedPayload);
+  } catch {
+    return DESERIALIZATION_ERROR;
+  }
+
+  const { customerId, maxVersionAllowed, format } = payload;
+
+  if (customerId === undefined || maxVersionAllowed === undefined || format === undefined) {
+    return PAYLOAD_ERROR;
+  }
+
+  if (format !== FORMAT) {
+    return VERSION_ERROR;
+  }
+
   return {
     kind: 'verified',
     payload: {
-      customerId: payload.customerId,
-      maxVersionAllowed: payload.maxVersionAllowed,
+      customerId,
+      maxVersionAllowed,
     },
   };
 }
