bc1b2a708ce0d96e5aafa8ec56318df32e3a4f1d
marker dao ® <mark_nikolsky@outlook.com>
2023-08-23 15:18:14 +0400

    fix(tabPanel): Update TS

--- packages/devextreme/ts/dx.all.d.ts
+++ packages/devextreme/ts/dx.all.d.ts
@@ -25538,7 +25538,7 @@ declare module DevExpress.ui {
     /**
      * [descr:dxTabPanelOptions.iconPosition]
      */
-    iconPosition?: DevExpress.common.PositionLogical;
+    iconPosition?: DevExpress.common.PositionRelative;
     /**
      * [descr:dxTabPanelOptions.items]
      */
