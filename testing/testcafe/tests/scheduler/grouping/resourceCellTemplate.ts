4dc272ae3f7756cb5cb8ec2bd6df0c245408eb23
Mark Allen Ramirez <markallenramirez@gmail.com>
2023-08-24 17:46:02 +0800

    move test

---
+++ testing/testcafe/tests/scheduler/grouping/resourceCellTemplate.ts
@@ -0,0 +1,38 @@
+import { Selector } from 'testcafe';
+import createWidget from '../../../helpers/createWidget';
+import url from '../../../helpers/getPageUrl';
+
+fixture.disablePageReloads`ResourceCellTemplate`
+  .page(url(__dirname, '../../container.html'));
+
+test('resourceCellTemplate layout should be rendered right in the agenda view', async (t) => {
+  const groupHeader = Selector('.dx-scheduler-group-header-content');
+
+  await t.expect(groupHeader.textContent).eql('Custom resource text');
+}).before(async () => {
+  const currentDate = new Date(2017, 4, 25);
+  await createWidget('dxScheduler', {
+    dataSource: [{
+      text: 'appointment',
+      startDate: currentDate,
+      endDate: currentDate,
+      resource: 1,
+    }],
+    views: ['agenda'],
+    currentView: 'agenda',
+    currentDate,
+    resourceCellTemplate() {
+      return 'Custom resource text';
+    },
+    groups: ['resource'],
+    resources: [{
+      fieldExpr: 'resource',
+      dataSource: [{
+        text: 'Resource text',
+        id: 1,
+      }],
+      label: 'Resource',
+    }],
+    height: 600,
+  });
+});
