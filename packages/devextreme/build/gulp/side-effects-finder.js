abc059007601ca69014988bf1b671a031c6ced9d
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2023-08-28 18:00:22 +0400

    refactor

--- packages/devextreme/build/gulp/side-effects-finder.js
+++ packages/devextreme/build/gulp/side-effects-finder.js
@@ -4,74 +4,73 @@ const fs = require('fs');
 const path = require('path');
 
 class SideEffectFinder {
-    #checkingPathsStack = new Set();
-    #checkedPaths = new Map();
+    checkedPaths = new Map();
+    recursiveHandlingPaths = new Set();
 
     getModuleSideEffectFiles(moduleFilePath) {
         try {
-            const moduleSideEffectFiles = [...this.#findSideEffectsInModule(moduleFilePath)]
+            const moduleSideEffectFiles = [...this.findSideEffectsInModule(moduleFilePath)]
                 .map((importPath) => importPath.replace(/\\/g, '/'));
 
             return moduleSideEffectFiles;
-        } catch (e) {
+        } catch(e) {
             const message = (e instanceof Error) ? e.message : e;
             throw (`Exception while check side effects. in ${moduleFilePath} \nException: ` + message + '\n');
         }
     }
 
-    #findSideEffectsInModule(moduleFilePath) {
-        let foundPaths = this.#checkedPaths.get(moduleFilePath);
+    findSideEffectsInModule(moduleFilePath) {
+        let foundPaths = this.checkedPaths.get(moduleFilePath);
 
-        if (!foundPaths) {
+        if(!foundPaths) {
             const code = fs.readFileSync(moduleFilePath, 'utf8');
 
-            foundPaths = this.#findSideEffectsInImports(moduleFilePath, code);
+            foundPaths = this.findSideEffectsInImports(moduleFilePath, code);
         }
 
         foundPaths = foundPaths || new Set();
 
-        this.#checkedPaths.set(moduleFilePath, foundPaths);
+        this.checkedPaths.set(moduleFilePath, foundPaths);
 
         return foundPaths;
     }
 
-    #findSideEffectsInImports(modulePath, code) {
-        const hasRelativePathRegExp = /['"]\.?\.\/.+/;
+    findSideEffectsInImports(modulePath, code) {
         const relativePathRegExp = /['"]\.?\.\/.+['"]/;
         const isSideEffectImportRegExp = /^\s*import\s+['"]/ms;
 
         const imports = code.match(/^\s*import[^;]+;/mg) || [];
         let foundPaths = new Set();
 
-        imports.filter((str) => hasRelativePathRegExp.test(str))
+        imports.filter((str) => relativePathRegExp.test(str))
             .forEach((str) => {
                 let importPath = str.match(relativePathRegExp)[0].replace(/(^['"]|['"]$)/g, '');
 
-                importPath = path.join(path.dirname(modulePath), importPath)+'.js';
+                importPath = path.join(path.dirname(modulePath), importPath) + '.js';
 
                 if(!fs.existsSync(importPath)) {
-                    importPath = importPath.replace(/\.js$/, '/index.js')
+                    importPath = importPath.replace(/\.js$/, '/index.js');
                 }
 
-                if (isSideEffectImportRegExp.test(str)) {
+                if(isSideEffectImportRegExp.test(str)) {
                     foundPaths.add(importPath);
                 }
 
-                const isInLoop = this.#checkingPathsStack.has(importPath);
+                const isInLoop = this.recursiveHandlingPaths.has(importPath);
 
-                if (!isInLoop) {
-                    this.#checkingPathsStack.add(importPath);
+                if(!isInLoop) {
+                    this.recursiveHandlingPaths.add(importPath);
 
-                    foundPaths = new Set([...foundPaths, ...this.#findSideEffectsInModule(importPath)]);
+                    foundPaths = new Set([...foundPaths, ...this.findSideEffectsInModule(importPath)]);
 
-                    this.#checkingPathsStack.delete(importPath);
+                    this.recursiveHandlingPaths.delete(importPath);
                 }
             });
 
-        return foundPaths ;
+        return foundPaths;
     }
 }
 
 module.exports = {
     SideEffectFinder
-}
+};
