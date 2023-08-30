e9a6beda28cf94a6eb609a7a63beec3923d447dc
marker dao ® | Roman Ty <mark_nikolsky@outlook.com>
2023-08-30 20:02:53 +0400

    fix(d.ts): Update d.ts

--- packages/devextreme/ts/dx.all.d.ts
+++ packages/devextreme/ts/dx.all.d.ts
@@ -1657,7 +1657,6 @@ declare namespace DevExpress.common {
     | 'right bottom'
     | 'right top'
     | 'top';
-  export type PositionLogical = 'top' | 'end' | 'bottom' | 'start';
   /**
    * [descr:RangeRule]
    */
