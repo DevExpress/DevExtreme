34452a007ba2196028450349eac9a6763d1cb1f0
Georgy K <36447502+Lykoi18@users.noreply.github.com>
2023-08-30 16:25:10 +0400

    Fix package version in package.json from artifacts (#25475)

--- packages/devextreme/build/preinstall.js
+++ packages/devextreme/build/preinstall.js
@@ -4,18 +4,11 @@ const fs = require('fs');
 const path = require('path');
 
 const pkg = require('../package.json');
-const version = require('./gulp/version')(
-    pkg.version,
-    process.env.DEVEXTREME_DXBUILD_LABEL,
-    process.env.DEVEXTREME_DXBUILD_FLAVOR,
-    process.env.DEVEXTREME_DXBUILD_REVISION
-);
 
 const generateProdPackageFile = (sourcePkg) => {
     const packagePath = path.resolve('./artifacts/npm/devextreme/package.json');
 
     sourcePkg.name = 'devextreme';
-    sourcePkg.version = version.package;
     sourcePkg.bin = {
         'devextreme-bundler-init': 'bin/bundler-init.js',
         'devextreme-bundler': 'bin/bundler.js'
