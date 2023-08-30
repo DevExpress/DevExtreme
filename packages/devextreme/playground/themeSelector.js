18039cebe717db05c2868d1170e6a7bac11cb0d2
Andrew Makarov <andrewmakarov@inbox.ru>
2023-08-30 19:13:15 +0400

    Add theme list

--- packages/devextreme/playground/themeSelector.js
+++ packages/devextreme/playground/themeSelector.js
@@ -3,27 +3,72 @@
 
 const themeKey = 'currentThemeId';
 
-const themes = [
-    { id: 'light', name: 'light' },
-    { id: 'light.compact', name: 'light compact' },
-    { id: 'material.lime.dark.compact', name: 'material lime dark compact' },
-    { id: 'material.purple.light', name: 'material purple light' },
+const themeList = [
+    'light',
+    'light.compact',
+
+    'carmine.compact',
+    'carmine',
+
+    'contrast.compact',
+    'contrast',
+
+    'dark.compact',
+    'dark',
+
+    'darkmoon.compact',
+    'darkmoon',
+
+    'darkviolet.compact',
+    'darkviolet',
+
+    'fluent.blue.dark.compact',
+    'fluent.blue.dark',
+    'fluent.blue.light.compact',
+    'fluent.blue.light',
+
+    'greenmist.compact',
+    'greenmist',
+
+    'softblue.compact',
+    'softblue',
+
+    'material.blue.dark.compact',
+    'material.blue.dark',
+    'material.blue.light.compact',
+    'material.blue.light',
+
+    'material.lime.dark.compact',
+    'material.lime.dark',
+    'material.lime.light.compact',
+    'material.lime.light',
+
+    'material.orange.dark.compact',
+    'material.orange.dark',
+    'material.orange.light.compact',
+    'material.orange.light',
+
+    'material.purple.dark.compact',
+    'material.purple.dark',
+    'material.purple.light.compact',
+    'material.purple.light',
+
+    'material.teal.dark.compact',
+    'material.teal.dark',
+    'material.teal.light.compact',
+    'material.teal.light'
 ];
 
-$(function() {
-    const value = window.localStorage.getItem(themeKey) || themes[0].id;
-
+$(() => {
     const onValueChanged = (e) => {
         window.localStorage.setItem(themeKey, e.value);
         window.location.reload();
     };
 
     $('#theme-selector').dxSelectBox({
-        width: 200,
-        dataSource: themes,
-        valueExpr: 'id',
-        displayExpr: 'name',
-        value,
+        width: 300,
+        items: themeList,
+        value: window.localStorage.getItem(themeKey) || themeList[0].id,
         onValueChanged
     });
 });
