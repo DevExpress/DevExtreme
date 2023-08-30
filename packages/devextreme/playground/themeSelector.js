f7173c880e1141eb3d9cd00f8220e3d118d16e1e
Andrew Makarov <andrewmakarov@inbox.ru>
2023-08-30 17:16:01 +0400

    Implement theme selector

---
+++ packages/devextreme/playground/themeSelector.js
@@ -0,0 +1,29 @@
+/* global window */
+/* global $ */
+
+const themeKey = 'currentThemeId';
+
+const themes = [
+    { id: 'light', name: 'light' },
+    { id: 'light.compact', name: 'light compact' },
+    { id: 'material.lime.dark.compact', name: 'material lime dark compact' },
+    { id: 'material.purple.light', name: 'material purple light' },
+];
+
+$(function() {
+    const value = window.localStorage.getItem(themeKey) || themes[0].id;
+
+    const onValueChanged = (e) => {
+        window.localStorage.setItem(themeKey, e.value);
+        window.location.reload();
+    };
+
+    $('#theme-selector').dxSelectBox({
+        width: 200,
+        dataSource: themes,
+        valueExpr: 'id',
+        displayExpr: 'name',
+        value,
+        onValueChanged
+    });
+});
