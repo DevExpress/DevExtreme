dc2d19a7de60cb230023d9c13e862302c47a1cbd
marker dao ® <mark_nikolsky@outlook.com>
2023-08-30 17:51:56 +0400

    regenerate-all

--- packages/devextreme/ts/dx.all.d.ts
+++ packages/devextreme/ts/dx.all.d.ts
@@ -25536,10 +25536,6 @@ declare module DevExpress.ui {
      */
     iconPosition?: DevExpress.common.TabsIconPosition;
     /**
-     * [descr:dxTabPanelOptions.iconPosition]
-     */
-    iconPosition?: DevExpress.ui.dxTabs.IconPosition;
-    /**
      * [descr:dxTabPanelOptions.items]
      */
     items?: Array<TItem>;
