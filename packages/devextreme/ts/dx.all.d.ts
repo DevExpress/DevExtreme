7e141134a04d3942b9f94f602cc843cab229310b
marker dao ® | Roman Ty <mark_nikolsky@outlook.com>
2023-08-31 18:05:59 +0400

    feat(regenerate-all)

--- packages/devextreme/ts/dx.all.d.ts
+++ packages/devextreme/ts/dx.all.d.ts
@@ -25757,11 +25757,7 @@ declare module DevExpress.ui {
      */
     showNavButtons?: boolean;
     /**
-<<<<<<< HEAD
      * [descr:dxTabsOptions.stylingMode]
-=======
-     * [descr:dxTabsOptions.iconPosition]
->>>>>>> 78d2e71d65 (feat(d.ts): Update d.ts)
      */
     stylingMode?: DevExpress.common.TabsStylingMode;
   }
