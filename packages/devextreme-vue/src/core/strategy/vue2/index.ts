7f85fb241f2d7a36b12333e5e6006b4d09fb2224
dxvladislavvolkov <vladislav.volkov@devexpress.com>
2023-08-31 14:12:34 +0400

    Fix tests

--- packages/devextreme-vue/src/core/strategy/vue2/index.ts
+++ packages/devextreme-vue/src/core/strategy/vue2/index.ts
@@ -1,9 +1,9 @@
-export function createComponent() {
+export function createComponent(): any {
   throw new Error(
     'Since v23.2, devextreme-vue no longer supports Vue 2. See: https://supportcenter.devexpress.com/internal/ticket/details/T1186771',
   );
 }
 
-export function createConfigurationComponent() {}
+export function createConfigurationComponent(): any {}
 
-export function createExtensionComponent() {}
+export function createExtensionComponent(): any {}
