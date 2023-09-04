6dbf19ab7613edfaf780ae2a8ac07031b546744f
Andrew Makarov <andrewmakarov@inbox.ru>
2023-09-04 11:31:02 +0400

    Rename test file

--- testing/testcafe/tests/dataGrid/editing/editingEvents.ts
+++ testing/testcafe/tests/dataGrid/editing/editingEvents.ts
@@ -3,7 +3,7 @@ import createWidget from '../../../helpers/createWidget';
 import url from '../../../helpers/getPageUrl';
 import DataGrid from '../../../model/dataGrid';
 
-fixture`Test name`
+fixture`T1186997`
   .page(url(__dirname, '../../container.html'));
 
 // T1186997
@@ -22,8 +22,22 @@ fixture`Test name`
   onRowUpdating: ClientFunction((e) => {
     e.cancel = true;
   }),
+}, {
+  name: 't3',
+  expected: false,
+  onRowUpdating: ClientFunction((e) => {
+    e.cancel = new Promise((resolve) => {
+      resolve(false);
+    });
+  }),
+}, {
+  name: 't4',
+  expected: false,
+  onRowUpdating: ClientFunction((e) => {
+    e.cancel = false;
+  }),
 }].forEach(({ name, expected, onRowUpdating }) => {
-  test(`test5 ${name}`, async (t) => {
+  test(`test in case ${name}`, async (t) => {
     const dataGrid = new DataGrid('#container');
     const dataRow = dataGrid.getDataRow(0);
 
@@ -55,4 +69,42 @@ fixture`Test name`
       onRowUpdating,
     }));
   });
+});
+
+test('test', async (t) => {
+  const dataGrid = new DataGrid('#container');
+  const dataRow = dataGrid.getDataRow(0);
+
+  await t.click(dataRow.getDataCell(1).getLinkEdit());
+
+  await t
+    .typeText(dataRow.getDataCell(0).getEditor().element, 'test text')
+    .click(dataRow.getDataCell(1).getLinkSave());
+
+  await t.expect(dataRow.getDataCell(1).getLinkSave().exists).eql(true);
+}).before(async () => {
+  await createWidget('dxDataGrid', () => ({
+    dataSource: [{
+      ID: 1,
+      FirstName: 'John',
+    },
+    ],
+    columns: [{
+      dataField: 'FirstName',
+      caption: 'Firs tName',
+    }],
+    height: 400,
+    editing: {
+      mode: 'row',
+      allowUpdating: true,
+      // allowDeleting: true,
+      // allowAdding: true,
+    },
+    onRowUpdating: ClientFunction((e) => {
+      e.cancel = true;
+      // e.cancel = new Promise((resolve) => {
+      //   resolve(true);
+      // });
+    }),
+  }));
 });
