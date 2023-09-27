ba3ab3a36d7c495071a625c00248f3b08f9e3523
alexlavrov <alex.s.lavrov@gmail.com>
2023-09-27 14:29:32 +0400

    Migrate to ts, rename npm script, make refactoring based on code review. Add TODO comment

--- build/pack-all.ts
+++ build/pack-all.ts
@@ -1,5 +1,5 @@
-const path = require('path');
-const sh = require("shelljs");
+import sh from "shelljs";
+import path from "node:path";
 
 const MONOREPO_ROOT = path.join(__dirname, '..');
 const INTERNAL_TOOLS_ARTIFACTS = path.join(MONOREPO_ROOT, 'artifacts', 'internal-tools');
@@ -16,32 +16,39 @@ sh.exec(`git clone -b 23_2 --depth 1 --config core.longpaths=true https://github
 
 sh.pushd(DOCUMENTATION_DIR);
 sh.exec('npm i');
-sh.exec(`npm run update-topics -- --declarations-path ${path.join(INTERNAL_TOOLS_ARTIFACTS, 'Declarations.json')} --artifacts ${INTERNAL_TOOLS_ARTIFACTS}`);
+sh.exec(`npm run update-topics -- --artifacts ${INTERNAL_TOOLS_ARTIFACTS}`);
 sh.popd();
 
 sh.exec('npm run devextreme:inject-descriptions');
 
-// Build devextreme, devextreme-dist
 sh.exec('npm run build-dist -w devextreme-main');
 
-// npm version ${version} -ws --workspaces-update=false
-// npm i devextreme@${version} --save-exact -w devextreme-angular -w devextreme-react -w devextreme-vue
-
 const DEVEXTREME_NPM_DIR = path.join(MONOREPO_ROOT, 'packages/devextreme/artifacts/npm');
-['devextreme', 'devextreme-dist'].forEach((pkg) => {
-   sh.pushd(path.join(DEVEXTREME_NPM_DIR, pkg))
-   sh.exec('npm pack');
-   sh.cp('*.tgz', OUTPUT_DIR);
-   sh.popd();
-});
+
+/*
+TODO: We might need this when running on farm to bump non-semver versions for all packages.
+sh.exec(`npm version ${version} -ws --workspaces-update=false`);
+sh.sed('-i', /"devextreme": ".*"/, `"devextreme": "${version}"`, path.join(MONOREPO_ROOT, 'packages', '**', 'package.json'));
+sh.exec('npm i');
+*/
+
+sh.pushd(path.join(DEVEXTREME_NPM_DIR, 'devextreme'))
+sh.exec('npm pack');
+sh.cp('*.tgz', OUTPUT_DIR);
+sh.popd();
+
+sh.pushd(path.join(DEVEXTREME_NPM_DIR, 'devextreme-dist'))
+sh.exec('npm pack');
+sh.cp('*.tgz', OUTPUT_DIR);
+sh.popd();
 
 sh.pushd(path.join(MONOREPO_ROOT, 'packages', 'devextreme-themebuilder'))
 sh.exec('npm run build && npm run pack');
 sh.cp('dist/*.tgz', OUTPUT_DIR);
 sh.popd();
 
-// Build and pack wrappers
 sh.exec('npm run pack --ws --if-present');
-['devextreme-angular', 'devextreme-react', 'devextreme-vue'].forEach((pkg) => {
-    sh.cp(path.join(MONOREPO_ROOT, 'packages', pkg, 'npm', '**', '*.tgz'), OUTPUT_DIR);
-});
+
+sh.cp(path.join(MONOREPO_ROOT, 'packages', 'devextreme-angular', 'npm', 'dist', '*.tgz'), OUTPUT_DIR);
+sh.cp(path.join(MONOREPO_ROOT, 'packages', 'devextreme-react', 'npm', '*.tgz'), OUTPUT_DIR);
+sh.cp(path.join(MONOREPO_ROOT, 'packages', 'devextreme-vue', 'npm', '*.tgz'), OUTPUT_DIR);
