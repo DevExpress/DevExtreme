00915f2f3612c34501e049ce7f6381ccca6f13ab
marker dao ® <mark_nikolsky@outlook.com>
2023-09-27 15:27:07 +0400

    TextEditor: Fill placeholder attribute with placeholder option value

--- packages/devextreme/js/ui/text_box/ui.text_editor.base.js
+++ packages/devextreme/js/ui/text_box/ui.text_editor.base.js
@@ -326,17 +326,22 @@ const TextEditorBase = Editor.inherit({
             .css('minHeight', this.option('height') ? '0' : '');
     },
 
-    _getDefaultAttributes: function() {
-        const defaultAttributes = {
-            autocomplete: 'off'
-        };
-
+    _getPlaceholderAttr() {
         const { ios, mac } = devices.real();
-        if(ios || mac) {
-            // WA to fix vAlign (T898735)
-            // https://bugs.webkit.org/show_bug.cgi?id=142968
-            defaultAttributes.placeholder = ' ';
-        }
+        const { placeholder } = this.option();
+
+        // WA to fix vAlign (T898735)
+        // https://bugs.webkit.org/show_bug.cgi?id=142968
+        const value = placeholder || ((ios || mac) ? ' ' : null);
+
+        return value;
+    },
+
+    _getDefaultAttributes() {
+        const defaultAttributes = {
+            autocomplete: 'off',
+            placeholder: this._getPlaceholderAttr(),
+        };
 
         return defaultAttributes;
     },
@@ -484,14 +489,17 @@ const TextEditorBase = Editor.inherit({
     },
 
     _setFieldAria(force) {
+        const { 'aria-label': ariaLabel } = this.option('inputAttr');
+
         const labelId = this._label.getId();
         const placeholderId = this._$placeholder?.attr('id');
 
-        const value = [labelId, placeholderId].filter(Boolean).join(' ');
+        const value = ariaLabel ? undefined : [labelId, placeholderId].filter(Boolean).join(' ');
 
         if(value || force) {
             const aria = {
                 'labelledby': value || undefined,
+                label: ariaLabel,
             };
             this.setAria(aria, this._getFieldElement());
         }
@@ -801,6 +809,7 @@ const TextEditorBase = Editor.inherit({
             case 'placeholder':
                 this._renderPlaceholder();
                 this._setFieldAria(true);
+                this._input().attr({ placeholder: this._getPlaceholderAttr() });
                 break;
             case 'label':
                 this._label.updateText(value);
