b548bc83c455c303ce7fcc8b566cc9e18c765f28
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2023-09-28 16:35:11 +0400

    Add ESM builds to devextreme-react

--- packages/devextreme-react/gulpfile.js
+++ packages/devextreme-react/gulpfile.js
@@ -4,9 +4,9 @@ const gulp = require('gulp');
 const shell = require('gulp-shell');
 const header = require('gulp-header');
 const ts = require('gulp-typescript');
-const config = require("./build.config");
-const path = require("path");
-const pkg = require("./package.json");
+const config = require('./build.config');
+const path = require('path');
+const pkg = require('./package.json');
 const generateReactComponents = require('devextreme-internal-tools').generateReactComponents;
 
 const GENERATE = 'generate';
@@ -18,9 +18,9 @@ const NPM_LICENSE = 'npm.license';
 const NPM_BUILD_WITH_HEADERS = 'npm.license-headers';
 const NPM_README = 'npm.readme';
 const NPM_BUILD = 'npm.build';
-const NPM_BUILD_ESM = "npm.build-esm";
-const NPM_BUILD_CJS = "npm.build-cjs";
-const NPM_PREPARE_MODULES = "npm.prepare-modules";
+const NPM_BUILD_ESM = 'npm.build-esm';
+const NPM_BUILD_CJS = 'npm.build-cjs';
+const NPM_PREPARE_MODULES = 'npm.prepare-modules';
 const NPM_PACK = 'npm.pack';
 
 gulp.task(CLEAN, (c) =>
@@ -75,8 +75,8 @@ gulp.task(
   gulp.series(() =>
     gulp
       .src([config.src, `!${config.testSrc}`])
-      .pipe(ts("tsconfig.esm.json"))
-      .pipe(gulp.dest(config.npm.dist + "/esm"))
+      .pipe(ts('tsconfig.esm.json'))
+      .pipe(gulp.dest(config.npm.dist + '/esm'))
   )
 );
 
@@ -85,27 +85,27 @@ gulp.task(
   gulp.series(() =>
     gulp
       .src([config.src, `!${config.testSrc}`])
-      .pipe(ts("tsconfig.json"))
-      .pipe(gulp.dest(config.npm.dist + "/cjs"))
+      .pipe(ts('tsconfig.json'))
+      .pipe(gulp.dest(config.npm.dist + '/cjs'))
   )
 );
 
 gulp.task(NPM_PREPARE_MODULES, (done) => {
   const packParamsForFolders = [
-    ["common"],
-    ["core", ["template", "config"]],
-    ["common/data"],
+    ['common'],
+    ['core', ['template', 'config']],
+    ['common/data'],
   ];
   const modulesImportsFromIndex = fs.readFileSync(
-    config.npm.dist + "esm/index.js",
-    "utf8"
+    config.npm.dist + 'esm/index.js',
+    'utf8'
   );
   const modulesPaths = modulesImportsFromIndex.matchAll(/from "\.\/([^;]+)";/g);
   const packParamsForModules = [...modulesPaths].map(([, modulePath]) => {
     const [, , moduleFilePath, moduleFileName] =
       modulePath.match(/((.*)\/)?([^/]+$)/);
 
-    return ["", [moduleFileName], moduleFilePath];
+    return ['', [moduleFileName], moduleFilePath];
   });
 
   [...packParamsForFolders, ...packParamsForModules].forEach(
@@ -198,13 +198,13 @@ function generatePackageJsonFile(folder, moduleFileName, filePath = folder) {
     const moduleFilePath = (filePath ? filePath + '/' : '') + (moduleName || 'index');
     const relativePath = path.relative(
       absoluteModulePath,
-      path.join(__dirname, config.npm.dist, "esm", moduleFilePath + ".js")
+      path.join(__dirname, config.npm.dist, 'esm', moduleFilePath + '.js')
     );
 
     const relativeBase = '../'.repeat(relativePath.split('..').length - 1);
 
     fs.writeFileSync(
-      path.join(absoluteModulePath, "package.json"),
+      path.join(absoluteModulePath, 'package.json'),
       JSON.stringify(
         {
           sideEffects: false,
