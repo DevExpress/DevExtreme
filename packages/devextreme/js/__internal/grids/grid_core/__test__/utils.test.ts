e44a817d933f3f6c468f4249c00387b8898f40b9
Andrew Makarov <andrewmakarov@inbox.ru>
2023-09-28 19:14:19 +0400

    Fix naming in tests

--- packages/devextreme/js/__internal/grids/grid_core/__test__/utils.test.ts
+++ packages/devextreme/js/__internal/grids/grid_core/__test__/utils.test.ts
@@ -2,28 +2,30 @@
 import { popupUtils } from '../m_utils';
 
 describe('utils', () => {
-  test('done button should be return valid obj', () => {
-    const result = popupUtils.createDoneButton({});
+  describe('popup dialogs', () => {
+    test('done button should be return valid config. All properties are required', () => {
+      const result = popupUtils.createDoneButton({});
 
-    const expected = {
-      location: 'after',
-      shortcut: 'done',
-      toolbar: 'bottom',
-      options: {},
-    };
+      const expected = {
+        location: 'after',
+        shortcut: 'done',
+        toolbar: 'bottom',
+        options: {},
+      };
 
-    expect(result).toEqual(expected);
-  });
-  test('cancel button should be return valid obj', () => {
-    const result = popupUtils.createCancelButton({});
+      expect(result).toEqual(expected);
+    });
+    test('cancel button should be return valid config. All properties are required', () => {
+      const result = popupUtils.createCancelButton({});
 
-    const expected = {
-      location: 'after',
-      shortcut: 'cancel',
-      toolbar: 'bottom',
-      options: {},
-    };
+      const expected = {
+        location: 'after',
+        shortcut: 'cancel',
+        toolbar: 'bottom',
+        options: {},
+      };
 
-    expect(result).toEqual(expected);
+      expect(result).toEqual(expected);
+    });
   });
 });
