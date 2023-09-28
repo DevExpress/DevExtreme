7198f650634839bf86828e4f3f09746e5732a635
Eldar Iusupzhanov <ferrarijed@gmail.com>
2023-09-28 15:45:02 +0800

    small refactoring

--- js/__internal/grids/tree_list/data_controller/m_data_controller.ts
+++ js/__internal/grids/tree_list/data_controller/m_data_controller.ts
@@ -39,10 +39,6 @@ export const DataController = (dataControllerModule.controllers as any).data.inh
     },
 
     _isItemEquals(item1, item2) {
-      if (!this.callBase.apply(this, arguments)) {
-        return false;
-      }
-
       if (item1.isSelected !== item2.isSelected) {
         return false;
       }
@@ -55,7 +51,7 @@ export const DataController = (dataControllerModule.controllers as any).data.inh
         return false;
       }
 
-      return true;
+      return this.callBase.apply(this, arguments);
     },
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
