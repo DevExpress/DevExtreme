392c3561401e1765047b55bb4b79666a537e4167
Andrew Makarov <andrewmakarov@inbox.ru>
2023-09-28 18:35:50 +0400

    Refactor test

--- packages/devextreme/js/__internal/grids/grid_core/__test__/utils.test.ts
+++ packages/devextreme/js/__internal/grids/grid_core/__test__/utils.test.ts
@@ -2,9 +2,28 @@
 import { popupUtils } from '../m_utils';
 
 describe('utils', () => {
-  test('popup buttons', () => {
+  test('done button should be return valid obj', () => {
     const result = popupUtils.createDoneButton({});
 
-    expect(result).toEqual({});
+    const expected = {
+      location: 'after',
+      shortcut: 'done',
+      toolbar: 'bottom',
+      options: {},
+    };
+
+    expect(result).toEqual(expected);
+  });
+  test('cancel button should be return valid obj', () => {
+    const result = popupUtils.createCancelButton({});
+
+    const expected = {
+      location: 'after',
+      shortcut: 'cancel',
+      toolbar: 'bottom',
+      options: {},
+    };
+
+    expect(result).toEqual(expected);
   });
 });
