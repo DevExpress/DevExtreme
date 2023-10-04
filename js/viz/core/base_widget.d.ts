8c05a156324b6425acb95426a6782bab2313b0d0
alexander.polosatov <alexander.polosatov@devexpress.com>
2023-10-04 18:16:11 +0400

    fix type in docid

--- js/viz/core/base_widget.d.ts
+++ js/viz/core/base_widget.d.ts
@@ -245,7 +245,7 @@ export interface BaseWidgetLoadingIndicator {
     /**
      * @docid BaseWidgetOptions.loadingIndicator.font
      * @default '#767676' &prop(color)
-     * @type object
+     * @type Font
      * @public
      */
     font?: CommonFont;
@@ -313,7 +313,7 @@ export interface BaseWidgetTitle {
      * @default 28 &prop(size)
      * @default 200 &prop(weight)
      * @default "'Segoe UI Light', 'Helvetica Neue Light', 'Segoe UI', 'Helvetica Neue', 'Trebuchet MS', Verdana, sans-serif" &prop(family)
-     * @type object
+     * @type Font
      * @public
      */
     font?: CommonFont;
@@ -367,7 +367,7 @@ export interface BaseWidgetTitle {
        * @default 16 &prop(size)
        * @default 200 &prop(weight)
        * @default "'Segoe UI Light', 'Helvetica Neue Light', 'Segoe UI', 'Helvetica Neue', 'Trebuchet MS', Verdana, sans-serif" &prop(family)
-       * @type object
+       * @type Font
        */
       font?: CommonFont;
       /**
@@ -483,7 +483,7 @@ export interface BaseWidgetTooltip {
     /**
      * @docid BaseWidgetOptions.tooltip.font
      * @default '#232323' &prop(color)
-     * @type object
+     * @type Font
      * @public
      */
     font?: CommonFont;
@@ -692,7 +692,7 @@ export interface BaseWidgetAnnotationConfig {
     /**
      * @docid
      * @default '#333333' &prop(color)
-     * @type object
+     * @type Font
      * @public
      */
     font?: CommonFont;
