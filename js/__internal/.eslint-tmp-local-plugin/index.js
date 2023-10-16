a1e91f9d24e27d05ddf11ec116f068986a74a78a
Roman Semenov <roman.semenov@devexpress.com>
2023-10-16 16:34:19 +0400

    Lint - remove tmp package (#25815)

--- js/__internal/.eslint-tmp-local-plugin/index.js
+++
@@ -1,9 +0,0 @@
-// TODO Vinogradov: Move this plugin to this package:
-//   https://github.com/DevExpress/eslint-config-devextreme
-const { rules } = require('@typescript-eslint/eslint-plugin');
-
-module.exports = {
-    rules: {
-        'no-restricted-imports': rules['no-restricted-imports'],
-    },
-};
