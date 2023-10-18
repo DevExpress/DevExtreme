e45e91242749429ded6962feaf293fe4481e9f3a
alexlavrov <alex.s.lavrov@gmail.com>
2023-10-18 18:05:02 +0400

    Include workspace root in script. Input version in workflow

--- build/bump-version.ts
+++ build/bump-version.ts
@@ -7,7 +7,7 @@ const MONOREPO_ROOT = path.join(__dirname, '..');
 const packagesPath = path.join(MONOREPO_ROOT, 'packages', '**', 'package.json');
 const playgroundsPath = path.join(MONOREPO_ROOT, 'playgrounds', '**', 'package.json');
 
-sh.exec(`npm version ${version} -ws --workspaces-update=false`);
+sh.exec(`npm version ${version} -ws --include-workspace-root --workspaces-update=false`);
 
 const setStrictVersion = /.*-(build|alpha|beta).*/.test(version);
 
