6bca1b9fc3f8af6e002fa241ed8e7ba6f9e6c512
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2023-10-20 21:58:02 +0400

    Safe check changed option when its iterable value becomes scalar + test

---
+++ packages/devextreme-angular/tests/src/ui/calendar.spec.ts
@@ -0,0 +1,54 @@
+/* tslint:disable:component-selector */
+
+import {
+    Component,
+    ViewChild
+} from '@angular/core';
+
+import { TestBed } from '@angular/core/testing';
+import { BrowserTransferStateModule } from '@angular/platform-browser';
+
+import {
+  DxCalendarModule,
+  DxCalendarComponent,
+} from "devextreme-angular";
+
+@Component({
+    selector: 'test-container-component',
+    template: '',
+})
+class TestContainerComponent {
+    @ViewChild(DxCalendarComponent) calendar: DxCalendarComponent;
+
+    value: any = [];
+}
+
+describe('DxCalendar', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule(
+            {
+                declarations: [TestContainerComponent],
+                imports: [DxCalendarModule, BrowserTransferStateModule]
+            });
+    });
+
+    // spec
+    it('should accept iterable and non-iterable values without exception', () => {
+        TestBed.overrideComponent(TestContainerComponent, {
+            set: {
+                template: `<dx-calendar [value]="value" selectionMode="multiple"></dx-calendar>`
+            }
+        });
+
+        const fixture = TestBed.createComponent(TestContainerComponent);
+
+        fixture.detectChanges();
+
+        const calendar = fixture.componentInstance.calendar;
+
+        calendar.selectionMode = 'single';
+        calendar.instance.option('value', new Date());
+
+        fixture.detectChanges();
+    });
+});
