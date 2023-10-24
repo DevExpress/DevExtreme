309ab790615d9706af06803ce4bc3c6344f37c94
Alex Lavrov <36633600+alexslavr@users.noreply.github.com>
2023-10-24 20:01:36 +0400

    Prepare all necessary JS artifacts for build task (#25842)
    Co-authored-by: Lykoi18 <aiden.ghost666@gmail.com>

---
+++ build/update-version.ts
@@ -0,0 +1,19 @@
+import sh from 'shelljs';
+import path from 'node:path';
+
+const version = process.argv[2];
+
+if (version == null) {
+    console.error(`Usage: 'npm run all:update-version -- $version' (XX.X.X)`);
+    process.exit(1);
+}
+
+const MONOREPO_ROOT = path.join(__dirname, '..');
+const packagesPath = path.join(MONOREPO_ROOT, 'packages', '**', 'package.json');
+const playgroundsPath = path.join(MONOREPO_ROOT, 'playgrounds', '**', 'package.json');
+
+sh.exec(`npm version ${version} -ws --include-workspace-root --git-tag-version=false --workspaces-update=false`);
+
+sh.sed('-i', /"devextreme(-angular|-react|-vue)?": ".*"/, `"devextreme$1": "~${version}"`, [packagesPath, playgroundsPath]);
+
+sh.exec('npm i');
