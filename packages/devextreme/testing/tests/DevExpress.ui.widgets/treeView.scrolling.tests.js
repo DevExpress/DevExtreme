5e81f38c2399e1ba66c755fc85ddd544e7c31731
AlexanderMoiseev <moiseevalexandr1@gmail.com>
2023-10-26 17:17:15 +0400

    test treeview width changed

--- packages/devextreme/testing/tests/DevExpress.ui.widgets/treeView.scrolling.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.widgets/treeView.scrolling.tests.js
@@ -35,7 +35,7 @@ QUnit.module('scrollToItem', {
             displayExpr: 'id',
             scrollDirection: scrollDirection,
             height: 150,
-            width: 170,
+            width: 175,
             animationEnabled: false,
             items: items,
             rtlEnabled: rtlEnabled,
