c37932c267f970936b03ce12e686fe6f3f19edcd
dxvladslavvolkov <vladislav.volkov@devexpress.com>
2023-10-27 14:08:38 +0400

    Add module to ignore coverage section

--- packages/devextreme/jest.config.js
+++ packages/devextreme/jest.config.js
@@ -28,6 +28,7 @@ module.exports = {
         './js/renovation/ui/scheduler/appointment_edit_form/layout.tsx', // NOTE: covering with TestCafe
         './js/renovation/ui/scheduler/workspaces/utils.ts',
         './js/renovation/utils/get_computed_style.ts',
+        './js/renovation/utils/get_element_offset.ts',
         './js/renovation/utils/dom.ts',
         './js/renovation/utils/render_template.ts', // TODO: this is temporary file
     ],
