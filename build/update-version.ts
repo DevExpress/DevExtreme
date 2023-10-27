85d4d7ba7437b11ae25f0905ca38098608b932c1
Georgy K <36447502+Lykoi18@users.noreply.github.com>
2023-10-27 20:16:20 +0400

    Fix packing "devextreme-themebuilder" package (#25908)

--- build/update-version.ts
+++ build/update-version.ts
@@ -8,11 +8,13 @@ if (version == null) {
     process.exit(1);
 }
 
+sh.set('-e');
+
 const MONOREPO_ROOT = path.join(__dirname, '..');
 const packagesPath = path.join(MONOREPO_ROOT, 'packages', '**', 'package.json');
 const playgroundsPath = path.join(MONOREPO_ROOT, 'playgrounds', '**', 'package.json');
 
-sh.exec(`npm version ${version} -ws --include-workspace-root --git-tag-version=false --workspaces-update=false`);
+sh.exec(`npm version ${version} -ws --allow-same-version --include-workspace-root --git-tag-version=false --workspaces-update=false`);
 
 sh.sed('-i', /"devextreme(-angular|-react|-vue)?": ".*"/, `"devextreme$1": "~${version}"`, [packagesPath, playgroundsPath]);
 
