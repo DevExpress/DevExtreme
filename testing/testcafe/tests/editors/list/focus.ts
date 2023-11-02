67e748b499c2da6ea3d6da796788cb9f76b9a2a6
Alexander Bulychev <git@bulychev.pro>
2023-11-02 11:03:07 +0400

    List: select next item after deletion (#25864)
    Co-authored-by: Alexander Bulychev <git@bulychev.net>

--- testing/testcafe/tests/editors/list/focus.ts
+++ testing/testcafe/tests/editors/list/focus.ts
@@ -7,10 +7,11 @@ import createWidget from '../../../helpers/createWidget';
 fixture`List`
   .page(url(__dirname, '../../container.html'));
 
-const createList = (selectionMode) => createWidget('dxList', {
+const createList = (selectionMode, allowItemDeleting = false) => createWidget('dxList', {
   items: ['item1', 'item2', 'item3'],
   showSelectionControls: true,
   selectionMode,
+  allowItemDeleting,
 });
 
 test('Should apply styles on selectAll checkbox after tab button press', async (t) => {
@@ -82,4 +83,64 @@ test('Should apply styles on selectAll checkbox after enter button press on it',
       .expect(compareResults.isValid())
       .ok(compareResults.errorMessages());
   }).before(async () => createList(selectionMode));
+});
+
+test('Should select next item after delete by keyboard', async (t) => {
+  const list = new List('#container');
+  const firstItem = list.getItem(0);
+
+  await t
+    .expect(list.getVisibleItems().count).eql(3)
+    .click(firstItem.element)
+    .pressKey('delete');
+
+  const item = list.getItem(0);
+
+  await t.expect(item.isFocused)
+    .ok();
+  await t.expect(item.text)
+    .eql('item2');
+  await t
+    .expect(list.getItems().count).eql(2);
+}).before(async () => createList('none', true));
+
+test('Should select previous item after delete last item', async (t) => {
+  const list = new List('#container');
+  const lastItem = list.getItem(2);
+
+  await t
+    .expect(list.getVisibleItems().count).eql(3)
+    .click(lastItem.element)
+    .pressKey('delete');
+
+  const item = list.getItem(1);
+
+  await t.expect(item.isFocused)
+    .ok();
+  await t.expect(item.text)
+    .eql('item2');
+  await t
+    .expect(list.getItems().count).eql(2);
+}).before(async () => createList('none', true));
+
+[[2, 0], [1, 2]].forEach(([selectItemIdx, deleteItemIdx]) => {
+  test(`Should not change selection after delete another (not selected) item (${selectItemIdx}, ${selectItemIdx})`, async (t) => {
+    const list = new List('#container');
+    const itemToSelect = list.getItem(selectItemIdx);
+    const itemToDelete = list.getItem(deleteItemIdx);
+
+    await t
+      .expect(list.getVisibleItems().count).eql(3)
+      .click(itemToSelect.element)
+      .click(itemToDelete.element.find('.dx-button'));
+
+    const item = list.getItem(deleteItemIdx > selectItemIdx ? selectItemIdx : selectItemIdx - 1);
+
+    await t.expect(item.isFocused)
+      .ok();
+    await t.expect(item.text)
+      .eql(`item${selectItemIdx + 1}`);
+    await t
+      .expect(list.getItems().count).eql(2);
+  }).before(async () => createList('none', true));
 });
