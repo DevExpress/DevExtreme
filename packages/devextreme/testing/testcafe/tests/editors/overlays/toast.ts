d988ba5cf06e4da1ea0288d7c83103c32790f250
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2023-11-06 22:43:27 +0400

    fix remarks after review

--- packages/devextreme/testing/testcafe/tests/editors/overlays/toast.ts
+++ packages/devextreme/testing/testcafe/tests/editors/overlays/toast.ts
@@ -1,14 +1,14 @@
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
-import { ClientFunction } from 'testcafe';
+import { ClientFunction, Selector } from 'testcafe';
 import url from '../../../helpers/getPageUrl';
-import { testScreenshot } from '../../../helpers/themeUtils';
-import { insertStylesheetRulesToPage, setAttribute } from '../../../helpers/domUtils';
+import { getThemeName, testScreenshot } from '../../../helpers/themeUtils';
+import { insertStylesheetRulesToPage, setClassAttribute } from '../../../helpers/domUtils';
 
-fixture`Toast`
+fixture.disablePageReloads`Toast`
   .page(url(__dirname, '../../container.html'));
 
 const types = ['info', 'warning', 'error', 'success'];
-const STACK_TONTAINER_SELECTOR = '.dx-toast-stack';
+const STACK_CONTAINER_SELECTOR = '.dx-toast-stack';
 
 const showToast = ClientFunction(
   (type) => {
@@ -39,10 +39,10 @@ const hideAllToasts = ClientFunction(() => {
 test('Toasts', async (t) => {
   const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-  await testScreenshot(t, takeScreenshot, 'Toasts.png', { element: STACK_TONTAINER_SELECTOR, shouldTestInCompact: true });
+  await testScreenshot(t, takeScreenshot, 'Toasts.png', { element: STACK_CONTAINER_SELECTOR, shouldTestInCompact: true });
   await testScreenshot(t, takeScreenshot, 'Toasts.png', {
-    element: STACK_TONTAINER_SELECTOR,
-    theme: process.env.theme?.replace('.light', '.dark'),
+    element: STACK_CONTAINER_SELECTOR,
+    theme: `${getThemeName()}.dark`,
   });
 
   await t
@@ -51,8 +51,12 @@ test('Toasts', async (t) => {
 }).before(async () => {
   await Promise.all(types.map((type) => showToast(type)));
 
-  await insertStylesheetRulesToPage(`${STACK_TONTAINER_SELECTOR} { padding: 20px; }`);
-  await setAttribute('body', 'class', `dx-theme-${process.env.theme}-typography`);
+  await insertStylesheetRulesToPage(`${STACK_CONTAINER_SELECTOR} { padding: 20px; }`);
+  await setClassAttribute(Selector(STACK_CONTAINER_SELECTOR), `dx-theme-${getThemeName()}-typography`);
 }).after(async () => {
   await hideAllToasts();
+
+  await ClientFunction(() => {
+    $(STACK_CONTAINER_SELECTOR).remove();
+  })();
 });
