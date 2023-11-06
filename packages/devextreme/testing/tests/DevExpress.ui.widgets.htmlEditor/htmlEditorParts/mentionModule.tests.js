a4d4862edc96ea34ba84fd4cbc8bd8f655c8be87
Alexander Bulychev <git@bulychev.net>
2023-11-06 17:16:48 +0400

    fix tests

--- packages/devextreme/testing/tests/DevExpress.ui.widgets.htmlEditor/htmlEditorParts/mentionModule.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.widgets.htmlEditor/htmlEditorParts/mentionModule.tests.js
@@ -142,7 +142,9 @@ QUnit.module('Mention format', () => {
         assert.strictEqual(element.dataset.marker, '@', 'correct marker');
         assert.strictEqual(element.dataset.mentionValue, 'John Smith', 'correct value');
         assert.strictEqual(element.dataset.id, 'JohnSm', 'correct id');
-        assert.strictEqual(element.innerText, '@John Smith', 'correct inner text');
+
+        new MentionFormat({}, element);
+        assert.strictEqual(element.children[0].innerText, '@John Smith', 'correct inner text');
     });
 
     test('Get data from element', function(assert) {
@@ -161,32 +163,53 @@ QUnit.module('Mention format', () => {
         };
 
         const element = MentionFormat.create(data);
-        assert.strictEqual(element.innerText, '#John Smith', 'correct inner text');
+
+        new MentionFormat({}, element);
+        assert.strictEqual(element.children[0].innerText, '#John Smith', 'correct inner text');
     });
 
     test('Change default content renderer', function(assert) {
-        const data = {
+        const expectedData = {
             value: 'John Smith',
             marker: '@',
-            id: 'JohnSm',
+            id: 'JohnSm'
+        };
+        const data = {
+            ...expectedData,
             keyInTemplateStorage: 'my_key_in_storage'
         };
 
         MentionFormat.addTemplate({ marker: '@', editorKey: 'my_key_in_storage' }, {
             render: ({ container, model: mentionData }) => {
                 container.innerText = 'test';
-                assert.deepEqual(mentionData, data);
+                assert.deepEqual(mentionData, expectedData);
             }
         });
 
         let element = MentionFormat.create(data);
 
-        assert.strictEqual(element.innerText, 'test');
+        new MentionFormat({}, element);
+        assert.strictEqual(element.children[0].innerText, 'test');
 
         MentionFormat.removeTemplate({ marker: '@', editorKey: 'my_key_in_storage' });
         element = MentionFormat.create(data);
+        new MentionFormat({}, element);
+        assert.strictEqual(element.children[0].innerText, '@John Smith');
+    });
 
-        assert.strictEqual(element.innerText, '@John Smith');
+    test('async template renders correctly in React strict mode (T1187274)', function(assert) {
+        let spy;
+        try {
+            const node = MentionFormat.create({ marker: '@', value: 'val', id: 'id' });
+            spy = sinon.spy(MentionFormat.prototype, 'renderContent');
+
+            new MentionFormat({}, node);
+
+            assert.ok(spy.calledOnce);
+            assert.strictEqual(spy.getCall(0).args[0], node.children[0]);
+        } finally {
+            spy && spy.restore();
+        }
     });
 });
 
@@ -441,8 +464,10 @@ QUnit.module('Mentions module', moduleConfig, () => {
             return;
         }
 
+        const totalItems = 60;
+        const pageSize = 50;
         const items = [];
-        for(let i = 0; i < 60; i++) {
+        for(let i = 0; i < totalItems; i++) {
             items.push(i);
         }
 
@@ -454,7 +479,7 @@ QUnit.module('Mentions module', moduleConfig, () => {
         this.options.mentions = [{
             dataSource: {
                 store: items,
-                pageSize: 50,
+                pageSize,
                 paginate: true
             },
         }];
@@ -468,15 +493,15 @@ QUnit.module('Mentions module', moduleConfig, () => {
         this.clock.tick(10);
 
         let $items = $(`.${SUGGESTION_LIST_CLASS} .${LIST_ITEM_CLASS}`);
-        assert.strictEqual($items.length, 50);
+        assert.strictEqual($items.length, pageSize);
 
         this.$element.trigger($.Event('keydown', { key: 'ArrowUp', which: KEY_CODES.ARROW_UP }));
 
         $items = $(`.${SUGGESTION_LIST_CLASS} .${LIST_ITEM_CLASS}`);
-        const isLastItemOnPageFocused = $items.eq(49).hasClass(FOCUSED_STATE_CLASS);
+        const isLastItemOnPageFocused = $items.eq(pageSize - 1).hasClass(FOCUSED_STATE_CLASS);
 
         assert.ok(isLastItemOnPageFocused);
-        assert.strictEqual($items.length, 60, 'next page has loaded');
+        assert.strictEqual($items.length, totalItems, 'next page has loaded');
     });
 
     test('trigger \'arrow up\' should focus previous list item', function(assert) {
