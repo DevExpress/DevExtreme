e0b9fd45296927add0e3ee35f0e7ce8917129a73
Mikhail Preyskurantov <mikhail.preyskurantov@devexpress.com>
2023-11-09 12:17:04 +0200

    Publish Query type (#25970)

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -6320,7 +6320,6 @@ declare module DevExpress.data {
   export function query(url: string, queryOptions: any): Query;
   /**
    * [descr:Query]
-   * @deprecated Attention! This type is for internal purposes only. If you used it previously, please submit a ticket to our {@link https://supportcenter.devexpress.com/ticket/create Support Center}. We will check if there is an alternative solution.
    */
   export interface Query {
     /**
