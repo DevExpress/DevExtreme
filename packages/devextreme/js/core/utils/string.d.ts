12fa10d803b19be4a98cc493a4e0e830cb0e74b2
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2023-11-23 18:55:17 +0400

    Add missing declarations

--- packages/devextreme/js/core/utils/string.d.ts
+++ packages/devextreme/js/core/utils/string.d.ts
@@ -1,1 +1,13 @@
 export { xorTransform } from '../../__internal/core/license/byte_utils';
+
+export function encodeHtml(str: string): string;
+
+export function splitQuad(raw: any): any;
+
+export function quadToObject(raw: any): Record<'top' | 'left' | 'right' | 'bottom', number>;
+
+export function format(template: string, ...values: any[]): string;
+
+export function replaceAll(text: string, searchToken: string, replacementToken: string): string;
+
+export function isEmpty(text: string): boolean;
