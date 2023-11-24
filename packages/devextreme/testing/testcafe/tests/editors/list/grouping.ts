c9322cdaf2fdf41fbfdd19eedafff8ae60d47565
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2023-11-24 19:53:14 +0400

    update scenario

--- packages/devextreme/testing/testcafe/tests/editors/list/grouping.ts
+++ packages/devextreme/testing/testcafe/tests/editors/list/grouping.ts
@@ -29,11 +29,16 @@ test('Grouped list appearance', async (t) => {
   height: 500,
   dataSource: [{
     key: 'group_1',
-    items: [{ text: 'item_1_1', showChevron: true, badge: 'item_1_1' }, { text: 'item_1_2', badge: 'item_1_2' }, 'item_1_3'],
+    items: ['item_1_1', 'item_1_2', 'item_1_3'],
     expanded: false,
   }, {
     key: 'group_2',
-    items: [{ text: 'item_2_1', disabled: true }, { text: 'item_2_2', icon: 'home' }, 'item_2_3'],
+    items: [
+      { text: 'item_2_1', disabled: true },
+      { text: 'item_2_2', icon: 'home' },
+      { text: 'item_2_3', showChevron: true, badge: 'item_2_3' },
+      { text: 'item_2_4', badge: 'item_2_4' },
+      'item_2_5'],
   }, {
     key: 'group_3',
     items: ['item_3_1', 'item_3_2', 'item_3_3'],
