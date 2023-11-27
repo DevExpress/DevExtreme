2700ecfcd06c10bbf908dac0623329a76c02d3a0
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2023-11-27 16:10:26 +0200

    Fix lint

--- packages/devextreme/js/__internal/core/license/license_validation_internal.ts
+++ packages/devextreme/js/__internal/core/license/license_validation_internal.ts
@@ -7,7 +7,7 @@ export function parseLicenseKey(encodedKey: string | undefined): Token {}
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function validateLicense(licenseKey: string, version?: string): void {}
 
-// @ts-expect-error
+// @ts-expect-error - only for internal usage
 export function peekValidationPerformed(): boolean {}
 
 export function setLicenseCheckSkipCondition(): void {}
