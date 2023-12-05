e08b2a5278fca11725355ae2559dda771e652461
Alyar <Alyar666@users.noreply.github.com>
2023-12-06 00:56:43 +0400

    DataGrid: Fix keyboard event bubbling when pressing escape key (T1202731) (#26127)
    Co-authored-by: Alyar <>

--- packages/devextreme/js/__internal/grids/grid_core/keyboard_navigation/m_keyboard_navigation.ts
+++ packages/devextreme/js/__internal/grids/grid_core/keyboard_navigation/m_keyboard_navigation.ts
@@ -472,8 +472,7 @@ export class KeyboardNavigationController extends modules.ViewController {
           break;
 
         case 'escape':
-          this._escapeKeyHandler(e, isEditing);
-          isHandled = true;
+          isHandled = this._escapeKeyHandler(e, isEditing);
           break;
 
         case 'F':
@@ -1060,7 +1059,7 @@ export class KeyboardNavigationController extends modules.ViewController {
     }
   }
 
-  _escapeKeyHandler(eventArgs, isEditing) {
+  _escapeKeyHandler(eventArgs, isEditing): boolean {
     const $cell = this._getCellElementFromTarget(
       eventArgs.originalEvent.target,
     );
@@ -1081,7 +1080,11 @@ export class KeyboardNavigationController extends modules.ViewController {
         }
       }
       eventArgs.originalEvent.preventDefault();
+
+      return true;
     }
+
+    return false;
   }
 
   _ctrlFKeyHandler(eventArgs) {
