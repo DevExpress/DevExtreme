01422f89817e6f8e8d17d500d21658545f3689e7
marker dao ® <mark_nikolsky@outlook.com>
2023-12-11 15:16:45 +0400

    TabPanel: Fix tabpanel container height (T1203780)

--- packages/devextreme/testing/tests/DevExpress.ui.widgets/tabPanel.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.widgets/tabPanel.tests.js
@@ -42,6 +42,7 @@ const DISABLED_FOCUSED_TAB_CLASS = 'dx-disabled-focused-tab';
 const FOCUSED_DISABLED_NEXT_TAB_CLASS = 'dx-focused-disabled-next-tab';
 const FOCUSED_DISABLED_PREV_TAB_CLASS = 'dx-focused-disabled-prev-tab';
 const FOCUS_STATE_CLASS = 'dx-state-focused';
+const TABPANEL_CONTAINER_CLASS = 'dx-tabpanel-container';
 
 const TABPANEL_TABS_POSITION_CLASS = {
     top: 'dx-tabpanel-tabs-position-top',
@@ -156,6 +157,32 @@ QUnit.module('rendering', {
                 ? iconRect.right - textRect.right - iconRect.width
                 : textRect.left - iconRect.left - iconRect.width;
             assert.strictEqual(horizontalMargin, 8, `correct horizontal alignment of icon ${JSON.stringify(iconRect)} and text ${JSON.stringify(textRect)}`);
+        });
+    });
+
+    ['top', 'right', 'left', 'bottom'].forEach(tabsPosition => {
+        QUnit.test(`TabPanel container has correct height when tabsPosition=${tabsPosition}`, function(assert) {
+            const items = [
+                {
+                    title: 1,
+                    text: 'Mariya Elizabeth Thomas Grace Sophia Rose Alexandra Victoria Isabella Natalie Olivia Emily Jennifer Margaret Stephanie',
+                },
+            ];
+
+            const $tabPanel = $('<div>').appendTo('#qunit-fixture').dxTabPanel({
+                items,
+                width: 100,
+                height: 100,
+                tabsPosition,
+            });
+
+            const $tabPanelContainer = $tabPanel.find(`.${TABPANEL_CONTAINER_CLASS}`);
+
+            if(tabsPosition === 'top' || tabsPosition === 'bottom') {
+                assert.strictEqual($tabPanelContainer.get(0).clientHeight, 62);
+            } else {
+                assert.strictEqual($tabPanelContainer.get(0).clientWidth, 72);
+            }
         });
     });
 });
@@ -681,7 +708,6 @@ QUnit.module('keyboard navigation', {
     }
 });
 
-
 QUnit.module('Disabled items', {
     beforeEach() {
         const items = [
@@ -745,7 +771,6 @@ QUnit.module('Disabled items', {
         assert.strictEqual($(this.$element).hasClass(DISABLED_FOCUSED_TAB_CLASS), false, 'class not set');
     });
 });
-
 
 QUnit.module('aria accessibility', () => {
     QUnit.test('active tab should have aria-controls attribute pointing to active multiview item', function(assert) {
