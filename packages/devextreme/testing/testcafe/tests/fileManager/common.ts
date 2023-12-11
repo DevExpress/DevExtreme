c815c8c684947c8f34cca312151fd6da7ce4f974
Alexander Bulychev <git@bulychev.net>
2023-12-11 14:14:28 +0400

    Review fixes

--- packages/devextreme/testing/testcafe/tests/fileManager/common.ts
+++ packages/devextreme/testing/testcafe/tests/fileManager/common.ts
@@ -1,31 +1,24 @@
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import { Selector } from 'testcafe';
-import { isFluent, isMaterial } from '../../helpers/themeUtils';
+import { testScreenshot } from '../../helpers/themeUtils';
 import url from '../../helpers/getPageUrl';
 import createWidget from '../../helpers/createWidget';
 
-fixture`FileManager`
+fixture.disablePageReloads`FileManager`
   .page(url(__dirname, '../container.html'));
 
 test('Custom DropDown width for Material and Fluent themes', async (t) => {
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
   const viewModeButton = Selector('.dx-filemanager-toolbar-viewmode-item');
 
   await t
     .click(viewModeButton);
 
-  const popupContainer = Selector('.dx-filemanager-view-switcher-popup .dx-overlay-content');
-  let expectedWidth;
-
-  if (isFluent()) {
-    expectedWidth = 40;
-  } else if (isMaterial()) {
-    expectedWidth = 36;
-  } else {
-    expectedWidth = 34;
-  }
+  await testScreenshot(t, takeScreenshot, 'drop down width.png', { element: '#container' });
 
   await t
-    .expect(popupContainer.clientWidth)
-    .eql(expectedWidth);
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
 }).before(async () => createWidget('dxFileManager', {
   name: 'fileManager',
   fileSystemProvider: [],
