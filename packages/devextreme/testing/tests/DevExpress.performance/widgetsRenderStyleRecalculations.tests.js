de0131085b611844d43f46e4eab3d0861ecbbbd6
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2023-12-12 13:08:14 +0400

    TextEditor: rendering process takes much time

--- packages/devextreme/testing/tests/DevExpress.performance/widgetsRenderStyleRecalculations.tests.js
+++ packages/devextreme/testing/tests/DevExpress.performance/widgetsRenderStyleRecalculations.tests.js
@@ -6,168 +6,28 @@ require('generic_light.css!');
 const $ = require('jquery');
 const componentName = 'dxSwitch';
 
-require('ui/switch');
+const Switch = require('ui/switch');
 
 QUnit.testStart(function() {
     $('#qunit-fixture').addClass('qunit-fixture-visible');
 });
 
-QUnit.performanceTest(componentName + ' performance test', function(assert) {
+QUnit.performanceTest('dxSwitch should not force relayout on creation', function(assert) {
     const measureFunction = function() {
-        $('<div>').appendTo('#qunit-fixture')[componentName]({});
+        $('<div>').appendTo('#qunit-fixture').dxSwitch({});
     };
 
     assert.measureStyleRecalculation(measureFunction, 1);
 });
 
-QUnit.performanceTest(componentName + ' performance test', function(assert) {
+// TODO: all editors components
+// TODO: all label modes + all styling modes
+// TODO: buttons containers + before / after
+QUnit.performanceTest('dxTextBox should not force relayout on creation', function(assert) {
     const measureFunction = function() {
-        $('<div>').appendTo('#qunit-fixture')[componentName]({});
+        $('<div>').appendTo('#qunit-fixture').dxTextBox({});
     };
 
     assert.measureStyleRecalculation(measureFunction, 1);
 });
 
-QUnit.performanceTest(componentName + ' performance test', function(assert) {
-    const measureFunction = function() {
-        $('<div>').appendTo('#qunit-fixture')[componentName]({});
-    };
-
-    assert.measureStyleRecalculation(measureFunction, 1);
-});
-
-QUnit.performanceTest(componentName + ' performance test', function(assert) {
-    const measureFunction = function() {
-        $('<div>').appendTo('#qunit-fixture')[componentName]({});
-    };
-
-    assert.measureStyleRecalculation(measureFunction, 1);
-});
-
-QUnit.performanceTest(componentName + ' performance test', function(assert) {
-    const measureFunction = function() {
-        $('<div>').appendTo('#qunit-fixture')[componentName]({});
-    };
-
-    assert.measureStyleRecalculation(measureFunction, 1);
-});
-
-QUnit.performanceTest(componentName + ' performance test', function(assert) {
-    const measureFunction = function() {
-        $('<div>').appendTo('#qunit-fixture')[componentName]({});
-    };
-
-    assert.measureStyleRecalculation(measureFunction, 1);
-});
-
-QUnit.performanceTest(componentName + ' performance test', function(assert) {
-    const measureFunction = function() {
-        $('<div>').appendTo('#qunit-fixture')[componentName]({});
-    };
-
-    assert.measureStyleRecalculation(measureFunction, 1);
-});
-
-QUnit.performanceTest(componentName + ' performance test', function(assert) {
-    const measureFunction = function() {
-        $('<div>').appendTo('#qunit-fixture')[componentName]({});
-    };
-
-    assert.measureStyleRecalculation(measureFunction, 1);
-});
-
-QUnit.performanceTest(componentName + ' performance test', function(assert) {
-    const measureFunction = function() {
-        $('<div>').appendTo('#qunit-fixture')[componentName]({});
-    };
-
-    assert.measureStyleRecalculation(measureFunction, 1);
-});
-
-QUnit.performanceTest(componentName + ' performance test', function(assert) {
-    const measureFunction = function() {
-        $('<div>').appendTo('#qunit-fixture')[componentName]({});
-    };
-
-    assert.measureStyleRecalculation(measureFunction, 1);
-});
-
-QUnit.performanceTest(componentName + ' performance test', function(assert) {
-    const measureFunction = function() {
-        $('<div>').appendTo('#qunit-fixture')[componentName]({});
-    };
-
-    assert.measureStyleRecalculation(measureFunction, 1);
-});
-
-QUnit.performanceTest(componentName + ' performance test', function(assert) {
-    const measureFunction = function() {
-        $('<div>').appendTo('#qunit-fixture')[componentName]({});
-    };
-
-    assert.measureStyleRecalculation(measureFunction, 1);
-});
-
-QUnit.performanceTest(componentName + ' performance test', function(assert) {
-    const measureFunction = function() {
-        $('<div>').appendTo('#qunit-fixture')[componentName]({});
-    };
-
-    assert.measureStyleRecalculation(measureFunction, 1);
-});
-
-QUnit.performanceTest(componentName + ' performance test', function(assert) {
-    const measureFunction = function() {
-        $('<div>').appendTo('#qunit-fixture')[componentName]({});
-    };
-
-    assert.measureStyleRecalculation(measureFunction, 1);
-});
-
-QUnit.performanceTest(componentName + ' performance test', function(assert) {
-    const measureFunction = function() {
-        $('<div>').appendTo('#qunit-fixture')[componentName]({});
-    };
-
-    assert.measureStyleRecalculation(measureFunction, 1);
-});
-
-QUnit.performanceTest(componentName + ' performance test', function(assert) {
-    const measureFunction = function() {
-        $('<div>').appendTo('#qunit-fixture')[componentName]({});
-    };
-
-    assert.measureStyleRecalculation(measureFunction, 1);
-});
-
-QUnit.performanceTest(componentName + ' performance test', function(assert) {
-    const measureFunction = function() {
-        $('<div>').appendTo('#qunit-fixture')[componentName]({});
-    };
-
-    assert.measureStyleRecalculation(measureFunction, 1);
-});
-
-QUnit.performanceTest(componentName + ' performance test', function(assert) {
-    const measureFunction = function() {
-        $('<div>').appendTo('#qunit-fixture')[componentName]({});
-    };
-
-    assert.measureStyleRecalculation(measureFunction, 1);
-});
-
-QUnit.performanceTest(componentName + ' performance test', function(assert) {
-    const measureFunction = function() {
-        $('<div>').appendTo('#qunit-fixture')[componentName]({});
-    };
-
-    assert.measureStyleRecalculation(measureFunction, 1);
-});
-
-QUnit.performanceTest(componentName + ' performance test', function(assert) {
-    const measureFunction = function() {
-        $('<div>').appendTo('#qunit-fixture')[componentName]({});
-    };
-
-    assert.measureStyleRecalculation(measureFunction, 1);
-});
