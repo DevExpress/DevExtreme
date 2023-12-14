869f944dae441a4eb16ddc5802cfc8c7c152dfb7
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2023-12-14 13:10:02 +0400

    Update widgetsRenderStyleRecalculations.tests.js

--- packages/devextreme/testing/tests/DevExpress.performance/widgetsRenderStyleRecalculations.tests.js
+++ packages/devextreme/testing/tests/DevExpress.performance/widgetsRenderStyleRecalculations.tests.js
@@ -26,52 +26,176 @@ QUnit.testStart(function() {
     $('#qunit-fixture').addClass('qunit-fixture-visible');
 });
 
-const LABEL_MODES = ['hidden', 'static', 'floating', 'outside'];
 const components = [
-    { name: 'dxAutocomplete', expectedRecalculations: 2 },
-    { name: 'dxCalendar', expectedRecalculations: 1 },
-    { name: 'dxCheckBox', expectedRecalculations: 1 },
-    { name: 'dxColorBox', expectedRecalculations: 2 },
-    { name: 'dxDateBox', expectedRecalculations: 4 },
-    { name: 'dxDropDownBox', expectedRecalculations: 2 },
-    { name: 'dxLookup', expectedRecalculations: 2 },
-    { name: 'dxNumberBox', expectedRecalculations: 1 },
-    { name: 'dxRadioGroup', expectedRecalculations: 1 },
-    { name: 'dxRangeSlider', expectedRecalculations: 7 },
-    { name: 'dxSelectBox', expectedRecalculations: 2 },
-    { name: 'dxSlider', expectedRecalculations: 3 },
-    { name: 'dxSwitch', expectedRecalculations: 1 },
-    { name: 'dxTagBox', expectedRecalculations: 2 },
-    { name: 'dxTextArea', expectedRecalculations: 1 },
-    { name: 'dxTextBox', expectedRecalculations: 1 },
-    { name: 'dxDateRangeBox', expectedRecalculations: 1 },
+    { name: 'dxAutocomplete', config: {}, expectedRecalculations: 2 },
+    { name: 'dxAutocomplete', config: { labelMode: 'hidden' }, expectedRecalculations: 2 },
+    { name: 'dxAutocomplete', config: { labelMode: 'static' }, expectedRecalculations: 2 },
+    { name: 'dxAutocomplete', config: { labelMode: 'floating' }, expectedRecalculations: 2 },
+    { name: 'dxAutocomplete', config: { labelMode: 'outside' }, expectedRecalculations: 2 },
+    { name: 'dxAutocomplete', config: { label: 'Label', labelMode: 'hidden' }, expectedRecalculations: 2 },
+    { name: 'dxAutocomplete', config: { label: 'Label', labelMode: 'static' }, expectedRecalculations: 2 },
+    { name: 'dxAutocomplete', config: { label: 'Label', labelMode: 'floating' }, expectedRecalculations: 2 },
+    { name: 'dxAutocomplete', config: { label: 'Label', labelMode: 'outside' }, expectedRecalculations: 2 },
+    { name: 'dxAutocomplete', config: { label: 'Label', labelMode: 'hidden', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+    { name: 'dxAutocomplete', config: { label: 'Label', labelMode: 'static', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+    { name: 'dxAutocomplete', config: { label: 'Label', labelMode: 'floating', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+    { name: 'dxAutocomplete', config: { label: 'Label', labelMode: 'outside', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+
+    { name: 'dxCalendar', config: {}, expectedRecalculations: 1 },
+    { name: 'dxCheckBox', config: {}, expectedRecalculations: 1 },
+
+    { name: 'dxColorBox', config: {}, expectedRecalculations: 2 },
+    { name: 'dxColorBox', config: { labelMode: 'hidden' }, expectedRecalculations: 2 },
+    { name: 'dxColorBox', config: { labelMode: 'static' }, expectedRecalculations: 2 },
+    { name: 'dxColorBox', config: { labelMode: 'floating' }, expectedRecalculations: 2 },
+    { name: 'dxColorBox', config: { labelMode: 'outside' }, expectedRecalculations: 2 },
+    { name: 'dxColorBox', config: { label: 'Label', labelMode: 'hidden' }, expectedRecalculations: 2 },
+    { name: 'dxColorBox', config: { label: 'Label', labelMode: 'static' }, expectedRecalculations: 2 },
+    { name: 'dxColorBox', config: { label: 'Label', labelMode: 'floating' }, expectedRecalculations: 2 },
+    { name: 'dxColorBox', config: { label: 'Label', labelMode: 'outside' }, expectedRecalculations: 2 },
+    { name: 'dxColorBox', config: { label: 'Label', labelMode: 'hidden', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+    { name: 'dxColorBox', config: { label: 'Label', labelMode: 'static', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+    { name: 'dxColorBox', config: { label: 'Label', labelMode: 'floating', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+    { name: 'dxColorBox', config: { label: 'Label', labelMode: 'outside', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+
+    { name: 'dxDateBox', config: {}, expectedRecalculations: 4 },
+    { name: 'dxDateBox', config: { labelMode: 'hidden' }, expectedRecalculations: 4 },
+    { name: 'dxDateBox', config: { labelMode: 'static' }, expectedRecalculations: 5 },
+    { name: 'dxDateBox', config: { labelMode: 'floating' }, expectedRecalculations: 4 },
+    { name: 'dxDateBox', config: { labelMode: 'outside' }, expectedRecalculations: 4 },
+    { name: 'dxDateBox', config: { label: 'Label', labelMode: 'hidden' }, expectedRecalculations: 4 },
+    { name: 'dxDateBox', config: { label: 'Label', labelMode: 'static' }, expectedRecalculations: 4 },
+    { name: 'dxDateBox', config: { label: 'Label', labelMode: 'floating' }, expectedRecalculations: 4 },
+    { name: 'dxDateBox', config: { label: 'Label', labelMode: 'outside' }, expectedRecalculations: 4 },
+    { name: 'dxDateBox', config: { label: 'Label', labelMode: 'hidden', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 4 },
+    { name: 'dxDateBox', config: { label: 'Label', labelMode: 'static', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 4 },
+    { name: 'dxDateBox', config: { label: 'Label', labelMode: 'floating', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 4 },
+    { name: 'dxDateBox', config: { label: 'Label', labelMode: 'outside', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 4 },
+
+    { name: 'dxDropDownBox', config: {}, expectedRecalculations: 2 },
+    { name: 'dxDropDownBox', config: { labelMode: 'hidden' }, expectedRecalculations: 2 },
+    { name: 'dxDropDownBox', config: { labelMode: 'static' }, expectedRecalculations: 2 },
+    { name: 'dxDropDownBox', config: { labelMode: 'floating' }, expectedRecalculations: 2 },
+    { name: 'dxDropDownBox', config: { labelMode: 'outside' }, expectedRecalculations: 2 },
+    { name: 'dxDropDownBox', config: { label: 'Label', labelMode: 'hidden' }, expectedRecalculations: 2 },
+    { name: 'dxDropDownBox', config: { label: 'Label', labelMode: 'static' }, expectedRecalculations: 2 },
+    { name: 'dxDropDownBox', config: { label: 'Label', labelMode: 'floating' }, expectedRecalculations: 2 },
+    { name: 'dxDropDownBox', config: { label: 'Label', labelMode: 'outside' }, expectedRecalculations: 2 },
+    { name: 'dxDropDownBox', config: { label: 'Label', labelMode: 'hidden', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+    { name: 'dxDropDownBox', config: { label: 'Label', labelMode: 'static', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+    { name: 'dxDropDownBox', config: { label: 'Label', labelMode: 'floating', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+    { name: 'dxDropDownBox', config: { label: 'Label', labelMode: 'outside', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+
+    { name: 'dxLookup', config: {}, expectedRecalculations: 2 },
+    { name: 'dxLookup', config: { labelMode: 'hidden' }, expectedRecalculations: 2 },
+    { name: 'dxLookup', config: { labelMode: 'static' }, expectedRecalculations: 2 },
+    { name: 'dxLookup', config: { labelMode: 'floating' }, expectedRecalculations: 2 },
+    { name: 'dxLookup', config: { labelMode: 'outside' }, expectedRecalculations: 2 },
+    { name: 'dxLookup', config: { label: 'Label', labelMode: 'hidden' }, expectedRecalculations: 2 },
+    { name: 'dxLookup', config: { label: 'Label', labelMode: 'static' }, expectedRecalculations: 2 },
+    { name: 'dxLookup', config: { label: 'Label', labelMode: 'floating' }, expectedRecalculations: 2 },
+    { name: 'dxLookup', config: { label: 'Label', labelMode: 'outside' }, expectedRecalculations: 2 },
+
+    { name: 'dxNumberBox', config: {}, expectedRecalculations: 1 },
+    { name: 'dxNumberBox', config: { labelMode: 'hidden' }, expectedRecalculations: 2 },
+    { name: 'dxNumberBox', config: { labelMode: 'static' }, expectedRecalculations: 2 },
+    { name: 'dxNumberBox', config: { labelMode: 'floating' }, expectedRecalculations: 2 },
+    { name: 'dxNumberBox', config: { labelMode: 'outside' }, expectedRecalculations: 2 },
+    { name: 'dxNumberBox', config: { label: 'Label', labelMode: 'hidden' }, expectedRecalculations: 2 },
+    { name: 'dxNumberBox', config: { label: 'Label', labelMode: 'static' }, expectedRecalculations: 2 },
+    { name: 'dxNumberBox', config: { label: 'Label', labelMode: 'floating' }, expectedRecalculations: 2 },
+    { name: 'dxNumberBox', config: { label: 'Label', labelMode: 'outside' }, expectedRecalculations: 2 },
+    { name: 'dxNumberBox', config: { label: 'Label', labelMode: 'hidden', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+    { name: 'dxNumberBox', config: { label: 'Label', labelMode: 'static', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+    { name: 'dxNumberBox', config: { label: 'Label', labelMode: 'floating', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+    { name: 'dxNumberBox', config: { label: 'Label', labelMode: 'outside', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+
+    { name: 'dxRadioGroup', config: {}, expectedRecalculations: 1 },
+    { name: 'dxSlider', config: {}, expectedRecalculations: 3 },
+    { name: 'dxRangeSlider', config: {}, expectedRecalculations: 7 },
+    { name: 'dxSwitch', config: {}, expectedRecalculations: 1 },
+
+    { name: 'dxSelectBox', config: {}, expectedRecalculations: 2 },
+    { name: 'dxSelectBox', config: { labelMode: 'hidden' }, expectedRecalculations: 2 },
+    { name: 'dxSelectBox', config: { labelMode: 'static' }, expectedRecalculations: 2 },
+    { name: 'dxSelectBox', config: { labelMode: 'floating' }, expectedRecalculations: 2 },
+    { name: 'dxSelectBox', config: { labelMode: 'outside' }, expectedRecalculations: 2 },
+    { name: 'dxSelectBox', config: { label: 'Label', labelMode: 'hidden' }, expectedRecalculations: 2 },
+    { name: 'dxSelectBox', config: { label: 'Label', labelMode: 'static' }, expectedRecalculations: 2 },
+    { name: 'dxSelectBox', config: { label: 'Label', labelMode: 'floating' }, expectedRecalculations: 2 },
+    { name: 'dxSelectBox', config: { label: 'Label', labelMode: 'outside' }, expectedRecalculations: 2 },
+    { name: 'dxSelectBox', config: { label: 'Label', labelMode: 'hidden', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+    { name: 'dxSelectBox', config: { label: 'Label', labelMode: 'static', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+    { name: 'dxSelectBox', config: { label: 'Label', labelMode: 'floating', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+    { name: 'dxSelectBox', config: { label: 'Label', labelMode: 'outside', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+
+    { name: 'dxTagBox', config: {}, expectedRecalculations: 2 },
+    { name: 'dxTagBox', config: { labelMode: 'hidden' }, expectedRecalculations: 2 },
+    { name: 'dxTagBox', config: { labelMode: 'static' }, expectedRecalculations: 2 },
+    { name: 'dxTagBox', config: { labelMode: 'floating' }, expectedRecalculations: 2 },
+    { name: 'dxTagBox', config: { labelMode: 'outside' }, expectedRecalculations: 2 },
+    { name: 'dxTagBox', config: { label: 'Label', labelMode: 'hidden' }, expectedRecalculations: 2 },
+    { name: 'dxTagBox', config: { label: 'Label', labelMode: 'static' }, expectedRecalculations: 2 },
+    { name: 'dxTagBox', config: { label: 'Label', labelMode: 'floating' }, expectedRecalculations: 2 },
+    { name: 'dxTagBox', config: { label: 'Label', labelMode: 'outside' }, expectedRecalculations: 2 },
+    { name: 'dxTagBox', config: { label: 'Label', labelMode: 'hidden', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+    { name: 'dxTagBox', config: { label: 'Label', labelMode: 'static', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+    { name: 'dxTagBox', config: { label: 'Label', labelMode: 'floating', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+    { name: 'dxTagBox', config: { label: 'Label', labelMode: 'outside', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 2 },
+
+    { name: 'dxTextArea', config: {}, expectedRecalculations: 1 },
+    { name: 'dxTextArea', config: { labelMode: 'hidden' }, expectedRecalculations: 1 },
+    { name: 'dxTextArea', config: { labelMode: 'static' }, expectedRecalculations: 1 },
+    { name: 'dxTextArea', config: { labelMode: 'floating' }, expectedRecalculations: 1 },
+    { name: 'dxTextArea', config: { labelMode: 'outside' }, expectedRecalculations: 1 },
+    { name: 'dxTextArea', config: { label: 'Label', labelMode: 'hidden' }, expectedRecalculations: 1 },
+    { name: 'dxTextArea', config: { label: 'Label', labelMode: 'static' }, expectedRecalculations: 1 },
+    { name: 'dxTextArea', config: { label: 'Label', labelMode: 'floating' }, expectedRecalculations: 1 },
+    { name: 'dxTextArea', config: { label: 'Label', labelMode: 'outside' }, expectedRecalculations: 1 },
+
+    { name: 'dxTextBox', config: {}, expectedRecalculations: 1 },
+    { name: 'dxTextBox', config: { mode: 'search' }, expectedRecalculations: 1 },
+    { name: 'dxTextBox', config: { labelMode: 'hidden' }, expectedRecalculations: 1 },
+    { name: 'dxTextBox', config: { labelMode: 'static' }, expectedRecalculations: 1 },
+    { name: 'dxTextBox', config: { labelMode: 'floating' }, expectedRecalculations: 1 },
+    { name: 'dxTextBox', config: { labelMode: 'outside' }, expectedRecalculations: 1 },
+    { name: 'dxTextBox', config: { label: 'Label', labelMode: 'hidden' }, expectedRecalculations: 1 },
+    { name: 'dxTextBox', config: { label: 'Label', labelMode: 'static' }, expectedRecalculations: 1 },
+    { name: 'dxTextBox', config: { label: 'Label', labelMode: 'floating' }, expectedRecalculations: 1 },
+    { name: 'dxTextBox', config: { label: 'Label', labelMode: 'outside' }, expectedRecalculations: 1 },
+    { name: 'dxTextBox', config: { mode: 'search', label: 'Label', labelMode: 'hidden' }, expectedRecalculations: 1 },
+    { name: 'dxTextBox', config: { mode: 'search', label: 'Label', labelMode: 'static' }, expectedRecalculations: 1 },
+    { name: 'dxTextBox', config: { mode: 'search', label: 'Label', labelMode: 'floating' }, expectedRecalculations: 1 },
+    { name: 'dxTextBox', config: { mode: 'search', label: 'Label', labelMode: 'outside' }, expectedRecalculations: 1 },
+    { name: 'dxTextBox', config: { label: 'Label', labelMode: 'hidden', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 1 },
+    { name: 'dxTextBox', config: { label: 'Label', labelMode: 'static', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 1 },
+    { name: 'dxTextBox', config: { label: 'Label', labelMode: 'floating', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 1 },
+    { name: 'dxTextBox', config: { label: 'Label', labelMode: 'outside', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 1 },
+
+    { name: 'dxDateRangeBox', config: {}, expectedRecalculations: 11 },
+    { name: 'dxDateRangeBox', config: { labelMode: 'hidden' }, expectedRecalculations: 11 },
+    { name: 'dxDateRangeBox', config: { labelMode: 'static' }, expectedRecalculations: 11 },
+    { name: 'dxDateRangeBox', config: { labelMode: 'floating' }, expectedRecalculations: 11 },
+    { name: 'dxDateRangeBox', config: { labelMode: 'outside' }, expectedRecalculations: 11 },
+    { name: 'dxDateRangeBox', config: { label: 'Label', labelMode: 'hidden' }, expectedRecalculations: 11 },
+    { name: 'dxDateRangeBox', config: { label: 'Label', labelMode: 'static' }, expectedRecalculations: 11 },
+    { name: 'dxDateRangeBox', config: { label: 'Label', labelMode: 'floating' }, expectedRecalculations: 11 },
+    { name: 'dxDateRangeBox', config: { label: 'Label', labelMode: 'outside' }, expectedRecalculations: 11 },
+    { name: 'dxDateRangeBox', config: { label: 'Label', labelMode: 'hidden', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 11 },
+    { name: 'dxDateRangeBox', config: { label: 'Label', labelMode: 'static', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 11 },
+    { name: 'dxDateRangeBox', config: { label: 'Label', labelMode: 'floating', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 11 },
+    { name: 'dxDateRangeBox', config: { label: 'Label', labelMode: 'outside', buttons: [{ name: 'custom', location: 'before' }] }, expectedRecalculations: 11 },
 ];
 
-components.forEach((component) => {
-    QUnit.performanceTest(`${component.name} relayouts on creation`, function(assert) {
+components.forEach(({ name, config, expectedRecalculations }) => {
+    QUnit.performanceTest(`${name} relayouts on creation, config: ${JSON.stringify(config)}`, function(assert) {
         const measureFunction = function() {
             const $element = $('<div>');
             $element.appendTo('#qunit-fixture');
 
-            $element[component.name]({});
+            $element[name]({ config });
         };
 
-        assert.measureStyleRecalculation(measureFunction, component.expectedRecalculations);
-    });
-
-    LABEL_MODES.forEach((labelMode) => {
-        QUnit.performanceTest(`${component.name} relayouts on creation with label when labelMode is ${labelMode}`, function(assert) {
-            const measureFunction = function() {
-                const $element = $('<div>');
-                $element.appendTo('#qunit-fixture');
-
-                $element[component.name]({
-                    labelMode,
-                    label: 'Label',
-                });
-            };
-
-            assert.measureStyleRecalculation(measureFunction, component.expectedRecalculations);
-        });
+        assert.measureStyleRecalculation(measureFunction, expectedRecalculations);
     });
 });
