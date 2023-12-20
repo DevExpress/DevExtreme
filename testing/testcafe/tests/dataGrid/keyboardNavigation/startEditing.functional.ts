49d0db8088d67d6b06994c8345b8b7c84c146ff8
Andrey Dolzhikov <eylau-andrew@mail.ru>
2023-12-20 09:48:23 +0300

    DataGrid briefly shows and then removes the 'minus' sign if you press the minus key to start editing a cell with a numeric value (T1201166) (#26288)

--- testing/testcafe/tests/dataGrid/keyboardNavigation/startEditing.functional.ts
+++ testing/testcafe/tests/dataGrid/keyboardNavigation/startEditing.functional.ts
@@ -35,3 +35,42 @@ test('Editing should start by pressing enter after scrolling content with scroll
     height: 300,
   });
 });
+
+test('DataGrid should not remove the minus symbol when editing started (T1201166)', async (t) => {
+  const dataGrid = new DataGrid(DATA_GRID_SELECTOR);
+
+  await t
+    .click(dataGrid.getDataCell(0, 0).element)
+    .pressKey('- 1')
+    .pressKey('enter')
+    .expect(dataGrid.getDataCell(0, 0).element.innerText)
+    .eql('-1');
+}).before(async () => {
+  await createWidget('dxDataGrid', {
+    dataSource: [
+      { id: undefined, text: '1' },
+      { id: 2, text: '2' },
+      { id: 3, text: '3' },
+    ],
+    columns: [{
+      dataField: 'id',
+      dataType: 'number',
+      editorOptions: {
+        format: { type: 'decimal' },
+      },
+    },
+    'text',
+    ],
+    editing: {
+      allowUpdating: true,
+      selectTextOnEditStart: true,
+      mode: 'batch',
+      startEditAction: 'dblClick',
+    },
+    keyboardNavigation: {
+      editOnKeyPress: true,
+      enterKeyAction: 'moveFocus',
+      enterKeyDirection: 'column',
+    },
+  });
+});
