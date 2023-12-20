643eb5a2b9d698e61a58e8e0430e3be43edbc988
Alexander Bulychev <git@bulychev.pro>
2023-12-20 12:53:45 +0400

    Viz: make LegendItem public (T1191979) (#26150)
    Co-authored-by: Alexander Bulychev <git@bulychev.net>
    
    (cherry picked from commit 823a8640f5424c1f04414fac99a502e45b375095)

--- ts/dx.all.d.ts
+++ ts/dx.all.d.ts
@@ -30468,29 +30468,16 @@ declare module DevExpress.viz {
     /**
      * [descr:BaseChartOptions.legend.customizeItems]
      */
-    customizeItems?: (
-      items: Array<BaseChartLegendItem>
-    ) => Array<BaseChartLegendItem>;
+    customizeItems?: (items: Array<LegendItem>) => Array<LegendItem>;
     /**
      * [descr:BaseChartOptions.legend.markerTemplate]
      */
     markerTemplate?:
       | DevExpress.core.template
       | ((
-          legendItem: BaseChartLegendItem,
+          legendItem: LegendItem,
           element: SVGGElement
         ) => string | DevExpress.core.UserDefinedElement<SVGElement>);
-  }
-  /**
-   * [descr:BaseChartLegendItem]
-   * @deprecated Attention! This type is for internal purposes only. If you used it previously, please submit a ticket to our {@link https://supportcenter.devexpress.com/ticket/create Support Center}. We will check if there is an alternative solution.
-   */
-  export interface BaseChartLegendItem
-    extends DevExpress.common.charts.BaseLegendItem {
-    /**
-     * [descr:BaseChartLegendItem.series]
-     */
-    series?: baseSeriesObject;
   }
   /**
    * [descr:BaseChartOptions]
@@ -41719,6 +41706,15 @@ declare module DevExpress.viz {
    * [descr:viz.getTheme(theme)]
    */
   export function getTheme(theme: string): any;
+  /**
+   * [descr:LegendItem]
+   */
+  export interface LegendItem extends DevExpress.common.charts.BaseLegendItem {
+    /**
+     * [descr:LegendItem.series]
+     */
+    series?: baseSeriesObject;
+  }
   /**
    * [descr:MapLayer]
    * @deprecated Attention! This type is for internal purposes only. If you used it previously, please submit a ticket to our {@link https://supportcenter.devexpress.com/ticket/create Support Center}. We will check if there is an alternative solution.
