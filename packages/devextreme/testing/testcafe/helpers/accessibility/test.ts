b907ded4f476465e3ff9e73e3171ada181d5c96f
marker dao ® <mark_nikolsky@outlook.com>
2023-12-22 13:54:48 +0400

    Accessibility: Add testAccessibility helper, add configuration import util, test Tabs and TreeView
    Co-authored-by: marker dao ® <youdontknow@marker-dao.eth>
    Co-authored-by: Roman Semenov <roman.semenov@devexpress.com>

---
+++ packages/devextreme/testing/testcafe/helpers/accessibility/test.ts
@@ -0,0 +1,57 @@
+import { ElementContext } from 'axe-core';
+import createWidget, { WidgetName } from '../createWidget';
+import { isMaterialBased } from '../themeUtils';
+import { a11yCheck, A11yCheckOptions } from './utils';
+import { generateOptionMatrix, Options } from '../generateOptionMatrix';
+
+export interface Configuration<TComponentOptions=unknown> {
+  component: WidgetName;
+  options?: Options<TComponentOptions>;
+  a11yCheckConfig?: A11yCheckOptions;
+  selector?: ElementContext;
+  created?: (t: TestController, optionConfiguration?: Options) => Promise<void>;
+}
+
+const defaultSelector = '#container';
+const defaultOptions = {};
+const defaultCreated = async () => {};
+const defaultA11yCheckConfig = isMaterialBased() ? {
+  runOnly: 'color-contrast',
+} : {};
+
+const getOptionConfigurations = (options: Options | undefined) => {
+  if (!(options && Object.keys(options).length)) {
+    return [defaultOptions];
+  }
+
+  const configurations: Options[] = generateOptionMatrix(options);
+
+  return configurations;
+};
+
+export const testAccessibility = <TComponentOptions=unknown>(
+  configuration: Configuration<TComponentOptions>,
+): void => {
+  const {
+    component,
+    options,
+    selector = defaultSelector,
+    a11yCheckConfig = defaultA11yCheckConfig,
+    created = defaultCreated,
+  } = configuration;
+
+  const optionConfigurations: Options[] = getOptionConfigurations(options);
+
+  optionConfigurations.forEach((optionConfiguration, index) => {
+    test(`${component}: test with axe #${index}`, async (t) => {
+      await a11yCheck(t, a11yCheckConfig, selector, optionConfiguration);
+    }).before(async (t) => {
+      await createWidget(
+        component,
+        optionConfiguration,
+      );
+
+      await created(t, optionConfiguration);
+    });
+  });
+};
--- packages/devextreme/testing/testcafe/helpers/accessibility/utils.ts
+++ packages/devextreme/testing/testcafe/helpers/accessibility/utils.ts
@@ -2,20 +2,31 @@
 import { axeCheck, createReport } from '@testcafe-community/axe';
 import { ElementContext, RunOptions } from 'axe-core';
 
+export interface A11yCheckOptions extends RunOptions {
+  runOnly?: any;
+}
+
 const defaultOptions = {
   rules: {
     'color-contrast': { enabled: false },
   },
 };
 
-interface A11yCheckOptions extends RunOptions {
-  runOnly?: any;
-}
+const createFullReport = (results, configuration) => {
+  let report = createReport(results.violations);
+
+  if (results.violations.length && configuration) {
+    report += `\n${JSON.stringify(configuration)}\n`;
+  }
+
+  return report;
+};
 
 export const a11yCheck = async (
   t: TestController,
   options: A11yCheckOptions = defaultOptions,
   selector?: ElementContext,
+  configuration = {},
 ):
 Promise<void> => {
   const { error, results } = await axeCheck(t, selector, { rules: {}, ...options });
@@ -24,5 +35,5 @@ Promise<void> => {
     .expect(error)
     .eql(null)
     .expect(results.violations.length === 0)
-    .ok(createReport(results.violations));
+    .ok(createFullReport(results, configuration));
 };
