a5963e172453ee1790212336dbde1287eb36b66e
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2023-12-25 15:27:10 +0400

    add wait

--- packages/devextreme/testing/testcafe/tests/editors/map/map.ts
+++ packages/devextreme/testing/testcafe/tests/editors/map/map.ts
@@ -10,9 +10,8 @@ test('Map should not raise errors when it is disposed immediately after creating
 
   await t
     .click($editButton)
+    .wait(1000)
     .expect(true).ok();
-
-  await t.eval(() => location.reload());
 }).before(async () => createWidget('dxDataGrid', {
   dataSource: [{
     ID: 1,
