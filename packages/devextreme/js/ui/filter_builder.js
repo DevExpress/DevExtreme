059cc218578661074d9930fb17687d1bab9c6dfd
Ilya Vinogradov <williamvinogradov@gmail.com>
2023-12-27 03:01:18 +0400

    FilterBuilder(TS): Fixes after move JS->TS
    1) Fixed TS errors in new TS files
    2) Fixed ESlint errors in new TS files
    3) Fixed imports in js/ui/filter_builder.js
    4) Doc comments moved to js/ui/filter_builder.js
    5) STYLE comment moved to js/ui/filter_builder.js

--- packages/devextreme/js/ui/filter_builder.js
+++ packages/devextreme/js/ui/filter_builder.js
@@ -1,8 +1,17 @@
-import FilterBuilder from './filter_builder/filter_builder';
+import FilterBuilder from '../__internal/filter_builder/m_filter_builder';
+
+// STYLE filterBuilder
+
 export default FilterBuilder;
 
 /**
  * @name dxFilterBuilderCustomOperation
  * @namespace DevExpress.ui
  * @type object
+ */
+
+/**
+ * @name dxFilterBuilderField.defaultFilterOperation
+ * @type Enums.FilterBuilderOperation | string
+ * @hidden
  */
