405b50b77bb7b3cfb2527097ea45477e10b4ed62
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2023-12-28 18:27:57 +0200

    Remove host tar usage

--- build/change-package-scope.ts
+++ build/change-package-scope.ts
@@ -1,7 +1,9 @@
 import sh from 'shelljs';
 import fs from 'fs';
 import path from 'path';
+import tar from 'tar-fs';
 import yargs from 'yargs';
+import { createUnzip } from 'zlib';
 import { npm } from './common/npm-utils';
 import { ensureEmptyDir } from './common/fs-utils';
 
@@ -18,39 +20,42 @@ sh.config.silent = true;
 const dirName = path.basename(args.tgz).replace(/\.tgz$/, '');
 ensureEmptyDir(dirName);
 
-sh.exec(`tar -xzf ${args.tgz} --strip-components=1  --directory ${dirName}`);
-sh.pushd(dirName);
+fs.createReadStream(args.tgz).pipe(createUnzip()).pipe(tar.extract(dirName, { strip: 1 })).on('finish', () => {
+  sh.pushd(dirName);
 
-const name = npm.pkg.get('name')?.match(/^(@(.*)\/)?(.*)$/)?.[3];
-const version = npm.pkg.get('version');
+  const name = npm.pkg.get('name')?.match(/^(@(.*)\/)?(.*)$/)?.[3];
+  const version = npm.pkg.get('version');
 
-if(!name) {
-  throw new Error('Unable to get package name');
-}
-
-if(!version) {
-  throw new Error('Unable to get package version');
-}
-
-let newDirName: string | undefined;
-
-if(args.scope) {
-  npm.pkg.set({ name: `@${args.scope}/${name}` });
-  newDirName = `${args.scope}-${name}-${version}`;
-}
-
-if(args.removeScope) {
-  npm.pkg.set({ name });
-  newDirName = `${name}-${version}`;
-}
-
-sh.popd();
-
-if(newDirName) {
-  if(fs.existsSync(newDirName)) {
-    fs.rmSync(newDirName, { force: true, recursive: true })
+  if(!name) {
+    throw new Error('Unable to get package name');
   }
-  fs.renameSync(dirName, newDirName);
-}
 
-console.log(newDirName ?? dirName); // return value, used in GA
+  if(!version) {
+    throw new Error('Unable to get package version');
+  }
+
+  let newDirName: string | undefined;
+
+  if(args.scope) {
+    npm.pkg.set({ name: `@${args.scope}/${name}` });
+    newDirName = `${args.scope}-${name}-${version}`;
+  }
+
+  if(args.removeScope) {
+    npm.pkg.set({ name });
+    newDirName = `${name}-${version}`;
+  }
+
+  sh.popd();
+
+  if(newDirName) {
+    if(fs.existsSync(newDirName)) {
+      fs.rmSync(newDirName, { force: true, recursive: true })
+    }
+    fs.renameSync(dirName, newDirName);
+  }
+
+  console.log(newDirName ?? dirName); // return value, used in GA
+}).on('error', () => {
+  throw new Error(`Unexpected error occured during extracting from archive: ${args.tgz}`);
+});
