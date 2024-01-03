2b2244de7990afe1b8d4f4622a09870f2bb27280
ilya.kharchenko <14272298+IlyaKhD@users.noreply.github.com>
2024-01-03 13:16:23 +0400

    Refactor build scripts (#26395)

--- build/make-artifacts-package.ts
+++ build/make-artifacts-package.ts
@@ -1,34 +1,21 @@
-import sh from 'shelljs';
-import fs from 'fs';
+import sh from 'shelljs'; 
 import path from 'path';
 
-function recreateDir(path: string): void {
-  if (fs.existsSync(path)) {
-    sh.rm('-r', path)
-  }
-  sh.mkdir('-p', path)
-}
-
-const MONOREPO_ROOT = path.join(__dirname, '..');
-const INTERNAL_TOOLS_ARTIFACTS = path.join(MONOREPO_ROOT, 'artifacts', 'internal-tools');
-
-const ARTIFACTS_DIR = path.join(MONOREPO_ROOT, 'artifacts');
-const ARTIFACTS_PACKAGE_DIR = path.join(ARTIFACTS_DIR, 'devextreme-artifacts');
+import { ARTIFACTS_DIR, INTERNAL_TOOLS_ARTIFACTS } from './common/paths';
+import { ensureEmptyDir } from './common/fs-utils';
 
 import { version, license, author } from '../package.json';
+import { npm } from './common/npm-utils';
 
-recreateDir(ARTIFACTS_PACKAGE_DIR);
+const ARTIFACTS_PACKAGE_DIR = path.join(ARTIFACTS_DIR, 'devextreme-artifacts');
+
+ensureEmptyDir(ARTIFACTS_PACKAGE_DIR);
 
 sh.cp('-r', INTERNAL_TOOLS_ARTIFACTS, ARTIFACTS_PACKAGE_DIR);
 sh.pushd(ARTIFACTS_PACKAGE_DIR);
 {
-  fs.writeFileSync('package.json', JSON.stringify({}));
-
-  sh.exec('npm pkg set name=devextreme-artifacts');
-  sh.exec(`npm pkg set version=${version}`);
-  sh.exec(`npm pkg set license=${license}`)
-  sh.exec(`npm pkg set author="${author}"`);
-
-  sh.exec('npm pack --pack-destination="../npm"');
+  npm.initEmpty();
+  npm.pkg.set({ name: 'devextreme-artifacts', version, license, author });
+  npm.pack({destination: '../npm'});
 }
 sh.popd();
