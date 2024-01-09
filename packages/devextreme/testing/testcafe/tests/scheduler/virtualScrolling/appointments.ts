3515d6da3e4c02bd463040cdfa0aee5c14a8d79a
timbset <timbset@gmail.com>
2024-01-10 00:51:58 +0400

    Add matrix testing based on views and group orientation

--- packages/devextreme/testing/testcafe/tests/scheduler/virtualScrolling/appointments.ts
+++ packages/devextreme/testing/testcafe/tests/scheduler/virtualScrolling/appointments.ts
@@ -3,7 +3,8 @@ import createWidget from '../../../helpers/createWidget';
 import url from '../../../helpers/getPageUrl';
 import Scheduler from '../../../model/scheduler';
 import { scrollTo } from './utils';
-import type { Appointment } from '../../../../../js/ui/scheduler';
+import type { Appointment, ViewType, Orientation } from '../../../../../js/ui/scheduler';
+import { generateOptionMatrix } from '../../../helpers/generateOptionMatrix';
 
 fixture.disablePageReloads`Scheduler: Virtual Scrolling`
   .page(url(__dirname, '../../container.html'));
@@ -41,114 +42,121 @@ test('Appointment should not repaint after scrolling if present on viewport', as
   });
 });
 
-test('Targeted appointment data should match original appointment data (T1205120)', async (t) => {
-  const appointmentTextRegex = /^tid\[(\d+)\] hid\[\1\]/;
+const viewTypes: ViewType[] = ['agenda', 'day', 'month', 'timelineDay', 'timelineMonth', 'timelineWeek', 'timelineWorkWeek', 'week', 'workWeek'];
+const groupOrientations: Orientation[] = ['horizontal', 'vertical'];
 
-  const scheduler = new Scheduler('#container');
+generateOptionMatrix({
+  viewType: viewTypes,
+  groupOrientation: groupOrientations,
+}).forEach(({ viewType, groupOrientation }) => {
+  test(`targetedAppointmentData should be correct with groups in ${viewType} view with ${groupOrientation} orientation (T1205120)`, async (t) => {
+    const appointmentTextRegex = /^tid\[(\d+)\] gid\[\1\]/;
 
-  const expectAllElementsMatchRegex = async (
-    tc: TestController,
-    elements: Selector,
-    regex: RegExp,
-  ) => {
-    const count = await elements.count;
+    const scheduler = new Scheduler('#container');
 
-    for (let i = 0; i < count; i += 1) {
-      await tc
-        .expect(elements.nth(i).innerText)
-        .match(regex);
-    }
-  };
+    const expectAllElementsMatchRegex = async (
+      tc: TestController,
+      elements: Selector,
+      regex: RegExp,
+    ) => {
+      const count = await elements.count;
 
-  await expectAllElementsMatchRegex(t, scheduler.element.find('.dx-scheduler-appointment'), appointmentTextRegex);
-
-  await scrollTo(new Date(2024, 0, 2, 8));
-
-  await expectAllElementsMatchRegex(t, scheduler.element.find('.dx-scheduler-appointment'), appointmentTextRegex);
-
-  await scrollTo(new Date(2024, 0, 3, 8));
-
-  await expectAllElementsMatchRegex(t, scheduler.element.find('.dx-scheduler-appointment'), appointmentTextRegex);
-
-  await scrollTo(new Date(2024, 0, 2, 8));
-
-  await expectAllElementsMatchRegex(t, scheduler.element.find('.dx-scheduler-appointment'), appointmentTextRegex);
-
-  await scrollTo(new Date(2024, 0, 1, 8));
-
-  await expectAllElementsMatchRegex(t, scheduler.element.find('.dx-scheduler-appointment'), appointmentTextRegex);
-}).before(async () => {
-  const startDate = new Date(2024, 0, 1, 8);
-  const endDate = new Date(2024, 0, 3, 10);
-  const startDayHour = 8;
-  const endDayHour = 20;
-  const resourceCount = 30;
-
-  const resourceDataSource = Array.from({ length: resourceCount }, (_, index) => ({
-    id: index,
-    text: `Resource ${index}`,
-  }));
-
-  const resourceAppointmentCount = Math.floor(
-    (endDate.getTime() - startDate.getTime())
-    / ((1000 * 60 * 60 * 24) / 2),
-  );
-
-  const startDates = Array
-    .from({ length: resourceAppointmentCount })
-    .map<Date>((_, index) => new Date(2024, 0, 1 + index, 8));
-
-  const endDates = startDates.map((date) => {
-    const result = new Date(date);
-    result.setHours(result.getHours() + 2);
-    return result;
-  });
-
-  const appointments = resourceDataSource.reduce<Appointment[]>((acc, resource) => acc.concat(
-    Array
-      .from({ length: resourceAppointmentCount })
-      .map<Appointment>((_, index) => ({
-      text: resource.text,
-      startDate: startDates[index],
-      endDate: endDates[index],
-      humanId: resource.id,
-    })),
-  ), []);
-
-  await createWidget('dxScheduler', {
-    height: 600,
-    width: 800,
-    currentDate: new Date(2024, 0, 2),
-    startDayHour,
-    endDayHour,
-    scrolling: {
-      mode: 'virtual',
-      // orientation: 'both',
-    },
-    groups: ['humanId'],
-    views: ['week', 'month', 'timelineWorkWeek'],
-    currentView: 'timelineWorkWeek',
-    dataSource: appointments,
-    resources: [
-      {
-        fieldExpr: 'humanId',
-        allowMultiple: true,
-        dataSource: resourceDataSource,
-        label: 'Employees',
-        displayExpr: 'id',
-      },
-    ],
-    appointmentTemplate({ targetedAppointmentData, appointmentData }) {
-      const { text, humanId: targetedId } = targetedAppointmentData;
-      const { humanId } = appointmentData;
-
-      const $element = $(`<div>tid[${targetedId}] hid[${humanId}] ${text}</div>`);
-
-      if (humanId !== targetedId) {
-        $element.css('background-color', 'red');
+      for (let i = 0; i < count; i += 1) {
+        await tc
+          .expect(elements.nth(i).innerText)
+          .match(regex);
       }
+    };
 
-      return $element;
-    },
+    const datesToScroll = [
+      new Date(2024, 0, 1, 8),
+      new Date(2024, 0, 2, 8),
+      new Date(2024, 0, 3, 8),
+    ];
+
+    // eslint-disable-next-line no-restricted-syntax
+    for (const date of [...datesToScroll, ...[...datesToScroll].reverse()]) {
+      await scrollTo(date, { groupId: 0 });
+      await expectAllElementsMatchRegex(t, scheduler.element.find('.dx-scheduler-appointment'), appointmentTextRegex);
+    }
+  }).before(async () => {
+    const startDate = new Date(2024, 0, 1, 8);
+    const endDate = new Date(2024, 0, 3, 10);
+    const startDayHour = 8;
+    const endDayHour = 20;
+    const resourceCount = 30;
+
+    const resourceDataSource = Array.from({ length: resourceCount }, (_, index) => ({
+      id: index,
+      text: `Resource ${index}`,
+    }));
+
+    const resourceAppointmentCount = Math.floor(
+      (endDate.getTime() - startDate.getTime())
+      / ((1000 * 60 * 60 * 24) / 2),
+    );
+
+    const startDates = Array
+      .from({ length: resourceAppointmentCount })
+      .map<Date>((_, index) => new Date(2024, 0, 1 + index, 8));
+
+    const endDates = startDates.map((date) => {
+      const result = new Date(date);
+      result.setHours(result.getHours() + 2);
+      return result;
+    });
+
+    const appointments = resourceDataSource.reduce<Appointment[]>((acc, resource) => acc.concat(
+      Array
+        .from({ length: resourceAppointmentCount })
+        .map<Appointment>((_, index) => ({
+        text: resource.text,
+        startDate: startDates[index],
+        endDate: endDates[index],
+        groupId: resource.id,
+      })),
+    ), []);
+
+    await createWidget('dxScheduler', {
+      height: 600,
+      width: 800,
+      currentDate: new Date(2024, 0, 2),
+      startDayHour,
+      endDayHour,
+      scrolling: {
+        mode: 'virtual',
+        // orientation: 'both',
+      },
+      groups: ['groupId'],
+      views: [
+        {
+          type: viewType,
+          groupOrientation,
+        },
+      ],
+      currentView: viewType,
+      dataSource: appointments,
+      resources: [
+        {
+          fieldExpr: 'groupId',
+          allowMultiple: true,
+          dataSource: resourceDataSource,
+          label: 'Employees',
+          displayExpr: 'id',
+        },
+      ],
+      appointmentTemplate({ targetedAppointmentData, appointmentData }) {
+        const { groupId: targetedId } = targetedAppointmentData;
+        const { groupId } = appointmentData;
+
+        const $element = $(`<div>tid[${targetedId}] gid[${groupId}]</div>`);
+
+        if (groupId !== targetedId) {
+          $element.css('background-color', 'red');
+        }
+
+        return $element;
+      },
+    });
   });
 });
