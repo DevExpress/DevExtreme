6476f349df9cc80437cbdd67b9ed594c23373863
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-01-12 16:33:35 +0400

    Lookup - Editor has incorrect width if width is set to fit-content (T1210435)

--- packages/devextreme/testing/testcafe/tests/editors/lookup/common.ts
+++ packages/devextreme/testing/testcafe/tests/editors/lookup/common.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 import { ClientFunction, Selector } from 'testcafe';
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import { isMaterial, isMaterialBased, testScreenshot } from '../../../helpers/themeUtils';
@@ -5,11 +6,21 @@ import url from '../../../helpers/getPageUrl';
 import Lookup from '../../../model/lookup';
 import createWidget from '../../../helpers/createWidget';
 import { safeSizeTest } from '../../../helpers/safeSizeTest';
+import {
+  appendElementTo, insertStylesheetRulesToPage, setStyleAttribute,
+} from '../../../helpers/domUtils';
+import Guid from '../../../../../js/core/guid';
+import { clearTestPage } from '../../../helpers/clearPage';
 
+const LOOKUP_CLASS = 'dx-lookup';
 const LOOKUP_FIELD_CLASS = 'dx-lookup-field';
 
+const stylingModes = ['outlined', 'underlined', 'filled'];
+const labelModes = ['static', 'floating', 'hidden', 'outside'];
+
 fixture.disablePageReloads`Lookup`
-  .page(url(__dirname, '../../container.html'));
+  .page(url(__dirname, '../../container.html'))
+  .afterEach(async () => clearTestPage());
 
 test('Popup should not be closed if lookup is placed at the page bottom (T1018037)', async (t) => {
   const lookup = new Lookup('#container');
@@ -128,3 +139,48 @@ test('Placeholder is visible after items option change when value is not chosen 
   width: 300,
   placeholder: 'Choose a value',
 }));
+
+test('Lookup appearance', async (t) => {
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+  await t.debug();
+  await testScreenshot(t, takeScreenshot, 'Lookup appearance.png', { shouldTestInCompact: true });
+
+  for (const id of t.ctx.ids) {
+    await setStyleAttribute(Selector(`#${id}`), 'width: fit-content;');
+  }
+
+  await testScreenshot(t, takeScreenshot, 'Lookup width adjust to fit its content.png', { shouldTestInCompact: true });
+
+  await t
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async (t) => {
+  t.ctx.ids = [];
+
+  await insertStylesheetRulesToPage(`.${LOOKUP_CLASS} { display: inline-block; margin: 5px; }`);
+
+  for (const stylingMode of stylingModes) {
+    for (const labelMode of labelModes) {
+      for (const rtlEnabled of [true, false]) {
+        for (const value of [null, 'Choosen value']) {
+          const id = `${`dx${new Guid()}`}`;
+
+          t.ctx.ids.push(id);
+          await appendElementTo('#container', 'div', id, { });
+
+          const options: any = {
+            width: 220,
+            label: 'label text',
+            labelMode,
+            stylingMode,
+            rtlEnabled,
+            value,
+          };
+
+          await createWidget('dxLookup', options, `#${id}`);
+        }
+      }
+    }
+  }
+});
