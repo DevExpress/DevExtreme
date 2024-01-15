7c8f2ed6f3397d518c4ff5d8569b0d98a4c42bd9
Ilya Vinogradov <williamvinogradov@gmail.com>
2024-01-15 13:33:36 -0800

    Scheduler(T1206036): Fix multi cell selection + offset issue.
    - Re-implemented cell selection algorithm in view_provider.
    - Increased the selection e2e test matrix.

--- packages/devextreme/testing/testcafe/tests/scheduler/viewOffset/multiCellSelection.ts
+++ packages/devextreme/testing/testcafe/tests/scheduler/viewOffset/multiCellSelection.ts
@@ -1,4 +1,5 @@
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import { ClientFunction } from 'testcafe';
 import createWidget from '../../../helpers/createWidget';
 import url from '../../../helpers/getPageUrl';
 import Scheduler from '../../../model/scheduler';
@@ -8,31 +9,96 @@ fixture.disablePageReloads`Offset: Multi cell selection`
 
 const SCHEDULER_SELECTOR = '#container';
 
+// TODO Vinogradov: Change it to a general dispose widget helper function.
+const disposeScheduler = ClientFunction(() => {
+  const scheduler = ($(SCHEDULER_SELECTOR) as any).dxScheduler('instance');
+
+  scheduler?.dispose();
+}, { dependencies: { SCHEDULER_SELECTOR } });
+
 [
   0,
   -120,
   120,
 ].forEach((offset) => {
-  [
-    { view: 'day', dragOptions: { from: [0, 0], to: [5, 0] } },
-    { view: 'week', dragOptions: { from: [2, 2], to: [3, 3] } },
-    { view: 'month', dragOptions: { from: [2, 2], to: [3, 1] } },
-    { view: 'timelineDay', dragOptions: { from: [0, 0], to: [0, 3] } },
-    { view: 'timelineMonth', dragOptions: { from: [0, 0], to: [0, 3] } },
-  ].forEach(({ view, dragOptions }) => {
-    test(`Multi cell selection should work (view: ${view}, offset: ${offset})`, async (t) => {
+  [true, false].forEach((rtlEnabled) => {
+    [
+      { view: 'day', dragOptions: { direction: 'increase', from: [0, 0], to: [7, 0] } },
+      { view: 'day', dragOptions: { direction: 'decrease', from: [7, 0], to: [0, 0] } },
+      { view: 'week', dragOptions: { direction: 'increase', from: [0, 2], to: [7, 2] } },
+      { view: 'week', dragOptions: { direction: 'decrease', from: [7, 2], to: [0, 2] } },
+      { view: 'week', dragOptions: { direction: 'increase', from: [1, 3], to: [8, 4] } },
+      { view: 'week', dragOptions: { direction: 'decrease', from: [8, 4], to: [1, 3] } },
+      { view: 'week', dragOptions: { direction: 'increase', from: [6, 3], to: [6, 5] } },
+      { view: 'week', dragOptions: { direction: 'decrease', from: [6, 5], to: [6, 3] } },
+      { view: 'week', dragOptions: { direction: 'increase', from: [0, 0], to: [11, 6] } },
+      { view: 'week', dragOptions: { direction: 'decrease', from: [11, 6], to: [0, 0] } },
+      { view: 'month', dragOptions: { direction: 'increase', from: [2, 2], to: [3, 1] } },
+      { view: 'month', dragOptions: { direction: 'decrease', from: [3, 1], to: [2, 2] } },
+      { view: 'timelineDay', dragOptions: { direction: 'increase', from: [0, 0], to: [0, 5] } },
+      { view: 'timelineDay', dragOptions: { direction: 'decrease', from: [0, 5], to: [0, 0] } },
+      { view: 'timelineMonth', dragOptions: { direction: 'increase', from: [0, 0], to: [0, 3] } },
+      { view: 'timelineMonth', dragOptions: { direction: 'decrease', from: [0, 3], to: [0, 0] } },
+    ].forEach(({ view, dragOptions }) => {
+      test(`Multi cell selection should work (
+view: ${view},
+offset: ${offset},
+direction: ${dragOptions.direction},
+rtl: ${rtlEnabled},
+)`, async (t) => {
+        const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+        const scheduler = new Scheduler(SCHEDULER_SELECTOR);
+
+        const { direction, from: [fromRow, fromCell], to: [toRow, toCell] } = dragOptions;
+        const firstCellSelector = scheduler.getDateTableCell(fromRow, fromCell);
+        const secondCellSelector = scheduler.getDateTableCell(toRow, toCell);
+
+        await t.dragToElement(firstCellSelector, secondCellSelector);
+
+        await takeScreenshot(
+          `offset_multi-cell-select_${view}_offset-${offset}_${direction}${rtlEnabled ? '_rtl' : ''}.png`,
+          scheduler.workSpace,
+        );
+
+        await t.click(secondCellSelector);
+        await t.expect(scheduler.appointmentPopup.element).ok();
+
+        await takeScreenshot(
+          `offset_multi-cell-select_dialog_${view}_offset-${offset}_${direction}${rtlEnabled ? '_rtl' : ''}.png`,
+          scheduler.workSpace,
+        );
+
+        await t.click(scheduler.appointmentPopup.cancelButton);
+
+        await t.expect(compareResults.isValid())
+          .ok(compareResults.errorMessages());
+      }).before(async () => {
+        await createWidget('dxScheduler', {
+          currentDate: '2023-09-07',
+          height: 800,
+          dataSource: [],
+          currentView: view,
+          offset,
+          rtlEnabled,
+        });
+      }).after(async () => disposeScheduler());
+    });
+
+    test(`Multi cell selection in the all-day panel should work (
+view: week,
+offset: ${offset},
+rtl: ${rtlEnabled},
+)`, async (t) => {
       const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
       const scheduler = new Scheduler(SCHEDULER_SELECTOR);
 
-      const [fromRow, fromCell] = dragOptions.from;
-      const [toRow, toCell] = dragOptions.to;
-      const firstCellSelector = scheduler.getDateTableCell(fromRow, fromCell);
-      const secondCellSelector = scheduler.getDateTableCell(toRow, toCell);
+      const firstCellSelector = scheduler.getAllDayTableCell(0);
+      const secondCellSelector = scheduler.getAllDayTableCell(3);
 
       await t.dragToElement(firstCellSelector, secondCellSelector);
 
       await takeScreenshot(
-        `offset_multi-cell-select_${view}_offset-${offset}.png`,
+        `offset_multi-cell-select_week-all-day_offset-${offset}${rtlEnabled ? '_rtl' : ''}.png`,
         scheduler.workSpace,
       );
 
@@ -40,11 +106,9 @@ const SCHEDULER_SELECTOR = '#container';
       await t.expect(scheduler.appointmentPopup.element).ok();
 
       await takeScreenshot(
-        `offset_multi-cell-select_dialog_${view}_offset-${offset}.png`,
+        `offset_multi-cell-select_dialog_week-all-day_offset-${offset}${rtlEnabled ? '_rtl' : ''}.png`,
         scheduler.workSpace,
       );
-
-      await t.click(scheduler.appointmentPopup.cancelButton);
 
       await t.expect(compareResults.isValid())
         .ok(compareResults.errorMessages());
@@ -53,43 +117,9 @@ const SCHEDULER_SELECTOR = '#container';
         currentDate: '2023-09-07',
         height: 800,
         dataSource: [],
-        currentView: view,
+        currentView: 'week',
         offset,
       });
-    });
-  });
-
-  test(`Multi cell selection in the all-day panel should work (view: week, offset: ${offset})`, async (t) => {
-    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
-    const scheduler = new Scheduler(SCHEDULER_SELECTOR);
-
-    const firstCellSelector = scheduler.getAllDayTableCell(0);
-    const secondCellSelector = scheduler.getAllDayTableCell(3);
-
-    await t.dragToElement(firstCellSelector, secondCellSelector);
-
-    await takeScreenshot(
-      `offset_multi-cell-select_week-all-day_offset-${offset}.png`,
-      scheduler.workSpace,
-    );
-
-    await t.click(secondCellSelector);
-    await t.expect(scheduler.appointmentPopup.element).ok();
-
-    await takeScreenshot(
-      `offset_multi-cell-select_dialog_week-all-day_offset-${offset}.png`,
-      scheduler.workSpace,
-    );
-
-    await t.expect(compareResults.isValid())
-      .ok(compareResults.errorMessages());
-  }).before(async () => {
-    await createWidget('dxScheduler', {
-      currentDate: '2023-09-07',
-      height: 800,
-      dataSource: [],
-      currentView: 'week',
-      offset,
-    });
+    }).after(async () => disposeScheduler());
   });
 });
