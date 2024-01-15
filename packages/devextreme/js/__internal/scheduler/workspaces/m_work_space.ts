5a6cdb8211442705afb7b906f4b44ffe41760efe
timbset <timbset@gmail.com>
2024-01-15 21:10:40 +0400

    Fix vertical virtual scrolling

--- packages/devextreme/js/__internal/scheduler/workspaces/m_work_space.ts
+++ packages/devextreme/js/__internal/scheduler/workspaces/m_work_space.ts
@@ -1585,7 +1585,7 @@ class SchedulerWorkSpace extends WidgetObserver {
 
   // NOTE: refactor leftIndex calculation
   getCellIndexByCoordinates(coordinates, allDay) {
-    const { horizontalScrollingState } = this.virtualScrollingDispatcher;
+    const { horizontalScrollingState, verticalScrollingState } = this.virtualScrollingDispatcher;
 
     const cellCount = horizontalScrollingState?.itemCount ?? this._getTotalCellCount(this._getGroupCount());
 
@@ -1593,8 +1593,9 @@ class SchedulerWorkSpace extends WidgetObserver {
     const cellHeight = allDay ? this.getAllDayHeight() : this.getCellHeight();
 
     const leftCoordinateOffset = horizontalScrollingState?.virtualItemSizeBefore ?? 0;
+    const topCoordinateOffset = verticalScrollingState?.virtualItemSizeBefore ?? 0;
 
-    const topIndex = Math.floor(Math.floor(coordinates.top) / Math.floor(cellHeight));
+    const topIndex = Math.floor(Math.floor(coordinates.top - topCoordinateOffset) / Math.floor(cellHeight));
     let leftIndex = (coordinates.left - leftCoordinateOffset) / cellWidth;
     leftIndex = Math.floor(leftIndex + CELL_INDEX_CALCULATION_EPSILON);
 
