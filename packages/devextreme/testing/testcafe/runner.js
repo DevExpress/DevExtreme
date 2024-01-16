b273c71c7d49cefdf2d6e158d1139e137999c682
timbset <timbset@gmail.com>
2024-01-17 00:45:44 +0400

    Turn off quarantine mode

--- packages/devextreme/testing/testcafe/runner.js
+++ packages/devextreme/testing/testcafe/runner.js
@@ -88,7 +88,7 @@ createTestCafe({
         }
 
         const runOptions = {
-            quarantineMode: { successThreshold: 1, attemptLimit: 5 },
+            quarantineMode: false,
         };
 
         if(args.componentFolder.trim() !== 'renovation') {
