5f4be7fa4469ec403fcee6447c1b6e4b95d8ae8a
Marker DAO ® <mark_nikolsky@outlook.com>
2024-01-23 14:57:45 +0400

    Gallery: Add img alt into the data source

--- Demos/Gallery/Overview/ReactJs/data.js
+++ Demos/Gallery/Overview/ReactJs/data.js
@@ -1,11 +1,38 @@
 export const gallery = [
-  '../../../../images/gallery/1.jpg',
-  '../../../../images/gallery/2.jpg',
-  '../../../../images/gallery/3.jpg',
-  '../../../../images/gallery/4.jpg',
-  '../../../../images/gallery/5.jpg',
-  '../../../../images/gallery/6.jpg',
-  '../../../../images/gallery/7.jpg',
-  '../../../../images/gallery/8.jpg',
-  '../../../../images/gallery/9.jpg',
+  {
+    imageAlt: 'Image 1',
+    imageSrc: '../../../../images/gallery/1.jpg',
+  },
+  {
+    imageAlt: 'Image 2',
+    imageSrc: '../../../../images/gallery/2.jpg',
+  },
+  {
+    imageAlt: 'Image 3',
+    imageSrc: '../../../../images/gallery/3.jpg',
+  },
+  {
+    imageAlt: 'Image 4',
+    imageSrc: '../../../../images/gallery/4.jpg',
+  },
+  {
+    imageAlt: 'Image 5',
+    imageSrc: '../../../../images/gallery/5.jpg',
+  },
+  {
+    imageAlt: 'Image 6',
+    imageSrc: '../../../../images/gallery/6.jpg',
+  },
+  {
+    imageAlt: 'Image 7',
+    imageSrc: '../../../../images/gallery/7.jpg',
+  },
+  {
+    imageAlt: 'Image 8',
+    imageSrc: '../../../../images/gallery/8.jpg',
+  },
+  {
+    imageAlt: 'Image 9',
+    imageSrc: '../../../../images/gallery/9.jpg',
+  },
 ];
