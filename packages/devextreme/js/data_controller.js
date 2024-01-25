e4f74b25486cd6a93417cdafba1ec2457f185d6a
Alexander Bulychev <git@bulychev.net>
2024-01-25 13:11:41 +0400

    Cleanup

--- packages/devextreme/js/data_controller.js
+++ packages/devextreme/js/data_controller.js
@@ -1,50 +1,10 @@
-import { noop } from './core/utils/common';
-import { Deferred } from './core/utils/deferred';
 import { DataSource } from './data/data_source/data_source';
 import { normalizeDataSourceOptions } from './data/data_source/utils';
 import { extend } from './core/utils/extend';
 import ArrayStore from './data/array_store';
 
-const DataControllerMock = {
-    load: () => Deferred().reject(),
-    loadSingle: () => Deferred().reject(),
-    loadFromStore: () => Deferred().reject(),
-    loadNextPage: () => Deferred().reject(),
-    loadOptions: noop,
-    userData: noop,
-    cancel: noop,
-    cancelAll: noop,
-    filter: noop,
-    addSearchFilter: noop,
-    group: noop,
-    paginate: noop,
-    pageSize: noop,
-    pageIndex: noop,
-    resetDataSourcePageIndex: noop,
-    totalCount: noop,
-    isLastPage: noop,
-    isLoading: noop,
-    isLoaded: noop,
-    searchValue: noop,
-    searchOperation: noop,
-    searchExpr: noop,
-    select: noop,
-    key: noop,
-    keyOf: noop,
-    store: noop,
-    items: noop,
-    applyMapFunction: noop,
-    getDataSource: noop,
-    reload: noop,
-    on: noop,
-    off: noop,
-};
-
 class DataController {
     constructor(dataSourceOptions) {
-        if(!dataSourceOptions) {
-            return DataControllerMock;
-        }
         this._initDataSource(dataSourceOptions);
     }
 
@@ -73,49 +33,21 @@ class DataController {
                 widgetDataSourceOptions = {};
                 dataSourceType = DataSource;
 
-                dataSourceOptions = normalizeDataSourceOptions(dataSourceOptions, {
-                    fromUrlLoadMode: null // (DATA_SOURCE_FROM_URL_LOAD_MODE_METHOD in this) && this[DATA_SOURCE_FROM_URL_LOAD_MODE_METHOD]()
-                });
-
+                dataSourceOptions = normalizeDataSourceOptions(dataSourceOptions);
                 this._dataSource = new dataSourceType(extend(true, {}, widgetDataSourceOptions, dataSourceOptions));
             }
-
-            // if(NORMALIZE_DATA_SOURCE in this) {
-            //     this._dataSource = this[NORMALIZE_DATA_SOURCE](this._dataSource);
-            // }
-
-            this._addReadyWatcher();
         }
-    }
-
-    _addReadyWatcher() {
-        // this.readyWatcher = (function(isLoading) {
-        //     this._ready && this._ready(!isLoading);
-        // }).bind(this);
-        // this._dataSource.on('loadingChanged', this.readyWatcher);
     }
 
     _disposeDataSource() {
         if(this._dataSource) {
             if(this._isSharedDataSource) {
                 delete this._isSharedDataSource;
-
-                // this._proxiedDataSourceChangedHandler && this._dataSource.off('changed', this._proxiedDataSourceChangedHandler);
-                // this._proxiedDataSourceLoadErrorHandler && this._dataSource.off('loadError', this._proxiedDataSourceLoadErrorHandler);
-                // this._proxiedDataSourceLoadingChangedHandler && this._dataSource.off('loadingChanged', this._proxiedDataSourceLoadingChangedHandler);
-
-                if(this._dataSource._eventsStrategy) {
-                    // this._dataSource._eventsStrategy.off('loadingChanged', this.readyWatcher);
-                }
             } else {
                 this._dataSource.dispose();
             }
 
             delete this._dataSource;
-
-            // delete this._proxiedDataSourceChangedHandler;
-            // delete this._proxiedDataSourceLoadErrorHandler;
-            // delete this._proxiedDataSourceLoadingChangedHandler;
         }
     }
 
