f53948c1e6d15fe7aff85701e49b4897abbdc2c1
marker dao ® <youdontknow@marker-dao.eth>
2024-01-25 15:11:44 +0700

    feat(selectBox): Add Axe tests

---
+++ packages/devextreme/testing/testcafe/tests/accessibility/selectBox.ts
@@ -0,0 +1,69 @@
+import url from '../../helpers/getPageUrl';
+import { clearTestPage } from '../../helpers/clearPage';
+import { defaultSelector, testAccessibility, Configuration } from '../../helpers/accessibility/test';
+import { isMaterial, isMaterialBased } from '../../helpers/themeUtils';
+import { Options } from '../../helpers/generateOptionMatrix';
+import { Properties } from '../../../../js/ui/select_box.d';
+import SelectBox from '../../model/selectBox';
+
+fixture.disablePageReloads`Accessibility`
+  .page(url(__dirname, '../container.html'))
+  .afterEach(async () => clearTestPage());
+
+const items = [
+  'HD Video Player',
+  'SuperHD Video Player',
+  'SuperPlasma 50',
+];
+
+const options: Options<Properties> = {
+  dataSource: [[], items],
+  value: [undefined, items[0]],
+  disabled: [true, false],
+  readOnly: [true, false],
+  searchEnabled: [true, false],
+  searchTimeout: [0],
+  showClearButton: [true, false],
+  placeholder: [undefined, 'placeholder'],
+  inputAttr: [{ 'aria-label': 'aria-label' }],
+  buttons: [
+    undefined,
+    [
+      {
+        name: 'today',
+        location: 'before',
+        options: {
+          text: 'Today',
+          stylingMode: 'text',
+          onClick: () => {},
+        },
+      },
+    ],
+  ],
+};
+
+const created = async (t: TestController, optionConfiguration): Promise<void> => {
+  if (optionConfiguration.disabled) {
+    return;
+  }
+
+  const selectBox = new SelectBox(defaultSelector);
+  const { input } = selectBox;
+
+  await t.typeText(input, 'hd');
+};
+
+const a11yCheckConfig = isMaterialBased() ? {
+  // NOTE: color-contrast issues in Material
+  runOnly: isMaterial() ? '' : 'color-contrast',
+  rules: { 'color-contrast': { enabled: !isMaterial() } },
+} : {};
+
+const configuration: Configuration = {
+  component: 'dxSelectBox',
+  a11yCheckConfig,
+  options,
+  created,
+};
+
+testAccessibility(configuration);
