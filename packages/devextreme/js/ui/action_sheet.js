962da98ce41843874aa204b1f5759e7fa90f278c
alexander.polosatov <alexander.polosatov@devexpress.com>
2024-01-26 19:33:04 +0400

    fixes after moving to TS

---
+++ packages/devextreme/js/ui/action_sheet.js
@@ -0,0 +1,96 @@
+import ActionSheet from '../__internal/ui/m_action_sheet';
+
+export default ActionSheet;
+
+// STYLE actionSheet
+
+/**
+ * @name dxActionSheetOptions.noDataText
+ * @hidden
+ */
+
+
+/**
+ * @name dxActionSheetOptions.activeStateEnabled
+ * @hidden
+ */
+
+/**
+ * @name dxActionSheetOptions.selectedIndex
+ * @hidden
+ */
+
+/**
+ * @name dxActionSheetOptions.selectedItem
+ * @hidden
+ */
+
+/**
+ * @name dxActionSheetOptions.onSelectionChanged
+ * @action
+ * @hidden
+ */
+
+/**
+ * @name dxActionSheetOptions.selectedItems
+ * @hidden
+ */
+
+/**
+ * @name dxActionSheetOptions.selectedItemKeys
+ * @hidden
+ */
+
+/**
+ * @name dxActionSheetOptions.keyExpr
+ * @hidden
+ */
+
+/**
+ * @name dxActionSheetOptions.accessKey
+ * @hidden
+ */
+
+/**
+ * @name dxActionSheetOptions.tabIndex
+ * @hidden
+ */
+
+/**
+ * @name dxActionSheetOptions.focusStateEnabled
+ * @type boolean
+ * @default false
+ * @hidden
+ */
+
+/**
+ * @name dxActionSheetItem.visible
+ * @type boolean
+ * @default true
+ * @hidden
+ */
+
+/**
+ * @name dxActionSheetItem.html
+ * @type String
+ * @hidden
+ */
+
+/**
+ * @name dxActionSheet.registerKeyHandler
+ * @publicName registerKeyHandler(key, handler)
+ * @hidden
+ */
+
+/**
+ * @name dxActionSheet.focus
+ * @publicName focus()
+ * @hidden
+ */
+
+
+/**
+ * @name dxActionSheetItem
+ * @inherits CollectionWidgetItem
+ * @type object
+ */
---
+++ packages/devextreme/js/ui/select_box.js
@@ -0,0 +1,5 @@
+import SelectBox from '../__internal/ui/m_select_box';
+
+export default SelectBox;
+
+// STYLE selectBox
