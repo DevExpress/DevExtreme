725eb831b73f3029f453cb2228fa576cba0e03de
alexander.polosatov <alexander.polosatov@devexpress.com>
2024-01-26 19:33:04 +0400

    fixes after moving to TS

--- js/__internal/ui/m_autocomplete.ts
+++ js/__internal/ui/m_autocomplete.ts
@@ -1,223 +1,186 @@
-import $ from '../core/renderer';
-import { noop } from '../core/utils/common';
-import registerComponent from '../core/component_registrator';
-import { extend } from '../core/utils/extend';
-import DropDownList from './drop_down_editor/ui.drop_down_list';
-import { Deferred } from '../core/utils/deferred';
-import { isCommandKeyPressed } from '../events/utils/index';
-
-// STYLE autocomplete
+import registerComponent from '@js/core/component_registrator';
+import $ from '@js/core/renderer';
+import { noop } from '@js/core/utils/common';
+import { Deferred } from '@js/core/utils/deferred';
+import { extend } from '@js/core/utils/extend';
+import { isCommandKeyPressed } from '@js/events/utils/index';
+import DropDownList from '@js/ui/drop_down_editor/ui.drop_down_list';
 
 const AUTOCOMPLETE_CLASS = 'dx-autocomplete';
 const AUTOCOMPLETE_POPUP_WRAPPER_CLASS = 'dx-autocomplete-popup-wrapper';
 
-const Autocomplete = DropDownList.inherit({
+const Autocomplete = (DropDownList as any).inherit({
 
-    _supportedKeys: function() {
-        let item = this._list ? this._list.option('focusedElement') : null;
-        const parent = this.callBase();
+  _supportedKeys() {
+    let item = this._list ? this._list.option('focusedElement') : null;
+    const parent = this.callBase();
 
-        item = item && $(item);
+    item = item && $(item);
 
-        return extend({}, parent, {
-            upArrow: function(e) {
-                if(parent.upArrow.apply(this, arguments) && !isCommandKeyPressed(e)) {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    if(item && !this._calcNextItem(-1)) {
-                        this._clearFocusedItem();
-                        return false;
-                    }
-                }
-                return true;
-            },
-            downArrow: function(e) {
-                if(parent.downArrow.apply(this, arguments) && !isCommandKeyPressed(e)) {
-                    e.preventDefault();
-                    e.stopPropagation();
-                    if(item && !this._calcNextItem(1)) {
-                        this._clearFocusedItem();
-                        return false;
-                    }
-                }
-                return true;
-            },
-            enter: function(e) {
-                if(!item) {
-                    this.close();
-                }
-                const opened = this.option('opened');
-                if(opened) {
-                    e.preventDefault();
-                }
-                return opened;
-            }
-        });
-    },
-
-    _getDefaultOptions: function() {
-        return extend(this.callBase(), {
-
-            minSearchLength: 1,
-
-            maxItemCount: 10,
-
-            /**
-            * @name dxAutocompleteOptions.noDataText
-            * @type string
-            * @default ""
-            * @hidden
-            */
-            noDataText: '',
-
-            showDropDownButton: false,
-
-            searchEnabled: true
-
-            /**
-            * @name dxAutocompleteOptions.displayExpr
-            * @hidden
-            */
-
-            /**
-            * @name dxAutocompleteOptions.acceptCustomValue
-            * @hidden
-            */
-
-            /**
-            * @name dxAutocompleteOptions.searchEnabled
-            * @hidden
-            */
-
-            /**
-            * @name dxAutocompleteOptions.showDataBeforeSearch
-            * @hidden
-            */
-        });
-    },
-
-    /**
-    * @name dxAutocomplete.open
-    * @publicName open()
-    * @hidden
-    */
-    /**
-    * @name dxAutocomplete.close
-    * @publicName close()
-    * @hidden
-    */
-
-    _initMarkup: function() {
-        this.callBase();
-        this.$element().addClass(AUTOCOMPLETE_CLASS);
-        this.setAria('autocomplete', 'inline');
-    },
-
-    _displayGetterExpr: function() {
-        return this.option('valueExpr');
-    },
-
-    _closeOutsideDropDownHandler: function({ target }) {
-        return !$(target).closest(this.$element()).length;
-    },
-
-    _renderDimensions: function() {
-        this.callBase();
-
-        this._updatePopupWidth();
-        this._updateListDimensions();
-    },
-
-    _popupWrapperClass: function() {
-        return this.callBase() + ' ' + AUTOCOMPLETE_POPUP_WRAPPER_CLASS;
-    },
-
-    _listConfig: function() {
-        return extend(this.callBase(), {
-            pageLoadMode: 'none',
-            onSelectionChanged: (e) => {
-                this._setSelectedItem(e.addedItems[0]);
-            }
-        });
-    },
-
-    _listItemClickHandler: function(e) {
-        this._saveValueChangeEvent(e.event);
-        const value = this._displayGetter(e.itemData);
-        this.option('value', value);
-        this.close();
-    },
-
-    _setListDataSource: function() {
-        if(!this._list) {
-            return;
+    return extend({}, parent, {
+      upArrow(e) {
+        if (parent.upArrow.apply(this, arguments) && !isCommandKeyPressed(e)) {
+          e.preventDefault();
+          e.stopPropagation();
+          if (item && !this._calcNextItem(-1)) {
+            this._clearFocusedItem();
+            return false;
+          }
         }
-
-        this._list.option('selectedItems', []);
-        this.callBase();
-    },
-
-    _refreshSelected: noop,
-
-    _searchCanceled: function() {
-        this.callBase();
-        this.close();
-    },
-
-    _loadItem: function(value, cache) {
-        const selectedItem = this._getItemFromPlain(value, cache);
-
-        return new Deferred().resolve(selectedItem).promise();
-    },
-
-    _dataSourceOptions: function() {
-        return {
-            paginate: true,
-            pageSize: this.option('maxItemCount')
-        };
-    },
-
-    _searchDataSource: function(searchValue) {
-        this._dataSource.pageSize(this.option('maxItemCount'));
-        this.callBase(searchValue);
-        this._clearFocusedItem();
-    },
-
-    _clearFocusedItem: function() {
-        if(this._list) {
-            this._list.option('focusedElement', null);
-            this._list.option('selectedIndex', -1);
+        return true;
+      },
+      downArrow(e) {
+        if (parent.downArrow.apply(this, arguments) && !isCommandKeyPressed(e)) {
+          e.preventDefault();
+          e.stopPropagation();
+          if (item && !this._calcNextItem(1)) {
+            this._clearFocusedItem();
+            return false;
+          }
         }
-    },
-
-    _renderValueEventName: function() {
-        return 'input keyup';
-    },
-
-    _valueChangeEventHandler: function(e) {
-        const value = this._input().val() || null;
-        return this.callBase(e, value);
-    },
-
-    _optionChanged: function(args) {
-        switch(args.name) {
-            case 'maxItemCount':
-                this._searchDataSource();
-                break;
-            case 'valueExpr':
-                this._compileDisplayGetter();
-                this._setListOption('displayExpr', this._displayGetterExpr());
-                this.callBase(args);
-                break;
-            default:
-                this.callBase(args);
+        return true;
+      },
+      enter(e) {
+        if (!item) {
+          this.close();
         }
-    },
+        const opened = this.option('opened');
+        if (opened) {
+          e.preventDefault();
+        }
+        return opened;
+      },
+    });
+  },
 
-    reset: function() {
-        this.callBase();
-        this.close();
+  _getDefaultOptions() {
+    return extend(this.callBase(), {
+
+      minSearchLength: 1,
+
+      maxItemCount: 10,
+
+      noDataText: '',
+
+      showDropDownButton: false,
+
+      searchEnabled: true,
+
+    });
+  },
+
+  _initMarkup() {
+    this.callBase();
+    this.$element().addClass(AUTOCOMPLETE_CLASS);
+    this.setAria('autocomplete', 'inline');
+  },
+
+  _displayGetterExpr() {
+    return this.option('valueExpr');
+  },
+
+  _closeOutsideDropDownHandler({ target }) {
+    return !$(target).closest(this.$element()).length;
+  },
+
+  _renderDimensions() {
+    this.callBase();
+
+    this._updatePopupWidth();
+    this._updateListDimensions();
+  },
+
+  _popupWrapperClass() {
+    return `${this.callBase()} ${AUTOCOMPLETE_POPUP_WRAPPER_CLASS}`;
+  },
+
+  _listConfig() {
+    return extend(this.callBase(), {
+      pageLoadMode: 'none',
+      onSelectionChanged: (e) => {
+        this._setSelectedItem(e.addedItems[0]);
+      },
+    });
+  },
+
+  _listItemClickHandler(e) {
+    this._saveValueChangeEvent(e.event);
+    const value = this._displayGetter(e.itemData);
+    this.option('value', value);
+    this.close();
+  },
+
+  _setListDataSource() {
+    if (!this._list) {
+      return;
     }
+
+    this._list.option('selectedItems', []);
+    this.callBase();
+  },
+
+  _refreshSelected: noop,
+
+  _searchCanceled() {
+    this.callBase();
+    this.close();
+  },
+
+  _loadItem(value, cache) {
+    const selectedItem = this._getItemFromPlain(value, cache);
+
+    // @ts-expect-error
+    return new Deferred().resolve(selectedItem).promise();
+  },
+
+  _dataSourceOptions() {
+    return {
+      paginate: true,
+      pageSize: this.option('maxItemCount'),
+    };
+  },
+
+  _searchDataSource(searchValue) {
+    this._dataSource.pageSize(this.option('maxItemCount'));
+    this.callBase(searchValue);
+    this._clearFocusedItem();
+  },
+
+  _clearFocusedItem() {
+    if (this._list) {
+      this._list.option('focusedElement', null);
+      this._list.option('selectedIndex', -1);
+    }
+  },
+
+  _renderValueEventName() {
+    return 'input keyup';
+  },
+
+  _valueChangeEventHandler(e) {
+    const value = this._input().val() || null;
+    return this.callBase(e, value);
+  },
+
+  _optionChanged(args) {
+    switch (args.name) {
+      case 'maxItemCount':
+        this._searchDataSource();
+        break;
+      case 'valueExpr':
+        this._compileDisplayGetter();
+        this._setListOption('displayExpr', this._displayGetterExpr());
+        this.callBase(args);
+        break;
+      default:
+        this.callBase(args);
+    }
+  },
+
+  reset() {
+    this.callBase();
+    this.close();
+  },
 });
 
 registerComponent('dxAutocomplete', Autocomplete);
---
+++ js/ui/box.js
@@ -0,0 +1,75 @@
+import Box from '../__internal/ui/m_box';
+
+export default Box;
+
+// STYLE box
+
+/**
+ * @name dxBoxOptions.activeStateEnabled
+ * @hidden
+ */
+
+/**
+ * @name dxBoxOptions.focusStateEnabled
+ * @hidden
+ */
+
+/**
+ * @name dxBoxOptions.hint
+ * @hidden
+ */
+/**
+ * @name dxBoxOptions.noDataText
+ * @hidden
+ */
+/**
+ * @name dxBoxOptions.onSelectionChanged
+ * @action
+ * @hidden
+ */
+/**
+ * @name dxBoxOptions.selectedIndex
+ * @hidden
+ */
+/**
+ * @name dxBoxOptions.selectedItem
+ * @hidden
+ */
+/**
+ * @name dxBoxOptions.selectedItems
+ * @hidden
+ */
+/**
+ * @name dxBoxOptions.selectedItemKeys
+ * @hidden
+ */
+/**
+ * @name dxBoxOptions.keyExpr
+ * @hidden
+ */
+/**
+ * @name dxBoxOptions.tabIndex
+ * @hidden
+ */
+/**
+ * @name dxBoxOptions.accessKey
+ * @hidden
+ */
+
+/**
+ * @name dxBox.registerKeyHandler
+ * @publicName registerKeyHandler(key, handler)
+ * @hidden
+ */
+
+/**
+ * @name dxBox.focus
+ * @publicName focus()
+ * @hidden
+ */
+
+/**
+ * @name dxBoxItem
+ * @inherits CollectionWidgetItem
+ * @type object
+ */
