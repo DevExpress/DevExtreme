b62f7394ddc0e298c708d5f9e15340c0d6d4fa7f
Mark Allen Ramirez <markallenramirez@gmail.com>
2024-01-29 17:41:36 +0800

    manual clean up

--- playgrounds/react/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-it('renders without crashing', () => {
-  const root = createRoot(document.createElement('div'));
-  root.render(<App />);
-});
