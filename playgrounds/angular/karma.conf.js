069fdf028da2cb09d6983c74c55e0916325ffb01
Mark Allen Ramirez <markallenramirez@gmail.com>
2024-01-30 18:02:03 +0800

    angular build and test

---
+++ playgrounds/angular/karma.conf.js
@@ -0,0 +1,29 @@
+/* global process */
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-junit-reporter'),
+      require('karma-webpack'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      jasmine: {
+        random: false,
+      }
+    },
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['ChromeHeadless'],
+    singleRun: true,
+    concurrency: Infinity,
+  });
+};
