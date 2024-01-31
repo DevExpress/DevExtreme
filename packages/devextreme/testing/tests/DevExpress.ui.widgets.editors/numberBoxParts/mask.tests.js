9fc06918a215b3950f48863a05304fc148769633
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2024-02-01 01:55:18 +0400

    fix double-click in data-grid if lazy-loading (T1208575)

--- packages/devextreme/testing/tests/DevExpress.ui.widgets.editors/numberBoxParts/mask.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.widgets.editors/numberBoxParts/mask.tests.js
@@ -2102,9 +2102,11 @@ QUnit.module('format: caret boundaries', moduleConfig, () => {
         this.input.focus();
         this.clock.tick(CARET_TIMEOUT_DURATION);
 
-        this.keyboard.caret(3);
-        this.input.trigger('dxclick');
-        this.clock.tick(CARET_TIMEOUT_DURATION);
+        for(let i = 0; i < 2; ++i) {
+            this.keyboard.caret(3);
+            this.input.trigger('dxclick');
+            this.clock.tick(CARET_TIMEOUT_DURATION);
+        }
 
         assert.timeout(100);
         assert.deepEqual(this.keyboard.caret(), { start: 1, end: 1 }, 'caret is on integer part end');
