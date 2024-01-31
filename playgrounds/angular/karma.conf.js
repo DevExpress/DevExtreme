817d57b2e56da7ce17abae2563fc8712e086d755
Mark Allen Ramirez <markallenramirez@gmail.com>
2024-01-31 15:57:19 +0800

    angular test

--- playgrounds/angular/karma.conf.js
+++ playgrounds/angular/karma.conf.js
@@ -1,15 +1,10 @@
-/* global process */
-process.env.CHROME_BIN = require('puppeteer').executablePath();
-
 module.exports = function (config) {
   config.set({
-    basePath: '',
+    basePath: './',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
-      require('karma-junit-reporter'),
-      require('karma-webpack'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
