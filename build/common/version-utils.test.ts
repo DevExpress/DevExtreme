cd9abf3e4b54799f69eb9d831c059ce56fa3f1f0
alexlavrov <alex.s.lavrov@gmail.com>
2024-02-01 18:57:46 +0400

    Add types for test.each. Remove empty lines

--- build/common/version-utils.test.ts
+++ build/common/version-utils.test.ts
@@ -5,7 +5,7 @@ import {
 
 describe('version utils', () => {
 
-  test.each([
+  test.each<{ value: string | undefined, expected: string | undefined }>([
     { value: undefined, expected: undefined },
     { value: '', expected: undefined },
     { value: '23.2.1', expected: '23.2.1' },
@@ -18,32 +18,32 @@ describe('version utils', () => {
     expect(formatVersion(value)).toBe(expected);
   })
 
-  test.each([
+  test.each<{ baseVersion: string, daily: boolean, date: Date, expected: string }>([
     // minor
-    { baseVersion: '23.2.4', type: 'label', date: new Date(2000, 2, 30, 4, 15), expected: '23.2.4' },
+    { baseVersion: '23.2.4', daily: false, date: new Date(2000, 2, 30, 4, 15), expected: '23.2.4' },
     // daily, possible hotfix
-    { baseVersion: '23.2.4', type: 'daily', date: new Date(1234, 5, 16, 7, 18), expected: '23.2.5-build-34167-0718' },
+    { baseVersion: '23.2.4', daily: true, date: new Date(1234, 5, 16, 7, 18), expected: '23.2.5-build-34167-0718' },
     // before EAP
-    { baseVersion: '24.1.0', type: 'daily', date: new Date(2234, 5, 16, 7, 18), expected: '24.1.1-alpha-34167-0718' },
+    { baseVersion: '24.1.0', daily: true, date: new Date(2234, 5, 16, 7, 18), expected: '24.1.1-alpha-34167-0718' },
     // EAP and after it
-    { baseVersion: '24.1.1', type: 'daily', date: new Date(2000, 0, 1, 22, 33), expected: '24.1.2-alpha-00001-2233' },
+    { baseVersion: '24.1.1', daily: true, date: new Date(2000, 0, 1, 22, 33), expected: '24.1.2-alpha-00001-2233' },
     // beta
-    { baseVersion: '24.1.2', type: 'label', date: new Date(2000, 2, 30, 4, 15), expected: '24.1.2-beta' },
+    { baseVersion: '24.1.2', daily: false, date: new Date(2000, 2, 30, 4, 15), expected: '24.1.2-beta' },
     // daily after beta
-    { baseVersion: '24.1.2', type: 'daily', date: new Date(2000, 2, 30, 4, 15), expected: '24.1.3-build-00090-0415' },
+    { baseVersion: '24.1.2', daily: true, date: new Date(2000, 2, 30, 4, 15), expected: '24.1.3-build-00090-0415' },
     // RTM
-    { baseVersion: '24.1.3', type: 'label', date: new Date(2000, 2, 30, 4, 15), expected: '24.1.3' },
+    { baseVersion: '24.1.3', daily: false, date: new Date(2000, 2, 30, 4, 15), expected: '24.1.3' },
     // daily, possible hotfix
-    { baseVersion: '24.1.3', type: 'daily', date: new Date(1234, 5, 16, 7, 18), expected: '24.1.4-build-34167-0718' },
+    { baseVersion: '24.1.3', daily: true, date: new Date(1234, 5, 16, 7, 18), expected: '24.1.4-build-34167-0718' },
 
-    { baseVersion: '23.2.3', type: 'daily', date: new Date(2001, 2, 30, 4, 15), expected: '23.2.4-build-01089-0415' },
-    { baseVersion: '23.2.3', type: 'daily', date: new Date(2023, 5, 10, 0, 10), expected: '23.2.4-build-23161-0010' },
-   ])('makeVersion [%#]', ({ baseVersion, type, date, expected }) => {
-    expect(makeVersion(baseVersion, type === 'daily',  date)).toBe(expected);
+    { baseVersion: '23.2.3', daily: true, date: new Date(2001, 2, 30, 4, 15), expected: '23.2.4-build-01089-0415' },
+    { baseVersion: '23.2.3', daily: true, date: new Date(2023, 5, 10, 0, 10), expected: '23.2.4-build-23161-0010' },
+   ])('makeVersion [%#]', ({ baseVersion, daily, date, expected }) => {
+    expect(makeVersion(baseVersion, daily, date)).toBe(expected);
   })
 
-  test.each([
-    { date: new Date(2023, 5, 5, 12, 30) },
+  test.each<{ baseVersion: string | undefined, date: Date }>([
+    { baseVersion: undefined, date: new Date(2023, 5, 5, 12, 30) },
     { baseVersion: '1234.5', date: new Date(2023, 5, 5, 12, 30) },
     { baseVersion: '123456', date: new Date(2023, 5, 5, 12, 30) },
     { baseVersion: 'abcdef', date: new Date(2023, 5, 5, 12, 30) },
@@ -51,6 +51,3 @@ describe('version utils', () => {
     expect(() => { makeVersion(baseVersion, true,  date) }).toThrow(Error);
   })
 });
-
-
-
