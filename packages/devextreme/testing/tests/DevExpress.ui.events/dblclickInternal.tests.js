8ee07c76b0e7f94d9a6bd600676550362fefc3f6
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2024-02-03 01:11:43 +0400

    WIP

--- packages/devextreme/testing/tests/DevExpress.ui.events/dblclickInternal.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.events/dblclickInternal.tests.js
@@ -1,30 +1,33 @@
 const $ = require('jquery');
+const { on, off, trigger } = require('events');
 const dblclickEvent = { name: 'dxdblclick' };
 
 QUnit.testStart(function() {
     const markup =
-        '<div id="element"></div>';
+        '<div id="element">TEST</div>';
 
     $('#qunit-fixture').html(markup);
 });
 
 QUnit.module('event firing');
 
-QUnit.test('click handler on document for dxddblclick should be only one despite many removing', function(assert) {
+QUnit.test('dxdblclick should be added correctly even if its module imported between "on()" calls (T1208575)', function(assert) {
     const el = $('#element');
     const handler = sinon.stub();
 
-    el.on(dblclickEvent.name, handler);
+    on(el, dblclickEvent.name, handler);
 
     const dblClickEventOrig = require('events/dblclick');
 
-    assert.equal(dblClickEventOrig.name, 'dxdblclick');
+    assert.equal(dblClickEventOrig.name, dblclickEvent.name);
 
-    el.off(dblclickEvent.name);
+    off(el, dblclickEvent.name);
 
-    el.on(dblclickEvent.name, handler);
-    el.trigger(dblclickEvent.name);
+    on(el, dblclickEvent.name, handler);
+    on(el, dblclickEvent.name, () => {});
 
-    assert.equal(handler.callCount, 1);
+    trigger(el, 'dxclick');
+
+    assert.equal(handler.callCount, 0);
 });
 
