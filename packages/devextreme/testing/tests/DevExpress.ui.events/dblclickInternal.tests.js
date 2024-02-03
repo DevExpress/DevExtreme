b14d1bde329ffb24eb21178d77e63c717bd61309
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2024-02-03 11:00:22 +0400

    WIP

--- packages/devextreme/testing/tests/DevExpress.ui.events/dblclickInternal.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.events/dblclickInternal.tests.js
@@ -27,7 +27,8 @@ QUnit.test('dxdblclick should be added correctly even if its module imported bet
     on(el, dblclickEvent.name, () => {});
 
     trigger(el, 'dxclick');
+    trigger(el, 'dxclick');
 
-    assert.equal(handler.callCount, 1);
+    assert.equal(handler.callCount, 2);
 });
 
