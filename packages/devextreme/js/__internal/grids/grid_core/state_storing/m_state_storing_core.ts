834fc7f1162ddcc34ee5f741fe20753d025507fa
Roman Semenov <roman.semenov@devexpress.com>
2024-02-03 22:59:40 +0400

    move state storing controller

--- packages/devextreme/js/__internal/grids/grid_core/state_storing/m_state_storing_core.ts
+++ packages/devextreme/js/__internal/grids/grid_core/state_storing/m_state_storing_core.ts
@@ -26,139 +26,147 @@ const parseDates = function (state) {
   });
 };
 
-const StateStoringController = modules.ViewController.inherit((function () {
-  const getStorage = function (options) {
-    const storage = options.type === 'sessionStorage' ? sessionStorage() : getWindow().localStorage;
+const getStorage = function (options) {
+  const storage = options.type === 'sessionStorage' ? sessionStorage() : getWindow().localStorage;
 
-    if (!storage) {
-      throw new Error('E1007');
+  if (!storage) {
+    throw new Error('E1007');
+  }
+
+  return storage;
+};
+
+const getUniqueStorageKey = function (options) {
+  return isDefined(options.storageKey) ? options.storageKey : 'storage';
+};
+
+class StateStoringController extends modules.ViewController {
+  private _state: any;
+
+  private _isLoaded: any;
+
+  private _isLoading: any;
+
+  private _windowUnloadHandler: any;
+
+  private _savingTimeoutID: any;
+
+  _loadState() {
+    const options = this.option('stateStoring')!;
+
+    if (options.type === 'custom') {
+      return options.customLoad && options.customLoad();
     }
+    try {
+      // @ts-expect-error
+      return JSON.parse(getStorage(options).getItem(getUniqueStorageKey(options)));
+    } catch (e: any) {
+      errors.log('W1022', 'State storing', e.message);
+    }
+  }
 
-    return storage;
-  };
+  _saveState(state) {
+    const options = this.option('stateStoring')!;
 
-  const getUniqueStorageKey = function (options) {
-    return isDefined(options.storageKey) ? options.storageKey : 'storage';
-  };
+    if (options.type === 'custom') {
+      options.customSave && options.customSave(state);
+      return;
+    }
+    try {
+      getStorage(options).setItem(getUniqueStorageKey(options), JSON.stringify(state));
+    } catch (e: any) {
+      errors.log(e.message);
+    }
+  }
 
-  return {
-    _loadState() {
-      const options = this.option('stateStoring');
+  publicMethods() {
+    return ['state'];
+  }
 
-      if (options.type === 'custom') {
-        return options.customLoad && options.customLoad();
-      }
-      try {
-        // @ts-expect-error
-        return JSON.parse(getStorage(options).getItem(getUniqueStorageKey(options)));
-      } catch (e: any) {
-        errors.log('W1022', 'State storing', e.message);
-      }
-    },
+  isEnabled() {
+    return this.option('stateStoring.enabled');
+  }
 
-    _saveState(state) {
-      const options = this.option('stateStoring');
+  init() {
+    const that = this;
 
-      if (options.type === 'custom') {
-        options.customSave && options.customSave(state);
-        return;
-      }
-      try {
-        getStorage(options).setItem(getUniqueStorageKey(options), JSON.stringify(state));
-      } catch (e: any) {
-        errors.log(e.message);
-      }
-    },
+    that._state = {};
+    that._isLoaded = false;
+    that._isLoading = false;
 
-    publicMethods() {
-      return ['state'];
-    },
-
-    isEnabled() {
-      return this.option('stateStoring.enabled');
-    },
-
-    init() {
-      const that = this;
-
-      that._state = {};
-      that._isLoaded = false;
-      that._isLoading = false;
-
-      that._windowUnloadHandler = function () {
-        if (that._savingTimeoutID !== undefined) {
-          that._saveState(that.state());
-        }
-      };
-
-      eventsEngine.on(getWindow(), 'unload', that._windowUnloadHandler);
-
-      return that;
-    },
-
-    isLoaded() {
-      return this._isLoaded;
-    },
-
-    isLoading() {
-      return this._isLoading;
-    },
-
-    load() {
-      this._isLoading = true;
-      const loadResult = fromPromise(this._loadState());
-      loadResult.always(() => {
-        this._isLoaded = true;
-        this._isLoading = false;
-      }).done((state) => {
-        if (state !== null && !isEmptyObject(state)) {
-          this.state(state);
-        }
-      });
-      return loadResult;
-    },
-
-    state(state) {
-      const that = this;
-
-      if (!arguments.length) {
-        return extend(true, {}, that._state);
-      }
-      that._state = extend({}, state);
-      parseDates(that._state);
-    },
-
-    save() {
-      const that = this;
-
-      clearTimeout(that._savingTimeoutID);
-      that._savingTimeoutID = setTimeout(() => {
+    that._windowUnloadHandler = function () {
+      if (that._savingTimeoutID !== undefined) {
         that._saveState(that.state());
-        that._savingTimeoutID = undefined;
-      }, that.option('stateStoring.savingTimeout'));
-    },
-
-    optionChanged(args) {
-      const that = this;
-
-      switch (args.name) {
-        case 'stateStoring':
-          if (that.isEnabled() && !that.isLoading()) {
-            that.load();
-          }
-
-          args.handled = true;
-          break;
-        default:
-          that.callBase(args);
       }
-    },
+    };
 
-    dispose() {
-      clearTimeout(this._savingTimeoutID);
-      eventsEngine.off(getWindow(), 'unload', this._windowUnloadHandler);
-    },
-  };
-})());
+    eventsEngine.on(getWindow(), 'unload', that._windowUnloadHandler);
+
+    return that;
+  }
+
+  isLoaded() {
+    return this._isLoaded;
+  }
+
+  isLoading() {
+    return this._isLoading;
+  }
+
+  load() {
+    this._isLoading = true;
+    const loadResult = fromPromise(this._loadState());
+    loadResult.always(() => {
+      this._isLoaded = true;
+      this._isLoading = false;
+    }).done((state) => {
+      if (state !== null && !isEmptyObject(state)) {
+        this.state(state);
+      }
+    });
+    return loadResult;
+  }
+
+  state(state?) {
+    const that = this;
+
+    if (!arguments.length) {
+      return extend(true, {}, that._state);
+    }
+    that._state = extend({}, state);
+    parseDates(that._state);
+  }
+
+  save() {
+    const that = this;
+
+    clearTimeout(that._savingTimeoutID);
+    that._savingTimeoutID = setTimeout(() => {
+      that._saveState(that.state());
+      that._savingTimeoutID = undefined;
+    }, that.option('stateStoring.savingTimeout'));
+  }
+
+  optionChanged(args) {
+    const that = this;
+
+    switch (args.name) {
+      case 'stateStoring':
+        if (that.isEnabled() && !that.isLoading()) {
+          that.load();
+        }
+
+        args.handled = true;
+        break;
+      default:
+        super.optionChanged(args);
+    }
+  }
+
+  dispose() {
+    clearTimeout(this._savingTimeoutID);
+    eventsEngine.off(getWindow(), 'unload', this._windowUnloadHandler);
+  }
+}
 
 export default { StateStoringController };
