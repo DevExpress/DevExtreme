fb3fd5a709113296ef979141bf1757746e92160d
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2024-02-03 13:14:55 +0400

    WIP

--- packages/devextreme/testing/tests/DevExpress.ui.events/dblclickInternal.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.events/dblclickInternal.tests.js
@@ -16,16 +16,15 @@ QUnit.test('dxdblclick should be works correctly even if its module imported bet
     const el = $('#element');
     const handler = sinon.stub();
 
-    // const dblClickEventOrig = require('events/dblclick');
+    const dblClickEventOrig = require('events/dblclick');
 
-    // assert.equal(dblClickEventOrig.name, dblclickEvent.name);
+    assert.equal(dblClickEventOrig.name, dblclickEvent.name);
 
     el.off(dblclickEvent.name);
 
     el.on(dblclickEvent.name, handler);
     el.on(dblclickEvent.name, () => {});
 
-    el.trigger('dxclick');
     el.trigger('dxclick');
 
     assert.equal(handler.callCount, 3);
