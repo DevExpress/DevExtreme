d6d4d63281c3916b281dc27d6b1a767c78aa124f
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2024-02-03 15:08:04 +0400

    WIP

--- packages/devextreme/testing/tests/DevExpress.ui.events/dblclickInternal.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.events/dblclickInternal.tests.js
@@ -15,18 +15,22 @@ QUnit.module('event firing');
 QUnit.test('dxdblclick should be works correctly even if its module imported between "on()" calls (T1208575)', function(assert) {
     const el = $('#element');
     const handler = sinon.stub();
+    const done = assert.async();
 
-    const dblClickEventOrig = require('events/dblclick');
+    import('events/dblclick').then((m) => {
+        assert.equal(m.name, dblclickEvent.name);
 
-    assert.equal(dblClickEventOrig.name, dblclickEvent.name);
+        el.off(dblclickEvent.name);
 
-    el.off(dblclickEvent.name);
+        el.on(dblclickEvent.name, handler);
+        el.on(dblclickEvent.name, () => {});
 
-    el.on(dblclickEvent.name, handler);
-    el.on(dblclickEvent.name, () => {});
+        el.trigger('dxclick');
 
-    el.trigger('dxclick');
+        assert.equal(handler.callCount, 3);
+        done();
+    });
 
-    assert.equal(handler.callCount, 3);
+
 });
 
