be685bca0feea8fa243ec9133f41a280854259b6
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2024-02-04 13:41:59 +0400

    WIP

--- packages/devextreme/testing/tests/DevExpress.ui.events/dblclickInternal.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.events/dblclickInternal.tests.js
@@ -1,4 +1,10 @@
 const $ = require('jquery');
+import registerEvent from './core/event_registrator';
+registerEvent.inject(function(name, config) {
+    top.LOG = top.LOG || ['registerEvent '];
+    return registerEvent(name, config);
+});
+
 const dblclickEvent = { name: 'dxdblclick' };
 
 QUnit.testStart(function() {
