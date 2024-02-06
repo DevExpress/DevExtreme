7ef56e59a66d3e83de6b69c77786e6e5ba8b8241
Roman Semenov <roman.semenov@devexpress.com>
2024-02-07 00:11:08 +0400

    DataGrid - native classes - state storing (#26558)
    (cherry picked from commit 42f4767a6a503e9a7c15aeb689d05306750a12f9)
    (cherry picked from commit 473e5895002a1ec423a86f2595734991b5093a66)

--- js/__internal/grids/grid_core/views/m_rows_view.ts
+++ js/__internal/grids/grid_core/views/m_rows_view.ts
@@ -1079,7 +1079,7 @@ export class RowsView extends ColumnsView {
     return undefined;
   }
 
-  setLoading(isLoading, messageText) {
+  setLoading(isLoading, messageText?) {
     const that = this;
     let loadPanel = that._loadPanel;
     const dataController = that._dataController;
--- js/__internal/grids/tree_list/m_state_storing.ts
+++ js/__internal/grids/tree_list/m_state_storing.ts
@@ -1,30 +1,43 @@
-import { extend } from '@js/core/utils/extend';
+// eslint-disable-next-line max-classes-per-file
 import { stateStoringModule } from '@ts/grids/grid_core/state_storing/m_state_storing';
 
+import { DataController } from '../grid_core/data_controller/m_data_controller';
+import { ModuleType } from '../grid_core/m_types';
+import { StateStoringController } from '../grid_core/state_storing/m_state_storing_core';
 import treeListCore from './m_core';
 
-const origApplyState = stateStoringModule.extenders.controllers.stateStoring.applyState;
+const stateStoring = (
+  Base: ModuleType<StateStoringController>,
+) => class TreeListStateStoringExtender extends stateStoringModule.extenders.controllers.stateStoring(Base) {
+  applyState(state) {
+    super.applyState(state);
+    // @ts-expect-error
+    this.option('expandedRowKeys', state.expandedRowKeys ? state.expandedRowKeys.slice() : []);
+  }
+};
 
-treeListCore.registerModule('stateStoring', extend(true, {}, stateStoringModule, {
+const data = (
+  Base: ModuleType<DataController>,
+) => class TreeListStateStoringDataExtender extends stateStoringModule.extenders.controllers.data(Base) {
+  getUserState() {
+    const state = super.getUserState();
+
+    if (!this.option('autoExpandAll')) {
+      state.expandedRowKeys = this.option('expandedRowKeys');
+    }
+
+    return state;
+  }
+};
+
+treeListCore.registerModule('stateStoring', {
+  ...stateStoringModule,
   extenders: {
+    ...stateStoringModule.extenders,
     controllers: {
-      stateStoring: {
-        applyState(state) {
-          origApplyState.apply(this, arguments as any);
-          this.option('expandedRowKeys', state.expandedRowKeys ? state.expandedRowKeys.slice() : []);
-        },
-      },
-      data: {
-        getUserState() {
-          const state = this.callBase.apply(this, arguments);
-
-          if (!this.option('autoExpandAll')) {
-            state.expandedRowKeys = this.option('expandedRowKeys');
-          }
-
-          return state;
-        },
-      },
+      ...stateStoringModule.extenders.controllers,
+      stateStoring,
+      data,
     },
   },
-}));
+});
