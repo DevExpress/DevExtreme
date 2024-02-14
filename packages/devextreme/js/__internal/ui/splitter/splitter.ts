2198d36b18c67470586fd8adbe478c035e2ff85f
AlexanderMoiseev <moiseevalexandr1@gmail.com>
2024-02-14 19:28:41 +0400

    add resize options for splitter

--- packages/devextreme/js/__internal/ui/splitter/splitter.ts
+++ packages/devextreme/js/__internal/ui/splitter/splitter.ts
@@ -4,12 +4,24 @@ import { extend } from '@js/core/utils/extend';
 import CollectionWidgetItem from '@js/ui/collection/item';
 import type { CollectionWidgetItem as Item } from '@js/ui/collection/ui.collection_widget.base';
 import CollectionWidget from '@js/ui/collection/ui.collection_widget.live_update';
+import { ResizeEndEvent, ResizeStartEvent } from '@js/ui/resizable';
+
+import Guid from '../../../core/guid';
+import eventsEngine from '../../../events/core/events_engine';
+import { end as eventEnd, move as eventMove, start as eventStart } from '../../../events/drag';
+import { addNamespace } from '../../../events/utils/index';
 
 const SPLITTER_CLASS = 'dx-splitter';
 const SPLITTER_ITEM_CLASS = 'dx-splitter-item';
 const SPLITTER_ITEM_DATA_KEY = 'dxSplitterItemData';
 const HORIZONTAL_DIRECTION_CLASS = 'dx-splitter-horizontal';
 const VERTICAL_DIRECTION_CLASS = 'dx-splitter-vertical';
+const SPLITTER_MODULE_NAMESPACE = 'dxSplitter';
+
+const RESIZE_DIRECTION = {
+  horizontal: 'horizontal',
+  vertical: 'vertical',
+};
 
 class SplitterItem extends CollectionWidgetItem {
 }
@@ -19,8 +31,19 @@ class Splitter extends (CollectionWidget as any) {
   _getDefaultOptions(): Record<string, unknown> {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return extend(super._getDefaultOptions(), {
-      direction: 'horizontal',
+      direction: RESIZE_DIRECTION.horizontal,
+      onResize: null,
+      onResizeEnd: null,
+      onResizeStart: null,
     });
+  }
+
+  _init(): void {
+    super._init();
+    const namespace = `${SPLITTER_MODULE_NAMESPACE}${new Guid().toString()}`;
+    this.RESIZE_START_EVENT_NAME = addNamespace(eventStart, namespace);
+    this.RESIZE_EVENT_NAME = addNamespace(eventMove, namespace);
+    this.RESIZE_END_EVENT_NAME = addNamespace(eventEnd, namespace);
   }
 
   // eslint-disable-next-line class-methods-use-this
@@ -36,8 +59,36 @@ class Splitter extends (CollectionWidget as any) {
   _initMarkup(): void {
     this.$element().addClass(SPLITTER_CLASS);
 
-    this._toggleDirection();
+    this._toggleDirectionClass();
     super._initMarkup();
+  }
+
+  _render(): void {
+    super._render();
+
+    this._createResizeStartAction();
+    this._createResizeAction();
+    this._createResizeEndAction();
+    this._attachEventHandlers();
+  }
+
+  _resizeStartHandler(e: ResizeStartEvent): void {
+    this._resizeStartAction({
+      event: e,
+    });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  _resizeHandler(e): void {
+    this._resizeAction({
+      event: e,
+    });
+  }
+
+  _resizeEndHandler(e: ResizeEndEvent): void {
+    this._resizeEndAction({
+      event: e,
+    });
   }
 
   _renderItems(items: Item[]): void {
@@ -57,16 +108,64 @@ class Splitter extends (CollectionWidget as any) {
     return this.option('direction') === 'horizontal';
   }
 
-  _toggleDirection(): void {
+  _toggleDirectionClass(): void {
     this.$element().toggleClass(HORIZONTAL_DIRECTION_CLASS, this._isHorizontalDirection());
     this.$element().toggleClass(VERTICAL_DIRECTION_CLASS, !this._isHorizontalDirection());
+  }
+
+  _createResizeAction(): void {
+    this._resizeAction = this._createActionByOption('onResize');
+  }
+
+  _createResizeStartAction(): void {
+    this._resizeStartAction = this._createActionByOption('onResizeStart');
+  }
+
+  _createResizeEndAction(): void {
+    this._resizeEndAction = this._createActionByOption('onResizeEnd');
+  }
+
+  _attachEventHandlers(): void {
+    const eventData = { direction: this.option('direction'), immediate: true };
+
+    eventsEngine.on(
+      this.$element(),
+      this.RESIZE_START_EVENT_NAME,
+      eventData,
+      this._resizeStartHandler.bind(this),
+    );
+
+    eventsEngine.on(
+      this.$element(),
+      this.RESIZE_EVENT_NAME,
+      eventData,
+      this._resizeHandler.bind(this),
+    );
+
+    eventsEngine.on(
+      this.$element(),
+      this.RESIZE_END_EVENT_NAME,
+      eventData,
+      this._resizeEndHandler.bind(this),
+    );
+  }
+
+  _detachEventHandlers(): void {
+    // @ts-expect-error todo: make optional parameters for eventsEngine
+    eventsEngine.off(this.$element(), this.RESIZE_START_EVENT_NAME);
+    // @ts-expect-error todo: make optional parameters for eventsEngine
+    eventsEngine.off(this.$element(), this.RESIZE_EVENT_NAME);
+    // @ts-expect-error todo: make optional parameters for eventsEngine
+    eventsEngine.off(this.$element(), this.RESIZE_END_EVENT_NAME);
   }
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   _optionChanged(args): void {
     switch (args.name) {
       case 'direction':
-        this._toggleDirection();
+        this._toggleDirectionClass();
+        this._detachEventHandlers();
+        this._attachEventHandlers();
         break;
       default:
         super._optionChanged(args);
--- packages/devextreme/testing/tests/DevExpress.ui.widgets.editors/splitter.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.widgets.editors/splitter.tests.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import Splitter from 'ui/splitter';
 import fx from 'animation/fx';
+import pointerMock from '../../helpers/pointerMock.js';
 
 QUnit.testStart(() => {
     const markup =
@@ -19,6 +20,12 @@ const moduleConfig = {
         };
 
         init();
+
+        this.reinit = (options) => {
+            this.instance.dispose();
+
+            init(options);
+        };
     },
     afterEach: function() {
         fx.off = false;
@@ -26,11 +33,63 @@ const moduleConfig = {
 };
 
 QUnit.module('Splitter Initialization', moduleConfig, () => {
-    QUnit.test('Splitter should be initialized with correct type', function(assert) {
+    QUnit.test('Splitter should be initialized with Splitter type', function(assert) {
         assert.ok(this.instance instanceof Splitter);
     });
+});
 
-    QUnit.test('Splitter should be initialized with horizontal direction by default', function(assert) {
-        assert.strictEqual(this.instance.option('direction'), 'horizontal');
+QUnit.module('Events', moduleConfig, () => {
+    const checkHandlerCalled = (init, assert, pointer) => {
+        const resizeHandlerStub = sinon.stub();
+        init(resizeHandlerStub);
+        pointer.start().dragStart().drag(0, 50).dragEnd();
+        assert.strictEqual(resizeHandlerStub.callCount, 1);
+    };
+
+    ['onResizeStart', 'onResize', 'onResizeEnd'].forEach(eventHandler => {
+        QUnit.test(`${eventHandler} event handler should be fired`, function(assert) {
+            checkHandlerCalled((resizeHandlerStub) => this.reinit({ [eventHandler]: resizeHandlerStub }), assert, pointerMock(this.$element));
+        });
+
+        QUnit.test(`${eventHandler} event handler should be able to be updated at runtime`, function(assert) {
+            checkHandlerCalled((resizeHandlerStub) => this.instance.option('onResizeStart', resizeHandlerStub), assert, pointerMock(this.$element));
+        });
+
+        QUnit.test(`${eventHandler} should be called once after direction option changed`, function(assert) {
+            checkHandlerCalled((resizeHandlerStub) => {
+                this.reinit({ [eventHandler]: resizeHandlerStub });
+                this.instance.option('direction', 'vertical');
+            }, assert, pointerMock(this.$element));
+        });
+    });
+
+    ['resizeStart', 'resize', 'resizeEnd'].forEach(eventHandler => {
+        QUnit.test(`${eventHandler} event handler should be correctly added with "on" function`, function(assert) {
+            checkHandlerCalled((resizeHandlerStub) => this.instance.on(eventHandler, resizeHandlerStub), assert, pointerMock(this.$element));
+        });
+    });
+
+    [['onResizeStart', 'dxdragstart'], ['onResize', 'dxdrag'], ['onResizeEnd', 'dxdragend']].forEach(eventData => {
+        QUnit.test(`${eventData[0]} event handler should recieve correct arguments`, function(assert) {
+            const eventHandlerName = eventData[0];
+            const eventName = eventData[1];
+
+            const checkArgs = (e, expectedEventName) => {
+                assert.strictEqual(e.component.NAME, this.instance.NAME);
+                assert.strictEqual($(e.element).get(0), this.$element.get(0));
+                assert.strictEqual(e.event.type, expectedEventName);
+                assert.strictEqual($(e.event.target).get(0), this.$element.get(0));
+            };
+
+            this.reinit({
+                [eventHandlerName]: function(e) {
+                    checkArgs(e, eventName);
+                },
+            });
+
+            const pointer = pointerMock(this.$element);
+
+            pointer.start().dragStart().drag(0, 10).dragEnd();
+        });
     });
 });
