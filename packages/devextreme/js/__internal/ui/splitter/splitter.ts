382ea4506f7f7fcbea61ae29f37628cff2afd501
AlexanderMoiseev <moiseevalexandr1@gmail.com>
2024-02-16 14:21:44 +0400

    Splitter: add resize options and render handle after splitter item (#26669)
    Co-authored-by: EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>

--- packages/devextreme/js/__internal/ui/splitter/splitter.ts
+++ packages/devextreme/js/__internal/ui/splitter/splitter.ts
@@ -1,15 +1,27 @@
 // eslint-disable-next-line max-classes-per-file
 import registerComponent from '@js/core/component_registrator';
+import $ from '@js/core/renderer';
 import { extend } from '@js/core/utils/extend';
 import CollectionWidgetItem from '@js/ui/collection/item';
 import type { CollectionWidgetItem as Item } from '@js/ui/collection/ui.collection_widget.base';
 import CollectionWidget from '@js/ui/collection/ui.collection_widget.live_update';
 
+import ResizeHandle from './resize_handle';
+import {
+  getActionNameByEventName,
+  RESIZE_EVENT,
+} from './utils/event';
+
 const SPLITTER_CLASS = 'dx-splitter';
 const SPLITTER_ITEM_CLASS = 'dx-splitter-item';
 const SPLITTER_ITEM_DATA_KEY = 'dxSplitterItemData';
-const HORIZONTAL_DIRECTION_CLASS = 'dx-splitter-horizontal';
-const VERTICAL_DIRECTION_CLASS = 'dx-splitter-vertical';
+const HORIZONTAL_ORIENTATION_CLASS = 'dx-splitter-horizontal';
+const VERTICAL_ORIENTATION_CLASS = 'dx-splitter-vertical';
+
+const ORIENTATION = {
+  horizontal: 'horizontal',
+  vertical: 'vertical',
+};
 
 class SplitterItem extends CollectionWidgetItem {
 }
@@ -19,7 +31,10 @@ class Splitter extends (CollectionWidget as any) {
   _getDefaultOptions(): Record<string, unknown> {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-return
     return extend(super._getDefaultOptions(), {
-      direction: 'horizontal',
+      orientation: ORIENTATION.horizontal,
+      onResize: null,
+      onResizeEnd: null,
+      onResizeStart: null,
     });
   }
 
@@ -36,12 +51,64 @@ class Splitter extends (CollectionWidget as any) {
   _initMarkup(): void {
     this.$element().addClass(SPLITTER_CLASS);
 
-    this._toggleDirection();
+    this._toggleOrientationClass();
     super._initMarkup();
   }
 
   _renderItems(items: Item[]): void {
     super._renderItems(items);
+  }
+
+  _itemsCount(): number {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return this.option('items').length;
+  }
+
+  _isLastItem(index: number): boolean {
+    return index === this._itemsCount() - 1;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  _renderItem(index, itemData, $container, $itemToReplace): unknown {
+    const $itemFrame = super._renderItem(index, itemData, $container, $itemToReplace);
+
+    if (!this._isLastItem(index)) {
+      this._renderResizeHandle();
+    }
+
+    return $itemFrame;
+  }
+
+  _renderResizeHandle(): void {
+    const $resizeHandle = $('<div>').appendTo(this.$element());
+
+    this._createComponent($resizeHandle, ResizeHandle, this._getResizeHandleConfig());
+  }
+
+  _getAction(eventName: string): (e) => void {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return this[getActionNameByEventName(eventName)] ?? this._createActionByOption(eventName);
+  }
+
+  _getResizeHandleConfig(): object {
+    return {
+      direction: this.option('orientation') === ORIENTATION.vertical ? 'horizontal' : 'vertical',
+      onResizeStart: (e): void => {
+        this._getAction(RESIZE_EVENT.onResizeStart)({
+          event: e,
+        });
+      },
+      onResize: (e): void => {
+        this._getAction(RESIZE_EVENT.onResize)({
+          event: e,
+        });
+      },
+      onResizeEnd: (e): void => {
+        this._getAction(RESIZE_EVENT.onResizeEnd)({
+          event: e,
+        });
+      },
+    };
   }
 
   _renderItemContent(args: object): object {
@@ -53,20 +120,27 @@ class Splitter extends (CollectionWidget as any) {
     super._itemOptionChanged(item, property, value);
   }
 
-  _isHorizontalDirection(): boolean {
-    return this.option('direction') === 'horizontal';
+  _isHorizontalOrientation(): boolean {
+    return this.option('orientation') === ORIENTATION.horizontal;
   }
 
-  _toggleDirection(): void {
-    this.$element().toggleClass(HORIZONTAL_DIRECTION_CLASS, this._isHorizontalDirection());
-    this.$element().toggleClass(VERTICAL_DIRECTION_CLASS, !this._isHorizontalDirection());
+  _toggleOrientationClass(): void {
+    this.$element().toggleClass(HORIZONTAL_ORIENTATION_CLASS, this._isHorizontalOrientation());
+    this.$element().toggleClass(VERTICAL_ORIENTATION_CLASS, !this._isHorizontalOrientation());
   }
 
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   _optionChanged(args): void {
-    switch (args.name) {
-      case 'direction':
-        this._toggleDirection();
+    const { name } = args;
+
+    switch (name) {
+      case 'orientation':
+        this._toggleOrientationClass();
+        break;
+      case 'onResizeStart':
+      case 'onResizeEnd':
+      case 'onResize':
+        this[getActionNameByEventName(name)] = this._createActionByOption(name);
         break;
       default:
         super._optionChanged(args);
--- packages/devextreme/testing/tests/DevExpress.ui.widgets.editors/splitter.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.widgets.editors/splitter.tests.js
@@ -1,6 +1,10 @@
 import $ from 'jquery';
 import Splitter from 'ui/splitter';
 import fx from 'animation/fx';
+import pointerMock from '../../helpers/pointerMock.js';
+
+const SPLITTER_ITEM_CLASS = 'dx-splitter-item';
+const RESIZE_HANDLE = 'dx-resize-handle';
 
 QUnit.testStart(() => {
     const markup =
@@ -19,18 +23,104 @@ const moduleConfig = {
         };
 
         init();
+
+        this.reinit = (options) => {
+            this.instance.dispose();
+
+            init(options);
+        };
     },
     afterEach: function() {
         fx.off = false;
     }
 };
 
-QUnit.module('Splitter Initialization', moduleConfig, () => {
-    QUnit.test('Splitter should be initialized with correct type', function(assert) {
+QUnit.module('Initialization', moduleConfig, () => {
+    QUnit.test('Splitter should be initialized with Splitter type', function(assert) {
         assert.ok(this.instance instanceof Splitter);
     });
 
-    QUnit.test('Splitter should be initialized with horizontal direction by default', function(assert) {
-        assert.strictEqual(this.instance.option('direction'), 'horizontal');
+    QUnit.test('items count should be the same as datasource items count', function(assert) {
+        this.reinit({ dataSource: [{ text: 'pane 1' }, { text: 'pane 2' }, { text: 'pane 3' }] });
+
+        const items = this.$element.find(`.${SPLITTER_ITEM_CLASS}`);
+
+        assert.strictEqual(items.length, 3);
+    });
+
+    QUnit.test('items should be able to be initialized with template', function(assert) {
+        this.reinit({ dataSource: [{
+            template: () => $('<div>').text('Pane 1') }, {
+            template: () => $('<div>').text('Pane 2') }, {
+            template: () => $('<div>').text('Pane 3') }]
+        });
+
+        const items = this.$element.find(`.${SPLITTER_ITEM_CLASS}`);
+
+        assert.strictEqual(items.length, 3);
+    });
+
+    QUnit.test('Splitter with three items should have two resize handles', function(assert) {
+        this.reinit({ dataSource: [{ text: 'pane 1' }, { text: 'pane 2' }, { text: 'pane 3' }] });
+
+        const handles = this.$element.find(`.${RESIZE_HANDLE}`);
+
+        assert.strictEqual(handles.length, 2);
+    });
+
+    QUnit.test('Splitter with one item should have no handles', function(assert) {
+        this.reinit({ dataSource: [{ template: () => $('<div>').text('Pane 1') }] });
+        const handles = this.$element.find(`.${RESIZE_HANDLE}`);
+
+        assert.strictEqual(handles.length, 0);
+    });
+
+    QUnit.test('Splitter with no items should have no handles', function(assert) {
+        const handles = this.$element.find(`.${RESIZE_HANDLE}`);
+
+        assert.strictEqual(handles.length, 0);
+    });
+});
+
+
+QUnit.module('Events', moduleConfig, () => {
+    ['onResizeStart', 'onResize', 'onResizeEnd'].forEach(eventHandler => {
+        QUnit.test(`${eventHandler} should be called when handle dragged`, function(assert) {
+            const resizeHandlerStub = sinon.stub();
+            this.reinit({
+                [eventHandler]: resizeHandlerStub,
+                dataSource: [{ text: 'pane 1' }, { text: 'pane 2' }]
+            });
+
+            const handles = this.$element.find(`.${RESIZE_HANDLE}`);
+
+            const pointer = pointerMock(handles[0]);
+
+            pointer.start().dragStart().drag(0, 50).dragEnd();
+
+            assert.strictEqual(resizeHandlerStub.callCount, 1);
+        });
+
+        QUnit.test(`${eventHandler} event handler should be able to be updated at runtime`, function(assert) {
+            const handlerStub = sinon.stub();
+            const handlerStubAfterUpdate = sinon.stub();
+
+            this.reinit({
+                [eventHandler]: handlerStub,
+                dataSource: [{ text: 'pane 1' }, { text: 'pane 2' }]
+            });
+
+            const handles = this.$element.find(`.${RESIZE_HANDLE}`);
+            const pointer = pointerMock(handles[0]);
+
+            pointer.start().dragStart().drag(0, 50).dragEnd();
+
+            this.instance.option(eventHandler, handlerStubAfterUpdate);
+
+            pointer.start().dragStart().drag(0, 50).dragEnd();
+
+            assert.strictEqual(handlerStub.callCount, 1);
+            assert.strictEqual(handlerStubAfterUpdate.callCount, 1);
+        });
     });
 });
