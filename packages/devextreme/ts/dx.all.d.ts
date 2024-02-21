89f6844360673e54e2c3c870aa62f7254af6abb5
Roman Semenov <roman.semenov@devexpress.com>
2024-02-21 13:57:47 +0400

    update dts

--- packages/devextreme/ts/dx.all.d.ts
+++ packages/devextreme/ts/dx.all.d.ts
@@ -4820,10 +4820,13 @@ declare module DevExpress.core {
   /**
    * @deprecated Attention! This type is for internal purposes only. If you used it previously, please submit a ticket to our {@link https://supportcenter.devexpress.com/ticket/create Support Center}. We will check if there is an alternative solution.
    */
-  type ComponentFactory<TComponent> = new (
-    element: UserDefinedElement,
-    options?: Record<string, unknown>
-  ) => TComponent;
+  type ComponentFactory<TComponent> = {
+    new (
+      element: UserDefinedElement,
+      options?: Record<string, unknown>
+    ): TComponent;
+    getInstance(element: UserDefinedElement): TComponent;
+  };
   /**
    * @deprecated Attention! This type is for internal purposes only. If you used it previously, please submit a ticket to our {@link https://supportcenter.devexpress.com/ticket/create Support Center}. We will check if there is an alternative solution.
    */
