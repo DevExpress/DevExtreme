a77b8c470b7cb8be256d02f825999d1f17190e31
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2024-02-27 02:17:06 +0400

    WIP. ajax via NG HttpClient
    changes for tests

--- packages/devextreme/js/core/utils/_webpack.cfg.js
+++ packages/devextreme/js/core/utils/_webpack.cfg.js
@@ -2,9 +2,9 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    // entry: './js/core/utils/ajax.prev.js',
-    entry: './js/core/utils/ajax.old.js',
+    // mode: 'development',
+    entry: './js/core/utils/ajax.prev.js',
+    // entry: './js/core/utils/ajax.old.js',
     output: {
         // eslint-disable-next-line no-undef
         path: 'C:\\_work_projects\\DevExtreme-monorepos\\packages\\devextreme\\js\\core\\utils', // path.resolve(__dirname, 'packages/devextreme'),
