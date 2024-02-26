04881b77ae90fbd5736d0d083fdf2ccded0e7e7a
Alyar <>
2024-02-27 00:28:15 +0400

    Migrate GridView to native class

--- packages/devextreme/js/__internal/grids/tree_list/m_grid_view.ts
+++ packages/devextreme/js/__internal/grids/tree_list/m_grid_view.ts
@@ -1,25 +1,27 @@
+import type { ModuleType } from '@ts/grids/grid_core/m_types';
+import type { ResizingController } from '@ts/grids/grid_core/views/m_grid_view';
 import { gridViewModule } from '@ts/grids/grid_core/views/m_grid_view';
 
 import treeListCore from './m_core';
 
-const ResizingController = gridViewModule.controllers.resizing.inherit({
+const resizing = (Base: ModuleType<ResizingController>) => class TreeListResizingControllerExtender extends Base {
   _getWidgetAriaLabel() {
     return 'dxTreeList-ariaTreeList';
-  },
+  }
 
   _toggleBestFitMode(isBestFit) {
-    this.callBase(isBestFit);
+    super._toggleBestFitMode(isBestFit);
 
     const $rowsTable = this._rowsView.getTableElement();
     $rowsTable.find('.dx-treelist-cell-expandable').toggleClass(this.addWidgetPrefix('best-fit'), isBestFit);
-  },
-});
+  }
+};
 
 treeListCore.registerModule('gridView', {
   defaultOptions: gridViewModule.defaultOptions,
   controllers: {
     ...gridViewModule.controllers,
-    resizing: ResizingController,
+    resizing,
   },
   views: gridViewModule.views,
 });
