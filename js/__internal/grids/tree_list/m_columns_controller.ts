c032ab6229ca89eb8bf336c9b2b146a3c4dccc7e
Alyar <>
2024-02-27 00:09:13 +0400

    Migrate DataController and ColumnsController to native classes

--- js/__internal/grids/tree_list/m_columns_controller.ts
+++ js/__internal/grids/tree_list/m_columns_controller.ts
@@ -1,33 +1,34 @@
 import { isDefined } from '@js/core/utils/type';
+import type { ColumnsController } from '@ts/grids/grid_core/columns_controller/m_columns_controller';
 import { columnsControllerModule } from '@ts/grids/grid_core/columns_controller/m_columns_controller';
+import type { ModuleType } from '@ts/grids/grid_core/m_types';
 
 import treeListCore from './m_core';
 
-export const ColumnsController = (columnsControllerModule as any).controllers.columns.inherit((function () {
-  return {
-    _getFirstItems(dataSourceAdapter) {
-      return this.callBase(dataSourceAdapter).map((node) => node.data);
-    },
-    getFirstDataColumnIndex() {
-      const visibleColumns = this.getVisibleColumns();
-      const visibleColumnsLength = visibleColumns.length;
-      let firstDataColumnIndex = 0;
+const columns = (Base: ModuleType<ColumnsController>) => class TreeListColumnsControllerExtender extends Base {
+  _getFirstItems(dataSourceAdapter) {
+    return super._getFirstItems(dataSourceAdapter).map((node) => node.data);
+  }
 
-      for (let i = 0; i <= visibleColumnsLength - 1; i++) {
-        if (!isDefined(visibleColumns[i].command)) {
-          firstDataColumnIndex = visibleColumns[i].index;
-          break;
-        }
+  getFirstDataColumnIndex() {
+    const visibleColumns = this.getVisibleColumns();
+    const visibleColumnsLength = visibleColumns.length;
+    let firstDataColumnIndex = 0;
+
+    for (let i = 0; i <= visibleColumnsLength - 1; i++) {
+      if (!isDefined(visibleColumns[i].command)) {
+        firstDataColumnIndex = visibleColumns[i].index;
+        break;
       }
+    }
 
-      return firstDataColumnIndex;
-    },
-  };
-})());
+    return firstDataColumnIndex;
+  }
+};
 
 treeListCore.registerModule('columns', {
   defaultOptions: columnsControllerModule.defaultOptions,
   controllers: {
-    columns: ColumnsController,
+    columns,
   },
 });
