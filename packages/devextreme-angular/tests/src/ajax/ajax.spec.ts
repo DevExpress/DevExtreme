6a6a261daeb86a43ea9a795b0323ef53e59a29f7
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2024-03-01 00:34:13 +0400

    dxAjaxModule
    add test

--- packages/devextreme-angular/tests/src/ajax/ajax.spec.ts
+++ packages/devextreme-angular/tests/src/ajax/ajax.spec.ts
@@ -1,56 +1,63 @@
 import { TestBed, getTestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import {
-  HttpEvent, HttpRequest, HttpResponse, HttpInterceptor, HTTP_INTERCEPTORS, HttpHandler,
+  HttpRequest, HttpInterceptor, HTTP_INTERCEPTORS, HttpHandler,
 } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
 import { Injectable } from '@angular/core';
-// import { DxAjaxModule } from 'devextreme-angular/ajax';
+import { DxAjaxModule } from 'devextreme-angular/ajax';
 import DataSource from 'devextreme/data/data_source';
 import ODataStore from 'devextreme/data/odata/store';
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-const mockHandler = {
-  handle: (req: HttpRequest<any>): Observable<HttpEvent<any>> => of(new HttpResponse(req)),
-};
+const ctx: Record<string, any> = {};
+
+ctx.interceptorFn = () => {};
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000;
 
 @Injectable()
 export class TestInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler) {
+    ctx.interceptorFn();
     return next.handle(req);
   }
 }
 
-describe('Ajax request', () => {
+describe('Ajax request using DxAjaxModule', () => {
   let injector: TestBed;
-  let httpMock: HttpTestingController;
+  let httpTestingControllerMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [],
-      imports: [HttpClientTestingModule /*DxAjaxModule*/],
+      imports: [HttpClientTestingModule, DxAjaxModule],
       providers: [{ provide: HTTP_INTERCEPTORS, useClass: TestInterceptor, multi: true }],
     });
     injector = getTestBed();
-    httpMock = injector.get(HttpTestingController);
+    httpTestingControllerMock = injector.get(HttpTestingController);
   });
 
   afterEach(() => {
-    httpMock.verify();
+    httpTestingControllerMock.verify();
   });
 
-  it('should be intercepted', async () => {
+  it('should be intercepted', (done) => {
+    // eslint-disable-next-line no-restricted-globals
+    const interceptorFnSpy = spyOn(ctx, 'interceptorFn');
+    const url = 'https://js.devexpress.com/Demos/WidgetsGallery/odata/HierarchicalItems';
     const dataSource = new DataSource({
       store: new ODataStore({
         version: 2,
-        url: 'https://js.devexpress.com/Demos/WidgetsGallery/odata/HierarchicalItems',
+        url,
       }),
     });
 
-    const spy = spyOn(mockHandler, 'handle');
+    // eslint-disable-next-line no-void
+    void dataSource.load().then(() => {
+      expect(interceptorFnSpy).toHaveBeenCalledTimes(1);
+      done();
+    });
 
-    await dataSource.load();
-
-    expect(spy).toHaveBeenCalled();
+    const req = httpTestingControllerMock.expectOne(`${url}?%24top=20`);
+    req.flush({ message: 'OK' });
   });
 });
