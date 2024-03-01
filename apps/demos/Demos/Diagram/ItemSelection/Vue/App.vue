f42d94f9bcc46ea4d525a9831b002d6ef0a33543
Joshua Victoria <jdvictoria.tech@gmail.com>
2024-03-01 14:29:09 +0800

    Diagram/ItemSelection

--- apps/demos/Demos/Diagram/ItemSelection/Vue/App.vue
+++ apps/demos/Demos/Diagram/ItemSelection/Vue/App.vue
@@ -9,7 +9,7 @@
       <DxNodes
         :data-source="dataSource"
         :key-expr="'ID'"
-        :text-expr="'Full_Name'"
+        :text-expr="textExpression"
         :parent-key-expr="'Head_ID'"
       >
         <DxAutoLayout
@@ -30,7 +30,7 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import {
-  DxDiagram, DxNodes, DxAutoLayout, DxToolbox, DxPropertiesPanel,
+  DxDiagram, DxNodes, DxAutoLayout, DxToolbox, DxPropertiesPanel, DxDiagramTypes,
 } from 'devextreme-vue/diagram';
 import ArrayStore from 'devextreme/data/array_store';
 import service from './data.ts';
@@ -41,10 +41,12 @@ const dataSource = new ArrayStore({
 });
 const selectedItemNames = ref('Nobody has been selected');
 
-function onContentReady(e) {
+const textExpression = 'Full_Name';
+
+function onContentReady(e: DxDiagramTypes.ContentReadyEvent) {
   const diagram = e.component;
   // preselect some shape
-  const items = diagram.getItems().filter(({ itemType, text }) => itemType === 'shape' && (text === 'Greta Sims'));
+  const items = diagram.getItems().filter(({ itemType, dataItem }) => itemType === 'shape' && (dataItem[textExpression] === 'Greta Sims'));
   if (items.length > 0) {
     diagram.setSelectedItems(items);
     diagram.scrollToItem(items[0]);
