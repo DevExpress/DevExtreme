c87866a8b6c76fc93e33ef025bd877175927f3f3
Alexander Bulychev <git@bulychev.pro>
2024-03-01 13:57:51 +0400

    Review fixes

--- build/install-wrappers.ts
+++ build/install-wrappers.ts
@@ -1,24 +1,21 @@
-import sh from 'shelljs';
 import fs from 'fs';
 import tar from 'tar-fs';
 import { createUnzip } from 'zlib';
-
-sh.set('-e');
 
 console.log('This is a workaround for working with demos until the wrappers are reworked');
 console.log('Please run "npm run all:build" before this script');
 console.log('This script should be run again after each "npm install" and "npm run all:build"\n\n');
 
 async function main() {
-  const version = '24.1.0';
+  const VERSION = '24.1.0';
   const PACKAGES = ['devextreme-angular', 'devextreme-react', 'devextreme-vue'];
 
   await Promise.all(PACKAGES.map(packageName => new Promise((resolve) => {
-    const tgzPath = `artifacts/npm/${packageName}-${version}.tgz`;
+    const tgzPath = `artifacts/npm/${packageName}-${VERSION}.tgz`;
     const targetPath = `node_modules/${packageName}`;
 
-    sh.rm('-rf', targetPath);
-    sh.mkdir(targetPath);
+    fs.rmSync(targetPath, { recursive: true, force: true });
+    fs.mkdirSync(targetPath);
 
     fs.createReadStream(tgzPath).pipe(createUnzip()).pipe(tar.extract(targetPath, { strip: 1 })).on('finish', () => {
       console.log(`Installed ${packageName}`);
