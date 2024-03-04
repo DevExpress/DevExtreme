d1a51b794dcf0cb2827637bac5a1f405ffcb7c85
williamvinogradov <williamvinogradov@gmail.com>
2024-03-04 13:02:57 +0400

    GridCore: sorting access modifiers + getController/getView

--- packages/devextreme/js/__internal/grids/grid_core/sorting/m_sorting_mixin.ts
+++ packages/devextreme/js/__internal/grids/grid_core/sorting/m_sorting_mixin.ts
@@ -11,8 +11,11 @@ const SORT_INDEX_CLASS = 'dx-sort-index';
 const SORT_INDEX_ICON_CLASS = 'dx-sort-index-icon';
 const HEADERS_ACTION_CLASS = 'action';
 
+// TODO improve types of this mixin
+//  Now all members - protected by default (it may be wrong)
+// TODO getController
 const sortingMixin = (Base: ModuleType<any>) => class SortingMixin extends Base {
-  _applyColumnState(options) {
+  protected _applyColumnState(options) {
     const that = this;
     let ariaSortState;
     let $sortIndicator;
@@ -54,7 +57,7 @@ const sortingMixin = (Base: ModuleType<any>) => class SortingMixin extends Base 
     return super._applyColumnState(options);
   }
 
-  _setAriaSortAttribute(column, ariaSortState, $rootElement, hasSeveralSortIndexes) {
+  protected _setAriaSortAttribute(column, ariaSortState, $rootElement, hasSeveralSortIndexes) {
     $rootElement.removeAttr('aria-roledescription');
 
     if (column.isGrouped) {
@@ -83,7 +86,7 @@ const sortingMixin = (Base: ModuleType<any>) => class SortingMixin extends Base 
     }
   }
 
-  _getIndicatorClassName(name) {
+  protected _getIndicatorClassName(name) {
     if (name === 'sort') {
       return SORT_CLASS;
     } if (name === 'sortIndex') {
@@ -92,7 +95,7 @@ const sortingMixin = (Base: ModuleType<any>) => class SortingMixin extends Base 
     return super._getIndicatorClassName(name);
   }
 
-  _renderIndicator(options) {
+  protected _renderIndicator(options) {
     const { column } = options;
     const $container = options.container;
     const $indicator = options.indicator;
@@ -113,7 +116,7 @@ const sortingMixin = (Base: ModuleType<any>) => class SortingMixin extends Base 
     super._renderIndicator(options);
   }
 
-  _updateIndicator($cell, column, indicatorName) {
+  protected _updateIndicator($cell, column, indicatorName) {
     if (indicatorName === 'sort' && isDefined(column.groupIndex)) {
       return;
     }
@@ -121,7 +124,7 @@ const sortingMixin = (Base: ModuleType<any>) => class SortingMixin extends Base 
     return super._updateIndicator.apply(this, arguments as any);
   }
 
-  _getIndicatorElements($cell, returnAll) {
+  protected _getIndicatorElements($cell, returnAll) {
     const $indicatorElements = super._getIndicatorElements($cell);
 
     return returnAll ? $indicatorElements : $indicatorElements && $indicatorElements.not(`.${SORT_NONE_CLASS}`);
