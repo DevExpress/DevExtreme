d1e210a426ebf388e315230f289d195e8ff8e825
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2024-03-05 02:45:55 +0400

    dxAjaxModule
    refactor

--- packages/devextreme-angular/src/ajax/dx-ajax.ts
+++ packages/devextreme-angular/src/ajax/dx-ajax.ts
@@ -1,6 +1,6 @@
 import { Deferred } from 'devextreme/core/utils/deferred';
 import { HttpClient, HttpEventType, HttpParams } from '@angular/common/http';
-import { throwError, Subject } from 'rxjs';
+import { throwError, Subject, Observable } from 'rxjs';
 import { takeUntil, timeoutWith } from 'rxjs/operators';
 import { getWindow } from 'devextreme/core/utils/window';
 import { isDefined } from 'devextreme/core/utils/type';
@@ -26,7 +26,7 @@ const CONTENT_TYPE = 'Content-Type';
 const URLENCODED = 'application/x-www-form-urlencoded';
 
 function assignResponseProps(xhrSurrogate, response) {
-  function getResponseHeader(name) {
+  function getResponseHeader(name: string) {
     return response.headers.get(name);
   }
 
@@ -50,7 +50,7 @@ function assignResponseProps(xhrSurrogate, response) {
   return xhrSurrogate;
 }
 
-function subscribeRequest(request, options, d, xhrSurrogate) {
+function subscribeRequest(request: Observable<any>, options: Record<string, any>, d, xhrSurrogate) {
   const needScriptEvaluation = isNeedScriptEvaluation(options);
 
   request.subscribe(
@@ -65,16 +65,16 @@ function subscribeRequest(request, options, d, xhrSurrogate) {
         assignResponseProps(xhrSurrogate, response),
       );
     },
-    (response) => {
-      const errorStatus = options.dataType === 'json' && response.message.includes('parsing')
+    (error) => {
+      const errorStatus = options.dataType === 'json' && error.message.includes('parsing')
         ? PARSER_ERROR
-        : response?.timeout || 'error';
+        : error?.timeout || 'error';
 
-      return d.reject(assignResponseProps(xhrSurrogate, response), errorStatus, response);
+      return d.reject(assignResponseProps(xhrSurrogate, error), errorStatus, error);
     },
   );
 }
-function subscribeUpload(request, options, d, xhrSurrogate) {
+function subscribeUpload(request: Observable<any>, options: Record<string, any>, d, xhrSurrogate) {
   let total = 0;
   let isUploadStarted = false;
 
@@ -97,9 +97,9 @@ function subscribeUpload(request, options, d, xhrSurrogate) {
       }
       return null;
     },
-    (response) => {
-      options.upload.onerror?.(assignResponseProps(xhrSurrogate, response));
-      return d.reject(assignResponseProps(xhrSurrogate, response), response.status, response);
+    (error) => {
+      options.upload.onerror?.(assignResponseProps(xhrSurrogate, error));
+      return d.reject(assignResponseProps(xhrSurrogate, error), error.status, error);
     },
     () => {},
   );
@@ -133,101 +133,100 @@ function isGetRequest(options) {
   return (options.method || 'get').toLowerCase() === 'get';
 }
 
-export const sendRequestFactory = (httpClient: HttpClient) => {
-  return (sendOptions: Record<string, any>) => {
-    const options = { ...sendOptions };
-    const destroy$ = new Subject<void>();
-    const d = Deferred();
-    const method = (options.method || 'get').toLowerCase();
-    const isGet = isGetRequest(options);
-    const needScriptEvaluation = isNeedScriptEvaluation(options);
+export const sendRequestFactory = (httpClient: HttpClient) => (sendOptions: Record<string, any>) => {
+  const options = { ...sendOptions };
+  const destroy$ = new Subject<void>();
+  const d = Deferred();
+  const method = (options.method || 'get').toLowerCase();
+  const isGet = isGetRequest(options);
+  const needScriptEvaluation = isNeedScriptEvaluation(options);
 
-    if (options.cache === undefined) {
-      options.cache = !needScriptEvaluation;
-    }
+  if (options.cache === undefined) {
+    options.cache = !needScriptEvaluation;
+  }
 
-    options.crossDomain = isCrossDomain(options.url);
+  options.crossDomain = isCrossDomain(options.url);
 
-    const jsonpCallBackName = getJsonpCallbackName(options);
-    const headers = getCleanedRequestHeaders(options);
-    const requestOptions = getRequestOptions(options, headers);
-    const { url } = requestOptions;
-    const { parameters } = requestOptions;
-    const data = parameters;
-    const { upload } = options;
-    const { beforeSend } = options;
-    const { xhrFields } = options;
-    const result: Result = d.promise() as Result;
+  const jsonpCallBackName = getJsonpCallbackName(options);
+  const headers = getCleanedRequestHeaders(options);
+  const requestOptions = getRequestOptions(options, headers);
+  const { url } = requestOptions;
+  const { parameters } = requestOptions;
+  const data = parameters;
+  const { upload } = options;
+  const { beforeSend } = options;
+  const { xhrFields } = options;
+  const result: Result = d.promise() as Result;
 
-    result.abort = (): void => {
-      destroy$.next();
-      destroy$.complete();
-      upload?.onabort?.(xhrSurrogate);
-      // eslint-disable-next-line no-void
-      void d.reject({ status: STATUS_ABORT }, 'error');
-    };
-
-    const xhrSurrogate = {
-      type: 'XMLHttpRequestSurrogate',
-      abort(): void { result.abort(); },
-    };
-
-    if (jsonpCallBackName) {
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-expect-error
-      window[jsonpCallBackName] = (callback_data) => d.resolve(callback_data, SUCCESS, xhrSurrogate);
-    }
-
-    if (options.cache === false && isGet && data) {
-      data._ = Date.now() + 1;
-    }
-
-    const params = isGet ? data : undefined;
-
-    const body = (() => {
-      if (isGet) {
-        return undefined;
-      }
-
-      if (!upload && typeof data === 'object' && headers[CONTENT_TYPE].indexOf(URLENCODED) === 0) {
-        const httpParams = new HttpParams();
-        Object.keys(data).forEach((key) => httpParams.set(key, data[key]));
-        return httpParams.toString();
-      }
-
-      return data;
-    })();
-
-    if (beforeSend) {
-      beforeSend(xhrSurrogate);
-    }
-
-    const request = options.crossDomain && needScriptEvaluation
-      ? httpClient.jsonp(url, jsonpCallBackName)
-      : httpClient.request(
-        method,
-        url,
-        {
-          params,
-          body,
-          headers,
-          reportProgress: true,
-          withCredentials: xhrFields?.withCredentials,
-          observe: upload ? 'events' : 'response',
-          responseType: options.responseType || (['jsonp', 'script'].includes(options.dataType) ? 'text' : options.dataType),
-        },
-      ).pipe(takeUntil(destroy$) as any);
-
-    const requestWithTimeout = options.timeout
-      ? request.pipe(timeoutWith(options.timeout, throwError(() => ({ timeout: TIMEOUT }))) as any)
-      : request;
-
-    if (upload) {
-      subscribeUpload(requestWithTimeout, options, d, xhrSurrogate);
-    } else {
-      subscribeRequest(requestWithTimeout, options, d, xhrSurrogate);
-    }
-
-    return result;
+  result.abort = (): void => {
+    destroy$.next();
+    destroy$.complete();
+    upload?.onabort?.(xhrSurrogate);
+    // eslint-disable-next-line no-void
+    void d.reject({ status: STATUS_ABORT }, 'error');
   };
+
+  const xhrSurrogate = {
+    type: 'XMLHttpRequestSurrogate',
+    abort(): void { result.abort(); },
+  };
+
+  if (jsonpCallBackName) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    window[jsonpCallBackName] = (callback_data: unknown) => d.resolve(callback_data, SUCCESS, xhrSurrogate);
+  }
+
+  if (options.cache === false && isGet && data) {
+    data._ = Date.now() + 1;
+  }
+
+  const params = isGet ? data : undefined;
+
+  const body = (() => {
+    if (isGet) {
+      return undefined;
+    }
+
+    if (!upload && typeof data === 'object' && headers[CONTENT_TYPE].indexOf(URLENCODED) === 0) {
+      const httpParams = new HttpParams();
+      Object.keys(data).forEach((key) => httpParams.set(key, data[key]));
+      return httpParams.toString();
+    }
+
+    return data;
+  })();
+
+  if (beforeSend) {
+    beforeSend(xhrSurrogate);
+  }
+
+  const request = options.crossDomain && needScriptEvaluation
+    ? httpClient.jsonp(url, jsonpCallBackName)
+    : httpClient.request(
+      method,
+      url,
+      {
+        params,
+        body,
+        headers,
+        reportProgress: true,
+        withCredentials: xhrFields?.withCredentials,
+        observe: upload ? 'events' : 'response',
+        responseType: options.responseType || (['jsonp', 'script'].includes(options.dataType) ? 'text' : options.dataType),
+      },
+    );
+
+  const requestWithTimeout = request.pipe(
+    takeUntil(destroy$) as any,
+    timeoutWith(options.timeout || 0, throwError(() => ({ timeout: TIMEOUT }))) as any,
+  ) as unknown as Observable<any>;
+
+  if (upload) {
+    subscribeUpload(requestWithTimeout, options, d, xhrSurrogate);
+  } else {
+    subscribeRequest(requestWithTimeout, options, d, xhrSurrogate);
+  }
+
+  return result;
 };
