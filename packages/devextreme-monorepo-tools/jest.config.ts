10a71baa1ab7d10d872e36786198e941f1b4cf5d
ilyakhd <14272298+IlyaKhD@users.noreply.github.com>
2024-03-05 19:09:29 +0200

    Switch testing from tools/scripts to devextreme-monorepo-tools

--- packages/devextreme-monorepo-tools/jest.config.ts
+++ packages/devextreme-monorepo-tools/jest.config.ts
@@ -1,7 +1,6 @@
 /* eslint-disable */
 export default {
   displayName: 'devextreme-monorepo-tools',
-  preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.test.json' }],
