9a0380ecdbc7f4b74994e3ffa17f17d41f13044d
Ilya Vinogradov <48182348+williamvinogradov@users.noreply.github.com>
2024-03-07 19:15:20 +0700

    Scheduler: fix timezone tests (#26855) (#26860)

--- packages/devextreme/testing/testcafe/tests/scheduler/timezones/renderDst.ts
+++ packages/devextreme/testing/testcafe/tests/scheduler/timezones/renderDst.ts
@@ -14,7 +14,16 @@ fixture.disablePageReloads`Scheduler render during DST`
   .page(url(__dirname, '../../container.html'));
 
 const SCHEDULER_SELECTOR = '#container';
-const REDUCE_CELLS_CSS = `
+// TODO remove first two selectors after fix of the currentTimeIndicator option
+const CUSTOM_CSS = `
+#container .dx-scheduler-header-panel-cell {
+  color: rgba(0,0,0,.54);
+}
+
+#container .dx-scheduler-header-panel-cell::before {
+  display: none;
+}
+
 .dx-scheduler-cell-sizes-vertical {
   height: 25px;
 }`;
@@ -67,13 +76,14 @@ generateOptionMatrix({
         .ok(compareResults.errorMessages());
     },
   ).before(async () => {
-    await insertStylesheetRulesToPage(REDUCE_CELLS_CSS);
+    await insertStylesheetRulesToPage(CUSTOM_CSS);
     await createWidget('dxScheduler', {
       timeZone: timezone,
       dataSource,
       currentView,
       currentDate,
       offset,
+      showCurrentTimeIndicator: false,
       firstDayOfWeek: 4,
       cellDuration: 60,
       height: 800,
