fc2a5be4b9f3fa31d5ea7795d0498e47ee621cd5
timbset <timbset@gmail.com>
2024-03-10 17:58:39 +0400

    Add tests work week and work week views

--- packages/devextreme/testing/testcafe/tests/scheduler/virtualScrolling/many-cells.ts
+++ packages/devextreme/testing/testcafe/tests/scheduler/virtualScrolling/many-cells.ts
@@ -3,67 +3,92 @@ import { takeScreenshot } from 'devextreme-screenshot-comparer/build/src/take-sc
 import { createWidget } from '../../../helpers/createWidget';
 import url from '../../../helpers/getPageUrl';
 import Scheduler from '../../../model/scheduler';
+import { generateOptionMatrix } from '../../../helpers/generateOptionMatrix';
+import type { ViewType } from '../../../../../js/ui/scheduler';
 
 fixture.disablePageReloads`Scheduler: Virtual scrolling (many cells)`
   .page(url(__dirname, '../../container.html'));
 
-test('it should correctly render virtual table if more than 1000 cells are virtualized (T1091980)', async (t) => {
-  const scheduler = new Scheduler('#container');
+const buildScreenshotName = (viewType: ViewType, step: string) => `virtual-scrolling-many-cells-${viewType}-horizontal-${step}.png`;
 
-  await takeScreenshot(t, scheduler.element, 'virtual-scrolling-many-cells-start.png');
+const testCases = generateOptionMatrix({
+  views: [
+    [
+      {
+        type: 'month' as ViewType,
+        groupOrientation: 'horizontal',
+      },
+    ],
+    [
+      {
+        type: 'week' as ViewType,
+        groupOrientation: 'horizontal',
+      },
+    ],
+    [
+      {
+        type: 'workWeek' as ViewType,
+        groupOrientation: 'horizontal',
+      },
+    ],
+  ],
+});
 
-  await scheduler.scrollTo(new Date(2024, 1, 1, 12), { groupId: 100 });
+testCases.forEach(({ views }) => {
+  const viewType = views[0].type;
+  const resourceCount = 400;
 
-  await takeScreenshot(t, scheduler.element, 'virtual-scrolling-many-cells-middle.png');
+  test(`it should correctly render virtual table if more than 1000 cells are virtualized for ${viewType} view (T1091980)`, async (t) => {
+    const scheduler = new Scheduler('#container');
 
-  await scheduler.scrollTo(new Date(2024, 1, 1, 12), { groupId: 199 });
+    await takeScreenshot(t, scheduler.element, buildScreenshotName(viewType, 'start'));
 
-  await takeScreenshot(t, scheduler.element, 'virtual-scrolling-many-cells-end.png');
-}).before(async () => {
-  const resourceCount = 200;
+    await scheduler.scrollTo(new Date(2024, 1, 1, 12), { groupId: resourceCount / 2 });
 
-  const resources = Array.from({ length: resourceCount }, (_, i) => ({
-    id: i,
-    text: `Resource ${i}`,
-  }));
+    await takeScreenshot(t, scheduler.element, buildScreenshotName(viewType, 'middle'));
 
-  const appointmentDateInfo = Array.from({ length: 29 })
-    .map((_, i) => ({
-      startDate: new Date(2024, 1, i + 1, 12),
-      endDate: new Date(2024, 1, i + 1, 20),
+    await scheduler.scrollTo(new Date(2024, 1, 1, 12), { groupId: resourceCount - 1 });
+
+    await takeScreenshot(t, scheduler.element, buildScreenshotName(viewType, 'end'));
+  }).before(async () => {
+    const resources = Array.from({ length: resourceCount }, (_, i) => ({
+      id: i,
+      text: `Resource ${i}`,
     }));
 
-  const appointments = Array.from({ length: resourceCount })
-    .map((_, resourceIndex) => appointmentDateInfo.map(({ startDate, endDate }) => ({
-      text: `Appointment for Resource ${resourceIndex}`,
-      startDate,
-      endDate,
-      groupId: resourceIndex,
-    })))
-    .flat();
+    const appointmentDateInfo = Array.from({ length: 29 })
+      .map((_, i) => ({
+        startDate: new Date(2024, 1, i + 1, 12),
+        endDate: new Date(2024, 1, i + 1, 20),
+      }));
 
-  await createWidget(
-    'dxScheduler',
-    {
-      height: 600,
-      currentDate: new Date(2024, 1, 1),
-      dataSource: appointments,
-      views: [
-        {
-          type: 'month',
-          groupOrientation: 'horizontal',
+    const appointments = Array.from({ length: resourceCount })
+      .map((_, resourceIndex) => appointmentDateInfo.map(({ startDate, endDate }) => ({
+        text: `Appointment for Resource ${resourceIndex}`,
+        startDate,
+        endDate,
+        groupId: resourceIndex,
+      })))
+      .flat();
+
+    await createWidget(
+      'dxScheduler',
+      {
+        height: 600,
+        currentDate: new Date(2024, 1, 1),
+        dataSource: appointments,
+        views,
+        currentView: viewType,
+        scrolling: {
+          mode: 'virtual',
         },
-      ],
-      currentView: 'month',
-      scrolling: {
-        mode: 'virtual',
+        groups: ['groupId'],
+        resources: [{
+          fieldExpr: 'groupId',
+          dataSource: resources,
+          label: 'Group',
+        }],
       },
-      groups: ['groupId'],
-      resources: [{
-        fieldExpr: 'groupId',
-        dataSource: resources,
-        label: 'Group',
-      }],
-    },
-  );
+    );
+  });
 });
