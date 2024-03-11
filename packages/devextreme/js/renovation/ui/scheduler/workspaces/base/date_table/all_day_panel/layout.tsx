09b71da82d3f4f6d8e7db2f982ed91f743607d73
williamvinogradov <williamvinogradov@gmail.com>
2024-03-11 10:25:33 +0400

    Scheduler: remove unused in the prod R1 code

--- packages/devextreme/js/renovation/ui/scheduler/workspaces/base/date_table/all_day_panel/layout.tsx
+++ packages/devextreme/js/renovation/ui/scheduler/workspaces/base/date_table/all_day_panel/layout.tsx
@@ -1,7 +1,6 @@
 import {
   Component, ComponentBindings, ForwardRef, JSXComponent, RefObject, Slot,
 } from '@devextreme-generator/declarations';
-import { AppointmentLayout } from '../../../../appointment/layout';
 import { LayoutProps } from '../../layout_props';
 import { AllDayTable } from './table';
 
@@ -16,7 +15,6 @@ export const viewFunction = ({
   <div
     className="dx-scheduler-all-day-panel"
   >
-    <AppointmentLayout isAllDay />
     <AllDayTable
       tableRef={tableRef}
       viewData={viewData}
