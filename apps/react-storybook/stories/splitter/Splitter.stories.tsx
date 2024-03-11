9e336d42ba9b08fa2ecfdded05bdc2a046465a07
alexlavrov <alex.s.lavrov@gmail.com>
2024-03-11 14:55:29 +0400

    Add simple stories for horizontal, vertical splitter. Refactor folder structure

--- apps/react-storybook/stories/splitter/Splitter.stories.tsx
+++ apps/react-storybook/stories/splitter/Splitter.stories.tsx
@@ -1,45 +1,80 @@
 import React from 'react';
-import { Splitter, Item } from 'devextreme-react/splitter'
-import type { Meta, StoryObj } from '@storybook/react';
+import {Splitter, Item} from 'devextreme-react/splitter'
+import type {Meta, StoryObj} from '@storybook/react';
 
-import DataGrid from './DataGrid';
-import Scheduler from './Scheduler';
-import PieChart from './PieChart';
+import DataGridSample from './samples/DataGrid';
+import SchedulerSample from './samples/Scheduler';
+import PieChartSample from './samples/PieChart';
 
 const meta: Meta<typeof Splitter> = {
-  title: 'Components/Splitter',
-  component: Splitter,
-  parameters: {
-    layout: 'fullscreen',
-  }
+    title: 'Components/Splitter',
+    component: Splitter,
+    parameters: {
+        layout: 'fullscreen',
+    }
 };
 
 export default meta;
 
 type Story = StoryObj<typeof Splitter>;
-export const Default: Story = {
-  render: () => {
-    return (
-        <div style={{ height: 1600 }}>
-          <Splitter orientation="vertical">
-              <Item maxSize={400}>
-                <Scheduler />
-              </Item>
-              <Item>
-                <Splitter>
-                  <Item>
-                    <DataGrid />
-                  </Item>
-                  <Item>
-                    <PieChart />
-                  </Item>
+
+export const Horizontal: Story = {
+    render: () => {
+        return (
+            <div style={{height: 900}}>
+                <Splitter orientation="horizontal" >
+                    <Item>
+                        <PieChartSample/>
+                    </Item>
+                    <Item>
+                        <DataGridSample/>
+                    </Item>
                 </Splitter>
-              </Item>
-              <Item>
-                  <DataGrid />
-              </Item>
-            </Splitter>
-        </div>
-    );
-  }
+            </div>
+        );
+    }
+}
+
+export const Vertical: Story = {
+    render: () => {
+        return (
+            <div style={{height: 800}}>
+                <Splitter orientation="vertical">
+                    <Item maxSize={400}>
+                        <DataGridSample/>
+                    </Item>
+                    <Item>
+                        <DataGridSample/>
+                    </Item>
+                </Splitter>
+            </div>
+        );
+    }
+}
+
+export const Nested: Story = {
+    render: () => {
+        return (
+            <div style={{height: 1200}}>
+                <Splitter orientation="vertical">
+                    <Item maxSize={400}>
+                        <SchedulerSample/>
+                    </Item>
+                    <Item>
+                        <Splitter>
+                            <Item>
+                                <DataGridSample/>
+                            </Item>
+                            <Item>
+                                <PieChartSample/>
+                            </Item>
+                        </Splitter>
+                    </Item>
+                    <Item>
+                        <DataGridSample/>
+                    </Item>
+                </Splitter>
+            </div>
+        );
+    }
 }
