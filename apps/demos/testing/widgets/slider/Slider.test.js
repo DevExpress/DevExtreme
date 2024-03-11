03fd5db0deb03667fc03f64e8ba0caff6764c6ef
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-03-11 18:07:13 +0800

    Create Slider TestCafe

---
+++ apps/demos/testing/widgets/slider/Slider.test.js
@@ -0,0 +1,20 @@
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
+
+fixture('Slider.Overview')
+  .page('http://localhost:8080/')
+  .beforeEach(async (t) => {
+    await t.resizeWindow(900, 800);
+  });
+
+runManualTest('Slider', 'Overview', ['jQuery', 'React', 'Vue', 'Angular'], (test) => {
+  test('Custom Slider Appearance', async (t) => {
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+    await takeScreenshot('slider_overview (material.blue.dark).png');
+
+    await t
+      .expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
+  });
+});
