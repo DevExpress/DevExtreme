b50fa6dc6cec7ecc879f03fe13b0af9e0beb5469
Andrey Dolzhikov <eylau-andrew@mail.ru>
2024-03-13 18:53:32 +0200

    add ts-expect-error

--- packages/devextreme/js/__internal/grids/data_grid/export/m_export.ts
+++ packages/devextreme/js/__internal/grids/data_grid/export/m_export.ts
@@ -610,10 +610,14 @@ export class ExportController extends dataGridCore.ViewController {
   }
 
   private _getSummaryCells(summaryTotalItems, totalAggregates) {
-    const dataController = this._dataController as any;
-    const columnsController = dataController._columnsController;
-
-    return dataController._calculateSummaryCells(summaryTotalItems, totalAggregates, columnsController.getVisibleColumns(null, true), (summaryItem, column) => (dataController._isDataColumn(column) ? column.index : -1));
+    // @ts-expect-error
+    return this._dataController._calculateSummaryCells(
+      summaryTotalItems,
+      totalAggregates,
+      this._columnsController.getVisibleColumns(null, true),
+      // @ts-expect-error
+      (summaryItem, column) => (this._dataController._isDataColumn(column) ? column.index : -1),
+    );
   }
 
   public _getSelectedItems() {
