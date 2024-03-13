37c9b2445e61d37aa2d595782658fda6c11c6b1b
Nikki Gonzales <38495263+nikkithelegendarypokemonster@users.noreply.github.com>
2024-03-13 18:03:06 +0800

    TextArea: rewrite the testcafe test scripts to compare appearance of textarea on different scenarios

--- packages/devextreme/testing/testcafe/tests/editors/textArea/index.ts
+++ packages/devextreme/testing/testcafe/tests/editors/textArea/index.ts
@@ -1,101 +1,104 @@
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import { testScreenshot } from '../../../helpers/themeUtils';
 import url from '../../../helpers/getPageUrl';
 import { createWidget } from '../../../helpers/createWidget';
-import TextArea from '../../../model/textArea';
-import { getPropertyValue, insertStylesheetRulesToPage } from '../../../helpers/domUtils';
-import { isMaterialBased } from '../../../helpers/themeUtils';
+import {
+  appendElementTo, setAttribute,
+} from '../../../helpers/domUtils';
+import { clearTestPage } from '../../../helpers/clearPage';
 
-const testFixture = () => {
-  if (isMaterialBased()) {
-    return fixture.disablePageReloads.skip;
-  }
-  return fixture.disablePageReloads;
-};
-
-testFixture()`TextArea_Height`
-  .page(url(__dirname, '../../container.html'));
+fixture.disablePageReloads`TextArea_Height`
+  .page(url(__dirname, '../../container.html'))
+  .afterEach(async () => clearTestPage());
 
 const text = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.';
+const longText = 'Prepare 2013 Marketing Plan: We need to double revenues in 2013 and our marketing strategy is going to be key here. R&D is improving existing products and creating new products so we can deliver great AV equipment to our customers.Robert, please make certain to create a PowerPoint presentation for the members of the executive team.';
 
-[true, false].forEach((autoResizeEnabled) => {
-  test(`TextArea has correct height with "autoResizeEnabled" is ${autoResizeEnabled} and height is 7em & maxHeight is 5em`, async (t) => {
-    const textArea = new TextArea('#container');
+test('TextArea has correct height with autoResizeEnabled and height is 7em & maxHeight is 5em', async (t) => {
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-    await t
-      .expect(getPropertyValue(textArea.element, 'maxHeight'))
-      .eql('5em')
-      .expect(getPropertyValue(textArea.element, 'height'))
-      .eql('7em')
-      .expect(textArea.element.getStyleProperty('height'))
-      .eql('70px')
-      .expect(getPropertyValue(textArea.input, 'height'))
-      .eql('');
-  }).before(async () => createWidget('dxTextArea', {
+  await t.debug();
+  await testScreenshot(t, takeScreenshot, 'TextArea has correct height with autoResizeEnabled and height is 7em & maxHeight is 5em.png', { element: '#container' });
+
+  await t
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => {
+  await setAttribute('#container', 'style', 'width: 300px; height: 400px;');
+
+  const config = {
     maxHeight: '5em',
     height: '7em',
-    autoResizeEnabled,
-    width: 200,
+    width: '100%',
     value: text,
-  }));
+  };
 
-  test(`TextArea has correct height with "autoResizeEnabled" is ${autoResizeEnabled} and height is 5em & maxHeight is 7em`, async (t) => {
-    const textArea = new TextArea('#container');
+  await appendElementTo('#container', 'div', 'textArea1');
+  await appendElementTo('#container', 'div', 'textArea2');
 
-    await t
-      .expect(getPropertyValue(textArea.element, 'maxHeight'))
-      .eql('7em')
-      .expect(getPropertyValue(textArea.element, 'height'))
-      .eql('5em')
-      .expect(textArea.element.getStyleProperty('height'))
-      .eql('70px')
-      .expect(getPropertyValue(textArea.input, 'height'))
-      .eql('');
-  }).before(async () => createWidget('dxTextArea', {
-    maxHeight: '7em',
-    height: '5em',
-    autoResizeEnabled,
-    width: 200,
+  await createWidget('dxTextArea', {
+    ...config,
+    autoResizeEnabled: true,
+  }, '#textArea1');
+
+  await createWidget('dxTextArea', {
+    ...config,
+    autoResizeEnabled: false,
+  }, '#textArea2');
+});
+
+test('TextArea has correct height with autoResizeEnabled and have different data string length inside', async (t) => {
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+  await t.debug();
+  await testScreenshot(t, takeScreenshot, 'TextArea with autoResizeEnabled and have different data string length inside.png', { element: '#container' });
+
+  await t
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => {
+  await setAttribute('#container', 'style', 'width: 300px;');
+
+  const config = {
+    width: '100%',
     value: text,
-  }));
+    autoResizeEnabled: true,
+  };
 
-  test(`TextArea has correct height with "autoResizeEnabled" is ${autoResizeEnabled} and maxHeight option is 5em`, async (t) => {
-    const textArea = new TextArea('#container');
+  await appendElementTo('#container', 'div', 'textArea1');
+  await appendElementTo('#container', 'div', 'textArea2');
 
-    await t
-      .expect(getPropertyValue(textArea.element, 'maxHeight'))
-      .eql('5em')
-      .expect(getPropertyValue(textArea.element, 'height'))
-      .eql(autoResizeEnabled ? 'auto' : '')
-      .expect(textArea.element.getStyleProperty('height'))
-      .eql(autoResizeEnabled ? '70px' : '55px')
-      .expect(getPropertyValue(textArea.input, 'height'))
-      .eql(autoResizeEnabled ? '68px' : '');
-  }).before(async () => createWidget('dxTextArea', {
-    maxHeight: '5em',
-    autoResizeEnabled,
-    width: 200,
-    value: text,
-  }));
+  await createWidget('dxTextArea', {
+    ...config,
+  }, '#textArea1');
 
-  test(`TextArea with font-size style has correct height with "autoResizeEnabled" is ${autoResizeEnabled} and maxHeight option is 5em`, async (t) => {
-    const textArea = new TextArea('#container');
+  await createWidget('dxTextArea', {
+    ...config,
+    value: text + text,
+  }, '#textArea2');
+});
 
-    await t
-      .expect(getPropertyValue(textArea.element, 'maxHeight'))
-      .eql('5em')
-      .expect(getPropertyValue(textArea.element, 'height'))
-      .eql(autoResizeEnabled ? 'auto' : '')
-      .expect(parseInt(await textArea.element.getStyleProperty('height'), 10))
-      .eql(autoResizeEnabled ? 60 : 49)
-      .expect(getPropertyValue(textArea.input, 'height'))
-      .eql(autoResizeEnabled ? '58px' : '');
-  }).before(async () => {
-    await insertStylesheetRulesToPage('#container { font-size: 12px; }');
+test('Check for error in bug (T1221869) where when there is vale in maxHeight and autResizeEnabled is enabled', async (t) => {
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-    return createWidget('dxTextArea', {
-      maxHeight: '5em',
-      autoResizeEnabled,
-      width: 200,
-      value: text,
-    });
-  });
+  await t.debug();
+  await testScreenshot(t, takeScreenshot, 'Check for error in bug (T1221869) where when there is vale in maxHeight and autResizeEnabled is enabled.png', { element: '#container' });
+
+  await t
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => {
+  await setAttribute('#container', 'style', 'width: 300px; height: 400px;');
+  const config = {
+    value: longText,
+    width: 1000,
+    maxHeight: 80,
+    inputAttr: { 'aria-label': 'Notes' },
+    autoResizeEnabled: true,
+  };
+
+  await appendElementTo('#container', 'div', 'textArea1');
+  await createWidget('dxTextArea', {
+    ...config,
+  }, '#textArea1');
 });
