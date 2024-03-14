3b23d20d525b28878d60c22fb60bc4e5c56c3828
Alyar <>
2024-03-14 17:34:17 +0400

    code refactoring

--- packages/devextreme/testing/testcafe/helpers/clearPage.js
+++ packages/devextreme/testing/testcafe/helpers/clearPage.js
@@ -9,25 +9,23 @@ module.exports = {
             const body = document.querySelector('body');
             const parentContainer = document.getElementById('parentContainer');
 
-            if(parentContainer) {
-                if(shadowDom) {
-                    parentContainer.remove();
-                } else {
-                    $(parentContainer).remove();
-                }
-
-                const containerElement = document.createElement('div');
-                containerElement.setAttribute('id', 'container');
-
-                const otherContainerElement = document.createElement('div');
-                otherContainerElement.setAttribute('id', 'otherContainer');
-
-                const parentContainerElement = document.createElement('div');
-                parentContainerElement.setAttribute('id', 'parentContainer');
-
-                parentContainerElement.append(containerElement, otherContainerElement);
-                body.prepend(parentContainerElement);
+            if(shadowDom) {
+                parentContainer?.remove();
+            } else {
+                $(parentContainer).remove();
             }
+
+            const containerElement = document.createElement('div');
+            containerElement.setAttribute('id', 'container');
+
+            const otherContainerElement = document.createElement('div');
+            otherContainerElement.setAttribute('id', 'otherContainer');
+
+            const parentContainerElement = document.createElement('div');
+            parentContainerElement.setAttribute('id', 'parentContainer');
+
+            parentContainerElement.append(containerElement, otherContainerElement);
+            body.prepend(parentContainerElement);
 
             $('#stylesheetRules').remove();
         }, {
