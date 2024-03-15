b5024fad624c5596bda774534aba753842fffd75
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2024-03-15 17:54:03 +0400

    update performance test

--- packages/devextreme/testing/tests/DevExpress.data/queryArray.tests.js
+++ packages/devextreme/testing/tests/DevExpress.data/queryArray.tests.js
@@ -708,6 +708,7 @@ QUnit.module('Filtering performance');
 
 QUnit.test('execute quickly if criteria is huge sequence of ["prop", "=", value] filters are grouped by OR (T1217184)', function(assert) {
     const done = assert.async();
+    const TARGET_TIME = 300;
     const input = [...new Array(20000)].map((_, index) => ({ id: index }));
     const filters = [...new Array(5000)]
         .flatMap((_, index) => [['id', '=', index], 'or']);
@@ -720,11 +721,12 @@ QUnit.test('execute quickly if criteria is huge sequence of ["prop", "=", value]
     [1, 2, 3].forEach(() => {
         const startTime = Date.now();
         const r = arrayQuery.filter(filters).toArray();
+
         bestTime = Math.min(Date.now() - startTime, bestTime);
         assert.equal(r.length, 5000);
     });
 
-    assert.ok(bestTime < 300, `Execution time is ${bestTime}. It must be less than 300ms`);
+    assert.ok(bestTime < TARGET_TIME, `Execution time is ${bestTime}. It must be less than ${TARGET_TIME}ms`);
 
     done();
 });
