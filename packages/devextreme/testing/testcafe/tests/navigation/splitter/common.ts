c31dcfb137f66c8f6cafdcd1a9dde170a3638e67
Nikki Gonzales <38495263+nikkithelegendarypokemonster@users.noreply.github.com>
2024-03-18 22:22:10 +0800

    Splitter: create test cafe test to test different state of splitter in different themes

--- packages/devextreme/testing/testcafe/tests/navigation/splitter/common.ts
+++ packages/devextreme/testing/testcafe/tests/navigation/splitter/common.ts
@@ -1,10 +1,70 @@
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import url from '../../../helpers/getPageUrl';
 import { createWidget } from '../../../helpers/createWidget';
-import { testScreenshot } from '../../../helpers/themeUtils';
+import { getFullThemeName, testScreenshot } from '../../../helpers/themeUtils';
+import {
+  appendElementTo, setAttribute,
+} from '../../../helpers/domUtils';
 
 fixture.disablePageReloads`Splitter_Icon_Results`
   .page(url(__dirname, '../../container.html'));
+
+test('Splitter appearance on different appearance and themes', async (t) => {
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+  await testScreenshot(t, takeScreenshot, 'splitter-appearance state=normal.png', { element: '#normal' });
+  await testScreenshot(t, takeScreenshot, 'splitter-appearance state=normal.png', { element: '#normal', theme: getFullThemeName().replace('light', 'dark') });
+
+  t.hover('#hover');
+  await testScreenshot(t, takeScreenshot, 'splitter-appearance state=hover.png', { element: '#hover' });
+  await testScreenshot(t, takeScreenshot, 'splitter-appearance state=hover.png', { element: '#hover', theme: getFullThemeName().replace('light', 'dark') });
+
+  t.click('#focused');
+  await testScreenshot(t, takeScreenshot, 'splitter-appearance state=focus.png', { element: '#focused' });
+  await testScreenshot(t, takeScreenshot, 'splitter-appearance state=focus.png', { element: '#focused', theme: getFullThemeName().replace('light', 'dark') });
+
+  await t
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => {
+  await setAttribute('#container', 'style', 'display:flex; height: 400px;');
+
+  await appendElementTo('#container', 'div', 'normal', { });
+  await appendElementTo('#container', 'div', 'hover', { });
+  await appendElementTo('#container', 'div', 'focused', { });
+  await createWidget('dxSplitter', {
+    orientation: 'horizontal',
+    width: 600,
+    height: 300,
+    dataSource: [{
+      text: 'pane_1',
+    }, {
+      text: 'pane_2',
+    },
+    ],
+  }, '#normal');
+  await createWidget('dxSplitter', {
+    orientation: 'horizontal',
+    width: 600,
+    height: 300,
+    dataSource: [{
+      text: 'pane_1',
+    }, {
+      text: 'pane_2',
+    },
+    ],
+  }, '#hover');
+  await createWidget('dxSplitter', {
+    orientation: 'horizontal',
+    width: 600,
+    height: 300,
+    dataSource: [{
+      text: 'pane_1',
+    }, {
+      text: 'pane_2',
+    },
+    ],
+  }, '#focused');
+});
 
 ['horizontal', 'vertical'].forEach((orientation) => {
   test(`Splitter appearance, orientation='${orientation}'`, async (t) => {
