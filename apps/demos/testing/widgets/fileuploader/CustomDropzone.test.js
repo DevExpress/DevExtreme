76efc12b931941323e3e0dcf312d27f4c7fd5162
Joshua Victoria <jdvictoria.tech@gmail.com>
2024-03-19 17:59:52 +0800

    Rename dropzone external variable

--- apps/demos/testing/widgets/fileuploader/CustomDropzone.test.js
+++ apps/demos/testing/widgets/fileuploader/CustomDropzone.test.js
@@ -3,7 +3,7 @@ import { Selector, ClientFunction } from 'testcafe';
 import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
 import { testScreenshot } from '../../../utils/visual-tests/helpers/theme-utils';
 
-const DROPZONE_EXTERNAL_CLASS = 'dropzone-external';
+const DROPZONE_EXTERNAL_ID = 'dropzone-external';
 const DROPZONE_ACTIVE_CLASS = 'dropzone-active';
 
 fixture('FileUploader.CustomDropzone')
@@ -31,30 +31,30 @@ runManualTest('FileUploader', 'CustomDropzone', ['jQuery'], (test) => {
   };
 
   test('dropzone-active class is added to the dropzone element when single valid file is dragged over it', async (t) => {
-    await triggerDragEnter(`#${DROPZONE_EXTERNAL_CLASS}`, [{ type: 'image/png' }]);
+    await triggerDragEnter(`#${DROPZONE_EXTERNAL_ID}`, [{ type: 'image/png' }]);
 
-    await t.expect(Selector(`#${DROPZONE_EXTERNAL_CLASS}`).hasClass(DROPZONE_ACTIVE_CLASS)).ok();
+    await t.expect(Selector(`#${DROPZONE_EXTERNAL_ID}`).hasClass(DROPZONE_ACTIVE_CLASS)).ok();
   });
 
   test('dropzone-active class is not added to the dropzone element when an invalid file format is dragged', async (t) => {
-    await triggerDragEnter(`#${DROPZONE_EXTERNAL_CLASS}`, [{ type: 'image/xlsx' }]);
+    await triggerDragEnter(`#${DROPZONE_EXTERNAL_ID}`, [{ type: 'image/xlsx' }]);
 
-    await t.expect(Selector(`#${DROPZONE_EXTERNAL_CLASS}`).hasClass(DROPZONE_ACTIVE_CLASS)).notOk();
+    await t.expect(Selector(`#${DROPZONE_EXTERNAL_ID}`).hasClass(DROPZONE_ACTIVE_CLASS)).notOk();
   });
 
   test('dropzone-active class is not added to the dropzone element when multiple items are dragged', async (t) => {
-    await triggerDragEnter(`#${DROPZONE_EXTERNAL_CLASS}`, [
+    await triggerDragEnter(`#${DROPZONE_EXTERNAL_ID}`, [
       { type: 'image/png' },
       { type: 'image/png' },
     ]);
 
-    await t.expect(Selector(`#${DROPZONE_EXTERNAL_CLASS}`).hasClass(DROPZONE_ACTIVE_CLASS)).notOk();
+    await t.expect(Selector(`#${DROPZONE_EXTERNAL_ID}`).hasClass(DROPZONE_ACTIVE_CLASS)).notOk();
   });
 
   test('custom dropzone user interface appearance when dropzone-active is applied', async (t) => {
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-    await triggerDragEnter(`#${DROPZONE_EXTERNAL_CLASS}`, [{ type: 'image/png' }]);
+    await triggerDragEnter(`#${DROPZONE_EXTERNAL_ID}`, [{ type: 'image/png' }]);
 
     await testScreenshot(t, takeScreenshot, 'custom_dropzone_valid_file.png');
 
