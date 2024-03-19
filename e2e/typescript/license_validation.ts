90f948a60992ab26c93ee6c5bcbec552e3dbc5be
Roman Semenov <roman.semenov@devexpress.com>
2024-03-20 01:31:11 +0400

    fix: update imports

--- e2e/typescript/license_validation.ts
+++ e2e/typescript/license_validation.ts
@@ -1,7 +1,8 @@
 import { assertType, toAssertion } from './consts';
 
-import type * as LicenseValidationModule from '../../js/__internal/core/license/license_validation';
-import type * as LicenseValidationInternalModule from '../../js/__internal/core/license/license_validation_internal';
+// NOTE: better not to do this, e2e should test only public things, not internal
+import type * as LicenseValidationModule from '../../packages/devextreme/js/__internal/core/license/license_validation';
+import type * as LicenseValidationInternalModule from '../../packages/devextreme/js/__internal/core/license/license_validation_internal';
 
 assertType<typeof LicenseValidationModule>(toAssertion<typeof LicenseValidationInternalModule>());
 assertType<typeof LicenseValidationInternalModule>(toAssertion<typeof LicenseValidationModule>());
