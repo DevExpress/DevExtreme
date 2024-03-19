9816afb1816ff2614a8308d054dba01c81759b13
Joshua Victoria <jdvictoria.tech@gmail.com>
2024-03-19 15:34:24 +0800

    Remove .only

--- apps/demos/testing/widgets/fileuploader/CustomDropzone.test.js
+++ apps/demos/testing/widgets/fileuploader/CustomDropzone.test.js
@@ -29,25 +29,25 @@ runManualTest('FileUploader', 'CustomDropzone', ['jQuery'], (test) => {
     }, { dependencies: { dropZoneSelector, fileType } })();
   };
 
-  test.only('dropzone-active class is added to the dropzone element when single valid file is dragged over it', async (t) => {
+  test('dropzone-active class is added to the dropzone element when single valid file is dragged over it', async (t) => {
     await triggerDragEnter(t, `#${DROPZONE_EXTERNAL_CLASS}`, 'image/png');
 
     await t.expect(Selector(`#${DROPZONE_EXTERNAL_CLASS}`).hasClass('dropzone-active')).ok();
   });
 
-  test.only('dropzone-active class is not added to the dropzone element when an invalid file format is dragged', async (t) => {
+  test('dropzone-active class is not added to the dropzone element when an invalid file format is dragged', async (t) => {
     await triggerDragEnter(t, `#${DROPZONE_EXTERNAL_CLASS}`, 'image/xlsx');
 
     await t.expect(Selector(`#${DROPZONE_EXTERNAL_CLASS}`).hasClass('dropzone-active')).notOk();
   });
 
-  test.only('dropzone-active class is not added to the dropzone element when multiple items are dragged', async (t) => {
+  test('dropzone-active class is not added to the dropzone element when multiple items are dragged', async (t) => {
     await triggerDragEnter(t, `#${DROPZONE_EXTERNAL_CLASS}`, 'image/png');
 
     await t.expect(Selector(`#${DROPZONE_EXTERNAL_CLASS}`).hasClass('dropzone-active')).notOk();
   });
 
-  test.only('custom dropzone user interface appearance when dropzone-active is applied', async (t) => {
+  test('custom dropzone user interface appearance when dropzone-active is applied', async (t) => {
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
     await triggerDragEnter(t, `#${DROPZONE_EXTERNAL_CLASS}`, 'image/png');
