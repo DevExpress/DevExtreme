4d89bcf6c580b28247b1258f628e7fb5e9868d7c
Roman Semenov <roman.semenov@devexpress.com>
2024-03-20 12:46:21 +0400

    Testcafe - remove old code for multiplatform tests (#26955)

--- packages/devextreme/testing/testcafe/model/scrollView/scrollView.ts
+++ packages/devextreme/testing/testcafe/model/scrollView/scrollView.ts
@@ -1,20 +1,7 @@
-import type Scrollable from './internal/scrollable';
-import type { PlatformType } from '../../helpers/multi-platform-test/platform-type';
-import { ScrollableFactory } from './internal/scrollable';
+import Scrollable from './internal/scrollable';
 import type { WidgetName } from '../../helpers/widgetTypings';
 
-const getScrollView = (
-  platform: PlatformType,
-// eslint-disable-next-line @typescript-eslint/no-extra-parens
-) => class ScrollView extends (ScrollableFactory[platform] as typeof Scrollable) {
+export default class ScrollView extends Scrollable {
   // eslint-disable-next-line class-methods-use-this
   getName(): WidgetName { return 'dxScrollView'; }
-};
-
-export const ScrollViewFactory = {
-  jquery: getScrollView('jquery'),
-  angular: getScrollView('angular'),
-  react: getScrollView('react'),
-};
-
-export default ScrollViewFactory.jquery;
+}
