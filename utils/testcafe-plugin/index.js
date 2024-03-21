378d0b41f1703dd3097e9c069f85170cb3a719b2
Roman Semenov <roman.semenov@devexpress.com>
2024-03-22 00:02:20 +0400

    add

--- utils/testcafe-plugin/index.js
+++ utils/testcafe-plugin/index.js
@@ -1,31 +1,49 @@
-const { dirname } = require("path");
+const { dirname, join } = require("path");
 const {readJsonFile } = require('@nx/devkit');
 const glob = require('glob');
 
+/**
+ * @type {import('@nx/devkit').CreateNodes}
+ */
 module.exports.createNodes = [
-  'e2e/qunit/package.json',
+  'e2e/testcafe-devextreme/package.json',
   (projectConfigurationFile, opts, context) => {
-    const tests = glob.sync('e2e/qunit/tests/**/*.tests.js');
+    const path = join(context.workspaceRoot, 'e2e/testcafe-devextreme/tests/')
+    const tests = glob.sync(path + '**/*.ts').map((test) => test.slice(path.length, -3));
+
+    const testFileTargets = Object.fromEntries(tests.map((test) => [
+      'test--' + test,
+      {
+        executor: 'nx:run-commands',
+        options: {
+          cwd: 'e2e/testcafe-devextreme',
+          command: `node runner.js --componentFolder '' --file ${test} --concurrency 1`,
+        }
+      }
+    ]));
 
     /**
      * @type {import('@nx/devkit').ProjectConfiguration}
      */
     const projectConfiguration = {
-      targets: Object.fromEntries(tests.map((test) => [
-        'test--' + test,
-        {
-          executor: 'nx:run-command',
-          options: {
-            cwd: 'e2e/qunit',
-            command: 'echo hello',
-          }
-        }
-      ]))
+      targets: {
+        e2e: {
+          executor: 'nx:noop',
+          dependsOn: Object.keys(testFileTargets).map((target) => ({
+            target,
+            project: 'self',
+            params: 'forward'
+          }))
+        },
+        ...testFileTargets
+      }
     };
+
+
 
     return {
       projects: {
-        "e2e/qunit": projectConfiguration,
+        "e2e/testcafe-devextreme": projectConfiguration,
       },
     };
   },
