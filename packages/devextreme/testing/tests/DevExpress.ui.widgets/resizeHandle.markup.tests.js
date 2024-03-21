abc2c2709d3865114971cfd95a87b346e3665533
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-03-21 04:38:14 +0400

    Update resizeHandle.markup.tests.js

--- packages/devextreme/testing/tests/DevExpress.ui.widgets/resizeHandle.markup.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.widgets/resizeHandle.markup.tests.js
@@ -90,7 +90,43 @@ QUnit.module('ResizeHandle markup', moduleConfig, () => {
 
                         assert.strictEqual(this.$element.hasClass(STATE_DISABLED_CLASS), isInactive ? true : false, 'disable state');
                         assert.strictEqual(this.instance.option(isHorizontalDirection ? 'width' : 'height'), isInactive ? INACTIVE_SIZE : DEFAULT_SIZE, 'handle size');
-                        assert.strictEqual(this.instance.option(isHorizontalDirection ? 'height' : 'width'), undefined, 'handle size');
+                        assert.strictEqual(this.instance.option(isHorizontalDirection ? 'height' : 'width'), null, 'handle size');
+                    });
+
+                    QUnit.test(`should have the 'dx-state-disabled' class and the correct width when control elements are turned off, and vice versa, ${JSON.stringify(options)} after change resizable to ${!resizable} in runtime`, function(assert) {
+                        this.reinit({ separatorSize: DEFAULT_SIZE, resizable, showCollapseNext, showCollapsePrev, direction });
+
+                        const newOptionValue = !resizable;
+                        this.instance.option('resizable', newOptionValue);
+
+                        const isInactiveState = !showCollapseNext && !showCollapsePrev && !newOptionValue;
+                        assert.strictEqual(this.$element.hasClass(STATE_DISABLED_CLASS), isInactiveState ? true : false, 'disable state');
+                        assert.strictEqual(this.instance.option(isHorizontalDirection ? 'width' : 'height'), isInactiveState ? INACTIVE_SIZE : DEFAULT_SIZE, 'handle size');
+                        assert.strictEqual(this.instance.option(isHorizontalDirection ? 'height' : 'width'), null, 'handle size');
+                    });
+
+                    QUnit.test(`should have the 'dx-state-disabled' class and the correct width when control elements are turned off, and vice versa, ${JSON.stringify(options)} after change showCollapsePrev to ${!showCollapsePrev} in runtime`, function(assert) {
+                        this.reinit({ separatorSize: DEFAULT_SIZE, resizable, showCollapseNext, showCollapsePrev, direction });
+
+                        const newOptionValue = !showCollapsePrev;
+                        this.instance.option('showCollapsePrev', newOptionValue);
+
+                        const isInactiveState = !showCollapseNext && !resizable && !newOptionValue;
+                        assert.strictEqual(this.$element.hasClass(STATE_DISABLED_CLASS), isInactiveState ? true : false, 'disable state');
+                        assert.strictEqual(this.instance.option(isHorizontalDirection ? 'width' : 'height'), isInactiveState ? INACTIVE_SIZE : DEFAULT_SIZE, 'handle size');
+                        assert.strictEqual(this.instance.option(isHorizontalDirection ? 'height' : 'width'), null, 'handle size');
+                    });
+
+                    QUnit.test(`should have the 'dx-state-disabled' class and the correct width when control elements are turned off, and vice versa, ${JSON.stringify(options)} after change showCollapseNex to ${!showCollapseNext} in runtime`, function(assert) {
+                        this.reinit({ separatorSize: DEFAULT_SIZE, resizable, showCollapseNext, showCollapsePrev, direction });
+
+                        const newOptionValue = !showCollapseNext;
+                        this.instance.option('showCollapseNext', newOptionValue);
+
+                        const isInactiveState = !newOptionValue && !showCollapsePrev && !resizable;
+                        assert.strictEqual(this.$element.hasClass(STATE_DISABLED_CLASS), isInactiveState ? true : false, 'disable state');
+                        assert.strictEqual(this.instance.option(isHorizontalDirection ? 'width' : 'height'), isInactiveState ? INACTIVE_SIZE : DEFAULT_SIZE, 'handle size');
+                        assert.strictEqual(this.instance.option(isHorizontalDirection ? 'height' : 'width'), null, 'handle size');
                     });
                 });
             });
