0f497ebbe94842e3d6cc9b982eadadabad07ee46
Nikki Gonzales <38495263+nikkithelegendarypokemonster@users.noreply.github.com>
2024-03-21 20:26:41 +0800

    splitter demo: refactor jquery demo based from recomendations

--- apps/demos/Demos/Splitter/Overview/jQuery/index.js
+++ apps/demos/Demos/Splitter/Overview/jQuery/index.js
@@ -1,7 +1,105 @@
 $(() => {
+
+  const createElementWithClass = function(text, className) {
+    return $("<div>").text(text).addClass(className);
+};
+
+const getStateText = function(data) {
+    if(data.resizable !== false && !data.collapsible) {
+        return 'Resizable only';
+    }
+    
+    return `${data.resizable ? "Resizable" : "Non-resizable"} and ${data.collapsible ? "collapsible" : "non-collapsible"}`;
+};
+
+const paneContentTemplate = function(data, paneName) {
+    const $content = createElementWithClass("", "pane-content");
+
+    $content.append(createElementWithClass(paneName, "pane-title"));
+
+    $content.append(createElementWithClass(getStateText(data), "pane-state"));
+
+    const dimensionOptions = new Set(["size", "minSize", "maxSize"]);
+
+    Object.entries(data)
+        .filter(([key]) => dimensionOptions.has(key))
+        .forEach(([key, value]) => {
+            $content.append(createElementWithClass(`${key}: ${value}`, "pane-option"));
+        });
+    
+        
+    return $content;
+};
+
+
 $("#splitter").dxSplitter({
     height: "100%",
     width: "100%",
-    items: paneData,
+    items: [
+      {
+        resizable: true,
+        minSize: "60px",
+        size:"140px",
+        template: function (data) {
+          return paneContentTemplate(data, 'Left Panel');
+        }
+      },
+      {
+        minSize: "20%",
+        splitter: {
+          orientation: "vertical",
+          items: [
+            {
+              resizable: true,
+              collapsible: true,
+              maxSize: "75%",
+              template: function (data) {
+                  return paneContentTemplate(data, 'Central Panel');
+              }
+            },
+            {
+              collapsible: true,
+              splitter: {
+                orientation: "horizontal",
+                items: [
+                  {
+                    resizable: true,
+                    collapsible: true,
+                    size: "30%",
+                    minSize: "5%",
+                    template: function (data) {
+                      return paneContentTemplate(data, 'Nested Left Panel');
+                    }
+                  },
+                  {
+                    collapsible: true,
+                    template: function (data) {
+                      return paneContentTemplate(data, 'Nested Central Panel');
+                    }
+                  },
+                  {
+                    resizable: true,
+                    collapsible: true,
+                    size: "30%",
+                    minSize: "5%",
+                    template: function (data) {
+                      return paneContentTemplate(data, 'Nested Right Panel');
+                    }
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      },
+      {
+        size:"140px",
+        resizable: false,
+        collapsible: false,
+        template: function (data) {
+          return paneContentTemplate(data, 'Right Panel');
+        }
+      }
+    ],
   });
 });
