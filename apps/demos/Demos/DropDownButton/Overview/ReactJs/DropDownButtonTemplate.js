1882c4a67e1841e9bf2fbcc1415ec1c4f6cb695e
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-03-25 20:59:03 +0800

    Run convert-to-js (React)

--- apps/demos/Demos/DropDownButton/Overview/ReactJs/DropDownButtonTemplate.js
+++ apps/demos/Demos/DropDownButton/Overview/ReactJs/DropDownButtonTemplate.js
@@ -3,22 +3,26 @@ import React from 'react';
 const Avatar = ({ icon }) => (
   <div className="button-img-container">
     <div className="button-img-indicator"></div>
-    <img className="button-img" src={icon} alt="employee" />
+    <img
+      className="button-img"
+      src={icon}
+      alt="employee"
+    />
   </div>
 );
-
 const ButtonDescription = ({ text, description }) => (
   <div className="text-container">
     <div className="button-title">{text}</div>
     <div className="button-row">{description}</div>
   </div>
 );
-
 const DropDownButtonTemplate = ({ text }) => (
   <React.Fragment>
     <Avatar icon="../../../../images/employees/51.png" />
-    <ButtonDescription text={text} description="IT Manager" />
+    <ButtonDescription
+      text={text}
+      description="IT Manager"
+    />
   </React.Fragment>
 );
-
 export default DropDownButtonTemplate;
