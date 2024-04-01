95b445585348fd643d71fb642b9abc659519e77c
Nikki Gonzales <38495263+nikkithelegendarypokemonster@users.noreply.github.com>
2024-04-01 20:50:33 +0800

    Splitter demo: refactor code on react demo

--- apps/demos/Demos/Splitter/Overview/React/PaneContent.tsx
+++ apps/demos/Demos/Splitter/Overview/React/PaneContent.tsx
@@ -2,32 +2,33 @@ import React, { useMemo } from 'react';
 
 const dimensionOptions = new Set(['size', 'minSize', 'maxSize'])
 
-export default function PaneContent({data}) {
-  
-  const getPaneState = (data)=>{
-    if (data.resizable !== false && !data.collapsible) {
-      return 'Resizable only';
-    }
-    const resizableText = data.resizable ? 'Resizable' : 'Non-resizable';
-    const collapsibleText = data.collapsible ? 'collapsible' : 'non-collapsible';
-    
-    return `${resizableText} and ${collapsibleText}`;
+const getPaneState = (data) => {
+  if (data.resizable !== false && !data.collapsible) {
+    return 'Resizable only';
   }
+  const resizableText = data.resizable ? 'Resizable' : 'Non-resizable';
+  const collapsibleText = data.collapsible ? 'collapsible' : 'non-collapsible';
   
-  const filterDimensionOptions = Object.entries(data)
-  .filter(([key]) => dimensionOptions.has(key))
-  .map(([key, value]) => ({key, value}));
-  
-  return (
-    <div className="pane-content">
-      <div className="pane-title">{data.paneName}</div>
-      <div className="pane-state">{getPaneState(data)}</div>
-      {filterDimensionOptions.map((item, index) => (
-        <div className="pane-option" key={index}>
-        {item.key}: {item.value}
-        </div>
-        ))}
-    </div>
-      );
-    };
+  return `${resizableText} and ${collapsibleText}`;
+}
+
+const getFilterDimensionOptions = (data) => Object.entries(data)
+.filter(([key]) => dimensionOptions.has(key))
+.map(([key, value]) => ({key, value}));
+
+const PaneContent = (data) => (
+  <div className="pane-content">
+    <div className="pane-title">{data.title}</div>
+    <div className="pane-state">{getPaneState(data)}</div>
+    {getFilterDimensionOptions(data).map((item, index) => (
+      <div className="pane-option" key={index}>
+      {item.key}: {item.value}
+      </div>
+      ))}
+  </div>
+);
+
+export default PaneContent;
+
+
     
