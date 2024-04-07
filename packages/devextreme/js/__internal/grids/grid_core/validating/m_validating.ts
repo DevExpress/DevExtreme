36269f60ddf863722369fa5bf620a1cad3724269
Roman Semenov <roman.semenov@devexpress.com>
2024-04-08 03:32:53 +0400

    fix

--- packages/devextreme/js/__internal/grids/grid_core/validating/m_validating.ts
+++ packages/devextreme/js/__internal/grids/grid_core/validating/m_validating.ts
@@ -1415,7 +1415,7 @@ export const validatingEditorFactoryExtender = (Base: ModuleType<EditorFactory>)
         this._editingController.waitForDeferredOperations().done(() => {
           // NOTE: after waiting for deferred operations another rerender may occur.
           // In this case this validating is outdated
-          const isDetached = $element.closest('tr').length === 0;
+          const isDetached = !this.elementIsInsideGrid($element);
           if (isDetached) {
             return;
           }
