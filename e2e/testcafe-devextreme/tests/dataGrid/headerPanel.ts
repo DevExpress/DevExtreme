29c6f36f99a5f94542fc891f739006cc6b87b815
Roman Semenov <roman.semenov@devexpress.com>
2024-04-08 12:26:56 +0400

    test

--- e2e/testcafe-devextreme/tests/dataGrid/headerPanel.ts
+++ e2e/testcafe-devextreme/tests/dataGrid/headerPanel.ts
@@ -117,3 +117,33 @@ test('Drop-down window should be positioned correctly after resizing the toolbar
     await changeTheme(Themes.genericLight);
   });
 });
+
+
+test(`Toolbar should render on changing visibility if visibiliti is false initially`, async (t) => {
+  const dataGrid = new DataGrid('#container');
+
+  await t
+    .expect(dataGrid.getHeaderPanel().element.visible)
+    .notOk();
+  
+  await dataGrid.option('toolbar.visible', true);
+  
+  await t
+    .expect(dataGrid.getHeaderPanel().element.visible)
+    .ok()
+    .expect(dataGrid.getHeaderPanel().element.find('.dx-button').textContent)
+    .eql('myTestButton');
+
+}).before(async () => createWidget('dxDataGrid', {
+  showBorders: true,
+  toolbar: {
+    items: [{
+        location: "before",
+        widget: "dxButton",
+        options: {
+            text: "myTestButton",
+        },
+    }],
+    visible: false,
+  },
+}));
