a103e670b0b08bfed33981c6fbf4247fd11930eb
Alyar <>
2024-04-08 16:49:47 +0400

    Fix tests

--- packages/devextreme/testing/tests/DevExpress.ui.widgets.scheduler/dragAndDropAppointments.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.widgets.scheduler/dragAndDropAppointments.tests.js
@@ -2320,24 +2320,30 @@ module('Appointment dragging', {
 
     ['default', 'outlook'].forEach((draggingMode) => {
         test(`Drag and drop appointment when draggingMode = ${draggingMode}`, function(assert) {
-            const tasks = [
-                { text: 'Task', startDate: new Date(2015, 2, 17), endDate: new Date(2015, 2, 17, 0, 30) }
-            ];
-            const dataSource = new DataSource({
-                store: tasks
-            });
-            this.createInstance({
-                currentView: 'week',
-                currentDate: new Date(2015, 2, 16),
-                dataSource: dataSource,
-                editing: true,
-                _draggingMode: draggingMode,
-            });
+            $('#qunit-fixture').addClass('qunit-fixture-visible');
 
-            this.scheduler.appointmentList[0].drag.toCell(4);
+            try {
+                const tasks = [
+                    { text: 'Task', startDate: new Date(2015, 2, 17), endDate: new Date(2015, 2, 17, 0, 30) }
+                ];
+                const dataSource = new DataSource({
+                    store: tasks
+                });
+                this.createInstance({
+                    currentView: 'week',
+                    currentDate: new Date(2015, 2, 16),
+                    dataSource: dataSource,
+                    editing: true,
+                    _draggingMode: draggingMode,
+                });
 
-            assert.deepEqual(tasks[0].startDate, new Date(2015, 2, 19), 'Start date is correct');
-            assert.deepEqual(tasks[0].endDate, new Date(2015, 2, 19, 0, 30), 'End date is correct');
+                this.scheduler.appointmentList[0].drag.toCell(4);
+
+                assert.deepEqual(tasks[0].startDate, new Date(2015, 2, 19), 'Start date is correct');
+                assert.deepEqual(tasks[0].endDate, new Date(2015, 2, 19, 0, 30), 'End date is correct');
+            } finally {
+                $('#qunit-fixture').removeClass('qunit-fixture-visible');
+            }
         });
     });
 
