150d26dba99448c55c6714cf143174db1d78d7e2
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-04-11 10:35:41 +0400

    Update grouping.ts

--- e2e/testcafe-devextreme/tests/editors/list/grouping.ts
+++ e2e/testcafe-devextreme/tests/editors/list/grouping.ts
@@ -60,13 +60,13 @@ test('Grouped list appearance', async (t) => {
     items: ['item_1_1', 'item_1_2', 'item_1_3'],
     expanded: false,
   }, {
-  key: 'group_2',
-  items: [
-    { text: 'item_2_1', disabled: true },
-    { text: 'item_2_2', icon: 'home' },
-    { text: 'item_2_3', showChevron: true, badge: 'item_2_3' },
-    { text: 'item_2_4', badge: 'item_2_4' },
-    'item_2_5'],
+    key: 'group_2',
+    items: [
+      { text: 'item_2_1', disabled: true },
+      { text: 'item_2_2', icon: 'home' },
+      { text: 'item_2_3', showChevron: true, badge: 'item_2_3' },
+      { text: 'item_2_4', badge: 'item_2_4' },
+      'item_2_5'],
   }, {
     key: 'group_3',
     items: ['item_3_1', 'item_3_2', 'item_3_3'],
@@ -88,8 +88,8 @@ test('Grouped list appearance', async (t) => {
     const list = new List('#container');
 
     await t
-    .click(list.getGroup(0).header)
-    .click(list.getGroup(2).header);
+      .click(list.getGroup(0).header)
+      .click(list.getGroup(2).header);
 
     await testScreenshot(t, takeScreenshot, `Grouped list appearance with template. rtlEnabled=${rtlEnabled}.png`, { element: '#container' });
 
