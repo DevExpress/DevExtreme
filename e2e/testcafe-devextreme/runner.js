88f4a576cc2445fc93870f93e27f4cb3e0b9fa99
Roman Semenov <roman.semenov@devexpress.com>
2024-04-12 17:25:19 +0400

    fix: make arg boolean

--- e2e/testcafe-devextreme/runner.js
+++ e2e/testcafe-devextreme/runner.js
@@ -182,6 +182,7 @@ function getArgs() {
             theme: '',
             shadowDom: false,
             withUnstable: false,
-        }
+        },
+        boolean: ['withUnstable'],
     });
 }
