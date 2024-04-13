1e28a19c6b97c9f872a7044433b4cc292f5a487d
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-04-13 13:46:11 +0800

    Refactor test

--- packages/devextreme/testing/tests/DevExpress.viz.core/translator2D.tests.js
+++ packages/devextreme/testing/tests/DevExpress.viz.core/translator2D.tests.js
@@ -798,17 +798,13 @@ QUnit.test('Translate. Update translator with business range with empty scale br
     assert.strictEqual(translator.translate(450), 1000);
 });
 
-QUnit.test('Translate. Scale breaks. Translator should handle division by zero (T1222629)', function(assert) {
+QUnit.test('Translate. Scale breaks. Translator should calculate ratioOfCanvasRange correctly (T1222629)', function(assert) {
     const breaks = [{ from: -11.9, to: -10.3, cumulativeWidth: 5 }, { from: -10.3, to: 20.3, cumulativeWidth: 10 }];
-    const translator = createTranslatorWithScaleBreaks.call(this, {});
+    const translator = createTranslatorWithScaleBreaks.call(this, {
+        min: -11.9, max: 20.3, breaks: breaks
+    });
 
-    translator.updateBusinessRange({ min: -11.9, max: 20.3, breaks: breaks });
-
-    const denominator = translator._canvasOptions.rangeMaxVisible - translator._canvasOptions.rangeMinVisible - translator._breaks[translator._breaks.length - 1].length;
-
-    if(denominator === 0) {
-        assert.notEqual(translator._canvasOptions.ratioOfCanvasRange, Infinity);
-    }
+    assert.notEqual(translator.ratioOfCanvasRange(), Infinity);
 });
 
 QUnit.test('Translate. Scale breaks. Values out of the breaks and should be traslated', function(assert) {
