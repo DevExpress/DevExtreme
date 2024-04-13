18d1cada281d262e73f7aef41f2dda769df142e5
Joshua Victoria <jdvictoria.tech@gmail.com>
2024-04-13 12:07:42 +0800

    Pass breaks as arg

--- testing/tests/DevExpress.viz.core/translator2D.tests.js
+++ testing/tests/DevExpress.viz.core/translator2D.tests.js
@@ -800,9 +800,9 @@ QUnit.test('Translate. Update translator with business range with empty scale br
 
 QUnit.test('Translate. Scale breaks. Translator should handle division by zero (T1222629)', function(assert) {
     const breaks = [{ from: -11.9, to: -10.3, cumulativeWidth: 5 }, { from: -10.3, to: 20.3, cumulativeWidth: 10 }];
-    const translator = createTranslatorWithScaleBreaks.call(this, {});
-
-    translator.updateBusinessRange({ min: -11.9, max: 20.3, breaks: breaks });
+    const translator = createTranslatorWithScaleBreaks.call(this, {
+        min: -11.9, max: 20.3, breaks: breaks
+    });
 
     const denominator = translator._canvasOptions.rangeMaxVisible - translator._canvasOptions.rangeMinVisible - translator._breaks[translator._breaks.length - 1].length;
 
