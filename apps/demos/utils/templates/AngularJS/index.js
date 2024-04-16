a15b2e1cedbddfeddc159f8c96db8e6d9f112ca8
Vasily Strelyaev <VasilyStrelyaev@users.noreply.github.com>
2024-04-16 19:28:42 +0300

    chore: remove angularjs integration (#27121)

--- apps/demos/utils/templates/AngularJS/index.js
+++
@@ -1,9 +0,0 @@
-const DemoApp = angular.module('DemoApp', ['dx']);
-
-DemoApp.controller('DemoController', ($scope) => {
-  $scope.dataGridOptions = {
-    dataSource: customers,
-    columns: ['CompanyName', 'City', 'State', 'Phone', 'Fax'],
-    showBorders: true,
-  };
-});
