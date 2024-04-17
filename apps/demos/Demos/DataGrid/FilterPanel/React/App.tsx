6dbc75b6c3a9b0b20b505fcd385ec3de002327b2
dxArtemiusz <artem.kazakov@devexpress.com>
2024-04-18 01:16:20 +0400

    FilterPanel demo fix
    Added aria-label.
    
    Replaced editorOptions from the onIntialized event handler to mark-up declaration in Angular

--- apps/demos/Demos/DataGrid/FilterPanel/React/App.tsx
+++ apps/demos/Demos/DataGrid/FilterPanel/React/App.tsx
@@ -9,7 +9,13 @@ import DataGrid, {
 } from 'devextreme-react/data-grid';
 import { orders } from './data.ts';
 
-const saleAmountEditorOptions = { format: 'currency', showClearButton: true };
+const saleAmountEditorOptions = { 
+  format: 'currency', 
+  showClearButton: true, 
+  inputAttr: {
+    'aria-label': 'Filter Cell',
+  }, 
+};
 const getOrderDay = (rowData) => (new Date(rowData.OrderDate)).getDay();
 
 const filterBuilderPopupPosition = {
