4b063aa18e35961a6efb4506176a0de23492e8ad
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-04-17 17:27:03 +0800

    Revert isColumnStyle test & Integrate rtl

--- e2e/testcafe-devextreme/tests/editors/checkBox/common.ts
+++ e2e/testcafe-devextreme/tests/editors/checkBox/common.ts
@@ -24,53 +24,76 @@ const DISABLED_STATE_CLASS = 'dx-state-disabled';
 fixture.disablePageReloads`CheckBox render`
   .page(url(__dirname, '../../container.html'));
 
-[false, true].forEach((rtlEnabled) => {
-  [20, 30].forEach((iconSize) => {
-    [false, true].forEach((limitedWidth) => {
-      test(`Checkbox styles, rtlEnabled=${rtlEnabled} iconSize=${iconSize} limitedWidth=${limitedWidth}`, async (t) => {
-        const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+[false, true].forEach((isColumnCountStyle) => {
+  [false, true].forEach((limitedWidth) => {
+    test.only(`Checkbox styles, ${!isColumnCountStyle ? 'default' : 'with column-count style on container'} limitedWidth=${limitedWidth}`, async (t) => {
+      const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-        await testScreenshot(t, takeScreenshot, `CheckBox states rtlEnabled=${rtlEnabled} iconSize=${iconSize} limitedWidth=${limitedWidth}.png`, { element: '#container', shouldTestInCompact: true });
+      await testScreenshot(t, takeScreenshot, `CheckBox states${isColumnCountStyle ? ' with column count style' : ''} limitedWidth=${limitedWidth}.png`, { element: '#container', shouldTestInCompact: true });
 
-        await testScreenshot(t, takeScreenshot, `CheckBox states rtlEnabled=${rtlEnabled} iconSize=${iconSize} limitedWidth=${limitedWidth}.png`, { element: '#container', theme: getFullThemeName().replace('light', 'dark') });
+      await testScreenshot(t, takeScreenshot, `CheckBox states${isColumnCountStyle ? ' with column count style' : ''} limitedWidth=${limitedWidth}.png`, { element: '#container', theme: getFullThemeName().replace('light', 'dark') });
 
-        await t
-          .expect(compareResults.isValid())
-          .ok(compareResults.errorMessages());
-      }).before(async () => {
-        const croppedWidth = iconSize === 20 ? 400 : 450;
-        const fullWidth = iconSize === 20 ? 450 : 500;
+      await t
+        .expect(compareResults.isValid())
+        .ok(compareResults.errorMessages());
+    }).before(async () => {
+      // const croppedWidth = iconSize === 20 ? 400 : 450;
+      // const fullWidth = iconSize === 20 ? 450 : 500;
 
-        await setStyleAttribute(Selector('#container'), `padding: 5px; width: ${limitedWidth ? croppedWidth : fullWidth}px; height: 200px; column-count: 6;`);
+      await setStyleAttribute(Selector('#container'), `padding: 5px; width: ${limitedWidth ? 350 : 400}px; height: 200px; ${isColumnCountStyle ? 'column-count: 6' : ''};`);
+      await setStyleAttribute(Selector('#otherContainer'), `padding: 5px; width: ${limitedWidth ? 350 : 400}px; height: 200px; ${isColumnCountStyle ? 'column-count: 6' : ''};`);
 
-        await insertStylesheetRulesToPage(`.${CHECKBOX_CLASS} { display: block; }`);
+      await insertStylesheetRulesToPage(`.${CHECKBOX_CLASS} { display: block; }`);
 
-        for (const state of [
-          READONLY_STATE_CLASS,
-          DEFAULT_STATE_CLASS,
-          ACTIVE_STATE_CLASS,
-          HOVER_STATE_CLASS,
-          FOCUSED_STATE_CLASS,
-          DISABLED_STATE_CLASS,
-        ] as any[]
-        ) {
-          for (const valueMode of valueModes) {
-            const id = `${`dx${new Guid()}`}`;
+      for (const state of [
+        READONLY_STATE_CLASS,
+        DEFAULT_STATE_CLASS,
+        ACTIVE_STATE_CLASS,
+        HOVER_STATE_CLASS,
+        FOCUSED_STATE_CLASS,
+        DISABLED_STATE_CLASS,
+      ] as any[]
+      ) {
+        for (const valueMode of valueModes) {
+          const id = `${`dx${new Guid()}`}`;
 
-            await appendElementTo('#container', 'div', id, { });
+          await appendElementTo('#container', 'div', id, { });
 
-            const options: any = {
-              text: 'Label',
-              value: valueMode,
-              iconSize,
-              rtlEnabled,
-            };
+          const options: any = {
+            text: 'Label',
+            value: valueMode,
+            rtlEnabled: false,
+          };
 
-            await createWidget('dxCheckBox', options, `#${id}`);
-            await setClassAttribute(Selector(`#${id}`), state);
-          }
+          await createWidget('dxCheckBox', options, `#${id}`);
+          await setClassAttribute(Selector(`#${id}`), state);
         }
-      });
+      }
+
+      for (const state of [
+        READONLY_STATE_CLASS,
+        DEFAULT_STATE_CLASS,
+        ACTIVE_STATE_CLASS,
+        HOVER_STATE_CLASS,
+        FOCUSED_STATE_CLASS,
+        DISABLED_STATE_CLASS,
+      ] as any[]
+      ) {
+        for (const valueMode of valueModes) {
+          const id = `${`dx${new Guid()}`}`;
+
+          await appendElementTo('#otherContainer', 'div', id, { });
+
+          const options: any = {
+            text: 'Label',
+            value: valueMode,
+            rtlEnabled: true,
+          };
+
+          await createWidget('dxCheckBox', options, `#${id}`);
+          await setClassAttribute(Selector(`#${id}`), state);
+        }
+      }
     });
   });
 });
