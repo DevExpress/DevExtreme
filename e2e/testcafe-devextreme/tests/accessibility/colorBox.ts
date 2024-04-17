e08b01e4a82b01465c2631c4107964d2f51eee13
Roman Semenov <roman.semenov@devexpress.com>
2024-04-17 16:27:33 +0400

    fix: remove excess clearTestPage

--- e2e/testcafe-devextreme/tests/accessibility/colorBox.ts
+++ e2e/testcafe-devextreme/tests/accessibility/colorBox.ts
@@ -1,12 +1,10 @@
 import { Properties } from 'devextreme/ui/color_box.d';
 import url from '../../helpers/getPageUrl';
-import { clearTestPage } from '../../helpers/clearPage';
 import { testAccessibility, Configuration } from '../../helpers/accessibility/test';
 import { Options } from '../../helpers/generateOptionMatrix';
 
 fixture.disablePageReloads`Accessibility`
-  .page(url(__dirname, '../container.html'))
-  .afterEach(async () => clearTestPage());
+  .page(url(__dirname, '../container.html'));
 
 const options: Options<Properties> = {
   value: [undefined, '#f05b41'],
