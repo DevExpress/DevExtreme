547f7cdda415ba9c96825d6c95976448b80bdda1
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-04-17 15:50:58 +0800

    Refactor import

--- e2e/testcafe-devextreme/tests/editors/checkBox/common.ts
+++ e2e/testcafe-devextreme/tests/editors/checkBox/common.ts
@@ -1,5 +1,5 @@
-import { Selector } from 'testcafe';
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import { Selector } from 'testcafe';
 import Guid from 'devextreme/core/guid';
 import url from '../../../helpers/getPageUrl';
 import { createWidget } from '../../../helpers/createWidget';
