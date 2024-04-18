5f05346cec2a21b856fd483d058940e54eb9a19e
Mikhail Preyskurantov <mikhail.preyskurantov@devexpress.com>
2024-04-18 11:29:30 +0300

    Add initializer for Angular component's instance field (#27187)

--- packages/devextreme-angular/src/ui/switch/index.ts
+++ packages/devextreme-angular/src/ui/switch/index.ts
@@ -69,7 +69,7 @@ const CUSTOM_VALUE_ACCESSOR_PROVIDER = {
     ]
 })
 export class DxSwitchComponent extends DxComponent implements OnDestroy, ControlValueAccessor, OnChanges, DoCheck {
-    instance: DxSwitch;
+    instance: DxSwitch = null;
 
     /**
      * [descr:WidgetOptions.accessKey]
