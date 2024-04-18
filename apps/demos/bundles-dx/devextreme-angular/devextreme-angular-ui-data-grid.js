fe9fc446d8265325cd2ae3df5b1b9792e0e89c67
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2024-04-18 16:32:40 +0400

    DEMOS. update dx-angular local bandles

--- apps/demos/bundles-dx/devextreme-angular/devextreme-angular-ui-data-grid.js
+++ apps/demos/bundles-dx/devextreme-angular/devextreme-angular-ui-data-grid.js
@@ -1,9 +1,9 @@
-System.register(['@angular/platform-browser', '@angular/core', './devextreme-angular-ui-data-grid-Clr9tJTx.js', '@angular/common'], (function (exports) {
+System.register(['@angular/platform-browser', '@angular/core', 'devextreme/bundles/dx.all', 'devextreme/ui/data_grid', './devextreme-angular-ui-data-grid-CuoRQ-MY.js', '@angular/common', 'devextreme/core/dom_adapter', 'devextreme/events', 'devextreme/core/utils/common', 'devextreme/core/renderer', 'devextreme/core/http_request', 'devextreme/core/utils/ready_callbacks', 'devextreme/events/core/events_engine', 'devextreme/core/utils/ajax', 'devextreme/core/utils/deferred'], (function (exports) {
 	'use strict';
 	return {
-		setters: [null, null, function (module) {
-			exports({ DxDataGridComponent: module.iE, DxDataGridModule: module.iD });
-		}, null],
+		setters: [null, null, null, null, function (module) {
+			exports({ DxDataGridComponent: module.dl, DxDataGridModule: module.dk });
+		}, null, null, null, null, null, null, null, null, null, null],
 		execute: (function () {
 
 
