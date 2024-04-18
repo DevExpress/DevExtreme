c11f5c33e27dbf622785b469be0c14313c4134cf
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-04-18 16:25:05 +0800

    Remove code & test

--- packages/devextreme/js/renovation/ui/editors/check_box/check_box_icon.tsx
+++ packages/devextreme/js/renovation/ui/editors/check_box/check_box_icon.tsx
@@ -1,11 +1,7 @@
 import {
-  Component, ComponentBindings, JSXComponent, Ref, Effect, OneWay, RefObject,
+  Component, ComponentBindings, JSXComponent, Ref, OneWay, RefObject,
 } from '@devextreme-generator/declarations';
-import getElementComputedStyle from '../../../utils/get_computed_style';
-import { hasWindow } from '../../../../core/utils/window';
 import { normalizeStyleProp } from '../../../../core/utils/style';
-import { isNumeric as isNumber } from '../../../../core/utils/type';
-import { getFontSizeByIconSize } from './utils';
 
 export const viewFunction = (viewModel: CheckBoxIcon): JSX.Element => {
   const { elementRef, cssStyles } = viewModel;
@@ -27,45 +23,6 @@ export class CheckBoxIconProps {
 })
 export class CheckBoxIcon extends JSXComponent(CheckBoxIconProps) {
   @Ref() elementRef!: RefObject<HTMLDivElement>;
-
-  @Effect()
-  updateFontSize(): void {
-    const { size, isChecked } = this.props;
-
-    if (hasWindow() && size) {
-      const newIconSize = this.getIconSize(size);
-      const newFontSize = getFontSizeByIconSize(newIconSize, isChecked);
-
-      this.setIconFontSize(newFontSize);
-    }
-  }
-
-  setIconFontSize(fontSize: number): void {
-    const element = this.elementRef.current!;
-
-    element.style.fontSize = `${fontSize}px`;
-  }
-
-  getIconSize(size: number | string): number {
-    if (isNumber(size)) {
-      return size;
-    }
-
-    if (size.endsWith('px')) {
-      return parseInt(size, 10);
-    }
-
-    return this.getComputedIconSize();
-  }
-
-  getComputedIconSize(): number {
-    const element = this.elementRef.current!;
-
-    const iconComputedStyle = getElementComputedStyle(element) as CSSStyleDeclaration;
-    const computedIconSize = parseInt(iconComputedStyle?.width, 10);
-
-    return computedIconSize;
-  }
 
   get cssStyles(): { [key: string]: string | number } {
     const { size } = this.props;
