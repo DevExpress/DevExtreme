b4fb131cf733f2f061d418e8451bebe37fd6eb89
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-04-23 16:54:45 +0800

    Create TestCafe tests

--- e2e/testcafe-devextreme/tests/editors/checkBox/common.ts
+++ e2e/testcafe-devextreme/tests/editors/checkBox/common.ts
@@ -1,14 +1,26 @@
+/* eslint-disable no-restricted-syntax */
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import { Selector } from 'testcafe';
+import Guid from 'devextreme/core/guid';
 import url from '../../../helpers/getPageUrl';
 import { createWidget } from '../../../helpers/createWidget';
 import {
   appendElementTo,
-  insertStylesheetRulesToPage, setStyleAttribute,
+  insertStylesheetRulesToPage,
+  setClassAttribute,
+  setStyleAttribute,
 } from '../../../helpers/domUtils';
-import { testScreenshot } from '../../../helpers/themeUtils';
+import { getDarkThemeName, testScreenshot } from '../../../helpers/themeUtils';
+
+const valueModes = [false, true, undefined];
 
 const CHECKBOX_CLASS = 'dx-checkbox';
+const READONLY_STATE_CLASS = 'dx-state-readonly';
+const DEFAULT_STATE_CLASS = '';
+const ACTIVE_STATE_CLASS = 'dx-state-active';
+const HOVER_STATE_CLASS = 'dx-state-hover';
+const FOCUSED_STATE_CLASS = 'dx-state-focused';
+const DISABLED_STATE_CLASS = 'dx-state-disabled';
 
 fixture.disablePageReloads`CheckBox`
   .page(url(__dirname, '../../container.html'));
@@ -46,4 +58,67 @@ fixture.disablePageReloads`CheckBox`
     await appendElementTo('#container', 'div', 'indeterminateRTL');
     await createWidget('dxCheckBox', { value: undefined, text: 'indeterminate', rtlEnabled: true }, '#indeterminateRTL');
   });
+});
+
+test('Checkbox configuration, different Checkbox icon sizes, states, value Modes', async (t) => {
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+  await testScreenshot(t, takeScreenshot, 'CheckBox configurations different iconSizes.png', { element: '#container', shouldTestInCompact: true });
+
+  await testScreenshot(t, takeScreenshot, 'CheckBox configurations different iconSizes.png', { element: '#container', theme: getDarkThemeName() });
+
+  await t
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => {
+  for (const iconSize of [20, undefined, 45]) {
+    for (const state of [
+      READONLY_STATE_CLASS,
+      DEFAULT_STATE_CLASS,
+      ACTIVE_STATE_CLASS,
+      HOVER_STATE_CLASS,
+      FOCUSED_STATE_CLASS,
+      DISABLED_STATE_CLASS,
+    ] as string[]
+    ) {
+      for (const value of valueModes) {
+        const id = `dx${new Guid()}`;
+        await appendElementTo('#container', 'div', id, {});
+
+        await createWidget('dxCheckBox', {
+          value,
+          iconSize,
+        }, `#${id}`);
+        await setClassAttribute(Selector(`#${id}`), state);
+      }
+    }
+  }
+});
+
+test('Checkbox configuration, different Checkbox orientation and width', async (t) => {
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+  await testScreenshot(t, takeScreenshot, 'CheckBox configurations different rtlOrientation and width.png', { element: '#container', shouldTestInCompact: true });
+
+  await testScreenshot(t, takeScreenshot, 'CheckBox configurations different rtlOrientation and width.png', { element: '#container', theme: getDarkThemeName() });
+
+  await t
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => {
+  for (const rtlEnabled of [false, true]) {
+    for (const width of [undefined, 45, 65]) {
+      for (const text of [undefined, 'label', 'one two three']) {
+        const id = `dx${new Guid()}`;
+        await appendElementTo('#container', 'div', id, {});
+
+        await createWidget('dxCheckBox', {
+          text,
+          width,
+          rtlEnabled,
+        }, `#${id}`);
+        await setClassAttribute(Selector(`#${id}`), DEFAULT_STATE_CLASS);
+      }
+    }
+  }
 });
