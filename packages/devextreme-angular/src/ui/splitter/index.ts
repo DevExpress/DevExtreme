67851dc38049dc3adcca7c5bbf6ed3f584acbbf3
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2024-04-23 21:31:11 +0400

    DX-NG. Update to Angular 17 +regenerate

---
+++ packages/devextreme-angular/src/ui/splitter/index.ts
@@ -0,0 +1,563 @@
+/* tslint:disable:max-line-length */
+
+
+import { TransferState } from '@angular/platform-browser';
+
+import {
+    Component,
+    NgModule,
+    ElementRef,
+    NgZone,
+    PLATFORM_ID,
+    Inject,
+
+    Input,
+    Output,
+    OnDestroy,
+    EventEmitter,
+    OnChanges,
+    DoCheck,
+    SimpleChanges,
+    ContentChildren,
+    QueryList
+} from '@angular/core';
+
+export { ExplicitTypes } from 'devextreme/ui/splitter';
+
+import DevExpress from 'devextreme/bundles/dx.all';
+import { Orientation } from 'devextreme/common';
+import { Store } from 'devextreme/data';
+import DataSource, { Options as DataSourceOptions } from 'devextreme/data/data_source';
+import { ContentReadyEvent, DisposingEvent, InitializedEvent, ItemClickEvent, ItemCollapsedEvent, ItemContextMenuEvent, ItemExpandedEvent, ItemRenderedEvent, OptionChangedEvent, Properties as dxSplitterOptions, ResizeEndEvent, ResizeEvent, ResizeStartEvent } from 'devextreme/ui/splitter';
+
+import DxSplitter from 'devextreme/ui/splitter';
+
+
+import {
+    DxComponent,
+    DxTemplateHost,
+    DxIntegrationModule,
+    DxTemplateModule,
+    NestedOptionHost,
+    IterableDifferHelper,
+    WatcherHelper
+} from 'devextreme-angular/core';
+
+import { DxiItemModule } from 'devextreme-angular/ui/nested';
+import { DxoSplitterModule } from 'devextreme-angular/ui/nested';
+
+import { DxiItemComponent } from 'devextreme-angular/ui/nested';
+
+
+
+/**
+ * [descr:dxSplitter]
+
+ */
+@Component({
+    selector: 'dx-splitter',
+    template: '',
+    providers: [
+        DxTemplateHost,
+        WatcherHelper,
+        NestedOptionHost,
+        IterableDifferHelper
+    ]
+})
+export class DxSplitterComponent<TItem = any, TKey = any> extends DxComponent implements OnDestroy, OnChanges, DoCheck {
+    instance: DxSplitter<TItem, TKey> = null;
+
+    /**
+     * [descr:dxSplitterOptions.allowKeyboardNavigation]
+    
+     */
+    @Input()
+    get allowKeyboardNavigation(): boolean {
+        return this._getOption('allowKeyboardNavigation');
+    }
+    set allowKeyboardNavigation(value: boolean) {
+        this._setOption('allowKeyboardNavigation', value);
+    }
+
+
+    /**
+     * [descr:dxSplitterOptions.dataSource]
+    
+     */
+    @Input()
+    get dataSource(): DataSource | DataSourceOptions | Store | null | string | Array<string | DevExpress.ui.dxSplitterItem | any> {
+        return this._getOption('dataSource');
+    }
+    set dataSource(value: DataSource | DataSourceOptions | Store | null | string | Array<string | DevExpress.ui.dxSplitterItem | any>) {
+        this._setOption('dataSource', value);
+    }
+
+
+    /**
+     * [descr:WidgetOptions.disabled]
+    
+     */
+    @Input()
+    get disabled(): boolean {
+        return this._getOption('disabled');
+    }
+    set disabled(value: boolean) {
+        this._setOption('disabled', value);
+    }
+
+
+    /**
+     * [descr:DOMComponentOptions.elementAttr]
+    
+     */
+    @Input()
+    get elementAttr(): any {
+        return this._getOption('elementAttr');
+    }
+    set elementAttr(value: any) {
+        this._setOption('elementAttr', value);
+    }
+
+
+    /**
+     * [descr:DOMComponentOptions.height]
+    
+     */
+    @Input()
+    get height(): number | Function | string | undefined {
+        return this._getOption('height');
+    }
+    set height(value: number | Function | string | undefined) {
+        this._setOption('height', value);
+    }
+
+
+    /**
+     * [descr:WidgetOptions.hoverStateEnabled]
+    
+     */
+    @Input()
+    get hoverStateEnabled(): boolean {
+        return this._getOption('hoverStateEnabled');
+    }
+    set hoverStateEnabled(value: boolean) {
+        this._setOption('hoverStateEnabled', value);
+    }
+
+
+    /**
+     * [descr:dxSplitterOptions.items]
+    
+     */
+    @Input()
+    get items(): Array<string | any | { collapsed?: boolean, collapsedSize?: number | string | undefined, collapsible?: boolean, maxSize?: number | string | undefined, minSize?: number | string | undefined, resizable?: boolean, size?: number | string | undefined, splitter?: dxSplitterOptions | undefined, template?: any, text?: string, visible?: boolean }> {
+        return this._getOption('items');
+    }
+    set items(value: Array<string | any | { collapsed?: boolean, collapsedSize?: number | string | undefined, collapsible?: boolean, maxSize?: number | string | undefined, minSize?: number | string | undefined, resizable?: boolean, size?: number | string | undefined, splitter?: dxSplitterOptions | undefined, template?: any, text?: string, visible?: boolean }>) {
+        this._setOption('items', value);
+    }
+
+
+    /**
+     * [descr:CollectionWidgetOptions.itemTemplate]
+    
+     */
+    @Input()
+    get itemTemplate(): any {
+        return this._getOption('itemTemplate');
+    }
+    set itemTemplate(value: any) {
+        this._setOption('itemTemplate', value);
+    }
+
+
+    /**
+     * [descr:dxSplitterOptions.orientation]
+    
+     */
+    @Input()
+    get orientation(): Orientation {
+        return this._getOption('orientation');
+    }
+    set orientation(value: Orientation) {
+        this._setOption('orientation', value);
+    }
+
+
+    /**
+     * [descr:dxSplitterOptions.repaintChangesOnly]
+    
+     */
+    @Input()
+    get repaintChangesOnly(): boolean {
+        return this._getOption('repaintChangesOnly');
+    }
+    set repaintChangesOnly(value: boolean) {
+        this._setOption('repaintChangesOnly', value);
+    }
+
+
+    /**
+     * [descr:DOMComponentOptions.rtlEnabled]
+    
+     */
+    @Input()
+    get rtlEnabled(): boolean {
+        return this._getOption('rtlEnabled');
+    }
+    set rtlEnabled(value: boolean) {
+        this._setOption('rtlEnabled', value);
+    }
+
+
+    /**
+     * [descr:dxSplitterOptions.separatorSize]
+    
+     */
+    @Input()
+    get separatorSize(): number {
+        return this._getOption('separatorSize');
+    }
+    set separatorSize(value: number) {
+        this._setOption('separatorSize', value);
+    }
+
+
+    /**
+     * [descr:WidgetOptions.visible]
+    
+     */
+    @Input()
+    get visible(): boolean {
+        return this._getOption('visible');
+    }
+    set visible(value: boolean) {
+        this._setOption('visible', value);
+    }
+
+
+    /**
+     * [descr:DOMComponentOptions.width]
+    
+     */
+    @Input()
+    get width(): number | Function | string | undefined {
+        return this._getOption('width');
+    }
+    set width(value: number | Function | string | undefined) {
+        this._setOption('width', value);
+    }
+
+    /**
+    
+     * [descr:dxSplitterOptions.onContentReady]
+    
+    
+     */
+    @Output() onContentReady: EventEmitter<ContentReadyEvent>;
+
+    /**
+    
+     * [descr:dxSplitterOptions.onDisposing]
+    
+    
+     */
+    @Output() onDisposing: EventEmitter<DisposingEvent>;
+
+    /**
+    
+     * [descr:dxSplitterOptions.onInitialized]
+    
+    
+     */
+    @Output() onInitialized: EventEmitter<InitializedEvent>;
+
+    /**
+    
+     * [descr:dxSplitterOptions.onItemClick]
+    
+    
+     */
+    @Output() onItemClick: EventEmitter<ItemClickEvent>;
+
+    /**
+    
+     * [descr:dxSplitterOptions.onItemCollapsed]
+    
+    
+     */
+    @Output() onItemCollapsed: EventEmitter<ItemCollapsedEvent>;
+
+    /**
+    
+     * [descr:dxSplitterOptions.onItemContextMenu]
+    
+    
+     */
+    @Output() onItemContextMenu: EventEmitter<ItemContextMenuEvent>;
+
+    /**
+    
+     * [descr:dxSplitterOptions.onItemExpanded]
+    
+    
+     */
+    @Output() onItemExpanded: EventEmitter<ItemExpandedEvent>;
+
+    /**
+    
+     * [descr:dxSplitterOptions.onItemRendered]
+    
+    
+     */
+    @Output() onItemRendered: EventEmitter<ItemRenderedEvent>;
+
+    /**
+    
+     * [descr:dxSplitterOptions.onOptionChanged]
+    
+    
+     */
+    @Output() onOptionChanged: EventEmitter<OptionChangedEvent>;
+
+    /**
+    
+     * [descr:dxSplitterOptions.onResize]
+    
+    
+     */
+    @Output() onResize: EventEmitter<ResizeEvent>;
+
+    /**
+    
+     * [descr:dxSplitterOptions.onResizeEnd]
+    
+    
+     */
+    @Output() onResizeEnd: EventEmitter<ResizeEndEvent>;
+
+    /**
+    
+     * [descr:dxSplitterOptions.onResizeStart]
+    
+    
+     */
+    @Output() onResizeStart: EventEmitter<ResizeStartEvent>;
+
+    /**
+    
+     * This member supports the internal infrastructure and is not intended to be used directly from your code.
+    
+     */
+    @Output() allowKeyboardNavigationChange: EventEmitter<boolean>;
+
+    /**
+    
+     * This member supports the internal infrastructure and is not intended to be used directly from your code.
+    
+     */
+    @Output() dataSourceChange: EventEmitter<DataSource | DataSourceOptions | Store | null | string | Array<string | DevExpress.ui.dxSplitterItem | any>>;
+
+    /**
+    
+     * This member supports the internal infrastructure and is not intended to be used directly from your code.
+    
+     */
+    @Output() disabledChange: EventEmitter<boolean>;
+
+    /**
+    
+     * This member supports the internal infrastructure and is not intended to be used directly from your code.
+    
+     */
+    @Output() elementAttrChange: EventEmitter<any>;
+
+    /**
+    
+     * This member supports the internal infrastructure and is not intended to be used directly from your code.
+    
+     */
+    @Output() heightChange: EventEmitter<number | Function | string | undefined>;
+
+    /**
+    
+     * This member supports the internal infrastructure and is not intended to be used directly from your code.
+    
+     */
+    @Output() hoverStateEnabledChange: EventEmitter<boolean>;
+
+    /**
+    
+     * This member supports the internal infrastructure and is not intended to be used directly from your code.
+    
+     */
+    @Output() itemsChange: EventEmitter<Array<string | any | { collapsed?: boolean, collapsedSize?: number | string | undefined, collapsible?: boolean, maxSize?: number | string | undefined, minSize?: number | string | undefined, resizable?: boolean, size?: number | string | undefined, splitter?: dxSplitterOptions | undefined, template?: any, text?: string, visible?: boolean }>>;
+
+    /**
+    
+     * This member supports the internal infrastructure and is not intended to be used directly from your code.
+    
+     */
+    @Output() itemTemplateChange: EventEmitter<any>;
+
+    /**
+    
+     * This member supports the internal infrastructure and is not intended to be used directly from your code.
+    
+     */
+    @Output() orientationChange: EventEmitter<Orientation>;
+
+    /**
+    
+     * This member supports the internal infrastructure and is not intended to be used directly from your code.
+    
+     */
+    @Output() repaintChangesOnlyChange: EventEmitter<boolean>;
+
+    /**
+    
+     * This member supports the internal infrastructure and is not intended to be used directly from your code.
+    
+     */
+    @Output() rtlEnabledChange: EventEmitter<boolean>;
+
+    /**
+    
+     * This member supports the internal infrastructure and is not intended to be used directly from your code.
+    
+     */
+    @Output() separatorSizeChange: EventEmitter<number>;
+
+    /**
+    
+     * This member supports the internal infrastructure and is not intended to be used directly from your code.
+    
+     */
+    @Output() visibleChange: EventEmitter<boolean>;
+
+    /**
+    
+     * This member supports the internal infrastructure and is not intended to be used directly from your code.
+    
+     */
+    @Output() widthChange: EventEmitter<number | Function | string | undefined>;
+
+
+
+
+    @ContentChildren(DxiItemComponent)
+    get itemsChildren(): QueryList<DxiItemComponent> {
+        return this._getOption('items');
+    }
+    set itemsChildren(value) {
+        this.setChildren('items', value);
+    }
+
+
+
+
+    constructor(elementRef: ElementRef, ngZone: NgZone, templateHost: DxTemplateHost,
+            private _watcherHelper: WatcherHelper,
+            private _idh: IterableDifferHelper,
+            optionHost: NestedOptionHost,
+            transferState: TransferState,
+            @Inject(PLATFORM_ID) platformId: any) {
+
+        super(elementRef, ngZone, templateHost, _watcherHelper, transferState, platformId);
+
+        this._createEventEmitters([
+            { subscribe: 'contentReady', emit: 'onContentReady' },
+            { subscribe: 'disposing', emit: 'onDisposing' },
+            { subscribe: 'initialized', emit: 'onInitialized' },
+            { subscribe: 'itemClick', emit: 'onItemClick' },
+            { subscribe: 'itemCollapsed', emit: 'onItemCollapsed' },
+            { subscribe: 'itemContextMenu', emit: 'onItemContextMenu' },
+            { subscribe: 'itemExpanded', emit: 'onItemExpanded' },
+            { subscribe: 'itemRendered', emit: 'onItemRendered' },
+            { subscribe: 'optionChanged', emit: 'onOptionChanged' },
+            { subscribe: 'resize', emit: 'onResize' },
+            { subscribe: 'resizeEnd', emit: 'onResizeEnd' },
+            { subscribe: 'resizeStart', emit: 'onResizeStart' },
+            { emit: 'allowKeyboardNavigationChange' },
+            { emit: 'dataSourceChange' },
+            { emit: 'disabledChange' },
+            { emit: 'elementAttrChange' },
+            { emit: 'heightChange' },
+            { emit: 'hoverStateEnabledChange' },
+            { emit: 'itemsChange' },
+            { emit: 'itemTemplateChange' },
+            { emit: 'orientationChange' },
+            { emit: 'repaintChangesOnlyChange' },
+            { emit: 'rtlEnabledChange' },
+            { emit: 'separatorSizeChange' },
+            { emit: 'visibleChange' },
+            { emit: 'widthChange' }
+        ]);
+
+        this._idh.setHost(this);
+        optionHost.setHost(this);
+    }
+
+    protected _createInstance(element, options) {
+
+        return new DxSplitter(element, options);
+    }
+
+
+    ngOnDestroy() {
+        this._destroyWidget();
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        super.ngOnChanges(changes);
+        this.setupChanges('dataSource', changes);
+        this.setupChanges('items', changes);
+    }
+
+    setupChanges(prop: string, changes: SimpleChanges) {
+        if (!(prop in this._optionsToUpdate)) {
+            this._idh.setup(prop, changes);
+        }
+    }
+
+    ngDoCheck() {
+        this._idh.doCheck('dataSource');
+        this._idh.doCheck('items');
+        this._watcherHelper.checkWatchers();
+        super.ngDoCheck();
+        super.clearChangedOptions();
+    }
+
+    _setOption(name: string, value: any) {
+        let isSetup = this._idh.setupSingle(name, value);
+        let isChanged = this._idh.getChanges(name, value) !== null;
+
+        if (isSetup || isChanged) {
+            super._setOption(name, value);
+        }
+    }
+}
+
+@NgModule({
+  imports: [
+    DxiItemModule,
+    DxoSplitterModule,
+    DxIntegrationModule,
+    DxTemplateModule
+  ],
+  declarations: [
+    DxSplitterComponent
+  ],
+  exports: [
+    DxSplitterComponent,
+    DxiItemModule,
+    DxoSplitterModule,
+    DxTemplateModule
+  ]
+})
+export class DxSplitterModule { }
+
+import type * as DxSplitterTypes from "devextreme/ui/splitter_types";
+export { DxSplitterTypes };
+
+
