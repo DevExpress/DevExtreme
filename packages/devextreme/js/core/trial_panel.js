3d16426e74b18800e5219adb0e72ae5b48348385
Vasily Strelyaev <VasilyStrelyaev@users.noreply.github.com>
2024-04-24 09:24:32 +0300

    Add trial panel (#27186)

---
+++ packages/devextreme/js/core/trial_panel.js
@@ -0,0 +1,6 @@
+import { registerTrialPanelComponents, showTrialPanel } from '../__internal/core/license/trial_panel';
+
+export default {
+    registerTrialPanelComponents,
+    showTrialPanel,
+};
