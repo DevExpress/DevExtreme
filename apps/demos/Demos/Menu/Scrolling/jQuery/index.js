12ab33c7d6dbd0eb52579949680f5a8f8da16994
Alexander Bulychev <git@bulychev.net>
2024-04-25 10:36:55 +0400

    Review fixes

--- apps/demos/Demos/Menu/Scrolling/jQuery/index.js
+++ apps/demos/Demos/Menu/Scrolling/jQuery/index.js
@@ -1,57 +1,19 @@
 $(() => {
   const SUBMENU_HEIGHT = 200;
   let submenuMaxHeight = 0;
-  const dxMenu = $('#menu').dxMenu({
+
+  $('#menu').dxMenu({
     dataSource: menuData,
     hideSubmenuOnMouseLeave: false,
     onSubmenuShowing: ({ submenuContainer }) => {
       $(submenuContainer).css('maxHeight', submenuMaxHeight || '');
     },
-    onItemClick(data) {
-      const item = data.itemData;
-      if (item.price) {
-        $('#product-details').removeClass('hidden');
-        $('#product-details > img').attr('src', item.icon);
-        $('#product-details > .price').text(`$${item.price}`);
-        $('#product-details > .name').text(item.name);
+    onItemClick(e) {
+      if (!e.itemData.items) {
+        DevExpress.ui.notify(`The "${e.itemData.text}" item was clicked`, 'success', 1500);
       }
     },
   }).dxMenu('instance');
-
-  const showSubmenuModes = [{
-    name: 'onHover',
-    delay: { show: 0, hide: 500 },
-  }, {
-    name: 'onClick',
-    delay: { show: 0, hide: 300 },
-  }];
-
-  $('#show-submenu-mode').dxSelectBox({
-    items: showSubmenuModes,
-    value: showSubmenuModes[1],
-    inputAttr: { 'aria-label': 'Submenu Mode' },
-    displayExpr: 'name',
-    onValueChanged(data) {
-      dxMenu.option('showFirstSubmenuMode', data.value);
-    },
-  });
-
-  $('#orientation').dxSelectBox({
-    items: ['horizontal', 'vertical'],
-    inputAttr: { 'aria-label': 'Orientation' },
-    value: 'horizontal',
-    onValueChanged(data) {
-      dxMenu.option('orientation', data.value);
-    },
-  });
-
-  $('#mouse-leave').dxCheckBox({
-    value: false,
-    text: 'Hide Submenu on Mouse Leave',
-    onValueChanged(data) {
-      dxMenu.option('hideSubmenuOnMouseLeave', data.value);
-    },
-  });
 
   $('#limit-height').dxCheckBox({
     value: false,
