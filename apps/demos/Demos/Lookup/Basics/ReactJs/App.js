f5a94c9da618d132c3bb96547e9a93cc65916ce9
dxArtemiusz <artem.kazakov@devexpress.com>
2024-04-25 13:29:35 +0400

    Update App.js

--- apps/demos/Demos/Lookup/Basics/ReactJs/App.js
+++ apps/demos/Demos/Lookup/Basics/ReactJs/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Lookup, DropDownOptions } from 'devextreme-react/lookup';
 import DataSource from 'devextreme/data/data_source';
-import ArrayStore from 'devextreme/data/array_store.js';
+import ArrayStore from 'devextreme/data/array_store';
 import { employeesList, employeesTasks } from './data.js';
 
 const simpleLookupLabel = { 'aria-label': 'Simple lookup' };
