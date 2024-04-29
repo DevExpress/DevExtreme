74274b987ca9bd9e62cbb3ecd69569137a4f0cae
Nikki Gonzales <38495263+nikkithelegendarypokemonster@users.noreply.github.com>
2024-04-29 16:13:22 +0800

    ContextMenu / Scrolling: Apply in React Demo

---
+++ apps/demos/Demos/ContextMenu/Scrolling/React/App.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import ContextMenu, { ContextMenuTypes } from 'devextreme-react/context-menu';
+import notify from 'devextreme/ui/notify';
+import { contextMenuItems as items } from './data.ts';
+
+function itemClick(e){
+  if (!e.itemData.items) {
+    notify(`The "${e.itemData.text}" item was clicked`, 'success', 1500);
+  }
+}
+
+function App() {
+
+  return (
+    <React.Fragment>
+      <div className="target-area">
+        Right click here to show the context menu
+      </div>
+      <ContextMenu
+        dataSource={items}
+        width={200}
+        target= ".target-area"
+        onItemClick= {itemClick}
+      />
+    </React.Fragment>
+  );
+}
+
+export default App;
