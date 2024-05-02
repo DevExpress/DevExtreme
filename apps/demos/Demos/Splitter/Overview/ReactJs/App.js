37bc2a332e0b2d6f167758571fc549134ff01537
Nikki Gonzales <38495263+nikkithelegendarypokemonster@users.noreply.github.com>
2024-05-02 18:28:13 +0800

    Splitter: update ReactJs demo

--- apps/demos/Demos/Splitter/Overview/ReactJs/App.js
+++ apps/demos/Demos/Splitter/Overview/ReactJs/App.js
@@ -12,57 +12,69 @@ const PaneContentWithTitleRender = (title, initialSize) => {
   );
   return PaneContentRender;
 };
-const App = () => (
-  <React.Fragment>
-    <Splitter id="splitter">
-      <Item
-        resizable={true}
-        size="140px"
-        minSize="70px"
-        render={PaneContentWithTitleRender('Left Pane', '140px')}
-      />
-      <Item resizable={true}>
-        <Splitter orientation="vertical">
-          <Item
-            resizable={true}
-            collapsible={true}
-            maxSize="75%"
-            render={PaneContentWithTitleRender('Central Pane')}
-          />
-          <Item
-            resizable={true}
-            collapsible={true}
+const App = () => {
+  const itemRendered = (e) => {
+    const item = e.itemElement.querySelectorAll('.dx-splitter-item-content')[0];
+    item.setAttribute('tabIndex', '0');
+  };
+  return (
+    <React.Fragment>
+      <Splitter
+        id="splitter"
+        onItemRendered={itemRendered}
+      >
+        <Item
+          resizable={true}
+          size="140px"
+          minSize="70px"
+          render={PaneContentWithTitleRender('Left Pane', '140px')}
+        />
+        <Item resizable={true}>
+          <Splitter
+            orientation="vertical"
+            onItemRendered={itemRendered}
           >
-            <Splitter>
-              <Item
-                resizable={true}
-                collapsible={true}
-                size="30%"
-                minSize="5%"
-                render={PaneContentWithTitleRender('Nested Left Pane', '30%')}
-              />
-              <Item
-                resizable={true}
-                render={PaneContentWithTitleRender('Nested Central Pane')}
-              />
-              <Item
-                resizable={true}
-                collapsible={true}
-                size="30%"
-                minSize="5%"
-                render={PaneContentWithTitleRender('Nested Right Pane', '30%')}
-              />
-            </Splitter>
-          </Item>
-        </Splitter>
-      </Item>
-      <Item
-        resizable={false}
-        collapsible={false}
-        size="140px"
-        render={PaneContentWithTitleRender('Right Pane', '140px')}
-      />
-    </Splitter>
-  </React.Fragment>
-);
+            <Item
+              resizable={true}
+              collapsible={true}
+              maxSize="75%"
+              render={PaneContentWithTitleRender('Central Pane')}
+            />
+            <Item
+              resizable={true}
+              collapsible={true}
+            >
+              <Splitter onItemRendered={itemRendered}>
+                <Item
+                  resizable={true}
+                  collapsible={true}
+                  size="30%"
+                  minSize="5%"
+                  render={PaneContentWithTitleRender('Nested Left Pane', '30%')}
+                />
+                <Item
+                  resizable={true}
+                  render={PaneContentWithTitleRender('Nested Central Pane')}
+                />
+                <Item
+                  resizable={true}
+                  collapsible={true}
+                  size="30%"
+                  minSize="5%"
+                  render={PaneContentWithTitleRender('Nested Right Pane', '30%')}
+                />
+              </Splitter>
+            </Item>
+          </Splitter>
+        </Item>
+        <Item
+          resizable={false}
+          collapsible={false}
+          size="140px"
+          render={PaneContentWithTitleRender('Right Pane', '140px')}
+        />
+      </Splitter>
+    </React.Fragment>
+  );
+};
 export default App;
