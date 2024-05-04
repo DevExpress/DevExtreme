d38d7760edd6d6a50d0fc6fcf993c6af97438df7
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-05-04 21:36:20 +0400

    update testcafe-runner

--- apps/demos/utils/visual-tests/testcafe-runner.mjs
+++ apps/demos/utils/visual-tests/testcafe-runner.mjs
@@ -218,7 +218,7 @@ async function main() {
     .reporter(reporters)
     .browsers(process.env.THEME === 'material.blue.light'
       ? 'chrome:headless --disable-gpu'
-      : 'chrome --disable-partial-raster --disable-skia-runtime-opts --run-all-compositor-stages-before-draw --disable-new-content-rendering-timeout --disable-threaded-animation --disable-threaded-scrolling --disable-checker-imaging --disable-image-animation-resync --use-gl="swiftshader" --disable-features=PaintHolding --js-flags=--random-seed=2147483647 --font-render-hinting=none --disable-font-subpixel-positioning')
+      : process.env.BROWSERS || 'chrome:headless --disable-partial-raster --disable-skia-runtime-opts --run-all-compositor-stages-before-draw --disable-new-content-rendering-timeout --disable-threaded-animation --disable-threaded-scrolling --disable-checker-imaging --disable-image-animation-resync --use-gl="swiftshader" --disable-features=PaintHolding --js-flags=--random-seed=2147483647 --font-render-hinting=none --disable-font-subpixel-positioning')
     .concurrency(concurrency || 1)
     .run({
       quarantineMode: process.env.TCQUARANTINE ? { successThreshold: 1, attemptLimit: 5 } : false,
