65e9b7f7d24dbcdf12f1485fbd60622071d9d235
williamvinogradov <williamvinogradov@gmail.com>
2024-05-06 22:19:22 +0400

    Re-exports: move STYLE comments

--- packages/devextreme/js/ui/data_grid/ui.data_grid.js
+++ packages/devextreme/js/ui/data_grid/ui.data_grid.js
@@ -1,5 +1,3 @@
 import DataGrid from '../../__internal/grids/data_grid/m_widget';
 // deprecated
 export default DataGrid;
-
-// STYLE dataGrid
--- packages/devextreme/js/ui/tree_list/ui.tree_list.js
+++ packages/devextreme/js/ui/tree_list/ui.tree_list.js
@@ -1,4 +1,2 @@
 import TreeList from '../../__internal/grids/tree_list/m_widget';
 export default TreeList;
-
-// STYLE treeList
