2e536e2c33282b21fc2f6024f65d71ea10d59646
Rochmar Nicolas (DevExpress) <rochmar.nicolas@devsoft.ph>
2024-05-06 10:51:32 +0800

    Add guard handlers test for non strict mode

--- packages/devextreme-react/src/core/__tests__/props-updating.test.tsx
+++ packages/devextreme-react/src/core/__tests__/props-updating.test.tsx
@@ -676,6 +676,25 @@ describe('cfg-component option control', () => {
     expect(Widget.option.mock.calls[1]).toEqual(['nestedOption.b', 'const']);
   });
 
+  it('invokes option change guard handlers', () => {
+    const TestContainer = ({ value }: { value: number }) => {
+      return (
+        <ControlledComponent>
+          <NestedComponent a={value} />
+        </ControlledComponent>
+      );
+    };
+
+    const { rerender } = render(<TestContainer value={123} />);
+
+    fireOptionChange('nestedOption.a', 234);
+
+    rerender(<TestContainer value={123} />)
+    jest.runAllTimers();
+
+    expect(Widget.option).toHaveBeenCalledWith('nestedOption.a', 123);
+  });
+
   it('invokes option change guard handlers in strict mode', () => {
     const TestContainer = ({ value }: { value: number }) => {
       return (
