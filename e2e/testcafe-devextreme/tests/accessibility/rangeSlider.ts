7719ed5fda13b4961fe67a42f5d296d833b8bdf6
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-05-06 18:07:48 +0800

    Create rangeSlider.ts

---
+++ e2e/testcafe-devextreme/tests/accessibility/rangeSlider.ts
@@ -0,0 +1,37 @@
+import { Properties } from 'devextreme/ui/range_slider.d';
+import url from '../../helpers/getPageUrl';
+import { testAccessibility, Configuration } from '../../helpers/accessibility/test';
+import { Options } from '../../helpers/generateOptionMatrix';
+
+fixture.disablePageReloads`Accessibility`
+  .page(url(__dirname, '../container.html'));
+
+const options: Options<Properties> = {
+  value: [[0, 0], [40, 60]],
+  disabled: [true, false],
+  readOnly: [true, false],
+  hint: [undefined, 'hint'],
+  startName: ['', 'startName'],
+  endName: ['', 'endName'],
+  height: [undefined, 250],
+  width: [450, 'auto', '100%'],
+  min: [undefined, 10],
+  max: [undefined, 90],
+  focusStateEnabled: [false, true],
+  validationMessageMode: [undefined, 'auto', 'always'],
+  validationStatus: [undefined, 'valid', 'invalid', 'pending'],
+  elementAttr: [{ 'aria-label': 'Start Value' }, { 'aria-label': 'End Value' }],
+};
+
+const a11yCheckConfig = {
+  // NOTE: color-contrast issues
+  rules: { 'color-contrast': { enabled: false } },
+};
+
+const configuration: Configuration = {
+  component: 'dxCheckBox',
+  a11yCheckConfig,
+  options,
+};
+
+testAccessibility(configuration);
