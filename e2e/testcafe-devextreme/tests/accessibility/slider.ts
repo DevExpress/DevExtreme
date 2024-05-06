3c9872094e1ed5a4ac515039a599c1817db692b5
Joshua Victoria <jdvictoria.tech@gmail.com>
2024-05-06 23:24:17 +0800

    Refactor configuration (dxSlider)

--- e2e/testcafe-devextreme/tests/accessibility/slider.ts
+++ e2e/testcafe-devextreme/tests/accessibility/slider.ts
@@ -7,13 +7,13 @@ fixture.disablePageReloads`Accessibility`
   .page(url(__dirname, '../container.html'));
 
 const options: Options<Properties> = {
-  value: [10, 90],
+  value: [45],
   disabled: [true, false],
   readOnly: [true, false],
   hint: [undefined, 'hint'],
   name: ['', 'name'],
   height: [undefined, 250],
-  width: [450, 'auto', '100%'],
+  width: [undefined, '50%'],
   min: [undefined, 10],
   max: [undefined, 90],
   focusStateEnabled: [true],
