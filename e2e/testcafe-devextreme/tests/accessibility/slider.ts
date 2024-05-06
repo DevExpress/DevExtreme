6e13dbe919102186f8d5d15bae3cee3f3b27fc0d
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-05-06 19:19:49 +0800

    Create slider.ts

---
+++ e2e/testcafe-devextreme/tests/accessibility/slider.ts
@@ -0,0 +1,35 @@
+import { Properties } from 'devextreme/ui/slider.d';
+import url from '../../helpers/getPageUrl';
+import { testAccessibility, Configuration } from '../../helpers/accessibility/test';
+import { Options } from '../../helpers/generateOptionMatrix';
+
+fixture.disablePageReloads`Accessibility`
+  .page(url(__dirname, '../container.html'));
+
+const options: Options<Properties> = {
+  value: [0, 90],
+  disabled: [true, false],
+  readOnly: [true, false],
+  hint: [undefined, 'hint'],
+  name: ['', 'name'],
+  height: [undefined, 250],
+  width: [450, 'auto', '100%'],
+  min: [undefined, 10],
+  max: [undefined, 90],
+  focusStateEnabled: [true],
+  validationStatus: [undefined, 'invalid'],
+  elementAttr: [{ 'aria-label': 'Slider' }],
+};
+
+const a11yCheckConfig = {
+  // NOTE: color-contrast issues
+  rules: { 'color-contrast': { enabled: false } },
+};
+
+const configuration: Configuration = {
+  component: 'dxSlider',
+  a11yCheckConfig,
+  options,
+};
+
+testAccessibility(configuration);
