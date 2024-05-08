530c5c19959553b0ccf0c22c7bea5cce98d9a57f
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-05-08 17:20:39 +0800

    Test configuration (dxHtmlEditor)

--- e2e/testcafe-devextreme/tests/accessibility/htmlEditor.ts
+++ e2e/testcafe-devextreme/tests/accessibility/htmlEditor.ts
@@ -1,6 +1,8 @@
+import { ClientFunction } from 'testcafe';
 import { Properties } from 'devextreme/ui/html_editor.d';
+import HtmlEditor from 'devextreme-testcafe-models/htmlEditor';
 import url from '../../helpers/getPageUrl';
-import { testAccessibility, Configuration } from '../../helpers/accessibility/test';
+import { defaultSelector, testAccessibility, Configuration } from '../../helpers/accessibility/test';
 import { Options } from '../../helpers/generateOptionMatrix';
 
 fixture.disablePageReloads`Accessibility`
@@ -33,6 +35,16 @@ const options: Options<Properties> = {
   ],
 };
 
+const created = async (t: TestController): Promise<void> => {
+  const htmlEditor = new HtmlEditor(defaultSelector);
+
+  await ClientFunction(() => {
+    htmlEditor.getInstance();
+  }, {
+    dependencies: { htmlEditor },
+  })();
+};
+
 const a11yCheckConfig = {
   // NOTE: color-contrast issues
   rules: { 'color-contrast': { enabled: false } },
@@ -42,6 +54,7 @@ const configuration: Configuration = {
   component: 'dxHtmlEditor',
   a11yCheckConfig,
   options,
+  created,
 };
 
 testAccessibility(configuration);
