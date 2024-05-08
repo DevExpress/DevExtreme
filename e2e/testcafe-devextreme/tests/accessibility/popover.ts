ff6322e4477d423f9d95be45b253a04bdc640adc
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-05-08 18:13:58 +0800

    Create popover.ts

---
+++ e2e/testcafe-devextreme/tests/accessibility/popover.ts
@@ -0,0 +1,70 @@
+import { Properties } from 'devextreme/ui/popover.d';
+import { ToolbarItem } from 'devextreme/ui/popup.d';
+import url from '../../helpers/getPageUrl';
+import { testAccessibility, Configuration } from '../../helpers/accessibility/test';
+import { Options } from '../../helpers/generateOptionMatrix';
+
+fixture.disablePageReloads`Accessibility`
+  .page(url(__dirname, '../container.html'));
+
+const toolbarItems: ToolbarItem[] = [
+  {
+    locateInMenu: 'always',
+    widget: 'dxButton',
+    toolbar: 'top',
+    options: { text: 'More info' },
+  }, {
+    widget: 'dxButton',
+    toolbar: 'bottom',
+    location: 'before',
+    options: { icon: 'email', text: 'Send' },
+  }, {
+    widget: 'dxButton',
+    toolbar: 'bottom',
+    location: 'after',
+    options: { text: 'Close' },
+  },
+];
+
+const options: Options<Properties> = {
+  width: [300],
+  visible: [true, false],
+  shading: [true, false],
+  disabled: [true, false],
+  showTitle: [true, false],
+  title: [undefined, 'title'],
+  hint: [undefined, 'hint'],
+  enableBodyScroll: [true, false],
+  deferRendering: [true, false],
+  toolbarItems: [undefined, toolbarItems],
+  animation: [
+    undefined,
+    {
+      show: {
+        type: 'fade',
+        duration: 600,
+        from: 0,
+        to: 1,
+      },
+      hide: {
+        type: 'fade',
+        duration: 600,
+        from: 1,
+        to: 0,
+      },
+    },
+  ],
+};
+
+const a11yCheckConfig = {
+  // NOTE: color-contrast issues
+  rules: { 'color-contrast': { enabled: false } },
+};
+
+const configuration: Configuration = {
+  component: 'dxPopover',
+  a11yCheckConfig,
+  options,
+};
+
+testAccessibility(configuration);
