340f1d7c943db01234c1e5f42d6fe68c128fe2d3
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-05-08 19:15:30 +0800

    Create floatingActionButton.ts

---
+++ e2e/testcafe-devextreme/tests/accessibility/floatingActionButton.ts
@@ -0,0 +1,28 @@
+import { Properties } from 'devextreme/ui/speed_dial_action.d';
+import url from '../../helpers/getPageUrl';
+import { testAccessibility, Configuration } from '../../helpers/accessibility/test';
+import { Options } from '../../helpers/generateOptionMatrix';
+
+fixture.disablePageReloads`Accessibility`
+  .page(url(__dirname, '../container.html'));
+
+const options: Options<Properties> = {
+  visible: [true, false],
+  label: ['', 'label'],
+  hint: [undefined, 'hint'],
+  icon: [undefined, 'save'],
+  focusStateEnabled: [true],
+};
+
+const a11yCheckConfig = {
+  // NOTE: color-contrast issues
+  rules: { 'color-contrast': { enabled: false } },
+};
+
+const configuration: Configuration = {
+  component: 'dxSpeedDialAction',
+  a11yCheckConfig,
+  options,
+};
+
+testAccessibility(configuration);
