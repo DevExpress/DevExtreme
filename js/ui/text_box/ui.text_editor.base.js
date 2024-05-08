58be043dfac987ed08e80e63b5fcba22b9658572
Nikki Gonzales <38495263+nikkithelegendarypokemonster@users.noreply.github.com>
2024-05-08 22:36:44 +0800

    TextBox: Safeguard against undefined inputAttr option (#27334)

--- js/ui/text_box/ui.text_editor.base.js
+++ js/ui/text_box/ui.text_editor.base.js
@@ -486,8 +486,8 @@ const TextEditorBase = Editor.inherit({
     },
 
     _setFieldAria(force) {
-        const { 'aria-label': ariaLabel } = this.option('inputAttr');
-
+        const inputAttr = this.option('inputAttr');
+        const ariaLabel = inputAttr?.['aria-label'];
         const labelId = this._label.getId();
 
         const value = ariaLabel ? undefined : labelId;
