1bedfca00254ad30b391996f060d831e1e96a14b
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-05-09 15:17:11 +0800

    Create htmlEditor.ts

---
+++ e2e/testcafe-devextreme/tests/accessibility/htmlEditor.ts
@@ -0,0 +1,38 @@
+import { Properties } from 'devextreme/ui/html_editor.d';
+import { Selector } from 'testcafe';
+import url from '../../helpers/getPageUrl';
+import { defaultSelector, testAccessibility, Configuration } from '../../helpers/accessibility/test';
+import { Options } from '../../helpers/generateOptionMatrix';
+
+fixture.disablePageReloads`Accessibility`
+  .page(url(__dirname, '../containerQuill.html'));
+
+const markup = '<p>He<em>llo</em></p>';
+
+const options: Options<Properties> = {
+  value: [undefined, markup],
+  toolbar: [
+    undefined,
+    {
+      items: ['bold', 'color'],
+    },
+  ],
+};
+
+const created = async (t: TestController): Promise<void> => {
+  await t.click(Selector(defaultSelector));
+};
+
+const a11yCheckConfig = {
+  // NOTE: color-contrast issues
+  rules: { 'color-contrast': { enabled: false } },
+};
+
+const configuration: Configuration = {
+  component: 'dxHtmlEditor',
+  a11yCheckConfig,
+  options,
+  created,
+};
+
+testAccessibility(configuration);
