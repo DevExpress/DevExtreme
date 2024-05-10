b99c3a2ff273edc8162f6ac6a44e2487ab1f00cb
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-05-11 01:48:26 +0800

    Create radioGroup.ts

---
+++ e2e/testcafe-devextreme/tests/accessibility/radioGroup.ts
@@ -0,0 +1,31 @@
+import { Properties } from 'devextreme/ui/radio_group.d';
+import url from '../../helpers/getPageUrl';
+import { testAccessibility, Configuration } from '../../helpers/accessibility/test';
+import { Options } from '../../helpers/generateOptionMatrix';
+
+fixture.disablePageReloads`Accessibility`
+  .page(url(__dirname, '../container.html'));
+
+const items = ['Item_1', 'Item_2', 'Item_3'];
+
+const options: Options<Properties> = {
+  items: [items],
+  disabled: [true, false],
+  readOnly: [true, false],
+  hint: [undefined, 'hint'],
+  name: [undefined, 'name'],
+  focusStateEnabled: [true],
+};
+
+const a11yCheckConfig = {
+  // NOTE: color-contrast issues
+  rules: { 'color-contrast': { enabled: false } },
+};
+
+const configuration: Configuration = {
+  component: 'dxAccordion',
+  a11yCheckConfig,
+  options,
+};
+
+testAccessibility(configuration);
