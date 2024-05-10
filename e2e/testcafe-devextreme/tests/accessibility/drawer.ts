9c25876b0875e11123cf17f2447650de80cf52d9
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-05-10 20:50:34 +0800

    Create drawer.ts

---
+++ e2e/testcafe-devextreme/tests/accessibility/drawer.ts
@@ -0,0 +1,28 @@
+import { Properties } from 'devextreme/ui/drawer.d';
+import url from '../../helpers/getPageUrl';
+import { testAccessibility, Configuration } from '../../helpers/accessibility/test';
+import { Options } from '../../helpers/generateOptionMatrix';
+
+fixture.disablePageReloads`Accessibility`
+  .page(url(__dirname, '../container.html'));
+
+const options: Options<Properties> = {
+  disabled: [true, false],
+  hint: [undefined, 'hint'],
+  revealMode: ['slide', 'expand'],
+  opened: [true, false],
+  position: ['top', 'bottom', 'left', 'right'],
+};
+
+const a11yCheckConfig = {
+  // NOTE: color-contrast issues
+  rules: { 'color-contrast': { enabled: false } },
+};
+
+const configuration: Configuration = {
+  component: 'dxDrawer',
+  a11yCheckConfig,
+  options,
+};
+
+testAccessibility(configuration);
