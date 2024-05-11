3059adc8c3ceac1d3acd505af6b78f006fa9bb71
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-05-11 20:44:51 +0800

    Refactor test (dxActionSheet)

--- e2e/testcafe-devextreme/tests/accessibility/actionSheet.ts
+++ e2e/testcafe-devextreme/tests/accessibility/actionSheet.ts
@@ -2,6 +2,7 @@ import { Properties } from 'devextreme/ui/action_sheet.d';
 import url from '../../helpers/getPageUrl';
 import { testAccessibility, Configuration } from '../../helpers/accessibility/test';
 import { Options } from '../../helpers/generateOptionMatrix';
+import { isMaterial, isMaterialBased } from '../../helpers/themeUtils';
 
 fixture.disablePageReloads`Accessibility`
   .page(url(__dirname, '../container.html'));
@@ -22,10 +23,11 @@ const options: Options<Properties> = {
   showCancelButton: [true, false],
 };
 
-const a11yCheckConfig = {
-  // NOTE: color-contrast issues
-  rules: { 'color-contrast': { enabled: false } },
-};
+const a11yCheckConfig = isMaterialBased() ? {
+  // NOTE: color-contrast issues in Material
+  runOnly: isMaterial() ? '' : 'color-contrast',
+  rules: { 'color-contrast': { enabled: !isMaterial() } },
+} : {};
 
 const configuration: Configuration = {
   component: 'dxActionSheet',
