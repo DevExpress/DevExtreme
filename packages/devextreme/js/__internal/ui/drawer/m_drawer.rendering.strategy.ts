90908b267c198a8c6a7541a5058efd210d8b2ea6
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-05-14 00:02:01 +0400

    Drawer: ignore errors after move to TS

--- packages/devextreme/js/__internal/ui/drawer/m_drawer.rendering.strategy.ts
+++ packages/devextreme/js/__internal/ui/drawer/m_drawer.rendering.strategy.ts
@@ -1,114 +1,131 @@
-import { setWidth, setHeight } from '../../core/utils/size';
-import $ from '../../core/renderer';
-import { animation } from './ui.drawer.animation';
-import { Deferred, when } from '../../core/utils/deferred';
+import $ from '@js/core/renderer';
+import { Deferred, when } from '@js/core/utils/deferred';
+import { setHeight, setWidth } from '@js/core/utils/size';
+import type Drawer from '@js/ui/drawer';
+
+import { animation } from './m_drawer.animation';
 
 class DrawerStrategy {
+  _drawer: Drawer;
 
-    constructor(drawer) {
-        this._drawer = drawer;
+  constructor(drawer) {
+    this._drawer = drawer;
+  }
+
+  getDrawerInstance(): Drawer {
+    return this._drawer;
+  }
+
+  renderPanelContent(whenPanelContentRendered) {
+    const drawer = this.getDrawerInstance();
+    const template = drawer._getTemplate(drawer.option('template'));
+    if (template) {
+      template.render({
+        container: drawer.content(),
+        // @ts-expect-error
+        onRendered: () => {
+          whenPanelContentRendered.resolve();
+        },
+      });
+    }
+  }
+
+  renderPosition(changePositionUsingFxAnimation, animationDuration) {
+    const whenPositionAnimationCompleted = Deferred();
+    const whenShaderAnimationCompleted = Deferred();
+
+    const drawer = this.getDrawerInstance();
+
+    if (changePositionUsingFxAnimation) {
+      when.apply($, [whenPositionAnimationCompleted, whenShaderAnimationCompleted]).done(() => {
+        // @ts-expect-error
+        drawer._animationCompleteHandler();
+      });
+    }
+    // @ts-expect-error
+    this._internalRenderPosition(changePositionUsingFxAnimation, whenPositionAnimationCompleted);
+
+    if (!changePositionUsingFxAnimation) {
+      // @ts-expect-error
+      drawer.resizeViewContent();
     }
 
-    getDrawerInstance() {
-        return this._drawer;
+    this.renderShaderVisibility(changePositionUsingFxAnimation, animationDuration, whenShaderAnimationCompleted);
+  }
+
+  _getPanelOffset(isDrawerOpened) {
+    const drawer = this.getDrawerInstance();
+    // @ts-expect-error
+    const size = drawer.isHorizontalDirection() ? drawer.getRealPanelWidth() : drawer.getRealPanelHeight();
+
+    if (isDrawerOpened) {
+      // @ts-expect-error
+      return -(size - drawer.getMaxSize());
     }
+    // @ts-expect-error
+    return -(size - drawer.getMinSize());
+  }
 
-    renderPanelContent(whenPanelContentRendered) {
-        const drawer = this.getDrawerInstance();
-        const template = drawer._getTemplate(drawer.option('template'));
-        if(template) {
-            template.render({
-                container: drawer.content(),
-                onRendered: () => {
-                    whenPanelContentRendered.resolve();
-                }
-            });
-        }
+  _getPanelSize(isDrawerOpened) {
+    return isDrawerOpened
+      // @ts-expect-error
+      ? this.getDrawerInstance().getMaxSize()
+      // @ts-expect-error
+      : this.getDrawerInstance().getMinSize();
+  }
+
+  renderShaderVisibility(changePositionUsingFxAnimation, duration, whenAnimationCompleted) {
+    const drawer = this.getDrawerInstance();
+    const isShaderVisible = drawer.option('opened');
+    const fadeConfig = isShaderVisible ? { from: 0, to: 1 } : { from: 1, to: 0 };
+
+    if (changePositionUsingFxAnimation) {
+      // @ts-expect-error
+      animation.fade($(drawer._$shader), fadeConfig, duration, () => {
+        // @ts-expect-error
+        this._drawer._toggleShaderVisibility(isShaderVisible);
+        whenAnimationCompleted.resolve();
+      });
+    } else {
+      // @ts-expect-error
+      drawer._toggleShaderVisibility(isShaderVisible);
+      // @ts-expect-error
+      drawer._$shader.css('opacity', fadeConfig.to);
     }
+  }
 
-    renderPosition(changePositionUsingFxAnimation, animationDuration) {
-        const whenPositionAnimationCompleted = new Deferred();
-        const whenShaderAnimationCompleted = new Deferred();
+  getPanelContent() {
+    return $(this.getDrawerInstance().content());
+  }
 
-        const drawer = this.getDrawerInstance();
+  setPanelSize(calcFromRealPanelSize) { // TODO: keep for ui.file_manager.adaptivity.js
+    this.refreshPanelElementSize(calcFromRealPanelSize);
+  }
 
-        if(changePositionUsingFxAnimation) {
-            when.apply($, [whenPositionAnimationCompleted, whenShaderAnimationCompleted]).done(() => {
-                drawer._animationCompleteHandler();
-            });
-        }
-
-        this._internalRenderPosition(changePositionUsingFxAnimation, whenPositionAnimationCompleted);
-
-        if(!changePositionUsingFxAnimation) {
-            drawer.resizeViewContent();
-        }
-
-        this.renderShaderVisibility(changePositionUsingFxAnimation, animationDuration, whenShaderAnimationCompleted);
+  refreshPanelElementSize(calcFromRealPanelSize) {
+    const drawer = this.getDrawerInstance();
+    const panelSize = this._getPanelSize(drawer.option('opened'));
+    // @ts-expect-error
+    if (drawer.isHorizontalDirection()) {
+      setWidth(
+        $(drawer.content()),
+        // @ts-expect-error
+        calcFromRealPanelSize ? drawer.getRealPanelWidth() : panelSize,
+      );
+    } else {
+      setHeight(
+        $(drawer.content()),
+        // @ts-expect-error
+        calcFromRealPanelSize ? drawer.getRealPanelHeight() : panelSize,
+      );
     }
+  }
 
-    _getPanelOffset(isDrawerOpened) {
-        const drawer = this.getDrawerInstance();
-        const size = drawer.isHorizontalDirection() ? drawer.getRealPanelWidth() : drawer.getRealPanelHeight();
+  isViewContentFirst(): boolean {
+    return false;
+  }
 
-        if(isDrawerOpened) {
-            return -(size - drawer.getMaxSize());
-        } else {
-            return -(size - drawer.getMinSize());
-        }
-    }
-
-    _getPanelSize(isDrawerOpened) {
-        return isDrawerOpened ? this.getDrawerInstance().getMaxSize() : this.getDrawerInstance().getMinSize();
-    }
-
-    renderShaderVisibility(changePositionUsingFxAnimation, duration, whenAnimationCompleted) {
-        const drawer = this.getDrawerInstance();
-        const isShaderVisible = drawer.option('opened');
-        const fadeConfig = isShaderVisible ? { from: 0, to: 1 } : { from: 1, to: 0 };
-
-        if(changePositionUsingFxAnimation) {
-            animation.fade($(drawer._$shader), fadeConfig, duration, () => {
-                this._drawer._toggleShaderVisibility(isShaderVisible);
-                whenAnimationCompleted.resolve();
-            });
-        } else {
-            drawer._toggleShaderVisibility(isShaderVisible);
-            drawer._$shader.css('opacity', fadeConfig.to);
-        }
-    }
-
-    getPanelContent() {
-        return $(this.getDrawerInstance().content());
-    }
-
-    setPanelSize(calcFromRealPanelSize) { // TODO: keep for ui.file_manager.adaptivity.js
-        this.refreshPanelElementSize(calcFromRealPanelSize);
-    }
-
-    refreshPanelElementSize(calcFromRealPanelSize) {
-        const drawer = this.getDrawerInstance();
-        const panelSize = this._getPanelSize(drawer.option('opened'));
-
-        if(drawer.isHorizontalDirection()) {
-            setWidth(
-                $(drawer.content()),
-                calcFromRealPanelSize ? drawer.getRealPanelWidth() : panelSize
-            );
-        } else {
-            setHeight(
-                $(drawer.content()),
-                calcFromRealPanelSize ? drawer.getRealPanelHeight() : panelSize
-            );
-        }
-    }
-
-    isViewContentFirst() {
-        return false;
-    }
-
-    onPanelContentRendered() {
-    }
+  onPanelContentRendered(): void { }
 }
 
 export default DrawerStrategy;
--- packages/devextreme/js/ui/drawer.js
+++ packages/devextreme/js/ui/drawer.js
@@ -1,2 +1,33 @@
-import Drawer from './drawer/ui.drawer';
+import Drawer from '../__internal/ui/drawer/m_drawer';
 export default Drawer;
+
+// STYLE drawer
+
+/**
+ * @name dxDrawerOptions.contentTemplate
+ * @type_function_param1 contentElement:DxElement
+ * @type template|function
+ * @hidden
+ * @default "content"
+ */
+
+/**
+ * @name dxDrawerOptions.onContentReady
+ * @hidden true
+ * @action
+ */
+
+/**
+ * @name dxDrawerOptions.focusStateEnabled
+ * @hidden
+ */
+
+/**
+ * @name dxDrawerOptions.accessKey
+ * @hidden
+ */
+
+/**
+ * @name dxDrawerOptions.tabIndex
+ * @hidden
+ */
