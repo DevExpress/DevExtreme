5313e4cf04c5576213d93e6d2308aacb2625e08d
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-05-13 18:42:21 +0400

    ColorView, ColorBox: ignore errors after move to TS

--- packages/devextreme/js/ui/color_box.js
+++ packages/devextreme/js/ui/color_box.js
@@ -1,2 +1,25 @@
-import ColorBox from './color_box/color_box';
+import ColorBox from '../__internal/ui/color_box/m_color_box';
 export default ColorBox;
+
+// STYLE colorBox
+
+/**
+  * @name dxColorBoxOptions.onContentReady
+  * @hidden true
+  * @action
+  */
+
+/**
+  * @name dxColorBoxOptions.maxLength
+  * @hidden
+  */
+
+/**
+  * @name dxColorBoxOptions.valueChangeEvent
+  * @hidden
+  */
+
+/**
+  * @name dxColorBoxOptions.spellcheck
+  * @hidden
+  */
