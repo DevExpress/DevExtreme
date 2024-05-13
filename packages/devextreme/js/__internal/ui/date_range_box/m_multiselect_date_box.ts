597a651c68f570e79d70976f99cd4564a607d410
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-05-13 18:12:42 +0400

    DateBox types: public DateBox class shouldn't have generic type (revert after migration to TS)

--- packages/devextreme/js/__internal/ui/date_range_box/m_multiselect_date_box.ts
+++ packages/devextreme/js/__internal/ui/date_range_box/m_multiselect_date_box.ts
@@ -1,9 +1,10 @@
+// eslint-disable-next-line max-classes-per-file
 import $ from '@js/core/renderer';
 import { getWidth } from '@js/core/utils/size';
 import eventsEngine from '@js/events/core/events_engine';
 import { addNamespace } from '@js/events/utils';
 import type { Properties } from '@js/ui/date_box';
-import DateBox from '@js/ui/date_box';
+import { DateBoxBase } from '@js/ui/date_box';
 import type Popup from '@js/ui/popup';
 
 import { getDeserializedDate, monthDifference } from './m_date_range.utils';
@@ -17,7 +18,11 @@ export interface MultiselectDateBoxProperties extends Properties {
   _showValidationMessage?: boolean;
 }
 
-class MultiselectDateBox extends DateBox<MultiselectDateBoxProperties> {
+declare class DateBox<TProperties = MultiselectDateBoxProperties> extends DateBoxBase<TProperties> {
+  reset(value?: Date | number | string | null): void;
+}
+
+class MultiselectDateBox extends DateBox {
   // Temporary solution. Move to component level
   public NAME!: string;
 
