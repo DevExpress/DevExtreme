f59591210fff777ccc59a61731b4dbc07c63deb3
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-05-13 13:29:36 +0800

    Create multiView.ts

---
+++ e2e/testcafe-devextreme/tests/accessibility/multiView.ts
@@ -0,0 +1,32 @@
+import { Properties } from 'devextreme/ui/multi_view.d';
+import url from '../../helpers/getPageUrl';
+import { testAccessibility, Configuration } from '../../helpers/accessibility/test';
+import { Options } from '../../helpers/generateOptionMatrix';
+
+fixture.disablePageReloads`Accessibility`
+  .page(url(__dirname, '../container.html'));
+
+const items = ['Item_1', 'Item_2', 'Item_3'];
+
+const options: Options<Properties> = {
+  items: [items],
+  height: [300],
+  loop: [true, false],
+  hint: [undefined, 'hint'],
+  noDataText: [undefined, 'no data text'],
+  swipeEnabled: [true, false],
+  focusStateEnabled: [true],
+};
+
+const a11yCheckConfig = {
+  // NOTE: color-contrast issues
+  rules: { 'color-contrast': { enabled: false } },
+};
+
+const configuration: Configuration = {
+  component: 'dxMultiView',
+  a11yCheckConfig,
+  options,
+};
+
+testAccessibility(configuration);
