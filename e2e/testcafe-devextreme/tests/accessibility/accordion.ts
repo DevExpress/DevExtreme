ec67e75ddf3fc588829f51a7b37c31333b4db9ff
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-05-13 13:29:25 +0800

    Create accordion.ts

---
+++ e2e/testcafe-devextreme/tests/accessibility/accordion.ts
@@ -0,0 +1,32 @@
+import { Properties } from 'devextreme/ui/accordion.d';
+import url from '../../helpers/getPageUrl';
+import { testAccessibility, Configuration } from '../../helpers/accessibility/test';
+import { Options } from '../../helpers/generateOptionMatrix';
+
+fixture.disablePageReloads`Accessibility`
+  .page(url(__dirname, '../container.html'));
+
+const items = ['Item_1', 'Item_2', 'Item_3'];
+
+const options: Options<Properties> = {
+  dataSource: [items],
+  collapsible: [true, false],
+  disabled: [true, false],
+  deferRendering: [true, false],
+  hint: [undefined, 'hint'],
+  multiple: [true, false],
+  focusStateEnabled: [true],
+};
+
+const a11yCheckConfig = {
+  // NOTE: color-contrast issues
+  rules: { 'color-contrast': { enabled: false } },
+};
+
+const configuration: Configuration = {
+  component: 'dxAccordion',
+  a11yCheckConfig,
+  options,
+};
+
+testAccessibility(configuration);
