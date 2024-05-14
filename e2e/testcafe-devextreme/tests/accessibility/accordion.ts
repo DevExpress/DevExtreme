f41893efa6b8cda65b7f2e704b28890fbc6bbc60
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-05-14 14:24:28 +0800

    Update accordion.ts

--- e2e/testcafe-devextreme/tests/accessibility/accordion.ts
+++ e2e/testcafe-devextreme/tests/accessibility/accordion.ts
@@ -1,4 +1,5 @@
 import { Properties } from 'devextreme/ui/accordion.d';
+import { Selector as $ } from 'testcafe';
 import url from '../../helpers/getPageUrl';
 import { testAccessibility, Configuration } from '../../helpers/accessibility/test';
 import { Options } from '../../helpers/generateOptionMatrix';
@@ -10,12 +11,27 @@ const items = ['Item_1', 'Item_2', 'Item_3'];
 
 const options: Options<Properties> = {
   dataSource: [items],
-  collapsible: [true, false],
   disabled: [true, false],
   deferRendering: [true, false],
   hint: [undefined, 'hint'],
   multiple: [true, false],
   focusStateEnabled: [true],
+};
+
+const created = async (t: TestController, optionConfiguration): Promise<void> => {
+  const { disabled, multiple } = optionConfiguration;
+
+  if (disabled || !multiple) {
+    return;
+  }
+
+  const itemTitle = $('.dx-accordion-item-title');
+
+  await t
+    .click(itemTitle.nth(0));
+
+  await t
+    .click(itemTitle.nth(1));
 };
 
 const a11yCheckConfig = {
@@ -27,6 +43,7 @@ const configuration: Configuration = {
   component: 'dxAccordion',
   a11yCheckConfig,
   options,
+  created,
 };
 
-// testAccessibility(configuration);
+testAccessibility(configuration);
