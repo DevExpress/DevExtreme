c8ba13be6203d64b23ebe1a32c45ca0127d60ba2
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-05-14 15:01:39 +0800

    Use created hook

--- e2e/testcafe-devextreme/tests/accessibility/accordion.ts
+++ e2e/testcafe-devextreme/tests/accessibility/accordion.ts
@@ -1,4 +1,5 @@
 import { Properties } from 'devextreme/ui/accordion.d';
+import { Selector as $ } from 'testcafe';
 import url from '../../helpers/getPageUrl';
 import { testAccessibility, Configuration } from '../../helpers/accessibility/test';
 import { Options } from '../../helpers/generateOptionMatrix';
@@ -17,6 +18,22 @@ const options: Options<Properties> = {
   focusStateEnabled: [true],
 };
 
+const created = async (t: TestController, optionConfiguration): Promise<void> => {
+  const { disabled, multiple } = optionConfiguration;
+
+  if (disabled || !multiple) {
+    return;
+  }
+
+  const itemTitle = $('.dx-accordion-item-title');
+
+  await t
+    .click(itemTitle.nth(0));
+
+  await t
+    .click(itemTitle.nth(1));
+};
+
 const a11yCheckConfig = {
   // NOTE: color-contrast issues
   rules: { 'color-contrast': { enabled: false } },
@@ -26,6 +43,7 @@ const configuration: Configuration = {
   component: 'dxAccordion',
   a11yCheckConfig,
   options,
+  created,
 };
 
 testAccessibility(configuration);
