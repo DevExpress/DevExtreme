8245a2defe5c36682f9ca4c03a90af8094a7e70a
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-05-14 20:25:47 +0800

    Create unit test

--- testing/tests/DevExpress.ui.widgets.editors/selectBox.tests.js
+++ testing/tests/DevExpress.ui.widgets.editors/selectBox.tests.js
@@ -15,6 +15,7 @@ import ariaAccessibilityTestHelper from '../../helpers/ariaAccessibilityTestHelp
 import { normalizeKeyName } from 'events/utils/index';
 
 import 'generic_light.css!';
+import 'ui/validator';
 
 const EMPTY_MESSAGE_CLASS = 'dx-empty-message';
 
@@ -3289,6 +3290,33 @@ QUnit.module('search', moduleSetup, () => {
         listItem.trigger('dxclick');
 
         assert.equal($selectBox.find(toSelector(TEXTEDITOR_INPUT_CLASS)).val(), 'Name 2', 'selectBox displays right value');
+    });
+
+    QUnit.testInActiveWindow('widget with fieldTemplate should not lose aria-required attribute after search and selection (T1230696)', function(assert) {
+        const dataSource = new DataSource(['one', 'two', 'three']);
+        const $selectBox = $('#selectBox').dxSelectBox({
+            dataSource: dataSource,
+            fieldTemplate: () => {
+                return $('<div>').dxTextBox({});
+            },
+            searchEnabled: true,
+            itemTemplate: () => {
+                return '<div><span></span></div>';
+            }
+        }).dxValidator({
+            validationRules: [ { type: 'required' } ]
+        });
+        assert.ok($selectBox.find(toSelector(TEXTEDITOR_INPUT_CLASS)).attr('aria-required'), true, 'initial render should have aria-required attribute');
+
+        const selectBox = $selectBox.dxSelectBox('instance');
+        const keyboard = keyboardMock($selectBox.find(toSelector(TEXTEDITOR_INPUT_CLASS)));
+
+        keyboard.type('a');
+
+        const listItem = $(selectBox.content()).find(toSelector(LIST_ITEM_CLASS)).eq(1);
+        listItem.trigger('dxclick');
+
+        assert.ok($selectBox.find(toSelector(TEXTEDITOR_INPUT_CLASS)).attr('aria-required'), true, 'aria-required should exist after search and selection');
     });
 
     [0, 1].forEach((value) => {
