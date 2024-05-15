48abfc085c8df902f72d709f73da2c4c1c610fed
Joshua Victoria <jdvictoria.tech@gmail.com>
2024-05-16 01:59:20 +0800

    Refactor tests

--- packages/devextreme/testing/tests/DevExpress.ui.widgets.editors/dropDownList.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.widgets.editors/dropDownList.tests.js
@@ -15,7 +15,6 @@ import ajaxMock from '../../helpers/ajaxMock.js';
 import 'ui/drop_down_editor/ui.drop_down_list';
 
 import 'generic_light.css!';
-import 'ui/validator';
 
 QUnit.testStart(() => {
     const markup =
@@ -795,32 +794,6 @@ QUnit.module('items & dataSource', moduleConfig, () => {
             this.clock.tick(TIME_TO_WAIT);
 
             assert.strictEqual(this.getListItemsCount(), 0, 'was search');
-        });
-
-        QUnit.testInActiveWindow('widget with fieldTemplate should not lose aria-required attribute after search and selection (T1230696)', function(assert) {
-            const $dropDownList = $('#dropDownList').dxDropDownList({
-                dataSource: ['one', 'two', 'three'],
-                fieldTemplate: () => {
-                    return $('<div>').dxTextBox({});
-                },
-                searchEnabled: true,
-                itemTemplate: () => {
-                    return '<div><span></span></div>';
-                }
-            }).dxValidator({
-                validationRules: [ { type: 'required' } ]
-            });
-            assert.strictEqual($dropDownList.find(`.${TEXTEDITOR_INPUT_CLASS}`).attr('aria-required'), 'true', 'initial render should have aria-required attribute set to true');
-
-            const dropDownList = $dropDownList.dxDropDownList('instance');
-            const keyboard = keyboardMock($dropDownList.find(`.${TEXTEDITOR_INPUT_CLASS}`));
-
-            keyboard.type('a');
-
-            const listItem = $(dropDownList.content()).find(`.${TEXTEDITOR_INPUT_CLASS}`).eq(1);
-            listItem.trigger('dxclick');
-
-            assert.strictEqual($dropDownList.find(`.${TEXTEDITOR_INPUT_CLASS}`).attr('aria-required'), 'true', 'aria-required should stay true after search and selection');
         });
     });
 
