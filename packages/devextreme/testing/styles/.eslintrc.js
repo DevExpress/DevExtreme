366bf2b27f9e66ed403f302dbeada1bc531a856f
Vasily Strelyaev <Vasily.Strelyaev@devexpress.com>
2024-05-17 19:33:38 +0300

    let files inside testing/styles use their own tsconfig when linted

---
+++ packages/devextreme/testing/styles/.eslintrc.js
@@ -0,0 +1,12 @@
+/* eslint-env node */
+/* eslint-disable spellcheck/spell-checker */
+module.exports = {
+    overrides: [
+        {
+            files: ['*'],
+            parserOptions: {
+                project: true,
+            },
+        },
+    ],
+};
