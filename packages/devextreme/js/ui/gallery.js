588566fe1a604ca2f7e396baa9df129ce7920236
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-05-28 17:06:43 +0400

    RadioButton, RadioGroup, Gallery, MultiView, TileView: ignore errors after move to TS

---
+++ packages/devextreme/js/ui/gallery.js
@@ -0,0 +1,38 @@
+import Gallery from '../__internal/ui/m_gallery';
+
+export default Gallery;
+
+// STYLE gallery
+
+/**
+ * @name dxGalleryOptions.activeStateEnabled
+ * @type boolean
+ * @default false
+ * @hidden
+ */
+
+/**
+ * @name dxGalleryOptions.selectedItems
+ * @hidden
+ */
+
+/**
+ * @name dxGalleryOptions.selectedItemKeys
+ * @hidden
+ */
+
+/**
+ * @name dxGalleryOptions.keyExpr
+ * @hidden
+ */
+
+/**
+ * @name dxGalleryItem.visible
+ * @hidden
+ */
+
+/**
+ * @name dxGalleryItem
+ * @type object
+ * @inherits CollectionWidgetItem
+ */
---
+++ packages/devextreme/js/ui/radio_group/radio_group.js
@@ -0,0 +1,5 @@
+import RadioGroup from '../../__internal/ui/radio_group/m_radio_group';
+
+export default RadioGroup;
+
+// STYLE radioGroup
