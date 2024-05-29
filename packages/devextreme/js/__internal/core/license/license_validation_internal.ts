f6cd54de55b1da68e6fab1854e0c50db8d552938
Vasily Strelyaev <Vasily.Strelyaev@devexpress.com>
2024-05-29 14:03:02 +0300

    internal module mock

--- packages/devextreme/js/__internal/core/license/license_validation_internal.ts
+++ packages/devextreme/js/__internal/core/license/license_validation_internal.ts
@@ -7,10 +7,15 @@ export function parseLicenseKey(encodedKey: string | undefined): Token {}
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function validateLicense(licenseKey: string, version?: string): void {}
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function reportDependentVersion(dependentName: string, version: string): void {}
+
 // @ts-expect-error - only for internal usage
 export function peekValidationPerformed(): boolean {}
 
 export function setLicenseCheckSkipCondition(): void {}
+
+export function clearDependentVersions(): void {}
 
 export default {
   validateLicense,
