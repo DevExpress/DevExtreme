dc45bd97587dfb87fe221acbab1eb68deb1f5081
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-05-30 23:48:43 +0800

    Fix test (dxNumberBox)

--- e2e/testcafe-devextreme/tests/accessibility/numberBox.ts
+++ e2e/testcafe-devextreme/tests/accessibility/numberBox.ts
@@ -13,6 +13,11 @@ const options: Options<Properties> = {
   readOnly: [true, false],
   showClearButton: [true, false],
   showSpinButtons: [true, false],
+  mode: ['number', 'tel', 'text'],
+  min: [undefined, 10],
+  max: [undefined, 90],
+  label: ['', 'label'],
+  name: ['', 'name'],
   // NOTE: Doesn't matter if there are contrast issues
   // stylingMode: ['outlined', 'filled', 'underlined'],
   inputAttr: [{ 'aria-label': 'aria-label' }],
