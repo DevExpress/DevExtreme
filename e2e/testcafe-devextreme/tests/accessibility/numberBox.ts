a5e159d226d828026162bef1a10d8dbccdbd99e7
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-05-30 22:06:22 +0800

    Test configurations (dxNumberBox)

--- e2e/testcafe-devextreme/tests/accessibility/numberBox.ts
+++ e2e/testcafe-devextreme/tests/accessibility/numberBox.ts
@@ -11,12 +11,6 @@ const options: Options<Properties> = {
   placeholder: [undefined, 'placeholder'],
   disabled: [true, false],
   readOnly: [true, false],
-  showClearButton: [true, false],
-  showSpinButtons: [true, false],
-  min: [undefined, 10],
-  max: [undefined, 90],
-  label: ['', 'label'],
-  name: ['', 'name'],
   // NOTE: Doesn't matter if there are contrast issues
   // stylingMode: ['outlined', 'filled', 'underlined'],
   inputAttr: [{ 'aria-label': 'aria-label' }],
