26e484c38afeccf5aab40f498d2ff5c9c80b8bd1
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-06-02 22:41:18 +0400

    Unskip QUnit tests after updating Chrome to version 121 (#27501)

--- testing/tests/DevExpress.ui.widgets/listParts/commonTests.js
+++ testing/tests/DevExpress.ui.widgets/listParts/commonTests.js
@@ -3784,7 +3784,7 @@ QUnit.module('keyboard navigation', {
         assert.equal(handler.callCount, 1);
     });
 
-    QUnit.skip('allow delete item using keyboard after set allowItemDeleting option from false to true', function(assert) {
+    QUnit.testInActiveWindow('allow delete item using keyboard after set allowItemDeleting option from false to true', function(assert) {
         const $element = $('#list');
         const $list = $element.dxList({
             items: [1, 2, 3, 4],
@@ -3832,7 +3832,7 @@ QUnit.module('Search', () => {
         assert.strictEqual(instance.option('searchValue'), '2', 'search value');
     });
 
-    QUnit.skip('Focusing widget when there is search editor', function(assert) {
+    QUnit.testInActiveWindow('Focusing widget when there is search editor', function(assert) {
         const $element = $('#list').dxList({
             dataSource: [1, 2, 3],
             searchEnabled: true,
