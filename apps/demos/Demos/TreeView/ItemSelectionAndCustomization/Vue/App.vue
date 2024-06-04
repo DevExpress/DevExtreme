a923161c09a75f36acaba1be87c692243baf0fbd
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-06-04 21:38:46 +0800

    Adjust margin left (Vue)

--- apps/demos/Demos/TreeView/ItemSelectionAndCustomization/Vue/App.vue
+++ apps/demos/Demos/TreeView/ItemSelectionAndCustomization/Vue/App.vue
@@ -140,7 +140,7 @@ function selectionModeValueChanged(e) {
 
 .selected-container {
   padding: 20px;
-  margin-left: 20px;
+  margin-left: 23.25px;
   background-color: rgba(191, 191, 191, 0.15);
   font-size: 115%;
   font-weight: bold;
