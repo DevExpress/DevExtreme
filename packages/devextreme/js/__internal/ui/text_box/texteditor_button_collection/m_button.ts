5e6642b14a510c68742c30448ab8e69364cafeb0
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-06-06 16:57:45 +0400

    TexteditorButtonCollection: ignore errors after move to TS

--- packages/devextreme/js/__internal/ui/text_box/texteditor_button_collection/m_button.ts
+++ packages/devextreme/js/__internal/ui/text_box/texteditor_button_collection/m_button.ts
@@ -1,82 +1,104 @@
-import $ from '../../../core/renderer';
+import type { dxElementWrapper } from '@js/core/renderer';
+import $ from '@js/core/renderer';
 
 export default class TextEditorButton {
-    constructor(name, editor, options) {
-        this.instance = null;
+  $container?: dxElementWrapper;
 
-        this.$container = null;
-        this.$placeMarker = null;
-        this.editor = editor;
-        this.name = name;
-        this.options = options || {};
+  $placeMarker?: dxElementWrapper;
+
+  instance?: any;
+
+  editor?: any;
+
+  name?: any;
+
+  options?: any;
+
+  constructor(name, editor, options) {
+    this.instance = null;
+
+    // @ts-expect-error
+    this.$container = null;
+    // @ts-expect-error
+    this.$placeMarker = null;
+    this.editor = editor;
+    this.name = name;
+    this.options = options || {};
+  }
+
+  _addPlaceMarker($container) {
+    this.$placeMarker = $('<div>').appendTo($container);
+  }
+
+  _addToContainer($element) {
+    const { $placeMarker, $container } = this;
+
+    $placeMarker ? $placeMarker.replaceWith($element) : $element.appendTo($container);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  _attachEvents(instance: any, $element: dxElementWrapper) {
+    // eslint-disable-next-line @typescript-eslint/no-throw-literal
+    throw 'Not implemented';
+  }
+
+  _create(): {
+    instance: any;
+    $element: dxElementWrapper;
+  } {
+    // eslint-disable-next-line @typescript-eslint/no-throw-literal
+    throw 'Not implemented';
+  }
+
+  _isRendered() {
+    return !!this.instance;
+  }
+
+  _isVisible() {
+    const { editor, options } = this;
+
+    return options.visible || !editor.option('readOnly');
+  }
+
+  _isDisabled() {
+    // eslint-disable-next-line @typescript-eslint/no-throw-literal
+    throw 'Not implemented';
+  }
+
+  _shouldRender() {
+    return this._isVisible() && !this._isRendered();
+  }
+
+  dispose() {
+    const { instance, $placeMarker } = this;
+
+    if (instance) {
+      // TODO: instance.dispose()
+      instance.dispose ? instance.dispose() : instance.remove();
+      this.instance = null;
     }
 
-    _addPlaceMarker($container) {
-        this.$placeMarker = $('<div>').appendTo($container);
+    $placeMarker && $placeMarker.remove();
+  }
+
+  render($container = this.$container) {
+    this.$container = $container;
+
+    if (this._isVisible()) {
+      const { instance, $element } = this._create();
+
+      this.instance = instance;
+      this._attachEvents(instance, $element);
+    } else {
+      this._addPlaceMarker($container);
+    }
+  }
+
+  update() {
+    if (this._shouldRender()) {
+      this.render();
     }
 
-    _addToContainer($element) {
-        const { $placeMarker, $container } = this;
-
-        $placeMarker ? $placeMarker.replaceWith($element) : $element.appendTo($container);
-    }
-
-    _attachEvents(/* instance, $element */) {
-        throw 'Not implemented';
-    }
-
-    _create() {
-        throw 'Not implemented';
-    }
-
-    _isRendered() {
-        return !!this.instance;
-    }
-
-    _isVisible() {
-        const { editor, options } = this;
-
-        return options.visible || !editor.option('readOnly');
-    }
-
-    _isDisabled() {
-        throw 'Not implemented';
-    }
-
-    _shouldRender() {
-        return this._isVisible() && !this._isRendered();
-    }
-
-    dispose() {
-        const { instance, $placeMarker } = this;
-
-        if(instance) {
-            // TODO: instance.dispose()
-            instance.dispose ? instance.dispose() : instance.remove();
-            this.instance = null;
-        }
-
-        $placeMarker && $placeMarker.remove();
-    }
-
-    render($container = this.$container) {
-        this.$container = $container;
-
-        if(this._isVisible()) {
-            const { instance, $element } = this._create();
-
-            this.instance = instance;
-            this._attachEvents(instance, $element);
-        } else {
-            this._addPlaceMarker($container);
-        }
-    }
-
-    update() {
-        if(this._shouldRender()) {
-            this.render();
-        }
-
-        return !!this.instance;
-    }
+    return !!this.instance;
+  }
 }
--- packages/devextreme/js/__internal/ui/text_box/texteditor_button_collection/m_custom.ts
+++ packages/devextreme/js/__internal/ui/text_box/texteditor_button_collection/m_custom.ts
@@ -1,72 +1,72 @@
-import $ from '../../../core/renderer';
-import TextEditorButton from './button';
-import Button from '../../button';
-import { extend } from '../../../core/utils/extend';
-import eventsEngine from '../../../events/core/events_engine';
-import { start, end } from '../../../events/hover';
-import { name as clickEventName } from '../../../events/click';
+import $ from '@js/core/renderer';
+import { extend } from '@js/core/utils/extend';
+import { name as clickEventName } from '@js/events/click';
+import eventsEngine from '@js/events/core/events_engine';
+import { end, start } from '@js/events/hover';
+import Button from '@js/ui/button';
+
+import TextEditorButton from './m_button';
 
 const CUSTOM_BUTTON_HOVERED_CLASS = 'dx-custom-button-hovered';
 
 export default class CustomButton extends TextEditorButton {
-    _attachEvents(instance, $element) {
-        const { editor } = this;
+  _attachEvents(instance, $element) {
+    const { editor } = this;
 
-        eventsEngine.on($element, start, () => {
-            editor.$element().addClass(CUSTOM_BUTTON_HOVERED_CLASS);
-        });
-        eventsEngine.on($element, end, () => {
-            editor.$element().removeClass(CUSTOM_BUTTON_HOVERED_CLASS);
-        });
-        eventsEngine.on($element, clickEventName, (e) => {
-            e.stopPropagation();
-        });
+    eventsEngine.on($element, start, () => {
+      editor.$element().addClass(CUSTOM_BUTTON_HOVERED_CLASS);
+    });
+    eventsEngine.on($element, end, () => {
+      editor.$element().removeClass(CUSTOM_BUTTON_HOVERED_CLASS);
+    });
+    eventsEngine.on($element, clickEventName, (e) => {
+      e.stopPropagation();
+    });
+  }
+
+  _create() {
+    const { editor } = this;
+    const $element = $('<div>');
+
+    this._addToContainer($element);
+
+    const instance = editor._createComponent($element, Button, extend({}, this.options, {
+      ignoreParentReadOnly: true,
+      disabled: this._isDisabled(),
+      integrationOptions: this._prepareIntegrationOptions(editor),
+    }));
+
+    return {
+      $element,
+      instance,
+    };
+  }
+
+  _prepareIntegrationOptions(editor) {
+    return extend({}, editor.option('integrationOptions'), { skipTemplates: ['content'] });
+  }
+
+  update() {
+    const isUpdated = super.update();
+
+    if (this.instance) {
+      this.instance.option('disabled', this._isDisabled());
     }
 
-    _create() {
-        const { editor } = this;
-        const $element = $('<div>');
+    return isUpdated;
+  }
 
-        this._addToContainer($element);
+  _isVisible() {
+    const { editor } = this;
 
-        const instance = editor._createComponent($element, Button, extend({}, this.options, {
-            ignoreParentReadOnly: true,
-            disabled: this._isDisabled(),
-            integrationOptions: this._prepareIntegrationOptions(editor)
-        }));
+    return editor.option('visible');
+  }
 
-        return {
-            $element,
-            instance
-        };
+  _isDisabled() {
+    const isDefinedByUser = this.options.disabled !== undefined;
+    if (isDefinedByUser) {
+      return this.instance ? this.instance.option('disabled') : this.options.disabled;
     }
-
-    _prepareIntegrationOptions(editor) {
-        return extend({}, editor.option('integrationOptions'), { skipTemplates: ['content'] });
-    }
-
-    update() {
-        const isUpdated = super.update();
-
-        if(this.instance) {
-            this.instance.option('disabled', this._isDisabled());
-        }
-
-        return isUpdated;
-    }
-
-    _isVisible() {
-        const { editor } = this;
-
-        return editor.option('visible');
-    }
-
-    _isDisabled() {
-        const isDefinedByUser = this.options.disabled !== undefined;
-        if(isDefinedByUser) {
-            return this.instance ? this.instance.option('disabled') : this.options.disabled;
-        } else {
-            return this.editor.option('readOnly');
-        }
-    }
+    return this.editor.option('readOnly');
+  }
 }
