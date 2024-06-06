49d4cc838b5980e2085026e117e92f3794d941b4
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-06-06 16:29:26 +0400

    TexteditorButtonCollection: move files to TS

--- packages/devextreme/js/__internal/ui/text_box/m_text_editor.clear.ts
+++ packages/devextreme/js/__internal/ui/text_box/m_text_editor.clear.ts
@@ -1,6 +1,6 @@
 import $ from '../../core/renderer';
 import eventsEngine from '../../events/core/events_engine';
-import TextEditorButton from './texteditor_button_collection/button';
+import TextEditorButton from '../../__internal/ui/text_box/texteditor_button_collection/m_button';
 import { addNamespace } from '../../events/utils/index';
 import pointer from '../../events/pointer';
 import { name as click } from '../../events/click';
