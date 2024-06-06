be74e1e62e1667fd0b4497a05873316b9335e8e0
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-06-06 19:39:41 +0800

    Remove <span>

--- apps/demos/Demos/TreeView/ItemSelectionAndCustomization/Vue/App.vue
+++ apps/demos/Demos/TreeView/ItemSelectionAndCustomization/Vue/App.vue
@@ -19,7 +19,7 @@
           {{ item.data.fullName + ' (' + item.data.position + ')' }}
         </template>
       </DxTreeView>
-      <span>{{ ' ' }}</span>
+      {{ ' ' }}
       <div class="selected-container">Selected employees
         <DxList
           id="selected-employees"
