765cc8a066e41c565fe61f7e1ad17a233ee2313e
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-06-06 16:57:45 +0400

    TexteditorButtonCollection: ignore errors after move to TS

--- packages/devextreme/js/__internal/ui/text_box/m_text_editor.clear.ts
+++ packages/devextreme/js/__internal/ui/text_box/m_text_editor.clear.ts
@@ -1,9 +1,9 @@
-import $ from '../../core/renderer';
-import eventsEngine from '../../events/core/events_engine';
-import TextEditorButton from '../../__internal/ui/text_box/texteditor_button_collection/m_button';
-import { addNamespace } from '../../events/utils/index';
-import pointer from '../../events/pointer';
-import { name as click } from '../../events/click';
+import $ from '@js/core/renderer';
+import { name as click } from '@js/events/click';
+import eventsEngine from '@js/events/core/events_engine';
+import pointer from '@js/events/pointer';
+import { addNamespace } from '@js/events/utils/index';
+import TextEditorButton from '@ts/ui/text_box/texteditor_button_collection/m_button';
 
 const pointerDown = pointer.down;
 
@@ -14,57 +14,62 @@ const TEXTEDITOR_ICON_CLASS = 'dx-icon';
 const TEXTEDITOR_SHOW_CLEAR_BUTTON_CLASS = 'dx-show-clear-button';
 
 export default class ClearButton extends TextEditorButton {
-    _create() {
-        const $element = $('<span>')
-            .addClass(TEXTEDITOR_CLEAR_BUTTON_CLASS)
-            .append($('<span>').addClass(TEXTEDITOR_ICON_CLASS).addClass(TEXTEDITOR_CLEAR_ICON_CLASS));
+  _create() {
+    const $element = $('<span>')
+      .addClass(TEXTEDITOR_CLEAR_BUTTON_CLASS)
+      .append($('<span>').addClass(TEXTEDITOR_ICON_CLASS).addClass(TEXTEDITOR_CLEAR_ICON_CLASS));
 
-        this._addToContainer($element);
-        this.update(true);
+    this._addToContainer($element);
+    this.update(true);
 
-        return {
-            instance: $element,
-            $element
-        };
-    }
+    return {
+      instance: $element,
+      $element,
+    };
+  }
 
-    _isVisible() {
-        const { editor } = this;
+  _isVisible() {
+    const { editor } = this;
 
-        return editor._isClearButtonVisible();
-    }
+    return editor._isClearButtonVisible();
+  }
 
-    _attachEvents(instance, $button) {
-        const { editor } = this;
-        const editorName = editor.NAME;
+  _attachEvents(instance, $button) {
+    const { editor } = this;
+    const editorName = editor.NAME;
 
-        eventsEngine.on($button, addNamespace(pointerDown, editorName),
-            (e) => {
-                e.preventDefault();
-                if(e.pointerType !== 'mouse') {
-                    editor._clearValueHandler(e);
-                }
-            }
-        );
+    eventsEngine.on(
+      $button,
+      addNamespace(pointerDown, editorName),
+      (e) => {
+        e.preventDefault();
+        if (e.pointerType !== 'mouse') {
+          editor._clearValueHandler(e);
+        }
+      },
+    );
 
-        eventsEngine.on($button, addNamespace(click, editorName),
-            (e) => editor._clearValueHandler(e)
-        );
-    }
+    eventsEngine.on(
+      $button,
+      addNamespace(click, editorName),
+      (e) => editor._clearValueHandler(e),
+    );
+  }
 
-    // TODO: get rid of it
-    _legacyRender($editor, isVisible) {
-        $editor.toggleClass(TEXTEDITOR_SHOW_CLEAR_BUTTON_CLASS, isVisible);
-    }
+  // TODO: get rid of it
+  _legacyRender($editor, isVisible) {
+    $editor.toggleClass(TEXTEDITOR_SHOW_CLEAR_BUTTON_CLASS, isVisible);
+  }
 
-    update(rendered = false) {
-        !rendered && super.update();
+  // @ts-expect-error
+  update(rendered = false) {
+    !rendered && super.update();
 
-        const { editor, instance } = this;
-        const $editor = editor.$element();
-        const isVisible = this._isVisible();
+    const { editor, instance } = this;
+    const $editor = editor.$element();
+    const isVisible = this._isVisible();
 
-        instance && instance.toggleClass(STATE_INVISIBLE_CLASS, !isVisible);
-        this._legacyRender($editor, isVisible);
-    }
+    instance && instance.toggleClass(STATE_INVISIBLE_CLASS, !isVisible);
+    this._legacyRender($editor, isVisible);
+  }
 }
