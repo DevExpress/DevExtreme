4a938856877c3f33258d4953db22e96a29bac6d8
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-06-06 16:12:45 +0400

    Lookup: ignore errors after move to TS

---
+++ packages/devextreme/js/ui/lookup.js
@@ -0,0 +1,112 @@
+import Lookup from '../__internal/ui/m_lookup';
+
+export default Lookup;
+
+// STYLE lookup
+
+/**
+ * @name dxLookupOptions.dropDownButtonTemplate
+ * @type template
+ * @hidden
+ */
+
+/**
+ * @name dxLookupOptions.openOnFieldClick
+ * @hidden
+ */
+
+/**
+ * @name dxLookupOptions.showDropDownButton
+ * @hidden
+ */
+
+/**
+ * @name dxLookupOptions.acceptCustomValue
+ * @hidden
+ */
+
+/**
+ * @name dxLookupOptions.readOnly
+ * @hidden
+ */
+
+/**
+ * @name dxLookupOptions.onFocusIn
+ * @hidden
+ * @action
+ */
+
+/**
+ * @name dxLookupOptions.onFocusOut
+ * @hidden
+ * @action
+ */
+
+/**
+ * @name dxLookupOptions.onKeyDown
+ * @hidden
+ * @action
+ */
+
+/**
+ * @name dxLookupOptions.onKeyUp
+ * @hidden
+ * @action
+ */
+
+/**
+ * @name dxLookupOptions.onChange
+ * @action
+ * @hidden
+ */
+
+/**
+ * @name dxLookupOptions.onInput
+ * @hidden
+ * @action
+ */
+
+/**
+ * @name dxLookupOptions.onCut
+ * @hidden
+ * @action
+ */
+
+/**
+ * @name dxLookupOptions.onCopy
+ * @hidden
+ * @action
+ */
+
+/**
+ * @name dxLookupOptions.onPaste
+ * @hidden
+ * @action
+ */
+
+/**
+ * @name dxLookupOptions.onEnterKey
+ * @hidden
+ * @action
+ */
+
+/**
+ * @name dxLookupOptions.maxLength
+ * @hidden
+ */
+
+/**
+ * @name dxLookupOptions.spellcheck
+ * @hidden
+ */
+
+/**
+ * @name dxLookupOptions.buttons
+ * @hidden
+ */
+
+/**
+ * @name dxLookup.getButton
+ * @publicName getButton(name)
+ * @hidden
+ */
