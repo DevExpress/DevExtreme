868571603aaff5a061eb3be7c3461d5fe957bbf3
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-06-06 16:22:33 +0400

    ProgressBar: ignore errors after move to TS

---
+++ packages/devextreme/js/ui/progress_bar.js
@@ -0,0 +1,37 @@
+import ProgressBar from '../__internal/ui/m_progress_bar';
+
+export default ProgressBar;
+
+// STYLE progressBar
+
+/**
+ * @name dxProgressBarOptions.activeStateEnabled
+ * @hidden
+ */
+
+/**
+ * @name dxProgressBarOptions.focusStateEnabled
+ * @hidden
+ */
+
+/**
+ * @name dxProgressBarOptions.accessKey
+ * @hidden
+ */
+
+/**
+ * @name dxProgressBarOptions.tabIndex
+ * @hidden
+ */
+
+/**
+ * @name dxProgressBar.registerKeyHandler
+ * @publicName registerKeyHandler(key, handler)
+ * @hidden
+ */
+
+/**
+ * @name dxProgressBar.focus
+ * @publicName focus()
+ * @hidden
+ */
