357b5cbfe7156a2924328af18b6129af219938c2
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-06-06 17:13:27 +0400

    DropDownButton: ignore errors after move to TS

---
+++ packages/devextreme/js/ui/drop_down_button.js
@@ -0,0 +1,21 @@
+import DropDownButton from '../__internal/ui/m_drop_down_button';
+
+export default DropDownButton;
+
+// STYLE dropDownButton
+
+/**
+ * @name dxDropDownButtonItem.key
+ * @hidden
+ */
+
+/**
+ * @name dxDropDownButtonItem.showChevron
+ * @hidden
+ */
+
+/**
+ * @name dxDropDownButtonItem
+ * @inherits dxListItem
+ * @type object
+ */
