9877138e0724ca5e32790a2cfb3c4b415a31f4af
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-06-06 19:28:16 +0400

    FileUploader: ignore errors after move to TS

---
+++ packages/devextreme/js/ui/file_uploader.js
@@ -0,0 +1,22 @@
+import FileUploader from '../__internal/ui/m_file_uploader';
+
+export default FileUploader;
+
+// STYLE fileUploader
+
+/**
+ * @name dxFileUploaderOptions.extendSelection
+ * @type boolean
+ * @default true
+ * @hidden
+ */
+
+/**
+ * @name dxFileUploaderOptions.validationMessageMode
+ * @hidden
+ */
+
+/**
+ * @name dxFileUploaderOptions.validationMessagePosition
+ * @hidden
+ */
