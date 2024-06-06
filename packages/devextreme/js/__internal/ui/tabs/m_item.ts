03f8c84b34b0e2883c74a4e5f9d6cc49bc774063
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-06-06 18:51:32 +0400

    Tabs, TabPanel: ignore errors after move to TS

--- packages/devextreme/js/__internal/ui/tabs/m_item.ts
+++ packages/devextreme/js/__internal/ui/tabs/m_item.ts
@@ -1,31 +1,31 @@
-import $ from '../../core/renderer';
-import CollectionWidgetItem from '../collection/item';
+import $ from '@js/core/renderer';
+import CollectionWidgetItem from '@js/ui/collection/item';
 
 const TABS_ITEM_BADGE_CLASS = 'dx-tabs-item-badge';
 const BADGE_CLASS = 'dx-badge';
 
 const TabsItem = CollectionWidgetItem.inherit({
 
-    _renderWatchers: function() {
-        this.callBase();
+  _renderWatchers() {
+    this.callBase();
 
-        this._startWatcher('badge', this._renderBadge.bind(this));
-    },
+    this._startWatcher('badge', this._renderBadge.bind(this));
+  },
 
-    _renderBadge: function(badge) {
-        this._$element.children('.' + BADGE_CLASS).remove();
+  _renderBadge(badge) {
+    this._$element.children(`.${BADGE_CLASS}`).remove();
 
-        if(!badge) {
-            return;
-        }
-
-        const $badge = $('<div>')
-            .addClass(TABS_ITEM_BADGE_CLASS)
-            .addClass(BADGE_CLASS)
-            .text(badge);
-
-        this._$element.append($badge);
+    if (!badge) {
+      return;
     }
+
+    const $badge = $('<div>')
+      .addClass(TABS_ITEM_BADGE_CLASS)
+      .addClass(BADGE_CLASS)
+      .text(badge);
+
+    this._$element.append($badge);
+  },
 
 });
 
