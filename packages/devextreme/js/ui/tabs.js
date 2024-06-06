2347abf7505f781dcc79a7bd1efb68767e4ae095
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-06-06 18:51:32 +0400

    Tabs, TabPanel: ignore errors after move to TS

---
+++ packages/devextreme/js/ui/tabs.js
@@ -0,0 +1,17 @@
+import Tabs from '../__internal/ui/tabs/m_tabs';
+
+export default Tabs;
+
+// STYLE tabs
+
+/**
+ * @name dxTabsOptions.activeStateEnabled
+ * @hidden
+ * @default true
+ */
+
+/**
+ * @name dxTabsItem
+ * @inherits CollectionWidgetItem
+ * @type object
+ */
