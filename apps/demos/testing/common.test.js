1ca0677274b215ddfe660b0a677786459515cd4d
Nikki Gonzales <38495263+nikkithelegendarypokemonster@users.noreply.github.com>
2024-06-09 20:22:04 +0800

    Tabs/Selection: fix content alignment in demos

--- apps/demos/testing/common.test.js
+++ apps/demos/testing/common.test.js
@@ -209,9 +209,6 @@ const SKIPPED_TESTS = {
     TreeList: [
       { demo: 'Overview', themes: [THEME.material] },
     ],
-    Tabs: [
-      { demo: 'Selection', themes: [THEME.material, THEME.fluent] },
-    ],
     List: [
       { demo: 'ListSelection', themes: [THEME.material] },
       { demo: 'ListWithSearchBar', themes: [THEME.material] },
