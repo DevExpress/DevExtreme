3edaf570347eef49a28bfe74de3bd10ad7ad939a
Alex Lavrov <36633600+alexslavr@users.noreply.github.com>
2024-06-10 19:07:23 +0400

    CI: Setup npm deps scanner (#27550)

--- tools/scripts/make-artifacts-package.ts
+++ tools/scripts/make-artifacts-package.ts
@@ -1,7 +1,7 @@
 import sh from 'shelljs';
 import path from 'path';
 
-import { ARTIFACTS_DIR, INTERNAL_TOOLS_ARTIFACTS, JS_ARTIFACTS, TS_ARTIFACTS, CSS_ARTIFACTS } from './common/paths';
+import { ARTIFACTS_DIR, INTERNAL_TOOLS_ARTIFACTS, JS_ARTIFACTS, TS_ARTIFACTS, CSS_ARTIFACTS, DEPS_SCANNER_ARTIFACTS } from './common/paths';
 import { ensureEmptyDir } from './common/monorepo-tools';
 
 import { version, license, author } from '../../package.json';
@@ -11,11 +11,11 @@ const ARTIFACTS_PACKAGE_DIR = path.join(ARTIFACTS_DIR, 'devextreme-artifacts');
 
 ensureEmptyDir(ARTIFACTS_PACKAGE_DIR);
 
-sh.cp('-r', [INTERNAL_TOOLS_ARTIFACTS, JS_ARTIFACTS, TS_ARTIFACTS, CSS_ARTIFACTS], ARTIFACTS_PACKAGE_DIR);
+sh.cp('-r', [INTERNAL_TOOLS_ARTIFACTS, JS_ARTIFACTS, TS_ARTIFACTS, CSS_ARTIFACTS, DEPS_SCANNER_ARTIFACTS], ARTIFACTS_PACKAGE_DIR);
 sh.pushd(ARTIFACTS_PACKAGE_DIR);
 {
   npm.initEmpty();
   npm.pkg.set({ name: 'devextreme-artifacts', version, license, author });
-  npm.pack({destination: '../npm'});
+  npm.pack({ destination: '../npm' });
 }
 sh.popd();
