a3724e604d8d39fe5aed46173b7fff565ab0d7e5
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-06-12 19:03:34 +0400

    Validator, ValidationMessage, ValidationSummary, ValidationEngine, ValidationGroup: ignore errors after move to TS

---
+++ packages/devextreme/js/ui/validation_summary.js
@@ -0,0 +1,141 @@
+import ValidationSummary from '../__internal/ui/m_validation_summary';
+
+export default ValidationSummary;
+
+/**
+ * @name dxValidationSummaryOptions.focusStateEnabled
+ * @hidden
+ */
+
+/**
+ * @name dxValidationSummaryOptions.noDataText
+ * @hidden
+ */
+
+/**
+ * @name dxValidationSummaryOptions.dataSource
+ * @hidden
+ */
+
+/**
+ * @name dxValidationSummaryOptions.activeStateEnabled
+ * @hidden
+ */
+
+/**
+ * @name dxValidationSummaryOptions.disabled
+ * @hidden
+ */
+
+/**
+ * @name dxValidationSummaryOptions.hint
+ * @hidden
+ */
+
+/**
+ * @name dxValidationSummaryOptions.itemHoldTimeout
+ * @hidden
+ */
+
+/**
+ * @name dxValidationSummaryOptions.rtlEnabled
+ * @hidden
+ */
+
+/**
+ * @name dxValidationSummaryOptions.selectedIndex
+ * @hidden
+ */
+
+/**
+ * @name dxValidationSummaryOptions.selectedItem
+ * @hidden
+ */
+
+/**
+ * @name dxValidationSummaryOptions.selectedItems
+ * @hidden
+ */
+
+/**
+ * @name dxValidationSummaryOptions.selectedItemKeys
+ * @hidden
+ */
+
+/**
+ * @name dxValidationSummaryOptions.keyExpr
+ * @hidden
+ */
+
+/**
+ * @name dxValidationSummaryOptions.visible
+ * @hidden
+ */
+
+/**
+ * @name dxValidationSummaryOptions.width
+ * @hidden
+ */
+
+/**
+ * @name dxValidationSummaryOptions.height
+ * @hidden
+ */
+
+/**
+ * @name dxValidationSummaryOptions.onItemHold
+ * @hidden
+ * @action
+ */
+
+/**
+ * @name dxValidationSummaryOptions.onItemRendered
+ * @hidden
+ * @action
+ */
+
+/**
+ * @name dxValidationSummaryOptions.onItemSelect
+ * @hidden
+ * @action
+ */
+
+/**
+ * @name dxValidationSummaryOptions.onSelectionChanged
+ * @hidden
+ * @action
+ */
+
+/**
+ * @name dxValidationSummaryOptions.onItemContextMenu
+ * @hidden
+ * @action
+ */
+
+/**
+ * @name dxValidationSummaryOptions.accessKey
+ * @hidden
+ */
+
+/**
+ * @name dxValidationSummaryOptions.tabIndex
+ * @hidden
+ */
+
+/**
+ * @name dxValidationSummary.registerKeyHandler
+ * @publicName registerKeyHandler(key, handler)
+ * @hidden
+ */
+
+/**
+ * @name dxValidationSummary.getDataSource
+ * @publicName getDataSource()
+ * @hidden
+ */
+
+/**
+ * @name dxValidationSummary.focus
+ * @publicName focus()
+ * @hidden
+ */
