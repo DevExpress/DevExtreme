68a6b8684813a217afa679cdbf6f913d26fa859c
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-06-12 19:03:34 +0400

    Validator, ValidationMessage, ValidationSummary, ValidationEngine, ValidationGroup: ignore errors after move to TS

---
+++ packages/devextreme/js/ui/validation_engine.js
@@ -0,0 +1,5 @@
+import ValidationEngine from '../__internal/ui/m_validation_engine';
+
+export default ValidationEngine;
+
+// STYLE validation
---
+++ packages/devextreme/js/ui/validation_message.js
@@ -0,0 +1,3 @@
+import ValidationMessage from '../__internal/ui/m_validation_message';
+
+export default ValidationMessage;
