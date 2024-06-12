994f73425f99b26b0439d2b704adf4428b7e2d93
Alyar <Alyar666@users.noreply.github.com>
2024-06-12 18:03:41 +0300

    DataGrid: Fix row going into edit mode when popup edit mode is enabled and editRowKey is initially specified (T1218553) (#27573)
    Co-authored-by: Alyar <>

--- js/__internal/grids/grid_core/editing/m_editing.ts
+++ js/__internal/grids/grid_core/editing/m_editing.ts
@@ -49,6 +49,7 @@ import {
   EDIT_FORM_CLASS,
   EDIT_ICON_CLASS,
   EDIT_LINK_CLASS,
+  EDIT_MODE_POPUP,
   EDIT_MODE_ROW,
   EDIT_MODES,
   EDITING_CHANGES_OPTION_NAME,
@@ -345,6 +346,11 @@ class EditingControllerImpl extends modules.ViewController {
     }
 
     return result;
+  }
+
+  protected isPopupEditMode() {
+    const editMode = this.option('editing.mode');
+    return editMode === EDIT_MODE_POPUP;
   }
 
   private _isButtonVisible(button, options) {
