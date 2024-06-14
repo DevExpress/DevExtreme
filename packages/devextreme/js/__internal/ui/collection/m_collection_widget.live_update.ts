5f6e5e4bc6b290d8228be520fc068904c35eedb8
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-06-14 11:21:40 +0400

    Base, Edit, Asinc, LiveUpdate Collections, DataController: ignore errors after move to TS

--- packages/devextreme/js/__internal/ui/collection/m_collection_widget.live_update.ts
+++ packages/devextreme/js/__internal/ui/collection/m_collection_widget.live_update.ts
@@ -1,265 +1,267 @@
-import $ from '../../core/renderer';
-import CollectionWidget from './ui.collection_widget.edit';
-import { extend } from '../../core/utils/extend';
-import { each } from '../../core/utils/iterator';
-import { update, insert, indexByKey } from '../../data/array_utils';
-import { keysEqual } from '../../data/utils';
-import { when } from '../../core/utils/deferred';
-import { findChanges } from '../../core/utils/array_compare';
-import domAdapter from '../../core/dom_adapter';
-import { noop } from '../../core/utils/common';
+import domAdapter from '@js/core/dom_adapter';
+import $ from '@js/core/renderer';
+import { findChanges } from '@js/core/utils/array_compare';
+import { noop } from '@js/core/utils/common';
+import { when } from '@js/core/utils/deferred';
+import { extend } from '@js/core/utils/extend';
+import { each } from '@js/core/utils/iterator';
+import { indexByKey, insert, update } from '@js/data/array_utils';
+// @ts-expect-error
+import { keysEqual } from '@js/data/utils';
+
+import CollectionWidget from './m_collection_widget.edit';
 
 const PRIVATE_KEY_FIELD = '__dx_key__';
 
 export default CollectionWidget.inherit({
-    _getDefaultOptions: function() {
-        return extend(this.callBase(), {
-            repaintChangesOnly: false
-        });
-    },
+  _getDefaultOptions() {
+    return extend(this.callBase(), {
+      repaintChangesOnly: false,
+    });
+  },
 
-    ctor: function() {
-        this.callBase.apply(this, arguments);
+  ctor() {
+    this.callBase.apply(this, arguments);
 
-        this._customizeStoreLoadOptions = (e) => {
-            const dataController = this._dataController;
+    this._customizeStoreLoadOptions = (e) => {
+      const dataController = this._dataController;
 
-            if(dataController.getDataSource() && !this._dataController.isLoaded()) {
-                this._correctionIndex = 0;
-            }
-            if(this._correctionIndex && e.storeLoadOptions) {
-                e.storeLoadOptions.skip += this._correctionIndex;
-            }
-        },
-
-        this._dataController?.on('customizeStoreLoadOptions', this._customizeStoreLoadOptions);
-    },
-
-    reload: function() {
+      if (dataController.getDataSource() && !this._dataController.isLoaded()) {
         this._correctionIndex = 0;
-    },
+      }
+      if (this._correctionIndex && e.storeLoadOptions) {
+        e.storeLoadOptions.skip += this._correctionIndex;
+      }
+    };
 
-    _init: function() {
-        this.callBase();
-        this._refreshItemsCache();
-        this._correctionIndex = 0;
-    },
+    this._dataController?.on('customizeStoreLoadOptions', this._customizeStoreLoadOptions);
+  },
 
-    _findItemElementByKey: function(key) {
-        let result = $();
-        const keyExpr = this.key();
-        this.itemElements().each((_, item) => {
-            const $item = $(item);
-            const itemData = this._getItemData($item);
-            if(keyExpr ? keysEqual(keyExpr, this.keyOf(itemData), key) : this._isItemEquals(itemData, key)) {
-                result = $item;
-                return false;
-            }
-        });
-        return result;
-    },
+  reload() {
+    this._correctionIndex = 0;
+  },
 
-    _dataSourceChangedHandler: function(newItems, e) {
-        if(e?.changes) {
-            this._modifyByChanges(e.changes);
-        } else {
-            this.callBase(newItems, e);
-            this._refreshItemsCache();
-        }
-    },
+  _init() {
+    this.callBase();
+    this._refreshItemsCache();
+    this._correctionIndex = 0;
+  },
 
-    _isItemEquals: function(item1, item2) {
-        if(item1 && item1[PRIVATE_KEY_FIELD]) {
-            item1 = item1.data;
-        }
-
-        try {
-            return JSON.stringify(item1) === JSON.stringify(item2);
-        } catch(e) {
-            return item1 === item2;
-        }
-    },
-
-    _isItemStrictEquals: function(item1, item2) {
-        return this._isItemEquals(item1, item2);
-    },
-
-    _shouldAddNewGroup: function(changes, items) {
-        let result = false;
-        if(this.option('grouped')) {
-            if(!changes.length) {
-                result = true;
-            }
-            each(changes, (i, change) => {
-                if(change.type === 'insert') {
-                    result = true;
-                    each(items, (_, item) => {
-                        if(change.data.key !== undefined && change.data.key === item.key) {
-                            result = false;
-                            return false;
-                        }
-                    });
-                }
-            });
-        }
-
-        return result;
-    },
-
-    _partialRefresh: function() {
-        if(this.option('repaintChangesOnly')) {
-            const keyOf = (data) => {
-                if(data && data[PRIVATE_KEY_FIELD] !== undefined) {
-                    return data[PRIVATE_KEY_FIELD];
-                }
-                return this.keyOf(data);
-            };
-            const result = findChanges(this._itemsCache, this._editStrategy.itemsGetter(), keyOf, this._isItemStrictEquals.bind(this));
-            if(result && this._itemsCache.length && !this._shouldAddNewGroup(result, this._itemsCache)) {
-                this._modifyByChanges(result, true);
-                this._renderEmptyMessage();
-                return true;
-            } else {
-                this._refreshItemsCache();
-            }
-        }
+  _findItemElementByKey(key) {
+    // @ts-expect-error
+    let result = $();
+    const keyExpr = this.key();
+    // @ts-expect-error
+    this.itemElements().each((_, item) => {
+      const $item = $(item);
+      const itemData = this._getItemData($item);
+      if (keyExpr ? keysEqual(keyExpr, this.keyOf(itemData), key) : this._isItemEquals(itemData, key)) {
+        result = $item;
         return false;
-    },
+      }
+    });
+    return result;
+  },
 
-    _refreshItemsCache: function() {
-        if(this.option('repaintChangesOnly')) {
-            const items = this._editStrategy.itemsGetter();
-            try {
-                this._itemsCache = extend(true, [], items);
-                if(!this.key()) {
-                    this._itemsCache = this._itemsCache.map((itemCache, index) => {
-                        return {
-                            [PRIVATE_KEY_FIELD]: items[index],
-                            data: itemCache
-                        };
-                    });
-                }
-            } catch(e) {
-                this._itemsCache = extend([], items);
-            }
-        }
-    },
-
-    _dispose: function() {
-        this._dataController.off('customizeStoreLoadOptions', this._customizeStoreLoadOptions);
-        this.callBase();
-    },
-
-    _updateByChange: function(keyInfo, items, change, isPartialRefresh) {
-        if(isPartialRefresh) {
-            this._renderItem(change.index, change.data, null, this._findItemElementByKey(change.key));
-        } else {
-            const changedItem = items[indexByKey(keyInfo, items, change.key)];
-            if(changedItem) {
-                update(keyInfo, items, change.key, change.data).done(() => {
-                    this._renderItem(items.indexOf(changedItem), changedItem, null, this._findItemElementByKey(change.key));
-                });
-            }
-        }
-    },
-
-    _insertByChange: function(keyInfo, items, change, isPartialRefresh) {
-        when(isPartialRefresh || insert(keyInfo, items, change.data, change.index)).done(() => {
-            this._beforeItemElementInserted(change);
-
-            this._renderItem(change.index ?? items.length, change.data);
-
-            this._afterItemElementInserted();
-            this._correctionIndex++;
-        });
-    },
-
-    _updateSelectionAfterRemoveByChange: function(removeIndex) {
-        const selectedIndex = this.option('selectedIndex');
-
-        if(selectedIndex > removeIndex) {
-            this.option('selectedIndex', selectedIndex - 1);
-        } else if(selectedIndex === removeIndex && this.option('selectedItems').length === 1) {
-            this.option('selectedItems', []);
-        } else {
-            this._normalizeSelectedItems();
-        }
-    },
-
-    _beforeItemElementInserted: function(change) {
-        const selectedIndex = this.option('selectedIndex');
-
-        if(change.index <= selectedIndex) {
-            this.option('selectedIndex', selectedIndex + 1);
-        }
-    },
-
-    _afterItemElementInserted: noop,
-
-    _removeByChange: function(keyInfo, items, change, isPartialRefresh) {
-        const index = isPartialRefresh ? change.index : indexByKey(keyInfo, items, change.key);
-        const removedItem = isPartialRefresh ? change.oldItem : items[index];
-        if(removedItem) {
-            const $removedItemElement = this._findItemElementByKey(change.key);
-            const deletedActionArgs = this._extendActionArgs($removedItemElement);
-
-            this._waitDeletingPrepare($removedItemElement).done(()=>{
-                if(isPartialRefresh) {
-                    this._updateIndicesAfterIndex(index - 1);
-                    this._afterItemElementDeleted($removedItemElement, deletedActionArgs);
-                    this._updateSelectionAfterRemoveByChange(index);
-                } else {
-                    this._deleteItemElementByIndex(index);
-                    this._afterItemElementDeleted($removedItemElement, deletedActionArgs);
-                }
-            });
-
-            this._correctionIndex--;
-        }
-    },
-
-    _modifyByChanges: function(changes, isPartialRefresh) {
-        const items = this._editStrategy.itemsGetter();
-        const keyInfo = { key: this.key.bind(this), keyOf: this.keyOf.bind(this) };
-        const dataController = this._dataController;
-        const paginate = dataController.paginate();
-        const group = dataController.group();
-
-        if(paginate || group) {
-            changes = changes.filter(item => item.type !== 'insert' || item.index !== undefined);
-        }
-
-        changes.forEach(change => this[`_${change.type}ByChange`](keyInfo, items, change, isPartialRefresh));
-        this._renderedItemsCount = items.length;
-        this._refreshItemsCache();
-        this._fireContentReadyAction();
-    },
-
-    _appendItemToContainer: function($container, $itemFrame, index) {
-        const nextSiblingElement = $container.children(this._itemSelector()).get(index);
-        domAdapter.insertElement($container.get(0), $itemFrame.get(0), nextSiblingElement);
-    },
-
-    _optionChanged: function(args) {
-        switch(args.name) {
-            case 'items': {
-                const isItemsUpdated = this._partialRefresh(args.value);
-                if(!isItemsUpdated) {
-                    this.callBase(args);
-                }
-                break;
-            }
-            case 'dataSource':
-                if(!this.option('repaintChangesOnly') || !args.value) {
-                    this.option('items', []);
-                }
-
-                this.callBase(args);
-                break;
-            case 'repaintChangesOnly':
-                break;
-            default:
-                this.callBase(args);
-        }
+  _dataSourceChangedHandler(newItems, e) {
+    if (e?.changes) {
+      this._modifyByChanges(e.changes);
+    } else {
+      this.callBase(newItems, e);
+      this._refreshItemsCache();
     }
+  },
+
+  _isItemEquals(item1, item2) {
+    if (item1 && item1[PRIVATE_KEY_FIELD]) {
+      item1 = item1.data;
+    }
+
+    try {
+      return JSON.stringify(item1) === JSON.stringify(item2);
+    } catch (e) {
+      return item1 === item2;
+    }
+  },
+
+  _isItemStrictEquals(item1, item2) {
+    return this._isItemEquals(item1, item2);
+  },
+
+  _shouldAddNewGroup(changes, items) {
+    let result = false;
+    if (this.option('grouped')) {
+      if (!changes.length) {
+        result = true;
+      }
+      each(changes, (i, change) => {
+        if (change.type === 'insert') {
+          result = true;
+          // @ts-expect-error
+          each(items, (_, item) => {
+            if (change.data.key !== undefined && change.data.key === item.key) {
+              result = false;
+              return false;
+            }
+          });
+        }
+      });
+    }
+
+    return result;
+  },
+
+  _partialRefresh() {
+    if (this.option('repaintChangesOnly')) {
+      const keyOf = (data) => {
+        if (data && data[PRIVATE_KEY_FIELD] !== undefined) {
+          return data[PRIVATE_KEY_FIELD];
+        }
+        return this.keyOf(data);
+      };
+      const result = findChanges(this._itemsCache, this._editStrategy.itemsGetter(), keyOf, this._isItemStrictEquals.bind(this));
+      if (result && this._itemsCache.length && !this._shouldAddNewGroup(result, this._itemsCache)) {
+        this._modifyByChanges(result, true);
+        this._renderEmptyMessage();
+        return true;
+      }
+      this._refreshItemsCache();
+    }
+    return false;
+  },
+
+  _refreshItemsCache() {
+    if (this.option('repaintChangesOnly')) {
+      const items = this._editStrategy.itemsGetter();
+      try {
+        this._itemsCache = extend(true, [], items);
+        if (!this.key()) {
+          this._itemsCache = this._itemsCache.map((itemCache, index) => ({
+            [PRIVATE_KEY_FIELD]: items[index],
+            data: itemCache,
+          }));
+        }
+      } catch (e) {
+        this._itemsCache = extend([], items);
+      }
+    }
+  },
+
+  _dispose() {
+    this._dataController.off('customizeStoreLoadOptions', this._customizeStoreLoadOptions);
+    this.callBase();
+  },
+
+  _updateByChange(keyInfo, items, change, isPartialRefresh) {
+    if (isPartialRefresh) {
+      this._renderItem(change.index, change.data, null, this._findItemElementByKey(change.key));
+    } else {
+      const changedItem = items[indexByKey(keyInfo, items, change.key)];
+      if (changedItem) {
+        update(keyInfo, items, change.key, change.data).done(() => {
+          this._renderItem(items.indexOf(changedItem), changedItem, null, this._findItemElementByKey(change.key));
+        });
+      }
+    }
+  },
+
+  _insertByChange(keyInfo, items, change, isPartialRefresh) {
+    when(isPartialRefresh || insert(keyInfo, items, change.data, change.index)).done(() => {
+      this._beforeItemElementInserted(change);
+
+      this._renderItem(change.index ?? items.length, change.data);
+
+      this._afterItemElementInserted();
+      this._correctionIndex++;
+    });
+  },
+
+  _updateSelectionAfterRemoveByChange(removeIndex) {
+    const selectedIndex = this.option('selectedIndex');
+
+    if (selectedIndex > removeIndex) {
+      this.option('selectedIndex', selectedIndex - 1);
+    } else if (selectedIndex === removeIndex && this.option('selectedItems').length === 1) {
+      this.option('selectedItems', []);
+    } else {
+      this._normalizeSelectedItems();
+    }
+  },
+
+  _beforeItemElementInserted(change) {
+    const selectedIndex = this.option('selectedIndex');
+
+    if (change.index <= selectedIndex) {
+      this.option('selectedIndex', selectedIndex + 1);
+    }
+  },
+
+  _afterItemElementInserted: noop,
+
+  _removeByChange(keyInfo, items, change, isPartialRefresh) {
+    const index = isPartialRefresh ? change.index : indexByKey(keyInfo, items, change.key);
+    const removedItem = isPartialRefresh ? change.oldItem : items[index];
+    if (removedItem) {
+      const $removedItemElement = this._findItemElementByKey(change.key);
+      const deletedActionArgs = this._extendActionArgs($removedItemElement);
+
+      this._waitDeletingPrepare($removedItemElement).done(() => {
+        if (isPartialRefresh) {
+          this._updateIndicesAfterIndex(index - 1);
+          this._afterItemElementDeleted($removedItemElement, deletedActionArgs);
+          this._updateSelectionAfterRemoveByChange(index);
+        } else {
+          this._deleteItemElementByIndex(index);
+          this._afterItemElementDeleted($removedItemElement, deletedActionArgs);
+        }
+      });
+
+      this._correctionIndex--;
+    }
+  },
+
+  _modifyByChanges(changes, isPartialRefresh) {
+    const items = this._editStrategy.itemsGetter();
+    const keyInfo = { key: this.key.bind(this), keyOf: this.keyOf.bind(this) };
+    const dataController = this._dataController;
+    const paginate = dataController.paginate();
+    const group = dataController.group();
+
+    if (paginate || group) {
+      changes = changes.filter((item) => item.type !== 'insert' || item.index !== undefined);
+    }
+
+    changes.forEach((change) => this[`_${change.type}ByChange`](keyInfo, items, change, isPartialRefresh));
+    this._renderedItemsCount = items.length;
+    this._refreshItemsCache();
+    this._fireContentReadyAction();
+  },
+
+  _appendItemToContainer($container, $itemFrame, index) {
+    const nextSiblingElement = $container.children(this._itemSelector()).get(index);
+    domAdapter.insertElement($container.get(0), $itemFrame.get(0), nextSiblingElement);
+  },
+
+  _optionChanged(args) {
+    switch (args.name) {
+      case 'items': {
+        const isItemsUpdated = this._partialRefresh(args.value);
+        if (!isItemsUpdated) {
+          this.callBase(args);
+        }
+        break;
+      }
+      case 'dataSource':
+        if (!this.option('repaintChangesOnly') || !args.value) {
+          this.option('items', []);
+        }
+
+        this.callBase(args);
+        break;
+      case 'repaintChangesOnly':
+        break;
+      default:
+        this.callBase(args);
+    }
+  },
 });
--- packages/devextreme/js/__internal/ui/collection/m_item.ts
+++ packages/devextreme/js/__internal/ui/collection/m_item.ts
@@ -1,109 +1,109 @@
-import $ from '../../core/renderer';
-import Class from '../../core/class';
-import { each } from '../../core/utils/iterator';
-import { attachInstanceToElement, getInstanceByElement } from '../../core/utils/public_component';
+import Class from '@js/core/class';
+import $ from '@js/core/renderer';
+import { each } from '@js/core/utils/iterator';
+import { attachInstanceToElement, getInstanceByElement } from '@js/core/utils/public_component';
 
 const INVISIBLE_STATE_CLASS = 'dx-state-invisible';
 const DISABLED_STATE_CLASS = 'dx-state-disabled';
 const ITEM_CONTENT_PLACEHOLDER_CLASS = 'dx-item-content-placeholder';
 
-const forcibleWatcher = function(watchMethod, fn, callback) {
-    const filteredCallback = (function() {
-        let oldValue;
-        return function(value) {
-            if(oldValue !== value) {
-                callback(value, oldValue);
-                oldValue = value;
-            }
-        };
-    })();
-
-    return {
-        dispose: watchMethod(fn, filteredCallback),
-        force: function() {
-            filteredCallback(fn());
-        }
+const forcibleWatcher = function (watchMethod, fn, callback) {
+  const filteredCallback = (function () {
+    let oldValue;
+    return function (value) {
+      if (oldValue !== value) {
+        callback(value, oldValue);
+        oldValue = value;
+      }
     };
+  }());
+
+  return {
+    dispose: watchMethod(fn, filteredCallback),
+    force() {
+      filteredCallback(fn());
+    },
+  };
 };
 
 const CollectionItem = Class.inherit({
 
-    ctor: function($element, options, rawData) {
-        this._$element = $element;
-        this._options = options;
-        this._rawData = rawData;
+  ctor($element, options, rawData) {
+    this._$element = $element;
+    this._options = options;
+    this._rawData = rawData;
 
-        attachInstanceToElement($element, this, this._dispose);
+    attachInstanceToElement($element, this, this._dispose);
 
-        this._render();
-    },
+    this._render();
+  },
 
-    _render: function() {
-        const $placeholder = $('<div>').addClass(ITEM_CONTENT_PLACEHOLDER_CLASS);
-        this._$element.append($placeholder);
+  _render() {
+    const $placeholder = $('<div>').addClass(ITEM_CONTENT_PLACEHOLDER_CLASS);
+    this._$element.append($placeholder);
 
-        this._watchers = [];
-        this._renderWatchers();
-    },
+    this._watchers = [];
+    this._renderWatchers();
+  },
 
-    _renderWatchers: function() {
-        this._startWatcher('disabled', this._renderDisabled.bind(this));
-        this._startWatcher('visible', this._renderVisible.bind(this));
-    },
+  _renderWatchers() {
+    this._startWatcher('disabled', this._renderDisabled.bind(this));
+    this._startWatcher('visible', this._renderVisible.bind(this));
+  },
 
-    _startWatcher: function(field, render) {
-        const rawData = this._rawData;
-        const exprGetter = this._options.fieldGetter(field);
+  _startWatcher(field, render) {
+    const rawData = this._rawData;
+    const exprGetter = this._options.fieldGetter(field);
 
-        const watcher = forcibleWatcher(this._options.watchMethod(), function() {
-            return exprGetter(rawData);
-        }, function(value, oldValue) {
-            this._dirty = true;
-            render(value, oldValue);
-        }.bind(this));
+    const watcher = forcibleWatcher(this._options.watchMethod(), () => exprGetter(rawData), (value, oldValue) => {
+      this._dirty = true;
+      render(value, oldValue);
+    });
 
-        this._watchers.push(watcher);
-    },
-
-    setDataField: function() {
-        this._dirty = false;
-        each(this._watchers, function(_, watcher) {
-            watcher.force();
-        });
-        if(this._dirty) {
-            return true;
-        }
-    },
-
-    _renderDisabled: function(value, oldValue) {
-        this._$element.toggleClass(DISABLED_STATE_CLASS, !!value);
-        this._$element.attr('aria-disabled', !!value);
-
-        this._updateOwnerFocus(value);
-    },
-
-    _updateOwnerFocus: function(isDisabled) {
-        const ownerComponent = this._options.owner;
-
-        if(ownerComponent && isDisabled) {
-            ownerComponent._resetItemFocus(this._$element);
-        }
-    },
-
-    _renderVisible: function(value, oldValue) {
-        this._$element.toggleClass(INVISIBLE_STATE_CLASS, value !== undefined && !value);
-    },
-
-    _dispose: function() {
-        each(this._watchers, function(_, watcher) {
-            watcher.dispose();
-        });
+    this._watchers.push(watcher);
+  },
+  // @ts-expect-error
+  setDataField() {
+    this._dirty = false;
+    each(this._watchers, (_, watcher) => {
+      watcher.force();
+    });
+    if (this._dirty) {
+      return true;
     }
+  },
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  _renderDisabled(value, oldValue) {
+    this._$element.toggleClass(DISABLED_STATE_CLASS, !!value);
+    this._$element.attr('aria-disabled', !!value);
+
+    this._updateOwnerFocus(value);
+  },
+
+  _updateOwnerFocus(isDisabled) {
+    const ownerComponent = this._options.owner;
+
+    if (ownerComponent && isDisabled) {
+      ownerComponent._resetItemFocus(this._$element);
+    }
+  },
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  _renderVisible(value, oldValue) {
+    this._$element.toggleClass(INVISIBLE_STATE_CLASS, value !== undefined && !value);
+  },
+
+  _dispose() {
+    each(this._watchers, (_, watcher) => {
+      watcher.dispose();
+    });
+  },
 
 });
-
-CollectionItem.getInstance = function($element) {
-    return getInstanceByElement($element, this);
+// @ts-expect-error
+CollectionItem.getInstance = function ($element) {
+  return getInstanceByElement($element, this);
 };
 
 export default CollectionItem;
