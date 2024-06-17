3e50d0d986a789fb07d824647a9e9dc48d978139
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-06-17 22:04:24 +0400

    Form: ignore errors after move to TS

---
+++ packages/devextreme/js/ui/form/ui.form.js
@@ -0,0 +1,5 @@
+import Form from '../../__internal/ui/form/m_form';
+
+export default Form;
+
+// STYLE form
--- packages/devextreme/testing/tests/DevExpress.ui.widgets/optionChanged_bundled.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.widgets/optionChanged_bundled.tests.js
@@ -38,7 +38,10 @@ QUnit.module('OptionChanged', {
                 name === 'validationMessageOffset' ||
                 name === 'templatesRenderAsynchronously' ||
                 name === 'ignoreChildEvents' ||
+                name === 'copyRootClassesToWrapper' ||
+                name === '_ignoreCopyRootClassesToWrapperDeprecation' ||
                 name === '_dataController' ||
+                name === '_ignoreElementAttrDeprecation' ||
                 name === '_ignorePreventScrollEventsDeprecation' ||
                 name === '_checkParentVisibility') {
                 return;
@@ -77,94 +80,70 @@ QUnit.module('OptionChanged', {
         }
     };
 
-    $.each(DevExpress.ui, function(componentName, componentConstructor) {
+    $.each(DevExpress.ui, (componentName, componentConstructor) => {
         if($.inArray(componentName, excludedComponents) !== -1
             || componentConstructor.IS_RENOVATED_WIDGET) {
             return;
         }
 
-        Component.prototype._optionChanged = function(args) {
-            const name = args.name;
+        const widgetName = componentName.replace('dx', '').toLowerCase();
+        if($.fn[componentName]) {
+            componentConstructor.prototype._defaultOptionsRules = function() {
+                return [];
+            };
 
-            if(this._getDeprecatedOptions()[name] ||
-                    name === 'rtlEnabled' ||
-                    name === 'onOptionChanged' ||
-                    name === 'onDisposing' ||
-                    name === 'onInitialized' ||
-                    name === 'defaultOptionsRules' ||
-                    name === 'useNativeScrolling' ||
-                    name === 'exportingAction' ||
-                    name === 'exportedAction' ||
-                    name === 'fileSavingAction' ||
-                    name === 'validationMessageOffset' ||
-                    name === 'templatesRenderAsynchronously' ||
-                    name === 'ignoreChildEvents' ||
-                    name === 'copyRootClassesToWrapper' ||
-                    name === '_ignoreCopyRootClassesToWrapperDeprecation' ||
-                    name === '_dataController' ||
-                    name === '_ignoreElementAttrDeprecation' ||
-                    name === '_ignorePreventScrollEventsDeprecation' ||
-                    name === '_checkParentVisibility') {
-                return;
-            }
-            // NOTE: workaround for inferno
-            // Internal renovated component call _optionChanged before QUnitAssert initialized
-            if(this.QUnitAssert) {
-                this.QUnitAssert.ok(false, 'Option \'' + name + '\' is not processed after runtime change');
-            }
-        };
+            QUnit.test(componentName, function(assert) {
+                const $element = this.$element;
+                const component = $element[componentName](getDefaultOptions(componentName))[componentName]('instance');
+                const options = component.option();
 
-        QUnit.test(componentName, function(assert) {
-            const $element = this.$element;
-            const component = $element[componentName](getDefaultOptions(componentName))[componentName]('instance');
-            const options = component.option();
+                component.QUnitAssert = assert;
 
-            component.QUnitAssert = assert;
+                const classes = $element.attr('class').split(' ');
 
-            const classes = $element.attr('class').split(' ');
+                $.each(classes, function(_, className) {
+                    className = className.replace('dx-', '');
 
-            $.each(classes, function(_, className) {
-                className = className.replace('dx-', '');
+                    if(className.indexOf(widgetName) === 0) {
+                        assert.ok(true, ' class name was checked');
+                    } else {
+                        className = className.replace('-', '');
 
-                if(className.indexOf(componentName) === 0) {
-                    assert.ok(true, ' class name was checked');
-                } else {
-                    className = className.replace('-', '');
-
-                    if(className.indexOf(componentName) === 0) {
-                        assert.ok(false, className + ' is failed');
+                        if(className.indexOf(widgetName) === 0) {
+                            assert.ok(false, className + ' is failed');
+                        }
                     }
-                }
-            });
+                });
 
-            $.each(options, function(option) {
-                const prevValue = options[option];
-                let newValue = prevValue;
+                $.each(options, function(option) {
+                    const prevValue = options[option];
+                    let newValue = prevValue;
 
-                // NOTE: some widgets doesn't support dataSource === null
-                if(option === 'dataSource') {
+                    // NOTE: some widgets doesn't support dataSource === null
+                    if(option === 'dataSource') {
                     // NOTE: dxResponsiveBox supports only plain object in items
-                    let item = componentName === 'dxResponsiveBox' ? { text: 1 } : 1;
-                    item = componentName === 'dxScheduler' ? { text: 1, startDate: new Date(2015, 0, 1) } : item;
+                        let item = componentName === 'dxResponsiveBox' ? { text: 1 } : 1;
+                        item = componentName === 'dxScheduler' ? { text: 1, startDate: new Date(2015, 0, 1) } : item;
 
-                    newValue = new DataSource([item]);
-                    options[option] = newValue;
-                }
+                        newValue = new DataSource([item]);
+                        options[option] = newValue;
+                    }
 
-                if(componentName === 'dxDateViewRoller' && option === 'selectedIndex') {
-                    return;
-                }
+                    if(componentName === 'dxDateViewRoller' && option === 'selectedIndex') {
+                        return;
+                    }
 
-                const assertOkSpy = sinon.spy(assert, 'ok');
-                component.beginUpdate();
-                component._notifyOptionChanged(option, newValue, prevValue);
-                component.endUpdate();
+                    const assertOkSpy = sinon.spy(assert, 'ok');
+                    component.beginUpdate();
+                    component._notifyOptionChanged(option, newValue, prevValue);
+                    component.endUpdate();
 
-                if(assertOkSpy.notCalled) {
-                    assert.ok(true, option);
-                }
-                assertOkSpy.restore();
+                    if(assertOkSpy.notCalled) {
+                        assert.ok(true, option);
+                    }
+                    assertOkSpy.restore();
+                });
             });
-        });
+        }
     });
 });
