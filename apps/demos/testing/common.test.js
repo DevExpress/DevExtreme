f7a5abe350afc6895c55b5316bbb01d0d3039bf6
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-06-18 15:30:07 +0800

    Toolbar Adaptability: Fix demos and unskip Testcafe tests (Angular, React and Vue) (#27509)
    Signed-off-by: Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>

--- apps/demos/testing/common.test.js
+++ apps/demos/testing/common.test.js
@@ -94,9 +94,6 @@ const SKIPPED_TESTS = {
       { demo: 'Overview', themes: [THEME.material] },
       { demo: 'ChartIntegration', themes: [THEME.material] },
     ],
-    Toolbar: [
-      { demo: 'Adaptability', themes: [THEME.fluent, THEME.material] },
-    ],
     TreeList: [
       { demo: 'BatchEditing', themes: [THEME.material] },
       { demo: 'RowEditing', themes: [THEME.material] },
@@ -142,9 +139,6 @@ const SKIPPED_TESTS = {
     Scheduler: [
       { demo: 'Overview', themes: [THEME.fluent, THEME.material] },
       { demo: 'GroupByDate', themes: [THEME.fluent, THEME.material] },
-    ],
-    Toolbar: [
-      { demo: 'Adaptability', themes: [THEME.fluent, THEME.material] },
     ],
     List: [
       { demo: 'ListWithSearchBar', themes: [THEME.material] },
@@ -199,9 +193,6 @@ const SKIPPED_TESTS = {
     ],
     TileView: [
       { demo: 'Directions', themes: [THEME.material] },
-    ],
-    Toolbar: [
-      { demo: 'Adaptability', themes: [THEME.fluent, THEME.material] },
     ],
     TreeList: [
       { demo: 'Overview', themes: [THEME.material] },
