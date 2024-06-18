e04359a819a74579b137c0d55272566d4e92c019
Roman Semenov <roman.semenov@devexpress.com>
2024-06-18 19:37:03 +0400

    fix: lint

--- e2e/testcafe-devextreme/tests/dataGrid/keyboardNavigation/editOnKeyPress.ts
+++ e2e/testcafe-devextreme/tests/dataGrid/keyboardNavigation/editOnKeyPress.ts
@@ -1,7 +1,6 @@
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import DataGrid from 'devextreme-testcafe-models/dataGrid';
 import { ClientFunction } from 'testcafe';
-import { Properties } from 'devextreme/ui/data_grid';
 import url from '../../../helpers/getPageUrl';
 import { createWidget } from '../../../helpers/createWidget';
 import { makeRowsViewTemplatesAsync } from '../helpers/asyncTemplates';
