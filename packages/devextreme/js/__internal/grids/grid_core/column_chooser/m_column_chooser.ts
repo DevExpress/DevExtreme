7691899649302f5fe56d63655ddc4d0b14969e67
Alyar <>
2024-06-19 00:00:44 +0400

    code refactoring

--- packages/devextreme/js/__internal/grids/grid_core/column_chooser/m_column_chooser.ts
+++ packages/devextreme/js/__internal/grids/grid_core/column_chooser/m_column_chooser.ts
@@ -303,7 +303,6 @@ export class ColumnChooserView extends ColumnsView {
     return {
       noDataText: columnChooserOptions.emptyPanelText,
       activeStateEnabled: false,
-      focusStateEnabled: true,
       hoverStateEnabled: false,
       itemTemplate(data, index, item) {
         $(item)
