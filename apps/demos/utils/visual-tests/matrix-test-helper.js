8c3cc2cb25cea1c4a9e31335071ec3306ba4503a
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-06-19 16:16:07 +0800

    Toolbar Adaptability: Fix demos and unskip Testcafe tests (Angular, React and Vue) (#27615)

--- apps/demos/utils/visual-tests/matrix-test-helper.js
+++ apps/demos/utils/visual-tests/matrix-test-helper.js
@@ -213,9 +213,6 @@ const SKIPPED_TESTS = {
     Scheduler: [
       'CustomDragAndDrop',
     ],
-    Toolbar: [
-      { demo: 'Adaptability', themes: [THEME.fluent, THEME.material] },
-    ],
   },
   Vue: {
     Charts: [
@@ -245,9 +242,6 @@ const SKIPPED_TESTS = {
     TabPanel: [
       { demo: 'Overview', themes: [THEME.material] },
     ],
-    Toolbar: [
-      { demo: 'Adaptability', themes: [THEME.fluent, THEME.material] },
-    ],
   },
   React: {
     Charts: [
@@ -269,9 +263,6 @@ const SKIPPED_TESTS = {
     Scheduler: [
       { demo: 'Overview', themes: [THEME.fluent, THEME.material] },
       { demo: 'Templates', themes: [THEME.fluent, THEME.material] },
-    ],
-    Toolbar: [
-      { demo: 'Adaptability', themes: [THEME.fluent, THEME.material] },
     ],
   },
 };
