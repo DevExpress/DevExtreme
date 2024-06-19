708d19022e0ea098c40cea1dbebd1ae09161cfee
Andrey Dolzhikov <eylau-andrew@mail.ru>
2024-06-19 20:41:45 +0300

    DataGrid - Dragged column from the column chooser is frozen when the Esc key is pressed (T1219785) (#27551)
    Co-authored-by: Alyar <>

--- packages/testcafe-models/dataGrid/columnChooser.ts
+++ packages/testcafe-models/dataGrid/columnChooser.ts
@@ -1,4 +1,4 @@
-import { Selector } from 'testcafe';
+import { ClientFunction, Selector } from 'testcafe';
 // eslint-disable-next-line max-classes-per-file
 import FocusableElement from '../internal/focusable';
 
@@ -7,6 +7,8 @@ const CLASS = {
   overlayWrapper: 'dx-overlay-wrapper',
   columnChooser: 'dx-datagrid-column-chooser',
   checkboxIcon: 'dx-checkbox-icon',
+  treeViewItem: 'dx-treeview-item',
+  treeView: 'dx-treeview',
 };
 
 export default class ColumnChooser extends FocusableElement {
@@ -24,7 +26,25 @@ export default class ColumnChooser extends FocusableElement {
     this.isOpened = this.body.find(`.${CLASS.overlayWrapper}.${CLASS.columnChooser}`).exists;
   }
 
+  async focusList() {
+    const treeView = this.content.find(`.${CLASS.treeView}`);
+
+    await ClientFunction((container) => {
+      const $treeView = $(container());
+
+      $treeView.trigger('focus');
+    }, {
+      dependencies: {
+        treeView,
+      }
+    })(treeView);
+  }
+
   getCheckboxIcon(nth = 0): Selector {
     return this.content.find(`.${CLASS.checkboxIcon}`).nth(nth);
+  }
+
+  getColumn(index = 0): Selector {
+    return this.content.find(`.${CLASS.treeViewItem}`).nth(index);
   }
 }
