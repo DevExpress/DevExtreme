55ae2d4da29a43dd842925797e2984a927ace600
Alyar <Alyar666@users.noreply.github.com>
2024-06-20 17:29:09 +0300

    TestCafe testing: Merge all pages for testing into one page (#27577)
    Co-authored-by: Alyar <>

--- e2e/testcafe-devextreme/tests/form/form.labelMode.ts
+++ e2e/testcafe-devextreme/tests/form/form.labelMode.ts
@@ -4,7 +4,7 @@ import { createWidget } from '../../helpers/createWidget';
 import { isMaterialBased, testScreenshot } from '../../helpers/themeUtils';
 
 fixture.disablePageReloads`Form`
-  .page(url(__dirname, '../containerQuill.html'));
+  .page(url(__dirname, '../container.html'));
 
 ['left', 'right', 'top'].forEach((formLabelLocation) => {
   ['outside', 'floating', 'hidden', 'static'].forEach((formLabelMode) => {
