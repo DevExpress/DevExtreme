e1d72a38029e8276b46db6852d1ccf573b9f8fbd
Joshua Victoria <133762589+jdvictoria@users.noreply.github.com>
2024-06-24 19:37:25 +0800

    Form: Fix form validation error when items are cleared (T1233487) (#27435)

--- packages/devextreme/js/__internal/ui/m_validation_summary.ts
+++ packages/devextreme/js/__internal/ui/m_validation_summary.ts
@@ -37,7 +37,7 @@ const ValidationSummary = CollectionWidget.inherit({
     const $element = this.$element();
     const group = this.option('validationGroup')
             || ValidationEngine.findGroup($element, this._modelByElement($element));
-    const groupConfig = ValidationEngine.addGroup(group);
+    const groupConfig = ValidationEngine.addGroup(group, true);
 
     this._unsubscribeGroup();
 
