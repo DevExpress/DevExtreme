8af98af7e18d8f135f0d0b670a94574346dbf72c
Joshua Victoria <jdvictoria.tech@gmail.com>
2024-06-27 18:46:40 +0800

    revert wrong fix

--- packages/devextreme/js/__internal/ui/selection/m_selection.strategy.ts
+++ packages/devextreme/js/__internal/ui/selection/m_selection.strategy.ts
@@ -5,7 +5,7 @@ import {
   noop,
 } from '@js/core/utils/common';
 import { Deferred } from '@js/core/utils/deferred';
-import { isFunction, isObject, isPlainObject } from '@js/core/utils/type';
+import { isObject, isPlainObject } from '@js/core/utils/type';
 import dataQuery from '@js/data/query';
 
 export default class SelectionStrategy {
@@ -88,9 +88,8 @@ export default class SelectionStrategy {
   }
 
   _loadFilteredData(remoteFilter, localFilter?: any, select?: any, isSelectAll?: boolean) {
-    const keyField = this.options.key();
     const filterLength = encodeURI(JSON.stringify(this._removeTemplateProperty(remoteFilter))).length;
-    const needLoadAllData = (this.options.maxFilterLengthInRequest && (filterLength > this.options.maxFilterLengthInRequest)) || isFunction(keyField);
+    const needLoadAllData = this.options.maxFilterLengthInRequest && (filterLength > this.options.maxFilterLengthInRequest);
     const deferred = Deferred();
     const loadOptions = {
       filter: needLoadAllData ? undefined : remoteFilter,
