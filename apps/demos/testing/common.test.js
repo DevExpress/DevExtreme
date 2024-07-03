dd8d4f01ec4b40544d13ce027d76d93c43807086
ivanblinov2k17 <ivanblinov2015@mail.ru>
2024-07-03 15:16:10 +0400

    Change order change theme and url + fix settings.json

--- apps/demos/testing/common.test.js
+++ apps/demos/testing/common.test.js
@@ -305,12 +305,13 @@ const SKIPPED_TESTS = {
     }
 
     const isGithubDemos = process.env.ISGITHUBDEMOS;
-    let pageURL = `http://127.0.0.1:808${getPortByIndex(index)}/apps/demos/Demos/${widgetName}/${demoName}/${approach}/`;
+    let pageURL = '';
     const theme = process.env.THEME.replace('generic.', '');
     if (isGithubDemos) {
       pageURL = `http://127.0.0.1:808${getPortByIndex(index)}/Demos/${widgetName}/${demoName}/${approach}/?theme=dx.${theme}`;
     } else {
       changeTheme(__dirname, `../${demoPath}/index.html`, process.env.THEME);
+      pageURL = `http://127.0.0.1:808${getPortByIndex(index)}/apps/demos/Demos/${widgetName}/${demoName}/${approach}/`
     }
     // remove when tests enabled not only for datagrid
     if (isGithubDemos && (widgetName !== 'DataGrid' || githubIgnored.includes(demoName))) {
