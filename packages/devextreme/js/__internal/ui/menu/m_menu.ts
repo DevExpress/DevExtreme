74693b115c1fbcc8ff3c70e70ec771601d5841ef
Nikki Gonzales <38495263+nikkithelegendarypokemonster@users.noreply.github.com>
2024-07-04 18:49:53 +0800

    TreeView: Remove modified addContent method that appends <a> tag when url option is passed (T1237378) (#27647)

--- packages/devextreme/js/__internal/ui/menu/m_menu.ts
+++ packages/devextreme/js/__internal/ui/menu/m_menu.ts
@@ -438,6 +438,7 @@ class Menu extends MenuBase {
       selectNodesRecursive: false,
       selectByClick: this.option('selectByClick'),
       expandEvent: 'click',
+      _supportItemUrl: true,
     });
   }
 
