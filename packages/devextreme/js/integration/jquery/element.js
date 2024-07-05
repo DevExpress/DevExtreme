48dea853b2951b43c8952952cbca60993b1eb46a
tongsonbarbs <tongsonbarbs@gmail.com>
2024-07-05 16:31:54 +0800

    remove jQuery condition to run setPublicElementWrapper

--- packages/devextreme/js/integration/jquery/element.js
+++ packages/devextreme/js/integration/jquery/element.js
@@ -1,11 +1,7 @@
 import { setPublicElementWrapper } from '../../core/element';
-import useJQueryFn from './use_jquery';
-const useJQuery = useJQueryFn();
 
 const getPublicElement = function($element) {
     return $element;
 };
 
-if(useJQuery) {
-    setPublicElementWrapper(getPublicElement);
-}
+setPublicElementWrapper(getPublicElement);
