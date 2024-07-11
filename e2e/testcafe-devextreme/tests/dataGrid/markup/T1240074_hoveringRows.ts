2d30bbef0191653c8626cf066e550fd2e89bc556
tongsonbarbs <tongsonbarbs@gmail.com>
2024-07-11 19:36:09 +0800

    T1240074 fix eslint issues

--- e2e/testcafe-devextreme/tests/dataGrid/markup/T1240074_hoveringRows.ts
+++ e2e/testcafe-devextreme/tests/dataGrid/markup/T1240074_hoveringRows.ts
@@ -1,27 +1,27 @@
-import DataGrid from "devextreme-testcafe-models/dataGrid";
-import { createWidget } from "../../../helpers/createWidget";
-import url from "../../../helpers/getPageUrl";
-import { getData } from "../helpers/generateDataSourceData";
+import DataGrid from 'devextreme-testcafe-models/dataGrid';
+import { createWidget } from '../../../helpers/createWidget';
+import url from '../../../helpers/getPageUrl';
+import { getData } from '../helpers/generateDataSourceData';
 
 fixture.disablePageReloads`HoveringRows`.page(
-  url(__dirname, "../../container.html")
+  url(__dirname, '../../container.html'),
 );
 
-test("Hover over a row in DataGrid", async (t) => {
-  const dataGrid = new DataGrid("#container");
+test('Hover over a row in DataGrid', async (t) => {
+  const dataGrid = new DataGrid('#container');
   const firstRow = dataGrid.getDataRow(0);
 
   await t
     .hover(firstRow.element)
-    .expect(firstRow.element.hasClass("dx-state-hover"))
+    .expect(firstRow.element.hasClass('dx-state-hover'))
     .ok();
 }).before(async () => {
   await createWidget(
-    "dxDataGrid",
+    'dxDataGrid',
     {
       dataSource: getData(5, 3),
       hoverStateEnabled: true,
     },
-    "#container"
+    '#container',
   );
 });
