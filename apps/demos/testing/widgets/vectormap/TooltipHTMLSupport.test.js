2f44f6433d0718c13f22f0d88b4dd5e7f5238477
Alexander Bulychev <git@bulychev.pro>
2024-07-11 15:16:55 +0400

    Skip unstable test VectorMap.TooltipHTMLSupport

--- apps/demos/testing/widgets/vectormap/TooltipHTMLSupport.test.js
+++ apps/demos/testing/widgets/vectormap/TooltipHTMLSupport.test.js
@@ -3,13 +3,14 @@ import { Selector as $ } from 'testcafe';
 import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
 import { testScreenshot } from '../../../utils/visual-tests/helpers/theme-utils';
 
-fixture('VectorMap.TooltipHTMLSupport')
+// NOTE: very unstable
+fixture.skip('VectorMap.TooltipHTMLSupport')
   .page('http://localhost:8080/')
   .before(async (ctx) => {
     ctx.initialWindowSize = [900, 700];
   });
 
-runManualTest('VectorMap', 'TooltipHTMLSupport', ['jQuery'/* , 'React', 'Vue', 'Angular' */], (test) => {
+runManualTest('VectorMap', 'TooltipHTMLSupport', ['jQuery', 'React', 'Vue', 'Angular'], (test) => {
   test('Tooltip', async (t) => {
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
