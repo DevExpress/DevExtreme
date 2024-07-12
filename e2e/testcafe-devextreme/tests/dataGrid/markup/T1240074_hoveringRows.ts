3ca2b330cc9ccfc53bf648ac4561088d4a0f1718
tongsonbarbs <tongsonbarbs@gmail.com>
2024-07-12 18:08:48 +0800

    fix eslint

--- e2e/testcafe-devextreme/tests/dataGrid/markup/T1240074_hoveringRows.ts
+++ e2e/testcafe-devextreme/tests/dataGrid/markup/T1240074_hoveringRows.ts
@@ -7,50 +7,50 @@ fixture.disablePageReloads`HoveringRows`.page(
   url(__dirname, '../../container.html'),
 );
 
-test('Hover over rows in the middle', async(t) => {
+test('Hover over rows in the middle', async (t) => {
   const dataGrid = new DataGrid('#container');
   const firstRow = dataGrid.getDataRow(10);
   const secondRow = dataGrid.getDataRow(11);
-  
+
   await t.hover(firstRow.element)
-             .expect(firstRow.isHovered)
-             .ok();
+    .expect(firstRow.isHovered)
+    .ok();
 
   await t.hover(secondRow.element)
-             .expect(firstRow.isHovered)
-             .notOk()
-             .expect(secondRow.isHovered)
-             .ok();
+    .expect(firstRow.isHovered)
+    .notOk()
+    .expect(secondRow.isHovered)
+    .ok();
 }).before(async () => {
   await createWidget(
     'dxDataGrid',
     {
       dataSource: getData(20, 3),
       hoverStateEnabled: true,
-    }
-  )
+    },
+  );
 });
 
 test('Hover over first and last rows', async (t) => {
   const dataGrid = new DataGrid('#container');
   const firstRow = dataGrid.getDataRow(0);
   const lastRow = dataGrid.getDataRow(19);
-  
+
   await t.hover(firstRow.element)
-             .expect(firstRow.isHovered)
-             .ok();
+    .expect(firstRow.isHovered)
+    .ok();
 
   await t.hover(lastRow.element)
-             .expect(firstRow.isHovered)
-             .notOk()
-             .expect(lastRow.isHovered)
-             .ok();
+    .expect(firstRow.isHovered)
+    .notOk()
+    .expect(lastRow.isHovered)
+    .ok();
 }).before(async () => {
   await createWidget(
     'dxDataGrid',
     {
       dataSource: getData(20, 3),
       hoverStateEnabled: true,
-    }
-  )
+    },
+  );
 });
