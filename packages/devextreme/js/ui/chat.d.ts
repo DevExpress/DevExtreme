04abd4152c6065239ee3e85580849fbb74fa0109
marker dao ® <youdontknow@marker-dao.eth>
2024-07-15 19:02:59 +0400

    feat(chat): Prepare types

--- packages/devextreme/js/ui/chat.d.ts
+++ packages/devextreme/js/ui/chat.d.ts
@@ -1,14 +1,37 @@
-import Widget, {
-    WidgetOptions,
-} from './widget/ui.widget';
+import Widget, { WidgetOptions } from './widget/ui.widget';
+import { Cancelable, NativeEventInfo } from '../events/index';
 
 /**
- * @docid _ui_splitter_ResizeStartEvent
+ * @docid _ui_chat_User
  * @public
  * @type object
- * @inherits Cancelable,NativeEventInfo,_ui_splitter_ResizeInfo
  */
-export type MessageSendEvent = any;
+export interface User {
+    id: number;
+    firstName: string;
+    lastName: string;
+    avatarUrl?: string;
+}
+
+/**
+ * @docid _ui_chat_Message
+ * @public
+ * @type object
+ */
+export type Message = {
+    timestamp: Date;
+    author: User;
+    text: string;
+    typing?: boolean;
+};
+
+/**
+ * @docid _ui_chat_MessageSendEvent
+ * @public
+ * @type object
+ * @inherits Cancelable,NativeEventInfo,_ui_chat_Message
+ */
+export type MessageSendEvent = Cancelable & NativeEventInfo<dxChat, KeyboardEvent | PointerEvent | MouseEvent | TouchEvent> & Message;
 
 /**
  * @deprecated use Properties instead
@@ -19,11 +42,11 @@ export type MessageSendEvent = any;
 export interface dxChatOptions extends WidgetOptions<dxChat> {
     /**
      * @docid
-     * @type Array<dxChatItem>
+     * @type Array<Message>
      * @fires dxChatOptions.onOptionChanged
      * @public
      */
-    items?: [];
+    items?: Array<Message>;
     /**
      * @docid
      * @default null
@@ -40,3 +63,6 @@ export interface dxChatOptions extends WidgetOptions<dxChat> {
  * @public
  */
 export default class dxChat extends Widget<dxChatOptions> { }
+
+/** @public */
+export type Properties = dxChatOptions;
--- packages/devextreme/js/ui/chat.js
+++ packages/devextreme/js/ui/chat.js
@@ -1,3 +1,36 @@
 import Chat from '../__internal/ui/chat/chat';
 
 export default Chat;
+
+// STYLE chat
+
+/**
+ * @name dxChatOptions.activeStateEnabled
+ * @hidden
+ */
+
+/**
+ * @name dxChatOptions.focusStateEnabled
+ * @hidden
+ */
+
+/**
+ * @name dxChatOptions.hint
+ * @hidden
+ */
+
+/**
+ * @name dxChatOptions.tabIndex
+ * @hidden
+ */
+
+/**
+ * @name dxChatOptions.accessKey
+ * @hidden
+ */
+
+/**
+ * @name dxChat.focus
+ * @publicName focus()
+ * @hidden
+ */
