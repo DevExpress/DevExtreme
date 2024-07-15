a398ae6eeecf8b1b74a0a261b550ed41c2e7911d
dxArtemiusz <artem.kazakov@devexpress.com>
2024-07-15 23:38:00 +0400

    Update App.js

--- apps/demos/Demos/DataGrid/SimpleArray/ReactJs/App.js
+++ apps/demos/Demos/DataGrid/SimpleArray/ReactJs/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import DataGrid from 'devextreme-react/data-grid';
-import { customers } from './data.js';
+import DataGrid, { Column } from 'devextreme-react/data-grid';
+import { customers } from './data.ts';
 
 const columns = ['CompanyName', 'City', 'State', 'Phone', 'Fax'];
+
 const App = () => (
   <DataGrid
     dataSource={customers}
     keyExpr="ID"
     defaultColumns={columns}
     showBorders={true}
-  />
+  >
+    {columns.map((column, index) => (
+      <Column
+        dataField={column}
+        key={index}
+      />
+    ))}
+  </DataGrid>
 );
+
 export default App;
