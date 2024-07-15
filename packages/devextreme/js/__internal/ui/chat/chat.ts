b0488d1da7bc8f3d3c7661df520a85f9f1cac7d5
marker dao ® <youdontknow@marker-dao.eth>
2024-07-15 14:01:09 +0400

    Chat: Register component

---
+++ packages/devextreme/js/__internal/ui/chat/chat.ts
@@ -0,0 +1,64 @@
+import registerComponent from '@js/core/component_registrator';
+import type { WidgetOptions } from '@js/ui/widget/ui.widget';
+
+import Widget from '../widget';
+
+export interface ChatOptions extends WidgetOptions<Chat> {
+  items: [];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onMessageSend: any;
+}
+
+export interface User {
+  id: string;
+  name: string;
+}
+
+class Chat extends Widget<ChatOptions> {
+  _supportedKeys(): Record<string, (e: KeyboardEvent) => void> {
+    return {
+      ...super._supportedKeys(),
+    };
+  }
+
+  _getDefaultOptions(): ChatOptions {
+    return {
+      ...super._getDefaultOptions(),
+      ...{
+        items: [],
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        onMessageSend: (e: any): any => e,
+      },
+    };
+  }
+
+  _init(): void {
+    super._init();
+  }
+
+  _initMarkup(): void {
+    super._initMarkup();
+  }
+
+  _render(): void {
+    super._render();
+  }
+
+  _clean(): void {
+    super._clean();
+  }
+
+  _optionChanged(args: Record<string, unknown>): void {
+    const { name } = args;
+
+    switch (name) {
+      default:
+        super._optionChanged(args);
+    }
+  }
+}
+
+// @ts-expect-error ts-error
+registerComponent('dxChat', Chat);
+
+export default Chat;
