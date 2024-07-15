0626ff234020b31872c69fde5fb5220e28d3b4b8
Roman Semenov <roman.semenov@devexpress.com>
2024-07-15 20:12:48 +0400

    refactor: replace devextreme-main with devextreme

--- tools/scripts/build-all.ts
+++ tools/scripts/build-all.ts
@@ -53,9 +53,9 @@ if (!devMode) {
 }
 
 if (devMode) {
-    sh.exec('pnpx nx build devextreme-main');
+    sh.exec('pnpx nx build devextreme');
 } else {
-    sh.exec('pnpx nx build-dist devextreme-main --skipNxCache', {
+    sh.exec('pnpx nx build-dist devextreme --skipNxCache', {
         env: {
             ...sh.env,
             BUILD_INTERNAL_PACKAGE: 'false'
@@ -99,7 +99,7 @@ sh.cp(path.join(ROOT_DIR, 'packages', 'devextreme-react', 'npm', '*.tgz'), NPM_D
 sh.cp(path.join(ROOT_DIR, 'packages', 'devextreme-vue', 'npm', '*.tgz'), NPM_DIR);
 
 if (sh.env.BUILD_INTERNAL_PACKAGE === 'true') {
-    sh.exec('pnpx nx build-dist devextreme-main');
+    sh.exec('pnpx nx build-dist devextreme');
 
     sh.pushd(path.join(DEVEXTREME_NPM_DIR, 'devextreme-internal'));
         sh.exec(`pnpm pkg set version="${devextremeNpmVersion}"`);
