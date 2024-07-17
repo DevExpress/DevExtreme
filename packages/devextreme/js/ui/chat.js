2db679069103185e8f2e647edd097bb4cfb123ff
marker dao ® <mark_nikolsky@outlook.com>
2024-07-17 20:05:11 +0400

    Chat: Add doc declarations

--- packages/devextreme/js/ui/chat.js
+++ packages/devextreme/js/ui/chat.js
@@ -1,3 +1,34 @@
 import Chat from '../__internal/ui/chat/chat';
 
 export default Chat;
+
+/**
+ * @name dxChatOptions.accessKey
+ * @hidden
+ */
+
+/**
+ * @name dxChatOptions.activeStateEnabled
+ * @hidden
+ */
+
+/**
+ * @name dxChatOptions.focusStateEnabled
+ * @hidden
+ */
+
+/**
+ * @name dxChatOptions.hint
+ * @hidden
+ */
+
+/**
+ * @name dxChatOptions.onContentReady
+ * @hidden true
+ * @action
+ */
+
+/**
+ * @name dxChatOptions.tabIndex
+ * @hidden
+ */
