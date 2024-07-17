9f0624d5d2b72432e4d5b3d91a6585c581946d03
marker dao ® <youdontknow@marker-dao.eth>
2024-07-17 16:33:44 +0400

    feat(chat): Add messages

--- packages/devextreme/js/__internal/ui/chat/chat_message_list.ts
+++ packages/devextreme/js/__internal/ui/chat/chat_message_list.ts
@@ -4,6 +4,9 @@ import $ from '@js/core/renderer';
 
 const CHAT_MESSAGE_LIST_CLASS = 'dx-chat-message-list';
 const CHAT_MESSAGE_INNER_LIST_CLASS = 'dx-chat-message-inner-list';
+const CHAT_MESSAGE_INNER_LIST_ITEM_CLASS = 'dx-chat-message-inner-list-item';
+const CHAT_MESSAGE_AVATAR_CLASS = 'dx-chat-message-avatar';
+const CHAT_MESSAGE_AVATAR_LETTERS_CLASS = 'dx-chat-message-avatar-letters';
 
 let items = [];
 
@@ -15,9 +18,38 @@ const renderMessageBody = (element) => $('<div>')
   .addClass(CHAT_MESSAGE_LIST_CLASS)
   .appendTo(element);
 
-const renderMessageInnerList = (element) => $('<div>')
-  .addClass(CHAT_MESSAGE_INNER_LIST_CLASS)
-  .appendTo(element);
+const renderAvatar = (item, element) => {
+  const { author } = item;
+
+  const $avatar = $('<div>').addClass(CHAT_MESSAGE_AVATAR_CLASS);
+
+  const initials = `${author.firstName.charAt(0).toUpperCase()}${author.lastName.charAt(0).toUpperCase()}`;
+
+  $('<div>')
+    .addClass(CHAT_MESSAGE_AVATAR_LETTERS_CLASS)
+    .text(initials)
+    .appendTo($avatar);
+
+  $avatar.appendTo(element);
+};
+
+const renderListItem = (item, index, element) => {
+  const $listItem = $('<li>').addClass(CHAT_MESSAGE_INNER_LIST_ITEM_CLASS);
+
+  renderAvatar(item, $listItem);
+
+  $listItem.appendTo(element);
+};
+
+const renderMessageInnerList = (element) => {
+  const $list = $('<ul>').addClass(CHAT_MESSAGE_INNER_LIST_CLASS);
+
+  items.forEach((item, index) => {
+    renderListItem(item, index, $list);
+  });
+
+  $list.appendTo(element);
+};
 
 export const renderMessageList = (element) => {
   const body = renderMessageBody(element);
