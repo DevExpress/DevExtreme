e955436325bb62a36a04f1c19fe50789e6b471aa
marker dao ® <youdontknow@marker-dao.eth>
2024-07-18 11:57:53 +0400

    rebase(24_2)

--- packages/devextreme-vue/src/chat.ts
+++ packages/devextreme-vue/src/chat.ts
@@ -1,20 +1,20 @@
-import Chat, { Properties } from 'devextreme/ui/chat';
-import type * as DxChatTypes from 'devextreme/ui/chat_types';
-import { createComponent, createConfigurationComponent } from './core/index';
+import Chat, { Properties } from "devextreme/ui/chat";
+import { createComponent } from "./core/index";
+import { createConfigurationComponent } from "./core/index";
 
 type AccessibleOptions = Pick<Properties,
-'disabled' |
-'elementAttr' |
-'height' |
-'hoverStateEnabled' |
-'items' |
-'onDisposing' |
-'onInitialized' |
-'onMessageSend' |
-'onOptionChanged' |
-'rtlEnabled' |
-'visible' |
-'width'
+  "disabled" |
+  "elementAttr" |
+  "height" |
+  "hoverStateEnabled" |
+  "items" |
+  "onDisposing" |
+  "onInitialized" |
+  "onMessageSend" |
+  "onOptionChanged" |
+  "rtlEnabled" |
+  "visible" |
+  "width"
 >;
 
 interface DxChat extends AccessibleOptions {
@@ -33,101 +33,39 @@ const DxChat = createComponent({
     onOptionChanged: Function,
     rtlEnabled: Boolean,
     visible: Boolean,
-    width: [Function, Number, String],
+    width: [Function, Number, String]
   },
   emits: {
-    'update:isActive': null,
-    'update:hoveredElement': null,
-    'update:disabled': null,
-    'update:elementAttr': null,
-    'update:height': null,
-    'update:hoverStateEnabled': null,
-    'update:items': null,
-    'update:onDisposing': null,
-    'update:onInitialized': null,
-    'update:onMessageSend': null,
-    'update:onOptionChanged': null,
-    'update:rtlEnabled': null,
-    'update:visible': null,
-    'update:width': null,
+    "update:isActive": null,
+    "update:hoveredElement": null,
+    "update:disabled": null,
+    "update:elementAttr": null,
+    "update:height": null,
+    "update:hoverStateEnabled": null,
+    "update:items": null,
+    "update:onDisposing": null,
+    "update:onInitialized": null,
+    "update:onMessageSend": null,
+    "update:onOptionChanged": null,
+    "update:rtlEnabled": null,
+    "update:visible": null,
+    "update:width": null,
   },
   computed: {
     instance(): Chat {
       return (this as any).$_instance;
-    },
+    }
   },
   beforeCreate() {
-    this.$_WidgetClass = Chat;
-    this.$_hasAsyncTemplate = true;
-    this.$_expectedChildren = {
-      item: { isCollectionItem: true, optionName: 'items' },
+    (this as any).$_WidgetClass = Chat;
+    (this as any).$_hasAsyncTemplate = true;
+    (this as any).$_expectedChildren = {
+      item: { isCollectionItem: true, optionName: "items" }
     };
-  },
-});
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-const DxAuthor = createConfigurationComponent({
-  emits: {
-    'update:isActive': null,
-    'update:hoveredElement': null,
-    'update:avatarUrl': null,
-    'update:firstName': null,
-    'update:id': null,
-    'update:lastName': null,
-  },
-  props: {
-    avatarUrl: String,
-    firstName: String,
-    id: Number,
-    lastName: String,
-  },
-});
-DxAuthor.$_optionName = 'author';
-const DxItem = createConfigurationComponent({
-  emits: {
-    'update:isActive': null,
-    'update:hoveredElement': null,
-    'update:author': null,
-    'update:text': null,
-    'update:timestamp': null,
-    'update:typing': null,
-=======
-const DxItem = createConfigurationComponent({
-  emits: {
-    "update:isActive": null,
-    "update:hoveredElement": null,
-    "update:text": null,
-    "update:timestamp": null,
-    "update:typing": null,
-    "update:user": null,
->>>>>>> 9de0c11d9b (feat(chat): Add default values && Set not required)
-  },
-  props: {
-    text: String,
-    timestamp: String,
-    typing: Boolean,
-<<<<<<< HEAD
-  },
-});
-DxItem.$_optionName = 'items';
-DxItem.$_isCollectionItem = true;
-DxItem.$_expectedChildren = {
-  author: { isCollectionItem: false, optionName: 'author' },
-=======
-    user: Object
   }
 });
-(DxItem as any).$_optionName = "items";
-(DxItem as any).$_isCollectionItem = true;
-(DxItem as any).$_expectedChildren = {
-  user: { isCollectionItem: false, optionName: "user" }
->>>>>>> 9de0c11d9b (feat(chat): Add default values && Set not required)
-};
-const DxUser = createConfigurationComponent({
-=======
+
 const DxAuthor = createConfigurationComponent({
->>>>>>> 6748d12586 (refactor(chat))
   emits: {
     "update:isActive": null,
     "update:hoveredElement": null,
@@ -167,17 +105,8 @@ const DxItem = createConfigurationComponent({
 export default DxChat;
 export {
   DxChat,
-<<<<<<< HEAD
-<<<<<<< HEAD
-  DxAuthor,
-  DxItem,
-=======
-  DxItem,
-  DxUser
->>>>>>> 9de0c11d9b (feat(chat): Add default values && Set not required)
-=======
   DxAuthor,
   DxItem
->>>>>>> 6748d12586 (refactor(chat))
 };
+import type * as DxChatTypes from "devextreme/ui/chat_types";
 export { DxChatTypes };
