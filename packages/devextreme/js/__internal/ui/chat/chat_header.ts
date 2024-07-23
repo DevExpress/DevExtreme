bdba400394473a4e4029f089daf02e1ed38c6ee0
marker dao ® <youdontknow@marker-dao.eth>
2024-07-23 14:32:11 +0400

    revert(chat header): Remove clean

--- packages/devextreme/js/__internal/ui/chat/chat_header.ts
+++ packages/devextreme/js/__internal/ui/chat/chat_header.ts
@@ -53,13 +53,6 @@ class ChatHeader extends Widget<ChatHeaderOptions> {
         super._optionChanged(args);
     }
   }
-
-  _clean(): void {
-    this._$headerText?.remove();
-    this._$headerText = undefined;
-
-    super._clean();
-  }
 }
 
 export default ChatHeader;
