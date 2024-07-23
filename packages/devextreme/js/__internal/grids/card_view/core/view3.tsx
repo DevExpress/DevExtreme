3e1c9fc6e129c72a5c4195882e108a32c6967b74
Roman Semenov <roman.semenov@devexpress.com>
2024-07-23 17:56:46 +0400

    inprogress

---
+++ packages/devextreme/js/__internal/grids/card_view/core/view3.tsx
@@ -0,0 +1,29 @@
+import { Component } from 'inferno';
+
+import { OptionsController } from '../options_controller/options_controller';
+
+interface State {
+  count: number;
+}
+
+export class MyView extends Component<{}, State> {
+  static dependencies = [OptionsController];
+
+  constructor(
+    private readonly options: OptionsController,
+  ) {
+    super();
+
+    this.state = {
+      count: 1,
+    };
+
+    this.options.oneWay('searchText').subscribe(() => {
+      this.setState({ count: 1 });
+    });
+  }
+
+  render() {
+    return <div>{this.state.count}</div>;
+  }
+}
