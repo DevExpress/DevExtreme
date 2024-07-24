97c3b1083815478b00d8e3bf78458ef0ab6091d8
marker dao ® <youdontknow@marker-dao.eth>
2024-07-24 17:01:53 +0400

    feat(chat): Add User

--- packages/devextreme/js/__internal/ui/chat/chat.ts
+++ packages/devextreme/js/__internal/ui/chat/chat.ts
@@ -49,7 +49,7 @@ class Chat extends Widget<Properties> {
   _renderMessageList(): void {
     const { items, user } = this.option();
 
-    const currentUserId = user?.id;
+    const currentUserId = user?.id ?? Math.random();
     const $messageList = $('<div>').appendTo(this.element());
 
     this._messageList = this._createComponent($messageList, MessageList, {
@@ -73,6 +73,9 @@ class Chat extends Widget<Properties> {
         this._chatHeader?.option(name, value);
         break;
       case 'user':
+        // @ts-expect-error
+        this._messageList?.option('currentUserId', value.id);
+        break;
       case 'items':
         // @ts-expect-error
         this._messageList?.option(name, value);
