a676e70ebf9d3ca2857c0c07d1c828fda1796593
tongsonbarbs <tongsonbarbs@gmail.com>
2024-07-25 21:06:52 +0800

    Update testcase to simpler one

--- e2e/testcafe-devextreme/tests/dataGrid/tagBox.ts
+++ e2e/testcafe-devextreme/tests/dataGrid/tagBox.ts
@@ -2,6 +2,7 @@
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import DataGrid from 'devextreme-testcafe-models/dataGrid';
 import url from '../../helpers/getPageUrl';
+import { changeTheme } from '../../helpers/changeTheme';
 import { createWidget } from '../../helpers/createWidget';
 
 fixture.disablePageReloads`Tagbox Columns`.page(
@@ -11,11 +12,7 @@ fixture.disablePageReloads`Tagbox Columns`.page(
 test('Datagrid tagbox column should not look broken', async (t) => {
   const dataGrid = new DataGrid('#container');
   const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
-
-  await t
-    .expect(dataGrid.isReady)
-    .ok();
-
+  await changeTheme('material.blue.light');
   await t
     .click(dataGrid.getDataCell(0, 1).element)
     .expect(await takeScreenshot('T1228720-grid-tagbox-on-edit.png', dataGrid.element))
@@ -31,14 +28,6 @@ test('Datagrid tagbox column should not look broken', async (t) => {
     ],
     columns: ['id', {
       dataField: 'items',
-      lookup: {
-        dataSource: Array.from({ length: 10 }, (_, index) => ({
-          id: index + 1,
-          text: `item ${index + 1}`,
-        })),
-        valueExpr: 'id',
-        displayExpr: 'text',
-      },
       editCellTemplate(container, cellInfo) {
         ($('<div/>') as any).dxTagBox({
           dataSource: Array.from({ length: 10 }, (_, index) => ({
@@ -56,16 +45,6 @@ test('Datagrid tagbox column should not look broken', async (t) => {
           },
           searchEnabled: true,
         }).appendTo(container);
-      },
-      cellTemplate(container, options) {
-        const noBreakSpace = '\u00A0';
-
-        const vals = options.value.map(
-          (itemId) => options.column.lookup!.calculateCellValue!(itemId),
-        );
-        const text = vals.join(', ');
-        // @ts-expect-error text
-        container.text(text || noBreakSpace).attr('title', text);
       },
     }],
     editing: { mode: 'batch', allowUpdating: true },
