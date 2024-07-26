25bbf553c260cb8948b6566b01a3cbc4675e62e5
Vasily Strelyaev <Vasily.Strelyaev@devexpress.com>
2024-07-26 17:42:52 +0300

    remove old fix + add test for SelectBox regression

--- packages/devextreme/js/__internal/ui/drop_down_editor/m_drop_down_editor.ts
+++ packages/devextreme/js/__internal/ui/drop_down_editor/m_drop_down_editor.ts
@@ -344,12 +344,6 @@ const DropDownEditor = TextBox.inherit({
       model: data,
       container: getPublicElement($templateWrapper),
       onRendered: () => {
-        const isRenderedInRoot = !!this.$element().find($templateWrapper).length;
-
-        if (!isRenderedInRoot) {
-          return;
-        }
-
         const $input = this._input();
 
         if (!$input.length) {
