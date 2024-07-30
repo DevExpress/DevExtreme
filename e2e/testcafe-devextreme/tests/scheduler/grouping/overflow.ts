2c9acb2ed1f4c915c8f809d445c93f73e5b6312d
Roman Semenov <roman.semenov@devexpress.com>
2024-07-30 11:19:04 +0400

    Scheduler - Appointments overflow groups in certain cases (T1223907) (#27739)

---
+++ e2e/testcafe-devextreme/tests/scheduler/grouping/overflow.ts
@@ -0,0 +1,81 @@
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import Scheduler from 'devextreme-testcafe-models/scheduler';
+import { createWidget } from '../../../helpers/createWidget';
+import url from '../../../helpers/getPageUrl';
+
+fixture.disablePageReloads`Scheduler: Grouping overflow`
+  .page(url(__dirname, '../../container.html'));
+
+['week', 'month'].forEach((viewType) => {
+  ['vertical', 'horizontal'].forEach((groupOrientation) => {
+    ['hidden', 'allDay'].forEach((allDayPanelMode) => {
+      [[9, 14, 60], [0, 24, 360]].forEach(([startDayHour, endDayHour, cellDuration]) => {
+        const allParams = `${viewType}-${groupOrientation}-${allDayPanelMode}-${startDayHour}-${endDayHour}`;
+
+        test(`Long appointments should not overflow group view (${allParams})`, async (t) => {
+          const scheduler = new Scheduler('#container');
+          const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+          await t
+            .expect(await takeScreenshot(`group-overflow-(${allParams}).png`, scheduler.element))
+            .ok()
+            .expect(compareResults.isValid())
+            .ok(compareResults.errorMessages());
+        }).before(async () => createWidget('dxScheduler', {
+          dataSource: [
+            {
+              text: '1',
+              priorityId: 1,
+              startDate: '2021-04-19T16:30:00',
+              endDate: '2021-04-25T18:30:00',
+            }, {
+              text: '2',
+              priorityId: 2,
+              startDate: '2021-04-19T16:30:00',
+              endDate: '2021-04-25T18:30:00',
+            }, {
+              text: '3',
+              priorityId: 3,
+              startDate: '2021-04-19T16:30:00',
+              endDate: '2021-04-25T18:30:00',
+            },
+          ],
+          views: [{
+            type: viewType,
+            name: 'myView',
+            groupOrientation,
+          }],
+          cellDuration,
+          currentView: 'myView',
+          currentDate: new Date(2021, 3, 21),
+          allDayPanelMode,
+          startDayHour,
+          endDayHour,
+          groups: ['priorityId'],
+          resources: [
+            {
+              fieldExpr: 'priorityId',
+              dataSource: [
+                {
+                  text: 'Low Priority',
+                  id: 1,
+                  color: '#1e90ff',
+                }, {
+                  text: 'High Priority',
+                  id: 2,
+                  color: '#ff9747',
+                },
+                {
+                  text: 'Custom',
+                  id: 3,
+                  color: 'red',
+                },
+              ],
+            },
+          ],
+          showAllDayPanel: false,
+        }));
+      });
+    });
+  });
+});
