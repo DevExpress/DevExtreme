87760bff2f1f9ad225dfbad83710ed727a671798
Andrey Dolzhikov <eylau-andrew@mail.ru>
2024-08-01 18:37:54 +0300

    Pager

---
+++ packages/devextreme/js/__internal/pager/pager.tsx
@@ -0,0 +1,72 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import type { InfernoEffect } from '@devextreme/runtime/inferno';
+import { createReRenderEffect, InfernoWrapperComponent } from '@devextreme/runtime/inferno';
+
+import { combineClasses } from '../core/r1/utils/render_utils';
+import type { InternalPagerProps, PagerProps } from './common/pager_props';
+import { PagerDefaultProps } from './common/pager_props';
+import { PagerContent } from './content';
+import { ResizableContainer } from './resizable_container';
+
+export class Pager extends InfernoWrapperComponent<PagerProps> {
+  public __getterCache = {};
+
+  constructor(props) {
+    super(props);
+    this.pageIndexChange = this.pageIndexChange.bind(this);
+    this.pageSizeChange = this.pageSizeChange.bind(this);
+  }
+
+  createEffects(): InfernoEffect[] {
+    return [createReRenderEffect()];
+  }
+
+  pageIndexChange(newPageIndex: number): void {
+    if (this.props.gridCompatibility) {
+      this.props.pageIndex = newPageIndex + 1;
+    } else {
+      this.props.pageIndex = newPageIndex;
+    }
+  }
+
+  getPageIndex(): number {
+    if (this.props.gridCompatibility) {
+      return this.props.pageIndex - 1;
+    }
+    return this.props.pageIndex;
+  }
+
+  pageSizeChange(newPageSize: number): void {
+    this.props.pageSize = newPageSize;
+  }
+
+  getClassName(): string | undefined {
+    if (this.props.gridCompatibility) {
+      return combineClasses({
+        'dx-datagrid-pager': true,
+        [`${this.props.className}`]: !!this.props.className,
+      });
+    }
+    return this.props.className;
+  }
+
+  getPagerProps(): InternalPagerProps {
+    return {
+      ...this.props,
+      className: this.getClassName(),
+      pageIndex: this.getPageIndex(),
+      pageIndexChange: (pageIndex: number): void => this.pageIndexChange(pageIndex),
+      pageSizeChange: (pageSize: number): void => this.pageSizeChange(pageSize),
+    };
+  }
+
+  render(): JSX.Element {
+    return (
+      <ResizableContainer
+        contentTemplate={PagerContent}
+        pagerProps={this.getPagerProps()}
+      />
+    );
+  }
+}
+Pager.defaultProps = PagerDefaultProps;
---
+++ packages/devextreme/js/__internal/pager/wrappers/pager.ts
@@ -0,0 +1,55 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import registerComponent from '@js/core/component_registrator';
+
+import { Pager as PagerComponent } from '../pager';
+import { GridPagerWrapper } from './grid_pager';
+
+export default class Pager extends GridPagerWrapper {
+  getProps(): Record<string, unknown> {
+    const props = super.getProps();
+    props.onKeyDown = this._wrapKeyDownHandler(props.onKeyDown as any);
+    return props;
+  }
+
+  get _propsInfo(): any {
+    return {
+      twoWay: [],
+      allowNull: [],
+      elements: [],
+      templates: [],
+      props: [
+        'defaultPageSize',
+        'pageSizeChange',
+        'defaultPageIndex',
+        'pageIndexChange',
+        'gridCompatibility',
+        'className',
+        'showInfo',
+        'infoText',
+        'lightModeEnabled',
+        'displayMode',
+        'maxPagesCount',
+        'pageCount',
+        'pagesCountText',
+        'visible',
+        'hasKnownLastPage',
+        'pagesNavigatorVisible',
+        'showPageSizes',
+        'pageSizes',
+        'rtlEnabled',
+        'showNavigationButtons',
+        'totalCount',
+        'label',
+        'onKeyDown',
+        'pageSize',
+        'pageIndex',
+      ],
+    };
+  }
+
+  // @ts-expect-error types error in R1
+  get _viewComponent(): typeof PagerComponent {
+    return PagerComponent;
+  }
+}
+registerComponent('dxPager', Pager);
