e8369d00ad4434e7585f44313f6a4ee1ae858165
Alyar <Alyar666@users.noreply.github.com>
2024-08-06 03:15:19 +0400

    DataGrid/TreeList: Implement the core part of sticky columns (#27862)
    Co-authored-by: Alyar <>

---
+++ packages/devextreme/js/__internal/grids/grid_core/sticky_columns/const.ts
@@ -0,0 +1,12 @@
+export enum StickyPosition {
+  Left = 'left',
+  Right = 'right',
+}
+
+export const CLASSES = {
+  leftStickyColumn: 'left-sticky-column',
+  rightStickyColumn: 'right-sticky-column',
+  firstRightStickyColumn: 'first-right-sticky-column',
+  lastLeftStickyColumn: 'last-left-sticky-column',
+  stickyColumns: 'sticky-columns',
+};
