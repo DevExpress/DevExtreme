cc46ece70d4a757bdb2b437f32bfa5c4fb007071
Alyar <Alyar666@users.noreply.github.com>
2024-08-06 03:15:19 +0400

    DataGrid/TreeList: Implement the core part of sticky columns (#27862)
    Co-authored-by: Alyar <>

---
+++ packages/devextreme/js/__internal/grids/data_grid/module_not_extended/sticky_columns.ts
@@ -0,0 +1,5 @@
+import { stickyColumnsModule } from '@ts/grids/grid_core/sticky_columns/m_sticky_columns';
+
+import gridCore from '../m_core';
+
+gridCore.registerModule('stickyColumns', stickyColumnsModule);
