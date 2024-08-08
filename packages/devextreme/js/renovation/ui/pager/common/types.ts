bd4b2fa19d2c58aae61395c2b344cb486d732466
Andrey Dolzhikov <eylau-andrew@mail.ru>
2024-08-08 13:02:23 +0300

    Remove Pager renovation

--- packages/devextreme/js/renovation/ui/pager/common/types.ts
+++
@@ -1,1 +0,0 @@
-export interface FullPageSize { text: string; value: number }
--- packages/devextreme/js/renovation/ui/pager/utils/calculate_values_fitted_width.ts
+++
@@ -1,4 +0,0 @@
-export const oneDigitWidth = 10;
-export function calculateValuesFittedWidth(minWidth: number, values: number[]): number {
-  return minWidth + oneDigitWidth * Math.max(...values).toString().length;
-}
