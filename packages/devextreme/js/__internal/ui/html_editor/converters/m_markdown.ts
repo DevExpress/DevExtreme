7fcb2537d4ce52181c49d086e709126fb57c8574
marker dao ® <youdontknow@marker-dao.eth>
2024-08-12 22:16:58 +0400

    Spike md --> html

--- packages/devextreme/js/__internal/ui/html_editor/converters/m_markdown.ts
+++ packages/devextreme/js/__internal/ui/html_editor/converters/m_markdown.ts
@@ -2,14 +2,23 @@
 /* eslint-disable spellcheck/spell-checker */
 import { getWindow } from '@js/core/utils/window';
 import Errors from '@js/ui/widget/ui.errors';
+// import rehypeParse from 'rehype-parse';
+import rehypeRaw from 'rehype-raw';
+// import rehypeRemark from 'rehype-remark';
+import rehypeSanitize from 'rehype-sanitize';
 import rehypeStringify from 'rehype-stringify';
+import remarkGfm from 'remark-gfm';
+// import remarkHtml from 'remark-html';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
-// import ShowDown from 'showdown';
+// import remarkStringify from 'remark-stringify';
+import ShowDown from 'showdown';
 import TurnDown from 'turndown';
 import { unified } from 'unified';
 
 import converterController from '../m_converterController';
+
+const withUnified = true;
 
 class MarkdownConverter {
   _markdown2Html: any;
@@ -21,18 +30,20 @@ class MarkdownConverter {
     // @ts-expect-error
     const turndown = window && window.TurndownService || TurnDown;
 
-    // const showdown = window && window.showdown || ShowDown;
+    // @ts-expect-error
+    const showdown = window && window.showdown || ShowDown;
 
     if (!turndown) {
       throw Errors.Error('E1041', 'Turndown');
     }
 
-    // if (!showdown) {
-    //   throw Errors.Error('E1041', 'Unified');
-    // }
+    if (!showdown) {
+      throw Errors.Error('E1041', 'Unified');
+    }
 
     // eslint-disable-next-line new-cap
     this._html2Markdown = new turndown();
+    // this._html2Markdown = unified;
 
     if (this._html2Markdown?.addRule) {
       this._html2Markdown.addRule('emptyLine', {
@@ -44,27 +55,65 @@ class MarkdownConverter {
       this._html2Markdown.keep(['table']);
     }
 
-    // this._markdown2Html = new showdown.Converter({
-    //   simpleLineBreaks: true,
-    //   strikethrough: true,
-    //   tables: true,
-    // });
-
-    this._markdown2Html = unified;
+    if (withUnified) {
+      this._markdown2Html = unified;
+    } else {
+      this._markdown2Html = new showdown.Converter({
+        simpleLineBreaks: true,
+        strikethrough: true,
+        tables: true,
+      });
+    }
   }
 
   toMarkdown(htmlMarkup) {
-    return this._html2Markdown.turndown(htmlMarkup || '');
+    const value = this._html2Markdown.turndown(htmlMarkup || '');
+
+    return value;
   }
 
+  // toMarkdown(htmlMarkup) {
+  //   const markup = this._html2Markdown()
+  //     .use(rehypeParse, { fragment: true })
+  //     .use(rehypeRemark)
+  //     .use(remarkGfm)
+  //     .use(remarkStringify)
+  //     .processSync(htmlMarkup);
+
+  //   debugger;
+
+  //   return markup.value;
+  // }
+
   toHtml(markdownMarkup) {
-    let markup = this._markdown2Html
+    if (withUnified) {
+      return this.withUnified(markdownMarkup);
+    }
+
+    return this.withShowdown(markdownMarkup);
+  }
+
+  withUnified(markdownMarkup) {
+    const obj = this._markdown2Html()
       .use(remarkParse)
-      // eslint-disable-next-line spellcheck/spell-checker
-      .use(remarkRehype)
-      // eslint-disable-next-line spellcheck/spell-checker
+      .use(remarkGfm)
+      .use(remarkRehype, { allowDangerousHtml: true })
+      .use(rehypeRaw)
+      .use(rehypeSanitize)
       .use(rehypeStringify)
       .processSync(markdownMarkup);
+
+    let markup = String(obj);
+
+    if (markup) {
+      markup = markup.replace(new RegExp('\\r?\\n', 'g'), '');
+    }
+
+    return markup;
+  }
+
+  withShowdown(markdownMarkup) {
+    let markup = this._markdown2Html.makeHtml(markdownMarkup);
 
     if (markup) {
       markup = markup.replace(new RegExp('\\r?\\n', 'g'), '');
