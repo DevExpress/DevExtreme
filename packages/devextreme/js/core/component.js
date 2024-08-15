0193d7e7d0e95c092b136dcf8c43f9c0f3040e02
Roman Semenov <roman.semenov@devexpress.com>
2024-08-15 18:36:24 +0400

    Refactor - base native classes - fix errors

---
+++ packages/devextreme/js/core/component.js
@@ -0,0 +1,8 @@
+export * from '../__internal/core/widget/component';
+
+/**
+ * @name Component.ctor
+ * @publicName ctor(options)
+ * @param1 options:ComponentOptions|undefined
+ * @hidden
+ */
---
+++ packages/devextreme/js/ui/widget/ui.widget.js
@@ -0,0 +1,27 @@
+import Widget from '../../__internal/core/widget/widget';
+export default Widget;
+
+/**
+ * @section Utils
+ * @type function
+ * @default null
+ * @type_function_param1 e:object
+ * @type_function_param1_field1 component:this
+ * @type_function_param1_field2 element:DxElement
+ * @type_function_param1_field3 model:object
+ * @name WidgetOptions.onFocusIn
+ * @action
+ * @hidden
+ */
+/**
+ * @section Utils
+ * @type function
+ * @default null
+ * @type_function_param1 e:object
+ * @type_function_param1_field1 component:this
+ * @type_function_param1_field2 element:DxElement
+ * @type_function_param1_field3 model:object
+ * @name WidgetOptions.onFocusOut
+ * @action
+ * @hidden
+ */
