824f73af08894937eb3488084620392c6eb5d519
tongsonbarbs <tongsonbarbs@gmail.com>
2024-08-16 14:38:09 +0800

    revert test

--- e2e/testcafe-devextreme/tests/dataGrid/sorting/sorting.ts
+++ e2e/testcafe-devextreme/tests/dataGrid/sorting/sorting.ts
@@ -18,18 +18,40 @@ test('Filter expression should be valid when sortingMethod, remoteOperations, an
     .expect(dataGrid.getErrorRow().exists)
     .eql(false);
 }).before(async () => createWidget('dxDataGrid', () => {
+  const sampleData = Array.from({ length: 20 }, (_, i) => ({
+    ID: i + 1,
+    Name: `Name ${i + 1}`,
+  }));
+  const sampleAPI = new (window as any).DevExpress.data.ArrayStore(
+    sampleData,
+  );
   const store = new (window as any).DevExpress.data.CustomStore({
     key: 'ID',
     load(o) {
       if (o.filter && typeof o.filter[0] === 'function') {
         return Promise.reject();
       }
+      return Promise.all([sampleAPI.load(o), sampleAPI.totalCount(o)]).then(
+        (res) => ({
+          data: res[0],
+          totalCount: res[1],
+        }),
+      );
     },
   });
   return {
     dataSource: store,
     remoteOperations: true,
+    columns: [
+      'ID',
+      {
+        dataField: 'Name',
+        sortOrder: 'asc',
+        sortingMethod() {
+          return 1;
+        },
       },
+    ],
     paging: { pageSize: 5 },
     scrolling: { mode: 'virtual' },
     height: 200,
