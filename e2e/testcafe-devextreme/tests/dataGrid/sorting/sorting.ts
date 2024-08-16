84eebb1e92d15a3e0ca409e394156b311f5a2342
tongsonbarbs <tongsonbarbs@gmail.com>
2024-08-16 14:42:32 +0800

    revert previous test

--- e2e/testcafe-devextreme/tests/dataGrid/sorting/sorting.ts
+++ e2e/testcafe-devextreme/tests/dataGrid/sorting/sorting.ts
@@ -5,9 +5,8 @@ import url from '../../../helpers/getPageUrl';
 import { changeTheme } from '../../../helpers/changeTheme';
 import { Themes } from '../../../helpers/themes';
 
-fixture.disablePageReloads`Sorting`.page(
-  url(__dirname, '../../container.html'),
-);
+fixture.disablePageReloads`Sorting`
+  .page(url(__dirname, '../../container.html'));
 
 test('Filter expression should be valid when sortingMethod, remoteOperations, and autoNavigateToFocusedRow are specified (T1200546)', async (t) => {
   const dataGrid = new DataGrid('#container');
@@ -18,40 +17,30 @@ test('Filter expression should be valid when sortingMethod, remoteOperations, an
     .expect(dataGrid.getErrorRow().exists)
     .eql(false);
 }).before(async () => createWidget('dxDataGrid', () => {
-  const sampleData = Array.from({ length: 20 }, (_, i) => ({
-    ID: i + 1,
-    Name: `Name ${i + 1}`,
-  }));
-  const sampleAPI = new (window as any).DevExpress.data.ArrayStore(
-    sampleData,
-  );
+  const sampleData = Array.from({ length: 20 }, (_, i) => ({ ID: i + 1, Name: `Name ${i + 1}` }));
+  const sampleAPI = new (window as any).DevExpress.data.ArrayStore(sampleData);
   const store = new (window as any).DevExpress.data.CustomStore({
     key: 'ID',
     load(o) {
       if (o.filter && typeof o.filter[0] === 'function') {
         return Promise.reject();
       }
-      return Promise.all([sampleAPI.load(o), sampleAPI.totalCount(o)]).then(
-        (res) => ({
-          data: res[0],
-          totalCount: res[1],
-        }),
-      );
+      return Promise.all([sampleAPI.load(o), sampleAPI.totalCount(o)]).then((res) => ({
+        data: res[0],
+        totalCount: res[1],
+      }));
     },
   });
   return {
     dataSource: store,
     remoteOperations: true,
-    columns: [
-      'ID',
-      {
-        dataField: 'Name',
-        sortOrder: 'asc',
-        sortingMethod() {
-          return 1;
-        },
+    columns: ['ID', {
+      dataField: 'Name',
+      sortOrder: 'asc',
+      sortingMethod() {
+        return 1;
       },
-    ],
+    }],
     paging: { pageSize: 5 },
     scrolling: { mode: 'virtual' },
     height: 200,
