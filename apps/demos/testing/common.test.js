81979ea919dbfc9cc1cfad0ce4120a98d521c368
Andrey Dolzhikov <eylau-andrew@mail.ru>
2024-08-19 16:07:39 +0300

    Remove exceptions

--- apps/demos/testing/common.test.js
+++ apps/demos/testing/common.test.js
@@ -76,7 +76,6 @@ const SKIPPED_TESTS = {
       { demo: 'CellEditingAndEditingAPI', themes: [THEME.material] },
       { demo: 'MultipleRecordSelectionAPI', themes: [THEME.material] },
       { demo: 'RemoteGrouping', themes: [THEME.generic] },
-      { demo: 'RightToLeftSupport', themes: [THEME.generic, THEME.material, THEME.fluent] },
     ],
     Charts: [
       { demo: 'Overview', themes: [THEME.material] },
@@ -144,7 +143,6 @@ const SKIPPED_TESTS = {
       { demo: 'ToolbarCustomization', themes: [THEME.fluent, THEME.material] },
       { demo: 'MultipleRecordSelectionAPI', themes: [THEME.material] },
       { demo: 'CellEditingAndEditingAPI', themes: [THEME.material] },
-      { demo: 'RightToLeftSupport', themes: [THEME.generic, THEME.material, THEME.fluent] },
     ],
     Gantt: [
       { demo: 'Validation', themes: [THEME.generic, THEME.material, THEME.fluent] },
@@ -207,7 +205,6 @@ const SKIPPED_TESTS = {
       { demo: 'CellEditingAndEditingAPI', themes: [THEME.material] },
       { demo: 'PopupEditing', themes: [THEME.generic] },
       { demo: 'RecordPaging', themes: [THEME.generic] },
-      { demo: 'RightToLeftSupport', themes: [THEME.generic, THEME.material, THEME.fluent] },
     ],
     FieldSet: [
       { demo: 'Overview', themes: [THEME.fluent] },
