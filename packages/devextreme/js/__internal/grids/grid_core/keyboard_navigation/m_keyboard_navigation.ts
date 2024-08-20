642b864faf375f95e91f6ab0cd75852aafd21673
Joshua Barbosa <44610149+tongsonbarbs@users.noreply.github.com>
2024-08-20 18:20:57 +0800

    Grids a11y: TreeList - Keyboard navigation on Expand/Collapse buttons is broken if the mouse used before (T1234949) (#27933)

--- packages/devextreme/js/__internal/grids/grid_core/keyboard_navigation/m_keyboard_navigation.ts
+++ packages/devextreme/js/__internal/grids/grid_core/keyboard_navigation/m_keyboard_navigation.ts
@@ -1656,7 +1656,7 @@ export class KeyboardNavigationController extends modules.ViewController {
   public _resetFocusedCell(preventScroll?) {
     const $cell = this._getFocusedCell();
 
-    isElementDefined($cell) && $cell.removeAttr('tabindex');
+    isElementDefined($cell) && $cell.removeAttr('tabindex').removeClass(CELL_FOCUS_DISABLED_CLASS);
     this._isNeedFocus = false;
     this._isNeedScroll = false;
     this._focusedCellPosition = {};
