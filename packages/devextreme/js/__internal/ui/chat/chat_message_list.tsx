ee48f1104b1f69c8fd3ae5ccfcd26bb63ac8bf68
Roman Semenov <roman.semenov@devexpress.com>
2024-08-23 05:36:18 +0400

    refactor message list

--- packages/devextreme/js/__internal/ui/chat/chat_message_list.tsx
+++ packages/devextreme/js/__internal/ui/chat/chat_message_list.tsx
@@ -1,56 +1,59 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
-import type { dxElementWrapper } from '@js/core/renderer';
-import $ from '@js/core/renderer';
 import { hasWindow } from '@js/core/utils/window';
-import type { Message, User } from '@js/ui/chat';
+import type { Message } from '@js/ui/chat';
 import type dxScrollable from '@js/ui/scroll_view/ui.scrollable';
 import Scrollable from '@js/ui/scroll_view/ui.scrollable';
-import type { WidgetOptions } from '@js/ui/widget/ui.widget';
+import type { InfernoNode } from 'inferno';
+import { Component, createPortal, createRef } from 'inferno';
 
-import Widget from '../widget';
 import MessageGroup from './chat_message_group';
 
 const CHAT_MESSAGE_LIST_CLASS = 'dx-chat-message-list';
 const CHAT_MESSAGE_LIST_CONTENT_CLASS = 'dx-chat-message-list-content';
 
-export interface MessageListOptions extends WidgetOptions<MessageList> {
+export interface MessageListOptions {
   items?: Message[];
   currentUserId?: number | string;
 }
 
-class MessageList extends Widget<MessageListOptions> {
-  _messageGroups?: MessageGroup[];
+class MessageList extends Component<MessageListOptions> {
+  scrollableRef = createRef<HTMLDivElement>();
 
-  private _$content?: dxElementWrapper;
+  scrollableContentRef = createRef<HTMLDivElement>();
 
-  private _scrollable?: dxScrollable<unknown>;
+  lastMessageGroupRef = createRef<HTMLDivElement>();
 
-  _getDefaultOptions(): MessageListOptions {
-    return {
-      ...super._getDefaultOptions(),
-      items: [],
-      currentUserId: undefined,
-    };
+  private _scrollable!: dxScrollable<unknown>;
+
+  render(): InfernoNode {
+    return (
+      <div className={CHAT_MESSAGE_LIST_CLASS}>
+        <div ref={this.scrollableRef}></div>
+        {this.scrollableContentRef.current && createPortal(
+          this._renderMessageListContent(),
+          this.scrollableContentRef.current,
+        )}
+      </div>
+    );
   }
 
-  _init(): void {
-    super._init();
-
-    this._messageGroups = [];
+  componentDidMount(): void {
+    this._scrollable = new Scrollable<unknown>(this.scrollableRef.current!, { useNative: true });
+    // @ts-expect-error
+    this.scrollableContentRef.current = this._scrollable.$content().get(0);
+    // just for triggering re-render
+    this.setState({
+      scrollableRendered: true,
+    });
   }
 
-  _initMarkup(): void {
-    $(this.element()).addClass(CHAT_MESSAGE_LIST_CLASS);
-
-    super._initMarkup();
-
-    this._renderScrollable();
-    this._renderMessageListContent();
+  componentDidUpdate(): void {
     this._scrollContentToLastMessageGroup();
   }
 
   _isCurrentUser(id): boolean {
-    const { currentUserId } = this.option();
+    const { currentUserId } = this.props;
 
     return currentUserId === id;
   }
@@ -59,111 +62,66 @@ class MessageList extends Widget<MessageListOptions> {
     return this._isCurrentUser(id) ? 'end' : 'start';
   }
 
-  _createMessageGroupComponent(items, userId): void {
-    if (!this._$content) {
-      return;
-    }
-
-    const $messageGroup = $('<div>').appendTo(this._$content);
-
-    const options = {
-      items,
-      alignment: this._messageGroupAlignment(userId),
-    };
-
-    const messageGroup = this._createComponent($messageGroup, MessageGroup, options);
-
-    this._messageGroups?.push(messageGroup);
+  _createMessageGroupComponent(items, userId, isLast = false): InfernoNode {
+    return (
+      <MessageGroup
+        items={items}
+        alignment={this._messageGroupAlignment(userId)}
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        innerRef={isLast ? this.lastMessageGroupRef as any : undefined}
+      />
+    );
   }
 
-  _renderScrollable(): void {
-    this._scrollable = this._createComponent('<div>', Scrollable, { useNative: true });
-    this.$element().append(this._scrollable.$element());
-  }
+  _renderMessageListContent(): InfernoNode {
+    const { items } = this.props;
 
-  _renderMessageListContent(): void {
-    const { items } = this.option();
+    const groupItems: InfernoNode[] = [];
 
-    this._$content = $('<div>')
-      .addClass(CHAT_MESSAGE_LIST_CONTENT_CLASS)
-      // @ts-expect-error
-      .appendTo(this._scrollable?.$content());
-
-    if (!items?.length) {
-      return;
-    }
-
-    let currentMessageGroupUserId = items[0]?.author?.id;
+    let currentMessageGroupUserId = items?.[0]?.author?.id;
     let currentMessageGroupItems: Message[] = [];
 
-    items.forEach((item, index) => {
+    (items ?? []).forEach((item, index) => {
       const id = item?.author?.id;
 
       if (id === currentMessageGroupUserId) {
         currentMessageGroupItems.push(item);
       } else {
-        this._createMessageGroupComponent(currentMessageGroupItems, currentMessageGroupUserId);
+        groupItems.push(
+          this._createMessageGroupComponent(currentMessageGroupItems, currentMessageGroupUserId),
+        );
 
         currentMessageGroupUserId = id;
         currentMessageGroupItems = [];
         currentMessageGroupItems.push(item);
       }
 
-      if (items.length - 1 === index) {
-        this._createMessageGroupComponent(currentMessageGroupItems, currentMessageGroupUserId);
+      if (items!.length - 1 === index) {
+        groupItems.push(
+          this._createMessageGroupComponent(
+            currentMessageGroupItems,
+            currentMessageGroupUserId,
+            true,
+          ),
+        );
       }
     });
-  }
 
-  _renderMessage(message: Message, newItems: Message[], sender: User): void {
-    this._setOptionWithoutOptionChange('items', newItems);
-
-    const lastMessageGroup = this._messageGroups?.[this._messageGroups.length - 1];
-
-    if (lastMessageGroup) {
-      const lastMessageGroupUserId = lastMessageGroup.option('items')[0].author?.id;
-
-      if (sender.id === lastMessageGroupUserId) {
-        lastMessageGroup._renderMessage(message);
-
-        this._scrollContentToLastMessageGroup();
-
-        return;
-      }
-    }
-
-    this._createMessageGroupComponent([message], sender.id);
-    this._scrollContentToLastMessageGroup();
+    return (
+      <div className={CHAT_MESSAGE_LIST_CONTENT_CLASS}>
+        {groupItems}
+      </div>
+    );
   }
 
   _scrollContentToLastMessageGroup(): void {
-    if (!(this._messageGroups?.length && this._scrollable && hasWindow())) {
+    if (!(this.lastMessageGroupRef.current && this._scrollable && hasWindow())) {
       return;
     }
 
-    const lastMessageGroup = this._messageGroups[this._messageGroups.length - 1];
-    const element = lastMessageGroup.$element()[0];
+    const lastMessageGroup = this.lastMessageGroupRef.current;
 
-    this._scrollable.scrollToElement(element);
-  }
-
-  _clean(): void {
-    this._messageGroups = [];
-
-    super._clean();
-  }
-
-  _optionChanged(args: Record<string, unknown>): void {
-    const { name } = args;
-
-    switch (name) {
-      case 'items':
-      case 'currentUserId':
-        this._invalidate();
-        break;
-      default:
-        super._optionChanged(args);
-    }
+    this._scrollable.scrollToElement(lastMessageGroup);
   }
 }
 
