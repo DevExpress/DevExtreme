72e9cc42e0a041bf5856e50867a6d90cac83c6dd
Roman Semenov <roman.semenov@devexpress.com>
2024-08-29 12:32:21 +0400

    figma vars

--- packages/devextreme-scss/gulpfile.js
+++ packages/devextreme-scss/gulpfile.js
@@ -5,6 +5,9 @@ const gulp = require('gulp');
 const env = require('../devextreme/build/gulp/env-variables');
 const cache = require('gulp-cache');
 const shell = require('gulp-shell');
+const replace = require('gulp-replace');
+const through = require('through2');
+const { getFigmaVarValue, setFigmaVarAlias } = require('./generate-figma');
 
 gulp.task('clean', function(callback) {
     require('del').sync([
@@ -35,3 +38,28 @@ gulp.task('default', createStyleCompilerBatch());
 gulp.task('watch', gulp.series(
     'style-compiler-themes-watch'
 ));
+
+gulp.task('generate', gulp.series(
+    () => gulp
+        .src('scss/widgets/**/*.scss')
+        .pipe(through.obj(function(file, encoding, callback) {
+            const content = file.contents.toString(encoding);
+            const iter = content.matchAll(/(\s*\/\/ getFigmaVariable\('(.+?)', *'(.+?)', *'(.+?)' *\)\n\s*(\$.+?): *)(.+?)( !default;)/g);
+            for (let [match, beginning,  name, collection, mode, scssName, scssValue, end] of iter) {
+                setFigmaVarAlias(name, scssName, collection);
+            }
+            callback(null, file);
+        })),
+    () => gulp
+        .src('scss/widgets/**/*.scss')
+        .pipe(through.obj(function(file, encoding, callback) {
+            const content = file.contents.toString(encoding);
+            const newContent = content.replace(/(\s*\/\/ getFigmaVariable\('(.+?)', *'(.+?)', *'(.+?)' *\)\n\s*(\$.+?): *)(.+?)( !default;)/g, function(match, beginning,  name, collection, mode, scssName, scssValue, end) {
+                return beginning + getFigmaVarValue(name, collection, mode) + end;
+            })
+            file.contents = Buffer.from(newContent)
+            callback(null, file);
+        }))
+        .pipe(gulp.dest('scss/widgets')),
+
+))
