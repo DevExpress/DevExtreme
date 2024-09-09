36896be2c92aec755d018493642b302f6da0525f
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-09-09 14:27:02 +0400

    Chat: implementation empty list view with design

--- packages/devextreme/js/__internal/ui/chat/chat_message_list.ts
+++ packages/devextreme/js/__internal/ui/chat/chat_message_list.ts
@@ -1,6 +1,7 @@
 import type { dxElementWrapper } from '@js/core/renderer';
 import $ from '@js/core/renderer';
 import { hasWindow } from '@js/core/utils/window';
+import messageLocalization from '@js/localization/message';
 import type { Message } from '@js/ui/chat';
 import Scrollable from '@js/ui/scroll_view/ui.scrollable';
 import type { WidgetOptions } from '@js/ui/widget/ui.widget';
@@ -11,6 +12,13 @@ import type { MessageGroupAlignment } from './chat_message_group';
 import MessageGroup from './chat_message_group';
 
 const CHAT_MESSAGE_LIST_CLASS = 'dx-chat-message-list';
+const CHAT_MESSAGE_LIST_EMPTY = 'dx-chat-message-list-empty';
+
+const CHAT_MESSAGE_LIST_EMPTY_CLASS = 'dx-chat-message-list-empty';
+const CHAT_MESSAGE_LIST_EMPTY_VIEW_CLASS = 'dx-chat-empty-view';
+const CHAT_MESSAGE_LIST_EMPTY_IMAGE_CLASS = 'dx-chat-empty-image';
+const CHAT_MESSAGE_LIST_EMPTY_MESSAGE_CLASS = 'dx-chat-empty-message';
+const CHAT_MESSAGE_LIST_EMPTY_PROMPT_CLASS = 'dx-chat-empty-prompt';
 
 export interface Properties extends WidgetOptions<MessageList> {
   items: Message[];
@@ -21,6 +29,8 @@ class MessageList extends Widget<Properties> {
   _messageGroups?: MessageGroup[];
 
   private _$content!: dxElementWrapper;
+
+  private _$emptyView?: dxElementWrapper;
 
   private _scrollable?: Scrollable<unknown>;
 
@@ -43,10 +53,49 @@ class MessageList extends Widget<Properties> {
 
     super._initMarkup();
 
-    this._renderMessageListContent();
     this._renderScrollable();
 
+    this._renderMessageListContent();
+
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    this._scrollable?.update();
+
     this._scrollContentToLastMessageGroup();
+  }
+
+  _renderEmptyViewContent(): void {
+    this.$element().addClass(CHAT_MESSAGE_LIST_EMPTY_CLASS);
+
+    this._$emptyView = $('<div>')
+      .addClass(CHAT_MESSAGE_LIST_EMPTY_VIEW_CLASS);
+
+    $('<div>')
+      .appendTo(this._$emptyView)
+      .addClass(CHAT_MESSAGE_LIST_EMPTY_IMAGE_CLASS);
+
+    const messageText = messageLocalization.format('dxChat-emptyListMessage');
+    $('<div>')
+      .appendTo(this._$emptyView)
+      .addClass(CHAT_MESSAGE_LIST_EMPTY_MESSAGE_CLASS)
+      .text(messageText);
+
+    const promptText = messageLocalization.format('dxChat-emptyListPrompt');
+    $('<div>')
+      .appendTo(this._$emptyView)
+      .addClass(CHAT_MESSAGE_LIST_EMPTY_PROMPT_CLASS)
+      .text(promptText);
+
+    this._$emptyView.appendTo(this._$content);
+  }
+
+  _removeEmptyViewElement(): void {
+    this._$emptyView?.remove();
+  }
+
+  _isEmpty(): boolean {
+    const { items } = this.option();
+
+    return items.length === 0;
   }
 
   _isCurrentUser(id: string | number | undefined): boolean {
@@ -60,8 +109,8 @@ class MessageList extends Widget<Properties> {
   }
 
   _createMessageGroupComponent(items: Message[], userId: string | number | undefined): void {
-    const $messageGroupContainer = this._scrollable ? this._scrollable.content() : this._$content;
-    const $messageGroup = $('<div>').appendTo($messageGroupContainer);
+    // const $messageGroupContainer = this._scrollable ? this._scrollable.content() : this._$content;
+    const $messageGroup = $('<div>').appendTo(this._$content);
 
     const messageGroup = this._createComponent($messageGroup, MessageGroup, {
       items,
@@ -72,20 +121,24 @@ class MessageList extends Widget<Properties> {
   }
 
   _renderScrollable(): void {
-    this._scrollable = this._createComponent(this._$content, Scrollable, {
+    const $scrollable = $('<div>')
+      .appendTo(this.$element());
+
+    this._scrollable = this._createComponent($scrollable, Scrollable, {
       useNative: true,
     });
+
+    this._$content = $(this._scrollable.content());
   }
 
   _renderMessageListContent(): void {
-    const { items } = this.option();
+    if (this._isEmpty()) {
+      this._renderEmptyViewContent();
 
-    this._$content = $('<div>')
-      .appendTo(this.$element());
-
-    if (!items?.length) {
       return;
     }
+
+    const { items } = this.option();
 
     let currentMessageGroupUserId = items[0]?.author?.id;
     let currentMessageGroupItems: Message[] = [];
@@ -113,6 +166,10 @@ class MessageList extends Widget<Properties> {
 
   _renderMessage(message: Message, newItems: Message[]): void {
     const sender = message.author;
+
+    if (this.$element().find('.dx-chat-empty-view').length > 0) {
+      this._removeEmptyViewElement();
+    }
 
     this._setOptionWithoutOptionChange('items', newItems);
 
@@ -179,6 +236,8 @@ class MessageList extends Widget<Properties> {
     if (shouldItemsBeUpdatedCompletely) {
       this._invalidate();
     } else {
+      this._removeEmptyViewElement();
+
       const newMessage = value[value.length - 1];
 
       this._renderMessage(newMessage ?? {}, value);
