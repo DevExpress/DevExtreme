b09a1f130e04dcaead5d6b3a83496ba341d6792b
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2024-09-11 17:03:36 +0400

    refactor devextreme-vue for TS in IDE
    refactor

---
+++ packages/devextreme-vue/src/core/index.ts
@@ -0,0 +1,9 @@
+import * as vue3Stategy from './strategy/vue3/index';
+
+const strategy = vue3Stategy;
+
+export const {
+    prepareComponentConfig,
+    prepareConfigurationComponentConfig,
+    prepareExtensionComponentConfig
+} = strategy;
