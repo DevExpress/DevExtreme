adbf760d44aca7732f41fa46404c14a4e8f018f0
Andrey Dolzhikov <eylau-andrew@mail.ru>
2024-09-12 16:29:10 +0300

    add tests

--- e2e/testcafe-devextreme/tests/pager/baseProperties.ts
+++ e2e/testcafe-devextreme/tests/pager/baseProperties.ts
@@ -1,4 +1,5 @@
 import Pager from 'devextreme-testcafe-models/pager';
+import { ClientFunction } from 'testcafe';
 import url from '../../helpers/getPageUrl';
 import { createWidget } from '../../helpers/createWidget';
 
@@ -36,4 +37,53 @@ test('Pager elementAttr property', async (t) => {
     'data-test': 'custom data',
     id: 'bestPager',
   },
+}));
+
+test('Pager hint, disabled and accessKey properties', async (t) => {
+  const pager = new Pager('#container');
+  await t
+    .expect(pager.element.getAttribute('aria-disabled'))
+    .eql('true')
+    .expect(pager.element.hasClass('dx-state-disabled'))
+    .ok()
+    .expect(pager.element.getAttribute('accesskey'))
+    .eql('F')
+    .expect(pager.element.getAttribute('title'))
+    .eql('Best Pager');
+}).before(async () => createWidget('dxPager', {
+  hint: 'Best Pager',
+  disabled: true,
+  accessKey: 'F',
+}));
+
+test('Pager tabindex and state properties', async (t) => {
+  const pager = new Pager('#container');
+  await t
+    .expect(pager.element.getAttribute('tabindex'))
+    .eql('7')
+
+    .click(pager.getNavPage('3').element)
+    .expect(pager.element.hasClass('dx-state-focused'))
+    .ok()
+    .expect(pager.element.hasClass('dx-state-hover'))
+    .ok()
+    .expect(pager.element.hasClass('dx-state-active'))
+    .notOk();
+
+  await ClientFunction((_pager) => {
+    const $root = $(_pager.element());
+
+    $root.trigger($.Event('dxpointerdown', {
+      pointers: [{ pointerId: 1 }],
+    }));
+  })(pager);
+
+  await t
+    .expect(pager.element.hasClass('dx-state-active'))
+    .ok();
+}).before(async () => createWidget('dxPager', {
+  tabIndex: 7,
+  focusStateEnabled: true,
+  hoverStateEnabled: true,
+  activeStateEnabled: true,
 }));
