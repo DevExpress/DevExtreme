b9c3bf3aec4d24ea15f32fae1b9cd43233b8cb20
Andrey Dolzhikov <eylau-andrew@mail.ru>
2024-09-16 17:31:30 +0300

    🎹 Pager: Support Widget class properties (focus(), disabled, focusStateEnabled, activeStateEnabled, hoverStateEnabled) (#28034)

--- e2e/testcafe-devextreme/tests/pager/baseProperties.ts
+++ e2e/testcafe-devextreme/tests/pager/baseProperties.ts
@@ -1,8 +1,9 @@
 import Pager from 'devextreme-testcafe-models/pager';
+import { ClientFunction } from 'testcafe';
 import url from '../../helpers/getPageUrl';
 import { createWidget } from '../../helpers/createWidget';
 
-fixture.disablePageReloads`Pager Base Properties`
+fixture`Pager Base Properties`
   .page(url(__dirname, '../container.html'));
 
 test('Pager width and height property', async (t) => {
@@ -22,18 +23,100 @@ test('Pager width and height property', async (t) => {
 }));
 
 test('Pager elementAttr property', async (t) => {
-  const pager = new Pager('#bestPager');
+  const pager = new Pager('#container');
   await t
     .expect(pager.element.getAttribute('aria-label'))
     .eql('some description')
     .expect(pager.element.getAttribute('data-test'))
-    .eql('custom data')
-    .expect(pager.element.getAttribute('id'))
-    .eql('bestPager');
+    .eql('custom data');
 }).before(async () => createWidget('dxPager', {
   elementAttr: {
     'aria-label': 'some description',
     'data-test': 'custom data',
-    id: 'bestPager',
   },
+}));
+
+test('Pager hint, disabled and accessKey properties', async (t) => {
+  const pager = new Pager('#container');
+  await t
+    .expect(pager.element.getAttribute('aria-disabled'))
+    .eql('true')
+    .expect(pager.element.hasClass('dx-state-disabled'))
+    .ok()
+    .expect(pager.element.getAttribute('accesskey'))
+    .eql('F')
+    .expect(pager.element.getAttribute('title'))
+    .eql('Best Pager');
+}).before(async () => createWidget('dxPager', {
+  hint: 'Best Pager',
+  disabled: true,
+  accessKey: 'F',
+}));
+
+test('Pager tabindex and state properties', async (t) => {
+  const pager = new Pager('#container');
+  await t
+    .expect(pager.element.getAttribute('tabindex'))
+    .eql('7')
+
+    .click(pager.getNavPage('3').element)
+    .expect(pager.element.hasClass('dx-state-focused'))
+    .ok()
+    .expect(pager.element.hasClass('dx-state-hover'))
+    .ok()
+    .expect(pager.element.hasClass('dx-state-active'))
+    .notOk();
+
+  await ClientFunction((_pager) => {
+    const $root = $(_pager.element());
+
+    $root.trigger($.Event('dxpointerdown', {
+      pointers: [{ pointerId: 1 }],
+    }));
+  })(pager);
+
+  await t
+    .expect(pager.element.hasClass('dx-state-active'))
+    .ok();
+}).before(async () => createWidget('dxPager', {
+  disabled: false,
+  width: '100%',
+  focusStateEnabled: true,
+  hoverStateEnabled: true,
+  activeStateEnabled: true,
+  tabIndex: 7,
+}));
+
+test('Pager focus method without focusStateEnabled', async (t) => {
+  const pager = new Pager('#container');
+  await t
+    .expect(pager.getPageSize(0).element.focused)
+    .notOk();
+
+  await ClientFunction((_pager) => {
+    _pager.getInstance().focus();
+  })(pager);
+
+  await t
+    .expect(pager.getPageSize(0).element.focused)
+    .ok();
+}).before(async () => createWidget('dxPager', {
+  focusStateEnabled: false,
+}));
+
+test('Pager focus method with focusStateEnabled', async (t) => {
+  const pager = new Pager('#container');
+  await t
+    .expect(pager.element.focused)
+    .notOk();
+
+  await ClientFunction((_pager) => {
+    _pager.getInstance().focus();
+  })(pager);
+
+  await t
+    .expect(pager.element.focused)
+    .ok();
+}).before(async () => createWidget('dxPager', {
+  focusStateEnabled: true,
 }));
