a5978510fc981ca43e00faafd32f1fbcca0c3195
Andrey Dolzhikov <eylau-andrew@mail.ru>
2024-09-18 14:22:44 +0300

    Option validation spike

--- packages/devextreme/js/__internal/pager/wrappers/grid_pager.ts
+++ packages/devextreme/js/__internal/pager/wrappers/grid_pager.ts
@@ -23,4 +23,78 @@ export class GridPagerWrapper extends ComponentWrapper {
     }
     super._optionChanged(args);
   }
+
+  _validateOptions(options: Record<string, unknown>): Record<string, unknown> {
+    const initialOptions = super._validateOptions(options);
+    let {
+      pageSize,
+      pageIndex,
+      totalCount,
+      // eslint-disable-next-line prefer-const
+      ...rest
+    } = initialOptions;
+
+    if (pageSize === undefined) {
+      pageSize = this.option('pageSize') as number;
+    }
+
+    if (pageIndex === undefined) {
+      pageIndex = this.option('pageIndex') as number;
+    }
+
+    if (totalCount === undefined) {
+      totalCount = this.option('totalCount') as number;
+    }
+
+    const validatedOptions = this._validateOptionsCore(
+      pageSize as number,
+      pageIndex as number,
+      totalCount as number,
+    );
+
+    return {
+      ...rest,
+      ...validatedOptions,
+    };
+  }
+
+  _validateOptionsCore(
+    pageSize: number,
+    oldPageIndex: number,
+    oldItemCount: number,
+  ): Record<string, number> {
+    const itemCount = this.getItemCount(oldItemCount);
+    const pageCount = this.getPageCount(pageSize, oldItemCount);
+    const pageIndex = this.getPageIndex(oldPageIndex, pageSize, itemCount);
+
+    return {
+      pageSize,
+      pageIndex,
+      totalCount: itemCount,
+      pageCount,
+    };
+  }
+
+  private getItemCount(itemCount: number): number {
+    if (itemCount < 1) {
+      return 1;
+    }
+    return itemCount;
+  }
+
+  private getPageCount(pageSize: number, itemCount: number): number {
+    if (pageSize > 0 && itemCount > 0) {
+      return Math.max(1, Math.ceil(itemCount / pageSize));
+    }
+    return 1;
+  }
+
+  private getPageIndex(pageIndex: number, pageSize: number, itemCount: number): number {
+    if (pageIndex < 1) {
+      return 1;
+    }
+
+    const pageCount = this.getPageCount(pageSize, itemCount);
+    return Math.min(pageIndex, pageCount);
+  }
 }
