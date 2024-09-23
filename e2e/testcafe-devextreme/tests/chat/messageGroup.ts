a94d438f367e55b39c2ef3827b28c7443ebbc0e5
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-09-23 11:03:21 +0400

    Chat: Change the maxHeight of the TextArea and limit the Chat's minimum dimensions (#28082)

--- e2e/testcafe-devextreme/tests/chat/messageGroup.ts
+++ e2e/testcafe-devextreme/tests/chat/messageGroup.ts
@@ -111,7 +111,7 @@ test('Messagegroup scenarios in disabled state', async (t) => {
   const userFirst = createUser(1, 'First');
   const userSecond = createUser(2, 'Second');
 
-  await insertStylesheetRulesToPage('#container { display: flex; flex-wrap: wrap; gap: 20px; padding: 20px; }');
+  await insertStylesheetRulesToPage('#container { display: flex; flex-wrap: wrap; gap: 2px; padding: 20px; }');
 
   await asyncForEach([1, 2, 3, 4], async (bubbleCount, idx) => {
     const chatId = `#chat_${idx}`;
@@ -141,7 +141,7 @@ test('Messagegroup scenarios in RTL mode', async (t) => {
   const userFirst = createUser(1, 'First');
   const userSecond = createUser(2, 'Second');
 
-  await insertStylesheetRulesToPage('#container { display: flex; flex-wrap: wrap; gap: 20px; padding: 20px; }');
+  await insertStylesheetRulesToPage('#container { display: flex; flex-wrap: wrap; gap: 2px; padding: 20px; }');
 
   await asyncForEach([1, 2, 3, 4], async (bubbleCount, idx) => {
     const chatId = `#chat_${idx}`;
