6e75c291db25668668e89cfe6514ef1e954ac5f9
Andrey Dolzhikov <eylau-andrew@mail.ru>
2024-09-26 17:13:35 +0300

    test

--- e2e/testcafe-devextreme/tests/pager/accessibility.ts
+++ e2e/testcafe-devextreme/tests/pager/accessibility.ts
@@ -53,7 +53,7 @@ const created = async (t: TestController, optionConfiguration): Promise<void> =>
 };
 
 const a11yCheckConfig = {
-  // NOTE: color-contrast issues
+  // NOTE:  color-contrast issues
   rules: { 'color-contrast': { enabled: false } },
 };
 
