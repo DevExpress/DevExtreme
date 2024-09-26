67c428f470f04453031f77263ee81bbcdf52cf72
marker dao ® <youdontknow@marker-dao.eth>
2024-09-26 23:35:49 +0200

    fix

--- packages/devextreme/testing/tests/DevExpress.utils/utils.date.tests.js
+++ packages/devextreme/testing/tests/DevExpress.utils/utils.date.tests.js
@@ -4,7 +4,6 @@ const { test, module } = QUnit;
 module('getRangesByDates', {}, () => {
     test('getRangesByDates should generate correctly ranges', function(assert) {
         const dates = [
-            new Date(1716745334957), // May 26 2024
             '2024-11-27T13:27:01',
             '2024-10-27T16:54:10',
             '2024-09-20T16:54:48',
@@ -18,9 +17,6 @@ module('getRangesByDates', {}, () => {
 
         const result = dateUtils.getRangesByDates(dates);
         const expectedResult = [
-            [
-                new Date('2024-05-26T00:00:00'),
-            ],
             [
                 new Date('2024-09-20T00:00:00'),
                 new Date('2024-09-21T00:00:00'),
