779ebc8d86572ccfd38c2a2d05de713a16d35f26
Vasily Strelyaev <VasilyStrelyaev@users.noreply.github.com>
2024-09-30 15:10:34 +0300

    fix: Angular throws an error when displaying the Trial Panel in SSR mode (T1248015) (#28112)

--- packages/devextreme/js/__internal/core/license/license_validation.server.test.ts
+++ packages/devextreme/js/__internal/core/license/license_validation.server.test.ts
@@ -18,4 +18,19 @@ describe('license token', () => {
   test('API inside trial_panel should not be triggered on the server', () => {
     expect(() => validateLicense('', '1.0.4')).not.toThrow();
   });
+
+  test('API inside trial_panel should not be triggered in Angular, where HTMLElement is mocked', () => {
+    if (global.HTMLElement) {
+      throw Error('Wrong environment for this test!');
+    }
+
+    try {
+      // @ts-expect-error mocking HTMLElement with a symbol
+      global.HTMLElement = Symbol('HTMLElement mock');
+      expect(() => validateLicense('', '1.0.4')).not.toThrow();
+    } finally {
+      // @ts-expect-error removing the mock
+      delete global.HTMLElement;
+    }
+  });
 });
