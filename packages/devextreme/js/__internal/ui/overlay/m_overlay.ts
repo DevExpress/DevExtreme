a6ac4ee04df6f44e005edec1adde549739677b4f
marker dao ® <youdontknow@marker-dao.eth>
2024-10-03 15:48:46 +0200

    refactor

--- packages/devextreme/js/__internal/ui/overlay/m_overlay.ts
+++ packages/devextreme/js/__internal/ui/overlay/m_overlay.ts
@@ -716,13 +716,6 @@ const Overlay: typeof OverlayInstance = Widget.inherit({
   },
 
   _tabKeyHandler(e) {
-    /**
-     * 0. Завести BC + написать пост + позвать PM
-     * 1. Приватная пропа _loopFocus в Overlay
-     * 2. Для lookup эта опция всегда в true, если usePopover: false
-     * 3. Если usePopover: true, то используем _popupTabHandler уровня ddEditor
-     * 4. Меняем дефолт у hideOnOutsideClick: true
-     */
     // debugger
     if (normalizeKeyName(e) !== TAB_KEY || !this._isTopOverlay()) {
       return;
