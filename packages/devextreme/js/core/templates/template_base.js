bb475cd43fb838cbaea413a9b8a48871c76721dd
ivanblinov2k17 <ivanblinov2015@mail.ru>
2024-10-07 20:20:15 +0400

    Core: add missing template reexport

--- packages/devextreme/js/core/templates/template_base.js
+++ packages/devextreme/js/core/templates/template_base.js
@@ -1,2 +1,2 @@
 // deprecated
-export { TemplateBase } from '../../__internal/core/templates/m_template_base';
+export { TemplateBase, renderedCallbacks } from '../../__internal/core/templates/m_template_base';
