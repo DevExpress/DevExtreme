91e1fd46110582d406f65b64f2a9f7d0ee1db159
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-10-09 17:21:00 +0400

    set disableNativeAutomation to true

--- e2e/testcafe-devextreme/runner.js
+++ e2e/testcafe-devextreme/runner.js
@@ -158,7 +158,7 @@ function setShadowDom(args) {
 function expandBrowserAlias(browser) {
     switch(browser) {
         case 'chrome:devextreme-shr2':
-            return 'chrome:headless --disable-gpu --window-size=1200,800';
+            return 'chrome --headless=old --disable-gpu --window-size=1200,800';
         case 'chrome:docker':
             return 'chromium:headless --no-sandbox --disable-gpu --window-size=1200,800';
     }
