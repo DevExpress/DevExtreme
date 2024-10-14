b2e2f4f93062069e394ccbc8c48b623f75e3a8ad
Roman Semenov <roman.semenov@devexpress.com>
2024-10-14 15:45:52 +0400

    toolbar

--- packages/devextreme/js/__internal/grids/new/card_view/content_view/content/card/card.tsx
+++ packages/devextreme/js/__internal/grids/new/card_view/content_view/content/card/card.tsx
@@ -1,6 +1,7 @@
 import $ from '@js/core/renderer';
 import type { DataRow } from '@ts/grids/new/grid_core/columns_controller/types';
 import { PureComponent } from '@ts/grids/new/grid_core/core/pure_component';
+import { Toolbar } from '@ts/grids/new/grid_core/inferno_wrappers/toolbar';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { type ComponentType, type InfernoNode, render } from 'inferno';
 
@@ -24,6 +25,44 @@ export class Card extends PureComponent<CardProps> {
     const FieldTemplate = this.props.fieldTemplate ?? Field;
     return (
       <div className={CLASSES.card} tabIndex={0}>
+        <Toolbar
+          items={[
+            {
+              location: 'before',
+              widget: 'dxCheckBox',
+            },
+            {
+              location: 'before',
+              template: (model, _index, container): void => {
+                render(<span>Card Header</span>, $(container).get(0));
+              },
+            },
+            {
+              location: 'after',
+              widget: 'dxButton',
+              options: {
+                icon: 'save',
+                stylingMode: 'text',
+              },
+            },
+            {
+              location: 'after',
+              widget: 'dxButton',
+              options: {
+                icon: 'edit',
+                stylingMode: 'text',
+              },
+            },
+            {
+              location: 'after',
+              widget: 'dxButton',
+              options: {
+                icon: 'remove',
+                stylingMode: 'text',
+              },
+            },
+          ]}
+        />
         {this.props.row.cells.map((cell, index) => (
           <FieldTemplate
             index={index}
