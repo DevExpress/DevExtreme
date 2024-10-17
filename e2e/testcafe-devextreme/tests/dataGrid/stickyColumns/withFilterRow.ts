8164d520968b7b7ae438c90d60073d18b9b5b7e8
Alyar <>
2024-10-17 22:13:16 +0400

    Fix background for fixed cells in filter row (for fluent and material themes)

--- e2e/testcafe-devextreme/tests/dataGrid/stickyColumns/withFilterRow.ts
+++ e2e/testcafe-devextreme/tests/dataGrid/stickyColumns/withFilterRow.ts
@@ -4,30 +4,41 @@ import { safeSizeTest } from '../../../helpers/safeSizeTest';
 import { createWidget } from '../../../helpers/createWidget';
 import url from '../../../helpers/getPageUrl';
 import { defaultConfig } from './data';
+import { changeTheme } from '../../../helpers/changeTheme';
+import { Themes } from '../../../helpers/themes';
 
 const DATA_GRID_SELECTOR = '#container';
 
 fixture.disablePageReloads`Sticky columns - Filter row`
   .page(url(__dirname, '../../container.html'));
 
-safeSizeTest('Filter row with sticky columns', async (t) => {
-  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
-  const dataGrid = new DataGrid(DATA_GRID_SELECTOR);
+[Themes.genericLight, Themes.materialBlue, Themes.fluentBlue].forEach((theme) => {
+  safeSizeTest(`Filter row with sticky columns (${theme} theme)`, async (t) => {
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+    const dataGrid = new DataGrid(DATA_GRID_SELECTOR);
 
-  await t.click(dataGrid.getHeaders().getFilterRow().getFilterCell(1).element);
+    await t.click(dataGrid.getHeaders().getFilterRow().getFilterCell(1).element);
 
-  await takeScreenshot('filter_row_with_sticky_columns_1.png', dataGrid.element);
+    await takeScreenshot(`filter_row_with_sticky_columns_1_(${theme}).png`, dataGrid.element);
 
-  await dataGrid.scrollTo(t, { x: 10000 });
+    await dataGrid.scrollTo(t, { x: 10000 });
 
-  await takeScreenshot('filter_row_with_sticky_columns_2.png', dataGrid.element);
+    await takeScreenshot(`filter_row_with_sticky_columns_2_(${theme}).png`, dataGrid.element);
 
-  await t
-    .expect(compareResults.isValid())
-    .ok(compareResults.errorMessages());
-}, [800, 800]).before(async () => createWidget('dxDataGrid', {
-  ...defaultConfig,
-  filterRow: {
-    visible: true,
-  },
-}));
+    await t
+      .expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
+  }, [800, 800])
+    .before(async () => {
+      await changeTheme(theme);
+      await createWidget('dxDataGrid', {
+        ...defaultConfig,
+        filterRow: {
+          visible: true,
+        },
+      });
+    })
+    .after(async () => {
+      await changeTheme(Themes.genericLight);
+    });
+});
