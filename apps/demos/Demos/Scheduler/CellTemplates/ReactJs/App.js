88e385afa49bbd6a979a6c3490af5055a4d4b039
tongsonbarbs <tongsonbarbs@gmail.com>
2024-10-21 15:30:11 +0800

    final

--- apps/demos/Demos/Scheduler/CellTemplates/ReactJs/App.js
+++ apps/demos/Demos/Scheduler/CellTemplates/ReactJs/App.js
@@ -24,7 +24,7 @@ const ariaDescription = () => {
       day: 'numeric',
     });
   })
-  .filter(dateText => dateText);
+  .filter((dateText) => dateText);
   if (disabledDates?.length > 0) {
     return disabledDates.map((dateText) => `${dateText} is a disabled date`).join('. ');
   }
