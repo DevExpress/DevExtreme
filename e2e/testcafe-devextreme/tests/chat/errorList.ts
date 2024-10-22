9c2ed32a37966cd51360136006332783c741f201
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2024-10-22 19:42:50 +0400

    fix spaces

--- e2e/testcafe-devextreme/tests/chat/errorList.ts
+++ e2e/testcafe-devextreme/tests/chat/errorList.ts
@@ -9,83 +9,82 @@ import { getFullThemeName, testScreenshot } from '../../helpers/themeUtils';
 fixture.disablePageReloads`ChatErrorList`
   .page(url(__dirname, '../container.html'));
 
-test
-  .clientScripts([
-    { module: 'mockdate' },
-    { content: 'window.MockDate = MockDate;' },
-  ])('Errorlist appearance', async (t) => {
-    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
-    const chat = new Chat('#container');
+test.clientScripts([
+  { module: 'mockdate' },
+  { content: 'window.MockDate = MockDate;' },
+])('Errorlist appearance', async (t) => {
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+  const chat = new Chat('#container');
 
-    await testScreenshot(t, takeScreenshot, 'Errorlist with one error.png', { element: '#container' });
+  await testScreenshot(t, takeScreenshot, 'Errorlist with one error.png', { element: '#container' });
 
-    await chat.option('errors', [
-      { id: 1, message: 'Error Message 1. Error Description...' },
-      { id: 2, message: 'Error Message 2. Message was not sent' },
-      { id: 3, message: 'Error Message 3. An unexpected issue occurred while processing your request. Please check your internet connection or contact support for further assistance.' },
-    ]);
+  await chat.option('errors', [
+    { id: 1, message: 'Error Message 1. Error Description...' },
+    { id: 2, message: 'Error Message 2. Message was not sent' },
+    { id: 3, message: 'Error Message 3. An unexpected issue occurred while processing your request. Please check your internet connection or contact support for further assistance.' },
+  ]);
 
-    await testScreenshot(t, takeScreenshot, 'Errorlist with long text in error.png', {
-      element: '#container',
-      shouldTestInCompact: true,
-      compactCallBack: async () => {
-        await chat.repaint();
-      },
-    });
-
-    const darkTheme = getFullThemeName().replace('light', 'dark');
-    await testScreenshot(t, takeScreenshot, 'Errorlist with long text in error.png', { element: '#container', theme: darkTheme });
-
-    await chat.option('rtlEnabled', true);
-
-    await testScreenshot(t, takeScreenshot, 'Errorlist appearance in RTL mode.png', { element: '#container' });
-
-    await t
-      .expect(compareResults.isValid())
-      .ok(compareResults.errorMessages());
-  }).before(async () => {
-    await ClientFunction(() => {
-      (window as any).MockDate.set('2024/10/18');
-    })();
-
-    const userFirst = createUser(1, 'First');
-    const userSecond = createUser(2, 'Second');
-    const msInDay = 86400000;
-    const today = new Date('2024/10/18').setHours(7, 22, 0, 0);
-    const yesterday = today - msInDay;
-
-    const items = [{
-      timestamp: yesterday,
-      author: userSecond,
-      text: 'Message text 1',
-    }, {
-      timestamp: yesterday,
-      author: userSecond,
-      text: 'Message text 2',
-    }, {
-      timestamp: today,
-      author: userFirst,
-      text: 'Message text 3',
-    }, {
-      timestamp: today,
-      author: userFirst,
-      text: 'Message text 4',
-    }, {
-      timestamp: today,
-      author: userFirst,
-      text: 'Message text 5',
-    }];
-
-    return createWidget('dxChat', {
-      items,
-      user: userFirst,
-      width: 400,
-      height: 600,
-      errors: [{ id: 1, message: 'Error Message 1. Error Description...' }],
-    });
-    // eslint-disable-next-line @typescript-eslint/require-await
-  }).after(async () => {
-    await ClientFunction(() => {
-      (window as any).MockDate.reset();
-    })();
+  await testScreenshot(t, takeScreenshot, 'Errorlist with long text in error.png', {
+    element: '#container',
+    shouldTestInCompact: true,
+    compactCallBack: async () => {
+      await chat.repaint();
+    },
   });
+
+  const darkTheme = getFullThemeName().replace('light', 'dark');
+  await testScreenshot(t, takeScreenshot, 'Errorlist with long text in error.png', { element: '#container', theme: darkTheme });
+
+  await chat.option('rtlEnabled', true);
+
+  await testScreenshot(t, takeScreenshot, 'Errorlist appearance in RTL mode.png', { element: '#container' });
+
+  await t
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => {
+  await ClientFunction(() => {
+    (window as any).MockDate.set('2024/10/18');
+  })();
+
+  const userFirst = createUser(1, 'First');
+  const userSecond = createUser(2, 'Second');
+  const msInDay = 86400000;
+  const today = new Date('2024/10/18').setHours(7, 22, 0, 0);
+  const yesterday = today - msInDay;
+
+  const items = [{
+    timestamp: yesterday,
+    author: userSecond,
+    text: 'Message text 1',
+  }, {
+    timestamp: yesterday,
+    author: userSecond,
+    text: 'Message text 2',
+  }, {
+    timestamp: today,
+    author: userFirst,
+    text: 'Message text 3',
+  }, {
+    timestamp: today,
+    author: userFirst,
+    text: 'Message text 4',
+  }, {
+    timestamp: today,
+    author: userFirst,
+    text: 'Message text 5',
+  }];
+
+  return createWidget('dxChat', {
+    items,
+    user: userFirst,
+    width: 400,
+    height: 600,
+    errors: [{ id: 1, message: 'Error Message 1. Error Description...' }],
+  });
+  // eslint-disable-next-line @typescript-eslint/require-await
+}).after(async () => {
+  await ClientFunction(() => {
+    (window as any).MockDate.reset();
+  })();
+});
