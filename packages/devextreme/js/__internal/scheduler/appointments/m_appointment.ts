5e5423bc7a9cf5e554e17cf007678afd0d2ab11c
Roman Semenov <roman.semenov@devexpress.com>
2024-10-23 13:15:09 +0400

    fix: remove title duplication

--- packages/devextreme/js/__internal/scheduler/appointments/m_appointment.ts
+++ packages/devextreme/js/__internal/scheduler/appointments/m_appointment.ts
@@ -150,10 +150,8 @@ export class Appointment extends DOMComponent {
     this._renderDragSourceClass();
     this._renderDirection();
 
-    const text = ExpressionUtils.getField(this.option('dataAccessors'), 'text', this.rawAppointment);
-    (this.$element() as any).attr('title', text);
     (this.$element() as any).data('dxAppointmentStartDate', this.option('startDate'));
-    (this.$element() as any).attr('role', 'application');
+    (this.$element() as any).attr('role', 'button');
 
     this._renderRecurrenceClass();
     this._renderResizable();
