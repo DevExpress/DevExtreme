902f19cba6f916edc8e1080960a160da318cf3a0
Roman Semenov <roman.semenov@devexpress.com>
2024-10-29 01:32:04 +0400

    check box icon props

--- packages/devextreme/js/__internal/ui/check_box/check_box_icon.tsx
+++ packages/devextreme/js/__internal/ui/check_box/check_box_icon.tsx
@@ -1,11 +1,13 @@
 import _extends from "@babel/runtime/helpers/esm/extends";
 import _objectWithoutPropertiesLoose from "@babel/runtime/helpers/esm/objectWithoutPropertiesLoose";
 const _excluded = ["size"];
-import { createVNode } from "inferno";
+import { createVNode, RefObject } from "inferno";
+import { createRef as infernoCreateRef } from 'inferno';
 import { BaseInfernoComponent } from '@devextreme/runtime/inferno';
 import { normalizeStyles } from '@devextreme/runtime/inferno';
 import { normalizeStyleProp } from '@js/core/utils/style';
 import '@js/ui/themes';
+
 export const viewFunction = viewModel => {
   const {
     cssStyles,
@@ -15,15 +17,25 @@ export const viewFunction = viewModel => {
     "style": normalizeStyles(cssStyles)
   }, null, elementRef);
 };
-export const CheckBoxIconProps = {};
-import { createRef as infernoCreateRef } from 'inferno';
+
+export interface CheckBoxIconProps {
+  size?: number | string;
+}
+
+export const defaultCheckBoxIconProps = {};
+
 export class CheckBoxIcon extends BaseInfernoComponent {
+  elementRef!: RefObject<HTMLDivElement>;
+
+  private __getterCache: any;
+
   constructor(props) {
     super(props);
     this.state = {};
     this.elementRef = infernoCreateRef();
     this.__getterCache = {};
   }
+
   get cssStyles() {
     if (this.__getterCache['cssStyles'] !== undefined) {
       return this.__getterCache['cssStyles'];
@@ -38,16 +50,19 @@ export class CheckBoxIcon extends BaseInfernoComponent {
       };
     })();
   }
+
   get restAttributes() {
     const _this$props = this.props,
       restProps = _objectWithoutPropertiesLoose(_this$props, _excluded);
     return restProps;
   }
+
   componentWillUpdate(nextProps, nextState, context) {
     if (this.props['size'] !== nextProps['size']) {
       this.__getterCache['cssStyles'] = undefined;
     }
   }
+
   render() {
     const props = this.props;
     return viewFunction({
@@ -58,4 +73,5 @@ export class CheckBoxIcon extends BaseInfernoComponent {
     });
   }
 }
-CheckBoxIcon.defaultProps = CheckBoxIconProps;
+
+CheckBoxIcon.defaultProps = defaultCheckBoxIconProps;
