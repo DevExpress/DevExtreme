1e098083be98f0a63e2580294beacad4180d9834
Roman Semenov <roman.semenov@devexpress.com>
2024-10-29 01:29:46 +0400

    validation message - fix eslint

--- packages/devextreme/js/__internal/ui/check_box/wrappers/validation_message.tsx
+++ packages/devextreme/js/__internal/ui/check_box/wrappers/validation_message.tsx
@@ -1,7 +1,8 @@
 import { BaseInfernoComponent } from '@devextreme/runtime/inferno';
 import LegacyValidationMessage from '@js/ui/validation_message';
+import type { BaseWidgetProps } from '@ts/core/r1/base_props';
+import { BaseWidgetDefaultProps } from '@ts/core/r1/base_props';
 import { DomComponentWrapper } from '@ts/core/r1/dom_component_wrapper';
-import { BaseWidgetProps, BaseWidgetDefaultProps } from '@ts/core/r1/base_props';
 
 export interface ValidationMessageProps extends BaseWidgetProps {
   mode?: 'auto' | 'always';
@@ -27,28 +28,28 @@ export const defaultValidationMessageProps = {
   positionSide: 'top',
   offset: Object.freeze({
     h: 0,
-    v: 0
+    v: 0,
   }),
-  isReactComponentWrapper: true
-}
+  isReactComponentWrapper: true,
+};
 
 export class ValidationMessage extends BaseInfernoComponent<ValidationMessageProps> {
-  constructor(props) {
+  constructor(props: ValidationMessageProps) {
     super(props);
     this.state = {};
   }
-  get componentProps() {
-    return this.props;
-  }
-  get restAttributes() {
+
+  get restAttributes(): Record<string, unknown> {
     const {
+      // eslint-disable-next-line max-len
       accessKey, activeStateEnabled, boundary, className, contentId, disabled, focusStateEnabled, height, hint, hoverStateEnabled, mode, offset, onClick, onKeyDown, positionSide, rtlEnabled, tabIndex, target, validationErrors, visible, visualContainer, width,
       ...restProps
     } = this.props;
 
     return restProps;
   }
-  render() {
+
+  render(): JSX.Element {
     return (
       <DomComponentWrapper
         componentType={LegacyValidationMessage}
