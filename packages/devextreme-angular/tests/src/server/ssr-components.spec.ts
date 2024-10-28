1907c56731cb182c799350c510445228cf8aca22
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2024-10-29 01:21:53 +0400

    add test

--- packages/devextreme-angular/tests/src/server/ssr-components.spec.ts
+++ packages/devextreme-angular/tests/src/server/ssr-components.spec.ts
@@ -8,20 +8,25 @@ import {
 } from '@angular/core/testing';
 
 import {
-    DevExtremeModule
+    DevExtremeModule, DxDataGridModule
 } from 'devextreme-angular';
+
+import renderer from 'devextreme/core/renderer';
 
 import { DxServerModule } from 'devextreme-angular/server';
 
 import {
     componentNames
 } from './component-names';
+import {DxoDataGridGroupPanelModule} from "devextreme-angular/ui/data-grid/nested";
 
 @Component({
     selector: 'test-container-component',
     template: ''
 })
 class TestContainerComponent {
+    dataSource = [{"FullName": "bob"}, {"FullName": "charlie"}, {"FullName": "cheryl"}];
+    isVisible: true
 }
 
 describe('Universal', () => {
@@ -32,6 +37,8 @@ describe('Universal', () => {
             imports: [
                 DxServerModule,
                 DevExtremeModule,
+                DxDataGridModule,
+                DxoDataGridGroupPanelModule,
             ]
         });
     });
@@ -48,5 +55,19 @@ describe('Universal', () => {
 
         let fixture = TestBed.createComponent(TestContainerComponent);
         expect(fixture.detectChanges.bind(fixture)).not.toThrow();
+    });
+
+    fit('should no error if renderer is called (T1255582)', async () => {
+        TestBed.overrideComponent(TestContainerComponent, {
+            set: {
+                template: `<div></div>`
+            }
+        });
+
+        let fixture = TestBed.createComponent(TestContainerComponent);
+
+        fixture.detectChanges();
+
+        expect(() => renderer(fixture.nativeElement).filter(':visible')).not.toThrow();
     });
 });
