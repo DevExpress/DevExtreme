c7218bbc29fc55b54a8f7001bbf35817b52fb99b
marker dao ® <youdontknow@marker-dao.eth>
2024-10-28 21:42:03 +0100

    Chat: Get rid of excessive ts-expect-error

--- packages/devextreme/js/__internal/ui/chat/messagegroup.ts
+++ packages/devextreme/js/__internal/ui/chat/messagegroup.ts
@@ -83,7 +83,6 @@ class MessageGroup extends Widget<Properties> {
     const { author } = items[0];
     const authorName = author?.name;
     const authorAvatarUrl = author?.avatarUrl;
-    // @ts-expect-error
     const authorAvatarAlt = author?.avatarAlt;
 
     this._avatar = this._createComponent($avatar, Avatar, {
