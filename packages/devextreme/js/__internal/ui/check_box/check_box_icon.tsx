fcb1226886769a5a11a640c492ef2e677bd0eb37
Roman Semenov <roman.semenov@devexpress.com>
2024-10-29 01:40:32 +0400

    check box icon - fix eslint

--- packages/devextreme/js/__internal/ui/check_box/check_box_icon.tsx
+++ packages/devextreme/js/__internal/ui/check_box/check_box_icon.tsx
@@ -1,8 +1,10 @@
-import { RefObject } from "inferno";
-import { createRef as infernoCreateRef } from 'inferno';
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import '@js/ui/themes';
+
 import { BaseInfernoComponent, normalizeStyles } from '@devextreme/runtime/inferno';
 import { normalizeStyleProp } from '@js/core/utils/style';
-import '@js/ui/themes';
+import type { RefObject } from 'inferno';
+import { createRef as infernoCreateRef } from 'inferno';
 
 export interface CheckBoxIconProps {
   size?: number | string;
@@ -10,45 +12,47 @@ export interface CheckBoxIconProps {
 
 export const defaultCheckBoxIconProps = {};
 
-export class CheckBoxIcon extends BaseInfernoComponent {
+export class CheckBoxIcon extends BaseInfernoComponent<CheckBoxIconProps> {
   elementRef!: RefObject<HTMLDivElement>;
 
-  private __getterCache: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  private readonly __getterCache: any;
 
-  constructor(props) {
+  constructor(props: CheckBoxIconProps) {
     super(props);
     this.state = {};
     this.elementRef = infernoCreateRef();
     this.__getterCache = {};
   }
 
-  get cssStyles() {
-    if (this.__getterCache['cssStyles'] !== undefined) {
-      return this.__getterCache['cssStyles'];
+  get cssStyles(): Record<string, unknown> {
+    if (this.__getterCache.cssStyles !== undefined) {
+      return this.__getterCache.cssStyles;
     }
-    return this.__getterCache['cssStyles'] = (() => {
+    // eslint-disable-next-line no-return-assign
+    return this.__getterCache.cssStyles = ((): Record<string, unknown> => {
       const {
-        size
+        size,
       } = this.props;
       const fontSize = normalizeStyleProp('fontSize', size);
       return {
-        fontSize
+        fontSize,
       };
     })();
   }
 
-  get restAttributes() {
+  get restAttributes(): Record<string, unknown> {
     const { size, ...restProps } = this.props;
     return restProps;
   }
 
-  componentWillUpdate(nextProps, nextState, context) {
-    if (this.props['size'] !== nextProps['size']) {
-      this.__getterCache['cssStyles'] = undefined;
+  componentWillUpdate(nextProps: CheckBoxIconProps): void {
+    if (this.props.size !== nextProps.size) {
+      this.__getterCache.cssStyles = undefined;
     }
   }
 
-  render() {
+  render(): JSX.Element {
     const { elementRef, cssStyles } = this;
     return (
       <span
