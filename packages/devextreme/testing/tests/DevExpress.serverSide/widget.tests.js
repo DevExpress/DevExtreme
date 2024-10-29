0a01b1eb688f655ef5dba96e4821133181eb3853
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2024-10-30 00:49:42 +0400

    fix test name

--- packages/devextreme/testing/tests/DevExpress.serverSide/widget.tests.js
+++ packages/devextreme/testing/tests/DevExpress.serverSide/widget.tests.js
@@ -6,8 +6,8 @@ import registerComponent from 'core/component_registrator';
 const DxWidget = Widget.inherit({});
 registerComponent('dxWidget', DxWidget);
 
-QUnit.module('Widgets in SSR mode (T1259705)', {}, () => {
-    QUnit.test('checking visibility in SSR mode not throw errors', function(assert) {
+QUnit.module('Widgets in SSR mode', {}, () => {
+    QUnit.test('checking visibility in SSR mode not throw errors (T1259705)', function(assert) {
         const TestWidget = Widget.inherit({});
 
         const $element = $('#widget').dxWidget({ width: 0, height: 0 });
