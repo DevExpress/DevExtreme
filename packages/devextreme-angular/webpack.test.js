30d4545823091e71254d46c61ef0c012c7b81bf4
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2024-10-30 22:46:40 +0400

    update webpack change puppeteer version

--- packages/devextreme-angular/webpack.test.js
+++ packages/devextreme-angular/webpack.test.js
@@ -1,17 +1,8 @@
 module.exports = {
     devtool: 'inline-source-map',
     mode: 'development',
-    plugins: [
-        new webpack.ContextReplacementPlugin(
-            /angular(\\|\/)core(\\|\/)(@angular|esm5)/,
-            path.join(__dirname, '../tests'),
-            {}
-        )
-    ],
+    plugins: [],
     resolve: {
-        alias: {
-            'devextreme-angular': path.resolve(__dirname, 'npm/dist'),
-        },
         fallback: { "stream": require.resolve("stream-browserify")}
     },
     module: {
