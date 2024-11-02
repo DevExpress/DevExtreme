e351d1c906b460d90143368ae371701d2081a077
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2024-11-02 14:12:36 +0400

    update webpack

--- packages/devextreme-angular/karma.test.shim.js
+++ packages/devextreme-angular/karma.test.shim.js
@@ -1,5 +1,4 @@
 require('./karma.common.test.shim');
-const path = require('path');
 
 const testing = require('@angular/core/testing');
 const browser = require('@angular/platform-browser-dynamic/testing');
@@ -9,7 +8,6 @@ testing.TestBed.initTestEnvironment(
   browser.platformBrowserDynamicTesting(),
 );
 
-console.log('-----path----->', path.resolve('./tests/dist'));
 const context = require.context('./tests/dist', true, /^.\/(?!.*\/ssr-components\.spec.js$).*\.spec\.js$/);
 context.keys().map(context);
 __karma__.start();
