1b86fe47d59d719bd69dff1ae0904babe117da28
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2024-11-02 13:24:23 +0400

    add log msg to karma conf

--- packages/devextreme-angular/karma.test.shim.js
+++ packages/devextreme-angular/karma.test.shim.js
@@ -1,4 +1,5 @@
 require('./karma.common.test.shim');
+const path = require('path');
 
 const testing = require('@angular/core/testing');
 const browser = require('@angular/platform-browser-dynamic/testing');
@@ -8,6 +9,7 @@ testing.TestBed.initTestEnvironment(
   browser.platformBrowserDynamicTesting(),
 );
 
+console.log('-----path----->', path.resolve('./tests/dist'));
 const context = require.context('./tests/dist', true, /^.\/(?!.*\/ssr-components\.spec.js$).*\.spec\.js$/);
 context.keys().map(context);
 __karma__.start();
