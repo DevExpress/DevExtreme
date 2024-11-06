1872773d9267872a55055040a09b63f4c5390fe1
Andrey Dolzhikov <eylau-andrew@mail.ru>
2024-11-06 19:44:35 +0200

    Fix generated code

--- apps/demos/Demos/TreeList/Reordering/ReactJs/App.js
+++ apps/demos/Demos/TreeList/Reordering/ReactJs/App.js
@@ -30,7 +30,7 @@ const App = () => (
       dataField="Address"
       width={190}
       fixed={true}
-      fixedPosition='sticky'
+      fixedPosition="sticky"
     />
     <Column dataField="Zipcode" />
     <Column
@@ -44,12 +44,12 @@ const App = () => (
     <Column
       dataField="City"
       fixed={true}
-      fixedPosition='right'
+      fixedPosition="right"
     />
     <Column
       dataField="State"
       fixed={true}
-      fixedPosition='right'
+      fixedPosition="right"
     />
     <Column dataField="Department" />
     <Column dataField="HomePhone" />
