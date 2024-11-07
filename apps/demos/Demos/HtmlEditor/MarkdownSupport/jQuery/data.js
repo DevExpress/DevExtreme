f855eb37e31f67969ecb773de1dc8840ba28cf77
marker dao ® <youdontknow@marker-dao.eth>
2024-11-07 15:01:40 +0100

    HtmlEditor: Markdown support demo

---
+++ apps/demos/Demos/HtmlEditor/MarkdownSupport/jQuery/data.js
@@ -0,0 +1,4 @@
+const markup = `## Formatted Text Editor (HTML Editor)
+
+DevExtreme JavaScript HTML Editor is a client-side WYSIWYG text editor that allows its users to format textual and visual content and store it as HTML.
+`;
