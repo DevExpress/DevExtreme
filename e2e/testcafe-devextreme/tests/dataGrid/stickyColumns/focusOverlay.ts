d42694e11fd80e5a843ef8f82375de1a64b0761a
Andrey Dolzhikov <eylau-andrew@mail.ru>
2024-11-19 10:10:58 +0200

    Add etalons

--- e2e/testcafe-devextreme/tests/dataGrid/stickyColumns/focusOverlay.ts
+++ e2e/testcafe-devextreme/tests/dataGrid/stickyColumns/focusOverlay.ts
@@ -38,84 +38,83 @@ test('Focus overlay should be displayed correctly if sticky columns are turned o
     .expect(compareResults.isValid())
     .ok(compareResults.errorMessages());
 }).before(async () => createWidget('dxDataGrid', {
-    dataSource: getData(20, 40),
-    columnFixing: {
-        enabled: true
+  dataSource: getData(20, 40),
+  columnFixing: {
+    enabled: true,
+  },
+  groupPanel: {
+    visible: true,
+  },
+  width: 800,
+  showColumnHeaders: true,
+  columnAutoWidth: true,
+  allowColumnReordering: true,
+  allowColumnResizing: true,
+  summary: {
+    totalItems: [{
+      column: 'field_1',
+      summaryType: 'count',
+    }, {
+      column: 'field_6',
+      summaryType: 'count',
+    }],
+    groupItems: [{
+      column: 'field_0',
+      summaryType: 'count',
+      showInGroupFooter: false,
+      alignByColumn: true,
     },
-    groupPanel: {
-      visible: true
-    },
-    width: 800,
-    showColumnHeaders: true,
-    columnAutoWidth: true,
-    allowColumnReordering: true,
-    allowColumnResizing: true,
-    summary: {
-      totalItems: [{
-        column: 'field_1',
-        summaryType: 'count',
-      }, {
-        column: 'field_6',
-        summaryType: 'count',
-      }],
-      groupItems: [{
-        column: 'field_0',
-        summaryType: 'count',
-        showInGroupFooter: false,
-        alignByColumn: true,
+    {
+      column: 'field_11',
+      summaryType: 'count',
+      showInGroupFooter: false,
+      alignByColumn: true,
+    }, {
+      column: 'field_6',
+      summaryType: 'count',
+      showInGroupFooter: true,
+    }],
+  },
+  customizeColumns(columns) {
+    columns[5].fixed = true;
+    columns[6].fixed = true;
+
+    columns[11].fixed = true;
+    columns[11].fixedPosition = 'right';
+    columns[12].fixed = true;
+    columns[12].fixedPosition = 'right';
+
+    columns.splice(15, 5, {
+      caption: 'Band column 1',
+      columns: [{
+        caption: 'Nested column 1',
+        columns: ['field_15', 'field_16'],
       },
+      'field_17',
       {
-        column: 'field_11',
-        summaryType: 'count',
-        showInGroupFooter: false,
-        alignByColumn: true,
-      },  {
-        column: 'field_6',
-        summaryType: 'count',
-        showInGroupFooter: true,
-      }]
-    },
-    customizeColumns: function(columns) {
-        columns[5].fixed = true;
-        columns[6].fixed = true;
+        caption: 'Nested column 2',
+        columns: ['field_18', 'field_19'],
+      }],
+    });
 
-        columns[11].fixed = true;
-        columns[11].fixedPosition = 'right';
-        columns[12].fixed = true;
-        columns[12].fixedPosition = 'right';
+    columns.splice(25, 4, {
+      caption: 'Band column 2',
+      columns: [
+        'field_29',
+        {
+          caption: 'Nested column 3',
+          columns: ['field_30', 'field_31'],
+        },
+        'field_32',
+      ],
+    });
 
+    columns[0].hidingPriority = 0;
+    columns[columns.length - 1].hidingPriority = 1;
+    columns[columns.length - 2].hidingPriority = 2;
+    columns[columns.length - 3].hidingPriority = 3;
 
-        columns.splice(15, 5, {
-          caption: 'Band column 1',
-          columns: [{
-            caption: 'Nested column 1',
-            columns: ['field_15', 'field_16']
-          }, 
-          'field_17',
-          {
-            caption: 'Nested column 2',
-            columns: ['field_18', 'field_19']
-          }]
-        });
-
-        columns.splice(25, 4, {
-          caption: 'Band column 2',
-          columns: [
-            'field_29', 
-            {
-              caption: 'Nested column 3',
-              columns: ['field_30', 'field_31']
-            }, 
-            'field_32'
-          ]
-        });
-
-        columns[0].hidingPriority = 0;
-        columns[columns.length - 1].hidingPriority = 1;
-        columns[columns.length - 2].hidingPriority = 2;
-        columns[columns.length - 3].hidingPriority = 3;
-
-        columns[1].groupIndex = 0;
-        columns[2].groupIndex = 1;
-    }
+    columns[1].groupIndex = 0;
+    columns[2].groupIndex = 1;
+  },
 }));
