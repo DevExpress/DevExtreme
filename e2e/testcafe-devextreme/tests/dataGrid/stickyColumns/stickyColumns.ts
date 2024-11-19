8740ea25d86892b7a24c7e1ecdb123216b34a617
Alyar <>
2024-11-19 05:05:19 +0400

    Fix row hover for generic theme

--- e2e/testcafe-devextreme/tests/dataGrid/stickyColumns/stickyColumns.ts
+++ e2e/testcafe-devextreme/tests/dataGrid/stickyColumns/stickyColumns.ts
@@ -1229,4 +1229,42 @@ safeSizeTest('The simulated scrollbar should display correctly when there are st
     .after(async () => {
       await changeTheme(Themes.genericLight);
     });
+
+  safeSizeTest(`Row hover should display correctly when there are fixed columns (${theme} theme)`, async (t) => {
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+    const dataGrid = new DataGrid(DATA_GRID_SELECTOR);
+
+    await t.expect(dataGrid.isReady()).ok();
+
+    await t.hover(dataGrid.getDataRow(1).element);
+
+    await takeScreenshot(`row_hover_with_fixed_columns_(${theme}).png`, dataGrid.element);
+
+    await t
+      .expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
+  }, [900, 800])
+    .before(async () => {
+      await changeTheme(theme);
+      await createWidget('dxDataGrid', {
+        dataSource: getData(20, 15),
+        columnWidth: 100,
+        columnAutoWidth: true,
+        hoverStateEnabled: true,
+        customizeColumns: (columns) => {
+          columns[5].fixed = true;
+          columns[5].fixedPosition = 'left';
+          columns[6].fixed = true;
+          columns[6].fixedPosition = 'left';
+
+          columns[8].fixed = true;
+          columns[8].fixedPosition = 'right';
+          columns[9].fixed = true;
+          columns[9].fixedPosition = 'right';
+        },
+      });
+    })
+    .after(async () => {
+      await changeTheme(Themes.genericLight);
+    });
 });
--- e2e/testcafe-devextreme/tests/treeList/stickyColumns/stickyColumns.ts
+++ e2e/testcafe-devextreme/tests/treeList/stickyColumns/stickyColumns.ts
@@ -62,4 +62,55 @@ fixture.disablePageReloads`Sticky columns - Drag and Drop`
     .after(async () => {
       await changeTheme(Themes.genericLight);
     });
+
+  safeSizeTest(`Row hover should display correctly when there are fixed columns (${theme} theme)`, async (t) => {
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+    const treeList = new TreeList(TREE_LIST_SELECTOR);
+
+    await t.expect(treeList.isReady()).ok();
+
+    await t.hover(treeList.getDataRow(1).element);
+
+    await takeScreenshot(`row_hover_with_fixed_columns_(${theme}).png`, treeList.element);
+
+    await t
+      .expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
+  }, [900, 800])
+    .before(async () => {
+      await changeTheme(theme);
+      await createWidget('dxTreeList', {
+        dataSource: new Array(20).fill(null).map((_, index) => {
+          const item = {
+            id: index + 1,
+            parentId: index % 5,
+          };
+
+          for (let i = 0; i < 13; i += 1) {
+            item[`field${i}`] = `test ${i} ${index + 2}`;
+          }
+
+          return item;
+        }),
+        keyExpr: 'id',
+        columnFixing: {
+          enabled: true,
+        },
+        width: 850,
+        autoExpandAll: true,
+        columnAutoWidth: true,
+        customizeColumns(columns) {
+          columns[5].fixed = true;
+          columns[6].fixed = true;
+
+          columns[8].fixed = true;
+          columns[8].fixedPosition = 'right';
+          columns[9].fixed = true;
+          columns[9].fixedPosition = 'right';
+        },
+      });
+    })
+    .after(async () => {
+      await changeTheme(Themes.genericLight);
+    });
 });
