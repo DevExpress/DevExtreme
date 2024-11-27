bb62b0610ae6e96b5ecd55dbdd17a84f5d0d14fa
Nikki Gonzales <38495263+nikkithelegendarypokemonster@users.noreply.github.com>
2024-11-27 14:36:25 +0800

    Calendar: Included dates are highlighted in calendar multi-view mode (T1253076) (#28423)
    Co-authored-by: ksercs <ksercs0@gmail.com>
    # Conflicts:
    #	packages/devextreme/js/__internal/core/utils/m_date.ts

--- packages/devextreme/js/__internal/ui/calendar/m_calendar.range.selection.strategy.ts
+++ packages/devextreme/js/__internal/ui/calendar/m_calendar.range.selection.strategy.ts
@@ -117,8 +117,8 @@ class CalendarRangeSelectionStrategy extends CalendarSelectionStrategy {
 
     const { currentDate, viewsCount } = this.calendar.option();
     const isAdditionalViewDate = this.calendar._isAdditionalViewDate(currentDate);
-    const firstDateInViews = dateUtils.getFirstMonthDate(dateUtils.addDateInterval(currentDate, 'month', isAdditionalViewDate ? -2 : -1));
-    const lastDateInViews = dateUtils.getLastMonthDate(dateUtils.addDateInterval(currentDate, 'month', isAdditionalViewDate ? 1 : viewsCount));
+    const firstDateInViews = dateUtils.getFirstMonthDate(currentDate, isAdditionalViewDate ? -2 : -1);
+    const lastDateInViews = dateUtils.getLastMonthDate(currentDate, isAdditionalViewDate ? 1 : viewsCount);
 
     // @ts-expect-error
     const rangeStartDate = new Date(Math.max(firstDateInViews, startDate));
