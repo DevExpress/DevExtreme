437d129449a1cea8add1561c7ee3f3a26558a632
Sergey Arzamasov <sergey.arzamasov@devexpress.com>
2024-12-04 13:42:11 +0400

    add pagination component

--- apps/demos/Demos/Pagination/Overview/Vue/App.vue
+++ apps/demos/Demos/Pagination/Overview/Vue/App.vue
@@ -1,9 +1,4 @@
 <template>
-  <DxDataGrid
-    :data-source="dataSource"
-    :columns="columns"
-    :show-borders="true"
-  />
   <DxPagination
     :show-info="showInfo"
     :show-navigation-buttons="showPageSizeSelector"
@@ -11,33 +6,40 @@
     :page-index="pageIndex"
     :page-size="pageSize"
     :item-count="itemCount"
+    @option-changed="handlePropertyChange"
   />
 </template>
 <script>
 
 import { computed, ref } from 'vue';
-import DxDataGrid from 'devextreme-vue/data-grid';
 import DxPagination from 'devextreme-vue/pagination';
-import { customers } from './data.js';
+import { employees } from './data.ts';
 
 const pageSizes = [4, 6];
-
 const showInfo = true;
 const showPageSizeSelector = true;
-const pageIndex = 1;
-const pageSize = 4;
-const itemCount = customers.length;
+let pageIndex = 1;
+let pageSize = 4;
+const itemCount = employees.length;
 
 export default {
   components: {
-    DxDataGrid,
     DxPagination,
   },
   data() {
     return {
-      dataSource: customers,
-      columns: ['CompanyName', 'City', 'State', 'Phone', 'Fax'],
+      showInfo,
+      showPageSizeSelector,
+      pageSizes,
+      pageIndex,
+      pageSize,
+      itemCount,
     };
   },
+  methods: {
+    handlePropertyChange(e) {
+      
+    }
+  }
 };
 </script>
