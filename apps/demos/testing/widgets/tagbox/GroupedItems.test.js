af253de1b93d7769ed8fc7d365731f832c245824
ksercs <ksercs0@gmail.com>
2024-12-05 18:12:05 +0400

    add tests

---
+++ apps/demos/testing/widgets/tagbox/GroupedItems.test.js
@@ -0,0 +1,47 @@
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import { Selector as $ } from 'testcafe';
+import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
+import { testScreenshot } from '../../../utils/visual-tests/helpers/theme-utils';
+
+fixture('TagBox.GroupedItems')
+  .page('http://localhost:8080/')
+  .before(async (ctx) => {
+    ctx.initialWindowSize = [900, 600];
+  });
+
+runManualTest('TagBox', 'GroupedItems', ['jQuery', 'React', 'Vue', 'Angular'], (test) => {
+  test('GroupedItems', async (t) => {
+    const TAG_BOX_CLASS = 'dx-tagbox';
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+    await t
+      .click($(`.${TAG_BOX_CLASS}`).nth(0))
+      .wait(200);
+
+    await testScreenshot(t, takeScreenshot, 'tagbox_groupeditems_first_opened.png');
+
+    await t
+      .click($(`.${TAG_BOX_CLASS}`).nth(0))
+      .wait(200);
+
+    await t
+      .click($(`.${TAG_BOX_CLASS}`).nth(1))
+      .wait(200);
+
+    await testScreenshot(t, takeScreenshot, 'tagbox_groupeditems_second_opened.png');
+
+    await t
+      .click($(`.${TAG_BOX_CLASS}`).nth(1))
+      .wait(200);
+
+    await t
+      .click($(`.${TAG_BOX_CLASS}`).nth(2))
+      .wait(200);
+
+    await testScreenshot(t, takeScreenshot, 'tagbox_groupeditems_third_opened.png');
+
+    await t
+      .expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
+  });
+});
