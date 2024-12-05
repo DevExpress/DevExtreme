d58fb8203eeb3312afbfd16d3a9159dcd8667292
ksercs <ksercs0@gmail.com>
2024-12-05 18:55:44 +0400

    try to click input to not remove tags accidently

--- apps/demos/testing/widgets/tagbox/GroupedItems.test.js
+++ apps/demos/testing/widgets/tagbox/GroupedItems.test.js
@@ -12,30 +12,31 @@ fixture('TagBox.GroupedItems')
 runManualTest('TagBox', 'GroupedItems', ['jQuery', 'React', 'Vue', 'Angular'], (test) => {
   test('GroupedItems', async (t) => {
     const TAG_BOX_CLASS = 'dx-tagbox';
+    const $tagBoxes = $(`.${TAG_BOX_CLASS} input`);
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
     await t
-      .click($(`.${TAG_BOX_CLASS}`).nth(0))
+      .click($tagBoxes.nth(0))
       .wait(200);
 
     await testScreenshot(t, takeScreenshot, 'tagbox_groupeditems_first_opened.png');
 
     await t
-      .click($(`.${TAG_BOX_CLASS}`).nth(0))
+      .click($tagBoxes.nth(0))
       .wait(200);
 
     await t
-      .click($(`.${TAG_BOX_CLASS}`).nth(1))
+      .click($tagBoxes.nth(1))
       .wait(200);
 
     await testScreenshot(t, takeScreenshot, 'tagbox_groupeditems_second_opened.png');
 
     await t
-      .click($(`.${TAG_BOX_CLASS}`).nth(1))
+      .click($tagBoxes.nth(1))
       .wait(200);
 
     await t
-      .click($(`.${TAG_BOX_CLASS}`).nth(2))
+      .click($tagBoxes.nth(2))
       .wait(200);
 
     await testScreenshot(t, takeScreenshot, 'tagbox_groupeditems_third_opened.png');
