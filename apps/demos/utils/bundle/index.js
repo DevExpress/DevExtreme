57a86d5527ace84ea1123ff754206716e3bb4376
Vasily Strelyaev <Vasily.Strelyaev@devexpress.com>
2024-12-06 20:08:57 +0200

    refactor system.js build fix

--- apps/demos/utils/bundle/index.js
+++ apps/demos/utils/bundle/index.js
@@ -217,6 +217,12 @@ const prepareConfigs = (framework)=> {
             ]
           };
 
+          // This auto-generated runtime import is useless because grid.js exports only types,
+          // but System.js transpiles this import into code that crashes when triggered in a Demo.
+          const removeImportTranspiledToCrashingCode = (code) => code.includes(GRID_COMMON_STAR_IMPORT)
+            ? code.replace(GRID_COMMON_STAR_IMPORT, '')
+            : code;
+
           const result = new Promise((resolve) => {
             // systemjs-builder uses babel 6, so we use babel 7 here for transpiling ES2020
             babel.transformFile(url.fileURLToPath(load.name), babelOptions, (err, result) => {
@@ -224,10 +230,7 @@ const prepareConfigs = (framework)=> {
                   fetch(load).then(r => resolve(r));
                   console.log('Unexpected transipling error (babel 7): ' + err);
                 } else {
-                  if(result.code.includes(GRID_COMMON_STAR_IMPORT)) {
-                    result.code = result.code.replace(GRID_COMMON_STAR_IMPORT, '');
-                  }
-                  resolve(result.code);
+                  resolve(removeImportTranspiledToCrashingCode(result.code));
                 }
             });
           })
