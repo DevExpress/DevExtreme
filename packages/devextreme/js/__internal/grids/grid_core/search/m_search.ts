78fc030673a8bac3dcb9fc904863171802771ff8
Roman Semenov <roman.semenov@devexpress.com>
2024-12-20 12:12:47 +0400

    DataGrid - In the Fluent theme, a padding is set when Toolbar is not visible (T1261773) (#28551)

--- packages/devextreme/js/__internal/grids/grid_core/search/m_search.ts
+++ packages/devextreme/js/__internal/grids/grid_core/search/m_search.ts
@@ -194,11 +194,6 @@ const headerPanel = (
     }
     return null;
   }
-
-  public isVisible() {
-    const searchPanelOptions = this.option('searchPanel');
-    return super.isVisible() || !!searchPanelOptions?.visible;
-  }
 };
 
 const rowsView = (
