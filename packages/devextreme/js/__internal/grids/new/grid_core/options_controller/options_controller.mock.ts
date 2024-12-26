71471727d5a7a3c1797768fa2e34f2dec542aae6
Roman Semenov <roman.semenov@devexpress.com>
2024-12-26 13:23:58 +0400

    CardView - add default values for optionsController mock (#28590)

--- packages/devextreme/js/__internal/grids/new/grid_core/options_controller/options_controller.mock.ts
+++ packages/devextreme/js/__internal/grids/new/grid_core/options_controller/options_controller.mock.ts
@@ -1,6 +1,11 @@
-import type { defaultOptions, Options } from '../options';
+import type { Options } from '../options';
+import { defaultOptions } from '../options';
 import { OptionsControllerMock as OptionsControllerBaseMock } from './options_controller_base.mock';
 
 export class OptionsControllerMock extends OptionsControllerBaseMock<
 Options, typeof defaultOptions
-> {}
+> {
+  constructor(options: Options) {
+    super(options, defaultOptions);
+  }
+}
