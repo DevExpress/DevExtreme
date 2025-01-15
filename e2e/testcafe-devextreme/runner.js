caf63e09bb80296e9936cfb169a6d1ff339b29bd
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2025-01-15 13:44:11 +0400

    try pager tests

--- e2e/testcafe-devextreme/runner.js
+++ e2e/testcafe-devextreme/runner.js
@@ -158,7 +158,9 @@ function setShadowDom(args) {
 function expandBrowserAlias(browser) {
     switch(browser) {
         case 'chrome:devextreme-shr2':
-            if(process.env.theme === 'material.blue.light') {
+            if(process.env.theme === 'material.blue.light'
+                && (componentFolder === 'pagination' || componentFolder === 'form')
+            ) {
                 return 'chrome:headless --disable-gpu --window-size=1200,800 --disable-partial-raster --disable-skia-runtime-opts --run-all-compositor-stages-before-draw --disable-new-content-rendering-timeout --disable-threaded-animation --disable-threaded-scrolling --disable-checker-imaging --disable-image-animation-resync --use-gl="swiftshader" --disable-features=PaintHolding --js-flags=--random-seed=2147483647 --font-render-hinting=none --disable-font-subpixel-positioning';
             }
             return 'chrome:headless --disable-gpu --window-size=1200,800';
