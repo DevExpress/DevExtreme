6163e287689ca3f9907ff7703d999a24429a3fa0
Roman Semenov <roman.semenov@devexpress.com>
2025-01-20 18:59:45 +0400

    fix: typing

--- packages/devextreme/js/__internal/grids/new/grid_core/inferno_wrappers/pager.ts
+++ packages/devextreme/js/__internal/grids/new/grid_core/inferno_wrappers/pager.ts
@@ -1,25 +1,10 @@
-import PagerWidget from '@js/ui/pagination';
+import type { Properties as PaginationProperties } from '@js/ui/pagination';
+import dxPagination from '@js/ui/pagination';
 
 import { InfernoWrapper } from './widget_wrapper';
 
-// TODO: remove when public .d.ts for pager is created
-export interface PagerProps {
-  pageIndex?: number;
-  pageSize?: number;
-
-  pageIndexChanged?: (value: number) => void;
-  pageSizeChanged?: (value: number) => void;
-
-  gridCompatibility?: boolean;
-
-  pageSizes?: number[];
-
-  pageCount?: number;
-}
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export class Pager extends InfernoWrapper<PagerProps, any> {
-  protected getComponentFabric(): typeof PagerWidget {
-    return PagerWidget;
+export class Pager extends InfernoWrapper<PaginationProperties, dxPagination> {
+  protected getComponentFabric(): typeof dxPagination {
+    return dxPagination;
   }
 }
--- packages/devextreme/js/__internal/grids/new/grid_core/pager/pager.tsx
+++ packages/devextreme/js/__internal/grids/new/grid_core/pager/pager.tsx
@@ -1,15 +1,12 @@
 import type { PagerBase } from '@js/ui/pagination';
 import type { InfernoNode } from 'inferno';
-import { Component } from 'inferno';
 
 import { Pager } from '../inferno_wrappers/pager';
 
 export type PagerProps = PagerBase & { visible: boolean };
 
-export class PagerView extends Component<PagerProps> {
-  public render(): InfernoNode {
-    return (
-      this.props.visible && <Pager {...this.props}></Pager>
-    );
-  }
+export function PagerView(props: PagerProps): InfernoNode {
+  return (
+    props.visible && <Pager {...props}></Pager>
+  );
 }
