26521a0c3fcc249af63457f76f3fdcb5a2a8ce01
anna.shakhova <anna.shakhova@devexpress.com>
2025-01-20 13:56:04 +0100

    Chat: Fix html parsing in AI demo

--- apps/demos/Demos/Chat/AIAndChatbotIntegration/Vue/data.ts
+++ apps/demos/Demos/Chat/AIAndChatbotIntegration/Vue/data.ts
@@ -1,9 +1,10 @@
-import CustomStore from "devextreme/data/custom_store";
-import DataSource from "devextreme/data/data_source";
+import CustomStore from 'devextreme/data/custom_store';
+import DataSource from 'devextreme/data/data_source';
 import { unified } from 'unified';
 import remarkParse from 'remark-parse';
 import remarkRehype from 'remark-rehype';
 import rehypeStringify from 'rehype-stringify';
+import rehypeMinifyWhitespace from 'rehype-minify-whitespace';
 
 export const dictionary = {
   en: {
@@ -11,7 +12,7 @@ export const dictionary = {
     'dxChat-emptyListPrompt': 'AI Assistant is ready to answer your questions.',
     'dxChat-textareaPlaceholder': 'Ask AI Assistant...',
   },
-}
+};
 
 export const AzureOpenAIConfig = {
   dangerouslyAllowBrowser: true,
@@ -19,7 +20,7 @@ export const AzureOpenAIConfig = {
   apiVersion: '2024-02-01',
   endpoint: 'https://public-api.devexpress.com/demo-openai',
   apiKey: 'DEMO',
-}
+};
 
 export const REGENERATION_TEXT = 'Regeneration...';
 export const CHAT_DISABLED_CLASS = 'dx-chat-disabled';
@@ -39,35 +40,32 @@ export const messages = [];
 
 const customStore = new CustomStore({
   key: 'id',
-  load: () => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve([...store]);
-      }, 0);
+  load: () => new Promise((resolve) => {
+    setTimeout(() => {
+      resolve([...store]);
+    }, 0);
+  }),
+  insert: (message) => new Promise((resolve) => {
+    setTimeout(() => {
+      store.push(message);
+      resolve(message);
     });
-  },
-  insert: (message) => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        store.push(message);
-        resolve(message);
-      });
-    });
-  },
+  }),
 });
 
 export const dataSource = new DataSource({
   store: customStore,
   paginate: false,
-})
+});
 
 export function convertToHtml(value: string) {
   const result = unified()
-      .use(remarkParse)
-      .use(remarkRehype)
-      .use(rehypeStringify)
-      .processSync(value)
-      .toString();
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeMinifyWhitespace)
+    .use(rehypeStringify)
+    .processSync(value)
+    .toString();
 
   return result;
 }
