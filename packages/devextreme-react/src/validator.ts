7d1d13f4a3f2f8ab98b61977cee5b91d211d62ff
Mikhail Preyskurantov <5574159+mpreyskurantov@users.noreply.github.com>
2025-01-21 08:08:46 +0200

    Validator - onInitialized event parameter has incorrect typing (T1269388) (#28686)

--- packages/devextreme-react/src/validator.ts
+++ packages/devextreme-react/src/validator.ts
@@ -9,9 +9,20 @@ import { ExtensionComponent as BaseComponent } from "./core/extension-component"
 import { IHtmlOptions, ComponentRef, NestedComponentMeta } from "./core/component";
 import NestedOption from "./core/nested-option";
 
+import type { DisposingEvent, InitializedEvent, ValidatedEvent } from "devextreme/ui/validator";
 import type { ValidationRuleType, ComparisonOperator } from "devextreme/common";
 
-type IValidatorOptions = React.PropsWithChildren<Properties & IHtmlOptions>
+type ReplaceFieldTypes<TSource, TReplacement> = {
+  [P in keyof TSource]: P extends keyof TReplacement ? TReplacement[P] : TSource[P];
+}
+
+type IValidatorOptionsNarrowedEvents = {
+  onDisposing?: ((e: DisposingEvent) => void);
+  onInitialized?: ((e: InitializedEvent) => void);
+  onValidated?: ((validatedInfo: ValidatedEvent) => void);
+}
+
+type IValidatorOptions = React.PropsWithChildren<ReplaceFieldTypes<Properties, IValidatorOptionsNarrowedEvents> & IHtmlOptions>
 
 interface ValidatorRef {
   instance: () => dxValidator;
