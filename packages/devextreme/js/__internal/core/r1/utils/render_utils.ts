f5c883dd35543bb6d2ce34ceaa55aefa202eca8c
Roman Semenov <roman.semenov@devexpress.com>
2025-02-14 12:22:05 +0400

    chore: remove dup implementation of combineClasses

--- packages/devextreme/js/__internal/core/r1/utils/render_utils.ts
+++
@@ -1,5 +0,0 @@
-export const combineClasses = (
-  classesMap: { [key: string]: boolean },
-): string => Object.keys(classesMap)
-  .filter((cssClass) => !!cssClass && classesMap[cssClass])
-  .join(' ');
