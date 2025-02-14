b58a08397373eb37e9fc938ae790f7628e5f70da
anna.shakhova <anna.shakhova@devexpress.com>
2025-02-14 17:11:09 +0100

    testcafe: fix htmlEditor screenshot test size

--- e2e/testcafe-devextreme/tests/htmlEditor/common.ts
+++ e2e/testcafe-devextreme/tests/htmlEditor/common.ts
@@ -3,6 +3,7 @@ import { Selector } from 'testcafe';
 import { createWidget } from '../../helpers/createWidget';
 import url from '../../helpers/getPageUrl';
 import { testScreenshot } from '../../helpers/themeUtils';
+import {appendElementTo, setStyleAttribute} from "../../helpers/domUtils";
 
 fixture.disablePageReloads`HtmlEditor`
   .page(url(__dirname, '../container.html'));
@@ -21,24 +22,30 @@ fixture.disablePageReloads`HtmlEditor`
       .click(Selector(clickTarget));
 
     await testScreenshot(t, takeScreenshot, `${baseScreenName}-focused.png`, { element: selector });
+    // await t.debug();
 
     await t
       .expect(compareResults.isValid())
       .ok(compareResults.errorMessages());
   }).before(async () => {
-    await createWidget('dxHtmlEditor', {
-      height: 200,
-      width: 200,
-      value: Array(100).fill('string').join('\n'),
-    });
+    await setStyleAttribute(Selector('#container'), 'box-sizing: border-box; height: 200px; width: 200px');
+    await setStyleAttribute(Selector('#otherContainer'), 'box-sizing: border-box; height: 200px; width: 200px');
+    await appendElementTo('#container', 'div', 'editor');
+    await appendElementTo('#otherContainer', 'div', 'editorWithToolbar');
 
-    return createWidget('dxHtmlEditor', {
-      height: 200,
-      width: 200,
+    await createWidget('dxHtmlEditor', {
+      height: '100%',
+      width: '100%',
+      value: Array(100).fill('string').join('\n'),
+    }, '#editor');
+
+    await createWidget('dxHtmlEditor', {
+      height: '100%',
+      width: '100%',
       value: Array(100).fill('string').join('\n'),
       toolbar: {
         items: ['bold', 'color'],
       },
-    }, '#otherContainer');
+    }, '#editorWithToolbar');
   });
 });
