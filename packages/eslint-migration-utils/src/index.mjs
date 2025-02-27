d25e579a5817f1ec75583b7a6c3596aaaec138e8
wdevfx <ilya.vinogradov@devexpress.com>
2025-02-28 00:01:48 +0400

    refactor(eslint-migration-utils): add package for common eslint code

---
+++ packages/eslint-migration-utils/src/index.mjs
@@ -0,0 +1,21 @@
+import { rules as stylisticRules } from '@eslint-stylistic/metadata';
+
+const REMOVED_TYPESCRIPT_RULES = [
+    '@typescript-eslint/no-throw-literal',
+    '@typescript-eslint/ban-types',
+];
+
+// TODO Salimov: We need to remove this function after updating eslint-config-devextreme
+export const changeRulesToStylistic = (devExtremeRules) => (
+    {
+        ...Object.fromEntries(Object
+            .entries(devExtremeRules)
+            .filter(([key]) => !REMOVED_TYPESCRIPT_RULES.includes(key))
+            .map(([key, value]) => {
+                const rule = stylisticRules.find((r) => key.includes(r.name));
+                const newKey = rule ? `@stylistic/${rule.name}` : key;
+
+                return [newKey, value];
+            })),
+    }
+);
