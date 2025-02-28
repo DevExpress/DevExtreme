e950e6d593ebce4dd4694ec0b6c7461766152f37
Vasily Strelyaev <VasilyStrelyaev@users.noreply.github.com>
2025-02-28 19:36:40 +0400

    fix: Next.js hydration throws a class mismatch error (#28855)

--- packages/devextreme/js/__internal/core/widget/dom_component.ts
+++ packages/devextreme/js/__internal/core/widget/dom_component.ts
@@ -17,6 +17,7 @@ import { isDefined, isFunction, isString } from '@js/core/utils/type';
 import { hasWindow } from '@js/core/utils/window';
 import license, { peekValidationPerformed } from '@ts/core/license/license_validation';
 import TemplateManagerModule from '@ts/core/m_template_manager';
+import { uiLayerInitialized } from '@ts/core/utils/m_common';
 
 import { Component } from './component';
 import type { OptionChanged } from './types';
@@ -98,6 +99,8 @@ class DOMComponent<
     if (!validationAlreadyPerformed && peekValidationPerformed()) {
       config({ licenseKey: '' });
     }
+
+    uiLayerInitialized.resolve();
   }
 
   _createElement(element: Element): void {
