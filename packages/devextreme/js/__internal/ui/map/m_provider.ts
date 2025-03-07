63dad9ea1dbd0581dbb35a12dda8b6995ea92c5b
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2025-03-07 08:41:42 +0400

    Map, Providers: convert into ES6 class (#29190)

--- packages/devextreme/js/__internal/ui/map/m_provider.ts
+++ packages/devextreme/js/__internal/ui/map/m_provider.ts
@@ -1,51 +1,73 @@
 import { addNamespace } from '@js/common/core/events/utils/index';
 import Class from '@js/core/class';
+import type { dxElementWrapper } from '@js/core/renderer';
 import { map } from '@js/core/utils/iterator';
 import { isNumeric, isPlainObject } from '@js/core/utils/type';
 
-const { abstract } = Class;
+// @ts-expect-error dxClass inheritance issue
+class Provider extends (Class.inherit({}) as new() => {}) {
+  _mapWidget?: any;
 
-const Provider = Class.inherit({
+  _map?: any;
 
-  _defaultRouteWeight() {
+  _$container!: dxElementWrapper;
+
+  _defaultRouteWeight(): number {
     return 5;
-  },
+  }
 
-  _defaultRouteOpacity() {
+  _defaultRouteOpacity(): number {
     return 0.5;
-  },
+  }
 
-  _defaultRouteColor() {
+  _defaultRouteColor(): string {
     return '#0000FF';
-  },
+  }
 
-  ctor(map, $container) {
+  ctor(map, $container: dxElementWrapper): void {
     this._mapWidget = map;
     this._$container = $container;
-  },
+  }
 
   render(markerOptions, routeOptions) {
+    // @ts-expect-error ts-error
     return this._renderImpl().then(() => Promise.all([
       this._applyFunctionIfNeeded('addMarkers', markerOptions),
       this._applyFunctionIfNeeded('addRoutes', routeOptions),
     ]).then(() => true));
-  },
+  }
 
-  _renderImpl: abstract,
+  _renderImpl(): void {
+    Class.abstract();
+  }
 
-  updateDimensions: abstract,
+  updateDimensions(): void {
+    Class.abstract();
+  }
 
-  updateMapType: abstract,
+  updateMapType(): void {
+    Class.abstract();
+  }
 
-  updateDisabled: abstract,
+  updateDisabled(): void {
+    Class.abstract();
+  }
 
-  updateBounds: abstract,
+  updateBounds(): void {
+    Class.abstract();
+  }
 
-  updateCenter: abstract,
+  updateCenter(): void {
+    Class.abstract();
+  }
 
-  updateZoom: abstract,
+  updateZoom(): void {
+    Class.abstract();
+  }
 
-  updateControls: abstract,
+  updateControls(): void {
+    Class.abstract();
+  }
 
   updateMarkers(markerOptionsToRemove, markerOptionsToAdd) {
     // eslint-disable-next-line no-promise-executor-return
@@ -54,13 +76,21 @@ const Provider = Class.inherit({
         resolve(addValue || removeValue);
       });
     }));
-  },
+  }
 
-  addMarkers: abstract,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  addMarkers(options): void {
+    Class.abstract();
+  }
 
-  removeMarkers: abstract,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  removeMarkers(options): void {
+    Class.abstract();
+  }
 
-  adjustViewport: abstract,
+  adjustViewport(): void {
+    Class.abstract();
+  }
 
   updateRoutes(routeOptionsToRemove, routeOptionsToAdd) {
     // eslint-disable-next-line no-promise-executor-return
@@ -69,42 +99,51 @@ const Provider = Class.inherit({
         resolve(addValue || removeValue);
       });
     }));
-  },
+  }
 
-  addRoutes: abstract,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  addRoutes(options) {
+    Class.abstract();
+  }
 
-  removeRoutes: abstract,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  removeRoutes(options) {
+    Class.abstract();
+  }
 
-  clean: abstract,
+  clean(): void {
+    Class.abstract();
+  }
 
   map() {
     return this._map;
-  },
+  }
 
-  isEventsCanceled() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  isEventsCanceled(e) {
     return false;
-  },
+  }
 
-  _option(name, value) {
+  _option(name?, value?) {
     if (value === undefined) {
       return this._mapWidget.option(name);
     }
 
     this._mapWidget.setOptionSilent(name, value);
-  },
+  }
 
   _keyOption(providerName) {
     const key = this._option('apiKey');
 
     return key[providerName] === undefined ? key : key[providerName];
-  },
+  }
 
   _parseTooltipOptions(option) {
     return {
       text: option.text || option,
       visible: option.isShown || false,
     };
-  },
+  }
 
   _getLatLng(location) {
     if (typeof location === 'string') {
@@ -121,15 +160,15 @@ const Provider = Class.inherit({
     }
 
     return null;
-  },
+  }
 
   _areBoundsSet() {
     return this._option('bounds.northEast') && this._option('bounds.southWest');
-  },
+  }
 
   _addEventNamespace(name) {
     return addNamespace(name, this._mapWidget.NAME);
-  },
+  }
 
   _applyFunctionIfNeeded(fnName, array) {
     if (!array.length) {
@@ -137,32 +176,31 @@ const Provider = Class.inherit({
     }
 
     return this[fnName](array);
-  },
+  }
 
-  _fireAction(name, actionArguments) {
+  _fireAction(name, actionArguments): void {
     this._mapWidget._createActionByOption(name)(actionArguments);
-  },
+  }
 
-  _fireClickAction(actionArguments) {
+  _fireClickAction(actionArguments): void {
     this._fireAction('onClick', actionArguments);
-  },
+  }
 
-  _fireMarkerAddedAction(actionArguments) {
+  _fireMarkerAddedAction(actionArguments): void {
     this._fireAction('onMarkerAdded', actionArguments);
-  },
+  }
 
-  _fireMarkerRemovedAction(actionArguments) {
+  _fireMarkerRemovedAction(actionArguments): void {
     this._fireAction('onMarkerRemoved', actionArguments);
-  },
+  }
 
-  _fireRouteAddedAction(actionArguments) {
+  _fireRouteAddedAction(actionArguments): void {
     this._fireAction('onRouteAdded', actionArguments);
-  },
+  }
 
-  _fireRouteRemovedAction(actionArguments) {
+  _fireRouteRemovedAction(actionArguments): void {
     this._fireAction('onRouteRemoved', actionArguments);
-  },
-
-});
+  }
+}
 
 export default Provider;
