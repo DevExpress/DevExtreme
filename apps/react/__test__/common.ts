3c27d0dde8c7cd8546e7b6d72738ba07690b32df
sergey-kras <krasnoluckiy280799@icloud.com>
2025-03-14 18:01:16 +0400

    chore(e2e-playground): fixed tests

---
+++ apps/react/__test__/common.ts
@@ -0,0 +1,6 @@
+import * as path from 'path';
+
+fixture('Common DevExtreme React Playground')
+    .page(path.resolve(__dirname, '../public/index.html'));
+
+test('App has no errors', async () => {});
--- apps/react/runner.js
+++ apps/react/runner.js
@@ -1,17 +1,25 @@
 const process = require('process');
 const createTestCafe = require('testcafe');
+const path = require('path');
 
 let testCafe;
+
 createTestCafe('127.0.0.1')
     .then(tsc => {
         testCafe = tsc;
-
         const runner = tsc.createRunner();
+
         return runner
-            .src('test.js')
-            .browsers('chrome:headless --disable-gpu --window-size=1200,800')
+            .src(path.join(__dirname, './__test__/*.ts'))
+            .browsers('chrome --disable-gpu --window-size=1200,800')
             .run();
-    }).then(failedCount => {
+    })
+    .then(failedCount => {
         testCafe.close();
         process.exit(failedCount);
+    })
+    .catch(err => {
+        console.error('Playground TestCafe encountered an error:', err);
+        if (testCafe) testCafe.close();
+        process.exit(1);
     });
