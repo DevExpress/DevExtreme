9df1aba8cd4f3927567dd093b44cf8dcf50f2e90
marker dao ® <youdontknow@marker-dao.eth>
2025-03-14 19:57:41 +0700

    feat(martix-test-helper.js): Unskip another tests

--- apps/demos/utils/visual-tests/matrix-test-helper.js
+++ apps/demos/utils/visual-tests/matrix-test-helper.js
@@ -199,12 +199,6 @@ const SKIPPED_TESTS = {
         Accordion: [
             { demo: 'Overview', themes: [THEME.generic, THEME.material, THEME.fluent] },
         ],
-        Common: [
-            { demo: 'EditorAppearanceVariants', themes: [THEME.generic, THEME.material, THEME.fluent] },
-        ],
-        DropDownButton: [
-            { demo: 'Overview', themes: [THEME.generic, THEME.material, THEME.fluent] },
-        ],
         Charts: [
             { demo: 'Overview', themes: [THEME.material] },
             { demo: 'Crosshair', themes: [THEME.generic, THEME.material, THEME.fluent] },
@@ -223,21 +217,6 @@ const SKIPPED_TESTS = {
         VectorMap: [
             { demo: 'TooltipHTMLSupport', themes: [THEME.material] },
             { demo: 'DynamicViewport', themes: [THEME.generic, THEME.material, THEME.fluent] },
-        ],
-        DropDownBox: [
-            { demo: 'MultipleSelection', themes: [THEME.generic, THEME.material, THEME.fluent] },
-        ],
-        List: [
-            { demo: 'ItemDragging', themes: [THEME.generic, THEME.material, THEME.fluent] },
-        ],
-        TabPanel: [
-            { demo: 'Overview', themes: [THEME.generic, THEME.material, THEME.fluent] },
-        ],
-        Splitter: [
-            { demo: 'Overview', themes: [THEME.generic, THEME.material, THEME.fluent] },
-        ],
-        Gauges: [
-            { demo: 'VariableNumberOfBars', themes: [THEME.generic, THEME.material, THEME.fluent] },
         ],
         DataGrid: [
             { demo: 'Appearance', themes: [THEME.generic, THEME.material, THEME.fluent] },
@@ -276,12 +255,6 @@ const SKIPPED_TESTS = {
             { demo: 'Resources', themes: [THEME.fluent] },
             { demo: 'CellTemplates', themes: [THEME.generic, THEME.material, THEME.fluent] },
             { demo: 'Resources', themes: [THEME.generic, THEME.material, THEME.fluent] },
-        ],
-        Popup: [
-            { demo: 'Scrolling', themes: [THEME.generic, THEME.material, THEME.fluent] },
-        ],
-        Sortable: [
-            { demo: 'Kanban', themes: [THEME.generic, THEME.material, THEME.fluent] },
         ],
         PivotGrid: [
             { demo: 'IntegratedFieldChooser', themes: [THEME.generic, THEME.material, THEME.fluent] },
