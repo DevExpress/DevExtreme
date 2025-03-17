e2aafe09c8f065b4449fef5013fac681c4d55a9a
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2025-03-17 13:19:32 +0400

    Stepper: handle invalid state class (#29311)

--- packages/devextreme/js/__internal/ui/stepper/stepper_item.ts
+++ packages/devextreme/js/__internal/ui/stepper/stepper_item.ts
@@ -3,11 +3,28 @@ import type {
 } from '@js/ui/collection/ui.collection_widget.base';
 import CollectionWidgetItem from '@ts/ui/collection/m_item';
 
+export const STEP_INVALID_CLASS = 'dx-step-invalid';
+
 export interface StepperItemProperties extends CollectionWidgetItemProperties {
   icon?: string;
   title?: string;
+  isValid?: boolean;
 }
 
-class StepperItem extends CollectionWidgetItem {}
+class StepperItem extends CollectionWidgetItem<StepperItemProperties> {
+  _renderWatchers(): void {
+    super._renderWatchers();
+
+    this._startWatcher('isValid', this._renderValidState.bind(this));
+  }
+
+  _renderValidState(
+    value: boolean | undefined,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    oldValue?: boolean | undefined,
+  ): void {
+    this._$element.toggleClass(STEP_INVALID_CLASS, value !== undefined && !value);
+  }
+}
 
 export default StepperItem;
