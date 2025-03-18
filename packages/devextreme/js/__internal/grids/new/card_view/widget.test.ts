7f658372dec9c1e46224027ea5707b844f2c76e9
Roman Semenov <roman.semenov@devexpress.com>
2025-03-18 06:11:31 +0400

    fix: option leak

--- packages/devextreme/js/__internal/grids/new/card_view/widget.test.ts
+++ packages/devextreme/js/__internal/grids/new/card_view/widget.test.ts
@@ -13,3 +13,26 @@ describe('common', () => {
     });
   });
 });
+
+describe('regressions', () => {
+  it('should not have leaks to defaultOptions after changing option', () => {
+    const container = document.createElement('div');
+    let cardView = new CardView(container, {
+      keyExpr: 'a',
+      dataSource: [{ a: 'a' }],
+    });
+
+    expect(cardView.option('pager.showPageSizeSelector')).toBe(false);
+
+    cardView.option('pager.showPageSizeSelector', true);
+    expect(cardView.option('pager.showPageSizeSelector')).toBe(true);
+
+    cardView.dispose();
+
+    cardView = new CardView(container, {
+      keyExpr: 'a',
+      dataSource: [{ a: 'a' }],
+    });
+    expect(cardView.option('pager.showPageSizeSelector')).toBe(false);
+  });
+});
