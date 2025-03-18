641f715d9aa7caea1f8d27d951160e25c1ff0580
marker dao ® <youdontknow@marker-dao.eth>
2025-03-19 00:08:24 +0700

    feat(ai): Add RequestManager

---
+++ packages/devextreme/js/__internal/core/ai/request_manager.ts
@@ -0,0 +1,34 @@
+import type { AIProvider, Prompt, RequestCallbacks } from '../ai-types';
+
+export class RequestManager {
+  private readonly provider: AIProvider;
+
+  constructor(provider: AIProvider) {
+    this.provider = provider;
+  }
+
+  public sendRequest(prompt: Prompt, callbacks: RequestCallbacks): () => void {
+    if (typeof this.provider.sendRequest === 'function') {
+      let accumulator = '';
+
+      const params = {
+        prompt,
+        onChunk: (chunk: string): void => {
+          accumulator += chunk;
+
+          callbacks.onChunk?.(chunk);
+        },
+      };
+
+      const { promise, abort } = this.provider.sendRequest(params);
+
+      promise
+        .then(() => { callbacks.onComplete?.(accumulator); })
+        .catch((e) => { callbacks.onError?.(e); });
+
+      return abort;
+    }
+
+    throw new Error('No method for queries has been implemented');
+  }
+}
