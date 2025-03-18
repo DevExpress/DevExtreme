92308f60bc166588703443d3904cb01a895ab945
marker dao ® <youdontknow@marker-dao.eth>
2025-03-19 00:02:49 +0700

    feat(ai): Add PromptManager

---
+++ packages/devextreme/js/__internal/core/ai/prompt_manager.ts
@@ -0,0 +1,46 @@
+import type { Prompt } from '../ai-types';
+import { templates } from './templates';
+
+export interface PromptData {
+  system: Record<string, string>;
+  user: Record<string, string>;
+}
+
+export interface PromptTemplate {
+  system: string;
+  user: string;
+}
+
+export type TemplateName = 'translate';
+
+export class PromptManager {
+  private readonly templates: Map<TemplateName, PromptTemplate>;
+
+  constructor() {
+    this.templates = new Map(Object.entries(templates) as [TemplateName, PromptTemplate][]);
+  }
+
+  public buildPrompt(templateName: TemplateName, data: PromptData): Prompt {
+    const template = this.templates.get(templateName);
+
+    if (!template) {
+      throw new Error('Template not found');
+    }
+
+    const system = this.replacePlaceholders(template.system, data.system);
+    const user = this.replacePlaceholders(template.user, data.user);
+
+    const prompt = { system, user };
+
+    return prompt;
+  }
+
+  private replacePlaceholders(prompt: string, placeholders: Record<string, string>): string {
+    const result = Object.entries(placeholders).reduce(
+      (acc, [key, value]) => acc.split(`{{${key}}}`).join(value),
+      prompt,
+    );
+
+    return result;
+  }
+}
---
+++ packages/devextreme/js/__internal/core/ai/templates.ts
@@ -0,0 +1,6 @@
+export const templates = {
+  translate: {
+    system: 'You are a translation assistant.',
+    user: 'Translate {{text}} to {{lang}} language.',
+  },
+};
