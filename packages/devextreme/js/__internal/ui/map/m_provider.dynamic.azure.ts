5e3f77ef63784b390ec8f4b0490814a1f6a42639
Alexander Kozlovskiy <zedwag@gmail.com>
2025-03-20 19:44:01 +0400

    Map - add mapReady promise for Azure provider

--- packages/devextreme/js/__internal/ui/map/m_provider.dynamic.azure.ts
+++ packages/devextreme/js/__internal/ui/map/m_provider.dynamic.azure.ts
@@ -32,6 +32,8 @@ let azureMapsLoader;
 class AzureProvider extends DynamicProvider {
   _preventZoomChangeEvent?: boolean;
 
+  _mapReadyPromise?: Promise<void>;
+
   _mapType(type) {
     const mapTypes = {
       roadmap: 'road',
@@ -165,7 +167,7 @@ class AzureProvider extends DynamicProvider {
   _init() {
     this._createMap();
 
-    return Promise.resolve();
+    return this._mapReadyPromise;
   }
 
   _createMap() {
@@ -177,6 +179,12 @@ class AzureProvider extends DynamicProvider {
       zoom: this._option('zoom'),
       style: this._mapType(this._option('type')),
       interactive: !this._option('disabled'),
+    });
+
+    this._mapReadyPromise = new Promise<void>((resolve) => {
+      this._map.events.add('ready', () => {
+        resolve();
+      });
     });
 
     this.updateControls();
@@ -413,11 +421,10 @@ class AzureProvider extends DynamicProvider {
           });
         }
       }).catch((e) => {
-        const errorMessage = e.responseJSON.error.message;
         const dataSource = new atlas.source.DataSource();
         const lineLayer = new atlas.layer.LineLayer(dataSource, null, {});
 
-        errors.log('W1006', errorMessage);
+        errors.log('W1006', e);
 
         resolve({
           instance: { dataSource, lineLayer },
