e9881c4d01862f6b08f6bd2181541f6ac8f38081
Alyar <>
2025-03-25 10:13:10 +0400

    Fix tests

--- e2e/testcafe-devextreme/tests/dataGrid/filtering.ts
+++ e2e/testcafe-devextreme/tests/dataGrid/filtering.ts
@@ -1,7 +1,7 @@
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import DataGrid from 'devextreme-testcafe-models/dataGrid';
-import url from '../../../helpers/getPageUrl';
-import { createWidget } from '../../../helpers/createWidget';
+import url from '../../helpers/getPageUrl';
+import { createWidget } from '../../helpers/createWidget';
 
 fixture.disablePageReloads`Filtering`
   .page(url(__dirname, '../../container.html'));
