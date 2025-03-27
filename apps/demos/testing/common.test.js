8236d75995b42fb8ec37389d57f3f592bacd2cc7
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2025-03-27 15:45:25 +0400

    Revert "CI: run a single local server for demo tests"
    This reverts commit 5f39d7199568f00992f8a8dc3cca2e4e17d55fb6.

--- apps/demos/testing/common.test.js
+++ apps/demos/testing/common.test.js
@@ -6,7 +6,7 @@ import { existsSync } from 'fs';
 import { compareScreenshot } from 'devextreme-screenshot-comparer';
 import { axeCheck, createReport } from '@testcafe-community/axe';
 import {
-  localServerUrl,
+  getPortByIndex,
   runTestAtPage,
   shouldRunFramework,
   shouldRunTestAtIndex,
@@ -388,10 +388,10 @@ const SKIPPED_TESTS = {
     let pageURL = '';
     const theme = process.env.THEME.replace('generic.', '');
     if (isGitHubDemos) {
-      pageURL = `${localServerUrl}Demos/${widgetName}/${demoName}/${approach}/?theme=dx.${theme}`;
+      pageURL = `http://127.0.0.1:808${getPortByIndex(index)}/Demos/${widgetName}/${demoName}/${approach}/?theme=dx.${theme}`;
     } else {
       changeTheme(__dirname, `../${demoPath}/index.html`, process.env.THEME);
-      pageURL = `${localServerUrl}apps/demos/Demos/${widgetName}/${demoName}/${approach}/`;
+      pageURL = `http://127.0.0.1:808${getPortByIndex(index)}/apps/demos/Demos/${widgetName}/${demoName}/${approach}/`;
     }
     // remove when tests enabled not only for datagrid
     if (isGitHubDemos && (widgetName !== 'DataGrid' || gitHubIgnored.includes(demoName))) {
--- apps/demos/utils/visual-tests/matrix-test-helper.js
+++ apps/demos/utils/visual-tests/matrix-test-helper.js
@@ -4,8 +4,6 @@ import { ClientFunction, Selector } from 'testcafe';
 import { THEME } from './helpers/theme-utils';
 import { gitHubIgnored } from './github-ignored-list';
 
-export const localServerUrl = 'http://localhost:8080/'
-
 const settings = {
   concurrency: undefined,
   targetFramework: undefined,
@@ -367,6 +365,19 @@ export function runTestAtPage(test, demoUrl, shouldSkipJsError) {
   return executor.page(demoUrl);
 }
 
+export function getPortByFramework(framework) {
+  switch (framework) {
+    case 'Angular':
+      return '8081'
+    case 'Vue':
+      return '8082'
+    case 'React':
+      return '8083'
+    default:
+      return '8080'
+  }
+}
+
 export function runManualTestCore(
   testObject,
   widget,
@@ -393,11 +404,11 @@ export function runManualTestCore(
     }
 
     const theme = process.env.THEME.replace('generic.', '');
-    testURL = `${localServerUrl}Demos/${widget}/${demo}/${framework}/?theme=dx.${theme}`;
+    testURL = `http://127.0.0.1:8080/Demos/${widget}/${demo}/${framework}/?theme=dx.${theme}`;
   } else {
     changeTheme(__dirname, `../../Demos/${widget}/${demo}/${framework}/index.html`, process.env.THEME);
     
-    testURL = `${localServerUrl}apps/demos/Demos/${widget}/${demo}/${framework}/`;
+    testURL = `http://127.0.0.1:${getPortByFramework(framework)}/apps/demos/Demos/${widget}/${demo}/${framework}/`;
   }
 
   const test = testObject.clientScripts([
@@ -439,6 +450,10 @@ export function runManualTest(widget, demo, framework, callback) {
   } else {
     runManualTestCore(test, widget, demo, framework, callback);
   }
+}
+
+export function getPortByIndex(testIndex) {
+  return (settings.total && (Math.floor(testIndex / settings.total) % settings.concurrency)) || 0;
 }
 
 export function updateConfig(customSettings) {
