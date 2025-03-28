6fd67b68504be53afe7a92f6925c97968d9be7f2
marker dao ® <youdontknow@marker-dao.eth>
2025-03-28 14:21:53 +0700

    refactor(translate.test.ts): Remove mocks

--- packages/devextreme/js/__internal/core/ai/commands/translate.test.ts
+++ packages/devextreme/js/__internal/core/ai/commands/translate.test.ts
@@ -7,12 +7,13 @@ import {
 } from '@jest/globals';
 import type { AIProvider, RequestCallbacks, TranslateCommandParams } from '@js/ai/ai';
 import { TranslateCommand } from '@ts/core/ai/commands/translate';
-// import type { PromptData } from '@ts/core/ai/core/prompt_manager';
+import type { PromptData } from '@ts/core/ai/core/prompt_manager';
 import { PromptManager } from '@ts/core/ai/core/prompt_manager';
 import { RequestManager } from '@ts/core/ai/core/request_manager';
 import { Provider } from '@ts/core/ai/testUtils/provider_mock';
 
 describe('TranslateCommand', () => {
+  const params: TranslateCommandParams = { text: 'text to translate', lang: 'French' };
   // eslint-disable-next-line @typescript-eslint/init-declarations
   let promptManager: PromptManager;
   // eslint-disable-next-line @typescript-eslint/init-declarations
@@ -29,51 +30,46 @@ describe('TranslateCommand', () => {
     command = new TranslateCommand(promptManager, requestManager);
   });
 
-  // describe('getTemplateName', () => {
-  //   it('returns name of template correctly', () => {
-  //     // @ts-expect-error
-  //     const templateName = command.getTemplateName();
+  describe('getTemplateName', () => {
+    it('returns name of template correctly', () => {
+      // @ts-expect-error
+      const templateName = command.getTemplateName();
 
-  //     expect(templateName).toBe('translate');
-  //   });
-  // });
+      expect(templateName).toBe('translate');
+    });
+  });
 
-  // describe('buildPromptData', () => {
-  //   it('forms PromptData with text and lang in user-section', () => {
-  //     const params: TranslateCommandParams = {
-  //       text: 'Hello world',
-  //       lang: 'French',
-  //     };
+  describe('buildPromptData', () => {
+    it('forms PromptData with text and lang in user-section', () => {
+      // @ts-expect-error
+      const promptData: PromptData = command.buildPromptData(params);
 
-  //     // @ts-expect-error
-  //     const promptData: PromptData = command.buildPromptData(params);
+      expect(promptData).toEqual({
+        system: {
+          lang: 'French',
+        },
+        user: {
+          text: 'text to translate',
+          lang: 'French',
+        },
+      });
+    });
+  });
 
-  //     expect(promptData).toEqual({
-  //       user: {
-  //         text: 'Hello world',
-  //         lang: 'French',
-  //       },
-  //       system: {
-  //         lang: 'French',
-  //       },
-  //     });
-  //   });
-  // });
+  describe('parseResult', () => {
+    it('returns the string without changes', () => {
+      const response = 'Translated text';
+      // @ts-expect-error
+      const result = command.parseResult(response);
 
-  // describe('parseResult', () => {
-  //   it('returns the string without changes', () => {
-  //     const response = 'Translated text';
-  //     // @ts-expect-error
-  //     const result = command.parseResult(response);
-
-  //     expect(result).toBe(response);
-  //   });
-  // });
+      expect(result).toBe(response);
+    });
+  });
 
   describe('execute', () => {
     it('correctly calls promptManager.buildPrompt and returns the abort function', () => {
-      const params: TranslateCommandParams = { text: 'text to translate', lang: 'French' };
       const callbacks: RequestCallbacks = { onComplete: () => {} };
+
       const buildPromptSpy = jest.spyOn(promptManager, 'buildPrompt');
       const sendRequestSpy = jest.spyOn(requestManager, 'sendRequest');
 
@@ -95,48 +91,48 @@ describe('TranslateCommand', () => {
       expect(sendRequestSpy).toHaveBeenCalledTimes(1);
     });
 
-    // it('pulls onChunk callback when getting a chunk', () => {
-    //   const callbacks: RequestCallbacks = { onChunk: jest.fn() };
+    it('pulls onChunk callback when getting a chunk', () => {
+      const callbacks: RequestCallbacks = { onChunk: jest.fn() };
+      const sendRequestSpy = jest.spyOn(requestManager, 'sendRequest');
 
-    //   command.execute({ text: 'text to translate', lang: 'French' }, callbacks);
+      command.execute(params, callbacks);
 
-    //   const sendRequestParams = (requestManager.sendRequest as jest.Mock).mock.calls[0];
-    //   const sendRequestCallbacks = sendRequestParams[1] as RequestCallbacks;
+      const sendRequestCallbacks = sendRequestSpy.mock.calls[0][1];
 
-    //   sendRequestCallbacks.onChunk?.('chunk');
+      sendRequestCallbacks.onChunk?.('chunk');
 
-    //   expect(callbacks.onChunk).toHaveBeenCalledWith('chunk');
-    // });
+      expect(callbacks.onChunk).toHaveBeenCalledWith('chunk');
+    });
 
-    // it('calls onComplete callback, passing the result of parseResult to it', () => {
-    //   const callbacks: RequestCallbacks = { onComplete: jest.fn() };
-    //   // @ts-expect-error
-    //   const spy = jest.spyOn(command, 'parseResult');
+    it('calls onComplete callback, passing the result of parseResult to it', () => {
+      const callbacks: RequestCallbacks = { onComplete: jest.fn() };
 
-    //   command.execute({ text: 'text to translate', lang: 'French' }, callbacks);
+      const sendRequestSpy = jest.spyOn(requestManager, 'sendRequest');
+      // @ts-expect-error
+      const parseResultSpy = jest.spyOn(command, 'parseResult');
 
-    //   const sendRequestParams = (requestManager.sendRequest as jest.Mock).mock.calls[0];
-    //   const sendRequestCallbacks = sendRequestParams[1] as RequestCallbacks;
+      command.execute(params, callbacks);
 
-    //   sendRequestCallbacks.onComplete?.('AI response');
+      const sendRequestCallbacks = sendRequestSpy.mock.calls[0][1];
 
-    //   expect(spy).toHaveBeenCalledWith('AI response');
-    //   expect(callbacks.onComplete).toHaveBeenCalledWith('AI response');
-    // });
+      sendRequestCallbacks.onComplete?.('AI response');
 
-    // it('calls onError callback when an error occurs', () => {
-    //   const callbacks: RequestCallbacks = { onError: jest.fn() };
+      expect(parseResultSpy).toHaveBeenCalledWith('AI response');
+      expect(callbacks.onComplete).toHaveBeenCalledWith('AI response');
+    });
 
-    //   command.execute({ text: 'text to translate', lang: 'French' }, callbacks);
+    it('calls onError callback when an error occurs', () => {
+      const callbacks: RequestCallbacks = { onError: jest.fn() };
+      const sendRequestSpy = jest.spyOn(requestManager, 'sendRequest');
 
-    //   const sendRequestParams = (requestManager.sendRequest as jest.Mock).mock.calls[0];
-    //   const sendRequestCallbacks = sendRequestParams[1] as RequestCallbacks;
+      command.execute(params, callbacks);
 
-    //   const error = new Error('Test error');
+      const sendRequestCallbacks = sendRequestSpy.mock.calls[0][1];
+      const error = new Error('Test error');
 
-    //   sendRequestCallbacks.onError?.(error);
+      sendRequestCallbacks.onError?.(error);
 
-    //   expect(callbacks.onError).toHaveBeenCalledWith(error);
-    // });
+      expect(callbacks.onError).toHaveBeenCalledWith(error);
+    });
   });
 });
