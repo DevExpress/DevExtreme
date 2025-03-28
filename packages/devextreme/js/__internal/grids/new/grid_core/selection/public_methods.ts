82c976e66ab5330cb9689b2919fb8510102ef8ea
Alyar <>
2025-03-28 05:26:01 +0400

    CardView: Add public methods

--- packages/devextreme/js/__internal/grids/new/grid_core/selection/public_methods.ts
+++ packages/devextreme/js/__internal/grids/new/grid_core/selection/public_methods.ts
@@ -1,5 +1,8 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
+import type { DeferredObj } from '@js/core/utils/deferred';
+
+import type { DataRow } from '../columns_controller/types';
 import type { Key } from '../data_controller/types';
 import type { Constructor } from '../types';
 import type { GridCoreNewBase } from '../widget';
@@ -10,12 +13,40 @@ export function PublicMethods<TBase extends Constructor<GridCoreNewBase>>(GridCo
       return this.selectionController.isCardSelected(key);
     }
 
-    public clearSelection(): void {
-      this.selectionController.clearSelection();
-    }
-
     public getSelectedCardKeys(): Key[] {
       return this.selectionController.getSelectedCardKeys();
+    }
+
+    public getSelectedCards(): DataRow[] {
+      return this.selectionController.getSelectedCards();
+    }
+
+    public selectCards(keys: Key[], preserve = false): DeferredObj<unknown> | undefined {
+      return this.selectionController.selectCards(keys, preserve);
+    }
+
+    public deselectCards(keys: Key[]): DeferredObj<unknown> | undefined {
+      return this.selectionController.deselectCards(keys);
+    }
+
+    public selectCardsByIndexes(indexes: number[]): DeferredObj<unknown> | undefined {
+      return this.selectionController.selectCardsByIndexes(indexes);
+    }
+
+    public deselectCardsByIndexes(indexes: number[]): DeferredObj<unknown> | undefined {
+      return this.selectionController.deselectCardsByIndexes(indexes);
+    }
+
+    public selectAll(): DeferredObj<unknown> | undefined {
+      return this.selectionController.selectAll();
+    }
+
+    public deselectAll(): DeferredObj<unknown> | undefined {
+      return this.selectionController.deselectAll();
+    }
+
+    public clearSelection(): void {
+      this.selectionController.clearSelection();
     }
   };
 }
