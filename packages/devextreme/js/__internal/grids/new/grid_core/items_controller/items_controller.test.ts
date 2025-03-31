021d7ee92043dcb0de3ed351f497330c8a9fd331
Roman Semenov <roman.semenov@devexpress.com>
2025-04-01 00:45:39 +0400

    itemscontroller

--- packages/devextreme/js/__internal/grids/new/grid_core/items_controller/items_controller.test.ts
+++ packages/devextreme/js/__internal/grids/new/grid_core/items_controller/items_controller.test.ts
@@ -1,20 +1,27 @@
 /* eslint-disable spellcheck/spell-checker */
 import { describe, expect, it } from '@jest/globals';
+import { SearchController } from '@ts/grids/new/grid_core/search';
 
 import { ColumnsController } from '../columns_controller/columns_controller';
 import { DataController } from '../data_controller';
+import { FilterController } from '../filtering';
 import type { Options } from '../options';
 import { OptionsControllerMock } from '../options_controller/options_controller.mock';
+import { SortingController } from '../sorting_controller';
 import { ItemsController } from './items_controller';
 
 const setup = (config: Options = {}) => {
   const options = new OptionsControllerMock(config);
-
-  const dataController = new DataController(options);
-
   const columnsController = new ColumnsController(options);
-
-  const itemsController = new ItemsController(dataController, columnsController);
+  const filterController = new FilterController(options);
+  const sortingController = new SortingController(options, columnsController);
+  const searchController = new SearchController(options);
+  const dataController = new DataController(options, sortingController, filterController);
+  const itemsController = new ItemsController(
+    dataController,
+    columnsController,
+    searchController,
+  );
 
   return {
     options,
@@ -54,8 +61,21 @@ describe('ItemsController', () => {
       });
 
       const columns = columnsController.columns.unreactive_get();
-      const dataRow = itemsController.createDataRow(dataObject, columns, 0);
+      const dataRow = itemsController.createDataRow(dataObject, columns, 0, [1]);
       expect(dataRow).toMatchSnapshot();
+    });
+  });
+
+  describe('setSelectionState', () => {
+    it('should update the select state of the item', () => {
+      const { itemsController } = setup({
+        keyExpr: 'id',
+        dataSource: [{ id: 1, a: 'my a value' }],
+      });
+
+      itemsController.setSelectionState([1]);
+
+      expect(itemsController.items).toMatchSnapshot();
     });
   });
 });
