3754399a1798c4c9b63822ce7683659aa1d6b929
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2025-03-31 20:33:06 +0400

    Splitter: resizeHandle should not be duplicated when changing the template option at runtime (T1279224)

--- packages/devextreme/js/__internal/ui/splitter/splitter.ts
+++ packages/devextreme/js/__internal/ui/splitter/splitter.ts
@@ -545,6 +545,7 @@ class Splitter extends CollectionWidget<Properties> {
           event,
           handleElement: getPublicElement<HTMLElement>($resizeHandle),
         };
+
         // eslint-disable-next-line @typescript-eslint/no-floating-promises
         this._feedbackDeferred?.resolve();
         this._toggleActiveState($resizeHandle, false);
@@ -683,6 +684,12 @@ class Splitter extends CollectionWidget<Properties> {
         this._itemVisibleOptionChanged(item, value as PropertyType, prevValue as PropertyType);
         break;
       }
+      case 'template':
+        super._itemOptionChanged(item, property, value, prevValue);
+
+        this._applyStylesFromLayout(this.getLayout());
+        this._updateResizeHandles();
+        break;
       default:
         super._itemOptionChanged(item, property, value, prevValue);
     }
