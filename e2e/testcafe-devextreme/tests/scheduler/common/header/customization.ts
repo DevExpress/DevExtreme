b03bfa7d74916084f4df912b2a367f5c81c48afe
Vladimir Bushmanov <vladimir.bushmanov@devexpress.com>
2025-04-01 14:35:46 +0500

    update screenshots

--- e2e/testcafe-devextreme/tests/scheduler/common/header/customization.ts
+++ e2e/testcafe-devextreme/tests/scheduler/common/header/customization.ts
@@ -36,10 +36,18 @@ test('Scheduler default toolbar should works', async (t) => {
   currentDate: new Date(2021, 3, 27),
 }));
 
+test('Scheduler toolbar should be hided', async (t) => {
+  const scheduler = new Scheduler('#container');
+
+  await t.expect(scheduler.toolbar.element.exists).notOk();
+}).before(async () => createWidget('dxScheduler', {
+  currentDate: new Date(2021, 3, 27),
+  toolbar: { visible: false },
+}));
+
 [
   { toolbar: { items: customToolbarItems }, description: 'custom toolbar' },
   { toolbar: { items: ['today', 'dateNavigator', 'viewSwitcher'] }, description: 'toolbar with today' },
-  { toolbar: { visible: false }, description: 'hided toolbar' },
   { toolbar: { disabled: true, items: customToolbarItems }, description: 'disabled toolbar' },
 ].forEach(({ toolbar, description }) => {
   test(`Scheduler ${description} should works`, async (t) => {
