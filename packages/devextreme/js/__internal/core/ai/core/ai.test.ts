d98051d4ba1f9152ab07db4df3036976c51fdb43
marker dao ® <youdontknow@marker-dao.eth>
2025-04-02 20:56:52 +0700

    feat(ai): Add Command Store && Add tests

--- packages/devextreme/js/__internal/core/ai/core/ai.test.ts
+++ packages/devextreme/js/__internal/core/ai/core/ai.test.ts
@@ -1,4 +1,5 @@
 import {
+  afterEach,
   beforeEach,
   describe,
   expect,
@@ -11,13 +12,14 @@ import type {
   TranslateCommandParams,
 } from '@js/ai';
 import { TranslateCommand } from '@ts/core/ai/commands/translate';
-import { AI } from '@ts/core/ai/core/ai';
+import { AI, CommandNames } from '@ts/core/ai/core/ai';
 import { PromptManager } from '@ts/core/ai/core/prompt_manager';
 import { RequestManager } from '@ts/core/ai/core/request_manager';
 import { Provider } from '@ts/core/ai/test_utils/provider_mock';
 
 describe('AI', () => {
   const params: TranslateCommandParams = { text: 'text for translation', lang: 'French' };
+  const params2: TranslateCommandParams = { text: 'text for translation 2', lang: 'Spanish' };
 
   let provider = null as unknown as AIProvider;
   let ai = null as unknown as AI;
@@ -25,6 +27,10 @@ describe('AI', () => {
   beforeEach(() => {
     provider = new Provider();
     ai = new AI(provider);
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
   });
 
   describe('constructor', () => {
@@ -63,6 +69,31 @@ describe('AI', () => {
       const abortRequest = ai.translate(params, {});
 
       expect(abortRequest).toBe(abort);
+    });
+
+    it('reuses the same command instance for multiple translate calls', () => {
+      const callbacks: RequestCallbacks = {};
+      const executeSpy = jest.spyOn(TranslateCommand.prototype, 'execute');
+
+      ai.translate(params, callbacks);
+
+      // @ts-expect-error Access to protected property for a test
+      const commandsMap = ai.commands;
+      const translateCommand = commandsMap.get(CommandNames.Translate);
+
+      expect(commandsMap.size).toBe(1);
+      expect(translateCommand).toBeInstanceOf(TranslateCommand);
+
+      expect(executeSpy).toHaveBeenCalledTimes(1);
+      expect(executeSpy).toHaveBeenCalledWith(params, callbacks);
+
+      ai.translate(params2, callbacks);
+
+      expect(commandsMap.size).toBe(1);
+      expect(commandsMap.get(CommandNames.Translate)).toBe(translateCommand);
+
+      expect(executeSpy).toHaveBeenCalledTimes(2);
+      expect(executeSpy).toHaveBeenLastCalledWith(params2, callbacks);
     });
   });
 });
--- packages/devextreme/js/__internal/core/ai/core/ai.ts
+++ packages/devextreme/js/__internal/core/ai/core/ai.ts
@@ -1,46 +1,69 @@
 import type {
   AI as IAI,
   AIProvider,
-  BaseCommandResult,
   RequestCallbacks,
   TranslateCommandParams,
   TranslateCommandResult,
 } from '@js/ai';
-import type { BaseCommand } from '@ts/core/ai/commands/base';
 import { TranslateCommand } from '@ts/core/ai/commands/translate';
 import { PromptManager } from '@ts/core/ai/core/prompt_manager';
 import { RequestManager } from '@ts/core/ai/core/request_manager';
+
+export const enum CommandNames {
+  Translate = 'translate',
+}
+
+export interface Commands {
+  [CommandNames.Translate]: {
+    params: TranslateCommandParams;
+    result: TranslateCommandResult;
+    command: TranslateCommand;
+  };
+}
+
+export const COMMANDS: {
+  [K in CommandNames]: new (
+    promptManager: PromptManager,
+    requestManager: RequestManager
+  ) => Commands[K]['command'];
+} = {
+  [CommandNames.Translate]: TranslateCommand,
+};
 
 export class AI implements IAI {
   private readonly promptManager: PromptManager;
 
   private readonly requestManager: RequestManager;
 
+  private readonly commands: Map<CommandNames, Commands[CommandNames]['command']>;
+
   constructor(provider: AIProvider) {
     this.promptManager = new PromptManager();
     this.requestManager = new RequestManager(provider);
+    this.commands = new Map();
   }
 
-  private execute<
-    TParams,
-    TResult extends BaseCommandResult,
-    F extends BaseCommand<TParams, TResult>,
-  >(
-    Command: new (
-      promptManager: PromptManager,
-      requestManager: RequestManager,
-    ) => F,
-    params: TParams,
+  private execute<K extends CommandNames>(
+    commandName: K,
+    params: Commands[K]['params'],
     callbacks: RequestCallbacks,
   ): () => void {
-    const command = new Command(this.promptManager, this.requestManager);
+    let command = this.commands.get(commandName) as Commands[K]['command'] | undefined;
+
+    if (!command) {
+      const Command = COMMANDS[commandName];
+
+      command = new Command(this.promptManager, this.requestManager);
+
+      this.commands.set(commandName, command);
+    }
 
     return command.execute(params, callbacks);
   }
 
   public translate(params: TranslateCommandParams, callbacks: RequestCallbacks): () => void {
-    return this.execute<TranslateCommandParams, TranslateCommandResult, TranslateCommand>(
-      TranslateCommand,
+    return this.execute(
+      CommandNames.Translate,
       params,
       callbacks,
     );
