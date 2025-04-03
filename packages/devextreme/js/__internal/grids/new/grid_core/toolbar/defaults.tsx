0edc738d696d761d88fd832bf1ecd9d9d897ed1b
Roman Semenov <roman.semenov@devexpress.com>
2025-04-03 19:38:22 +0400

    CardView - add needRender param to toolbar (#29511)
    Co-authored-by: Eldar Iusupzhanov <84278206+Tucchhaa@users.noreply.github.com>

--- packages/devextreme/js/__internal/grids/new/grid_core/toolbar/defaults.tsx
+++ packages/devextreme/js/__internal/grids/new/grid_core/toolbar/defaults.tsx
@@ -2,5 +2,9 @@
  * used for defining default toolbar items and their default order in header panel
  */
 export const DEFAULT_TOOLBAR_ITEMS = [
+  'columnChooserButton',
   'searchPanel',
+  'addCardButton',
+  'selectAllButton',
+  'clearSelectionButton',
 ] as const;
