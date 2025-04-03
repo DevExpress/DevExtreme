fc321cf952225579c03061ac787403145775be4e
Roman Semenov <roman.semenov@devexpress.com>
2025-04-03 09:32:51 +0400

    fix

--- e2e/testcafe-devextreme/tests/cardView/headerPanel.ts
+++ e2e/testcafe-devextreme/tests/cardView/headerPanel.ts
@@ -2,6 +2,7 @@ import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import CardView from 'devextreme-testcafe-models/cardView';
 import url from '../../helpers/getPageUrl';
 import { createWidget } from '../../helpers/createWidget';
+import { testScreenshot } from '../../helpers/themeUtils';
 
 fixture.disablePageReloads`CardView - HeaderPanel`
   .page(url(__dirname, '../container.html'));
@@ -10,7 +11,7 @@ test('default render', async (t) => {
   const cardView = new CardView('#container');
   const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-  await takeScreenshot('header-panel', cardView.getHeaderPanel().element);
+  await testScreenshot(t, takeScreenshot, 'card-view_search_text-highlighting.png', { element: cardView.element });
 
   await t
     .expect(compareResults.isValid())
