28ce878f47fd750b9b636340de1ab184f37fbbfc
Roman Semenov <roman.semenov@devexpress.com>
2025-04-07 08:26:34 +0400

    fix

--- e2e/testcafe-devextreme/tests/cardView/headerFilter/functional.ts
+++ e2e/testcafe-devextreme/tests/cardView/headerFilter/functional.ts
@@ -283,6 +283,7 @@ test('should update column options with filterType and values (selectAll case #1
 
   await t.click(cardView.element);
 }).before(async () => createWidget('dxCardView', {
+  columns: ['A', 'B', 'C'],
   dataSource: [
     { A: 'A_0', B: 'B_0', C: 'C_0' },
     { A: 'A_1', B: 'B_1', C: 'C_1' },
