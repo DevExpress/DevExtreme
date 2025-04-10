323b73b32694f2b0bcfdd1dbfa02e2f3ce1bbdf1
Eldar Iusupzhanov <ferrarijed@gmail.com>
2025-04-10 20:29:54 +0800

    register basecontroller in cardview

--- packages/devextreme/js/__internal/grids/new/card_view/di.ts
+++ packages/devextreme/js/__internal/grids/new/card_view/di.ts
@@ -1,6 +1,7 @@
 /* eslint-disable spellcheck/spell-checker */
 import type { DIContext } from '@ts/core/di';
 
+import { BaseContextMenuController } from '../grid_core/context_menu/controller';
 import { register as gridCoreDIRegister } from '../grid_core/di';
 import * as ContentViewModule from './content_view/index';
 import { ContextMenuController } from './context_menu/controller';
@@ -13,5 +14,7 @@ export function register(diContext: DIContext): void {
   diContext.register(ContentViewModule.View);
   diContext.register(HeaderPanelView);
   diContext.register(ContextMenuView);
+
   diContext.register(ContextMenuController);
+  diContext.register(BaseContextMenuController, ContextMenuController);
 }
