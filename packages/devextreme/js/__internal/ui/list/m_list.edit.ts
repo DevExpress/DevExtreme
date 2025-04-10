90eb90e4c845b29f8ca9141f4ab36bda2ee795f9
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2025-04-10 10:47:26 +0400

    List should not display "[object Object]" in the aria-label attribute when in multiple selection mode, especially when displayExpr and default text field are missed (T1285078)

--- packages/devextreme/js/__internal/ui/list/m_list.edit.ts
+++ packages/devextreme/js/__internal/ui/list/m_list.edit.ts
@@ -4,7 +4,7 @@ import type { dxElementWrapper } from '@js/core/renderer';
 import $ from '@js/core/renderer';
 import { extend } from '@js/core/utils/extend';
 import type { Item } from '@js/ui/list';
-import { isNumeric } from '@ts/core/utils/m_type';
+import { isNumeric, isObject } from '@ts/core/utils/m_type';
 import type { OptionChanged } from '@ts/core/widget/types';
 
 import type { ListBaseProperties } from './m_list.base';
@@ -229,11 +229,15 @@ class ListEdit extends ListBase {
     return $itemFrame;
   }
 
-  _updateItemAriaLabel($itemFrame, itemData) {
+  _updateItemAriaLabel($itemFrame: dxElementWrapper, itemData: Item): void {
     // @ts-expect-error ts-error
     const label = this._displayGetter?.(itemData) ?? itemData?.text ?? itemData;
 
-    this.setAria('label', label, $itemFrame);
+    this.setAria(
+      'label',
+      isObject(label) ? localizationMessage.format('dxList-listAriaLabel-itemContent') : label,
+      $itemFrame,
+    );
   }
 
   _selectedItemClass() {
