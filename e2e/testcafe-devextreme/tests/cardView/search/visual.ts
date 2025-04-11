9d4223ee7dca732aa24ffa2e01efc7011aff6175
Roman Semenov <roman.semenov@devexpress.com>
2025-04-11 11:43:59 +0400

    fix lint

--- e2e/testcafe-devextreme/tests/cardView/search/visual.ts
+++ e2e/testcafe-devextreme/tests/cardView/search/visual.ts
@@ -3,7 +3,6 @@ import CardView from 'devextreme-testcafe-models/cardView';
 import url from '../../../helpers/getPageUrl';
 import { createWidget } from '../../../helpers/createWidget';
 import { testScreenshot } from '../../../helpers/themeUtils';
-import { baseConfig } from '../filterPanel/helpers/baseConfig';
 
 fixture`Search.Visual`
   .page(url(__dirname, '../../container.html'));
