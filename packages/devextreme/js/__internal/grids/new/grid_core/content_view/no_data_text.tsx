ff8f50397181bc54ed1b6059e0d237f85428f547
Roman Semenov <roman.semenov@devexpress.com>
2025-04-14 08:58:29 +0400

    fix: nodata element

--- packages/devextreme/js/__internal/grids/new/grid_core/content_view/no_data_text.tsx
+++ packages/devextreme/js/__internal/grids/new/grid_core/content_view/no_data_text.tsx
@@ -1,26 +1,39 @@
 import type { ComponentType, InfernoNode } from 'inferno';
 import { Component } from 'inferno';
 
+import { Icon } from '../icon';
+
 export interface NoDataTextProperties {
   text: string;
   template?: ComponentType<{ text: string }>;
 }
 
 export const CLASSES = {
-  noData: 'dx-gridcore-nodata',
+  container: 'dx-gridcore-nodata-container',
+  element: 'dx-gridcore-nodata-element',
+  iconContainer: 'dx-gridcore-nodata-icon-container',
+  text: 'dx-gridcore-nodata-text',
 };
 
 export class NoDataText extends Component<NoDataTextProperties> {
   public render(): InfernoNode {
-    if (this.props.template) {
-      const Template = this.props.template;
-      return <Template text={this.props.text} />;
-    }
+    const Template = this.props.template;
 
     return (
-      <span className={CLASSES.noData}>
-        { this.props.text }
-      </span>
+      <div className={CLASSES.container}>
+        {Template ? (
+          <Template text={this.props.text} />
+        ) : (
+          <div className={CLASSES.element}>
+            <div className={CLASSES.iconContainer}>
+              <Icon name='cardcontent'/>
+            </div>
+            <div className={CLASSES.text}>
+              { this.props.text }
+            </div>
+          </div>
+        )}
+      </div>
     );
   }
 }
