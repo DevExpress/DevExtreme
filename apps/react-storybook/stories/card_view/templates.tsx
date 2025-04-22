a1653ebde2a536e498834acc7c3ea85880566cb2
Roman Semenov <roman.semenov@devexpress.com>
2025-04-22 08:07:49 +0400

    CardView - enhance storybook (#29634)

--- apps/react-storybook/stories/card_view/templates.tsx
+++ apps/react-storybook/stories/card_view/templates.tsx
@@ -2,6 +2,11 @@ import Button from "devextreme/ui/button"
 
 export function renderFooter() {
   const container = document.createElement('div');
+  container.style.display = 'flex';
+  container.style.justifyContent = 'space-evenly';
+  container.style.padding = '5px';
+  container.style.backgroundColor = 'red';
+
   const button1 = document.createElement('div');
   const button2 = document.createElement('div');
 
