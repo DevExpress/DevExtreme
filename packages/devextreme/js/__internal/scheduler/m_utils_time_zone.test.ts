738bbf1d35e89b3229a0847707069c91b1b19131
Vladimir Bushmanov <vladimir.bushmanov@devexpress.com>
2025-04-23 06:19:54 -0700

    fix tests

--- packages/devextreme/js/__internal/scheduler/m_utils_time_zone.test.ts
+++ packages/devextreme/js/__internal/scheduler/m_utils_time_zone.test.ts
@@ -4,7 +4,6 @@ import {
 import { macroTaskArray } from '@ts/scheduler/utils/index';
 
 import timeZoneUtils from './m_utils_time_zone';
-import { timeZonesMock } from './m_utils_time_zone.mock';
 import timeZoneList from './timezones/timezone_list';
 
 const defaultTimeZones = timeZoneList.value;
@@ -38,9 +37,17 @@ describe('timezone utils', () => {
 
   describe('getTimeZones', () => {
     it('should return timezones with offsets of default timezones list', () => {
+      timeZoneList.value = [
+        'Etc/GMT+12',
+        'Etc/GMT+11',
+      ];
       expect(timeZoneUtils.getTimeZones(
         new Date('2025-04-23T10:00:00Z'),
-      )).toEqual(timeZonesMock);
+      )).toEqual([
+        { id: 'Etc/GMT+12', title: '(GMT -12:00) Etc - GMT+12', offset: -12 },
+        { id: 'Etc/GMT+11', title: '(GMT -11:00) Etc - GMT+11', offset: -11 },
+      ]);
+      timeZoneList.value = defaultTimeZones;
     });
 
     it('should return timezones with offsets of custom timezones list', () => {
