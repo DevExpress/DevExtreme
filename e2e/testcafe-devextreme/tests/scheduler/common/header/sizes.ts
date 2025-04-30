f0ec1e99056aa8b60a77d4ff87840a68e5d9d701
Vladimir Bushmanov <vladimir.bushmanov@devexpress.com>
2025-04-30 18:22:26 +0500

    add test of header appearance. fix screenshots

---
+++ e2e/testcafe-devextreme/tests/scheduler/common/header/sizes.ts
@@ -0,0 +1,55 @@
+import { compareScreenshot } from 'devextreme-screenshot-comparer';
+import Scheduler from 'devextreme-testcafe-models/scheduler';
+import { createWidget } from '../../../../helpers/createWidget';
+import url from '../../../../helpers/getPageUrl';
+import { changeTheme } from '../../../../helpers/changeTheme';
+import { Themes } from '../../../../helpers/themes';
+
+fixture.disablePageReloads`Scheduler header sizes`
+  .page(url(__dirname, '../../../container.html'));
+
+const buttons = Array.from({ length: 4 }).map((_, index) => ({
+  location: 'before',
+  locateInMenu: 'auto',
+  widget: 'dxButton',
+  options: { text: `Button ${index}` },
+}));
+
+test('items inside toolbar menu should stretch', async (t) => {
+  const scheduler = new Scheduler('#container');
+
+  await t.click(scheduler.toolbar.menuButton);
+  await t.expect(await compareScreenshot(t, 'scheduler-toolbar-menu.png', scheduler.toolbar.menuOverlay)).ok();
+}).before(async () => {
+  await changeTheme('fluent.blue.light');
+  return createWidget('dxScheduler', {
+    width: 320,
+    toolbar: {
+      items: ['today', 'dateNavigator', ...buttons, {
+        location: 'after',
+        locateInMenu: 'auto',
+        name: 'viewSwitcher',
+      }],
+    },
+  });
+});
+
+[
+  Themes.genericLight,
+  Themes.genericLightCompact,
+  Themes.materialBlue,
+  Themes.materialBlueCompact,
+  Themes.fluentBlue,
+  Themes.fluentBlueCompact,
+].forEach((theme) => {
+  test('Scheduler header should have correct sizes', async (t) => {
+    const scheduler = new Scheduler('#container');
+
+    await t.expect(await compareScreenshot(t, `scheduler-toolbar-${theme}.png`, scheduler.toolbar.element)).ok();
+  }).before(async () => {
+    await changeTheme(theme);
+    return createWidget('dxScheduler', {
+      toolbar: { items: ['today', 'dateNavigator', ...buttons, 'viewSwitcher'] },
+    });
+  });
+});
--- packages/testcafe-models/scheduler/toolbar.ts
+++ packages/testcafe-models/scheduler/toolbar.ts
@@ -4,6 +4,8 @@ import ViewSwitcher from './viewSwitcher';
 const CLASS = {
   toolbar: 'dx-toolbar',
   toolbarTodayButton: 'dx-scheduler-today',
+  menuButton: 'dx-toolbar-menu-container',
+  menuOverlay: 'dx-overlay-content',
 };
 
 export default class Toolbar {
@@ -15,10 +17,16 @@ export default class Toolbar {
 
   readonly viewSwitcher: ViewSwitcher;
 
+  readonly menuButton: Selector;
+
+  readonly menuOverlay: Selector;
+
   constructor(scheduler: Selector) {
     this.element = scheduler.find(`.${CLASS.toolbar}`);
     this.todayButton = this.element.find(`.${CLASS.toolbarTodayButton}`);
     this.navigator = new Navigator(this.element);
     this.viewSwitcher = new ViewSwitcher(this.element);
+    this.menuButton = this.element.find(`.${CLASS.menuButton}`);
+    this.menuOverlay = this.element.find(`.${CLASS.menuOverlay}`);
   }
 }
