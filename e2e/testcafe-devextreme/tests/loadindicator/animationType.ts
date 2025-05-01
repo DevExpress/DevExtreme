a40a7396065ea1c934921306c0e0df4d472628d2
marker dao ® <youdontknow@marker-dao.eth>
2025-05-01 13:08:36 +0700

    feat(li-testcafe): Add screenshot tests

---
+++ e2e/testcafe-devextreme/tests/loadindicator/animationType.ts
@@ -0,0 +1,47 @@
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import LoadIndicator from 'devextreme-testcafe-models/loadindicator';
+import url from '../../helpers/getPageUrl';
+import { createWidget } from '../../helpers/createWidget';
+import { getFullThemeName, testScreenshot } from '../../helpers/themeUtils';
+import { insertStylesheetRulesToPage } from '../../helpers/domUtils';
+
+const LOADINDICATOR_SEGMENT_CLASS = 'dx-loadindicator-segment';
+
+fixture.disablePageReloads`LoadIndicator`
+  .page(url(__dirname, '../container.html'));
+
+['circle', 'sparkle'].forEach((animationType) => {
+  test(`LoadIndicator: start stage of the ${animationType} animation`, async (t) => {
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+    const loadIndicator = new LoadIndicator('#container');
+
+    await testScreenshot(t, takeScreenshot, `LoadIndicator with ${animationType} animation.png`, {
+      element: '#container',
+      compactCallBack: async () => {
+        await loadIndicator.repaint();
+      },
+    });
+
+    const darkTheme = getFullThemeName().replace('light', 'dark');
+
+    await testScreenshot(t, takeScreenshot, `LoadIndicator with ${animationType} animation.png`, {
+      element: '#container',
+      theme: darkTheme,
+      themeChanged: async () => {
+        await loadIndicator.repaint();
+      },
+    });
+
+    await t
+      .expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
+  }).before(async () => {
+    await insertStylesheetRulesToPage(`.${LOADINDICATOR_SEGMENT_CLASS} { animation: none !important; }`);
+
+    return createWidget('dxLoadIndicator', {
+      width: 128,
+      height: 128,
+      _animationType: animationType,
+    });
+  });
+});
