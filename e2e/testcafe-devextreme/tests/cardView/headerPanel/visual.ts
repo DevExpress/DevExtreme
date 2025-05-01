38add4b7cee715aeaba19e2cb05724d21729752a
wdevfx <ilya.vinogradov@devexpress.com>
2025-05-01 14:51:06 +0400

    tests(CardView): add e2e tests for header (visual + a11y checks)

---
+++ e2e/testcafe-devextreme/tests/cardView/headerPanel/visual.ts
@@ -0,0 +1,195 @@
+import CardView from 'devextreme-testcafe-models/cardView';
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import url from '../../../helpers/getPageUrl';
+import { testScreenshot } from '../../../helpers/themeUtils';
+import { createWidget } from '../../../helpers/createWidget';
+import { a11yCheck } from '../../../helpers/accessibility/utils';
+
+fixture`CardView - HeaderPanel`
+  .page(url(__dirname, '../../container.html'));
+
+const CARD_VIEW_SELECTOR = '#container';
+const HEADER_PANEL_SELECTOR = `${CARD_VIEW_SELECTOR} .dx-cardview-headers`;
+const a11yCheckConfig = {
+  rules: { 'color-contrast': { enabled: true } },
+};
+
+const visualTest = (etalonName: string) => async (t: TestController) => {
+  const cardView = new CardView(CARD_VIEW_SELECTOR);
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+  await testScreenshot(
+    t,
+    takeScreenshot,
+    etalonName,
+    { element: cardView.getHeaderPanel().element },
+  );
+
+  await t
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+  await a11yCheck(t, a11yCheckConfig, HEADER_PANEL_SELECTOR);
+};
+
+test(
+  'default render',
+  visualTest('default-render.png'),
+)
+  .before(async () => createWidget('dxCardView', {
+    dataSource: [
+      {
+        id: 0, filedA: 'A_0', filedB: 'B_0', fieldC: 'C_0',
+      },
+    ],
+    width: 600,
+  }));
+
+test(
+  'render with header filter enabled',
+  visualTest('header-filter-enabled.png'),
+)
+  .before(async () => createWidget('dxCardView', {
+    dataSource: [
+      {
+        id: 0, filedA: 'A_0', filedB: 'B_0', fieldC: 'C_0',
+      },
+    ],
+    headerFilter: {
+      visible: true,
+    },
+    width: 600,
+  }));
+
+test(
+  'render with header filter enabled with filter values',
+  visualTest('header-filter-enabled-with-values.png'),
+)
+  .before(async () => createWidget('dxCardView', {
+    dataSource: [
+      {
+        id: 0, filedA: 'A_0', filedB: 'B_0', fieldC: 'C_0',
+      },
+    ],
+    columns: [
+      'id',
+      'filedA',
+      {
+        dataField: 'filedB',
+        filterValues: ['B_0'],
+      },
+      {
+        dataField: 'filedC',
+        filterValues: ['C_0'],
+      },
+    ],
+    headerFilter: {
+      visible: true,
+    },
+    width: 600,
+  }));
+
+test(
+  'render with single sorting',
+  visualTest('single-sorting.png'),
+).before(async () => createWidget('dxCardView', {
+  dataSource: [
+    {
+      id: 0, filedA: 'A_0', filedB: 'B_0', fieldC: 'C_0',
+    },
+  ],
+  columns: [
+    'id',
+    'filedA',
+    {
+      dataField: 'filedB',
+      sortOrder: 'asc',
+    },
+    'fieldC',
+  ],
+  width: 600,
+}));
+
+test(
+  'render with single sorting and header filter enabled',
+  visualTest('single-sorting-with-header-filter-enabled.png'),
+).before(async () => createWidget('dxCardView', {
+  dataSource: [
+    {
+      id: 0, filedA: 'A_0', filedB: 'B_0', fieldC: 'C_0',
+    },
+  ],
+  columns: [
+    'id',
+    'filedA',
+    {
+      dataField: 'filedB',
+      sortOrder: 'asc',
+    },
+    'fieldC',
+  ],
+  headerFilter: {
+    visible: true,
+  },
+  width: 600,
+}));
+
+test(
+  'render with multiple sorting',
+  visualTest('multiple-sorting.png'),
+).before(async () => createWidget('dxCardView', {
+  dataSource: [
+    {
+      id: 0, filedA: 'A_0', filedB: 'B_0', fieldC: 'C_0',
+    },
+  ],
+  columns: [
+    'id',
+    'filedA',
+    {
+      dataField: 'filedB',
+      sortOrder: 'asc',
+      sortIndex: 1,
+    },
+    {
+      dataField: 'filedC',
+      sortOrder: 'desc',
+      sortIndex: 0,
+    },
+  ],
+  sorting: {
+    mode: 'multiple',
+  },
+  width: 600,
+}));
+
+test(
+  'render with multiple sorting and header filter',
+  visualTest('multiple-sorting-with-header-filter-enabled.png'),
+).before(async () => createWidget('dxCardView', {
+  dataSource: [
+    {
+      id: 0, filedA: 'A_0', filedB: 'B_0', fieldC: 'C_0',
+    },
+  ],
+  columns: [
+    'id',
+    'filedA',
+    {
+      dataField: 'filedB',
+      sortOrder: 'asc',
+      sortIndex: 1,
+    },
+    {
+      dataField: 'filedC',
+      sortOrder: 'desc',
+      sortIndex: 0,
+    },
+  ],
+  sorting: {
+    mode: 'multiple',
+  },
+  headerFilter: {
+    visible: true,
+  },
+  width: 600,
+}));
