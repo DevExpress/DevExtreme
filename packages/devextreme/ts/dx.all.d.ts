f75126ece309ba00339c665f4e0051e89f4caf2b
Mikhail Preyskurantov <5574159+mpreyskurantov@users.noreply.github.com>
2025-05-07 18:27:14 +0300

    chat editing: extracted Editing type -> to inline

--- packages/devextreme/ts/dx.all.d.ts
+++ packages/devextreme/ts/dx.all.d.ts
@@ -10108,7 +10108,26 @@ declare module DevExpress.ui {
     /**
      * [descr:dxChatOptions.editing]
      */
-    editing?: DevExpress.ui.dxChat.Editing;
+    editing?: {
+      /**
+       * [descr:dxChatOptions.editing.allowUpdating]
+       */
+      allowUpdating?:
+        | boolean
+        | ((options: {
+            component?: dxChat;
+            message?: DevExpress.ui.dxChat.Message;
+          }) => boolean);
+      /**
+       * [descr:dxChatOptions.editing.allowDeleting]
+       */
+      allowDeleting?:
+        | boolean
+        | ((options: {
+            component?: dxChat;
+            message?: DevExpress.ui.dxChat.Message;
+          }) => boolean);
+    };
     /**
      * [descr:dxChatOptions.dataSource]
      */
@@ -31886,23 +31905,6 @@ declare module DevExpress.ui.dxChat {
      * [descr:Alert.message]
      */
     message?: string;
-  };
-  /**
-   * [descr:Editing]
-   */
-  export type Editing = {
-    /**
-     * [descr:Editing.allowUpdating]
-     */
-    allowUpdating?:
-      | boolean
-      | ((options: { component?: dxChat; message?: Message }) => boolean);
-    /**
-     * [descr:Editing.allowDeleting]
-     */
-    allowDeleting?:
-      | boolean
-      | ((options: { component?: dxChat; message?: Message }) => boolean);
   };
   /**
    * [descr:Message]
