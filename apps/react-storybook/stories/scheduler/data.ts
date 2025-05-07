b0753b3e1ec979a4e9451137598edaaf5be377c2
Vladimir Bushmanov <bushmanov.vova@yandex.ru>
2025-05-07 09:42:36 +0200

    Scheduler: toolbar story. Add custom set of toolbar items (qte49aJ8) (#29781)
    Co-authored-by: Vladimir Bushmanov <vladimir.bushmanov@devexpress.com>

---
+++ apps/react-storybook/stories/scheduler/data.ts
@@ -0,0 +1,19 @@
+export const assignees = [
+  {
+    text: 'Samantha Bright',
+    id: 1,
+    color: '#727bd2',
+  }, {
+    text: 'John Heart',
+    id: 2,
+    color: '#32c9ed',
+  }, {
+    text: 'Todd Hoffman',
+    id: 3,
+    color: '#2a7ee4',
+  }, {
+    text: 'Sandra Johnson',
+    id: 4,
+    color: '#7b49d3',
+  },
+];
