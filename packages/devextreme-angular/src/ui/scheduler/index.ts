49cdfca79714fae52d6b32a26c4ed542c8387dc5
Vladimir Bushmanov <bushmanov.vova@yandex.ru>
2025-05-07 13:56:40 +0200

    Scheduler - publish Toolbar - implement API (#29109)
    Co-authored-by: Vladimir Bushmanov <vladimir.bushmanov@devexpress.com>
    Co-authored-by: alexlavrov <alex.s.lavrov@gmail.com>

--- packages/devextreme-angular/src/ui/scheduler/index.ts
+++ packages/devextreme-angular/src/ui/scheduler/index.ts
@@ -26,7 +26,7 @@ import dxScheduler from 'devextreme/ui/scheduler';
 import dxSortable from 'devextreme/ui/sortable';
 import dxDraggable from 'devextreme/ui/draggable';
 import DataSource from 'devextreme/data/data_source';
-import { AllDayPanelMode, ViewType, dxSchedulerAppointment, CellAppointmentsLimit, AppointmentAddedEvent, AppointmentAddingEvent, AppointmentClickEvent, AppointmentContextMenuEvent, AppointmentDblClickEvent, AppointmentDeletedEvent, AppointmentDeletingEvent, AppointmentFormOpeningEvent, AppointmentRenderedEvent, AppointmentTooltipShowingEvent, AppointmentUpdatedEvent, AppointmentUpdatingEvent, CellClickEvent, CellContextMenuEvent, ContentReadyEvent, DisposingEvent, InitializedEvent, OptionChangedEvent, RecurrenceEditMode, dxSchedulerScrolling } from 'devextreme/ui/scheduler';
+import { AllDayPanelMode, ViewType, dxSchedulerAppointment, CellAppointmentsLimit, AppointmentAddedEvent, AppointmentAddingEvent, AppointmentClickEvent, AppointmentContextMenuEvent, AppointmentDblClickEvent, AppointmentDeletedEvent, AppointmentDeletingEvent, AppointmentFormOpeningEvent, AppointmentRenderedEvent, AppointmentTooltipShowingEvent, AppointmentUpdatedEvent, AppointmentUpdatingEvent, CellClickEvent, CellContextMenuEvent, ContentReadyEvent, DisposingEvent, InitializedEvent, OptionChangedEvent, RecurrenceEditMode, dxSchedulerScrolling, dxSchedulerToolbar } from 'devextreme/ui/scheduler';
 import { event } from 'devextreme/events/events.types';
 import { DataSourceOptions } from 'devextreme/data/data_source';
 import { Store } from 'devextreme/data/store';
@@ -53,8 +53,13 @@ import { DxiViewModule } from 'devextreme-angular/ui/nested';
 
 import { DxoSchedulerAppointmentDraggingModule } from 'devextreme-angular/ui/scheduler/nested';
 import { DxoSchedulerEditingModule } from 'devextreme-angular/ui/scheduler/nested';
+import { DxiSchedulerItemModule } from 'devextreme-angular/ui/scheduler/nested';
+import { DxoSchedulerOptionsModule } from 'devextreme-angular/ui/scheduler/nested';
+import { DxiSchedulerOptionsItemModule } from 'devextreme-angular/ui/scheduler/nested';
 import { DxiSchedulerResourceModule } from 'devextreme-angular/ui/scheduler/nested';
 import { DxoSchedulerScrollingModule } from 'devextreme-angular/ui/scheduler/nested';
+import { DxoSchedulerToolbarModule } from 'devextreme-angular/ui/scheduler/nested';
+import { DxiSchedulerToolbarItemModule } from 'devextreme-angular/ui/scheduler/nested';
 import { DxiSchedulerViewModule } from 'devextreme-angular/ui/scheduler/nested';
 
 import { DxiResourceComponent } from 'devextreme-angular/ui/nested';
@@ -815,6 +820,19 @@ export class DxSchedulerComponent extends DxComponent implements OnDestroy, OnCh
 
 
     /**
+     * [descr:dxSchedulerOptions.toolbar]
+    
+     */
+    @Input()
+    get toolbar(): dxSchedulerToolbar | undefined {
+        return this._getOption('toolbar');
+    }
+    set toolbar(value: dxSchedulerToolbar | undefined) {
+        this._setOption('toolbar', value);
+    }
+
+
+    /**
      * [descr:dxSchedulerOptions.useDropDownViewSwitcher]
     
      */
@@ -1406,6 +1424,13 @@ export class DxSchedulerComponent extends DxComponent implements OnDestroy, OnCh
      * This member supports the internal infrastructure and is not intended to be used directly from your code.
     
      */
+    @Output() toolbarChange: EventEmitter<dxSchedulerToolbar | undefined>;
+
+    /**
+    
+     * This member supports the internal infrastructure and is not intended to be used directly from your code.
+    
+     */
     @Output() useDropDownViewSwitcherChange: EventEmitter<boolean>;
 
     /**
@@ -1552,6 +1577,7 @@ export class DxSchedulerComponent extends DxComponent implements OnDestroy, OnCh
             { emit: 'textExprChange' },
             { emit: 'timeCellTemplateChange' },
             { emit: 'timeZoneChange' },
+            { emit: 'toolbarChange' },
             { emit: 'useDropDownViewSwitcherChange' },
             { emit: 'viewsChange' },
             { emit: 'visibleChange' },
@@ -1618,8 +1644,13 @@ export class DxSchedulerComponent extends DxComponent implements OnDestroy, OnCh
     DxiViewModule,
     DxoSchedulerAppointmentDraggingModule,
     DxoSchedulerEditingModule,
+    DxiSchedulerItemModule,
+    DxoSchedulerOptionsModule,
+    DxiSchedulerOptionsItemModule,
     DxiSchedulerResourceModule,
     DxoSchedulerScrollingModule,
+    DxoSchedulerToolbarModule,
+    DxiSchedulerToolbarItemModule,
     DxiSchedulerViewModule,
     DxIntegrationModule,
     DxTemplateModule
@@ -1633,8 +1664,13 @@ export class DxSchedulerComponent extends DxComponent implements OnDestroy, OnCh
     DxiViewModule,
     DxoSchedulerAppointmentDraggingModule,
     DxoSchedulerEditingModule,
+    DxiSchedulerItemModule,
+    DxoSchedulerOptionsModule,
+    DxiSchedulerOptionsItemModule,
     DxiSchedulerResourceModule,
     DxoSchedulerScrollingModule,
+    DxoSchedulerToolbarModule,
+    DxiSchedulerToolbarItemModule,
     DxiSchedulerViewModule,
     DxTemplateModule
   ]
--- packages/devextreme-angular/src/ui/scheduler/nested/index.ts
+++ packages/devextreme-angular/src/ui/scheduler/nested/index.ts
@@ -1,6 +1,11 @@
 export * from './appointment-dragging';
 export * from './editing';
+export * from './item-dxi';
+export * from './options-item-dxi';
+export * from './options';
 export * from './resource-dxi';
 export * from './scrolling';
+export * from './toolbar-item-dxi';
+export * from './toolbar';
 export * from './view-dxi';
 
