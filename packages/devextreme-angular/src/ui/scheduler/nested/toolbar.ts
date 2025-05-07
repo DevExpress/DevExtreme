19252b3b8d36beb1d5bb7344de2fafef7fd00e19
Vladimir Bushmanov <bushmanov.vova@yandex.ru>
2025-05-07 13:56:40 +0200

    Scheduler - publish Toolbar - implement API (#29109)
    Co-authored-by: Vladimir Bushmanov <vladimir.bushmanov@devexpress.com>
    Co-authored-by: alexlavrov <alex.s.lavrov@gmail.com>

---
+++ packages/devextreme-angular/src/ui/scheduler/nested/toolbar.ts
@@ -0,0 +1,121 @@
+/* tslint:disable:max-line-length */
+
+
+import {
+    Component,
+    OnInit,
+    OnDestroy,
+    NgModule,
+    Host,
+    SkipSelf,
+    Input,
+    ContentChildren,
+    forwardRef,
+    QueryList
+} from '@angular/core';
+
+
+
+
+import { dxSchedulerToolbarItem, SchedulerPredefinedToolbarItem } from 'devextreme/ui/scheduler';
+
+import {
+    DxIntegrationModule,
+    NestedOptionHost,
+} from 'devextreme-angular/core';
+import { NestedOption } from 'devextreme-angular/core';
+import { DxiSchedulerItemComponent } from './item-dxi';
+import { DxiSchedulerToolbarItemComponent } from './toolbar-item-dxi';
+
+
+@Component({
+    selector: 'dxo-scheduler-toolbar',
+    standalone: true,
+    template: '',
+    styles: [''],
+    imports: [ DxIntegrationModule ],
+    providers: [NestedOptionHost]
+})
+export class DxoSchedulerToolbarComponent extends NestedOption implements OnDestroy, OnInit  {
+    @Input()
+    get disabled(): boolean {
+        return this._getOption('disabled');
+    }
+    set disabled(value: boolean) {
+        this._setOption('disabled', value);
+    }
+
+    @Input()
+    get items(): Array<dxSchedulerToolbarItem | SchedulerPredefinedToolbarItem> {
+        return this._getOption('items');
+    }
+    set items(value: Array<dxSchedulerToolbarItem | SchedulerPredefinedToolbarItem>) {
+        this._setOption('items', value);
+    }
+
+    @Input()
+    get multiline(): boolean {
+        return this._getOption('multiline');
+    }
+    set multiline(value: boolean) {
+        this._setOption('multiline', value);
+    }
+
+    @Input()
+    get visible(): boolean | undefined {
+        return this._getOption('visible');
+    }
+    set visible(value: boolean | undefined) {
+        this._setOption('visible', value);
+    }
+
+
+    protected get _optionPath() {
+        return 'toolbar';
+    }
+
+
+    @ContentChildren(forwardRef(() => DxiSchedulerItemComponent))
+    get itemsChildren(): QueryList<DxiSchedulerItemComponent> {
+        return this._getOption('items');
+    }
+    set itemsChildren(value) {
+        this.setChildren('items', value);
+    }
+
+    @ContentChildren(forwardRef(() => DxiSchedulerToolbarItemComponent))
+    get toolbarItemsChildren(): QueryList<DxiSchedulerToolbarItemComponent> {
+        return this._getOption('items');
+    }
+    set toolbarItemsChildren(value) {
+        this.setChildren('items', value);
+    }
+
+    constructor(@SkipSelf() @Host() parentOptionHost: NestedOptionHost,
+            @Host() optionHost: NestedOptionHost) {
+        super();
+        parentOptionHost.setNestedOption(this);
+        optionHost.setHost(this, this._fullOptionPath.bind(this));
+    }
+
+
+    ngOnInit() {
+        this._addRecreatedComponent();
+    }
+
+    ngOnDestroy() {
+        this._addRemovedOption(this._getOptionPath());
+    }
+
+
+}
+
+@NgModule({
+  imports: [
+    DxoSchedulerToolbarComponent
+  ],
+  exports: [
+    DxoSchedulerToolbarComponent
+  ],
+})
+export class DxoSchedulerToolbarModule { }
