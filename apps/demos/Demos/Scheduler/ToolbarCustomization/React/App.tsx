6cb0d1d25912c188b1d61eb79f618e22df35babb
Vladimir Bushmanov <vladimir.bushmanov@devexpress.com>
2025-05-08 15:31:30 +0500

    fix react demo

--- apps/demos/Demos/Scheduler/ToolbarCustomization/React/App.tsx
+++ apps/demos/Demos/Scheduler/ToolbarCustomization/React/App.tsx
@@ -2,57 +2,62 @@ import React, { useCallback, useMemo, useRef, useState } from 'react';
 
 import Scheduler, { Toolbar, Item, Resource, SchedulerTypes, SchedulerRef } from 'devextreme-react/scheduler';
 
-import { data, assignees } from './data.ts';
+import { data, assignees, schedulerDataSource, currentDate } from './data.ts';
 import TagBox, { TagBoxTypes } from "devextreme-react/cjs/tag-box";
 
-const initialCurrentDate = new Date(2021, 3, 27);
 const views: SchedulerTypes.ViewType[] = ['day', 'week', 'workWeek', 'month'];
 const inputAttr = { 'aria-label': 'Assignees' };
 const elementAttr = { class: 'assignees-tag-box' };
 
 const App = () => {
   const schedulerRef = useRef<SchedulerRef>(null);
-  const [currentDate, setCurrentDate] = useState<Date>(initialCurrentDate);
-  const [assigneesFilterValue, setAssigneesFilterValue] = useState<number[]>([]);
-
-  const filteredData = useMemo(() => (
-      assigneesFilterValue.length
-        ? data.filter((item) => assigneesFilterValue.some((id) => item.assigneeId.includes(id)))
-        : data
-    ),
-    [assigneesFilterValue]
-  );
+  const [assigneesFilterValue, setAssigneesFilterValue] = useState<number>();
 
   const onAssigneesFilterChange = useCallback((event: TagBoxTypes.ValueChangedEvent) => {
+    const filter = event.value ? ['assigneeId', 'contains', event.value] : null;
+
+    schedulerDataSource.filter(filter);
     setAssigneesFilterValue(event.value);
   }, []);
-  const dateNavigatorOptions = useMemo(() => ({
-    onItemClick: (event) => {
-      if (event.itemData.key === 'today') {
-        setCurrentDate(new Date());
-      }
-    },
-    items: [
-      { key: 'today', text: 'Today' },
-      'prev',
-      'next',
-      'current'
-    ],
-  }), []);
   const toggleButtonOptions = useMemo(() => ({
     icon: 'plus',
-    text: 'New event',
-    onClick: () => {
-      schedulerRef.current?.instance().showAppointmentPopup({
-        startDate: new Date(),
-      }, true);
+    text: 'New Appointment',
+    stylingMode: 'outlined',
+    type: 'normal',
+    onClick() {
+      const scheduler = schedulerRef.current?.instance();
+      if (!scheduler) {
+        return;
+      }
+
+      const selected = scheduler.option('selectedCellData') ?? [];
+
+      if (selected.length) {
+        scheduler.showAppointmentPopup({
+          ...selected[0].groups,
+          allDay: selected[0].allDay,
+          startDate: new Date(selected[0].startDateUTC),
+          endDate: new Date(selected.at(-1).endDateUTC),
+        }, true);
+      } else {
+        const currentDate = scheduler.option('currentDate');
+        const cellDuration = scheduler.option('cellDuration') as number;
+        const cellDurationMs = cellDuration * 60 * 1000; // ms
+        const currentTime = new Date(currentDate as Date).getTime();
+        const roundTime = Math.round(currentTime / cellDurationMs) * cellDurationMs;
+
+        scheduler.showAppointmentPopup({
+          startDate: new Date(roundTime),
+          endDate: new Date(roundTime + cellDurationMs),
+        }, true);
+      }
     },
   }), []);
 
   return (
     <Scheduler
       timeZone="America/Los_Angeles"
-      dataSource={filteredData}
+      dataSource={schedulerDataSource}
       views={views}
       defaultCurrentView="workWeek"
       defaultCurrentDate={currentDate}
@@ -68,7 +73,8 @@ const App = () => {
         label="Assignee"
       />
       <Toolbar>
-        <Item location="before" name="dateNavigator" options={dateNavigatorOptions} />
+        <Item name="today" />
+        <Item name="dateNavigator" />
         <Item
           location="before"
           locateInMenu="auto"
