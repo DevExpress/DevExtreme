a6767a7f472ea8895f49c1a5a6f9320efbc2a710
wdevfx <ilya.vinogradov@devexpress.com>
2025-05-08 05:08:05 +0400

    test(CardView): move tests after rebase on 25_1

--- e2e/testcafe-devextreme/tests/cardView/headerPanel/visual.ts
+++ e2e/testcafe-devextreme/tests/cardView/headerPanel/visual.ts
@@ -193,3 +193,22 @@ test(
   },
   width: 600,
 }));
+
+test('headerPanel column chooser link opens column chooser on click', async (t) => {
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+  const cardView = new CardView('#container');
+  const headerPanel = cardView.getHeaderPanel();
+
+  await t.click(headerPanel.getColumnChooserLink());
+
+  await testScreenshot(t, takeScreenshot, 'card-view-column-chooser-opened-on-empty-header-panel-link-click.png', { element: cardView.element });
+
+  await t
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => createWidget('dxCardView', {
+  height: 600,
+  columns: [
+    { dataField: 'Column 1', visible: false },
+  ],
+}));
