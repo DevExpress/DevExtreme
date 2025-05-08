1353d6fa9bfd70717aec2e7f492ade90f18e4c41
anna.shakhova <anna.shakhova@devexpress.com>
2025-05-08 10:29:56 +0200

    List: fix animation on collapse group (T1282693)

--- packages/devextreme/testing/tests/DevExpress.ui.widgets/listParts/commonTests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.widgets/listParts/commonTests.js
@@ -296,6 +296,34 @@ QUnit.module('collapsible groups', moduleSetup, () => {
         assert.ok(!$group.hasClass(LIST_GROUP_COLLAPSED_CLASS), 'collapsed class is not present');
     });
 
+    QUnit.test('group body should be correctly collapsed on fast multiple clicks on header (T1282693)', function(assert) {
+        this.clock.restore();
+        const done = assert.async();
+
+        const $element = this.element.dxList({
+            items: [{ key: 'a', items: ['0'] }],
+            grouped: true,
+            collapsibleGroups: true
+        });
+
+        const $group = $element.find('.' + LIST_GROUP_CLASS);
+        const $groupBody = $group.find('.' + LIST_GROUP_BODY_CLASS);
+        const $groupHeader = $element.find('.' + LIST_GROUP_HEADER_CLASS);
+
+        const animationDuration = 200;
+
+        for(let i = 0; i < 11; i++) {
+            $groupHeader.trigger('dxclick');
+        }
+
+        setTimeout(() => {
+            assert.strictEqual($group.hasClass(LIST_GROUP_COLLAPSED_CLASS), true, 'collapsed class is present');
+            assert.strictEqual($groupBody.height(), 0, 'group items are hidden');
+
+            done();
+        }, 2 * animationDuration);
+    });
+
     QUnit.test('group body should be not collapsed by click on header if collapsibleGroups is disabled', function(assert) {
         const $element = this.element.dxList({
             items: [{ key: 'a', items: ['0'] }],
