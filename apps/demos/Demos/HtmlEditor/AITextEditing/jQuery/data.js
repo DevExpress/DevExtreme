c92241f5db13e82febf8747ed7906e4b859364a4
ksercs <ksercs0@gmail.com>
2025-05-12 16:24:26 +0400

    replace h1 with h2

--- apps/demos/Demos/HtmlEditor/AITextEditing/jQuery/data.js
+++ apps/demos/Demos/HtmlEditor/AITextEditing/jQuery/data.js
@@ -4,10 +4,10 @@ const endpoint = 'https://public-api.devexpress.com/demo-openai';
 const apiKey = 'DEMO';
 
 const markup = `
-    <h1>
+    <h2>
         <img src="../../../../images/widgets/HtmlEditor.svg" alt="HtmlEditor">
         Formatted Text Editor (HTML Editor)
-    </h1>
+    </h2>
     <br>
     <p>DevExtreme JavaScript HTML Editor is a client-side WYSIWYG text editor that allows its users to format textual and visual content and store it as HTML.</p>
     <p>Supported features:</p>
