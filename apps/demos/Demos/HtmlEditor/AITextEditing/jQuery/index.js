66fae35e1b7d481007c12663f7c450f8546a19f3
ksercs <ksercs0@gmail.com>
2025-05-12 15:31:37 +0400

    configurate AI item

--- apps/demos/Demos/HtmlEditor/AITextEditing/jQuery/index.js
+++ apps/demos/Demos/HtmlEditor/AITextEditing/jQuery/index.js
@@ -2,10 +2,29 @@ $(() => {
   $('.html-editor').dxHtmlEditor({
     height: 725,
     value: markup,
+    aiIntegration: {},
     toolbar: {
-      items: [
-        'ai', 'separator', 'undo', 'redo',
-      ],
+      items: [{
+        name: 'ai',
+        commands: [
+            'summarize',
+            'proofread',
+            'expand',
+            'shorten',
+            'changeStyle',
+            'changeTone',
+            'translate',
+            'askAI',
+            {
+                name: 'custom',
+                text: 'Extract Keywords',
+                prompt: () => {
+                    return 'Extract a list of keywords from the text and return them as a comma-separated string';
+                },
+            },
+        ],
+      }, 'separator', 'undo', 'redo'
+    ],
     }
   });
 });
