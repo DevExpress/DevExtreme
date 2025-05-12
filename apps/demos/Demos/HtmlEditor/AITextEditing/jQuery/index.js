8085143f7293212b06fc289ef824bed655d14ba3
ksercs <ksercs0@gmail.com>
2025-05-12 15:26:21 +0400

    add basic config

--- apps/demos/Demos/HtmlEditor/AITextEditing/jQuery/index.js
+++ apps/demos/Demos/HtmlEditor/AITextEditing/jQuery/index.js
@@ -1,2 +1,11 @@
 $(() => {
+  $('.html-editor').dxHtmlEditor({
+    height: 725,
+    value: markup,
+    toolbar: {
+      items: [
+        'ai', 'separator', 'undo', 'redo',
+      ],
+    }
+  });
 });
