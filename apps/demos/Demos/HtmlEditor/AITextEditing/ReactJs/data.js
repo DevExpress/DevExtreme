e02f0df3e8d3a8fcd0dbe16bdc7dfb83e6167bcd
ksercs <ksercs0@gmail.com>
2025-05-13 13:41:21 +0400

    update data

--- apps/demos/Demos/HtmlEditor/AITextEditing/ReactJs/data.js
+++ apps/demos/Demos/HtmlEditor/AITextEditing/ReactJs/data.js
@@ -55,7 +55,7 @@ export const markup = `
     <ul>
         <li>Inline formats:
             <ul>
-                <li><strong>Bold</strong>, <em>italic</em>, <s>strikethrough</s> text formatting</li>
+                <li>Bold, italic, strikethrough text formatting</li>
                 <li>Font, size, color changes (HTML only)</li>
             </ul>
         </li>
@@ -75,26 +75,6 @@ export const markup = `
         <li>Copy-paste rich content (unsupported formats are removed)</li>
         <li>Tables support</li>
     </ul>
-    <br>
-    <p>Supported frameworks and libraries</p>
-    <table>
-        <tr>
-            <td><strong>jQuery</strong></td>
-            <td style="text-align: right;">v2.1 - v2.2 and v3.x</td>
-        </tr>
-        <tr>
-            <td><strong>Angular</strong></td>
-            <td style="text-align: right;">v7.0+</td>
-        </tr>
-        <tr>
-            <td><strong>React</strong></td>
-            <td style="text-align: right;">v16.2+</td>
-        </tr>
-        <tr>
-            <td><strong>Vue</strong></td>
-            <td style="text-align: right;">v2.6.3+</td>
-        </tr>
-    </table>
 `;
 export const commands = [
   'summarize',
