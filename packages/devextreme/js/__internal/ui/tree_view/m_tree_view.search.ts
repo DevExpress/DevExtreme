5c55255638bfa8e8130b26d47144dd1400d9a460
tongsonbarbs <tongsonbarbs@gmail.com>
2025-05-13 15:43:57 +0800

    fix error

--- packages/devextreme/js/__internal/ui/tree_view/m_tree_view.search.ts
+++ packages/devextreme/js/__internal/ui/tree_view/m_tree_view.search.ts
@@ -27,7 +27,7 @@ const TreeViewSearch = TreeViewBase.inherit(searchBoxMixin).inherit({
         this._initDataAdapter();
         this._updateSearch();
         this._repaintContainer();
-        if (this._searchEditor.id !== this.option('focusedElement')?.id) {
+        if (this._searchEditor?.id !== this.option('focusedElement')?.id) {
           this.option('focusedElement', null);
         }
         break;
