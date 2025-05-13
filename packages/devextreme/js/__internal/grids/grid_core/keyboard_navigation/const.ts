58a797be568458093c0773295646a202b00a135e
Alyar <Alyar666@users.noreply.github.com>
2025-05-13 10:07:02 +0400

    DataGrid/TreeList - Support new shortcuts for quick navigation through grid rows (#29832)
    Co-authored-by: Alyar <>

--- packages/devextreme/js/__internal/grids/grid_core/keyboard_navigation/const.ts
+++ packages/devextreme/js/__internal/grids/grid_core/keyboard_navigation/const.ts
@@ -52,6 +52,7 @@ export const FOCUS_TYPE_CELL = 'cell';
 export const COLUMN_HEADERS_VIEW = 'columnHeadersView';
 export const ROWS_VIEW = 'rowsView';
 export const FUNCTIONAL_KEYS = ['shift', 'control', 'alt'];
+export const DRAG_COLUMN_NAME = 'drag';
 
 export enum Direction {
   Next = 'next',
