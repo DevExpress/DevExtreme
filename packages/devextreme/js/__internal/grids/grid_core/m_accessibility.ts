5fdfe135ca878f09bddb2b44352dfa6ff0ac361b
Alyar <Alyar666@users.noreply.github.com>
2025-05-13 13:45:08 +0400

    DataGrid: Fix double calling of the onKeyDown event for headers and group panel (#29845)
    Co-authored-by: Alyar <>

--- packages/devextreme/js/__internal/grids/grid_core/m_accessibility.ts
+++ packages/devextreme/js/__internal/grids/grid_core/m_accessibility.ts
@@ -1,16 +1,20 @@
 import * as accessibility from '@js/ui/shared/accessibility';
 
 export const registerKeyboardAction = function (viewName, instance, $element, selector, action) {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  let executeKeyDown = (args) => {};
   const keyboardController = instance.getController('keyboardNavigation');
   if (instance.option('useLegacyKeyboardNavigation') || (keyboardController && !keyboardController.isKeyboardEnabled())) {
     return;
   }
 
-  const executeKeyDown = (args) => {
-    instance.executeAction('onKeyDown', args);
-  };
+  if (viewName === 'filterPanel') {
+    executeKeyDown = (args) => {
+      instance.executeAction('onKeyDown', args);
+    };
 
-  instance.createAction('onKeyDown');
+    instance.createAction('onKeyDown');
+  }
 
   accessibility.registerKeyboardAction(viewName, instance, $element, selector, action, executeKeyDown);
 };
