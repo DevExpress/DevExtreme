2141a40dbc85834246ec0de11c6b24d91039dc0f
sergey-kras <vip.41243@gmail.com>
2025-05-13 20:12:17 +0500

    chore: added e2e tests

--- e2e/wrappers/builders/angular19/src/utils/componentFinder.ts
+++ e2e/wrappers/builders/angular19/src/utils/componentFinder.ts
@@ -4,6 +4,11 @@ export const COMPONENTS = [
     name: 'Button',
     loadComponent: () => import('@external/button/angular/button.component').then((m) => m.ButtonComponent),
   },
+  {
+    path: 'inputs-list-in-form',
+    name: 'InputsListInForm', 
+    loadComponent: () => import('@external/inputs-list-in-form/angular/inputs-list-in-form.component').then((m) => m.InputsListInFormComponent),
+  },
 ];
 
 export function findComponentByPath(path: string) {
--- e2e/wrappers/builders/vue3/src/utils/componentFinder.js
+++ e2e/wrappers/builders/vue3/src/utils/componentFinder.js
@@ -2,11 +2,16 @@ const componentModules = import.meta.glob('../../../examples/**/vue3/index.vue')
 
 const COMPONENTS = [
     {
-      path: 'button',
-      name: 'Button',
-      component: () => import('@examples/button/vue/index.vue')
+        path: 'button',
+        name: 'Button',
+        component: () => import('@examples/button/vue/index.vue')
+    },
+    {
+        path: 'inputs-list-in-form',
+        name: 'InputsListInForm',
+        component: () => import('@examples/inputs-list-in-form/vue/index.vue')
     }
-  ];
+];
 
 console.log('Component Modules:', componentModules);
-export default COMPONENTS; 
+export default COMPONENTS;
