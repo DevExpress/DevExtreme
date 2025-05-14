77b482568be10b6aa86ffb06e696f05e24591044
sergey-kras <vip.41243@gmail.com>
2025-05-14 18:46:49 +0500

    fix: fixed test

--- e2e/wrappers/tests/inputs-list-in-form.test.js
+++ e2e/wrappers/tests/inputs-list-in-form.test.js
@@ -1,9 +1,11 @@
 import { Selector } from 'testcafe';
 import { testInFramework } from '../test-helpers';
 
-testInFramework('inputs-list-in-form', async (t) => {
-    const addButton = Selector('.dx-button-text').withText('Add phone');
-    const deleteButton = Selector('.dx-icon-trash').parent('.dx-button');
+testInFramework('inputs-list-in-form scenarios', 'inputs-list-in-form', [
+    'Phone inputs should adding and deleting correctly',
+    async (t) => {
+        const addButton = Selector('.dx-button-text').withText('Add phone');
+        const deleteButton = Selector('.dx-icon-trash').parent('.dx-button');
 
     const phoneGroup = Selector('[aria-labelledby$="_phones-container"]');
 
@@ -15,5 +17,6 @@ testInFramework('inputs-list-in-form', async (t) => {
     await t.expect(phoneInputs.count).eql(1, 'There should be exactly one phone input after adding');
 
     await t.click(deleteButton);
-    await t.expect(phoneInputs.count).eql(0, 'Phone inputs should be removed after deletion');
-});
+        await t.expect(phoneInputs.count).eql(0, 'Phone inputs should be removed after deletion');
+    }
+]);
