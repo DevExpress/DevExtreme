8ec43dd1251ed00e46849a34708d7526f09f1526
Adel Khamatov <hamatovadel@gmail.com>
2025-05-15 09:37:41 +0300

    fix(StateManager): replace only `setup_state_manager.js`

--- packages/devextreme/build/gulp/state_manager/constants.js
+++ packages/devextreme/build/gulp/state_manager/constants.js
@@ -2,9 +2,6 @@ const path = require('path');
 
 const STATE_MANAGER_FOLDER_PATH = path.join('__internal', 'core', 'state_manager');
 
-const PRODUCTION_FILE_NAME = 'production.js';
-
 module.exports = {
     STATE_MANAGER_FOLDER_PATH,
-    PRODUCTION_FILE_NAME
 };
