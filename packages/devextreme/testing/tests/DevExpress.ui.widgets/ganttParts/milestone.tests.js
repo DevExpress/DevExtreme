cc9a29724f7feaeb010bd68bdff0cd4d8b14f1b8
Marcin Rabiak <marcin.rabiak@devexpress.com>
2025-05-15 15:57:01 +0200

    added position test

--- packages/devextreme/testing/tests/DevExpress.ui.widgets/ganttParts/milestone.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.widgets/ganttParts/milestone.tests.js
@@ -12,6 +12,7 @@ const moduleConfig = {
         this.createInstance = (settings) => {
             this.instance = this.$element.dxGantt({
                 tasks: { dataSource: this._customTasks },
+                taskTitlePosition: 'outside',
                 ...settings
             }).dxGantt('instance');
         };
@@ -91,5 +92,54 @@ QUnit.module('Milestone', moduleConfig, () => {
         const milestoneWrapper = this.$element.find(Consts.MILESTONE_WRAPPER_SELECTOR);
         assert.equal(milestoneWrapper.length, 1, 'milestone wrapper exists');
         assert.equal(milestoneWrapper.html(), customHtml[0].outerHTML, 'milestone html matches custom html');
+    });
+
+    test('task title inside', function(assert) {
+        const options = {
+            taskTitlePosition: 'inside',
+            taskContentTemplate: (item) => item.taskHTML
+        };
+        this.createInstance(options);
+        this.clock.tick(10);
+        const milestoneWrapper = this.$element.find(Consts.MILESTONE_WRAPPER_SELECTOR);
+        assert.equal(milestoneWrapper.length, 1, 'milestone wrapper exists');
+        const children = milestoneWrapper.children().eq(0).children();
+        assert.equal(children.length, 1, 'milestone has one child');
+        assert.true(children.eq(0).hasClass(Consts.MILESTONE_SELECTOR), 'child is milestone');
+        assert.false(children.eq(0).hasClass(Consts.TASK_TITLE_SELECTOR), 'child is not task title');
+    });
+
+    test('position is correctly calculated', function(assert) {
+        // TODO: figure out how to add styles properly
+        // const style = document.createElement('style');
+        // style.textContent = '.dx-gantt-milestoneWrapper{position:absolute}';
+        // document.head.appendChild(style);
+        const customTasks = [
+            { 'id': 1, 'parentId': 0, 'title': 'Software Development', 'start': new Date('2019-02-21'), 'end': new Date('2019-03-26'), 'progress': 0 },
+            { 'id': 2, 'parentId': 1, 'title': 'Milestone 1', 'start': new Date('2019-02-21'), 'end': new Date('2019-02-21'), 'progress': 0 },
+            { 'id': 3, 'parentId': 2, 'title': 'Milestone 2', 'start': new Date('2019-02-21'), 'end': new Date('2019-02-21'), 'progress': 0 },
+        ];
+        let firstMilestone = true;
+        const customTaskFunction = (item, container) => {
+            if(item.isMilestone) {
+                if(firstMilestone) {
+                    firstMilestone = false;
+                    return $(document.createElement('div')).attr('style', 'width: 10px; height: 10px;');
+                } else {
+                    return $(document.createElement('div')).attr('style', 'width: 100px; height: 50px;');
+                }
+            }
+            return item.taskHTML;
+        };
+        const options = {
+            tasks: { dataSource: customTasks },
+            taskContentTemplate: customTaskFunction
+        };
+        this.createInstance(options);
+        this.clock.tick(10);
+        const milestoneWrapper = this.$element.find(Consts.MILESTONE_WRAPPER_SELECTOR);
+        const firstLeft = parseFloat(milestoneWrapper.eq(0).css('left'));
+        const secondLeft = parseFloat(milestoneWrapper.eq(1).css('left'));
+        assert.equal(firstLeft - secondLeft, 45, 'left position is the same');
     });
 });
