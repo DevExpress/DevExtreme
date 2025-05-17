a6fa470ea5e8e9faa1edd0e731f7ef3834cf7d06
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2025-05-17 23:59:35 +0400

    fix renamed demos in tests

--- apps/demos/testing/widgets/form/CustomomizeItem.test.js
+++ apps/demos/testing/widgets/form/CustomomizeItem.test.js
@@ -3,13 +3,13 @@ import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
 import { testScreenshot } from '../../../utils/visual-tests/helpers/theme-utils';
 
-fixture('Form.Toolbar')
+fixture('Form.ItemCustomization')
   .before(async (ctx) => {
     ctx.initialWindowSize = [900, 800];
   });
 
-runManualTest('Form', 'Toolbar', (test) => {
-  test('Toolbar', async (t) => {
+runManualTest('Form', 'ItemCustomization', (test) => {
+  test('ItemCustomization', async (t) => {
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
     await t.hover($('#helpedInfo'), {
