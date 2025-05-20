2bdbf3ef9991b25fb303421c7489bf41db4c8f35
Roman Semenov <roman.semenov@devexpress.com>
2025-05-20 18:36:08 +0400

    review fix: add typing to React

--- apps/demos/Demos/CardView/FilterPanel/React/data.ts
+++ apps/demos/Demos/CardView/FilterPanel/React/data.ts
@@ -1,4 +1,15 @@
-export const customers = [{
+export interface Customer {
+  ID: number;
+  CompanyName: string;
+  Address: string;
+  City: string;
+  State: string;
+  Zipcode: number;
+  Phone: string;
+  Fax: string;
+}
+
+export const customers: Customer[] = [{
   ID: 1,
   CompanyName: 'Super Mart of the West',
   Address: '702 SW 8th Street',
