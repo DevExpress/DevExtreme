3df9972cb39968c0529b76b8c70c46fd95584d0c
Roman Semenov <roman.semenov@devexpress.com>
2025-05-20 20:09:21 +0400

    CardView - demos - Simple Array (#29917)

---
+++ apps/demos/Demos/CardView/ColumnHeaderFilter/React/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import CardView, { Column } from 'devextreme-react/card-view';
+import { customers } from './data.ts';
+
+const columns = ['CompanyName', 'City', 'State', 'Phone', 'Fax'];
+
+const App = () => (
+  <CardView
+    dataSource={customers}
+    keyExpr="ID"
+  >
+    { columns.map((column, index) => <Column dataField={column} key={index} />) }
+  </CardView>
+);
+
+export default App;
