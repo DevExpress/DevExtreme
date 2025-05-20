f8ffde57727bc1c30e14125e6f0d8b9211178703
Alexander Kozlovskiy <zedwag@gmail.com>
2025-05-20 11:38:25 +0400

    DropDownBox: reduce count of accessibility checks (#29875)

--- e2e/testcafe-devextreme/tests/accessibility/common/dropDownBox/common.ts
+++ e2e/testcafe-devextreme/tests/accessibility/common/dropDownBox/common.ts
@@ -1,10 +1,10 @@
 import { Properties } from 'devextreme/ui/drop_down_box.d';
-import url from '../../../helpers/getPageUrl';
-import { testAccessibility, Configuration } from '../../../helpers/accessibility/test';
-import { Options } from '../../../helpers/generateOptionMatrix';
+import url from '../../../../helpers/getPageUrl';
+import { testAccessibility, Configuration } from '../../../../helpers/accessibility/test';
+import { Options } from '../../../../helpers/generateOptionMatrix';
 
 fixture.disablePageReloads`Accessibility`
-  .page(url(__dirname, '../../container.html'));
+  .page(url(__dirname, '../../../container.html'));
 
 const items = ['Download Trial For Visual Studio', 'Download Trial For All Platforms', 'Package Managers'];
 
@@ -12,27 +12,8 @@ const options: Options<Properties> = {
   dataSource: [[], items],
   disabled: [true, false],
   readOnly: [true, false],
-  name: [undefined, 'name'],
-  text: [undefined, 'Download Trial'],
-  label: [undefined, 'label'],
-  showClearButton: [true, false],
-  showDropDownButton: [true, false],
   opened: [true, false],
   deferRendering: [true, false],
-  buttons: [
-    undefined,
-    [
-      {
-        name: 'today',
-        location: 'before',
-        options: {
-          text: 'Today',
-          stylingMode: 'text',
-          onClick: () => {},
-        },
-      },
-    ],
-  ],
   inputAttr: [{ 'aria-label': 'DropDownBox' }],
 };
 
