70c4b3441c59cfb0429b452455d85234b9683bcf
Ruslan Farkhutdinov <ruslan.farkhutdinov@devexpress.com>
2025-05-20 13:46:38 +0300

    Revert "feat(toolbar && menu): Hide before element in Submenu in Generic"
    This reverts commit 29ea45d178aaaac4f4fd4d274a331edf0092b27b.

--- packages/devextreme/js/__internal/ui/html_editor/modules/m_toolbar.ts
+++ packages/devextreme/js/__internal/ui/html_editor/modules/m_toolbar.ts
@@ -15,7 +15,7 @@ import {
   isDefined, isEmptyObject, isObject, isString,
 } from '@js/core/utils/type';
 import type { AICommandName, AICustomCommand, AIToolbarItem } from '@js/ui/html_editor';
-import type { ContentReadyEvent, ItemClickEvent, SubmenuShownEvent } from '@js/ui/menu';
+import type { ContentReadyEvent, ItemClickEvent } from '@js/ui/menu';
 import type { Item } from '@js/ui/toolbar';
 import Toolbar from '@js/ui/toolbar';
 import errors from '@js/ui/widget/ui.errors';
@@ -487,9 +487,6 @@ if (Quill) {
       const options = {
         dataSource,
         disabled: isMenuDisabled,
-        onSubmenuShown: ({ submenuContainer }: SubmenuShownEvent): void => {
-          $(submenuContainer).parent().addClass(TOOLBAR_AI_OVERLAY_CONTENT_CLASS);
-        },
         onContentReady: (e: ContentReadyEvent): void => {
           const $item = $(e.element).find(`.${DX_MENU_ITEM_CLASS}`).first();
           $item.attr('aria-label', localizationMessage.format('dxHtmlEditor-aiToolbarItemAriaLabel'));
