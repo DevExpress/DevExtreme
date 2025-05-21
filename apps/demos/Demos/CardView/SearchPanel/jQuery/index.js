dcb0bb1248e52437e552eb696e63c17889646e07
Roman Semenov <roman.semenov@devexpress.com>
2025-05-21 10:19:04 +0400

    implement

--- apps/demos/Demos/CardView/SearchPanel/jQuery/index.js
+++ apps/demos/Demos/CardView/SearchPanel/jQuery/index.js
@@ -1,7 +1,38 @@
 $(() => {
   $('#card-view').dxCardView({
-    dataSource: customers,
-    keyExpr: 'ID',
-    columns: ['CompanyName', 'City', 'State', 'Phone', 'Fax'],
+    dataSource: tasks,
+    keyExpr: 'Task_ID',
+    wordWrapEnabled: true,
+    columns: [
+      {
+        caption: 'Subject',
+        dataField: 'Task_Subject',
+      },
+      {
+        dataField: 'Task_Start_Date',
+        caption: 'Start Date',
+        dataType: 'date',
+      },
+      {
+        dataField: 'Task_Due_Date',
+        caption: 'Due Date',
+        dataType: 'date',
+      },
+      {
+        caption: 'Priority',
+        dataField: 'Task_Priority',
+      },
+      {
+        caption: 'Status',
+        dataField: 'Task_Status',
+      },
+    ],
+    headerFilter: {
+      visible: true,
+    },
+    searchPanel: {
+      visible: true,
+      text: 'an'
+    },
   });
 });
