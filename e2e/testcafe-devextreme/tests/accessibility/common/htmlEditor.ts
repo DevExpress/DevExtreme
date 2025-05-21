5fdba1b53c09bdbe649a191cff8aa08c9f871e96
marker dao ® <mark_nikolsky@outlook.com>
2025-05-21 11:54:56 +0300

    HtmlEditor: Add screenshot, a11y tests for AI toolbar item and AIDialog (#29871)
    Co-authored-by: marker dao ® <youdontknow@marker-dao.eth>
    Co-authored-by: Ruslan Farkhutdinov <ruslan.farkhutdinov@devexpress.com>

--- e2e/testcafe-devextreme/tests/accessibility/common/htmlEditor.ts
+++ e2e/testcafe-devextreme/tests/accessibility/common/htmlEditor.ts
@@ -1,8 +1,12 @@
 import { Properties } from 'devextreme/ui/html_editor.d';
+import HtmlEditor from 'devextreme-testcafe-models/htmlEditor';
 import { Selector } from 'testcafe';
 import url from '../../../helpers/getPageUrl';
 import { defaultSelector, testAccessibility, Configuration } from '../../../helpers/accessibility/test';
 import { Options } from '../../../helpers/generateOptionMatrix';
+
+const MENU_ITEM_CLASS = 'dx-menu-item';
+const SUBMENU_CLASS = 'dx-submenu';
 
 fixture.disablePageReloads`Accessibility`
   .page(url(__dirname, '../../container.html'));
@@ -42,3 +46,38 @@ const configuration: Configuration = {
 };
 
 testAccessibility(configuration);
+
+const aiOptions: Options<Properties> = {
+  value: [markup],
+  focusStateEnabled: [true],
+  toolbar: [{ items: ['ai'] }],
+  aiIntegration: [
+    ({} as any),
+    { changeStyle() {} },
+    { changeStyle(_, { onComplete }) { onComplete?.('Result'); } },
+    { changeStyle(_, { onError }) { onError?.('Error' as any); } },
+  ],
+};
+
+const aiCreated = async (t: TestController): Promise<void> => {
+  const htmlEditor = new HtmlEditor('#container');
+
+  await t.click(Selector(defaultSelector));
+
+  await t
+    .click(htmlEditor.toolbar.getItemByName('ai'))
+    .click(Selector(`.${SUBMENU_CLASS} .${MENU_ITEM_CLASS}`).nth(4));
+
+  await t
+    .click(Selector(`.${SUBMENU_CLASS} .${MENU_ITEM_CLASS}`).nth(4)
+      .find(`.${SUBMENU_CLASS} .${MENU_ITEM_CLASS}`).nth(0));
+};
+
+const aiConfiguration: Configuration = {
+  component: 'dxHtmlEditor',
+  a11yCheckConfig,
+  options: aiOptions,
+  created: aiCreated,
+};
+
+testAccessibility(aiConfiguration);
