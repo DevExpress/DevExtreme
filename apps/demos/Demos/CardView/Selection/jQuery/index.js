ce164b062b5ca57212482eeec19e0c0aa308d018
Roman Semenov <roman.semenov@devexpress.com>
2025-05-22 09:02:35 +0400

    implement: jquery

--- apps/demos/Demos/CardView/Selection/jQuery/index.js
+++ apps/demos/Demos/CardView/Selection/jQuery/index.js
@@ -1,7 +1,70 @@
 $(() => {
-  $('#card-view').dxCardView({
-    dataSource: customers,
+  const cardView = $('#card-view').dxCardView({
+    selection: {
+      mode: 'multiple'
+    },
+    selectedCardKeys: [4, 6],
+    dataSource: employees,
     keyExpr: 'ID',
-    columns: ['CompanyName', 'City', 'State', 'Phone', 'Fax'],
-  });
+    columns: ['FullName', 'Position', 'Phone', 'Email'],
+    cardCover: {
+      altExpr: ({ FullName }) => `Photo of ${FullName}`,
+      imageExpr: ({ FullName }) => `../../../../images/employees/new/${FullName}.jpg`,
+    }
+  }).dxCardView('instance');
+
+  function renderOptions() {
+    $("#selection-mode").dxSelectBox({
+      dataSource: ['single', 'multiple'],
+      value: cardView.option('selection.mode'),
+      onOptionChanged(e) {
+        if (e.name === 'value') {
+          cardView.option('selection.mode', e.value);
+          cardView.clearSelection();
+          renderOptions();
+        }
+      },
+    });
+
+    $("#show-checkboxes-mode").dxSelectBox({
+      dataSource: ['always', 'none', 'onClick', 'onLongTap'],
+      value: cardView.option('selection.showCheckBoxesMode'),
+      onOptionChanged(e) {
+        if (e.name === 'value') {
+          cardView.option('selection.showCheckBoxesMode', e.value);
+          renderOptions();
+        }
+      },
+      disabled: cardView.option('selection.mode') !== 'multiple',
+    });
+  
+    $("#allow-select-all").dxSelectBox({
+      dataSource: [true, false],
+      value: cardView.option('selection.allowSelectAll'),
+      onOptionChanged(e) {
+        if (e.name === 'value') {
+          cardView.option('selection.allowSelectAll', e.value);
+          renderOptions();
+        }
+      },
+      disabled: cardView.option('selection.mode') !== 'multiple',
+    });
+
+    $("#select-all-mode").dxSelectBox({
+      dataSource: ['allPages', 'page'],
+      value: cardView.option('selection.selectAllMode'),
+      onOptionChanged(e) {
+        if (e.name === 'value') {
+          cardView.option('selection.selectAllMode', e.value);
+          renderOptions();
+        }
+      },
+      disabled: (
+        cardView.option('selection.mode') !== 'multiple' || 
+        !cardView.option('selection.allowSelectAll')
+      ),
+    });
+  }
+
+  renderOptions();
 });
