eebf5486f2a5c669a0a93e16b12fc8467d7f595b
Roman Semenov <roman.semenov@devexpress.com>
2025-05-23 11:52:11 +0400

    implement

--- apps/demos/Demos/CardView/ColumnsReordering/Angular/app/app.component.ts
+++ apps/demos/Demos/CardView/ColumnsReordering/Angular/app/app.component.ts
@@ -2,7 +2,7 @@ import { NgModule, Component, enableProdMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { DxCardViewModule } from 'devextreme-angular';
-import { Customer, Service } from './app.service';
+import { Order, Service } from './app.service';
 
 if (!/localhost/.test(document.location.host)) {
   enableProdMode();
@@ -20,12 +20,10 @@ if (window && window.config?.packageConfigPaths) {
   providers: [Service],
 })
 export class AppComponent {
-  customers: Customer[];
-
-  columns = ['CompanyName', 'City', 'State', 'Phone', 'Fax'];
+  orders: Order[];
 
   constructor(service: Service) {
-    this.customers = service.getCustomers();
+    this.orders = service.getOrder();
   }
 }
 
