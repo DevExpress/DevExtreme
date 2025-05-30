f49871558fb12c78002c42537be97f1c952641c1
Roman Semenov <roman.semenov@devexpress.com>
2025-05-30 13:50:24 +0400

    CardView - demos - selection (#29980)

--- apps/demos/Demos/CardView/Selection/ReactJs/App.js
+++ apps/demos/Demos/CardView/Selection/ReactJs/App.js
@@ -1,19 +1,94 @@
-import React from 'react';
-import CardView, { Column } from 'devextreme-react/card-view';
-import { customers } from './data.js';
+import React, { useState, useRef, useCallback } from 'react';
+import CardView, { Column, CardCover, Selection } from 'devextreme-react/card-view';
+import SelectBox from 'devextreme-react/select-box';
+import CheckBox from 'devextreme-react/check-box';
+import { employees } from './data.js';
 
-const columns = ['CompanyName', 'City', 'State', 'Phone', 'Fax'];
-const App = () => (
-  <CardView
-    dataSource={customers}
-    keyExpr="ID"
-  >
-    {columns.map((column, index) => (
-      <Column
-        dataField={column}
-        key={index}
-      />
-    ))}
-  </CardView>
-);
+function altExpr({ FullName }) {
+  return `Photo of ${FullName}`;
+}
+function imageExpr({ FullName }) {
+  return `../../../../images/employees/new/${FullName}.jpg`;
+}
+const selectionModeLabel = { 'aria-label': 'Selection Mode' };
+const showCheckBoxesModeLabel = { 'aria-label': 'Show Checkboxes Mode' };
+const selectAllModeLabel = { 'aria-label': 'Select All Mode' };
+const App = () => {
+  const [selectionMode, setSelectionMode] = useState('multiple');
+  const [allowSelectAll, setAllowSelectAll] = useState(true);
+  const [showCheckBoxesMode, setShowCheckBoxesMode] = useState('always');
+  const [selectAllMode, setSelectAllMode] = useState('allPages');
+  const cardViewRef = useRef(null);
+  const onSelectionModeChange = useCallback((value) => {
+    setSelectionMode(value);
+    cardViewRef.current.instance().clearSelection();
+  }, []);
+  return (
+    <React.Fragment>
+      <div className="options">
+        <div className="caption">Options</div>
+        <div className="options-container">
+          <div className="option">
+            <span>Selection Mode</span>
+            <SelectBox
+              inputAttr={selectionModeLabel}
+              dataSource={['single', 'multiple']}
+              value={selectionMode}
+              onValueChange={onSelectionModeChange}
+            ></SelectBox>
+          </div>
+          <div className="option">
+            <span>Show Checkboxes Mode</span>
+            <SelectBox
+              inputAttr={showCheckBoxesModeLabel}
+              dataSource={['always', 'none', 'onClick', 'onLongTap']}
+              value={showCheckBoxesMode}
+              onValueChange={setShowCheckBoxesMode}
+              disabled={selectionMode !== 'multiple'}
+            ></SelectBox>
+          </div>
+          <div className="option">
+            <span>Select All Mode</span>
+            <SelectBox
+              inputAttr={selectAllModeLabel}
+              dataSource={['allPages', 'page']}
+              value={selectAllMode}
+              onValueChange={setSelectAllMode}
+              disabled={selectionMode !== 'multiple' || !allowSelectAll}
+            ></SelectBox>
+          </div>
+          <div className="option">
+            <CheckBox
+              text="Allow Select All"
+              value={allowSelectAll}
+              onValueChange={setAllowSelectAll}
+              disabled={selectionMode !== 'multiple'}
+            ></CheckBox>
+          </div>
+        </div>
+      </div>
+      <CardView
+        dataSource={employees}
+        keyExpr="ID"
+        defaultSelectedCardKeys={[4, 6]}
+        ref={cardViewRef}
+      >
+        <CardCover
+          altExpr={altExpr}
+          imageExpr={imageExpr}
+        />
+        <Selection
+          mode={selectionMode}
+          showCheckBoxesMode={showCheckBoxesMode}
+          allowSelectAll={allowSelectAll}
+          selectAllMode={selectAllMode}
+        />
+        <Column dataField="FullName" />
+        <Column dataField="Position" />
+        <Column dataField="Phone" />
+        <Column dataField="Email" />
+      </CardView>
+    </React.Fragment>
+  );
+};
 export default App;
