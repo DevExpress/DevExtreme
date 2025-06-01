4349d367d57bf2110b5519fe28192acc6cd2fff5
Andrey Dolzhikov <eylau-andrew@mail.ru>
2025-06-02 00:01:52 +0300

    React.js refactoring

--- apps/demos/Demos/CardView/CardTemplate/ReactJs/App.js
+++ apps/demos/Demos/CardView/CardTemplate/ReactJs/App.js
@@ -1,38 +1,67 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import CardView, { Column } from 'devextreme-react/card-view';
+import Popup, { Position } from 'devextreme-react/popup';
 import { vehicles } from './data.js';
 import VehicleCard from './VehicleCard.js';
+import LicenseInfo from './LicenseInfo.js';
 
-const cardTemplate = (model) => <VehicleCard vehicle={model.card.data} />;
-const App = () => (
-  <CardView
-    dataSource={vehicles}
-    cardsPerRow="auto"
-    cardMinWidth={260}
-    cardMaxWidth={260}
-    // todo: move to nested
-    searchPanel={{
-      visible: true,
-    }}
-    paging={{
-      pageSize: 12,
-    }}
-    headerFilter={{
-      visible: true,
-    }}
-    cardRender={cardTemplate}
-  >
-    <Column dataField="TrademarkName"></Column>
-    <Column dataField="Name"></Column>
-    <Column
-      dataField="Price"
-      format="currency"
-    ></Column>
-    <Column dataField="CategoryName"></Column>
+const App = () => {
+  const [popupVisible, setPopupVisible] = useState(false);
+  const [currentVehicle, setCurrentVehicle] = useState(null);
+  const showInfo = useCallback((vehicle) => {
+    setCurrentVehicle(vehicle);
+    setPopupVisible(true);
+  }, []);
+  const hideInfo = useCallback(() => {
+    setPopupVisible(false);
+  }, []);
+  return (
+    <React.Fragment>
+      <CardView
+        dataSource={vehicles}
+        cardsPerRow="auto"
+        cardMinWidth={260}
+        cardMaxWidth={260}
+        searchPanel={{ visible: true }}
+        paging={{ pageSize: 12 }}
+        headerFilter={{ visible: true }}
+        cardRender={(model) => (
+          <VehicleCard
+            vehicle={model.card.data}
+            onShowInfo={showInfo}
+          />
+        )}
+      >
+        <Column dataField="TrademarkName" />
+        <Column dataField="Name" />
+        <Column
+          dataField="Price"
+          format="currency"
+        />
+        <Column dataField="CategoryName" />
+        <Column dataField="Modification" />
+        <Column dataField="BodyStyleName" />
+        <Column dataField="Horsepower" />
+      </CardView>
 
-    <Column dataField="Modification"></Column>
-    <Column dataField="BodyStyleName"></Column>
-    <Column dataField="Horsepower"></Column>
-  </CardView>
-);
+      <Popup
+        width={350}
+        height={190}
+        visible={popupVisible}
+        dragEnabled={false}
+        hideOnOutsideClick={true}
+        showCloseButton={false}
+        showTitle={false}
+        onHiding={hideInfo}
+        contentRender={() => (currentVehicle ? <LicenseInfo vehicle={currentVehicle} /> : null)}
+      >
+        <Position
+          at="center"
+          my="center"
+          collision="fit"
+        />
+      </Popup>
+    </React.Fragment>
+  );
+};
 export default App;
--- apps/demos/Demos/CardView/CardTemplate/ReactJs/VehicleCard.js
+++ apps/demos/Demos/CardView/CardTemplate/ReactJs/VehicleCard.js
@@ -1,66 +1,41 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 import Button from 'devextreme-react/button';
-import Popup, { Position } from 'devextreme-react/popup';
-import LicenseInfo from './LicenseInfo.js';
 
-const VehicleCard = ({ vehicle }) => {
-  const [popupVisible, setPopupVisible] = useState(false);
-  const hideInfo = useCallback(() => {
-    setPopupVisible(false);
-  }, [setPopupVisible]);
-  return (
-    <div className="vehicle__card">
-      <div className="vehicle__img-wrapper">
-        <img
-          className="vehicle__img"
-          src={`../../../../images/vehicles/image_${vehicle.ID}.png`}
-          alt={`${vehicle.TrademarkName} ${vehicle.Name}`}
-        />
+const VehicleCard = ({ vehicle, onShowInfo }) => (
+  <div className="vehicle__card">
+    <div className="vehicle__img-wrapper">
+      <img
+        className="vehicle__img"
+        src={`../../../../images/vehicles/image_${vehicle.ID}.png`}
+        alt={`${vehicle.TrademarkName} ${vehicle.Name}`}
+      />
+    </div>
+    <div className="vehicle__info">
+      <div
+        className="vehicle__name"
+        title={`${vehicle.TrademarkName} ${vehicle.Name}`}
+      >
+        {vehicle.TrademarkName} {vehicle.Name}
       </div>
-      <div className="vehicle__info">
-        <div
-          className="vehicle__name"
-          title={`${vehicle.TrademarkName} ${vehicle.Name}`}
-        >
-          {vehicle.TrademarkName} {vehicle.Name}
+      <div className="vehicle__price">${vehicle.Price}</div>
+      <div className="vehicle__type-container">
+        <div className="vehicle__type">{vehicle.CategoryName}</div>
+      </div>
+      <div className="vehicle__spec-container">
+        <div className="vehicle__modification">{vehicle.Modification}</div>
+        <div className="vehicle__modification">
+          {vehicle.BodyStyleName} {vehicle.Horsepower} h.p.
         </div>
-        <div className="vehicle__price">${vehicle.Price}</div>
-        <div className="vehicle__type-container">
-          <div className="vehicle__type">{vehicle.CategoryName}</div>
-        </div>
-        <div className="vehicle__spec-container">
-          <div className="vehicle__modification">{vehicle.Modification}</div>
-          <div className="vehicle__modification">
-            {vehicle.BodyStyleName} {vehicle.Horsepower} h.p.
-          </div>
-        </div>
-        <div className="vehicle__footer-container">
-          <Button
-            text="Image Info"
-            type="default"
-            width="100%"
-            onClick={() => setPopupVisible(true)}
-          ></Button>
-          <Popup
-            width={350}
-            height={190}
-            visible={popupVisible}
-            dragEnabled={false}
-            hideOnOutsideClick={true}
-            showCloseButton={false}
-            showTitle={false}
-            onHiding={hideInfo}
-            contentRender={() => <LicenseInfo vehicle={vehicle}></LicenseInfo>}
-          >
-            <Position
-              at="center"
-              my="center"
-              collision="fit"
-            />
-          </Popup>
-        </div>
+      </div>
+      <div className="vehicle__footer-container">
+        <Button
+          text="Image Info"
+          type="default"
+          width="100%"
+          onClick={() => onShowInfo(vehicle)}
+        ></Button>
       </div>
     </div>
-  );
-};
+  </div>
+);
 export default VehicleCard;
