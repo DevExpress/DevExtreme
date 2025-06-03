414a50cbb87ae73541c38f98070c79dbd7c73b02
Roman Semenov <roman.semenov@devexpress.com>
2025-06-03 09:22:36 +0400

    fix: adjust toolbar items order

--- packages/devextreme/js/__internal/grids/new/grid_core/toolbar/const.tsx
+++ packages/devextreme/js/__internal/grids/new/grid_core/toolbar/const.tsx
@@ -2,9 +2,9 @@
  * used for defining default toolbar items and their default order in header panel
  */
 export const DEFAULT_TOOLBAR_ITEMS = [
-  'columnChooserButton',
-  'searchPanel',
-  'addCardButton',
   'selectAllButton',
   'clearSelectionButton',
+  'addCardButton',
+  'columnChooserButton',
+  'searchPanel',
 ] as const;
