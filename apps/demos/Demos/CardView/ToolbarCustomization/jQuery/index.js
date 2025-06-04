c06d797e93c934b751e232334dfd9000a7db5d6b
Roman Semenov <roman.semenov@devexpress.com>
2025-06-04 15:03:36 +0400

    fix: remove toolbarCustomization demo

--- apps/demos/Demos/CardView/ToolbarCustomization/jQuery/index.js
+++
@@ -1,9 +0,0 @@
-$(() => {
-  $('#card-view').dxCardView({
-    dataSource: customers,
-    keyExpr: 'ID',
-    cardMinWidth: 100,
-    wordWrapEnabled: true,
-    columns: ['CompanyName', 'City', 'State', 'Phone', 'Fax'],
-  });
-});
