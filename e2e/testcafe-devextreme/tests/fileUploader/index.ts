c8081c9e3329c3b1deddcde18005e0795c4f1e9b
Ruslan Farkhutdinov <ruslan.farkhutdinov@devexpress.com>
2025-06-05 16:47:24 +0300

    Return file uploading in test

--- e2e/testcafe-devextreme/tests/fileUploader/index.ts
+++ e2e/testcafe-devextreme/tests/fileUploader/index.ts
@@ -4,6 +4,7 @@ import { createWidget } from '../../helpers/createWidget';
 import url from '../../helpers/getPageUrl';
 
 const TEST_FILE = './images/test-image-1.png';
+const INPUT_SELECTOR = '.dx-fileuploader input[type=file]';
 
 fixture.disablePageReloads`FileUploader - file list visibility`.page(url(__dirname, '../container.html'));
 
@@ -11,10 +12,12 @@ fixture.disablePageReloads`FileUploader - file list visibility`.page(url(__dirna
   test(`FileUploader with showFileList: ${showFileList} - after file selected`, async (t) => {
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
+    await t.setFilesToUpload(INPUT_SELECTOR, [TEST_FILE]);
+
     await testScreenshot(t, takeScreenshot, `fileuploader-show-filelist-${showFileList}.png`, {
       element: '#container',
     });
 
     await t.expect(compareResults.isValid()).ok(compareResults.errorMessages());
-  }).before(async () => { await createWidget('dxFileUploader', { showFileList, value: TEST_FILE }); });
+  }).before(async () => createWidget('dxFileUploader', { showFileList }));
 });
