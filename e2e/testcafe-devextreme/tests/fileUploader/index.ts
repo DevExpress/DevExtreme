31a7d5607552979026a47ba2085b003e875e18f5
Ruslan Farkhutdinov <ruslan.farkhutdinov@devexpress.com>
2025-06-05 11:11:20 +0300

    FileUploader: Update screenshot test

--- e2e/testcafe-devextreme/tests/fileUploader/index.ts
+++ e2e/testcafe-devextreme/tests/fileUploader/index.ts
@@ -1,19 +1,18 @@
-import { Selector } from 'testcafe';
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import { testScreenshot } from '../../helpers/themeUtils';
 import { createWidget } from '../../helpers/createWidget';
 import url from '../../helpers/getPageUrl';
 
 const TEST_FILE = './images/test-image-1.png';
+const INPUT_SELECTOR = '.dx-fileuploader input[type=file]';
 
 fixture.disablePageReloads`FileUploader - file list visibility`.page(url(__dirname, '../container.html'));
 
 [true, false].forEach((showFileList) => {
   test(`FileUploader with showFileList: ${showFileList} - after file selected`, async (t) => {
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
-    const input = Selector('.dx-fileuploader input[type=file]');
 
-    await t.setFilesToUpload(input, [TEST_FILE]);
+    await t.setFilesToUpload(INPUT_SELECTOR, [TEST_FILE]);
 
     await testScreenshot(t, takeScreenshot, `fileuploader-show-filelist-${showFileList}.png`, {
       element: '#container',
