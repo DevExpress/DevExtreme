2e42994222d0ac0a4e2924586c22b6edaf8c95d7
Ruslan Farkhutdinov <ruslan.farkhutdinov@devexpress.com>
2025-06-05 14:29:42 +0300

    FileUploader: Update test to display file selected initially

--- e2e/testcafe-devextreme/tests/fileUploader/index.ts
+++ e2e/testcafe-devextreme/tests/fileUploader/index.ts
@@ -4,7 +4,6 @@ import { createWidget } from '../../helpers/createWidget';
 import url from '../../helpers/getPageUrl';
 
 const TEST_FILE = './images/test-image-1.png';
-const INPUT_SELECTOR = '.dx-fileuploader input[type=file]';
 
 fixture.disablePageReloads`FileUploader - file list visibility`.page(url(__dirname, '../container.html'));
 
@@ -12,12 +11,10 @@ fixture.disablePageReloads`FileUploader - file list visibility`.page(url(__dirna
   test(`FileUploader with showFileList: ${showFileList} - after file selected`, async (t) => {
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-    await t.setFilesToUpload(INPUT_SELECTOR, [TEST_FILE]);
-
     await testScreenshot(t, takeScreenshot, `fileuploader-show-filelist-${showFileList}.png`, {
       element: '#container',
     });
 
     await t.expect(compareResults.isValid()).ok(compareResults.errorMessages());
-  }).before(async () => createWidget('dxFileUploader', { showFileList }));
+  }).before(async () => { await createWidget('dxFileUploader', { showFileList, value: TEST_FILE }); });
 });
