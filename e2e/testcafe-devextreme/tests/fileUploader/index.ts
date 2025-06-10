03a4291774b2e7fe5a35ecce644a4e01dfadd010
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2025-06-10 12:48:31 +0400

    emove disablePageReload

--- e2e/testcafe-devextreme/tests/fileUploader/index.ts
+++ e2e/testcafe-devextreme/tests/fileUploader/index.ts
@@ -7,7 +7,8 @@ import url from '../../helpers/getPageUrl';
 const TEST_FILE = './images/test-image-1.png';
 const INPUT_SELECTOR = '.dx-fileuploader input';
 
-fixture.disablePageReloads`FileUploader - file list visibility`.page(url(__dirname, '../container.html'));
+fixture`FileUploader - file list visibility`
+  .page(url(__dirname, '../container.html'));
 
 [true, false].forEach((showFileList) => {
   test(`FileUploader with showFileList: ${showFileList} - after file selected`, async (t) => {
