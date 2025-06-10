6f16fc2fe3e2bf52328de13d9b493e711fddb2d9
Andrey Dolzhikov <eylau-andrew@mail.ru>
2025-06-10 17:04:31 +0300

    CardView: Card template demo (#30029)
    Co-authored-by: wdevfx <ilya.vinogradov@devexpress.com>

--- apps/demos/Demos/CardView/CardTemplate/Vue/App.vue
+++ apps/demos/Demos/CardView/CardTemplate/Vue/App.vue
@@ -1,17 +1,86 @@
 <template>
   <DxCardView
-    :data-source="customers"
-    key-expr="ID"
+    :data-source="vehicles"
+    cards-per-row="auto"
+    :card-min-width="260"
+    card-template="cardTemplate"
   >
+    <DxHeaderFilter :visible="true" />
+    <DxSearchPanel :visible="true" />
+    <DxPaging :pageSize="12" />
+
+    <DxColumn data-field="TrademarkName"/>
+    <DxColumn data-field="Name"/>
     <DxColumn
-      v-for="column in columns"        
-      :data-field="column"
-    />
+      data-field="Price"
+      format="currency"
+    >
+      <DxHeaderFilter :groupInterval="20000" />
+    </DxColumn>
+    <DxColumn data-field="CategoryName"/>
+    <DxColumn data-field="Modification"/>
+    <DxColumn data-field="BodyStyleName"/>
+    <DxColumn data-field="Horsepower"/>
+    <template #cardTemplate="{
+        data: {
+          card: {
+            data: vehicle,
+            data: {
+              ID,
+              TrademarkName,
+              Name,
+              CategoryName,
+              Modification,
+              BodyStyleName,
+              Horsepower,
+            },
+            fields,
+          }
+        }
+      }">
+        <VehicleCard
+          :vehicle="vehicle"
+          :id="ID"
+          :model="`${TrademarkName} ${Name}`"
+          :price="fields?.find(f => f.column?.dataField === 'Price')?.text"
+          :category-name="CategoryName"
+          :modification="Modification"
+          :body-style-name="BodyStyleName"
+          :horsepower="Horsepower"
+          @show-info="showInfo" />
+    </template>
   </DxCardView>
+  <DxPopup
+    :width="350"
+    :height="240"
+    v-model:visible="popupVisible"
+    :dragEnabled="false"
+    :hideOnOutsideClick="true"
+    title="Image Info"
+    :onHiding="hideInfo"
+  >
+    <DxPosition at="center" my="center" collision="fit" />
+      <template #content>
+        <LicenseInfo :vehicle="currentVehicle" />
+      </template>
+  </DxPopup>
 </template>
 <script setup lang="ts">
-  import { DxCardView, DxColumn } from 'devextreme-vue/card-view';
-  import { customers } from './data.ts';
+import { DxCardView, DxColumn, DxHeaderFilter, DxSearchPanel, DxPaging } from 'devextreme-vue/card-view';
+import { DxPopup, DxPosition } from 'devextreme-vue/popup';
+import LicenseInfo from './LicenseInfo.vue';
+import VehicleCard from './VehicleCard.vue';
+import { vehicles } from './data.ts';
+import { ref } from 'vue';
 
-  const columns = ['CompanyName', 'City', 'State', 'Phone', 'Fax'];
+const popupVisible = ref(false);
+const currentVehicle = ref(null);
+
+function showInfo(vehicle) {
+  currentVehicle.value = vehicle;
+  popupVisible.value = true;
+}
+function hideInfo() {
+  popupVisible.value = false;
+}
 </script>
