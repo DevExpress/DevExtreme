d568f19249b82b8bedd33573e7be7f18d019505e
Ruslan Farkhutdinov <ruslan.farkhutdinov@devexpress.com>
2025-06-10 13:44:33 +0300

    FileUploader: Replace Selecgor with FileUploader model

--- e2e/testcafe-devextreme/tests/fileUploader/index.ts
+++ e2e/testcafe-devextreme/tests/fileUploader/index.ts
@@ -1,26 +1,26 @@
-import { Selector } from 'testcafe';
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import FileUploader from 'testcafe-models/fileUploader';
 import { testScreenshot } from '../../helpers/themeUtils';
 import { createWidget } from '../../helpers/createWidget';
 import url from '../../helpers/getPageUrl';
 
 const TEST_FILE = './images/test-image-1.png';
-const INPUT_SELECTOR = '.dx-fileuploader input';
 
 fixture`FileUploader - file list visibility`
   .page(url(__dirname, '../container.html'));
 
 [true, false].forEach((showFileList) => {
   test(`FileUploader with showFileList: ${showFileList} - after file selected`, async (t) => {
+    const fileUploader = new FileUploader('#container');
     const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
-    await t.setFilesToUpload(Selector(INPUT_SELECTOR), [TEST_FILE]);
+    await t.setFilesToUpload(fileUploader.input, [TEST_FILE]);
 
     await testScreenshot(t, takeScreenshot, `fileuploader-show-filelist-${showFileList}.png`, {
       element: '#container',
     });
 
-    await t.clearUpload(Selector(INPUT_SELECTOR));
+    await t.clearUpload(fileUploader.input);
 
     await t.expect(compareResults.isValid()).ok(compareResults.errorMessages());
   }).before(async () => createWidget('dxFileUploader', { showFileList }));
