db6b055d32f94ac95ce71096c63ab84c72a3f7b2
Roman Semenov <roman.semenov@devexpress.com>
2025-06-12 08:23:04 +0400

    fix

--- packages/devextreme/js/__internal/grids/new/grid_core/filtering/utils.ts
+++ packages/devextreme/js/__internal/grids/new/grid_core/filtering/utils.ts
@@ -1,6 +1,9 @@
+import type { FilterDescriptor } from '@js/data';
+import { isString } from '@ts/core/utils/m_type';
 import { getFilterExpression } from '@ts/filter_builder/m_utils';
 
 import type { Column } from '../columns_controller/types';
+import { addDataFieldToComputedColumns } from '../columns_controller/utils';
 import type { AppliedFilters } from './types';
 
 export const getAppliedFilterExpressions = (
@@ -10,14 +13,38 @@ export const getAppliedFilterExpressions = (
   filterSyncEnabled: boolean,
 ): unknown[] => {
   const filters = [
-    getFilterExpression(appliedFilters.filterPanel, columns, customOperations, 'filterBuilder'),
+    getFilterExpression(appliedFilters.filterPanel, addDataFieldToComputedColumns(columns), customOperations, 'filterBuilder'),
     // Note: Search filters do not contain filter expressions
     appliedFilters.search,
   ];
 
   if (!filterSyncEnabled) {
-    filters.push(getFilterExpression(appliedFilters.headerFilter, columns, customOperations, 'headerFilter'));
+    filters.push(getFilterExpression(appliedFilters.headerFilter, addDataFieldToComputedColumns(columns), customOperations, 'headerFilter'));
   }
 
   return filters.filter((filter) => filter);
+};
+
+export const normalizeFilterWithSelectors = (
+  filter: FilterDescriptor,
+  columns: Column[],
+  remoteFiltering: boolean,
+): FilterDescriptor => {
+  if (!Array.isArray(filter)) return filter;
+
+  const resultFilter = [...filter];
+
+  if (isString(resultFilter[0]) && resultFilter[0] !== '!') {
+    const column = columns.find((c) => (c.dataField ?? c.name) === resultFilter[0]);
+
+    if (column && !remoteFiltering) {
+      resultFilter[0] = column.calculateFieldValue.bind(column);
+    }
+  }
+
+  for (let i = 0; i < resultFilter.length; i += 1) {
+    resultFilter[i] = normalizeFilterWithSelectors(resultFilter[i], columns, remoteFiltering);
+  }
+
+  return resultFilter;
 };
