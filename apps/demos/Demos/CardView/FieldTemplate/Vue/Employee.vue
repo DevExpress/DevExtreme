81360240f6921de065f6d398474259e8a46f0a66
Andrey Dolzhikov <eylau-andrew@mail.ru>
2025-06-13 13:45:53 +0300

    CardView: Field Template demo (#30079)

---
+++ apps/demos/Demos/CardView/FieldTemplate/Vue/Employee.vue
@@ -0,0 +1,16 @@
+<template>
+    <a href="#">{{ employee.Name }}</a>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { employees } from './data.ts';
+
+const props = defineProps<{
+  employeeID: number,
+}>();
+
+const employee = computed(() =>
+  employees.find(e => e.ID === props.employeeID)
+);
+</script>
