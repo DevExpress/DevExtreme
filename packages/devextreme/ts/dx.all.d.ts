2c6ccaa81007b06cfc9f6c5a9f12761d89f9cffb
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2025-07-02 21:59:09 +0400

    TabPanel: add itemIndex to the TitleClickEvent typescript definition (T1296996) (#30258)

--- packages/devextreme/ts/dx.all.d.ts
+++ packages/devextreme/ts/dx.all.d.ts
@@ -26758,6 +26758,10 @@ declare module DevExpress.ui {
        * [descr:TabPanelItemInfo.itemElement]
        */
       readonly itemElement?: DevExpress.core.DxElement;
+      /**
+       * [descr:TabPanelItemInfo.itemIndex]
+       */
+      readonly itemIndex: number;
     }
     /**
      * [descr:_ui_tab_panel_TitleClickEvent]
