356c57daa66d8619c083c9bbeb154d8a28efc562
Alexander Kozlovskiy <zedwag@gmail.com>
2025-07-10 18:58:55 +0400

    extract svg to separate files

--- apps/demos/Demos/ColorBox/Overview/jQuery/index.js
+++ apps/demos/Demos/ColorBox/Overview/jQuery/index.js
@@ -46,10 +46,10 @@ $(() => {
       'aria-label': 'Event Handling',
     },
     onValueChanged(e) {
-      $('.brush').css('fill', e.component.option('value'));
+      $('.brush').css('color', e.component.option('value'));
     },
     onInput(e) {
-      $('.brush').css('fill', e.event.target.value);
+      $('.brush').css('color', e.event.target.value);
     },
   });
 });
