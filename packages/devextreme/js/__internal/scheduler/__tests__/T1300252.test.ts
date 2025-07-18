a18cc27106b686685b96b1266ea54ebccb64cc3e
Vladimir Bushmanov <vladimir.bushmanov@devexpress.com>
2025-07-18 19:49:22 +0200

    add jest test

---
+++ packages/devextreme/js/__internal/scheduler/__tests__/T1300252.test.ts
@@ -0,0 +1,58 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+import {
+  describe, expect, it,
+} from '@jest/globals';
+import { DataSource } from '@ts/data/data_source/m_data_source';
+import CustomStore from '@ts/data/m_custom_store';
+
+import Scheduler from '../m_scheduler';
+import { setupSchedulerTestEnvironment } from './__mock__/m_mock_scheduler';
+
+const dataSource = [
+  {
+    startDate: new Date(2024, 8, 7, 0, 0),
+    endDate: new Date(2024, 8, 7, 8, 0),
+    roomId: 1,
+    priorityId: 1,
+    text: 'Install New Database',
+  },
+];
+const rooms = [
+  { id: 1, text: 'Room 2', color: 'rgb(142, 205, 60)' },
+];
+const getAppointmentColor = (container: HTMLDivElement): string => {
+  const appointment = container.querySelector('.dx-scheduler-appointment') as HTMLDivElement;
+  return appointment.style.backgroundColor;
+};
+
+describe('T1300252', () => {
+  it('should render correct appointment color for remote datasource', async () => {
+    setupSchedulerTestEnvironment();
+
+    const dataPromise = new Promise((resolve) => {
+      setTimeout(resolve, 100, rooms);
+    });
+    const container = document.createElement('div');
+    const scheduler = new Scheduler(container, {
+      views: ['month'],
+      currentView: 'month',
+      currentDate: new Date(2024, 8, 8),
+      dataSource,
+      resources: [{
+        fieldExpr: 'roomId',
+        label: 'Room',
+        dataSource: new DataSource({
+          store: new CustomStore({
+            load: () => dataPromise,
+          }),
+          paginate: false,
+        }),
+      }],
+    } as any);
+    await dataPromise;
+    await new Promise(process.nextTick);
+
+    expect(getAppointmentColor(container)).toBe(rooms[0].color);
+  });
+});
