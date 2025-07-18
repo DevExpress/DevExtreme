3eb50d77100d5191d8541b4f8bd7c447eebede3a
Vladimir Bushmanov <vladimir.bushmanov@devexpress.com>
2025-07-18 16:13:45 +0200

    lint fix

--- packages/devextreme/jsdom-with-timezone.js
+++ packages/devextreme/jsdom-with-timezone.js
@@ -2,7 +2,7 @@ const JSDOMEnvironment = require('jest-environment-jsdom').TestEnvironment;
 
 module.exports = class TimezoneAwareJSDOMEnvironment extends JSDOMEnvironment {
     constructor(config, context) {
-        // eslint-disable-next-line spellcheck/spell-checker
+        // eslint-disable-next-line
         process.env.TZ = context.docblockPragmas.timezone || process.env.TZ || 'UTC';
 
         super(config, context);
