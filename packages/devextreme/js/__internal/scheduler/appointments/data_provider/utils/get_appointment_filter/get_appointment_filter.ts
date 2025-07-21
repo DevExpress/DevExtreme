d2fe9993c1a9a33e858e3e1903af8edf62c691ad
wdevfx <ilya.vinogradov@devexpress.com>
2025-07-21 15:12:20 +0400

    test(Scheduler): add test to all-day shift functions in filtration algorithm

--- packages/devextreme/js/__internal/scheduler/appointments/data_provider/utils/get_appointment_filter/get_appointment_filter.ts
+++ packages/devextreme/js/__internal/scheduler/appointments/data_provider/utils/get_appointment_filter/get_appointment_filter.ts
@@ -1,5 +1,6 @@
-import { dateUtilsTs } from '@ts/core/utils/date';
-import { dateUtils } from '@ts/core/utils/m_date';
+import {
+  getAppointmentOccurrenceDates,
+} from '@ts/scheduler/appointments/data_provider/utils/get_appointment_filter/get_appointment_occurrence_dates';
 
 import type { TimeZoneCalculator } from '../../../../r1/timezone_calculator/calculator';
 import { isAppointmentTakesAllDay } from '../../../../r1/utils/index';
@@ -8,86 +9,6 @@ import type { FilterOptions } from '../type';
 import { getAppointmentsOccurrences } from './get_appointments_occurrences';
 import { isAppointmentMatchedIntervals } from './is_appointment_matched_intervals';
 import { isAppointmentMatchedResources } from './is_appointment_matched_resources';
-
-const toMs = dateUtils.dateToMilliseconds;
-const SECOND_MS = toMs('second');
-const DAY_MS = toMs('day');
-const DAY_WITHOUT_ONE_SECOND_MS = toMs('day') - toMs('second');
-
-const getShiftedAllDayStartDate = (
-  originalStartDate: Date,
-  viewOffset: number,
-): Date => {
-  const trimmedDate = dateUtils.trimTime(originalStartDate);
-  const startOfDay = dateUtilsTs.addOffsets(trimmedDate, [viewOffset]);
-  const endOfDay = dateUtilsTs.addOffsets(trimmedDate, [DAY_WITHOUT_ONE_SECOND_MS, viewOffset]);
-
-  switch (true) {
-    case originalStartDate > endOfDay:
-      return dateUtilsTs.addOffsets(endOfDay, [SECOND_MS]);
-    case originalStartDate < startOfDay:
-      return dateUtilsTs.addOffsets(startOfDay, [-DAY_MS]);
-    // NOTE: originalStartDate in interval [startOfDay, endOfDay]
-    // (include border points)
-    default:
-      return startOfDay;
-  }
-};
-
-const getShiftedAllDayEndDate = (
-  originalEndDate: Date,
-  viewOffset: number,
-): Date => {
-  const trimmedDate = dateUtils.trimTime(originalEndDate);
-  const startOfDay = dateUtilsTs.addOffsets(trimmedDate, [viewOffset]);
-  const endOfDay = dateUtilsTs.addOffsets(trimmedDate, [DAY_WITHOUT_ONE_SECOND_MS, viewOffset]);
-
-  switch (true) {
-    case originalEndDate > endOfDay:
-      return dateUtilsTs.addOffsets(endOfDay, [DAY_MS]);
-    case originalEndDate < startOfDay:
-      return dateUtilsTs.addOffsets(startOfDay, [-SECOND_MS]);
-    // NOTE: originalEndDate in interval [startOfDay, endOfDay]
-    // (include border points)
-    default:
-      return endOfDay;
-  }
-};
-
-const getShiftedAppointmentOccurrenceDates = (
-  {
-    startDate: originalStartDate,
-    endDate: originalEndDate,
-    allDay,
-  }: AppointmentDataItem,
-  viewOffset: number,
-): { startDate: Date; endDate: Date } => {
-  switch (true) {
-    // NOTE: For regular appointments -> return original dates
-    case !allDay:
-      return {
-        startDate: originalStartDate,
-        endDate: originalEndDate,
-      };
-    // NOTE: If viewOffset isn't set -> "round" dates
-    // E.g: ['2024-02-01T10:00:00', '2024-02-02T11:00:00']
-    // -> ['2024-02-01T00:00:00', '2024-02-02T23:59:59']
-    case viewOffset === 0:
-      return {
-        startDate: dateUtils.trimTime(originalStartDate),
-        endDate: dateUtilsTs.addOffsets(
-          dateUtils.trimTime(originalEndDate),
-          [DAY_WITHOUT_ONE_SECOND_MS],
-        ),
-      };
-    // NOTE: allDay appointment + viewOffset is set case
-    default:
-      return {
-        startDate: getShiftedAllDayStartDate(originalStartDate, viewOffset),
-        endDate: getShiftedAllDayEndDate(originalEndDate, viewOffset),
-      };
-  }
-};
 
 export const getAppointmentFilter = (
   filterOptions: FilterOptions,
@@ -144,7 +65,7 @@ export const getAppointmentFilter = (
       timeZoneCalculator,
     ).map((occurrence) => ({
       ...occurrence,
-      ...getShiftedAppointmentOccurrenceDates(occurrence, viewOffset),
+      ...getAppointmentOccurrenceDates(occurrence, viewOffset),
     }));
 
     return appointmentOccurrences.some(
---
+++ packages/devextreme/js/__internal/scheduler/appointments/data_provider/utils/get_appointment_filter/get_appointment_occurrence_dates.ts
@@ -0,0 +1,83 @@
+import { dateUtilsTs } from '@ts/core/utils/date';
+import { dateUtils } from '@ts/core/utils/m_date';
+import type { AppointmentDataItem } from '@ts/scheduler/types';
+
+const toMs = dateUtils.dateToMilliseconds;
+const SECOND_MS = toMs('second');
+const DAY_MS = toMs('day');
+const DAY_WITHOUT_ONE_SECOND_MS = toMs('day') - toMs('second');
+
+export const getShiftedAllDayStartDate = (
+  originalStartDate: Date,
+  viewOffset: number,
+): Date => {
+  const trimmedDate = dateUtils.trimTime(originalStartDate);
+  const startOfDay = dateUtilsTs.addOffsets(trimmedDate, [viewOffset]);
+  const endOfDay = dateUtilsTs.addOffsets(trimmedDate, [DAY_WITHOUT_ONE_SECOND_MS, viewOffset]);
+
+  switch (true) {
+    case originalStartDate > endOfDay:
+      return dateUtilsTs.addOffsets(endOfDay, [SECOND_MS]);
+    case originalStartDate < startOfDay:
+      return dateUtilsTs.addOffsets(startOfDay, [-DAY_MS]);
+    // NOTE: originalStartDate in interval [startOfDay, endOfDay]
+    // (include border points)
+    default:
+      return startOfDay;
+  }
+};
+
+export const getShiftedAllDayEndDate = (
+  originalEndDate: Date,
+  viewOffset: number,
+): Date => {
+  const trimmedDate = dateUtils.trimTime(originalEndDate);
+  const startOfDay = dateUtilsTs.addOffsets(trimmedDate, [viewOffset]);
+  const endOfDay = dateUtilsTs.addOffsets(trimmedDate, [DAY_WITHOUT_ONE_SECOND_MS, viewOffset]);
+
+  switch (true) {
+    case originalEndDate > endOfDay:
+      return dateUtilsTs.addOffsets(endOfDay, [DAY_MS]);
+    case originalEndDate < startOfDay:
+      return dateUtilsTs.addOffsets(startOfDay, [-SECOND_MS]);
+    // NOTE: originalEndDate in interval [startOfDay, endOfDay]
+    // (include border points)
+    default:
+      return endOfDay;
+  }
+};
+
+export const getAppointmentOccurrenceDates = (
+  {
+    startDate: originalStartDate,
+    endDate: originalEndDate,
+    allDay,
+  }: AppointmentDataItem,
+  viewOffset: number,
+): { startDate: Date; endDate: Date } => {
+  switch (true) {
+    // NOTE: For regular appointments -> return original dates
+    case !allDay:
+      return {
+        startDate: originalStartDate,
+        endDate: originalEndDate,
+      };
+    // NOTE: If viewOffset isn't set -> "round" dates
+    // E.g: ['2024-02-01T10:00:00', '2024-02-02T11:00:00']
+    // -> ['2024-02-01T00:00:00', '2024-02-02T23:59:59']
+    case viewOffset === 0:
+      return {
+        startDate: dateUtils.trimTime(originalStartDate),
+        endDate: dateUtilsTs.addOffsets(
+          dateUtils.trimTime(originalEndDate),
+          [DAY_WITHOUT_ONE_SECOND_MS],
+        ),
+      };
+    // NOTE: allDay appointment + viewOffset is set case
+    default:
+      return {
+        startDate: getShiftedAllDayStartDate(originalStartDate, viewOffset),
+        endDate: getShiftedAllDayEndDate(originalEndDate, viewOffset),
+      };
+  }
+};
