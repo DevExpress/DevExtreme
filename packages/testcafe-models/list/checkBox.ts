24fe366c8fda20046963551742fec8f0270b0150
wdevfx <ilya.vinogradov@devexpress.com>
2025-07-28 15:03:56 +0400

    feat(CheckBox): add indeterminate state to POM model

--- packages/testcafe-models/list/checkBox.ts
+++ packages/testcafe-models/list/checkBox.ts
@@ -1,6 +1,7 @@
 const CLASS = {
   checkbox: 'dx-checkbox',
   checkboxChecked: 'dx-checkbox-checked',
+  indeterminate: 'dx-checkbox-indeterminate',
   focused: 'dx-state-focused',
 };
 
@@ -11,9 +12,28 @@ export default class CheckBox {
 
   isFocused: Promise<boolean>;
 
+  isIndeterminate: Promise<boolean>;
+
   constructor(item: Selector) {
     this.element = item.find(`.${CLASS.checkbox}`);
     this.isChecked = this.element.hasClass(CLASS.checkboxChecked);
+    this.isIndeterminate = this.element.hasClass(CLASS.indeterminate);
     this.isFocused = item.hasClass(CLASS.focused);
+  }
+
+  async getCheckBoxState(): Promise<'checked' | 'unchecked' | 'indeterminate'> {
+    const isChecked = await this.isChecked;
+    const isIndeterminate = await this.isIndeterminate;
+
+    switch (true) {
+      case isChecked && !isIndeterminate:
+        return 'checked';
+        case !isChecked && !isIndeterminate:
+          return 'unchecked';
+      case !isChecked && isIndeterminate:
+        return 'indeterminate';
+      default:
+        throw Error(`Invalid checkbox state. checked = ${isChecked}, indeterminate = ${isIndeterminate}`);
+    }
   }
 }
