236266b0124661ed26dea82959e8a9477727f813
anna.shakhova <anna.shakhova@devexpress.com>
2025-07-29 12:36:53 +0200

    Menu & ContextMenu: fix imports in tests

--- packages/devextreme/testing/tests/DevExpress.ui.widgets/menu.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.widgets/menu.tests.js
@@ -6,7 +6,7 @@ import config from 'core/config';
 import Submenu from '../../../js/__internal/ui/menu/submenu';
 import resizeCallbacks from 'core/utils/resize_callbacks';
 import domAdapter from '__internal/core/m_dom_adapter';
-import Menu from '__internal/ui/menu/m_menu';
+import Menu from '__internal/ui/menu/menu';
 import keyboardMock from '../../helpers/keyboardMock.js';
 import fixtures from '../../helpers/positionFixtures.js';
 import { CustomStore } from 'common/data/custom_store';
