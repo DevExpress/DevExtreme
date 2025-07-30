aea8e7b4fc1d7ad4087469e2183b0861730b5afa
Vladimir Bushmanov <bushmanov.vova@yandex.ru>
2025-07-30 12:13:02 +0200

    Scheduler: fix jest timezones (#30526)
    Co-authored-by: Vladimir Bushmanov <vladimir.bushmanov@devexpress.com>
    Co-authored-by: wdevfx <ilya.vinogradov@devexpress.com>

--- packages/devextreme/jsdom-with-timezone.js
+++ packages/devextreme/jsdom-with-timezone.js
@@ -3,7 +3,10 @@ const JSDOMEnvironment = require('jest-environment-jsdom').TestEnvironment;
 module.exports = class TimezoneAwareJSDOMEnvironment extends JSDOMEnvironment {
     constructor(config, context) {
         // eslint-disable-next-line spellcheck/spell-checker
-        process.env.TZ = context.docblockPragmas.timezone || process.env.TZ || 'UTC';
+        const { timezone } = context.docblockPragmas;
+        if(timezone) {
+            process.env.TZ = timezone;
+        }
 
         super(config, context);
     }
