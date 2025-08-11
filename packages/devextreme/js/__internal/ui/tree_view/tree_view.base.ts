69945c61eac67ec0c2e5f87433b668cbd8383802
andrei.kharitonov <pharret31@gmail.com>
2025-08-11 17:31:01 +0200

    Add export

--- packages/devextreme/js/__internal/ui/tree_view/tree_view.base.ts
+++ packages/devextreme/js/__internal/ui/tree_view/tree_view.base.ts
@@ -55,7 +55,7 @@ const NODE_LOAD_INDICATOR_CLASS = `${NODE_CLASS}-loadindicator`;
 const OPENED_NODE_CONTAINER_CLASS = `${NODE_CLASS}-container-opened`;
 const IS_LEAF = `${NODE_CLASS}-is-leaf`;
 
-const ITEM_CLASS = `${WIDGET_CLASS}-item`;
+export const ITEM_CLASS = `${WIDGET_CLASS}-item`;
 const ITEM_WITH_CHECKBOX_CLASS = `${ITEM_CLASS}-with-checkbox`;
 const ITEM_WITH_CUSTOM_EXPANDER_ICON_CLASS = `${ITEM_CLASS}-with-custom-expander-icon`;
 const CUSTOM_EXPANDER_ICON_ITEM_CONTAINER_CLASS = `${WIDGET_CLASS}-custom-expander-icon-item-container`;
