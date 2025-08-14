7fc5f3391ca2b8397df05220fb4c2851b3ea8a40
Vladimir Bushmanov <vladimir.bushmanov@devexpress.com>
2025-08-14 09:42:18 +0200

    Scheduler: unskip demo tests

--- apps/demos/testing/skipped-tests.js
+++ apps/demos/testing/skipped-tests.js
@@ -49,9 +49,6 @@ export const skippedTests = {
       { demo: 'Markers', themes: [THEME.generic, THEME.material, THEME.fluent] },
       { demo: 'Routes', themes: [THEME.generic, THEME.material, THEME.fluent] },
     ],
-    Scheduler: [
-      { demo: 'Overview', themes: [THEME.fluent, THEME.material] },
-    ],
     PivotGrid: [
       { demo: 'Overview', themes: [THEME.material] },
       { demo: 'ChartIntegration', themes: [THEME.material] },
@@ -115,10 +112,6 @@ export const skippedTests = {
     Gantt: [
       { demo: 'Validation', themes: [THEME.generic, THEME.material, THEME.fluent] },
       { demo: 'ContextMenu', themes: [THEME.material] },
-    ],
-    Scheduler: [
-      { demo: 'Overview', themes: [THEME.fluent, THEME.material] },
-      { demo: 'GroupByDate', themes: [THEME.fluent, THEME.material] },
     ],
     List: [
       { demo: 'Search', themes: [THEME.material] },
