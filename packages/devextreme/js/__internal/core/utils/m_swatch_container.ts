252d9e78fff72b1d0d11e0735bd3ec02f794646e
marker dao ® <youdontknow@marker-dao.eth>
2025-08-15 17:40:47 +0200

    SwatchContainer: Improve typing

--- packages/devextreme/js/__internal/core/utils/m_swatch_container.ts
+++ packages/devextreme/js/__internal/core/utils/m_swatch_container.ts
@@ -1,13 +1,17 @@
+import type { dxElementWrapper } from '@js/core/renderer';
 import $ from '@js/core/renderer';
 import { value } from '@js/core/utils/view_port';
 
 const SWATCH_CONTAINER_CLASS_PREFIX = 'dx-swatch-';
 
-const getSwatchContainer = (element) => {
+const getSwatchContainer = (element: Element): dxElementWrapper => {
   const $element = $(element);
   const swatchContainer = $element.closest(`[class^="${SWATCH_CONTAINER_CLASS_PREFIX}"], [class*=" ${SWATCH_CONTAINER_CLASS_PREFIX}"]`);
-  const viewport = value();
-  if (!swatchContainer.length) return viewport;
+  const viewport: dxElementWrapper = value();
+
+  if (!swatchContainer.length) {
+    return viewport;
+  }
 
   const swatchClassRegex = new RegExp(`(\\s|^)(${SWATCH_CONTAINER_CLASS_PREFIX}.*?)(\\s|$)`);
   const swatchClass = swatchContainer[0].className.match(swatchClassRegex)[2];
