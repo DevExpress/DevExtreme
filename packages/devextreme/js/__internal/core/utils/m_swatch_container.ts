9bfb079dfb884d926cbf5f0cb92613286ac71bf6
marker dao ® <youdontknow@marker-dao.eth>
2025-08-15 17:56:10 +0200

    fix

--- packages/devextreme/js/__internal/core/utils/m_swatch_container.ts
+++ packages/devextreme/js/__internal/core/utils/m_swatch_container.ts
@@ -4,7 +4,9 @@ import { value } from '@js/core/utils/view_port';
 
 const SWATCH_CONTAINER_CLASS_PREFIX = 'dx-swatch-';
 
-const getSwatchContainer = (element: Element): dxElementWrapper => {
+const getSwatchContainer = (
+  element: Element | dxElementWrapper,
+): dxElementWrapper => {
   const $element = $(element);
   const swatchContainer = $element.closest(`[class^="${SWATCH_CONTAINER_CLASS_PREFIX}"], [class*=" ${SWATCH_CONTAINER_CLASS_PREFIX}"]`);
   const viewport: dxElementWrapper = value();
