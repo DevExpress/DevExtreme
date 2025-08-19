e8d52e0bfe4032d003bf26ec4a2728a062b6f73f
Anton Evzhakov <anton@evz.name>
2025-08-19 14:03:10 +0300

    internal-tools: new configs for upcoming update
    # Conflicts:
    #	packages/devextreme-metadata/aspnet/enums.ts
    #	packages/devextreme-metadata/common/index.ts
    #	packages/devextreme-metadata/make-angular-metadata.ts
    #	packages/devextreme-metadata/make-aspnet-metadata.ts
    #	packages/devextreme-metadata/make-integration-metadata.ts

---
+++ packages/devextreme-metadata/common-smd-mutations.ts
@@ -0,0 +1,30 @@
+import {
+  replaceTypes,
+  ReplaceTypesMutation,
+} from '../../../devextreme-internal-tools/ts/config/metadata';
+
+const commonSeriesOptions = [
+  "area", "bar", "bubble", "candlestick", "fullStackedArea", "fullStackedBar", "fullStackedLine",
+  "fullStackedSplineArea", "fullstackedSpline", "line", "rangeArea", "rangeBar", "scatter", "splineArea", "spline",
+  "stackedArea", "stackedBar", "stackedLine", "stackedSplineArea", "stackedSpline", "stepArea", "stepLine", "stock"
+].map(i => i.toLowerCase());
+
+export const replaceTypesMutations: ReplaceTypesMutation[] = [
+  replaceTypes(
+    commonSeriesOptions.map(p => `viz/chart:CommonSeriesSettings.${p}`),
+    ["any"],
+    [{ kind: "unknown", customName: "ChartCommonSeriesSettings" }],
+  ),
+
+  replaceTypes(
+    commonSeriesOptions.map(p => `viz/polar_chart:CommonSeriesSettings.${p}`),
+    ["any"],
+    [{ kind: "unknown", customName: "PolarChartCommonSeriesSettings" }],
+  ),
+
+  replaceTypes(
+    ["viz/range_selector:dxRangeSelectorOptions.chart.commonSeriesSettings"],
+    ["*"],
+    [{ kind: "unknown", customName: "ChartCommonSeriesSettings" }],
+  )
+]
