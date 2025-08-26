0b1365417a49b329457d03f7d1838aa934f66ae0
Sergey Bur <zacsergey@gmail.com>
2025-08-26 17:29:08 +0200

    feat: try to refactor m_local_store and m_remote_store to es6 classes

--- packages/devextreme/js/__internal/data/m_local_store.ts
+++ packages/devextreme/js/__internal/data/m_local_store.ts
@@ -1,45 +1,49 @@
+/* eslint-disable max-classes-per-file */
 import eventsEngine from '@js/common/core/events/core/events_engine';
 import ArrayStore from '@js/common/data/array_store';
 import { errors } from '@js/common/data/errors';
-import Class from '@js/core/class';
 import domAdapter from '@js/core/dom_adapter';
 import { getWindow } from '@js/core/utils/window';
+import Store from '@js/data/abstract_store';
 
 const window = getWindow();
-const { abstract } = Class;
+abstract class LocalStoreBackend {
+  _store: any;
 
-const LocalStoreBackend = Class.inherit({
+  _dirty: boolean;
 
-  ctor(store, storeOptions) {
+  _immediate: boolean;
+
+  constructor(store, storeOptions) {
     this._store = store;
     this._dirty = !!storeOptions.data;
     this.save();
 
-    const immediate = this._immediate = storeOptions.immediate;
+    this._immediate = storeOptions.immediate;
     const flushInterval = Math.max(100, storeOptions.flushInterval || 10 * 1000);
 
-    if (!immediate) {
+    if (!this._immediate) {
       const saveProxy = this.save.bind(this);
       setInterval(saveProxy, flushInterval);
       eventsEngine.on(window, 'beforeunload', saveProxy);
-      // @ts-expect-error
+      // @ts-expect-error Cordova environment hook
       if (window.cordova) {
         domAdapter.listen(domAdapter.getDocument(), 'pause', saveProxy, false);
       }
     }
-  },
+  }
 
   notifyChanged() {
     this._dirty = true;
     if (this._immediate) {
       this.save();
     }
-  },
+  }
 
   load() {
     this._store._array = this._loadImpl();
     this._dirty = false;
-  },
+  }
 
   save() {
     if (!this._dirty) {
@@ -48,32 +52,32 @@ const LocalStoreBackend = Class.inherit({
 
     this._saveImpl(this._store._array);
     this._dirty = false;
-  },
+  }
 
-  _loadImpl: abstract,
-  _saveImpl: abstract,
-});
+  // impl in backends
+  abstract _loadImpl();
+  abstract _saveImpl(array);
+}
 
-const DomLocalStoreBackend = LocalStoreBackend.inherit({
+class DomLocalStoreBackend extends LocalStoreBackend {
+  _key: string;
 
-  ctor(store, storeOptions) {
+  constructor(store, storeOptions) {
+    super(store, storeOptions);
     const { name } = storeOptions;
     if (!name) {
       throw errors.Error('E4013');
     }
     this._key = `dx-data-localStore-${name}`;
-
-    this.callBase(store, storeOptions);
-  },
+  }
 
   _loadImpl() {
     const raw = window.localStorage.getItem(this._key);
-
     if (raw) {
       return JSON.parse(raw);
     }
     return [];
-  },
+  }
 
   _saveImpl(array) {
     if (!array.length) {
@@ -81,51 +85,55 @@ const DomLocalStoreBackend = LocalStoreBackend.inherit({
     } else {
       window.localStorage.setItem(this._key, JSON.stringify(array));
     }
-  },
-
-});
+  }
+}
 
 const localStoreBackends = {
   dom: DomLocalStoreBackend,
 };
-const LocalStore = ArrayStore.inherit({
+class LocalStore extends ArrayStore {
+  _backend: any;
 
-  ctor(options) {
+  constructor(options) {
     if (typeof options === 'string') {
       options = { name: options };
     } else {
       options = options || {};
     }
 
-    this.callBase(options);
+    super(options);
 
     this._backend = new localStoreBackends[options.backend || 'dom'](this, options);
     this._backend.load();
-  },
+  }
 
   _clearCache() {
     this._backend.load();
-  },
+  }
 
   clear() {
-    this.callBase();
+    super.clear();
     this._backend.notifyChanged();
-  },
+  }
 
   _insertImpl(values) {
     const b = this._backend;
-    return this.callBase(values).done(b.notifyChanged.bind(b));
-  },
+    return super._insertImpl(values).done(b.notifyChanged.bind(b));
+  }
 
   _updateImpl(key, values) {
     const b = this._backend;
-    return this.callBase(key, values).done(b.notifyChanged.bind(b));
-  },
+    return super._updateImpl(key, values).done(b.notifyChanged.bind(b));
+  }
 
   _removeImpl(key) {
     const b = this._backend;
-    return this.callBase(key).done(b.notifyChanged.bind(b));
-  },
-}, 'local');
+    return super._removeImpl(key).done(b.notifyChanged.bind(b));
+  }
+}
+
+// Preserve alias registration used by Store.create('local', ...)
+// @ts-expect-error register ES6 class with alias
+Store.registerClass(LocalStore as any, 'local');
 
 export default LocalStore;
