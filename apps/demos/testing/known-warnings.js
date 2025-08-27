9aada18320bff809aacd07302b5612d9677f60ad
Ruslan Farkhutdinov <fr3ddy4@yandex.ru>
2025-08-27 10:47:59 +0300

    DropDownBox, SelectBox, ColorBox, TagBox: Log fieldTemplate deprecation warning (#30846)
    Co-authored-by: Ruslan Farkhutdinov <ruslan.farkhutdinov@devexpress.com>

--- apps/demos/testing/known-warnings.js
+++ apps/demos/testing/known-warnings.js
@@ -1,5 +1,13 @@
+export const dropDownEditorsFieldTemplateWarnings = [
+  'W0001 - dxSelectBox - \'fieldTemplate\'',
+  'W0001 - dxColorBox - \'fieldTemplate\'',
+  'W0001 - dxDropDownBox - \'fieldTemplate\'',
+  'W0001 - dxTagBox - \'fieldTemplate\'',
+];
+
 export const knownWarnings = [
   'W0019 -',
   'W0022 -',
   'W2108 -',
+  ...dropDownEditorsFieldTemplateWarnings,
 ];
