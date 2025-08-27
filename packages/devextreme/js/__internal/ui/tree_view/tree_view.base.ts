d72d6ba6fc6096aa23ae7496d2474f58d38d18ca
Julia Volkova <juleari@users.noreply.github.com>
2025-08-27 15:31:26 +0300

    TreeView: focus treeview element in selectAllEnabled and items empty (T1294524) (#30887)

--- packages/devextreme/js/__internal/ui/tree_view/tree_view.base.ts
+++ packages/devextreme/js/__internal/ui/tree_view/tree_view.base.ts
@@ -1911,7 +1911,8 @@ class TreeViewBase extends HierarchicalCollectionWidget<TreeViewBaseProperties, 
   }
 
   focus(): void {
-    if (this._selectAllEnabled()) {
+    const { items = [] } = this.option();
+    if (this._selectAllEnabled() && items.length) {
       // @ts-expect-error ts-error
       eventsEngine.trigger(this._$selectAllItem, 'focus');
       return;
