4f7d75c61303ffad656055c13c7a031a563586ea
Andrey Dolzhikov <eylau-andrew@mail.ru>
2025-08-28 13:37:42 +0300

    DataGrid - Focused cell appearance is applied to non-editable CheckBox cells on mouse clicks (#30767)

--- packages/devextreme/js/__internal/grids/grid_core/keyboard_navigation/m_keyboard_navigation.ts
+++ packages/devextreme/js/__internal/grids/grid_core/keyboard_navigation/m_keyboard_navigation.ts
@@ -1363,6 +1363,7 @@ export class KeyboardNavigationController extends modules.ViewController {
   private _clickTargetCellHandler(event, $cell) {
     const column = this._getColumnByCellElement($cell);
     const isCellEditMode = this._isCellEditMode();
+    const isEditing = this._editingController.isEditing();
 
     this.setCellFocusType();
 
@@ -1397,14 +1398,18 @@ export class KeyboardNavigationController extends modules.ViewController {
         const $target = event
           && $(event.target).closest(`${NON_FOCUSABLE_ELEMENTS_SELECTOR}, td`);
         const skipFocusEvent = $target && $target.not($cell).is(NON_FOCUSABLE_ELEMENTS_SELECTOR);
-        const isEditor = !!column && !column.command && $cell.hasClass(EDITOR_CELL_CLASS);
-        const isDisabled = !isEditor && (!args.isHighlighted || skipFocusEvent);
+        const isEditCell = !column?.command
+          && isEditing
+          && $cell.hasClass(EDITOR_CELL_CLASS);
+
+        const isDisabled = !isEditCell
+          && (!args.isHighlighted || skipFocusEvent);
         this._focus($cell, isDisabled, skipFocusEvent);
       }
     } else {
       this.setRowFocusType();
       this.setFocusedRowIndex(args.prevRowIndex);
-      if (this._editingController.isEditing() && isCellEditMode) {
+      if (isEditing && isCellEditMode) {
         this._closeEditCell();
       }
     }
