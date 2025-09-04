c8fbb0a8e4b5072272dda5fc9c2b3bfc22a34b32
Mikhail Preyskurantov <5574159+mpreyskurantov@users.noreply.github.com>
2025-09-04 16:10:19 +0300

    use existing endpoint for similar entity instead of odata devav endpoint

--- apps/demos/Demos/FilterBuilder/WithDataGrid/jQuery/data.js
+++ apps/demos/Demos/FilterBuilder/WithDataGrid/jQuery/data.js
@@ -1,39 +1,25 @@
 const filter = [
-  ['Product_Current_Inventory', '<>', 0],
-  'or',
+  // ['Product_Current_Inventory', '<>', 0],
+  // 'or',
   [
-    ['Product_Name', 'contains', 'HD'],
+    ['ProductName', 'contains', 'Ch'],
     'and',
-    ['Product_Cost', '<', 200],
+    ['UnitPrice', '<', 20],
   ],
 ];
 const fields = [
   {
     caption: 'ID',
     width: 50,
-    dataField: 'Product_ID',
+    dataField: 'ProductID',
     dataType: 'number',
   }, {
-    dataField: 'Product_Name',
+    dataField: 'ProductName',
     dataType: 'string',
   }, {
     caption: 'Cost',
-    dataField: 'Product_Cost',
+    dataField: 'UnitPrice',
     dataType: 'number',
     format: 'currency',
-  }, {
-    dataField: 'Product_Sale_Price',
-    caption: 'Sale Price',
-    dataType: 'number',
-    format: 'currency',
-  }, {
-    dataField: 'Product_Retail_Price',
-    caption: 'Retail Price',
-    dataType: 'number',
-    format: 'currency',
-  }, {
-    dataField: 'Product_Current_Inventory',
-    dataType: 'number',
-    caption: 'Inventory',
   },
 ];
--- apps/demos/Demos/FilterBuilder/WithDataGrid/jQuery/index.js
+++ apps/demos/Demos/FilterBuilder/WithDataGrid/jQuery/index.js
@@ -16,26 +16,10 @@ $(() => {
   $('#dataGrid').dxDataGrid({
     columns: fields,
     showBorders: true,
-    dataSource: {
-      store: {
-        type: 'odata',
-        version: 2,
-        fieldTypes: {
-          Product_Cost: 'Decimal',
-          Product_Sale_Price: 'Decimal',
-          Product_Retail_Price: 'Decimal',
-        },
-        url: 'https://js.devexpress.com/Demos/DevAV/odata/Products',
-      },
-      select: [
-        'Product_ID',
-        'Product_Name',
-        'Product_Cost',
-        'Product_Sale_Price',
-        'Product_Retail_Price',
-        'Product_Current_Inventory',
-      ],
-    },
+    dataSource: DevExpress.data.AspNet.createStore({
+      loadUrl: 'https://js.devexpress.com/Demos/NetCore/api/ListData',
+      key: 'ProductID',
+    }),
     filterValue: filter,
     height: 300,
   });
