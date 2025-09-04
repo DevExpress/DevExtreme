0b941f629bf89916d26657febc17ec60508160e7
Julia Volkova <iuliia.volkova@devexpress.com>
2025-09-04 14:13:54 +0300

    updated texts after TW verification, getting text from option

--- apps/demos/Demos/Form/SmartPaste/jQuery/index.js
+++ apps/demos/Demos/Form/SmartPaste/jQuery/index.js
@@ -44,6 +44,20 @@ $(() => {
     },
   });
 
+  const customSmartPasteHandler = () => {
+    navigator.clipboard.readText()
+      .then((text) => {
+        if (text) {
+          form.smartPaste(text);
+        } else {
+          DevExpress.ui.notify('Clipboard is empty', 'error', 1500);
+        }
+      })
+      .catch(() => {
+        DevExpress.ui.notify('Could not access the clipboard', 'error', 1500);
+      });
+  }
+
   const form = $('#form').dxForm({
     formData: {},
     aiIntegration,
@@ -51,28 +65,163 @@ $(() => {
     showColonAfterLabel: false,
     labelLocation: 'top',
     minColWidth: 220,
-    items,
+    items: [{
+      itemType: 'group',
+      caption: 'Billing Summary',
+      colCountByScreen: {
+        xs: 2,
+        sm: 2,
+        md: 2,
+        lg: 2,
+      },
+      items: [{
+        dataField: 'Amount Due',
+        editorType: 'dxTextBox',
+        editorOptions: {
+          placeholder: '$0.00',
+          stylingMode: 'filled',
+        },
+        aiOptions: {
+          instruction: 'Format as the following: $0.00',
+        },
+      }, {
+        dataField: 'Statement Date',
+        editorType: 'dxDateBox',
+        editorOptions: {
+          placeholder: 'MM/DD/YYYY',
+          stylingMode: 'filled',
+        },
+        aiOptions: {
+          instruction: 'Format as the following: MM/DD/YYYY',
+        },
+      }],
+    }, {
+      itemType: 'group',
+      caption: 'Billing Information',
+      colCountByScreen: {
+        xs: 2,
+        sm: 2,
+        md: 2,
+        lg: 2,
+      },
+      items: [{
+        dataField: 'First Name',
+        editorType: 'dxTextBox',
+        editorOptions: {
+          stylingMode: 'filled',
+        },
+      }, {
+        dataField: 'Last Name',
+        editorType: 'dxTextBox',
+        editorOptions: {
+          stylingMode: 'filled',
+        },
+      }, {
+        dataField: 'Phone Number',
+        editorType: 'dxTextBox',
+        editorOptions: {
+          placeholder: '(000) 000-0000',
+          stylingMode: 'filled',
+        },
+        aiOptions: {
+          instruction: 'Format as the following: (000) 000-0000',
+        },
+      }, {
+        dataField: 'Email',
+        editorType: 'dxTextBox',
+        editorOptions: {
+          stylingMode: 'filled',
+        },
+        validationRules: [{ type: 'email' }],
+        aiOptions: {
+          instruction: 'Do not fill this field if the text contains an invalid email address. A valid email is in the following format: email@example.com',
+        },
+      }]
+    }, {
+      itemType: 'group',
+      caption: 'Billing Address',
+      colCountByScreen: {
+        xs: 2,
+        sm: 2,
+        md: 2,
+        lg: 2,
+      },
+      items: [{
+        dataField: 'Street Address',
+        editorType: 'dxTextBox',
+        editorOptions: {
+          stylingMode: 'filled',
+        },
+      }, {
+        dataField: 'City',
+        editorType: 'dxTextBox',
+        editorOptions: {
+          stylingMode: 'filled',
+        },
+      }, {
+        dataField: 'State/Province/Region',
+        editorType: 'dxTextBox',
+        editorOptions: {
+          stylingMode: 'filled',
+        },
+      }, {
+        dataField: 'ZIP',
+        editorType: 'dxNumberBox',
+        editorOptions: {
+          stylingMode: 'filled',
+          mode: 'text',
+          value: null,
+        },
+        aiOptions: {
+          instruction: 'If the text does not contain a ZIP, determine the ZIP code from the provided address.',
+        },
+      }]
+    }, {
+      itemType: 'group',
+      cssClass: 'buttons-group',
+      colCountByScreen: {
+        xs: 2,
+        sm: 2,
+        md: 2,
+        lg: 2,
+      },
+      items: [{
+        itemType: 'button',
+        name: 'smartPaste',
+        buttonOptions: {
+          stylingMode: 'contained',
+          type: 'default',
+          onClick: () => {
+            customSmartPasteHandler();
+          },
+        },
+      }, {
+        itemType: 'button',
+        name: 'reset',
+        buttonOptions: {
+          stylingMode: 'outlined',
+          type: 'normal',
+        },
+      }],
+    }],
   }).dxForm('instance');
 
-  let text = defaultText;
-
-  $('#textarea').dxTextArea({
-    value: text,
+  const textarea = $('#textarea').dxTextArea({
+    value: defaultText,
     stylingMode: 'filled',
     height: '100%',
-    onValueChanged: (data) => {
-      text = data.value;
-    }
-  });
+  }).dxTextArea('instance');
 
   $('#copy').dxButton({
     text: 'Copy Text',
     icon: 'copy',
-    stylingMode: 'Contained',
+    stylingMode: 'contained',
     type: 'default',
     width: 'fit-content',
     onClick: () => {
-      navigator.clipboard.writeText(text);
+      const { value } = textarea.option();
+      navigator.clipboard.writeText(value);
+
       DevExpress.ui.notify({
         message: 'Text copied to clipboard',
         position: {
@@ -88,9 +237,9 @@ $(() => {
     }
   });
 
-  addEventListener("keypress", (event) => {
-    if (event.ctrlKey && event.shiftKey && event.key === 'V') {
-      form.smartPaste();
+  form.registerKeyHandler("V", (event) => {
+    if (event.ctrlKey && event.shiftKey) {
+      customSmartPasteHandler();
     }
-  })
+  });
 });
