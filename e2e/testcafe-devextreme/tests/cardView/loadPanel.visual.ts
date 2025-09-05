321694ef1194b63009b235f066cd8a77e3d97172
Alyar <>
2025-09-05 11:10:31 +0400

    Add testcafe test

--- e2e/testcafe-devextreme/tests/cardView/loadPanel.visual.ts
+++ e2e/testcafe-devextreme/tests/cardView/loadPanel.visual.ts
@@ -1,4 +1,5 @@
 import CardView from 'devextreme-testcafe-models/cardView';
+import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import url from '../../helpers/getPageUrl';
 import { createWidget } from '../../helpers/createWidget';
 
@@ -7,7 +8,26 @@ fixture.disablePageReloads`CardView - LoadPanel`
 
 const CARD_VIEW_SELECTOR = '#container';
 
-test('default render', async (t) => {
+test('Default render', async (t) => {
+  const cardView = new CardView(CARD_VIEW_SELECTOR);
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+  await takeScreenshot('load-panel.png', cardView.element);
+
+  await t
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => createWidget('dxCardView', {
+  width: 300,
+  height: 200,
+  dataSource: {
+    key: 'id',
+    load: () => new Promise(() => {}),
+  },
+  columns: ['A', 'B', 'C', 'D'],
+}));
+
+test('The load panel should match the size of the component’s root container', async (t) => {
   const cardView = new CardView(CARD_VIEW_SELECTOR);
   const loadPanel = cardView.getLoadPanel();
   const cardViewOffset = await cardView.getElementOffset();
