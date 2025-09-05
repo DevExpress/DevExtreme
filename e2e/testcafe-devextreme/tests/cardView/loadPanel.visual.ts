c533084bb0237c8df7022ca60bf1c340851151d4
Alyar <>
2025-09-05 17:24:15 +0400

    Fix loadPanel position

--- e2e/testcafe-devextreme/tests/cardView/loadPanel.visual.ts
+++ e2e/testcafe-devextreme/tests/cardView/loadPanel.visual.ts
@@ -2,13 +2,14 @@ import CardView from 'devextreme-testcafe-models/cardView';
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import url from '../../helpers/getPageUrl';
 import { createWidget } from '../../helpers/createWidget';
+import { safeSizeTest } from '../../helpers/safeSizeTest';
 
 fixture.disablePageReloads`CardView - LoadPanel`
   .page(url(__dirname, '../container.html'));
 
 const CARD_VIEW_SELECTOR = '#container';
 
-test('Default render', async (t) => {
+safeSizeTest('Default render', async (t) => {
   const cardView = new CardView(CARD_VIEW_SELECTOR);
   const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
@@ -17,9 +18,9 @@ test('Default render', async (t) => {
   await t
     .expect(compareResults.isValid())
     .ok(compareResults.errorMessages());
-}).before(async () => createWidget('dxCardView', {
-  width: 300,
-  height: 200,
+}, [800, 800]).before(async () => createWidget('dxCardView', {
+  width: 500,
+  height: 300,
   dataSource: {
     key: 'id',
     load: () => new Promise(() => {}),
@@ -27,21 +28,40 @@ test('Default render', async (t) => {
   columns: ['A', 'B', 'C', 'D'],
 }));
 
-test('The load panel should match the size of the component’s root container', async (t) => {
+safeSizeTest('Default render when CardView has a large height', async (t) => {
+  const cardView = new CardView(CARD_VIEW_SELECTOR);
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+  await takeScreenshot('load-panel-with-large-height.png', cardView.element);
+
+  await t
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}, [800, 800]).before(async () => createWidget('dxCardView', {
+  width: 500,
+  height: 3000,
+  dataSource: {
+    key: 'id',
+    load: () => new Promise(() => {}),
+  },
+  columns: ['A', 'B', 'C', 'D'],
+}));
+
+safeSizeTest('The load panel should match the size of the component’s root container', async (t) => {
   const cardView = new CardView(CARD_VIEW_SELECTOR);
   const loadPanel = cardView.getLoadPanel();
   const cardViewOffset = await cardView.getElementOffset();
 
   await t
     .expect(loadPanel.getShadowWidth())
-    .eql(300)
+    .eql(500)
     .expect(loadPanel.getShadowHeight())
-    .eql(200)
+    .eql(300)
     .expect(loadPanel.getShadowOffset())
     .eql(cardViewOffset);
-}).before(async () => createWidget('dxCardView', {
-  width: 300,
-  height: 200,
+}, [800, 800]).before(async () => createWidget('dxCardView', {
+  width: 500,
+  height: 300,
   dataSource: {
     key: 'id',
     load: () => new Promise(() => {}),
--- packages/devextreme/js/__internal/grids/new/grid_core/content_view/load_panel.tsx
+++ packages/devextreme/js/__internal/grids/new/grid_core/content_view/load_panel.tsx
@@ -1,3 +1,4 @@
+import type { PositionConfig } from '@js/common/core/animation';
 import { getWindow } from '@js/core/utils/window';
 import { BaseInfernoComponent } from '@ts/core/r1/runtime/inferno/index';
 
@@ -8,15 +9,25 @@ import { LoadPanel as LoadPanelBase } from '../inferno_wrappers/load_panel';
 export { LoadPanelProperties };
 
 export class LoadPanel extends BaseInfernoComponent<LoadPanelProperties> {
+  private calculatePosition(rootElement: HTMLElement): PositionConfig {
+    const window = getWindow();
+
+    if (rootElement.offsetHeight > window.innerHeight) {
+      return {
+        of: window,
+        boundary: rootElement,
+        collision: 'fit',
+      };
+    }
+    return { of: rootElement };
+  }
+
   public render(): JSX.Element {
     const { rootElementRef } = this.context[CommonPropsContext.id];
+
     const loadPanelProperties: LoadPanelProperties = {
       container: rootElementRef.current,
-      position: {
-        of: getWindow(),
-        boundary: rootElementRef.current,
-        collision: 'fit',
-      },
+      position: this.calculatePosition(rootElementRef.current),
       ...this.props,
     };
 
