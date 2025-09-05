5436f6d7bba21b2910397dfc69839a3ee42e46b9
marker dao ® <youdontknow@marker-dao.eth>
2025-09-05 14:01:27 +0200

    revert(calendar.d.ts)

--- packages/devextreme/ts/dx.all.d.ts
+++ packages/devextreme/ts/dx.all.d.ts
@@ -9805,7 +9805,7 @@ declare module DevExpress.ui {
      * [descr:dxCalendar.reset(value)]
      */
     reset(
-      value?: DevExpress.common.DateLike | DevExpress.common.DateLike[]
+      value?: DevExpress.common.DateLike | Array<Date | number | string | null>
     ): void;
   }
   module dxCalendar {
