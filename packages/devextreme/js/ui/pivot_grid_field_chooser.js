c58185a6c834e1dfccb2a21f1be7b5f79241bc7c
Sergio Bur <sjbur.dev@gmail.com>
2025-09-17 15:20:00 +0200

    fix: fix export

--- packages/devextreme/js/ui/pivot_grid_field_chooser.js
+++ packages/devextreme/js/ui/pivot_grid_field_chooser.js
@@ -1,2 +1,2 @@
-import FieldChooser from '../__internal/grids/pivot_grid/field_chooser/m_field_chooser';
+import { FieldChooser } from '../__internal/grids/pivot_grid/field_chooser/m_field_chooser';
 export default FieldChooser;
