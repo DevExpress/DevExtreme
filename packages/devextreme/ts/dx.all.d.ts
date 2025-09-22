ab2bf1c3c7fca607b9028c80f1cfd89a2657cce3
Sergio Bur <sjbur.dev@gmail.com>
2025-09-22 15:58:09 +0200

    fix: fix build

--- packages/devextreme/ts/dx.all.d.ts
+++ packages/devextreme/ts/dx.all.d.ts
@@ -7078,8 +7078,9 @@ declare module DevExpress.core.utils {
     * [descr:DxPromise]
     * @deprecated Attention! This type is for internal purposes only. If you used it previously, please submit a ticket to our {@link https://supportcenter.devexpress.com/ticket/create Support Center}. We will check if there is an alternative solution.
     */
-   export type DxPromise<T = void> =
-    {} extends PromiseType<T> ? Promise<T> : PromiseType<T>;
+   export type DxPromise<T = void> = {} extends PromiseType<T>
+    ? Promise<T>
+    : PromiseType<T>;
 }
 declare module DevExpress.data {
   /**
