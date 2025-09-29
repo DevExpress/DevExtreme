00be15ab6a6c77d859609eeab16be7d20fa0a852
Adel Khamatov <adel.khamatov@devexpress.com>
2025-09-29 16:29:03 +0300

    fix: return back `versionAssertions` to the global config

--- packages/devextreme/js/__internal/core/m_config.ts
+++ packages/devextreme/js/__internal/core/m_config.ts
@@ -17,6 +17,7 @@ const config = {
   useJQuery: undefined,
   editorStylingMode: undefined,
   useLegacyVisibleIndex: false,
+  versionAssertions: [],
   copyStylesToShadowDom: true,
 
   floatingActionButtonConfig: {
