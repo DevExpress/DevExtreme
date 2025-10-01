6cd8add4f96351a42ad67de0e59b7fe608b7507a
Sergio Bur <sjbur.dev@gmail.com>
2025-10-01 19:28:19 +0200

    fix: revert angular

--- packages/devextreme-angular/src/ui/scheduler/nested/item-dxi.ts
+++ packages/devextreme-angular/src/ui/scheduler/nested/item-dxi.ts
@@ -10,23 +10,15 @@ import {
     Inject,
     AfterViewInit,
     SkipSelf,
-    Input,
-    ContentChildren,
-    forwardRef,
-    QueryList,
-    AfterContentInit
+    Input
 } from '@angular/core';
 
 import { DOCUMENT } from '@angular/common';
 
 
-import * as CommonTypes from 'devextreme/common';
-import { FormItemComponent, FormItemType, LabelLocation, FormPredefinedButtonItem, dxFormButtonItem, dxFormEmptyItem, dxFormGroupItem, dxFormSimpleItem, dxFormTabbedItem } from 'devextreme/ui/form';
-import { HorizontalAlignment, VerticalAlignment, ToolbarItemLocation, ToolbarItemComponent, ButtonType } from 'devextreme/common';
-import { SchedulerPredefinedToolbarItem, DateNavigatorItemProperties } from 'devextreme/ui/scheduler';
-import { dxTabPanelOptions } from 'devextreme/ui/tab_panel';
-import { dxButtonOptions } from 'devextreme/ui/button';
 import { LocateInMenuMode, ShowTextMode } from 'devextreme/ui/toolbar';
+import { ToolbarItemLocation, ToolbarItemComponent, ButtonType } from 'devextreme/common';
+import { SchedulerPredefinedToolbarItem, DateNavigatorItemProperties } from 'devextreme/ui/scheduler';
 
 import {
     DxIntegrationModule,
@@ -37,17 +29,6 @@ import {
     DxTemplateHost
 } from 'devextreme-angular/core';
 import { CollectionNestedOption } from 'devextreme-angular/core';
-import { DxiSchedulerAsyncRuleComponent } from './async-rule-dxi';
-import { DxiSchedulerCompareRuleComponent } from './compare-rule-dxi';
-import { DxiSchedulerCustomRuleComponent } from './custom-rule-dxi';
-import { DxiSchedulerEmailRuleComponent } from './email-rule-dxi';
-import { DxiSchedulerNumericRuleComponent } from './numeric-rule-dxi';
-import { DxiSchedulerPatternRuleComponent } from './pattern-rule-dxi';
-import { DxiSchedulerRangeRuleComponent } from './range-rule-dxi';
-import { DxiSchedulerRequiredRuleComponent } from './required-rule-dxi';
-import { DxiSchedulerStringLengthRuleComponent } from './string-length-rule-dxi';
-import { DxiSchedulerTabComponent } from './tab-dxi';
-import { DxiSchedulerValidationRuleComponent } from './validation-rule-dxi';
 
 
 @Component({
@@ -59,13 +40,13 @@ import { DxiSchedulerValidationRuleComponent } from './validation-rule-dxi';
     providers: [NestedOptionHost, DxTemplateHost]
 })
 export class DxiSchedulerItemComponent extends CollectionNestedOption implements AfterViewInit,
-    IDxTemplateHost, AfterContentInit  {
+    IDxTemplateHost {
     @Input()
-    get badge(): string {
-        return this._getOption('badge');
+    get cssClass(): string | undefined {
+        return this._getOption('cssClass');
     }
-    set badge(value: string) {
-        this._setOption('badge', value);
+    set cssClass(value: string | undefined) {
+        this._setOption('cssClass', value);
     }
 
     @Input()
@@ -82,246 +63,6 @@ export class DxiSchedulerItemComponent extends CollectionNestedOption implements
     }
     set html(value: string) {
         this._setOption('html', value);
-    }
-
-    @Input()
-    get icon(): string {
-        return this._getOption('icon');
-    }
-    set icon(value: string) {
-        this._setOption('icon', value);
-    }
-
-    @Input()
-    get tabTemplate(): any {
-        return this._getOption('tabTemplate');
-    }
-    set tabTemplate(value: any) {
-        this._setOption('tabTemplate', value);
-    }
-
-    @Input()
-    get template(): any {
-        return this._getOption('template');
-    }
-    set template(value: any) {
-        this._setOption('template', value);
-    }
-
-    @Input()
-    get text(): string {
-        return this._getOption('text');
-    }
-    set text(value: string) {
-        this._setOption('text', value);
-    }
-
-    @Input()
-    get title(): string {
-        return this._getOption('title');
-    }
-    set title(value: string) {
-        this._setOption('title', value);
-    }
-
-    @Input()
-    get visible(): boolean {
-        return this._getOption('visible');
-    }
-    set visible(value: boolean) {
-        this._setOption('visible', value);
-    }
-
-    @Input()
-    get aiOptions(): { disabled?: boolean, instruction?: string | undefined } {
-        return this._getOption('aiOptions');
-    }
-    set aiOptions(value: { disabled?: boolean, instruction?: string | undefined }) {
-        this._setOption('aiOptions', value);
-    }
-
-    @Input()
-    get colSpan(): number | undefined {
-        return this._getOption('colSpan');
-    }
-    set colSpan(value: number | undefined) {
-        this._setOption('colSpan', value);
-    }
-
-    @Input()
-    get cssClass(): string | undefined {
-        return this._getOption('cssClass');
-    }
-    set cssClass(value: string | undefined) {
-        this._setOption('cssClass', value);
-    }
-
-    @Input()
-    get dataField(): string | undefined {
-        return this._getOption('dataField');
-    }
-    set dataField(value: string | undefined) {
-        this._setOption('dataField', value);
-    }
-
-    @Input()
-    get editorOptions(): any | undefined {
-        return this._getOption('editorOptions');
-    }
-    set editorOptions(value: any | undefined) {
-        this._setOption('editorOptions', value);
-    }
-
-    @Input()
-    get editorType(): FormItemComponent {
-        return this._getOption('editorType');
-    }
-    set editorType(value: FormItemComponent) {
-        this._setOption('editorType', value);
-    }
-
-    @Input()
-    get helpText(): string | undefined {
-        return this._getOption('helpText');
-    }
-    set helpText(value: string | undefined) {
-        this._setOption('helpText', value);
-    }
-
-    @Input()
-    get isRequired(): boolean | undefined {
-        return this._getOption('isRequired');
-    }
-    set isRequired(value: boolean | undefined) {
-        this._setOption('isRequired', value);
-    }
-
-    @Input()
-    get itemType(): FormItemType {
-        return this._getOption('itemType');
-    }
-    set itemType(value: FormItemType) {
-        this._setOption('itemType', value);
-    }
-
-    @Input()
-    get label(): { alignment?: HorizontalAlignment, location?: LabelLocation, showColon?: boolean, template?: any, text?: string | undefined, visible?: boolean } {
-        return this._getOption('label');
-    }
-    set label(value: { alignment?: HorizontalAlignment, location?: LabelLocation, showColon?: boolean, template?: any, text?: string | undefined, visible?: boolean }) {
-        this._setOption('label', value);
-    }
-
-    @Input()
-    get name(): string | undefined | FormPredefinedButtonItem | SchedulerPredefinedToolbarItem {
-        return this._getOption('name');
-    }
-    set name(value: string | undefined | FormPredefinedButtonItem | SchedulerPredefinedToolbarItem) {
-        this._setOption('name', value);
-    }
-
-    @Input()
-    get validationRules(): Array<CommonTypes.ValidationRule> {
-        return this._getOption('validationRules');
-    }
-    set validationRules(value: Array<CommonTypes.ValidationRule>) {
-        this._setOption('validationRules', value);
-    }
-
-    @Input()
-    get visibleIndex(): number | undefined {
-        return this._getOption('visibleIndex');
-    }
-    set visibleIndex(value: number | undefined) {
-        this._setOption('visibleIndex', value);
-    }
-
-    @Input()
-    get alignItemLabels(): boolean {
-        return this._getOption('alignItemLabels');
-    }
-    set alignItemLabels(value: boolean) {
-        this._setOption('alignItemLabels', value);
-    }
-
-    @Input()
-    get caption(): string | undefined {
-        return this._getOption('caption');
-    }
-    set caption(value: string | undefined) {
-        this._setOption('caption', value);
-    }
-
-    @Input()
-    get captionTemplate(): any {
-        return this._getOption('captionTemplate');
-    }
-    set captionTemplate(value: any) {
-        this._setOption('captionTemplate', value);
-    }
-
-    @Input()
-    get colCount(): number {
-        return this._getOption('colCount');
-    }
-    set colCount(value: number) {
-        this._setOption('colCount', value);
-    }
-
-    @Input()
-    get colCountByScreen(): { lg?: number | undefined, md?: number | undefined, sm?: number | undefined, xs?: number | undefined } {
-        return this._getOption('colCountByScreen');
-    }
-    set colCountByScreen(value: { lg?: number | undefined, md?: number | undefined, sm?: number | undefined, xs?: number | undefined }) {
-        this._setOption('colCountByScreen', value);
-    }
-
-    @Input()
-    get items(): Array<dxFormButtonItem | dxFormEmptyItem | dxFormGroupItem | dxFormSimpleItem | dxFormTabbedItem> {
-        return this._getOption('items');
-    }
-    set items(value: Array<dxFormButtonItem | dxFormEmptyItem | dxFormGroupItem | dxFormSimpleItem | dxFormTabbedItem>) {
-        this._setOption('items', value);
-    }
-
-    @Input()
-    get tabPanelOptions(): dxTabPanelOptions | undefined {
-        return this._getOption('tabPanelOptions');
-    }
-    set tabPanelOptions(value: dxTabPanelOptions | undefined) {
-        this._setOption('tabPanelOptions', value);
-    }
-
-    @Input()
-    get tabs(): { alignItemLabels?: boolean, badge?: string | undefined, colCount?: number, colCountByScreen?: { lg?: number | undefined, md?: number | undefined, sm?: number | undefined, xs?: number | undefined }, disabled?: boolean, icon?: string | undefined, items?: Array<dxFormButtonItem | dxFormEmptyItem | dxFormGroupItem | dxFormSimpleItem | dxFormTabbedItem>, tabTemplate?: any, template?: any, title?: string | undefined }[] {
-        return this._getOption('tabs');
-    }
-    set tabs(value: { alignItemLabels?: boolean, badge?: string | undefined, colCount?: number, colCountByScreen?: { lg?: number | undefined, md?: number | undefined, sm?: number | undefined, xs?: number | undefined }, disabled?: boolean, icon?: string | undefined, items?: Array<dxFormButtonItem | dxFormEmptyItem | dxFormGroupItem | dxFormSimpleItem | dxFormTabbedItem>, tabTemplate?: any, template?: any, title?: string | undefined }[]) {
-        this._setOption('tabs', value);
-    }
-
-    @Input()
-    get buttonOptions(): dxButtonOptions | undefined {
-        return this._getOption('buttonOptions');
-    }
-    set buttonOptions(value: dxButtonOptions | undefined) {
-        this._setOption('buttonOptions', value);
-    }
-
-    @Input()
-    get horizontalAlignment(): HorizontalAlignment {
-        return this._getOption('horizontalAlignment');
-    }
-    set horizontalAlignment(value: HorizontalAlignment) {
-        this._setOption('horizontalAlignment', value);
-    }
-
-    @Input()
-    get verticalAlignment(): VerticalAlignment {
-        return this._getOption('verticalAlignment');
-    }
-    set verticalAlignment(value: VerticalAlignment) {
-        this._setOption('verticalAlignment', value);
     }
 
     @Input()
@@ -349,6 +90,14 @@ export class DxiSchedulerItemComponent extends CollectionNestedOption implements
     }
 
     @Input()
+    get name(): SchedulerPredefinedToolbarItem {
+        return this._getOption('name');
+    }
+    set name(value: SchedulerPredefinedToolbarItem) {
+        this._setOption('name', value);
+    }
+
+    @Input()
     get options(): DateNavigatorItemProperties | Record<string, any> {
         return this._getOption('options');
     }
@@ -362,6 +111,30 @@ export class DxiSchedulerItemComponent extends CollectionNestedOption implements
     }
     set showText(value: ShowTextMode) {
         this._setOption('showText', value);
+    }
+
+    @Input()
+    get template(): any {
+        return this._getOption('template');
+    }
+    set template(value: any) {
+        this._setOption('template', value);
+    }
+
+    @Input()
+    get text(): string {
+        return this._getOption('text');
+    }
+    set text(value: string) {
+        this._setOption('text', value);
+    }
+
+    @Input()
+    get visible(): boolean {
+        return this._getOption('visible');
+    }
+    set visible(value: boolean) {
+        this._setOption('visible', value);
     }
 
     @Input()
@@ -389,6 +162,14 @@ export class DxiSchedulerItemComponent extends CollectionNestedOption implements
     }
 
     @Input()
+    get icon(): string {
+        return this._getOption('icon');
+    }
+    set icon(value: string) {
+        this._setOption('icon', value);
+    }
+
+    @Input()
     get type(): ButtonType | string {
         return this._getOption('type');
     }
@@ -401,69 +182,6 @@ export class DxiSchedulerItemComponent extends CollectionNestedOption implements
         return 'items';
     }
 
-
-    @ContentChildren(forwardRef(() => DxiSchedulerAsyncRuleComponent)) asyncRulesChildren!: QueryList<DxiSchedulerAsyncRuleComponent>
-    
-    @ContentChildren(forwardRef(() => DxiSchedulerCompareRuleComponent)) compareRulesChildren!: QueryList<DxiSchedulerCompareRuleComponent>
-    
-    @ContentChildren(forwardRef(() => DxiSchedulerCustomRuleComponent)) customRulesChildren!: QueryList<DxiSchedulerCustomRuleComponent>
-    
-    @ContentChildren(forwardRef(() => DxiSchedulerEmailRuleComponent)) emailRulesChildren!: QueryList<DxiSchedulerEmailRuleComponent>
-    
-    @ContentChildren(forwardRef(() => DxiSchedulerNumericRuleComponent)) numericRulesChildren!: QueryList<DxiSchedulerNumericRuleComponent>
-    
-    @ContentChildren(forwardRef(() => DxiSchedulerPatternRuleComponent)) patternRulesChildren!: QueryList<DxiSchedulerPatternRuleComponent>
-    
-    @ContentChildren(forwardRef(() => DxiSchedulerRangeRuleComponent)) rangeRulesChildren!: QueryList<DxiSchedulerRangeRuleComponent>
-    
-    @ContentChildren(forwardRef(() => DxiSchedulerRequiredRuleComponent)) requiredRulesChildren!: QueryList<DxiSchedulerRequiredRuleComponent>
-    
-    @ContentChildren(forwardRef(() => DxiSchedulerStringLengthRuleComponent)) stringLengthRulesChildren!: QueryList<DxiSchedulerStringLengthRuleComponent>
-    
-    @ContentChildren(forwardRef(() => DxiSchedulerValidationRuleComponent)) validationRulesChildren!: QueryList<DxiSchedulerValidationRuleComponent>
-    
-    setValidationRules() {
-        const q: QueryList<any> = new QueryList();
-        q.reset([
-            ...this.asyncRulesChildren.toArray(),
-            ...this.compareRulesChildren.toArray(),
-            ...this.customRulesChildren.toArray(),
-            ...this.emailRulesChildren.toArray(),
-            ...this.numericRulesChildren.toArray(),
-            ...this.patternRulesChildren.toArray(),
-            ...this.rangeRulesChildren.toArray(),
-            ...this.requiredRulesChildren.toArray(),
-            ...this.stringLengthRulesChildren.toArray(),
-            ...this.validationRulesChildren.toArray(),
-        ]);
-        this.setChildren('validationRules', q);
-    }
-
-
-
-
-
-
-
-
-
-
-    @ContentChildren(forwardRef(() => DxiSchedulerTabComponent))
-    get tabsChildren(): QueryList<DxiSchedulerTabComponent> {
-        return this._getOption('tabs');
-    }
-    set tabsChildren(value) {
-        this.setChildren('tabs', value);
-    }
-
-
-    @ContentChildren(forwardRef(() => DxiSchedulerItemComponent))
-    get itemsChildren(): QueryList<DxiSchedulerItemComponent> {
-        return this._getOption('items');
-    }
-    set itemsChildren(value) {
-        this.setChildren('items', value);
-    }
 
     constructor(@SkipSelf() @Host() parentOptionHost: NestedOptionHost,
             @Host() optionHost: NestedOptionHost,
@@ -490,20 +208,6 @@ export class DxiSchedulerItemComponent extends CollectionNestedOption implements
         this._deleteRemovedOptions(this._fullOptionPath());
     }
 
-    ngAfterContentInit() {
-        this.setValidationRules();
-        
-        this.asyncRulesChildren.changes.subscribe(() => { this.setValidationRules() });
-        this.compareRulesChildren.changes.subscribe(() => { this.setValidationRules() });
-        this.customRulesChildren.changes.subscribe(() => { this.setValidationRules() });
-        this.emailRulesChildren.changes.subscribe(() => { this.setValidationRules() });
-        this.numericRulesChildren.changes.subscribe(() => { this.setValidationRules() });
-        this.patternRulesChildren.changes.subscribe(() => { this.setValidationRules() });
-        this.rangeRulesChildren.changes.subscribe(() => { this.setValidationRules() });
-        this.requiredRulesChildren.changes.subscribe(() => { this.setValidationRules() });
-        this.stringLengthRulesChildren.changes.subscribe(() => { this.setValidationRules() });
-        this.validationRulesChildren.changes.subscribe(() => { this.setValidationRules() });
-    }
 }
 
 @NgModule({
