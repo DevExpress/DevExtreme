0543a0d8fb56b10ae8f046f574ec6951dc7bef49
Sergio Bur <sjbur.dev@gmail.com>
2025-10-02 10:37:22 +0200

    fix: fix test

--- apps/demos/testing/widgets/fileuploader/CustomDropzone.test.ts
+++ apps/demos/testing/widgets/fileuploader/CustomDropzone.test.ts
@@ -1,5 +1,5 @@
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
-import { Selector as $, ClientFunction } from 'testcafe';
+import { Selector, ClientFunction } from 'testcafe';
 import { runManualTest } from '../../../utils/visual-tests/matrix-test-helper';
 import { testScreenshot } from '../../../utils/visual-tests/helpers/theme-utils';
 
@@ -14,12 +14,12 @@ fixture('FileUploader.CustomDropzone')
 runManualTest('FileUploader', 'CustomDropzone', (test) => {
   const triggerDragEnter = async (dropZoneSelector, items) => {
     await ClientFunction(() => {
+      // @ts-expect-error $ is not typed
       const $dropZone = $(dropZoneSelector);
-      // @ts-expect-error Selector.offset is not typed
       const { left, top } = $dropZone.offset();
-      // @ts-expect-error Selector.trigger is not typed
+      // @ts-expect-error $ is not typed
       $dropZone.trigger($.Event('dragenter', {
-        // @ts-expect-error Selector.Event is not typed
+        // @ts-expect-error $ is not typed
         originalEvent: $.Event('dragenter', {
           dataTransfer: {
             items,
@@ -35,13 +35,13 @@ runManualTest('FileUploader', 'CustomDropzone', (test) => {
   test('dropzone-active class is added to the dropzone element when single valid file is dragged over it', async (t) => {
     await triggerDragEnter(`#${DROPZONE_EXTERNAL_ID}`, [{ type: 'image/png' }]);
 
-    await t.expect($(`#${DROPZONE_EXTERNAL_ID}`).hasClass(DROPZONE_ACTIVE_CLASS)).ok();
+    await t.expect(Selector(`#${DROPZONE_EXTERNAL_ID}`).hasClass(DROPZONE_ACTIVE_CLASS)).ok();
   });
 
   test('dropzone-active class is not added to the dropzone element when an invalid file format is dragged', async (t) => {
     await triggerDragEnter(`#${DROPZONE_EXTERNAL_ID}`, [{ type: 'image/xlsx' }]);
 
-    await t.expect($(`#${DROPZONE_EXTERNAL_ID}`).hasClass(DROPZONE_ACTIVE_CLASS)).notOk();
+    await t.expect(Selector(`#${DROPZONE_EXTERNAL_ID}`).hasClass(DROPZONE_ACTIVE_CLASS)).notOk();
   });
 
   test('dropzone-active class is not added to the dropzone element when multiple items are dragged', async (t) => {
@@ -50,7 +50,7 @@ runManualTest('FileUploader', 'CustomDropzone', (test) => {
       { type: 'image/png' },
     ]);
 
-    await t.expect($(`#${DROPZONE_EXTERNAL_ID}`).hasClass(DROPZONE_ACTIVE_CLASS)).notOk();
+    await t.expect(Selector(`#${DROPZONE_EXTERNAL_ID}`).hasClass(DROPZONE_ACTIVE_CLASS)).notOk();
   });
 
   test('custom dropzone user interface appearance when dropzone-active is applied', async (t) => {
