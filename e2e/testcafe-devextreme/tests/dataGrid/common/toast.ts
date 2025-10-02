442a517183b59b1f15882274a2bf2cefb7094e2e
dmlvr <lvr@dmlvr.com>
2025-10-02 15:47:31 +0300

    fix lint

--- e2e/testcafe-devextreme/tests/dataGrid/common/toast.ts
+++ e2e/testcafe-devextreme/tests/dataGrid/common/toast.ts
@@ -1,14 +1,13 @@
-import url from '../../../helpers/getPageUrl';
-import { createWidget } from '../../../helpers/createWidget';
 import DataGrid from 'devextreme-testcafe-models/dataGrid';
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import url from '../../../helpers/getPageUrl';
+import { createWidget } from '../../../helpers/createWidget';
 
 fixture.disablePageReloads`Toasts in DataGrid`.page(
   url(__dirname, '../../container.html'),
 );
 
 test('Toast should be visible after calling and should be not visible after default display time', async (t) => {
-
   const dataGrid = new DataGrid('#container');
   const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
   await dataGrid.isReady();
@@ -20,7 +19,6 @@ test('Toast should be visible after calling and should be not visible after defa
     .expect(compareResults.isValid())
     .ok(compareResults.errorMessages());
   await t.expect(dataGrid.getToast().exists).notOk();
-
 }).before(async () => {
   createWidget('dxDataGrid', {});
 });
