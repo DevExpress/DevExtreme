3521166f113cae97f0b738de19c367a8c83edd5b
Vladimir Bushmanov <vladimir.bushmanov@devexpress.com>
2025-10-06 10:25:49 +0200

    Scheduler: fix toolbar demos

--- apps/demos/Demos/Scheduler/Toolbar/ReactJs/data.js
+++ apps/demos/Demos/Scheduler/Toolbar/ReactJs/data.js
@@ -1,10 +1,9 @@
 import { DataSource } from 'devextreme-react/common/data';
 
-const ONE_MONTH_DAYS = 30;
 const addDays = (date, days) => new Date(new Date(date).setUTCDate(date.getUTCDate() + days));
 const now = new Date(new Date().setUTCHours(0, 0, 0, 0));
 const startOfTheWeek = addDays(now, -now.getUTCDay());
-export const currentDate = addDays(now, ONE_MONTH_DAYS);
+export const currentDate = new Date(2025, 5, 10);
 const currentStartOfTheWeek = addDays(currentDate, -currentDate.getUTCDay());
 const data = [
   {
