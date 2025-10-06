ec5f89759f4d3c15ab26f9087016a8b6b9f0849b
Eldar Iusupzhanov <ferrarijed@gmail.com>
2025-10-06 17:01:59 +0800

    implement storybook

---
+++ apps/react-storybook/stories/scheduler/SchedulerAppointmentForm.stories.tsx
@@ -0,0 +1,53 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import dxScheduler from "devextreme/ui/scheduler";
+import { wrapDxWithReact } from "../utils";
+import { data, resources } from "./data";
+
+const Scheduler = wrapDxWithReact(dxScheduler);
+
+const meta: Meta<typeof Scheduler> = {
+  title: 'Components/Scheduler/AppointmentForm',
+  component: Scheduler,
+  parameters: {
+    layout: 'padded',
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Scheduler>;
+
+export const Overview: Story = {
+  args: {
+    'editing.allowAdding': true,
+    'editing.allowUpdating': true,
+    'editing.allowDeleting': true,
+    'editing.allowDragging': true,
+    'editing.allowResizing': true,
+    'editing.allowTimeZoneEditing': false,
+    resources,
+    height: 600,
+    views: ['day', 'week', 'workWeek', 'month'],
+    currentView: 'workWeek',
+    currentDate: new Date(2021, 3, 29),
+    dataSource: data
+  },
+  argTypes: {
+    'editing.allowAdding': { 
+      control: 'boolean',
+    },
+    'editing.allowUpdating': { 
+      control: 'boolean',
+    },
+    'editing.allowTimeZoneEditing': { 
+      control: 'boolean',
+    },
+    'showResources': {
+      control: 'boolean',
+    },
+    'resources': {
+      control: 'boolean',
+      mapping: { true: resources, false: [] },
+    }
+  },
+}
