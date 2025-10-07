77214771eb6b2af122692f1976ef7367b25cde36
andrei.kharitonov <pharret31@gmail.com>
2025-10-07 17:59:09 +0200

    Move gallery axe test to axe folder

--- e2e/testcafe-devextreme/tests/accessibility/common/gallery/gallery.ts
+++ e2e/testcafe-devextreme/tests/accessibility/common/gallery/gallery.ts
@@ -1,10 +1,10 @@
-import { Properties } from 'devextreme/ui/gallery.d';
-import url from '../../../helpers/getPageUrl';
-import { testAccessibility, Configuration } from '../../../helpers/accessibility/test';
-import { Options } from '../../../helpers/generateOptionMatrix';
+import { Properties } from 'devextreme/ui/gallery';
+import url from '../../../../helpers/getPageUrl';
+import { testAccessibility, Configuration } from '../../../../helpers/accessibility/test';
+import { Options } from '../../../../helpers/generateOptionMatrix';
 
 fixture.disablePageReloads`Accessibility`
-  .page(url(__dirname, '../../container.html'));
+  .page(url(__dirname, '../../../container.html'));
 
 const gallery = [
   {
---
+++ e2e/testcafe-devextreme/tests/accessibility/common/gallery/optionsExistance.ts
@@ -0,0 +1,50 @@
+import { Properties } from 'devextreme/ui/gallery';
+import url from '../../../../helpers/getPageUrl';
+import { testAccessibility, Configuration } from '../../../../helpers/accessibility/test';
+import { Options } from '../../../../helpers/generateOptionMatrix';
+
+fixture.disablePageReloads`Accessibility`
+  .page(url(__dirname, '../../../container.html'));
+
+interface GalleryItem {
+  ID: string;
+  Name: string;
+}
+
+const defaultItems: GalleryItem[] = [{
+  ID: '1',
+  Name: 'First',
+}, {
+  ID: '2',
+  Name: 'Second',
+}];
+
+function defaultItemTemplate(item: GalleryItem) {
+  const result = document.createElement('div');
+  const span = document.createElement('span');
+
+  span.innerText = item.Name;
+  result.appendChild(span);
+
+  return result;
+}
+
+const options: Options<Properties> = {
+  items: [undefined, defaultItems],
+  itemTemplate: [undefined, defaultItemTemplate],
+  width: [undefined, '100%'],
+  loop: [undefined, true],
+  showIndicator: [undefined, false],
+};
+
+const a11yCheckConfig = {
+  rules: { 'image-alt': { enabled: false } },
+};
+
+const configuration: Configuration = {
+  component: 'dxGallery',
+  a11yCheckConfig,
+  options,
+};
+
+testAccessibility(configuration);
