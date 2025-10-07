2adc663bd0d8746f0993e340ca7fec513257925b
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2025-10-07 09:51:58 +0200

    + update devextreme-angular tests Rework nested components in devextreme-angular

--- packages/devextreme-angular/tests/src/ui/validator.spec.ts
+++ packages/devextreme-angular/tests/src/ui/validator.spec.ts
@@ -25,12 +25,16 @@ class TestContainerComponent {
 
   @ViewChild(DxValidatorComponent) validator: DxValidatorComponent;
 
-  text = 'Some text';
+  text = 'some@email.ok';
 
   validationRules = [
     {
       type: 'required',
       message: 'Report number is required.',
+    },
+    {
+      type: 'email',
+      message: 'Correct email is required.',
     },
   ];
 
@@ -54,61 +58,96 @@ describe('DxValidator', () => {
     );
   });
 
-  // spec
-  it('should work with dx-validator', () => {
-    TestBed.overrideComponent(TestContainerComponent, {
-      set: {
-        template: `
+  [
+    {
+      name: 'legacy',
+      tpl: `<dx-validator>
+                @for (rule of validationRules; track rule){ 
+                    <dxi-validation-rule [type]="rule.type"></dxi-validation-rule>
+                }
+            </dx-validator>`,
+    },
+    {
+      name: 'by props',
+      tpl: '<dx-validator [validationRules]="validationRules"></dx-validator>',
+    },
+    {
+      name: 'modern',
+      tpl: `<dx-validator>
+            @for (rule of validationRules; track rule){ 
+                @if (rule.type === 'email') {
+                    <dxi-validator-email-rule ></dxi-validator-email-rule>
+                } @else {
+                    <dxi-validator-required-rule></dxi-validator-required-rule>
+                 }
+            }</dx-validator>`,
+    },
+  ].forEach(({ name, tpl }) => {
+    // spec
+    it('should work with dx-validator', () => {
+      TestBed.overrideComponent(TestContainerComponent, {
+        set: {
+          template: `
                     <dx-text-box [value]="text">
-                        <dx-validator [validationRules]="validationRules"></dx-validator>
+                        ${tpl}
                     </dx-text-box>
                 `,
-      },
+        },
+      });
+      const fixture = TestBed.createComponent(TestContainerComponent);
+      fixture.detectChanges();
+
+      const testComponent = fixture.componentInstance;
+      const { instance } = testComponent.textBox;
+
+      fixture.detectChanges();
+      expect(instance.element().classList.contains('dx-invalid')).toBe(false);
+
+      testComponent.text = '';
+      fixture.detectChanges();
+      expect(instance.element().classList.contains('dx-invalid')).toBe(true);
+
+      testComponent.text = 'Some text';
+      fixture.detectChanges();
+      expect(instance.element().classList.contains('dx-invalid')).toBe(true);
+
+      testComponent.text = 'some@ema.il';
+      fixture.detectChanges();
+      expect(instance.element().classList.contains('dx-invalid')).toBe(false);
     });
-    const fixture = TestBed.createComponent(TestContainerComponent);
-    fixture.detectChanges();
 
-    const testComponent = fixture.componentInstance;
-    const { instance } = testComponent.textBox;
-
-    fixture.detectChanges();
-    expect(instance.element().classList.contains('dx-invalid')).toBe(false);
-
-    testComponent.text = '';
-    fixture.detectChanges();
-    expect(instance.element().classList.contains('dx-invalid')).toBe(true);
-
-    testComponent.text = 'Some text';
-    fixture.detectChanges();
-    expect(instance.element().classList.contains('dx-invalid')).toBe(false);
-  });
-
-  it('should work with custom editor', () => {
-    TestBed.overrideComponent(TestContainerComponent, {
-      set: {
-        template: `
+    it(`should work with custom editor (use ${name})`, () => {
+      TestBed.overrideComponent(TestContainerComponent, {
+        set: {
+          template: `
                     <input [value]="text"/>
-                    <dx-validator [validationRules]="validationRules" [adapter]="testAdapter"></dx-validator>
+                    ${tpl.replace('<dx-validator', '<dx-validator [adapter]="testAdapter"')}
                 `,
-      },
+        },
+      });
+
+      const fixture = TestBed.createComponent(TestContainerComponent);
+      fixture.detectChanges();
+
+      const testComponent = fixture.componentInstance;
+
+      testComponent.validator.instance.validate();
+      expect(testComponent.isValid).toBe(true);
+
+      testComponent.text = '';
+      fixture.detectChanges();
+      testComponent.validator.instance.validate();
+      expect(testComponent.isValid).toBe(false);
+
+      testComponent.text = 'Some text';
+      fixture.detectChanges();
+      testComponent.validator.instance.validate();
+      expect(testComponent.isValid).toBe(false);
+
+      testComponent.text = 'some@email.yes';
+      fixture.detectChanges();
+      testComponent.validator.instance.validate();
+      expect(testComponent.isValid).toBe(true);
     });
-
-    const fixture = TestBed.createComponent(TestContainerComponent);
-    fixture.detectChanges();
-
-    const testComponent = fixture.componentInstance;
-
-    testComponent.validator.instance.validate();
-    expect(testComponent.isValid).toBe(true);
-
-    testComponent.text = '';
-    fixture.detectChanges();
-    testComponent.validator.instance.validate();
-    expect(testComponent.isValid).toBe(false);
-
-    testComponent.text = 'Some text';
-    fixture.detectChanges();
-    testComponent.validator.instance.validate();
-    expect(testComponent.isValid).toBe(true);
   });
 });
