0ea48f4b9ff85448fb1d8c5d2491325037441839
dmlvr <lvr@dmlvr.com>
2025-10-08 08:15:07 +0300

    check column types

--- packages/devextreme/js/__internal/grids/grid_core/columns_controller/m_columns_controller.ts
+++ packages/devextreme/js/__internal/grids/grid_core/columns_controller/m_columns_controller.ts
@@ -84,7 +84,8 @@ import {
   updateSerializers,
 } from './m_columns_controller_utils';
 
-const COMMAND_COLUMN_TYPES = ['adaptive', 'buttons', 'selection', 'drag', 'ai'];
+// const UNSUPPORTED_GROUPING_COLUMN_TYPES = ['adaptive', 'buttons', 'detailExpand', 'groupExpand', 'selection', 'drag', 'ai'];
+const UNSUPPORTED_GROUPING_COLUMN_TYPES = ['buttons', 'ai'];
 
 export interface Column extends ColumnBase {
   parseValue: (text: string) => unknown;
@@ -457,7 +458,7 @@ export class ColumnsController extends modules.Controller {
   private _columnCanBeGrouped(column) {
     if (!column) return false;
 
-    return isDefined(column.groupIndex) && !column.command && !COMMAND_COLUMN_TYPES.includes(column.type);
+    return isDefined(column.groupIndex) && !column.command && !UNSUPPORTED_GROUPING_COLUMN_TYPES.includes(column.type);
   }
 
   public getGroupColumns() {
