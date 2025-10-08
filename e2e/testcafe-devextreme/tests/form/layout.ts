e350c7a0b020c146f5d568a6b1e26fed1b245c66
Anna Shakhova <68295572+anna-shakhova@users.noreply.github.com>
2025-10-08 09:03:37 +0200

    Form: rearrange screenshot tests (#31245)

--- e2e/testcafe-devextreme/tests/form/layout.ts
+++ e2e/testcafe-devextreme/tests/form/layout.ts
@@ -1,5 +1,5 @@
-import { ClientFunction } from 'testcafe';
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
+import { ClientFunction } from 'testcafe';
 import url from '../../helpers/getPageUrl';
 import { createWidget } from '../../helpers/createWidget';
 import { testScreenshot } from '../../helpers/themeUtils';
@@ -9,6 +9,70 @@ const waitFont = ClientFunction(() => (window as any).DevExpress.ui.themes.waitW
 
 fixture.disablePageReloads`Form`
   .page(url(__dirname, '../container.html'));
+
+test('SimpleItem: item1_cSpan_2', async (t) => {
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+  await testScreenshot(t, takeScreenshot, 'SimpleItem,item1_cSpan_2.png', { element: '#container' });
+
+  await t
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => {
+  await waitFont();
+  await setAttribute('#container', 'style', 'width: 500px;');
+
+  for (let colCount = 1; colCount <= 4; colCount += 1) {
+    const formId = `form${colCount}`;
+
+    await appendElementTo('#container', 'div', formId);
+    await addCaptionTo(`#${formId}`, `colCount = ${colCount}`);
+
+    const formOptions = {
+      elementAttr: { style: 'margin-bottom: 20px' },
+      labelMode: 'static',
+      colCount,
+      items: [{ dataField: 'item_1', colSpan: 2 }],
+    };
+
+    await createWidget('dxForm', formOptions, `#${formId}`);
+  }
+});
+
+[[1, 2], [2, 1], [2, 2]].forEach(([colSpan1, colSpan2]) => {
+  const testName = `SimpleItem,item1_cSpan_${colSpan1},item2_cSpan_${colSpan2}`;
+  test(testName, async (t) => {
+    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+    await testScreenshot(t, takeScreenshot, `${testName}.png`, { element: '#container' });
+
+    await t
+      .expect(compareResults.isValid())
+      .ok(compareResults.errorMessages());
+  }).before(async () => {
+    await waitFont();
+    await setAttribute('#container', 'style', 'width: 600px;');
+
+    for (let colCount = 1; colCount <= 4; colCount += 1) {
+      const formId = `form${colCount}`;
+
+      await appendElementTo('#container', 'div', formId);
+      await addCaptionTo(`#${formId}`, `colCount = ${colCount}`);
+
+      const formOptions = {
+        elementAttr: { style: 'margin-bottom: 20px' },
+        labelMode: 'static',
+        colCount,
+        items: [
+          { dataField: `item_1_span_${colSpan1}`, colSpan: colSpan1 },
+          { dataField: `item_2_span_${colSpan2}`, colSpan: colSpan2 },
+        ],
+      };
+
+      await createWidget('dxForm', formOptions, `#${formId}`);
+    }
+  });
+});
 
 [false, true].forEach((rtlEnabled) => {
   [1, 2, 3, 4, 5, 6].forEach((itemsCount) => {
