826f5bdf506923c22adf1888085d9bcc140d3118
andrei.kharitonov <pharret31@gmail.com>
2025-10-08 11:27:45 +0200

    Reduce form tests amount

--- e2e/testcafe-devextreme/tests/accessibility/common/form/common.ts
+++ e2e/testcafe-devextreme/tests/accessibility/common/form/common.ts
@@ -1,28 +1,26 @@
-import { Properties } from 'devextreme/ui/form.d';
-import url from '../../../helpers/getPageUrl';
-import { testAccessibility, Configuration } from '../../../helpers/accessibility/test';
-import { Options } from '../../../helpers/generateOptionMatrix';
+import { Properties } from 'devextreme/ui/form';
+import url from '../../../../helpers/getPageUrl';
+import { testAccessibility, Configuration } from '../../../../helpers/accessibility/test';
+import { Options } from '../../../../helpers/generateOptionMatrix';
 
 fixture.disablePageReloads`Accessibility`
-  .page(url(__dirname, '../../container.html'));
+  .page(url(__dirname, '../../../container.html'));
 
 const formData = {
   ID: 1,
   FirstName: 'John',
   LastName: 'Heart',
   Position: 'CEO',
+  Active: true,
 };
 
 const options: Options<Properties> = {
   height: [200],
   hint: [undefined, 'hint'],
-  requiredMark: [undefined, '#'],
   alignItemLabels: [true, false],
-  showRequiredMark: [true, false],
   showOptionalMark: [true, false],
-  showValidationSummary: [true, false],
   showColonAfterLabel: [true, false],
-  formData: [undefined, formData],
+  formData: [formData],
 };
 
 const a11yCheckConfig = {
