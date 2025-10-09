eaed1f3e8c40987db0493a8086db8987f4e3ed5e
Julia Volkova <iuliia.volkova@devexpress.com>
2025-10-09 13:57:28 +0300

    a11y lookup, popup, slider, stepper, textbox, toast

--- e2e/testcafe-devextreme/tests/accessibility/common/slider.ts
+++ e2e/testcafe-devextreme/tests/accessibility/common/slider.ts
@@ -8,14 +8,6 @@ fixture.disablePageReloads`Accessibility`
 
 const options: Options<Properties> = {
   value: [45],
-  disabled: [true, false],
-  readOnly: [true, false],
-  hint: [undefined, 'hint'],
-  name: ['', 'name'],
-  height: [undefined, 250],
-  width: [undefined, '50%'],
-  min: [undefined, 10],
-  max: [undefined, 90],
   label: [
     {
       visible: true,
@@ -42,10 +34,30 @@ const a11yCheckConfig = {
   rules: { 'color-contrast': { enabled: false } },
 };
 
-const configuration: Configuration = {
+const inputConfiguration: Configuration = {
   component: 'dxSlider',
   a11yCheckConfig,
-  options,
+  options: {
+    ...options,
+    disabled: [true, false],
+    readOnly: [true, false],
+    hint: [undefined, 'hint'],
+    name: ['', 'name'],
+  },
 };
 
-testAccessibility(configuration);
+testAccessibility(inputConfiguration);
+
+const valueConfiguration: Configuration = {
+  component: 'dxSlider',
+  a11yCheckConfig,
+  options: {
+    ...options,
+    height: [undefined, 250],
+    width: [undefined, '50%'],
+    min: [undefined, 10],
+    max: [undefined, 90],
+  },
+};
+
+testAccessibility(valueConfiguration);
--- e2e/testcafe-devextreme/tests/accessibility/common/textBox.ts
+++ e2e/testcafe-devextreme/tests/accessibility/common/textBox.ts
@@ -9,12 +9,8 @@ fixture.disablePageReloads`Accessibility`
 const options: Options<Properties> = {
   value: [undefined, 'value'],
   placeholder: [undefined, 'placeholder'],
-  disabled: [true, false],
-  readOnly: [true, false],
-  // showClearButton: [true, false],
-  // mode: [undefined, 'password', 'email', 'search', 'tel', 'text', 'url'],
-  label: ['', 'label'],
-  name: ['', 'name'],
+  showClearButton: [true, false],
+  mode: ['password', 'email', 'search', 'tel', 'text', 'url'],
   // NOTE: Doesn't matter if there are contrast issues
   // stylingMode: ['outlined', 'filled', 'underlined'],
   inputAttr: [{ 'aria-label': 'aria-label' }],
@@ -25,24 +21,26 @@ const a11yCheckConfig = {
   rules: { 'color-contrast': { enabled: false } },
 };
 
-const inputConfiguration: Configuration = {
+const availabilityConfiguration: Configuration = {
   component: 'dxTextBox',
   a11yCheckConfig,
   options: {
     ...options,
-    showClearButton: [true, false],
+    disabled: [true, false],
+    readOnly: [true, false],
   },
 };
 
-testAccessibility(inputConfiguration);
+testAccessibility(availabilityConfiguration);
 
-const modeConfiguration: Configuration = {
+const infoConfiguration: Configuration = {
   component: 'dxTextBox',
   a11yCheckConfig,
   options: {
     ...options,
-    mode: [undefined, 'password', 'email', 'search', 'tel', 'text', 'url'],
+    label: ['', 'label'],
+    name: ['', 'name'],
   },
 };
 
-testAccessibility(modeConfiguration);
+testAccessibility(infoConfiguration);
