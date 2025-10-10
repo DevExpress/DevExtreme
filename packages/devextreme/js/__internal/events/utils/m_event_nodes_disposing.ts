8a893e7b6fdc57fcee2fe6409c6a90f307740af7
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2025-10-10 09:13:31 +0200

    fix events_engine (T1307313)

--- packages/devextreme/js/__internal/events/utils/m_event_nodes_disposing.ts
+++ packages/devextreme/js/__internal/events/utils/m_event_nodes_disposing.ts
@@ -2,12 +2,18 @@ import eventsEngine from '@js/common/core/events/core/events_engine';
 import { removeEvent } from '@js/common/core/events/remove';
 
 function nodesByEvent(event) {
+  const insideDxParent = (element) => {
+    const el = element?.closest?.('[class^="dx-"], [class*=" dx-"]');
+
+    return !!el;
+  };
+
   return event && [
     event.target,
     event.delegateTarget,
     event.relatedTarget,
     event.currentTarget,
-  ].filter((node) => !!node);
+  ].filter((node) => !!node && insideDxParent(node));
 }
 
 export const subscribeNodesDisposing = (event, callback) => {
