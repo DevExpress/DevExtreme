a1436e63036534f3f0852d3272701c744fc094af
Julia Volkova <iuliia.volkova@devexpress.com>
2025-10-13 02:30:49 +0300

    datebox validation

--- e2e/testcafe-devextreme/tests/editors/dateBox/validationMessage.ts
+++ e2e/testcafe-devextreme/tests/editors/dateBox/validationMessage.ts
@@ -12,37 +12,6 @@ fixture.disablePageReloads`DateBox ValidationMessagePosition`
 
 const positions = ['top', 'right', 'bottom', 'left'];
 
-positions.forEach((position) => {
-  safeSizeTest(`DateBox ValidationMessage position is correct (${position})`, async (t) => {
-    const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
-
-    const dateBox = new DateBox('#container');
-    await dateBox.option('value', new Date(2022, 6, 14));
-
-    await testScreenshot(t, takeScreenshot, `Datebox validation message with position=${position}.png`);
-
-    await t
-      .expect(compareResults.isValid())
-      .ok(compareResults.errorMessages());
-  }, [300, 200]).before(async () => {
-    await createWidget('dxDateBox', {
-      elementAttr: { style: 'margin: 50px 0 0 100px;' },
-      width: 100,
-      height: 40,
-      validationMessageMode: 'always',
-      validationMessagePosition: position,
-    });
-
-    return createWidget('dxValidator', {
-      validationRules: [{
-        type: 'range',
-        max: new Date(1),
-        message: 'out of range',
-      }],
-    });
-  });
-});
-
 safeSizeTest('DateBox ValidationMessage position is correct', async (t) => {
   const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
