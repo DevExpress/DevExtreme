be7fbd5ec57d758abfdd6f3806d5b1983068bc05
marker dao ® <youdontknow@marker-dao.eth>
2025-10-17 15:44:57 +0200

    fix && refactor(caret): Get rid of !isElementInBody(input) condition

--- packages/devextreme/js/__internal/ui/text_box/m_utils.caret.ts
+++ packages/devextreme/js/__internal/ui/text_box/m_utils.caret.ts
@@ -1,25 +1,42 @@
 import devices from '@js/core/devices';
 import domAdapter from '@js/core/dom_adapter';
+import type { dxElementWrapper } from '@js/core/renderer';
 import $ from '@js/core/renderer';
 import { isDefined } from '@js/core/utils/type';
 
-export interface CaretRange { start: number; end: number }
+export interface CaretRange {
+  start: number;
+  end: number;
+}
 
 const {
   ios,
-  // @ts-expect-error
+  // @ts-expect-error Device type doesn't contain mac
   mac,
 } = devices.real();
+
 // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
 const isFocusingOnCaretChange = ios || mac;
 
+// const isElementInBody = (element: Element): boolean => {
+//   const body = domAdapter.getBody();
+
+//   const isElementItselfInBody = body.contains(element);
+//   const isHostElementInBody = body.contains((element.getRootNode() as ShadowRoot).host);
+
+//   return isElementItselfInBody || isHostElementInBody;
+// };
+
 const getCaret = (input: HTMLInputElement): CaretRange => {
-  let range;
+  let range: CaretRange = {
+    start: 0,
+    end: 0,
+  };
 
   try {
     range = {
-      start: input.selectionStart,
-      end: input.selectionEnd,
+      start: input.selectionStart ?? 0,
+      end: input.selectionEnd ?? 0,
     };
   } catch (e) {
     range = {
@@ -31,40 +48,43 @@ const getCaret = (input: HTMLInputElement): CaretRange => {
   return range;
 };
 
-/**
- * вторая часть выражения была добавлена во время поддержки ShodowDOM
- * первая часть выражения судя по-всему была добавлена для того,
- * чтобы запревентить input === undefined, НО
- * input может существовать, но не быть приаттаченным к DOM
- * да даже если input не принадлежит к этому body, то какая может быть опасность?
- */
-
-export const setCaret = (input, position) => {
-  // const body = domAdapter.getBody();
-  // if (!body.contains(input) && !body.contains(input.getRootNode().host)) {
+export const setCaret = (
+  input: HTMLInputElement,
+  selection: CaretRange,
+): void => {
+  // if (!isElementInBody(input)) {
   //   return;
   // }
 
-  // try {
-  input.selectionStart = position.start;
-  input.selectionEnd = position.end;
-  // } catch (e) { /* empty */ }
+  try {
+    input.selectionStart = selection.start;
+    input.selectionEnd = selection.end;
+  } catch { /** empty */ }
 };
-// @ts-expect-error
 
-const caret = (input, position?: any, force = false): CaretRange | undefined => {
-  input = $(input).get(0);
+const caret = (
+  input: HTMLInputElement | dxElementWrapper,
+  selection?: CaretRange,
+  force = false,
+): CaretRange | undefined => {
+  const inputElement = $(input).get(0) as HTMLInputElement;
 
-  if (!isDefined(position)) {
-    return getCaret(input);
+  if (!isDefined(selection)) {
+    return getCaret(inputElement);
   }
 
   // NOTE: AppleWebKit-based browsers focuses element input after caret position has changed
-  if (!force && isFocusingOnCaretChange && domAdapter.getActiveElement(input) !== input) {
-    return;
+  if (
+    !force
+    && isFocusingOnCaretChange
+    && domAdapter.getActiveElement(inputElement) !== inputElement
+  ) {
+    return undefined;
   }
 
-  setCaret(input, position);
+  setCaret(inputElement, selection);
+
+  return undefined;
 };
 
 export default caret;
