8f6516bbf9ec67ad5d7438c5cdbc27b25234c338
Julia Volkova <juleari@users.noreply.github.com>
2025-10-20 13:37:49 +0300

    QUnit tests: remove renovation parts (#31388)

--- packages/devextreme/js/renovation/test_utils/create_ref.ts
+++
@@ -1,5 +0,0 @@
-import { RefObject } from '@devextreme-generator/declarations';
-
-export function createTestRef<T = HTMLDivElement>(current: unknown = {}): RefObject<T> {
-  return { current } as RefObject<T>;
-}
