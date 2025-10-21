a875df0b8d652aec83a4c26f8f5bc35943c50ba6
marker dao ® <youdontknow@marker-dao.eth>
2025-10-21 15:34:30 +0200

    feat(chat file): Add default options (data: Attachment, onDownload: undefined)

--- packages/devextreme/js/__internal/ui/chat/file_view/file.ts
+++ packages/devextreme/js/__internal/ui/chat/file_view/file.ts
@@ -1,46 +1,52 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import $ from '@js/core/renderer';
-import type {
-  ClickEvent,
-  InitializedEvent,
-} from '@js/ui/button';
+// import type {
+//   ClickEvent,
+//   InitializedEvent,
+// } from '@js/ui/button';
 import type Button from '@js/ui/button';
+import type {
+  Attachment,
+  AttachmentDownloadEvent,
+} from '@js/ui/chat';
 import type { OptionChanged } from '@ts/core/widget/types';
 import type { WidgetProperties } from '@ts/core/widget/widget';
 import Widget from '@ts/core/widget/widget';
 
 export type Properties = WidgetProperties & {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  data: any;
+  data?: Attachment;
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  onDownload: (e: any) => void;
+  onDownload?: (e: Partial<AttachmentDownloadEvent>) => void;
 };
 
 class File extends Widget<Properties> {
   _downloadButton?: Button | null;
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  _downloadAction?: (e: any) => void;
+  _downloadAction?: (e: Partial<AttachmentDownloadEvent>) => void;
 
   _getDefaultOptions(): Properties {
     return {
       ...super._getDefaultOptions(),
+      data: {
+        name: '',
+        size: 0,
+      },
+      onDownload: undefined,
     };
   }
 
   _init(): void {
     super._init();
 
-    this._createSendAction();
+    // this._createSendAction();
   }
 
-  _createSendAction(): void {
-    this._downloadAction = this._createActionByOption(
-      'onDownload',
-      { excludeValidators: ['disabled'] },
-    );
-  }
+  // _createSendAction(): void {
+  //   this._downloadAction = this._createActionByOption(
+  //     'onDownload',
+  //     { excludeValidators: ['disabled'] },
+  //   );
+  // }
 
   _initMarkup(): void {
     super._initMarkup();
@@ -51,31 +57,30 @@ class File extends Widget<Properties> {
 
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  _getSendButtonConfig(): any {
-    const {
-      activeStateEnabled,
-      focusStateEnabled,
-      hoverStateEnabled,
-    } = this.option();
+  // _getSendButtonConfig(): any {
+  //   const {
+  //     activeStateEnabled,
+  //     focusStateEnabled,
+  //     hoverStateEnabled,
+  //   } = this.option();
 
-    const configuration = {
-      activeStateEnabled,
-      focusStateEnabled,
-      hoverStateEnabled,
-      icon: 'arrowright',
-      type: 'default',
-      stylingMode: 'contained',
-      onClick: (e: ClickEvent): void => {
-        this._downloadAction?.(e);
-      },
-      onInitialized: (e: InitializedEvent): void => {
-        this._downloadButton = e.component;
-      },
-    };
+  //   const configuration = {
+  //     activeStateEnabled,
+  //     focusStateEnabled,
+  //     hoverStateEnabled,
+  //     icon: 'arrowright',
+  //     type: 'default',
+  //     stylingMode: 'contained',
+  //     onClick: (e: ClickEvent): void => {
+  //       this._downloadAction?.(e);
+  //     },
+  //     onInitialized: (e: InitializedEvent): void => {
+  //       this._downloadButton = e.component;
+  //     },
+  //   };
 
-    return configuration;
-  }
+  //   return configuration;
+  // }
 
   _optionChanged(args: OptionChanged<Properties>): void {
     const { name, value } = args;
@@ -88,12 +93,12 @@ class File extends Widget<Properties> {
         break;
 
       case 'data': {
-        /** invalidate */
+        this._invalidate();
         break;
       }
 
       case 'onDownload':
-        this._createSendAction();
+        // this._createSendAction();
         break;
 
       default:
