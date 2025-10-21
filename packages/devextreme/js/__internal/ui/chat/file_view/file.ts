4c6fc80beafd7f5e09f0b453131d9d135a147685
marker dao ® <youdontknow@marker-dao.eth>
2025-10-21 16:26:29 +0200

    feat(chat file): Render 4 sections (icon, name, size, download button)

--- packages/devextreme/js/__internal/ui/chat/file_view/file.ts
+++ packages/devextreme/js/__internal/ui/chat/file_view/file.ts
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import type { dxElementWrapper } from '@js/core/renderer';
 import $ from '@js/core/renderer';
 // import type {
 //   ClickEvent,
@@ -9,6 +9,7 @@ import type {
   Attachment,
   AttachmentDownloadEvent,
 } from '@js/ui/chat';
+import { getImageContainer } from '@ts/core/utils/m_icon';
 import type { OptionChanged } from '@ts/core/widget/types';
 import type { WidgetProperties } from '@ts/core/widget/widget';
 import Widget from '@ts/core/widget/widget';
@@ -18,6 +19,13 @@ export type Properties = WidgetProperties & {
 
   onDownload?: (e: Partial<AttachmentDownloadEvent>) => void;
 };
+
+const CHAT_FILE_CLASS = 'dx-chat-file';
+const CHAT_FILE_MAIN_CLASS = 'dx-chat-file-main';
+const CHAT_FILE_META_CLASS = 'dx-chat-file-meta';
+const CHAT_FILE_NAME_CLASS = 'dx-chat-file-name';
+const CHAT_FILE_SIZE_CLASS = 'dx-chat-file-size';
+const CHAT_FILE_DOWNLOAD_BUTTON_CLASS = 'dx-chat-file-download-button';
 
 class File extends Widget<Properties> {
   _downloadButton?: Button | null;
@@ -49,12 +57,68 @@ class File extends Widget<Properties> {
   // }
 
   _initMarkup(): void {
+    this.$element().addClass(CHAT_FILE_CLASS);
     super._initMarkup();
     this._renderSections();
   }
 
   _renderSections(): void {
+    this._renderMain();
+    this._renderMeta();
+  }
 
+  _renderMain(): void {
+    const $mainSection = $('<div>').addClass(CHAT_FILE_MAIN_CLASS);
+
+    this.$element().append($mainSection);
+
+    this._renderIcon($mainSection);
+    this._renderName($mainSection);
+  }
+
+  _renderIcon($section: dxElementWrapper): void {
+    const $icon = getImageContainer('file') ?? $('<div>');
+
+    $section.append($icon);
+  }
+
+  _renderName($section: dxElementWrapper): void {
+    const { data } = this.option();
+    const { name } = data ?? {};
+
+    const $name = $('<span>')
+      .addClass(CHAT_FILE_NAME_CLASS)
+      .text(name ?? '');
+
+    $section.append($name);
+  }
+
+  _renderMeta(): void {
+    const $metaSection = $('<div>').addClass(CHAT_FILE_META_CLASS);
+
+    this.$element().append($metaSection);
+
+    this._renderSize($metaSection);
+    this._renderButton($metaSection);
+  }
+
+  _renderSize($section: dxElementWrapper): void {
+    const { data } = this.option();
+    const { size } = data ?? {};
+
+    const text = `${size ?? 0} KB`;
+
+    const $size = $('<span>')
+      .addClass(CHAT_FILE_SIZE_CLASS)
+      .text(text);
+
+    $section.append($size);
+  }
+
+  _renderButton($section: dxElementWrapper): void {
+    const $button = $('<div>').addClass(CHAT_FILE_DOWNLOAD_BUTTON_CLASS);
+
+    $section.append($button);
   }
 
   // _getSendButtonConfig(): any {
