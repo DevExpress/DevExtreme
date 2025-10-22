41109c2d0a86c0b49a17b725677de4903af23602
dmlvr <lvr@dmlvr.com>
2025-10-22 14:56:37 +0300

    modified scrolling test

--- e2e/testcafe-devextreme/tests/filterBuilder/filterBuilderScrolling.ts
+++ e2e/testcafe-devextreme/tests/filterBuilder/filterBuilderScrolling.ts
@@ -1,20 +1,16 @@
 import FilterBuilder from 'devextreme-testcafe-models/filterBuilder';
-import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import url from '../../helpers/getPageUrl';
 import { fields, filter } from './data';
-import { safeSizeTest } from '../../helpers/safeSizeTest';
 import { createWidget } from '../../helpers/createWidget';
-import { testScreenshot } from '../../helpers/themeUtils';
 import { insertStylesheetRulesToPage } from '../../helpers/domUtils';
 
 fixture`Filter Builder Scrolling Test`.page(
   url(__dirname, '../container.html'),
 );
 
-// T1273328
-safeSizeTest('FilterBuilder - The field drop-down window moves with the page scroll', async (t) => {
+// T1273328 > T1294239
+test('FilterBuilder - The field drop-down closes with the page scroll', async (t) => {
   const filterBuilder = new FilterBuilder('#container');
-  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
   await filterBuilder.isReady();
 
@@ -22,10 +18,8 @@ safeSizeTest('FilterBuilder - The field drop-down window moves with the page scr
     .click(filterBuilder.getItem('operation'))
     .scrollIntoView(filterBuilder.getItem('operation', 4));
 
-  await testScreenshot(t, takeScreenshot, 'filterBuilder_scroll_with_popup.png', { element: filterBuilder.element });
-  await t
-    .expect(compareResults.isValid())
-    .ok(compareResults.errorMessages());
+  await t.expect(FilterBuilder.getPopupTreeView().exists).notOk();
+
 }).before(async () => {
   await insertStylesheetRulesToPage('#container {height: 150px; overflow: scroll;}');
 
