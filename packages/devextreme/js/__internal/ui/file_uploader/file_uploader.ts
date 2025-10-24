c3c8fcd9e9a424548f23d724934343595a4e268c
marker dao ® <mark_nikolsky@outlook.com>
2025-10-24 16:34:52 +0200

    FileUploader: Support accessibility in FileList (#31454)
    Co-authored-by: marker dao ® <youdontknow@marker-dao.eth>

--- packages/devextreme/js/__internal/ui/file_uploader/file_uploader.ts
+++ packages/devextreme/js/__internal/ui/file_uploader/file_uploader.ts
@@ -652,10 +652,23 @@ class FileUploader extends Editor<FileUploaderProperties> {
     }
 
     this.$element().toggleClass(FILEUPLOADER_SHOW_FILE_LIST_CLASS, showFileList);
+    this._toggleFileContainerAria(Boolean(showFileList && this._files?.length));
     this._toggleFileUploaderEmptyClassName();
     this._updateFileNameMaxWidth();
 
     this._validationMessage?.repaint();
+  }
+
+  _toggleFileContainerAria(applyAria: boolean): void {
+    const aria = {
+      role: applyAria ? 'list' : null,
+      'aria-label': applyAria
+        ? messageLocalization.format('dxFileUploader-fileListLabel')
+        : null,
+    };
+
+    // @ts-expect-error attr type should be extdened
+    this._$filesContainer?.attr(aria);
   }
 
   _renderFile(file: FileUploaderItem): void {
@@ -667,7 +680,8 @@ class FileUploader extends Editor<FileUploaderProperties> {
 
     const $fileContainer = $('<div>')
       .addClass(FILEUPLOADER_FILE_CONTAINER_CLASS)
-      .appendTo(this._$filesContainer);
+      .appendTo(this._$filesContainer)
+      .attr('role', 'listitem');
 
     this._renderFileIcon(value.name, $fileContainer);
 
@@ -800,6 +814,10 @@ class FileUploader extends Editor<FileUploaderProperties> {
         integrationOptions: {},
         hoverStateEnabled,
         stylingMode: _buttonStylingMode,
+        elementAttr: {
+          // @ts-expect-error foramat params should be extended
+          'aria-label': messageLocalization.format('dxFileUploader-removeFileButtonLabel', file?.value?.name ?? ''),
+        },
       },
     );
 
@@ -825,6 +843,10 @@ class FileUploader extends Editor<FileUploaderProperties> {
         icon: 'upload',
         hoverStateEnabled,
         stylingMode: _buttonStylingMode,
+        elementAttr: {
+          // @ts-expect-error foramat params should be extended
+          'aria-label': messageLocalization.format('dxFileUploader-uploadFileButtonLabel', file?.value?.name ?? ''),
+        },
       },
     );
 
@@ -858,6 +880,10 @@ class FileUploader extends Editor<FileUploaderProperties> {
     this._preventRecreatingFiles = true;
     this.option({ value: valueCopy });
     this._preventRecreatingFiles = false;
+
+    if (this._files?.length === 0) {
+      this._toggleFileContainerAria(false);
+    }
 
     this._toggleFileUploaderEmptyClassName();
     this._resetInputValue(true);
