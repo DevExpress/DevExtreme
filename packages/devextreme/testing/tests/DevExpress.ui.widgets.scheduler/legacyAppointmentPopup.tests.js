5dcc5e233eaea460654e8d54ea98875793fa9d1a
Sergio Bur <sjbur.dev@gmail.com>
2025-10-24 17:28:21 +0200

    fix: fix legacyAppointment test

--- packages/devextreme/testing/tests/DevExpress.ui.widgets.scheduler/legacyAppointmentPopup.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.widgets.scheduler/legacyAppointmentPopup.tests.js
@@ -615,7 +615,8 @@ QUnit.module('Appointment popup form', moduleConfig, () => {
                 currentView: 'week',
                 currentDate: new Date(2017, 4, 25),
                 editing: {
-                    allowUpdating: true
+                    allowUpdating: true,
+                    legacyForm: true
                 }
             });
 
