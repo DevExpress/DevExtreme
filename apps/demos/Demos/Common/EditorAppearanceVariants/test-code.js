a0a2ad4cf01d6c6d51d1dfeb16565a4263725e2c
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2025-10-29 17:50:37 +0400

    e2e demos tests: switch on native automation (#31362)
    Co-authored-by: Andrei Kharitonov <pharret31@gmail.com>

--- apps/demos/Demos/Common/EditorAppearanceVariants/test-code.js
+++
@@ -1,6 +0,0 @@
-testUtils.importAnd(() => 'devextreme/animation/fx', () => DevExpress.fx, (fx) => {
-  fx.off = true;
-
-  return testUtils
-    .postponeUntilFound('.dx-texteditor-with-label.dx-invalid', 100, 5000);
-});
