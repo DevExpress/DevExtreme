30ecbeff406cb1b54c966df3c302fa8872daa077
Julia Volkova <iuliia.volkova@devexpress.com>
2025-10-29 13:22:44 +0200

    unskipped T1210807

--- e2e/testcafe-devextreme/tests/common/pivotGrid/virtualScrolling_T1210807.ts
+++ e2e/testcafe-devextreme/tests/common/pivotGrid/virtualScrolling_T1210807.ts
@@ -2,16 +2,9 @@ import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import PivotGrid from 'devextreme-testcafe-models/pivotGrid';
 import url from '../../../helpers/getPageUrl';
 import { createWidget } from '../../../helpers/createWidget';
-import { isMaterialBased, testScreenshot } from '../../../helpers/themeUtils';
+import { testScreenshot } from '../../../helpers/themeUtils';
 
-const testFixture = (): FixtureFn => {
-  if (isMaterialBased()) {
-    return fixture.disablePageReloads.skip;
-  }
-  return fixture.disablePageReloads;
-};
-
-testFixture().disablePageReloads`PivotGrid_scrolling`
+fixture.disablePageReloads`PivotGrid_scrolling`
   .page(url(__dirname, '../../container.html'));
 
 const createData = (count, innerCount) => {
@@ -32,7 +25,7 @@ const createData = (count, innerCount) => {
   return result;
 };
 
-test.meta({ unstable: true })('Row fields overlap data fields if dataFieldArea is set to "row" and virtual scrolling is enabled (T1210807)', async (t) => {
+test('Row fields overlap data fields if dataFieldArea is set to "row" and virtual scrolling is enabled (T1210807)', async (t) => {
   const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
   const pivotGrid = new PivotGrid('#container');
   const firstHeaderRow = pivotGrid.getRowsArea(2).getCell(0);
