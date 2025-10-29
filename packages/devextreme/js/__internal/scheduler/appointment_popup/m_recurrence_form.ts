67cb5818e028fb80551b42e669903a3687f3452b
Sergio Bur <sjbur.dev@gmail.com>
2025-10-29 17:05:58 +0100

    fix: revert css button fixed sizing

--- packages/devextreme/js/__internal/scheduler/appointment_popup/m_recurrence_form.ts
+++ packages/devextreme/js/__internal/scheduler/appointment_popup/m_recurrence_form.ts
@@ -334,9 +334,6 @@ export class RecurrenceForm {
 
           this._weekDayButtons[item.key] = this.scheduler.createComponent(buttonContainer, Button, {
             text: item.text,
-            onContentReady: (e): void => {
-              $(e.element).removeClass('dx-button-has-text');
-            },
             onClick: (): void => {
               const isSelected = this.recurrenceRule.byDay.includes(item.key);
 
