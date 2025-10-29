ffe2b7f46ead9f92934e9f82f80f0f5ada850d4f
Danil Mirgaev <danil.mirgaev@devexpress.com>
2025-10-29 18:00:23 +0400

    test(DataGrid): add integration tests

--- packages/devextreme/js/__internal/grids/grid_core/columns_resizing_reordering/utils.test.ts
+++ packages/devextreme/js/__internal/grids/grid_core/columns_resizing_reordering/utils.test.ts
@@ -13,62 +13,60 @@ const getDraggingPanelMock = () => ({
   }),
 } as any);
 
-describe('m_columns_resizing_reordering/utils.test.ts', () => {
-  describe('getDraggingPanelBoundingRects', () => {
-    it('returns equal amount of elements in tuple', () => {
-      const draggingPanels = [
-        getDraggingPanelMock(),
-        getDraggingPanelMock(),
-        getDraggingPanelMock(),
-      ];
-      const result = getDraggingPanelBoundingRects(draggingPanels);
+describe('getDraggingPanelBoundingRects', () => {
+  it('returns equal amount of elements in tuple', () => {
+    const draggingPanels = [
+      getDraggingPanelMock(),
+      getDraggingPanelMock(),
+      getDraggingPanelMock(),
+    ];
+    const result = getDraggingPanelBoundingRects(draggingPanels);
 
-      expect(result?.length).toBe(draggingPanels.length);
+    expect(result?.length).toBe(draggingPanels.length);
+  });
+
+  it('returns the same dragging panel objects', () => {
+    const draggingPanels = [
+      getDraggingPanelMock(),
+      getDraggingPanelMock(),
+      getDraggingPanelMock(),
+    ];
+    const result = getDraggingPanelBoundingRects(draggingPanels);
+
+    expect(result).not.toBeNull();
+    result?.forEach((draggingPanelBoundingRect, index) => {
+      expect(draggingPanelBoundingRect.draggingPanel).toBe(draggingPanels[index]);
     });
+  });
 
-    it('returns the same dragging panel objects', () => {
-      const draggingPanels = [
-        getDraggingPanelMock(),
-        getDraggingPanelMock(),
-        getDraggingPanelMock(),
-      ];
-      const result = getDraggingPanelBoundingRects(draggingPanels);
+  it('returns filtered tuple without empty objects', () => {
+    const draggingPanels = [
+      getDraggingPanelMock(),
+      undefined,
+      getDraggingPanelMock(),
+      null,
+    ];
+    const result = getDraggingPanelBoundingRects(draggingPanels);
 
-      expect(result).not.toBeNull();
-      result?.forEach((draggingPanelBoundingRect, index) => {
-        expect(draggingPanelBoundingRect.draggingPanel).toBe(draggingPanels[index]);
-      });
-    });
+    expect(result?.length).toBe(draggingPanels.filter(Boolean).length);
+  });
 
-    it('returns filtered tuple without empty objects', () => {
-      const draggingPanels = [
-        getDraggingPanelMock(),
-        undefined,
-        getDraggingPanelMock(),
-        null,
-      ];
-      const result = getDraggingPanelBoundingRects(draggingPanels);
+  it('returns null if the result is empty array', () => {
+    const draggingPanels = [undefined, null] as any[];
+    const result = getDraggingPanelBoundingRects(draggingPanels);
 
-      expect(result?.length).toBe(draggingPanels.filter(Boolean).length);
-    });
+    expect(result).toBeNull();
+  });
 
-    it('returns null if the result is empty array', () => {
-      const draggingPanels = [undefined, null] as any[];
-      const result = getDraggingPanelBoundingRects(draggingPanels);
+  it('returns null if draggingPanels array is empty', () => {
+    const draggingPanels = [] as any[];
+    const result = getDraggingPanelBoundingRects(draggingPanels);
 
-      expect(result).toBeNull();
-    });
+    expect(result).toBeNull();
+  });
 
-    it('returns null if draggingPanels array is empty', () => {
-      const draggingPanels = [] as any[];
-      const result = getDraggingPanelBoundingRects(draggingPanels);
-
-      expect(result).toBeNull();
-    });
-
-    it('returns null if draggingPanels is null or undefined', () => {
-      expect(getDraggingPanelBoundingRects(undefined as any)).toBeNull();
-      expect(getDraggingPanelBoundingRects(null as any)).toBeNull();
-    });
+  it('returns null if draggingPanels is null or undefined', () => {
+    expect(getDraggingPanelBoundingRects(undefined as any)).toBeNull();
+    expect(getDraggingPanelBoundingRects(null as any)).toBeNull();
   });
 });
