4bc81ef95830fe5af27e501ddc5ecc7944d70a9d
Sergio Bur <sjbur.dev@gmail.com>
2025-10-30 13:39:53 +0100

    feat: add no subject title

--- packages/devextreme/js/__internal/scheduler/m_scheduler.ts
+++ packages/devextreme/js/__internal/scheduler/m_scheduler.ts
@@ -2028,7 +2028,7 @@ class Scheduler extends SchedulerOptionsBaseWidget {
       rawAppointment,
       this._dataAccessors,
     );
-    appointment.text = appointment.text || '';
+    appointment.text = appointment.text || messageLocalization.format('dxScheduler-noSubject');
 
     const serializedAppointment = appointment.serialize().source;
 
