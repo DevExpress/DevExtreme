c768957754cb2cfe52faf446885dbff390cbcce2
Danil Mirgaev <danil.mirgaev@devexpress.com>
2025-10-31 16:33:19 +0400

    fix(FilterBuilder Demo): convert to js

--- apps/demos/Demos/FilterBuilder/WithDataGrid/ReactJs/data.js
+++ apps/demos/Demos/FilterBuilder/WithDataGrid/ReactJs/data.js
@@ -3,68 +3,75 @@ export const filter = [
   'or',
   [['Product_Name', 'contains', 'HD'], 'and', ['Product_Cost', '<', 200]],
 ];
-
 export const fields = [
   {
     caption: 'ID',
     dataField: 'Product_ID',
     dataType: 'number',
-  }, {
+  },
+  {
     dataField: 'Product_Name',
     dataType: 'string',
-  }, {
+  },
+  {
     caption: 'Cost',
     dataField: 'Product_Cost',
     dataType: 'number',
     format: 'currency',
-  }, {
+  },
+  {
     dataField: 'Product_Sale_Price',
     caption: 'Sale Price',
     dataType: 'number',
     format: 'currency',
-  }, {
+  },
+  {
     dataField: 'Product_Retail_Price',
     caption: 'Retail Price',
     dataType: 'number',
     format: 'currency',
-  }, {
+  },
+  {
     dataField: 'Product_Current_Inventory',
     dataType: 'number',
     caption: 'Inventory',
   },
 ];
-
 export const columns = [
   {
     caption: 'ID',
     dataField: 'Product_ID',
     dataType: 'number',
     width: 50,
-  }, {
+  },
+  {
     dataField: 'Product_Name',
     dataType: 'string',
-  }, {
+  },
+  {
     caption: 'Cost',
     dataField: 'Product_Cost',
     dataType: 'number',
     format: 'currency',
-  }, {
+  },
+  {
     dataField: 'Product_Sale_Price',
     caption: 'Sale Price',
     dataType: 'number',
     format: 'currency',
-  }, {
+  },
+  {
     dataField: 'Product_Retail_Price',
     caption: 'Retail Price',
     dataType: 'number',
     format: 'currency',
-  }, {
+  },
+  {
     dataField: 'Product_Current_Inventory',
     dataType: 'number',
     caption: 'Inventory',
   },
 ];
-
 export const products = [
   {
     Product_ID: 1,
