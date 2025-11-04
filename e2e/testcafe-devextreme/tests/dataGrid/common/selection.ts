5236234c260bdfacd1af9ce5b49ac3a5e85e7866
Julia Volkova <iuliia.volkova@devexpress.com>
2025-11-04 12:53:05 +0200

    change number

--- e2e/testcafe-devextreme/tests/dataGrid/common/selection.ts
+++ e2e/testcafe-devextreme/tests/dataGrid/common/selection.ts
@@ -96,7 +96,7 @@ test('The Select All checkbox should be visible when a column headerCellTemplate
 test('Select rows by shift should work when grid has real time updates', async (t) => {
   const dataGrid = new DataGrid('#container');
   const secondRow = dataGrid.getDataRow(1);
-  const seventhRow = dataGrid.getDataRow(6);
+  const fifthRow = dataGrid.getDataRow(4);
   const checkRowSelectionStates = async (startRowIndex: number, endRowIndex: number) => {
     for (let i = startRowIndex; i <= endRowIndex; i += 1) {
       await t
@@ -116,10 +116,10 @@ test('Select rows by shift should work when grid has real time updates', async (
     .eql('test123');
 
   // act
-  await t.click(seventhRow.element, { modifiers: { shift: true } });
+  await t.click(fifthRow.element, { modifiers: { shift: true } });
 
   // assert
-  await checkRowSelectionStates(1, 6);
+  await checkRowSelectionStates(1, 4);
 }).before(async () => createWidget('dxDataGrid', {
   dataSource: {
     store: {
