7d55ff8e377aec0943d3363986f6520b8c027877
andrei.kharitonov <pharret31@gmail.com>
2025-11-04 14:24:50 +0100

    Gantt: fix ts issues (ui.gantt.js)

---
+++ packages/devextreme/js/ui/diagram/ui.diagram.js
@@ -0,0 +1,34 @@
+import Diagram from '../../__internal/ui/diagram/ui.diagram';
+
+export default Diagram;
+
+// STYLE diagram
+
+/**
+ * @name dxDiagramOptions.accessKey
+ * @hidden true
+ */
+/**
+ * @name dxDiagramOptions.activeStateEnabled
+ * @hidden true
+ */
+/**
+ * @name dxDiagramOptions.focusStateEnabled
+ * @hidden true
+ */
+/**
+ * @name dxDiagramOptions.hint
+ * @hidden true
+ */
+/**
+ * @name dxDiagramOptions.hoverStateEnabled
+ * @hidden true
+ */
+/**
+ * @name dxDiagramOptions.tabIndex
+ * @hidden true
+ */
+/**
+ * @name dxDiagram.registerKeyHandler(key, handler)
+ * @hidden true
+ */
