3d1c6edab3ec15ac99fdc93719f8cf899add5dd2
Sergio Bur <sjbur.dev@gmail.com>
2025-11-05 11:35:13 +0100

    fix: fix settings button display in material theme

--- packages/devextreme/js/__internal/scheduler/appointment_popup/m_form.ts
+++ packages/devextreme/js/__internal/scheduler/appointment_popup/m_form.ts
@@ -859,7 +859,7 @@ export class AppointmentForm {
             this.showRecurrenceGroup();
           },
           elementAttr: {
-            class: CLASSES.recurrenceSettingsButton,
+            class: `${CLASSES.recurrenceSettingsButton} dx-shape-standard`,
           },
         },
       });
