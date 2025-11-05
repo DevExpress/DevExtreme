f36c37e8eebf1a750c5ccf700663d5f296b59738
andrei.kharitonov <pharret31@gmail.com>
2025-11-05 18:53:45 +0100

    Gantt: fix ts issues (all files except ui.gantt.js)

--- packages/devextreme/js/__internal/ui/gantt/ui.gantt.data_changes_processing_helper.ts
+++ packages/devextreme/js/__internal/ui/gantt/ui.gantt.data_changes_processing_helper.ts
@@ -1,43 +1,66 @@
 export class GanttDataChangesProcessingHelper {
-    constructor() {
-        this._waitingForGanttViewReady = false;
-        this._waitingForTreeListReady = false;
-        this._completionActions = [];
+  _waitingForGanttViewReady: boolean;
+
+  _waitingForTreeListReady: boolean;
+
+  _completionActions: (() => void)[];
+
+  constructor() {
+    this._waitingForGanttViewReady = false;
+    this._waitingForTreeListReady = false;
+    this._completionActions = [];
+  }
+
+  onGanttViewReady(): void {
+    this._stopWaitForGanttViewReady();
+    this.executeActionsIfPossible();
+  }
+
+  onTreeListReady(): void {
+    this._stopWaitForTreeListReady();
+    this.executeActionsIfPossible();
+  }
+
+  addCompletionAction(
+    action: () => void,
+    waitGanttViewReady: boolean,
+    waitTreeListReady: boolean,
+  ): void {
+    if (action) {
+      if (waitGanttViewReady) {
+        this._startWaitForGanttViewReady();
+      }
+      if (waitTreeListReady) {
+        this._startWaitForTreeListReady();
+      }
+      this._completionActions.push(action);
     }
-    onGanttViewReady() {
-        this._stopWaitForGanttViewReady();
-        this.executeActionsIfPossible();
+  }
+
+  executeActionsIfPossible(): void {
+    if (this._canExecuteActions()) {
+      this._completionActions.forEach((act) => act());
+      this._completionActions = [];
     }
-    onTreeListReady() {
-        this._stopWaitForTreeListReady();
-        this.executeActionsIfPossible();
-    }
-    addCompletionAction(action, waitGanttViewReady, waitTreeListReady) {
-        if(action) {
-            if(waitGanttViewReady) { this._startWaitForGanttViewReady(); }
-            if(waitTreeListReady) { this._startWaitForTreeListReady(); }
-            this._completionActions.push(action);
-        }
-    }
-    executeActionsIfPossible() {
-        if(this._canExecuteActions()) {
-            this._completionActions.forEach(act => act());
-            this._completionActions = [];
-        }
-    }
-    _startWaitForGanttViewReady() {
-        this._waitingForGanttViewReady = true;
-    }
-    _stopWaitForGanttViewReady() {
-        this._waitingForGanttViewReady = false;
-    }
-    _startWaitForTreeListReady() {
-        this._waitingForTreeListReady = true;
-    }
-    _stopWaitForTreeListReady() {
-        this._waitingForTreeListReady = false;
-    }
-    _canExecuteActions() {
-        return !(this._waitingForGanttViewReady || this._waitingForTreeListReady);
-    }
+  }
+
+  _startWaitForGanttViewReady(): void {
+    this._waitingForGanttViewReady = true;
+  }
+
+  _stopWaitForGanttViewReady(): void {
+    this._waitingForGanttViewReady = false;
+  }
+
+  _startWaitForTreeListReady(): void {
+    this._waitingForTreeListReady = true;
+  }
+
+  _stopWaitForTreeListReady(): void {
+    this._waitingForTreeListReady = false;
+  }
+
+  _canExecuteActions(): boolean {
+    return !(this._waitingForGanttViewReady || this._waitingForTreeListReady);
+  }
 }
