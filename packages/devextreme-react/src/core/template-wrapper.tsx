80192b596f039624de4789bebb302ad37de06d11
Vasily Strelyaev <VasilyStrelyaev@users.noreply.github.com>
2025-11-06 15:00:37 +0200

    fix the second case of two-template-roots regression (T1311798) (#31591)

--- packages/devextreme-react/src/core/template-wrapper.tsx
+++ packages/devextreme-react/src/core/template-wrapper.tsx
@@ -66,10 +66,7 @@ const TemplateWrapperComponent: FC<TemplateWrapperProps> = ({
       removalListenerElement.current,
     ].forEach((el) => el && events.off(el, DX_REMOVE_EVENT, onTemplateRemoved));
 
-    // In case of multiple root elements, letting the widget remove them all sync
-    Promise.resolve().then(() => {
-      onRemoved(componentKey);
-    });
+    onRemoved(componentKey);
   }, [onRemoved]);
 
   useLayoutEffect(() => {
