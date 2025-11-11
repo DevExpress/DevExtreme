a88124baf02b49cdab11e2c32d0a56491d09ff58
ksercs <ksercs0@gmail.com>
2025-11-11 16:44:20 +0400

    FileUploader: revert maxWidth calculation update

--- packages/devextreme/js/__internal/ui/file_uploader/file_uploader.ts
+++ packages/devextreme/js/__internal/ui/file_uploader/file_uploader.ts
@@ -73,8 +73,6 @@ const FILEUPLOADER_INVALID_CLASS = 'dx-fileuploader-invalid';
 
 const FILEUPLOADER_AFTER_LOAD_DELAY = 400;
 const DRAG_EVENT_DELTA = 1;
-const GAP = 10;
-const REFERENCE_TEXT = '1023 bytes';
 
 const DIALOG_TRIGGER_EVENT_NAMESPACE = 'dxFileUploaderDialogTrigger';
 
@@ -804,12 +802,11 @@ class FileUploader extends Editor<FileUploaderProperties> {
     const iconWidth = _showFileIcon ? getOuterWidth($icon) : 0;
 
     const prevFileSize = $fileSize?.text();
-    $fileSize?.text(REFERENCE_TEXT);
+    $fileSize?.text('1000 Mb');
     const fileSizeWidth = getWidth($fileSize);
     $fileSize?.text(prevFileSize ?? '');
 
-    const maxWidth = filesContainerWidth - buttonsWidth - fileSizeWidth - iconWidth - GAP;
-
+    const maxWidth = filesContainerWidth - buttonsWidth - fileSizeWidth - iconWidth;
     this._$filesContainer?.find(`.${FILEUPLOADER_FILE_NAME_CLASS}`).css('maxWidth', maxWidth);
   }
 
