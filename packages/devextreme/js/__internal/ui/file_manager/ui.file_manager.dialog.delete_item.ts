40580dffb72bc3c638e7677aa709ce1d831f57c9
pharret31 <pharret31@gmail.com>
2025-11-13 19:54:27 +0100

    FileManager: fix ts issues (ui.file_manager.dialog.*, ui.file_manager.context_menu.ts, ui.file_manager.editing.ts)

--- packages/devextreme/js/__internal/ui/file_manager/ui.file_manager.dialog.delete_item.ts
+++ packages/devextreme/js/__internal/ui/file_manager/ui.file_manager.dialog.delete_item.ts
@@ -1,57 +1,75 @@
-import $ from '../../core/renderer';
-import { extend } from '../../core/utils/extend';
-
-import messageLocalization from '../../common/core/localization/message';
-
-import ScrollView from '../scroll_view';
-import FileManagerDialogBase from './ui.file_manager.dialog';
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import messageLocalization from '@js/common/core/localization/message';
+import type { dxElementWrapper } from '@js/core/renderer';
+import $ from '@js/core/renderer';
+import ScrollView from '@js/ui/scroll_view';
+import type { DialogOptions } from '@ts/ui/file_manager/ui.file_manager.dialog';
+import FileManagerDialogBase from '@ts/ui/file_manager/ui.file_manager.dialog';
 
 const FILE_MANAGER_DIALOG_DELETE_ITEM = 'dx-filemanager-dialog-delete-item';
 const FILE_MANAGER_DIALOG_DELETE_ITEM_POPUP = 'dx-filemanager-dialog-delete-item-popup'; // TODO ensure needed
 
 class FileManagerDeleteItemDialog extends FileManagerDialogBase {
+  _$text?: dxElementWrapper;
 
-    show({ itemName, itemCount }) {
-        const text = itemCount === 1
-            ? messageLocalization.format('dxFileManager-dialogDeleteItemSingleItemConfirmation', itemName)
-            : messageLocalization.format('dxFileManager-dialogDeleteItemMultipleItemsConfirmation', itemCount);
+  _initialText?: string;
 
-        if(this._$text) {
-            this._$text.text(text);
-        } else {
-            this._initialText = text;
-        }
+  // @ts-expect-error ts-error
+  show({ itemName, itemCount }): void {
+    const text = itemCount === 1
+      ? messageLocalization.format(
+        'dxFileManager-dialogDeleteItemSingleItemConfirmation',
+        // @ts-expect-error ts-error
+        itemName,
+      )
+      : messageLocalization.format(
+        'dxFileManager-dialogDeleteItemMultipleItemsConfirmation',
+        // @ts-expect-error ts-error
+        itemCount,
+      );
 
-        super.show();
+    if (this._$text) {
+      this._$text.text(text);
+    } else {
+      this._initialText = text;
     }
 
-    _getDialogOptions() {
-        return extend(super._getDialogOptions(), {
-            title: messageLocalization.format('dxFileManager-dialogDeleteItemTitle'),
-            buttonText: messageLocalization.format('dxFileManager-dialogDeleteItemButtonText'),
-            contentCssClass: FILE_MANAGER_DIALOG_DELETE_ITEM,
-            popupCssClass: FILE_MANAGER_DIALOG_DELETE_ITEM_POPUP,
-            height: 'auto',
-            maxHeight: '80vh'
-        });
-    }
+    super.show();
+  }
 
-    _createContentTemplate(element) {
-        super._createContentTemplate(element);
+  _getDialogOptions(): DialogOptions {
+    return {
+      ...super._getDialogOptions(),
+      title: messageLocalization.format('dxFileManager-dialogDeleteItemTitle'),
+      buttonText: messageLocalization.format(
+        'dxFileManager-dialogDeleteItemButtonText',
+      ),
+      contentCssClass: FILE_MANAGER_DIALOG_DELETE_ITEM,
+      popupCssClass: FILE_MANAGER_DIALOG_DELETE_ITEM_POPUP,
+      height: 'auto',
+      maxHeight: '80vh',
+    };
+  }
 
-        this._$text = $('<div>')
-            .text(this._initialText)
-            .appendTo(this._$contentElement);
+  _createContentTemplate(element: dxElementWrapper): void {
+    super._createContentTemplate(element);
 
-        this._createComponent(this._$contentElement, ScrollView, {
-            width: '100%',
-            height: '100%'
-        });
-    }
+    this._$text = $('<div>')
+      // @ts-expect-error ts-error
+      .text(this._initialText)
+      .appendTo(this._$contentElement);
 
-    _getDialogResult() {
-        return {};
-    }
+    this._createComponent(this._$contentElement, ScrollView, {
+      width: '100%',
+      height: '100%',
+    });
+  }
+
+  // @ts-expect-error ts-error
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  _getDialogResult() {
+    return {};
+  }
 }
 
 export default FileManagerDeleteItemDialog;
--- packages/devextreme/js/__internal/ui/file_manager/ui.file_manager.dialog.folder_chooser.ts
+++ packages/devextreme/js/__internal/ui/file_manager/ui.file_manager.dialog.folder_chooser.ts
@@ -1,128 +1,171 @@
-import $ from '../../core/renderer';
-import { extend } from '../../core/utils/extend';
-
-import messageLocalization from '../../common/core/localization/message';
-import { getMapFromObject } from './ui.file_manager.common';
-
-import FileManagerDialogBase from './ui.file_manager.dialog';
-import FileManagerFilesTreeView from './ui.file_manager.files_tree_view';
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import messageLocalization from '@js/common/core/localization/message';
+import $, { type dxElementWrapper } from '@js/core/renderer';
+import { extend } from '@js/core/utils/extend';
+import { getMapFromObject } from '@js/ui/file_manager/ui.file_manager.common';
+import FileManagerFilesTreeView from '@js/ui/file_manager/ui.file_manager.files_tree_view';
+import type { DialogOptions } from '@ts/ui/file_manager/ui.file_manager.dialog';
+import FileManagerDialogBase from '@ts/ui/file_manager/ui.file_manager.dialog';
 
 const FILE_MANAGER_DIALOG_FOLDER_CHOOSER = 'dx-filemanager-dialog-folder-chooser';
 const FILE_MANAGER_DIALOG_FOLDER_CHOOSER_POPUP = 'dx-filemanager-dialog-folder-chooser-popup';
 
 class FileManagerFolderChooserDialog extends FileManagerDialogBase {
+  _filesTreeView?: FileManagerFilesTreeView;
 
-    show() {
-        this._setSelectedDirInfo(null);
-        this._filesTreeView?.refresh();
-        super.show();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  _targetItemInfos?: any;
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  _selectedDirectoryInfo?: any;
+
+  show(): void {
+    this._setSelectedDirInfo(null);
+    this._filesTreeView?.refresh();
+    super.show();
+  }
+
+  switchToCopyDialog(targetItemInfos): void {
+    this._targetItemInfos = targetItemInfos;
+    this._setTitle(
+      messageLocalization.format(
+        'dxFileManager-dialogDirectoryChooserCopyTitle',
+      ),
+    );
+    this._setApplyButtonOptions({
+      text: messageLocalization.format(
+        'dxFileManager-dialogDirectoryChooserCopyButtonText',
+      ),
+      disabled: true,
+    });
+  }
+
+  switchToMoveDialog(targetItemInfos): void {
+    this._targetItemInfos = targetItemInfos;
+    this._setTitle(
+      messageLocalization.format(
+        'dxFileManager-dialogDirectoryChooserMoveTitle',
+      ),
+    );
+    this._setApplyButtonOptions({
+      text: messageLocalization.format(
+        'dxFileManager-dialogDirectoryChooserMoveButtonText',
+      ),
+      disabled: true,
+    });
+  }
+
+  _getDialogOptions(): DialogOptions {
+    return {
+      ...super._getDialogOptions(),
+      contentCssClass: FILE_MANAGER_DIALOG_FOLDER_CHOOSER,
+      popupCssClass: FILE_MANAGER_DIALOG_FOLDER_CHOOSER_POPUP,
+    };
+  }
+
+  _createContentTemplate(element: dxElementWrapper): void {
+    super._createContentTemplate(element);
+
+    // @ts-expect-error ts-error
+    const { getDirectories } = this.option();
+
+    this._filesTreeView = this._createComponent(
+      $('<div>'),
+      FileManagerFilesTreeView,
+      {
+        getDirectories,
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        getCurrentDirectory: () => this._getDialogSelectedDirectory(),
+        onDirectoryClick: (e): void => this._onFilesTreeViewDirectoryClick(e),
+        onFilesTreeViewContentReady: (): void => this._toggleUnavailableLocationsDisabled(true),
+      },
+    );
+
+    this._$contentElement?.append($(this._filesTreeView?.$element()));
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  _getDialogResult() {
+    const result = this._getDialogSelectedDirectory();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return result ? { folder: result } : result;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  _getDefaultOptions() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return extend(super._getDefaultOptions(), {
+      getItems: null,
+    });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  _getDialogSelectedDirectory() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return this._selectedDirectoryInfo;
+  }
+
+  _onFilesTreeViewDirectoryClick({ itemData }): void {
+    this._setSelectedDirInfo(itemData);
+    this._filesTreeView?.updateCurrentDirectory();
+  }
+
+  _setSelectedDirInfo(dirInfo): void {
+    this._selectedDirectoryInfo = dirInfo;
+    this._setApplyButtonOptions({ disabled: !dirInfo });
+  }
+
+  _onPopupShown(): void {
+    this._toggleUnavailableLocationsDisabled(true);
+    super._onPopupShown();
+  }
+
+  _onPopupHiding(): void {
+    this._toggleUnavailableLocationsDisabled(false);
+    super._onPopupHiding();
+  }
+
+  _toggleUnavailableLocationsDisabled(isDisabled: boolean): void {
+    if (!this._filesTreeView) {
+      return;
     }
+    const locations = this._getLocationsToProcess(isDisabled);
+    this._filesTreeView
+      .toggleDirectoryExpandedStateRecursive(
+        locations.locationsToExpand[0],
+        isDisabled,
+      )
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      .then(() => this._filesTreeView?.toggleDirectoryLineExpandedState(
+        locations.locationsToCollapse,
+        !isDisabled,
+      )
+        .then((): void => locations.locationKeysToDisable
+          .forEach((key): void => this._filesTreeView?.toggleNodeDisabledState(key, isDisabled))));
+  }
 
-    switchToCopyDialog(targetItemInfos) {
-        this._targetItemInfos = targetItemInfos;
-        this._setTitle(messageLocalization.format('dxFileManager-dialogDirectoryChooserCopyTitle'));
-        this._setApplyButtonOptions({
-            text: messageLocalization.format('dxFileManager-dialogDirectoryChooserCopyButtonText'),
-            disabled: true
-        });
-    }
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  _getLocationsToProcess(isDisabled: boolean) {
+    const expandLocations = {};
+    const collapseLocations = {};
+    this._targetItemInfos.forEach((itemInfo): void => {
+      if (itemInfo.parentDirectory) {
+        expandLocations[itemInfo.parentDirectory.getInternalKey()] = itemInfo.parentDirectory;
+      }
+      if (itemInfo.fileItem.isDirectory) {
+        collapseLocations[itemInfo.getInternalKey()] = itemInfo;
+      }
+    });
 
-    switchToMoveDialog(targetItemInfos) {
-        this._targetItemInfos = targetItemInfos;
-        this._setTitle(messageLocalization.format('dxFileManager-dialogDirectoryChooserMoveTitle'));
-        this._setApplyButtonOptions({
-            text: messageLocalization.format('dxFileManager-dialogDirectoryChooserMoveButtonText'),
-            disabled: true
-        });
-    }
+    const expandMap = getMapFromObject(expandLocations);
+    const collapseMap = getMapFromObject(collapseLocations);
 
-    _getDialogOptions() {
-        return extend(super._getDialogOptions(), {
-            contentCssClass: FILE_MANAGER_DIALOG_FOLDER_CHOOSER,
-            popupCssClass: FILE_MANAGER_DIALOG_FOLDER_CHOOSER_POPUP
-        });
-    }
-
-    _createContentTemplate(element) {
-        super._createContentTemplate(element);
-
-        this._filesTreeView = this._createComponent($('<div>'), FileManagerFilesTreeView, {
-            getDirectories: this.option('getDirectories'),
-            getCurrentDirectory: () => this._getDialogSelectedDirectory(),
-            onDirectoryClick: (e) => this._onFilesTreeViewDirectoryClick(e),
-            onFilesTreeViewContentReady: () => this._toggleUnavailableLocationsDisabled(true)
-        });
-
-        this._$contentElement.append(this._filesTreeView.$element());
-    }
-
-    _getDialogResult() {
-        const result = this._getDialogSelectedDirectory();
-        return result ? { folder: result } : result;
-    }
-
-    _getDefaultOptions() {
-        return extend(super._getDefaultOptions(), {
-            getItems: null
-        });
-    }
-
-    _getDialogSelectedDirectory() {
-        return this._selectedDirectoryInfo;
-    }
-
-    _onFilesTreeViewDirectoryClick({ itemData }) {
-        this._setSelectedDirInfo(itemData);
-        this._filesTreeView.updateCurrentDirectory();
-    }
-
-    _setSelectedDirInfo(dirInfo) {
-        this._selectedDirectoryInfo = dirInfo;
-        this._setApplyButtonOptions({ disabled: !dirInfo });
-    }
-
-    _onPopupShown() {
-        this._toggleUnavailableLocationsDisabled(true);
-        super._onPopupShown();
-    }
-
-    _onPopupHiding() {
-        this._toggleUnavailableLocationsDisabled(false);
-        super._onPopupHiding();
-    }
-
-    _toggleUnavailableLocationsDisabled(isDisabled) {
-        if(!this._filesTreeView) {
-            return;
-        }
-        const locations = this._getLocationsToProcess(isDisabled);
-        this._filesTreeView.toggleDirectoryExpandedStateRecursive(locations.locationsToExpand[0], isDisabled)
-            .then(() => this._filesTreeView.toggleDirectoryLineExpandedState(locations.locationsToCollapse, !isDisabled)
-                .then(() => locations.locationKeysToDisable.forEach(key => this._filesTreeView.toggleNodeDisabledState(key, isDisabled))));
-    }
-
-    _getLocationsToProcess(isDisabled) {
-        const expandLocations = {};
-        const collapseLocations = {};
-        this._targetItemInfos.forEach(itemInfo => {
-            if(itemInfo.parentDirectory) {
-                expandLocations[itemInfo.parentDirectory.getInternalKey()] = itemInfo.parentDirectory;
-            }
-            if(itemInfo.fileItem.isDirectory) {
-                collapseLocations[itemInfo.getInternalKey()] = itemInfo;
-            }
-        });
-
-        const expandMap = getMapFromObject(expandLocations);
-        const collapseMap = getMapFromObject(collapseLocations);
-
-        return {
-            locationsToExpand: isDisabled ? expandMap.values : [],
-            locationsToCollapse: isDisabled ? collapseMap.values : [],
-            locationKeysToDisable: expandMap.keys.concat(...collapseMap.keys)
-        };
-    }
-
+    return {
+      locationsToExpand: isDisabled ? expandMap.values : [],
+      locationsToCollapse: isDisabled ? collapseMap.values : [],
+      locationKeysToDisable: expandMap.keys.concat(...collapseMap.keys),
+    };
+  }
 }
 
 export default FileManagerFolderChooserDialog;
