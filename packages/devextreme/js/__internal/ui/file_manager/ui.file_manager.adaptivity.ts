576aaaa74a92d04491ef3313017dc762aa6b8159
pharret31 <pharret31@gmail.com>
2025-11-13 12:50:44 +0100

    FileManager: fix ts issues (ui.file_manager.* adaptivity,breadcrumbs,command_manager,toolbar)

--- packages/devextreme/js/__internal/ui/file_manager/ui.file_manager.adaptivity.ts
+++ packages/devextreme/js/__internal/ui/file_manager/ui.file_manager.adaptivity.ts
@@ -1,12 +1,13 @@
-import { getWidth } from '../../core/utils/size';
-import $ from '../../core/renderer';
-import { extend } from '../../core/utils/extend';
-import { isFunction } from '../../core/utils/type';
-import { getWindow, hasWindow } from '../../core/utils/window';
-
-import Widget from '../widget/ui.widget';
-import Drawer from '../drawer';
-import SplitterControl from '../splitter_control';
+import type { dxElementWrapper } from '@js/core/renderer';
+import $ from '@js/core/renderer';
+import { getWidth } from '@js/core/utils/size';
+import { isFunction } from '@js/core/utils/type';
+import { getWindow, hasWindow } from '@js/core/utils/window';
+import SplitterControl from '@js/ui/splitter_control';
+import type { OptionChanged } from '@ts/core/widget/types';
+import type { WidgetProperties } from '@ts/core/widget/widget';
+import Widget from '@ts/core/widget/widget';
+import Drawer from '@ts/ui/drawer/drawer';
 
 const window = getWindow();
 const ADAPTIVE_STATE_SCREEN_WIDTH = 573;
@@ -15,159 +16,193 @@ const FILE_MANAGER_ADAPTIVITY_DRAWER_PANEL_CLASS = 'dx-filemanager-adaptivity-dr
 const DRAWER_PANEL_CONTENT_INITIAL = 'dx-drawer-panel-content-initial';
 const DRAWER_PANEL_CONTENT_ADAPTIVE = 'dx-drawer-panel-content-adaptive';
 
-class FileManagerAdaptivityControl extends Widget {
+interface PanelSize {
+  leftPanelWidth: string | number;
+  rightPanelWidth: string | number;
+}
 
-    _initMarkup() {
-        super._initMarkup();
+interface FileManagerAdaptivityControlOptions extends WidgetProperties {
+  drawerTemplate?: (container: dxElementWrapper | Element) => void;
+  contentTemplate?: (container: dxElementWrapper) => void;
+  onAdaptiveStateChanged?: (e: { enabled: boolean }) => void;
+}
 
-        this._initActions();
+class FileManagerAdaptivityControl extends Widget<FileManagerAdaptivityControlOptions> {
+  _isInAdaptiveState?: boolean;
 
-        this._isInAdaptiveState = false;
+  _drawer?: Drawer;
 
-        const $drawer = $('<div>').appendTo(this.$element());
+  _splitter?: SplitterControl;
 
-        $('<div>')
-            .addClass(FILE_MANAGER_ADAPTIVITY_DRAWER_PANEL_CLASS)
-            .appendTo($drawer);
+  _actions!: { onAdaptiveStateChanged?: ({ enabled }) => void };
 
-        this._drawer = this._createComponent($drawer, Drawer);
-        this._drawer.option({
-            opened: true,
-            template: this._createDrawerTemplate.bind(this)
-        });
-        $(this._drawer.content()).addClass(DRAWER_PANEL_CONTENT_INITIAL);
+  _initMarkup(): void {
+    super._initMarkup();
 
-        const $drawerContent = $drawer.find(`.${FILE_MANAGER_ADAPTIVITY_DRAWER_PANEL_CLASS}`).first();
+    this._initActions();
 
-        const contentRenderer = this.option('contentTemplate');
-        if(isFunction(contentRenderer)) {
-            contentRenderer($drawerContent);
-        }
-        this._updateDrawerMaxSize();
+    this._isInAdaptiveState = false;
+
+    const $drawer = $('<div>').appendTo(this.$element());
+
+    $('<div>')
+      .addClass(FILE_MANAGER_ADAPTIVITY_DRAWER_PANEL_CLASS)
+      .appendTo($drawer);
+
+    // @ts-expect-error ts-error
+    this._drawer = this._createComponent($drawer, Drawer);
+    this._drawer.option({
+      opened: true,
+      template: this._createDrawerTemplate.bind(this),
+    });
+    $(this._drawer.content()).addClass(DRAWER_PANEL_CONTENT_INITIAL);
+
+    const $drawerContent = $drawer
+      .find(`.${FILE_MANAGER_ADAPTIVITY_DRAWER_PANEL_CLASS}`)
+      .first();
+
+    const { contentTemplate: contentRenderer } = this.option();
+    if (isFunction(contentRenderer)) {
+      contentRenderer($drawerContent);
+    }
+    this._updateDrawerMaxSize();
+  }
+
+  _createDrawerTemplate(container: HTMLElement | dxElementWrapper | Element): void {
+    const { drawerTemplate } = this.option();
+    drawerTemplate?.(container);
+    this._splitter = this._createComponent('<div>', SplitterControl, {
+      container: this.$element(),
+      leftElement: $(this._drawer?.content()),
+      rightElement: $(this._drawer?.viewContent()),
+      onApplyPanelSize: this._onApplyPanelSize.bind(this),
+      onActiveStateChanged: this._onActiveStateChanged.bind(this),
+    });
+    this._splitter.$element().appendTo(container);
+    this._splitter.disableSplitterCalculation(true);
+  }
+
+  _render(): void {
+    super._render();
+    this._checkAdaptiveState();
+  }
+
+  _onApplyPanelSize(e: PanelSize): void {
+    if (!hasWindow()) {
+      return;
     }
 
-    _createDrawerTemplate(container) {
-        this.option('drawerTemplate')(container);
-        this._splitter = this._createComponent('<div>', SplitterControl, {
-            container: this.$element(),
-            leftElement: $(this._drawer.content()),
-            rightElement: $(this._drawer.viewContent()),
-            onApplyPanelSize: this._onApplyPanelSize.bind(this),
-            onActiveStateChanged: this._onActiveStateChanged.bind(this)
-        });
-        this._splitter.$element().appendTo(container);
-        this._splitter.disableSplitterCalculation(true);
+    if (!this._splitter?.isSplitterMoved()) {
+      this._setDrawerWidth('');
+      return;
     }
+    $(this._drawer?.content()).removeClass(DRAWER_PANEL_CONTENT_INITIAL);
+    this._setDrawerWidth(e.leftPanelWidth);
+  }
 
-    _render() {
-        super._render();
-        this._checkAdaptiveState();
+  _onActiveStateChanged({ isActive }: { isActive: boolean }): void {
+    this._splitter?.disableSplitterCalculation(!isActive);
+    if (!isActive) {
+      this._splitter?.$element().css('left', 'auto');
     }
+  }
 
-    _onApplyPanelSize(e) {
-        if(!hasWindow()) {
-            return;
-        }
+  _setDrawerWidth(width: string | number): void {
+    $(this._drawer?.content()).css('width', width);
+    this._updateDrawerMaxSize();
+    this._drawer?.resizeViewContent();
+  }
 
-        if(!this._splitter.isSplitterMoved()) {
-            this._setDrawerWidth('');
-            return;
-        }
-        $(this._drawer.content()).removeClass(DRAWER_PANEL_CONTENT_INITIAL);
-        this._setDrawerWidth(e.leftPanelWidth);
+  _updateDrawerMaxSize(): void {
+    this._drawer?.option('maxSize', this._drawer.getRealPanelWidth());
+  }
+
+  // @ts-expect-error ts-error
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  _dimensionChanged(dimension): void {
+    if (!dimension || dimension !== 'height') {
+      this._checkAdaptiveState();
     }
+  }
 
-    _onActiveStateChanged({ isActive }) {
-        this._splitter.disableSplitterCalculation(!isActive);
-        !isActive && this._splitter.$element().css('left', 'auto');
+  _checkAdaptiveState(): void {
+    const oldState = this._isInAdaptiveState;
+    this._isInAdaptiveState = this._isSmallScreen();
+    if (oldState !== this._isInAdaptiveState) {
+      this.toggleDrawer(!this._isInAdaptiveState, true);
+      $(this._drawer?.content()).toggleClass(
+        DRAWER_PANEL_CONTENT_ADAPTIVE,
+        this._isInAdaptiveState,
+      );
+      this._raiseAdaptiveStateChanged(this._isInAdaptiveState);
     }
-
-    _setDrawerWidth(width) {
-        $(this._drawer.content()).css('width', width);
-        this._updateDrawerMaxSize();
-        this._drawer.resizeViewContent();
+    if (this._isInAdaptiveState && this._isDrawerOpened()) {
+      this._updateDrawerMaxSize();
     }
+  }
 
-    _updateDrawerMaxSize() {
-        this._drawer.option('maxSize', this._drawer.getRealPanelWidth());
+  _isSmallScreen(): boolean {
+    return getWidth(window) <= ADAPTIVE_STATE_SCREEN_WIDTH;
+  }
+
+  _isDrawerOpened(): boolean | undefined {
+    const { opened } = this._drawer?.option() ?? {};
+    return opened;
+  }
+
+  _initActions(): void {
+    this._actions = {
+      onAdaptiveStateChanged: this._createActionByOption(
+        'onAdaptiveStateChanged',
+      ),
+    };
+  }
+
+  _raiseAdaptiveStateChanged(enabled: boolean): void {
+    this._actions?.onAdaptiveStateChanged?.({ enabled });
+  }
+
+  _getDefaultOptions(): FileManagerAdaptivityControlOptions {
+    return {
+      ...super._getDefaultOptions(),
+      drawerTemplate: undefined,
+      contentTemplate: undefined,
+      onAdaptiveStateChanged: undefined,
+    };
+  }
+
+  _optionChanged(args: OptionChanged<FileManagerAdaptivityControlOptions>): void {
+    const { name } = args;
+
+    switch (name) {
+      case 'drawerTemplate':
+      case 'contentTemplate':
+        this.repaint();
+        break;
+      case 'onAdaptiveStateChanged':
+        this._actions[name] = this._createActionByOption(name);
+        break;
+      default:
+        super._optionChanged(args);
     }
+  }
 
-    _dimensionChanged(dimension) {
-        if(!dimension || dimension !== 'height') {
-            this._checkAdaptiveState();
-        }
-    }
+  isInAdaptiveState(): boolean | undefined {
+    return this._isInAdaptiveState;
+  }
 
-    _checkAdaptiveState() {
-        const oldState = this._isInAdaptiveState;
-        this._isInAdaptiveState = this._isSmallScreen();
-        if(oldState !== this._isInAdaptiveState) {
-            this.toggleDrawer(!this._isInAdaptiveState, true);
-            $(this._drawer.content()).toggleClass(DRAWER_PANEL_CONTENT_ADAPTIVE, this._isInAdaptiveState);
-            this._raiseAdaptiveStateChanged(this._isInAdaptiveState);
-        }
-        if(this._isInAdaptiveState && this._isDrawerOpened()) {
-            this._updateDrawerMaxSize();
-        }
-    }
+  toggleDrawer(showing?: boolean, skipAnimation?: boolean): void {
+    this._updateDrawerMaxSize();
+    this._drawer?.option('animationEnabled', !skipAnimation);
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    this._drawer?.toggle(showing);
+    const isSplitterActive = this._isDrawerOpened() && !this.isInAdaptiveState();
+    this._splitter?.toggleDisabled(!isSplitterActive);
+  }
 
-    _isSmallScreen() {
-        return getWidth(window) <= ADAPTIVE_STATE_SCREEN_WIDTH;
-    }
-
-    _isDrawerOpened() {
-        return this._drawer.option('opened');
-    }
-
-    _initActions() {
-        this._actions = {
-            onAdaptiveStateChanged: this._createActionByOption('onAdaptiveStateChanged')
-        };
-    }
-
-    _raiseAdaptiveStateChanged(enabled) {
-        this._actions.onAdaptiveStateChanged({ enabled });
-    }
-
-    _getDefaultOptions() {
-        return extend(super._getDefaultOptions(), {
-            drawerTemplate: null,
-            contentTemplate: null,
-            onAdaptiveStateChanged: null,
-        });
-    }
-
-    _optionChanged(args) {
-        const name = args.name;
-
-        switch(name) {
-            case 'drawerTemplate':
-            case 'contentTemplate':
-                this.repaint();
-                break;
-            case 'onAdaptiveStateChanged':
-                this._actions[name] = this._createActionByOption(name);
-                break;
-            default:
-                super._optionChanged(args);
-        }
-    }
-
-    isInAdaptiveState() {
-        return this._isInAdaptiveState;
-    }
-
-    toggleDrawer(showing, skipAnimation) {
-        this._updateDrawerMaxSize();
-        this._drawer.option('animationEnabled', !skipAnimation);
-        this._drawer.toggle(showing);
-        const isSplitterActive = this._isDrawerOpened() && !this.isInAdaptiveState();
-        this._splitter.toggleDisabled(!isSplitterActive);
-    }
-
-    getSplitterElement() {
-        return this._splitter.getSplitterBorderElement().get(0);
-    }
+  getSplitterElement(): Element | undefined {
+    return this._splitter?.getSplitterBorderElement()?.get(0);
+  }
 }
 
 export default FileManagerAdaptivityControl;
