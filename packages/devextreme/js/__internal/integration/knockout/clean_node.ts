dc803dc9c048c9ed8a7d3d3093bb657fc924deb5
anna.shakhova <anna.shakhova@devexpress.com>
2025-11-14 10:39:59 +0100

    Integration: fix ts issues

--- packages/devextreme/js/__internal/integration/knockout/clean_node.ts
+++ packages/devextreme/js/__internal/integration/knockout/clean_node.ts
@@ -1,51 +1,59 @@
-import { afterCleanData, strategyChanging, cleanData } from '../../core/element_data';
-// eslint-disable-next-line no-restricted-imports
+import { afterCleanData, cleanData, strategyChanging } from '@ts/core/m_element_data';
+import { compare as compareVersion } from '@ts/core/utils/m_version';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import ko from 'knockout';
-import { compare as compareVersion } from '../../core/utils/version';
+
 import { getClosestNodeWithKoCreation } from './utils';
 
-if(ko) {
-    const originalKOCleanExternalData = ko.utils.domNodeDisposal.cleanExternalData;
-    const patchCleanData = function() {
-        afterCleanData(function(nodes) {
-            let i;
-            for(i = 0; i < nodes.length; i++) {
-                nodes[i].cleanedByJquery = true;
-            }
+interface CleanedNode extends Node {
+  cleanedByKo?: boolean;
+  cleanedByJquery?: boolean;
+}
 
-            for(i = 0; i < nodes.length; i++) {
-                if(!nodes[i].cleanedByKo) {
-                    ko.cleanNode(nodes[i]);
-                }
-                delete nodes[i].cleanedByKo;
-            }
+if (ko) {
+  const originalKOCleanExternalData = ko.utils.domNodeDisposal.cleanExternalData;
+  const patchCleanData = (): void => {
+    afterCleanData((nodes: CleanedNode[]) => {
+      // eslint-disable-next-line @typescript-eslint/prefer-for-of
+      for (let i = 0; i < nodes.length; i += 1) {
+        nodes[i].cleanedByJquery = true;
+      }
 
-            for(i = 0; i < nodes.length; i++) {
-                delete nodes[i].cleanedByJquery;
-            }
-        });
-
-        ko.utils.domNodeDisposal.cleanExternalData = function(node) {
-            node.cleanedByKo = true;
-            if(getClosestNodeWithKoCreation(node)) {
-                if(!node.cleanedByJquery) {
-                    cleanData([node]);
-                }
-            }
-        };
-    };
-
-    const restoreOriginCleanData = function() {
-        afterCleanData(function() {});
-        ko.utils.domNodeDisposal.cleanExternalData = originalKOCleanExternalData;
-    };
-
-    patchCleanData();
-
-    strategyChanging.add(function(strategy) {
-        const isJQuery = !!strategy.fn;
-        if(isJQuery && compareVersion(strategy.fn.jquery, [2, 0]) < 0) {
-            restoreOriginCleanData();
+      // eslint-disable-next-line @typescript-eslint/prefer-for-of
+      for (let i = 0; i < nodes.length; i += 1) {
+        if (!nodes[i].cleanedByKo) {
+          ko.cleanNode(nodes[i]);
         }
+        delete nodes[i].cleanedByKo;
+      }
+
+      // eslint-disable-next-line @typescript-eslint/prefer-for-of
+      for (let i = 0; i < nodes.length; i += 1) {
+        delete nodes[i].cleanedByJquery;
+      }
     });
+
+    ko.utils.domNodeDisposal.cleanExternalData = (node: CleanedNode): void => {
+      node.cleanedByKo = true;
+      if (getClosestNodeWithKoCreation(node)) {
+        if (!node.cleanedByJquery) {
+          cleanData([node]);
+        }
+      }
+    };
+  };
+
+  const restoreOriginCleanData = (): void => {
+    afterCleanData(() => {});
+    ko.utils.domNodeDisposal.cleanExternalData = originalKOCleanExternalData;
+  };
+
+  patchCleanData();
+
+  strategyChanging.add((strategy): void => {
+    const isJQuery = !!strategy.fn;
+    if (isJQuery && compareVersion(strategy.fn.jquery, [2, 0]) < 0) {
+      restoreOriginCleanData();
+    }
+  });
 }
