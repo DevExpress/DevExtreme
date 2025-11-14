e2e6413fc7e7f514a89d08d91148d418cab97a58
Sergio Bur <sjbur.dev@gmail.com>
2025-11-14 15:56:03 +0100

    refactor: move test

--- packages/devextreme-react/src/core/__tests__/integration/scheduler.test.tsx
+++ packages/devextreme-react/src/core/__tests__/integration/scheduler.test.tsx
@@ -1,7 +1,7 @@
 import * as testingLib from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import * as React from 'react';
-import { Scheduler } from '../../scheduler';
+import { Scheduler } from '../../../scheduler';
 
 describe('Scheduler component', () => {
   beforeEach(() => {
