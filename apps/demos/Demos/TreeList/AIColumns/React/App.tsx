f55908eb05a5029f8b71bbb11cdb717e04912732
dmlvr <lvr@dmlvr.com>
2025-11-17 11:08:33 +0200

    add showBorders prop

--- apps/demos/Demos/TreeList/AIColumns/React/App.tsx
+++ apps/demos/Demos/TreeList/AIColumns/React/App.tsx
@@ -30,6 +30,7 @@ export default function App() {
   return (
     <TreeList
       id="employees"
+      showBorders={true}
       dataSource={employees}
       keyExpr="ID"
       parentIdExpr="Head_ID"
