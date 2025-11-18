7964bb1390423434b358b1d05b396eaab9bf644a
Sergio Bur <sjbur.dev@gmail.com>
2025-11-18 14:25:32 +0100

    test: undo changes to test add_emptiness

--- packages/devextreme/js/__internal/scheduler/view_model/generate_view_model/steps/add_emptiness.test.ts
+++ packages/devextreme/js/__internal/scheduler/view_model/generate_view_model/steps/add_emptiness.test.ts
@@ -3,93 +3,37 @@ import { describe, expect, it } from '@jest/globals';
 import { addEmptiness } from './add_emptiness';
 
 describe('addEmptiness', () => {
-  it('should set empty for timeline view when height is below min threshold', () => {
+  it('should return set empty for timeline view', () => {
     expect(addEmptiness([
-      {
-        height: 34, width: 41, allDay: false, isAllDayPanelOccupied: false,
-      },
-      {
-        height: 36, width: 41, allDay: false, isAllDayPanelOccupied: false,
-      },
+      { height: 34, width: 41 },
+      { height: 36, width: 41 },
     ] as any, { isTimelineView: true, isAdaptivityEnabled: false, isMonthView: false })).toEqual([
-      {
-        height: 34, width: 41, allDay: false, isAllDayPanelOccupied: false, empty: true,
-      },
-      {
-        height: 36, width: 41, allDay: false, isAllDayPanelOccupied: false, empty: false,
-      },
+      { height: 34, width: 41, empty: true },
+      { height: 36, width: 41, empty: false },
     ]);
   });
 
-  it('should use reduced min height for day and week views', () => {
+  it('should return set empty for general view', () => {
     expect(addEmptiness([
-      {
-        height: 10, width: 39, allDay: false, isAllDayPanelOccupied: false,
-      },
-      {
-        height: 14, width: 41, allDay: false, isAllDayPanelOccupied: false,
-      },
+      { height: 34, width: 39 },
+      { height: 36, width: 41 },
+      { height: 20, width: 20, isAllDayPanelOccupied: true },
     ] as any, { isTimelineView: false, isAdaptivityEnabled: false, isMonthView: false })).toEqual([
+      { height: 34, width: 39, empty: true },
+      { height: 36, width: 41, empty: false },
       {
-        height: 10, width: 39, allDay: false, isAllDayPanelOccupied: false, empty: true,
-      },
-      {
-        height: 14, width: 41, allDay: false, isAllDayPanelOccupied: false, empty: false,
-      },
-    ]);
-  });
-
-  it('should keep legacy min height for month view', () => {
-    expect(addEmptiness([
-      {
-        height: 19, width: 41, allDay: false, isAllDayPanelOccupied: false,
-      },
-      {
-        height: 22, width: 41, allDay: false, isAllDayPanelOccupied: false,
-      },
-    ] as any, { isTimelineView: false, isAdaptivityEnabled: false, isMonthView: true })).toEqual([
-      {
-        height: 19, width: 41, allDay: false, isAllDayPanelOccupied: false, empty: true,
-      },
-      {
-        height: 22, width: 41, allDay: false, isAllDayPanelOccupied: false, empty: false,
+        height: 20, width: 20, isAllDayPanelOccupied: true, empty: false,
       },
     ]);
   });
 
-  it('should keep legacy min height for allDay appointments placed in regular panel', () => {
+  it('should return set empty for adaptivity view', () => {
     expect(addEmptiness([
-      {
-        height: 15, width: 41, allDay: true, isAllDayPanelOccupied: false,
-      },
-      {
-        height: 25, width: 41, allDay: true, isAllDayPanelOccupied: false,
-      },
-    ] as any, { isTimelineView: false, isAdaptivityEnabled: false, isMonthView: false })).toEqual([
-      {
-        height: 15, width: 41, allDay: true, isAllDayPanelOccupied: false, empty: true,
-      },
-      {
-        height: 25, width: 41, allDay: true, isAllDayPanelOccupied: false, empty: false,
-      },
-    ]);
-  });
-
-  it('should respect adaptivity min size', () => {
-    expect(addEmptiness([
-      {
-        height: 25, width: 40, allDay: false, isAllDayPanelOccupied: false,
-      },
-      {
-        height: 30, width: 40, allDay: false, isAllDayPanelOccupied: false,
-      },
+      { height: 25, width: 40 },
+      { height: 30, width: 40 },
     ] as any, { isTimelineView: false, isAdaptivityEnabled: true, isMonthView: false })).toEqual([
-      {
-        height: 25, width: 40, allDay: false, isAllDayPanelOccupied: false, empty: true,
-      },
-      {
-        height: 30, width: 40, allDay: false, isAllDayPanelOccupied: false, empty: false,
-      },
+      { height: 25, width: 40, empty: true },
+      { height: 30, width: 40, empty: false },
     ]);
   });
 });
