e4aca4427bb99a3c8c525ff4506dc9d5be168bfd
Alex Lavrov <36633600+alexslavr@users.noreply.github.com>
2025-11-18 19:18:26 +0400

    Generate DataGrid, TreeList editing form nesteds (#31736)

--- packages/devextreme-angular/src/ui/tree-list/nested/item-dxi.ts
+++ packages/devextreme-angular/src/ui/tree-list/nested/item-dxi.ts
@@ -10,15 +10,21 @@ import {
     Inject,
     AfterViewInit,
     SkipSelf,
-    Input
+    Input,
+    ContentChildren,
+    QueryList
 } from '@angular/core';
 
 import { DOCUMENT } from '@angular/common';
 
 
+import * as CommonTypes from 'devextreme/common';
 import { LocateInMenuMode, ShowTextMode } from 'devextreme/ui/toolbar';
-import { ToolbarItemLocation, ToolbarItemComponent } from 'devextreme/common';
+import { ToolbarItemLocation, ToolbarItemComponent, HorizontalAlignment, VerticalAlignment } from 'devextreme/common';
 import { TreeListPredefinedToolbarItem } from 'devextreme/ui/tree_list';
+import { FormItemComponent, FormItemType, LabelLocation, dxFormButtonItem, dxFormEmptyItem, dxFormGroupItem, dxFormSimpleItem, dxFormTabbedItem } from 'devextreme/ui/form';
+import { dxTabPanelOptions } from 'devextreme/ui/tab_panel';
+import { dxButtonOptions } from 'devextreme/ui/button';
 
 import {
     DxIntegrationModule,
@@ -30,7 +36,11 @@ import {
 } from 'devextreme-angular/core';
 import { CollectionNestedOption } from 'devextreme-angular/core';
 
-import { PROPERTY_TOKEN_items } from 'devextreme-angular/core/tokens';
+import {
+    PROPERTY_TOKEN_validationRules,
+    PROPERTY_TOKEN_tabs,
+    PROPERTY_TOKEN_items,
+} from 'devextreme-angular/core/tokens';
 
 @Component({
     selector: 'dxi-tree-list-item',
@@ -49,6 +59,21 @@ import { PROPERTY_TOKEN_items } from 'devextreme-angular/core/tokens';
 })
 export class DxiTreeListItemComponent extends CollectionNestedOption implements AfterViewInit,
     IDxTemplateHost {
+    @ContentChildren(PROPERTY_TOKEN_validationRules)
+    set _validationRulesContentChildren(value: QueryList<CollectionNestedOption>) {
+        this.setChildren('validationRules', value);
+    }
+    
+    @ContentChildren(PROPERTY_TOKEN_tabs)
+    set _tabsContentChildren(value: QueryList<CollectionNestedOption>) {
+        this.setChildren('tabs', value);
+    }
+    
+    @ContentChildren(PROPERTY_TOKEN_items)
+    set _itemsContentChildren(value: QueryList<CollectionNestedOption>) {
+        this.setChildren('items', value);
+    }
+    
     @Input()
     get cssClass(): string | undefined {
         return this._getOption('cssClass');
@@ -151,6 +176,214 @@ export class DxiTreeListItemComponent extends CollectionNestedOption implements 
     }
     set widget(value: ToolbarItemComponent) {
         this._setOption('widget', value);
+    }
+
+    @Input()
+    get badge(): string {
+        return this._getOption('badge');
+    }
+    set badge(value: string) {
+        this._setOption('badge', value);
+    }
+
+    @Input()
+    get icon(): string {
+        return this._getOption('icon');
+    }
+    set icon(value: string) {
+        this._setOption('icon', value);
+    }
+
+    @Input()
+    get tabTemplate(): any {
+        return this._getOption('tabTemplate');
+    }
+    set tabTemplate(value: any) {
+        this._setOption('tabTemplate', value);
+    }
+
+    @Input()
+    get title(): string {
+        return this._getOption('title');
+    }
+    set title(value: string) {
+        this._setOption('title', value);
+    }
+
+    @Input()
+    get aiOptions(): { disabled?: boolean, instruction?: string | undefined } {
+        return this._getOption('aiOptions');
+    }
+    set aiOptions(value: { disabled?: boolean, instruction?: string | undefined }) {
+        this._setOption('aiOptions', value);
+    }
+
+    @Input()
+    get colSpan(): number | undefined {
+        return this._getOption('colSpan');
+    }
+    set colSpan(value: number | undefined) {
+        this._setOption('colSpan', value);
+    }
+
+    @Input()
+    get dataField(): string | undefined {
+        return this._getOption('dataField');
+    }
+    set dataField(value: string | undefined) {
+        this._setOption('dataField', value);
+    }
+
+    @Input()
+    get editorOptions(): any | undefined {
+        return this._getOption('editorOptions');
+    }
+    set editorOptions(value: any | undefined) {
+        this._setOption('editorOptions', value);
+    }
+
+    @Input()
+    get editorType(): FormItemComponent {
+        return this._getOption('editorType');
+    }
+    set editorType(value: FormItemComponent) {
+        this._setOption('editorType', value);
+    }
+
+    @Input()
+    get helpText(): string | undefined {
+        return this._getOption('helpText');
+    }
+    set helpText(value: string | undefined) {
+        this._setOption('helpText', value);
+    }
+
+    @Input()
+    get isRequired(): boolean | undefined {
+        return this._getOption('isRequired');
+    }
+    set isRequired(value: boolean | undefined) {
+        this._setOption('isRequired', value);
+    }
+
+    @Input()
+    get itemType(): FormItemType {
+        return this._getOption('itemType');
+    }
+    set itemType(value: FormItemType) {
+        this._setOption('itemType', value);
+    }
+
+    @Input()
+    get label(): { alignment?: HorizontalAlignment, location?: LabelLocation, showColon?: boolean, template?: any, text?: string | undefined, visible?: boolean } {
+        return this._getOption('label');
+    }
+    set label(value: { alignment?: HorizontalAlignment, location?: LabelLocation, showColon?: boolean, template?: any, text?: string | undefined, visible?: boolean }) {
+        this._setOption('label', value);
+    }
+
+    @Input()
+    get validationRules(): Array<CommonTypes.ValidationRule> {
+        return this._getOption('validationRules');
+    }
+    set validationRules(value: Array<CommonTypes.ValidationRule>) {
+        this._setOption('validationRules', value);
+    }
+
+    @Input()
+    get visibleIndex(): number | undefined {
+        return this._getOption('visibleIndex');
+    }
+    set visibleIndex(value: number | undefined) {
+        this._setOption('visibleIndex', value);
+    }
+
+    @Input()
+    get alignItemLabels(): boolean {
+        return this._getOption('alignItemLabels');
+    }
+    set alignItemLabels(value: boolean) {
+        this._setOption('alignItemLabels', value);
+    }
+
+    @Input()
+    get caption(): string | undefined {
+        return this._getOption('caption');
+    }
+    set caption(value: string | undefined) {
+        this._setOption('caption', value);
+    }
+
+    @Input()
+    get captionTemplate(): any {
+        return this._getOption('captionTemplate');
+    }
+    set captionTemplate(value: any) {
+        this._setOption('captionTemplate', value);
+    }
+
+    @Input()
+    get colCount(): number {
+        return this._getOption('colCount');
+    }
+    set colCount(value: number) {
+        this._setOption('colCount', value);
+    }
+
+    @Input()
+    get colCountByScreen(): { lg?: number | undefined, md?: number | undefined, sm?: number | undefined, xs?: number | undefined } {
+        return this._getOption('colCountByScreen');
+    }
+    set colCountByScreen(value: { lg?: number | undefined, md?: number | undefined, sm?: number | undefined, xs?: number | undefined }) {
+        this._setOption('colCountByScreen', value);
+    }
+
+    @Input()
+    get items(): Array<dxFormButtonItem | dxFormEmptyItem | dxFormGroupItem | dxFormSimpleItem | dxFormTabbedItem> {
+        return this._getOption('items');
+    }
+    set items(value: Array<dxFormButtonItem | dxFormEmptyItem | dxFormGroupItem | dxFormSimpleItem | dxFormTabbedItem>) {
+        this._setOption('items', value);
+    }
+
+    @Input()
+    get tabPanelOptions(): dxTabPanelOptions | undefined {
+        return this._getOption('tabPanelOptions');
+    }
+    set tabPanelOptions(value: dxTabPanelOptions | undefined) {
+        this._setOption('tabPanelOptions', value);
+    }
+
+    @Input()
+    get tabs(): { alignItemLabels?: boolean, badge?: string | undefined, colCount?: number, colCountByScreen?: { lg?: number | undefined, md?: number | undefined, sm?: number | undefined, xs?: number | undefined }, disabled?: boolean, icon?: string | undefined, items?: Array<dxFormButtonItem | dxFormEmptyItem | dxFormGroupItem | dxFormSimpleItem | dxFormTabbedItem>, tabTemplate?: any, template?: any, title?: string | undefined }[] {
+        return this._getOption('tabs');
+    }
+    set tabs(value: { alignItemLabels?: boolean, badge?: string | undefined, colCount?: number, colCountByScreen?: { lg?: number | undefined, md?: number | undefined, sm?: number | undefined, xs?: number | undefined }, disabled?: boolean, icon?: string | undefined, items?: Array<dxFormButtonItem | dxFormEmptyItem | dxFormGroupItem | dxFormSimpleItem | dxFormTabbedItem>, tabTemplate?: any, template?: any, title?: string | undefined }[]) {
+        this._setOption('tabs', value);
+    }
+
+    @Input()
+    get buttonOptions(): dxButtonOptions | undefined {
+        return this._getOption('buttonOptions');
+    }
+    set buttonOptions(value: dxButtonOptions | undefined) {
+        this._setOption('buttonOptions', value);
+    }
+
+    @Input()
+    get horizontalAlignment(): HorizontalAlignment {
+        return this._getOption('horizontalAlignment');
+    }
+    set horizontalAlignment(value: HorizontalAlignment) {
+        this._setOption('horizontalAlignment', value);
+    }
+
+    @Input()
+    get verticalAlignment(): VerticalAlignment {
+        return this._getOption('verticalAlignment');
+    }
+    set verticalAlignment(value: VerticalAlignment) {
+        this._setOption('verticalAlignment', value);
     }
 
 
