930f177c4731deb7b7693fd4a485ac7121d171fc
pharret31 <pharret31@gmail.com>
2025-11-18 23:34:26 +0100

    FileManagement: fix ts issues

--- packages/devextreme/js/__internal/file_management/utils.ts
+++ packages/devextreme/js/__internal/file_management/utils.ts
@@ -1,74 +1,73 @@
-import { each } from '../core/utils/iterator';
+import { each } from '@js/core/utils/iterator';
 
 export const PATH_SEPARATOR = '/';
 
-export const getFileExtension = path => {
-    const index = path.lastIndexOf('.');
-    return index !== -1 ? path.substr(index) : '';
+export const getFileExtension = (path: string): string => {
+  const index = path.lastIndexOf('.');
+  return index !== -1 ? path.substring(index) : '';
 };
 
-export const getName = path => {
-    const index = path.lastIndexOf(PATH_SEPARATOR);
-    return index !== -1 ? path.substr(index + PATH_SEPARATOR.length) : path;
+export const getName = (path: string): string => {
+  const index = path.lastIndexOf(PATH_SEPARATOR);
+  return index !== -1 ? path.substring(index + PATH_SEPARATOR.length) : path;
 };
 
-export const getParentPath = path => {
-    const index = path.lastIndexOf(PATH_SEPARATOR);
-    return index !== -1 ? path.substr(0, index) : '';
+export const getParentPath = (path: string): string => {
+  const index = path.lastIndexOf(PATH_SEPARATOR);
+  return index !== -1 ? path.substring(0, index) : '';
 };
 
-export const getPathParts = (path, includeFullPath) => {
-    if(!path || path === '/') {
-        return [];
+export const getEscapedFileName = (fileName: string): string => fileName.replace(/\/{1,1}/g, '//');
+
+export const pathCombine = (...args: string[]): string => {
+  let result = '';
+
+  each(args, (_: number, arg: string): void => {
+    if (arg) {
+      if (result) {
+        result += PATH_SEPARATOR;
+      }
+
+      result += arg;
     }
-    const result = [];
-    let pathPart = '';
+  });
 
-    for(let i = 0; i < path.length; i++) {
-        let char = path.charAt(i);
-        if(char === PATH_SEPARATOR) {
-            const nextChar = path.charAt(i + 1);
-            if(nextChar !== PATH_SEPARATOR) {
-                if(pathPart) {
-                    result.push(pathPart);
-                    pathPart = '';
-                }
-                char = nextChar;
-            }
-            i++;
+  return result;
+};
+
+export const getPathParts = (path?: string, includeFullPath?: boolean): string[] => {
+  if (!path || path === '/') {
+    return [];
+  }
+  const result: string[] = [];
+  let pathPart = '';
+
+  for (let i = 0; i < path.length; i += 1) {
+    let char = path.charAt(i);
+    if (char === PATH_SEPARATOR) {
+      const nextChar = path.charAt(i + 1);
+      if (nextChar !== PATH_SEPARATOR) {
+        // eslint-disable-next-line max-depth
+        if (pathPart) {
+          result.push(pathPart);
+          pathPart = '';
         }
-        pathPart += char;
+        char = nextChar;
+      }
+      i += 1;
     }
+    pathPart += char;
+  }
 
-    if(pathPart) {
-        result.push(pathPart);
+  if (pathPart) {
+    result.push(pathPart);
+  }
+
+  if (includeFullPath) {
+    for (let i = 0; i < result.length; i += 1) {
+      result[i] = pathCombine(i === 0 ? '' : result[i - 1], getEscapedFileName(result[i]));
     }
+  }
 
-    if(includeFullPath) {
-        for(let i = 0; i < result.length; i++) {
-            result[i] = pathCombine(i === 0 ? '' : result[i - 1], getEscapedFileName(result[i]));
-        }
-    }
-
-    return result;
-};
-
-export const getEscapedFileName = function(fileName) {
-    return fileName.replace(/\/{1,1}/g, '//');
-};
-
-export const pathCombine = function() {
-    let result = '';
-
-    each(arguments, (_, arg) => {
-        if(arg) {
-            if(result) {
-                result += PATH_SEPARATOR;
-            }
-
-            result += arg;
-        }
-    });
-
-    return result;
+  return result;
 };
