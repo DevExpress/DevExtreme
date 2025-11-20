0195740480db5da6c7f600c9e96b5ec9504907c1
pharret31 <pharret31@gmail.com>
2025-11-20 14:43:16 +0100

    Test

--- packages/devextreme-themebuilder/tests/modules/themes.test.ts
+++ packages/devextreme-themebuilder/tests/modules/themes.test.ts
@@ -1,5 +1,6 @@
 /* eslint no-console: 0 */
-import { getThemes } from '../../../devextreme-scss/build/theme-options';
+// import { getThemes } from '../../../devextreme-scss/build/theme-options';
+const { getThemes } = require('../../../devextreme-scss/build/theme-options.cjs');
 import themes from '../../src/modules/themes';
 
 describe('Themes', () => {
