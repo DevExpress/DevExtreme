70c00f543401dcb537dfff55477bb2f16bb379f0
Alyar <>
2025-11-22 17:09:02 +0400

    DataGrid - AI Column: Test unsupported Editing properties

--- packages/devextreme/js/__internal/grids/grid_core/ai_column/__tests__/unsupported_base_properties.integration.test.ts
+++ packages/devextreme/js/__internal/grids/grid_core/ai_column/__tests__/unsupported_base_properties.integration.test.ts
@@ -10,6 +10,11 @@ import errors from '@js/ui/widget/ui.errors';
 import { AIIntegration } from '@ts/core/ai_integration/core/ai_integration';
 import { DataGridModel } from '@ts/grids/data_grid/__tests__/__mock__/model/data_grid';
 
+interface RequestResult {
+  promise: Promise<GenerateGridColumnCommandResponse>;
+  abort: () => void;
+}
+
 const SELECTORS = {
   gridContainer: '#gridContainer',
 };
@@ -43,7 +48,7 @@ const createDataGrid = async (
   const instance = new DataGrid($container.get(0) as HTMLDivElement, options);
   const component = new DataGridModel($container.get(0) as HTMLElement);
 
-  jest.runOnlyPendingTimers();
+  jest.runAllTimers();
   resolve({
     $container,
     component,
@@ -455,6 +460,238 @@ describe('Unsupported properties', () => {
 
       expect(component.getDataCell(0, 1).getText()).toBe(EMPTY_CELL_TEXT);
       expect(component.getDataCell(1, 1).getText()).toBe('Response Item 2');
+    });
+  });
+
+  describe('Editing properties', () => {
+    const aiIntegration = new AIIntegration({
+      sendRequest(): RequestResult {
+        return {
+          promise: new Promise((resolve) => {
+            resolve('{"1":"Test1","2":"Test2","3":"Test3"}');
+          }),
+          abort: (): void => {},
+        };
+      },
+    });
+
+    describe('when editing.mode = row', () => {
+      it('should not allow editing in AI column', async () => {
+        const { component, instance } = await createDataGrid({
+          dataSource,
+          keyExpr: 'id',
+          editing: {
+            allowUpdating: true,
+            mode: 'row',
+          },
+          aiIntegration,
+          columns: [
+            'id',
+            {
+              caption: 'AI',
+              type: 'ai',
+              name: 'AItest',
+              ai: {
+                prompt: 'Initial prompt',
+              },
+            },
+          ],
+        });
+
+        instance.editRow(0);
+        jest.runAllTimers();
+
+        expect(component.getDataRow(0).isEditRow).toBe(true);
+        expect(component.getDataCell(0, 1).isEditCell).toBe(false);
+        expect(component.getDataCell(0, 1).getHTML()).toBe('Test1');
+      });
+
+      it('should not apply editing options to AI column', async () => {
+        const { component, instance } = await createDataGrid({
+          dataSource,
+          keyExpr: 'id',
+          editing: {
+            allowUpdating: true,
+            mode: 'row',
+          },
+          aiIntegration,
+          columns: [
+            'id',
+            {
+              caption: 'AI',
+              type: 'ai',
+              name: 'AItest',
+              ai: {
+                prompt: 'Initial prompt',
+              },
+              showEditorAlways: true,
+              allowEditing: true,
+              dataType: 'date',
+              editorOptions: {
+                text: 'abc',
+              },
+              editCellTemplate: (): dxElementWrapper => $('<input value="test" />'),
+            },
+          ],
+        });
+        let aiCell = component.getDataCell(0, 1);
+
+        expect(aiCell.isEditCell).toBe(false);
+        expect(aiCell.getHTML()).toBe('Test1');
+
+        instance.editRow(0);
+        jest.runAllTimers();
+
+        aiCell = component.getDataCell(0, 1);
+        expect(component.getDataRow(0).isEditRow).toBe(true);
+        expect(aiCell.isEditCell).toBe(false);
+        expect(aiCell.getHTML()).toBe('Test1');
+      });
+    });
+
+    describe('when editing.mode = batch', () => {
+      it('should not allow editing in AI column', async () => {
+        const { component, instance } = await createDataGrid({
+          dataSource,
+          keyExpr: 'id',
+          editing: {
+            allowUpdating: true,
+            mode: 'batch',
+          },
+          aiIntegration,
+          columns: [
+            'id',
+            {
+              caption: 'AI',
+              type: 'ai',
+              name: 'AItest',
+              ai: {
+                prompt: 'Initial prompt',
+              },
+            },
+          ],
+        });
+
+        instance.editCell(0, 1);
+        jest.runAllTimers();
+
+        expect(component.getDataCell(0, 1).isEditCell).toBe(false);
+        expect(component.getDataCell(0, 1).getHTML()).toBe('Test1');
+      });
+    });
+
+    describe('when editing.mode = form', () => {
+      it('should not allow editing in AI column', async () => {
+        const { component, instance } = await createDataGrid({
+          dataSource,
+          keyExpr: 'id',
+          editing: {
+            allowUpdating: true,
+            mode: 'form',
+          },
+          aiIntegration,
+          columns: [
+            'id',
+            {
+              caption: 'AI',
+              type: 'ai',
+              name: 'AItest',
+              ai: {
+                prompt: 'Initial prompt',
+              },
+            },
+          ],
+        });
+
+        instance.editRow(0);
+        jest.runAllTimers();
+
+        expect(component.getEditForm().getItems()?.length).toBe(1);
+        expect(component.getEditForm().getItem('id')).not.toBeNull();
+      });
+
+      it('should not apply editing options to AI column', async () => {
+        const { component, instance } = await createDataGrid({
+          dataSource,
+          keyExpr: 'id',
+          editing: {
+            allowUpdating: true,
+            mode: 'form',
+          },
+          aiIntegration,
+          columns: [
+            'id',
+            {
+              caption: 'AI',
+              type: 'ai',
+              name: 'AItest',
+              ai: {
+                prompt: 'Initial prompt',
+              },
+              showEditorAlways: true,
+              allowEditing: true,
+              dataType: 'date',
+              editorOptions: {
+                text: 'abc',
+              },
+              formItem: {
+                label: { text: 'AI Column' },
+              },
+              editCellTemplate: (): dxElementWrapper => $('<input value="test" />'),
+            },
+          ],
+        });
+        const aiCell = component.getDataCell(0, 1);
+
+        expect(aiCell.isEditCell).toBe(false);
+        expect(aiCell.getHTML()).toBe('Test1');
+
+        instance.editRow(0);
+        jest.runAllTimers();
+
+        expect(component.getEditForm().getItems()?.length).toBe(1);
+        expect(component.getEditForm().getItem('id')).not.toBeNull();
+      });
+    });
+
+    it('should not apply validation to AI column', async () => {
+      const { component, instance } = await createDataGrid({
+        dataSource,
+        keyExpr: 'id',
+        aiIntegration: new AIIntegration({
+          sendRequest(): RequestResult {
+            return {
+              promise: new Promise((resolve) => {
+                resolve('{"1":"","2":"","3":""}');
+              }),
+              abort: (): void => {},
+            };
+          },
+        }),
+        columns: [
+          'id',
+          {
+            caption: 'AI',
+            type: 'ai',
+            name: 'AItest',
+            ai: {
+              prompt: 'Initial prompt',
+            },
+            validationRules: [{ type: 'required' }],
+          },
+        ],
+      });
+      let aiCell = component.getDataCell(0, 1);
+
+      expect(aiCell.isValidCell).toBe(true);
+      expect(aiCell.getText()).toBe(EMPTY_CELL_TEXT);
+
+      instance.columnOption('AItest', 'prompt', 'Updated prompt');
+      await Promise.resolve();
+
+      aiCell = component.getDataCell(0, 1);
+      expect(aiCell.isValidCell).toBe(true);
+      expect(aiCell.getText()).toBe(EMPTY_CELL_TEXT);
     });
   });
 });
