04cd567eff00e4d82e7fe96d57f3938b36a7fc0a
Alexander Kozlovskiy <zedwag@gmail.com>
2025-11-27 00:20:11 +0400

    DateBox: ui should not freeze on non digit key press when non arabic numbers locale is used (T1311652) (#31852)

--- packages/devextreme/js/__internal/ui/date_box/m_date_box.mask.ts
+++ packages/devextreme/js/__internal/ui/date_box/m_date_box.mask.ts
@@ -314,8 +314,10 @@ class DateBoxMask extends DateBoxBase {
   }
 
   _searchString(char) {
-    // eslint-disable-next-line radix
-    if (!isNaN(parseInt(this._getActivePartProp('text')))) {
+    const text = this._getActivePartProp('text');
+    const convertedText = numberLocalization.convertDigits(text, true);
+
+    if (!isNaN(parseInt(convertedText, 10))) {
       return;
     }
 
