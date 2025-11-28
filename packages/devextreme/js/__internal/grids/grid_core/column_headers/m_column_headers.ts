f47d9b0fcff24d62708d4331fcab58039aeaf4ec
Raushen <eylau-andrew@mail.ru>
2025-11-28 10:32:51 +0200

    Linter fix

--- packages/devextreme/js/__internal/grids/grid_core/column_headers/m_column_headers.ts
+++ packages/devextreme/js/__internal/grids/grid_core/column_headers/m_column_headers.ts
@@ -5,7 +5,6 @@ import type { dxElementWrapper } from '@js/core/renderer';
 import $ from '@js/core/renderer';
 import { Deferred } from '@js/core/utils/deferred';
 import { extend } from '@js/core/utils/extend';
-import { each } from '@js/core/utils/iterator';
 import { getHeight } from '@js/core/utils/size';
 import { isDefined } from '@js/core/utils/type';
 import { ColumnContextMenuMixin } from '@ts/grids/grid_core/context_menu/m_column_context_menu_mixin';
