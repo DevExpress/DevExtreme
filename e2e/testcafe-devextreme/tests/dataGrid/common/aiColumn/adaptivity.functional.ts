4d5cbe92e9571028021eae855ed4b25e07a61416
dmlvr <lvr@dmlvr.com>
2025-12-03 16:14:02 +0200

    write test for this case

--- e2e/testcafe-devextreme/tests/dataGrid/common/aiColumn/adaptivity.functional.ts
+++ e2e/testcafe-devextreme/tests/dataGrid/common/aiColumn/adaptivity.functional.ts
@@ -3,7 +3,7 @@ import url from '../../../../helpers/getPageUrl';
 import { createWidget } from '../../../../helpers/createWidget';
 
 fixture.disablePageReloads`Ai Column.Adaptivity`
-  .page(url(__dirname, '../../../container.html'));
+  .page(url(__dirname, './pages/containerWithAIIntegration.html'));
 
 const DATA_GRID_SELECTOR = '#container';
 
@@ -33,6 +33,7 @@ test('The AI column should be hidden when columnHidingEnabled is true', async (t
   width: 350,
   columnWidth: 100,
   columnHidingEnabled: true,
+
   columns: [
     { dataField: 'id', caption: 'ID' },
     { dataField: 'name', caption: 'Name' },
@@ -129,3 +130,85 @@ test('The AI column should not be hidden when there is a second AI column with a
     { dataField: 'value', caption: 'Value' },
   ],
 }));
+
+test('The AI column should have value in the adaptive detail row', async (t) => {
+  const dataGrid = new DataGrid(DATA_GRID_SELECTOR);
+  await dataGrid.isReady();
+
+  const adaptiveButton = dataGrid.getAdaptiveButton();
+
+  await t.expect(adaptiveButton.exists).ok();
+
+  await t.click(adaptiveButton);
+
+  const adaptiveRow = dataGrid.getAdaptiveRow(0);
+
+  await t.expect(adaptiveRow.element.exists).ok();
+
+  const aiFormItem = adaptiveRow.element.find('.dx-field-item').withText('AI Column');
+
+  await t.expect(aiFormItem.exists).ok();
+
+  const aiFormItemValue = aiFormItem.find('.dx-field-item-content');
+
+  await t.expect(aiFormItemValue.textContent).contains('Response 1');
+}).before(async () => createWidget('dxDataGrid', () => ({
+  dataSource: [
+    { id: 1, name: 'Name 1', value: 10 },
+    { id: 2, name: 'Name 2', value: 20 },
+    { id: 3, name: 'Name 3', value: 30 },
+  ],
+  keyExpr: 'id',
+  // eslint-disable-next-line new-cap
+  aiIntegration: new (window as any).DevExpress.aiIntegration({
+    sendRequest({ data }) {
+      let timeoutId: ReturnType<typeof setTimeout> | null = null;
+      const obj = {};
+      Object.entries(data?.data).forEach(([key, value]) => {
+        obj[key] = `Response ${(value as any).id}`;
+      });
+      const promise = new Promise((resolve) => {
+        timeoutId = setTimeout(() => {
+          resolve(JSON.stringify(obj));
+        }, 1000);
+      });
+
+      const result = {
+        promise,
+        abort: () => {
+          if (timeoutId) {
+            clearTimeout(timeoutId);
+            timeoutId = null;
+          }
+        },
+      };
+
+      return result;
+    },
+  }),
+  width: 400,
+  columnHidingEnabled: true,
+  showBorders: true,
+  columns: [
+    {
+      dataField: 'id', caption: 'ID', width: 150, hidingPriority: 3,
+    },
+    {
+      dataField: 'name', caption: 'Name', width: 150, hidingPriority: 2,
+    },
+    {
+      dataField: 'value', caption: 'Value', width: 100, hidingPriority: 1,
+    },
+    {
+      name: 'AIcolumn',
+      caption: 'AI Column',
+      type: 'ai',
+      ai: {
+        prompt: 'Send me nothing',
+        mode: 'auto',
+      },
+      width: 200,
+      hidingPriority: 0,
+    },
+  ],
+})));
