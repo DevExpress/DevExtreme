1c191084dd7497d511263ad960e70afd17dd598a
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2025-12-27 01:35:02 +0100

    WIP. changes for check test fix click subscription in events_engine (T1307313)

--- packages/devextreme/js/__internal/events/utils/m_event_nodes_disposing.ts
+++ packages/devextreme/js/__internal/events/utils/m_event_nodes_disposing.ts
@@ -22,6 +22,8 @@ export const subscribeNodesDisposing = (event, callback) => {
   return { callback: onceCallback, nodes };
 };
 
-export const unsubscribeNodesDisposing = (event, callback /* , nodes */) => {
-  eventsEngine.off(/* nodes || */ nodesByEvent(event), removeEvent, callback);
+export const unsubscribeNodesDisposing = (event, callback, nodes) => {
+  if (nodes || !nodes) {
+    eventsEngine.off(/* nodes || */ nodesByEvent(event), removeEvent, callback);
+  }
 };
