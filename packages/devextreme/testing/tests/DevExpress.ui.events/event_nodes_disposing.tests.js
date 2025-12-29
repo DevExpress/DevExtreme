0c94bde9eb4ee4165df5c6cb7a78d5b51205f3b5
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2025-12-29 16:40:46 +0100

    WIP. changes for check test fix click subscription in events_engine (T1307313)

--- packages/devextreme/testing/tests/DevExpress.ui.events/event_nodes_disposing.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.events/event_nodes_disposing.tests.js
@@ -1,6 +1,14 @@
 import eventsEngine from 'common/core/events/core/events_engine';
 import domAdapter from '__internal/core/m_dom_adapter';
 
+QUnit.testStart(function() {
+    const markup = '';
+    const fixture = document.getElementById('qunit-fixture');
+    if(fixture) {
+        fixture.innerHTML = markup;
+    }
+});
+
 QUnit.module('event nodes disposing', {
     afterEach: function() {
         const document = domAdapter.getDocument();
@@ -10,12 +18,22 @@ QUnit.module('event nodes disposing', {
 
 QUnit.test('should not leak memory when clicking on body with dxclick subscription on document', function(assert) {
     const document = domAdapter.getDocument();
-    $(document).on('dxclick', function() {});
+    const body = document.body;
+
+    eventsEngine.on(document, 'dxclick', function() {});
+
+    if(typeof globalThis !== 'undefined' && typeof globalThis.gc === 'function') {
+        globalThis.gc();
+    }
 
     const initialMemory = performance.memory.usedJSHeapSize;
 
     for(let i = 0; i < 100; i++) {
-        $('body').trigger('click');
+        eventsEngine.trigger(body, 'click');
+    }
+
+    if(typeof globalThis !== 'undefined' && typeof globalThis.gc === 'function') {
+        globalThis.gc();
     }
 
     const finalMemory = performance.memory.usedJSHeapSize;
