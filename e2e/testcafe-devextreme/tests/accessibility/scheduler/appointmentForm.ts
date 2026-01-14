5f607e765542ea3f1cf1b5e3f015c8f740e9b712
Eldar Iusupzhanov <ferrarijed@gmail.com>
2026-01-14 15:18:07 +0800

    add aria-label to time editors & test

---
+++ e2e/testcafe-devextreme/tests/accessibility/scheduler/appointmentForm.ts
@@ -0,0 +1,32 @@
+import Scheduler from 'devextreme-testcafe-models/scheduler';
+import { a11yCheck } from '../../../helpers/accessibility/utils';
+import { createWidget } from '../../../helpers/createWidget';
+import url from '../../../helpers/getPageUrl';
+import { checkOptions } from './axe_options';
+
+fixture.disablePageReloads`Scheduler - Appointment`
+  .page(url(__dirname, '../../container.html'));
+
+const APPOINTMENT_POPUP_CONTENT_SELECTOR = '.dx-scheduler-appointment-popup .dx-overlay-content';
+
+test('Appointment Form with axe', async (t) => {
+  const scheduler = new Scheduler('#container');
+
+  const appointment = scheduler.getAppointment('App 1');
+
+  await t.doubleClick(appointment.element);
+  await t.wait(300);
+
+  await a11yCheck(t, checkOptions, APPOINTMENT_POPUP_CONTENT_SELECTOR);
+}).before(async () => {
+  await createWidget('dxScheduler', {
+    timeZone: 'UTC',
+    dataSource: [{
+      text: 'App 1',
+      startDate: Date.UTC(2021, 1, 1, 12),
+      endDate: Date.UTC(2021, 1, 1, 13),
+    }],
+    currentView: 'week',
+    currentDate: new Date(2021, 1, 1),
+  });
+});
