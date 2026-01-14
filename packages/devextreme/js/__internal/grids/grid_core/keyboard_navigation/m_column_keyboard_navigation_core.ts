ac576f1a42d58ba4250ba9fd85551034e3594961
Alyar <>
2026-01-14 22:05:40 +0400

    Fix tests

--- packages/devextreme/js/__internal/grids/grid_core/keyboard_navigation/m_column_keyboard_navigation_core.ts
+++ packages/devextreme/js/__internal/grids/grid_core/keyboard_navigation/m_column_keyboard_navigation_core.ts
@@ -1,5 +1,6 @@
 import { isDefined, isEmptyObject } from '@js/core/utils/type';
 
+import type { Column } from '../columns_controller/m_columns_controller';
 import { Direction } from './const';
 import type { ColumnFocusDispatcher } from './m_column_focus_dispatcher';
 import { KeyboardNavigationController as KeyboardNavigationControllerCore } from './m_keyboard_navigation_core';
@@ -12,13 +13,10 @@ export class ColumnKeyboardNavigationController extends KeyboardNavigationContro
   }
 
   protected getVisibleIndex(
-    column,
+    column: Column,
     rowIndex?: number,
   ): number {
-    const visibleIndex = this._columnsController.getVisibleIndex(column.index, rowIndex);
-    const columnIndexOffset = this.getColumnIndexOffset(visibleIndex);
-
-    return visibleIndex >= 0 ? visibleIndex + columnIndexOffset : -1;
+    return this._columnsController.getVisibleIndex(column.index, rowIndex);
   }
 
   protected getNewVisibleIndex(
@@ -73,7 +71,7 @@ export class ColumnKeyboardNavigationController extends KeyboardNavigationContro
 
   public moveColumn(column, direction = Direction.Next, rowIndex = 0) {
     const viewName = this.getFocusedView().getName();
-    const visibleIndex = this._columnsController.getVisibleIndex(column.index, rowIndex);
+    const visibleIndex = this.getVisibleIndex(column, rowIndex);
     const newVisibleIndex = this.getNewVisibleIndex(visibleIndex, rowIndex, direction);
     const newFocusedColumnIndex = this.getNewFocusedColumnIndex(newVisibleIndex, direction);
 
