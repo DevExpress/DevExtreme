3a8014d5214206c2cb9ebc3729b142066fcefa55
Dmitry Lavrinovich <52966626+dmlvr@users.noreply.github.com>
2026-01-15 16:08:30 +0200

    Navigation: Fix unstable e2e tests (Cherry-pick) (#32214)

--- apps/demos/testing/skipped-tests.js
+++ apps/demos/testing/skipped-tests.js
@@ -7,21 +7,17 @@ export const skippedTests = {
     DataGrid: ['SignalRService'],
     Scheduler: ['Templates'],
     Map: ['ProvidersAndTypes', 'Markers', 'Routes'],
-    Gantt: ['TaskTemplate', 'Validation'],
   },
   React: {
     DataGrid: ['SignalRService'],
     // NOTE: 'GroupByDate' demo has problems with rendering
     Scheduler: ['GroupByDate', 'Templates'],
     Map: ['ProvidersAndTypes', 'Markers', 'Routes'],
-    Gantt: ['Validation'],
   },
   Vue: {
     Common: ['PopupAndNotificationsOverview'],
-    Charts: ['PointsAggregation'],
     Scheduler: ['Templates'],
     DataGrid: ['SignalRService'],
     Map: ['ProvidersAndTypes', 'Markers', 'Routes'],
-    Gantt: ['Validation'],
   },
 };
