9de9ce92bd460ce1361295f2e7dda8a618508fad
Eldar Iusupzhanov <ferrarijed@gmail.com>
2026-01-15 21:51:31 +0800

    fix & test

--- packages/devextreme/js/__internal/scheduler/workspaces/m_work_space.ts
+++ packages/devextreme/js/__internal/scheduler/workspaces/m_work_space.ts
@@ -1407,29 +1407,20 @@ class SchedulerWorkSpace extends Widget<WorkspaceOptionsInternal> {
     return (this.$element() as any).find(`.${GROUP_HEADER_CLASS}`);
   }
 
-  _getScrollCoordinates(hours, minutes, date, groupIndex?: any, allDay?: any) {
+  _getScrollCoordinates(date, groupIndex?: any, allDay?: any) {
     const currentDate = date || new Date(this.option('currentDate'));
-    const startDayHour = this.option('startDayHour');
-    const endDayHour = this.option('endDayHour');
-    const viewOffset = this.option('viewOffset');
 
-    if (viewOffset === 0) {
-      if (hours < startDayHour) {
-        hours = startDayHour;
-      }
+    const cell = this.viewDataProvider.findGlobalCellPosition(currentDate, groupIndex, allDay, true);
 
-      if (hours >= endDayHour) {
-        hours = endDayHour - 1;
-      }
+    if (!cell) {
+      return undefined;
     }
 
-    currentDate.setHours(hours, minutes, 0, 0);
-
-    const cell = this.viewDataProvider.findGlobalCellPosition(currentDate, groupIndex, allDay);
+    currentDate.setHours(cell.cellData.startDate.getHours(), currentDate.getMinutes(), 0, 0);
 
     return this.virtualScrollingDispatcher.calculateCoordinatesByDataAndPosition(
-      cell?.cellData,
-      cell?.position,
+      cell.cellData,
+      cell.position,
       currentDate,
       isDateAndTimeView(this.type as any),
       this.viewDirection === 'vertical',
@@ -1836,7 +1827,11 @@ class SchedulerWorkSpace extends Widget<WorkspaceOptionsInternal> {
       : 0;
     const isScrollToAllDay = allDay && this.isAllDayPanelVisible;
 
-    const coordinates = this._getScrollCoordinates(date.getHours(), date.getMinutes(), date, groupIndex, isScrollToAllDay);
+    const coordinates = this._getScrollCoordinates(date, groupIndex, isScrollToAllDay);
+
+    if (!coordinates) {
+      return;
+    }
 
     const scrollable = this.getScrollable();
     const $scrollable = scrollable.$element();
@@ -1869,13 +1864,10 @@ class SchedulerWorkSpace extends Widget<WorkspaceOptionsInternal> {
 
   _isValidScrollDate(date, throwWarning = true) {
     const viewOffset = this.option('viewOffset') as number;
-    const min = this.getStartViewDate();
-    const max = this.getEndViewDate();
+    const min = new Date(this.getStartViewDate().getTime() + viewOffset);
+    const max = new Date(this.getEndViewDate().getTime() + viewOffset);
 
-    const extendedMin = new Date(min.getTime() - viewOffset);
-    const extendedMax = new Date(max.getTime() + viewOffset);
-
-    if (date < extendedMin || date > extendedMax) {
+    if (date < min || date > max) {
       throwWarning && errors.log('W1008', date);
       return false;
     }
