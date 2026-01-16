7d84cb72728f56c38703f80b10322f3a21deb64b
Mikhail Preyskurantov <5574159+mpreyskurantov@users.noreply.github.com>
2026-01-16 11:29:28 +0200

    lint

--- apps/demos/Demos/DataGrid/BatchUpdateRequest/ReactJs/App.js
+++ apps/demos/Demos/DataGrid/BatchUpdateRequest/ReactJs/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import DataGrid, { Column, Editing, Pager } from 'devextreme-react/data-grid';
 import { createStore } from 'devextreme-aspnet-data-nojquery';
 import 'whatwg-fetch';
+
 const BASE_PATH = 'https://js.devexpress.com/Demos/NetCore';
 const URL = `${BASE_PATH}/api/DataGridBatchUpdateWebApi`;
 async function fetchAntiForgeryToken() {
