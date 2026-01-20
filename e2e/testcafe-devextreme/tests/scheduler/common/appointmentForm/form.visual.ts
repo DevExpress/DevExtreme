80d9e32dd53a8bac5326837f0fc11e6b00e62bc4
Eldar Iusupzhanov <84278206+Tucchhaa@users.noreply.github.com>
2026-01-20 19:15:38 +0800

    Scheduler - Appointment Form - Fix KBN issues (#32225)
    Co-authored-by: Sergio Bur <sjbur.dev@gmail.com>

--- e2e/testcafe-devextreme/tests/scheduler/common/appointmentForm/form.visual.ts
+++ e2e/testcafe-devextreme/tests/scheduler/common/appointmentForm/form.visual.ts
@@ -300,3 +300,40 @@ test.meta({ browserSize: [1500, 1500] })('appointment main form with opened star
     currentDate: new Date(2021, 2, 25),
   });
 });
+
+test.meta({ browserSize: [1500, 1500] })('Recurrence settings button should have correct focus state', async (t) => {
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+  const appointment = {
+    text: 'Appointment',
+    startDate: new Date('2021-04-26T16:30:00.000Z'),
+    endDate: new Date('2021-04-26T18:30:00.000Z'),
+    allDay: false,
+    recurrenceRule: 'FREQ=WEEKLY;BYDAY=MO,TH;COUNT=10',
+  };
+
+  const scheduler = new Scheduler(SCHEDULER_SELECTOR);
+  const appointmentPopup = await scheduler.openAppointmentPopup(t, appointment, true);
+
+  await t
+    .click(appointmentPopup.repeatEditor.element)
+    .pressKey('tab');
+
+  await testScreenshot(
+    t,
+    takeScreenshot,
+    'scheduler__appointment__recurrence-settings-button__focus-state.png',
+    { element: appointmentPopup.contentElement },
+  );
+
+  await t
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => {
+  await createWidget('dxScheduler', {
+    dataSource: [],
+    views: ['week'],
+    currentView: 'week',
+    currentDate: new Date(2021, 2, 25),
+  });
+});
