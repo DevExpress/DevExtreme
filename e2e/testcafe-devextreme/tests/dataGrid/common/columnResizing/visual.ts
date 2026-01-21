47127e97820ca4e3170958e36b113111b9a2a7cb
anna.shakhova <anna.shakhova@devexpress.com>
2026-01-21 16:15:59 +0100

    DataGrid: prevent column resizing in band area

--- e2e/testcafe-devextreme/tests/dataGrid/common/columnResizing/visual.ts
+++ e2e/testcafe-devextreme/tests/dataGrid/common/columnResizing/visual.ts
@@ -1,12 +1,12 @@
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import DataGrid from 'devextreme-testcafe-models/dataGrid';
 
-import url from '../../../helpers/getPageUrl';
-import { createWidget } from '../../../helpers/createWidget';
-import { testScreenshot } from '../../../helpers/themeUtils';
+import url from '../../../../helpers/getPageUrl';
+import { createWidget } from '../../../../helpers/createWidget';
+import { testScreenshot } from '../../../../helpers/themeUtils';
 
 fixture.disablePageReloads`Column resizing`
-  .page(url(__dirname, '../../container.html'));
+  .page(url(__dirname, '../../../container.html'));
 
 test.meta({ unstable: true })('column separator should starts from the parent', async (t) => {
   const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
@@ -74,55 +74,3 @@ test.meta({ unstable: true })('column separator should starts from the parent', 
     columns: ['GDP_Total', 'Population_Urban'],
   }, 'Area'],
 }));
-
-// T1314667
-test('DataGrid – Resize indicator is moved when resizing a grouped column if showWhenGrouped is set to true', async (t) => {
-  const dataGrid = new DataGrid('#container');
-
-  await t.expect(dataGrid.isReady()).ok();
-
-  await dataGrid.resizeHeader(3, 30, false);
-
-  await t
-    .expect(dataGrid.getHeaders().getHeaderRow(1).getHeaderCell(0).element.clientWidth)
-    .within(128, 130);
-}).before(async () => {
-  await createWidget('dxDataGrid', {
-    dataSource: [{
-      ID: 1,
-      Country: 'Brazil',
-      Area: 8515767,
-      Population_Urban: 0.85,
-      Population_Rural: 0.15,
-      Population_Total: 205809000,
-    }],
-    keyExpr: 'ID',
-    allowColumnResizing: true,
-    columnResizingMode: 'widget',
-    width: 500,
-    columns: [
-      {
-        dataField: 'ID',
-        fixed: true,
-        allowReordering: false,
-        width: 50,
-      },
-
-      {
-        caption: 'Population',
-        columns: [
-          {
-            dataField: 'Country',
-            showWhenGrouped: true,
-            width: 100,
-            groupIndex: 0,
-          },
-          { dataField: 'Area' },
-          { dataField: 'Population_Total' },
-          { dataField: 'Population_Urban' },
-          { dataField: 'Population_Rural' },
-        ],
-      },
-    ],
-  });
-});
