1f7db346217722e5541660e4b3360960abed5d59
Alyar <>
2026-01-22 22:40:36 +0400

    keyboardNavigation: unskip tests

--- e2e/testcafe-devextreme/tests/dataGrid/common/keyboardNavigation/keyboardNavigation.visual.ts
+++ e2e/testcafe-devextreme/tests/dataGrid/common/keyboardNavigation/keyboardNavigation.visual.ts
@@ -497,7 +497,7 @@ test.meta({ unstable: true })('Navigate to last cell in the last row when virtua
   },
 }));
 
-test.meta({ unstable: true })('Navigate to first cell in the first row when virtual scrolling is enabled', async (t) => {
+test('Navigate to first cell in the first row when virtual scrolling is enabled', async (t) => {
   // arrange
   const dataGrid = new DataGrid('#container');
   const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
