9ad7989be118eacf7c2c0e5f04b482f14897a295
Raushen <eylau-andrew@mail.ru>
2026-01-26 21:06:33 +0200

    TODO mark

--- packages/devextreme/js/__internal/grids/grid_core/ai_column/__tests__/ai_column.integration.test.ts
+++ packages/devextreme/js/__internal/grids/grid_core/ai_column/__tests__/ai_column.integration.test.ts
@@ -4749,7 +4749,7 @@ describe('AI data', () => {
   });
 
   describe('when repaintChangesOnly is enabled', () => {
-    /* We skip this test for now until we refactor columnsChanged callback */
+    /* TODO: We skip this test for now until we refactor columnsChanged callback */
     it.skip('should render only AI cells', async () => {
       const { component } = await createDataGrid({
         dataSource: items,
