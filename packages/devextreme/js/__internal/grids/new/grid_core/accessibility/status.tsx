b4ef8d0b3b826b64f3739220129aeb86ed393a63
pharret31 <pharret31@gmail.com>
2026-01-26 13:53:56 +0100

    revert grid changes

--- packages/devextreme/js/__internal/grids/new/grid_core/accessibility/status.tsx
+++ packages/devextreme/js/__internal/grids/new/grid_core/accessibility/status.tsx
@@ -4,7 +4,7 @@ import { CLASSES as BASE_CLASSES } from '../const';
 
 const CLASSES = {
   ...BASE_CLASSES,
-  container: 'dx-screen-reader-only',
+  container: 'dx-gridbase-a11y-status-container',
 };
 
 export interface A11yStatusContainerComponentProps {
