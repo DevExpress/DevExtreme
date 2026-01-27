6ca308bb4c4915f6b1392278976524f5df92b88b
Raushen <eylau-andrew@mail.ru>
2026-01-27 21:56:09 +0200

    T1320031

---
+++ packages/devextreme/js/__internal/grids/grid_core/column_headers/__tests__/m_column_headers.test.ts
@@ -0,0 +1,85 @@
+import {
+  afterEach, beforeEach, describe, expect, it, jest,
+} from '@jest/globals';
+import type { dxElementWrapper } from '@js/core/renderer';
+import $ from '@js/core/renderer';
+import type { Properties as DataGridProperties } from '@js/ui/data_grid';
+import DataGrid from '@js/ui/data_grid';
+import { DataGridModel } from '@ts/grids/data_grid/__tests__/__mock__/model/data_grid';
+
+const SELECTORS = {
+  gridContainer: '#gridContainer',
+};
+
+const GRID_CONTAINER_ID = 'gridContainer';
+
+const createDataGrid = async (
+  options: DataGridProperties = {},
+): Promise<{
+  $container: dxElementWrapper;
+  component: DataGridModel;
+  instance: DataGrid;
+}> => new Promise((resolve) => {
+  const $container = $('<div>')
+    .attr('id', GRID_CONTAINER_ID)
+    .appendTo(document.body);
+
+  const dataGridOptions: DataGridProperties = {
+    keyExpr: 'id',
+    ...options,
+  };
+
+  const instance = new DataGrid($container.get(0) as HTMLDivElement, dataGridOptions);
+  const component = new DataGridModel($container.get(0) as HTMLElement);
+
+  jest.runAllTimers();
+
+  resolve({
+    $container,
+    component,
+    instance,
+  });
+});
+
+const beforeTest = (): void => {
+  jest.useFakeTimers();
+};
+
+const afterTest = (): void => {
+  const $container = $(SELECTORS.gridContainer);
+  const dataGrid = ($container as any).dxDataGrid('instance') as DataGrid;
+
+  dataGrid?.dispose();
+  $container.remove();
+  jest.clearAllMocks();
+  jest.useRealTimers();
+};
+
+describe('Column Headers', () => {
+  beforeEach(beforeTest);
+  afterEach(afterTest);
+
+  describe('headerCellTemplate', () => {
+    it('should apply right alighnment to number column when headerCellTemplate is used', async () => {
+      const { component } = await createDataGrid({
+        dataSource: [],
+        showBorders: true,
+        headerFilter: {
+          visible: true,
+        },
+        columns: [
+          {
+            dataField: 'test',
+            dataType: 'number',
+            headerCellTemplate(headerElement) {
+              $('<span>')
+                .text('Test')
+                .appendTo(headerElement);
+            },
+          },
+        ],
+      });
+      expect(component.getHeaderCell(0).getAlignment()).toBe('right');
+    });
+  });
+});
