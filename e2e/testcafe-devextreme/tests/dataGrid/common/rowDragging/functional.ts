2bf075985610e882051996343495f72347e176df
Alyar <>
2026-01-27 15:45:01 +0400

    Update etalon. And move the rowDragging tests to a separate folder.

--- e2e/testcafe-devextreme/tests/dataGrid/common/rowDragging/functional.ts
+++ e2e/testcafe-devextreme/tests/dataGrid/common/rowDragging/functional.ts
@@ -1,12 +1,10 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { ClientFunction, Selector } from 'testcafe';
-import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import DataGrid, { CLASS as DataGridClassNames } from 'devextreme-testcafe-models/dataGrid';
 import { ClassNames } from 'devextreme-testcafe-models/dataGrid/classNames';
-import { MouseUpEvents, MouseAction } from '../../../helpers/mouseUpEvents';
-import url from '../../../helpers/getPageUrl';
-import { createWidget } from '../../../helpers/createWidget';
-import { testScreenshot } from '../../../helpers/themeUtils';
+import { MouseUpEvents, MouseAction } from '../../../../helpers/mouseUpEvents';
+import url from '../../../../helpers/getPageUrl';
+import { createWidget } from '../../../../helpers/createWidget';
 
 const CLASS = { ...DataGridClassNames, ...ClassNames };
 
@@ -55,8 +53,8 @@ const generateData = (rowCount, columnCount): Record<string, unknown>[] => {
   return items;
 };
 
-fixture`Row dragging`
-  .page(url(__dirname, '../../container.html'));
+fixture`Row dragging.Functional`
+  .page(url(__dirname, '../../../container.html'));
 
 // T903351
 test('The placeholder should appear when a cross-component dragging rows after scrolling the window', async (t) => {
@@ -783,44 +781,5 @@ test('Item should appear in a correct spot when dragging to a different page wit
       }, { dependencies: { items } }),
     },
     showBorders: true,
-  });
-});
-
-// T1179218
-test('Rows should appear correctly during dragging when virtual scrolling is enabled and rowDragging.dropFeedbackMode = "push"', async (t) => {
-  const dataGrid = new DataGrid('#container');
-  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
-
-  // drag the row down
-  await dataGrid.moveRow(0, 30, 150, true);
-  await dataGrid.moveRow(0, 30, 350);
-
-  // waiting for autoscrolling
-  await t.wait(2000);
-
-  // drag the row up
-  await dataGrid.moveRow(0, 30, 100);
-
-  // waiting for autoscrolling
-  await t.wait(1000);
-
-  await testScreenshot(t, takeScreenshot, 'T1179218-virtual-scrolling-dragging-row.png', { element: dataGrid.element });
-  await t
-    .expect(compareResults.isValid())
-    .ok(compareResults.errorMessages());
-}).before(async (t) => {
-  await t.maximizeWindow();
-  return createWidget('dxDataGrid', {
-    height: 440,
-    keyExpr: 'id',
-    scrolling: {
-      mode: 'virtual',
-    },
-    dataSource: [...new Array(100)].fill(null).map((_, index) => ({ id: index })),
-    columns: ['id'],
-    rowDragging: {
-      allowReordering: true,
-      dropFeedbackMode: 'push',
-    },
   });
 });
