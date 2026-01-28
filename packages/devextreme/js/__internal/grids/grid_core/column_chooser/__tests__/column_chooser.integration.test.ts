8385136ffaffbcd336688419e13c1d6e4fb6a29c
Andrey Dolzhikov <16618553+Raushen@users.noreply.github.com>
2026-01-28 18:12:01 +0200

    T1320031 - DataGrid - Header filter icon is misaligned when using headerCellTemplate (#32335)

--- packages/devextreme/js/__internal/grids/grid_core/column_chooser/__tests__/column_chooser.integration.test.ts
+++ packages/devextreme/js/__internal/grids/grid_core/column_chooser/__tests__/column_chooser.integration.test.ts
@@ -1,65 +1,20 @@
 import {
   afterEach, beforeEach, describe, expect, it, jest,
 } from '@jest/globals';
-import type { dxElementWrapper } from '@js/core/renderer';
-import $ from '@js/core/renderer';
-import type { Properties as DataGridProperties } from '@js/ui/data_grid';
-import DataGrid from '@js/ui/data_grid';
 import errors from '@js/ui/widget/ui.errors';
-import { DataGridModel } from '@ts/grids/data_grid/__tests__/__mock__/model/data_grid';
 
-const SELECTORS = {
-  gridContainer: '#gridContainer',
-};
-
-const GRID_CONTAINER_ID = 'gridContainer';
-
-const createDataGrid = async (
-  options: DataGridProperties = {},
-): Promise<{
-  $container: dxElementWrapper;
-  component: DataGridModel;
-  instance: DataGrid;
-}> => new Promise((resolve) => {
-  const $container = $('<div>')
-    .attr('id', GRID_CONTAINER_ID)
-    .appendTo(document.body);
-
-  const dataGridOptions: DataGridProperties = {
-    keyExpr: 'id',
-    ...options,
-  };
-
-  const instance = new DataGrid($container.get(0) as HTMLDivElement, dataGridOptions);
-  const component = new DataGridModel($container.get(0) as HTMLElement);
-
-  jest.runAllTimers();
-
-  resolve({
-    $container,
-    component,
-    instance,
-  });
-});
-
-const beforeTest = (): void => {
-  jest.useFakeTimers();
-  jest.spyOn(errors, 'log').mockImplementation(jest.fn());
-  jest.spyOn(errors, 'Error').mockImplementation(() => ({}));
-};
-
-const afterTest = (): void => {
-  const $container = $(SELECTORS.gridContainer);
-  const dataGrid = ($container as any).dxDataGrid('instance') as DataGrid;
-
-  dataGrid.dispose();
-  $container.remove();
-  jest.clearAllMocks();
-  jest.useRealTimers();
-};
+import {
+  afterTest,
+  beforeTest,
+  createDataGrid,
+} from '../../__tests__/__mock__/helpers/utils';
 
 describe('Bugs', () => {
-  beforeEach(beforeTest);
+  beforeEach(() => {
+    beforeTest();
+    jest.spyOn(errors, 'log').mockImplementation(jest.fn());
+    jest.spyOn(errors, 'Error').mockImplementation(() => ({}));
+  });
   afterEach(afterTest);
 
   describe('T1311329 - DataGrid - Column chooser hides a banded column on using search and recursive selection', () => {
