bc017cbaa208a071397af6077716cf966d6bc50d
Mikhail Preyskurantov <5574159+mpreyskurantov@users.noreply.github.com>
2026-01-29 01:06:12 +0200

    export Properties, where was not export keyword, (3) private files (dom_component.d.ts, ui.drop_down_editor.d.ts, overlay.d.ts)

--- packages/devextreme/ts/dx.all.d.ts
+++ packages/devextreme/ts/dx.all.d.ts
@@ -698,7 +698,7 @@ declare module DevExpress {
     /**
      * @deprecated Attention! This type is for internal purposes only. If you used it previously, please submit a ticket to our {@link https://supportcenter.devexpress.com/ticket/create Support Center}. We will check if there is an alternative solution.
      */
-    type Properties = DOMComponentOptions<DOMComponentInstance>;
+    export type Properties = DOMComponentOptions<DOMComponentInstance>;
   }
 
   /**
@@ -16808,7 +16808,7 @@ declare module DevExpress.ui {
     /**
      * @deprecated Attention! This type is for internal purposes only. If you used it previously, please submit a ticket to our {@link https://supportcenter.devexpress.com/ticket/create Support Center}. We will check if there is an alternative solution.
      */
-    type Properties = dxDropDownEditorOptions<DropDownEditorInstance>;
+    export type Properties = dxDropDownEditorOptions<DropDownEditorInstance>;
   }
   /**
    * [descr:dxDropDownEditorOptions]
