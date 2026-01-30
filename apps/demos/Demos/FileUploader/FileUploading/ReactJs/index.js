0fed61df20d1afde6ec20b388ca49bdd5efaad03
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2026-01-30 16:33:55 +0100

    fix lint warnings

--- apps/demos/Demos/FileUploader/FileUploading/ReactJs/index.js
+++ apps/demos/Demos/FileUploader/FileUploading/ReactJs/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // eslint-disable-next-line import/no-unresolved
 import 'anti-forgery';
-
 import App from './App.js';
 
-ReactDOM.render(
-  <App />, 
-  document.getElementById('app'),
-);
+ReactDOM.render(<App />, document.getElementById('app'));
