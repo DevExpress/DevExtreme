8739a7cfcfc7df590aff31212aa99bed0eb91eb4
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2026-02-04 23:59:20 +0100

    add LCP bundler-plugin

--- packages/devextreme/build/gulp/npm.js
+++ packages/devextreme/build/gulp/npm.js
@@ -107,7 +107,7 @@ const sources = (src, dist, distGlob) => (() => merge(
         .pipe(gulp.dest(dist)),
 
     gulp
-        .src('build/npm-bin/*.js')
+        .src('build/npm-bin/**/*.js')
         .pipe(eol('\n'))
         .pipe(gulp.dest(`${dist}/bin`)),
 
