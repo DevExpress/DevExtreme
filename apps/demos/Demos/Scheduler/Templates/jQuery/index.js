454927948716e9a30d9a3fbbb79596978072e11d
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2026-02-05 23:56:49 +0400

    Accordion, Chart, Scheduler Demos: Fix 'nested-interactive',  'empty-table-header', 'image-alt' accessibility violations

--- apps/demos/Demos/Scheduler/Templates/jQuery/index.js
+++ apps/demos/Demos/Scheduler/Templates/jQuery/index.js
@@ -136,7 +136,7 @@ $(() => {
     return $(`
       <div class='movie-preview'>
         <div class='movie-preview-image'>
-          <img src='${movie.image}' />
+          <img src='${movie.image}' alt='${movie.text}'/>
         </div>
         <div class='movie-details'>
           <div class='title'>${movie.text}</div>
@@ -154,7 +154,7 @@ $(() => {
     return $(`
       <div class='movie-info'>
         <div class='movie-preview-image'>
-          <img src='${movie.image}' />
+          <img src='${movie.image}' alt='${movie.text}'/>
         </div>
         <div class='movie-details'>
           <div class='title'>${movie.text} (${movie.year})</div>
