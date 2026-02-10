9a3c6540eecba3789362cfa2af02145be3e0b8e7
marker dao ® <youdontknow@marker-dao.eth>
2026-02-11 00:47:43 +0100

    fix(tests): Update with metaTemplates

--- packages/devextreme/js/__internal/core/ai_integration/commands/base.test.ts
+++ packages/devextreme/js/__internal/core/ai_integration/commands/base.test.ts
@@ -13,6 +13,7 @@ import { RequestManager } from '@ts/core/ai_integration/core/request_manager';
 import { Provider } from '@ts/core/ai_integration/test_utils/provider_mock';
 
 jest.mock('@ts/core/ai_integration/templates/index', () => ({
+  metaTemplates: {},
   templates: {
     'test-template-name': {
       system: 'System test template with {{first}}',
