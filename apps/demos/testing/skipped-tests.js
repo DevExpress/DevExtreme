9629b24475280a80237c3b9a35fe255d8ac47301
marker dao ® <youdontknow@marker-dao.eth>
2026-02-10 16:51:05 +0100

    Map: Fix skipped Testcafe tests

--- apps/demos/testing/skipped-tests.js
+++ apps/demos/testing/skipped-tests.js
@@ -1,20 +1,14 @@
 export const skippedTests = {
-  jQuery: {
-    Map: ['ProvidersAndTypes', 'Markers', 'Routes'],
-  },
   Angular: {
     Common: ['PopupAndNotificationsOverview'],
     Scheduler: ['Templates'],
-    Map: ['ProvidersAndTypes', 'Markers', 'Routes'],
   },
   React: {
     // NOTE: 'GroupByDate' demo has problems with rendering
     Scheduler: ['GroupByDate', 'Templates'],
-    Map: ['ProvidersAndTypes', 'Markers', 'Routes'],
   },
   Vue: {
     Common: ['PopupAndNotificationsOverview'],
     Scheduler: ['Templates'],
-    Map: ['ProvidersAndTypes', 'Markers', 'Routes'],
   },
 };
