68cbac186352260043c2da18f18a3bd1b230d9e6
marker dao ® <youdontknow@marker-dao.eth>
2026-02-11 17:50:27 +0100

    Popup and FileUploader: Fix skipped Testcafe tests

--- apps/demos/testing/skipped-tests.js
+++ apps/demos/testing/skipped-tests.js
@@ -1,20 +1,24 @@
 export const skippedTests = {
   jQuery: {
+    // WebGL initialization error at the provider. There is no point in adding a mask
     Map: ['ProvidersAndTypes', 'Markers', 'Routes'],
   },
   Angular: {
-    Common: ['PopupAndNotificationsOverview'],
+    // Common: ['PopupAndNotificationsOverview'],
     Scheduler: ['Templates'],
+    // WebGL initialization error at the provider. There is no point in adding a mask
     Map: ['ProvidersAndTypes', 'Markers', 'Routes'],
   },
   React: {
     // NOTE: 'GroupByDate' demo has problems with rendering
     Scheduler: ['GroupByDate', 'Templates'],
+    // WebGL initialization error at the provider. There is no point in adding a mask
     Map: ['ProvidersAndTypes', 'Markers', 'Routes'],
   },
   Vue: {
-    Common: ['PopupAndNotificationsOverview'],
+    // Common: ['PopupAndNotificationsOverview'],
     Scheduler: ['Templates'],
+    // WebGL initialization error at the provider. There is no point in adding a mask
     Map: ['ProvidersAndTypes', 'Markers', 'Routes'],
   },
 };
