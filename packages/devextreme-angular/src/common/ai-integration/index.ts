3d7db2b7cdba46565ecfbfc0a1399b114937ec9d
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2026-02-17 23:17:14 +0100

    regenerate devextreme-angular

--- packages/devextreme-angular/src/common/ai-integration/index.ts
+++ packages/devextreme-angular/src/common/ai-integration/index.ts
@@ -1,1 +1,2 @@
-export type { AIIntegration, AIIntegrationOptions, AIProvider, AIResponse, GenerateGridColumnCommandResponse, Prompt, RequestParams, RequestParamsData, Response } from 'devextreme/common/ai-integration';
+export { AIIntegration } from 'devextreme/common/ai-integration';
+export type { AIIntegrationOptions, AIProvider, AIResponse, GenerateGridColumnCommandResponse, Prompt, RequestParams, RequestParamsData, Response } from 'devextreme/common/ai-integration';
