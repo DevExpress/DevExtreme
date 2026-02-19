97cdcfb4c266686b362986657b8c4c32af6ce670
Sergio Bur <sjbur.dev@gmail.com>
2026-02-19 11:41:55 +0100

    test: remove skipped scheduler tests

--- apps/demos/testing/skipped-tests.js
+++ apps/demos/testing/skipped-tests.js
@@ -5,18 +5,15 @@ export const skippedTests = {
   Angular: {
     Common: ['PopupAndNotificationsOverview'],
     DataGrid: ['SignalRService'],
-    Scheduler: ['Templates'],
     Map: ['ProvidersAndTypes', 'Markers', 'Routes'],
   },
   React: {
     DataGrid: ['SignalRService'],
     // NOTE: 'GroupByDate' demo has problems with rendering
-    Scheduler: ['GroupByDate', 'Templates'],
     Map: ['ProvidersAndTypes', 'Markers', 'Routes'],
   },
   Vue: {
     Common: ['PopupAndNotificationsOverview'],
-    Scheduler: ['Templates'],
     DataGrid: ['SignalRService'],
     Map: ['ProvidersAndTypes', 'Markers', 'Routes'],
   },
