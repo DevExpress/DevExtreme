fd57ba886403bb5f1d9ffdee7abed28b8868f8e5
Sergio Bur <sjbur.dev@gmail.com>
2026-02-20 13:36:16 +0100

    test: add test for delete button tabindex

--- packages/devextreme/js/__internal/scheduler/__tests__/appointment_tooltip.test.ts
+++ packages/devextreme/js/__internal/scheduler/__tests__/appointment_tooltip.test.ts
@@ -44,6 +44,31 @@ describe('Appointment tooltip behavior', () => {
     expect(POM.tooltip.isVisible()).toBe(false);
   });
 
+  it('delete button in tooltip should not be focusable using tab', async () => {
+    const { POM } = await createScheduler({
+      dataSource: [
+        {
+          text: 'Apt1',
+          startDate: new Date(2017, 4, 22, 9, 30),
+          endDate: new Date(2017, 4, 22, 10, 30),
+        },
+        {
+          text: 'Apt2',
+          startDate: new Date(2017, 4, 22, 9, 30),
+          endDate: new Date(2017, 4, 22, 10, 30),
+        },
+      ],
+      views: [{ type: 'month', maxAppointmentsPerCell: 1 }],
+      currentView: 'month',
+      currentDate: new Date(2017, 4, 22),
+      height: 600,
+    });
+
+    POM.getCollectorButton().click();
+
+    expect(POM.tooltip.getDeleteButton().getAttribute('tabindex')).toBe('-1');
+  });
+
   it('should not delete disabled appointment by Delete key when focused in tooltip from collector', async () => {
     const { POM } = await createScheduler({
       dataSource: [
