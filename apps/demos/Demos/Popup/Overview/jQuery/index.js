758671483caf9f06f9dd9df08dc293e65fd578fe
marker dao ® <youdontknow@marker-dao.eth>
2026-02-24 18:13:51 +0100

    fix(m_popup.ts): Get rid of replacing in Vue

--- apps/demos/Demos/Popup/Overview/jQuery/index.js
+++ apps/demos/Demos/Popup/Overview/jQuery/index.js
@@ -1,50 +1,13 @@
 $(() => {
   let employee = null;
-  const popupContentTemplate = function () {
-    return $('<div>').append(
-      $(`<p>Full Name: <span>${employee.FirstName}</span>
-                         <span>${employee.LastName}</span></p>`),
-      $(`<p>Birth Date: <span>${employee.BirthDate}</span></p>`),
-      $(`<p>Address: <span>${employee.Address}</span></p>`),
-      $(`<p>Hire Date: <span>${employee.HireDate}</span></p>`),
-      $(`<p>Position: <span>${employee.Position}</span></p>`),
-    );
-  };
-  const popup = $('#popup').dxPopup({
-    contentTemplate: popupContentTemplate,
-    width: 300,
-    height: 280,
-    container: '.dx-viewport',
-    showTitle: true,
-    title: 'Information',
-    visible: false,
-    dragEnabled: false,
-    hideOnOutsideClick: true,
-    showCloseButton: false,
-    position: {
-      at: 'bottom',
-      my: 'center',
-      collision: 'fit',
-    },
-    toolbarItems: [{
-      locateInMenu: 'always',
-      widget: 'dxButton',
-      toolbar: 'top',
-      options: {
-        text: 'More info',
-        onClick() {
-          const message = `More info about ${employee.FirstName} ${employee.LastName}`;
 
-          DevExpress.ui.notify({
-            message,
-            position: {
-              my: 'center top',
-              at: 'center top',
-            },
-          }, 'success', 3000);
-        },
-      },
-    }, {
+  const popup = $('#popup').dxPopup({
+    width: 450,
+    height: 600,
+    container: '.dx-viewport',
+    visible: true,
+    dragEnabled: false,
+    toolbarItems: [{
       widget: 'dxButton',
       toolbar: 'bottom',
       location: 'before',
@@ -95,7 +58,6 @@ $(() => {
           onClick() {
             employee = currentEmployee;
             popup.option({
-              contentTemplate: () => popupContentTemplate(),
               'position.of': `#image${employee.ID}`,
             });
             popup.show();
