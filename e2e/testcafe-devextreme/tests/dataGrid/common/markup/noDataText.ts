d3e088c73f1b7ba529f40183a165b990dd1fbd11
Mark Allen Ramirez <markallenramirez@gmail.com>
2026-03-03 17:59:54 +0800

    test

--- e2e/testcafe-devextreme/tests/dataGrid/common/markup/noDataText.ts
+++ e2e/testcafe-devextreme/tests/dataGrid/common/markup/noDataText.ts
@@ -1,5 +1,6 @@
 import { createScreenshotsComparer } from 'devextreme-screenshot-comparer';
 import DataGrid from 'devextreme-testcafe-models/dataGrid';
+import List from 'devextreme-testcafe-models/list';
 import url from '../../../../helpers/getPageUrl';
 import { createWidget } from '../../../../helpers/createWidget';
 import { testScreenshot } from '../../../../helpers/themeUtils';
@@ -8,6 +9,7 @@ fixture.disablePageReloads`No Data`
   .page(url(__dirname, '../../../container.html'));
 
 const GRID_CONTAINER = '#container';
+const OVERLAY_SELECTOR = '.dx-overlay-wrapper';
 
 test('The noDataText element should be centered (T1178289)', async (t) => {
   const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
@@ -29,5 +31,52 @@ test('The noDataText element should be centered (T1178289)', async (t) => {
       enabled: true,
       storageKey: 'testStorageKey',
     },
+  });
+});
+
+test('The noDataText element should be rendered when a lookup column is filtered (T1293839)', async (t) => {
+  // arrange
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+  const dataGrid = new DataGrid(GRID_CONTAINER);
+  const filterRow = dataGrid.getHeaders().getFilterRow();
+  const nameFilterCell = filterRow.getFilterCell(0);
+  const nameFilterEditor = nameFilterCell.getEditorInput();
+  const lookupFilterCell = filterRow.getFilterCell(1);
+
+  // act
+  await t.click(lookupFilterCell.element);
+  const lookupList = new List(OVERLAY_SELECTOR);
+  const lookupItem = lookupList.getItem(1);
+  await t.click(lookupItem.element);
+  
+  await t.typeText(nameFilterEditor.element, 'test');
+
+  await dataGrid.isReady();
+
+  // assert
+  await testScreenshot(t, takeScreenshot, 'T1293839-grid-no-data-text-rendered.png', { element: dataGrid.element });
+  await t
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => {
+  await createWidget('dxDataGrid', {
+    dataSource: [
+      { ID: 1, Name: 'John', Lookup: 1 },
+      { ID: 2, Name: 'Jane', Lookup: 2 },
+    ],
+    keyExpr: 'ID',
+    columns: ['Name', {
+      dataField: 'Lookup',
+      lookup: {
+        dataSource: [
+          { ID: 1, Text: 'Item 1' },
+          { ID: 2, Text: 'Item 2' },
+        ],
+        valueExpr: 'ID',
+        displayExpr: 'Text',
+      },
+    }],
+    showBorders: true,
+    filterRow: { visible: true },
   });
 });
