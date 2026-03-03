9e83ccee355bbe2fa1b0551b32656e777a7ac97b
Danil Mirgaev <danil.mirgaev@devexpress.com>
2026-03-03 11:17:49 +0400

    decompose _getNextCell method

--- packages/devextreme/js/__internal/grids/grid_core/keyboard_navigation/types.ts
+++ packages/devextreme/js/__internal/grids/grid_core/keyboard_navigation/types.ts
@@ -1,1 +1,5 @@
 export type NavigationDirection = 'next' | 'previous' | 'nextInRow' | 'previousInRow';
+
+export type NavigationDirectionExtended = NavigationDirection | 'upArrow' | 'downArrow';
+
+export type NavigationElementType = 'cell' | 'row';
