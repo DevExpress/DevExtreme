80d518124d0dbba9b23f9768f13a438abba52ec6
marker dao ® <youdontknow@marker-dao.eth>
2026-03-04 21:11:07 +0100

    fix(runners): Set offset 1

--- e2e/testcafe-devextreme/runner.ts
+++ e2e/testcafe-devextreme/runner.ts
@@ -272,7 +272,7 @@ async function main() {
                 window.getSelection()?.removeAllRanges();
               }).with({ boundTestRun: t })();
 
-              await t.hover('html', { offsetX: -9999, offsetY: -9999 });
+              await t.hover('html', { offsetX: 1, offsetY: 1 });
 
               const [width, height] = meta?.browserSize || DEFAULT_BROWSER_SIZE;
               await t.resizeWindow(width, height);
