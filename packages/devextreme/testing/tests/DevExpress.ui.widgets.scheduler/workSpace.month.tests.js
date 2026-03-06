558e789b3f2e7e690ef7b50431d2e1737b1bc212
Sergio Bur <sjbur.dev@gmail.com>
2026-03-06 17:17:48 +0100

    fix: fix tests

--- packages/devextreme/testing/tests/DevExpress.ui.widgets.scheduler/workSpace.month.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.widgets.scheduler/workSpace.month.tests.js
@@ -249,7 +249,7 @@ module('Work Space Month', () => {
                 firstDayOfWeek: 1,
             });
 
-            const $cell = this.instance._getCells().eq(14);
+            const $cell = this.instance.getCells().eq(14);
 
             assert.deepEqual($cell.data('dxCellData'), {
                 startDate: new Date(2016, 2, 14, 5, 0),
