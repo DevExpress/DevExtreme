6b66dc89d435522ba1e354cf51aad6fb548ed1a5
Sergio Bur <sjbur.dev@gmail.com>
2026-03-06 15:25:13 +0100

    fix: fix error

--- packages/devextreme/js/__internal/scheduler/workspaces/m_work_space.ts
+++ packages/devextreme/js/__internal/scheduler/workspaces/m_work_space.ts
@@ -700,7 +700,7 @@ class SchedulerWorkSpace extends Widget<WorkspaceOptionsInternal> {
     return config;
   }
 
-  protected createCrossScrollingConfig({ onScroll }): any {
+  createCrossScrollingConfig({ onScroll }): any {
     return {
       direction: 'both',
       onScroll: (event) => {
