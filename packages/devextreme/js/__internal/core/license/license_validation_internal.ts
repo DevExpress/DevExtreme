800aa4ed692be119134734521cae45904e78e17c
Arman Jivanyan <arman.jivanyan@devexpress.com>
2026-03-12 03:37:24 +0400

    fix type error

--- packages/devextreme/js/__internal/core/license/license_validation_internal.ts
+++ packages/devextreme/js/__internal/core/license/license_validation_internal.ts
@@ -4,6 +4,9 @@ import type { Token } from './types';
 // @ts-expect-error - only for internal usage
 export function parseLicenseKey(encodedKey: string | undefined): Token {}
 
+// @ts-expect-error - only for internal usage
+export function isUnsupportedKeyFormat(licenseKey: string | undefined): boolean {}
+
 export function validateLicense(licenseKey: string, version?: string): void {}
 
 // @ts-expect-error - only for internal usage
