6db9f39d08689c6948243c9f260f7a94aa47d5d0
Mark Allen Ramirez <markallenramirez@gmail.com>
2026-03-12 09:41:44 +0800

    DataGrid - TextArea editor should have the aria-invalid attribute when invalid (T1296376) (#32801)
    (cherry picked from commit d6f5b115684598b2fd5888e3f58b315069a26dd3)

---
+++ packages/devextreme/js/__internal/ui/__tests__/__mock__/model/text_area.ts
@@ -0,0 +1,25 @@
+import TextArea from '@ts/ui/m_text_area';
+
+const CLASSES = {
+  textArea: 'dx-texteditor-input',
+};
+
+export class TextAreaModel {
+  constructor(protected readonly root: HTMLElement) {}
+
+  public getElement(): HTMLElement {
+    return this.root;
+  }
+
+  public getInputElement(): HTMLTextAreaElement {
+    return this.root.querySelector(`.${CLASSES.textArea}`) as HTMLTextAreaElement;
+  }
+
+  public setValue(value: string): void {
+    this.getInstance()?.option('value', value);
+  }
+
+  public getInstance(): TextArea {
+    return TextArea.getInstance(this.root);
+  }
+}
