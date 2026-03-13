51587ca6151061fb61c8df6904bbd7e11c5d81e5
Andrei Kharitonov <pharret31@gmail.com>
2026-03-13 16:43:06 +0100

    Charts: update date in demos (part 3) (#32889)

--- apps/demos/Demos/Charts/AreaSparklines/jQuery/index.js
+++ apps/demos/Demos/Charts/AreaSparklines/jQuery/index.js
@@ -2,7 +2,7 @@ $(() => {
   const copOptions = {
     dataSource: copperCosts,
     argumentField: 'month',
-    valueField: '2010',
+    valueField: '2021',
     type: 'area',
     showMinMax: true,
     tooltip: {
@@ -12,7 +12,7 @@ $(() => {
   const nickOptions = {
     dataSource: nickelCosts,
     argumentField: 'month',
-    valueField: '2010',
+    valueField: '2021',
     type: 'splinearea',
     lineColor: '#8076bb',
     minColor: '#6babac',
@@ -27,7 +27,7 @@ $(() => {
   const palOptions = {
     dataSource: palladiumCosts,
     argumentField: 'month',
-    valueField: '2010',
+    valueField: '2021',
     firstLastColor: '#e55253',
     lineColor: '#7e4452',
     lineWidth: 3,
@@ -35,19 +35,22 @@ $(() => {
     pointSymbol: 'polygon',
     type: 'steparea',
     tooltip: {
-      format: 'currency',
+      format: {
+        type: 'currency',
+        precision: 2,
+      },
     },
   };
 
-  $('.area2010').dxSparkline(copOptions);
-  $('.splinearea2010').dxSparkline(nickOptions);
-  $('.steparea2010').dxSparkline(palOptions);
+  $('.area2021').dxSparkline(copOptions);
+  $('.splinearea2021').dxSparkline(nickOptions);
+  $('.steparea2021').dxSparkline(palOptions);
 
-  $('.area2011').dxSparkline($.extend(copOptions, { valueField: '2011' }));
-  $('.splinearea2011').dxSparkline($.extend(nickOptions, { valueField: '2011' }));
-  $('.steparea2011').dxSparkline($.extend(palOptions, { valueField: '2011' }));
+  $('.area2022').dxSparkline($.extend(copOptions, { valueField: '2022' }));
+  $('.splinearea2022').dxSparkline($.extend(nickOptions, { valueField: '2022' }));
+  $('.steparea2022').dxSparkline($.extend(palOptions, { valueField: '2022' }));
 
-  $('.area2012').dxSparkline($.extend(copOptions, { valueField: '2012' }));
-  $('.splinearea2012').dxSparkline($.extend(nickOptions, { valueField: '2012' }));
-  $('.steparea2012').dxSparkline($.extend(palOptions, { valueField: '2012' }));
+  $('.area2023').dxSparkline($.extend(copOptions, { valueField: '2023' }));
+  $('.splinearea2023').dxSparkline($.extend(nickOptions, { valueField: '2023' }));
+  $('.steparea2023').dxSparkline($.extend(palOptions, { valueField: '2023' }));
 });
