9e6d5178b9f0b5004b1dd9e36033f4556b78995d
Sergio Bur <sjbur.dev@gmail.com>
2026-03-18 14:50:46 +0100

    feat: add test for main form without icons

--- e2e/testcafe-devextreme/tests/scheduler/common/appointmentForm/form.visual.ts
+++ e2e/testcafe-devextreme/tests/scheduler/common/appointmentForm/form.visual.ts
@@ -227,6 +227,35 @@ test.meta({ browserSize: [450, 1000] })('main form on mobile screen', async (t) 
   },
 }));
 
+test.meta({ browserSize: [1500, 1500] })('main form without icons', async (t) => {
+  const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
+
+  const scheduler = new Scheduler(SCHEDULER_SELECTOR);
+
+  await scheduler.openAppointmentPopup(t, undefined, false);
+
+  await testScreenshot(
+    t,
+    takeScreenshot,
+    'scheduler__appointment__main-form__icons-mode-none.png',
+  );
+
+  await t
+    .expect(compareResults.isValid())
+    .ok(compareResults.errorMessages());
+}).before(async () => createWidget('dxScheduler', {
+  dataSource: [],
+  views: ['week'],
+  currentView: 'week',
+  currentDate: new Date(2021, 2, 25),
+  resources: getResources(true),
+  editing: {
+    form: {
+      iconsShowMode: 'none',
+    },
+  },
+}));
+
 test.meta({ browserSize: [1500, 1500] })('appointment form resource with multiple selection', async (t) => {
   const { takeScreenshot, compareResults } = createScreenshotsComparer(t);
 
