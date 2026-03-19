f86a00e343150f6a22c810360fdef259d3cc42b4
Ruslan Farkhutdinov <ruslan.farkhutdinov@devexpress.com>
2026-03-19 16:49:42 +0200

    AIIntegration: Update module export to match types (T1324402)

--- packages/devextreme/build/bundle-templates/dx.ai-integration.js
+++ packages/devextreme/build/bundle-templates/dx.ai-integration.js
@@ -2,4 +2,5 @@
 /* eslint-disable import/no-commonjs */
 import { AIIntegration } from '../common/ai-integration';
 
-module.exports = DevExpress.aiIntegration = AIIntegration;
+DevExpress.aiIntegration = {};
+module.exports = DevExpress.aiIntegration.AIIntegration = AIIntegration;
