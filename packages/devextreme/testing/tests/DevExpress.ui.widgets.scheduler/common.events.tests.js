a67247013528c2044cf3671658e5caa741c4a8f6
Aleksey Semikozov <alwexy@gmail.com>
2026-03-20 07:04:26 -0300

    Scheduler - Qunit tests - remove watchers of private methods

--- packages/devextreme/testing/tests/DevExpress.ui.widgets.scheduler/common.events.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.widgets.scheduler/common.events.tests.js
@@ -671,12 +671,14 @@ QUnit.module('Events', {
             dataSource: [appointment]
         });
 
-        const workspaceSpy = sinon.spy(scheduler.instance._workSpace, '_dimensionChanged');
-        const appointmentsSpy = sinon.spy(scheduler.instance._appointments, 'repaintAppointments');
+        const $element = scheduler.instance.$element();
+        const initialAppointmentWidth = $element.find('.dx-scheduler-appointment').outerWidth();
 
+        scheduler.instance.option('width', 400);
         resizeCallbacks.fire();
 
-        assert.ok(appointmentsSpy.calledAfter(workspaceSpy), 'workSpace dimension changing was called before appointments repainting');
+        const updatedAppointmentWidth = $element.find('.dx-scheduler-appointment').outerWidth();
+        assert.ok(updatedAppointmentWidth < initialAppointmentWidth, 'appointment width is recalculated after resize');
     });
 
     QUnit.test('ContentReady event should be fired after render completely ready (T902483)', async function(assert) {
