97bef08cee2f17f55300ec2d6d36c4f76b062864
Aleksey Semikozov <alwexy@gmail.com>
2026-03-20 11:44:00 -0300

    Playground - use jQuery API for Scheduler

--- packages/devextreme/playground/scheduler-example.ts
+++ packages/devextreme/playground/scheduler-example.ts
@@ -1,34 +1,28 @@
-import '../js/__internal/integration/jquery';
+import '../js/integration/jquery';
 import $ from 'jquery';
 import { setupThemeSelector } from './newthemeSelector';
-import Scheduler from '../js/__internal/scheduler/m_scheduler';
 
 const dataSource = [
     {
         text: "Meeting with John",
         startDate: new Date(2024, 0, 10, 9, 0),
         endDate: new Date(2024, 0, 10, 10, 30),
-        allDay: false
     },
     {
-        text: "Conference Call", 
+        text: "Conference Call",
         startDate: new Date(2024, 0, 10, 14, 0),
         endDate: new Date(2024, 0, 10, 15, 0),
-        allDay: false
     },
     {
         text: "Team Building Event",
         startDate: new Date(2024, 0, 11, 10, 0),
         endDate: new Date(2024, 0, 11, 17, 0),
-        allDay: false
-    }
+    },
 ];
 
-window.addEventListener('load', () => 
+window.addEventListener('load', () =>
   setupThemeSelector('theme-selector').then(() => {
-
-
-    new (Scheduler as any)($('#container'), {
+    $('#container').dxScheduler({
         dataSource,
         views: ['day', 'week', 'workWeek', 'month'],
         currentView: 'week',
@@ -36,21 +30,5 @@ window.addEventListener('load', () =>
         startDayHour: 8,
         endDayHour: 18,
         height: 600,
-        editing: {
-            allowAdding: true,
-            allowDeleting: true,
-            allowUpdating: true,
-            allowResizing: true,
-            allowDragging: true
-        },
-        onAppointmentAdded: (e) => {
-            console.log('Appointment added:', e.appointmentData);
-        },
-        onAppointmentUpdated: (e) => {
-            console.log('Appointment updated:', e.appointmentData);
-        },
-        onAppointmentDeleted: (e) => {
-            console.log('Appointment deleted:', e.appointmentData);
-        }
     });
 }));
