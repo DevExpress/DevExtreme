0ee6a32009e69a85079543f4fe6600ffa8e4accf
Aleksey Semikozov <alwexy@gmail.com>
2026-03-24 16:55:20 -0300

    Очистка gh-pages: удалён старый Playground (Demos) и артефакты
    Удалены папки Demos/, css/, data/, images/, scripts/ и пустой preview/pr-/.
    Освобождено ~1.8 ГБ. GitHub Pages падал из-за превышения лимита размера сайта (2.1 ГБ при рекомендованном 1 ГБ).
    Оставлены только Storybook-превью: preview/pr-32957 и preview/pr-33021.

--- Demos/DataGrid/PDFExportMultipleGrids/Angular/chunk-CD2RZIJI.js
+++
@@ -1,1 +0,0 @@
-function o(e){"@babel/helpers - typeof";return o=typeof Symbol=="function"&&typeof Symbol.iterator=="symbol"?function(t){return typeof t}:function(t){return t&&typeof Symbol=="function"&&t.constructor===Symbol&&t!==Symbol.prototype?"symbol":typeof t},o(e)}export{o as a};
