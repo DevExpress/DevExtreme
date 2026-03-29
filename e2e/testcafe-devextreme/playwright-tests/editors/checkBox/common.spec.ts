65d3da8a735c8ee84a22440a27635b82040a166f
Aleksey Semikozov <alwexy@gmail.com>
2026-03-29 17:13:54 -0300

    Playwright - replace devextreme/core/guid import, add CI-matching Dockerfile

--- e2e/testcafe-devextreme/playwright-tests/editors/checkBox/common.spec.ts
+++ e2e/testcafe-devextreme/playwright-tests/editors/checkBox/common.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { createWidget, testScreenshot, appendElementTo, setStyleAttribute, setClassAttribute, insertStylesheetRulesToPage } from '../../../playwright-helpers';
 import path from 'path';
-import Guid from 'devextreme/core/guid';
+const Guid = () => ({ toString: () => Math.random().toString(36).slice(2, 10) });
 
 const containerUrl = `file://${path.resolve(__dirname, '../../../tests/container.html')}`;
 
