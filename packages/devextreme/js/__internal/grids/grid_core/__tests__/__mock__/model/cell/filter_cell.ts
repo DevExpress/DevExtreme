d66adbff0df4297af3a0f1d936b3dda73e031bdd
Andrey Dolzhikov <16618553+Raushen@users.noreply.github.com>
2026-04-06 18:46:21 +0300

    T1306840 - DataGrid - TagBox in the filter row doesn't show selected tags if values are numbers (#33174)

---
+++ packages/devextreme/js/__internal/grids/grid_core/__tests__/__mock__/model/cell/filter_cell.ts
@@ -0,0 +1,18 @@
+const SELECTORS = {
+  editorCell: 'dx-editor-cell',
+  editorContainer: 'dx-editor-container',
+  widget: 'dx-widget',
+};
+
+export class FilterCellModel {
+  constructor(protected readonly root: HTMLElement | null) {}
+
+  public getElement(): HTMLElement | null {
+    return this.root;
+  }
+
+  public getEditor<T>(EditorModel: new (element: HTMLElement) => T): T {
+    const editorElement = this.root?.querySelector(`.${SELECTORS.editorContainer} .${SELECTORS.widget}`) as HTMLElement;
+    return new EditorModel(editorElement);
+  }
+}
