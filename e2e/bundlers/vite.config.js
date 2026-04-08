471513872bfa5e343b431c4d7f55b3355e41abd1
Ruslan Farkhutdinov <ruslan.farkhutdinov@devexpress.com>
2026-04-08 13:24:11 +0300

    Revert "Add resolve alias for jszip in vite config"
    This reverts commit 45f79ec25c47d24db5b6e1ff48edc41e90caf678.

--- e2e/bundlers/vite.config.js
+++ e2e/bundlers/vite.config.js
@@ -1,14 +1,8 @@
 import rollupConfig from './rollup.config.mjs';
-const jszipEntry = require.resolve('jszip/lib/index.js');
 
 delete rollupConfig.output.file;
 
 module.exports = {
-    resolve: {
-        alias: {
-            jszip: jszipEntry,
-        },
-    },
     build: {
         outDir: './dist/dist_vite',
         rollupOptions: rollupConfig
