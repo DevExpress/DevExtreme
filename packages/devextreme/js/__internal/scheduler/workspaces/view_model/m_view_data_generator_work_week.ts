daa6e6cf999ee9c320918b9ef43a79b191d1b8c6
Sergio Bur <sjbur.dev@gmail.com>
2026-04-13 15:39:00 +0200

    fix: fix test

--- packages/devextreme/js/__internal/scheduler/workspaces/view_model/m_view_data_generator_work_week.ts
+++ packages/devextreme/js/__internal/scheduler/workspaces/view_model/m_view_data_generator_work_week.ts
@@ -13,7 +13,7 @@ export class ViewDataGeneratorWorkWeek extends ViewDataGeneratorWeek {
       options.startDate,
       this._getIntervalDuration(options.intervalCount),
       this.getFirstDayOfWeek(options.firstDayOfWeek),
-      this.skippedDays,
+      options.skippedDays ?? this.skippedDays,
     );
   }
 
