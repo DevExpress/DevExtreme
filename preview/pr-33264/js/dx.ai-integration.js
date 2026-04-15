4a553f1b9e3bb01333278237bbe8cfe6c45eaa71
marker-dao <marker-dao@users.noreply.github.com>
2026-04-15 07:10:32 +0000

    Deploy preview for PR  🛫

--- preview/pr-33264/js/dx.ai-integration.js
+++ preview/pr-33264/js/dx.ai-integration.js
@@ -1,7 +1,7 @@
 /*!
 * DevExtreme (dx.ai-integration.js)
 * Version: 26.1.0
-* Build date: Tue Apr 14 2026
+* Build date: Wed Apr 15 2026
 *
 * Copyright (c) 2012 - 2026 Developer Express Inc. ALL RIGHTS RESERVED
 * Read about DevExtreme licensing here: https://js.devexpress.com/Licensing/
