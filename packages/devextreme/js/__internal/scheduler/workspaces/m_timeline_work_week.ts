e3f8d673775bb5b65dbde915d9ee933c14dafcd0
Sergio Bur <sjbur.dev@gmail.com>
2026-04-16 10:53:53 +0200

    fix: remove skippedDays tests from workspace

--- packages/devextreme/js/__internal/scheduler/workspaces/m_timeline_work_week.ts
+++ packages/devextreme/js/__internal/scheduler/workspaces/m_timeline_work_week.ts
@@ -1,5 +1,4 @@
 import registerComponent from '@js/core/component_registrator';
-import { extend } from '@js/core/utils/extend';
 
 import { VIEWS } from '../utils/options/constants_view';
 import SchedulerTimelineWeek from './m_timeline_week';
@@ -8,12 +7,6 @@ const TIMELINE_CLASS = 'dx-scheduler-timeline-work-week';
 
 class SchedulerTimelineWorkWeek extends SchedulerTimelineWeek {
   get type() { return VIEWS.TIMELINE_WORK_WEEK; }
-
-  _getDefaultOptions() {
-    return extend(super._getDefaultOptions(), {
-      skippedDays: [0, 6],
-    });
-  }
 
   protected override getElementClass() {
     return TIMELINE_CLASS;
