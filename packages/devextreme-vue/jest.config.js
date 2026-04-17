d06066db31c18bafd9ef4f6261c4093765458ab7
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2026-04-17 13:27:31 +0400

    fix jest config after bump @vue/test-utils to v2.4.6

--- packages/devextreme-vue/jest.config.js
+++ packages/devextreme-vue/jest.config.js
@@ -7,6 +7,20 @@ module.exports = {
   ...base,
   name: packageName,
   displayName: packageName,
+  testEnvironmentOptions: {
+    customExportConditions: ['node', 'node-addons'],
+  },
+  transform: {
+    '.*': [
+      'ts-jest',
+      {
+        diagnostics: false,
+        tsconfig: {
+          noEmitOnError: false,
+        },
+      },
+    ],
+  },
   moduleNameMapper: {
     "^vue$": "vue/dist/vue.cjs",
     "^@/(.*)$": "<rootDir>/src/$1"
