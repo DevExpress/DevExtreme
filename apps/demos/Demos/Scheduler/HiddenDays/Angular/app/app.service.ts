f56d70e8e64a4fda2a4520c6bc2852b039fcc960
Aleksei Semikozov <alwexy@gmail.com>
2026-04-17 10:03:14 -0300

    Scheduler - Hide days in week/month: write demo (#33318)

---
+++ apps/demos/Demos/Scheduler/HiddenDays/Angular/app/app.service.ts
@@ -0,0 +1,121 @@
+import { Injectable } from '@angular/core';
+
+export class Appointment {
+  id: number;
+
+  text: string;
+
+  startDate: Date;
+
+  endDate: Date;
+
+  allDay?: boolean;
+}
+
+const appointments: Appointment[] = [
+  {
+    id: 1,
+    text: 'Website Re-Design Plan',
+    startDate: new Date('2021-04-26T16:30:00.000Z'),
+    endDate: new Date('2021-04-26T18:30:00.000Z'),
+  },
+  {
+    id: 2,
+    text: 'Book Flights to San Fran for Sales Trip',
+    startDate: new Date('2021-04-26T19:00:00.000Z'),
+    endDate: new Date('2021-04-26T20:00:00.000Z'),
+    allDay: true,
+  },
+  {
+    id: 3,
+    text: 'Install New Router in Dev Room',
+    startDate: new Date('2021-04-26T21:30:00.000Z'),
+    endDate: new Date('2021-04-26T22:30:00.000Z'),
+  },
+  {
+    id: 4,
+    text: 'Approve Personal Computer Upgrade Plan',
+    startDate: new Date('2021-04-27T17:00:00.000Z'),
+    endDate: new Date('2021-04-27T18:00:00.000Z'),
+  },
+  {
+    id: 5,
+    text: 'Final Budget Review',
+    startDate: new Date('2021-04-27T19:00:00.000Z'),
+    endDate: new Date('2021-04-27T20:35:00.000Z'),
+  },
+  {
+    id: 6,
+    text: 'New Brochures',
+    startDate: new Date('2021-04-27T21:30:00.000Z'),
+    endDate: new Date('2021-04-27T22:45:00.000Z'),
+  },
+  {
+    id: 7,
+    text: 'Install New Database',
+    startDate: new Date('2021-04-28T16:45:00.000Z'),
+    endDate: new Date('2021-04-28T18:15:00.000Z'),
+  },
+  {
+    id: 8,
+    text: 'Approve New Online Marketing Strategy',
+    startDate: new Date('2021-04-28T19:00:00.000Z'),
+    endDate: new Date('2021-04-28T21:00:00.000Z'),
+  },
+  {
+    id: 9,
+    text: 'Upgrade Personal Computers',
+    startDate: new Date('2021-04-28T22:15:00.000Z'),
+    endDate: new Date('2021-04-28T23:30:00.000Z'),
+  },
+  {
+    id: 10,
+    text: 'Customer Workshop',
+    startDate: new Date('2021-04-29T18:00:00.000Z'),
+    endDate: new Date('2021-04-29T19:00:00.000Z'),
+    allDay: true,
+  },
+  {
+    id: 11,
+    text: 'Prepare 2021 Marketing Plan',
+    startDate: new Date('2021-04-29T18:00:00.000Z'),
+    endDate: new Date('2021-04-29T20:30:00.000Z'),
+  },
+  {
+    id: 12,
+    text: 'Brochure Design Review',
+    startDate: new Date('2021-04-29T21:00:00.000Z'),
+    endDate: new Date('2021-04-29T22:30:00.000Z'),
+  },
+  {
+    id: 13,
+    text: 'Create Icons for Website',
+    startDate: new Date('2021-04-30T17:00:00.000Z'),
+    endDate: new Date('2021-04-30T18:30:00.000Z'),
+  },
+  {
+    id: 14,
+    text: 'Upgrade Server Hardware',
+    startDate: new Date('2021-04-30T21:30:00.000Z'),
+    endDate: new Date('2021-04-30T23:00:00.000Z'),
+  },
+  {
+    id: 15,
+    text: 'Submit New Website Design',
+    startDate: new Date('2021-04-30T23:30:00.000Z'),
+    endDate: new Date('2021-05-01T01:00:00.000Z'),
+  },
+  {
+    id: 16,
+    text: 'Launch New Website',
+    startDate: new Date('2021-04-30T19:20:00.000Z'),
+    endDate: new Date('2021-04-30T21:00:00.000Z'),
+  },
+];
+
+@Injectable()
+export class DataService {
+  getAppointments(): Appointment[] {
+    return appointments;
+  }
+}
