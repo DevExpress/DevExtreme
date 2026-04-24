208be8f3c0566f24a076f864614d30e8eef18d2c
Mark Allen Ramirez <markallenramirez@gmail.com>
2026-04-24 13:25:04 +0800

    resizable

--- apps/demos/Demos/Resizable/Overview/React/data.ts
+++ apps/demos/Demos/Resizable/Overview/React/data.ts
@@ -1,7 +1,7 @@
 export const orders = [{
   ID: 1,
   OrderNumber: 35703,
-  OrderDate: '2014/04/10',
+  OrderDate: '2024/04/10',
   SaleAmount: 11800,
   Terms: '15 Days',
   CustomerStoreState: 'California',
@@ -10,7 +10,7 @@ export const orders = [{
 }, {
   ID: 4,
   OrderNumber: 35711,
-  OrderDate: '2014/01/12',
+  OrderDate: '2024/01/12',
   SaleAmount: 16050,
   Terms: '15 Days',
   CustomerStoreState: 'California',
@@ -19,7 +19,7 @@ export const orders = [{
 }, {
   ID: 5,
   OrderNumber: 35714,
-  OrderDate: '2014/01/22',
+  OrderDate: '2024/01/22',
   SaleAmount: 14750,
   Terms: '15 Days',
   CustomerStoreState: 'Nevada',
@@ -28,7 +28,7 @@ export const orders = [{
 }, {
   ID: 7,
   OrderNumber: 35983,
-  OrderDate: '2014/02/07',
+  OrderDate: '2024/02/07',
   SaleAmount: 3725,
   Terms: '15 Days',
   CustomerStoreState: 'Colorado',
@@ -37,7 +37,7 @@ export const orders = [{
 }, {
   ID: 9,
   OrderNumber: 36987,
-  OrderDate: '2014/03/11',
+  OrderDate: '2024/03/11',
   SaleAmount: 14200,
   Terms: '15 Days',
   CustomerStoreState: 'Utah',
@@ -46,7 +46,7 @@ export const orders = [{
 }, {
   ID: 11,
   OrderNumber: 38466,
-  OrderDate: '2014/03/01',
+  OrderDate: '2024/03/01',
   SaleAmount: 7800,
   Terms: '15 Days',
   CustomerStoreState: 'California',
@@ -55,7 +55,7 @@ export const orders = [{
 }, {
   ID: 14,
   OrderNumber: 39420,
-  OrderDate: '2014/02/15',
+  OrderDate: '2024/02/15',
   SaleAmount: 20500,
   Terms: '15 Days',
   CustomerStoreState: 'California',
@@ -64,7 +64,7 @@ export const orders = [{
 }, {
   ID: 15,
   OrderNumber: 39874,
-  OrderDate: '2014/02/04',
+  OrderDate: '2024/02/04',
   SaleAmount: 9050,
   Terms: '30 Days',
   CustomerStoreState: 'Nevada',
@@ -73,7 +73,7 @@ export const orders = [{
 }, {
   ID: 18,
   OrderNumber: 42847,
-  OrderDate: '2014/02/15',
+  OrderDate: '2024/02/15',
   SaleAmount: 20400,
   Terms: '30 Days',
   CustomerStoreState: 'Wyoming',
@@ -82,7 +82,7 @@ export const orders = [{
 }, {
   ID: 19,
   OrderNumber: 43982,
-  OrderDate: '2014/05/29',
+  OrderDate: '2024/05/29',
   SaleAmount: 6050,
   Terms: '30 Days',
   CustomerStoreState: 'Utah',
@@ -91,7 +91,7 @@ export const orders = [{
 }, {
   ID: 29,
   OrderNumber: 56272,
-  OrderDate: '2014/02/06',
+  OrderDate: '2024/02/06',
   SaleAmount: 15850,
   Terms: '30 Days',
   CustomerStoreState: 'Utah',
@@ -100,7 +100,7 @@ export const orders = [{
 }, {
   ID: 30,
   OrderNumber: 57429,
-  OrderDate: '2014/05/16',
+  OrderDate: '2023/12/31',
   SaleAmount: 11050,
   Terms: '30 Days',
   CustomerStoreState: 'Arizona',
@@ -109,7 +109,7 @@ export const orders = [{
 }, {
   ID: 32,
   OrderNumber: 58292,
-  OrderDate: '2014/05/13',
+  OrderDate: '2024/05/13',
   SaleAmount: 13500,
   Terms: '15 Days',
   CustomerStoreState: 'California',
@@ -118,7 +118,7 @@ export const orders = [{
 }, {
   ID: 36,
   OrderNumber: 62427,
-  OrderDate: '2014/01/27',
+  OrderDate: '2024/01/27',
   SaleAmount: 23500,
   Terms: '15 Days',
   CustomerStoreState: 'Nevada',
@@ -127,7 +127,7 @@ export const orders = [{
 }, {
   ID: 39,
   OrderNumber: 65977,
-  OrderDate: '2014/02/05',
+  OrderDate: '2024/02/05',
   SaleAmount: 2550,
   Terms: '15 Days',
   CustomerStoreState: 'Wyoming',
@@ -136,7 +136,7 @@ export const orders = [{
 }, {
   ID: 40,
   OrderNumber: 66947,
-  OrderDate: '2014/03/23',
+  OrderDate: '2024/03/23',
   SaleAmount: 3500,
   Terms: '15 Days',
   CustomerStoreState: 'Utah',
@@ -145,7 +145,7 @@ export const orders = [{
 }, {
   ID: 42,
   OrderNumber: 68428,
-  OrderDate: '2014/04/10',
+  OrderDate: '2024/04/10',
   SaleAmount: 10500,
   Terms: '15 Days',
   CustomerStoreState: 'California',
@@ -154,7 +154,7 @@ export const orders = [{
 }, {
   ID: 43,
   OrderNumber: 69477,
-  OrderDate: '2014/03/09',
+  OrderDate: '2024/03/09',
   SaleAmount: 14200,
   Terms: '15 Days',
   CustomerStoreState: 'California',
@@ -163,7 +163,7 @@ export const orders = [{
 }, {
   ID: 46,
   OrderNumber: 72947,
-  OrderDate: '2014/01/14',
+  OrderDate: '2024/01/14',
   SaleAmount: 13350,
   Terms: '30 Days',
   CustomerStoreState: 'Nevada',
@@ -172,7 +172,7 @@ export const orders = [{
 }, {
   ID: 47,
   OrderNumber: 73088,
-  OrderDate: '2014/03/25',
+  OrderDate: '2024/03/25',
   SaleAmount: 8600,
   Terms: '30 Days',
   CustomerStoreState: 'Nevada',
@@ -181,7 +181,7 @@ export const orders = [{
 }, {
   ID: 50,
   OrderNumber: 76927,
-  OrderDate: '2014/04/27',
+  OrderDate: '2024/04/27',
   SaleAmount: 9800,
   Terms: '30 Days',
   CustomerStoreState: 'Utah',
@@ -190,7 +190,7 @@ export const orders = [{
 }, {
   ID: 51,
   OrderNumber: 77297,
-  OrderDate: '2014/04/30',
+  OrderDate: '2024/04/30',
   SaleAmount: 10850,
   Terms: '30 Days',
   CustomerStoreState: 'Arizona',
@@ -199,7 +199,7 @@ export const orders = [{
 }, {
   ID: 56,
   OrderNumber: 84744,
-  OrderDate: '2014/02/10',
+  OrderDate: '2024/02/10',
   SaleAmount: 4650,
   Terms: '30 Days',
   CustomerStoreState: 'Nevada',
@@ -208,7 +208,7 @@ export const orders = [{
 }, {
   ID: 57,
   OrderNumber: 85028,
-  OrderDate: '2014/05/17',
+  OrderDate: '2024/05/17',
   SaleAmount: 2575,
   Terms: '30 Days',
   CustomerStoreState: 'Nevada',
@@ -217,7 +217,7 @@ export const orders = [{
 }, {
   ID: 59,
   OrderNumber: 87297,
-  OrderDate: '2014/04/21',
+  OrderDate: '2024/04/21',
   SaleAmount: 14200,
   Terms: '30 Days',
   CustomerStoreState: 'Wyoming',
@@ -226,7 +226,7 @@ export const orders = [{
 }, {
   ID: 60,
   OrderNumber: 88027,
-  OrderDate: '2014/02/14',
+  OrderDate: '2024/02/14',
   SaleAmount: 13650,
   Terms: '30 Days',
   CustomerStoreState: 'Utah',
@@ -235,7 +235,7 @@ export const orders = [{
 }, {
   ID: 65,
   OrderNumber: 94726,
-  OrderDate: '2014/05/22',
+  OrderDate: '2024/05/22',
   SaleAmount: 20500,
   Terms: '15 Days',
   CustomerStoreState: 'California',
@@ -244,7 +244,7 @@ export const orders = [{
 }, {
   ID: 66,
   OrderNumber: 95266,
-  OrderDate: '2014/03/10',
+  OrderDate: '2024/03/10',
   SaleAmount: 9050,
   Terms: '15 Days',
   CustomerStoreState: 'Nevada',
@@ -253,7 +253,7 @@ export const orders = [{
 }, {
   ID: 69,
   OrderNumber: 98477,
-  OrderDate: '2014/01/01',
+  OrderDate: '2024/01/01',
   SaleAmount: 23500,
   Terms: '15 Days',
   CustomerStoreState: 'Wyoming',
@@ -262,7 +262,7 @@ export const orders = [{
 }, {
   ID: 70,
   OrderNumber: 99247,
-  OrderDate: '2014/02/08',
+  OrderDate: '2024/02/08',
   SaleAmount: 2100,
   Terms: '15 Days',
   CustomerStoreState: 'Utah',
@@ -271,7 +271,7 @@ export const orders = [{
 }, {
   ID: 78,
   OrderNumber: 174884,
-  OrderDate: '2014/04/10',
+  OrderDate: '2024/04/10',
   SaleAmount: 7200,
   Terms: '30 Days',
   CustomerStoreState: 'Colorado',
@@ -280,7 +280,7 @@ export const orders = [{
 }, {
   ID: 81,
   OrderNumber: 188877,
-  OrderDate: '2014/02/11',
+  OrderDate: '2024/02/11',
   SaleAmount: 8750,
   Terms: '30 Days',
   CustomerStoreState: 'Arizona',
@@ -289,7 +289,7 @@ export const orders = [{
 }, {
   ID: 82,
   OrderNumber: 191883,
-  OrderDate: '2014/02/05',
+  OrderDate: '2024/02/05',
   SaleAmount: 9900,
   Terms: '30 Days',
   CustomerStoreState: 'California',
@@ -298,7 +298,7 @@ export const orders = [{
 }, {
   ID: 83,
   OrderNumber: 192474,
-  OrderDate: '2014/01/21',
+  OrderDate: '2024/01/21',
   SaleAmount: 12800,
   Terms: '30 Days',
   CustomerStoreState: 'California',
@@ -307,7 +307,7 @@ export const orders = [{
 }, {
   ID: 84,
   OrderNumber: 193847,
-  OrderDate: '2014/03/21',
+  OrderDate: '2024/03/21',
   SaleAmount: 14100,
   Terms: '30 Days',
   CustomerStoreState: 'California',
@@ -316,7 +316,7 @@ export const orders = [{
 }, {
   ID: 85,
   OrderNumber: 194877,
-  OrderDate: '2014/03/06',
+  OrderDate: '2024/03/06',
   SaleAmount: 4750,
   Terms: '30 Days',
   CustomerStoreState: 'California',
@@ -325,7 +325,7 @@ export const orders = [{
 }, {
   ID: 86,
   OrderNumber: 195746,
-  OrderDate: '2014/05/26',
+  OrderDate: '2024/05/26',
   SaleAmount: 9050,
   Terms: '30 Days',
   CustomerStoreState: 'Nevada',
@@ -334,7 +334,7 @@ export const orders = [{
 }, {
   ID: 87,
   OrderNumber: 197474,
-  OrderDate: '2014/03/02',
+  OrderDate: '2024/03/02',
   SaleAmount: 6400,
   Terms: '30 Days',
   CustomerStoreState: 'Nevada',
@@ -343,7 +343,7 @@ export const orders = [{
 }, {
   ID: 88,
   OrderNumber: 198746,
-  OrderDate: '2014/05/09',
+  OrderDate: '2024/05/09',
   SaleAmount: 15700,
   Terms: '30 Days',
   CustomerStoreState: 'Colorado',
@@ -352,7 +352,7 @@ export const orders = [{
 }, {
   ID: 91,
   OrderNumber: 214222,
-  OrderDate: '2014/02/08',
+  OrderDate: '2024/02/08',
   SaleAmount: 11050,
   Terms: '30 Days',
   CustomerStoreState: 'Arizona',
