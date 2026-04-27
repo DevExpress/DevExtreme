1b3e7c7b9d08d41aec6ae65a764b5daa90944c0f
Mark Allen Ramirez <markallenramirez@gmail.com>
2026-04-27 16:18:41 +0800

    Demo DataGrid - Update years in Orders data (#33401)

--- apps/demos/Demos/DataGrid/CustomSummaries/React/data.ts
+++ apps/demos/Demos/DataGrid/CustomSummaries/React/data.ts
@@ -1,7 +1,7 @@
 export const orders = [{
   ID: 1,
   OrderNumber: 35703,
-  OrderDate: '2014-04-10',
+  OrderDate: '2024-04-10',
   SaleAmount: 11800,
   Terms: '15 Days',
   TotalAmount: 12175,
@@ -11,7 +11,7 @@ export const orders = [{
 }, {
   ID: 4,
   OrderNumber: 35711,
-  OrderDate: '2014-01-12',
+  OrderDate: '2024-01-12',
   SaleAmount: 16050,
   Terms: '15 Days',
   TotalAmount: 16550,
@@ -21,7 +21,7 @@ export const orders = [{
 }, {
   ID: 5,
   OrderNumber: 35714,
-  OrderDate: '2014-01-22',
+  OrderDate: '2024-01-22',
   SaleAmount: 14750,
   Terms: '15 Days',
   TotalAmount: 15250,
@@ -31,7 +31,7 @@ export const orders = [{
 }, {
   ID: 7,
   OrderNumber: 35983,
-  OrderDate: '2014-02-07',
+  OrderDate: '2024-02-07',
   SaleAmount: 3725,
   Terms: '15 Days',
   TotalAmount: 3850,
@@ -41,7 +41,7 @@ export const orders = [{
 }, {
   ID: 9,
   OrderNumber: 36987,
-  OrderDate: '2014-03-11',
+  OrderDate: '2024-03-11',
   SaleAmount: 14200,
   Terms: '15 Days',
   TotalAmount: 14800,
@@ -51,7 +51,7 @@ export const orders = [{
 }, {
   ID: 11,
   OrderNumber: 38466,
-  OrderDate: '2014-03-01',
+  OrderDate: '2024-03-01',
   SaleAmount: 7800,
   Terms: '15 Days',
   TotalAmount: 8200,
@@ -61,7 +61,7 @@ export const orders = [{
 }, {
   ID: 15,
   OrderNumber: 39874,
-  OrderDate: '2014-02-04',
+  OrderDate: '2024-02-04',
   SaleAmount: 9050,
   Terms: '30 Days',
   TotalAmount: 19100,
@@ -71,7 +71,7 @@ export const orders = [{
 }, {
   ID: 18,
   OrderNumber: 42847,
-  OrderDate: '2014-02-15',
+  OrderDate: '2024-02-15',
   SaleAmount: 20400,
   Terms: '30 Days',
   TotalAmount: 20800,
@@ -81,7 +81,7 @@ export const orders = [{
 }, {
   ID: 19,
   OrderNumber: 43982,
-  OrderDate: '2014-05-29',
+  OrderDate: '2024-05-29',
   SaleAmount: 6050,
   Terms: '30 Days',
   TotalAmount: 6250,
@@ -91,7 +91,7 @@ export const orders = [{
 }, {
   ID: 29,
   OrderNumber: 56272,
-  OrderDate: '2014-02-06',
+  OrderDate: '2024-02-06',
   SaleAmount: 15850,
   Terms: '30 Days',
   TotalAmount: 16350,
@@ -101,7 +101,7 @@ export const orders = [{
 }, {
   ID: 30,
   OrderNumber: 57429,
-  OrderDate: '2013-12-31',
+  OrderDate: '2023-12-31',
   SaleAmount: 11050,
   Terms: '30 Days',
   TotalAmount: 11400,
@@ -111,7 +111,7 @@ export const orders = [{
 }, {
   ID: 32,
   OrderNumber: 58292,
-  OrderDate: '2014-05-13',
+  OrderDate: '2024-05-13',
   SaleAmount: 13500,
   Terms: '15 Days',
   TotalAmount: 13800,
--- apps/demos/Demos/DataGrid/RecalculateWhileEditing/ReactJs/data.js
+++ apps/demos/Demos/DataGrid/RecalculateWhileEditing/ReactJs/data.js
@@ -2,7 +2,7 @@ export const orders = [
   {
     ID: 1,
     OrderNumber: 35703,
-    OrderDate: '2014-04-10',
+    OrderDate: '2024-04-10',
     SaleAmount: 11800,
     Terms: '15 Days',
     TotalAmount: 12175,
@@ -13,7 +13,7 @@ export const orders = [
   {
     ID: 4,
     OrderNumber: 35711,
-    OrderDate: '2014-01-12',
+    OrderDate: '2024-01-12',
     SaleAmount: 16050,
     Terms: '15 Days',
     TotalAmount: 16550,
@@ -24,7 +24,7 @@ export const orders = [
   {
     ID: 5,
     OrderNumber: 35714,
-    OrderDate: '2014-01-22',
+    OrderDate: '2024-01-22',
     SaleAmount: 14750,
     Terms: '15 Days',
     TotalAmount: 15250,
@@ -35,7 +35,7 @@ export const orders = [
   {
     ID: 7,
     OrderNumber: 35983,
-    OrderDate: '2014-02-07',
+    OrderDate: '2024-02-07',
     SaleAmount: 3725,
     Terms: '15 Days',
     TotalAmount: 3850,
@@ -46,7 +46,7 @@ export const orders = [
   {
     ID: 9,
     OrderNumber: 36987,
-    OrderDate: '2014-03-11',
+    OrderDate: '2024-03-11',
     SaleAmount: 14200,
     Terms: '15 Days',
     TotalAmount: 14800,
@@ -57,7 +57,7 @@ export const orders = [
   {
     ID: 11,
     OrderNumber: 38466,
-    OrderDate: '2014-03-01',
+    OrderDate: '2024-03-01',
     SaleAmount: 7800,
     Terms: '15 Days',
     TotalAmount: 8200,
@@ -68,7 +68,7 @@ export const orders = [
   {
     ID: 15,
     OrderNumber: 39874,
-    OrderDate: '2014-02-04',
+    OrderDate: '2024-02-04',
     SaleAmount: 9050,
     Terms: '30 Days',
     TotalAmount: 19100,
@@ -79,7 +79,7 @@ export const orders = [
   {
     ID: 18,
     OrderNumber: 42847,
-    OrderDate: '2014-02-15',
+    OrderDate: '2024-02-15',
     SaleAmount: 20400,
     Terms: '30 Days',
     TotalAmount: 20800,
@@ -90,7 +90,7 @@ export const orders = [
   {
     ID: 19,
     OrderNumber: 43982,
-    OrderDate: '2014-05-29',
+    OrderDate: '2024-05-29',
     SaleAmount: 6050,
     Terms: '30 Days',
     TotalAmount: 6250,
@@ -101,7 +101,7 @@ export const orders = [
   {
     ID: 29,
     OrderNumber: 56272,
-    OrderDate: '2014-02-06',
+    OrderDate: '2024-02-06',
     SaleAmount: 15850,
     Terms: '30 Days',
     TotalAmount: 16350,
@@ -112,7 +112,7 @@ export const orders = [
   {
     ID: 30,
     OrderNumber: 57429,
-    OrderDate: '2013-12-31',
+    OrderDate: '2023-12-31',
     SaleAmount: 11050,
     Terms: '30 Days',
     TotalAmount: 11400,
@@ -123,7 +123,7 @@ export const orders = [
   {
     ID: 32,
     OrderNumber: 58292,
-    OrderDate: '2014-05-13',
+    OrderDate: '2024-05-13',
     SaleAmount: 13500,
     Terms: '15 Days',
     TotalAmount: 13800,
@@ -134,7 +134,7 @@ export const orders = [
   {
     ID: 36,
     OrderNumber: 62427,
-    OrderDate: '2014-01-27',
+    OrderDate: '2024-01-27',
     SaleAmount: 23500,
     Terms: '15 Days',
     TotalAmount: 24000,
@@ -145,7 +145,7 @@ export const orders = [
   {
     ID: 39,
     OrderNumber: 65977,
-    OrderDate: '2014-02-05',
+    OrderDate: '2024-02-05',
     SaleAmount: 2550,
     Terms: '15 Days',
     TotalAmount: 2625,
@@ -156,7 +156,7 @@ export const orders = [
   {
     ID: 40,
     OrderNumber: 66947,
-    OrderDate: '2014-03-23',
+    OrderDate: '2024-03-23',
     SaleAmount: 3500,
     Terms: '15 Days',
     TotalAmount: 3600,
@@ -167,7 +167,7 @@ export const orders = [
   {
     ID: 42,
     OrderNumber: 68428,
-    OrderDate: '2014-04-10',
+    OrderDate: '2024-04-10',
     SaleAmount: 10500,
     Terms: '15 Days',
     TotalAmount: 10900,
@@ -178,7 +178,7 @@ export const orders = [
   {
     ID: 43,
     OrderNumber: 69477,
-    OrderDate: '2014-03-09',
+    OrderDate: '2024-03-09',
     SaleAmount: 14200,
     Terms: '15 Days',
     TotalAmount: 14500,
@@ -189,7 +189,7 @@ export const orders = [
   {
     ID: 46,
     OrderNumber: 72947,
-    OrderDate: '2014-01-14',
+    OrderDate: '2024-01-14',
     SaleAmount: 13350,
     Terms: '30 Days',
     TotalAmount: 13650,
@@ -200,7 +200,7 @@ export const orders = [
   {
     ID: 47,
     OrderNumber: 73088,
-    OrderDate: '2014-03-25',
+    OrderDate: '2024-03-25',
     SaleAmount: 8600,
     Terms: '30 Days',
     TotalAmount: 8850,
@@ -211,7 +211,7 @@ export const orders = [
   {
     ID: 50,
     OrderNumber: 76927,
-    OrderDate: '2014-04-27',
+    OrderDate: '2024-04-27',
     SaleAmount: 9800,
     Terms: '30 Days',
     TotalAmount: 10050,
@@ -222,7 +222,7 @@ export const orders = [
   {
     ID: 51,
     OrderNumber: 77297,
-    OrderDate: '2014-04-30',
+    OrderDate: '2024-04-30',
     SaleAmount: 10850,
     Terms: '30 Days',
     TotalAmount: 11100,
@@ -233,7 +233,7 @@ export const orders = [
   {
     ID: 56,
     OrderNumber: 84744,
-    OrderDate: '2014-02-10',
+    OrderDate: '2024-02-10',
     SaleAmount: 4650,
     Terms: '30 Days',
     TotalAmount: 4750,
@@ -244,7 +244,7 @@ export const orders = [
   {
     ID: 57,
     OrderNumber: 85028,
-    OrderDate: '2014-05-17',
+    OrderDate: '2024-05-17',
     SaleAmount: 2575,
     Terms: '30 Days',
     TotalAmount: 2625,
@@ -255,7 +255,7 @@ export const orders = [
   {
     ID: 59,
     OrderNumber: 87297,
-    OrderDate: '2014-04-21',
+    OrderDate: '2024-04-21',
     SaleAmount: 14200,
     Terms: '30 Days',
     TotalAmount: 0,
@@ -266,7 +266,7 @@ export const orders = [
   {
     ID: 60,
     OrderNumber: 88027,
-    OrderDate: '2014-02-14',
+    OrderDate: '2024-02-14',
     SaleAmount: 13650,
     Terms: '30 Days',
     TotalAmount: 14050,
@@ -277,7 +277,7 @@ export const orders = [
   {
     ID: 65,
     OrderNumber: 94726,
-    OrderDate: '2014-05-22',
+    OrderDate: '2024-05-22',
     SaleAmount: 20500,
     Terms: '15 Days',
     TotalAmount: 20800,
@@ -288,7 +288,7 @@ export const orders = [
   {
     ID: 66,
     OrderNumber: 95266,
-    OrderDate: '2014-03-10',
+    OrderDate: '2024-03-10',
     SaleAmount: 9050,
     Terms: '15 Days',
     TotalAmount: 9250,
@@ -299,7 +299,7 @@ export const orders = [
   {
     ID: 69,
     OrderNumber: 98477,
-    OrderDate: '2014-01-01',
+    OrderDate: '2024-01-01',
     SaleAmount: 23500,
     Terms: '15 Days',
     TotalAmount: 23800,
@@ -310,7 +310,7 @@ export const orders = [
   {
     ID: 70,
     OrderNumber: 99247,
-    OrderDate: '2014-02-08',
+    OrderDate: '2024-02-08',
     SaleAmount: 2100,
     Terms: '15 Days',
     TotalAmount: 2150,
@@ -321,7 +321,7 @@ export const orders = [
   {
     ID: 78,
     OrderNumber: 174884,
-    OrderDate: '2014-04-10',
+    OrderDate: '2024-04-10',
     SaleAmount: 7200,
     Terms: '30 Days',
     TotalAmount: 7350,
@@ -332,7 +332,7 @@ export const orders = [
   {
     ID: 81,
     OrderNumber: 188877,
-    OrderDate: '2014-02-11',
+    OrderDate: '2024-02-11',
     SaleAmount: 8750,
     Terms: '30 Days',
     TotalAmount: 8900,
@@ -343,7 +343,7 @@ export const orders = [
   {
     ID: 82,
     OrderNumber: 191883,
-    OrderDate: '2014-02-05',
+    OrderDate: '2024-02-05',
     SaleAmount: 9900,
     Terms: '30 Days',
     TotalAmount: 10150,
@@ -354,7 +354,7 @@ export const orders = [
   {
     ID: 83,
     OrderNumber: 192474,
-    OrderDate: '2014-01-21',
+    OrderDate: '2024-01-21',
     SaleAmount: 12800,
     Terms: '30 Days',
     TotalAmount: 13100,
@@ -365,7 +365,7 @@ export const orders = [
   {
     ID: 84,
     OrderNumber: 193847,
-    OrderDate: '2014-03-21',
+    OrderDate: '2024-03-21',
     SaleAmount: 14100,
     Terms: '30 Days',
     TotalAmount: 14350,
@@ -376,7 +376,7 @@ export const orders = [
   {
     ID: 85,
     OrderNumber: 194877,
-    OrderDate: '2014-03-06',
+    OrderDate: '2024-03-06',
     SaleAmount: 4750,
     Terms: '30 Days',
     TotalAmount: 4950,
@@ -387,7 +387,7 @@ export const orders = [
   {
     ID: 86,
     OrderNumber: 195746,
-    OrderDate: '2014-05-26',
+    OrderDate: '2024-05-26',
     SaleAmount: 9050,
     Terms: '30 Days',
     TotalAmount: 9250,
@@ -398,7 +398,7 @@ export const orders = [
   {
     ID: 87,
     OrderNumber: 197474,
-    OrderDate: '2014-03-02',
+    OrderDate: '2024-03-02',
     SaleAmount: 6400,
     Terms: '30 Days',
     TotalAmount: 6600,
@@ -409,7 +409,7 @@ export const orders = [
   {
     ID: 88,
     OrderNumber: 198746,
-    OrderDate: '2014-05-09',
+    OrderDate: '2024-05-09',
     SaleAmount: 15700,
     Terms: '30 Days',
     TotalAmount: 16050,
@@ -420,7 +420,7 @@ export const orders = [
   {
     ID: 91,
     OrderNumber: 214222,
-    OrderDate: '2014-02-08T00:00:00',
+    OrderDate: '2024-02-08T00:00:00',
     SaleAmount: 11050,
     Terms: '30 Days',
     TotalAmount: 11250,
