6b3d9a7bb9e0e96d52e51ccf2bd286efcbeb005f
dxvladislavvolkov <vladislav.volkov@devexpress.com>
2026-05-12 15:48:07 +0400

    Fix for Windows

--- apps/demos/utils/ts-to-js-converter/converter.ts
+++ apps/demos/utils/ts-to-js-converter/converter.ts
@@ -79,7 +79,7 @@ const pipeSource = async (
 };
 
 const execTsc = async (directory: string, args: string[]): Promise<string> => new Promise((resolve, reject) => {
-  const proc = cps.spawn('tsc', args, { cwd: directory });
+  const proc = cps.spawn('tsc', args, { cwd: directory, shell: isWindows() });
   let stdout = '';
   let stderr = '';
   proc.stdout.on('data', (data: Buffer) => { stdout += data.toString(); });
