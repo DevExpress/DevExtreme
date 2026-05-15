e946f7924e41d3bd7ca502e93b0590deb15648e4
Aleksey <alwexy@gmail.com>
2026-05-14 23:37:30 -0300

    PivotGrid - fix expand icon for parent-child OLAP hierarchies (T1283598)

--- packages/devextreme/testing/tests/DevExpress.ui.widgets.pivotGrid/store.xmla.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.widgets.pivotGrid/store.xmla.tests.js
@@ -465,6 +465,35 @@ QUnit.module('Hierarchies', stubsEnvironment, () => {
         assert.equal(query, 'with set [DX_columns] as NonEmpty({[Product].[Product Categories].[All],[Product].[Product Categories].[Category]}, {[Measures].[Customer Count]}) set [DX_rows] as NonEmpty({Descendants({[Ship Date].[Calendar].[Month].&[2003]&[8]}, [Ship Date].[Calendar].[Date], SELF_AND_BEFORE)}, {[Measures].[Customer Count]}) SELECT CrossJoin([DX_columns],{[Measures].[Customer Count]}) DIMENSION PROPERTIES PARENT_UNIQUE_NAME,HIERARCHY_UNIQUE_NAME, MEMBER_VALUE ON columns,[DX_rows] DIMENSION PROPERTIES PARENT_UNIQUE_NAME,HIERARCHY_UNIQUE_NAME, MEMBER_VALUE ON rows FROM [Adventure Works]  CELL PROPERTIES VALUE, FORMAT_STRING, LANGUAGE, BACK_COLOR, FORE_COLOR, FONT_FLAGS');
     });
 
+    QUnit.test('T1283598. Hierarchy slice should not duplicate hierarchy prefix when path value is a full unique name', function(assert) {
+        this.store.load({
+            columns: [{ dataField: '[Active Month].[Active Month]' }],
+            rows: [
+                { dataField: '[Product].[Product Purchaser Type].[Purchaser Type]', hierarchyName: '[Product].[Product Purchaser Type]' },
+                { dataField: '[Product].[Product Purchaser Type].[Purchaser Sub Type]', hierarchyName: '[Product].[Product Purchaser Type]' },
+                { dataField: '[Product].[Product Nbr]' }
+            ],
+            values: [{ dataField: '[Measures].[Members]', caption: 'Members' }],
+            headerName: 'rows',
+            path: [
+                '[Product].[Product Purchaser Type].[Purchaser Type].&[Commercial]',
+                '[Product].[Product Purchaser Type].[Purchaser Type].&[Commercial].&[Group]'
+            ]
+        });
+        const query = this.getQuery();
+
+        assert.notStrictEqual(
+            query.indexOf('WHERE ([Product].[Product Purchaser Type].[Purchaser Type].&[Commercial].&[Group])'),
+            -1,
+            'WHERE slice contains the full unique name once'
+        );
+        assert.strictEqual(
+            query.indexOf('[Product].[Product Purchaser Type].[Purchaser Sub Type].[Product].[Product Purchaser Type]'),
+            -1,
+            'WHERE slice does not duplicate the hierarchy prefix'
+        );
+    });
+
     QUnit.test('Hierarchy. Expand child when opposite axis expanded on several levels', function(assert) {
         this.store.load({
             columns: [{
