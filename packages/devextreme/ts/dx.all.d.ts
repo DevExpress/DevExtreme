1c933649cb2d890b1241ad05e710ebbc88c88ede
Andrey Dolzhikov <16618553+Raushen@users.noreply.github.com>
2026-05-15 15:23:50 +0300

    T1328465: TreeList — No alternative for the deprecated dxTreeListToolbarItem type (#33598)

--- packages/devextreme/ts/dx.all.d.ts
+++ packages/devextreme/ts/dx.all.d.ts
@@ -30273,9 +30273,6 @@ declare module DevExpress.ui {
       readonly rowType: string;
       readonly watch?: Function;
     };
-    /**
-     * @deprecated Attention! This type is for internal purposes only. If you used it previously, please submit a ticket to our {@link https://supportcenter.devexpress.com/ticket/create Support Center}. We will check if there is an alternative solution.
-     */
     export type ColumnHeaderCellTemplateData<TRowData = any, TKey = any> = {
       readonly component: dxTreeList<TRowData, TKey>;
       readonly columnIndex: number;
@@ -33941,7 +33938,6 @@ declare module DevExpress.ui.dxTreeList {
   };
   /**
    * [descr:dxTreeListToolbarItem]
-   * @deprecated Attention! This type is for internal purposes only. If you used it previously, please submit a ticket to our {@link https://supportcenter.devexpress.com/ticket/create Support Center}. We will check if there is an alternative solution.
    */
   export interface ToolbarItem extends dxToolbarItem {
     /**
