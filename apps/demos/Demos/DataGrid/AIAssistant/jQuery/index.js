fe2bc9fa6a8a5822860e65b8eaf6d7bc1201dd56
Andrei_Ser <andrey.sergeyev@devexpress.com>
2026-05-21 15:15:31 +0400

    fix lint

--- apps/demos/Demos/DataGrid/AIAssistant/jQuery/index.js
+++ apps/demos/Demos/DataGrid/AIAssistant/jQuery/index.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-unresolved
-import { AzureOpenAI } from "https://esm.sh/openai@4.73.1";
+import { AzureOpenAI } from 'https://esm.sh/openai@4.73.1';
 
 window.AzureOpenAI = AzureOpenAI;
 
