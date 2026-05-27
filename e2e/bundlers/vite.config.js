fe6cc51b1c66cff260fd6382722bc0a4b39d9f05
Maksim Zakharov <251575087+bit-byte0@users.noreply.github.com>
2026-05-27 18:13:12 +0400

    Update vite to 7.3.2 and fix vite config for CJS interop

--- e2e/bundlers/vite.config.js
+++ e2e/bundlers/vite.config.js
@@ -1,6 +1,7 @@
 import rollupConfig from './rollup.config.mjs';
 
 delete rollupConfig.output.file;
+rollupConfig.plugins = rollupConfig.plugins.filter(p => p.name !== 'commonjs');
 
 module.exports = {
     build: {
