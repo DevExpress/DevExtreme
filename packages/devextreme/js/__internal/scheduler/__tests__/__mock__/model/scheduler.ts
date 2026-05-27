f01ee247dd4c153242aadcf0347a22ceb171ce0f
Eldar Iusupzhanov <ferrarijed@gmail.com>
2026-05-27 17:09:57 +0800

    add POM.getWorkSpace()

--- packages/devextreme/js/__internal/scheduler/__tests__/__mock__/model/scheduler.ts
+++ packages/devextreme/js/__internal/scheduler/__tests__/__mock__/model/scheduler.ts
@@ -108,6 +108,16 @@ export class SchedulerModel {
     return result as HTMLElement;
   }
 
+  getWorkspace(): HTMLElement {
+    const result = this.container.querySelector('.dx-scheduler-work-space');
+
+    if (!result) {
+      throw new Error('Workspace not found');
+    }
+
+    return result as HTMLElement;
+  }
+
   getHeaderPanelContent(): string[] {
     const cells = this.container.querySelectorAll('.dx-scheduler-header-panel-cell');
     return getTexts(cells);
