cf75a82fb2e9f0e7f821e91b61d2301365f9e0d0
Maksim Zakharov <251575087+bit-byte0@users.noreply.github.com>
2026-05-28 10:48:30 +0400

    Security - Update tmp, vite, @eslint/plugin-kit and @parcel/reporter-dev-server (#33746)

--- e2e/bundlers/vite.config.js
+++ e2e/bundlers/vite.config.js
@@ -2,6 +2,8 @@ import rollupConfig from './rollup.config.mjs';
 
 delete rollupConfig.output.file;
 
+rollupConfig.plugins = rollupConfig.plugins.filter(p => p.name !== 'commonjs');
+
 module.exports = {
     build: {
         outDir: './dist/dist_vite',
