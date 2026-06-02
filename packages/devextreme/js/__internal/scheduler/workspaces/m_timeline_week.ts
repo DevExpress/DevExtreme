7fe56dba0e1cf635dd318c252267ecf5ae0378a1
Sergio Bur <noreply@github.com>
2026-06-02 13:27:28 +0200

    refactor: refactor ts

--- packages/devextreme/js/__internal/scheduler/workspaces/m_timeline_week.ts
+++ packages/devextreme/js/__internal/scheduler/workspaces/m_timeline_week.ts
@@ -8,17 +8,18 @@ const TIMELINE_CLASS = 'dx-scheduler-timeline-week';
 export default class SchedulerTimelineWeek extends SchedulerTimeline {
   get type() { return VIEWS.TIMELINE_WEEK; }
 
-  protected override getElementClass() {
+  protected override getElementClass(): string {
     return TIMELINE_CLASS;
   }
 
-  protected override needRenderWeekHeader() {
+  protected override needRenderWeekHeader(): boolean {
     return true;
   }
 
-  protected override incrementDate(date) {
+  protected override incrementDate(date: Date): void {
     date.setDate(date.getDate() + 1);
   }
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 registerComponent('dxSchedulerTimelineWeek', SchedulerTimelineWeek as any);
