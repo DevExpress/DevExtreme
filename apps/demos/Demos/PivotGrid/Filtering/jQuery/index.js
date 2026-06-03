8314893ee3805ba4168ebf8a5ee87ab6e2a40c72
Mikhail Preyskurantov <5574159+mpreyskurantov@users.noreply.github.com>
2026-06-03 14:59:25 +0300

    PivotGrid OLAP Demos: New 2026 Initial Catalog / Dates

--- apps/demos/Demos/PivotGrid/Filtering/jQuery/index.js
+++ apps/demos/Demos/PivotGrid/Filtering/jQuery/index.js
@@ -39,7 +39,7 @@ $(() => {
       store: {
         type: 'xmla',
         url: 'https://demos.devexpress.com/Services/OLAP/msmdpump.dll',
-        catalog: 'Adventure Works DW Standard Edition',
+        catalog: 'Adventure Works DW Standard Edition 2026',
         cube: 'Adventure Works',
       },
     },
