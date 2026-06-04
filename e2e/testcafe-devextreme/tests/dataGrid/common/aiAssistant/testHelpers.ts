beef8d93d86d10e34ba3dc4975a83f059c9daac7
Alyar <>
2026-06-04 12:22:08 +0400

    Remove unnecessary helper methods

--- e2e/testcafe-devextreme/tests/dataGrid/common/aiAssistant/testHelpers.ts
+++ e2e/testcafe-devextreme/tests/dataGrid/common/aiAssistant/testHelpers.ts
@@ -1,16 +1,5 @@
-/* eslint-disable no-underscore-dangle */
-import { ClientFunction } from 'testcafe';
 import url from '../../../../helpers/getPageUrl';
 
 export const GRID_SELECTOR = '#container';
 
 export const AI_INTEGRATION_PAGE = url(__dirname, '../../../container-ai-integration.html');
-
-export const getRequestColumnNames = ClientFunction(
-  (index: number) => (window as any).__aiRequests[index].data.context.columns
-    .map((c: any) => c.dataField),
-);
-
-export const formatMessage = ClientFunction(
-  (key: string) => (window as any).DevExpress.localization.formatMessage(key),
-);
