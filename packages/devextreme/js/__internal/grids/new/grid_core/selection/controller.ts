191d1f01eac30d92cb308cf1afcf4d8d326e6303
Mark Allen Ramirez <markallenramirez@gmail.com>
2026-06-04 19:03:42 +0800

    move fix

--- packages/devextreme/js/__internal/grids/new/grid_core/selection/controller.ts
+++ packages/devextreme/js/__internal/grids/new/grid_core/selection/controller.ts
@@ -154,16 +154,14 @@ export class SelectionController {
       Need to get rid of `selectionHelper.peek()` inside of selectCards()
       and pass selectionHelper from here
       */
-      const selectionHelper = this.selectionHelper.value;
+      // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+      this.selectionHelper.value;
+
       const isLoaded = this.dataController.isLoaded.value;
       if (isLoaded) {
-        const selectedCardKeys = this.selectedCardKeys.value;
-        const previousSelectedKeys = selectionHelper?.getSelectedItemKeys() ?? [];
-        const areKeysEqual = selectionHelper?.areKeysEqual(selectedCardKeys, previousSelectedKeys);
+        const selectedCardKeys = this.selectedCardKeys.peek();
 
-        if (!areKeysEqual) {
-          this.selectCards(selectedCardKeys);
-        }
+        this.selectCards(selectedCardKeys);
       }
     });
 
@@ -172,7 +170,16 @@ export class SelectionController {
       this.dataController.items.value;
       // eslint-disable-next-line @typescript-eslint/no-unused-expressions
       this.dataController.isLoaded.value;
-      this.updateSelectionToolbarButtons(this.normalizedSelectedCardKeys.value);
+
+      const selectedCardKeys = this.normalizedSelectedCardKeys.value;
+      this.updateSelectionToolbarButtons(selectedCardKeys);
+
+      const selectionHelper = this.selectionHelper.peek();
+      const previousSelectedKeys = selectionHelper?.getSelectedItemKeys() ?? [];
+      const areKeysEqual = selectionHelper?.areKeysEqual(selectedCardKeys, previousSelectedKeys);
+      if (!areKeysEqual) {
+        this.selectCards(selectedCardKeys);
+      }
     });
   }
 
