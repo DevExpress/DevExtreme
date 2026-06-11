33fbe19404c697ff45b165330945849fcb58e929
Sergei Burkatskii <noreply@github.com>
2026-06-11 14:17:47 +0200

    fix: fix header class

--- packages/devextreme/js/__internal/scheduler/workspaces/work_space.ts
+++ packages/devextreme/js/__internal/scheduler/workspaces/work_space.ts
@@ -2888,7 +2888,11 @@ class SchedulerWorkSpace extends Widget<WorkspaceOptionsInternal> {
   protected getGroupHeaderClass(i?: number): string {
     const cellClass = GROUP_HEADER_CLASS;
 
-    return this.groupedStrategy.addAdditionalGroupCellClasses(cellClass, (i ?? 0) + 1, 0, 0);
+    if (i === undefined) {
+      return cellClass;
+    }
+
+    return this.groupedStrategy.addAdditionalGroupCellClasses(cellClass, i + 1, 0, 0);
   }
 
   protected initWorkSpaceUnits(): void {
