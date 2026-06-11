e437f58bfee82c0a36f6083adf759799612c97ff
Sergei Burkatskii <noreply@github.com>
2026-06-11 15:41:17 +0200

    fix: fix tests

--- packages/devextreme/js/__internal/scheduler/workspaces/work_space.ts
+++ packages/devextreme/js/__internal/scheduler/workspaces/work_space.ts
@@ -3246,11 +3246,7 @@ class SchedulerWorkSpace extends Widget<WorkspaceOptionsInternal> {
       const groupRows = this.makeGroupRows(this.option().groups, this.option().groupByDate);
       this.attachGroupCountClass();
       const { elements } = groupRows;
-      if (Array.isArray(elements)) {
-        $container.append(elements);
-      } else {
-        $container.append(elements);
-      }
+      $container.append(Array.isArray(elements) ? elements : elements.toArray());
       cellTemplates = groupRows.cellTemplates;
     } else {
       this.detachGroupCountClass();
