8bfe9ed0b4d81a1b1b3e122f49a7dbeb30540942
marker dao ® <youdontknow@marker-dao.eth>
2026-06-25 19:54:38 +0200

    ESLint: Add JSDoc default matches type plugin

--- packages/devextreme/eslint_plugins/index.js
+++ packages/devextreme/eslint_plugins/index.js
@@ -1,11 +1,14 @@
+/* eslint-disable spellcheck/spell-checker */
 const noDirectPreactSignalsCoreImport = require('./no_direct_preact_signals_core_import');
 const preferSwitchTrue = require('./prefer_switch_true');
 const noDeferred = require('./no_deferred');
+const jsdocDefaultMatchesType = require('./jsdoc_default_matches_type');
 
 module.exports = {
     rules: {
         'no-direct-preact-signals-core-import': noDirectPreactSignalsCoreImport,
         'prefer-switch-true': preferSwitchTrue,
         'no-deferred': noDeferred,
+        'jsdoc-default-matches-type': jsdocDefaultMatchesType,
     },
 };
