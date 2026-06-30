8ef0996e2e4d4e5bec85dff27300ecb570a3917c
markallenramirez <markallenramirez@gmail.com>
2026-06-30 20:03:42 +0800

    testcafe

--- e2e/testcafe-devextreme/tests/dataGrid/common/virtualColumns/functional.ts
+++ e2e/testcafe-devextreme/tests/dataGrid/common/virtualColumns/functional.ts
@@ -1,3 +1,4 @@
+import { ClientFunction } from 'testcafe';
 import DataGrid from 'devextreme-testcafe-models/dataGrid';
 import url from '../../../../helpers/getPageUrl';
 import { createWidget } from '../../../../helpers/createWidget';
@@ -85,3 +86,74 @@ test('DataGrid should not scroll back to the focused cell after horizontal scrol
     columnRenderingMode: 'virtual',
   },
 }));
+
+test('DataGrid with async templates should call load method only once on initial render', async (t) => {
+  const dataGrid = new DataGrid('#container');
+  const getLoadCount = ClientFunction(() => (window as any)._loadCount);
+
+  await t.expect(dataGrid.isReady()).ok();
+
+  await t.expect(dataGrid.getDataCell(1000, 0).element.textContent).eql('1001 custom');
+
+  await t.expect(getLoadCount()).eql(1);
+}).before(async () => createWidget('dxDataGrid', () => {
+  (window as any)._loadCount = 0;
+
+  const sampleAPI = new (window as any).DevExpress.data.ArrayStore({
+    key: 'id',
+    data: Array.from({ length: 10000 }, (_, index) => ({
+      id: index + 1,
+      text: `item long text ${index + 1}`,
+    })),
+  });
+
+  const store = new (window as any).DevExpress.data.CustomStore({
+    key: 'id',
+    load(o: any) {
+      (window as any)._loadCount += 1;
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          sampleAPI.load(o).then(resolve);
+        }, 100);
+      });
+    },
+    totalCount(o: any) {
+      return sampleAPI.totalCount(o);
+    },
+  });
+
+  return {
+    dataSource: store,
+    showBorders: true,
+    remoteOperations: true,
+    paging: {
+      pageSize: 100,
+      pageIndex: 10,
+    },
+    scrolling: {
+      mode: 'virtual',
+      rowRenderingMode: 'virtual',
+    },
+    columns: [
+      { dataField: 'id', width: 90, cellTemplate: 'myTemplate' },
+      { dataField: 'text', width: 150 },
+    ],
+    height: 1500,
+    wordWrapEnabled: true,
+    templatesRenderAsynchronously: true,
+    integrationOptions: {
+      templates: {
+        myTemplate: {
+          render(e: any) {
+            setTimeout(() => {
+              $('<div>').text(`${e.model.text} custom`).appendTo(e.container);
+              e.onRendered?.();
+            }, 100);
+          },
+        },
+      },
+    },
+  };
+})).after(async () => ClientFunction(() => {
+  delete (window as any)._loadCount;
+})());
