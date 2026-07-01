e60a0477942296095644dd0bb5ac2c724ffbe5a6
Arman Jivanyan <arman.jivanyan@devexpress.com>
2026-07-01 11:30:03 +0400

    review comments fixes

--- apps/demos/scripts/prepare-shared.js
+++
@@ -1,7 +0,0 @@
-const { copyJsSharedResources, copyMvcSharedResources } = require('../utils/copy-shared-resources/copy');
-
-copyJsSharedResources(() => {
-  copyMvcSharedResources(() => {
-    console.log('prepare-shared: done');
-  });
-});
