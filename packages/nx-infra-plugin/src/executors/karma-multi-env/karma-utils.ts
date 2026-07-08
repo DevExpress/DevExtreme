14ed23dea4a9e05f5ed9714a2ca2d0b2d0984abd
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2026-07-08 14:08:40 +0400

    CI: fix wrapper_tests on chrome install step (#34226)

--- packages/nx-infra-plugin/src/executors/karma-multi-env/karma-utils.ts
+++ packages/nx-infra-plugin/src/executors/karma-multi-env/karma-utils.ts
@@ -46,6 +46,7 @@ export interface KarmaModule {
     parseConfig(
       configPath: string | null,
       cliOptions: Record<string, unknown>,
+      parseOptions?: { promiseConfig?: boolean; throwErrors?: boolean },
     ): Promise<KarmaConfigOptions>;
   };
 }
