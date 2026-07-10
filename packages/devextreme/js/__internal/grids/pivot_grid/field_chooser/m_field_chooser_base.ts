54e25009448c029da5e26f0c6b9394ef2c7da6d2
Aleksei Semikozov <2846685+aleksei-semikozov@users.noreply.github.com>
2026-07-10 14:20:44 -0300

    Scheduler/PivotGrid: align @default tags with option types in public d.ts (#34198)

--- packages/devextreme/js/__internal/grids/pivot_grid/field_chooser/m_field_chooser_base.ts
+++ packages/devextreme/js/__internal/grids/pivot_grid/field_chooser/m_field_chooser_base.ts
@@ -124,7 +124,7 @@ export class FieldChooserBase extends mixinWidget {
       ...{
         allowFieldDragging: true,
         applyChangesMode: 'instantly',
-        state: null,
+        state: undefined,
         headerFilter: {
           width: 252,
           height: 325,
