95199aa53c6a5e86d74cafb803ef685170dbf308
Arman Jivanyan <arman.jivanyan@devexpress.com>
2026-07-13 13:05:51 +0400

    fix internal signature

--- packages/devextreme/js/__internal/core/license/license_validation_internal.ts
+++ packages/devextreme/js/__internal/core/license/license_validation_internal.ts
@@ -1,13 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import type { Token } from './types';
 
-// @ts-expect-error - only for internal usage
-export function parseLicenseKey(encodedKey: string | undefined): Token {}
+export declare function parseLicenseKey(
+  encodedKey: string | undefined,
+  allowAspNetProductCompatibility?: boolean,
+): Token;
 
 export function validateLicense(licenseKey: string, version?: string): void {}
 
-// @ts-expect-error - only for internal usage
-export function peekValidationPerformed(): boolean {}
+export declare function peekValidationPerformed(): boolean;
 
 export function setLicenseCheckSkipCondition(): void {}
 
