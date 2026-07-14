468f08ac0b54a788d6d3262e65bd10719eda8f64
Eldar Iusupzhanov <ferrarijed@gmail.com>
2026-07-14 10:02:32 +0600

    stabilize toast hiding test

--- e2e/testcafe-devextreme/tests/common/treeList/toast.ts
+++ e2e/testcafe-devextreme/tests/common/treeList/toast.ts
@@ -43,13 +43,13 @@ test('Toast should hide after the display time', async (t) => {
     .expect(treeList.isReady())
     .ok();
 
-  await setToastDisplayTime(100);
+  await setToastDisplayTime(1000);
   await treeList.apiShowErrorToast();
   await t
     .expect(treeList.getToast().exists).ok();
 
   await t
-    .wait(150)
+    .wait(1100)
     .expect(treeList.getToast().exists).notOk();
 }).before(async () => {
   await createWidget('dxTreeList', {});
