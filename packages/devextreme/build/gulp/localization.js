5652f52b5328d3f4158521d247a5e460cca6baa3
Andrey Vorobev <738482+vorobey@users.noreply.github.com>
2026-07-14 18:51:01 +0300

    Gulp: localization generate-community-locales gulp task move to nx ex… (#34286)

--- packages/devextreme/build/gulp/localization.js
+++ packages/devextreme/build/gulp/localization.js
@@ -1,50 +1,8 @@
 'use strict';
 
 const gulp = require('gulp');
-const path = require('path');
 const shell = require('gulp-shell');
-const through = require('through2');
-const fs = require('fs');
-
-const DEFAULT_LOCALE = 'en';
-const DICTIONARY_SOURCE_FOLDER = 'js/localization/messages';
 
 gulp.task('localization', shell.task('pnpm nx build:localization devextreme'));
 
-gulp.task('generate-community-locales', () => {
-    const defaultFile = fs.readFileSync(path.join(DICTIONARY_SOURCE_FOLDER, DEFAULT_LOCALE + '.json')).toString();
-    const defaultDictionaryKeys = Object.keys(JSON.parse(defaultFile)[DEFAULT_LOCALE]);
-
-    return gulp
-        .src([
-            'js/localization/messages/*.json',
-            '!js/localization/messages/en.json'
-        ])
-        .pipe(through.obj(function(file, encoding, callback) {
-            const parsedFile = JSON.parse(file.contents.toString(encoding));
-
-            const [locale] = Object.keys(parsedFile);
-            const dictionary = parsedFile[locale];
-
-            let newFile = defaultFile.replace(`"${DEFAULT_LOCALE}"`, `"${locale}"`);
-
-            defaultDictionaryKeys.forEach((key) => {
-                let replaceValue = null;
-                // eslint-disable-next-line no-prototype-builtins
-                if(dictionary.hasOwnProperty(key)) {
-                    const val = dictionary[key];
-                    if(!val.includes('TODO')) {
-                        replaceValue = val.replace(/"/g, '\\"');
-                    }
-                }
-
-                if(replaceValue != null) {
-                    newFile = newFile.replace(new RegExp(`"${key}":.*"(,)?`), `"${key}": "${replaceValue}"$1`);
-                }
-            });
-
-            file.contents = Buffer.from(newFile, encoding);
-            callback(null, file);
-        }))
-        .pipe(gulp.dest(DICTIONARY_SOURCE_FOLDER));
-});
+gulp.task('generate-community-locales', shell.task('pnpm nx build:community-localization devextreme'));
