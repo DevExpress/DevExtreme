2474c90c251e85304caa7c775634659047a00be0
Arman Jivanyan <arman.jivanyan@devexpress.com>
2026-07-16 17:58:43 +0400

    remove dev-watch, migrate to nx

--- packages/devextreme/build/gulp/transpile.js
+++ packages/devextreme/build/gulp/transpile.js
@@ -3,9 +3,4 @@
 const gulp = require('gulp');
 const shell = require('gulp-shell');
 
-// Migrated to native Nx targets (devextreme-nx-infra-plugin:babel-transform /
-// build-typescript with watch mode). These gulp tasks are thin delegates kept only
-// while the dev-watch orchestrator still references them; remove once dev-watch is migrated.
-gulp.task('transpile-watch', shell.task('pnpm nx build:transpile:watch devextreme'));
-
 gulp.task('transpile-tests', shell.task('pnpm nx transpile:tests devextreme'));
