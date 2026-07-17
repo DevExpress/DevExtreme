4cd9935f0d548fddcb4ef7149cb2c6f138f32519
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2026-07-17 19:07:29 +0200

    replace scripts

--- tools/scripts/update-version.ts
+++ tools/scripts/update-version.ts
@@ -4,7 +4,7 @@ import { updateVersion, updateVersionJs } from './common/version';
 const version = process.argv[2];
 
 if (version == null) {
-  console.error(`Usage: 'pnpm run all:update-version $version' (XX.X.X)`);
+  console.error(`Usage: 'pnpm nx run workflows:update-version -- XX.X.X'`);
   process.exit(1);
 }
 
