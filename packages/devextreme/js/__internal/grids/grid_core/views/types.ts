d8628bb231191d0fdc0f883c77d5e4d4466ba7fe
Alyar <>
2026-07-22 01:17:17 +0400

    Fix copilot comment

--- packages/devextreme/js/__internal/grids/grid_core/views/types.ts
+++ packages/devextreme/js/__internal/grids/grid_core/views/types.ts
@@ -3,5 +3,6 @@ import type dxScrollable from '@js/ui/scroll_view/ui.scrollable';
 
 export type RowsViewScrollEvent = Partial<ScrollEventInfo<dxScrollable>> & {
   component: dxScrollable;
+  scrollOffset: { top: number; left: number };
   forceUpdateScrollPosition?: boolean;
 };
