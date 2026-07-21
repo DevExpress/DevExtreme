3ee4d737dde6ad6f00fefb7389d55ae8c1df9d7a
Mikhail Preyskurantov <5574159+mpreyskurantov@users.noreply.github.com>
2026-07-21 17:44:21 +0300

    devextreme-angular (14)

--- packages/devextreme-angular/src/index.ts
+++ packages/devextreme-angular/src/index.ts
@@ -1,8 +1,10 @@
-export * from 'devextreme-angular/core';
-export * from './ui/all';
 import './common';
 import './common/grids';
 import './common/charts';
+
+export * from 'devextreme-angular/core';
+export * from './ui/all';
+
 export { DxAccordionComponent, DxAccordionModule } from 'devextreme-angular/ui/accordion';
 export { DxActionSheetComponent, DxActionSheetModule } from 'devextreme-angular/ui/action-sheet';
 export { DxAutocompleteComponent, DxAutocompleteModule } from 'devextreme-angular/ui/autocomplete';
