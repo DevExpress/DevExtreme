6417b6f9c3f4e9b0e06ecc984768c132e442dd7b
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2026-07-23 09:23:20 +0400

    register new dxdsfluent for viz

---
+++ packages/devextreme/js/__internal/viz/core/themes/dxdsfluent/index.ts
@@ -0,0 +1,52 @@
+const themes = [
+  {
+    baseThemeName: 'fluent.blue.light',
+    theme: {
+      name: 'dxdsfluent.blue.light',
+    },
+  },
+  {
+    baseThemeName: 'dxdsfluent.blue.light',
+    theme: {
+      name: 'dxdsfluent.blue.light.compact',
+    },
+  },
+  {
+    baseThemeName: 'fluent.saas.light',
+    theme: {
+      name: 'dxdsfluent.saas.light',
+    },
+  },
+  {
+    baseThemeName: 'dxdsfluent.saas.light',
+    theme: {
+      name: 'dxdsfluent.saas.light.compact',
+    },
+  },
+  {
+    baseThemeName: 'fluent.blue.dark',
+    theme: {
+      name: 'dxdsfluent.blue.dark',
+    },
+  },
+  {
+    baseThemeName: 'dxdsfluent.blue.dark',
+    theme: {
+      name: 'dxdsfluent.blue.dark.compact',
+    },
+  },
+  {
+    baseThemeName: 'fluent.saas.dark',
+    theme: {
+      name: 'dxdsfluent.saas.dark',
+    },
+  },
+  {
+    baseThemeName: 'dxdsfluent.saas.dark',
+    theme: {
+      name: 'dxdsfluent.saas.dark.compact',
+    },
+  },
+];
+
+export default themes;
--- packages/devextreme/testing/tests/DevExpress.viz.core/themes.tests.js
+++ packages/devextreme/testing/tests/DevExpress.viz.core/themes.tests.js
@@ -358,6 +358,23 @@ QUnit.test('Invalid input data (with color scheme)', function(assert) {
     });
 });
 
+[
+    'dxdsfluent.blue.light',
+    'dxdsfluent.blue.light.compact',
+    'dxdsfluent.blue.dark',
+    'dxdsfluent.blue.dark.compact',
+    'dxdsfluent.saas.light',
+    'dxdsfluent.saas.light.compact',
+    'dxdsfluent.saas.dark',
+    'dxdsfluent.saas.dark.compact',
+].forEach((theme) => {
+    QUnit.test(`dxdsfluent theme should be registered: ${theme}`, function(assert) {
+        themeModule.currentTheme(theme);
+
+        assert.strictEqual(themeModule.currentTheme(), theme);
+    });
+});
+
 
 QUnit.module('refresh all', {
     createItem: function() {
