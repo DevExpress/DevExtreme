e97af1c2a0e8289bbe8899c2933389335345e53d
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2026-07-28 22:28:24 +0200

    use ESM bundling in qunit. fix css in dataGrid

--- packages/devextreme/js/__internal/grids/grid_core/views/m_rows_view.ts
+++ packages/devextreme/js/__internal/grids/grid_core/views/m_rows_view.ts
@@ -536,7 +536,7 @@ export class RowsView extends ColumnsView {
       $cell = that._createCell({
         column: columns[i], rowType: 'freeSpace', columnIndex: i, columns,
       });
-      isNumeric(height) && $cell.css('height', height);
+      isNumeric(height) && setHeight($cell, height);
 
       $row.append($cell);
     }
@@ -1035,7 +1035,7 @@ export class RowsView extends ColumnsView {
 
             if (showFreeSpaceRow) {
               deferRender(() => {
-                freeSpaceRowElements.css('height', resultHeight);
+                setHeight(freeSpaceRowElements, resultHeight);
                 isFreeSpaceRowVisible = true;
                 freeSpaceRowElements.show();
               });
@@ -1044,7 +1044,7 @@ export class RowsView extends ColumnsView {
           });
         }
       } else {
-        freeSpaceRowElements.css('height', 0);
+        setHeight(freeSpaceRowElements, 0);
         freeSpaceRowElements.show();
         this._updateLastRowBorder(true);
       }
