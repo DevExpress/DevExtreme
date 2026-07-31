26b03ce2a6b367bcdac917b732ce391702d2045e
Andrei Kharitonov <pharret31@users.noreply.github.com>
2026-07-31 14:57:15 +0200

    Design System: fix tests

--- e2e/testcafe-devextreme/tests/scheduler/common/dragAndDrop/T1080232.ts
+++ e2e/testcafe-devextreme/tests/scheduler/common/dragAndDrop/T1080232.ts
@@ -17,7 +17,7 @@ test('it should correctly drag external item to the appointment after drag appoi
   await t
     .dragToElement(appt01.element, cell01, { speed: 0.5 })
     .expect((await appt01.element.boundingClientRect).top)
-    .eql(183)
+    .eql((await cell01.boundingClientRect).top + (await cell01.boundingClientRect).height)
     .dragToElement(dragItem, appt02.element, { speed: 0.5 })
     .expect(appt02.element.find('.dx-item-content').getAttribute('data-status'))
     .eql('Added');
