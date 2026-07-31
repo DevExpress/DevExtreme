a3445ad4978728be2a0cef801f263d980a031809
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2026-07-31 16:13:55 +0400

    Global format config: fix errors after removing m_ prefix

--- packages/devextreme/js/__internal/core/global_format_config.ts
+++ packages/devextreme/js/__internal/core/global_format_config.ts
@@ -1,85 +1,93 @@
-import config from '@js/core/config';
 import coreLocalization from '@js/common/core/localization/core';
+import config from '@js/core/config';
+import { isFunction, isPlainObject, isString } from '@js/core/utils/type';
+import type { Format } from '@js/localization';
 import parentLocales from '@ts/core/localization/cldr-data/parent_locales';
 import getParentLocale from '@ts/core/localization/parentLocale';
-import { isFunction, isPlainObject, isString } from '@js/core/utils/type';
+
+type LocaleMap = Record<string, Format>;
+
+type GlobalFormatValue = Format | LocaleMap;
+
+type GlobalFormatOptionName = 'dateFormat' | 'dateTimeFormat' | 'timeFormat' | 'numberFormat';
 
 const hasOwn = Object.prototype.hasOwnProperty;
 
-const resolveByLocaleMap = (localeMap) => {
-    let currentLocale = coreLocalization.locale();
+const resolveByLocaleMap = (localeMap: LocaleMap): Format | undefined => {
+  let currentLocale: string | false = coreLocalization.locale();
 
-    while(currentLocale) {
-        if(hasOwn.call(localeMap, currentLocale) && localeMap[currentLocale] !== undefined) {
-            return localeMap[currentLocale];
-        }
-
-        currentLocale = getParentLocale(parentLocales, currentLocale);
+  while (currentLocale) {
+    if (hasOwn.call(localeMap, currentLocale) && localeMap[currentLocale] !== undefined) {
+      return localeMap[currentLocale];
     }
 
-    if(hasOwn.call(localeMap, 'default')) {
-        return localeMap.default;
-    }
+    currentLocale = getParentLocale(parentLocales, currentLocale);
+  }
 
+  if (hasOwn.call(localeMap, 'default')) {
+    return localeMap.default;
+  }
+
+  return undefined;
+};
+
+const resolveConfigValue = (value: GlobalFormatValue): Format | undefined => {
+  if (value === undefined) {
     return undefined;
+  }
+
+  if (isString(value) || isFunction(value)) {
+    return value;
+  }
+
+  if (isPlainObject(value)) {
+    // NOTE: any plain object is treated as a locale map ({ 'de-DE': format, default: format }).
+    // A FormatObject or Intl options object has no locale keys, so it resolves to undefined.
+    return resolveByLocaleMap(value as LocaleMap);
+  }
+
+  return undefined;
 };
 
-const resolveConfigValue = (value) => {
-    if(value === undefined) {
-        return undefined;
-    }
+const resolveGlobalFormat = (optionName: GlobalFormatOptionName): Format | undefined => {
+  const { [optionName]: optionValue } = config();
 
-    if(isString(value) || isFunction(value)) {
-        return value;
-    }
+  return resolveConfigValue(optionValue);
+};
 
-    if(isPlainObject(value)) {
-        return resolveByLocaleMap(value);
-    }
+export const getGlobalFormatByDataType = (dataType: string): Format | undefined => {
+  switch (dataType) {
+    case 'date':
+      return resolveGlobalFormat('dateFormat');
+    case 'datetime':
+      return resolveGlobalFormat('dateTimeFormat');
+    case 'time':
+      return resolveGlobalFormat('timeFormat');
+    case 'number':
+      return resolveGlobalFormat('numberFormat');
+    default:
+      return undefined;
+  }
+};
 
+export const resolvePresetOverride = (presetName: string): Format | undefined => {
+  const { dateTimeFormatPresets: presets } = config();
+
+  if (!presets || !isPlainObject(presets)) {
     return undefined;
-};
+  }
 
-const resolveGlobalFormat = (optionName) => {
-    const optionValue = config()[optionName];
-    return resolveConfigValue(optionValue);
-};
+  const lowerName = presetName.toLowerCase();
+  const matchedKey = Object.keys(presets).find((key) => key.toLowerCase() === lowerName);
 
-export const getGlobalFormatByDataType = (dataType) => {
-    switch(dataType) {
-        case 'date':
-            return resolveGlobalFormat('dateFormat');
-        case 'datetime':
-            return resolveGlobalFormat('dateTimeFormat');
-        case 'time':
-            return resolveGlobalFormat('timeFormat');
-        case 'number':
-            return resolveGlobalFormat('numberFormat');
-        default:
-            return undefined;
-    }
-};
-
-export const resolvePresetOverride = (presetName) => {
-    const presets = config().dateTimeFormatPresets;
-
-    if(!presets || !isPlainObject(presets)) {
-        return undefined;
-    }
-
-    const lowerName = presetName.toLowerCase();
-    const keys = Object.keys(presets);
-
-    for(let i = 0; i < keys.length; i++) {
-        if(keys[i].toLowerCase() === lowerName) {
-            return resolveConfigValue(presets[keys[i]]);
-        }
-    }
-
+  if (matchedKey === undefined) {
     return undefined;
+  }
+
+  return resolveConfigValue(presets[matchedKey]);
 };
 
 export default {
-    getGlobalFormatByDataType,
-    resolvePresetOverride,
+  getGlobalFormatByDataType,
+  resolvePresetOverride,
 };
