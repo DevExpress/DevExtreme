ae1a4a06adeb57673b1be67ebb7a4e3ace9d8f48
Aleksey Semikozov <aleksei.semikozov@devexpress.com>
2026-07-31 09:24:38 -0300

    PivotGrid - A11y: show a full focus outline on the field panel fields

--- e2e/testcafe-devextreme/tests/common/pivotGrid/kbn/fields.ts
+++ e2e/testcafe-devextreme/tests/common/pivotGrid/kbn/fields.ts
@@ -406,6 +406,26 @@ test('FieldChooser: Should traverse fields in all areas by tab', async (t) => {
     .ok('first field in data area is focused');
 }).before(async () => createWidget('dxPivotGrid', createConfig()));
 
+test('PivotGrid: Keyboard focus outline of a field should be drawn inside the field box', async (t) => {
+  const focusedElement = Selector(':focus');
+
+  for (let i = 0; i < 10; i += 1) {
+    await t.pressKey('tab');
+
+    if (await focusedElement.hasClass('dx-area-field')) {
+      break;
+    }
+  }
+
+  await t
+    .expect(focusedElement.hasClass('dx-area-field'))
+    .ok('a field is focused by keyboard')
+    .expect(focusedElement.getStyleProperty('outline-style'))
+    .eql('solid', 'the field has its own focus outline instead of the browser default one')
+    .expect(focusedElement.getStyleProperty('outline-offset'))
+    .eql('-2px', 'the outline is drawn inside the field box, so it is not cropped by the area overflow');
+}).before(async () => createWidget('dxPivotGrid', createConfig()));
+
 // TestCafe's pressKey does not support function keys (F1-F12), so the Shift+F10
 // shortcut is reproduced by dispatching the keydown event on the focused field.
 const SHIFT_F10_KEYDOWN = {
