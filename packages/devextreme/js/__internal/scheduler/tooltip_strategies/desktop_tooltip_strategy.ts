58ba85c0d6db157cee97370250f08f9e653be7f1
dmlvr <dmitrii.lavrinovich@devexpress.com>
2026-08-03 15:42:43 +0300

    update scheduler's appointment tooltip

--- packages/devextreme/js/__internal/scheduler/tooltip_strategies/desktop_tooltip_strategy.ts
+++ packages/devextreme/js/__internal/scheduler/tooltip_strategies/desktop_tooltip_strategy.ts
@@ -59,11 +59,12 @@ export class DesktopTooltipStrategy extends TooltipStrategyBase {
       onShown: this.onShown.bind(this),
       contentTemplate: this.getContentTemplate(dataList),
       wrapperAttr: { class: APPOINTMENT_TOOLTIP_WRAPPER_CLASS },
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      _popoverContentRole: 'dialog',
       tabFocusLoopEnabled: this.extraOptions?.tabFocusLoopEnabled,
     }) as Tooltip;
 
     tooltip.setAria({
-      role: 'dialog',
       label: messageLocalization.format('dxScheduler-appointmentListAriaLabel'),
     });
 
