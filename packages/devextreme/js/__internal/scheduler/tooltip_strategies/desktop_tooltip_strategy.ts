95970ab3db1e77e52468d1fdd54e9cdcd5717c8b
dmlvr <dmitrii.lavrinovich@devexpress.com>
2026-08-03 16:45:08 +0300

    prevent tooltip Focus Behavior for appointment tooltip

--- packages/devextreme/js/__internal/scheduler/tooltip_strategies/desktop_tooltip_strategy.ts
+++ packages/devextreme/js/__internal/scheduler/tooltip_strategies/desktop_tooltip_strategy.ts
@@ -59,9 +59,11 @@ export class DesktopTooltipStrategy extends TooltipStrategyBase {
       onShown: this.onShown.bind(this),
       contentTemplate: this.getContentTemplate(dataList),
       wrapperAttr: { class: APPOINTMENT_TOOLTIP_WRAPPER_CLASS },
+      tabFocusLoopEnabled: this.extraOptions?.tabFocusLoopEnabled,
       // eslint-disable-next-line @typescript-eslint/naming-convention
       _popoverContentRole: 'dialog',
-      tabFocusLoopEnabled: this.extraOptions?.tabFocusLoopEnabled,
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      _preventDialogContainerFocus: true,
     }) as Tooltip;
 
     tooltip.setAria({
