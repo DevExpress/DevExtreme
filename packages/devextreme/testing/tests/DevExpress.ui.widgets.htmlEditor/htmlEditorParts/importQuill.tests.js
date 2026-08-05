88c7bd860abdf3f9be6a3e678d58e5814c8181c6
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2026-08-05 15:16:21 +0200

    QUnit-ESM: use text without escapes

--- packages/devextreme/testing/tests/DevExpress.ui.widgets.htmlEditor/htmlEditorParts/importQuill.tests.js
+++ packages/devextreme/testing/tests/DevExpress.ui.widgets.htmlEditor/htmlEditorParts/importQuill.tests.js
@@ -1,13 +1,13 @@
 import { getQuill } from '__internal/ui/html_editor/m_quill_importer';
 
 QUnit.module('Import 3rd party', function() {
-    QUnit.test('it throw an error if the quill script isn\'t referenced', function(assert) {
+    QUnit.test('it throw an error if the quill script is not referenced', function(assert) {
         assert.throws(
             function() { getQuill(); },
             function(e) {
                 return /(E1041)[\s\S]*(Quill)/.test(e.message);
             },
-            'The Quill script isn\'t referenced'
+            'The Quill script is not referenced'
         );
     });
 });
