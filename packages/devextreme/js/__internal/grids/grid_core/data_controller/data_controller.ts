44efca40dc72b5e98535b7cd419f51f7f9c1a6c5
Maksim Zakharov <251575087+bit-byte0@users.noreply.github.com>
2026-08-05 13:47:13 +0400

    refactor(dataGrid): drop _ from changePaging and simplify paging flag type

--- packages/devextreme/js/__internal/grids/grid_core/data_controller/data_controller.ts
+++ packages/devextreme/js/__internal/grids/grid_core/data_controller/data_controller.ts
@@ -97,7 +97,7 @@ export class DataController extends DataHelperMixin(modules.Controller) {
 
   protected _changes!: any[];
 
-  private _skipProcessingPagingChange: boolean | undefined;
+  private _skipProcessingPagingChange?: boolean;
 
   private _useSortingGroupingFromColumns: boolean | undefined;
 
@@ -1622,7 +1622,7 @@ export class DataController extends DataHelperMixin(modules.Controller) {
     return result;
   }
 
-  private _changePaging(optionName: 'pageIndex' | 'pageSize', value?: number): any {
+  private changePaging(optionName: 'pageIndex' | 'pageSize', value?: number): any {
     const dataSource = this._dataSource;
 
     if (!dataSource) {
@@ -1662,11 +1662,11 @@ export class DataController extends DataHelperMixin(modules.Controller) {
    * @extended: virtual_scrolling
    */
   public pageIndex(value?: number): any {
-    return this._changePaging('pageIndex', value);
+    return this.changePaging('pageIndex', value);
   }
 
   public pageSize(value?: number): any {
-    return this._changePaging('pageSize', value);
+    return this.changePaging('pageSize', value);
   }
 
   public isCustomLoading() {
