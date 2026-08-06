f2a28ce9f5f885a9f9d20bca61b191be2b0f04b7
Dmitry Lavrinovich <52966626+dmlvr@users.noreply.github.com>
2026-08-06 15:40:21 +0300

    Design System: Rename theme from dxdsfluent to fluent-next (#34677)

--- packages/nx-infra-plugin/src/executors/scss-build/scss-build.impl.ts
+++ packages/nx-infra-plugin/src/executors/scss-build/scss-build.impl.ts
@@ -26,9 +26,9 @@ const DEFAULT_DEV_BUNDLE_NAMES = [
   'fluent.blue.dark',
   'fluent.saas.light',
   'fluent.saas.dark',
-  'dxdsfluent.blue.light',
-  'dxdsfluent.blue.light.compact',
-  'dxdsfluent.blue.dark',
+  'fluent-next.blue.light',
+  'fluent-next.blue.light.compact',
+  'fluent-next.blue.dark',
 ];
 
 interface BuildDependencies {
@@ -77,7 +77,7 @@ function prependLicenseAndMoveCharsetFirst(minifiedCss: string, license: string)
 function generateBundleName(theme: string, size: string, color: string, mode?: string): string {
   return (
     'dx'
-    + (theme === 'material' || theme === 'fluent' || theme === 'dxdsfluent' ? `.${theme}` : '')
+    + (theme === 'material' || theme === 'fluent' || theme === 'fluent-next' ? `.${theme}` : '')
     + `.${color}`
     + (mode ? `.${mode}` : '')
     + (size === 'default' ? '' : '.compact')
