dfe12cc7cabdbd45f4e89a8af528893f82b1d477
Andrey Vorobev <738482+vorobey@users.noreply.github.com>
2026-08-06 12:13:53 +0300

    Feature/demos remove system js infra (#34671)
    Co-authored-by: Arman Jivanyan <arman.jivanyan@devexpress.com>

--- apps/demos/scripts/prepare-js-configs.js
+++ apps/demos/scripts/prepare-js-configs.js
@@ -1,13 +1,7 @@
-const { join } = require('path');
 const { init } = require('../utils/shared/config-helper');
-const createConfig = require('../utils/internal/create-config');
 const { copyJsSharedResources } = require('../utils/copy-shared-resources/copy');
-
-const demosDir = join(__dirname, '..', 'Demos');
 
 init();
 copyJsSharedResources(() => {});
-createConfig.useBundles = false;
-createConfig.run(demosDir);
 
 console.log('prepare-js-configs: done');
--- apps/demos/utils/svg-loader.js
+++
@@ -1,1 +0,0 @@
-module.exports.translate = (data) => `module.exports = {default: \`data:image/svg+xml;base64,${btoa(data.source)}\`}`;
