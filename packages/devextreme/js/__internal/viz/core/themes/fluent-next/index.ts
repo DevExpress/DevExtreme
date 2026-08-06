f41cc50aa21e3960c297739be1fcd8fdf1cd0f9b
Dmitry Lavrinovich <52966626+dmlvr@users.noreply.github.com>
2026-08-06 15:40:21 +0300

    Design System: Rename theme from dxdsfluent to fluent-next (#34677)

---
+++ packages/devextreme/js/__internal/viz/core/themes/fluent-next/index.ts
@@ -0,0 +1,28 @@
+const themes = [
+  {
+    baseThemeName: 'fluent.blue.light',
+    theme: {
+      name: 'fluent-next.blue.light',
+    },
+  },
+  {
+    baseThemeName: 'fluent-next.blue.light',
+    theme: {
+      name: 'fluent-next.blue.light.compact',
+    },
+  },
+  {
+    baseThemeName: 'fluent.blue.dark',
+    theme: {
+      name: 'fluent-next.blue.dark',
+    },
+  },
+  {
+    baseThemeName: 'fluent-next.blue.dark',
+    theme: {
+      name: 'fluent-next.blue.dark.compact',
+    },
+  },
+];
+
+export default themes;
