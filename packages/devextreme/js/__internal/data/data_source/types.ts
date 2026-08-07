aecfd476432377d2336604a31f8be1f72b88aeca
Eldar Iusupzhanov <eldar.iusupzhanov@users.noreply.github.com>
2026-08-07 19:12:50 +0600

    apply minor comment

--- packages/devextreme/js/__internal/data/data_source/types.ts
+++ packages/devextreme/js/__internal/data/data_source/types.ts
@@ -1,15 +1,10 @@
 import type { DataSourceOptionsStub } from '@js/data/data_source';
 
-export interface StoreLoadOptions {
-  sort?: DataSourceOptionsStub['sort'];
-  filter?: DataSourceOptionsStub['filter'];
-  langParams?: DataSourceOptionsStub['langParams'];
-  select?: DataSourceOptionsStub['select'];
-  group?: DataSourceOptionsStub['group'];
-  requireTotalCount?: DataSourceOptionsStub['requireTotalCount'];
-  searchOperation?: DataSourceOptionsStub['searchOperation'];
-  searchValue?: DataSourceOptionsStub['searchValue'];
-  searchExpr?: DataSourceOptionsStub['searchExpr'];
+export interface StoreLoadOptions extends Pick<
+  DataSourceOptionsStub,
+    'sort' | 'filter' | 'langParams' | 'select' | 'group'
+    | 'requireTotalCount' | 'searchOperation' | 'searchValue' | 'searchExpr'
+> {
   skip?: number;
   take?: number;
   userData?: unknown;
