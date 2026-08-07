b060f579d5facf2be91bb76057f18df94f0c0f6f
Maksim Zakharov <251575087+bit-byte0@users.noreply.github.com>
2026-08-07 13:49:11 +0400

    refactor(dataGrid): extract remoteOperations and store checks into utils

--- packages/devextreme/js/__internal/grids/grid_core/data_controller/types.ts
+++ packages/devextreme/js/__internal/grids/grid_core/data_controller/types.ts
@@ -1,5 +1,7 @@
+import type { Mode } from '@js/common';
 import type { DataSource } from '@js/common/data';
 import type { DeferredObj } from '@js/core/utils/deferred';
+import type { Properties } from '@js/ui/data_grid';
 
 import type { OperationTypes } from '../data_source_adapter/types';
 
@@ -108,3 +110,7 @@ export type PagingResult = number | DeferredObj<unknown> | Promise<unknown>;
 export interface CallbackFlags {
   stopOnFalse: boolean;
 }
+
+export type RemoteOperations = Properties['remoteOperations'];
+
+export type RemoteOperationsOptions = Exclude<RemoteOperations, boolean | Mode | undefined>;
