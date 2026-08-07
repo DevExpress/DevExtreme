1b124d9001c06a726eeab7c60cef3bdaa24c09bc
Eldar Iusupzhanov <eldar.iusupzhanov@users.noreply.github.com>
2026-08-07 16:57:10 +0600

    fix

---
+++ packages/devextreme/js/__internal/data/data_source/types.ts
@@ -0,0 +1,21 @@
+import type { DataSourceOptionsStub } from '@js/data/data_source';
+
+export interface StoreLoadOptions {
+  sort?: DataSourceOptionsStub['sort'];
+  filter?: DataSourceOptionsStub['filter'];
+  langParams?: DataSourceOptionsStub['langParams'];
+  select?: DataSourceOptionsStub['select'];
+  group?: DataSourceOptionsStub['group'];
+  requireTotalCount?: DataSourceOptionsStub['requireTotalCount'];
+  searchOperation?: DataSourceOptionsStub['searchOperation'];
+  searchValue?: DataSourceOptionsStub['searchValue'];
+  searchExpr?: DataSourceOptionsStub['searchExpr'];
+  skip?: number;
+  take?: number;
+  userData?: unknown;
+}
+
+export interface LoadOperation {
+  operationId: number;
+  storeLoadOptions: StoreLoadOptions;
+}
