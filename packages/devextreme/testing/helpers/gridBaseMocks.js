854618cc13f372e870fdfc7cdddc1b23abed5de1
Maksim Zakharov <251575087+bit-byte0@users.noreply.github.com>
2026-08-17 17:34:22 +0400

    test(grids): stub filter-excluded-column accessors on MockDataController

--- packages/devextreme/testing/helpers/gridBaseMocks.js
+++ packages/devextreme/testing/helpers/gridBaseMocks.js
@@ -236,6 +236,10 @@ module.exports = function($, gridCore, columnResizingReordering, domUtils, commo
 
             getCombinedFilter: commonUtils.noop,
 
+            setFilterExcludedColumn: commonUtils.noop,
+
+            getFilterExcludedColumn: commonUtils.noop,
+
             getRowIndexByKey: function(key) {
                 return gridCore.getIndexByKey(key, options.items);
             },
