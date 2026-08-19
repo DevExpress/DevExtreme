ee114f8d5417f4fa1dceaf1503f4c3edc939e5f3
Alisher Amonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-08-19 13:12:27 +0300

    Update packages/devextreme/js/__internal/ui/errors.ts
    Co-authored-by: Arman Boyakhchyan <arman.boyakhchyan@devexpress.com>
    Signed-off-by: Alisher Amonulloev <AlisherAmonulloev@users.noreply.github.com>

--- packages/devextreme/js/__internal/ui/errors.ts
+++ packages/devextreme/js/__internal/ui/errors.ts
@@ -113,7 +113,7 @@ export default errorUtils(errors.ERROR_MESSAGES, {
 
   E1068: '\'aiIntegration\' is not configured for the AI Assistant.',
 
-  E1069: 'Could not find OpenLayers. Load the library before Map initialization.',
+  E1069: 'Could not find a compatible version of OpenLayers. Load a supported version before Map initialization.',
 
   W1001: 'The "key" option cannot be modified after initialization',
 
