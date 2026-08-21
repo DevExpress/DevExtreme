bed979f411d1231e60d4221759eaf6b9fff434bb
Alyar <Alyar666@users.noreply.github.com>
2026-08-21 15:21:43 +0300

    Grids: Move _adaptiveColumnsController from dataController to an extender and remove the underscore prefix from its name (#34865)

--- packages/devextreme/js/__internal/grids/grid_core/keyboard_navigation/m_keyboard_navigation.ts
+++ packages/devextreme/js/__internal/grids/grid_core/keyboard_navigation/m_keyboard_navigation.ts
@@ -143,7 +143,7 @@ export class KeyboardNavigationController extends KeyboardNavigationControllerCo
 
   private _focusController!: Controllers['focus'];
 
-  private _adaptiveColumnsController!: Controllers['adaptiveColumns'];
+  private adaptiveColumnsController!: Controllers['adaptiveColumns'];
 
   private _columnResizerController!: Controllers['columnsResizer'];
 
@@ -157,7 +157,7 @@ export class KeyboardNavigationController extends KeyboardNavigationControllerCo
     this._headerPanel = this.getView('headerPanel');
     this._editorFactory = this.getController('editorFactory');
     this._focusController = this.getController('focus');
-    this._adaptiveColumnsController = this.getController('adaptiveColumns');
+    this.adaptiveColumnsController = this.getController('adaptiveColumns');
     this._columnResizerController = this.getController('columnsResizer');
     this._rowsView = this.getView('rowsView');
 
@@ -1250,7 +1250,7 @@ export class KeyboardNavigationController extends KeyboardNavigationControllerCo
         (this._dataController as any).changeRowExpand(key);
       }
     } else if (needExpandAdaptiveRow) {
-      this._adaptiveColumnsController.toggleExpandAdaptiveDetailRow(key);
+      this.adaptiveColumnsController.toggleExpandAdaptiveDetailRow(key);
 
       this._updateFocusedCellPosition($cell);
     } else if (this.getMasterDetailCell($cell)?.is($cell)) {
