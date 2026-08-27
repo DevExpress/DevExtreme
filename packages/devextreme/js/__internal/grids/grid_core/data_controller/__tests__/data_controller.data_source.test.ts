348406701461e6bb978a5ef5bfffc9f7cb07ab12
Maksim Zakharov <251575087+bit-byte0@users.noreply.github.com>
2026-08-28 01:31:25 +0400

    GridCore data: Merge `DataHelperMixin` into `DataController` (#34956)

---
+++ packages/devextreme/js/__internal/grids/grid_core/data_controller/__tests__/data_controller.data_source.test.ts
@@ -0,0 +1,60 @@
+import {
+  afterEach,
+  beforeEach,
+  describe,
+  expect,
+  it,
+} from '@jest/globals';
+import DataSource from '@js/data/data_source';
+import type { DataGridInstance } from '@ts/grids/grid_core/__tests__/__mock__/helpers/utils';
+import {
+  afterTest,
+  beforeTest,
+  createDataGrid,
+  flushAsync,
+} from '@ts/grids/grid_core/__tests__/__mock__/helpers/utils';
+
+const DATA = [
+  { id: 1, value: 'a' },
+  { id: 2, value: 'b' },
+];
+
+const getIsSharedDataSource = (instance: DataGridInstance): unknown => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const dataController = instance.getController('data') as any;
+
+  return dataController.isSharedDataSource as unknown;
+};
+
+describe('DataController data source', () => {
+  beforeEach(beforeTest);
+  afterEach(afterTest);
+
+  describe('isSharedDataSource', () => {
+    it('should be true when a DataSource instance is passed', async () => {
+      const sharedDataSource = new DataSource({ store: DATA, key: 'id' });
+
+      const { instance } = await createDataGrid({ dataSource: sharedDataSource });
+
+      expect(getIsSharedDataSource(instance)).toBe(true);
+    });
+
+    it('should be false when a plain array is passed', async () => {
+      const { instance } = await createDataGrid({ dataSource: DATA });
+
+      expect(getIsSharedDataSource(instance)).toBe(false);
+    });
+
+    it('should reset to false after switching from a shared DataSource to a plain array', async () => {
+      const sharedDataSource = new DataSource({ store: DATA, key: 'id' });
+
+      const { instance } = await createDataGrid({ dataSource: sharedDataSource });
+      expect(getIsSharedDataSource(instance)).toBe(true);
+
+      instance.option('dataSource', DATA);
+      await flushAsync();
+
+      expect(getIsSharedDataSource(instance)).toBe(false);
+    });
+  });
+});
--- packages/devextreme/js/__internal/grids/tree_list/data_controller/m_data_controller.ts
+++ packages/devextreme/js/__internal/grids/tree_list/data_controller/m_data_controller.ts
@@ -157,7 +157,7 @@ export class TreeListDataController extends DataController {
       case 'dataStructure':
         this._columnsController.reset();
         this._items = [];
-        this._refreshDataSource();
+        this.resetDataSource();
         args.handled = true;
         break;
       case 'expandedRowKeys':
