59ecb491a4d940a1bd1f139a9bf217013394ba63
Aleksey Semikozov <aleksei.semikozov@devexpress.com>
2026-08-28 03:00:18 -0300

    Wrap the scrollTo offset table to the line limit

--- e2e/playwright/tests/scheduler/common/scrollTo.spec.ts
+++ e2e/playwright/tests/scheduler/common/scrollTo.spec.ts
@@ -220,21 +220,65 @@ test('ScrollTo works correctly in timeline RTL (native, sync header/workspace)',
 
 [
   // startDayHour: 6:00, endDayHour: 18:00
-  { offset: 0, targetDate: new Date(2021, 1, 3, 4, 0), expectedDate: new Date(2021, 1, 3, 6, 0) },
-  { offset: 0, targetDate: new Date(2021, 1, 3, 12, 0), expectedDate: new Date(2021, 1, 3, 12, 0) },
-  { offset: 0, targetDate: new Date(2021, 1, 3, 20, 0), expectedDate: new Date(2021, 1, 3, 18, 0) },
+  {
+    offset: 0,
+    targetDate: new Date(2021, 1, 3, 4, 0),
+    expectedDate: new Date(2021, 1, 3, 6, 0),
+  },
+  {
+    offset: 0,
+    targetDate: new Date(2021, 1, 3, 12, 0),
+    expectedDate: new Date(2021, 1, 3, 12, 0),
+  },
+  {
+    offset: 0,
+    targetDate: new Date(2021, 1, 3, 20, 0),
+    expectedDate: new Date(2021, 1, 3, 18, 0),
+  },
 
   // startDayHour: 18:00, endDayHour: next day 6:00
-  { offset: 720, targetDate: new Date(2021, 1, 3, 10, 0), expectedDate: new Date(2021, 1, 3, 6, 0) },
-  { offset: 720, targetDate: new Date(2021, 1, 3, 20, 0), expectedDate: new Date(2021, 1, 3, 20, 0) },
-  { offset: 720, targetDate: new Date(2021, 1, 4, 1, 0), expectedDate: new Date(2021, 1, 4, 1, 0) },
-  { offset: 720, targetDate: new Date(2021, 1, 4, 7, 0), expectedDate: new Date(2021, 1, 4, 6, 0) },
+  {
+    offset: 720,
+    targetDate: new Date(2021, 1, 3, 10, 0),
+    expectedDate: new Date(2021, 1, 3, 6, 0),
+  },
+  {
+    offset: 720,
+    targetDate: new Date(2021, 1, 3, 20, 0),
+    expectedDate: new Date(2021, 1, 3, 20, 0),
+  },
+  {
+    offset: 720,
+    targetDate: new Date(2021, 1, 4, 1, 0),
+    expectedDate: new Date(2021, 1, 4, 1, 0),
+  },
+  {
+    offset: 720,
+    targetDate: new Date(2021, 1, 4, 7, 0),
+    expectedDate: new Date(2021, 1, 4, 6, 0),
+  },
 
   // startDayHour: prev day 18:00, endDayHour: 6:00
-  { offset: -720, targetDate: new Date(2021, 1, 3, 16, 0), expectedDate: new Date(2021, 1, 3, 18, 0) },
-  { offset: -720, targetDate: new Date(2021, 1, 3, 21, 0), expectedDate: new Date(2021, 1, 3, 21, 0) },
-  { offset: -720, targetDate: new Date(2021, 1, 4, 3, 0), expectedDate: new Date(2021, 1, 4, 3, 0) },
-  { offset: -720, targetDate: new Date(2021, 1, 3, 7, 0), expectedDate: new Date(2021, 1, 3, 6, 0) },
+  {
+    offset: -720,
+    targetDate: new Date(2021, 1, 3, 16, 0),
+    expectedDate: new Date(2021, 1, 3, 18, 0),
+  },
+  {
+    offset: -720,
+    targetDate: new Date(2021, 1, 3, 21, 0),
+    expectedDate: new Date(2021, 1, 3, 21, 0),
+  },
+  {
+    offset: -720,
+    targetDate: new Date(2021, 1, 4, 3, 0),
+    expectedDate: new Date(2021, 1, 4, 3, 0),
+  },
+  {
+    offset: -720,
+    targetDate: new Date(2021, 1, 3, 7, 0),
+    expectedDate: new Date(2021, 1, 3, 6, 0),
+  },
 ].forEach(({ offset, targetDate, expectedDate }) => {
   test(`scrollTo should scroll to date with offset=${offset}, targetDate=${targetDate.toString()} (T1310544)`, async ({ page }) => {
     await createScheduler(page, {
