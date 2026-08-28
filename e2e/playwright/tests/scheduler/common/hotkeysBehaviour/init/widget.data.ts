b21b55e14b5883ee01d1d7cae8e933cc876fe2f7
Aleksey Semikozov <aleksei.semikozov@devexpress.com>
2026-08-28 06:03:19 -0300

    Migrate the Scheduler hotkeys behaviour tests

---
+++ e2e/playwright/tests/scheduler/common/hotkeysBehaviour/init/widget.data.ts
@@ -0,0 +1,42 @@
+export default [
+  {
+    text: 'Website Re-Design Plan',
+    startDate: new Date(2019, 3, 1, 9, 0),
+    endDate: new Date(2019, 3, 1, 11, 30),
+  },
+  {
+    text: 'Book Flights to San Fran for Sales Trip',
+    startDate: new Date(2019, 3, 1, 9, 0),
+    endDate: new Date(2019, 3, 1, 11, 30),
+  },
+  {
+    text: 'Install New Router in Dev Room',
+    startDate: new Date(2019, 3, 1, 9, 0),
+    endDate: new Date(2019, 3, 1, 11, 30),
+  },
+  {
+    text: 'Approve Personal Computer Upgrade Plan',
+    startDate: new Date(2019, 3, 1, 9, 0),
+    endDate: new Date(2019, 3, 1, 11, 30),
+  },
+  {
+    text: 'Final Budget Review',
+    startDate: new Date(2019, 3, 1, 9, 0),
+    endDate: new Date(2019, 3, 1, 11, 30),
+  },
+  {
+    text: 'New Brochures',
+    startDate: new Date(2019, 3, 1, 9, 0),
+    endDate: new Date(2019, 3, 1, 11, 30),
+  },
+  {
+    text: 'Install New Database',
+    startDate: new Date(2019, 3, 1, 9, 0),
+    endDate: new Date(2019, 3, 1, 11, 30),
+  },
+  {
+    text: 'Approve New Online Marketing Strategy',
+    startDate: new Date(2019, 3, 1, 9, 0),
+    endDate: new Date(2019, 3, 1, 11, 30),
+  },
+];
