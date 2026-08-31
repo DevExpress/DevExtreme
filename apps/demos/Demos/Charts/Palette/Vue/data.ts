b515e0736442d51b26c8f3df09fb6f27ffe90db2
Raushen <andrey.dolzhikov@devexpress.com>
2026-08-31 12:11:22 +0300

    Update demo palettes

--- apps/demos/Demos/Charts/Palette/Vue/data.ts
+++ apps/demos/Demos/Charts/Palette/Vue/data.ts
@@ -1,7 +1,7 @@
 import { type DxPieChartTypes } from 'devextreme-vue/pie-chart';
 import { type PaletteExtensionMode } from 'devextreme-vue/common/charts';
 
-export const paletteCollection: DxPieChartTypes.Palette[] = ['Material', 'Soft Pastel', 'Harmony Light', 'Pastel', 'Bright', 'Soft', 'Ocean', 'Office', 'Vintage', 'Violet', 'Carmine', 'Dark Moon', 'Soft Blue', 'Dark Violet', 'Green Mist'];
+export const paletteCollection: DxPieChartTypes.Palette[] = ['Material', 'Fluent Next', 'Soft Pastel', 'Harmony Light', 'Pastel', 'Bright', 'Soft', 'Ocean', 'Office', 'Vintage', 'Violet', 'Carmine', 'Dark Moon', 'Soft Blue', 'Dark Violet', 'Green Mist'];
 export const paletteExtensionModes: PaletteExtensionMode[] = ['alternate', 'blend', 'extrapolate'];
 
 const data = [];
