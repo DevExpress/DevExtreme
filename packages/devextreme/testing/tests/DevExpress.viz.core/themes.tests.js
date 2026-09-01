fc0845bfece2cc50a2f9c6a24c7bc0138a9845d3
Andrey Dolzhikov <16618553+Raushen@users.noreply.github.com>
2026-09-01 18:50:42 +0300

    Fluent-next: change colors in Viz palettes (#34981)

--- packages/devextreme/testing/tests/DevExpress.viz.core/themes.tests.js
+++ packages/devextreme/testing/tests/DevExpress.viz.core/themes.tests.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import themeModule from 'viz/themes';
+import { createPalette, getGradientPalette } from 'viz/palette';
 import uiThemeModule from 'ui/themes';
 
 uiThemeModule.setDefaultTimeout(0);
@@ -359,15 +360,41 @@ QUnit.test('Invalid input data (with color scheme)', function(assert) {
 });
 
 [
-    'fluent-next.blue.light',
-    'fluent-next.blue.light.compact',
-    'fluent-next.blue.dark',
-    'fluent-next.blue.dark.compact',
-].forEach((theme) => {
+    { theme: 'fluent-next.blue.light', accentColor: '#0F6CBD' },
+    { theme: 'fluent-next.blue.light.compact', accentColor: '#0F6CBD' },
+    { theme: 'fluent-next.blue.dark', accentColor: '#4B90D9' },
+    { theme: 'fluent-next.blue.dark.compact', accentColor: '#4B90D9' },
+].forEach(({ theme, accentColor }) => {
     QUnit.test(`fluent-next theme should be registered: ${theme}`, function(assert) {
         themeModule.currentTheme(theme);
 
         assert.strictEqual(themeModule.currentTheme(), theme);
+    });
+
+    QUnit.test(`fluent-next theme should color series with the Fluent Next palette: ${theme}`, function(assert) {
+        const defaultPalette = themeModule.getTheme(theme).defaultPalette;
+
+        const simpleSet = createPalette(undefined, {}, defaultPalette).generateColors(6);
+        const indicatingSet = createPalette(undefined, { type: 'indicatingSet' }, defaultPalette).generateColors(3);
+        const gradientSet = getGradientPalette(undefined, defaultPalette);
+
+        assert.deepEqual(simpleSet, ['#0078d4', '#c83d3d', '#008f04', '#eaa300', '#e43ba6', '#865cbf'], 'simpleSet');
+        assert.deepEqual(indicatingSet, ['#008f04', '#eaa300', '#c83d3d'], 'indicatingSet');
+        assert.deepEqual([gradientSet.getColor(0), gradientSet.getColor(1)], ['#0078d4', '#008f04'], 'gradientSet');
+    });
+
+    QUnit.test(`fluent-next theme should accent widgets with its own primary color: ${theme}`, function(assert) {
+        const registeredTheme = themeModule.getTheme(theme);
+
+        assert.strictEqual(registeredTheme.rangeSelector.selectedRangeColor, accentColor, 'rangeSelector selected range');
+        assert.strictEqual(registeredTheme.rangeSelector.sliderMarker.color, accentColor, 'rangeSelector slider marker');
+        assert.strictEqual(registeredTheme.rangeSelector.sliderHandle.color, accentColor, 'rangeSelector slider handle');
+        assert.strictEqual(registeredTheme.map['layer:marker:dot'].color, accentColor, 'map dot marker');
+        assert.strictEqual(registeredTheme.map['layer:marker:bubble'].color, accentColor, 'map bubble marker');
+        assert.strictEqual(registeredTheme.map.legend.markerColor, accentColor, 'map legend marker');
+        assert.strictEqual(registeredTheme.bullet.color, accentColor, 'bullet');
+        assert.strictEqual(registeredTheme.gauge.valueIndicators.rangebar.color, accentColor, 'gauge rangebar');
+        assert.strictEqual(registeredTheme.gauge.valueIndicators['textcloud'].color, accentColor, 'gauge textcloud');
     });
 });
 
