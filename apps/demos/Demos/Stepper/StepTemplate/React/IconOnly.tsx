94bfa174ec8a0e4a004874dae272bdeaff3fc865
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2026-09-06 01:58:45 +0400

    chore(deps): bump axe to v4.13.0

--- apps/demos/Demos/Stepper/StepTemplate/React/IconOnly.tsx
+++ apps/demos/Demos/Stepper/StepTemplate/React/IconOnly.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 interface IconOnlyProps {
   icon: string;
+  label: string;
 }
 
-export default function IconOnly({ icon }: IconOnlyProps) {
+export default function IconOnly({ icon, label }: IconOnlyProps) {
   return (
-    <i className={`dx-icon dx-icon-${icon}`}></i>
+    <i className={`dx-icon dx-icon-${icon}`} role="img" aria-label={label}></i>
   );
 }
