d89ef0a96800176a23b8216f52bf9b93ff805b27
Aleksei Semikozov <2846685+aleksei-semikozov@users.noreply.github.com>
2026-09-08 08:24:49 +0000

    fix (demos): Revert the ResolveTimeConflicts date hardcode and mask the fluent etalon instead (#35079)
    Co-authored-by: Aleksey Semikozov <aleksei.semikozov@devexpress.com>

--- apps/demos/Demos/Scheduler/ResolveTimeConflicts/ReactJs/App.js
+++ apps/demos/Demos/Scheduler/ResolveTimeConflicts/ReactJs/App.js
@@ -149,14 +149,6 @@ const App = () => {
           formRef.current?.validate();
         }
       });
-      e.component.on('contentReady', (contentReadyEvent) => {
-        const untilEditor = contentReadyEvent.component.getEditor('recurrenceEndUntilEditor');
-        untilEditor?.on('valueChanged', () => {
-          contentReadyEvent.component.getEditor('recurrenceRepeatEndEditor')?.option('value') !==
-            'until' && untilEditor?.option('value', new Date(2026, 8, 3));
-        });
-        untilEditor?.option('value', new Date(2026, 8, 3));
-      });
     },
     [setConflictError],
   );
