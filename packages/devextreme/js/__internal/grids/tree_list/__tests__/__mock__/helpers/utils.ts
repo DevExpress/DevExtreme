d96abb4dd33b642bb971644ed4c0b19adff2d651
Alyar <Alyar666@users.noreply.github.com>
2026-09-08 08:29:01 +0000

    Grids - DataController: Extract user state knowledge to extenders (#35064)
    Co-authored-by: Alyar <>

---
+++ packages/devextreme/js/__internal/grids/tree_list/__tests__/__mock__/helpers/utils.ts
@@ -0,0 +1,66 @@
+import { jest } from '@jest/globals';
+import fx from '@js/common/core/animation/fx';
+import type { dxElementWrapper } from '@js/core/renderer';
+import $ from '@js/core/renderer';
+import type { Properties as TreeListProperties } from '@js/ui/tree_list';
+import TreeList from '@js/ui/tree_list';
+import type { Controllers, Views } from '@ts/grids/grid_core/m_types';
+import { TreeListModel } from '@ts/grids/tree_list/__tests__/__mock__/model/tree_list';
+
+export interface TreeListInstance extends TreeList {
+  getController: <T extends keyof Controllers>(name: T) => Controllers[T];
+  getView: <T extends keyof Views>(name: T) => Views[T];
+}
+
+export const SELECTORS = {
+  treeListContainer: '#treeListContainer',
+};
+
+export const TREELIST_CONTAINER_ID = 'treeListContainer';
+
+export const createTreeList = (
+  options: TreeListProperties = {},
+): Promise<{
+  $container: dxElementWrapper;
+  component: TreeListModel;
+  instance: TreeListInstance;
+}> => new Promise((resolve) => {
+  const $container = $('<div>')
+    .attr('id', TREELIST_CONTAINER_ID)
+    .appendTo(document.body);
+
+  const treeListOptions: TreeListProperties = {
+    keyExpr: 'id',
+    ...options,
+  };
+
+  const instance = new TreeList(
+    $container.get(0) as HTMLDivElement,
+    treeListOptions,
+  ) as TreeListInstance;
+  const component = new TreeListModel($container.get(0) as HTMLElement);
+
+  jest.runAllTimers();
+
+  resolve({
+    $container,
+    component,
+    instance,
+  });
+});
+
+export const beforeTest = (): void => {
+  fx.off = true;
+  jest.useFakeTimers();
+};
+
+export const afterTest = (): void => {
+  const $container = $(SELECTORS.treeListContainer);
+  const treeList = TreeList.getInstance($container.get(0));
+
+  treeList?.dispose();
+  $container.remove();
+  jest.clearAllMocks();
+  jest.useRealTimers();
+  fx.off = false;
+};
