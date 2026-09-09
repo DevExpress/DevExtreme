5be9943eb385526c3a736685dd2fc48ffd124b01
Maksim Zakharov <251575087+bit-byte0@users.noreply.github.com>
2026-09-09 13:05:29 +0400

    Grids - DataController: Extract summary loadAll knowledge to extenders

--- packages/devextreme/js/__internal/grids/data_grid/summary/extenders/summary_data_controller.ts
+++ packages/devextreme/js/__internal/grids/data_grid/summary/extenders/summary_data_controller.ts
@@ -1,3 +1,4 @@
+import type { DeferredObj } from '@js/core/utils/deferred';
 import { extend } from '@js/core/utils/extend';
 import {
   isDefined, isEmptyObject, isString,
@@ -7,6 +8,7 @@ import type { SummaryGroupItem as SummaryGroupItemOption } from '@js/ui/data_gri
 import type { Column } from '@ts/grids/data_grid/types';
 import type { DataController } from '@ts/grids/grid_core/data_controller/data_controller';
 import type { DataChange, ItemProcessingOptions, ProcessedItem } from '@ts/grids/grid_core/data_controller/types';
+import type { CustomLoadResult } from '@ts/grids/grid_core/data_source_adapter/custom_loader';
 import type { RawItemData } from '@ts/grids/grid_core/data_source_adapter/types';
 import type { ModuleType, OptionChanged } from '@ts/grids/grid_core/m_types';
 
@@ -299,6 +301,15 @@ export const summaryDataControllerExtender = (
     }
 
     return super.getChangedColumnIndices(oldItem, newItem, visibleRowIndex, isLiveUpdate);
+  }
+
+  protected resolveLoadAllItems(
+    d: DeferredObj<ProcessedItem[]>,
+    loadResult: CustomLoadResult,
+  ): void {
+    const resolveWithSummary = d.resolve as (...args: unknown[]) => void;
+
+    resolveWithSummary(this.processLoadAllItems(loadResult), loadResult.extra?.summary);
   }
 
   protected _updateItemsCore(change: DataChange): void {
