3549421c23e3c8fa8a2b76f4fa38b1cbe3f3c477
Aliullov Vlad <vladislav.aliullov@devexpress.com>
2026-09-10 15:55:24 +0200

    Update to Angular 21: fix angular e2e tests

--- e2e/wrappers/builders/angular/src/app/app.config.ts
+++ e2e/wrappers/builders/angular/src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideClientHydration } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
 
@@ -8,6 +7,5 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(),
   ],
 };
