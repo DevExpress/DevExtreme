f14eabde145d7e7206e59c90af7b728c7d762116
Maksim Zakharov <251575087+bit-byte0@users.noreply.github.com>
2026-09-12 17:21:57 +0400

    refactor(grids): move updateFilter into FilterController as normalizeFilterSelectors

--- packages/devextreme/js/__internal/grids/grid_core/data_controller/data_controller.ts
+++ packages/devextreme/js/__internal/grids/grid_core/data_controller/data_controller.ts
@@ -341,7 +341,7 @@ export class DataController extends modules.Controller {
 
     const isRemoteFiltering = this._dataSource.remoteOperations().filtering || returnDataField;
 
-    combined = this._columnsController.updateFilter(combined, isRemoteFiltering);
+    combined = this.filterController.normalizeFilterSelectors(combined, isRemoteFiltering);
 
     return combined;
   }
