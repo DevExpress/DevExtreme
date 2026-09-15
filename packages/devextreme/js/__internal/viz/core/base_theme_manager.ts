9216b397b1e3b5bd4dd5c8af7d2b175505fe93ea
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2026-09-15 16:22:06 +0400

    Charts: convert the theme managers into ES6 classes and improve typing
    BaseThemeManager, the chart ThemeManager and the gauge ThemeManager
    leave Class.inherit(): constructor instead of ctor, super.x() instead of
    callBase, the chart config IIFE becomes class methods with a switch in
    getOptions(). Runtime kept as is, including the release of fields to null
    on dispose() and the numeric forceUserTickInterval coercion.
    
    polarChart.tests.js: resetStub walks own property names - sinon stubs of
    ES6 class methods are non-enumerable, so $.each() skipped them and the
    shared stub instance kept stale call history between tests.

--- packages/devextreme/js/__internal/viz/core/base_theme_manager.ts
+++ packages/devextreme/js/__internal/viz/core/base_theme_manager.ts
@@ -1,123 +1,154 @@
-/* eslint-disable no-return-assign */
-/* eslint-disable @typescript-eslint/no-this-alias */
-/* eslint-disable @typescript-eslint/naming-convention */
-/* eslint-disable no-multi-assign */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-/* eslint-disable @typescript-eslint/no-unused-expressions */
-
-import Class from '@js/core/class';
+import type { Font } from '@js/common/charts';
 import { extend } from '@js/core/utils/extend';
 import { each } from '@js/core/utils/iterator';
-import { isString as _isString } from '@js/core/utils/type';
-
+import { isString } from '@js/core/utils/type';
+import { parseScalar } from '@ts/viz/core/utils';
 import {
   createPalette as getPalette,
   getAccentColor as accentColor,
   getDiscretePalette,
   getGradientPalette,
-} from '../palette';
-import { addCacheItem, getTheme, removeCacheItem } from '../themes';
-import { parseScalar as _parseScalar } from './utils';
+} from '@ts/viz/palette';
+import { addCacheItem, getTheme, removeCacheItem } from '@ts/viz/themes';
 
-const _getTheme = getTheme;
-const _addCacheItem = addCacheItem;
-const _removeCacheItem = removeCacheItem;
-const _extend = extend;
-const _each = each;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any -- description theme option tree
+export type ThemeValue = any;
 
-function getThemePart(theme, path) {
-  let _theme = theme;
-  path && _each(path.split('.'), (_, pathItem) => (_theme = _theme[pathItem]));
-  return _theme;
+export interface Theme {
+  [key: string]: ThemeValue;
+  name?: string;
+  defaultPalette?: string | string[];
+  font?: Font;
+  rtlEnabled?: boolean;
+  _rtl?: Theme;
+}
+
+export type ThemeSetting = string | Theme;
+
+export type Palette = ReturnType<typeof getPalette>;
+
+export interface BaseThemeManagerOptions {
+  themeSection?: string;
+  fontFields?: string[];
+}
+
+function getThemePart(theme: ThemeValue, path?: string): ThemeValue {
+  let themePart = theme;
+  if (path) {
+    each(path.split('.'), (_, pathItem) => {
+      themePart = themePart[pathItem];
+      return themePart !== false;
+    });
+  }
+  return themePart;
 }
 
 // eslint-disable-next-line import/no-mutable-exports -- description seam for tests
-export let BaseThemeManager = Class.inherit({ // TODO: test hack
-  ctor(options) {
+export let BaseThemeManager = class BaseThemeManager {
+  _themeSection?: string;
+
+  _fontFields: string[];
+
+  _callback!: () => void;
+
+  _current?: ThemeSetting;
+
+  _rtl?: boolean;
+
+  _themeName?: string;
+
+  _defaultPalette?: string | string[];
+
+  _font?: Font;
+
+  _theme: ThemeValue;
+
+  constructor(options: BaseThemeManagerOptions) {
     this._themeSection = options.themeSection;
     this._fontFields = options.fontFields || [];
-    _addCacheItem(this);
-  },
+    addCacheItem(this);
+  }
 
-  dispose() {
-    const that = this;
-    _removeCacheItem(that);
-    that._callback = that._theme = that._font = null;
-    return that;
-  },
+  dispose(): this {
+    removeCacheItem(this);
+    Object.assign(this, { _font: null, _theme: null, _callback: null });
+    return this;
+  }
 
   // TODO: Move it to constructor when charts theme managers's constructor is removed
-  setCallback(callback) {
+  setCallback(callback: () => void): this {
     this._callback = callback;
     return this;
-  },
+  }
 
-  setTheme(theme, rtl) {
+  setTheme(theme?: ThemeSetting, rtl?: boolean): this {
     this._current = theme;
     this._rtl = rtl;
     return this.refresh();
-  },
+  }
 
   // Officially we do not support objects as "theme" option value - we should stop doing it in code
-  refresh() {
-    const that = this;
-    const current = that._current || {};
-    let theme = _getTheme(current.name || current);
-    that._themeName = theme.name;
-    that._defaultPalette = theme.defaultPalette;
-    that._font = _extend({}, theme.font, current.font);
-    that._themeSection && _each(that._themeSection.split('.'), (_, path) => {
-      theme = _extend(true, {}, theme[path]);
-    });
-    that._theme = _extend(true, {}, theme, _isString(current) ? {} : current);
-    that._initializeTheme();
-    if (_parseScalar(that._rtl, that._theme.rtlEnabled)) {
-      _extend(true, that._theme, that._theme._rtl);
+  refresh(): this {
+    const current: ThemeSetting = this._current || {};
+    const currentTheme: Theme = isString(current) ? {} : current;
+    let theme: Theme = getTheme(currentTheme.name || current);
+    this._themeName = theme.name;
+    this._defaultPalette = theme.defaultPalette;
+    this._font = extend({}, theme.font, currentTheme.font);
+    if (this._themeSection) {
+      each(this._themeSection.split('.'), (_, path) => {
+        theme = extend(true, {}, theme[path]);
+      });
     }
-    that._callback();
-    return that;
-  },
+    this._theme = extend(true, {}, theme, currentTheme);
+    this._initializeTheme();
+    if (parseScalar(this._rtl, this._theme.rtlEnabled)) {
+      extend(true, this._theme, this._theme._rtl);
+    }
+    this._callback();
+    return this;
+  }
 
-  theme(path) {
+  theme(path?: string): ThemeValue {
     return getThemePart(this._theme, path);
-  },
+  }
 
-  themeName() {
+  themeName(): string | undefined {
     return this._themeName;
-  },
+  }
 
   // TODO: May be we need some single method for all palettes?
 
-  createPalette(palette, options) {
+  createPalette(palette: ThemeValue, options: ThemeValue): Palette {
     return getPalette(palette, options, this._defaultPalette);
-  },
+  }
 
-  createDiscretePalette(palette, count) {
+  createDiscretePalette(palette: ThemeValue, count: number): ReturnType<typeof getDiscretePalette> {
     return getDiscretePalette(palette, count, this._defaultPalette);
-  },
+  }
 
-  createGradientPalette(palette) {
+  createGradientPalette(palette: ThemeValue): ReturnType<typeof getGradientPalette> {
     return getGradientPalette(palette, this._defaultPalette);
-  },
+  }
 
-  getAccentColor(palette) {
+  getAccentColor(palette: ThemeValue): ReturnType<typeof accentColor> {
     return accentColor(palette, this._defaultPalette);
-  },
+  }
 
-  _initializeTheme() {
-    const that = this;
-    _each(that._fontFields || [], (_, path) => {
-      that._initializeFont(getThemePart(that._theme, path));
+  _initializeTheme(): void {
+    each(this._fontFields || [], (_, path) => {
+      this._initializeFont(getThemePart(this._theme, path));
     });
-  },
+  }
 
-  _initializeFont(font) {
-    _extend(font, this._font, _extend({}, font));
-  },
-});
+  _initializeFont(font?: Font): void {
+    extend(font, this._font, extend({}, font));
+  }
+};
 
 /// #DEBUG
+/* eslint-disable-next-line @typescript-eslint/naming-convention
+  -- description seam setter for tests stubs */
 export function DEBUG_set_BaseThemeManager(value: typeof BaseThemeManager): void {
   BaseThemeManager = value;
 }
