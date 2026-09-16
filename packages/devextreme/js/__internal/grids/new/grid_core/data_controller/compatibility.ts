87f5a69200fdb39746e03ec1ebbb741f69148d59
Anna Shakhova <68295572+anna-shakhova@users.noreply.github.com>
2026-09-16 13:34:38 +0200

    CardView: answer hasAdapter from the dataSource option (#35236)

--- packages/devextreme/js/__internal/grids/new/grid_core/data_controller/compatibility.ts
+++ packages/devextreme/js/__internal/grids/new/grid_core/data_controller/compatibility.ts
@@ -2,15 +2,17 @@ import createCallback from '@js/core/utils/callbacks';
 import type DataSource from '@js/data/data_source';
 import { effect } from '@ts/core/state_manager/index';
 
+import { OptionsController } from '../options_controller/options_controller';
 import { DataController } from './data_controller';
 
 export class CompatibilityDataController {
   public dataSourceChanged = createCallback();
 
-  public static dependencies = [DataController] as const;
+  public static dependencies = [DataController, OptionsController] as const;
 
   constructor(
     private readonly realDataController: DataController,
+    private readonly options: OptionsController,
   ) {
     effect(() => {
       this.dataSourceChanged.fire(
@@ -19,7 +21,9 @@ export class CompatibilityDataController {
     });
   }
 
-  public dataSource(): DataSource {
-    return this.realDataController.dataSource.peek();
+  public dataSource(): DataSource | undefined {
+    return this.options.oneWay('dataSource').peek()
+      ? this.realDataController.dataSource.peek()
+      : undefined;
   }
 }
