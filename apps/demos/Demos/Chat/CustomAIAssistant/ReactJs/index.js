41fe65a9893f085bdb79bd81240196ab7ab13383
DuckTieCorpMember <assylbek.danyshbek@devexpress.com>
2026-09-18 11:25:35 +0500

    remove react js

--- apps/demos/Demos/Chat/CustomAIAssistant/ReactJs/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './src/App.js';
-
-ReactDOM.render(<App />, document.getElementById('app'));
--- apps/demos/Demos/Chat/CustomAIAssistant/ReactJs/src/types/types.js
+++
@@ -1,2 +0,0 @@
-export const ROUTER_TARGETS = new Set(['form', 'grid', 'mixed', 'none']);
-export const FORM_ACTION_TYPES = new Set(['clear_field', 'clear_all', 'smart_paste']);
