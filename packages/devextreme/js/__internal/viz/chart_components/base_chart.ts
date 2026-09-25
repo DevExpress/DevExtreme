d1e6cb6f21d067d8e5d39e9071a49cec5d9156f1
EugeniyKiyashko <EugeniyKiyashko@users.noreply.github.com>
2026-09-25 15:38:45 +0400

    Charts: drop the comment

--- packages/devextreme/js/__internal/viz/chart_components/base_chart.ts
+++ packages/devextreme/js/__internal/viz/chart_components/base_chart.ts
@@ -1408,7 +1408,6 @@ export class BaseChart extends BaseWidget {
 
   _change_FULL_RENDER() {
     this._forceRender();
-    // a visualRange change requested from now on (e.g. a canceled zoomEnd) has to render itself
     this._changes.remove('FULL_RENDER');
   }
 
